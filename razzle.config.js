3a73340501aef5d489fc41644b15a0f62c6d99b2
Sareh Heidari <sareh.heidari@gmail.com>
2018-05-21 23:18:50 +0100

    Adding babel-loader & babel-eslint

---
+++ razzle.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      }
+    ]
+  }
+}
