9ddeb47a9e6de1d114e886733ffb9ad4e9a38b98
Sareh Heidari <sareh.heidari@gmail.com>
2018-05-23 23:23:37 +0100

    Added routes tests

---
+++ src/app/routes.test.js
@@ -0,0 +1,15 @@
+import routes from './routes';
+
+describe('Routes', () => {
+  test('It should be an array', () => {
+    expect(routes).toEqual(expect.any(Array));
+  });
+
+  test('All routes should have path and component fields', () => {
+    routes.forEach((route) => {
+      expect(route).toEqual(expect.any(Object));
+      expect(route).toHaveProperty('path');
+      expect(route).toHaveProperty('component');
+    });
+  });
+});
