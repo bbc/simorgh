628acc48a39ad8ad77c05acac319680dbccb990a
Sareh <sareh.heidari@gmail.com>
2018-05-24 15:17:57 +0100

    Initial setup (#1)
    * Initial application setup with Razzle, After, React Router, ESLint, PropTypes, Jest & Enzyme.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Articles Renderer
+
+## Installation
+
+Install Node 8. [https://nodejs.org/en/](https://nodejs.org/en/)
+Update to use the latest npm `npm i -g npm`
+
+```
+git clone git@github.com:bbc/articles-renderer.git
+npm install
+```
+
+To run this application locally, run this
+```
+npm start
+```
+It will open the application on [http://localhost:3000](http://localhost:3000)
+
+## Tests 
+```
+npm test
+```
+
---
+++ src/server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { render } from '@jaredpalmer/after';
+import routes from '../app/routes';
+
+/* eslint-disable import/no-dynamic-require */
+const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
+
+const server = express();
+server
+  .disable('x-powered-by')
+  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
+  .get('/*', async (req, res) => {
+    try {
+      const html = await render({
+        req,
+        res,
+        routes,
+        assets,
+      });
+      res.send(html);
+    } catch (error) {
+      res.json(error);
+    }
+  });
+
+export default server;
