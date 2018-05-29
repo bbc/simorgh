13e7241d5b01d3ab00e1cef519e5b593993befcc
Sareh Heidari <sareh.heidari@gmail.com>
2018-05-29 12:07:43 +0100

    Setup for automatically showing bundle size at http://127.0.0.1:8888/

--- razzle.config.js
+++ razzle.config.js
@@ -1,7 +1,19 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-  plugins: [
-    new BundleAnalyzerPlugin()
-  ]
-}
+  modify(config, { target, dev }, webpack) {
+    const appConfig = config; // stay immutable here
+
+    if (target === 'node' && dev) {
+      appConfig.plugins = [
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'server',
+          reportFilename: 'webpackBundleReport.html',
+          statsFilename: 'webpackBundleStats.json',
+        }),
+      ];
+    }
+
+    return appConfig;
+  },
+};
