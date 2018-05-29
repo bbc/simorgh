bd8f5a40cf8f90ba4f11f04ab5d84a9e014cfe6a
Sareh Heidari <sareh.heidari@gmail.com>
2018-05-29 13:58:16 +0100

    Return a 200 response for /status

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,17 +1,11 @@
 import Article from './components/Article';
-import Status from './components/Status';
 
 const routes = [
   {
     path: '/',
     exact: true,
     component: Article,
-  },
-  {
-    path: '/status',
-    exact: true,
-    component: Status,
-  },
+  }
 ];
 
 export default routes;
