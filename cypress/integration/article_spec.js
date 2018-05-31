45ba8c0c9f7e39819055450c3f46ced64f3c6226
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-05-31 12:45:58 +0100

    use newer syntax

--- cypress/integration/article_spec.js
+++ cypress/integration/article_spec.js
@@ -1,22 +1,21 @@
-describe('News Article', function() {
-var url = 'http://localhost:3000/';
+describe('News Article', () => {
+const url = 'http://localhost:3000/';
 
-    it('should render a headline', function(){
+    it('should display 200/OK', () => {
+        cy.request(url + 'status').then((response) => {
+            expect(response.status).to.eq(200)
+        })
+    })
+
+    it('should render a headline', () => {
         cy.visit(url)
         cy.url().should('equal', url)
         cy.get('h1').should('contain','Article Headline')
     })
 
-    it('should render a title', function(){
+    it('should render a title', () => {
     	cy.visit(url)
     	cy.title().should('eq', 'Welcome to the Afterparty')
 
-    })
-
-    it('should display 200/OK', function(){
-    	cy.request(url + 'status').then((response) => {
-    		expect(response.status).to.eq(200)		
-    	})
-    	
     })
 })
