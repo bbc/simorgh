09bd9ce680c93d7f4bea0dc1ab207e2dd90a360e
Benjamin Hobbs <bjhobbs06@gmail.com>
2018-06-05 18:37:17 +0100

    Add storybook without custom webpack config

---
+++ .storybook/config.js
@@ -0,0 +1,8 @@
+import { configure } from '@storybook/react';
+
+function loadStories() {
+//   require('../stories/index.js');
+  // You can require as many stories as you need, just edit and uncomment the line above.
+}
+
+configure(loadStories, module);
