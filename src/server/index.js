2a4acc1225558760097175dd825ee3878b7f0d36
Jordan Tart <jordan.tart@bbc.co.uk>
2018-06-05 15:29:48 +0100

    Use single-quotes

--- src/server/index.js
+++ src/server/index.js
@@ -9,7 +9,7 @@ const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 const server = express();
 server
   .disable('x-powered-by')
-  .use("/data", express.static('data'))
+  .use('/data', express.static('data'))
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get('/status', (req, res) => {
     res.sendStatus(200);
