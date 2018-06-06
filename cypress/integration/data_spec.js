7573e71feeb3bd2a3f428956a1b9412bf28e535c
Jordan Tart <jordan.tart@bbc.co.uk>
2018-06-06 10:58:40 +0100

    Extract duplicated code

--- cypress/integration/data_spec.js
+++ cypress/integration/data_spec.js
@@ -1,21 +1,25 @@
 describe('Static Articles data', () => {
-  it('should return a status code of 200', () => {
-    cy.request('/data/scenario-01.json').then(({ status }) => {
-      expect(status).to.eq(200);
+  const testScenarioRequest = (testTitle, scenarioNumber, testAssertion) => {
+    it(testTitle, () => {
+      cy.request(`/data/scenario-${scenarioNumber}.json`).then(testAssertion);
     });
-  });
+  };
+
+  testScenarioRequest(
+    'should return a status code of 200',
+    '01',
+    ({ status }) => {
+      expect(status).to.eq(200);
+    },
+  );
 
   describe('Response Body', () => {
-    it('should be an object', () => {
-      cy.request('/data/scenario-01.json').then(({ body }) => {
-        expect(body).to.be.an('object');
-      });
+    testScenarioRequest('should be an object', '01', ({ body }) => {
+      expect(body).to.be.an('object');
     });
 
-    it('should contain a blocks object', () => {
-      cy.request('/data/scenario-01.json').then(({ body }) => {
-        expect(body).to.have.property('blocks');
-      });
+    testScenarioRequest('should contain a blocks object', '01', ({ body }) => {
+      expect(body).to.have.property('blocks');
     });
   });
 });
