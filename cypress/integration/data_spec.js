b8685aac7c504a0612a980e893c866ec7e0e25a0
Jordan Tart <jordan.tart@bbc.co.uk>
2018-06-06 09:41:24 +0100

    Tidy

--- cypress/integration/data_spec.js
+++ cypress/integration/data_spec.js
@@ -1,15 +1,13 @@
 describe('Static Articles data', () => {
-
   it('should display 200/OK', () => {
     cy.request('/data/scenario-01.json').then(({ status }) => {
-        expect(status).to.eq(200)
-
-  })
-})
+      expect(status).to.eq(200);
+    });
+  });
 
   it('should output valid JSON', () => {
-      cy.request('/data/scenario-01.json').then(({ body}) => {
-          expect(body).to.be.an("object")
-      })
-  })
-})
+    cy.request('/data/scenario-01.json').then(({ body }) => {
+      expect(body).to.be.an('object');
+    });
+  });
+});
