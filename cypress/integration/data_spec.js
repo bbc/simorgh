4aa1cd3eee30fd5630ac6c5e2b9cb2f6151feb20
Jordan Tart <jordan.tart@bbc.co.uk>
2018-06-06 09:49:23 +0100

    Wrap body tests in describe block

--- cypress/integration/data_spec.js
+++ cypress/integration/data_spec.js
@@ -5,9 +5,11 @@ describe('Static Articles data', () => {
     });
   });
 
-  it('should output valid JSON', () => {
-    cy.request('/data/scenario-01.json').then(({ body }) => {
-      expect(body).to.be.an('object');
+  describe('Response Body', () => {
+    it('should be an object', () => {
+      cy.request('/data/scenario-01.json').then(({ body }) => {
+        expect(body).to.be.an('object');
+      });
     });
   });
 });
