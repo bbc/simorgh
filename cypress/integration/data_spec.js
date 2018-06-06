42813c2a30af6205dde1232278dae49e031bf854
Jordan Tart <jordan.tart@bbc.co.uk>
2018-06-06 13:02:32 +0100

    Refactor to request data in beforeEach step

--- cypress/integration/data_spec.js
+++ cypress/integration/data_spec.js
@@ -1,21 +1,31 @@
 describe('Static Articles data', () => {
-  const testScenarioRequest = (testTitle, testAssertion) => {
-    it(testTitle, () => {
-      cy.request(`/data/scenario-01.json`).then(testAssertion);
+  let responseStatus;
+  let responseBody;
+
+  const testScenarioResponse = (title, assertion) => {
+    it(title, () => {
+      assertion();
     });
   };
 
-  testScenarioRequest('should return a 200 status code', ({ status }) => {
-    expect(status).to.eq(200);
+  beforeEach(() => {
+    cy.request(`/data/scenario-01.json`).then(({ status, body }) => {
+      responseStatus = status;
+      responseBody = body;
+    });
+  });
+
+  testScenarioResponse('should return a 200 status code', () => {
+    expect(responseStatus).to.eq(200);
   });
 
   describe('Response Body', () => {
-    testScenarioRequest('should be an object', ({ body }) => {
-      expect(body).to.be.an('object');
+    testScenarioResponse('should be an object', () => {
+      expect(responseBody).to.be.an('object');
     });
 
-    testScenarioRequest('should contain a blocks object', ({ body }) => {
-      expect(body).to.have.property('blocks');
+    testScenarioResponse('should contain a blocks object', () => {
+      expect(responseBody).to.have.property('blocks');
     });
   });
 });
