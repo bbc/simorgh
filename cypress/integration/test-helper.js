6cd648569a9953aa75fc2daed20327f439117a2f
James Brumpton <james.brumpton@bbc.co.uk>
2018-06-06 16:54:52 +0100

    Refactor e2es

--- cypress/integration/test-helper.js
+++ cypress/integration/test-helper.js
@@ -1,13 +1,5 @@
 export const testRequestResponse = (uri, responseCode) => {
-	cy.request(uri).then((response) => {
-         expect(response.status).to.eq(responseCode)
-
-
-
+	cy.request(uri).then(({ status }) => {
+         expect(status).to.eq(responseCode)
     })
 }
-
-
-    
-        
-  
