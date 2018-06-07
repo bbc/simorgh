ed75d2807feba78034223ad7c73e9c9d5708aa0f
Jordan Tart <jordan.tart@bbc.co.uk>
2018-06-07 11:42:40 +0100

    Add client + server getInitialProps tests

--- src/app/components/Article.test.jsx
+++ src/app/components/Article.test.jsx
@@ -18,4 +18,40 @@ const testElementTextValue = (testTitle, element, value) => {
 describe('Article', () => {
   testElementTextValue('renders the headline in an h1', 'h1', HEADLINE);
   testElementTextValue('renders the title', 'title', HEADLINE);
+
+  describe('getInitialProps', () => {
+    const callGetInitialProps = async context => {
+      fetch.mockResponseOnce(JSON.stringify({ data: '12345' }));
+      const response = await Article.getInitialProps(context);
+      return response;
+    };
+
+    const testGetInitialProps = (testTitle, testAssertion, context = {}) => {
+      it(testTitle, async () => {
+        callGetInitialProps(context);
+        testAssertion();
+      });
+    };
+
+    beforeEach(() => {
+      fetch.resetMocks();
+    });
+
+    describe('On client', () => {
+      testGetInitialProps('should call fetch with a relative URL', () => {
+        expect(fetch.mock.calls[0][0]).toEqual('/data/scenario-01.json');
+      });
+    });
+
+    describe('On Server', () => {
+      const context = { req: { exists: true } };
+      testGetInitialProps(
+        'should call fetch with an absolute URL',
+        () => {
+          expect(fetch.mock.calls[0][0]).toEqual('http://localhost:7080/data/scenario-01.json');
+        },
+        context,
+      );
+    });
+  });
 });
