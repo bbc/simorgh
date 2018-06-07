c35518344191c5afde7e2b072613572dff99b1f9
Jordan Tart <jordan.tart@bbc.co.uk>
2018-06-07 11:53:53 +0100

    Refactor test to remove duplication

--- src/app/components/Article.test.jsx
+++ src/app/components/Article.test.jsx
@@ -2,22 +2,29 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Article from './Article';
 
-const HEADLINE = 'Article Headline';
-
-const expectElementTextToEqual = (element, value) => {
-  const component = shallow(<Article />);
-  expect(component.find(element).text()).toEqual(value);
-};
-
-const testElementTextValue = (testTitle, element, value) => {
-  it(testTitle, () => {
-    expectElementTextToEqual(element, value);
-  });
-};
-
 describe('Article', () => {
-  testElementTextValue('renders the headline in an h1', 'h1', HEADLINE);
-  testElementTextValue('renders the title', 'title', HEADLINE);
+  const testWrapper = (testTitle, testAssertion) => {
+    it(testTitle, async () => {
+      testAssertion();
+    });
+  };
+
+  describe('Component', () => {
+    const HEADLINE = 'Article Headline';
+
+    const expectElementTextToEqual = (element, value) => {
+      const component = shallow(<Article />);
+      expect(component.find(element).text()).toEqual(value);
+    };
+
+    testWrapper('renders the headline in an h1', () => {
+      expectElementTextToEqual('h1', HEADLINE);
+    });
+
+    testWrapper('renders the title', () => {
+      expectElementTextToEqual('title', HEADLINE);
+    });
+  });
 
   describe('getInitialProps', () => {
     const callGetInitialProps = async context => {
@@ -26,32 +33,25 @@ describe('Article', () => {
       return response;
     };
 
-    const testGetInitialProps = (testTitle, testAssertion, context = {}) => {
-      it(testTitle, async () => {
-        callGetInitialProps(context);
-        testAssertion();
-      });
-    };
-
     beforeEach(() => {
       fetch.resetMocks();
     });
 
     describe('On client', () => {
-      testGetInitialProps('should call fetch with a relative URL', () => {
+      testWrapper('should call fetch with a relative URL', () => {
+        callGetInitialProps();
         expect(fetch.mock.calls[0][0]).toEqual('/data/scenario-01.json');
       });
     });
 
     describe('On Server', () => {
       const context = { req: { exists: true } };
-      testGetInitialProps(
-        'should call fetch with an absolute URL',
-        () => {
-          expect(fetch.mock.calls[0][0]).toEqual('http://localhost:7080/data/scenario-01.json');
-        },
-        context,
-      );
+      testWrapper('should call fetch with an absolute URL', () => {
+        callGetInitialProps(context);
+        expect(fetch.mock.calls[0][0]).toEqual(
+          'http://localhost:7080/data/scenario-01.json',
+        );
+      });
     });
   });
 });
