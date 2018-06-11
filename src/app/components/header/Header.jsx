4bcfd26f929ce0b7d970d9a098cbd7d136ed7009
Jonthan Mark Perry <perryj39@mc-s090640.national.core.bbc.co.uk>
2018-06-11 15:32:16 +0100

    Re-ordered styles to match their placement in the DOM

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -1,9 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
 
-const BBCNewsBranding = styled.div`
-  background-color: #bb1919;
-  height: 58px;
+const BBCNewsLink = styled.a`
+  font-family: ReithSans, Arial, Helvetica, freesans, sans-serif;
+  font-size: 2.4em;
+  color: #fff;
+  text-transform: uppercase;
+`;
+
+const BBCNewsNavigation = styled.div`
   width: 100%;
 `;
 
@@ -12,16 +17,12 @@ const BBCNewsBrandingLogo = styled.div`
   padding: 10px 16px 10px 16px;
 `;
 
-const BBCNewsNavigation = styled.div`
+const BBCNewsBranding = styled.div`
+  background-color: #bb1919;
+  height: 58px;
   width: 100%;
 `;
 
-const BBCNewsLink = styled.a`
-  font-family: ReithSans, Arial, Helvetica, freesans, sans-serif;
-  font-size: 2.4em;
-  color: #fff;
-  text-transform: uppercase;
-`;
 
 class Header extends Component {
   state = {
