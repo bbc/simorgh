d6ce40114e70f8d5588c81595e2af5f9581b8a1d
Paul Chinkwende <paul.chinkwende@bbc.co.uk>
2018-06-11 14:20:05 +0100

    only run best practice audit

--- custom-config.js
+++ custom-config.js
@@ -1,12 +1,7 @@
 module.exports = {
   extends: 'lighthouse:default',
   settings: {
-    onlyAudits: [
-      'first-meaningful-paint',
-      'speed-index-metric',
-      'estimated-input-latency',
-      'first-interactive',
-      'consistently-interactive',
-    ],
+    onlyCategories: ['best-practices'],
+    onlyAudits: [],
   },
 };
