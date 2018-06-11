9dfaecca2b58bc78088c0fa34d8317d6960491e3
Jonthan Mark Perry <perryj39@mc-s080830.local>
2018-06-11 15:56:31 +0100

    Formatted documents, updated broken path for story and updated snapshot tests

--- src/app/components/header/Header.stories.jsx
+++ src/app/components/header/Header.stories.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
-import Header from './header/Header';
+import Header from './Header';
 
 storiesOf('Header', module).add('default', () => <Header />);
