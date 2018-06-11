b849846af3403cc11c40f918ea4fffdca75fe070
Jordan Tart <jordan.tart@bbc.co.uk>
2018-06-11 16:14:34 +0100

    Move public dir test to helper func

--- src/server/config.test.js
+++ src/server/config.test.js
@@ -5,18 +5,27 @@ describe('Config', () => {
     process.env.RAZZLE_PUBLIC_DIR = 'test-public';
     process.env.RAZZLE_PUBLIC_DIR_DEV = `test-public-dev`;
 
-    describe('Production build', () => {
-      it('should equal the RAZZLE_PUBLIC_DIR environment variable', async () => {
-        process.env.NODE_ENV = 'production';
-        expect(getPublicDirectory()).toEqual(process.env.RAZZLE_PUBLIC_DIR);
+    const testOutput = (testTitle, environment, expectedOutput) => {
+      it(testTitle, () => {
+        process.env.NODE_ENV = environment;
+        expect(getPublicDirectory()).toEqual(expectedOutput);
       });
+    };
+
+    describe('Production build', () => {
+      testOutput(
+        'should equal the RAZZLE_PUBLIC_DIR environment variable',
+        'production',
+        process.env.RAZZLE_PUBLIC_DIR,
+      );
     });
 
     describe('Development build', () => {
-      it('should equal the RAZZLE_PUBLIC_DIR_DEV environment variable', async () => {
-        process.env.NODE_ENV = 'development';
-        expect(getPublicDirectory()).toEqual(process.env.RAZZLE_PUBLIC_DIR_DEV);
-      });
+      testOutput(
+        'should equal the RAZZLE_PUBLIC_DIR_DEV environment variable',
+        'development',
+        process.env.RAZZLE_PUBLIC_DIR_DEV,
+      );
     });
   });
 });
