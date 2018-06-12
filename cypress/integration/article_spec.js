b34e1e9c481961757df4b85071f2016f2eef2233
Jonthan Mark Perry <perryj39@mc-s080830.local>
2018-06-12 17:20:19 +0100

    Removed unused anchor tag from styled-components

--- cypress/integration/article_spec.js
+++ cypress/integration/article_spec.js
@@ -7,7 +7,7 @@ describe('News Article', () => {
     })
 
     it('should render the BBC News branding', () => {
-        cy.get('header a').should('contain', 'BBC News')
+        cy.get('header').should('contain', 'BBC News')
     })
 
     it('should render a headline', () => {
