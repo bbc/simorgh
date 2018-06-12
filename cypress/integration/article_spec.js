5611f1cbe3142b41205567154afb3e6fd16eae87
Jonthan Mark Perry <perryj39@mc-s080830.local>
2018-06-12 14:16:42 +0100

    Implemented integration test checking for rendering BBC News in the header

--- cypress/integration/article_spec.js
+++ cypress/integration/article_spec.js
@@ -6,8 +6,8 @@ describe('News Article', () => {
         cy.visit('/')
     })
 
-    it('should render the BBC News branding', () => {
-        cy.get('header a').should('contain', 'BBC News')
+    it('should render the BBC News branding', () =>{
+        cy.get('header').should('contain', 'BBC News')
     })
 
     it('should render a headline', () => {
