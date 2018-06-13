d8430995cc79abea73c880ec8473a4406e2b2dd9
Sareh Heidari <sareh.heidari@gmail.com>
2018-06-13 11:34:27 +0100

    Simplify data test using test-helper

--- cypress/integration/data_spec.js
+++ cypress/integration/data_spec.js
@@ -1,27 +1,9 @@
+import { testResponseCode } from './test-helper';
+
 describe('Static Articles data', () => {
-  let responseStatus;
-  let responseBody;
-
-  const testScenarioResponse = (title, assertion) => {
-    it(title, () => {
-      assertion();
-    });
-  };
-
-  beforeEach(() => {
-    cy.request(`/data/test/scenario-01.json`).then(({ status, body }) => {
-      responseStatus = status;
-      responseBody = body;
-    });
-  });
-
-  testScenarioResponse('should return a 200 status code', () => {
-    expect(responseStatus).to.eq(200);
-  });
-
-  describe('Response Body', () => {
-    testScenarioResponse('should be an object', () => {
-      expect(responseBody).to.be.an('object');
-    });
+  describe('Page Status', () => {
+      it('should display 200', () => {
+        testResponseCode('/data/test/scenario-01.json', 200)
+      })
   });
 });
