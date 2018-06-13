4cb05ccb8b318b6eb5dd66ceb4e26ccd2cc5a150
Sareh Heidari <sareh.heidari@gmail.com>
2018-06-13 11:43:22 +0100

    Simplify test & add accurate test description

--- cypress/integration/data_spec.js
+++ cypress/integration/data_spec.js
@@ -1,9 +1,7 @@
 import { testResponseCode } from './test-helper';
 
 describe('Static Articles data', () => {
-  describe('Page Status', () => {
-      it('should display 200', () => {
-        testResponseCode('/data/test/scenario-01.json', 200)
-      })
+  it('should return a 200 status code', () => {
+    testResponseCode('/data/test/scenario-01.json', 200)
   });
 });
