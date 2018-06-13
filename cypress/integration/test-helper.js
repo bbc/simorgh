4a0756ea809ae77bb5f35793e3e95aa6a024736c
James Brumpton <james.brumpton@bbc.co.uk>
2018-06-13 14:12:34 +0100

    Refined the refactor

--- cypress/integration/test-helper.js
+++ cypress/integration/test-helper.js
@@ -1,5 +1,20 @@
-export const testResponseCode = (path, responseCode) => {
+const testResponseCode = (path, responseCode) => {
 	cy.request(path).then(({ status }) => {
          expect(status).to.eq(responseCode)
     })
+}
+
+const getElement = (element) => cy.get(element)
+
+const shouldContainText = (element, text) => {
+	element.should('contain', text)
+}
+const shouldContainStyles = (element, css, styling) => {
+	element.should((el) => {
+		expect(el).to.have.css(css, styling)
+	})
+}
+
+export default {
+	testResponseCode, shouldContainText, shouldContainStyles, getElement
 }
