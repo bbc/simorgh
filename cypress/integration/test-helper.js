4ce8f137d0f2b6fe04e13fb93f6eceaeebce8e23
Jonthan Mark Perry <perryj39@mc-s080830.local>
2018-06-14 12:24:11 +0100

    Integration tests

--- cypress/integration/test-helper.js
+++ cypress/integration/test-helper.js
@@ -4,6 +4,21 @@ const testResponseCode = (path, responseCode) => {
   });
 };
 
+const getElement = element => cy.get(element);
+
+const shouldContainText = (element, text) => {
+  element.should('contain', text);
+};
+
+const shouldContainStyles = (element, css, styling) => {
+  element.should(el => {
+    expect(el).to.have.css(css, styling);
+  });
+};
+
 export default {
   testResponseCode,
+  shouldContainText,
+  shouldContainStyles,
+  getElement,
 };
