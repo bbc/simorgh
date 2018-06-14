0628ccddafe21baf2155cdde0640751bc978a940
Jonthan Mark Perry <perryj39@mc-s080830.local>
2018-06-14 11:41:14 +0100

    Resolved conflicting linting issues

--- cypress/integration/article_spec.js
+++ cypress/integration/article_spec.js
@@ -1,39 +1,47 @@
-import { testResponseCode, getElement, shouldContainText, shouldContainStyles } from './test-helper';
+import {
+  testResponseCode,
+  getElement,
+  shouldContainText,
+  shouldContainStyles,
+} from './test-helper';
 
 describe('News Article', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
-    beforeEach(() => {
-        cy.visit('/')
-    })
+  it('should render the BBC News branding', () => {
+    const headerElement = getElement('header');
+    shouldContainStyles(headerElement, 'height', '40px');
+    shouldContainStyles(headerElement, 'background-color', 'rgb(187, 25, 25)');
 
-    it('should render the BBC News branding', () => {
-        const headerElement = getElement('header')
-        shouldContainStyles(headerElement, 'height', '40px')
-        shouldContainStyles(headerElement, 'background-color', 'rgb(187, 25, 25)')
-        
-        const anchorElement = getElement('header a')
-        shouldContainText(anchorElement, 'BBC News')
-        shouldContainStyles(anchorElement, 'color', 'rgb(255, 255, 255)')
-        shouldContainStyles(anchorElement, 'font-family', 'ReithSans, Arial, Helvetica, freesans, sans-serif')
-    })
+    const anchorElement = getElement('header a');
+    shouldContainText(anchorElement, 'BBC News');
+    shouldContainStyles(anchorElement, 'color', 'rgb(255, 255, 255)');
+    shouldContainStyles(
+      anchorElement,
+      'font-family',
+      'ReithSans, Arial, Helvetica, freesans, sans-serif',
+    );
+  });
 
-    it('should render a headline', () => {
-        cy.get('h1').should('contain','Article Headline')
-    })
+  it('should render a headline', () => {
+    cy.get('h1').should('contain', 'Article Headline');
+  });
 
-    it('should render a title', () => {
-    	cy.title().should('eq', 'Article Headline')
-    })
-})
+  it('should render a title', () => {
+    cy.title().should('eq', 'Article Headline');
+  });
+});
 
 describe('Page Status', () => {
-    it('should display 200', () => {
-      testResponseCode('/', 200)  
-    })
-})
+  it('should display 200', () => {
+    testResponseCode('/', 200);
+  });
+});
 
 describe('Renderer Status', () => {
-    it('should display 200', () => {
-      testResponseCode('/status', 200)  
-    })
-})
+  it('should display 200', () => {
+    testResponseCode('/status', 200);
+  });
+});
--- cypress/integration/test-helper.js
+++ cypress/integration/test-helper.js
@@ -1,20 +1,24 @@
 const testResponseCode = (path, responseCode) => {
-	cy.request(path).then(({ status }) => {
-         expect(status).to.eq(responseCode)
-    })
-}
+  cy.request(path).then(({ status }) => {
+    expect(status).to.eq(responseCode);
+  });
+};
 
-const getElement = (element) => cy.get(element)
+const getElement = element => cy.get(element);
 
 const shouldContainText = (element, text) => {
-	element.should('contain', text)
-}
+  element.should('contain', text);
+};
+
 const shouldContainStyles = (element, css, styling) => {
-	element.should((el) => {
-		expect(el).to.have.css(css, styling)
-	})
-}
+  element.should(el => {
+    expect(el).to.have.css(css, styling);
+  });
+};
 
 export default {
-	testResponseCode, shouldContainText, shouldContainStyles, getElement
-}
+  testResponseCode,
+  shouldContainText,
+  shouldContainStyles,
+  getElement,
+};
