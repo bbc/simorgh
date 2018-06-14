7286c3be2abc2ca925316fe019402948b63e21f0
Jonthan Mark Perry <perryj39@mc-s080830.local>
2018-06-14 15:33:18 +0100

    Updated test case

--- src/app/components/Footer/index.test.jsx
+++ src/app/components/Footer/index.test.jsx
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import Footer from './index';
 
 describe('Footer', () => {
-  it('renders the component', () => {
+  it('should render correctly', () => {
     const tree = renderer.create(<Footer />);
     expect(tree).toMatchSnapshot();
   });
