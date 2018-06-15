a383d3b7fed0707d90c7fff9a2fa0d2ab718cf5b
Jordan Tart <jordan.tart@bbc.co.uk>
2018-06-15 10:12:55 +0100

    Inline Headline to reduce complexity

--- src/app/components/Headline/index.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Headline = styled.h1`
-  color: #222;
-  font-family: ReithSans, Arial, Helvetica, freesans, sans-serif;
-  font-size: 2em;
-`;
-
-export default Headline;
--- src/app/components/MainContent/index.jsx
+++ src/app/components/MainContent/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import Headline from '../Headline';
+import styled from 'styled-components';
+
+const Headline = styled.h1`
+  color: #222;
+  font-family: ReithSans, Arial, Helvetica, freesans, sans-serif;
+  font-size: 2em;
+`;
 
 const render = ({ blocks }) =>
   blocks.map(block => {
