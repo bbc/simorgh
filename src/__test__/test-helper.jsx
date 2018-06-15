7a879a61dba9c20f390284082fded57c3997e68a
Jonthan Mark Perry <perryj39@mc-s080830.local>
2018-06-15 11:11:31 +0100

    Added test suite to test helper

--- src/__test__/test-helper.jsx
+++ src/__test__/test-helper.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 const shouldMatchSnapshot = (Name, Component) => {
-  test(`"${Name}" should render correctly`, () => {
-    const root = renderer.create(<Component />);
-    expect(root).toMatchSnapshot();
+  describe(`"${Name}"`, () => {
+    test(`should render correctly`, () => {
+      const root = renderer.create(<Component />);
+      expect(root).toMatchSnapshot();
+    });
   });
 };
 
