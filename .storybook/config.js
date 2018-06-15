cb5a806a4ff2250cf60252f13a2c023963ccd2eb
Benjamin Hobbs <bjhobbs06@gmail.com>
2018-06-15 13:32:13 +0100

    Correct linting

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,8 @@
 import { configure } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
 
-const req = require.context('../src/app/components', true, /\.stories\.jsx$/)
+const req = require.context('../src/app/components', true, /\.stories\.jsx$/);
 
 function loadStories() {
-  req.keys().forEach((filename) => req(filename))
+  req.keys().forEach(filename => req(filename));
 }
 configure(loadStories, module);
