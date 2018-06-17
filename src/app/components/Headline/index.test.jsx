747ae134984f18fb2b6f0511a5c40e6d6e72546a
Jordan Tart <jordan.tart@bbc.co.uk>
2018-06-17 18:31:00 +0100

    Add tests

---
+++ src/app/components/Headline/index.test.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Headline from './index';
+
+describe('Headline', () => {
+  describe('with no data', () => {
+    it('should not render anything', () => {
+      const tree = renderer.create(<Headline />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe('with data', () => {
+    const data = {
+      model: {
+        blocks: [
+          {
+            model: {
+              blocks: [
+                {
+                  model: {
+                    text: 'This is a headline!',
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    };
+
+    it('should render correctly', () => {
+      const tree = renderer.create(<Headline {...data} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+});
