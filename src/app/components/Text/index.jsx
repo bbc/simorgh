c7ca7b905027a82ef0b04833cfae56f62c43453a
Jordan Tart <jordan.tart@bbc.co.uk>
2018-06-17 16:45:48 +0100

    Use blockId as key

--- src/app/components/Text/index.jsx
+++ src/app/components/Text/index.jsx
@@ -4,7 +4,9 @@ import propTypes from 'prop-types';
 const Text = ({ model }) => {
   const { blocks } = model;
 
-  blocks.map(({ model: blockModel }) => <p>{blockModel.text}</p>);
+  blocks.map(({ blockId, model: blockModel }) => (
+    <p key={blockId}>{blockModel.text}</p>
+  ));
 };
 
 Text.propTypes = {
