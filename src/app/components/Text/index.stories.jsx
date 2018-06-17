2b9fbd56f1f5b4c94afe6eab04489ef05259f03c
Jordan Tart <jordan.tart@bbc.co.uk>
2018-06-17 16:47:26 +0100

    Add text story

---
+++ src/app/components/Text/index.stories.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
+import Text from './index';
+
+const paragraphBlock = (blockId, text) => ({
+  blockId,
+  model: {
+    text,
+  },
+});
+
+const props = {
+  model: {
+    blocks: [
+      paragraphBlock('01', 'This is a text block.'),
+      paragraphBlock('02', 'This is another text block.'),
+      paragraphBlock('03', 'This is the final text block.'),
+    ],
+  },
+};
+
+storiesOf('Text', module).add('default', () => <Text {...props} />);
