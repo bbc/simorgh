db635a6be4431d6630394984c3e33899d80ecdd3
Jordan Tart <jordan.tart@bbc.co.uk>
2018-06-17 18:28:24 +0100

    Add prop types

--- src/app/components/Headline/index.jsx
+++ src/app/components/Headline/index.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 const Headline = ({ model }) => {
   const { text } = model.blocks[0].model.blocks[0].model;
 
   return <h1>{text}</h1>;
+};
+
+Headline.propTypes = {
+  model: propTypes.shape({
+    blocks: propTypes.arrayOf(
+      propTypes.shape({
+        model: propTypes.shape({
+          blocks: propTypes.arrayOf(
+            propTypes.shape({
+              text: propTypes.string,
+            }),
+          ),
+        }),
+      }),
+    ),
+  }),
+};
+
+Headline.defaultProps = {
+  model: {
+    blocks: [
+      {
+        model: {
+          blocks: [
+            {
+              model: {},
+            },
+          ],
+        },
+      },
+    ],
+  },
 };
 
 export default Headline;
