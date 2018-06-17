c970ded862a9641e72ddca650f92ca79e93bf4b2
Jordan Tart <jordan.tart@bbc.co.uk>
2018-06-17 16:41:09 +0100

    Add prop types

--- src/app/components/Text/index.jsx
+++ src/app/components/Text/index.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 const Text = ({ model }) => {
   const { blocks } = model;
 
   blocks.map(({ model: blockModel }) => <p>{blockModel.text}</p>);
+};
+
+Text.propTypes = {
+  model: propTypes.shape({
+    blocks: propTypes.arrayOf(
+      propTypes.shape({
+        model: propTypes.shape({
+          text: propTypes.string,
+        }),
+      }),
+    ),
+  }),
+};
+
+Text.defaultProps = {
+  model: {
+    blocks: [],
+  },
 };
 
 export default Text;
