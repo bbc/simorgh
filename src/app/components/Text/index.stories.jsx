9d9632c9398901206b1db903c738c35d81d9d0cb
Jordan Tart <jordan.tart@bbc.co.uk>
2018-06-18 09:49:42 +0100

    Clarify paragraph text

--- src/app/components/Text/index.stories.jsx
+++ src/app/components/Text/index.stories.jsx
@@ -11,9 +11,9 @@ const paragraphBlock = (blockId, text) => ({
 
 const props = {
   blocks: [
-    paragraphBlock('01', 'This is a text block.'),
-    paragraphBlock('02', 'This is another text block.'),
-    paragraphBlock('03', 'This is the final text block.'),
+    paragraphBlock('01', 'This is a paragraph.'),
+    paragraphBlock('02', 'This is another paragraph.'),
+    paragraphBlock('03', 'This is a final paragraph.'),
   ],
 };
 
