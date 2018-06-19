05ffdb23c83e7c86f6b78e5687e5a850d27ac0b2
Benjamin Hobbs <bjhobbs06@gmail.com>
2018-06-19 21:52:19 +0100

    Rmv implicit 200 tests & refactor remainder

--- cypress/integration/article_spec.js
+++ cypress/integration/article_spec.js
@@ -1,5 +1,4 @@
 import {
-  testResponseCode,
   getElement,
   shouldContainText,
   shouldContainStyles,
@@ -34,17 +33,5 @@ describe('News Article', () => {
 
   it('should render a title', () => {
     cy.title().should('eq', 'Article Headline');
-  });
-});
-
-describe('Page Status', () => {
-  it('should display 200', () => {
-    testResponseCode('/', 200);
-  });
-});
-
-describe('Renderer Status', () => {
-  it('should display 200', () => {
-    testResponseCode('/status', 200);
   });
 });
