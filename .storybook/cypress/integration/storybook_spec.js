5fb7f5a352d33b9180f951e3df95c57932da6674
Benjamin Hobbs <bjhobbs06@gmail.com>
2018-06-19 21:53:27 +0100

    Rmv redundant 200 code check for storybook

--- .storybook/cypress/integration/storybook_spec.js
+++ .storybook/cypress/integration/storybook_spec.js
@@ -1,5 +1,3 @@
-import { testResponseCode } from '../../../cypress/integration/test-helper';
-
 describe('Storybook Article', () => {
   // eslint-disable-next-line no-undef
   before(() => {
@@ -8,11 +6,5 @@ describe('Storybook Article', () => {
 
   it('should render a title', () => {
     cy.title().should('eq', 'Storybook');
-  });
-});
-
-describe('Page Status', () => {
-  it('should display 200', () => {
-    testResponseCode('/', 200);
   });
 });
