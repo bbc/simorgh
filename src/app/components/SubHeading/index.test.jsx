c6d1cd5edf3bd385a06a990a588178b3c1788c8f
Andrzej Pogonowski <pogona01@mc-n347976.local>
2018-06-20 16:18:16 +0100

    Added the basic component and the corresponding tests

---
+++ src/app/components/SubHeading/index.test.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import snapshotTestHelper from '../../../__test__/snapshotTestHelper';
+import SubHeading from './index';
+
+describe('SubHeading', () => {
+  describe('with no data', () => {
+    snapshotTestHelper.shouldMatchSnapshot(
+      'should not render anything',
+      <SubHeading />,
+    );
+  });
+
+  describe('with data', () => {
+    const data = {
+      blocks: [
+        {
+          model: {
+            blocks: [
+              {
+                model: {
+                  text: 'The amazing sub-heading!?',
+                },
+              },
+            ],
+          },
+        },
+      ],
+    };
+
+    snapshotTestHelper.shouldMatchSnapshot(
+      'should display the provided sub-heading',
+      <SubHeading {...data} />,
+    );
+  });
+});
