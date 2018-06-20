b10eb7c0de8a3a1104fcddff0e7d0cbf9f1a2bcb
Andrzej Pogonowski <pogona01@mc-n347976.local>
2018-06-20 16:18:16 +0100

    Added the basic component and the corresponding tests

---
+++ src/app/components/SubHeading/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const SubHeading = ({ blocks }) => {
+  const { text } = blocks[0].model.blocks[0].model;
+
+  return <h2>{text}</h2>;
+};
+
+SubHeading.propTypes = {
+  blocks: propTypes.arrayOf(
+    propTypes.shape({
+      model: propTypes.shape({
+        blocks: propTypes.arrayOf(
+          propTypes.shape({
+            text: propTypes.string,
+          }),
+        ),
+      }),
+    }),
+  ),
+};
+
+SubHeading.defaultProps = {
+  blocks: [
+    {
+      model: {
+        blocks: [
+          {
+            model: {},
+          },
+        ],
+      },
+    },
+  ],
+};
+
+export default SubHeading;
