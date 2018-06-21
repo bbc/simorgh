2feb19e22b4af81c20cc5e5b63ce79cf72d82cb7
Sareh Heidari <sareh.heidari@gmail.com>
2018-06-21 14:57:24 +0100

    Initial

--- src/app/components/Article.jsx
+++ src/app/components/Article.jsx
@@ -34,6 +34,27 @@ class Article extends Component {
 
   render() {
     const { headline } = this.state;
+    const mediaPlayerStyles = {
+      height: 270,
+      width: 480,
+    };
+    const playlistSettings = {
+      product: 'news',
+      superResponsive: true,
+      playlistObject: {
+        title:
+          "'In Russia we are allowed in the stadium' - Hundreds of Iranian women watched their first men's football match at the World Cup.",
+        holdingImageURL:
+          'http://ichef.bbci.co.uk/images/ic/$recipe/p06bpk3r.jpg',
+        items: [
+          {
+            versionID: 'p06bph9x',
+            kind: 'programme',
+            duration: 164,
+          },
+        ],
+      },
+    };
     return (
       <Fragment>
         <Helmet htmlAttributes={{ lang: 'en-GB' }}>
@@ -41,6 +62,16 @@ class Article extends Component {
         </Helmet>
         <Header />
         <Headline>{headline}</Headline>
+        <div id="mediaPlayer12345678" style={mediaPlayerStyles} />
+        <script type="text/javascript">
+          {/* eslint-disable-next-line global-require import/no-dynamic-require */
+          require(['bump-3'], $ => {
+            const mediaPlayer = $('#mediaPlayer12345678').player(
+              playlistSettings,
+            );
+            mediaPlayer.load();
+          })}
+        </script>
       </Fragment>
     );
   }
--- src/app/components/Document.jsx
+++ src/app/components/Document.jsx
@@ -26,6 +26,17 @@ class Document extends React.Component {
           <link rel="manifest" href="manifest.json" />
           {helmet.title.toComponent()}
           {styleTags}
+          <script
+            type="text/javascript"
+            src="https://static.bbci.co.uk/frameworks/requirejs/0.13.0/sharedmodules/require.js"
+          />
+          <script type="text/javascript">{
+            var bbcRequireMap = {
+              "jquery-1.9":"https://static.bbci.co.uk/frameworks/jquery/0.3.0/sharedmodules/jquery-1.9.1",
+              "bump-3":"https://emp.bbci.co.uk/emp/bump-3/bump-3"
+            }
+            requirejs.config({ paths: bbcRequireMap, waitSeconds: 30 });
+          }</script>
         </head>
         <body>
           <AfterRoot />
