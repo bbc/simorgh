ec4f1dcd0113bf4912e7e68b8d7c27d187869968
Sareh Heidari <sareh.heidari@gmail.com>
2018-06-26 14:36:16 +0100

    Lint fix to address changes pulled in from latest branch

--- src/app/components/SubHeading/index.jsx
+++ src/app/components/SubHeading/index.jsx
@@ -4,7 +4,11 @@ import { textPropTypes, textDefaultPropTypes } from '../../proptypes';
 const SubHeading = ({ blocks }) => {
   const { text } = blocks[0].model.blocks[0].model;
 
-  return <h2>{text}</h2>;
+  return (
+    <h2>
+      {text}
+    </h2>
+  );
 };
 
 SubHeading.propTypes = textPropTypes;
