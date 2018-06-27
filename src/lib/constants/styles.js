45d16221fb1859017eee0b0c9cdfb93a19d8b71a
Benjamin Hobbs <bjhobbs06@gmail.com>
2018-06-27 15:18:04 +0100

    Add all font variants

--- src/lib/constants/styles.js
+++ src/lib/constants/styles.js
@@ -35,6 +35,27 @@ injectGlobal`
   }
   @font-face {
     font-display: optional;
+    font-family: ReithSansNewsMedium;
+    font-style: normal;
+    font-weight: bold;
+    src: url('https://gel.files.bbci.co.uk/r2.302/BBCReithSans_W_Md.woff') format('woff'), url('https://gel.files.bbci.co.uk/r2.302/BBCReithSans_W_Md.woff2') format('woff2');
+  }
+  @font-face {
+    font-display: optional;
+    font-family: ReithSansNewsBold;
+    font-style: normal;
+    font-weight: bold;
+    src: url('https://gel.files.bbci.co.uk/r2.302/BBCReithSans_W_Bd.woff') format('woff'), url('https://gel.files.bbci.co.uk/r2.302/BBCReithSans_W_Bd.woff2') format('woff2');
+  }
+  @font-face {
+    font-display: optional;
+    font-family: ReithSerifNewsLight;
+    font-style: normal;
+    font-weight: 300;
+    src: url('https://gel.files.bbci.co.uk/r2.302/BBCReithSerif_W_Lt.woff') format('woff'), url('https://gel.files.bbci.co.uk/r2.302/BBCReithSerif_W_Lt.woff2') format('woff2');
+  }
+  @font-face {
+    font-display: optional;
     font-family: ReithSerifNewsRegular;
     font-style: normal;
     font-weight: 400;
@@ -46,5 +67,12 @@ injectGlobal`
     font-style: normal;
     font-weight: bold;
     src: url('https://gel.files.bbci.co.uk/r2.302/BBCReithSerif_W_Md.woff') format('woff'), url('https://gel.files.bbci.co.uk/r2.302/BBCReithSerif_W_Md.woff2') format('woff2');
+  }
+  @font-face {
+    font-display: optional;
+    font-family: ReithSerifNewsBold;
+    font-style: normal;
+    font-weight: bold;
+    src: url('https://gel.files.bbci.co.uk/r2.302/BBCReithSerif_W_Bd.woff') format('woff'), url('https://gel.files.bbci.co.uk/r2.302/BBCReithSerif_W_Bd.woff2') format('woff2');
   }
 `
