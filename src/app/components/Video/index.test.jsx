784662213e6df46a15728da532d26aaa9fc59b6c
Andrzej Pogonowski <pogon.ap@hotmail.com>
2018-06-27 14:34:03 +0100

    Adding a prop wide test

--- src/app/components/Video/index.test.jsx
+++ src/app/components/Video/index.test.jsx
@@ -9,4 +9,42 @@ describe('Video', () => {
           <Video />,
           );
     })
+
+    describe('with data', () => {
+        const videoData = {
+            "type": "video",
+            "model": {
+              "locator": "urn:bbc:pips:pid:p064nsyw",
+              "blocks": [
+                {
+                  "type": "rawVideo",
+                  "model": {
+                    "locator": "urn:bbc:pips:pid:p064nsyw",
+                    "versionID": "p064nsz3",
+                    "kind": "clip",
+                    "duration": 299
+                  }
+                },
+                {
+                  "type": "image",
+                  "model": {
+                    "blocks": [
+                      {
+                        "type": "rawImage",
+                        "model": {
+                          "locator": "/cpsprodpb/5BD5/production/_101690532_2.jpg",
+                        }
+                      }
+                    ]
+                  }
+                }
+              ]
+            }
+          };
+
+          snapshotTestHelper.shouldMatchSnapshot(
+            'should render the important props in divs',
+            <Video {...videoData} />
+          )
+    })
 });
