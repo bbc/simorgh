2beb8bd31e79114fcafec3a465244d83d6f8ee04
Andrzej Pogonowski <pogon.ap@hotmail.com>
2018-06-27 17:05:38 +0100

    removing useless comments

--- src/app/BlockHelpers/blockHelpers.js
+++ src/app/BlockHelpers/blockHelpers.js
@@ -1,8 +1,7 @@
 // blockHelpers.js
 // Contain various helper functions for managing props as well as findinging specific
-// elements and blopcks within them
+// elements and blocks within them
 
-// filterForBlockType
 // Filters array of blocks for a single block of given type
 const filterForBlockType = (arrayOfBlocks, type) =>
   arrayOfBlocks.filter(block => block.type === type)[0];
