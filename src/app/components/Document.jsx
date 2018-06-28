83e4b8bcc3bdfa9e29a6a99bbda197ef61ef1ac8
Sareh Heidari <sareh.heidari@gmail.com>
2018-06-28 10:19:20 +0100

    Enable script tags

--- src/app/components/Document.jsx
+++ src/app/components/Document.jsx
@@ -13,7 +13,7 @@ class Document extends React.Component {
   }
 
   render() {
-    const { helmet, assets, data, styleTags } = this.props; // eslint-disable-line react/prop-types
+    const { helmet, assets, data, script, styleTags } = this.props; // eslint-disable-line react/prop-types
     const htmlAttrs = helmet.htmlAttributes.toComponent();
 
     return (
@@ -26,6 +26,7 @@ class Document extends React.Component {
           <link rel="manifest" href="manifest.json" />
           {helmet.title.toComponent()}
           {styleTags}
+          {helmet.script.toComponent()}
         </head>
         <body>
           <AfterRoot />
