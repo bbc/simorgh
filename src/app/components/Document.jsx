8df69f055c8d2b9ab1e470cfc88cee43aba07851
Sareh Heidari <sareh.heidari@gmail.com>
2018-06-28 17:17:39 +0100

    Refactor to appease codeclimate

--- src/app/components/Document.jsx
+++ src/app/components/Document.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { AfterRoot, AfterData } from '@jaredpalmer/after';
 import { ServerStyleSheet } from 'styled-components';
+
+const resourceHints = () => (
+  <Fragment>
+    <link rel="preconnect" href="//ichef.bbci.co.uk" />
+    <link rel="preconnect" href="//static.bbci.co.uk" />
+    <link rel="preconnect" href="//gel.files.bbci.co.uk" />
+    <link rel="dns-prefetch" href="//ichef.bbci.co.uk" />
+    <link rel="dns-prefetch" href="//static.bbci.co.uk" />
+    <link rel="dns-prefetch" href="//gel.files.bbci.co.uk" />
+  </Fragment>
+);
 
 class Document extends React.Component {
   static async getInitialProps({ assets, data, renderPage }) {
@@ -24,12 +35,7 @@ class Document extends React.Component {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="robots" content="nofollow" />
           <link rel="manifest" href="manifest.json" />
-          <link rel="preconnect" href="//ichef.bbci.co.uk" />
-          <link rel="preconnect" href="//static.bbci.co.uk" />
-          <link rel="preconnect" href="//gel.files.bbci.co.uk" />
-          <link rel="dns-prefetch" href="//ichef.bbci.co.uk" />
-          <link rel="dns-prefetch" href="//static.bbci.co.uk" />
-          <link rel="dns-prefetch" href="//gel.files.bbci.co.uk" />
+          {resourceHints()}
           {helmet.title.toComponent()}
           {styleTags}
         </head>
