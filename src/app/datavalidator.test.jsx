2d3770dd012c7bbbf8edeaa7068160e936edafd0
Paul Chinkwende <paul.chinkwende@bbc.co.uk>
2018-06-28 17:12:23 +0100

    add required property test

--- src/app/datavalidator.test.jsx
+++ src/app/datavalidator.test.jsx
@@ -1,14 +1,32 @@
 const datavalidator = require('./datavalidator');
 
+const data = {
+	type: 'article'
+};
+
 describe('Data Validator', () => {
 	it('should throws error when schema type and data type do not match', () => {
-		const data = {
+	    expect(() => {
+				datavalidator.validateType('string', data);
+	    }).toThrowError(
+				'Error: Type does not match for article node - expected string got object'
+			);
+	});
+	it('should throw error when data does not have a required property', () => {
+		const schema = {
+			required: ['locator', 'passport', 'model'],
 			type: 'article',
 			properties: {}
-		}
-
-	    expect(() => {
-			datavalidator.validateType('string', data);
-	    }).toThrowError('Error: Type does not match for article node - expected string got object');
+		};
+			expect(() => {
+			datavalidator.validateRequired(schema, data);
+		}).toThrowError('Error: Missing required prop for locator');
+			const dataWithLocator = {
+				type: 'article',
+				locator: 'urn:bbc:optimo:asset:c0000000001o'
+			}
+			expect(() => {
+			datavalidator.validateRequired(schema, dataWithLocator);
+		}).toThrowError('Error: Missing required prop for passport');
 	});
 });
