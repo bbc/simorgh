eb603a57b8d33444dea25ca87071629bb5e52a04
Chris Hinds <chris.hinds@bbc.co.uk>
2018-06-29 14:49:13 +0100

    Inittial parsing of markdown without custom components

--- src/app/components/Text/index.jsx
+++ src/app/components/Text/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import remark from 'remark';
+import reactRenderer from 'remark-react';
 import { arrayOf, shape, string } from 'prop-types';
 
 const Text = ({ blocks }) =>
   blocks.map(({ blockId, model }) => (
     <p key={blockId}>
-      {model.text}
+      {remark().use(reactRenderer).processSync(model.text).contents}
     </p>
   ));
 
