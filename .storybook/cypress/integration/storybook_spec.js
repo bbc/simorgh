60a0650310b61e6bbecb8e463f5340ddfccbc7a6
Benjamin Hobbs <bjhobbs06@gmail.com>
2018-07-01 10:16:23 +0100

    Prove iframe tests can work

--- .storybook/cypress/integration/storybook_spec.js
+++ .storybook/cypress/integration/storybook_spec.js
@@ -7,4 +7,33 @@ describe('Storybook Article', () => {
   it('should render a title', () => {
     cy.title().should('eq', 'Storybook');
   });
+
+  // This is incredibly brittle, it only works if Article is first.
+  it('article story should have a header', () => {
+    cy.get('#storybook-preview-iframe').then(($iframe) => {
+      const $jroot = $iframe.contents().find('#root')
+      const $root = $jroot[0]
+      cy.wrap($root).find('header').should('exist')
+    });
+  });
+
+  it('first story should have children', () => {
+    cy.get('#storybook-preview-iframe').then(($iframe) => {
+      const $jroot = $iframe.contents().find('#root')
+      const $root = $jroot[0]
+      cy.wrap($root).children().should('exist')
+    });
+  });
+
+  it('each story should have children', () => {
+    cy.get('ul>li>a').each(($el) => {
+      cy.wrap($el).click();
+      cy.get('#storybook-preview-iframe').then(($iframe) => {
+        const $jroot = $iframe.contents().find('#root')
+        const $root = $jroot[0]
+        cy.wrap($root).children().should('exist')
+      });
+    })
+  });
+
 });
