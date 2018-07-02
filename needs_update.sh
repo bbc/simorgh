6389bf7d73a5eda8ea014e8cd2663d6b2b8ad7f9
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-07-02 18:10:32 +0100

    add cd to route logic so command can run anywhere in application

--- needs_update.sh
+++ needs_update.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#
+if [[ $(pwd) =~ .+simorgh ]]; then
+	# cd to application route directory
+	cd ${BASH_REMATCH}
 
-git pull origin latest
-
-test=$(git diff origin/latest -- package-lock.json | wc -l)
-
-if [ $test -ne 0 ]
-	then
-	npm install
+	# if the wordcount of the package-lock file diff is not zero
+	test=$(git diff origin/latest -- package-lock.json | wc -l)
+	if [ $test -ne 0 ]; then
+		echo "Your package-lock.json differs to origin/latest, attempting to pull latest into your branch"
+		git pull origin latest
+		echo "Removing package-lock from local branch"
+		rm package-lock.json
+		echo "Running 'npm install' to update the package-lock.json"
+		npm install
+	fi
 fi
-
