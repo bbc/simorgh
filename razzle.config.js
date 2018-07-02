9e9c7479fe2a02092b1ee065bc47b87a0c836562
Benjamin Hobbs <bjhobbs06@gmail.com>
2018-07-02 18:04:00 +0100

    Slightly less hacky

--- razzle.config.js
+++ razzle.config.js
@@ -1,5 +1,3 @@
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer'); // eslint-disable-line import/no-extraneous-dependencies
-
 module.exports = {
   modify: (config, { target, dev }) => {
     const appConfig = config;
@@ -11,32 +9,30 @@ module.exports = {
         A prod build will fail if the API changes so it is fairly safe.
       */
       appConfig.module.rules.shift();
-      
+
       // Setup bundle analyser
-      if (target === 'web') {
-        // `npm install --production' won't have this available
-        try {
-          appConfig.plugins.push(
-            new BundleAnalyzerPlugin({
-              analyzerMode: 'static',
-              defaultSizes: 'gzip',
-              generateStatsFile: true,
-              openAnalyzer: false,
-              reportFilename: '../../reports/webpackBundleReport.html',
-              statsFilename: '../../reports/webpackBundleReport.json',
-            }),
-          );
-        } catch (e) {
-          console.log('Production install does not have bundle analysis') // eslint-disable-line no-console
-        }
+      if (target === 'web' && !process.env.CI) {
+        const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer'); // eslint-disable-line import/no-extraneous-dependencies, global-require
+        appConfig.plugins.push(
+          new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            defaultSizes: 'gzip',
+            generateStatsFile: true,
+            openAnalyzer: false,
+            reportFilename: '../../reports/webpackBundleReport.html',
+            statsFilename: '../../reports/webpackBundleReport.json',
+          }),
+        );
       }
     }
 
     // This is to override bundle performance test
-    appConfig.performance = {
-        maxAssetSize: 350000,
-        maxEntrypointSize: 350000,
-    };
+    if (process.env.CI) {
+      appConfig.performance = {
+          maxAssetSize: 350000,
+          maxEntrypointSize: 350000,
+      };
+    }
 
     return appConfig;
   },
