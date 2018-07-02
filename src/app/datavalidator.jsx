a95c5f4709bae23612ce93567a6ddf415190edf3
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-07-02 15:18:54 +0100

    tidy package.json conflicts and update lock file

--- src/app/datavalidator.jsx
+++ src/app/datavalidator.jsx
@@ -1,7 +1,7 @@
  // disable "function-define-before-call" due to recursive nature of schema
  /* eslint no-use-before-define: 0 */
 
-const yaml = require('yaml-js');
+const yaml = require('yaml-js'); // eslint-disable-line import/no-extraneous-dependencies
 const fs = require('fs'); // eslint-disable-line import/no-extraneous-dependencies
 
 const yamlSchema = fs.readFileSync('././data/schema.yaml', 'utf8');
