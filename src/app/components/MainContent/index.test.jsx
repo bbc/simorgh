c7883f706bc47322facc31931a2adb86c0b99bd5
Jordan Tart <jordan.tart@bbc.co.uk>
2018-07-02 14:04:37 +0100

    Tidy test describe blocks

--- src/app/components/MainContent/index.test.jsx
+++ src/app/components/MainContent/index.test.jsx
@@ -2,26 +2,22 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import MainContent from './index';
 
-describe('Article', () => {
-  describe('Component', () => {
-    describe('with passed data', () => {
-      const data = {
-        blocks: [
-          {
-            type: 'headline',
-            blockId: '1',
-          },
-          {
-            type: 'test',
-            blockId: '2',
-          },
-        ],
-      };
+describe('MainContent', () => {
+  const data = {
+    blocks: [
+      {
+        type: 'headline',
+        blockId: '1',
+      },
+      {
+        type: 'test',
+        blockId: '2',
+      },
+    ],
+  };
 
-      it('should render correctly', () => {
-        const tree = renderer.create(<MainContent data={data} />).toJSON();
-        expect(tree).toMatchSnapshot();
-      });
-    });
+  it('should render correctly', () => {
+    const tree = renderer.create(<MainContent data={data} />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
