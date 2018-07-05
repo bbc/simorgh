ff29a74e279fbfd07cbc2edb8c72a7812e221fbe
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-07-05 12:26:24 +0100

    remove npm force install and echo a prompt instead

--- is-update-required.sh
+++ is-update-required.sh
@@ -4,13 +4,15 @@ if [[ $(pwd) =~ .+simorgh ]]; then
 	cd ${BASH_REMATCH}
 
 	# if the wordcount of the package-lock file diff is not zero
-	test=$(git diff origin/latest -- package-lock.json | wc -l)
-	if [ $test -ne 0 ]; then
-		echo "Your package-lock.json differs to origin/latest, attempting to pull latest into your branch"
-		git pull origin latest
-		echo "Removing package-lock from local branch"
-		rm package-lock.json
-		echo "Running 'npm install' to update the package-lock.json"
-		npm install
+	amount_of_additions=$(git diff origin/latest -- package-lock.json | grep "+  " | wc -l)
+
+	# assume a single addition is a version bump
+	if [ $amount_of_additions \> 1 ]; then
+		echo "Your package-lock.json differs to origin/latest with" \
+			$(git diff origin/latest -- package-lock.json | grep "+  " | wc -l) \
+			"additions"
+		echo "Unless you have made dependency changes on your feature branch it is likely your branch is out of date with origin/latest"
+		echo "We suggest you 'git pull origin latest' and run 'npm install' to update your package-lock.json"
+		echo "This ensures you are running the most recent version and also that the precommit and prepush hooks are valid on you local setup"
 	fi
 fi
