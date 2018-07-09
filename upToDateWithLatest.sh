ca7d62294657eba63c44f57f77a2c2dd67cd1ac8
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-07-09 19:02:07 +0100

    add bash script and run pre push

---
+++ upToDateWithLatest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [[ $(pwd) =~ .+simorgh ]]; then
+	# cd to application route directory this allows the commiting/pushing from any part of the application
+	cd ${BASH_REMATCH}
+
+	LATEST_COMMIT=$(git log origin/latest -n 1 --pretty=format:"%H")
+	FEATURE_BRANCH_COMMIT_LIST=$(git log --pretty=format:"%H")
+
+	# if the banch git log contains the most recent commit on origin/latest
+	if [[ $FEATURE_BRANCH_COMMIT_LIST != *$LATEST_COMMIT* ]]; then
+		echo "Your branch is not up-to-date with latest. Push to origin denied."
+		echo "Please pull in the most recent version of origin/latest"
+		exit
+	fi
+
+	echo "Up-to-date with latest"
+fi
