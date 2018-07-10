6c8d3ed67c5c1ff13797d489aacd51751bf67723
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-07-10 11:35:23 +0100

    add command hint

--- upToDateWithLatest.sh
+++ upToDateWithLatest.sh
@@ -9,7 +9,7 @@ if [[ $(pwd) =~ .+simorgh ]]; then
 	# if the banch git log contains the most recent commit on origin/latest
 	if [[ $FEATURE_BRANCH_COMMIT_LIST != *$LATEST_COMMIT* ]]; then
 		echo "Your branch is not up-to-date with latest. Push to origin denied."
-		echo "Please pull in the most recent version of origin/latest"
+		echo "Please pull in the most recent version of origin/latest. EG: git pull origin latest"
 		exit
 	fi
 
