272f18daab2065d30f1a7ca1e5a2695e7e469171
Sareh Heidari <sareh.heidari@gmail.com>
2018-07-12 14:37:44 +0100

    Use textBlockPropTypes

--- src/app/components/SubHeading/index.jsx
+++ src/app/components/SubHeading/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { extractText } from '../../helpers/blockHandlers';
-import { textPropTypes, textDefaultPropTypes } from '../../models/proptypes';
+import { textBlockPropTypes } from '../../models/propTypes/text';
+import { textDefaultPropTypes } from '../../models/proptypes';
 
 const SubHeading = ({ blocks }) => {
   const { text } = extractText(blocks);
@@ -12,7 +13,7 @@ const SubHeading = ({ blocks }) => {
   return <h2>{text}</h2>;
 };
 
-SubHeading.propTypes = textPropTypes;
+SubHeading.propTypes = textBlockPropTypes;
 
 SubHeading.defaultProps = textDefaultPropTypes;
 
