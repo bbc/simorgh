1a87e0633f189c5521849b5742467143442e945e
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-07-12 13:36:52 +0100

    silence console.error() and console.log()

--- src/app/helpers/tests/jest-setup.js
+++ src/app/helpers/tests/jest-setup.js
@@ -1,3 +1,5 @@
 import fetch from 'jest-fetch-mock'; // eslint-disable-line import/no-extraneous-dependencies
 
+// ignore console.error() in testing suite to silence node_dependency console statements
+global.console.error = jest.fn();
 global.fetch = fetch;
