fce9527b3b9605b60977e0a84985ce9fc4b7b9aa
Jordan Tart <jordan.tart@bbc.co.uk>
2018-07-12 16:07:35 +0100

    Add text container

---
+++ src/app/containers/Text/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { arrayOf, shape, string } from 'prop-types';
+import Markdown from '../../components/Markdown';
+
+const TextContainer = ({ blocks }) =>
+  blocks.map(({ blockId, model }) => <Markdown key={blockId} {...model} />);
+
+TextContainer.propTypes = {
+  blocks: arrayOf(
+    shape({
+      blockId: string,
+      model: shape({
+        text: string.isRequired,
+      }).isRequired,
+    }).isRequired,
+  ).isRequired,
+};
+
+export default TextContainer;
