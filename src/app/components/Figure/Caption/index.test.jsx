0b4b14099bda1abb17212ab683eb8a1e831c7db5
radiocontrolled <alison.benjamin@bbc.co.uk>
2018-07-13 14:40:01 +0100

    add snapshot test for Caption

---
+++ src/app/components/Figure/Caption/index.test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shouldMatchSnapshot } from '../../../helpers/tests/testHelpers';
+import Caption from './index';
+
+describe('Caption', () => {
+  shouldMatchSnapshot('should render correctly', <Caption />);
+});
