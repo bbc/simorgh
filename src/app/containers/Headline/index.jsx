f413766c441a2b73f55e51b85f8083b763400bfc
Jordan Tart <jordan.tart@bbc.co.uk>
2018-07-13 12:18:31 +0100

    Extracted styled h1 into component

--- src/app/containers/Headline/index.jsx
+++ src/app/containers/Headline/index.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import { extractText } from '../../helpers/blockHandlers';
 import { textDefaultPropTypes } from '../../models/proptypes';
 import { headlineModelPropTypes } from '../../models/propTypes/headline';
-import { C_EBON, FF_NEWS_SANS_REG } from '../../lib/constants/styles';
-
-const StyledHeadline = styled.h1`
-  color: ${C_EBON};
-  font-family: ${FF_NEWS_SANS_REG};
-  font-size: 2em;
-`;
+import Headline from '../../components/Headline';
 
 const HeadlineContainer = ({ blocks }) => {
   const { text } = extractText(blocks);
@@ -18,7 +11,7 @@ const HeadlineContainer = ({ blocks }) => {
     return null;
   }
 
-  return <StyledHeadline>{text}</StyledHeadline>;
+  return <Headline>{text}</Headline>;
 };
 
 HeadlineContainer.propTypes = headlineModelPropTypes;
