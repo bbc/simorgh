b01d9041df42623116143a8dc9a2025c64010663
Jordan Tart <jordan.tart@bbc.co.uk>
2018-07-13 17:44:10 +0100

    Use single italic

--- src/app/containers/Text/index.stories.jsx
+++ src/app/containers/Text/index.stories.jsx
@@ -16,7 +16,7 @@ const props = {
       "It was designed by London-based florist Philippa Craddock, who also created the floral displays for St George's Chapel and St George's Hall using locally sourced foliage, [which were later donated to local hospices](/news/articles/c000000000ro).",
     ),
     paragraphBlock('02', 'This is another paragraph with some **bold** text.'),
-    paragraphBlock('03', 'This is a paragraph with __italic__ text.'),
+    paragraphBlock('03', 'This is a paragraph with _italic_ text.'),
     paragraphBlock(
       '04',
       '~~This is a paragraph with some strike-through text~~.',
