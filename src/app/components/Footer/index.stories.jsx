04e02446cdff67c8a66f9429a1c9317860311e24
Benjamin Hobbs <bjhobbs06@gmail.com>
2018-07-16 21:55:49 +0100

    WIP continue stories and tests

--- src/app/components/Footer/index.stories.jsx
+++ src/app/components/Footer/index.stories.jsx
@@ -2,4 +2,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
 import Footer from './index';
 
-storiesOf('Footer', module).add('default', () => <Footer />);
+const link = {
+  href: 'Welcome to the BBC',
+  text: 'https://www.bbc.co.uk',
+};
+const list = [{ link }];
+
+storiesOf('Footer', module).add('default', () => (
+  <Footer list={list} text="text" link={link} />
+));
