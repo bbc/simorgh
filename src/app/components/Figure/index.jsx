3be3c5e89610c99fdc2e2b671a1933cb0e1d3db3
Jordan Tart <jordan.tart@bbc.co.uk>
2018-07-17 14:33:41 +0100

    Tidy Figure

--- src/app/components/Figure/index.jsx
+++ src/app/components/Figure/index.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import Caption from '../Caption';
+import Caption from './Caption';
 import Image from '../Image';
 
 function renderCaption(caption) {
   return caption ? <Caption>{caption}</Caption> : null;
 }
 
-const Figure = props => {
-  const { alt, src, caption } = props; // eslint-disable-line 
-  return (
-    <figure>
-      <Image alt={alt} src={src} />
-      {renderCaption(caption)}
-    </figure>
-  );
-};
+const Figure = ({ alt, src, caption }) => (
+  <figure>
+    <Image alt={alt} src={src} />
+    {renderCaption(caption)}
+  </figure>
+);
 
 export default Figure;
