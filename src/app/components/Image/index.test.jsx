d931da83f1aafb20beb2c03a88c020ba6ef84ec7
Jordan Tart <jordan.tart@bbc.co.uk>
2018-07-17 14:16:04 +0100

    Remove redundant test

--- src/app/components/Image/index.test.jsx
+++ src/app/components/Image/index.test.jsx
@@ -4,15 +4,4 @@ import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
 
 describe('Image', () => {
   shouldMatchSnapshot('should render correctly', <Image />);
-
-  const image = Image({ alt: 'kittens', src: 'path/kittens.jpg' });
-
-  it('should be an img', () => {
-    expect(image.type).toBe('img');
-  });
-
-  it('should have alt text and src', () => {
-    expect(image.props.alt).toBe('kittens');
-    expect(image.props.src).toBe('path/kittens.jpg');
-  });
 });
