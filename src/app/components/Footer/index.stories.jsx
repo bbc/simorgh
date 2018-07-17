b3499f5c3163d962417988b26d9615d87264238d
sareh <sareh.heidari@gmail.com>
2018-07-17 10:27:07 +0100

    Update test & stories to have required props

--- src/app/components/Footer/index.stories.jsx
+++ src/app/components/Footer/index.stories.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
 import Footer from './index';
 
-const link = {
-  href: 'Welcome to the BBC',
-  text: 'https://www.bbc.co.uk',
-};
-const list = [{ link }];
+const link = name => ({
+  href: 'https://www.bbc.co.uk/news',
+  text: `Link ${name}`,
+});
+
+const list = [link(1), link(2), link(3), link(4), link(5), link(6), link(7)];
 
 storiesOf('Footer', module).add('default', () => (
-  <Footer list={list} text="text" link={link} />
+  <Footer list={list} text="Text here. " link={link('last')} />
 ));
--- src/app/components/Footer/index.test.jsx
+++ src/app/components/Footer/index.test.jsx
@@ -4,5 +4,15 @@ import Footer from './index';
 import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
 
 describe(`Footer`, () => {
-  shouldMatchSnapshot('should render correctly', <Footer />);
+  const link = {
+    href: 'https://www.bbc.co.uk/news',
+    text: 'Link',
+  };
+
+  const list = [link];
+
+  shouldMatchSnapshot(
+    'should render correctly',
+    <Footer list={list} text="Text here. " link={link} />,
+  );
 });
