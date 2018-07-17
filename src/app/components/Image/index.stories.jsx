065cb5af440504ea983de45e3afb56bd5543d45c
Jordan Tart <jordan.tart@bbc.co.uk>
2018-07-17 14:16:53 +0100

    Pass props to Image story/test

--- src/app/components/Image/index.stories.jsx
+++ src/app/components/Image/index.stories.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
 import Image from './index';
 
-const data = {
+const props = {
   alt:
     'Map of the UK displaying Syrian refugees and asylum seekers per 10000 population. Ranges from 0 to 17.',
   src:
     'https://ichef.bbci.co.uk/news/640/cpsprodpb/439A/production/_100960371_syrians_and_asylum_v2-nc.png',
 };
 
-storiesOf('Image', module).add('image with alt text', () => (
-  <Image {...data} />
-));
+storiesOf('Image', module).add('default', () => <Image {...props} />);
--- src/app/components/Image/index.test.jsx
+++ src/app/components/Image/index.test.jsx
@@ -3,5 +3,12 @@ import Image from './index';
 import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
 
 describe('Image', () => {
-  shouldMatchSnapshot('should render correctly', <Image />);
+  const props = {
+    alt:
+      'Map of the UK displaying Syrian refugees and asylum seekers per 10000 population. Ranges from 0 to 17.',
+    src:
+      'https://ichef.bbci.co.uk/news/640/cpsprodpb/439A/production/_100960371_syrians_and_asylum_v2-nc.png',
+  };
+
+  shouldMatchSnapshot('should render correctly', <Image {...props} />);
 });
