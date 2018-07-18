c2d831625e6d0050b6ae350c95839bee758ef51a
Jordan Tart <jordan.tart@bbc.co.uk>
2018-07-18 13:12:25 +0100

    Remove redundant story

--- src/app/containers/Footer/index.stories.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
-import FooterContainer from './index';
-
-storiesOf('FooterContainer', module).add('default', () => <FooterContainer />);
