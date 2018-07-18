5d4c029c68238b273d572703fbdeffb3a08724f0
Jonthan Mark Perry <perryj39@mc-s080830.local>
2018-07-18 11:16:10 +0100

    Removed test code from client.js for manual testing, which has been conducted.

--- src/client.js
+++ src/client.js
@@ -8,13 +8,6 @@ import routes from './app/routes';
 
 const root = document.getElementById('root');
 
-const a = new Map();
-a.set(1, 'Hello from Map() index 1');
-a.set(2, 'Hello from Map() index 2');
-a.forEach(value => {
-  console.log(value); /* eslint-disable-line no-console */
-});
-
 ensureReady(routes).then(data =>
   hydrate(
     <BrowserRouter>
