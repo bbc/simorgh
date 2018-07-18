218299e1d1ab0fe79023ebf9b48c91205a6b9531
Benjamin Hobbs <bjhobbs06@gmail.com>
2018-07-18 22:51:53 +0100

    Test both prod scens and every screensize

--- a11y.js
+++ a11y.js
@@ -1,7 +1,19 @@
-/* eslint-disable */
-page("http://localhost:7080/article/scenario-01", {
-    skip: [
-      'Design: Content resizing: Text must be styled with units that are resizable in all browsers',
-      'Structure: Containers and landmarks: Exactly one main landmark'
-    ]
-  })
+const pageWidths = [320, 400, 600, 1008, 1280];
+const skipTests = [
+  'Design: Content resizing: Text must be styled with units that are resizable in all browsers',
+  'Structure: Containers and landmarks: Exactly one main landmark',
+];
+const urls = [
+  'http://localhost:7080/article/scenario-25',
+  'http://localhost:7080/article/scenario-27',
+];
+
+urls.forEach(url =>
+  pageWidths.forEach(width =>
+    // eslint-disable-next-line no-undef
+    page(url, {
+      skip: skipTests,
+      width,
+    }),
+  ),
+);
