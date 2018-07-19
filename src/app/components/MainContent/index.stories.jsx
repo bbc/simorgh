8e459a1ea150f64c87991714c2709b8b568eb212
Jordan Tart <jordan.tart@bbc.co.uk>
2018-07-19 12:30:03 +0100

    Pass just blocks, not data to MainContent test/stories

--- src/app/components/MainContent/index.stories.jsx
+++ src/app/components/MainContent/index.stories.jsx
@@ -2,63 +2,60 @@ import React from 'react';
 import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
 import MainContent from './index';
 
-const data = {
-  blocks: [
-    {
-      type: 'headline',
-      blockId: '1',
-      model: {
-        blocks: [
-          {
-            model: {
-              blocks: [
-                {
-                  model: {
-                    text: 'This is a headline!',
-                  },
+const blocks = [
+  {
+    type: 'headline',
+    blockId: '1',
+    model: {
+      blocks: [
+        {
+          model: {
+            blocks: [
+              {
+                model: {
+                  text: 'This is a headline!',
                 },
-              ],
-            },
+              },
+            ],
           },
-        ],
-      },
+        },
+      ],
     },
-    {
-      type: 'text',
-      blockId: '2',
-      model: {
-        blocks: [
-          {
-            model: {
-              text: 'This is some text content!',
-            },
+  },
+  {
+    type: 'text',
+    blockId: '2',
+    model: {
+      blocks: [
+        {
+          model: {
+            text: 'This is some text content!',
           },
-        ],
-      },
+        },
+      ],
     },
-    {
-      type: 'test',
-      blockId: '3',
-      model: {
-        blocks: [
-          {
-            model: {
-              text: 'This is some test content!',
-            },
+  },
+  {
+    type: 'test',
+    blockId: '3',
+    model: {
+      blocks: [
+        {
+          model: {
+            text: 'This is some test content!',
           },
-        ],
-      },
+        },
+      ],
     },
-  ],
-};
+  },
+];
 
 storiesOf('MainContent', module)
   .add('with just a headline', () => {
-    const blocksOnlyHeadline = data.blocks.filter(
-      ({ type }) => type === 'headline',
-    );
-    const dataOnlyHeadline = { blocks: blocksOnlyHeadline };
+    const blocksOnlyHeadline = blocks.filter(({ type }) => type === 'headline');
 
-    return <MainContent data={dataOnlyHeadline} />;
+    return <MainContent blocks={blocksOnlyHeadline} />;
   })
-  .add('with a headline and other blocks', () => <MainContent data={data} />);
+  .add('with a headline and other blocks', () => (
+    <MainContent blocks={blocks} />
+  ));
--- src/app/components/MainContent/index.test.jsx
+++ src/app/components/MainContent/index.test.jsx
@@ -3,64 +3,62 @@ import renderer from 'react-test-renderer';
 import MainContent from './index';
 
 describe('MainContent', () => {
-  const data = {
-    blocks: [
-      {
-        type: 'headline',
-        blockId: '1',
-        model: {
-          blocks: [
-            {
-              type: 'text',
-              blockId: '1-1',
-              model: {
-                blocks: [
-                  {
-                    type: 'paragraph',
-                    blockId: '1-1-1',
-                    model: {
-                      text: 'This is a headline!',
-                    },
+  const blocks = [
+    {
+      type: 'headline',
+      blockId: '1',
+      model: {
+        blocks: [
+          {
+            type: 'text',
+            blockId: '1-1',
+            model: {
+              blocks: [
+                {
+                  type: 'paragraph',
+                  blockId: '1-1-1',
+                  model: {
+                    text: 'This is a headline!',
                   },
-                ],
-              },
+                },
+              ],
             },
-          ],
-        },
+          },
+        ],
       },
-      {
-        type: 'text',
-        blockId: '2',
-        model: {
-          blocks: [
-            {
-              type: 'paragraph',
-              blockId: '2-1',
-              model: {
-                text: 'This is some text content!',
-              },
+    },
+    {
+      type: 'text',
+      blockId: '2',
+      model: {
+        blocks: [
+          {
+            type: 'paragraph',
+            blockId: '2-1',
+            model: {
+              text: 'This is some text content!',
             },
-          ],
-        },
+          },
+        ],
       },
-      {
-        type: 'test',
-        blockId: '3',
-        model: {
-          blocks: [
-            {
-              model: {
-                text: 'This is some test content!',
-              },
+    },
+    {
+      type: 'test',
+      blockId: '3',
+      model: {
+        blocks: [
+          {
+            model: {
+              text: 'This is some test content!',
             },
-          ],
-        },
+          },
+        ],
       },
-    ],
-  };
+    },
+  ];
 
   it('should render correctly', () => {
-    const tree = renderer.create(<MainContent data={data} />).toJSON();
+    const tree = renderer.create(<MainContent blocks={blocks} />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
