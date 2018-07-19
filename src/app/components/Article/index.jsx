55f566d90c424f9c9e750bc32e2ae75e7423640d
Jordan Tart <jordan.tart@bbc.co.uk>
2018-07-19 14:41:53 +0100

    Pass blocks not data

--- src/app/components/Article/index.jsx
+++ src/app/components/Article/index.jsx
@@ -4,20 +4,20 @@ import { string, any } from 'prop-types';
 import Header from '../Header';
 import MainContent from '../MainContent';
 
-const Article = ({ lang, title, data }) => (
+const Article = ({ lang, title, blocks }) => (
   <Fragment>
     <Helmet htmlAttributes={{ lang }}>
       <title>{title}</title>
     </Helmet>
     <Header />
-    <MainContent {...data} />
+    <MainContent blocks={blocks} />
   </Fragment>
 );
 
 Article.propTypes = {
   lang: string.isRequired,
   title: string.isRequired,
-  data: any.isRequired, // eslint-disable-line
+  blocks: any.isRequired, // eslint-disable-line
 };
 
 export default Article;
