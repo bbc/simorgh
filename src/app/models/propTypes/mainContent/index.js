0c00ec455562915a161c57a7be3bd6c9d8362bf0
radiocontrolled <alison.benjamin@bbc.co.uk>
2018-07-24 15:29:46 +0100

    revert mainContent props

--- src/app/models/propTypes/mainContent/index.js
+++ src/app/models/propTypes/mainContent/index.js
@@ -1,12 +1,10 @@
 import { blocksWithTypes } from '../general';
 import { headlineBlockPropTypes } from '../headline';
 import { textBlockPropTypes } from '../text';
-import { imagePropTypes } from '../index';
 
 const mainContentPropTypes = blocksWithTypes([
   headlineBlockPropTypes,
   textBlockPropTypes,
-  imagePropTypes,
 ]);
 
 export default mainContentPropTypes;
