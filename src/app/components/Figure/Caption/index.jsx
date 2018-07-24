85a07a2c873a636f96052da3928f0a3186bbb059
Benjamin Hobbs <bjhobbs06@gmail.com>
2018-07-24 21:45:34 +0100

    Styled caption typography, needs refactoring

--- src/app/components/Figure/Caption/index.jsx
+++ src/app/components/Figure/Caption/index.jsx
@@ -1,11 +1,26 @@
 import styled from 'styled-components';
 import { FF_NEWS_SANS_REG, C_STORM } from '../../../lib/constants/styles';
+import mediaQuery from '../../../helpers/mediaQueries';
 
 const Caption = styled.figcaption`
   background-color: #d5d0cd;
   color: ${C_STORM};
   font-family: ${FF_NEWS_SANS_REG};
+  font-size: 0.875em;
+  line-height: 1em;
   padding: 8px;
+  ${mediaQuery.smartPhone} {
+    font-size: 0.875em;
+    line-height: 1.125em;
+  }
+  ${mediaQuery.tablet} {
+    font-size: 0.875em;
+    line-height: 1.125em;
+  }
+  ${mediaQuery.desktop} {
+    font-size: 0.813em;
+    line-height: 1em;
+  }
 `;
 
 export default Caption;
