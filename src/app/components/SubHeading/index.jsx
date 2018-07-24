b3ad2e8edc784dc4b6509fc7a28080cea2b578c3
Benjamin Hobbs <bjhobbs06@gmail.com>
2018-07-24 21:41:54 +0100

    Subheading, needs refactoring

--- src/app/components/SubHeading/index.jsx
+++ src/app/components/SubHeading/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 import { extractText } from '../../helpers/blockHandlers';
 import { textBlockPropTypes } from '../../models/propTypes/text';
 import { textDefaultPropTypes } from '../../models/propTypes';
+import mediaQuery from '../../helpers/mediaQueries';
+
+const StyledSubHeading = styled.h2`
+  font-size: 1.25em;
+  line-height: 1.5em;
+  ${mediaQuery.smartPhone} {
+    font-size: 1.25em;
+    line-height: 1.5em;
+  }
+  ${mediaQuery.tablet} {
+    font-size: 1.625em;
+    line-height: 1.875em;
+  }
+  ${mediaQuery.desktop} {
+    font-size: 1.5em;
+    line-height: 1.75em;
+  }
+`;
 
 const SubHeading = ({ blocks }) => {
   const { text } = extractText(blocks);
@@ -10,7 +29,7 @@ const SubHeading = ({ blocks }) => {
     return null;
   }
 
-  return <h2>{text}</h2>;
+  return <StyledSubHeading>{text}</StyledSubHeading>;
 };
 
 SubHeading.propTypes = textBlockPropTypes;
