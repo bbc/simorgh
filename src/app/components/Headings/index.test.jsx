5639c20b8ea0b566326034694f1336f329e6a08c
Paul Chinkwende <paul.chinkwende@bbc.co.uk>
2018-07-25 12:08:00 +0100

    simplyfy tests

--- src/app/components/Headings/index.test.jsx
+++ src/app/components/Headings/index.test.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import { Headline, SubHeading } from './index';
 import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
 
-describe('Headings', () => {
-  describe('Headline component', () => {
-    shouldMatchSnapshot(
-      'should render correctly',
-      <Headline>This is my headline.</Headline>,
-    );
-  });
-  describe('SubHeading component', () => {
-    shouldMatchSnapshot(
-      'should render correctly',
-      <SubHeading>This is a SubHeading</SubHeading>,
-    );
-  });
+describe('Headline component', () => {
+  shouldMatchSnapshot(
+    'should render correctly',
+    <Headline>This is my headline.</Headline>,
+  );
+});
+describe('SubHeading component', () => {
+  shouldMatchSnapshot(
+    'should render correctly',
+    <SubHeading>This is a SubHeading</SubHeading>,
+  );
 });
