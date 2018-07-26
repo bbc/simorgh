9d6ea9e17830e197c6d55bceb8f11f32d8a478fa
radiocontrolled <alison.benjamin@bbc.co.uk>
2018-07-26 12:10:14 +0100

    WIP - commented out skip rules

--- a11y.js
+++ a11y.js
@@ -1,7 +1,7 @@
 const pageWidths = [320, 400, 600, 1008, 1280];
 const skipTests = [
-  'Design: Content resizing: Text must be styled with units that are resizable in all browsers',
-  'Structure: Containers and landmarks: Exactly one main landmark',
+  // 'Design: Content resizing: Text must be styled with units that are resizable in all browsers',
+  // 'Structure: Containers and landmarks: Exactly one main landmark',
 ];
 const urls = [
   'http://localhost:7080/article/scenario-25',
