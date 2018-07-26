2521bb6ff91f2dd64a266612ce9cee49871a49c5
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-07-26 13:09:23 +0100

    tidy

--- src/app/dataValidator/indirectSpyOn.test.js
+++ src/app/dataValidator/indirectSpyOn.test.js
@@ -1,8 +1,8 @@
-const video = require('./indirectSpyOn');
+const myModule = require('./indirectSpyOn');
 
 test('indirectly call play function', () => {
-  const fooSpy = jest.spyOn(video, 'foo');
-  video.bar();
+  const fooSpy = jest.spyOn(myModule, 'foo');
+  myModule.bar();
 
   expect(fooSpy).toHaveBeenCalledWith(true);
 });
