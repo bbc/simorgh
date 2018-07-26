f515ffa28cead9868f0d81df75edc6e2eae53fbc
Paul Chinkwende <paul.chinkwende@bbc.co.uk>
2018-07-26 16:19:40 +0100

    prettier

--- src/app/models/propTypes/mainContent/index.js
+++ src/app/models/propTypes/mainContent/index.js
@@ -1,8 +1,7 @@
 import { blocksWithTypes } from '../general';
 import { headlineBlockPropTypes } from '../headline';
 import { textBlockPropTypes } from '../text';
-import { imagePropTypes } from '../index';
-import { videoPropTypes } from '../index';
+import { imagePropTypes, videoPropTypes } from '../index';
 
 const mainContentPropTypes = blocksWithTypes([
   headlineBlockPropTypes,
