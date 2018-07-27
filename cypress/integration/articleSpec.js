9e08f467a145e50b88bed01fd69e227548453a28
thekp <khoa.phan@bbc.co.uk>
2018-07-27 16:18:47 +0100

    update e2e for header

--- cypress/integration/articleSpec.js
+++ cypress/integration/articleSpec.js
@@ -18,6 +18,10 @@ describe('News Article', () => {
 
     const anchorElement = getElement('header a');
     shouldContainText(anchorElement, 'BBC News');
+
+    const svgElement = getElement('header a svg');
+    svgElement.should('have.attr', 'focusable', 'false');
+    svgElement.should('have.attr', 'aria-hidden', 'true');
   });
 
   it('should render a headline', () => {
