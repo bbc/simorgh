6374f2636ef300327490e9280b3cdf82da4eb845
radiocontrolled <alison.benjamin@bbc.co.uk>
2018-07-27 12:18:12 +0100

    update C_POSTBOX const

--- src/app/components/Header/index.jsx
+++ src/app/components/Header/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import { C_RED, GEL_SPACING_DBL } from '../../lib/constants/styles';
+import { C_POSTBOX, GEL_SPACING_DBL } from '../../lib/constants/styles';
 
 const StyledHeader = styled.header`
-  background-color: ${C_RED};
+  background-color: ${C_POSTBOX};
   height: 80px;
   width: 100%;
   padding: ${GEL_SPACING_DBL}px;
