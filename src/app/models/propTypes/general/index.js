8ebdcdde7ded44129e1fd9eadcc4de9de07b48aa
Jordan Tart <jordan.tart@bbc.co.uk>
2018-07-30 09:59:00 +0100

    Extract block object from shape

--- src/app/models/propTypes/general/index.js
+++ src/app/models/propTypes/general/index.js
@@ -1,11 +1,13 @@
 import { shape, string, oneOf, arrayOf, oneOfType } from 'prop-types';
 
+export const blockObjectOfSpecificTypeAndModel = (type, model) => ({
+  blockId: string.isRequired,
+  type: oneOf([type]).isRequired,
+  model: shape(model).isRequired,
+});
+
 export const blockOfSpecificTypeAndModel = (type, model) =>
-  shape({
-    blockId: string.isRequired,
-    type: oneOf([type]).isRequired,
-    model: shape(model).isRequired,
-  });
+  shape(blockObjectOfSpecificTypeAndModel(type, model));
 
 export const blocksWithTypes = blockTypes => ({
   blocks: arrayOf(oneOfType(blockTypes).isRequired).isRequired,
