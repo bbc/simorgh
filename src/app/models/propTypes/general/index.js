5aa54d1f9b2a3097df512a47251c68537182a03d
Jordan Tart <jordan.tart@bbc.co.uk>
2018-07-30 10:00:12 +0100

    Export helper to check specific shape of array

--- src/app/models/propTypes/general/index.js
+++ src/app/models/propTypes/general/index.js
@@ -1,4 +1,11 @@
-import { shape, string, oneOf, arrayOf, oneOfType } from 'prop-types';
+import {
+  shape,
+  string,
+  oneOf,
+  arrayOf,
+  oneOfType,
+  checkPropTypes,
+} from 'prop-types';
 
 export const blockObjectOfSpecificTypeAndModel = (type, model) => ({
   blockId: string.isRequired,
@@ -12,3 +19,14 @@ export const blockOfSpecificTypeAndModel = (type, model) =>
 export const blocksWithTypes = blockTypes => ({
   blocks: arrayOf(oneOfType(blockTypes).isRequired).isRequired,
 });
+
+export const arrayOfSpecificObjects = (requiredPropCheck, propTypes) =>
+  arrayOf((props, key) => {
+    requiredPropCheck(props);
+    const prop = props[key];
+    const propType = propTypes[prop.type];
+
+    checkPropTypes(propType, prop, 'prop', prop.type);
+
+    return null;
+  });
