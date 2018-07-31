efa94d3e8432aea9aceeeca5ee4758798781635c
Jonthan Mark Perry <perryj39@mc-s080830.local>
2018-07-31 17:33:50 +0100

    Refactored tests to reflect change in body of validateRouteParameter

--- src/app/routeValidator/helpers/validateRouteParameter.test.js
+++ src/app/routeValidator/helpers/validateRouteParameter.test.js
@@ -4,16 +4,12 @@ describe('Validate Route Parameter', () => {
   it('should throw an error with an invalid route paremeter', () => {
     const routeParameter = `route-01`;
 
-    expect(() => {
-      validateRouteParameter(routeParameter);
-    }).toThrowError(`Router parameter is not valid.`);
+    expect(validateRouteParameter(routeParameter)).toBeFalsy();
   });
 
   it('should not error with a number length of 2 and of type Number', () => {
     const routeParameter = `scenario-13`;
 
-    expect(() => {
-      validateRouteParameter(routeParameter);
-    }).not.toThrowError();
+    expect(validateRouteParameter(routeParameter)).toBeTruthy();
   });
 });
