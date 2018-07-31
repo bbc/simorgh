c453399ebdefadeba57bac950b25df1f17499ee6
Jonthan Mark Perry <perryj39@mc-s080830.local>
2018-07-31 09:59:13 +0100

    Removed unrequired unit tests

--- src/app/routeValidator/helpers/validateRouteParameter.test.js
+++ src/app/routeValidator/helpers/validateRouteParameter.test.js
@@ -11,46 +11,6 @@ describe('Validate Route Parameter', () => {
     }).toThrowError(`Router parameter is not valid.`);
   });
 
-  // it('should throw a null error with a number length of 0 digits', () => {
-  //   const routeParameter = `scenario-`;
-
-  //   expect(() => {
-  //     validateRouteParameter(routeParameter);
-  //   }).toThrowError(
-  //     `Route contained 'scenario-' but did not specify a number.`,
-  //   );
-  // });
-
-  // it('should throw an error with an number length of 1 digit', () => {
-  //   const routeParameter = `scenario-6`;
-
-  //   expect(() => {
-  //     validateRouteParameter(routeParameter);
-  //   }).toThrowError(
-  //     `Route contained '${routeParameter}' but the number was not in a two digit format - e.g. 01.`,
-  //   );
-  // });
-
-  // it('should throw an error with a number length greater than 2 digits', () => {
-  //   const routeParameter = `scenario-123`;
-
-  //   expect(() => {
-  //     validateRouteParameter(routeParameter);
-  //   }).toThrowError(
-  //     `Route parameter is not valid. Scenario number is out of range.`,
-  //   );
-  // });
-
-  // it('should throw an error with a number type of string', () => {
-  //   const routeParameter = `scenario-ab`;
-
-  //   expect(() => {
-  //     validateRouteParameter(routeParameter);
-  //   }).toThrowError(
-  //     `Route contained 'scenario-' but did not specify a number.`,
-  //   );
-  // });
-
   it('should not error with a number length of 2 and of type Number', () => {
     const routeParameter = `scenario-13`;
 
