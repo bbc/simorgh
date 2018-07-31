4aa67b3f7889b0ad961020a7d5f9696d6affb776
Jonthan Mark Perry <perryj39@mc-s080830.local>
2018-07-31 17:36:28 +0100

    Removed throwError exception and replaced with boolean conditions

--- src/app/routeValidator/helpers/validateRouteParameter.js
+++ src/app/routeValidator/helpers/validateRouteParameter.js
@@ -1,12 +1,12 @@
-import { throwError } from '../../dataValidator/utilities/messaging';
-
 const validateRouteParameter = routeParam => {
   const regex = '^(scenario-[0-9]{2})$';
   const routeMatches = routeParam.match(regex);
 
   if (!routeMatches) {
-    throwError(`Router parameter is not valid.`);
+    return false;
   }
+
+  return true;
 };
 
 export default validateRouteParameter;
