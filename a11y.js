72f36a5da51d3b39b5e61b98b3eb4686c3de259e
radiocontrolled <alison.benjamin@bbc.co.uk>
2018-08-01 15:54:23 +0100

    skip broken text resizing rule

--- a11y.js
+++ a11y.js
@@ -1,7 +1,6 @@
 const pageWidths = [320, 400, 600, 1008, 1280];
 const skipTests = [
-  // 'Design: Content resizing: Text must be styled with units that are resizable in all browsers',
-  // 'Structure: Containers and landmarks: Exactly one main landmark',
+  'Design: Content resizing: Text must be styled with units that are resizable in all browsers',
 ];
 const urls = [
   'http://localhost:7080/article/scenario-25',
