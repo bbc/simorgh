63b5fb4603843084630ed85230a12429fdb76c90
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-02 16:39:25 +0100

    Add requiredPropCheck test file

---
+++ src/app/models/propTypes/general/requiredPropCheck.test.js
@@ -0,0 +1,7 @@
+import requiredPropCheck from './requiredPropCheck';
+
+describe('requiredPropCheck', () => {
+  it('should return an array of missing required props', () => {});
+
+  it('should return an empty array of no missing required props', () => {});
+});
