5ac4485f4f8e095adfb446603761a12b1c458e58
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-02 17:28:17 +0100

    Add tests for requiredPropCheck

--- src/app/models/propTypes/general/requiredPropCheck.test.js
+++ src/app/models/propTypes/general/requiredPropCheck.test.js
@@ -1,7 +1,48 @@
 import requiredPropCheck from './requiredPropCheck';
 
 describe('requiredPropCheck', () => {
-  it('should return an array of missing required props', () => {});
+  const props = [
+    { type: 'propOne' },
+    { type: 'propTwo' },
+    { type: 'propThree' },
+  ];
 
-  it('should return an empty array of no missing required props', () => {});
+  const types = [
+    { type: 'propOne', isRequired: true },
+    { type: 'propTwo', isRequired: true },
+    { type: 'propThree', isRequired: false },
+  ];
+
+  const testRequiredPropCheck = (propTypes, expectedObject) => {
+    const result = requiredPropCheck(propTypes, types);
+
+    expect(result).toEqual(expectedObject);
+  };
+
+  describe('missing props', () => {
+    it('should return an array of missing required props', () => {
+      /* eslint-disable react/destructuring-assignment */
+      const propsWithMissingRequiredProps = props.slice(2);
+
+      testRequiredPropCheck(propsWithMissingRequiredProps, [
+        'propOne',
+        'propTwo',
+      ]);
+    });
+
+    describe('missing non-required props', () => {
+      it('should return an array void of missing non-required props', () => {
+        /* eslint-disable react/destructuring-assignment */
+        const propsWithNonRequiredProps = props.slice(0, 1);
+
+        testRequiredPropCheck(propsWithNonRequiredProps, ['propTwo']);
+      });
+    });
+  });
+
+  describe('if no required props are missing', () => {
+    it('should return an empty arrays', () => {
+      testRequiredPropCheck(props, []);
+    });
+  });
 });
