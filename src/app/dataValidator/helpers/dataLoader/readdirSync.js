5372ee3c20fe88498d55d8fe6754ebf54d7d63b6
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-08-03 16:31:08 +0100

    test promise is returned; test readScenario & fileToValidate

--- src/app/dataValidator/helpers/dataLoader/readdirSync.js
+++ src/app/dataValidator/helpers/dataLoader/readdirSync.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
-const { readScenario } = require('./readScenario');
+// don't use destructuring assignment otherwise jest spy does not work
+const readScenario = require('./readScenario');
 
 module.exports.readdirSync = dirName => {
   const fileNames = fs.readdirSync(dirName);
 
   return Promise.all(
-    fileNames.map(fileName => readScenario(fileName, dirName)),
+    fileNames.map(fileName => readScenario.readScenario(fileName, dirName)),
   );
 };
--- src/app/dataValidator/helpers/dataLoader/readdirSync.test.js
+++ src/app/dataValidator/helpers/dataLoader/readdirSync.test.js
@@ -2,11 +2,24 @@ global.console.log = jest.fn(); // silence console.log during jest tests
 global.console.time = jest.fn(); // silence console.time during jest tests
 
 const { readdirSync } = require('./readdirSync');
+const readScenario = require('./readScenario');
 
 describe('readdirSync helper', () => {
-  it('should error if directory passed to readdirSync does not exist', () => {
-    expect(() => {
-      readdirSync('./././data');
-    }).not.toThrowError();
+  it('should call readScenario for every file in the /data directory', () => {
+    const readScenarioSpy = jest.spyOn(readScenario, 'readScenario');
+    return readdirSync('./././data').then(() => {
+      expect(readScenarioSpy).toHaveBeenCalledTimes(29);
+    });
+  });
+
+  it('should call fileToValidate for only the valid json file in the /data directory', () => {
+    const fileToValidateSpy = jest.spyOn(readScenario, 'fileToValidate');
+    return readdirSync('./././data').then(() => {
+      expect(fileToValidateSpy).toHaveBeenCalledTimes(25);
+    });
+  });
+
+  it('should return a promise', () => {
+    expect(readdirSync('./././data') instanceof Promise).toEqual(true);
   });
 });
