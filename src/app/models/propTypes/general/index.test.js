caca2599c9debde15585682a6df5a4968649fe91
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-06 15:16:16 +0100

    Init of logic to test

---
+++ src/app/models/propTypes/general/index.test.js
@@ -0,0 +1,7 @@
+describe('arrayOfSpecificBlocks', () => {
+  it('should return an error if prop is not an array', () => {});
+
+  it('should return an error if required props are missing', () => {});
+
+  it('should check the type of each item in prop array', () => {});
+});
