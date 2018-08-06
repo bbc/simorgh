e539d1ee4540b7501e9c3d3d26045f24dd07bf05
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-06 17:20:27 +0100

    Check that error is thrown for missing props

--- src/app/models/propTypes/general/index.test.js
+++ src/app/models/propTypes/general/index.test.js
@@ -1,4 +1,12 @@
 import { arrayOfSpecificBlocks } from './index';
+import * as requiredPropCheck from './requiredPropCheck';
+
+const propData = {
+  testProp: [
+    { type: 'propOne', value: 'Some data' },
+    { type: 'propTwo', value: 'Some More data' },
+  ],
+};
 
 const propTypes = [
   { type: 'propOne', props: { propWouldBe: 'here' }, isRequired: false },
@@ -11,10 +19,26 @@ describe('arrayOfSpecificBlocks', () => {
   it('should return an error if prop is not an array', () => {
     const propCheck = checkProps({ testProp: {} });
 
-    expect(propCheck).toEqual(Error('Invalid props: 0 is not an array.'));
+    expect(propCheck).toEqual(
+      Error('Invalid props: testProp is not an array.'),
+    );
   });
 
-  it('should return an error if required props are missing', () => {});
+  describe('missing required props', () => {
+    beforeEach(() => {
+      requiredPropCheck.default = jest
+        .fn()
+        .mockReturnValueOnce(['missingProp', 'missingPropTwo']);
+    });
+
+    it('should return an error', () => {
+      const propCheck = checkProps(propData);
+
+      expect(propCheck).toEqual(
+        Error('Missing required props: missingProp,missingPropTwo'),
+      );
+    });
+  });
 
   it('should check the type of each item in prop array', () => {});
 });
