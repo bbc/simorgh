ac7a0ec20a5c8a557318c2cf21795e2ed373a62b
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-06 16:14:57 +0100

    Test returns error if not an array

--- src/app/models/propTypes/general/index.test.js
+++ src/app/models/propTypes/general/index.test.js
@@ -1,5 +1,19 @@
+import { arrayOfSpecificBlocks } from './index';
+
 describe('arrayOfSpecificBlocks', () => {
-  it('should return an error if prop is not an array', () => {});
+  const propTypes = [
+    { type: 'propOne', props: { propWouldBe: 'here' }, isRequired: false },
+    { type: 'propTwo', props: { propWouldBe: 'here' }, isRequired: true },
+  ];
+
+  it('should return an error if prop is not an array', () => {
+    const propCheck = arrayOfSpecificBlocks(propTypes)(
+      { propThatIsntAnArray: {} },
+      0,
+    );
+
+    expect(propCheck).toEqual(Error('Invalid props: 0 is not an array.'));
+  });
 
   it('should return an error if required props are missing', () => {});
 
