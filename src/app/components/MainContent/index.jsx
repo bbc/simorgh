8efddf0f475885a201e1eb7d1eadce969355ec16
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-07 21:17:42 +0100

    Implement grid on Header and MainContent

--- src/app/components/MainContent/index.jsx
+++ src/app/components/MainContent/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import { arrayOf, element } from 'prop-types';
+import { layoutGridWrapper, layoutGridMain } from '../../lib/layoutGrid';
 
-const MainContent = ({ children }) => <main role="main">{children}</main>;
+const StyledMainContent = styled.main`
+  ${layoutGridWrapper};
+`;
+
+const StyledDiv = styled.div`
+  ${layoutGridMain};
+`;
+
+const MainContent = ({ children }) => (
+  <StyledMainContent role="main">
+    <StyledDiv>{children}</StyledDiv>
+  </StyledMainContent>
+);
 
 MainContent.propTypes = {
   children: arrayOf(element).isRequired,
