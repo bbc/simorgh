a7adfbe36f67f47cdefac5378924bcd3610f4e8a
Jonthan Mark Perry <perryj39@mc-s080830.local>
2018-08-08 10:52:19 +0100

    Increased max file size limit in webpack to 410kb to resolve failing CI build

--- razzle.config.js
+++ razzle.config.js
@@ -29,8 +29,8 @@ module.exports = {
     // This is to override bundle performance test
     if (process.env.CI) {
       appConfig.performance = {
-        maxAssetSize: 400000,
-        maxEntrypointSize: 400000,
+        maxAssetSize: 410000,
+        maxEntrypointSize: 410000,
       };
     }
 
