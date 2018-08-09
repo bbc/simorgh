0421eccecf2bd11232aebb5b6d3b1af5e5051417
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-09 15:52:36 +0100

    Add size hash regex

--- cypress/integration/metaSpec.js
+++ cypress/integration/metaSpec.js
@@ -62,7 +62,7 @@ describe('Article Meta Tests', () => {
     cy.get('script')
       .last()
       .should('have.attr', 'src')
-      .and('match', /(\/static\/js\/client\.\w+\.js)/g);
+      .and('match', /(\/static\/js\/client-\w+\.\w+\.js)/g);
   });
 
   it('should have resource hints', () => {
