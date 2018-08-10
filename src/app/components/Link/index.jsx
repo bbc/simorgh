67faae3b064a0140e14caa8c9415a529d0c70e42
Andrzej Pogonowski <pogon.ap@hotmail.com>
2018-08-10 15:02:51 +0100

    Refactored link to use props for inline style

--- src/app/components/Link/index.jsx
+++ src/app/components/Link/index.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
-import { string } from 'prop-types';
+import { string, boolean } from 'prop-types';
 import { C_WHITE, GEL_SPACING } from '../../lib/constants/styles';
+
+const getInline = props => {
+  const { inline } = props;
+  return inline;
+};
 
 const StyledLink = styled.a`
   color: ${C_WHITE};
   font-weight: 700; /* Used instead of Reith Sans Bold since it is not worth the performance cost in this case. */
   text-decoration: none;
   padding: ${GEL_SPACING}px 0;
-  display: block;
+  display: ${props => (getInline(props) === true ? 'inline' : 'block')};
   &:hover,
   &:focus {
     text-decoration: underline;
   }
 `;
 
-const Link = ({ text, href }) => <StyledLink href={href}>{text}</StyledLink>;
+const Link = ({ text, href, inline }) => (
+  <StyledLink inline={inline} href={href}>
+    {text}
+  </StyledLink>
+);
+
+Link.defaultProps = {
+  inline: false,
+};
 
 Link.propTypes = {
   href: string.isRequired,
   text: string.isRequired,
+  inline: boolean,
 };
 
 export default Link;
