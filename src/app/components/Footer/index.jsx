6ac97bf8ec0065e270d6f80cfe3515c50162c72d
Andrzej Pogonowski <pogon.ap@hotmail.com>
2018-08-10 15:25:04 +0100

    Refactored Footer to use the FooterList and styled the external link to be inline

--- src/app/components/Footer/index.jsx
+++ src/app/components/Footer/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { arrayOf, shape, string } from 'prop-types';
 import Link from '../Link';
+import FooterList from '../FooterList';
 import {
   C_ORBIT_GREY,
   C_WHITE,
@@ -16,61 +17,6 @@ const StyledFooter = styled.footer`
   padding: 0 ${GEL_SPACING}px;
 `;
 
-const StyledList = styled.ul`
-  border-bottom: 1px solid ${C_WHITE};
-  display: grid;
-  grid-auto-flow: column;
-  grid-template-columns: repeat(2, 50%);
-  grid-template-rows: repeat(4, 50%);
-  list-style-type: none;
-  margin: 0;
-  padding: ${GEL_SPACING}px 0;
-  > li:first-child {
-    border-bottom: 1px solid ${C_WHITE};
-    grid-column: 1/3;
-    padding-bottom: ${GEL_SPACING}px;
-    margin-bottom: ${GEL_SPACING}px;
-    @supports not (display: grid) {
-      width: 100%;
-    }
-  }
-`;
-
-const StyledListItem = styled.li`
-  min-width: 50%;
-  @supports not (display: grid) {
-    display: inline-block;
-  }
-`;
-
-// const StyledLink = styled.a`
-//   color: ${C_WHITE};
-//   font-weight: 700; /* Used instead of Reith Sans Bold since it is not worth the performance cost in this case. */
-//   text-decoration: none;
-//   padding: ${GEL_SPACING}px 0;
-//   display: block;
-
-//   &:hover,
-//   &:focus {
-//     text-decoration: underline;
-//   }
-// `;
-
-// const InlineBlockLink = styled(Link)`
-//   display: inline;
-//   padding: ${GEL_SPACING}px 0;
-// `;
-
-const BlockLink = styled(Link)`
-  display: block;
-  padding: ${GEL_SPACING}px 0;
-`;
-
-const InlineBlockLink = styled(Link)`
-  display: inline;
-  padding: ${GEL_SPACING}px 0;
-`;
-
 const StyledParagraph = styled.p`
   color: ${C_WHITE};
   padding: ${GEL_SPACING_DBL}px 0;
@@ -79,18 +25,10 @@ const StyledParagraph = styled.p`
 
 const Footer = ({ links, copyrightText, externalLink }) => (
   <StyledFooter role="contentinfo">
-    <StyledList role="list">
-      {links.map((link, index) => (
-        // It is redundant to add ids when list items are static, have no ids by default and are never reordered or filtered
-        // eslint-disable-next-line react/no-array-index-key
-        <StyledListItem key={index} role="listitem">
-          <BlockLink text={link.text} href={link.href} />
-        </StyledListItem>
-      ))}
-    </StyledList>
+    <FooterList links={links} />
     <StyledParagraph>
       {copyrightText}
-      <InlineBlockLink text={externalLink.text} href={externalLink.href} />
+      <Link text={externalLink.text} href={externalLink.href} inline />
     </StyledParagraph>
   </StyledFooter>
 );
