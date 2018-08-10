dfbad98a16645c444eccb0698e786370f63c16b9
Andrzej Pogonowski <pogon.ap@hotmail.com>
2018-08-10 16:16:15 +0100

    Added snapshot tests for the FooterList and Link

--- src/app/containers/Footer/index.stories.jsx
+++ src/app/containers/Footer/index.stories.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
 import FooterContainer from './index';
 
-storiesOf('Footer Component', module).add('default', () => <FooterContainer />);
+storiesOf('Footer Container', module).add('default', () => <FooterContainer />);
