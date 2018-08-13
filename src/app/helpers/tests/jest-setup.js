bb5a2fa4974a232f3c5b2f32a9868a08ee3e2887
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-13 16:55:42 +0100

    Rename manifest to mock

--- src/app/helpers/tests/jest-setup.js
+++ src/app/helpers/tests/jest-setup.js
@@ -1,4 +1,4 @@
 import fetch from 'jest-fetch-mock'; // eslint-disable-line import/no-extraneous-dependencies
 
 global.fetch = fetch;
-process.env.RAZZLE_ASSETS_MANIFEST = 'manifest.json';
+process.env.RAZZLE_ASSETS_MANIFEST = 'mockBundleAssets.json';
