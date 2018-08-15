895bcdb42f95bf81f3fec86024c87080a6ae84e7
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-08-15 15:13:54 +0100

    update Caption story; update snapshots; add comma back to offscreen text

--- src/app/components/Figure/Caption/index.stories.jsx
+++ src/app/components/Figure/Caption/index.stories.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
+import VisuallyHiddenText from '../../VisuallyHiddenText';
 import Caption from './index';
 
 storiesOf('Caption', module).add('default', () => (
-  <Caption>This is some Caption text</Caption>
+
+  <Caption>
+    <VisuallyHiddenText>Image caption,</VisuallyHiddenText>
+    This is some Caption text
+  </Caption>
 ));
