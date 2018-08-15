906f13b57971c2dac83b5ccdbd101d14fed65077
Jonathan Perry <perryj39@mc-s102677.national.core.bbc.co.uk>
2018-08-15 11:01:34 +0100

    Changed StyledItalic and StyledBold to components from Styled Components

--- src/app/components/Text/index.jsx
+++ src/app/components/Text/index.jsx
@@ -7,35 +7,27 @@ import mediaQuery from '../../helpers/mediaQueries';
 const StyledParagraph = styled.p`
   // Font styling below is a subset of BBC GEL Typography "Body Copy"
   font-size: 0.9375em;
-  line-height: 1.25em;
+  line-height: 1.25rem;
   ${mediaQuery.smartPhoneAndLarger} {
     font-size: 1em;
-    line-height: 1.375em;
+    line-height: 1.375rem;
   }
 `;
-const StyledItalic = styled.i`
-  // Font styling below is a subset of BBC GEL Typography "Body Copy"
-  font-size: 0.9375em;
-  line-height: 1.25em;
-  ${mediaQuery.smartPhoneAndLarger} {
-    font-size: 1em;
-    line-height: 1.375em;
-  }
-`;
-const StyledBold = styled.b`
-  // Font styling below is a subset of BBC GEL Typography "Body Copy"
-  font-size: 0.9375em;
-  line-height: 1.25em;
-  ${mediaQuery.smartPhoneAndLarger} {
-    font-size: 1em;
-    line-height: 1.375em;
-  }
-`;
+const StyledItalic = ({ children }) => <i>{children}</i>;
+const StyledBold = ({ children }) => <b>{children}</b>;
 
 const Text = ({ text, options }) => {
   if (!text) return null;
 
   return <Markdown options={options}>{text}</Markdown>;
+};
+
+StyledItalic.propTypes = {
+  children: string.isRequired,
+};
+
+StyledBold.propTypes = {
+  children: string.isRequired,
 };
 
 Text.propTypes = {
