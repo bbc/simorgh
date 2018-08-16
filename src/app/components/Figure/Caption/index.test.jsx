566880e58676afd800cc612084ee0e4c1d13d997
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-08-16 13:19:39 +0100

    test shouldn't pass text prop

--- src/app/components/Figure/Caption/index.test.jsx
+++ src/app/components/Figure/Caption/index.test.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '../../../helpers/tests/testHelpers';
+import VisuallyHiddenText from '../../VisuallyHiddenText';
 import Caption from './index';
 
 describe('Caption', () => {
   shouldMatchSnapshot(
     'should render correctly',
-    <Caption text="This is some Caption text" />,
+    <Caption>
+      <VisuallyHiddenText>Image caption,</VisuallyHiddenText>
+      This is some Caption text
+    </Caption>,
   );
 });
