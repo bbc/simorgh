2477d4d215f5aaa9f6b7d08b076f6651f6928e95
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-16 14:03:35 +0100

    Add inline-link component

--- src/app/components/Text/index.jsx
+++ src/app/components/Text/index.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { string, bool, shape } from 'prop-types';
 import Markdown from 'markdown-to-jsx';
 import mediaQuery from '../../helpers/mediaQueries';
+import { C_BLUEJAY } from '../../lib/constants/styles';
 
 const StyledParagraph = styled.p`
   // Font styling below is a subset of BBC GEL Typography "Body Copy"
@@ -12,6 +13,12 @@ const StyledParagraph = styled.p`
     font-size: 1em;
     line-height: 1.375rem;
   }
+`;
+
+const InlineLink = styled.a`
+  color: ${C_BLUEJAY};
+  border-bottom: 1px solid ${C_BLUEJAY};
+  text-decoration: none;
 `;
 
 const Text = ({ text, options }) => {
@@ -33,6 +40,9 @@ Text.defaultProps = {
     overrides: {
       p: {
         component: StyledParagraph,
+      },
+      a: {
+        component: InlineLink,
       },
     },
   },
