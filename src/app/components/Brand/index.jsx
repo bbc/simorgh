38a8da4c23aaed738869334b407a5bc36acb23d1
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-08-16 17:18:00 +0100

    brand should use the new VisuallyHiddenText comp

--- src/app/components/Brand/index.jsx
+++ src/app/components/Brand/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import VisuallyHiddenText from '../VisuallyHiddenText';
 import { bool } from 'prop-types';
 import {
   C_POSTBOX,
@@ -27,18 +28,6 @@ const StyledLink = styled.a`
     border-bottom: 4px solid ${C_WHITE};
     padding: ${GEL_SPACING_DBL} 0;
   }
-`;
-
-const VisuallyHiddenText = styled.span`
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  margin: -1px;
-  padding: 0;
-  border: 0;
-  overflow: hidden;
-  clip: rect(0 0 0 0);
-  top: 0;
 `;
 
 const Brand = ({ indentedLogo }) => {
