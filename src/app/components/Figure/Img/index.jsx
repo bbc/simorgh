8a92c3c7f04f26e64d13796b9739a319e6e1572b
Sareh Heidari <sareh.heidari@gmail.com>
2018-08-16 17:22:27 +0100

    In progress

---
+++ src/app/components/Figure/Img/index.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Img = styled.img`
+  display: block;
+  width: 100%;
+`;
+
+export const AmpImg = Img.withComponent('amp-img');
--- src/app/components/Figure/index.jsx
+++ src/app/components/Figure/index.jsx
@@ -1,38 +1,10 @@
-import React from 'react';
-import { string } from 'prop-types';
 import styled from 'styled-components';
-import Caption from './Caption';
 import { GEL_SPACING_DBL } from '../../lib/constants/styles';
 
-const renderCaption = caption =>
-  caption ? <Caption>{caption}</Caption> : null;
-
-const StyledFigure = styled.figure`
+const Figure = styled.figure`
   margin: 0;
   padding-bottom: ${GEL_SPACING_DBL};
   width: 100%;
 `;
-
-const Image = styled.img`
-  display: block;
-  width: 100%;
-`;
-
-const Figure = ({ src, alt, caption }) => (
-  <StyledFigure>
-    <Image src={src} alt={alt} />
-    {renderCaption(caption)}
-  </StyledFigure>
-);
-
-Figure.propTypes = {
-  alt: string.isRequired,
-  src: string.isRequired,
-  caption: string,
-};
-
-Figure.defaultProps = {
-  caption: null,
-};
 
 export default Figure;
