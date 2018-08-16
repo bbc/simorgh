abf2e64f6b07ea05264eec7e7f2144abb5a5aa26
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-16 13:55:49 +0100

    Export bluejay color

--- src/app/lib/constants/styles.js
+++ src/app/lib/constants/styles.js
@@ -6,6 +6,7 @@ export const C_EBON = '#222222';
 export const C_POSTBOX = '#B80000';
 export const C_STORM = '#404040';
 export const C_WHITE = '#FFFFFF';
+export const C_BLUEJAY = '#0f556c';
 
 // Colours from other services
 export const C_ORBIT_GREY = '#4C4C4C';
