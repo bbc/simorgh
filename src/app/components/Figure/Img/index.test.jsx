bbfc8754c535cde9a29f46bc4b15e98c0a6d29e8
Sareh Heidari <sareh.heidari@gmail.com>
2018-08-17 17:42:05 +0100

    In progress

---
+++ src/app/components/Figure/Img/index.test.jsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import { shouldMatchSnapshot } from '../../../helpers/tests/testHelpers';
+// import { Img, AmpImg } from './index';
+
+// describe('Img', () => {
+//   describe('Img', () => {
+//     shouldMatchSnapshot('should render correctly', <Img />);
+//   });
+
+//   describe('Amp Img', () => {
+//     shouldMatchSnapshot('should render correctly', <AmpImg />);
+//   });
+// });
