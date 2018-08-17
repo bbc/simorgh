c4bf373e994e5cc78626c48167221d70e9c30927
Sareh Heidari <sareh.heidari@gmail.com>
2018-08-17 17:42:05 +0100

    In progress

---
+++ src/app/components/Figure/Img/index.stories.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
+// import { Img, AmpImg } from './index';
+
+// const image = {
+//   alt:
+//     'Map of the UK displaying Syrian refugees and asylum seekers per 10000 population. Ranges from 0 to 17.',
+//   src:
+//     'https://ichef.bbci.co.uk/news/640/cpsprodpb/439A/production/_100960371_syrians_and_asylum_v2-nc.png',
+// };
+
+// storiesOf('Img', module)
+//   .add('Img', () => <Img {...image} />)
+//   .add('Amp Img', () => <AmpImg {...image} />);
