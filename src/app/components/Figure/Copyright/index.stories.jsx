c2446970d556556e0f1189004f31a469b6016c90
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-08-17 12:11:01 +0100

    copy&paste DOH

--- src/app/components/Figure/Copyright/index.stories.jsx
+++ src/app/components/Figure/Copyright/index.stories.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
 import Copyright from './index';
 
-storiesOf('Caption', module).add('default', () => (
+storiesOf('Copyright', module).add('default', () => (
   <Copyright>Getty images</Copyright>
 ));
