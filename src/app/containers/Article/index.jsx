cfe0a6a7df85785f2209eab9ad28422fcb97f800
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-20 19:44:01 +0100

    Remove static getInitialProps from Article

--- src/app/containers/Article/index.jsx
+++ src/app/containers/Article/index.jsx
@@ -1,55 +1,21 @@
-import React, { Component } from 'react';
-import 'isomorphic-fetch';
+import React from 'react';
 import Article from '../../components/Article';
 import articlePropTypes from '../../models/propTypes/article';
 
-class ArticleContainer extends Component {
-  static async getInitialProps({ req, match } = {}) {
-    try {
-      const { id } = match.params;
+const ArticleContainer = ({ data }) => {
+  const { content, metadata, promo } = data;
+  const { id: aresArticleId } = metadata;
 
-      const regex = '^(c[a-zA-Z0-9]{10}o)$';
-      const routeMatches = id.match(regex);
-
-      if (!routeMatches) {
-        throw new Error(
-          `Invalid route parameter: ${id}. ID parameter must be in format 'c[xxxxxxxxxx]o', where the middle part could be 0000000001 to 0000000027.`,
-        );
-      }
-
-      let url = `/data/${id}.json`;
-
-      if (req) {
-        url = `${process.env.RAZZLE_BASE_PATH}${url}`;
-      }
-
-      const response = await fetch(url);
-      const data = await response.json();
-
-      return { data };
-    } catch (error) {
-      console.log(error); // eslint-disable-line no-console
-      return {};
-    }
-  }
-
-  render() {
-    const { data } = this.props;
-    const { content, metadata, promo } = data;
-    const { id: aresArticleId } = metadata;
-
-    const id = aresArticleId.split(':').pop();
-
-    return (
-      <Article
-        id={id}
-        lang={metadata.passport.language}
-        title={promo.headlines.seoHeadline}
-        {...content.model}
-      />
-    );
-  }
-}
+  const id = aresArticleId.split(':').pop();
+  return (
+    <Article
+      id={id}
+      lang={metadata.passport.language}
+      title={promo.headlines.seoHeadline}
+      {...content.model}
+    />
+  );
+};
 
 ArticleContainer.propTypes = articlePropTypes;
 
