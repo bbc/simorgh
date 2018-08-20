2b3af5e00b10bf777d21f53db871a8d6968492af
Jordan Tart <jordan.tart@bbc.co.uk>
2018-08-20 13:04:41 +0100

    Extract InlineLink

--- src/app/components/Text/index.jsx
+++ src/app/components/Text/index.jsx
@@ -3,12 +3,7 @@ import styled from 'styled-components';
 import { string, bool, shape } from 'prop-types';
 import Markdown from 'markdown-to-jsx';
 import mediaQuery from '../../helpers/mediaQueries';
-import {
-  C_BLUEJAY,
-  C_BLUEJAY_LHT,
-  C_OAT_LHT,
-  C_PEBBLE,
-} from '../../lib/constants/styles';
+import InlineLink from '../InlineLink';
 
 const StyledParagraph = styled.p`
   // Font styling below is a subset of BBC GEL Typography "Body Copy"
@@ -17,27 +12,6 @@ const StyledParagraph = styled.p`
   ${mediaQuery.smartPhoneAndLarger} {
     font-size: 1em;
     line-height: 1.375rem;
-  }
-`;
-
-const InlineLink = styled.a`
-  color: ${C_BLUEJAY};
-  padding: 2px;
-  border-bottom: 1px solid ${C_BLUEJAY};
-  text-decoration: none;
-  &:visited {
-    color: ${C_PEBBLE};
-    border-bottom: 1px solid ${C_PEBBLE};
-  }
-  &:focus {
-    background-color: ${C_BLUEJAY};
-    border-bottom: 1px solid ${C_OAT_LHT};
-    color: ${C_OAT_LHT};
-  }
-  &:hover {
-    color: ${C_BLUEJAY};
-    background-color: ${C_BLUEJAY_LHT};
-    border-bottom: 1px solid ${C_BLUEJAY};
   }
 `;
 
