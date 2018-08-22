82055acf7e3f7daafb4430042a3734d8ea92c853
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-08-22 17:07:28 +0100

    pre fix the constant and add a explaining note

--- src/app/lib/constants/typography.js
+++ src/app/lib/constants/typography.js
@@ -1,8 +1,15 @@
+// Note - this file contains the css for the various typography groups we
+// use as defined in the GEL specs. Further details in /src/app/lib/README.md
+//
+// The typography constants are defined seperately to those in styles.js
+// as there are many typography groups and they differ per service meaning
+// there is scope for the typography definitions being isolated from the
+// other constants
+
 import { css } from 'styled-components';
 import mediaQuery from '../../helpers/mediaQueries';
 
-// Font styling below is a subset of BBC GEL Typography "Long Primer"
-const LONG_PRIMER = css`
+const T_LONG_PRIMER = css`
   font-size: 0.9375em;
   line-height: 1.125rem;
 
@@ -11,4 +18,4 @@ const LONG_PRIMER = css`
   }
 `;
 
-export default LONG_PRIMER;
+export default T_LONG_PRIMER;
