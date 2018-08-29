d5ad7fc1d53988a2961fbd648ca59fa894df64be
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-08-29 18:05:56 +0100

    create fixture data to handle CodeClimate duplication warnings

--- src/app/components/Figure/index.stories.jsx
+++ src/app/components/Figure/index.stories.jsx
@@ -1,39 +1,16 @@
-import React from 'react';
 import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
-import Figure from './index';
-import Image from './Image';
-import VisuallyHiddenText from '../VisuallyHiddenText';
-import Caption from './Caption';
-
-const imageAlt =
-  'Map of the UK displaying Syrian refugees and asylum seekers per 10000 population. Ranges from 0 to 17.';
-const imageSrc =
-  'https://ichef.bbci.co.uk/news/640/cpsprodpb/439A/production/_100960371_syrians_and_asylum_v2-nc.png';
-const captionValue = 'This is a caption';
-const copyrightText = 'Copyright Getty images';
+import {
+  FigureImage,
+  FigureImageWithCaption,
+  FigureImageWithCopyright,
+  FigureImageWithCopyrightAndCaption,
+} from './fixtureData';
 
 storiesOf('Figure', module)
-  .add('with a caption', () => (
-    <Figure>
-      <Image alt={imageAlt} src={imageSrc} />
-      <Caption>{captionValue}</Caption>
-    </Figure>
-  ))
-  .add('without a caption', () => (
-    <Figure>
-      <Image alt={imageAlt} src={imageSrc} />
-    </Figure>
-  ))
-  .add('with non-BBC copyright', () => (
-    <Figure>
-      <Image alt={imageAlt} src={imageSrc} />
-      <VisuallyHiddenText>{copyrightText}</VisuallyHiddenText>
-    </Figure>
-  ))
-  .add('with a caption and non-BBC copyright', () => (
-    <Figure>
-      <Image alt={imageAlt} src={imageSrc} />
-      <VisuallyHiddenText>{copyrightText}</VisuallyHiddenText>
-      <Caption>{captionValue}</Caption>
-    </Figure>
-  ));
+  .add('with a caption', () => FigureImageWithCaption)
+  .add('without a caption', () => FigureImage)
+  .add('with non-BBC copyright', () => FigureImageWithCopyright)
+  .add(
+    'with a caption and non-BBC copyright',
+    () => FigureImageWithCopyrightAndCaption,
+  );
--- src/app/components/Figure/index.test.jsx
+++ src/app/components/Figure/index.test.jsx
@@ -1,55 +1,28 @@
-import React from 'react';
 import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
-import Figure from './index';
-import Image from './Image';
-import Caption from './Caption';
-import VisuallyHiddenText from '../VisuallyHiddenText';
-
-const imageAlt =
-  'Map of the UK displaying Syrian refugees and asylum seekers per 10000 population. Ranges from 0 to 17.';
-const imageSrc =
-  'https://ichef.bbci.co.uk/news/640/cpsprodpb/439A/production/_100960371_syrians_and_asylum_v2-nc.png';
-const captionValue = 'This is a caption';
-const copyrightText = 'Copyright Getty images';
+import {
+  FigureImage,
+  FigureImageWithCaption,
+  FigureImageWithCopyright,
+  FigureImageWithCopyrightAndCaption,
+} from './fixtureData';
 
 describe('Figure', () => {
   describe('with a caption', () => {
-    shouldMatchSnapshot(
-      'should render correctly',
-      <Figure>
-        <Image alt={imageAlt} src={imageSrc} />
-        <Caption>{captionValue}</Caption>
-      </Figure>,
-    );
+    shouldMatchSnapshot('should render correctly', FigureImageWithCaption);
   });
 
   describe('without a caption', () => {
-    shouldMatchSnapshot(
-      'should render correctly',
-      <Figure>
-        <Image alt={imageAlt} src={imageSrc} />
-      </Figure>,
-    );
+    shouldMatchSnapshot('should render correctly', FigureImage);
   });
 
   describe('with non-BBC copyright', () => {
-    shouldMatchSnapshot(
-      'should render correctly',
-      <Figure>
-        <Image alt={imageAlt} src={imageSrc} />
-        <VisuallyHiddenText>{copyrightText}</VisuallyHiddenText>
-      </Figure>,
-    );
+    shouldMatchSnapshot('should render correctly', FigureImageWithCopyright);
   });
 
   describe('with caption and non-BBC copyright', () => {
     shouldMatchSnapshot(
       'should render correctly',
-      <Figure>
-        <Image alt={imageAlt} src={imageSrc} />
-        <VisuallyHiddenText>{copyrightText}</VisuallyHiddenText>
-        <Caption>{captionValue}</Caption>
-      </Figure>,
+      FigureImageWithCopyrightAndCaption,
     );
   });
 });
