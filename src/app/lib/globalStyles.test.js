e1cf0f15fe67bf883962706aeb8be0bbdae8c914
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-09-03 16:43:23 +0100

    does this run

--- src/app/lib/globalStyles.test.js
+++ src/app/lib/globalStyles.test.js
@@ -4,7 +4,11 @@ describe('globalStyles', () => {
   it('should call injectGlobal with args matching snapshot', async () => {
     jest.spyOn(styledComponents, 'injectGlobal');
 
+    console.log('globalStyles');
+
     await import('./globalStyles');
+
+    console.log('after await globalStyles');
 
     const injectGlobalArgs = styledComponents.injectGlobal.mock.calls[0];
 
