4c6494e90d14248ed858a9799bc38424a83b0182
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-09-03 12:27:49 +0100

    export named functions

--- src/app/components/Figure/Image/index.jsx
+++ src/app/components/Figure/Image/index.jsx
@@ -1,17 +1,8 @@
-import React from 'react';
-import { string } from 'prop-types';
 import styled from 'styled-components';
 
-const StyledImage = styled.img`
+const Image = styled.img`
   display: block;
   width: 100%;
 `;
-
-const Image = ({ src, alt }) => <StyledImage alt={alt} src={src} />;
-
-Image.propTypes = {
-  alt: string.isRequired,
-  src: string.isRequired,
-};
 
 export default Image;
--- src/app/components/Figure/index.jsx
+++ src/app/components/Figure/index.jsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 import { GEL_SPACING_DBL } from '../../lib/constants/styles';
 
-export default styled.figure`
+const Figure = styled.figure`
   margin: 0;
   padding-bottom: ${GEL_SPACING_DBL};
   width: 100%;
 `;
+
+export default Figure;
