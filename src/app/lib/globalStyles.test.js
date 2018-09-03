3f1d6b57e22abc9ccfe69576032db277520951b0
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-09-03 17:29:33 +0100

    try the fix

--- src/app/lib/globalStyles.test.js
+++ src/app/lib/globalStyles.test.js
@@ -4,13 +4,9 @@ describe('globalStyles', () => {
   it('should call injectGlobal with args matching snapshot', async () => {
     jest.spyOn(styledComponents, 'injectGlobal');
 
-    console.log('globalStyles');
-
     await import('./globalStyles');
 
-    console.log('after await globalStyles');
-
-    const injectGlobalArgs = styledComponents.injectGlobal.mock.calls[0];
+    const injectGlobalArgs = styledComponents['injectGlobal'].mock.calls[0]; // eslint-disable-line dot-notation
 
     expect(injectGlobalArgs).toMatchSnapshot();
   });
