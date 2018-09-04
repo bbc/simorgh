8f6d820ac561a8d38fc9e9844bfdad280621ee67
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-09-04 17:47:26 +0100

    bump package-lock

--- src/app/lib/globalStyles.test.js
+++ src/app/lib/globalStyles.test.js
@@ -3,9 +3,7 @@ import * as styledComponents from 'styled-components';
 describe('globalStyles', () => {
   it('should call injectGlobal with args matching snapshot', async () => {
     jest.spyOn(styledComponents, 'injectGlobal');
-
     await import('./globalStyles');
-
     const injectGlobalArgs = styledComponents.injectGlobal.mock.calls[0];
 
     expect(injectGlobalArgs).toMatchSnapshot();
