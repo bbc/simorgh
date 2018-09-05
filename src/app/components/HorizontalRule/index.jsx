3995a42a2df4f84dc93fcb146ba688857339f20e
Jordan Tart <jordan.tart@bbc.co.uk>
2018-09-05 14:38:42 +0100

    Add horizontal rule component

---
+++ src/app/components/HorizontalRule/index.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const HorizontalRule = styled.hr`
+  color: #b80000;
+  height: 4px;
+  width: 48px;
+  padding: 16px 0;
+`;
+
+export default HorizontalRule;
