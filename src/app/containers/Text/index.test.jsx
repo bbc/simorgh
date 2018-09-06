615c4de38071014c4615b8df5683117af4d0c226
Jordan Tart <jordan.tart@bbc.co.uk>
2018-09-06 14:07:46 +0100

    Wrap data tests in data block

--- src/app/containers/Text/index.test.jsx
+++ src/app/containers/Text/index.test.jsx
@@ -6,37 +6,39 @@ import {
 import TextContainer from './index';
 
 describe('TextContainer', () => {
-  const paragraphBlock = (blockId, text) => ({
-    blockId,
-    type: 'paragraph',
-    model: {
-      text,
-    },
-  });
-
-  const data = {
-    blocks: [
-      paragraphBlock('01', 'This is a 1st paragraph block.'),
-      paragraphBlock('02', 'This is a 2nd paragraph block.'),
-      paragraphBlock('03', 'This is a 3rd paragraph block.'),
-      paragraphBlock('04', 'This is a 4th paragraph block..'),
-      paragraphBlock('05', 'This is a 5th paragraph block.'),
-    ],
-  };
-
   describe('with no data', () => {
     isNull('should return null', <TextContainer />);
   });
 
-  describe('with a passed previous block type', () => {
+  describe('with data', () => {
+    const paragraphBlock = (blockId, text) => ({
+      blockId,
+      type: 'paragraph',
+      model: {
+        text,
+      },
+    });
+
+    const data = {
+      blocks: [
+        paragraphBlock('01', 'This is a 1st paragraph block.'),
+        paragraphBlock('02', 'This is a 2nd paragraph block.'),
+        paragraphBlock('03', 'This is a 3rd paragraph block.'),
+        paragraphBlock('04', 'This is a 4th paragraph block..'),
+        paragraphBlock('05', 'This is a 5th paragraph block.'),
+      ],
+    };
+
     shouldShallowMatchSnapshot(
       'should render correctly',
-      <TextContainer {...data} type="text" typeOfPreviousBlock="text" />,
+      <TextContainer {...data} />,
     );
-  });
 
-  shouldShallowMatchSnapshot(
-    'should render correctly',
-    <TextContainer {...data} />,
-  );
+    describe('with a passed previous block type', () => {
+      shouldShallowMatchSnapshot(
+        'should render correctly',
+        <TextContainer {...data} type="text" typeOfPreviousBlock="text" />,
+      );
+    });
+  });
 });
