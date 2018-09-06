23baba99672b000d46629d697ee0e48d50560bcb
Jordan Tart <jordan.tart@bbc.co.uk>
2018-09-06 14:16:59 +0100

    Move conditioanl to a func and hard-code the type

--- src/app/containers/Text/index.jsx
+++ src/app/containers/Text/index.jsx
@@ -3,10 +3,12 @@ import { string } from 'prop-types';
 import { textModelPropTypes } from '../../models/propTypes/text';
 import Text from '../../components/Text';
 
-const TextContainer = ({ type, blocks, typeOfPreviousBlock }) => {
+const shouldPrependHR = typeOfPreviousBlock => typeOfPreviousBlock === 'text';
+
+const TextContainer = ({ blocks, typeOfPreviousBlock }) => {
   if (!blocks) return null;
 
-  const HorizontalRule = typeOfPreviousBlock === type ? <hr /> : null;
+  const HorizontalRule = shouldPrependHR(typeOfPreviousBlock) ? <hr /> : null;
 
   const textBlocks = blocks.map(({ blockId, model }) => (
     <Text key={blockId} {...model} />
