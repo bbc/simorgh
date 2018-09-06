668915ecc3bf0dbf68e5a612528860ecdacd5d6d
Jordan Tart <jordan.tart@bbc.co.uk>
2018-09-06 11:07:04 +0100

    Use em, not px for margin

--- src/app/components/HorizontalRule/index.jsx
+++ src/app/components/HorizontalRule/index.jsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
-import { C_POSTBOX } from '../../lib/constants/styles';
+import { C_POSTBOX, GEL_SPACING_DBL } from '../../lib/constants/styles';
 
 const HorizontalRule = styled.hr`
   background-color: ${C_POSTBOX};
   height: 4px;
   width: 48px;
-  margin: 16px 0;
+  margin: ${GEL_SPACING_DBL} 0;
   border: 0;
 `;
 
