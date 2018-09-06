f5567134049c2f74a4946a878f3a28170c452001
Jordan Tart <jordan.tart@bbc.co.uk>
2018-09-06 10:12:14 +0100

    Add stories

---
+++ src/app/components/HorizontalRule/index.stories.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
+import HorizontalRule from './index';
+
+storiesOf('HorizontalRule', module).add('default', () => <HorizontalRule />);
