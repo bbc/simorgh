d0376e23f64654653f159be2eeadf4e9f68a1155
Sareh Heidari <sareh.heidari@gmail.com>
2018-09-11 17:31:22 +0100

    Remove Article component - move Header & Footer into Article Container

--- src/app/components/Article/index.stories.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
-import Article from './index';
-
-storiesOf('Article', module).add('default', () => <Article />);
