d15118c74838be74ca86e27b83900971236fa4db
David Boydell <david.boydell@bbc.co.uk>
2018-09-11 10:01:59 +0100

    Adding intial Routing test

---
+++ cypress/support/testData/services.js
@@ -0,0 +1,6 @@
+const services = [
+  { name: '/news', path: '/articles/c0000000027o', lang: 'en-gb' },
+  { name: '/persian', path: '/articles/c0000000028o', lang: 'fa' },
+];
+
+export default services;
