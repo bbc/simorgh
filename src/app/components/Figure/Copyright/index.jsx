3e0ee164c1a9cef487a4197dac7166287f34a43d
Ben Newcombe <bcmn96@gmail.com>
2018-09-12 16:53:55 +0100

    Adds visually hidden text to include "Image source" terminology for AT users

--- src/app/components/Figure/Copyright/index.jsx
+++ src/app/components/Figure/Copyright/index.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { node } from 'prop-types';
 import styled from 'styled-components';
 import {
   FF_NEWS_SANS_REG,
@@ -6,8 +8,9 @@ import {
   GEL_SPACING,
 } from '../../../lib/constants/styles';
 import { T_MINION } from '../../../lib/constants/typography';
+import VisuallyHiddenText from '../../VisuallyHiddenText';
 
-const Copyright = styled.span.attrs({
+const WrapperSpan = styled.span.attrs({
   role: 'text',
 })`
   ${T_MINION};
@@ -20,13 +23,24 @@ const Copyright = styled.span.attrs({
   right: 0;
 
   /*
-    The hidden '.' is required for accessibility reasons
-    https://github.com/bbc/simorgh/issues/456#issuecomment-419405600
-  */
+The hidden '.' is required for accessibility reasons
+https://github.com/bbc/simorgh/issues/456#issuecomment-419405600
+*/
   &::after {
     content: '.';
     color: ${C_EBON}; /* This needs to match the background-color */
   }
 `;
+
+const Copyright = ({ children }) => (
+  <WrapperSpan>
+    <VisuallyHiddenText>Image source,</VisuallyHiddenText>
+    {children}
+  </WrapperSpan>
+);
+
+Copyright.propTypes = {
+  children: node.isRequired,
+};
 
 export default Copyright;
