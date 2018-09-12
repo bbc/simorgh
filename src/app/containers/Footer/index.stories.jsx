7ac5c3932c840fa315079a713211741d03c09fdb
Benjamin Hobbs <bjhobbs06@gmail.com>
2018-09-12 11:28:59 +0100

    remove container stories

--- src/app/containers/Footer/index.stories.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
-import FooterContainer from './index';
-
-storiesOf('Footer Container', module).add('default', () => <FooterContainer />);
