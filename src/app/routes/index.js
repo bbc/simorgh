bda0ff1f3d7272c9767cabc747dc27bc8fa659d5
Jordan Tart <jordan.tart@bbc.co.uk>
2018-09-12 11:57:33 +0100

    Move data fetching to route and add tests/helpers

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,54 +1,5 @@
-import 'isomorphic-fetch';
 import Article from '../containers/Article';
-import isAmpPath from '../helpers/isAmpPath';
-
-const isServer = () => typeof window === 'undefined';
-
-const validateService = service => {
-  const services = ['news', 'persian'];
-  const serviceMatch = services.includes(service);
-
-  if (!serviceMatch) {
-    throw new Error(
-      `Invalid route parameter: ${service}. Service parameter must be news or persian.`,
-    );
-  }
-};
-
-const validateId = id => {
-  const regex = '^(c[a-zA-Z0-9]{10}o)$';
-  const routeMatches = id.match(regex);
-
-  if (!routeMatches) {
-    throw new Error(
-      `Invalid route parameter: ${id}. ID parameter must be in format 'c[xxxxxxxxxx]o', where the middle part could be 0000000001 to 0000000027.`,
-    );
-  }
-};
-
-const getInitialData = async ({ match }) => {
-  try {
-    const { path } = match;
-    const { id, service } = match.params;
-
-    validateService(service);
-    validateId(id);
-
-    let url = `/data/${id}.json`;
-
-    if (isServer()) {
-      url = `${process.env.RAZZLE_BASE_PATH}${url}`;
-    }
-
-    const response = await fetch(url);
-    const data = await response.json();
-    const amp = isAmpPath(path);
-
-    return { amp, data };
-  } catch (error) {
-    throw error;
-  }
-};
+import getInitialData from './getInitialData';
 
 const routes = [
   {
