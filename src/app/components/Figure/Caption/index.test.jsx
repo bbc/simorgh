0f9805c4e1719cc99d800e5a3ea130cb3b0581f7
Sareh Heidari <sareh.heidari@gmail.com>
2018-09-21 16:05:22 +0100

    Refactor Caption unit test
    Co-authored-by: Alex <alex.magana@andela.com>

--- src/app/components/Figure/Caption/index.test.jsx
+++ src/app/components/Figure/Caption/index.test.jsx
@@ -4,18 +4,20 @@ import Caption from './index';
 import { ServiceContext } from '../../ServiceContext';
 import serviceContextStub from '../../../helpers/contextHelpers';
 
+const CaptionWithService = (captionText, service) => (
+  <ServiceContext.Provider value={serviceContextStub[service]}>
+    <Caption>{captionText}</Caption>
+  </ServiceContext.Provider>
+);
+
 describe('Caption', () => {
   shouldMatchSnapshot(
-    'should render correctly for news',
-    <ServiceContext.Provider value={serviceContextStub.news}>
-      <Caption>This is some Caption text</Caption>
-    </ServiceContext.Provider>,
+    'should render correctly with news ServiceContext',
+    CaptionWithService('This is some Caption text', 'news'),
   );
 
   shouldMatchSnapshot(
-    'should render correctly for persian',
-    <ServiceContext.Provider value={serviceContextStub.persian}>
-      <Caption>توصیف چیزی که اتفاق می افتد</Caption>
-    </ServiceContext.Provider>,
+    'should render correctly with persian ServiceContext',
+    CaptionWithService('توصیف چیزی که اتفاق می افتد', 'persian'),
   );
 });
