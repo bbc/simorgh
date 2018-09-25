fe3846b36d003a671dd2256470b4a47675c3021a
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-09-25 14:37:41 +0100

    add test for injecting amp <script>

--- src/app/components/Document/index.test.jsx
+++ src/app/components/Document/index.test.jsx
@@ -2,45 +2,61 @@ import React from 'react';
 import { shallowRender } from '../../helpers/tests/testHelpers';
 import Document from './index';
 
-describe('Document', () => {
-  const assets = ['http://example.com/file.js'];
-  const data = { test: 'data' };
+const assets = ['http://example.com/file.js'];
+const data = { test: 'data' };
 
-  const mockHelmetToComponent = element => ({
-    toComponent: jest.fn().mockImplementation(() => element),
-  });
+const mockHelmet = element => ({
+  toComponent: jest.fn().mockImplementation(() => element),
+  toString: jest.fn().mockImplementation(() => String(element)),
+});
 
-  const helmet = {
-    htmlAttributes: mockHelmetToComponent({
-      lang: 'test',
-    }),
-    link: mockHelmetToComponent(
-      <link
-        rel="canonical"
-        href="https://www.bbc.com/news/articles/c0000000025o"
-      />,
-    ),
-    meta: mockHelmetToComponent(
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, minimum-scale=1"
-      />,
-    ),
-    title: mockHelmetToComponent(<title>Test title</title>),
-  };
-  const styleTags = <style>{'html { color: red; }'}</style>;
+const helmet = {
+  link: mockHelmet(
+    <link
+      rel="canonical"
+      href="https://www.bbc.com/news/articles/c0000000025o"
+    />,
+  ),
+  meta: mockHelmet(
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, minimum-scale=1"
+    />,
+  ),
+  title: mockHelmet(<title>Test title</title>),
+};
 
-  const shallowDocument = shallowRender(
+const helmetWithOutAmp = {
+  htmlAttributes: mockHelmet({
+    lang: 'test',
+  }),
+  ...helmet,
+};
+
+const helmetWithAmp = {
+  htmlAttributes: mockHelmet('amp'),
+  ...helmet,
+};
+
+const styleTags = <style>{'html { color: red; }'}</style>;
+
+const shallowDocument = helmetValue =>
+  shallowRender(
     <Document
       assets={assets}
       app={'<h1>App!</h1>'}
       data={data}
-      helmet={helmet}
+      helmet={helmetValue}
       styleTags={styleTags}
     />,
   );
 
+describe('Document', () => {
   it('should render correctly', () => {
-    expect(shallowDocument).toMatchSnapshot();
+    expect(shallowDocument(helmetWithOutAmp)).toMatchSnapshot();
+  });
+
+  it('should add amp <script> if "amp" in helmet HTML htmlAttributes', () => {
+    expect(shallowDocument(helmetWithAmp)).toMatchSnapshot();
   });
 });
