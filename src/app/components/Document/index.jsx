91b818451c40700fc951679e8d5c2b2332855a64
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-09-26 16:49:35 +0100

    PR is now to just add AMP script - other changes handled in future PR

--- src/app/components/Document/index.jsx
+++ src/app/components/Document/index.jsx
@@ -3,48 +3,22 @@ import ResourceHints from './ResourceHints';
 import '../../lib/globalStyles';
 import { C_POSTBOX } from '../../lib/constants/styles';
 
-const isAmpPayload = htmlAttributes =>
-  htmlAttributes.toString().includes('amp');
-
-/* eslint-disable react/no-danger */
-/* disabling the rule that bans the use of dangerouslySetInnerHTML until a more appropriate implementation can be implemented */
-const injectClientSideApp = serialisedData => {
-  if (serialisedData) {
-    return (
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `window.SIMORGH_DATA=${serialisedData}`,
-        }}
-      />
-    );
-  }
-
-  return null;
-};
-
 /* eslint-disable react/prop-types */
 const Document = ({ assets, app, data, styleTags, helmet }) => {
   const htmlAttrs = helmet.htmlAttributes.toComponent();
   const meta = helmet.meta.toComponent();
   const title = helmet.title.toComponent();
   const links = helmet.link.toComponent();
-  let scripts = [];
+  const serialisedData = JSON.stringify(data);
+  const scripts = assets.map(asset => (
+    <script key={asset} type="text/javascript" src={asset} defer />
+  ));
   let ampScript;
-  let serialisedData;
 
-  if (isAmpPayload(helmet.htmlAttributes)) {
+  if (helmet.htmlAttributes.toString().includes('amp')) {
     ampScript = (
       <script key="amp" async src="https://cdn.ampproject.org/v0.js" />
     );
-  }
-
-  if (!isAmpPayload(helmet.htmlAttributes)) {
-    serialisedData = !isAmpPayload(helmet.htmlAttributes)
-      ? JSON.stringify(data)
-      : null;
-    scripts = assets.map(asset => (
-      <script key={asset} type="text/javascript" src={asset} defer />
-    ));
   }
 
   return (
@@ -63,8 +37,14 @@ const Document = ({ assets, app, data, styleTags, helmet }) => {
         {ampScript}
       </head>
       <body>
+       {/* eslint-disable react/no-danger */
+        /* disabling the rule that bans the use of dangerouslySetInnerHTML until a more appropriate implementation can be implemented */}
         <div id="root" dangerouslySetInnerHTML={{ __html: app }} />
-        {injectClientSideApp(serialisedData)}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.SIMORGH_DATA=${serialisedData}`,
+          }}
+        />
         {scripts}
       </body>
     </html>
