638e3a1c79417ed449a476a0cc090d6cdb2d73b3
Jordan Tart <jordan.tart@bbc.co.uk>
2018-09-26 14:19:28 +0100

    Rename to attributeComponents

--- src/app/containers/Fragment/index.jsx
+++ src/app/containers/Fragment/index.jsx
@@ -4,7 +4,7 @@ import { string, node, arrayOf } from 'prop-types';
 const italic = ({ children }) => <i>{children}</i>;
 const bold = ({ children }) => <b>{children}</b>;
 
-const attributesToRender = {
+const attributeComponents = {
   italic,
   bold,
 };
@@ -14,7 +14,7 @@ const Fragment = ({ text, attributes }) =>
     Iterates through the attribute array and returns a component based on the attribute type (i.e. 'italic' or 'bold'). These components are nested inside each other as children as the array is iterated through. The text string is passed in as the initial value, so it is the first child or the returned value if there are no attributes.
   */
   attributes.reduce((previousAttribute, attribute) => {
-    const Attribute = attributesToRender[attribute];
+    const Attribute = attributeComponents[attribute];
     return <Attribute>{previousAttribute}</Attribute>;
   }, text);
 
