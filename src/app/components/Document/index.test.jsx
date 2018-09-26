09cb4d4c9b8bd0d8c185b3d8a579e939f2998bd7
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-09-26 17:20:16 +0100

    these no longer change

--- src/app/components/Document/index.test.jsx
+++ src/app/components/Document/index.test.jsx
@@ -6,57 +6,41 @@ describe('Document', () => {
   const assets = ['http://example.com/file.js'];
   const data = { test: 'data' };
 
-  const mockHelmet = element => ({
+  const mockHelmetToComponent = element => ({
     toComponent: jest.fn().mockImplementation(() => element),
-    toString: jest.fn().mockImplementation(() => String(element)),
   });
 
   const helmet = {
-    link: mockHelmet(
+    htmlAttributes: mockHelmetToComponent({
+      lang: 'test',
+    }),
+    link: mockHelmetToComponent(
       <link
         rel="canonical"
         href="https://www.bbc.com/news/articles/c0000000025o"
       />,
     ),
-    meta: mockHelmet(
+    meta: mockHelmetToComponent(
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, minimum-scale=1"
       />,
     ),
-    title: mockHelmet(<title>Test title</title>),
+    title: mockHelmetToComponent(<title>Test title</title>),
   };
-
-  const helmetWithOutAmp = {
-    htmlAttributes: mockHelmet({
-      lang: 'test',
-    }),
-    ...helmet,
-  };
-
-  const helmetWithAmp = {
-    htmlAttributes: mockHelmet('amp'),
-    ...helmet,
-  };
-
   const styleTags = <style>{'html { color: red; }'}</style>;
 
-  const shallowDocument = helmetValue =>
-    shallowRender(
-      <Document
-        assets={assets}
-        app={'<h1>App!</h1>'}
-        data={data}
-        helmet={helmetValue}
-        styleTags={styleTags}
-      />,
-    );
+  const shallowDocument = shallowRender(
+    <Document
+      assets={assets}
+      app={'<h1>App!</h1>'}
+      data={data}
+      helmet={helmet}
+      styleTags={styleTags}
+    />,
+  );
 
-  it('should render with client side application', () => {
-    expect(shallowDocument(helmetWithOutAmp)).toMatchSnapshot();
-  });
-
-  it('should add amp <script> if "amp" in helmet HTML htmlAttributes and not render client side application', () => {
-    expect(shallowDocument(helmetWithAmp)).toMatchSnapshot();
+  it('should render correctly', () => {
+    expect(shallowDocument).toMatchSnapshot();
   });
 });
