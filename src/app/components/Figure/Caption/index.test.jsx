7b32d28ad0e13a83be81ffc4a6024e117258291c
sareh <sareh.heidari@gmail.com>
2018-09-26 23:25:58 +0100

    Experimenting with enzyme and mocking the context

--- src/app/components/Figure/Caption/index.test.jsx
+++ src/app/components/Figure/Caption/index.test.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import { shouldMatchSnapshot } from '../../../helpers/tests/testHelpers';
-import Caption from './index';
-import { ServiceContext } from '../../ServiceContext';
 import serviceContextStub from '../../../helpers/contextHelpers';
 
-const CaptionWithService = (captionText, service) => (
-  <ServiceContext.Provider value={serviceContextStub[service]}>
-    <Caption>{captionText}</Caption>
-  </ServiceContext.Provider>
-);
+beforeEach(() => {
+  jest.resetModules();
+});
+
+const getCaptionWithServiceContext = serviceContext => {
+  jest.doMock('../../ServiceContext', () => ({
+    ServiceContextConsumer: ({ imageCaptionOffscreenText }) => (
+      <VisuallyHiddenText>{imageCaptionOffscreenText}</VisuallyHiddenText>
+    ),
+  }));
+
+  // return the updated LanguageSelector module that now includes the mocked context
+  /* eslint-disable-next-line global-require */
+  return require('./index').Caption;
+};
+
+const CaptionWithService = (captionText, service) => {
+  const serviceContext = serviceContextStub[service];
+  const Caption = getCaptionWithServiceContext(serviceContext);
+  return <Caption>{captionText}</Caption>;
+};
 
 describe('Caption', () => {
-  shouldMatchSnapshot(
-    'should render correctly with news ServiceContext',
-    CaptionWithService('This is some Caption text', 'news'),
-  );
+  it('should render correctly with news ServiceContext', () => {
+    const component = CaptionWithService('This is some Caption text', 'news');
+    expect(shallow(component)).toMatchSnapshot();
+  });
 
   shouldMatchSnapshot(
     'should render correctly with persian ServiceContext',
---
+++ src/app/helpers/tests/enzymeSetup.js
@@ -0,0 +1,4 @@
+import { configure } from 'enzyme'; // eslint-disable-line import/no-extraneous-dependencies
+import Adapter from 'enzyme-adapter-react-16'; // eslint-disable-line import/no-extraneous-dependencies
+
+configure({ adapter: new Adapter() });
