4050a4257ca33ef254df98a1a0649917eca22953
James Brumpton <james.brumpton@bbc.co.uk>
2018-09-26 14:38:28 +0100

    Adding tests to check the AMP script loads

--- cypress/integration/ampSpec.js
+++ cypress/integration/ampSpec.js
@@ -17,6 +17,19 @@ describe('AMP Tests', () => {
     getElement('html').should('have.attr', 'amp');
   });
 
+  it('should only load one JS script', () => {
+    const amountOfScripts = getElement('html script').its('length');
+    // This will return two JS scripts, however one is provided by Cypress and is not related to the AMP article.
+    amountOfScripts.should('be.lt', 3);
+    amountOfScripts.should('be.gt', 0);
+  });
+
+  it('should load the AMP framework', () => {
+    // .eq(1) gets the amp <script> as the first loaded is a Cypress <script>
+    const ampScript = getElement('html script').eq(1);
+    ampScript.should('have.attr', 'src', 'https://cdn.ampproject.org/v0.js');
+  });
+
   it('should not have an AMP attribute on the main article', () => {
     cy.visit('/news/articles/c0000000027o');
     getElement('html').should('not.have.attr', 'amp');
