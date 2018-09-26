45f68e946d76a4ac65b92c293150e757cb653c3b
Jordan Tart <jordan.tart@bbc.co.uk>
2018-09-26 14:12:07 +0100

    Add clarification

--- src/app/containers/Fragment/index.jsx
+++ src/app/containers/Fragment/index.jsx
@@ -10,9 +10,12 @@ const attributesToRender = {
 };
 
 const Fragment = ({ text, attributes }) =>
-  attributes.reduce((fragment, attribute) => {
+  /*
+    Iterates through the attribute array and returns a component based on the attribute type (i.e. 'italic' or 'bold'). These components are nested inside each other as children as the array is iterated through. The text string is passed in as the initial value, so it is the first child or the returned value if there are no attributes.
+  */
+  attributes.reduce((previousAttribute, attribute) => {
     const Attribute = attributesToRender[attribute];
-    return <Attribute>{fragment}</Attribute>;
+    return <Attribute>{previousAttribute}</Attribute>;
   }, text);
 
 Fragment.propTypes = {
