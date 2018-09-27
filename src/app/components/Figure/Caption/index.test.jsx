d0b9f6236a023d0b924b7623b6abe8e5635cd24f
sareh <sareh.heidari@gmail.com>
2018-09-27 07:56:33 +0100

    Using enzyme to shallow render and using mocks in the unit file

--- src/app/components/Figure/Caption/index.test.jsx
+++ src/app/components/Figure/Caption/index.test.jsx
@@ -1,38 +1,28 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { shouldMatchSnapshot } from '../../../helpers/tests/testHelpers';
-import serviceContextStub from '../../../helpers/contextHelpers';
-
-beforeEach(() => {
-  jest.resetModules();
-});
-
-const getCaptionWithServiceContext = serviceContext => {
-  jest.doMock('../../ServiceContext', () => ({
-    ServiceContextConsumer: ({ imageCaptionOffscreenText }) => (
-      <VisuallyHiddenText>{imageCaptionOffscreenText}</VisuallyHiddenText>
-    ),
-  }));
-
-  // return the updated LanguageSelector module that now includes the mocked context
-  /* eslint-disable-next-line global-require */
-  return require('./index').Caption;
-};
-
-const CaptionWithService = (captionText, service) => {
-  const serviceContext = serviceContextStub[service];
-  const Caption = getCaptionWithServiceContext(serviceContext);
-  return <Caption>{captionText}</Caption>;
-};
+import Caption from './index';
 
 describe('Caption', () => {
+  const CaptionWithContext = (captionText, stubbedContext) =>
+    shallow(<Caption>{captionText}</Caption>, {
+      context: { props: stubbedContext },
+    });
+
+  const newsContextStub = {
+    imageCaptionOffscreenText: 'Image caption, ',
+  };
   it('should render correctly with news ServiceContext', () => {
-    const component = CaptionWithService('This is some Caption text', 'news');
-    expect(shallow(component)).toMatchSnapshot();
+    expect(
+      CaptionWithContext('This is some Caption text', newsContextStub),
+    ).toMatchSnapshot();
   });
 
-  shouldMatchSnapshot(
-    'should render correctly with persian ServiceContext',
-    CaptionWithService('توصیف چیزی که اتفاق می افتد', 'persian'),
-  );
+  const persianContextStub = {
+    imageCaptionOffscreenText: ' ، عنوان تصویر',
+  };
+  it('should render correctly with news ServiceContext', () => {
+    expect(
+      CaptionWithContext('توصیف چیزی که اتفاق می افتد', persianContextStub),
+    ).toMatchSnapshot();
+  });
 });
