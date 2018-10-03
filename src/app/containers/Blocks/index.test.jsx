8a40587ec5edba405985ac7bdc4335bae31764ea
Ben Newcombe <bcmn96@gmail.com>
2018-10-03 10:15:15 +0100

    Rename subheading block type to subheadline

--- src/app/containers/Blocks/index.test.jsx
+++ src/app/containers/Blocks/index.test.jsx
@@ -6,7 +6,7 @@ import { blockContainingText, singleTextBlock } from '../../models/blocks';
 describe('Blocks', () => {
   const blocks = [
     blockContainingText('headline', 'This is a headline!'),
-    blockContainingText('subheading', 'This is a subheading!'),
+    blockContainingText('subheadline', 'This is a subheadline!'),
     singleTextBlock('This is some text content!'),
     {
       type: 'test',
@@ -50,7 +50,7 @@ describe('Blocks', () => {
 
   const componentsToRender = {
     headline: () => <h1>A headline</h1>,
-    subheading: () => <h2>A subheading</h2>,
+    subheadline: () => <h2>A subheadline</h2>,
     text: () => <p>Some text</p>,
   };
 
