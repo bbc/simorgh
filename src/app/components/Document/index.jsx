c5ee649e9743310418f74a6930c0a63ca4da6f89
Ben Newcombe <bcmn96@gmail.com>
2018-10-04 09:28:15 +0100

    Remove unnecessary link to favicon

--- src/app/components/Document/index.jsx
+++ src/app/components/Document/index.jsx
@@ -22,7 +22,6 @@ const Document = ({ assets, app, data, styleTags, helmet }) => {
         <meta name="robots" content="noindex,nofollow" />
         <meta name="theme-color" content={C_POSTBOX} />
         {meta}
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="manifest" href="/manifest.json" />
         <ResourceHints />
