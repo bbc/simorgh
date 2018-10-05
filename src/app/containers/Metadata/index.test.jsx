e9c5dbae7a2ff24dc7c9242045ff0bc76bb0d1fb
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-10-05 15:57:39 +0100

    shallow snapshot test match and update snapshot

--- src/app/containers/Metadata/index.test.jsx
+++ src/app/containers/Metadata/index.test.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
+import { shouldShallowMatchSnapshot } from '../../helpers/tests/testHelpers';
 import MetadataContainer from './index';
 import { ServiceContextProvider } from '../../components/ServiceContext';
 import { articleDataNews } from '../Article/fixtureData';
-import newsServiceContextStub from '../../lib/config/services/news';
 
 describe('MetadataContainer', () => {
   const { metadata, promo } = articleDataNews;
@@ -14,12 +13,10 @@ describe('MetadataContainer', () => {
         isAmp={false}
         metadata={metadata}
         promo={promo}
-        service={'news'}
+        service="news"
       />
     </ServiceContextProvider>
   );
 
-  shouldMatchSnapshot(
-    'should render correctly', MetadataWithContext()
-  );
+  shouldShallowMatchSnapshot('should render correctly', MetadataWithContext());
 });
