c011547f1d133dc24ba8ceaf2ef7e5caa87d40a1
Jordan Tart <jordan.tart@bbc.co.uk>
2018-10-10 15:11:02 +0100

    Add new inline link component that renders blocks

---
+++ src/app/containers/InlineLinkNew/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import InlineLink from '../../components/InlineLink';
+import Blocks from '../Blocks';
+import fragment from '../Fragment';
+
+const InternalInlineLink = InlineLink.withComponent(Link);
+
+const componentsToRender = { fragment };
+
+const InlineLinkContainer = ({ locator, blocks }) => {
+  const regex = '^/news/articles/c[a-zA-Z0-9]{10}o$';
+
+  // if URL matches a valid route, use a react-router link
+  if (locator.match(regex)) {
+    return (
+      <InternalInlineLink to={locator}>
+        <Blocks blocks={blocks} componentsToRender={componentsToRender} />
+      </InternalInlineLink>
+    );
+  }
+
+  // else return a normal hyperlink
+  return (
+    <InlineLink href={locator}>
+      <Blocks blocks={blocks} componentsToRender={componentsToRender} />
+    </InlineLink>
+  );
+};
+
+export default InlineLinkContainer;
