8ff85414f237f72d23f03cb973435882c43ea0ed
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-10-12 18:43:48 +0100

    tidy log commands

--- src/app/dataValidator/index.js
+++ src/app/dataValidator/index.js
@@ -1,14 +1,16 @@
 const { readdirSync } = require('./helpers/dataLoader/readdirSync');
 const { getScenariosCount } = require('./utilities/countScenarios');
 
+/* eslint-disable no-console */
 const validateAllScenarios = () => {
-  console.time('validateAllScenarios'); // eslint-disable-line no-console
+  console.time('validateAllScenarios');
   return readdirSync('./././data').then(() => {
     const scenariosCount = getScenariosCount();
-    console.log(`\nAll ${scenariosCount} scenarios validated!`); // eslint-disable-line no-console
-    console.timeEnd('validateAllScenarios'); // eslint-disable-line no-console
+    console.log(`\nAll ${scenariosCount} scenarios validated!`);
+    console.timeEnd('validateAllScenarios');
   });
 };
+/* eslint-enable no-console */
 
 validateAllScenarios();
 
