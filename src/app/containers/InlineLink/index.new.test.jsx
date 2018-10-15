51774825ba6ad5acb96bf0c0712db8f7e7dc0faa
Ben Newcombe <bcmn96@gmail.com>
2018-10-15 14:41:37 +0100

    Update tests to include absolute URL to '/news/articles/...'. Also switches to deep snapshot test, as otherwise (due to StaticRouter level) the tests weren't really testing the container.

--- src/app/containers/InlineLink/index.new.test.jsx
+++ src/app/containers/InlineLink/index.new.test.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StaticRouter } from 'react-router-dom';
-import { shouldShallowMatchSnapshot } from '../../helpers/tests/testHelpers';
+import {
+  shouldMatchSnapshot,
+  shouldShallowMatchSnapshot,
+} from '../../helpers/tests/testHelpers';
 import InlineLinkContainer from './index.new';
 
 const fragmentBlock = (text, attributes = []) => ({
@@ -13,14 +16,14 @@ const fragmentBlock = (text, attributes = []) => ({
 
 describe('InlineLinkContainer', () => {
   describe('with link matching routes for SPA', () => {
-    shouldShallowMatchSnapshot(
+    shouldMatchSnapshot(
       'should render correctly',
       /*
         for the value it would bring, it is much simpler to wrap a react-router Link in a Router, rather than mock a Router or pass come mocked context.
       */
       <StaticRouter>
         <InlineLinkContainer
-          locator="/news/articles/c0000000027o"
+          locator="https://www.bbc.com/news/articles/c0000000027o"
           blocks={[fragmentBlock('This is text for an internal link')]}
         />
       </StaticRouter>,
