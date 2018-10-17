d6bf6a7b9e5100d6d22f0b9aca69f9c7f44044f1
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-10-17 18:24:20 +0100

    hacky prototype for a grid in a grid supporting full width

--- src/app/components/MainContent/index.jsx
+++ src/app/components/MainContent/index.jsx
@@ -1,37 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import { node } from 'prop-types';
-import { layoutGridWrapper, layoutGridItem } from '../../lib/layoutGrid';
-import {
-  group4ScreenWidthMin,
-  group4ScreenWidthMax,
-  group5ScreenWidthMin,
-} from '../../lib/constants/styles';
 
 export const StyledMainContent = styled.main`
-  margin: auto;
-  @media (min-width: ${group4ScreenWidthMin}) and (max-width: ${group4ScreenWidthMax}) {
-    max-width: 1008px;
-  }
-  @media (min-width: ${group5ScreenWidthMin}) {
-    max-width: 1280px;
-  }
+  display: grid;
+  grid-template-columns: [leftCol] 1fr [mainCol] 1280px [rightCol] 1fr;
 `;
-
-const StyleGridWrapper = styled.div`
-  ${layoutGridWrapper};
-`;
-
-const GridItem = styled.div`
-  ${layoutGridItem};
-`;
-
 const MainContent = ({ children }) => (
-  <StyledMainContent role="main">
-    <StyleGridWrapper>
-      <GridItem>{children}</GridItem>
-    </StyleGridWrapper>
-  </StyledMainContent>
+  <StyledMainContent role="main">{children}</StyledMainContent>
 );
 
 MainContent.propTypes = {
--- src/app/containers/Blocks/index.jsx
+++ src/app/containers/Blocks/index.jsx
@@ -1,11 +1,76 @@
 import React from 'react';
 import { objectOf, arrayOf, func, shape, string, any } from 'prop-types';
 import nanoid from 'nanoid';
+import styled from 'styled-components';
+import { layoutGridItem, layoutGridWrapper } from '../../lib/layoutGrid';
+import {
+  group4ScreenWidthMin,
+  group4ScreenWidthMax,
+  group5ScreenWidthMin,
+} from '../../lib/constants/styles';
+
+const GridItem = styled.div`
+  ${layoutGridItem};
+`;
+
+const StyleGridWrapper = styled.div`
+  ${layoutGridWrapper};
+  grid-column: mainCol;
+  @media (min-width: ${group4ScreenWidthMin}) and (max-width: ${group4ScreenWidthMax}) {
+    max-width: 1008px;
+  }
+  @media (min-width: ${group5ScreenWidthMin}) {
+    max-width: 1280px;
+  }
+`;
+
+const StyleGridFullWidthWrapper = styled.div`
+  grid-column: 1 / -1;
+`;
 
 // Inlined as this is a temporary component
 const BlockString = props => {
   const stringProps = JSON.stringify(props);
   return <p>{stringProps}</p>;
+};
+
+const blockShouldHaveGridWrapper = (
+  Block,
+  type,
+  model,
+  typeOfPreviousBlock,
+) => {
+  /* 
+    if the previous block is undefined it's top level and needs a wrapper
+    if the previous block is 'pragraph' then the next item needs a wrapper
+    if the block is a heading it needs a wrapper (not sure why it isn't undefined?!)
+  */
+  if (
+    (typeOfPreviousBlock === undefined && type !== 'fragment') ||
+    typeOfPreviousBlock === 'paragraph' ||
+    type === 'subheadline'
+  ) {
+    return (
+      <StyleGridWrapper>
+        <GridItem>
+          <Block
+            key={nanoid()}
+            type={type}
+            typeOfPreviousBlock={typeOfPreviousBlock}
+            {...model}
+          />
+        </GridItem>
+      </StyleGridWrapper>
+    );
+  }
+  return (
+    <Block
+      key={nanoid()}
+      type={type}
+      typeOfPreviousBlock={typeOfPreviousBlock}
+      {...model}
+    />
+  );
 };
 
 const Blocks = ({ blocks, componentsToRender }) =>
@@ -16,14 +81,21 @@ const Blocks = ({ blocks, componentsToRender }) =>
 
     const Block = componentsToRender[type] || BlockString;
 
-    return (
-      <Block
-        key={nanoid()}
-        type={type}
-        typeOfPreviousBlock={typeOfPreviousBlock}
-        {...model}
-      />
-    );
+    // make all images full bleed
+    if (type === 'image') {
+      return (
+        <StyleGridFullWidthWrapper>
+          <Block
+            key={nanoid()}
+            type={type}
+            typeOfPreviousBlock={typeOfPreviousBlock}
+            {...model}
+          />
+        </StyleGridFullWidthWrapper>
+      );
+    }
+
+    return blockShouldHaveGridWrapper(Block, type, model, typeOfPreviousBlock);
   });
 
 Blocks.propTypes = {
