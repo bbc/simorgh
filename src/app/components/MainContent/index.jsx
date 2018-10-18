335a3c25e9b6a5c58320fac45c1446e2b42cc67e
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-10-18 13:07:01 +0100

    set the background colour to oat and use grid

--- src/app/components/MainContent/index.jsx
+++ src/app/components/MainContent/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { node } from 'prop-types';
+import { layoutGridWrapper } from '../../lib/layoutGrid';
+import { C_OAT } from '../../lib/constants/styles';
 
 export const StyledMainContent = styled.main`
-  display: grid;
-  grid-template-columns: [start] 1fr [main-start] repeat(10, minmax(0, 128px)) [main-end] 1fr [end];
+  ${layoutGridWrapper};
+  background-color: ${C_OAT};
 `;
 
 const MainContent = ({ children }) => (
