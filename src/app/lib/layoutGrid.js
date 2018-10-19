5bc0045e8ac939a716723cc9462c8c248cf8e715
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-10-19 12:43:58 +0100

    use relative units

--- src/app/lib/layoutGrid.js
+++ src/app/lib/layoutGrid.js
@@ -26,14 +26,14 @@ export const layoutGridWrapper = css`
       Math: (1008 - (7*16))/8 = 112
       Explainer: (width - 7 gaps of 16) / 8 columns = signle column width
     */
-    grid-template-columns: [start] 1fr repeat(8, [col] minmax(0, 112px)) 1fr [end];
+    grid-template-columns: [start] 1fr repeat(8, [col] minmax(0, 7rem)) 1fr [end];
   }
   @media (min-width: ${group5ScreenWidthMin}) {
     /* 
       Math: (1280 - (9*16))/10 = 113.6
       Explainer: (width - 9 gaps of 16) / 10 columns = signle column width
     */
-    grid-template-columns: [start] 1fr repeat(10, [col] minmax(0, 113.6px)) 1fr [end];
+    grid-template-columns: [start] 1fr repeat(10, [col] minmax(0, 7.1rem)) 1fr [end];
   }
 `;
 
