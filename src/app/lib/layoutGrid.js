c92f38f910690706e39189a0bfea94c3dd8d7d74
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-10-30 18:33:14 +0000

    Implement the full width grid to match UX grid
    - UX margins are exculsive of the UX columns
    - Above 1007px the UX grid is limited in width and our implementation creates static width columns allowing us to create two
    additional columns of flexible width. Therefore above 1279px we have 12 columns and above 1007px we have 10 columns.
    Because of these additional columns we have additional gutters which act as the UX margin's for layouts above 1007px.
    - Below the 1008px the UX grid is responsive the to full width of the viewport and therefore we don't require the additional
    two columns. Because of this we implement the UX margins as padding on the grid itself.
    
    Co-authored-by: bcmn <ben.newcombe@bbc.co.uk>

--- src/app/lib/layoutGrid.js
+++ src/app/lib/layoutGrid.js
@@ -11,61 +11,26 @@ import {
   group5ScreenWidthMin,
 } from './constants/styles';
 
-export const layoutGridWrapper = css`
-  display: grid;
-  @media (max-width: ${group3ScreenWidthMin}) {
-    grid-gap: ${GEL_SPACING}; /* Between grid items */
-    padding: 0 ${GEL_SPACING}; /* On grid wrapper */
-  }
-  @media (min-width: ${group3ScreenWidthMin}) {
-    grid-gap: ${GEL_SPACING_DBL}; /* Between grid items */
-    padding: 0 ${GEL_SPACING_DBL}; /* On grid wrapper */
-  }
-  @media (max-width: ${group3ScreenWidthMax}) {
-    grid-template-columns: repeat(6, 1fr);
-  }
-  @media (min-width: ${group4ScreenWidthMin}) and (max-width: ${group4ScreenWidthMax}) {
-    grid-template-columns: repeat(8, 1fr);
-  }
-  @media (min-width: ${group5ScreenWidthMin}) {
-    grid-template-columns: repeat(10, 1fr);
-  }
-`;
-
-export const layoutGridItem = css`
-  @media (max-width: ${group2ScreenWidthMax}) {
-    grid-column: 1 / -1;
-  }
-  @media (min-width: ${group3ScreenWidthMin}) and (max-width: ${group3ScreenWidthMax}) {
-    grid-column: 2 / -2;
-  }
-  @media (min-width: ${group4ScreenWidthMin}) and (max-width: ${group4ScreenWidthMax}) {
-    grid-column: 2 / -2;
-  }
-  @media (min-width: ${group5ScreenWidthMin}) {
-    grid-column: 3 / -3;
-  }
-`;
-
-export const layoutGridItemFullWidth = css`
-  grid-column: 1 / -1;
-`;
-
 const group4Const = `7rem`;
 /* (1008px width - 7 * 16px gap) / 8 columns = 112px = 7rem single column width */
 
 const group5Const = `7.1rem`;
 /* (1280px width - 9 * 16px gap) / 10 columns = 113.6px = 7.1rem single column width */
 
-export const gridWrapperStyles = css`
+export const layoutGridWrapper = css`
   display: grid;
   @media (max-width: ${group2ScreenWidthMin}) {
     grid-gap: ${GEL_SPACING};
+    padding: 0 ${GEL_SPACING};
   }
-  @media (min-width: ${group2ScreenWidthMin}) {
+  @media (min-width: ${group2ScreenWidthMin}) and (max-width: ${group3ScreenWidthMax}) {
+    padding: 0 ${GEL_SPACING_DBL};
+  }
+  @media (min-width: ${group3ScreenWidthMin}) {
     grid-gap: ${GEL_SPACING_DBL};
   }
   @media (max-width: ${group3ScreenWidthMax}) {
+    grid-gap: ${GEL_SPACING_DBL};
     grid-template-columns: repeat(6, 1fr);
   }
   @media (min-width: ${group4ScreenWidthMin}) and (max-width: ${group4ScreenWidthMax}) {
@@ -76,25 +41,23 @@ export const gridWrapperStyles = css`
   }
 `;
 
-export const gridItemStyles = css`
+export const layoutGridItem = css`
   @media (max-width: ${group2ScreenWidthMin}) {
     grid-column: 1 / -1;
-    padding: 0 ${GEL_SPACING};
   }
   @media (min-width: ${group2ScreenWidthMin}) and (max-width: ${group2ScreenWidthMax}) {
     grid-column: 1 / -1;
-    padding: 0 ${GEL_SPACING_DBL};
   }
   @media (min-width: ${group3ScreenWidthMin}) and (max-width: ${group3ScreenWidthMax}) {
     grid-column: 2 / -2;
-    padding: 0;
   }
   @media (min-width: ${group4ScreenWidthMin}) and (max-width: ${group4ScreenWidthMax}) {
     grid-column: 3 / -3;
-    padding: 0;
   }
   @media (min-width: ${group5ScreenWidthMin}) {
     grid-column: 4 / -4;
-    padding: 0;
-  }
+`;
+
+export const layoutGridItemFullWidth = css`
+  grid-column: 1 / -1;
 `;
