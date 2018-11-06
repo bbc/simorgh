d913190a88401d28f7dd72c96add9e25633fcb54
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-11-06 15:44:29 +0000

    Create a new const for layout wrappers without grid
    Using the GEL definitions the new const sets the left and right padding of the wrapper to act as the UX Margin. More info at https://www.bbc.co.uk/gel/guidelines/grid#grid-sizes

--- src/app/lib/layoutGrid.js
+++ src/app/lib/layoutGrid.js
@@ -19,12 +19,21 @@ const group4ColWidth = `6.75rem`;
 const group5ColWidth = `6.9rem`;
 /* (1280px - (2*16px margins + 9*16px gutters)  / 10 columns = 110.4px = 6.9rem single column width */
 
-/*
-  0-599px: 8px gutter
-  600+: 16px gutter
+export const layoutWrapperWithoutGrid = css`
+  @media (max-width: ${group2ScreenWidthMin}) {
+    padding: 0 ${GEL_MARGIN_BELOW_400PX};
+  }
+  @media (min-width: ${group2ScreenWidthMin}) {
+    padding: 0 ${GEL_MARGIN_ABOVE_400PX};
+  }
+`;
 
-  0-399px: 8px margin
-  400-1007px: 16px margin
+/*
+0-599px: 8px gutter
+600+: 16px gutter
+
+0-399px: 8px margin
+400-1007px: 16px margin
   1008+: no explicit margin, since we use 16px gutters as margin
 */
 
