f60cc43048e1c45d27e7f4b6c4eec63462602a2c
radiocontrolled <alison.benjamin@bbc.co.uk>
2018-11-08 15:02:31 +0000

    testMatch outside of src

--- scripts/lighthouse/index.test.js
+++ scripts/lighthouse/index.test.js
@@ -3,6 +3,8 @@ import { config } from './fixtures';
 
 const { indexRunner } = require('./index');
 
+jest.setTimeout(30000);
+
 describe('indexRunner', () => {
   it('Should resolve to true if called with passing config', async () => {
     await expect(indexRunner(config)).resolves.toEqual(true);
