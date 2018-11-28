bb0c43510904c7a71fa41c34367d4f9092edd952
Ben Newcombe <bcmn96@gmail.com>
2018-11-28 15:39:33 +0000

    Move detailed Figure Caption container snapshot tests from Figure to Caption container

---
+++ src/app/containers/Caption/index.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Caption from './index';
+import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
+import { ServiceContext } from '../../components/ServiceContext';
+
+const newsServiceContextStub = {
+  imageCaptionOffscreenText: 'Image caption, ',
+};
+const persianServiceContextStub = {
+  imageCaptionOffscreenText: ' ، عنوان تصویر',
+};
+
+const CaptionWithContext = contextStub => (
+  <ServiceContext.Provider value={contextStub}>
+    <Caption captionValue="Some caption text..." />
+  </ServiceContext.Provider>
+);
+
+shouldMatchSnapshot(
+  'should render caption text with example News offscreen text',
+  CaptionWithContext(newsServiceContextStub),
+);
+
+shouldMatchSnapshot(
+  'should render caption text with example Farsi offscreen text',
+  CaptionWithContext(persianServiceContextStub),
+);
--- src/app/containers/Figure/index.test.jsx
+++ src/app/containers/Figure/index.test.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import FigureContainer from './index';
-import {
-  shouldShallowMatchSnapshot,
-  shouldMatchSnapshot,
-} from '../../helpers/tests/testHelpers';
-import { ServiceContext } from '../../components/ServiceContext';
+import { shouldShallowMatchSnapshot } from '../../helpers/tests/testHelpers';
 
 const imageAlt =
   'Map of the UK displaying Syrian refugees and asylum seekers per 10000 population. Ranges from 0 to 17.';
@@ -12,19 +8,6 @@ const imageSrc =
   'https://ichef.bbci.co.uk/news/640/cpsprodpb/439A/production/_100960371_syrians_and_asylum_v2-nc.png';
 const caption = 'This is a caption';
 const copyrightText = 'Getty Images';
-
-const newsServiceContextStub = {
-  imageCaptionOffscreenText: 'Image caption, ',
-};
-const persianServiceContextStub = {
-  imageCaptionOffscreenText: ' ، عنوان تصویر',
-};
-
-const figureCaptionWithContext = contextStub => (
-  <ServiceContext.Provider value={contextStub}>
-    <FigureContainer src={imageSrc} alt={imageAlt} caption={caption} />
-  </ServiceContext.Provider>
-);
 
 describe('Figure', () => {
   shouldShallowMatchSnapshot(
@@ -37,13 +20,8 @@ describe('Figure', () => {
     <FigureContainer src={imageSrc} alt={imageAlt} copyright={copyrightText} />,
   );
 
-  shouldMatchSnapshot(
-    'should render caption text with provided offscreen text',
-    figureCaptionWithContext(newsServiceContextStub),
-  );
-
-  shouldMatchSnapshot(
-    'should render caption text with provided offscreen text',
-    figureCaptionWithContext(persianServiceContextStub),
+  shouldShallowMatchSnapshot(
+    'should render caption text',
+    <FigureContainer src={imageSrc} alt={imageAlt} caption={caption} />,
   );
 });
