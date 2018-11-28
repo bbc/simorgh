2f13d1f86f0f6b06708631e0a2cd38842b63d29a
Ben Newcombe <bcmn96@gmail.com>
2018-11-28 14:47:12 +0000

    Update tests to reflect Caption component as a logic-less thing, pulling the service context testing to the Caption container

--- src/app/components/Text/index.test.jsx
+++ src/app/components/Text/index.test.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { shouldMatchSnapshot, isNull } from '../../helpers/tests/testHelpers';
 import Text from './index';
 import Caption from '../Figure/Caption/index';
-import { ServiceContext } from '../ServiceContext';
 
 describe('Text', () => {
   describe('with no data', () => {
@@ -34,17 +33,11 @@ describe('Text', () => {
     <Text text="This is text that contains an [inline link](https://www.bbc.com/news) inside it." />,
   );
 
-  const serviceContextStubNews = {
-    imageCaptionOffscreenText: 'Image caption, ',
-  };
-
   shouldMatchSnapshot(
     'should render a <figcaption> as per the Caption component override',
-    <ServiceContext.Provider value={serviceContextStubNews}>
-      <Text
-        text="This is text that contains an [inline link](https://www.bbc.com/news) inside it."
-        paragraphOverride={Caption}
-      />
-    </ServiceContext.Provider>,
+    <Text
+      text="This is text that contains an [inline link](https://www.bbc.com/news) inside it."
+      paragraphOverride={Caption}
+    />,
   );
 });
--- src/app/containers/Figure/index.test.jsx
+++ src/app/containers/Figure/index.test.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import FigureContainer from './index';
-import { shouldShallowMatchSnapshot } from '../../helpers/tests/testHelpers';
+import {
+  shouldShallowMatchSnapshot,
+  shouldMatchSnapshot,
+} from '../../helpers/tests/testHelpers';
+import { ServiceContext } from '../../components/ServiceContext';
 
 const imageAlt =
   'Map of the UK displaying Syrian refugees and asylum seekers per 10000 population. Ranges from 0 to 17.';
@@ -8,6 +12,19 @@ const imageSrc =
   'https://ichef.bbci.co.uk/news/640/cpsprodpb/439A/production/_100960371_syrians_and_asylum_v2-nc.png';
 const caption = 'This is a caption';
 const copyrightText = 'Getty Images';
+
+const newsServiceContextStub = {
+  imageCaptionOffscreenText: 'Image caption, ',
+};
+const persianServiceContextStub = {
+  imageCaptionOffscreenText: ' ، عنوان تصویر',
+};
+
+const figureCaptionWithContext = contextStub => (
+  <ServiceContext.Provider value={contextStub}>
+    <FigureContainer src={imageSrc} alt={imageAlt} caption={caption} />
+  </ServiceContext.Provider>
+);
 
 describe('Figure', () => {
   shouldShallowMatchSnapshot(
@@ -20,8 +37,13 @@ describe('Figure', () => {
     <FigureContainer src={imageSrc} alt={imageAlt} copyright={copyrightText} />,
   );
 
-  shouldShallowMatchSnapshot(
-    'should render caption text',
-    <FigureContainer src={imageSrc} alt={imageAlt} caption={caption} />,
+  shouldMatchSnapshot(
+    'should render caption text with provided offscreen text',
+    figureCaptionWithContext(newsServiceContextStub),
+  );
+
+  shouldMatchSnapshot(
+    'should render caption text with provided offscreen text',
+    figureCaptionWithContext(persianServiceContextStub),
   );
 });
