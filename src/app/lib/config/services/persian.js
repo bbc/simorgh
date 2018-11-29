ce89f5eed32c87081a2e66f9c9235fce1f35b8d0
Ben Newcombe <bcmn96@gmail.com>
2018-11-29 15:24:05 +0000

    Add imageCaptionOffscreenText for News and Persian service configs

--- src/app/lib/config/services/persian.js
+++ src/app/lib/config/services/persian.js
@@ -1,11 +1,12 @@
 const persian = {
-  brandName: 'BBC News فارسی',
-  serviceName: 'Persian',
-  service: 'persian',
   articleAuthor: 'https://www.facebook.com/bbcnews',
+  brandName: 'BBC News فارسی',
   defaultImage: 'https://news.files.bbci.co.uk/ws/img/logos/og/persian.png',
   defaultImageAltText: 'BBC News فارسی',
+  imageCaptionOffscreenText: ' ، عنوان تصویر',
   locale: 'fa',
+  service: 'persian',
+  serviceName: 'Persian',
   twitterCreator: '@bbcpersian',
   twitterSite: '@bbcpersian',
 };
