211bade7a8448f3449f4677ce367322733b1fd73
Sareh Heidari <sareh.heidari@gmail.com>
2018-12-03 14:32:12 +0000

    Add service context config for image copyright

--- src/app/lib/config/services/news.js
+++ src/app/lib/config/services/news.js
@@ -6,6 +6,7 @@ const news = {
   defaultImage:
     'https://www.bbc.co.uk/news/special/2015/newsspec_10857/bbc_news_logo.png?cb=1',
   defaultImageAltText: 'BBC News',
+  imageCopyrightOffscreenText: 'Image source, ',
   locale: 'en_GB',
   twitterCreator: '@BBCNews',
   twitterSite: '@BBCNews',
--- src/app/lib/config/services/persian.js
+++ src/app/lib/config/services/persian.js
@@ -5,6 +5,7 @@ const persian = {
   articleAuthor: 'https://www.facebook.com/bbcnews',
   defaultImage: 'https://news.files.bbci.co.uk/ws/img/logos/og/persian.png',
   defaultImageAltText: 'BBC News فارسی',
+  imageCopyrightOffscreenText: ' ، منبع تصویر',
   locale: 'fa',
   twitterCreator: '@bbcpersian',
   twitterSite: '@bbcpersian',
