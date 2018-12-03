416d25dfaf4026f895a6b41554b23f4f30d18a3b
Sareh Heidari <sareh.heidari@gmail.com>
2018-12-03 14:57:01 +0000

    Update tests

--- src/app/containers/Copyright/index.test.jsx
+++ src/app/containers/Copyright/index.test.jsx
@@ -1,30 +1,30 @@
-// import React from 'react';
-// import CopyrightContainer from './index';
-// import { ServiceContext } from '../../components/ServiceContext';
-// import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
+import React from 'react';
+import CopyrightContainer from './index';
+import { ServiceContext } from '../../components/ServiceContext';
+import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
 
-// const CopyrightWithContext = (copyrightText, contextStub) => (
-//   <ServiceContext.Provider value={contextStub}>
-//     <CopyrightContainer>{copyrightText}</CopyrightContainer>
-//   </ServiceContext.Provider>
-// );
+const CopyrightWithContext = (copyrightText, contextStub) => (
+  <ServiceContext.Provider value={contextStub}>
+    <CopyrightContainer>{copyrightText}</CopyrightContainer>
+  </ServiceContext.Provider>
+);
 
-// const newsServiceContextStub = {
-//   imageCopyrightOffscreenText: 'Image source, ',
-// };
-// const persianServiceContextStub = {
-//   imageCopyrightOffscreenText: ' ، منبع تصویر',
-// };
+const newsServiceContextStub = {
+  imageCopyrightOffscreenText: 'Image source, ',
+};
+const persianServiceContextStub = {
+  imageCopyrightOffscreenText: ' ، منبع تصویر',
+};
 
-// shouldMatchSnapshot(
-//   'should render Copyright with news service context',
-//   CopyrightWithContext('This is some copyright text', newsServiceContextStub),
-// );
+shouldMatchSnapshot(
+  'should render Copyright with news service context',
+  CopyrightWithContext('This is some copyright text', newsServiceContextStub),
+);
 
-// shouldMatchSnapshot(
-//   'should render Copyright with persian service context',
-//   CopyrightWithContext(
-//     'توصیف چیزی که اتفاق می افتد',
-//     persianServiceContextStub,
-//   ),
-// );
+shouldMatchSnapshot(
+  'should render Copyright with persian service context',
+  CopyrightWithContext(
+    'توصیف چیزی که اتفاق می افتد',
+    persianServiceContextStub,
+  ),
+);
