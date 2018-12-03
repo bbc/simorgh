db63075c332febfffb9568e9e99218e4a9e650b5
Ben Newcombe <bcmn96@gmail.com>
2018-12-03 14:46:49 +0000

    Making Text component redundant, part two - make Caption container simly render plain text

--- src/app/containers/Caption/index.jsx
+++ src/app/containers/Caption/index.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
-import { node, string } from 'prop-types';
+import { string } from 'prop-types';
 import Caption from '../../components/Figure/Caption';
-import Text from '../../components/Text';
 import { ServiceContext } from '../../components/ServiceContext';
 import VisuallyHiddenText from '../../components/VisuallyHiddenText';
 
-const CaptionWithOffscreenText = ({ children }) => (
+const CaptionContainer = ({ captionValue }) => (
   <ServiceContext.Consumer>
     {({ imageCaptionOffscreenText }) => (
       <Caption>
         {imageCaptionOffscreenText ? (
           <VisuallyHiddenText>{imageCaptionOffscreenText}</VisuallyHiddenText>
         ) : null}
-        {children}
+        {captionValue}
       </Caption>
     )}
   </ServiceContext.Consumer>
-);
-
-CaptionWithOffscreenText.propTypes = {
-  children: node.isRequired,
-};
-
-const CaptionContainer = ({ captionValue }) => (
-  <Text text={captionValue} paragraphOverride={CaptionWithOffscreenText} />
 );
 
 CaptionContainer.propTypes = {
