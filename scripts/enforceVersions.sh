283e914cf5863350b040ce2841ecd601e724454c
Drew McMillan <drew.mcmillan@bbc.co.uk>
2018-12-04 19:10:56 +0000

    Add advice on how to upgrade

--- scripts/enforceVersions.sh
+++ scripts/enforceVersions.sh
@@ -4,8 +4,9 @@ if [[ $(pwd) =~ .+simorgh ]]; then
 
   if [[ $(node -v) != "$desired_node_version"* ]]; then
     tput setaf 1
-    echo "You're not using our version of node, please move to $desired_node_version"
+    echo "Oh no! You're not using our version of node, please move to $desired_node_version"
     tput sgr0
+    echo "This can be done by running 'nvm install $desired_node_version && nvm use $desired_node_version'"
     exit 1
   fi
 
@@ -13,8 +14,10 @@ if [[ $(pwd) =~ .+simorgh ]]; then
 
   if [[ $(npm -v) != $desired_npm_version ]]; then
     tput setaf 1
-    echo "You're not using our version of npm, please move to $desired_npm_version"
+    echo "Oh no! You're not using our version of npm, please move to version $desired_npm_version"
     tput sgr0
+    echo "This can be done by running 'npm i -g npm@$desired_npm_version'"
+    echo "Once you have done this please run 'npm install' again and commit any changes to your package-lock.json"
     exit 1
   fi
 fi
