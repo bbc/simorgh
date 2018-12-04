a84ee7ab494ed6d010c19446c98854195153633c
Sareh Heidari <sareh.heidari@gmail.com>
2018-12-04 21:45:56 +0000

    Use react-testing-library for deep rendering in shouldMatchSnapshot
    Snapshots changed to display dom nodes rather than react components

--- src/app/helpers/tests/testHelpers.js
+++ src/app/helpers/tests/testHelpers.js
@@ -1,11 +1,12 @@
 import renderer from 'react-test-renderer'; // eslint-disable-line import/no-extraneous-dependencies
 import 'jest-styled-components'; // eslint-disable-line import/no-extraneous-dependencies
 import ShallowRenderer from 'react-test-renderer/shallow'; // eslint-disable-line import/no-extraneous-dependencies
+import { render } from 'react-testing-library'; // eslint-disable-line import/no-extraneous-dependencies
 
 export const shouldMatchSnapshot = (title, component) => {
   it(title, () => {
-    const tree = renderer.create(component).toJSON();
-    expect(tree).toMatchSnapshot();
+    const { asFragment } = render(component);
+    expect(asFragment()).toMatchSnapshot();
   });
 };
 
