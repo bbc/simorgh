288ae4791be11310e79b695ffdefbb8f6c052efc
Drew McMillan <drew.mcmillan@bbc.co.uk>
2018-12-04 13:46:54 +0000

    Update GlobalStyles test

--- src/app/lib/globalStyles.test.js
+++ src/app/lib/globalStyles.test.js
@@ -7,7 +7,8 @@ describe('globalStyles', () => {
     await import('./globalStyles');
 
     const createGlobalStyleArgs =
-      styledComponents.createGlobalStyle.mock.calls[0];
+      styledComponents.createGlobalStyle.mock.calls[1];
+    // calls[1] as 'styled-normalize' calls createGlobalStyle first itself
 
     expect(createGlobalStyleArgs).toMatchSnapshot();
   });
