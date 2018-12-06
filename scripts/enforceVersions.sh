37f95f239c1de9ba2c73352b87a8800d899bd3d7
Phil Lee <phil.lee.ext@bbc.co.uk>
2018-12-06 17:48:34 +0000

    Update scripts/enforceVersions.sh
    Co-Authored-By: dr3 <drew.mcmillan@bbc.co.uk>

--- scripts/enforceVersions.sh
+++ scripts/enforceVersions.sh
@@ -15,7 +15,7 @@ if [[ $(pwd) =~ .+simorgh ]]; then
 
   if [[ $(npm -v) != $desired_npm_version ]]; then
     tput setaf 1
-    echo "Oh no! You're not using our version of npm, please move to version $desired_npm_version"
+    echo "\nOh no! You're not using our version of npm, please move to version $desired_npm_version in order to reduce bundling inconsistency"
     tput sgr0
     echo "This can be done by running 'npm i -g npm@$desired_npm_version'"
     echo "Once you have done this please run 'npm install' again and commit any changes to your package-lock.json"
