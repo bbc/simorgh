7f82e3c0ba732ca021c8f276cd70a31a111b3c47
Sareh Heidari <sareh.heidari@gmail.com>
2018-12-12 23:46:02 +0000

    Factor out regex url & article path matching logic to a pathIfMatching function
    If there is a match, it'll return the path

--- src/app/containers/InlineLink/index.jsx
+++ src/app/containers/InlineLink/index.jsx
@@ -21,24 +21,28 @@ const reactRouterLink = (path, blocks) => {
   );
 };
 
-const InlineLinkContainer = ({ locator, blocks }) => {
-  const regexp = pathToRegexp(articleRegexPath, [], {
+const pathIfMatching = (regexString, url) => {
+  const regexp = pathToRegexp(regexString, [], {
     start: false,
     end: false,
   });
+  const regexResultArray = regexp.exec(url);
+  if (!regexResultArray) {
+    return null;
+  }
+  const path = regexResultArray[0];
+  return path;
+};
 
-  const result = regexp.exec(locator);
-  // if URL matches a valid route, use a react-router link
-  if (result) {
-    // the path is the first item in the array
-    const path = result[0];
+const InlineLinkContainer = ({ locator, blocks }) => {
+  const path = pathIfMatching(articleRegexPath, locator);
 
+  if (path) {
     return reactRouterLink(path, blocks);
   }
 
   console.log('IAMASTANDARDLINK');
 
-  // else return a normal hyperlink
   return (
     <InlineLink href={locator}>
       <Blocks blocks={blocks} componentsToRender={componentsToRender} />
