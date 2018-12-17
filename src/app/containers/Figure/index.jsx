aafe6c3d557f9f1e3249b1ba649392bb6a0d48a2
Sareh Heidari <sareh.heidari@gmail.com>
2018-12-17 12:28:27 +0000

    Use Image in Figure Container

--- src/app/containers/Figure/index.jsx
+++ src/app/containers/Figure/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { string, number, objectOf, any } from 'prop-types';
 import Figure from '../../components/Figure';
-import { Img } from '../../components/Figure/Image';
+import Image from '../../components/Figure/Image';
 import ImagePlaceholder from '../../components/Figure/ImagePlaceholder';
 import Copyright from '../Copyright';
 import Caption from '../Caption';
@@ -14,7 +14,7 @@ const renderCaption = block => (block ? <Caption block={block} /> : null);
 const FigureContainer = ({ src, alt, ratio, copyright, captionBlock }) => (
   <Figure>
     <ImagePlaceholder ratio={ratio}>
-      <Img alt={alt} src={src} />
+      <Image alt={alt} src={src} />
       {renderCopyright(copyright)}
     </ImagePlaceholder>
     {renderCaption(captionBlock)}
