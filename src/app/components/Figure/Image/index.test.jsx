5c6a97321eb7ebbc529e1c974826a8fd73b05963
Sareh Heidari <sareh.heidari@gmail.com>
2018-12-17 12:36:52 +0000

    Refactor tests to remove duplication

--- src/app/components/Figure/Image/index.test.jsx
+++ src/app/components/Figure/Image/index.test.jsx
@@ -12,10 +12,10 @@ import {
   imageSrcCustom,
 } from './fixtureData';
 
-describe("Image - imported as '{ Img }'", () => {
+const snapshotTests = Component => {
   shouldMatchSnapshot(
     'should render portrait image correctly',
-    <Img
+    <Component
       alt={imageAltPortrait}
       src={imageSrcPortrait}
       height={723}
@@ -24,7 +24,7 @@ describe("Image - imported as '{ Img }'", () => {
   );
   shouldMatchSnapshot(
     'should render landscape image correctly',
-    <Img
+    <Component
       alt={imageAltLandscape}
       src={imageSrcLandscape}
       height={1024}
@@ -33,36 +33,7 @@ describe("Image - imported as '{ Img }'", () => {
   );
   shouldMatchSnapshot(
     'should render square image correctly',
-    <Img alt={imageAltSquare} src={imageSrcSquare} height={660} width={660} />,
-  );
-  shouldMatchSnapshot(
-    'should render image with custom dimensions correctly',
-    <Img alt={imageAltCustom} src={imageSrcCustom} height={660} width={660} />,
-  );
-});
-
-describe("Image - imported as default 'Image'", () => {
-  shouldMatchSnapshot(
-    'should render portrait image correctly',
-    <Image
-      alt={imageAltPortrait}
-      src={imageSrcPortrait}
-      height={723}
-      width={578}
-    />,
-  );
-  shouldMatchSnapshot(
-    'should render landscape image correctly',
-    <Image
-      alt={imageAltLandscape}
-      src={imageSrcLandscape}
-      height={1024}
-      width={578}
-    />,
-  );
-  shouldMatchSnapshot(
-    'should render square image correctly',
-    <Image
+    <Component
       alt={imageAltSquare}
       src={imageSrcSquare}
       height={660}
@@ -71,11 +42,19 @@ describe("Image - imported as default 'Image'", () => {
   );
   shouldMatchSnapshot(
     'should render image with custom dimensions correctly',
-    <Image
+    <Component
       alt={imageAltCustom}
       src={imageSrcCustom}
       height={660}
       width={660}
     />,
   );
+};
+
+describe("Image - imported as '{ Img }'", () => {
+  snapshotTests(Img);
+});
+
+describe("Image - imported as default 'Image'", () => {
+  snapshotTests(Image);
 });
