d2cc22be07ac61d53c12dd3543209a50af37c1be
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-01-02 09:03:36 +0000

    Make date consistant

--- src/app/containers/Footer/index.test.jsx
+++ src/app/containers/Footer/index.test.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import FooterContainer from './index';
-
 import { shouldShallowMatchSnapshot } from '../../helpers/tests/testHelpers';
 
+const RealDate = Date;
+
 describe(`FooterContainer`, () => {
+  beforeEach(() => {
+    global.Date = class extends RealDate {
+      constructor() {
+        super();
+        return new RealDate('3000-01-01T12:00:00');
+      }
+    };
+  });
+
+  afterEach(() => {
+    global.Date = RealDate;
+  });
+
   shouldShallowMatchSnapshot('should render correctly', <FooterContainer />);
 });
