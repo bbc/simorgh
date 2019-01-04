72205d7dd5c8e13edbac4e020d1120a3f0bafaad
ChrisBAshton <chrisashtonweb@gmail.com>
2019-01-04 16:52:04 +0000

    remove duplication in jest-setup.js

--- src/app/helpers/tests/jest-setup.js
+++ src/app/helpers/tests/jest-setup.js
@@ -2,8 +2,6 @@ import fetch from 'jest-fetch-mock'; // eslint-disable-line import/no-extraneous
 import path from 'path';
 
 global.fetch = fetch;
-process.env.BASE_URL = 'http://localhost:7080';
-process.env.PUBLIC_DIR = 'build/public';
 process.env.ASSETS_MANIFEST_PATH = path.resolve(
   __dirname,
   '../../../server/assets/fixture.json',
