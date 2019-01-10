4dd7cb82632880374e0a0e785ff5d92a328a89c2
radiocontrolled <alison.benjamin@bbc.co.uk>
2019-01-10 17:00:40 +0000

    enzyme Co-authored-by: sareh-heidari <sara.heidari@bbc.co.uk>

--- src/app/containers/Paragraph/index.test.jsx
+++ src/app/containers/Paragraph/index.test.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { render } from 'react-testing-library';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { shouldShallowMatchSnapshot } from '../../helpers/tests/testHelpers';
 import ParagraphContainer from './index';
+// Enzyme adapter provides compatibility with React 16.x:
+
+// configure enzyme to use the adapter you want it to use:
+Enzyme.configure({ adapter: new Adapter() });
+
 // import react-testing methods
 
 const fragmentBlock = (text, attributes = []) => ({
@@ -39,5 +46,34 @@ describe('A react testing lib test ', () => {
   it('does a thing', () => {
     const { container } = render(<ParagraphContainer blocks={blocks} />);
     expect(container.firstChild).toMatchSnapshot();
+  });
+});
+
+describe('The Enzyme tests', () => {
+  describe('Paragraph tested with Enzyme mount', () => {
+    it('should mount a paragraph without throwing an error', () => {
+      const wrapper = Enzyme.mount(<ParagraphContainer blocks={blocks} />);
+      expect(wrapper.length).toBe(1);
+    });
+  });
+
+  describe('Paragraph tested with Enzyme shallow render', () => {
+    it('should render correctly with no props', () => {
+      const component = Enzyme.shallow(<ParagraphContainer blocks={blocks} />);
+      expect(component).toMatchSnapshot();
+    });
+
+    it('should render correctly with text', () => {
+      const component = Enzyme.shallow(<ParagraphContainer blocks={blocks} />);
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe('Paragraph tested with Enzyme rendering to static HTML', () => {
+    it('should render to static HTML', () => {
+      expect(
+        Enzyme.render(<ParagraphContainer blocks={blocks} />).text(),
+      ).toEqual('This is some text.Some text for the  link!');
+    });
   });
 });
