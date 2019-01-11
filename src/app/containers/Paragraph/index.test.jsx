2fa6864e5622a99b3d2d79831dc20c8ffa09a254
radiocontrolled <alison.benjamin@bbc.co.uk>
2019-01-11 10:35:56 +0000

    tidy up example tests

--- src/app/containers/Paragraph/index.test.jsx
+++ src/app/containers/Paragraph/index.test.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
+// Import a react-testing-library method
 import { render } from 'react-testing-library';
 import Enzyme from 'enzyme';
+// Enzyme adapter provides compatibility with React 16.x:
 import Adapter from 'enzyme-adapter-react-16';
 import { shouldShallowMatchSnapshot } from '../../helpers/tests/testHelpers';
 import ParagraphContainer from './index';
-// Enzyme adapter provides compatibility with React 16.x:
 
-// configure enzyme to use the adapter you want it to use:
+// Cnfigure enzyme to use the adapter you want it to use:
 Enzyme.configure({ adapter: new Adapter() });
-
-// import react-testing methods
 
 const fragmentBlock = (text, attributes = []) => ({
   type: 'fragment',
@@ -35,42 +34,44 @@ const inlineLink = inlineLinkBlock('/bbc-test', [
 
 const blocks = [fragmentBlock('This is some text.', ['bold']), inlineLink];
 
-describe('ParagraphContainer', () => {
-  shouldShallowMatchSnapshot(
-    'should render correctly',
-    <ParagraphContainer blocks={blocks} />,
-  );
+describe('Existing snapshot test', () => {
+  describe('ParagraphContainer', () => {
+    shouldShallowMatchSnapshot(
+      'should render correctly',
+      <ParagraphContainer blocks={blocks} />,
+    );
+  });
 });
 
-describe('A react testing lib test ', () => {
-  it('does a thing', () => {
+describe('A react-testing-library snapthot test', () => {
+  it('Should render a ParagraphContainer', () => {
     const { container } = render(<ParagraphContainer blocks={blocks} />);
     expect(container.firstChild).toMatchSnapshot();
   });
 });
 
-describe('The Enzyme tests', () => {
-  describe('Paragraph tested with Enzyme mount', () => {
-    it('should mount a paragraph without throwing an error', () => {
+describe('Enzyme tests', () => {
+  describe('Enzyme mount', () => {
+    it('Should mount a paragraph without throwing an error', () => {
       const wrapper = Enzyme.mount(<ParagraphContainer blocks={blocks} />);
       expect(wrapper.length).toBe(1);
     });
   });
 
-  describe('Paragraph tested with Enzyme shallow render', () => {
-    it('should render correctly with no props', () => {
+  describe('Enzyme shallow render', () => {
+    it('Should render correctly with no props', () => {
       const component = Enzyme.shallow(<ParagraphContainer blocks={blocks} />);
       expect(component).toMatchSnapshot();
     });
 
-    it('should render correctly with text', () => {
+    it('Should render correctly with text', () => {
       const component = Enzyme.shallow(<ParagraphContainer blocks={blocks} />);
       expect(component).toMatchSnapshot();
     });
   });
 
-  describe('Paragraph tested with Enzyme rendering to static HTML', () => {
-    it('should render to static HTML', () => {
+  describe('Enzyme rendering to static HTML', () => {
+    it('Should render to static HTML', () => {
       expect(
         Enzyme.render(<ParagraphContainer blocks={blocks} />).text(),
       ).toEqual('This is some text.Some text for the  link!');
