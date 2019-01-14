c2d26a3bed793ab2846e10365d09717410cac1bd
radiocontrolled <alison.benjamin@bbc.co.uk>
2019-01-14 11:56:43 +0000

    Headings with react-testing-library

--- src/app/containers/Headings/index.test.jsx
+++ src/app/containers/Headings/index.test.jsx
@@ -3,29 +3,56 @@ import React from 'react';
 import { cleanup, render } from 'react-testing-library';
 import HeadingsContainer from './index';
 import { textBlock } from '../../models/blocks';
+import {
+  shouldShallowMatchSnapshot,
+  isNull,
+} from '../../helpers/tests/testHelpers';
 
 afterEach(cleanup);
 
-const template = (title, text, type) => {
-  describe(title, () => {
-    const data = {
-      ...textBlock(text),
-      type,
-    };
-    it(`Should render a HeadlineContainer ${title}`, () => {
-      const { baseElement } = render(<HeadingsContainer {...data} />);
-      expect(baseElement.firstChild).toMatchSnapshot();
-    });
-  });
-};
-
-template('with headline data', 'This is a headline!', 'headline');
-
-template('with subheadline data', 'This is a subheadline', 'subheadline');
-
 describe('React-testing-library snapshot tests', () => {
+  const template = (title, text, type) => {
+    describe(title, () => {
+      const data = {
+        ...textBlock(text),
+        type,
+      };
+      it(`Should render a HeadlineContainer ${title}`, () => {
+        const { baseElement } = render(<HeadingsContainer {...data} />);
+        expect(baseElement.firstChild).toMatchSnapshot();
+      });
+    });
+  };
+  template('with headline data', 'This is a headline!', 'headline');
+
+  template('with subheadline data', 'This is a subheadline', 'subheadline');
+
   it('return null with no data', () => {
     const { container } = render(<HeadingsContainer />);
     expect(container.firstChild).toMatchSnapshot();
+  });
+});
+
+describe('Existing Jest snapshot tests', () => {
+  const template = (title, text, type) => {
+    describe(title, () => {
+      const data = {
+        ...textBlock(text),
+        type,
+      };
+      shouldShallowMatchSnapshot(
+        'should render correctly',
+        <HeadingsContainer {...data} />,
+      );
+    });
+  };
+
+  describe('Headings', () => {
+    describe('with no data', () => {
+      isNull('should not render anything', <HeadingsContainer />);
+    });
+
+    template('with headline data', 'This is a headline!', 'headline');
+    template('with subheadline data', 'This is a subheadline', 'subheadline');
   });
 });
