417eb12012538f2e26ba929c11c1bcffdc5d4fb2
radiocontrolled <alison.benjamin@bbc.co.uk>
2019-01-14 13:48:03 +0000

    WIP - article snaps react-testing-library

--- src/app/containers/Article/index.test.jsx
+++ src/app/containers/Article/index.test.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
+import { render } from 'react-testing-library';
 import { shouldShallowMatchSnapshot } from '../../helpers/tests/testHelpers';
 import ArticleContainer from './index';
+// Import a react-testing-library method
 import { articleDataNews, articleDataPersian } from './fixtureData';
+
+const persianData = {
+  data: articleDataPersian,
+  isAmp: false,
+  service: 'persian',
+};
 
 // explicitly ignore console.log errors for Article/index:getInitialProps() error logging
 global.console.log = jest.fn();
+const newsData = { data: articleDataNews, isAmp: false, service: 'news' };
 
-describe('ArticleContainer', () => {
-  const newsData = { data: articleDataNews, isAmp: false, service: 'news' };
+// temporary: will be removed with https://github.com/BBC-News/simorgh/issues/836
+const newsDataNoHeadline = JSON.parse(JSON.stringify(newsData));
+newsDataNoHeadline.data.content.model.blocks.shift();
 
-  // temporary: will be removed with https://github.com/BBC-News/simorgh/issues/836
-  const newsDataNoHeadline = JSON.parse(JSON.stringify(newsData));
-  newsDataNoHeadline.data.content.model.blocks.shift();
-
-  const persianData = {
-    data: articleDataPersian,
-    isAmp: false,
-    service: 'persian',
-  };
-
+describe('ArticleContainer Jest', () => {
   describe('Component', () => {
     shouldShallowMatchSnapshot(
-      'should render correctly',
+      'should render news correctly',
       <ArticleContainer data={newsData} />,
     );
 
@@ -41,5 +42,29 @@ describe('ArticleContainer', () => {
         <ArticleContainer />,
       );
     });
+  });
+});
+
+describe('ArticleContainer react-testing-library', () => {
+  it('should render news correctly', () => {
+    const { baseElement } = render(<ArticleContainer data={newsData} />);
+    expect(baseElement.firstChild).toMatchSnapshot();
+  });
+
+  it('should render Persian article correctly', () => {
+    const { baseElement } = render(<ArticleContainer data={persianData} />);
+    expect(baseElement.firstChild).toMatchSnapshot();
+  });
+
+  it('should render correctly', () => {
+    const { baseElement } = render(<ArticleContainer data={newsData} />);
+    expect(baseElement.firstChild).toMatchSnapshot();
+  });
+
+  it('should render null if no headline block', () => {
+    const { container } = render(
+      <ArticleContainer data={newsDataNoHeadline} />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
