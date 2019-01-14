be32722850844a8d5d10b119f724e46f55d5e79a
radiocontrolled <alison.benjamin@bbc.co.uk>
2019-01-14 13:19:29 +0000

    tests brandcontainer with react-testing-library

--- src/app/containers/Brand/index.test.jsx
+++ src/app/containers/Brand/index.test.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { render } from 'react-testing-library';
 import BrandContainer from './index';
 import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
 
-describe(`BrandContainer`, () => {
+describe(`BrandContainer Jest `, () => {
   shouldMatchSnapshot('should render correctly', <BrandContainer />);
+});
+
+describe(`BrandContainer react-testing-library`, () => {
+  it('should render correctly ', () => {
+    const { baseElement } = render(<BrandContainer />);
+    expect(baseElement.firstChild).toMatchSnapshot();
+  });
 });
