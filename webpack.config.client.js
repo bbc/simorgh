9a477623b6aeed962d91a99470311813e639b1de
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-01-15 17:00:58 +0000

    Use correct base path in webpack

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -28,7 +28,7 @@ module.exports = ({ resolvePath, IS_CI, IS_PROD, START_DEV_SERVER }) => {
       filename: 'static/js/[name].[hash:8].js',
       // need full URL for dev server & HMR: https://github.com/webpack/docs/wiki/webpack-dev-server#combining-with-an-existing-server
       publicPath: IS_PROD
-        ? `${process.env.SIMORGH_BASE_URL}/`
+        ? `${process.env.PUBLIC_PATH}/`
         : `http://localhost:${webpackDevServerPort}/`,
     },
     optimization: {
