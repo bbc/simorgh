efaf894cb71157bfdfd51ba2e27c76d1d2e045f3
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-01-18 17:02:48 +0000

    Use real assets.json - unfinished

--- src/server/assets/index.js
+++ src/server/assets/index.js
@@ -7,19 +7,8 @@ const getAssetsArray = () => {
     const assetManifest = JSON.parse(
       fs.readFileSync(process.env[assetsManifestEnv]),
     );
-    const assetsManifestKeys = Object.keys(assetManifest);
 
-    /**
-     * Loops through the asset manifest, extracts the JS URL out of each entry and
-     * injects them into the assets array, which is passed to render.
-     * Loops backwards as the client bundle is output first, but needs to be last.
-     */
-    for (let i = assetsManifestKeys.length - 1; i >= 0; i -= 1) {
-      const key = assetsManifestKeys[i];
-      if (key.length > 0) {
-        assets.push(assetManifest[key].js);
-      }
-    }
+    Object.keys(assetManifest).forEach(key => assets.push(assetManifest[key]));
   } catch (error) {
     /* eslint-disable no-console */
     console.log(
