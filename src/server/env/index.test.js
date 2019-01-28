5d4ea7888b8c56884166864916b9b249da787195
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-01-28 13:34:54 +0000

    :white_check_mark: Dedupe the tests

--- src/server/env/index.test.js
+++ src/server/env/index.test.js
@@ -5,6 +5,13 @@ jest.mock('dotenv', () => ({
   config: jest.fn().mockImplementation(() => ({ foo: 'bar' })),
 }));
 
+const testGetEnv = path => {
+  const result = getEnv();
+
+  expect(dotenv.config).toHaveBeenCalledWith({ path });
+  expect(result).toEqual({ foo: 'bar' });
+};
+
 describe('getEnv', () => {
   beforeEach(() => {
     jest.clearAllMocks();
@@ -13,45 +20,30 @@ describe('getEnv', () => {
   it('calls dotenv with correct path when APP_ENV is "local"', async () => {
     process.env.APP_ENV = 'local';
 
-    const result = getEnv();
-
-    expect(dotenv.config).toHaveBeenCalledWith({ path: '.env' });
-    expect(result).toEqual({ foo: 'bar' });
+    testGetEnv('.env');
   });
 
   it('calls dotenv with correct path when APP_ENV is "test"', async () => {
     process.env.APP_ENV = 'test';
 
-    const result = getEnv();
-
-    expect(dotenv.config).toHaveBeenCalledWith({ path: '.env.test' });
-    expect(result).toEqual({ foo: 'bar' });
+    testGetEnv('.env.test');
   });
 
   it('calls dotenv with correct path when APP_ENV is "live"', async () => {
     process.env.APP_ENV = 'live';
 
-    const result = getEnv();
-
-    expect(dotenv.config).toHaveBeenCalledWith({ path: '.env.live' });
-    expect(result).toEqual({ foo: 'bar' });
+    testGetEnv('.env.live');
   });
 
   it('calls dotenv with correct path when APP_ENV is "foobar"', async () => {
     process.env.APP_ENV = 'foobar';
 
-    const result = getEnv();
-
-    expect(dotenv.config).toHaveBeenCalledWith({ path: '.env.foobar' });
-    expect(result).toEqual({ foo: 'bar' });
+    testGetEnv('.env.foobar');
   });
 
   it('calls dotenv with correct path when APP_ENV is undefined', async () => {
     delete process.env.APP_ENV;
 
-    const result = getEnv();
-
-    expect(dotenv.config).toHaveBeenCalledWith({ path: '.env' });
-    expect(result).toEqual({ foo: 'bar' });
+    testGetEnv('.env');
   });
 });
