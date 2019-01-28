57935c5c43535837b8b156c37080f8816e81f36e
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-01-28 20:26:58 +0000

    :white_check_mark: Update snapshot test

--- src/app/components/Manifest/index.test.js
+++ src/app/components/Manifest/index.test.js
@@ -2,10 +2,12 @@ import manifest from './index';
 
 describe('Manifest', () => {
   test('should render correctly for `news` service', () => {
+    process.env.SIMORGH_PUBLIC_STATIC_ASSETS_PATH = 'http://foobar.com';
     expect(manifest('news')).toMatchSnapshot();
   });
 
   test('should render correctly for `persian` service', () => {
+    process.env.SIMORGH_PUBLIC_STATIC_ASSETS_PATH = 'http://foobar.com';
     expect(manifest('persian')).toMatchSnapshot();
   });
 });
