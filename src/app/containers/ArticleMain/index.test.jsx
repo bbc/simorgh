945b0eb28bc8e4f540453a242d47bd850f9ad281
Ben Newcombe <bcmn96@gmail.com>
2019-02-08 10:50:50 +0000

    Change ArticleMain container tests to shallow snapshots, ensuring the call to the Metadata container is tested

--- src/app/containers/ArticleMain/index.test.jsx
+++ src/app/containers/ArticleMain/index.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ArticleMain from '.';
-import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
+import { shouldShallowMatchSnapshot } from '../../helpers/tests/testHelpers';
 import { articleDataNews, articleDataPersian } from '../Article/fixtureData';
 
 // temporary: will be removed with https://github.com/bbc/simorgh/issues/836
@@ -8,17 +8,17 @@ const articleDataNewsNoHeadline = JSON.parse(JSON.stringify(articleDataNews));
 articleDataNewsNoHeadline.content.model.blocks.shift();
 
 describe('ArticleMain', () => {
-  shouldMatchSnapshot(
+  shouldShallowMatchSnapshot(
     'should render a news article correctly',
     <ArticleMain service="news" articleData={articleDataNews} />,
   );
 
-  shouldMatchSnapshot(
+  shouldShallowMatchSnapshot(
     'should render a persian article correctly',
     <ArticleMain service="persian" articleData={articleDataPersian} />,
   );
 
-  shouldMatchSnapshot(
+  shouldShallowMatchSnapshot(
     'should render null if no headline block',
     <ArticleMain service="news" articleData={articleDataNewsNoHeadline} />,
   );
