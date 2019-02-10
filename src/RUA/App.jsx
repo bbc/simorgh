99192423b38a572723a5921d880b473a418b4d96
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-02-10 22:12:20 +0000

    Continue to add features/debug.

--- src/RUA/App.jsx
+++ src/RUA/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
 import { renderRoutes } from 'react-router-config';
 import { withRouter } from 'react-router-dom';
-
 import loadInitialData from './loadInitialData';
 
 const usePrevious = value => {
@@ -9,51 +8,43 @@ const usePrevious = value => {
   useEffect(() => {
     ref.current = value;
   });
-  return ref.current;
+  return ref.current || value;
 };
 
 const App = ({ initialData, location, routes }) => {
   const [data, setData] = useState(initialData);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [loadInitialDataPromise, setLoadInitialDataPromise] = useState(null);
-  const previousLocation = usePrevious(location);
+  const previousLocationPath = usePrevious(location.pathname);
 
-  const stuff = async () => {
-    if (previousLocation && previousLocation.pathname !== location.pathname) {
+  const fetchData = async () => {
+    try {
       setData(null);
       setLoading(true);
       setError(null);
-      setLoadInitialDataPromise(loadInitialData(location.pathname, routes));
-    }
-
-    if (loading) {
-      try {
-        setData(await loadInitialDataPromise);
-        setLoading(false);
-        setError(null);
-        setLoadInitialDataPromise(null);
-      } catch (err) {
-        setData(null);
-        setLoading(false);
-        setError(err);
-        setLoadInitialDataPromise(null);
-      }
-    }
+      setData(await loadInitialData(location.pathname, routes));
+      setLoading(false);
+    } catch (err) {
+      setData(null);
+      setLoading(false);
+      setError(err);
+    } 
   };
 
   useEffect(
     () => {
-      stuff();
+      if (previousLocationPath !== location.pathname) {
+        fetchData();
+      }
     },
-    [location, loading],
+    [location],
   );
 
   return renderRoutes(routes, {
     data,
     loading,
     error,
-  }); 
+  });
 };
 
 export default withRouter(App);
