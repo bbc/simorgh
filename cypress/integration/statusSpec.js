a9f983565e6d0cbc19c2149714a5fe48b84b2ebb
James Brumpton <james.brumpton@bbc.co.uk>
2019-02-11 16:13:30 +0000

    Adding/Deleting files

--- cypress/integration/statusSpec.js
+++ cypress/integration/statusSpec.js
@@ -5,13 +5,6 @@ describe('Simorgh Status', () => {
     testResponseCode('/status', 200);
   });
 
-    it('should return the correct service worker for the env', () => {
-    const response = cy.request('/news/articles/c9rpqy7pmypo').then(({ response }) => {
-    console.log(response)
-    expect(response).to.include('sw-local.js');
-    });
-  });
-
   it('should have the content type set to Javascript', () => {
     testContentType(
       '/news/articles/sw.js',
@@ -19,14 +12,3 @@ describe('Simorgh Status', () => {
     );
   });
 });
-    it('should do a thing', () => {
-    //   // const request = cy.request('/news/articles/c9rpqy7pmypo', 'headers');
-    //   //   console.log(request)
-    //   // // expect(request.categoryId).to.not.be.null
-    //   // });
-      cy.visit('/news/articles/c9rpqy7pmypo');
-      const script = cy.get('script');
-      console.log(script);
-    });
-
-
