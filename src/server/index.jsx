1504405a62191636495cfb65dbcb7ae1788fb117
Sareh Heidari <sareh.heidari@gmail.com>
2019-02-12 18:05:34 +0000

    Add manifest regex path and add a route to the json in the server

--- src/server/index.jsx
+++ src/server/index.jsx
@@ -14,6 +14,7 @@ import gnuTP from 'gnu-terry-pratchett';
 import routes, {
   articleRegexPath,
   articleDataRegexPath,
+  manifestRegexPath,
   swRegexPath,
 } from '../app/routes';
 import { getStyleTag } from './styles';
@@ -118,6 +119,15 @@ server
         res.status(500).send('Unable to find service worker.');
       }
     });
+  })
+  .get(manifestRegexPath, async (req, res) => {
+    const manifestPath = `${__dirname}/public/news/manifest.json`;
+    res.sendFile(manifestPath, {}, error => {
+      if (error) {
+        console.log(error);
+        res.status(500).send('Unable to find manifest');
+      }
+    })
   })
   .get(articleRegexPath, async ({ url }, res) => {
     try {
