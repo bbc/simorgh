7f3c02be080a7a2e2ae0f463c530aba6e718c71b
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-02-13 14:55:02 +0000

    :rocket: Implement very minimal service worker

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+var version = 'v0.1';
+
+self.addEventListener("install", function(event) {
+  console.log('SW ' + version + ': install event.');
+});
+
+self.addEventListener("fetch", function(event) {
+  console.debug('SW ' + version + ': fetch event for ' + event.request.url);
+});
+
+self.addEventListener("activate", function(event) {
+  console.log('SW ' + version + ': activate event.');
+});
--- src/client.test.jsx
+++ src/client.test.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import OfflinePluginRuntime from 'offline-plugin/runtime';
 import { ClientApp } from 'react-universal-app';
 import * as reactDom from 'react-dom';
 import routes from './app/routes';
-
-jest.mock('offline-plugin/runtime', () => ({
-  install: jest.fn(),
-  applyUpdate: jest.fn(),
-}));
 
 jest.mock('react-dom');
 
@@ -29,25 +23,6 @@ describe('Client', () => {
 
   afterAll(() => {
     window.SIMORGH_DATA = null;
-  });
-
-  describe('on production environment', () => {
-    process.env.NODE_ENV = 'production';
-
-    describe('offline-plugin', () => {
-      it('should install runtime with functions for service worker updates', async () => {
-        await import('./client');
-
-        expect(OfflinePluginRuntime.install).toHaveBeenCalledTimes(1);
-        const installConfig = OfflinePluginRuntime.install.mock.calls[0][0];
-
-        installConfig.onUpdateReady();
-        expect(OfflinePluginRuntime.applyUpdate).toHaveBeenCalledTimes(1);
-
-        installConfig.onUpdated();
-        expect(window.swUpdate).toEqual(true);
-      });
-    });
   });
 
   it('should hydrate client once routes are ready', async () => {
