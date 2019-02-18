bb90fac37cd80954cc62e694c5019d15e327a9b6
Ben Newcombe <bcmn96@gmail.com>
2019-02-18 15:25:23 +0000

    Update timestamp component text colour

--- src/app/components/Timestamp/index.jsx
+++ src/app/components/Timestamp/index.jsx
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 import { node, string } from 'prop-types';
 import { GEL_SPACING_DBL } from '@bbc/gel-foundations/spacings';
 import { GEL_BREVIER } from '@bbc/gel-foundations/typography';
-import { C_RHINO } from '@bbc/psammead-styles/colours';
+import { C_CLOUD_DARK } from '@bbc/psammead-styles/colours';
 import { FF_NEWS_SANS_REG } from '@bbc/psammead-styles/fonts';
 
 const StyledTimestamp = styled.time`
   ${GEL_BREVIER};
-  color: ${C_RHINO};
+  color: ${C_CLOUD_DARK};
   display: block;
   font-family: ${FF_NEWS_SANS_REG};
   padding-bottom: ${GEL_SPACING_DBL};
