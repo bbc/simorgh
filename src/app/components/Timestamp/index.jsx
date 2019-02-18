918c53466fcaaaeadff5ef5389b6204c116237fb
Ben Newcombe <bcmn96@gmail.com>
2019-02-18 16:00:12 +0000

    Add prefix and suffix props to Timestamp component, allowing for custom HTML before or after the semantic mark-up for the time itself

--- src/app/components/Timestamp/index.jsx
+++ src/app/components/Timestamp/index.jsx
@@ -6,7 +6,7 @@ import { GEL_BREVIER } from '@bbc/gel-foundations/typography';
 import { C_CLOUD_DARK } from '@bbc/psammead-styles/colours';
 import { FF_NEWS_SANS_REG } from '@bbc/psammead-styles/fonts';
 
-const StyledTimestamp = styled.time`
+const StyledTimestamp = styled.span`
   ${GEL_BREVIER};
   color: ${C_CLOUD_DARK};
   display: block;
@@ -14,13 +14,24 @@ const StyledTimestamp = styled.time`
   padding-bottom: ${GEL_SPACING_DBL};
 `;
 
-const Timestamp = ({ children, datetime }) => (
-  <StyledTimestamp dateTime={datetime}>{children}</StyledTimestamp>
+const Timestamp = ({ children, datetime, prefix, suffix }) => (
+  <StyledTimestamp>
+    {prefix}
+    <time dateTime={datetime}>{children}</time>
+    {suffix}
+  </StyledTimestamp>
 );
 
 Timestamp.propTypes = {
   children: node.isRequired,
   datetime: string.isRequired,
+  prefix: node,
+  suffix: node,
+};
+
+Timestamp.defaultProps = {
+  prefix: null,
+  suffix: null,
 };
 
 export default Timestamp;
