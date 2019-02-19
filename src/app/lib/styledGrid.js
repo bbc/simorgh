9994997ed15d7ebd8b34e347b0e218f32c3311e6
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-02-19 12:06:15 +0000

    :rocket: Make the background not quite white ever though it pretty much is just white

--- src/app/lib/styledGrid.js
+++ src/app/lib/styledGrid.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
+import { C_GHOST } from '@bbc/psammead-styles/colours';
 import { layoutGridWrapper, layoutGridItemConstrained } from './layoutGrid';
 
 export const Wrapper = styled.div`
   ${layoutGridWrapper};
+  background: ${C_GHOST};
 `;
 
 export const GridItemConstrained = styled.div`
