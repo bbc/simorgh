4ed89c8daa1bac82177e95327d8c97b695a861d5
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-03-01 18:31:00 +0000

    client entrypoint use SPArtacus

--- src/client.js
+++ src/client.js
@@ -1,18 +1,9 @@
 /* eslint-disable react/jsx-filename-extension  */
-import React from 'react';
-import { hydrate } from 'react-dom';
-import { ClientApp } from './app/containers/App';
+import { setupClient, data } from '@bbc/spartacus/client';
 import routes from './app/routes';
 
-const data = window.SIMORGH_DATA || {};
-const root = document.getElementById('root');
-
-hydrate(<ClientApp data={data} routes={routes} />, root);
+setupClient(routes, module);
 
 if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
   navigator.serviceWorker.register(`/${data.service}/articles/sw.js`);
-}
-
-if (module.hot) {
-  module.hot.accept();
 }
