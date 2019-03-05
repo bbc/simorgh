b62a550a5830b0594628c820cab319c2cd58d364
E Keane <egjkeane@gmail.com>
2019-03-05 16:34:42 +0000

    Refactor to reduce duplication

--- src/app/containers/Timestamp/relativeTimestamp.test.js
+++ src/app/containers/Timestamp/relativeTimestamp.test.js
@@ -1,58 +1,54 @@
 import relativeTimestamp from './relativeTimestamp';
 
+const timeStampGenerator = (timeDifference, magnitude) =>
+  Date.now() - timeDifference * magnitude;
+
+const timestampWithDiffInHours = hours =>
+  timeStampGenerator(hours, 60 * 60 * 1000);
+
+const timestampWithDiffInMins = mins => timeStampGenerator(mins, 60 * 1000);
+
 describe('relativeTimestamp', () => {
   it('returns a string which ends in ago', () => {
-    const milliseconds = 1 * 60 * 1000;
-    const timestamp = Date.now() - milliseconds;
+    const timestamp = timestampWithDiffInMins(1);
     const result = relativeTimestamp(timestamp);
     expect(typeof result).toEqual('string');
     expect(result.split(' ').pop()).toEqual('ago');
   });
 
   it('returns 1 minute ago', () => {
-    const milliseconds = 1 * 60 * 1000;
-    const timestamp = Date.now() - milliseconds;
+    const timestamp = timestampWithDiffInMins(1);
     const result = relativeTimestamp(timestamp);
     expect(result).toEqual('1 minute ago');
   });
 
   it('returns 5 minutes ago', () => {
-    const milliseconds = 5 * 60 * 1000;
-    const timestamp = Date.now() - milliseconds;
+    const timestamp = timestampWithDiffInMins(5);
     const result = relativeTimestamp(timestamp);
     expect(result).toEqual('5 minutes ago');
   });
 
   it('returns 1 hour ago', () => {
-    const milliseconds = 1 * 60 * 60 * 1000;
-    const timestamp = Date.now() - milliseconds;
+    const timestamp = timestampWithDiffInHours(1);
     const result = relativeTimestamp(timestamp);
     expect(result).toEqual('1 hour ago');
   });
 
   it('returns 5 hours ago', () => {
-    const milliseconds = 5 * 60 * 60 * 1000;
-    const timestamp = Date.now() - milliseconds;
+    const timestamp = timestampWithDiffInHours(5);
     const result = relativeTimestamp(timestamp);
     expect(result).toEqual('5 hours ago');
   });
 
   it('returns hours when greater than 60 minutes', () => {
-    const milliseconds = 94 * 60 * 60 * 1000;
-    const timestamp = Date.now() - milliseconds;
+    const timestamp = timestampWithDiffInHours(94);
     const result = relativeTimestamp(timestamp);
     expect(result).toEqual('94 hours ago');
   });
 
   it('returns 1 minute ago for 2 milliseconds', () => {
-    const milliseconds = 2;
-    const timestamp = Date.now() - milliseconds;
+    const timestamp = timeStampGenerator(2, 1);
     const result = relativeTimestamp(timestamp);
     expect(result).toEqual('1 minute ago');
   });
 });
-
-// const timeStampGenerator(timeDifference) {
-//   const currentTime = Date.now();
-//   return (currentTime + timeDifference).toString();
-// }
