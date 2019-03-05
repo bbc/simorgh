f711c3558f3a38f31da7c413b89c7308ecdf8e82
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-03-05 13:58:53 +0000

    remove shallow as it's purposefor being added is in SPArtacus now

--- src/app/helpers/tests/jest-setup.js
+++ src/app/helpers/tests/jest-setup.js
@@ -1,4 +1,4 @@
-import Enzyme, { shallow, mount } from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import fetch from 'jest-fetch-mock'; // eslint-disable-line import/no-extraneous-dependencies
 import path from 'path';
@@ -6,7 +6,6 @@ import path from 'path';
 Enzyme.configure({ adapter: new Adapter() });
 
 global.fetch = fetch;
-global.shallow = shallow;
 global.mount = mount;
 
 process.env.SPARTACUS_ASSETS_MANIFEST_PATH = path.resolve(
