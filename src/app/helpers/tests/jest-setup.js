adf1d3755d62d570f16657b95389c8802bb72d28
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-03-06 17:05:53 +0000

    remove the static asset mocking as the testing lives in SPArtacus

--- src/app/helpers/tests/jest-setup.js
+++ src/app/helpers/tests/jest-setup.js
@@ -1,9 +1,3 @@
 import fetch from 'jest-fetch-mock'; // eslint-disable-line import/no-extraneous-dependencies
-import path from 'path';
 
 global.fetch = fetch;
-
-process.env.SPARTACUS_ASSETS_MANIFEST_PATH = path.resolve(
-  __dirname,
-  '../../../server/assets/fixture.json',
-);
