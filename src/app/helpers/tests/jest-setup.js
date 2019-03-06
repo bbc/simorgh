e8bdd8729ca372937a17f7434350487d33893acb
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-03-06 17:05:27 +0000

    remove the dependencies needed for testing loadInitialData now its in SPArtacus

--- src/app/helpers/tests/jest-setup.js
+++ src/app/helpers/tests/jest-setup.js
@@ -1,12 +1,7 @@
-import Enzyme, { mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import fetch from 'jest-fetch-mock'; // eslint-disable-line import/no-extraneous-dependencies
 import path from 'path';
 
-Enzyme.configure({ adapter: new Adapter() });
-
 global.fetch = fetch;
-global.mount = mount;
 
 process.env.SPARTACUS_ASSETS_MANIFEST_PATH = path.resolve(
   __dirname,
