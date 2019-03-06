97447108e9ba2cf1321a89d6eb76de6c8e5619bd
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-03-06 16:58:06 +0000

    dotenv should be handled by spartacus

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,8 @@
 /* eslint-disable import/no-dynamic-require, global-require */
 const fs = require('fs');
-const dotenv = require('dotenv');
 
 // set the application root path to be set as `context` in the webpack config
 const appDirectory = fs.realpathSync(process.cwd());
-
-// Load in environment variables configured in `.env` file. Be aware the `.env` committed is changed by bake-scripts when on real servers.
-const result = dotenv.config();
-
-if (result.error) {
-  throw result.error;
-}
 
 const getWebpackConfig = require('@bbc/spartacus/webpack.config');
 
