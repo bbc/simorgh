d410b0f57e045b23323909679c2dccea45732e98
E Keane <egjkeane@gmail.com>
2019-03-08 11:12:48 +0000

    completed data structure in LinkData with lots of hard coding

--- src/app/components/LinkData/index.jsx
+++ src/app/components/LinkData/index.jsx
@@ -1,12 +1,61 @@
 import React, { Fragment } from 'react';
-import { string } from 'prop-types';
+import { string, number } from 'prop-types';
 
-const LinkData = ({ type, seoHeadline }) => {
+const LinkData = ({
+  type,
+  seoHeadline,
+  firstPublished,
+  lastUpdated,
+  optimoUrn,
+  service,
+}) => {
   const linkMetadata = {};
+
+  const imgObject = 'ImageObject';
+  const newsMediaOrg = 'NewsMediaOrganization';
+  const url =
+    'https://www.bbc.com/news/special/2015/newsspec_10857/bbc_news_logo.png?cb=1';
+
+  const optimoId = optimoUrn.split(':').pop();
+
+  const logo = {
+    '@type': imgObject,
+    width: 1024,
+    height: 576,
+    url,
+  };
+
+  const image = {
+    '@type': imgObject,
+    width: 1024,
+    height: 576,
+    url,
+  };
+
+  const publisher = {
+    '@type': newsMediaOrg,
+    name: service,
+    publishingPrinciples: 'http://www.bbc.com/news/help-41670342',
+    logo,
+  };
+
+  const author = {
+    '@type': newsMediaOrg,
+    name: service,
+    logo,
+    noBylinesPolicy: 'http://www.bbc.com/news/help-41670342#authorexpertise',
+  };
 
   linkMetadata['@context'] = 'http://schema.org';
   linkMetadata['@type'] = type;
+  linkMetadata.url = `https://www.bbc.com/${service}/articles/${optimoId}`;
+  linkMetadata.publisher = publisher;
+  linkMetadata.datePublished = new Date(firstPublished).toISOString();
+  linkMetadata.dateModified = new Date(lastUpdated).toISOString();
   linkMetadata.headline = seoHeadline;
+  linkMetadata.image = image;
+  linkMetadata.thumbnailUrl = url;
+  linkMetadata.author = author;
 
   return (
     <Fragment>
@@ -22,6 +71,10 @@ const LinkData = ({ type, seoHeadline }) => {
 LinkData.propTypes = {
   type: string.isRequired,
   seoHeadline: string.isRequired,
+  firstPublished: number.isRequired,
+  lastUpdated: number.isRequired,
+  optimoUrn: string.isRequired,
+  service: string.isRequired,
 };
 
 export default LinkData;
--- src/app/components/LinkData/index.test.jsx
+++ src/app/components/LinkData/index.test.jsx
@@ -3,10 +3,17 @@ import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
 import LinkData from '.';
 
 describe('LinkData', () => {
-  const props = { seoHeadline: 'nope', type: 'article' };
+  const props = {
+    seoHeadline: 'Royal wedding 2018: Bouquet laid on tomb of unknown warrior',
+    type: 'article',
+    optimoUrn: 'urn:bbc:optimo:asset:c9rpqy7pmypo',
+    service: 'news',
+    lastUpdated: 1551180310555,
+    firstPublished: 1539188371344,
+  };
 
   shouldMatchSnapshot(
-    'should correctly render for an error',
+    'should correctly render metadata for links',
     <LinkData {...props} />,
   );
 });
