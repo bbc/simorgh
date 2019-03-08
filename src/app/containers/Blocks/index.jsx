bf120aed1f17a83a5eccb9e63ae24138b99dcbd2
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-03-08 13:48:12 +0000

    :rocket: Remove string render fallback

--- src/app/containers/Blocks/index.jsx
+++ src/app/containers/Blocks/index.jsx
@@ -2,28 +2,22 @@ import React from 'react';
 import { objectOf, arrayOf, func, shape, string, any } from 'prop-types';
 import nanoid from 'nanoid';
 
-// Inlined as this is a temporary component
-const BlockString = props => {
-  const stringProps = JSON.stringify(props);
-  return <p>{stringProps}</p>;
-};
-
 const Blocks = ({ blocks, componentsToRender }) =>
   blocks.map((block, index) => {
     const { type, model } = block;
 
     const { type: typeOfPreviousBlock } = blocks[index - 1] || {};
 
-    const Block = componentsToRender[type] || BlockString;
+    const Block = componentsToRender[type];
 
-    return (
+    return Block ? (
       <Block
         key={nanoid()}
         type={type}
         typeOfPreviousBlock={typeOfPreviousBlock}
         {...model}
       />
-    );
+    ) : null;
   });
 
 Blocks.propTypes = {
