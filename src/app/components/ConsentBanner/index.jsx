3c4495323660ef6e672d3f6152532cd4b7f75fdc
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-03-11 18:14:42 +0000

    :rocket: Add consent banner

---
+++ src/app/components/ConsentBanner/index.jsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import { string, element } from 'prop-types';
+import styled from 'styled-components';
+import { FF_NEWS_SANS_REG } from '@bbc/psammead-styles/fonts';
+import {
+  GEL_GREAT_PRIMER,
+  GEL_LONG_PRIMER,
+} from '@bbc/gel-foundations/typography';
+import {
+  GEL_GROUP_3_SCREEN_WIDTH_MIN,
+  GEL_GROUP_2_SCREEN_WIDTH_MIN,
+} from '@bbc/gel-foundations/breakpoints';
+import {
+  GEL_MARGIN_BELOW_400PX,
+  GEL_MARGIN_ABOVE_400PX,
+} from '@bbc/gel-foundations/spacings';
+
+const GREY = '#323232';
+const ORANGE = '#fa9900';
+const WHITE = '#ffffff';
+const LIGHTGREY = '#bdbdbd';
+
+const Wrapper = styled.div`
+  font-family: ${FF_NEWS_SANS_REG};
+  background-color: ${GREY};
+  padding: 16px ${GEL_MARGIN_BELOW_400PX};
+
+  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
+    padding: ${GEL_MARGIN_ABOVE_400PX};
+  }
+`;
+
+const CenterWrapper = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+
+  &::after {
+    content: '\\0020';
+    display: block;
+    height: 0;
+    clear: both;
+    overflow: hidden;
+    visibility: hidden;
+  }
+`;
+
+const Title = styled.h2`
+  ${GEL_GREAT_PRIMER}
+  color: ${WHITE};
+  font-weight: 700;
+  padding: 0;
+  margin: 0;
+
+  @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
+    width: 22%;
+    margin-right: 3.5%;
+    float: left;
+  }
+`;
+
+const Options = styled.ul`
+  ${GEL_LONG_PRIMER}
+  font-weight: 600;
+  color: ${ORANGE};
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+
+  & li + li {
+    padding-top: 8px;
+  }
+
+  @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
+    width: 18%;
+    float: right;
+  }
+`;
+
+export const ConsentBannerText = styled.p`
+  color: ${LIGHTGREY};
+  ${GEL_LONG_PRIMER};
+
+  @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
+    margin: 0;
+    float: left;
+    width: 53%;
+  }
+`;
+
+export const ConsentBannerButton = styled.button`
+  ${GEL_GREAT_PRIMER}
+  font-weight: 700;
+  color: ${ORANGE};
+  background: none;
+  border: none;
+  padding: 0;
+  margin: 0;
+  cursor: pointer;
+`;
+
+export const ConsentBannerLink = styled.a`
+  text-decoration: none;
+  color: ${ORANGE};
+`;
+
+export const ConsentBanner = ({ title, text, accept, reject }) => (
+  <Wrapper>
+    <CenterWrapper>
+      <Title>{title}</Title>
+      {text}
+      <Options>
+        <li>{accept}</li>
+        <li>{reject}</li>
+      </Options>
+    </CenterWrapper>
+  </Wrapper>
+);
+
+ConsentBanner.propTypes = {
+  title: string.isRequired,
+  text: element.isRequired,
+  accept: element.isRequired,
+  reject: element.isRequired,
+};
---
+++ src/app/components/ConsentBanner/index.stories.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
+import {
+  ConsentBanner,
+  ConsentBannerButton,
+  ConsentBannerLink,
+  ConsentBannerText,
+} from '.';
+
+const Accept = <ConsentBannerButton onClick={() => {}}>OK</ConsentBannerButton>;
+
+const Reject = (
+  <ConsentBannerLink href="https://hello.com" onClick={() => {}}>
+    Find out whats changed
+  </ConsentBannerLink>
+);
+
+const Text = (
+  <ConsentBannerText>
+    We have made some important changes to our Privacy and Cookies Policy and we
+    want you to know what this means for you and your data.
+  </ConsentBannerText>
+);
+
+const props = {
+  title: "We've updated our Privacy and Cookies Policy",
+  text: Text,
+  accept: Accept,
+  reject: Reject,
+};
+
+storiesOf('ConsentBanner', module).add('default', () => (
+  <ConsentBanner {...props} />
+));
