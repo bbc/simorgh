e61eb834a10740b1dd6160ddd802e72ad38c51aa
E Keane <egjkeane@gmail.com>
2019-03-12 09:02:08 +0000

    Revert "different approach fails completely"
    This reverts commit aa73e25d94ed14395e7a014c3485ce300799663f.

--- src/app/components/LinkData/index.jsx
+++ src/app/components/LinkData/index.jsx
@@ -43,35 +43,26 @@ const LinkData = ({
     noBylinesPolicy: 'http://www.bbc.com/news/help-41670342#authorexpertise',
   };
 
-  // const linkMetadata = {
-  //   '@context': 'http://schema.org',
-  //   '@type': type,
-  //   url: `https://www.bbc.com/${service}/articles/${optimoId}`,
-  //   publisher,
-  //   datePublished: firstPublished,
-  //   dateModified: lastUpdated,
-  //   headline: seoHeadline,
-  //   image,
-  //   thumbnailUrl: url,
-  //   author,
-  // };
+  const linkMetadata = {
+    '@context': 'http://schema.org',
+    '@type': type,
+    url: `https://www.bbc.com/${service}/articles/${optimoId}`,
+    publisher,
+    datePublished: firstPublished,
+    dateModified: lastUpdated,
+    headline: seoHeadline,
+    image,
+    thumbnailUrl: url,
+    author,
+  };
 
   return (
     <Helmet>
-      <script type="application/ld+json">{`
-        {
-          "@context": "http://schema.org",
-          '@type': ${type},
-          url: ${`https://www.bbc.com/${service}/articles/${optimoId}`},
-          publisher: ${publisher},
-          datePublished: ${firstPublished},
-          dateModified: ${lastUpdated},
-          headline: ${seoHeadline},
-          image: ${image},
-          thumbnailUrl: ${url},
-          author: ${author},
-        }
-    `}</script>
+      {/* eslint-disable react/no-danger */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: linkMetadata }}
+      />
     </Helmet>
   );
 };
