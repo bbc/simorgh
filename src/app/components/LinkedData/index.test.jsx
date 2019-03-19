6e596ef0d81b46add48e7ef650b97c74c3852bde
ChrisBAshton <chrisashtonweb@gmail.com>
2019-03-19 13:27:44 +0000

    fix optimoId
    See https://github.com/bbc/simorgh/pull/1391#discussion_r266597392

--- src/app/components/LinkedData/index.test.jsx
+++ src/app/components/LinkedData/index.test.jsx
@@ -8,7 +8,7 @@ describe('LinkData', () => {
     lang: 'en-GB',
     seoHeadline: 'Royal wedding 2018: Bouquet laid on tomb of unknown warrior',
     type: 'article',
-    optimoId: 'urn:bbc:optimo:asset:c9rpqy7pmypo',
+    optimoId: 'c9rpqy7pmypo',
     service: 'news',
     lastUpdated: '2019-02-26T11:25:10.555Z',
     firstPublished: '2018-10-10T16:19:31.344Z',
