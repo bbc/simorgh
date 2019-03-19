f25694bc0ec18de2737e3e3c54e21fa7e2db3a99
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-03-19 14:31:18 +0000

    :pencil: Add comment

--- cypress/support/skipLiveDescribe.js
+++ cypress/support/skipLiveDescribe.js
@@ -1,3 +1,6 @@
+// Allows tests to skipped on live.
+// Used currently to stop persian tests running on
+// live due to them not yet being routed in the STMs
 const skipLiveDescribe = (name, func) => {
   if (Cypress.env('APP_ENV') !== 'live') {
     describe(name, func);
