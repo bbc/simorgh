c3a19ea5caa577b979e002d9af19456b858a42cc
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-03-20 18:26:58 +0000

    core-js 2.6.5 and set + map polyfills

--- src/client.js
+++ src/client.js
@@ -1,4 +1,5 @@
-/* eslint-disable react/jsx-filename-extension  */
+import 'core-js/es6/set'; // polyfill for >IE11
+import 'core-js/es6/map'; // polyfill for >IE11
 import React from 'react';
 import { hydrate } from 'react-dom';
 import { ClientApp } from './app/containers/App';
@@ -7,7 +8,7 @@ import routes from './app/routes';
 const data = window.SIMORGH_DATA || {};
 const root = document.getElementById('root');
 
-hydrate(<ClientApp data={data} routes={routes} />, root);
+hydrate(<ClientApp data={data} routes={routes} />, root); // eslint-disable-line react/jsx-filename-extension
 
 if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
   navigator.serviceWorker.register(`/${data.service}/articles/sw.js`);
