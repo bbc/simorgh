f0a85a4c154bbffe6df9518b301b234f0f4c3f1c
James Donohue <james.donohue@bbc.co.uk>
2019-03-21 17:34:44 +0000

    Config only static hostname not full URL

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -46,7 +46,9 @@ module.exports = ({ resolvePath, IS_CI, IS_PROD, START_DEV_SERVER }) => {
       // need full URL for dev server & HMR: https://github.com/webpack/docs/wiki/webpack-dev-server#combining-with-an-existing-server
       publicPath: START_DEV_SERVER
         ? `http://localhost:${webpackDevServerPort}/`
-        : `${process.env.SIMORGH_PUBLIC_STATIC_ASSETS_PATH}/`,
+        : `https://${
+            process.env.SIMORGH_PUBLIC_STATIC_ASSETS_HOST
+          }/include/articles/public/`,
     },
     optimization: {
       // specify min/max file sizes for each JS chunk for optimal performance
