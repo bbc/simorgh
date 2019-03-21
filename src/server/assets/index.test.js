bbcf00bab39b9902e9cf66365f7770f08de7717f
James Donohue <james.donohue@bbc.co.uk>
2019-03-21 17:31:04 +0000

    Add helper to determine asset domains

--- src/server/assets/index.test.js
+++ src/server/assets/index.test.js
@@ -16,7 +16,7 @@ describe('getAssetsArray', () => {
   describe('no assets manifest', () => {
     it('should log an error', async () => {
       delete process.env.SIMORGH_ASSETS_MANIFEST_PATH;
-      const getAssetsArray = require('./index.js').default;
+      const { getAssetsArray } = require('./index.js');
       getAssetsArray();
       expect(mockLogError).toHaveBeenCalledWith(
         `Error parsing assets manifest. SIMORGH_ASSETS_MANIFEST_PATH = ${
@@ -32,7 +32,7 @@ describe('getAssetsArray', () => {
         __dirname,
         'fixture.json',
       );
-      const getAssetsArray = require('./index.js').default;
+      const { getAssetsArray } = require('./index.js');
       expect(getAssetsArray()).toEqual(['one.js']);
       expect(mockLogError).not.toHaveBeenCalled();
     });
@@ -44,9 +44,36 @@ describe('getAssetsArray', () => {
         __dirname,
         'fixtureMissingKey.json',
       );
-      const getAssetsArray = require('./index.js').default;
+      const { getAssetsArray } = require('./index.js');
       expect(getAssetsArray()).toEqual(['foo.js']);
       expect(mockLogError).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('getAssetDomains', () => {
+    const defaultDomains = ['ichef.bbci.co.uk', 'gel.files.bbci.co.uk'];
+
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    describe('with static assets hostname', () => {
+      it('should return the manifest contents as key-value pairs', async () => {
+        process.env.SIMORGH_PUBLIC_STATIC_ASSETS_HOST = 'some.statichost.net';
+        const { getAssetDomains } = require('./index.js');
+        expect(getAssetDomains()).toEqual([
+          ...defaultDomains,
+          'some.statichost.net',
+        ]);
+      });
+    });
+
+    describe('without static assets hostname', () => {
+      it('should return the manifest contents as key-value pairs', async () => {
+        delete process.env.SIMORGH_PUBLIC_STATIC_ASSETS_HOST;
+        const { getAssetDomains } = require('./index.js');
+        expect(getAssetDomains()).toEqual(defaultDomains);
+      });
     });
   });
 });
