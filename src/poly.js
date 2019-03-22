0cce21243a3501072230966391f8379819785dda
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-03-22 18:00:47 +0000

    :pencil: Allllll the comments

--- src/poly.js
+++ src/poly.js
@@ -1,7 +1,16 @@
+// Needed by React 16 https://reactjs.org/docs/javascript-environment-requirements.html
+// IE11, IE10, IE9
 import 'core-js/es6/map';
 import 'core-js/es6/set';
+
+// Needed by Styled Components
+// IE10, IE9
 import 'core-js/es6/symbol';
+
+// IE9
 import 'core-js/fn/typed/uint8-array';
+
+// IE10, IE9
 import getRandomValues from 'polyfill-crypto.getrandomvalues';
 
 if (!window.crypto && !window.msCrypto) {
