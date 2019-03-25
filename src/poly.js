2223c5b75de1464f1d53051bdb49b30fa1a34b40
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-03-25 09:01:58 +0000

    Update poly.js

--- src/poly.js
+++ src/poly.js
@@ -1,10 +1,10 @@
 // Needed by React 16 https://reactjs.org/docs/javascript-environment-requirements.html
-// IE11, IE10, IE9
+// IE10, IE9
 import 'core-js/es6/map';
 import 'core-js/es6/set';
 
 // Needed by Styled Components
-// IE10, IE9
+// IE11, IE10, IE9
 import 'core-js/es6/symbol';
 
 // IE9
