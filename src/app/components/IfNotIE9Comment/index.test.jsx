03a212a1cc357e9a8bb26cd26e441479326c8f48
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-03-26 17:36:34 +0000

    :pencil: Update wording

--- src/app/components/IfNotIE9Comment/index.test.jsx
+++ src/app/components/IfNotIE9Comment/index.test.jsx
@@ -3,10 +3,13 @@ import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
 import { IfNotIE9Open, IfNotIE9Close } from '.';
 
 describe('Timestamp', () => {
-  shouldMatchSnapshot('should render IE9 limit comment open', <IfNotIE9Open />);
+  shouldMatchSnapshot(
+    'should render if not IE9 comment open',
+    <IfNotIE9Open />,
+  );
 
   shouldMatchSnapshot(
-    'should render IE9 limit comment close',
+    'should render if not IE9 comment close',
     <IfNotIE9Close />,
   );
 });
