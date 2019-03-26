04ceff0e2d07b8f94f138ebeb1514aeebc6895b9
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-03-26 13:03:55 +0000

    /status was for the public endpoint & service worker test not on TEST and LIVE

--- cypress/integration/statusSpec.js
+++ cypress/integration/statusSpec.js
@@ -1,10 +1,7 @@
-import { testResponseCode, testContentType } from '../support/metaTestHelper';
+import { testContentType } from '../support/metaTestHelper';
+import describeForLocalOnly from '../support/describeForLocalOnly';
 
-describe('Simorgh Status', () => {
-  it('should return 200', () => {
-    testResponseCode('/status', 200);
-  });
-
+describeForLocalOnly('Service worker', () => {
   it('should have the content type set to Javascript', () => {
     testContentType(
       '/news/articles/sw.js',
