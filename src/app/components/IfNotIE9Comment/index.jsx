f61ee5a832e4c794fcc695cacc724fad3e7abe1b
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-03-26 17:04:14 +0000

    :pencil: Update naming

--- src/app/components/IfNotIE9Comment/index.jsx
+++ src/app/components/IfNotIE9Comment/index.jsx
@@ -4,10 +4,10 @@ import React from 'react';
  * These, when used together, stop anything within them from loading on IE9 and below.
  */
 
-export const IE9LimitOpen = () => (
+export const IfNotIE9Open = () => (
   <div dangerouslySetInnerHTML={{ __html: '<!--[if !IE]><!-->' }} /> // eslint-disable-line react/no-danger
 );
 
-export const IE9LimitClose = () => (
+export const IfNotIE9Close = () => (
   <div dangerouslySetInnerHTML={{ __html: '<!--<![endif]-->' }} /> // eslint-disable-line react/no-danger
 );
