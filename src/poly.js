d21ae3dd967743250b2810cb35345ee4595aef2b
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-03-26 13:04:52 +0000

    :rocket: Stop the bundles loading on IE9

--- src/poly.js
+++ src/poly.js
@@ -1,16 +1,13 @@
 // Needed by React 16 https://reactjs.org/docs/javascript-environment-requirements.html
-// IE10, IE9
+// IE10
 import 'core-js/es6/map';
 import 'core-js/es6/set';
 
 // Needed by Styled Components
-// IE11, IE10, IE9
+// IE11, IE10
 import 'core-js/es6/symbol';
 
-// IE9
-import 'core-js/fn/typed/uint8-array';
-
-// IE10, IE9
+// IE10
 import getRandomValues from 'polyfill-crypto.getrandomvalues';
 
 if (!window.crypto && !window.msCrypto) {
