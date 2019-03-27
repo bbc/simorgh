2b586f8c1070878cd0a5de57a27f91dabdf02b74
E Keane <egjkeane@gmail.com>
2019-03-27 13:59:14 +0000

    add caption logic

--- src/app/lib/layoutGrid.js
+++ src/app/lib/layoutGrid.js
@@ -45,6 +45,18 @@ const starts1spans5of6MaxWidthScaleable = `83.33%`;
 const starts1spans4of6MaxWidthScaleable = `66.67%`;
 // (4 / 6) * 100 = 66.6666.. = 66.67%
 
+const starts2spans7of8MaxWidthGroup4 = `53.25rem`;
+/* (group4ColWidth 6.75rem * 7) + (6 * 16px gutters) = 47.25rem + 6rem = 53.25rem */
+
+const starts5spans15of20MaxWidthGroup5 = `58.25rem`;
+/* (group5ColWidth 2.95rem * 15) + (14 * 16px gutters) = 44.25rem + 14rem = 58.25rem */
+
+const starts2spans5of8MaxWidthGroup4 = `37.75rem`;
+/* (group4ColWidth 6.75rem * 5) + (4 * 16px gutters) = 33.75rem + 4rem = 37.75rem */
+
+const starts5spans13of20MaxWidthGroup5 = `??rem`;
+/* (group5ColWidth 2.95rem * 13) + (12 * 16px gutters) = 38.35rem + 12rem = 50.35rem */
+
 export const gelGridMargin = css`
   @media (max-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
     padding: 0 ${GEL_MARGIN_BELOW_400PX};
@@ -162,4 +174,58 @@ export const layoutGridItemSmall = css`
 
 export const layoutGridItem = css`
   grid-column: 1 / -1;
+`;
+
+export const captionGridItemLandscapeSquare = css`
+  ${gelGridMargin}
+
+  @media (max-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
+    grid-column: 1 / span 6;
+  }
+  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_2_SCREEN_WIDTH_MAX}) {
+    grid-column: 1 / span 6;
+  }
+  @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
+    grid-column: 1 / span 5;
+    max-width: ${starts1spans5of6MaxWidthScaleable};
+  }
+  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_4_SCREEN_WIDTH_MAX}) {
+    grid-column: 2 / span 7;
+    max-width: ${starts2spans7of8MaxWidthGroup4};
+  }
+  @media (min-width: ${GEL_GROUP_5_SCREEN_WIDTH_MIN}) {
+    grid-column: 5 / span 15;
+    max-width: ${starts5spans15of20MaxWidthGroup5};
+  }
+
+  @supports (display: grid) {
+    max-width: initial;
+  }
+`;
+
+export const captionGridItemPortrait = css`
+  ${gelGridMargin}
+
+  @media (max-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
+    grid-column: 1 / span 6;
+  }
+  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_2_SCREEN_WIDTH_MAX}) {
+    grid-column: 1 / span 6;
+  }
+  @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
+    grid-column: 1 / span 4;
+    max-width: ${starts1spans4of6MaxWidthScaleable};
+  }
+  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_4_SCREEN_WIDTH_MAX}) {
+    grid-column: 2 / span 5;
+    max-width: ${starts2spans5of8MaxWidthGroup4};
+  }
+  @media (min-width: ${GEL_GROUP_5_SCREEN_WIDTH_MIN}) {
+    grid-column: 5 / span 13;
+    max-width: ${starts5spans13of20MaxWidthGroup5};
+  }
+
+  @supports (display: grid) {
+    max-width: initial;
+  }
 `;
