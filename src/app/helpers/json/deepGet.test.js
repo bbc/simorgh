3a0722f124cc4ada4b30f24ae23dc55207789e3e
ChrisBAshton <chrisashtonweb@gmail.com>
2019-03-27 10:30:17 +0000

    adds tests for deepGet

---
+++ src/app/helpers/json/deepGet.test.js
@@ -0,0 +1,22 @@
+import deepGet from './deepGet';
+
+const fixtureData = {
+  foo: 123,
+  bar: {
+    baz: 456,
+  },
+};
+
+describe('deepGet', () => {
+  it('should retrieve items one level deep', () => {
+    expect(deepGet(['foo'], fixtureData)).toEqual(123);
+  });
+
+  it('should retrieve items two levels deep', () => {
+    expect(deepGet(['bar', 'baz'], fixtureData)).toEqual(456);
+  });
+
+  it('should return null for missing properties', () => {
+    expect(deepGet(['badKeyName', 'baz'], fixtureData)).toEqual(null);
+  });
+});
