03602c92ec540869ffe8588a2389bf1ab144861e
E Keane <egjkeane@gmail.com>
2019-04-12 09:02:08 +0100

    Revert "Increase number of files expected in tests"
    This reverts commit 26f528f7584ee45d4439c8ae9ec57403922e2cdb.

--- src/app/dataValidator/helpers/dataLoader/asyncValidateDir.test.js
+++ src/app/dataValidator/helpers/dataLoader/asyncValidateDir.test.js
@@ -33,13 +33,13 @@ describe('asyncValidateDir helper', () => {
   it('should call readScenario for every file in the /data directory', async () => {
     const readScenarioSpy = jest.spyOn(readScenario, 'readScenario');
 
-    await expectMethodToBeCalledTimes(50, readScenarioSpy);
+    await expectMethodToBeCalledTimes(49, readScenarioSpy);
   });
 
   it('should call fileToValidate for only the valid json file in the /data directory', async () => {
     fileToValidateSpy = jest.spyOn(readScenario, 'fileToValidate');
 
-    await expectMethodToBeCalledTimes(37, fileToValidateSpy);
+    await expectMethodToBeCalledTimes(36, fileToValidateSpy);
   });
 
   it('should call fileToValidate for only the files in /data/prod/news', async () => {
