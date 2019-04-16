30a34ea9d85234122078a7aacc6c50c7ef9d3e7f
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-04-16 14:38:52 +0100

    caption -> captionContainer

--- src/app/containers/Caption/index.test.jsx
+++ src/app/containers/Caption/index.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { latin, arabic } from '@bbc/gel-foundations/scripts';
-import Caption from '.';
+import CaptionContainer from '.';
 import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
 import { ServiceContext } from '../../contexts/ServiceContext';
 import { blockContainingText } from '../../models/blocks';
@@ -18,7 +18,7 @@ const block = blockContainingText('caption', 'Some caption text...');
 
 const CaptionWithContext = contextStub => (
   <ServiceContext.Provider value={contextStub}>
-    <Caption block={block} />
+    <CaptionContainer block={block} />
   </ServiceContext.Provider>
 );
 
