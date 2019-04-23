c43560027611da301c5fa0a8d3c7bf426699edef
Ross Gaskell <ross.gaskell@bbc.co.uk>
2019-04-23 17:07:32 +0100

    Use Dial Context to render beacon conditionally

--- src/app/containers/MPulseBeacon/index.jsx
+++ src/app/containers/MPulseBeacon/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { DialContext } from '../../contexts/DialContext';
 import MPulseBeacon from '../../components/MPulseBeacon';
 
 const MPulseBeaconContainer = () => {
+  const { mpulse } = useContext(DialContext);
   const API_KEY = process.env.MPULSE_API_KEY;
-  return API_KEY ? <MPulseBeacon apiKey={API_KEY} /> : null;
+  const isEnabled = mpulse && API_KEY;
+
+  return isEnabled ? <MPulseBeacon apiKey={API_KEY} /> : null;
 };
 
 export default MPulseBeaconContainer;
--- src/app/containers/MPulseBeacon/index.test.jsx
+++ src/app/containers/MPulseBeacon/index.test.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import { isNull } from '../../helpers/tests/testHelpers';
+import { DialContextProvider } from '../../contexts/DialContext';
 import MPulseBeaconContainer from './index';
 import MPulseBeacon from '../../components/MPulseBeacon';
 
 jest.mock('../../components/MPulseBeacon', () => () => <script>mPulse</script>);
 
-describe('MPulseBeacon', () => {
-  describe('env has mPulse api key', () => {
-    const apiKey = 'XXXXX-XXXXX-XXXXX-XXXXX-XXXX';
-    beforeEach(() => {
-      process.env.MPULSE_API_KEY = apiKey;
-    });
+// eslint-disable-next-line react/prop-types
+const beaconContainerWithContext = dials => (
+  <DialContextProvider dials={dials}>
+    <MPulseBeaconContainer />
+  </DialContextProvider>
+);
 
+describe('MPulseBeacon', () => {
+  const context = { mpulse: true };
+  const apiKey = 'XXXXX-XXXXX-XXXXX-XXXXX-XXXX';
+
+  beforeEach(() => {
+    context.mpulse = true;
+    process.env.MPULSE_API_KEY = apiKey;
+  });
+
+  describe('env has mPulse api key and mpulse dial is enabled', () => {
     it('should render a beacon component with the api key', () => {
-      const testRenderer = TestRenderer.create(<MPulseBeaconContainer />);
+      const testRenderer = TestRenderer.create(
+        beaconContainerWithContext(context),
+      );
       const testInstance = testRenderer.root;
 
       expect(testInstance.findAllByType(MPulseBeacon)).toHaveLength(1);
@@ -24,11 +37,19 @@ describe('MPulseBeacon', () => {
     });
   });
 
+  describe('mpulse dial is disabled', () => {
+    beforeEach(() => {
+      context.mpulse = false;
+    });
+
+    isNull('should not render the beacon', beaconContainerWithContext(context));
+  });
+
   describe('env does not have mPulse api key', () => {
     beforeEach(() => {
       delete process.env.MPULSE_API_KEY;
     });
 
-    isNull('should not render the beacon', <MPulseBeaconContainer />);
+    isNull('should not render the beacon', beaconContainerWithContext(context));
   });
 });
