b09e0da8ea0313cfeae462eb611f513a956fd1b1
Andrzej Pogonowski <pogon.ap@hotmail.com>
2019-04-23 15:47:37 +0100

    Some clarifications

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 # package.sh by Andrzej Pogonowski
 # What is this?: This is the script that packages all the necessary files for deployment
-# Why does this exists?: The final deliverable is thicc so we need to limit the amout of files that are passed
+# Why does this exists?: Apparently jenkins agents dump core and memory information into the directory
+# each of these files is 40MB, adding node_modules and the build folder the resulting zip file is 40MB
+# This bash script solves the problem by creating a folder and copying only the required folders 
 
 # Steps
 # 1. Removes all of the build and dependency directories (build, pack, node_modules)
 # 2. Copies all of the source files
-# 
+# 3. Copes configuration files and other necessary files
 
+# Cleanup
 rm -r pack
 rm -r build
 rm -r node_modules
 mkdir pack
 chmod -R 755 pack
-pwd
 
+# Copy needed directories
 cp -rf ./src ./pack/src
 cp -rf ./cypress ./pack/cypress
 cp -rf ./.storybook ./pack/.storybook
@@ -22,6 +25,7 @@ cp -rf ./envConfig ./pack/envConfig
 cp -rf ./public ./pack/public
 cp -rf ./data ./pack/data
 
+# Copy the needed files in the root directory
 cp package.json ./pack
 cp package-lock.json ./pack
 cp .babelrc ./pack
@@ -33,6 +37,7 @@ cp Jenkinsfile ./pack
 cp Makefile ./pack
 cp a11y.js ./pack
 
+# Copy the webpack configs
 cp webpack.config.js ./pack
 cp webpack.config.client.js ./pack
 cp webpack.config.server.js ./pack
