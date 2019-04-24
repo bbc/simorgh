1e3d9455e13c095053805997d7cc1424eb55245a
Ruth Ogendi <ruth.ogendi@andela.com>
2019-04-24 14:49:32 +0300

    Refactor test to remove repetition.

--- src/app/containers/Timestamp/timestampUtilities.test.js
+++ src/app/containers/Timestamp/timestampUtilities.test.js
@@ -20,9 +20,10 @@ describe('Timestamp utility functions', () => {
   });
 
   describe('formatUnixTimestamp', () => {
+    const GMTTimestamp = 1483275600000; // 1 January 2017 GMT
+    const BSTTimestamp = 1496235600000; // 31 May 2017 BST
+
     it('should return BST for a BST timestamp', () => {
-      // 31 May 2017 BST
-      const BSTTimestamp = 1496235600000;
       const result = formatUnixTimestamp(
         BSTTimestamp,
         'D MMMM YYYY, HH:mm z',
@@ -32,8 +33,6 @@ describe('Timestamp utility functions', () => {
     });
 
     it('should return GMT for a GMT timestamp', () => {
-      // 1 January 2017 GMT
-      const GMTTimestamp = 1483275600000;
       const result = formatUnixTimestamp(
         GMTTimestamp,
         'D MMMM YYYY, HH:mm z',
@@ -43,7 +42,6 @@ describe('Timestamp utility functions', () => {
     });
 
     it('should return date and time in expected format', () => {
-      const GMTTimestamp = 1483275600000;
       const result = formatUnixTimestamp(
         GMTTimestamp,
         'D MMMM YYYY, HH:mm z',
@@ -53,13 +51,11 @@ describe('Timestamp utility functions', () => {
     });
 
     it('should return short date in expected format', () => {
-      const GMTTimestamp = 1483275600000;
       const result = formatUnixTimestamp(GMTTimestamp, 'YYYY-MM-DD', timezone);
       expect(result).toEqual('2017-01-01');
     });
 
     it('should return long date in expected format', () => {
-      const GMTTimestamp = 1483275600000;
       const result = formatUnixTimestamp(GMTTimestamp, 'D MMMM YYYY', timezone);
       expect(result).toEqual('1 January 2017');
     });
