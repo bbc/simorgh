2c193135742f5be1f93be6dfc999c137cd128270
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-04-25 16:28:35 +0100

    :white_check_mark: Add fetchData.test.js tests

--- src/app/routes/getInitialData/utils/fetchData.test.js
+++ src/app/routes/getInitialData/utils/fetchData.test.js
@@ -1,29 +1,12 @@
-import loggerMock from '../../helpers/tests/loggerMock'; // Must be imported before getInitialData
-import baseUrl from './utils/getBaseUrl';
-import {
-  setWindowValue,
-  resetWindowValue,
-} from '../../helpers/tests/setWindowValue';
+import loggerMock from '../../../helpers/tests/loggerMock'; // Must be imported before getInitialData
+import preprocess from '../../../dataValidator/preprocessor';
 
-const getBaseUrlMockOrigin = 'https://www.mockSite.com';
-jest.mock('./utils/getBaseUrl', () => jest.fn());
-baseUrl.mockImplementation(() => getBaseUrlMockOrigin);
+jest.mock('../../../dataValidator/preprocessor', () => jest.fn());
+preprocess.mockImplementation(data => data);
 
-const getInitialData = require('./article').default;
+const fetchData = require('./fetchData').default;
 
-describe('getInitialData', () => {
-  const defaultIdParam = 'c0000000001o';
-  const defaultServiceParam = 'news';
-  const defaultAmpParam = '';
-  const defaultContext = {
-    match: {
-      params: {
-        id: defaultIdParam,
-        service: defaultServiceParam,
-        amp: defaultAmpParam,
-      },
-    },
-  };
+describe('fetchData', () => {
   const mockSuccessfulResponse = { data: '12345' };
 
   const mockFetchSuccess = () =>
@@ -41,141 +24,97 @@ describe('getInitialData', () => {
   const mockFetchTeapotStatus = () =>
     fetch.mockResponseOnce(JSON.stringify({}), { status: 418 });
 
-  const callGetInitialData = async (
-    context = defaultContext,
-    mockFetch = mockFetchSuccess,
-  ) => {
-    mockFetch();
-    const response = await getInitialData(context);
+  const requestedUrl = 'http://foobar.com/path/to/asset.json';
+
+  const callfetchData = async ({ url, preprocessorRules, mockFetch }) => {
+    if (mockFetch) {
+      mockFetch();
+    } else {
+      mockFetchSuccess();
+    }
+
+    const response = await fetchData({
+      url: url || requestedUrl,
+      preprocessorRules,
+    });
     return response;
   };
 
-  const windowLocation = window.location;
-
   afterEach(() => {
-    resetWindowValue('location', windowLocation);
     fetch.resetMocks();
+    jest.clearAllMocks();
   });
 
-  it('should return the fetch response', async () => {
-    const response = await callGetInitialData();
-    expect(response).toEqual({
-      isAmp: false,
-      data: mockSuccessfulResponse,
-      service: 'news',
-      status: 200,
-    });
-  });
+  describe('Succesful fetch', () => {
+    it('should return an empty object', async () => {
+      const response = await callfetchData({});
 
-  it('should return an amp value of true, when .amp passed in context params', async () => {
-    const contextWithAmp = {
-      match: {
-        params: {
-          id: defaultIdParam,
-          service: defaultServiceParam,
-          amp: '.amp',
+      expect(preprocess).toHaveBeenCalledWith(response.data, undefined);
+
+      expect(response).toEqual({
+        data: {
+          data: '12345',
         },
-      },
-    };
-
-    const response = await callGetInitialData(contextWithAmp);
-    expect(response).toEqual({
-      isAmp: true,
-      data: mockSuccessfulResponse,
-      service: 'news',
-      status: 200,
-    });
-  });
-
-  describe('using correct baseUrl', () => {
-    const BASE_PATH = 'https://www.test.com';
-    beforeEach(() => {
-      process.env.SIMORGH_BASE_URL = BASE_PATH;
-    });
-
-    it('should call fetch with an absolute URL using BASE_PATH environment variable when window location origin is undefined', () => {
-      setWindowValue('location', {
-        origin: undefined,
+        status: 200,
       });
-      callGetInitialData();
-      expect(fetch.mock.calls[0][0]).toEqual(
-        `${BASE_PATH}/${defaultServiceParam}/articles/${defaultIdParam}.json`,
-      );
-    });
-
-    it('should call fetch with an absolute URL using getBaseUrl() value when window location origin is available', () => {
-      setWindowValue('location', {
-        origin: 'https://website.com',
-      });
-      callGetInitialData();
-      expect(fetch.mock.calls[0][0]).toEqual(
-        `${getBaseUrlMockOrigin}/${defaultServiceParam}/articles/${defaultIdParam}.json`,
-      );
     });
   });
 
   describe('Rejected fetch', () => {
     it('should return an empty object', async () => {
-      const response = await callGetInitialData(
-        defaultContext,
-        mockFetchFailure,
-      );
+      const response = await callfetchData({ mockFetch: mockFetchFailure });
+
+      expect(preprocess).not.toHaveBeenCalled();
+
       expect(response).toEqual({
         data: undefined,
-        isAmp: false,
-        service: 'news',
         status: 502,
       });
     });
   });
 
-  describe('Ares returns 200 status code, but invalid JSON', () => {
+  describe('Request returns 200 status code, but invalid JSON', () => {
     it('should return a 502 error code', async () => {
-      const response = await callGetInitialData(
-        defaultContext,
-        mockFetchInvalidJSON,
-      );
+      const response = await callfetchData({ mockFetch: mockFetchInvalidJSON });
+
+      expect(preprocess).not.toHaveBeenCalled();
 
       expect(response).toEqual({
         data: undefined,
-        isAmp: false,
-        service: 'news',
         status: 502,
       });
     });
   });
 
-  describe('Ares returns a 404 status code', () => {
+  describe('Request returns a 404 status code', () => {
     it('should return the status code as 404', async () => {
-      const response = await callGetInitialData(
-        defaultContext,
-        mockFetchNotFoundStatus,
-      );
+      const response = await callfetchData({
+        mockFetch: mockFetchNotFoundStatus,
+      });
+
+      expect(preprocess).not.toHaveBeenCalled();
 
       expect(response).toEqual({
         data: undefined,
-        isAmp: false,
-        service: 'news',
         status: 404,
       });
     });
   });
 
-  describe('Ares returns a non-200, non-404 status code', () => {
+  describe('Request returns a non-200, non-404 status code', () => {
     it('should log, and return the status code as 502', async () => {
-      const response = await callGetInitialData(
-        defaultContext,
-        mockFetchTeapotStatus,
-      );
+      const response = await callfetchData({
+        mockFetch: mockFetchTeapotStatus,
+      });
+
+      expect(preprocess).not.toHaveBeenCalled();
 
       expect(loggerMock.warn).toBeCalledWith(
-        `Unexpected upstream response (HTTP status code 418) when requesting ${getBaseUrlMockOrigin}/news/articles/c0000000001o.json`,
+        `Unexpected upstream response (HTTP status code 418) when requesting ${requestedUrl}`,
       );
 
       expect(response).toEqual({
         data: undefined,
-        isAmp: false,
-        service: 'news',
         status: 502,
       });
     });
