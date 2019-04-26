00eadb72fe5ea7d7aca83c51acf05195959fc790
Ruth Ogendi <ruth.ogendi@andela.com>
2019-04-26 14:57:17 +0300

    Nest rules in /preprocessor. Move dataValidator/rules/topstories.js to /lib.

--- src/app/lib/utilities/preprocessor/rules/timestamp.test.js
+++ src/app/lib/utilities/preprocessor/rules/timestamp.test.js
@@ -1,5 +1,5 @@
 import applyTimestampRules from './timestamp';
-import deepClone from '../../../helpers/json/deepClone';
+import deepClone from '../../../../helpers/json/deepClone';
 
 const paragraphBlock = {
   type: 'text',
