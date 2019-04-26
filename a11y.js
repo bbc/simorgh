a963874858f07bfa807a0646cdbd96fd6bb8dd52
Ross Gaskell <ross.gaskell@bbc.co.uk>
2019-04-26 16:15:31 +0100

    Hide a11y errors raised by beacon

--- a11y.js
+++ a11y.js
@@ -3,10 +3,11 @@ const urls = [
   'http://localhost:7080/news/articles/cl55zn0w0l4o',
   'http://localhost:7080/news/articles/cn7769kpk9mo',
 ];
+const hide = ['/html/head/iframe'];
 
 urls.forEach(url =>
   pageWidths.forEach(width =>
     // eslint-disable-next-line no-undef
-    page(url, { width }),
+    page(url, { width, hide }),
   ),
 );
