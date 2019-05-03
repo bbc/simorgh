5de82358fd60110d27320afb91cafb060bc55259
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-05-03 09:52:22 +0100

    :bath: Eslint

--- src/app/containers/StoryPromo/index.test.jsx
+++ src/app/containers/StoryPromo/index.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, wait } from 'react-testing-library';
+import { render } from 'react-testing-library';
 import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
 import StoryPromo from '.';
 
