629501a04515132967e29035d4c488f78c056ba1
Ruth Ogendi <ruth.ogendi@andela.com>
2019-05-08 15:16:32 +0300

    Fix eslint issues.

--- src/app/containers/Video/videoMetadata.test.js
+++ src/app/containers/Video/videoMetadata.test.js
@@ -55,19 +55,20 @@ const noAresMediaMetadata = {
   type: 'aresMedia',
 };
 
-
 describe('videoMetadata', () => {
   it('returns correct video metadata', () => {
     const metadata = videoMetadata(aresMediaBlock);
     const output = {
-      'video': {
+      video: {
         '@list': [
           {
             '@type': 'VideoObject',
             name: 'Five things ants can teach us about management',
-            description: 'They may be tiny, but us humans could learn a thing or two from ants.',
+            description:
+              'They may be tiny, but us humans could learn a thing or two from ants.',
             duration: 191,
-            thumbnailUrl: 'https://ichef.test.bbci.co.uk/images/ic/$recipe/p01k6mtv.jpg',
+            thumbnailUrl:
+              'https://ichef.test.bbci.co.uk/images/ic/$recipe/p01k6mtv.jpg',
           },
         ],
       },
@@ -77,12 +78,12 @@ describe('videoMetadata', () => {
 
   it('handles aresMediaMetadata type not being present', () => {
     const metadata = videoMetadata(noAresMediaMetadata);
-    const output = { 'video': { '@list': [] } };
+    const output = { video: { '@list': [] } };
     expect(metadata).toEqual(output);
-  })
+  });
 
   it('handles empty input', () => {
     const metadata = videoMetadata([]);
-    expect(metadata).toEqual(undefined); 
+    expect(metadata).toEqual(undefined);
   });
 });
