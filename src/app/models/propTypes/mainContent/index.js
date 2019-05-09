444f2b0310e239750dcc74b1fcb7839ac29bfc24
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-05-09 15:27:46 +0100

    fix prop type error for timestamp

--- src/app/models/propTypes/mainContent/index.js
+++ src/app/models/propTypes/mainContent/index.js
@@ -1,10 +1,12 @@
 import { blocksWithTypes } from '../general';
 import { headlineBlockPropTypes } from '../headline';
 import { textBlockPropTypes } from '../text';
+import timestampBlockPropTypes from '../timestamp';
 import { imageBlockPropTypes } from '../image';
 
 const mainContentPropTypes = blocksWithTypes([
   headlineBlockPropTypes,
+  timestampBlockPropTypes,
   textBlockPropTypes,
   imageBlockPropTypes,
 ]);
