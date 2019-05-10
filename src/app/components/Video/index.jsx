8b1490057eae89a04d0d631b6f43ae9f18abe209
Rhys Hellyar <rhys.hellyar@bbc.co.uk>
2019-05-10 16:33:43 +0100

    add PID value

--- src/app/components/Video/index.jsx
+++ src/app/components/Video/index.jsx
@@ -5,23 +5,24 @@ import deepGet from '../../helpers/json/deepGet';
 const Video = ({ model }) => {
   const nestedModel = deepGet(['blocks', 0, 'model'], model);
   const kind = deepGet(['subType'], nestedModel);
+  const pid = deepGet(['id'], nestedModel);
   const title = deepGet(['title'], nestedModel);
   const version = deepGet(['versions', 0], nestedModel);
-  const vpid = deepGet(['versionId'], version);
   const duration = deepGet(['duration'], version);
+  const versionId = deepGet(['versionId'], version);
   const holdingImageUrl = deepGet(
     ['blocks', 1, 'model', 'blocks', 0, 'model', 'locator'],
     model,
   );
   const items = {
-    versionId: vpid,
+    versionId,
     kind,
     duration,
   };
 
   return (
     <div>
-      <div>video pid: {vpid}</div>
+      <div>video pid: {pid}</div>
       <div>kind: {kind}</div>
       <div>title: {title}</div>
       <div>holdingImageURL: {holdingImageUrl}</div>
