bba6e7e39ad343380b9aec458c549242e74850f7
E Keane <egjkeane@gmail.com>
2019-05-10 13:35:40 +0100

    Update text in stories so it's clear what the span of the item is

--- src/app/containers/ArticleMain/index.stories.jsx
+++ src/app/containers/ArticleMain/index.stories.jsx
@@ -29,28 +29,28 @@ storiesOf('Grid Examples', module)
     <GhostWrapper>
       <GridItemConstrainedSmall>
         <NestedGridItemSmall gridColumnStart="1" gridSpan="1">
-          <ColouredDiv colour={red}>span 1/4 or 5 or 8</ColouredDiv>
+          <ColouredDiv colour={red}>1/4 or 1/5 or 1/8</ColouredDiv>
         </NestedGridItemSmall>
         <NestedGridItemSmall gridColumnStart="1" gridSpan="2">
-          <ColouredDiv colour={orange}>span 2/4 or 5 or 8</ColouredDiv>
+          <ColouredDiv colour={orange}>2/4 or 2/5 or 2/8</ColouredDiv>
         </NestedGridItemSmall>
         <NestedGridItemSmall gridColumnStart="1" gridSpan="3">
-          <ColouredDiv colour={yellow}>span 3/4 or 5 or 8</ColouredDiv>
+          <ColouredDiv colour={yellow}>3/4 or 2/5 or 2/8</ColouredDiv>
         </NestedGridItemSmall>
         <NestedGridItemSmall gridColumnStart="1" gridSpan="4">
-          <ColouredDiv colour={green}>span 4/4 or 5 or 8</ColouredDiv>
+          <ColouredDiv colour={green}>4/4 or 2/5 or 2/8</ColouredDiv>
         </NestedGridItemSmall>
         <NestedGridItemSmall gridColumnStart="1" gridSpan="5">
-          <ColouredDiv colour={blue}>span 5/4 or 5 or 8</ColouredDiv>
+          <ColouredDiv colour={blue}>4/4 or 5/5 or 5/8</ColouredDiv>
         </NestedGridItemSmall>
         <NestedGridItemSmall gridColumnStart="1" gridSpan="6">
-          <ColouredDiv colour={indigo}>span 6/4 or 5 or 8</ColouredDiv>
+          <ColouredDiv colour={indigo}>4/4 or 5/5 or 6/8</ColouredDiv>
         </NestedGridItemSmall>
         <NestedGridItemSmall gridColumnStart="1" gridSpan="7">
-          <ColouredDiv colour={violet}>span 7/4 or 5 or 8</ColouredDiv>
+          <ColouredDiv colour={violet}>4/4 or 5/5 or 7/8</ColouredDiv>
         </NestedGridItemSmall>
         <NestedGridItemSmall gridColumnStart="1" gridSpan="8">
-          <ColouredDiv colour={red}>span 8/4 or 5 or 8</ColouredDiv>
+          <ColouredDiv colour={red}>4/4 or 5/5 or 8/8</ColouredDiv>
         </NestedGridItemSmall>
       </GridItemConstrainedSmall>
     </GhostWrapper>
@@ -59,34 +59,34 @@ storiesOf('Grid Examples', module)
     <GhostWrapper>
       <GridItemConstrainedMedium>
         <NestedGridItemMedium gridColumnStart="1" gridSpan="1">
-          <ColouredDiv colour={red}>span 1/5 or 10</ColouredDiv>
+          <ColouredDiv colour={red}>1/5 or 1/10</ColouredDiv>
         </NestedGridItemMedium>
         <NestedGridItemMedium gridColumnStart="1" gridSpan="2">
-          <ColouredDiv colour={orange}>span 2/5 or 10</ColouredDiv>
+          <ColouredDiv colour={orange}>2/5 or 2/10</ColouredDiv>
         </NestedGridItemMedium>
         <NestedGridItemMedium gridColumnStart="1" gridSpan="3">
-          <ColouredDiv colour={yellow}>span 3/5 or 10</ColouredDiv>
+          <ColouredDiv colour={yellow}>3/5 or 3/10</ColouredDiv>
         </NestedGridItemMedium>
         <NestedGridItemMedium gridColumnStart="1" gridSpan="4">
-          <ColouredDiv colour={green}>span 4/5 or 10</ColouredDiv>
+          <ColouredDiv colour={green}>4/5 or 4/10</ColouredDiv>
         </NestedGridItemMedium>
         <NestedGridItemMedium gridColumnStart="1" gridSpan="5">
-          <ColouredDiv colour={blue}>span 5/5 or 10</ColouredDiv>
+          <ColouredDiv colour={blue}>5/5 or 5/10</ColouredDiv>
         </NestedGridItemMedium>
         <NestedGridItemMedium gridColumnStart="1" gridSpan="6">
-          <ColouredDiv colour={violet}>span 6/10</ColouredDiv>
+          <ColouredDiv colour={violet}>5/5 or 6/10</ColouredDiv>
         </NestedGridItemMedium>
         <NestedGridItemMedium gridColumnStart="1" gridSpan="7">
-          <ColouredDiv colour={indigo}>span 7/10</ColouredDiv>
+          <ColouredDiv colour={indigo}>5/5 or 7/10</ColouredDiv>
         </NestedGridItemMedium>
         <NestedGridItemMedium gridColumnStart="1" gridSpan="8">
-          <ColouredDiv colour={red}>span 8/10</ColouredDiv>
+          <ColouredDiv colour={red}>5/5 or 8/10</ColouredDiv>
         </NestedGridItemMedium>
         <NestedGridItemMedium gridColumnStart="1" gridSpan="9">
-          <ColouredDiv colour={orange}>span 9/10</ColouredDiv>
+          <ColouredDiv colour={orange}>5/5 or 9/10</ColouredDiv>
         </NestedGridItemMedium>
         <NestedGridItemMedium gridColumnStart="1" gridSpan="10">
-          <ColouredDiv colour={yellow}>span 10/10</ColouredDiv>
+          <ColouredDiv colour={yellow}>5/5 or 10/10</ColouredDiv>
         </NestedGridItemMedium>
       </GridItemConstrainedMedium>
     </GhostWrapper>
@@ -95,40 +95,40 @@ storiesOf('Grid Examples', module)
     <GhostWrapper>
       <GridItemConstrainedLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="1">
-          <ColouredDiv colour={red}>span 1/6 or 12</ColouredDiv>
+          <ColouredDiv colour={red}>1/6 or 1/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="2">
-          <ColouredDiv colour={orange}>span 2/6 or 12</ColouredDiv>
+          <ColouredDiv colour={orange}>2/6 or 2/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="3">
-          <ColouredDiv colour={yellow}>span 3/6 or 12</ColouredDiv>
+          <ColouredDiv colour={yellow}>3/6 or 3/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="4">
-          <ColouredDiv colour={green}>span 4/6 or 12</ColouredDiv>
+          <ColouredDiv colour={green}>4/6 or 4/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="5">
-          <ColouredDiv colour={blue}>span 5/6 or 12</ColouredDiv>
+          <ColouredDiv colour={blue}>5/6 or 5/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="6">
-          <ColouredDiv colour={violet}>span 6/12</ColouredDiv>
+          <ColouredDiv colour={violet}>6/6 or 6/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="7">
-          <ColouredDiv colour={indigo}>span 7/12</ColouredDiv>
+          <ColouredDiv colour={indigo}>6/6 or 7/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="8">
-          <ColouredDiv colour={red}>span 8/12</ColouredDiv>
+          <ColouredDiv colour={red}>6/6 or 8/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="9">
-          <ColouredDiv colour={orange}>span 9/12</ColouredDiv>
+          <ColouredDiv colour={orange}>6/6 or 9/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="10">
-          <ColouredDiv colour={yellow}>span 10/12</ColouredDiv>
+          <ColouredDiv colour={yellow}>6/6 or 10/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="11">
-          <ColouredDiv colour={green}>span 11/12</ColouredDiv>
+          <ColouredDiv colour={green}>6/6 or 11/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="12">
-          <ColouredDiv colour={blue}>span 12/12</ColouredDiv>
+          <ColouredDiv colour={blue}>6/6 or 12/12</ColouredDiv>
         </NestedGridItemLarge>
       </GridItemConstrainedLarge>
     </GhostWrapper>
@@ -137,40 +137,40 @@ storiesOf('Grid Examples', module)
     <GhostWrapper>
       <GridItemConstrainedLargeNoMargin>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="1">
-          <ColouredDiv colour={red}>span 1/6 or 12</ColouredDiv>
+          <ColouredDiv colour={red}>1/6 or 1/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="2">
-          <ColouredDiv colour={orange}>span 2/6 or 12</ColouredDiv>
+          <ColouredDiv colour={orange}>2/6 or 2/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="3">
-          <ColouredDiv colour={yellow}>span 3/6 or 12</ColouredDiv>
+          <ColouredDiv colour={yellow}>3/6 or 3/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="4">
-          <ColouredDiv colour={green}>span 4/6 or 12</ColouredDiv>
+          <ColouredDiv colour={green}>4/6 or 4/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="5">
-          <ColouredDiv colour={blue}>span 5/6 or 12</ColouredDiv>
+          <ColouredDiv colour={blue}>5/6 or 5/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="6">
-          <ColouredDiv colour={violet}>span 6/12</ColouredDiv>
+          <ColouredDiv colour={violet}>6/6 or 6/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="7">
-          <ColouredDiv colour={indigo}>span 7/12</ColouredDiv>
+          <ColouredDiv colour={indigo}>6/6 or 7/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="8">
-          <ColouredDiv colour={red}>span 8/12</ColouredDiv>
+          <ColouredDiv colour={red}>6/6 or 8/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="9">
-          <ColouredDiv colour={orange}>span 9/12</ColouredDiv>
+          <ColouredDiv colour={orange}>6/6 or 9/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="10">
-          <ColouredDiv colour={yellow}>span 10/12</ColouredDiv>
+          <ColouredDiv colour={yellow}>6/6 or 10/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="11">
-          <ColouredDiv colour={green}>span 11/12</ColouredDiv>
+          <ColouredDiv colour={green}>6/6 or 11/12</ColouredDiv>
         </NestedGridItemLarge>
         <NestedGridItemLarge gridColumnStart="1" gridSpan="12">
-          <ColouredDiv colour={blue}>span 12/12</ColouredDiv>
+          <ColouredDiv colour={blue}>6/6 or 12/12</ColouredDiv>
         </NestedGridItemLarge>
       </GridItemConstrainedLargeNoMargin>
     </GhostWrapper>
