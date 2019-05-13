a8146fac98a30707fefeea63e8a43b0ae5fb99e6
Olu Niyi-Awosusi <o.niyiawosusi@gmail.com>
2019-05-13 10:32:37 +0100

    refactored createSrcset, removed snapshot tests checking for null/default origin code

--- src/app/containers/Image/helpers/srcSetHelper.js
+++ src/app/containers/Image/helpers/srcSetHelper.js
@@ -1,20 +1,14 @@
 const createSrcset = (originCode, locator) => {
-  let imgSet = '';
   const resolutions = [240, 320, 480, 624, 800];
-  if (originCode !== 'pips') {
-    const overridableOriginCode = originCode || 'cpsdevpb';
-
-    const urls = resolutions.map(
-      resolution =>
-        `https://ichef.bbci.co.uk/news/${resolution}/${overridableOriginCode}/${locator} ${resolution}w`,
-    );
-
-    imgSet = urls.join(', ');
-  } else {
-    imgSet = null;
+  if (originCode === 'pips') {
+    return null;
   }
+  const urls = resolutions.map(
+    resolution =>
+      `https://ichef.bbci.co.uk/news/${resolution}/${originCode}/${locator} ${resolution}w`,
+  );
 
-  return imgSet;
+  return urls.join(', ');
 };
 
 export default createSrcset;
