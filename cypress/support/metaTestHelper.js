d71df215dcd5acb65be304e6a9a1c98bae45911e
PriyaKR <Priyatharsini.kuduvarajan@bbc.co.uk>
2019-05-13 12:12:54 +0100

    added test for mozart failover

--- cypress/support/metaTestHelper.js
+++ cypress/support/metaTestHelper.js
@@ -6,6 +6,12 @@ export const testResponseCode = (path, responseCode) => {
   });
 };
 
+export const mozartFallbackStatus = path => {
+  cy.request(path).then(({ headers }) => {
+    expect(headers['X-Mozart-Fallback-Cache']).should('not.exist');
+  });
+};
+
 export const assertCookieValue = (cookieName, value) => {
   cy.getCookie(cookieName).should('have.property', 'value', value);
 };
