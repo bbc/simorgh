5ac8a5c1a09e0a03b82fa6a866b3dd7c379d4de1
E Keane <egjkeane@gmail.com>
2019-05-20 13:25:55 +0100

    Remove script undefined error

--- src/app/containers/Headings/index.stories.jsx
+++ src/app/containers/Headings/index.stories.jsx
@@ -1,16 +1,24 @@
 import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
 import React from 'react';
+import { latin } from '@bbc/gel-foundations/scripts';
 import HeadingsContainer from '.';
+import { ServiceContext } from '../../contexts/ServiceContext';
 import blocksSingleFragment from './testHelpers';
 
 const headline = blocksSingleFragment('This is a headline.', []);
 
 const subheadline = blocksSingleFragment('This is a subheadline.', []);
 
+const headingsContainerWithContext = (type, blocks) => (
+  <ServiceContext.Provider value={{ script: latin }}>
+    <HeadingsContainer type={type} blocks={blocks} />
+  </ServiceContext.Provider>
+);
+
 storiesOf('Heading Container')
-  .add('default heading', () => (
-    <HeadingsContainer blocks={headline} type="headline" />
-  ))
-  .add('default subheading', () => (
-    <HeadingsContainer blocks={subheadline} type="subheadline" />
-  ));
+  .add('default heading', () =>
+    headingsContainerWithContext('headline', headline),
+  )
+  .add('default subheading', () =>
+    headingsContainerWithContext('subheadline', subheadline),
+  );
