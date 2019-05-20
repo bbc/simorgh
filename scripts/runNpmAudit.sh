e29eaf11baba1b9089391e5d1585c1926cd7b955
Andrzej Pogonowski <pogon.ap@hotmail.com>
2019-05-20 14:03:57 +0100

    Debugging

--- scripts/runNpmAudit.sh
+++ scripts/runNpmAudit.sh
@@ -2,20 +2,21 @@
 echo "Running npm audit"
 
 for i in {1..20}
-do
-    npm audit
-    EXIT_CODE=$?
+    do
+        npm audit
+        EXIT_CODE=$?
 
-    if [ $EXIT_CODE -eq 0 ]
-    then
-        # npm audit returned succesfully, exiting
-        echo 'NPM audit passed. Exiting with code 0'
-        exit 0
-    else
-        echo $EXIT_CODE
-        echo 'NPM audit failed, retrying...'
-    fi
-done
+        if [ $EXIT_CODE -eq 0 ]
+        then
+            # npm audit returned succesfully, exiting
+            echo 'NPM audit passed. Exiting with code 0'
+            exit 0
+        else
+            echo $EXIT_CODE
+            echo 'NPM audit failed, retrying...'
+        fi
+        echo $i
+    done
 
 echo "npm audit failed 20 times, this usually highlights a genuine audit failure - please investigate"
 exit 1
