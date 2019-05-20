9f523b099c2acd5ee2b49881dfb4b2d6712786ce
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-05-20 16:37:43 +0100

    Use FrontPage fixture data in the test

--- src/app/containers/FrontPageMain/index.test.jsx
+++ src/app/containers/FrontPageMain/index.test.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import FrontPageMain from '.';
 import { shouldShallowMatchSnapshot } from '../../helpers/tests/testHelpers';
-import { articleDataNews, articleDataPersian } from '../Article/fixtureData';
-
-// temporary: will be removed with https://github.com/bbc/simorgh/issues/836
-const articleDataNewsNoHeadline = JSON.parse(JSON.stringify(articleDataNews));
-articleDataNewsNoHeadline.content.model.blocks.shift();
+import frontPageDataPersian from '../../../../data/prod/pidgin/frontpage';
 
 describe('FrontPageMain', () => {
   shouldShallowMatchSnapshot(
-    'should render a news article correctly',
-    <FrontPageMain service="news" frontPageData={articleDataNews} />,
-  );
-
-  shouldShallowMatchSnapshot(
     'should render a persian article correctly',
-    <FrontPageMain service="persian" frontPageData={articleDataPersian} />,
+    <FrontPageMain service="persian" frontPageData={frontPageDataPersian} />,
   );
 });
