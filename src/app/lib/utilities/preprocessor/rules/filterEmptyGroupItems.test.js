52a47a5daa884685ed5b4c76b028d66d3730ffb1
sadick <sadickjunior@gmail.com>
2019-05-23 15:02:49 +0300

    Add comments

--- src/app/lib/utilities/preprocessor/rules/filterEmptyGroupItems.test.js
+++ src/app/lib/utilities/preprocessor/rules/filterEmptyGroupItems.test.js
@@ -10,6 +10,7 @@ describe('Group rules', () => {
             items: [{ name: 'item1' }],
           },
           { type: 'must-see', items: [] },
+          /* better to be safe and handle unexpected items values */
           { type: 'must-see', items: null },
           { type: 'must-see', items: 'dummy string' },
           { type: 'must-see', items: {} },
