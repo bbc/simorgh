59c17363177f39b5a63566f1625254274d713284
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-05-24 08:44:21 +0100

    Populate homepage container

--- src/app/containers/FrontPageMain/index.jsx
+++ src/app/containers/FrontPageMain/index.jsx
@@ -2,19 +2,21 @@ import React, { Fragment } from 'react';
 import { shape } from 'prop-types';
 import { articleDataPropTypes } from '../../models/propTypes/article';
 import deepGet from '../../helpers/json/deepGet';
-import { GhostWrapper } from '../../lib/styledGrid';
+import { GhostWrapper, GridItemConstrainedLarge } from '../../lib/styledGrid';
+import FrontPageSection from '../FrontPageSection';
 
 const FrontPageMain = ({ frontPageData }) => {
-  const title = deepGet(
-    ['content', 'groups', 0, 'items', 0, 'headlines', 'headline'],
-    frontPageData,
-  );
+  const content = deepGet(['content', 'groups'], frontPageData);
 
   return (
     <Fragment>
       <main role="main">
         <GhostWrapper>
-          <h1>{title}</h1>
+          <GridItemConstrainedLarge>
+            {content.map(group => (
+              <FrontPageSection group={group} />
+            ))}
+          </GridItemConstrainedLarge>
         </GhostWrapper>
       </main>
     </Fragment>
