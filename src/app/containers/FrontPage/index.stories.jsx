a801266d273c30236cf623209815209e7a61731b
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-05-25 12:21:27 +0100

    Add Igbo and Pidgin stories

--- src/app/containers/FrontPage/index.stories.jsx
+++ src/app/containers/FrontPage/index.stories.jsx
@@ -1,21 +1,57 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
-import fixtureData from '../../../../data/prod/pidgin/frontpage';
+import igboData from '../../../../data/prod/igbo/frontpage';
+import yorubaData from '../../../../data/prod/yoruba/frontpage';
+import pidginData from '../../../../data/prod/pidgin/frontpage';
+
 import FrontPage from '.';
 
-storiesOf('Front Page', module).add('default', () => {
-  const data = {
-    pageData: fixtureData,
-    status: 200,
-  };
+storiesOf('Front Page', module)
+  .add('Igbo', () => {
+    const data = {
+      pageData: igboData,
+      status: 200,
+    };
 
-  return (
-    <FrontPage
-      data={data}
-      service="news"
-      isAmp={false}
-      loading={false}
-      error=""
-    />
-  );
-});
+    return (
+      <FrontPage
+        data={data}
+        service="igbo"
+        isAmp={false}
+        loading={false}
+        error=""
+      />
+    );
+  })
+  .add('Yoruba', () => {
+    const data = {
+      pageData: yorubaData,
+      status: 200,
+    };
+
+    return (
+      <FrontPage
+        data={data}
+        service="yoruba"
+        isAmp={false}
+        loading={false}
+        error=""
+      />
+    );
+  })
+  .add('Pidgin', () => {
+    const data = {
+      pageData: pidginData,
+      status: 200,
+    };
+
+    return (
+      <FrontPage
+        data={data}
+        service="pidgin"
+        isAmp={false}
+        loading={false}
+        error=""
+      />
+    );
+  });
