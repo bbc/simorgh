c67cc19d5b9e5d57d3cd1d2224d3103c1dfe6392
Andrzej Pogonowski <pogon.ap@hotmail.com>
2019-05-29 13:05:04 +0100

    Refactored and added unit tests

--- scripts/lighthouseBudget.js
+++ scripts/lighthouseBudget.js
@@ -42,6 +42,14 @@ const compareToBudget = (categories, scoreResult, scoreBudget) => {
   return result;
 };
 
+const readReport = path => {
+  console.log('Reading the report');
+  const rawdata = fs.readFileSync(path);
+  const result = JSON.parse(rawdata);
+
+  return result;
+};
+
 // START OF SCRIPT
 
 const budget = {
@@ -50,20 +58,44 @@ const budget = {
   seo: 90,
 };
 
-const testableProperties = ['ally', 'bestPractises', 'seo'];
+const run = () => {
+  const testableProperties = ['ally', 'bestPractises', 'seo'];
 
-const rawdata = fs.readFileSync('simorgh.report.json');
-const lighthouseReport = JSON.parse(rawdata);
-const extractedCategories = getCategoryScores(lighthouseReport);
+  const report = readReport('simorgh.report.json');
+  const extractedCategories = getCategoryScores(report);
 
-const result = compareToBudget(testableProperties, extractedCategories, budget);
+  const result = compareToBudget(
+    testableProperties,
+    extractedCategories,
+    budget,
+  );
 
-process.on('exit', code => console.log(`Exiting with code ${code}`));
+  process.on('exit', code => console.log(`Exiting with code ${code}`));
 
-if (!result) {
-  console.log('Lighthouse tests failed. See log table for details');
-  process.exit(1);
+  if (!result) {
+    console.log('Lighthouse tests failed. See log table for details');
+    process.exit(1);
+  }
+
+  console.log('All tests passed!');
+  process.exit(0);
+};
+
+module.exports = {
+  getCategoryScores,
+  isAboveThreshold,
+  logRow,
+  compareToBudget,
+  readReport,
+  run,
+};
+
+// Script run
+// A 'run' argument need to be passed in for the script to work
+// This was done to make the script unit testable without spliting
+// it to different files.
+const args = process.argv.slice(2);
+
+if (args[0] === 'run') {
+  run();
 }
-
-console.log('All tests passed!');
-process.exit(0);
