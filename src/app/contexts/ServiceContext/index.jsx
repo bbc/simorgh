c188bb9176ecc0fb7d5138542254b52045bdf64b
sadick <isaac.odhiambo@andela.com>
2019-05-29 12:42:02 +0300

    Move global styles to default page wrapper

--- src/app/contexts/ServiceContext/index.jsx
+++ src/app/contexts/ServiceContext/index.jsx
@@ -2,20 +2,14 @@ import React from 'react';
 import { node, string } from 'prop-types';
 import services from '../../lib/config/services';
 
-import GlobalStyle from '../../lib/globalStyles';
 /* Create ServiceContext using the default service */
 export const ServiceContext = React.createContext(services.default);
 
-export const ServiceContextProvider = ({ children, service }) => {
-  const { fonts } = services[service];
-
-  return (
-    <ServiceContext.Provider value={services[service]}>
-      <GlobalStyle fonts={fonts} />
-      {children}
-    </ServiceContext.Provider>
-  );
-};
+export const ServiceContextProvider = ({ children, service }) => (
+  <ServiceContext.Provider value={services[service]}>
+    {children}
+  </ServiceContext.Provider>
+);
 
 ServiceContextProvider.propTypes = {
   children: node.isRequired,
