9f2f4f1d0af3d1efb3a6094aabcf6c1c85cbc8d0
Ruth Ogendi <ruth.ogendi@andela.com>
2019-05-31 14:47:55 +0300

    Update asyncValidateDir.test.js and readScenario.js

--- src/app/dataValidator/helpers/dataLoader/asyncValidateDir.test.js
+++ src/app/dataValidator/helpers/dataLoader/asyncValidateDir.test.js
@@ -33,7 +33,7 @@ describe('asyncValidateDir helper', () => {
   it('should call readScenario for every file in the /data directory', async () => {
     const readScenarioSpy = jest.spyOn(readScenario, 'readScenario');
 
-    await expectMethodToBeCalledTimes(64, readScenarioSpy);
+    await expectMethodToBeCalledTimes(70, readScenarioSpy);
   });
 
   it('should call fileToValidate for only the valid json file in the /data directory', async () => {
