6f6f801906f27fbc9b2b189cb0dbdccedd368720
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-06-06 16:35:26 +0100

    Add snapshot tests

--- src/app/containers/FrontPageMain/index.test.jsx
+++ src/app/containers/FrontPageMain/index.test.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import FrontPageMain from '.';
+import { ServiceContextProvider } from '../../contexts/ServiceContext';
 import { shouldShallowMatchSnapshot } from '../../helpers/tests/testHelpers';
+import frontPageDataIgbo from '../../../../data/prod/igbo/frontpage';
 import frontPageDataPidgin from '../../../../data/prod/pidgin/frontpage';
+import frontPageDataYoruba from '../../../../data/prod/yoruba/frontpage';
 
 describe('FrontPageMain', () => {
   shouldShallowMatchSnapshot(
-    'should render a pidgin article correctly',
-    <FrontPageMain service="pidgin" frontPageData={frontPageDataPidgin} />,
+    'should render a igbo frontpage correctly',
+    <ServiceContextProvider service="igbo">
+      <FrontPageMain frontPageData={frontPageDataIgbo} />
+    </ServiceContextProvider>,
+  );
+
+  shouldShallowMatchSnapshot(
+    'should render a pidgin frontpage correctly',
+    <ServiceContextProvider service="pidgin">
+      <FrontPageMain frontPageData={frontPageDataPidgin} />
+    </ServiceContextProvider>,
+  );
+
+  shouldShallowMatchSnapshot(
+    'should render a yoruba frontpage correctly',
+    <ServiceContextProvider service="yoruba">
+      <FrontPageMain frontPageData={frontPageDataYoruba} />
+    </ServiceContextProvider>,
   );
 });
