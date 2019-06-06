7b8219a881896be5d5f1b58b9877c9d3b3c5fb7a
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-06-06 21:09:48 +0100

    :bath: Put context call back

--- src/app/containers/Timestamp/index.jsx
+++ src/app/containers/Timestamp/index.jsx
@@ -17,6 +17,7 @@ const TimestampContainer = ({
   suffix,
   timezone,
 }) => {
+  const { script } = useContext(ServiceContext);
   if (!isValidDateTime(new Date(timestamp))) {
     return null;
   }
