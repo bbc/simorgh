5fc15b7a3febdfdad34a65dd09ff3fbdfe11290d
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-06-07 11:50:24 +0100

    Add snapshots for the three services

--- src/app/containers/FrontPageMain/index.test.jsx
+++ src/app/containers/FrontPageMain/index.test.jsx
@@ -1,30 +1,84 @@
 import React from 'react';
 import FrontPageMain from '.';
-import { ServiceContextProvider } from '../../contexts/ServiceContext';
 import { shouldShallowMatchSnapshot } from '../../helpers/tests/testHelpers';
 import frontPageDataIgbo from '../../../../data/prod/igbo/frontpage';
 import frontPageDataPidgin from '../../../../data/prod/pidgin/frontpage';
 import frontPageDataYoruba from '../../../../data/prod/yoruba/frontpage';
 
+const igboConfig = {
+  brandName: 'BBC News Ìgbò',
+  translations: {
+    home: 'Akụkọ',
+  },
+};
+
+const pidginConfig = {
+  brandName: 'BBC News Pidgin',
+  translations: {
+    home: 'Home',
+  },
+};
+
+const yorubaConfig = {
+  brandName: 'BBC News Yorùbá',
+  translations: {
+    home: 'Ìròyìn',
+  },
+};
+
+jest.mock('react', () => {
+  const original = jest.requireActual('react');
+  return {
+    ...original,
+    useContext: jest.fn(),
+  };
+});
+
+const { useContext } = jest.requireMock('react');
+
 describe('FrontPageMain', () => {
-  shouldShallowMatchSnapshot(
-    'should render a igbo frontpage correctly',
-    <ServiceContextProvider service="igbo">
-      <FrontPageMain frontPageData={frontPageDataIgbo} />
-    </ServiceContextProvider>,
-  );
+  describe('Igbo snapshots', () => {
+    beforeEach(() => {
+      useContext.mockReturnValue(igboConfig);
+    });
 
-  shouldShallowMatchSnapshot(
-    'should render a pidgin frontpage correctly',
-    <ServiceContextProvider service="pidgin">
-      <FrontPageMain frontPageData={frontPageDataPidgin} />
-    </ServiceContextProvider>,
-  );
+    afterEach(() => {
+      useContext.mockReset();
+    });
 
-  shouldShallowMatchSnapshot(
-    'should render a yoruba frontpage correctly',
-    <ServiceContextProvider service="yoruba">
-      <FrontPageMain frontPageData={frontPageDataYoruba} />
-    </ServiceContextProvider>,
-  );
+    shouldShallowMatchSnapshot(
+      'should render an igbo frontpage correctly',
+      <FrontPageMain frontPageData={frontPageDataIgbo} />,
+    );
+  });
+
+  describe('Pidgin snapshot', () => {
+    beforeEach(() => {
+      useContext.mockReturnValue(pidginConfig);
+    });
+
+    afterEach(() => {
+      useContext.mockReset();
+    });
+
+    shouldShallowMatchSnapshot(
+      'should render a pidgin frontpage correctly',
+      <FrontPageMain frontPageData={frontPageDataPidgin} />,
+    );
+  });
+
+  describe('Yoruba snapshot', () => {
+    beforeEach(() => {
+      useContext.mockReturnValue(yorubaConfig);
+    });
+
+    afterEach(() => {
+      useContext.mockReset();
+    });
+
+    shouldShallowMatchSnapshot(
+      'should render a yoruba frontpage correctly',
+      <FrontPageMain frontPageData={frontPageDataYoruba} />,
+    );
+  });
 });
