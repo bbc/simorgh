38235b55837f1a4d5516eaaaf333e7d88d6e05ef
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-06-07 20:40:41 +0100

    Update test with just one service

--- src/app/containers/FrontPageMain/index.test.jsx
+++ src/app/containers/FrontPageMain/index.test.jsx
@@ -2,27 +2,11 @@ import React from 'react';
 import FrontPageMain from '.';
 import { shouldShallowMatchSnapshot } from '../../helpers/tests/testHelpers';
 import frontPageDataIgbo from '../../../../data/prod/igbo/frontpage';
-import frontPageDataPidgin from '../../../../data/prod/pidgin/frontpage';
-import frontPageDataYoruba from '../../../../data/prod/yoruba/frontpage';
 
 const igboConfig = {
   brandName: 'BBC News Ìgbò',
   translations: {
     home: 'Akụkọ',
-  },
-};
-
-const pidginConfig = {
-  brandName: 'BBC News Pidgin',
-  translations: {
-    home: 'Home',
-  },
-};
-
-const yorubaConfig = {
-  brandName: 'BBC News Yorùbá',
-  translations: {
-    home: 'Ìròyìn',
   },
 };
 
@@ -37,7 +21,7 @@ jest.mock('react', () => {
 const { useContext } = jest.requireMock('react');
 
 describe('FrontPageMain', () => {
-  describe('Igbo snapshots', () => {
+  describe('snapshots', () => {
     beforeEach(() => {
       useContext.mockReturnValue(igboConfig);
     });
@@ -49,36 +33,6 @@ describe('FrontPageMain', () => {
     shouldShallowMatchSnapshot(
       'should render an igbo frontpage correctly',
       <FrontPageMain frontPageData={frontPageDataIgbo} />,
-    );
-  });
-
-  describe('Pidgin snapshot', () => {
-    beforeEach(() => {
-      useContext.mockReturnValue(pidginConfig);
-    });
-
-    afterEach(() => {
-      useContext.mockReset();
-    });
-
-    shouldShallowMatchSnapshot(
-      'should render a pidgin frontpage correctly',
-      <FrontPageMain frontPageData={frontPageDataPidgin} />,
-    );
-  });
-
-  describe('Yoruba snapshot', () => {
-    beforeEach(() => {
-      useContext.mockReturnValue(yorubaConfig);
-    });
-
-    afterEach(() => {
-      useContext.mockReset();
-    });
-
-    shouldShallowMatchSnapshot(
-      'should render a yoruba frontpage correctly',
-      <FrontPageMain frontPageData={frontPageDataYoruba} />,
     );
   });
 });
