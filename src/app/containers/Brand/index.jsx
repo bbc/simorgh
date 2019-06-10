ddf3674ef956c7624f2079e97676784f18d5108d
Einstein <forensicslabnet@gmail.com>
2019-06-10 17:27:35 +0300

    Add a lang attribute to brand component

--- src/app/containers/Brand/index.jsx
+++ src/app/containers/Brand/index.jsx
@@ -1,11 +1,24 @@
-import React, { useContext } from 'react';
+import React, { Fragment, useContext } from 'react';
 import Brand from '@bbc/psammead-brand';
 import { ServiceContext } from '../../contexts/ServiceContext';
 
 const BrandContainer = () => {
   const { brandName, brandSVG } = useContext(ServiceContext);
+  const defaultBrandName = 'BBC News';
+  let localisedBrandName;
 
-  return <Brand brandName={brandName} svg={brandSVG} />;
+  if (brandName !== defaultBrandName) {
+    localisedBrandName = (
+      <Fragment>
+        <span lang="en_GB">{defaultBrandName}</span>
+        {brandName.substring(
+          brandName.indexOf(defaultBrandName) + defaultBrandName.length,
+        )}
+      </Fragment>
+    );
+  }
+
+  return <Brand brandName={localisedBrandName || brandName} svg={brandSVG} />;
 };
 
 export default BrandContainer;
