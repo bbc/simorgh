2631f23d02a55080039be96377bc6961961781e4
Benjamin Hobbs <bjhobbs06@gmail.com>
2019-06-12 22:54:53 +0100

    Mv blockHandlers.js

--- src/app/containers/Video/index.jsx
+++ src/app/containers/Video/index.jsx
@@ -10,7 +10,7 @@ import {
   videoPropTypes,
   emptyBlockArrayDefaultProps,
 } from '../../models/propTypes';
-import { filterForBlockType } from '../../helpers/blockHandlers';
+import { filterForBlockType } from '../../lib/blockHandlers';
 import { RequestContext } from '../../contexts/RequestContext';
 
 const VideoContainer = ({ blocks }) => {
