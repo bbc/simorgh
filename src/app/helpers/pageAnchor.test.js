e30f74f7a05d6cf452db5a4dad48aeccbad0f8e7
Rhys Hellyar <rhys.hellyar@bbc.co.uk>
2019-06-12 14:10:23 +0100

    Add test for urlWithPageAnchor when onClient returns false

--- src/app/helpers/pageAnchor.test.js
+++ src/app/helpers/pageAnchor.test.js
@@ -1,8 +1,16 @@
 import urlWithPageAnchor from './pageAnchor';
+import onClient from './onClient';
+
+let onClientMockRes = true;
+jest.mock('./onClient', () => jest.fn());
+onClient.mockImplementation(() => onClientMockRes);
 
 describe('urlWithPageAnchor', () => {
   beforeEach(() => {
     window.location.hash = '';
+    onClientMockRes = true;
+
+    jest.clearAllMocks();
   });
 
   it('should return false when no anchor', () => {
@@ -12,5 +20,12 @@ describe('urlWithPageAnchor', () => {
   it('should return the hash value when present', () => {
     window.location.hash = 'foobar';
     expect(urlWithPageAnchor()).toEqual('#foobar');
+  });
+
+  it('should return false when onClient returns false', () => {
+    onClientMockRes = false;
+    window.location.hash = 'this-should-still-return-false';
+
+    expect(urlWithPageAnchor()).toBe(false);
   });
 });
