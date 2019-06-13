71cf15ee3113698d17356a82cbf7a9616499b77c
Einstein <forensicslabnet@gmail.com>
2019-06-13 15:05:46 +0300

    Log join-us link on the console in production.

--- src/client.js
+++ src/client.js
@@ -4,6 +4,7 @@ import { hydrate } from 'react-dom';
 import { ClientApp } from './app/containers/App';
 import routes from './app/routes';
 import getRouteProps from './app/routes/getInitialData/utils/getRouteProps';
+import logger from './app/helpers/logger.web';
 
 const data = window.SIMORGH_DATA || {};
 const url = window.location.pathname || '/';
@@ -13,8 +14,12 @@ const { service } = getRouteProps(routes, url);
 
 hydrate(<ClientApp data={data} routes={routes} />, root);
 
-if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-  navigator.serviceWorker.register(`/${service}/articles/sw.js`);
+if (process.env.NODE_ENV === 'production') {
+  logger().info('https://bbc-news.github.io/join-us/');
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register(`/${service}/articles/sw.js`);
+  }
 }
 
 if (module.hot) {
