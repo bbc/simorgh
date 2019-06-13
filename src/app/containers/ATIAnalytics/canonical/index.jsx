39bde20b82ed8b29fe25a73d77a2ece9b9c5dece
Sareh Heidari <sareh.heidari@gmail.com>
2019-06-13 13:00:22 +0100

    Only send beacon for ATI on canonical pages when the ati url is updated due to article data changes

--- src/app/containers/ATIAnalytics/canonical/index.jsx
+++ src/app/containers/ATIAnalytics/canonical/index.jsx
@@ -1,30 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { shape } from 'prop-types';
 import articlePropTypes from '../../../models/propTypes/article';
 import { atiBaseUrl } from '../atiUrl';
-import onClient from '../../../helpers/onClient';
 import sendBeacon from '../../../lib/analyticsUtils/sendBeacon';
-import { getHref } from '../../../lib/analyticsUtils';
 import ArticleAtiParams from '../ArticleAtiParams';
 
 const CanonicalATIAnalytics = ({ articleData }) => {
-  const href = getHref('canonical');
-  const pageViewBeaconUrl = atiBaseUrl + ArticleAtiParams(articleData);
+  const [atiPageViewUrl] = useState(atiBaseUrl + ArticleAtiParams(articleData));
 
-  if (onClient()) {
-    // Only send page beacon when page href changes
-    // This is since when moving to front page
-    // will need to call need to call atiPageViewParams with front page values
-    // TODO: need to move use of hook outside of conditional statement.
-    // eslint-disable-next-line  react-hooks/rules-of-hooks
-    useEffect(() => {
-      sendBeacon(pageViewBeaconUrl);
-    }, [href, pageViewBeaconUrl]);
-  }
+  useEffect(() => {
+    sendBeacon(atiPageViewUrl);
+  }, [atiPageViewUrl]);
 
   return (
     <noscript>
-      <img height="1px" width="1px" alt="" src={pageViewBeaconUrl} />
+      <img height="1px" width="1px" alt="" src={atiPageViewUrl} />
     </noscript>
   );
 };
