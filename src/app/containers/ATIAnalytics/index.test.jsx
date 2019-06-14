11febcb20c6f6931e934ab2f6b075221b9949b5d
Sareh Heidari <sareh.heidari@gmail.com>
2019-06-14 15:13:05 +0100

    Replacing snapshots with assertion-based tests

--- src/app/containers/ATIAnalytics/index.test.jsx
+++ src/app/containers/ATIAnalytics/index.test.jsx
@@ -1,36 +1,99 @@
 import React from 'react';
-import { shouldMatchSnapshot } from '../../helpers/tests/testHelpers';
+import { node, string } from 'prop-types';
+import renderer from 'react-test-renderer';
+import { isNull } from '../../helpers/tests/testHelpers';
 import { RequestContextProvider } from '../../contexts/RequestContext';
 import { ServiceContextProvider } from '../../contexts/ServiceContext';
 
-const ATIAnalytics = require('./index').default;
+import ATIAnalytics from '.';
+import * as amp from './amp';
+import * as canonical from './canonical';
+import * as articleatiparams from './ArticleAtiParams';
 
-xdescribe('Page View Analytics Container', () => {
-  shouldMatchSnapshot(
-    'should render amp page view analytics correctly',
+const ContextWrap = ({ platform, children }) => (
+  <ServiceContextProvider service="news">
     <RequestContextProvider
       isUK
-      origin="https://www.bbc.com"
-      platform="amp"
-      statsDestination="NEWS_GNL"
+      platform={platform}
+      origin="https://www.test.bbc.co.uk"
+      service="news"
+      statsDestination="NEWS_PS_TEST"
+      statsPageIdentifier="news.articles.c0000000000o.page"
+      articleData={{}}
     >
-      <ServiceContextProvider service="news">
-        <ATIAnalytics />
-      </ServiceContextProvider>
-    </RequestContextProvider>,
-  );
+      {children}
+    </RequestContextProvider>
+  </ServiceContextProvider>
+);
 
-  shouldMatchSnapshot(
-    'should render canonical page view analytics correctly',
-    <RequestContextProvider
-      isUK
-      origin="https://www.bbc.com"
-      platform="canonical"
-      statsDestination="NEWS_GNL"
-    >
-      <ServiceContextProvider service="news">
-        <ATIAnalytics />
-      </ServiceContextProvider>
-    </RequestContextProvider>,
-  );
+ContextWrap.propTypes = {
+  children: node.isRequired,
+  platform: string.isRequired,
+};
+
+const mockData = {};
+const mockAtiQueryParams = 'key1=value1&key2=value2';
+
+describe('Page View Analytics Container', () => {
+  describe('pageType article', () => {
+    it('should call CanonicalATIAnalytics when platform is canonical', () => {
+      const mockCanonical = jest.fn().mockReturnValue('canonical-return-value');
+      canonical.default = mockCanonical;
+
+      const mockArticleAtiParams = jest
+        .fn()
+        .mockReturnValue(mockAtiQueryParams);
+      articleatiparams.default = mockArticleAtiParams;
+
+      renderer.create(
+        <ContextWrap platform="canonical">
+          <ATIAnalytics data={mockData} pageType="article" />
+        </ContextWrap>,
+      );
+
+      expect(mockCanonical).toHaveBeenCalledTimes(1);
+      expect(mockCanonical).toHaveBeenCalledWith(
+        {
+          pageviewParams: mockAtiQueryParams,
+        },
+        mockData,
+      );
+      expect(mockArticleAtiParams).toHaveBeenCalledTimes(1);
+      expect(mockArticleAtiParams).toHaveBeenCalledWith(mockData);
+    });
+
+    it('should call AmpATIAnalytics when platform is Amp', () => {
+      const mockAmp = jest.fn().mockReturnValue('amp-return-value');
+      amp.default = mockAmp;
+
+      const mockArticleAtiParams = jest
+        .fn()
+        .mockReturnValue(mockAtiQueryParams);
+      articleatiparams.default = mockArticleAtiParams;
+
+      renderer.create(
+        <ContextWrap platform="amp">
+          <ATIAnalytics data={mockData} pageType="article" />
+        </ContextWrap>,
+      );
+
+      expect(mockAmp).toHaveBeenCalledTimes(1);
+      expect(mockAmp).toHaveBeenCalledWith(
+        {
+          pageviewParams: mockAtiQueryParams,
+        },
+        mockData,
+      );
+      expect(mockArticleAtiParams).toHaveBeenCalledTimes(1);
+      expect(mockArticleAtiParams).toHaveBeenCalledWith(mockData);
+    });
+  });
+  describe('pageType not article', () => {
+    isNull(
+      'should render null',
+      <ContextWrap platform="canonical">
+        <ATIAnalytics data={mockData} pageType="randomvalue" />
+      </ContextWrap>,
+    );
+  });
 });
