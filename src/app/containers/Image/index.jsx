5f48d949bf978d828e052095d690ae93fabb9b15
Rhys Hellyar <rhys.hellyar@bbc.co.uk>
2019-06-14 14:14:54 +0100

    Move pageAnchor to src/app/lib/utilities

--- src/app/containers/Image/index.jsx
+++ src/app/containers/Image/index.jsx
@@ -9,7 +9,7 @@ import {
 } from '../../lib/styledGrid';
 import createSrcset from './helpers/srcSet';
 import getIChefURL from './helpers/ichefUrl';
-import urlWithPageAnchor from '../../helpers/pageAnchor';
+import urlWithPageAnchor from '../../lib/utilities/pageAnchor';
 
 const DEFAULT_IMAGE_RES = 640;
 const LAZYLOAD_FROM_BLOCK = 3;
