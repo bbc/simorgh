f4e9d6978df277ed95b16bb8a739ae2bd37cc979
rowland henshaw <rowland.henshaw@andela.com>
2019-06-14 15:40:56 +0100

    changed amp url in cypress test

--- cypress/integration/ampSpec.js
+++ cypress/integration/ampSpec.js
@@ -74,7 +74,7 @@ describe('AMP Tests on a .amp page', () => {
   it('should include the canonical URL', () => {
     const { origin } = window.location;
     const canonicalOrigin = origin.includes('localhost')
-      ? 'https://www.bbc.co.uk'
+      ? 'https://www.bbc.com'
       : origin;
 
     checkCanonicalURL(`${canonicalOrigin}/news/articles/${config.assets.news}`);
