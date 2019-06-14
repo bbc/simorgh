eca91101b792e8d767124105adcf076928c1aa1b
Sareh Heidari <sareh.heidari@gmail.com>
2019-06-14 13:12:57 +0100

    Testing canonical implementation for ATI

--- src/app/containers/ATIAnalytics/canonical/index.test.jsx
+++ src/app/containers/ATIAnalytics/canonical/index.test.jsx
@@ -1,19 +1,22 @@
 /* TODO undo this disable */
 /* eslint-disable */
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import renderer from  'react-test-renderer';
+import CanonicalATIAnalytics from '.';
+
 import { ServiceContextProvider } from '../../../contexts/ServiceContext';
 import { RequestContextProvider } from '../../../contexts/RequestContext';
-// import sendBeacon from './sendBeacon';
+import { shouldMatchSnapshot } from '../../../helpers/tests/testHelpers';
 
 const ContextWrap = props => (
   <ServiceContextProvider service="news">
     <RequestContextProvider
-      platform="canonical"
-      bbcOrigin="https://www.test.bbc.co.uk"
-      service="news"
-      statsDestination="NEWS_PS_TEST"
+      isUK={true}
+      platform='canonical'
+      origin='https://www.test.bbc.co.uk'
+      service='news'
+      statsDestination='NEWS_PS_TEST'
+      statsPageIdentifier='news.articles.c0000000000o.page'
       articleData={{}}
     >
       {props.children}
@@ -21,55 +24,22 @@ const ContextWrap = props => (
   </ServiceContextProvider>
 );
 
-let container;
+const mockPageviewParams = 'key=value&key2=value2';
 
-xdescribe('Canonical Consent Banner Container', () => {
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-
+describe('Canonical ATI Analytics', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should render privacy banner when both banners are set to be shown', () => {
-    jest.mock('./atiPageViewParams', () => jest.fn());
-    jest.mock('../../../lib/analyticsUtils/sendBeacon', () => jest.fn());
+  shouldMatchSnapshot('should render correctly',
+  <ContextWrap><CanonicalATIAnalytics pageviewParams={mockPageviewParams} /></ContextWrap>);
 
-    const sendBeacon = require('../../../lib/analyticsUtils/sendBeacon');
-    const atiPageViewParams = require('../atiUrl');
-
-    atiPageViewParams.mockImplementation(
-      ({ articleData }) => `beaconFor${articleData.promo.id}`,
-    );
-
-    const CanonicalATIAnalytics = require('./ind').default;
-
-    const articleData = {
-      promo: {
-        id: '1111111',
-      },
-    };
-
-    act(() => {
-      ReactDOM.render(
-        <ContextWrap>
-          <CanonicalATIAnalytics articleData={articleData} />
-        </ContextWrap>,
-        container,
-      );
-    });
-
-    expect(container.innerHTML).toBe('');
-    expect(atiPageViewParams).toHaveBeenCalledWith({"articleData": {"promo": {"id": "1111111"}}, "env": "live", "isUK": true, "platform": "canonical", "service": "news"});
-
-    expect(sendBeacon).toHaveBeenCalledWith('beaconFor1111111');
-
-    act(() => {
-      articleData.promo.id = '2222222';
-    });
-
-    expect(atiPageViewParams).toHaveBeenCalledWith({"articleData": {"promo": {"id": "2222222"}}, "env": "live", "isUK": true, "platform": "canonical", "service": "news"});
+  xit('should call sendBeacon function with the ATI url', () => {
+    const sendBeacon = require('../../../lib/analyticsUtils/sendBeacon').default;
+    const mock = jest.fn().mockReturnValue('foobar');
+    sendBeacon = mock;
+    renderer.create(<ContextWrap><CanonicalATIAnalytics pageviewParams={mockPageviewParams} /></ContextWrap>);
+    expect(sendBeacon).toHaveBeenCalledTimes(1);
+    expect(sendBeacon).toHaveBeenCalledWith('https://a1.api.bbc.co.uk/hit.xiti?key=value&key2=value2');
   });
 });
