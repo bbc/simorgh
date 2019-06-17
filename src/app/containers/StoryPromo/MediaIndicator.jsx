fedf7f76e66b5c545e4f23c761e7a7dff8168dbc
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-06-17 17:31:22 +0100

    put the media type in the offscreen text

--- src/app/containers/StoryPromo/MediaIndicator.jsx
+++ src/app/containers/StoryPromo/MediaIndicator.jsx
@@ -7,6 +7,8 @@ import { storyItem } from '../../models/propTypes/storyItem';
 import deepGet from '../../lib/utilities/deepGet';
 import formatDuration from '../../lib/utilities/formatDuration';
 
+const capitalise = str => str.slice(0, 1).toUpperCase() + str.slice(1);
+
 const MediaIndicator = ({ item }) => {
   const isMedia = deepGet(['cpsType'], item) === 'MAP';
   // Only build a media indicator if this is a media item.
@@ -27,14 +29,14 @@ const MediaIndicator = ({ item }) => {
         duration={durationString}
         datetime={isoDuration}
         // TODO make offscreenText optional in psammead-media-indicator
-        offscreenText=""
+        offscreenText={capitalise(type)}
         type={type}
       />
     );
   }
 
   // TODO make offscreenText optional in psammead-media-indicator
-  return <MediaIndicatorComp offscreenText="" type={type} />;
+  return <MediaIndicatorComp offscreenText={capitalise(type)} type={type} />;
 };
 
 MediaIndicator.propTypes = {
