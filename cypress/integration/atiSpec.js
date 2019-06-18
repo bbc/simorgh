b7ba8bea0bd9bc878818a5add08ae46fccebb304
PriyaKR <Priyatharsini.kuduvarajan@bbc.co.uk>
2019-06-18 17:38:22 +0100

    fixed the lazyloaded image tests

--- cypress/integration/atiSpec.js
+++ cypress/integration/atiSpec.js
@@ -8,13 +8,13 @@ import { describeForLocalOnly } from '../support/limitEnvRuns';
 describe('Article ATI for News', () => {
   describe('Canonical page', () => {
     it('should have a noscript tag with an 1px image with the ati url', () => {
-      cy.visit(`/news/articles/${config.assets.newsThreeSubheadlines}`);
+      cy.visit(`/news/articles/${config.assets.news}`);
       hasNoscriptImgAtiUrl('');
     });
   });
   describe('AMP page', () => {
     it('should have a noscript tag with an 1px image with the ati url', () => {
-      cy.visit(`/news/articles/${config.assets.newsThreeSubheadlines}.amp`);
+      cy.visit(`/news/articles/${config.assets.news}.amp`);
       hasAmpAnalyticsAtiUrl('');
     });
   });
