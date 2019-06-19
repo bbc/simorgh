1eeac0da4ddc765e6384b36b928f70ff9bf57dca
sadick <isaac.odhiambo@andela.com>
2019-06-19 15:33:48 +0300

    First

--- src/app/containers/FrontPageMain/index.test.jsx
+++ src/app/containers/FrontPageMain/index.test.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { render } from '@testing-library/react';
 import FrontPageMain from '.';
 import { shouldShallowMatchSnapshot } from '../../../testHelpers';
 import frontPageDataIgbo from '../../../../data/prod/pidgin/frontpage';
+import { ServiceContextProvider } from '../../contexts/ServiceContext';
 
 const igboConfig = {
   product: 'BBC News',
@@ -35,5 +37,30 @@ describe('FrontPageMain', () => {
       'should render an igbo frontpage correctly',
       <FrontPageMain frontPageData={frontPageDataIgbo} />,
     );
+  });
+
+  describe('assertions', () => {
+    beforeEach(() => {
+      useContext.mockReturnValue(igboConfig);
+    });
+
+    afterEach(() => {
+      useContext.mockReset();
+    });
+
+    it('should render with tab index and content attributes', () => {
+      const { container } = render(
+        <ServiceContextProvider service="news">
+          <FrontPageMain frontPageData={frontPageDataIgbo} />,
+        </ServiceContextProvider>,
+      );
+
+      const h1 = container.getElementByTagName('h1');
+      const content = h1.getAttribute('id');
+      const tabIndex = h1.getAttribute('tabIndex');
+
+      expect(content).toEqual('content');
+      expect(tabIndex).toBe(-1);
+    });
   });
 });
