9a6311bdf9b2af4796581d2437aad5b462a8ea77
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-06-19 16:43:21 +0100

    Testing that the page wrapper is setting pagetype in contexts

--- src/app/Layouts/defaultPageWrapper.test.jsx
+++ src/app/Layouts/defaultPageWrapper.test.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { func } from 'prop-types';
+import { render } from '@testing-library/react';
 import DefaultPageWrapper from './defaultPageWrapper';
 import { shouldShallowMatchSnapshot } from '../../testHelpers';
 import getOriginContext from '../contexts/RequestContext/getOriginContext';
 import getStatsDestination from '../contexts/RequestContext/getStatsDestination';
 import getStatsPageIdentifier from '../contexts/RequestContext/getStatsPageIdentifier';
+import { RequestContext } from '../contexts/RequestContext';
 
 jest.mock('../contexts/RequestContext/getOriginContext', () => jest.fn());
 
@@ -22,6 +25,20 @@ getStatsPageIdentifier.mockImplementation(
   () => 'news.articles.c0000000000o.page',
 );
 
+// Turns out it's difficult to test that a component correctly
+// sets a React.Context. This ugliness is a way to access the
+// RequestContext set by a parent component. Pass in a function
+// that stores its single argument somewhere useful for your tests.
+const GetThingsFromRequestContext = ({ fn }) => {
+  const rc = useContext(RequestContext);
+  fn(rc);
+  return <p>Placeholder element</p>;
+};
+
+GetThingsFromRequestContext.propTypes = {
+  fn: func.isRequired,
+};
+
 describe('defaultPageWrapper', () => {
   const propsWithChildren = {
     bbcOrigin: 'https://www.bbc.com',
@@ -31,8 +48,55 @@ describe('defaultPageWrapper', () => {
     isAmp: true,
     route: { pageType: 'article' },
   };
+
   shouldShallowMatchSnapshot(
     'should render with children',
     <DefaultPageWrapper {...propsWithChildren} />,
   );
+
+  it('passing pageType==article should pass along', () => {
+    const fixture = {
+      bbcOrigin: 'https://www.bbc.com',
+      id: 'c0000000000o',
+      service: 'news',
+      isAmp: true,
+      route: { pageType: 'article' },
+    };
+
+    let rc;
+    const getRc = foreign => {
+      rc = foreign;
+    };
+
+    render(
+      <DefaultPageWrapper {...fixture}>
+        <GetThingsFromRequestContext fn={getRc} />
+      </DefaultPageWrapper>,
+    );
+
+    expect(rc.pageType).toBe('article');
+  });
+
+  it('passing pageType==frontPage should pass along', () => {
+    const fixture = {
+      bbcOrigin: 'https://www.bbc.com',
+      id: 'c0000000000o',
+      service: 'news',
+      isAmp: true,
+      route: { pageType: 'frontPage' },
+    };
+
+    let rc;
+    const getRc = foreign => {
+      rc = foreign;
+    };
+
+    render(
+      <DefaultPageWrapper {...fixture}>
+        <GetThingsFromRequestContext fn={getRc} />
+      </DefaultPageWrapper>,
+    );
+
+    expect(rc.pageType).toBe('frontPage');
+  });
 });
