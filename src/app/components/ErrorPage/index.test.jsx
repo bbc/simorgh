0827013866daefd035d1b98a363999b19b44a3ec
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2019-06-20 10:55:36 +0100

    Apply react-fake-props to error page unit tests

--- src/app/components/ErrorPage/index.test.jsx
+++ src/app/components/ErrorPage/index.test.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
+import path from 'path';
 import { latin, arabic } from '@bbc/gel-foundations/scripts';
+import fakeProps from 'react-fake-props';
 import { shouldMatchSnapshot } from '../../../testHelpers';
 import ErrorMain from './index';
 
 describe('ErrorMain', () => {
-  const messaging = {
-    statusCode: 'StatusCode',
-    title: 'A error has occured',
-    message: 'Something happened, please try an option',
-    solutions: ['Option one', 'Option two'],
-    callToActionFirst: 'You can do ',
-    callToActionLinkText: 'this',
-    callToActionLinkUrl: 'https://www.bbc.com',
-    callToActionLast: ' thing',
-  };
+  const messaging = fakeProps(path.join(__dirname, './index.jsx'));
 
   shouldMatchSnapshot(
     'should correctly render for an error page for News',
@@ -23,5 +16,14 @@ describe('ErrorMain', () => {
   shouldMatchSnapshot(
     'should correctly render for an error page for Persian',
     <ErrorMain {...messaging} script={arabic} />,
+  );
+
+  shouldMatchSnapshot(
+    'should correctly render multiple solutions',
+    <ErrorMain
+      {...messaging}
+      script={latin}
+      solutions={['Solution A', 'Solution B']}
+    />,
   );
 });
