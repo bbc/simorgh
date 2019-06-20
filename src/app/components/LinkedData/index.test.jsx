c098fcb964eec2c39294ec6d1bf350f0014103ed
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2019-06-20 10:56:21 +0100

    Update LinkedData snapshots

--- src/app/components/LinkedData/index.test.jsx
+++ src/app/components/LinkedData/index.test.jsx
@@ -1,45 +1,15 @@
 import React from 'react';
+import path from 'path';
+import fakeProps from 'react-fake-props';
+
 import { shouldShallowMatchSnapshot } from '../../../testHelpers';
 import LinkData from '.';
 
 describe('LinkData', () => {
-  const props = {
-    isAmp: false,
-    brandName: 'BBC News',
-    canonicalLink: 'https://www.bbc.com/news/articles/c9rpqy7pmypo',
-    lang: 'en-GB',
-    seoHeadline: 'Royal wedding 2018: Bouquet laid on tomb of unknown warrior',
-    type: 'article',
-    lastUpdated: '2019-02-26T11:25:10.555Z',
-    firstPublished: '2018-10-10T16:19:31.344Z',
-    noBylinesPolicy: 'https://www.bbc.com/news/help-41670342#authorexpertise',
-    publishingPrinciples: 'https://www.bbc.com/news/help-41670342',
-    logoUrl:
-      'https://www.bbc.com/news/special/2015/newsspec_10857/bbc_news_logo.png',
-    about: [
-      {
-        '@type': 'Person',
-        name: 'Duchess of Sussex',
-        alternateName: 'Meghan Markle',
-        sameAs: ['http://dbpedia.org/resource/Meghan_Markle'],
-      },
-    ],
-  };
-
+  const props = fakeProps(path.join(__dirname, './index.jsx'));
   const propsWithNoAbout = {
-    isAmp: false,
-    brandName: 'BBC News',
-    canonicalLink: 'https://www.bbc.com/news/articles/c9rpqy7pmypo',
-    lang: 'en-GB',
-    seoHeadline: 'Royal wedding 2018: Bouquet laid on tomb of unknown warrior',
-    type: 'article',
-    service: 'news',
-    lastUpdated: '2019-02-26T11:25:10.555Z',
-    firstPublished: '2018-10-10T16:19:31.344Z',
-    noBylinesPolicy: 'https://www.bbc.com/news/help-41670342#authorexpertise',
-    publishingPrinciples: 'https://www.bbc.com/news/help-41670342',
-    logoUrl:
-      'https://www.bbc.com/news/special/2015/newsspec_10857/bbc_news_logo.png',
+    ...props,
+    about: null,
   };
 
   shouldShallowMatchSnapshot(
