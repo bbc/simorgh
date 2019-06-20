a9029e8b516a54fa58a9c1b801e4ae0a424aebb1
thekp <khoa.phan@bbc.co.uk>
2019-06-20 10:54:27 +0100

    update snapshots

--- src/app/lib/styledGrid/index.test.jsx
+++ src/app/lib/styledGrid/index.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shouldMatchSnapshot } from '../../testHelpers';
+import { shouldMatchSnapshot } from '../../../testHelpers';
 import {
   GhostWrapper,
   GridItemConstrainedSmall,
@@ -9,7 +9,7 @@ import {
   NestedGridItemSmall,
   NestedGridItemMedium,
   NestedGridItemLarge,
-} from './styledGrid';
+} from './index';
 
 describe('Styled Grid items', () => {
   describe('GridItemConstrainedSmall', () => {
