5514eabfff90bd0fefddabaf039f1f9dbf30b05f
E Keane <egjkeane@gmail.com>
2019-06-20 14:15:46 +0100

    Remove custom implementation in favour of extending existing grid item

--- src/app/lib/styledGrid.jsx
+++ src/app/lib/styledGrid.jsx
@@ -2,6 +2,7 @@ import { node, number } from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
 import { C_GHOST } from '@bbc/psammead-styles/colours';
+import { GEL_GROUP_5_SCREEN_WIDTH_MIN } from '@bbc/gel-foundations/breakpoints';
 import {
   layoutGridWrapper,
   layoutGridItemSmall,
@@ -14,7 +15,6 @@ import {
   gridContainerSmallCss,
   gridContainerMediumCss,
   gridContainerLargeCss,
-  customGridItemMedium,
 } from './layoutGrid';
 
 export const GhostWrapper = styled.div`
@@ -63,7 +63,10 @@ const NestedGridParentSmall = styled.div`
 `;
 
 const PopoutGridParentMedium = styled.div`
-  ${customGridItemMedium}
+  ${layoutGridItemMedium}
+  @media (min-width: ${GEL_GROUP_5_SCREEN_WIDTH_MIN}) {
+    max-height: 1.15rem;
+  }
 `;
 export const GridPopoutMedium = props => {
   const { children } = props;
@@ -118,4 +121,9 @@ GridPopoutMedium.propTypes = {
 GridPopoutMedium.defaultProps = {
   gridColumnStart: 3,
   gridSpan: 3,
+};
+
+GridItemConstrainedMedium.defaultProps = {
+  gridColumnStart: 6,
+  gridSpan: 10,
 };
