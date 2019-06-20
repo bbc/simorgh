431fb83732d5d46871f3c62900afcf2eaf7894af
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2019-06-20 11:43:20 +0100

    Metadata component

--- src/app/components/Metadata/index.test.jsx
+++ src/app/components/Metadata/index.test.jsx
@@ -2,230 +2,71 @@ import React from 'react';
 import Metadata from './index';
 import { shouldShallowMatchSnapshot } from '../../../testHelpers';
 
-const metadataSnapshotTest = (
-  testDescription,
-  isAmp,
-  alternateLinks,
-  ampLink,
-  articleAuthor,
-  articleSection,
-  appleTouchIcon,
-  brandName,
-  canonicalLink,
-  defaultImage,
-  defaultImageAltText,
-  description,
-  facebookAdmin,
-  facebookAppID,
-  lang,
-  locale,
-  metaTags,
-  themeColor,
-  timeLastPublished,
-  timeFirstPublished,
-  title,
-  twitterCreator,
-  twitterSite,
-) =>
+const metadataSnapshotTest = (testDescription, overrideProps) =>
   describe(testDescription, () => {
-    const metadataProps = {
-      isAmp,
-      alternateLinks,
-      ampLink,
-      articleAuthor,
-      articleSection,
-      appleTouchIcon,
-      brandName,
-      canonicalLink,
-      defaultImage,
-      defaultImageAltText,
-      description,
-      facebookAdmin,
-      facebookAppID,
-      lang,
-      locale,
-      metaTags,
-      themeColor,
-      timeLastPublished,
-      timeFirstPublished,
-      title,
-      twitterCreator,
-      twitterSite,
+    const defaultProps = {
+      isAmp: false,
+      alternateLinks: [
+        {
+          href: 'https://www.bbc.com/news/articles/c0000000001o',
+          hrefLang: 'x-default',
+        },
+        {
+          href: 'https://www.bbc.com/news/articles/c0000000001o',
+          hrefLang: 'en',
+        },
+        {
+          href: 'https://www.bbc.co.uk/news/articles/c0000000001o',
+          hrefLang: 'en-gb',
+        },
+      ],
+      ampLink: 'https://www.bbc.com/news/articles/c0000000001o.amp',
+      articleAuthor: 'BBC News',
+      articleSection: null,
+      appleTouchIcon: 'https://foo.com/static/news/image.png',
+      brandName: 'BBC News',
+      canonicalLink: 'https://www.bbc.com/news/articles/c0000000001o',
+      defaultImage: 'https://www.bbc.com/news/image.png',
+      defaultImageAltText: 'BBC News',
+      description: 'This is a description',
+      facebookAdmin: 101010,
+      facebookAppId: 202020,
+      lang: 'en-GB',
+      locale: 'en_GB',
+      metaTags: ['tagA', 'tagB'],
+      themeColor: '#B80000',
+      timeLastPublished: 1539188371344,
+      timeFirstPublished: 1514811600000,
+      title: 'An article title',
+      twitterCreator: '@BBCNews',
+      twitterSite: '@BBCNews',
     };
 
     shouldShallowMatchSnapshot(
       'should render correctly',
-      <Metadata {...metadataProps} />,
+      <Metadata {...defaultProps} {...overrideProps} />,
     );
   });
 
-describe('Metadata', () => {
-  metadataSnapshotTest(
-    'News article',
-    false,
-    [
-      {
-        href: 'https://www.bbc.com/news/articles/c0000000001o',
-        hrefLang: 'x-default',
-      },
-      {
-        href: 'https://www.bbc.com/news/articles/c0000000001o',
-        hrefLang: 'en',
-      },
-      {
-        href: 'https://www.bbc.co.uk/news/articles/c0000000001o',
-        hrefLang: 'en-gb',
-      },
-    ],
-    'https://www.bbc.com/news/articles/c0000000001o.amp',
-    'BBC News',
-    null,
-    'https://foo.com/static/news/image.png',
-    'BBC News',
-    'https://www.bbc.com/news/articles/c0000000001o',
-    'https://www.bbc.com/news/image.png',
-    'BBC News',
-    'This is a description',
-    101010,
-    202020,
-    'en-GB',
-    'en_GB',
-    ['tagA', 'tagB'],
-    '#B80000',
-    1539188371344,
-    1514811600000,
-    'An article title',
-    '@BBCNews',
-    '@BBCNews',
-  );
+describe.only('Metadata', () => {
+  metadataSnapshotTest('News article');
+  metadataSnapshotTest('News AMP article', { isAmp: true });
 
-  metadataSnapshotTest(
-    'News AMP article',
-    true,
-    [
-      {
-        href: 'https://www.bbc.com/news/articles/c0000000001o.amp',
-        hrefLang: 'x-default',
-      },
-      {
-        href: 'https://www.bbc.com/news/articles/c0000000001o.amp',
-        hrefLang: 'en',
-      },
-      {
-        href: 'https://www.bbc.co.uk/news/articles/c0000000001o.amp',
-        hrefLang: 'en-gb',
-      },
-    ],
-    'https://www.bbc.com/news/articles/c0000000001o.amp',
-    'BBC News',
-    null,
-    'https://foo.com/static/news/image.png',
-    'BBC News',
-    'https://www.bbc.com/news/articles/c0000000001o',
-    'https://www.bbc.com/news/image.png',
-    'BBC News',
-    'This is a description',
-    101010,
-    202020,
-    'en-GB',
-    'en_GB',
-    ['tagA', 'tagB'],
-    '#B80000',
-    1539188371344,
-    1514811600000,
-    'An article title',
-    '@BBCNews',
-    '@BBCNews',
-  );
-
-  metadataSnapshotTest(
-    'Persian article',
-    false,
-    [],
-    'https://www.bbc.com/persian/articles/c4vlle3q337o.amp',
-    'BBC News فارسی',
-    null,
-    'https://foo.com/static/persian/image.png',
-    'BBC News فارسی',
-    'https://www.bbc.com/persian/articles/c4vlle3q337o',
-    'https://www.bbc.com/persian/image.png',
-    'BBC News فارسی',
-    'This is a description',
-    101010,
-    202020,
-    'fa',
-    'fa',
-    ['tagA', 'tagB'],
-    '#B80000',
-    1539188371344,
-    1514811600000,
-    'پهپادی که برایتان قهوه می‌آورد',
-    '@bbcpersian',
-    '@bbcpersian',
-  );
-
-  metadataSnapshotTest(
-    'Persian AMP article',
-    true,
-    [],
-    'https://www.bbc.com/persian/articles/c4vlle3q337o.amp',
-    'BBC News فارسی',
-    null,
-    'https://foo.com/static/persian/image.png',
-    'BBC News فارسی',
-    'https://www.bbc.com/persian/articles/c4vlle3q337o',
-    'https://www.bbc.com/persian/image.png',
-    'BBC News فارسی',
-    'This is a description',
-    101010,
-    202020,
-    'fa',
-    'fa',
-    ['tagA', 'tagB'],
-    '#B80000',
-    1539188371344,
-    1514811600000,
-    'پهپادی که برایتان قهوه می‌آورد',
-    '@bbcpersian',
-    '@bbcpersian',
-  );
-
-  metadataSnapshotTest(
-    'articleSection is not null',
-    false,
-    [
-      {
-        href: 'https://www.bbc.com/news/articles/c0000000001o',
-        hrefLang: 'x-default',
-      },
-      {
-        href: 'https://www.bbc.com/news/articles/c0000000001o',
-        hrefLang: 'en',
-      },
-      {
-        href: 'https://www.bbc.co.uk/news/articles/c0000000001o',
-        hrefLang: 'en-gb',
-      },
-    ],
-    'https://www.bbc.com/news/articles/c0000000001o.amp',
-    'BBC News',
-    'Politics',
-    'https://foo.com/static/news/image.png',
-    'BBC News',
-    'https://www.bbc.com/news/articles/c0000000001o',
-    'https://www.bbc.com/news/image.png',
-    'BBC News',
-    'This is a description',
-    101010,
-    202020,
-    'en-GB',
-    'en_GB',
-    ['tagA', 'tagB'],
-    '#B80000',
-    1539188371344,
-    1514811600000,
-    'An article title',
-    '@BBCNews',
-    '@BBCNews',
-  );
+  const persianOverrides = {
+    articleAuthor: 'BBC News فارسی',
+    brandName: 'BBC News فارسی',
+    lang: 'fa',
+    locale: 'fa',
+    title: 'پهپادی که برایتان قهوه می‌آورد',
+    twitterCreator: '@bbcpersian',
+    twitterSite: '@bbcpersian',
+  };
+  metadataSnapshotTest('Persian article', persianOverrides);
+  metadataSnapshotTest('Persian AMP article', {
+    ...persianOverrides,
+    isAmp: true,
+  });
+  metadataSnapshotTest('articleSection is not null', {
+    articleSection: 'Politics',
+  });
 });
