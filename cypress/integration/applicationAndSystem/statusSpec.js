9d3b872dc91c2d371a565fcbfbfc30444665ec86
Benjamin Hobbs <bjhobbs06@gmail.com>
2019-06-22 12:30:15 +0100

    Delete specific check for mozart fallback as it runs on every testResponseCodeAndType test now

--- cypress/integration/applicationAndSystem/statusSpec.js
+++
@@ -1,9 +0,0 @@
-import config from '../../support/config';
-
-describe('Mozart status', () => {
-  it('should not contain the response header X-Mfa set to 1', () => {
-    cy.request(`/news/articles/${config.assets.news}`).then(({ headers }) => {
-      expect(headers).not.to.have.property('x-mfa');
-    });
-  });
-});
