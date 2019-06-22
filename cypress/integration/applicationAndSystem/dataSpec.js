f10485130630606b65116b60f5f3aff9c62da3fe
Benjamin Hobbs <bjhobbs06@gmail.com>
2019-06-22 11:30:57 +0100

    Add missed test

--- cypress/integration/applicationAndSystem/dataSpec.js
+++ cypress/integration/applicationAndSystem/dataSpec.js
@@ -2,6 +2,6 @@ import config from '../../support/config';
 
 describe('Static Articles data', () => {
   it('should return a 200 status code', () => {
-    cy.request(`/news/articles/${config.assets.news}.json`);
+    cy.testResponseCode(`/news/articles/${config.assets.news}.json`, 200);
   });
 });
