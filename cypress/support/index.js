c76b1b9140c464e890bda817f5c04228603920d8
Benjamin Hobbs <bjhobbs06@gmail.com>
2019-06-23 00:02:00 +0100

    Fix new e2es across envs

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,15 +1,22 @@
 import './commands';
 
+const configLocal = require('./configLocal');
+const configTest = require('./configTest');
+// Commented out until we use it
+// const configStage = require('./configStage');
+const configLive = require('./configLive');
+
 let env;
 
 if (Cypress.env('APP_ENV') === 'local') {
-  env = require('./configLocal'); // eslint-disable-line
+  env = configLocal;
 } else if (Cypress.env('APP_ENV') === 'test') {
-  env = require('./configTest'); // eslint-disable-line
-} else if (Cypress.env('APP_ENV' === 'stage')) {
-  env = require('./configStage'); // eslint-disable-line
-} else if (Cypress.env('APP_ENV' === 'live')) {
-  env = require('./configLive'); // eslint-disable-line
+  env = configTest;
+  // Commented out until we use it
+  // } else if (Cypress.env('APP_ENV') === 'stage') {
+  //   env = configStage;
+} else if (Cypress.env('APP_ENV') === 'live') {
+  env = configLive;
 }
 export const config = env; // eslint-disable-line import/prefer-default-export
 
