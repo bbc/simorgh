4158048d88aa71df6e602f9933020d6091c94a35
Benjamin Hobbs <bjhobbs06@gmail.com>
2019-06-22 12:25:37 +0100

    Check we're not getting mozart fallback

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,6 +3,8 @@ Cypress.Commands.add('testResponseCodeAndType', (path, responseCode, type) => {
     ({ status, headers }) => {
       expect(status).to.eq(responseCode);
       expect(headers['content-type']).to.include(type);
+      // Always ensure we're not seeing the Mozart fallback
+      expect(headers).not.to.have.property('x-mfa');
     },
   );
 });
