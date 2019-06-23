7dfee1c1ab9729d2c53f33c2c8750cf5ca9d5bea
Benjamin Hobbs <bjhobbs06@gmail.com>
2019-06-23 01:28:49 +0100

    Refactor data test

--- cypress/integration/articles/article.js
+++ cypress/integration/articles/article.js
@@ -9,11 +9,20 @@ Object.keys(config.services).forEach(index => {
       serviceConfig.pageTypes.articles !== undefined &&
       serviceConfig.pageTypes.articles.basicAsset !== undefined
     ) {
+      // can refactor to use the visit below for increased speed?
       it(`should return a 200 status code and HTML file for ${service}`, () => {
         cy.testResponseCodeAndType(
           `${serviceConfig.pageTypes.articles.basicAsset}`,
           200,
           'text/html',
+        );
+      });
+
+      it(`should have an available data endpoint (200 response with a JSON file) for ${service}`, () => {
+        cy.testResponseCodeAndType(
+          `${serviceConfig.pageTypes.articles.basicAsset}.json`,
+          200,
+          'application/json',
         );
       });
 
