972074cde585d9ff3ff0301be178226702594712
Benjamin Hobbs <bjhobbs06@gmail.com>
2019-06-23 12:46:48 +0100

    Rmv unused helpers

--- cypress/support/bodyTestHelper.js
+++ cypress/support/bodyTestHelper.js
@@ -1,5 +1,3 @@
-import { BBC_BLOCKS } from '@bbc/psammead-assets/svgs';
-
 export const firstSubheadlineDataWindow = () => {
   const getBlockByType = (blocks, blockType) => {
     let blockData;
@@ -34,28 +32,4 @@ export const clickInlineLinkAndTestPageHasHTML = (link, url) => {
   cy.get(link).click();
   cy.url().should('contain', url);
   cy.get('header a').should('contain', 'BBC News');
-};
-
-export const placeholderImageLoaded = placeholderImage => {
-  const shouldContainStyles = (element, css, styling) => {
-    element.should(el => {
-      expect(el).to.have.css(css, styling);
-    });
-  };
-
-  shouldContainStyles(
-    placeholderImage,
-    'background-image',
-    `url("data:image/svg+xml;base64,${BBC_BLOCKS}")`,
-  );
-};
-
-export const visibleImageWithCaption = figure => {
-  const figureVisibility = fig => {
-    fig.should('be.visible');
-    fig.should('to.have.descendants', 'img');
-  };
-
-  figureVisibility(figure);
-  figure.should('to.have.descendants', 'figcaption');
 };
