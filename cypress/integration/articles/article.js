253be54cb5034fc3e97a789f2eb50f8dcff3a4f6
Benjamin Hobbs <bjhobbs06@gmail.com>
2019-06-23 11:51:08 +0100

    Comments tests needed

--- cypress/integration/articles/article.js
+++ cypress/integration/articles/article.js
@@ -67,19 +67,60 @@ Object.keys(config.services).forEach(index => {
         });
       }
 
-      it(`should have an image copyright label with styling for ${service}`, () => {
-        cy.copyrightDataWindow();
-      });
+      // it('should render an H1, which contains/displays a styled headline', () => {
+      //   firstHeadlineDataWindow();
+      // });
 
-      it(`should render a title for ${service}`, () => {
-        cy.window().then(win => {
-          const { seoHeadline } = win.SIMORGH_DATA.pageData.promo.headlines;
-          cy.title().should(
-            'eq',
-            `${seoHeadline}${serviceConfig.meta.titleSuffix}`,
-          );
-        });
-      });
+      // news only
+      // it('should render a formatted timestamp', () => {
+      //   cy.window().then(win => {
+      //     const { lastPublished } = win.SIMORGH_DATA.pageData.metadata;
+      //     const timeStamp = Cypress.moment(lastPublished).format('D MMMM YYYY');
+      //     cy.get('time').should('contain', timeStamp);
+      //   });
+      // });
+
+      // news only
+      // it('should render an H2, which contains/displays a styled subheading', () => {
+      //   firstSubheadlineDataWindow();
+      // });
+
+      // it('should render a paragraph, which contains/displays styled text', () => {
+      //   firstParagraphDataWindow();
+      // });
+
+      // it('should have a placeholder image', () => {
+      //   placeholderImageLoaded(cy.get('figure div').eq(0));
+      // });
+
+      // it('should have a visible image without a caption, and also not be lazyloaded', () => {
+      //   const firstFigure = cy.get('figure').eq(0);
+
+      //   visibleImageNoCaption(firstFigure);
+      //   firstFigure.within(() => cy.get('noscript').should('not.exist'));
+      // });
+
+      // it('should have a visible image with a caption that is lazyloaded and has a noscript fallback image', () => {
+      //   const imageHasNotLoaded = cy.get('figure').eq(2);
+
+      //   imageHasNotLoaded.within(() => {
+      //     const lazyLoadPlaceholder = cy.get('div div');
+      //     lazyLoadPlaceholder.should('have.class', 'lazyload-placeholder');
+      //   });
+
+      //   imageHasNotLoaded.scrollIntoView();
+
+      //   const imageHasLoaded = cy.get('figure').eq(2);
+
+      //   visibleImageWithCaption(imageHasLoaded);
+      //   imageHasLoaded.within(() => {
+      //     const noscriptImg = cy.get('noscript');
+      //     noscriptImg.contains('<img ');
+
+      //     const ImageContainer = cy.get('div div');
+      //     ImageContainer.should('not.have.class', 'lazyload-placeholder');
+      //   });
+      // });
 
       if (serviceConfig.pageTypes.articles.featureFlags.footer === true) {
         it(`should render the footers's BBC News branding for ${service}`, () => {
@@ -94,6 +135,20 @@ Object.keys(config.services).forEach(index => {
           cy.footerTestCopyrightLink();
         });
       }
+
+      it(`should have an image copyright label with styling for ${service}`, () => {
+        cy.copyrightDataWindow();
+      });
+
+      it(`should render a title for ${service}`, () => {
+        cy.window().then(win => {
+          const { seoHeadline } = win.SIMORGH_DATA.pageData.promo.headlines;
+          cy.title().should(
+            'eq',
+            `${seoHeadline}${serviceConfig.meta.titleSuffix}`,
+          );
+        });
+      });
 
       it(`should have script to fetch bundle for ${service}`, () => {
         cy.hasScriptToFetchBundle();
