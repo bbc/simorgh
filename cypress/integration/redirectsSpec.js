13e53930104c3e41cd192f477960ac69fc51b876
Andrzej Pogonowski <pogon.ap@hotmail.com>
2019-06-24 19:09:46 +0100

    Changing to test

--- cypress/integration/redirectsSpec.js
+++ cypress/integration/redirectsSpec.js
@@ -4,18 +4,18 @@ describe('Persian redirects', () => {
   // We are testing redirects from .co.uk to com
   // This means we need to enforce these tests to call .co.uk
   // We are assuming that these tests are running outside of the uk
-  Cypress.config('baseUrl', 'https://www.bbc.co.uk');
+  Cypress.config('baseUrl', 'https://www.test.bbc.com');
 
   // Given I go to https://www.bbc.co.uk/persian/articles/:id
   // Then I am given a 301 redirect to https://www.bbc.com/persian/articles/:id
   it('should redirect me to .com if I am hitting the .co.uk on persian canonical', () => {
     cy.request({
-      url: `/persian/articles/${config.assets.persian}`,
+      url: `/persian/articles/c4vlle3q337o`,
       followRedirect: false,
     }).then(resp => {
       expect(resp.status).to.eq(302);
       expect(resp.redirectedToUrl).to.eq(
-        `https://www.bbc.com/persian/articles/${config.assets.persian}`,
+        `https://www.test.bbc.com/persian/articles/c4vlle3q337o`,
       );
     });
   });
