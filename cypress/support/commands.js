aa8f335edf8cf5e594b35e5a35b36736ddd39d13
Benjamin Hobbs <bjhobbs06@gmail.com>
2019-06-25 20:08:27 +0100

    Tidy up

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,8 @@
+// Overwriting Cypress Commands should very rarely be done.
 Cypress.Commands.overwrite('visit', (originalFn, url, options) => {
   cy.request({ url, failOnStatusCode: false }).then(({ headers }) => {
     // Always ensure we're not seeing the Mozart fallback
-    if (expect(headers).not.have.property('mxfa')) {
+    if (expect(headers).not.have.property('x-mfa')) {
       return originalFn(url, options);
     }
 
@@ -9,6 +10,7 @@ Cypress.Commands.overwrite('visit', (originalFn, url, options) => {
   });
 });
 
+// Addding Cypress Commands is safe, add away...
 Cypress.Commands.add('testResponseCodeAndType', (path, responseCode, type) => {
   cy.request({ url: path, failOnStatusCode: false }).then(
     ({ status, headers }) => {
