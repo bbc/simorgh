3be336fa5c2f794cbce31c74997674d581dd99a9
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-06-26 20:56:22 +0100

    :pencil: Write a legacy for the next generation

--- cypress/integration/atiSpec.js
+++ cypress/integration/atiSpec.js
@@ -5,6 +5,11 @@ import {
 } from '../support/bodyTestHelper';
 import { describeForLocalOnly } from '../support/limitEnvRuns';
 
+// NB: If these tests start failing unexpectedly it's a good sign that the dom is being
+// cleared during hydration. React won't render noscript tags on the client so if they
+// get cleared during hydration, the following render wont re-add them.
+// See https://github.com/facebook/react/issues/11423#issuecomment-341751071 for more infomation.
+
 describe('Article ATI for News', () => {
   describe('Canonical page', () => {
     it('should have a noscript tag with an 1px image with the ati url', () => {
