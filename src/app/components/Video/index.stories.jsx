d8454afcc57781f4fb23b17c64c3aae09a09e37e
Sareh Heidari <sareh.heidari@gmail.com>
2019-06-26 15:48:51 +0100

    Add Video & Audio examples

--- src/app/components/Video/index.stories.jsx
+++ src/app/components/Video/index.stories.jsx
@@ -2,14 +2,34 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Video from './index';
 import {
-  videoClipGlobalGuidanceBlock,
-  videoClipUkGuidanceBlock,
+  videoClipGlobalGuidance,
+  videoClipUkGuidance,
+  videoClipNonUkNoGuidance,
+  videoClipGlobalNoGuidancePortrait,
+  audioClipGlobalGuidance,
+  audioClipUkNoGuidance,
+  audioClipNonUkNoGuidance,
 } from './helpers/fixtures';
 
 storiesOf('Video', module)
   .add('video, clip, global, with guidance', () => (
-    <Video {...videoClipGlobalGuidanceBlock} />
+    <Video {...videoClipGlobalGuidance} />
   ))
   .add('video, clip, UK only, with guidance', () => (
-    <Video {...videoClipUkGuidanceBlock} />
+    <Video {...videoClipUkGuidance} />
+  ))
+  .add('video, clip, non-UK, without guidance', () => (
+    <Video {...videoClipNonUkNoGuidance} />
+  ))
+  .add('video, clip, global, no guidance, portrait', () => (
+    <Video {...videoClipGlobalNoGuidancePortrait} />
+  ))
+  .add('audio, clip, global, with guidance', () => (
+    <Video {...audioClipGlobalGuidance} />
+  ))
+  .add('audio, clip, UK, without guidance', () => (
+    <Video {...audioClipUkNoGuidance} />
+  ))
+  .add('audio, clip, non-UK, without guidance', () => (
+    <Video {...audioClipNonUkNoGuidance} />
   ));
--- src/app/components/Video/index.test.jsx
+++ src/app/components/Video/index.test.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { shouldShallowMatchSnapshot } from '../../../testHelpers';
 import Video from '.';
-import { videoClipGlobalGuidanceBlock } from './helpers/fixtures';
+import { videoClipGlobalGuidance } from './helpers/fixtures';
 
 describe('Video', () => {
   describe('with data', () => {
     shouldShallowMatchSnapshot(
       'should render the video with valid props',
-      <Video {...videoClipGlobalGuidanceBlock} />,
+      <Video {...videoClipGlobalGuidance} />,
     );
   });
 });
