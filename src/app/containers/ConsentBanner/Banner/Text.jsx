45465d6408e5a76728063cac6b1bd611df6dbcaa
thekp <khoa.phan@bbc.co.uk>
2019-06-26 15:24:03 +0100

    add script prop to consentBannerText

--- src/app/containers/ConsentBanner/Banner/Text.jsx
+++ src/app/containers/ConsentBanner/Banner/Text.jsx
@@ -2,16 +2,18 @@ import React, { useContext } from 'react';
 import { string, shape } from 'prop-types';
 import { ConsentBannerText } from '@bbc/psammead-consent-banner';
 import { RequestContext } from '../../../contexts/RequestContext';
+import { ServiceContext } from '../../../contexts/ServiceContext';
 
 const BannerText = ({ uk, international }) => {
   const { isUK } = useContext(RequestContext);
+  const { script } = useContext(ServiceContext);
 
   const { first, linkText, linkUrl, last } = isUK ? uk : international;
 
   const Link = linkUrl && linkText ? <a href={linkUrl}>{linkText}</a> : null;
 
   return (
-    <ConsentBannerText>
+    <ConsentBannerText script={script}>
       {first}
       {Link}
       {last}
