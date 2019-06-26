dd0c529aa0eb6b2a30e2f6cc9fc31e5423e7f094
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-06-26 13:10:32 +0100

    :pencil: Add issue to comment

--- puppeteer/bundleRequests.test.js
+++ puppeteer/bundleRequests.test.js
@@ -10,7 +10,8 @@ const config = cypressConfig('local');
 
 const services = [];
 
-// This logic is required to combine the service configs untill this is done in a follow up PR
+// This logic is required to combine the service configs untill
+// this is done in https://github.com/bbc/simorgh/issues/2131
 const rawConfig = {
   ...wsConfig,
   news: config.assets.news,
