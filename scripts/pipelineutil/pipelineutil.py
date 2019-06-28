cfb08cb24eecdc52e087e85bc70980c61bfea15b
Andrzej Pogonowski <pogon.ap@hotmail.com>
2019-06-28 14:22:52 +0100

    Made the script into a cli tool

--- scripts/pipelineutil/pipelineutil.py
+++ scripts/pipelineutil/pipelineutil.py
@@ -1,5 +1,5 @@
+import fire
 from pygit2 import Repository, GIT_STATUS_INDEX_MODIFIED, GIT_STATUS_WT_MODIFIED, GIT_DELTA_ADDED
-
 
 def is_doc(path):
   return path.endswith('.md')
@@ -7,7 +7,18 @@ def is_doc(path):
 def get_files(repo, ref):
   difference = repo.diff(ref)
   files = [patch.delta.new_file.path for patch in difference]
+  files.remove(".env")
   return files
+
+def print_files(files):
+  print('-----------------------------------')
+  print('FILES CHANGED')
+  print('-----------------------------------')
+  
+  for f in files:
+    print(f)
+
+  print('-----------------------------------')
 
 def test_files(files):
   for f in files:
@@ -16,16 +27,37 @@ def test_files(files):
 
   return True
 
+def log_start():
+  print("doc-change")
+  print("This script will check for changes in the current git repository")
+  print("I will check if you only changed markdown files (.md)")
 
-repo = Repository('.')
-head = repo.head
-print(head.shorthand)
+def log_result(outcome):
+  if outcome:
+    print("Only markdown files have been changed")
+    print("No need to run any tests")
+  else:
+    print("Files other than markdown were changed")
+    print("Tests are necessary")
 
 
-files = get_files(repo, head)
-print(files)
+class Doc:
+  def doc_change(self):
+    log_start()
+    repo = Repository('.')
+    head = repo.head
+    print("Branch: %s" % head.shorthand)
 
-result = test_files(files)
 
-print(result)
+    files = get_files(repo, head)
+    print_files(files)
 
+    result = test_files(files)
+
+    log_result(result)
+
+    print("Outcome: %s" % result)
+    return result
+
+if __name__ == '__main__':
+  fire.Fire(Doc)
