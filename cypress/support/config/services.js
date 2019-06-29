f03f3137b5caad2cbcee0dbdeb7469d4c9a00b47
Benjamin Hobbs <bjhobbs06@gmail.com>
2019-06-29 15:42:14 +0100

    Add variable asset for news

--- cypress/support/config/services.js
+++ cypress/support/config/services.js
@@ -1,13 +1,5 @@
 export default {
   igbo: {
-    assets: {
-      mockArticleURL: '/igbo/articles/c0000000000o',
-      mockAmpArticleURL: '/igbo/articles/c0000000000o.amp',
-      // privacyStatement: 'Anyị ewelitela ihe nzuzo anyị nakwa iwu dị na ya.',
-      // performanceStatement: 'Ka anyị mara ma i kwernyere na iwu ndị a',
-      // cookieAgreement: 'Eeh, ekwere m',
-      // privacyAgreement: 'Ọ dị mma',
-    },
     pageTypes: {
       articles: undefined,
       frontPage: '/igbo',
@@ -16,21 +8,13 @@ export default {
   news: {
     pageTypes: {
       articles: {
-        asset: 'c5ll353v7y9o',
+        asset:
+          Cypress.env('APP_ENV') === 'live' ? 'c5ll353v7y9o' : 'c6v11qzyv8po',
       },
       frontPage: undefined,
     },
   },
   persian: {
-    assets: {
-      mockArticleURL: '/persian/articles/c0000000000o',
-      mockAmpArticleURL: '/persian/articles/c0000000000o.amp',
-      privacyStatement:
-        'ما سیاست‌های حفظ حریم خصوصی و کوکی‌های خود را به روز کرده‌ایم',
-      performanceStatement: 'آیا با دریافت کوکی‌های ما موافقید؟',
-      cookieAgreement: 'بله، موافقم',
-      privacyAgreement: 'تایید',
-    },
     pageTypes: {
       articles: {
         asset: 'c4vlle3q337o',
@@ -39,29 +23,12 @@ export default {
     },
   },
   pidgin: {
-    assets: {
-      mockArticleURL: '/pidgin/articles/c0000000000o',
-      mockAmpArticleURL: '/pidgin/articles/c0000000000o.amp',
-      privacyStatement: 'We don update our Privacy and Cookies Policy',
-      performanceStatement: 'Let us know say you agree to cookies',
-      cookieAgreement: 'Yes, I agree',
-      privacyAgreement: 'OK',
-    },
     pageTypes: {
       articles: undefined,
       frontPage: '/pidgin',
     },
   },
   yoruba: {
-    assets: {
-      mockArticleURL: '/yoruba/articles/c0000000000o',
-      mockAmpArticleURL: '/yoruba/articles/c0000000000o.amp',
-      privacyStatement:
-        'Àgbéga ti bá ìlànà àgbékalẹ̀ ojú òpó ìdákọ́ńkọ́ àti ayélujára wa',
-      performanceStatement: 'Ẹ jẹ́ ká mọ̀ pé ẹ faramọ́ ìlànà òpó ìtàkún àgbáyé wa',
-      cookieAgreement: 'Bẹ́ẹ̀ni, mo fara mọ́ ọ',
-      privacyAgreement: 'Ó dára bẹ́ẹ̀',
-    },
     pageTypes: {
       articles: undefined,
       frontPage: '/yoruba',
