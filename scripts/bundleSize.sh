f84a338e29608708985c4070aca924ff61b7e974
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-07-01 18:18:47 +0100

    :pencil: Update build size

--- scripts/bundleSize.sh
+++ scripts/bundleSize.sh
@@ -2,8 +2,8 @@
 
 # Size limit for all bundles used by each service (K)
 # Keep these +/- 5K and update frequently!
-min=634
-max=649
+min=650
+max=668
 
 services=( "news" "persian" "igbo" "yoruba" "pidgin" )
 failure=false
