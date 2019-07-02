40ef42adfc8c585d7943931f4ae2155e3337c22d
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-07-02 16:36:47 +0100

    :bath: Remove old comment

--- src/server/assets/index.js
+++ src/server/assets/index.js
@@ -16,11 +16,6 @@ const getAssetsArray = service => {
     );
     const assetsManifestKeys = Object.keys(assetManifest);
 
-    /**
-     * Loops through the asset manifest, extracts the JS URL out of each entry and
-     * injects them into the assets array, which is passed to render.
-     * Loops backwards as the client bundle is output first, but needs to be last.
-     */
     for (let i = assetsManifestKeys.length - 1; i >= 0; i -= 1) {
       const key = assetsManifestKeys[i];
       if (key.length > 0) {
