bee645f7eb9fed3dcfedc39707ade6fd2de519ac
jimjohnsonrollings <jim.johnson-rollings@bbc.co.uk>
2019-07-03 09:58:27 +0100

    Update schema and tests

--- dataValidator/helpers/dataLoader/asyncValidateDir.test.js
+++ dataValidator/helpers/dataLoader/asyncValidateDir.test.js
@@ -33,13 +33,13 @@ describe('asyncValidateDir helper', () => {
   it('should call readScenario for every file in the /data directory', async () => {
     const readScenarioSpy = jest.spyOn(readScenario, 'readScenario');
 
-    await expectMethodToBeCalledTimes(73, readScenarioSpy);
+    await expectMethodToBeCalledTimes(74, readScenarioSpy);
   });
 
   it('should call fileToValidate for only the valid json file in the /data directory', async () => {
     fileToValidateSpy = jest.spyOn(readScenario, 'fileToValidate');
 
-    await expectMethodToBeCalledTimes(38, fileToValidateSpy);
+    await expectMethodToBeCalledTimes(39, fileToValidateSpy);
   });
 
   it('should call fileToValidate for only the files in /data/prod/news', async () => {
