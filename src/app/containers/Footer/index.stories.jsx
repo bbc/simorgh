8d51831c11ba534cb6fff7f7310ce7d237eb3160
thekp <khoa.phan@bbc.co.uk>
2019-07-03 16:12:33 +0100

    update footer story conatiner

--- src/app/containers/Footer/index.stories.jsx
+++ src/app/containers/Footer/index.stories.jsx
@@ -6,34 +6,11 @@ import { ServiceContextProvider } from '../../contexts/ServiceContext';
 import { dirDecorator } from '@bbc/psammead-storybook-helpers';
 import { select, withKnobs } from '@storybook/addon-knobs';
 
-const inputs = () => {
-  // capitalization is only for presentation purpose on the knob
-  const options = Object.keys(services)
-    .filter(key => key !== 'BBC_BLOCKS')
-    .map(key => key.charAt(0).toUpperCase() + key.slice(1));
-
-  const serviceChoice = select('Service SVG', options, 'News').toLowerCase();
-
-  return { serviceChoice };
-};
-
-// Object.keys(services)
-//   .filter(service => service !== 'default')
-//   .forEach(service => {
-//     storiesOf('Footer', module)
-//       .addDecorator(withKnobs)
-//       .addDecorator(dirDecorator)
-//       .add(service, () => (
-//         <ServiceContextProvider service={service}>
-//           <Footer />
-//         </ServiceContextProvider>
-//       ));
-//   });
 storiesOf('Footer', module)
   .addDecorator(withKnobs)
   .addDecorator(dirDecorator)
   .add('default', () => {
-    const { serviceChoice } = inputs();
+    const serviceChoice = select('Select a service', services, 'news');
 
     return (
       <ServiceContextProvider service={serviceChoice}>
