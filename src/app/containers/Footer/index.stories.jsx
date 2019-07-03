aded6e9548ae74d6399270c1d46a22ea2654499b
thekp <khoa.phan@bbc.co.uk>
2019-07-03 10:50:46 +0100

    wip

--- src/app/containers/Footer/index.stories.jsx
+++ src/app/containers/Footer/index.stories.jsx
@@ -4,17 +4,40 @@ import Footer from '.';
 import services from '../../lib/config/services';
 import { ServiceContextProvider } from '../../contexts/ServiceContext';
 import { dirDecorator } from '@bbc/psammead-storybook-helpers';
-import { withKnobs } from '@storybook/addon-knobs';
+import { select, withKnobs } from '@storybook/addon-knobs';
 
-Object.keys(services)
-  .filter(service => service !== 'default')
-  .forEach(service => {
-    storiesOf('Footer', module)
-      .addDecorator(withKnobs)
-      .addDecorator(dirDecorator)
-      .add(service, () => (
-        <ServiceContextProvider service={service}>
-          <Footer />
-        </ServiceContextProvider>
-      ));
+const inputs = () => {
+  // capitalization is only for presentation purpose on the knob
+  const options = Object.keys(services)
+    .filter(key => key !== 'BBC_BLOCKS')
+    .map(key => key.charAt(0).toUpperCase() + key.slice(1));
+
+  const serviceChoice = select('Service SVG', options, 'News').toLowerCase();
+
+  return { serviceChoice };
+};
+
+// Object.keys(services)
+//   .filter(service => service !== 'default')
+//   .forEach(service => {
+//     storiesOf('Footer', module)
+//       .addDecorator(withKnobs)
+//       .addDecorator(dirDecorator)
+//       .add(service, () => (
+//         <ServiceContextProvider service={service}>
+//           <Footer />
+//         </ServiceContextProvider>
+//       ));
+//   });
+storiesOf('Footer', module)
+  .addDecorator(withKnobs)
+  .addDecorator(dirDecorator)
+  .add('default', () => {
+    const { serviceChoice } = inputs();
+
+    return (
+      <ServiceContextProvider service={serviceChoice}>
+        <Footer />
+      </ServiceContextProvider>
+    );
   });
