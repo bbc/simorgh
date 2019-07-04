68ef0b824e2eae24eb40888fba6b86d12d59cf5e
thekp <khoa.phan@bbc.co.uk>
2019-07-04 17:08:09 +0100

    add story for header

---
+++ src/app/containers/Header/index.stories.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import * as svgs from '@bbc/psammead-assets/svgs';
+import { inputProvider } from '@bbc/psammead-storybook-helpers';
+import { withKnobs } from '@storybook/addon-knobs';
+import HeaderContainer from '.';
+import { RequestContextProvider } from '../../contexts/RequestContext';
+import services from '../../lib/config/services';
+import { ServiceContextProvider } from '../../contexts/ServiceContext';
+
+storiesOf('Header Container', module)
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    inputProvider(
+      null,
+      ({ service }) => {
+        const serviceContextStub = {
+          product: 'BBC News',
+          service,
+          brandSVG: svgs[service],
+          svgHeight: 24,
+          maxWidth: 280,
+          minWidth: 180,
+        };
+        return (
+          <ServiceContextProvider service={service} value={serviceContextStub}>
+            <RequestContextProvider
+              pageType="frontPage"
+              isAmp={false}
+              service={service}
+            >
+              <HeaderContainer />
+            </RequestContextProvider>
+          </ServiceContextProvider>
+        );
+      },
+      Object.keys(services),
+    ),
+  );
