7802eb39712e78242c227f1b0832952c30cc8529
Rhys Hellyar <rhys.hellyar@bbc.co.uk>
2019-07-04 11:21:07 +0100

    Tests for Chartbeat domain

--- src/app/lib/analyticsUtils/chartbeat.test.js
+++ src/app/lib/analyticsUtils/chartbeat.test.js
@@ -1,11 +1,28 @@
-import { chartbeatUID, useCanonical } from './chartbeat';
+import { chartbeatUID, useCanonical, domain } from './chartbeat';
 
-describe('Chartbeat metrics', () => {
+describe('Chartbeat utilities', () => {
   it('should return the correct chartbeat UID', () => {
     expect(chartbeatUID).toBe(50924);
   });
 
   it('useCanonical should be true', () => {
     expect(useCanonical).toBe(true);
+  });
+
+  describe('Chartbeat Domains', () => {
+    const services = [
+      ['news', 'bbc.co.uk'],
+      ['persian', 'persian.bbc.co.uk'],
+      ['igbo', 'igbo.bbc.co.uk'],
+      ['thai', 'thai.bbc.co.uk'],
+    ];
+
+    services.forEach(([service, expected]) => {
+      it(`domain should return "${expected}" when service is ${service}`, () => {
+        const res = domain(service);
+
+        expect(res).toBe(expected);
+      });
+    });
   });
 });
