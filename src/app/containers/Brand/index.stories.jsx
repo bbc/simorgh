641f7ce4d154448a2faee711dff92e2f1ac33d5f
thekp <khoa.phan@bbc.co.uk>
2019-07-04 15:54:19 +0100

    update brand container storybook

--- src/app/containers/Brand/index.stories.jsx
+++ src/app/containers/Brand/index.stories.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import * as svgs from '@bbc/psammead-assets/svgs';
+import { inputProvider } from '@bbc/psammead-storybook-helpers';
+import { withKnobs } from '@storybook/addon-knobs';
+import services from '../../lib/config/services';
 import { ServiceContextProvider } from '../../contexts/ServiceContext';
 import Brand from '.';
-import services from '../../lib/config/services';
 
-const stories = storiesOf('Brand', module);
-
-Object.keys(services)
-  .filter(service => service !== 'default')
-  .forEach(service => {
-    stories.add(service, () => (
-      <ServiceContextProvider service={service}>
-        <Brand
-          product="BBC News"
-          svg={svgs[service]}
-          svgHeight={24}
-          maxWidth={280}
-          minWidth={180}
-        />
-      </ServiceContextProvider>
-    ));
-  });
+storiesOf('Brand', module)
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    inputProvider(
+      null,
+      ({ service }) => {
+        return (
+          <ServiceContextProvider service={service}>
+            <Brand
+              product="BBC News"
+              svg={svgs[service]}
+              svgHeight={24}
+              maxWidth={280}
+              minWidth={180}
+            />
+          </ServiceContextProvider>
+        );
+      },
+      Object.keys(services),
+    ),
+  );
