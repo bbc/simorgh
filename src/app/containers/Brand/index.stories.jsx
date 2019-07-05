32c0dedfc8bf290ba0b678dab304f192975a9a76
thekp <khoa.phan@bbc.co.uk>
2019-07-05 13:51:50 +0100

    update Brandcontainer story

--- src/app/containers/Brand/index.stories.jsx
+++ src/app/containers/Brand/index.stories.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import * as svgs from '@bbc/psammead-assets/svgs';
 import { inputProvider } from '@bbc/psammead-storybook-helpers';
 import { withKnobs } from '@storybook/addon-knobs';
 import services from '../../lib/config/services';
 import { ServiceContextProvider } from '../../contexts/ServiceContext';
-import Brand from '.';
+import BrandContainer from '.';
 
-storiesOf('Brand', module)
+storiesOf('Brand Container', module)
   .addDecorator(withKnobs)
   .add(
     'default',
@@ -16,13 +15,7 @@ storiesOf('Brand', module)
       ({ service }) => {
         return (
           <ServiceContextProvider service={service}>
-            <Brand
-              product="BBC News"
-              svg={svgs[service]}
-              svgHeight={24}
-              maxWidth={280}
-              minWidth={180}
-            />
+            <BrandContainer />
           </ServiceContextProvider>
         );
       },
