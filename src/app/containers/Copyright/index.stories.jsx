09df8f4021a001693ac93557f306420ccd35e90b
thekp <khoa.phan@bbc.co.uk>
2019-07-05 11:30:44 +0100

    add storybook for copyright container

---
+++ src/app/containers/Copyright/index.stories.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { inputProvider } from '@bbc/psammead-storybook-helpers';
+import { withKnobs } from '@storybook/addon-knobs';
+import CopyRightContainer from '.';
+import services from '../../lib/config/services';
+import { ServiceContextProvider } from '../../contexts/ServiceContext';
+
+storiesOf('CopyRight Container', module)
+  .addDecorator(withKnobs)
+  .add(
+    'default',
+    inputProvider(
+      null,
+      ({ service }) => {
+        const imageCaptionOffscreenText = services[service]
+          .imageCopyrightOffscreenText
+          ? services[service].imageCopyrightOffscreenText
+          : 'Image source, ';
+
+        const serviceContextStub = {
+          product: 'BBC News',
+          imageCaptionOffscreenText,
+          lang: services[service].lang,
+          dir: services[service].dir,
+        };
+        return (
+          <ServiceContextProvider service={service} value={serviceContextStub}>
+            <CopyRightContainer>{imageCaptionOffscreenText}</CopyRightContainer>
+          </ServiceContextProvider>
+        );
+      },
+      Object.keys(services),
+    ),
+  );
