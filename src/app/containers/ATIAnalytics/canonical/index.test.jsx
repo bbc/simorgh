7117b04a1c1ad5641a2eeadc96dbe2999edb14a1
Sareh Heidari <sareh.heidari@gmail.com>
2019-07-05 15:01:16 +0100

    Add more test cases

--- src/app/containers/ATIAnalytics/canonical/index.test.jsx
+++ src/app/containers/ATIAnalytics/canonical/index.test.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { node, string } from 'prop-types';
-import renderer from 'react-test-renderer';
+import { act } from 'react-test-renderer';
+import ReactDOM from 'react-dom';
 import CanonicalATIAnalytics from '.';
-
 import { ServiceContextProvider } from '../../../contexts/ServiceContext';
 import { RequestContextProvider } from '../../../contexts/RequestContext';
 import { shouldMatchSnapshot } from '../../../../testHelpers';
@@ -27,51 +27,84 @@ ContextWrap.propTypes = {
   children: node.isRequired,
 };
 
-const mockPageviewParams = 'key=value&key2=value2';
-
 describe('Canonical ATI Analytics', () => {
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
   afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
     jest.clearAllMocks();
   });
+  const mockPageviewParams = 'key=value&key2=value2';
 
-  shouldMatchSnapshot(
-    'should render correctly for Live ATI endpoint',
-    <ContextWrap bbcOrigin="https://www.bbc.co.uk">
-      <CanonicalATIAnalytics pageviewParams={mockPageviewParams} />
-    </ContextWrap>,
-  );
-
+  // following tests are commented out so can debug them one at a time
   const testCases = [
-    {
-      describe: 'should call sendBeacon with Live ATI URL for live env',
-      atiUrl: 'https://a1.api.bbc.co.uk/hit.xiti?key=value&key2=value2',
-      bbcOrigin: 'https://www.bbc.co.uk',
-    },
     // {
-    //   describe: 'should call sendBeacon with Test ATI URL for test env',
+    //   describe:
+    //     'should call sendBeacon with Live ATI URL for live env (.co.uk)',
+    //   atiUrl: 'https://a1.api.bbc.co.uk/hit.xiti?key=value&key2=value2',
+    //   bbcOrigin: 'https://www.bbc.co.uk',
+    // },
+    // {
+    //   describe:
+    //     'should call sendBeacon with Test ATI URL for test env (.co.uk)',
     //   atiUrl: 'https://logws1363.ati-host.net?key=value&key2=value2',
     //   bbcOrigin: 'https://www.test.bbc.co.uk',
     // },
     // {
-    //   describe: 'should call sendBeacon with Test ATI URL for local env',
+    //   describe:
+    //     'should call sendBeacon with Test ATI URL for local env (.co.uk)',
     //   atiUrl: 'https://logws1363.ati-host.net?key=value&key2=value2',
     //   bbcOrigin: 'http://localhost.bbc.co.uk',
     // },
+    // {
+    //   describe: 'should call sendBeacon with Live ATI URL for live env (.com)',
+    //   atiUrl: 'https://a1.api.bbc.co.uk/hit.xiti?key=value&key2=value2',
+    //   bbcOrigin: 'https://www.bbc.com',
+    // },
+    // {
+    //   describe: 'should call sendBeacon with Test ATI URL for test env (.com)',
+    //   atiUrl: 'https://logws1363.ati-host.net?key=value&key2=value2',
+    //   bbcOrigin: 'https://www.test.bbc.com',
+    // },
+    // {
+    //   describe: 'should call sendBeacon with Test ATI URL for local env (.com)',
+    //   atiUrl: 'https://logws1363.ati-host.net?key=value&key2=value2',
+    //   bbcOrigin: 'http://localhost.bbc.com',
+    // },
   ];
+
+  const mockSendBeacon = jest.fn().mockReturnValue('beacon-return-value');
 
   testCases.forEach(({ describe, atiUrl, bbcOrigin }) => {
     it(describe, () => {
-      const mockSendBeacon = jest.fn().mockReturnValue('beacon-return-value');
+      // this is not working since useEffect is not being triggered.
       beacon.default = mockSendBeacon;
-
-      renderer.create(
-        <ContextWrap bbcOrigin={bbcOrigin}>
-          <CanonicalATIAnalytics pageviewParams={mockPageviewParams} />
-        </ContextWrap>,
-      );
+      act(() => {
+        ReactDOM.render(
+          <ContextWrap bbcOrigin={bbcOrigin}>
+            <CanonicalATIAnalytics pageviewParams={mockPageviewParams} />
+          </ContextWrap>,
+          container,
+        );
+      });
 
       expect(mockSendBeacon).toHaveBeenCalledTimes(1);
       expect(mockSendBeacon).toHaveBeenCalledWith(atiUrl);
+      expect(container.innerHTML).toBe(
+        `<noscript><img height="1px" width="1px" alt="" src=${atiUrl} /></noscript>`,
+      );
     });
   });
+
+  shouldMatchSnapshot(
+    'should render correctly',
+    <ContextWrap bbcOrigin="https://www.bbc.co.uk">
+      <CanonicalATIAnalytics pageviewParams={mockPageviewParams} />
+    </ContextWrap>,
+  );
 });
