5758024fa52966a5efc586ef9951c0f60a483e91
thekp <khoa.phan@bbc.co.uk>
2019-07-05 16:32:21 +0100

    update variable names

--- src/app/containers/Copyright/index.stories.jsx
+++ src/app/containers/Copyright/index.stories.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { inputProvider } from '@bbc/psammead-storybook-helpers';
 import { withKnobs } from '@storybook/addon-knobs';
-import CopyRightContainer from '.';
+import CopyrightContainer from '.';
 import services from '../../lib/config/services';
 import { ServiceContextProvider } from '../../contexts/ServiceContext';
 
-storiesOf('CopyRight Container', module)
+storiesOf('Copyright Container', module)
   .addDecorator(withKnobs)
   .add(
     'default',
     inputProvider(
       null,
       ({ service }) => {
-        const imageCaptionOffscreenText =
+        const imageCaptionText =
           services[service].imageCopyrightOffscreenText || 'Image source, ';
 
         const serviceContextStub = {
-          imageCaptionOffscreenText,
+          imageCaptionOffscreenText: imageCaptionText,
           lang: services[service].lang,
           dir: services[service].dir,
         };
         return (
           <ServiceContextProvider service={service} value={serviceContextStub}>
-            <CopyRightContainer>{imageCaptionOffscreenText}</CopyRightContainer>
+            <CopyrightContainer>{imageCaptionText}</CopyrightContainer>
           </ServiceContextProvider>
         );
       },
