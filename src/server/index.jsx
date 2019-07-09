f0a94ced90fd546468b3820b4aada9e68014b8d3
Rachael Njeri <rachael.njeri@andela.com>
2019-07-09 11:13:29 +0300

    Chain off the server methods
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- src/server/index.jsx
+++ src/server/index.jsx
@@ -73,13 +73,12 @@ if (process.env.APP_ENV === 'local') {
     });
   };
 
-  server.use((req, res, next) => {
-    if (req.url.substr(-1) === '/' && req.url.length > 1)
-      res.redirect(301, req.url.slice(0, -1));
-    else next();
-  });
-
   server
+    .use((req, res, next) => {
+      if (req.url.substr(-1) === '/' && req.url.length > 1)
+        res.redirect(301, req.url.slice(0, -1));
+      else next();
+    })
     .use(
       expressStaticGzip(publicDirectory, {
         enableBrotli: true,
