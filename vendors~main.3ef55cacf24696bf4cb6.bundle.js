6cdf28c763aba01045ae14215090db06f92a3e5c
BBC News CI <foo@bar.com>
2019-07-10 10:35:57 +0000

    Updates

--- vendors~main.3ef55cacf24696bf4cb6.bundle.js
+++ vendors~main.3ef55cacf24696bf4cb6.bundle.js
@@ -111,4 +111,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",function(){return isPlainObject})}]]);
-//# sourceMappingURL=vendors~main.b3fe200b0390520f1136.bundle.js.map
+//# sourceMappingURL=vendors~main.3ef55cacf24696bf4cb6.bundle.js.map
