53428db0b5555611a8ae4864f2ec70de6dae2060
rowland henshaw <rowland.henshaw@andela.com>
2019-07-10 16:24:28 +0100

    added documenntation for usePrevious

--- src/app/lib/utilities/usePrevious/index.js
+++ src/app/lib/utilities/usePrevious/index.js
@@ -1,5 +1,12 @@
 import { useRef, useEffect } from 'react';
 
+/**
+ * Custom React hook that returns the previous value of a prop.
+ * similar usage to `prevProps` in `componentWillRecieveProps`,
+ * ie `usePrevious(someProp)` returns `prevProps.someProp`.
+ * Tracked prop is initialised to null
+ * @param {Any} value of any given type
+ */
 const usePrevious = value => {
   const ref = useRef(null);
   useEffect(() => {
