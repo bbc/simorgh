1d2d257c697fab24113f82aaaef087561c7199b3
Rhys Hellyar <rhys.hellyar@bbc.co.uk>
2019-07-11 12:19:09 +0100

    Remove duplicate and excessive testing

--- src/app/containers/ATIAnalytics/amp/index.test.jsx
+++ src/app/containers/ATIAnalytics/amp/index.test.jsx
@@ -14,17 +14,19 @@ jest.mock('react', () => {
 
 const { useContext } = jest.requireMock('react');
 
-const contextToReturn = (bbcOrigin, env) => ({
+const atiBaseUrl = 'https://foobar.com?';
+const contextToReturn = {
   service: 'news',
   id: 'c0000000000o',
   isAmp: true,
   pageType: 'article',
-  env,
-  bbcOrigin,
-});
+  env: 'live',
+  bbcOrigin: 'https://www.bbc.co.uk',
+};
 
 describe('Amp ATI Analytics', () => {
   let container;
+
   beforeEach(() => {
     jest.resetModules();
     useContext.mockReset();
@@ -32,55 +34,29 @@ describe('Amp ATI Analytics', () => {
     document.body.appendChild(container);
   });
 
-  const testCases = [
-    {
-      describe:
-        'should render amp-analytics with script tag & contain live ATI url - live env',
-      env: 'live',
-      atiBaseUrl: 'https://a1.api.bbc.co.uk/hit.xiti?',
-      bbcOrigin: 'https://www.bbc.co.uk',
-    },
-    {
-      describe:
-        'should render amp-analytics with script tag & contain test ATI url - test env',
-      env: 'test',
-      atiBaseUrl: 'https://logws1363.ati-host.net?',
-      bbcOrigin: 'https://www.test.bbc.co.uk',
-    },
-    {
-      describe:
-        'should render amp-analytics with script tag & contain test ATI url - local env',
-      env: 'local',
-      atiBaseUrl: 'https://logws1363.ati-host.net?',
-      bbcOrigin: 'http://localhost.bbc.co.uk',
-    },
-  ];
+  it('creates an AMP analytics container with required parameters', () => {
+    const mockAtiBaseUrl = jest.fn().mockReturnValue(atiBaseUrl);
 
-  testCases.forEach(({ describe, env, atiBaseUrl, bbcOrigin }) => {
-    it(describe, () => {
-      const mockAtiBaseUrl = jest.fn().mockReturnValue(atiBaseUrl);
+    useContext.mockReturnValue(contextToReturn);
+    atiUrl.atiBaseUrl = mockAtiBaseUrl;
 
-      useContext.mockReturnValue(contextToReturn(bbcOrigin, env));
-      atiUrl.atiBaseUrl = mockAtiBaseUrl;
-
-      act(() => {
-        ReactDOM.render(
-          <AmpATIAnalytics pageviewParams="key1=value1&key2=value2" />,
-          container,
-        );
-      });
-
-      expect(mockAtiBaseUrl).toHaveBeenCalledWith(env);
-      expect(container.querySelectorAll('amp-analytics').length).toEqual(1);
-      expect(
-        container.querySelectorAll(
-          'amp-analytics script[type="application/json"]',
-        ).length,
-      ).toEqual(1);
-      expect(
-        container.querySelector('amp-analytics script[type="application/json"]')
-          .innerHTML,
-      ).toMatch(atiBaseUrl);
+    act(() => {
+      ReactDOM.render(
+        <AmpATIAnalytics pageviewParams="key1=value1&key2=value2" />,
+        container,
+      );
     });
+
+    expect(mockAtiBaseUrl).toHaveBeenCalledWith('live');
+    expect(container.querySelectorAll('amp-analytics').length).toEqual(1);
+    expect(
+      container.querySelectorAll(
+        'amp-analytics script[type="application/json"]',
+      ).length,
+    ).toEqual(1);
+    expect(
+      container.querySelector('amp-analytics script[type="application/json"]')
+        .innerHTML,
+    ).toMatch(atiBaseUrl);
   });
 });
--- src/app/containers/ATIAnalytics/canonical/index.test.jsx
+++ src/app/containers/ATIAnalytics/canonical/index.test.jsx
@@ -16,126 +16,57 @@ jest.mock('react', () => {
 
 const { useContext } = jest.requireMock('react');
 
-const contextToReturn = (
-  bbcOrigin = 'https://logws1363.ati-host.net',
-  env = 'test',
-) => ({
+const contextToReturn = {
   service: 'news',
   id: 'c0000000000o',
   isAmp: false,
   pageType: 'article',
-  env,
-  bbcOrigin,
-});
+  env: 'live',
+  bbcOrigin: 'https://www.bbc.co.uk',
+};
 
 describe('Canonical ATI Analytics', () => {
+  beforeEach(() => {
+    useContext.mockReturnValue(contextToReturn);
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
     useContext.mockReset();
   });
 
+  const atiBaseUrl = 'https://foobar.com?';
   const mockPageviewParams = 'key=value&key2=value2';
-
-  // following tests are commented out so can debug them one at a time
-  const testCases = [
-    {
-      describe:
-        'should call sendBeacon with Live ATI URL for live env (.co.uk)',
-      env: 'live',
-      atiBaseUrl: 'https://a1.api.bbc.co.uk/hit.xiti?',
-      bbcOrigin: 'https://www.bbc.co.uk',
-    },
-    {
-      describe:
-        'should call sendBeacon with Live ATI URL for stage env (.co.uk)',
-      env: 'live',
-      atiBaseUrl: 'https://a1.api.bbc.co.uk/hit.xiti?',
-      bbcOrigin: 'https://www.stage.bbc.co.uk',
-    },
-    {
-      describe:
-        'should call sendBeacon with Test ATI URL for test env (.co.uk)',
-      env: 'test',
-      atiBaseUrl: 'https://logws1363.ati-host.net?',
-      bbcOrigin: 'https://www.test.bbc.co.uk',
-    },
-    {
-      describe:
-        'should call sendBeacon with Test ATI URL for local env (.co.uk)',
-      env: 'local',
-      atiBaseUrl: 'https://logws1363.ati-host.net?',
-      bbcOrigin: 'http://localhost.bbc.co.uk',
-    },
-    {
-      describe: 'should call sendBeacon with Live ATI URL for live env (.com)',
-      env: 'live',
-      atiBaseUrl: 'https://a1.api.bbc.co.uk/hit.xiti?',
-      bbcOrigin: 'https://www.bbc.com',
-    },
-    {
-      describe: 'should call sendBeacon with Live ATI URL for stage env (.com)',
-      env: 'live',
-      atiBaseUrl: 'https://a1.api.bbc.co.uk/hit.xiti?',
-      bbcOrigin: 'https://www.stage.bbc.com',
-    },
-    {
-      describe: 'should call sendBeacon with Test ATI URL for test env (.com)',
-      env: 'test',
-      atiBaseUrl: 'https://logws1363.ati-host.net?',
-      bbcOrigin: 'https://www.test.bbc.com',
-    },
-    {
-      describe: 'should call sendBeacon with Test ATI URL for local env (.com)',
-      env: 'local',
-      atiBaseUrl: 'https://logws1363.ati-host.net?',
-      bbcOrigin: 'http://localhost.bbc.com',
-    },
-  ];
-
   const mockSendBeacon = jest.fn().mockReturnValue('beacon-return-value');
+  const mockAtiBaseUrl = jest.fn().mockReturnValue(atiBaseUrl);
 
-  testCases.forEach(({ describe, env, atiBaseUrl, bbcOrigin }) => {
-    it(describe, () => {
-      const mockAtiBaseUrl = jest.fn().mockReturnValue(atiBaseUrl);
-      const expectedUrl = `${atiBaseUrl}${mockPageviewParams}`;
+  it('calls atiBaseURL and sendBeacon with required params', () => {
+    const expectedUrl = `${atiBaseUrl}${mockPageviewParams}`;
 
-      useContext.mockReturnValue(contextToReturn(bbcOrigin, env));
+    atiUrl.atiBaseUrl = mockAtiBaseUrl;
+    beacon.default = mockSendBeacon;
 
-      atiUrl.atiBaseUrl = mockAtiBaseUrl;
-      beacon.default = mockSendBeacon;
-
-      act(() => {
-        create(<CanonicalATIAnalytics pageviewParams={mockPageviewParams} />);
-      });
-
-      expect(mockAtiBaseUrl).toHaveBeenCalledWith(env);
-      expect(mockSendBeacon).toHaveBeenCalledTimes(1);
-      expect(mockSendBeacon).toHaveBeenCalledWith(expectedUrl);
+    act(() => {
+      create(<CanonicalATIAnalytics pageviewParams={mockPageviewParams} />);
     });
+
+    expect(mockAtiBaseUrl).toHaveBeenCalledWith('live');
+    expect(mockSendBeacon).toHaveBeenCalledTimes(1);
+    expect(mockSendBeacon).toHaveBeenCalledWith(expectedUrl);
   });
 
   it('should render a noscript image for non-JS users', () => {
-    useContext.mockReturnValue(contextToReturn());
-
     const renderedATI = render(
       <CanonicalATIAnalytics pageviewParams={mockPageviewParams} />,
     );
 
     expect(renderedATI.html()).toBe(
-      `<img height="1px" width="1px" alt="" src="https://logws1363.ati-host.net?key=value&amp;key2=value2"/>`,
+      `<img height="1px" width="1px" alt="" src="https://foobar.com?key=value&amp;key2=value2"/>`,
     );
   });
 
-  describe('snapshots', () => {
-    beforeEach(() => {
-      useContext.mockReturnValue(
-        contextToReturn('https://www.bbc.co.uk', 'live'),
-      );
-    });
-
-    shouldMatchSnapshot(
-      'should render correctly',
-      <CanonicalATIAnalytics pageviewParams={mockPageviewParams} />,
-    );
-  });
+  shouldMatchSnapshot(
+    'should render correctly',
+    <CanonicalATIAnalytics pageviewParams={mockPageviewParams} />,
+  );
 });
