621e8477b6219e2bb16f0914072d378b1e5d96e7
Chris Hinds <chris.hinds@bbc.co.uk>
2019-07-12 09:08:58 +0100

    Update test structure

--- src/app/containers/Toggle/toggleUtils.test.js
+++ src/app/containers/Toggle/toggleUtils.test.js
@@ -10,18 +10,18 @@ const fixtureData = {
 };
 
 describe('isToggleEnabled', () => {
-  it('should return the value for a valid toggle name', () => {
-    describe('toggle `foo` should return a value of true', () => {
+  describe('should return the value for a valid toggle name', () => {
+    test('toggle `foo` should return a value of true', () => {
       expect(isToggleEnabled('foo', fixtureData)).toEqual({ enabled: true });
     });
 
-    describe('toggle `bar` should return a value of false', () => {
+    test('toggle `bar` should return a value of false', () => {
       expect(isToggleEnabled('bar', fixtureData)).toEqual({ enabled: false });
     });
   });
 
   describe('should return a default of false if the toggle does not exist', () => {
-    it('toggle `fooBar` should return false', () => {
+    test('toggle `fooBar` should return false', () => {
       expect(isToggleEnabled('fooBar', fixtureData)).toEqual(false);
     });
   });
