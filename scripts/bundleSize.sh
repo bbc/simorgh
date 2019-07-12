479698cb1c3e9851fd4462607449e84dfdbd2d24
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2019-07-12 12:55:50 +0100

    Update snapshots

--- scripts/bundleSize.sh
+++ scripts/bundleSize.sh
@@ -3,7 +3,7 @@
 # Size limit for all bundles used by each service (K)
 # Keep these +/- 5K and update frequently!
 min=605
-max=623
+max=625
 
 services=( "afaanoromoo" "afrique" "amharic" "arabic" "azeri" "bengali" "burmese" "cymrufyw" "gahuza" "gujarati" "hausa" "hindi" "igbo" "indonesia" "japanese" "korean" "kyrgyz" "marathi" "mundo" "naidheachdan" "nepali" "news" "pashto" "persian" "pidgin" "portuguese" "punjabi" "russian" "serbian" "sinhala" "somali" "swahili" "tamil" "telugu" "thai" "tigrinya" "turkce" "ukchina" "ukrainian" "urdu" "uzbek" "vietnamese" "yoruba" "zhongwen" )
 failure=false
