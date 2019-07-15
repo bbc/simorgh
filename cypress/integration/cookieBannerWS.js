b8c75873b777b0a0a7125fb2856b29bd0aa2a84a
Einstein <forensicslabnet@gmail.com>
2019-07-15 16:03:52 +0300

    Re-enable cookie banner tests for test env

--- cypress/integration/cookieBannerWS.js
+++ cypress/integration/cookieBannerWS.js
@@ -1,6 +1,6 @@
 import { worldServiceCookieBannerTranslations } from '../support/bodyTestHelper';
 import describeForEuOnly from '../support/describeForEuOnly';
-import { describeForLocalOnly } from '../support/limitEnvRuns';
+import { describeForLocalAndTest } from '../support/limitEnvRuns';
 import services from '../../src/app/lib/config/services';
 
 Object.keys(services).forEach(index => {
@@ -12,9 +12,7 @@ Object.keys(services).forEach(index => {
     return;
   }
 
-  // These tests work locally, but fail on Test & Live environments since they have
-  // not yet been set up to have the correct translated cookie banners on the error pages
-  describeForLocalOnly('World Service Cookie banner Translations', () => {
+  describeForLocalAndTest('World Service Cookie banner Translations', () => {
     describe('Canonical', () => {
       it(`should load the relevant translations for ${service}`, () => {
         worldServiceCookieBannerTranslations(
