4c3a3839d51c8096c7a82343ae95bcaff0e1aba9
PriyaKR <Priyatharsini.kuduvarajan@bbc.co.uk>
2019-07-15 12:27:34 +0100

    updated the location of fontFamily in config

--- cypress/support/config/services.js
+++ cypress/support/config/services.js
@@ -1,22 +1,23 @@
 export default {
   igbo: {
+    fontFamily: undefined,
     pageTypes: {
       articles: undefined,
-      fontStartsWith: undefined,
       frontPage: '/igbo',
     },
   },
   news: {
+    fontFamily: 'Reith',
     pageTypes: {
       articles: {
         asset:
           Cypress.env('APP_ENV') === 'live' ? 'c5ll353v7y9o' : 'c6v11qzyv8po',
       },
-      fontStartsWith: 'Reith',
       frontPage: undefined,
     },
   },
   persian: {
+    fontFamily: 'Nassim',
     pageTypes: {
       articles:
         Cypress.env('APP_ENV') === 'live'
@@ -24,21 +25,20 @@ export default {
           : {
               asset: 'c4vlle3q337o',
             },
-      fontStartsWith: 'Nassim',      
       frontPage: undefined,
     },
   },
   pidgin: {
+    fontFamily: undefined,
     pageTypes: {
       articles: undefined,
-      fontStartsWith: undefined,
       frontPage: '/pidgin',
     },
   },
   yoruba: {
+    fontFamily: undefined,
     pageTypes: {
       articles: undefined,
-      fontStartsWith: undefined,
       frontPage: '/yoruba',
     },
   },
