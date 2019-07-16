08f7fb09792101e020d5513922282ed38a294643
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-07-16 16:46:13 +0100

    Don't SSR if react router doesn't know the URL
    This was breaking google cache and web.archive

--- src/client.js
+++ src/client.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-filename-extension  */
 import React from 'react';
+import { matchRoutes } from 'react-router-config';
 import Loadable from 'react-loadable';
 import { hydrate } from 'react-dom';
 import { ClientApp } from './app/containers/App';
@@ -9,9 +10,11 @@ import { template, templateStyles } from './app/lib/joinUsTemplate';
 const data = window.SIMORGH_DATA || {};
 const root = document.getElementById('root');
 
-Loadable.preloadReady().then(() => {
-  hydrate(<ClientApp data={data} routes={routes} />, root);
-});
+if (matchRoutes(routes, window.location.pathname)) {
+  Loadable.preloadReady().then(() => {
+    hydrate(<ClientApp data={data} routes={routes} />, root);
+  });
+}
 
 if (process.env.NODE_ENV === 'production') {
   // eslint-disable-next-line no-console
