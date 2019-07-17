2d1189d3853f839d94ffc8010da289055ab0715f
rowland <rowland.henshaw@andela.com>
2019-07-17 17:49:13 +0100

    setup ChartBeats container with props

--- src/app/containers/ChartbeatAnalytics/index.jsx
+++ src/app/containers/ChartbeatAnalytics/index.jsx
@@ -1,19 +1,47 @@
-import React from 'react';
-import { bool } from 'prop-types';
+import React, { useContext } from 'react';
 import AmpChartbeatsBeacon from './amp';
 import CanonicalChartbeatsBeacon from './canonical';
+import { ServiceContext } from '../../contexts/ServiceContext';
+import { RequestContext } from '../../contexts/RequestContext';
+import {
+  chartbeatUID,
+  useCanonical,
+  getSylphidCookie,
+  getDomain,
+  buildSections,
+  getType,
+} from '../../lib/analyticsUtils/chartbeat';
 
-const ChartbeatAnalyticsBeacon = ({ isAmp }) => {
-  const beacon = isAmp ? (
-    <AmpChartbeatsBeacon />
-  ) : (
-    <CanonicalChartbeatsBeacon />
-  );
+const ChartbeatAnalyticsBeacon = () => {
+  const { service } = useContext(ServiceContext);
+  const { env, platform, pageType } = useContext(RequestContext);
+  const domain =
+    env === 'test' || env === 'local' ? getDomain('test') : getDomain(service);
+
+  const sections = buildSections(service, pageType);
+  const cookie = getSylphidCookie();
+  const type = getType(pageType);
+
+  const beacon =
+    platform === 'amp' ? (
+      <AmpChartbeatsBeacon
+        domain={domain}
+        type={type}
+        sections={sections}
+        cookie={cookie}
+        chartbeatUID={chartbeatUID}
+      />
+    ) : (
+      <CanonicalChartbeatsBeacon
+        domain={domain}
+        type={type}
+        sections={sections}
+        cookie={cookie}
+        chartbeatUID={chartbeatUID}
+        useCanonical={useCanonical}
+      />
+    );
   return beacon;
-};
-
-ChartbeatAnalyticsBeacon.propTypes = {
-  isAmp: bool.isRequired,
 };
 
 export default ChartbeatAnalyticsBeacon;
