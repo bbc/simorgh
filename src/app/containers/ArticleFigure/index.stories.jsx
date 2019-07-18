9a7b3a6978457ac89138bb9f34958588a8e69536
Rachael Njeri <rachael.njeri@andela.com>
2019-07-18 18:33:04 +0300

    Fixes
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- src/app/containers/ArticleFigure/index.stories.jsx
+++ src/app/containers/ArticleFigure/index.stories.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { dirDecorator } from '@bbc/psammead-storybook-helpers';
