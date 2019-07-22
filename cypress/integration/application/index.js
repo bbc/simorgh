ac455ad9ac702656f40c944129506f5cee5e4f28
Benjamin Hobbs <bjhobbs06@gmail.com>
2019-07-22 14:32:35 +0100

    Run sw on all services and manifest only where it exists

--- cypress/integration/application/index.js
+++ cypress/integration/application/index.js
@@ -1,59 +1,28 @@
-import { describeForLocalOnly } from '../../support/limitEnvRuns';
+import config from '../../../src/app/lib/config/services';
 
 describe('Application', () => {
-  it('Service worker files should return a 200 status code for news', () => {
-    cy.testResponseCodeAndType(
-      `/news/articles/sw.js`,
-      200,
-      'application/javascript',
-    );
-  });
-
-  it('Manifest.json files should return a 200 status code for news', () => {
-    cy.testResponseCodeAndType(
-      `/news/articles/manifest.json`,
-      200,
-      'application/json',
-    );
-  });
-});
-
-describeForLocalOnly('Application - Local Env', () => {
-  it('Local Env - Service worker files hould return a 200 status code for news', () => {
-    cy.testResponseCodeAndType(
-      `/news/articles/sw.js`,
-      200,
-      'application/javascript',
-    );
-  });
-
-  it(`Local Env - Service worker files should return a 200 status code for igbo, pidgin and yoruba`, () => {
-    ['igbo', 'pidgin', 'yoruba'].forEach(service => {
-      it(`should return a 200 status code for ${service}`, () => {
-        cy.testResponseCodeAndType(
-          `/${service}/sw.js`,
-          200,
-          'application/javascript',
-        );
-      });
+  Object.keys(config).forEach(service => {
+    if (service === 'default') {
+      return;
+    }
+    // All services test sws
+    it(`should return a 200 status code for ${service}'s service worker`, () => {
+      cy.testResponseCodeAndType(
+        `/${service}/sw.js`,
+        200,
+        'application/javascript',
+      );
     });
   });
 
-  it('Local Env - Manifest.json files should return a 200 status code for persian, igbo, pidgin and yoruba without /articles', () => {
-    cy.testResponseCodeAndType(
-      `/persian/articles/manifest.json`,
-      200,
-      'application/json',
-    );
-
-    ['igbo', 'pidgin', 'yoruba'].forEach(service => {
-      it(`should return a 200 status code for ${service} without /articles`, () => {
-        cy.testResponseCodeAndType(
-          `/${service}/manifest.json`,
-          200,
-          'application/json',
-        );
-      });
+  // Once all manifest are done this should be move into the object forEach above
+  ['igbo', 'news/articles', 'pidgin', 'yoruba'].forEach(service => {
+    it(`should return a 200 status code for ${service} manifest file`, () => {
+      cy.testResponseCodeAndType(
+        `/${service}/manifest.json`,
+        200,
+        'application/json',
+      );
     });
   });
 });
