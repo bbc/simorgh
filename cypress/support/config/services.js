98c65d8daf77d9b8fd92c7696bd31f378c11a364
Olu Niyi-Awosusi <o.niyiawosusi@gmail.com>
2019-07-23 17:07:33 +0100

    add other services

--- cypress/support/config/services.js
+++ cypress/support/config/services.js
@@ -1,10 +1,98 @@
 export default {
+  afaanoromoo: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  afrique: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  amharic: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  arabic: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  azeri: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  bengali: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  burmese: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  cymrufyw: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  gahauza: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  gujarati: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  hausa: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  hindi: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
   igbo: {
     font: undefined,
     pageTypes: {
       articles: undefined,
       frontPage: '/igbo',
     },
+  },
+  index: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  indonesia: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  japanese: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  korean: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  kyrgyz: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  loadableConfig: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  marathi: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  mundo: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  naidheadchan: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  nepali: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
   },
   news: {
     font: 'Reith',
@@ -15,6 +103,10 @@ export default {
       },
       frontPage: undefined,
     },
+  },
+  pashto: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
   },
   persian: {
     font: 'Nassim',
@@ -35,11 +127,83 @@ export default {
       frontPage: '/pidgin',
     },
   },
+  portuguese: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  punjabi: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  russian: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  serbian: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  sinhala: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  somali: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  swahili: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  tamil: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  telugu: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  thai: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  tigrinya: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  turkce: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  ukchina: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  ukranian: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  urdu: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  uzbec: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
+  vietnamese: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
+  },
   yoruba: {
     font: undefined,
     pageTypes: {
       articles: undefined,
       frontPage: '/yoruba',
     },
+  },
+  zhongwen: {
+    font: undefined,
+    pageTypes: { articles: undefined, frontPage: undefined },
   },
 };
