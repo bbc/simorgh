3855f2485cbbc72a352f45ddda43b6644841b38b
Olu Niyi-Awosusi <o.niyiawosusi@gmail.com>
2019-07-23 12:35:18 +0100

    use filter

--- cypress/integration/application/index.js
+++ cypress/integration/application/index.js
@@ -2,19 +2,21 @@ import config from '../../../src/app/lib/config/services';
 import { describeForLocalOnly } from '../../support/limitEnvRuns';
 
 describeForLocalOnly('Application', () => {
-  Object.keys(config).forEach(service => {
-    if (service === 'default') {
-      return;
-    }
-    // All services test sws
-    it(`should return a 200 status code for ${service}'s service worker`, () => {
-      cy.testResponseCodeAndType(
-        `/${service}/sw.js`,
-        200,
-        'application/javascript',
-      );
+  Object.keys(config)
+    .filter(service => service !== 'default')
+    .forEach(service => {
+      if (service === 'default') {
+        return;
+      }
+      // All services test sws
+      it(`should return a 200 status code for ${service}'s service worker`, () => {
+        cy.testResponseCodeAndType(
+          `/${service}/sw.js`,
+          200,
+          'application/javascript',
+        );
+      });
     });
-  });
 });
 
 describe('Application', () => {
