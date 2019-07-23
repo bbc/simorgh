e33e880d78c3638fe38b90dec1330ba83cdc0b04
sadick <isaac.odhiambo@andela.com>
2019-07-23 15:11:35 +0300

    Refactor test

--- src/app/lib/utilities/describeDuration/index.test.js
+++ src/app/lib/utilities/describeDuration/index.test.js
@@ -3,7 +3,7 @@ import describeDuration from '.';
 
 const translationStub = {
   h: 'Saa {x}',
-  hh: 'Saa {x}',
+  hh: 'Masaa {x}',
   m: 'Dakika {x}',
   mm: 'Dakika {x}',
   s: 'Sekunde {x}',
@@ -14,40 +14,32 @@ const getDuration = seconds => {
   return moment.duration(seconds, 'seconds');
 };
 
-const minuteSingular = getDuration(60);
-const minutePlural = getDuration(120);
-const secondSingular = getDuration(1);
-const secondPlural = getDuration(20);
-const minuteSecond = getDuration(61);
-const hourMinuteSecond = getDuration(3700);
+const second = getDuration(1);
+const seconds = getDuration(20);
+const minute = getDuration(60);
+const minutes = getDuration(120);
+const hour = getDuration(3600);
+const hours = getDuration(7200);
+const day = moment.duration(25, 'hours');
 
 describe('describeDuration', () => {
   it('returns the correct duration description', () => {
-    expect(describeDuration(minuteSingular)).toEqual('1 minute');
-    expect(describeDuration(minutePlural)).toEqual('2 minutes');
-    expect(describeDuration(secondSingular)).toEqual('1 second');
-    expect(describeDuration(secondPlural)).toEqual('20 seconds');
-    expect(describeDuration(minuteSecond)).toEqual('1 minute 1 second');
-    expect(describeDuration(hourMinuteSecond)).toEqual(
-      '1 hour 1 minute 40 seconds',
-    );
+    expect(describeDuration(minute)).toEqual('1 minute');
+    expect(describeDuration(minutes)).toEqual('2 minutes');
+    expect(describeDuration(second)).toEqual('1 second');
+    expect(describeDuration(seconds)).toEqual('20 seconds');
+    expect(describeDuration(hour)).toEqual('1 hour');
+    expect(describeDuration(hours)).toEqual('2 hours');
+    expect(describeDuration(day)).toEqual('25 hours');
   });
+
   it('returns the correct duration description given translation', () => {
-    expect(describeDuration(minuteSingular, translationStub)).toEqual(
-      'Dakika 1',
-    );
-    expect(describeDuration(minutePlural, translationStub)).toEqual('Dakika 2');
-    expect(describeDuration(secondSingular, translationStub)).toEqual(
-      'Sekunde 1',
-    );
-    expect(describeDuration(secondPlural, translationStub)).toEqual(
-      'Sekunde 20',
-    );
-    expect(describeDuration(minuteSecond, translationStub)).toEqual(
-      'Dakika 1 Sekunde 1',
-    );
-    expect(describeDuration(hourMinuteSecond, translationStub)).toEqual(
-      'Saa 1 Dakika 1 Sekunde 40',
-    );
+    expect(describeDuration(minute, translationStub)).toEqual('Dakika 1');
+    expect(describeDuration(minutes, translationStub)).toEqual('Dakika 2');
+    expect(describeDuration(second, translationStub)).toEqual('Sekunde 1');
+    expect(describeDuration(seconds, translationStub)).toEqual('Sekunde 20');
+    expect(describeDuration(hour, translationStub)).toEqual('Saa 1');
+    expect(describeDuration(hours, translationStub)).toEqual('Masaa 2');
+    expect(describeDuration(day, translationStub)).toEqual('Masaa 25');
   });
 });
