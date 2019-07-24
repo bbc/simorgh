3ddc557c9d1d95855b26911ba699168063aa5f02
James Donohue <james.donohue@bbc.co.uk>
2019-07-24 09:52:26 +0100

    Helper to enable tests for only when toggle enabled

--- cypress/support/limitEnvRuns.js
+++ cypress/support/limitEnvRuns.js
@@ -1,17 +1,25 @@
-// Allow tests to skipped on live or test and live.
-// Used currently to stop persian tests running on
-// live due to them not yet being routed in the STMs
+// Allow tests to be only run on certain environments or if the appropriate
+// toggle is enabled for that environment
 
-// For use before routes are open in test env
+import defaultToggles from '../../lib/config/toggles';
+
+// For use when behaviour can only be tested locally
 export const describeForLocalOnly = (name, func) => {
   if (Cypress.env('APP_ENV') === 'local') {
     describe(name, func);
   }
 };
 
-// For use before routes are open in live env
+// For use when behaviour can be tested locally and always in the test env
 export const describeForLocalAndTest = (name, func) => {
   if (Cypress.env('APP_ENV') === 'local' || Cypress.env('APP_ENV') === 'test') {
+    describe(name, func);
+  }
+};
+
+// For use when testability of behaviour for each envionment is governed by a toggle
+export const describeIfToggleEnabled = (toggleName, name, func) => {
+  if (defaultToggles(Cypress.env('APP_ENV'))[toggleName]['enabled']) {
     describe(name, func);
   }
 };
