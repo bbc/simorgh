8bf35a50f5d674d3581880f5b8764cdb031916dd
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2019-07-24 10:55:57 +0100

    Make initialData unit test more specific

--- src/app/routes/getInitialData/mediapage/index.test.js
+++ src/app/routes/getInitialData/mediapage/index.test.js
@@ -1,7 +1,11 @@
 import getMediaPageInitialData from '.';
 
+const mockData = { service: 'amharic', status: 200 };
+
 describe('getMediaPageInitialData', () => {
   it('returns a promise', async () => {
-    expect(getMediaPageInitialData({ service: 'amharic' }).then).toBeDefined();
+    expect(await getMediaPageInitialData({ service: 'amharic' })).toEqual(
+      mockData,
+    );
   });
 });
