1b099e8a0862227deadbc5b1cdc1ed2b9187534f
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-07-25 12:09:07 +0100

    add explanation

--- src/client.js
+++ src/client.js
@@ -10,6 +10,10 @@ import { template, templateStyles } from './app/lib/joinUsTemplate';
 const data = window.SIMORGH_DATA || {};
 const root = document.getElementById('root');
 
+// Only hydrate the client if we're on a known route
+// When on an unknown route, the SSR will be discarded and the user will only
+// see a blank screen. This should never happen on BBC domains, but web page
+// archival/caching sites such as
 if (matchRoutes(routes, window.location.pathname).length > 0) {
   Loadable.preloadReady().then(() => {
     hydrate(<ClientApp data={data} routes={routes} />, root);
