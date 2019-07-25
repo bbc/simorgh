99ffaf2a4a74f4701e34abe0d4f6bd83da12b82c
Mostafa Zain <46642770+mbzain76@users.noreply.github.com>
2019-07-25 19:43:55 +0200

    Fix issue 570
    Remove the overwrite function which cause the bug because we can't use if statement for negative result here

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,3 @@
-// Overwriting Cypress Commands should very rarely be done.
-Cypress.Commands.overwrite('visit', (originalFn, url, options) => {
-  cy.request({ url, failOnStatusCode: false }).then(({ headers }) => {
-    // Always ensure we're not seeing the Mozart fallback
-    if (expect(headers).not.have.property('x-mfa')) {
-      return originalFn(url, options);
-    }
-
-    return false;
-  });
-});
 
 // Addding Cypress Commands is safe, add away...
 Cypress.Commands.add('testResponseCodeAndType', (path, responseCode, type) => {
