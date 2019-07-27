79f5a12fcbe835daefddf974af9683e84e970886
billy-reilly <billy_reilly@hotmail.co.uk>
2019-07-27 21:35:29 +0100

    fix MIN_SIZE

--- scripts/bundleSizeConfig.js
+++ scripts/bundleSizeConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Size limit for all bundles used by each service (K)
   // Keep these +/- 5K and update frequently!
-  MIN_SIZE: 509,
+  MIN_SIZE: 495,
   MAX_SIZE: 510,
   // Minimum number of service bundles that should be analysed
   MIN_SERVICES: 44,
