93b06d1227c40714188d53385df61674675f6db0
E Keane <egjkeane@gmail.com>
2019-07-30 09:49:25 +0100

    Update tests so script agrees with service

--- src/app/components/InlineSpan/index.test.jsx
+++ src/app/components/InlineSpan/index.test.jsx
@@ -5,15 +5,15 @@ import Inline from './index';
 
 describe('Inline', () => {
   shouldMatchSnapshot(
-    'should render a correctly',
-    <Inline language="en" script={latin} service="persian">
+    'english inline span in persian service article',
+    <Inline language="en" script={arabic} service="persian">
       This is text in a Inline.
     </Inline>,
   );
 
   shouldMatchSnapshot(
-    'should render correctly with persian content',
-    <Inline language="fa" script={arabic} service="news">
+    'persian inline span in english service article',
+    <Inline language="fa" script={latin} service="news">
       بعض محتوى النص
     </Inline>,
   );
