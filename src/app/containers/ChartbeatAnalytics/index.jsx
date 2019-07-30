a7bacf4fdfd2a51864731251af58d5b44171964c
rowland <rowland.henshaw@andela.com>
2019-07-30 15:58:44 +0100

    added computation for more analytics values in Chartbeat container

--- src/app/containers/ChartbeatAnalytics/index.jsx
+++ src/app/containers/ChartbeatAnalytics/index.jsx
@@ -1,9 +1,12 @@
 import React, { useContext } from 'react';
+import { oneOfType } from 'prop-types';
 import useToggle from '../Toggle/useToggle';
 import AmpChartbeatBeacon from './amp';
 import CanonicalChartbeatBeacon from './canonical';
 import { ServiceContext } from '../../contexts/ServiceContext';
 import { RequestContext } from '../../contexts/RequestContext';
+import { articleDataPropTypes } from '../../models/propTypes/article';
+import { frontPageDataPropTypes } from '../../models/propTypes/frontPage';
 import {
   chartbeatUID,
   chartbeatSource,
@@ -12,27 +15,39 @@ import {
   getDomain,
   buildSections,
   getType,
+  getTitle,
 } from '../../lib/analyticsUtils/chartbeat';
 
-const ChartbeatAnalytics = () => {
-  const { service } = useContext(ServiceContext);
-  const { env, platform, pageType } = useContext(RequestContext);
+import { getReferrer } from '../../lib/analyticsUtils';
+
+const ChartbeatAnalytics = ({ data }) => {
+  const { service, brandName } = useContext(ServiceContext);
+  const { env, platform, pageType, previousPath, origin } = useContext(
+    RequestContext,
+  );
   const { enabled } = useToggle('chartbeatAnalytics');
 
   if (!enabled) {
     return null;
   }
-
+  const referrer = getReferrer(platform, origin, previousPath);
+  const title = getTitle(pageType, data, brandName);
   const domain = env !== 'live' ? getDomain('test') : getDomain(service);
   const sections = buildSections(service, pageType);
   const cookie = getSylphidCookie();
   const type = getType(pageType);
+  const hasCookie = !!cookie;
+  const hasReferrer = !!referrer;
   const commonProps = {
     domain,
     type,
     sections,
     cookie,
     chartbeatUID,
+    title,
+    hasCookie,
+    referrer,
+    hasReferrer,
   };
   return platform === 'amp' ? (
     <AmpChartbeatBeacon {...commonProps} />
@@ -43,6 +58,10 @@ const ChartbeatAnalytics = () => {
       chartbeatSource={chartbeatSource}
     />
   );
+};
+
+ChartbeatAnalytics.propTypes = {
+  data: oneOfType([articleDataPropTypes, frontPageDataPropTypes]).isRequired,
 };
 
 export default ChartbeatAnalytics;
