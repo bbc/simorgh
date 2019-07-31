8b23d8022d375d6b4483415a3782b29f5adeb911
E Keane <egjkeane@gmail.com>
2019-07-31 15:42:34 +0100

    Prop types updates

--- src/app/models/propTypes/inline/index.js
+++ src/app/models/propTypes/inline/index.js
@@ -1,11 +1,15 @@
-import { string, arrayOf, oneOf, shape } from 'prop-types';
+import { string, arrayOf, oneOfType, shape } from 'prop-types';
 import fragmentBlockPropTypes from '../fragment';
+import { inlineLinkBlockPropTypes } from '../inlineLink';
+import { blockOfTypesAndModel } from '../general';
 
-const inlineBlockPropTypes = shape({
+const inlineBlockPropTypes = blockOfTypesAndModel(['inline'], {
   type: 'inline',
   model: shape({
     text: string.isRequired,
-    blocks: arrayOf(oneOf([fragmentBlockPropTypes])),
+    blocks: arrayOf(
+      oneOfType([fragmentBlockPropTypes, inlineLinkBlockPropTypes]),
+    ),
     language: string.isRequired,
   }),
 });
