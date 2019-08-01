d33cae34dfde37abcb0ee4ba623b2d3f3168fe26
Benjamin Hobbs <bjhobbs06@gmail.com>
2019-08-01 12:56:29 +0100

    Rmv one command

--- cypress/integration/metaArticleSpec.js
+++ cypress/integration/metaArticleSpec.js
@@ -97,7 +97,46 @@ Object.keys(services)
       });
 
       it('should include metadata that matches the JSON data', () => {
-        cy.checkArticleMetadata();
+        cy.window().then(win => {
+          cy.get('head').within(() => {
+            cy.get('meta[name="description"]').should(
+              'have.attr',
+              'content',
+              win.SIMORGH_DATA.pageData.promo.summary ||
+                win.SIMORGH_DATA.pageData.promo.headlines.seoHeadline,
+            );
+            cy.get('meta[name="og:title"]').should(
+              'have.attr',
+              'content',
+              win.SIMORGH_DATA.pageData.promo.headlines.seoHeadline,
+            );
+            cy.get('meta[name="og:type"]').should(
+              'have.attr',
+              'content',
+              win.SIMORGH_DATA.pageData.metadata.type,
+            );
+            cy.get('meta[name="article:published_time"]').should(
+              'have.attr',
+              'content',
+              new Date(
+                win.SIMORGH_DATA.pageData.metadata.firstPublished,
+              ).toISOString(),
+            );
+            cy.get('meta[name="article:modified_time"]').should(
+              'have.attr',
+              'content',
+              new Date(
+                win.SIMORGH_DATA.pageData.metadata.lastPublished,
+              ).toISOString(),
+            );
+          });
+
+          cy.get('html').should(
+            'have.attr',
+            'lang',
+            win.SIMORGH_DATA.pageData.metadata.passport.language,
+          );
+        });
       });
 
       it('should include the canonical URL & ampHTML', () => {
--- cypress/support/commands/metaData.js
+++ cypress/support/commands/metaData.js
@@ -17,49 +17,6 @@ Cypress.Commands.add(
   },
 );
 
-Cypress.Commands.add('checkArticleMetadata', () => {
-  cy.window().then(win => {
-    cy.get('head').within(() => {
-      cy.get('meta[name="description"]').should(
-        'have.attr',
-        'content',
-        win.SIMORGH_DATA.pageData.promo.summary ||
-          win.SIMORGH_DATA.pageData.promo.headlines.seoHeadline,
-      );
-      cy.get('meta[name="og:title"]').should(
-        'have.attr',
-        'content',
-        win.SIMORGH_DATA.pageData.promo.headlines.seoHeadline,
-      );
-      cy.get('meta[name="og:type"]').should(
-        'have.attr',
-        'content',
-        win.SIMORGH_DATA.pageData.metadata.type,
-      );
-      cy.get('meta[name="article:published_time"]').should(
-        'have.attr',
-        'content',
-        new Date(
-          win.SIMORGH_DATA.pageData.metadata.firstPublished,
-        ).toISOString(),
-      );
-      cy.get('meta[name="article:modified_time"]').should(
-        'have.attr',
-        'content',
-        new Date(
-          win.SIMORGH_DATA.pageData.metadata.lastPublished,
-        ).toISOString(),
-      );
-    });
-
-    cy.get('html').should(
-      'have.attr',
-      'lang',
-      win.SIMORGH_DATA.pageData.metadata.passport.language,
-    );
-  });
-});
-
 Cypress.Commands.add('checkAMPArticleMetadata', (AMPURL, win) => {
   // This will only work if you visit the matching canonical
   // url prior to running this.
