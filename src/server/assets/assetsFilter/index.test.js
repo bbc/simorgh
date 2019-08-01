0cedbe15fe34e76c424c75f05509db87b9be42e3
Benjamin Hobbs <bjhobbs06@gmail.com>
2019-08-01 22:15:45 +0100

    update all old localhosst refs

--- src/server/assets/assetsFilter/index.test.js
+++ src/server/assets/assetsFilter/index.test.js
@@ -8,21 +8,21 @@ describe('assetsFilter', () => {
 
     it('should order assets service bundle, vendor, main on local', () => {
       const input = [
-        'http://localhost:7080/static/js/yoruba-31ecd969.d4952cef.js',
-        'http://localhost:7080/static/js/main-d0ae3f07.e24ffe78.js',
-        'http://localhost:7080/static/js/vendor-f9ca8911.836b5376.js',
-        'http://localhost:7080/static/js/pidgin-31ecd969.652d66cc.js',
-        'http://localhost:7080/static/js/news-31ecd969.c141cfdc.js',
-        'http://localhost:7080/static/js/vendor-b23bnb22.nmn32mn2.js',
-        'http://localhost:7080/static/js/vendor-1f20a385.ff6d3f55.js',
+        'http://localhost.bbc.com:7080/static/js/yoruba-31ecd969.d4952cef.js',
+        'http://localhost.bbc.com:7080/static/js/main-d0ae3f07.e24ffe78.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-f9ca8911.836b5376.js',
+        'http://localhost.bbc.com:7080/static/js/pidgin-31ecd969.652d66cc.js',
+        'http://localhost.bbc.com:7080/static/js/news-31ecd969.c141cfdc.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-b23bnb22.nmn32mn2.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-1f20a385.ff6d3f55.js',
       ];
 
       const output = [
-        'http://localhost:7080/static/js/yoruba-31ecd969.d4952cef.js',
-        'http://localhost:7080/static/js/vendor-f9ca8911.836b5376.js',
-        'http://localhost:7080/static/js/vendor-b23bnb22.nmn32mn2.js',
-        'http://localhost:7080/static/js/vendor-1f20a385.ff6d3f55.js',
-        'http://localhost:7080/static/js/main-d0ae3f07.e24ffe78.js',
+        'http://localhost.bbc.com:7080/static/js/yoruba-31ecd969.d4952cef.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-f9ca8911.836b5376.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-b23bnb22.nmn32mn2.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-1f20a385.ff6d3f55.js',
+        'http://localhost.bbc.com:7080/static/js/main-d0ae3f07.e24ffe78.js',
       ];
 
       expect(assetsFilter(input, 'yoruba')).toEqual(output);
@@ -74,29 +74,29 @@ describe('assetsFilter', () => {
 
     it('should order assets correctly with multiple of each bundle type', () => {
       const input = [
-        'http://localhost:7080/static/js/yoruba-31ecd969.d4952cef.js',
-        'http://localhost:7080/static/js/main-bh32bjhb.jhjkh423.js',
-        'http://localhost:7080/static/js/vendor-f9ca8911.836b5376.js',
-        'http://localhost:7080/static/js/pidgin-31ecd969.652d66cc.js',
-        'http://localhost:7080/static/js/main-d0ae3f07.e24ffe78.js',
-        'http://localhost:7080/static/js/yoruba-q2eq22q2.awdawdaw.js',
-        'http://localhost:7080/static/js/main-n32bnb23.jk2k2jk2.js',
-        'http://localhost:7080/static/js/news-31ecd969.c141cfdc.js',
-        'http://localhost:7080/static/js/vendor-b23bnb22.nmn32mn2.js',
-        'http://localhost:7080/static/js/main-lkkl21k2.mklk2l1l.js',
-        'http://localhost:7080/static/js/vendor-1f20a385.ff6d3f55.js',
+        'http://localhost.bbc.com:7080/static/js/yoruba-31ecd969.d4952cef.js',
+        'http://localhost.bbc.com:7080/static/js/main-bh32bjhb.jhjkh423.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-f9ca8911.836b5376.js',
+        'http://localhost.bbc.com:7080/static/js/pidgin-31ecd969.652d66cc.js',
+        'http://localhost.bbc.com:7080/static/js/main-d0ae3f07.e24ffe78.js',
+        'http://localhost.bbc.com:7080/static/js/yoruba-q2eq22q2.awdawdaw.js',
+        'http://localhost.bbc.com:7080/static/js/main-n32bnb23.jk2k2jk2.js',
+        'http://localhost.bbc.com:7080/static/js/news-31ecd969.c141cfdc.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-b23bnb22.nmn32mn2.js',
+        'http://localhost.bbc.com:7080/static/js/main-lkkl21k2.mklk2l1l.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-1f20a385.ff6d3f55.js',
       ];
 
       const output = [
-        'http://localhost:7080/static/js/yoruba-31ecd969.d4952cef.js',
-        'http://localhost:7080/static/js/yoruba-q2eq22q2.awdawdaw.js',
-        'http://localhost:7080/static/js/vendor-f9ca8911.836b5376.js',
-        'http://localhost:7080/static/js/vendor-b23bnb22.nmn32mn2.js',
-        'http://localhost:7080/static/js/vendor-1f20a385.ff6d3f55.js',
-        'http://localhost:7080/static/js/main-bh32bjhb.jhjkh423.js',
-        'http://localhost:7080/static/js/main-d0ae3f07.e24ffe78.js',
-        'http://localhost:7080/static/js/main-n32bnb23.jk2k2jk2.js',
-        'http://localhost:7080/static/js/main-lkkl21k2.mklk2l1l.js',
+        'http://localhost.bbc.com:7080/static/js/yoruba-31ecd969.d4952cef.js',
+        'http://localhost.bbc.com:7080/static/js/yoruba-q2eq22q2.awdawdaw.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-f9ca8911.836b5376.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-b23bnb22.nmn32mn2.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-1f20a385.ff6d3f55.js',
+        'http://localhost.bbc.com:7080/static/js/main-bh32bjhb.jhjkh423.js',
+        'http://localhost.bbc.com:7080/static/js/main-d0ae3f07.e24ffe78.js',
+        'http://localhost.bbc.com:7080/static/js/main-n32bnb23.jk2k2jk2.js',
+        'http://localhost.bbc.com:7080/static/js/main-lkkl21k2.mklk2l1l.js',
       ];
 
       expect(assetsFilter(input, 'yoruba')).toEqual(output);
@@ -104,23 +104,23 @@ describe('assetsFilter', () => {
 
     it('should remove any duplicates', () => {
       const input = [
-        'http://localhost:7080/static/js/yoruba-31ecd969.d4952cef.js',
-        'http://localhost:7080/static/js/yoruba-31ecd969.d4952cef.js',
-        'http://localhost:7080/static/js/main-bh32bjhb.jhjkh423.js',
-        'http://localhost:7080/static/js/main-bh32bjhb.jhjkh423.js',
-        'http://localhost:7080/static/js/yoruba-31ecd969.d4952cef.js',
-        'http://localhost:7080/static/js/vendor-f9ca8911.836b5376.js',
-        'http://localhost:7080/static/js/vendor-f9ca8911.836b5376.js',
-        'http://localhost:7080/static/js/yoruba-31ecd969.d4952cef.js',
-        'http://localhost:7080/static/js/main-bh32bjhb.jhjkh423.js',
-        'http://localhost:7080/static/js/main-bh32bjhb.jhjkh423.js',
-        'http://localhost:7080/static/js/vendor-f9ca8911.836b5376.js',
+        'http://localhost.bbc.com:7080/static/js/yoruba-31ecd969.d4952cef.js',
+        'http://localhost.bbc.com:7080/static/js/yoruba-31ecd969.d4952cef.js',
+        'http://localhost.bbc.com:7080/static/js/main-bh32bjhb.jhjkh423.js',
+        'http://localhost.bbc.com:7080/static/js/main-bh32bjhb.jhjkh423.js',
+        'http://localhost.bbc.com:7080/static/js/yoruba-31ecd969.d4952cef.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-f9ca8911.836b5376.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-f9ca8911.836b5376.js',
+        'http://localhost.bbc.com:7080/static/js/yoruba-31ecd969.d4952cef.js',
+        'http://localhost.bbc.com:7080/static/js/main-bh32bjhb.jhjkh423.js',
+        'http://localhost.bbc.com:7080/static/js/main-bh32bjhb.jhjkh423.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-f9ca8911.836b5376.js',
       ];
 
       const output = [
-        'http://localhost:7080/static/js/yoruba-31ecd969.d4952cef.js',
-        'http://localhost:7080/static/js/vendor-f9ca8911.836b5376.js',
-        'http://localhost:7080/static/js/main-bh32bjhb.jhjkh423.js',
+        'http://localhost.bbc.com:7080/static/js/yoruba-31ecd969.d4952cef.js',
+        'http://localhost.bbc.com:7080/static/js/vendor-f9ca8911.836b5376.js',
+        'http://localhost.bbc.com:7080/static/js/main-bh32bjhb.jhjkh423.js',
       ];
 
       expect(assetsFilter(input, 'yoruba')).toEqual(output);
