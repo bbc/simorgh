9dc226b5ae9d197838e54a40a62e4f71f52c622b
sadickisaac <isaac.odhiambo@andela.com>
2019-08-05 13:53:30 +0300

    Revert "Fail on console warnings"
    This reverts commit fbd77b1bebd9a273571c5cb23898b2c8d679f982.

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -3,10 +3,3 @@ import './commands';
 Cypress.Screenshot.defaults({
   screenshotOnRunFailure: false,
 });
-
-Cypress.on(`window:before:load`, win => {
-  cy.stub(win.console, `error`, msg => {
-    cy.now(`task`, `error`, msg);
-    throw new Error(msg);
-  });
-});
