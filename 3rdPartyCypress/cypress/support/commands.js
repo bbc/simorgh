00b8bef2ea1fb4ba459abb670ead007e0ff954a1
Benjamin Hobbs <15235276+benjaminhobbs@users.noreply.github.com>
2019-08-06 20:18:23 +0100

    Typo correction

--- 3rdPartyCypress/cypress/support/commands.js
+++ 3rdPartyCypress/cypress/support/commands.js
@@ -1,4 +1,4 @@
-// These are deliberately duplicated from the other cypress folder to avoid interreltionships blocking changes.
+// These are deliberately duplicated from the other cypress folder to avoid interrelationships blocking changes.
 Cypress.Commands.add('testResponseCodeAndType', (path, responseCode, type) => {
   cy.request({ url: path, failOnStatusCode: false }).then(
     ({ status, headers }) => {
