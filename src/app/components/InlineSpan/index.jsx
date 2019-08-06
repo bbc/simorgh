e099386e798046710c26866468638211fc05d854
James Donohue <james.donohue@bbc.co.uk>
2019-08-06 19:40:52 +0100

    Remove apparently unneeded styles from InlineSpan

--- src/app/components/InlineSpan/index.jsx
+++ src/app/components/InlineSpan/index.jsx
@@ -1,23 +1,14 @@
 import styled from 'styled-components';
-import { shape, string } from 'prop-types';
-import { C_SHADOW } from '@bbc/psammead-styles/colours';
+import { string } from 'prop-types';
 import { GEL_SPACING_TRPL } from '@bbc/gel-foundations/spacings';
-import { getBodyCopy } from '@bbc/gel-foundations/typography';
-import { scriptPropType } from '@bbc/gel-foundations/prop-types';
-import { getSansRegular } from '@bbc/psammead-styles/font-styles';
 
 const InlineSpan = styled.span`
-  ${props => (props.script ? getBodyCopy(props.script) : '')};
-  color: ${C_SHADOW};
-  ${({ service }) => getSansRegular(service)}
   padding-bottom: ${GEL_SPACING_TRPL};
   margin: 0; /* Reset */
 `;
 
 InlineSpan.propTypes = {
   lang: string.isRequired,
-  script: shape(scriptPropType).isRequired,
-  service: string.isRequired,
 };
 
 export default InlineSpan;
