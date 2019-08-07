119f03f840f3eabfda5a3d0ba978099655449ee4
Benjamin Hobbs <bjhobbs06@gmail.com>
2019-08-07 22:18:47 +0100

    Tidied up

--- cypress/integration/pages/errorPages/index.js
+++ cypress/integration/pages/errorPages/index.js
@@ -3,13 +3,10 @@ import config from '../../../support/config/services';
 import iterator from './pagehelper';
 
 // Do not change the config to enable these on test or live, those error pages are cached versions of what we see locally.
-// const serviceHasErrorPage = service =>
-//   config[service].pageTypes.errorPage404 !== undefined;
-// const filter = filter(config[service].pageTypes.errorPage404, );
 const filterCondition = service =>
   config[service].pageTypes.errorPage404 !== undefined;
 
-const test = (service) => {
+const tests = service => {
   describe(`${service} Test we get a 404`, () => {
     it('should return a 404 error code', () => {
       cy.testResponseCodeAndType(
@@ -19,121 +16,59 @@ const test = (service) => {
       );
     });
   });
+  describe(`${service} Test we get a 404`, () => {
+    it('should return a 404 error code', () => {
+      cy.testResponseCodeAndType(
+        config[service].pageTypes.errorPage404,
+        404,
+        'text/html',
+      );
+    });
+  });
+
+  describe(`${service} Article Error Page Tests`, () => {
+    before(() => {
+      cy.visit(config[service].pageTypes.errorPage404, {
+        failOnStatusCode: false,
+      });
+    });
+
+    it('should have the correct lang & dir attributes', () => {
+      cy.hasHtmlLangDirAttributes({
+        lang: `${testData[service].lang}`,
+        dir: `${testData[service].dir}`,
+      });
+    });
+
+    it('should display a relevant error message on screen', () => {
+      cy.get('h1 span').should(
+        'contain',
+        `${testData[service].translations.error[404].statusCode}`,
+      );
+      cy.get('h1').should(
+        'contain',
+        `${testData[service].translations.error[404].title}`,
+      );
+    });
+
+    it('should have an inline link on the page that is linked to the home page', () => {
+      cy.get('main p')
+        .eq(1)
+        .within(() => {
+          cy.get('a').should(
+            'have.attr',
+            'href',
+            `${testData[service].translations.error[404].callToActionLinkUrl}`,
+          );
+        });
+    });
+
+    it('should have a relevant error title in the head', () => {
+      cy.renderedTitle(
+        `${testData[service].translations.error[404].title} - ${testData[service].brandName}`,
+      );
+    });
+  });
 };
 
-iterator(filterCondition, test);
-// Object.keys(config)
-//   .filter(serviceHasErrorPage)
-//   .forEach(service => {
-//     describe(`${service} Test we get a 404`, () => {
-//       it('should return a 404 error code', () => {
-//         cy.testResponseCodeAndType(
-//           config[service].pageTypes.errorPage404,
-//           404,
-//           'text/html',
-//         );
-//       });
-//     });
-
-//     describe(`${service} Article Error Page Tests`, () => {
-//       before(() => {
-//         cy.visit(config[service].pageTypes.errorPage404, {
-//           failOnStatusCode: false,
-//         });
-//       });
-
-//       it('should have the correct lang & dir attributes', () => {
-//         cy.hasHtmlLangDirAttributes({
-//           lang: `${testData[service].lang}`,
-//           dir: `${testData[service].dir}`,
-//         });
-//       });
-
-//       it('should display a relevant error message on screen', () => {
-//         cy.get('h1 span').should(
-//           'contain',
-//           `${testData[service].translations.error[404].statusCode}`,
-//         );
-//         cy.get('h1').should(
-//           'contain',
-//           `${testData[service].translations.error[404].title}`,
-//         );
-//       });
-
-//       it('should have an inline link on the page that is linked to the home page', () => {
-//         cy.get('main p')
-//           .eq(1)
-//           .within(() => {
-//             cy.get('a').should(
-//               'have.attr',
-//               'href',
-//               `${testData[service].translations.error[404].callToActionLinkUrl}`,
-//             );
-//           });
-//       });
-
-//       it('should have a relevant error title in the head', () => {
-//         cy.renderedTitle(
-//           `${testData[service].translations.error[404].title} - ${testData[service].brandName}`,
-//         );
-//       });
-//     });
-//   });
-
-  // Object.keys(config)
-  // .filter(serviceHasErrorPage)
-  // .forEach(service => {
-  //   describe(`${service} Test we get a 404`, () => {
-  //     it('should return a 404 error code', () => {
-  //       cy.testResponseCodeAndType(
-  //         config[service].pageTypes.errorPage404,
-  //         404,
-  //         'text/html',
-  //       );
-  //     });
-  //   });
-
-  //   describe(`${service} Article Error Page Tests`, () => {
-  //     before(() => {
-  //       cy.visit(config[service].pageTypes.errorPage404, {
-  //         failOnStatusCode: false,
-  //       });
-  //     });
-
-  //     it('should have the correct lang & dir attributes', () => {
-  //       cy.hasHtmlLangDirAttributes({
-  //         lang: `${testData[service].lang}`,
-  //         dir: `${testData[service].dir}`,
-  //       });
-  //     });
-
-  //     it('should display a relevant error message on screen', () => {
-  //       cy.get('h1 span').should(
-  //         'contain',
-  //         `${testData[service].translations.error[404].statusCode}`,
-  //       );
-  //       cy.get('h1').should(
-  //         'contain',
-  //         `${testData[service].translations.error[404].title}`,
-  //       );
-  //     });
-
-  //     it('should have an inline link on the page that is linked to the home page', () => {
-  //       cy.get('main p')
-  //         .eq(1)
-  //         .within(() => {
-  //           cy.get('a').should(
-  //             'have.attr',
-  //             'href',
-  //             `${testData[service].translations.error[404].callToActionLinkUrl}`,
-  //           );
-  //         });
-  //     });
-
-  //     it('should have a relevant error title in the head', () => {
-  //       cy.renderedTitle(
-  //         `${testData[service].translations.error[404].title} - ${testData[service].brandName}`,
-  //       );
-  //     });
-  //   });
-  // });
+iterator(filterCondition, tests);
