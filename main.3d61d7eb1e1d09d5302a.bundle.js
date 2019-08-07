4b8d97e075bde8fa44c71a2bcae9fe4ef8bdeeaf
BBC News CI <foo@bar.com>
2019-08-07 12:19:39 +0000

    Updates

---
+++ main.3d61d7eb1e1d09d5302a.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{380:function(n,o,p){p(381),p(484),n.exports=p(804)},484:function(n,o,p){"use strict";p.r(o);p(485)}},[[380,1,2]]]);
--- main.687314c721c4b1d0325d.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{383:function(n,o,p){p(384),p(487),n.exports=p(808)},487:function(n,o,p){"use strict";p.r(o);p(488)}},[[383,1,2]]]);
