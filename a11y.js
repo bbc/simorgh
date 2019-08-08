0c860b7cea0a96d8f7ed6df1a3728cd7a3db0e18
Einstein <forensicslabnet@gmail.com>
2019-08-08 15:09:07 +0300

    Run a11y on all services

--- a11y.js
+++ a11y.js
@@ -1,17 +1,21 @@
 const fs = require('fs');
 
-const getServiceNames = () => {
-  const dataDir = 'data';
-  return fs
-    .readdirSync(dataDir, { withFileTypes: true })
-    .filter(file => file.isDirectory())
-    .map(dir => dir.name);
-};
+const dataDir = `${__dirname}/data`;
+const dirNames = fs.readdirSync(dataDir);
+const getServiceDir = service => fs.readdirSync(`${dataDir}/${service}`);
+const getServiceNames = () =>
+  dirNames.filter(file => fs.statSync(`${dataDir}/${file}`).isDirectory());
+
+const serviceHasFrontPage = service =>
+  getServiceDir(service).find(file => file === 'frontpage');
 
 const pageWidths = [320, 400, 600, 1008, 1280];
 const baseUrl = 'http://localhost.bbc.com:7080/';
 const services = getServiceNames();
-const urls = services.map(service => `${baseUrl}${service}`);
+
+const urls = services
+  .filter(serviceHasFrontPage)
+  .map(service => `${baseUrl}${service}`);
 
 // Added to prevent false negatives from mPulse beacon
 // which creates iframe in document head
