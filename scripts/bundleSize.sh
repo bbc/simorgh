f016f550d0609c9350b229379b48a1d332abe381
thekp <khoa.phan@bbc.co.uk>
2019-08-08 21:18:01 +0100

    increase bundle size

--- scripts/bundleSize.sh
+++ scripts/bundleSize.sh
@@ -6,7 +6,7 @@
 # - The min value should be 5K less than the smallest service bundle and max should be 5K greater than the largest bundle
 # - Bundle sizes can be viewed by running a production build
 min=494
-max=522
+max=534
 
 
 services=( "afaanoromoo" "afrique" "amharic" "arabic" "azeri" "bengali" "burmese" "cymrufyw" "gahuza" "gujarati" "hausa" "hindi" "igbo" "indonesia" "japanese" "korean" "kyrgyz" "marathi" "mundo" "naidheachdan" "nepali" "news" "pashto" "persian" "pidgin" "portuguese" "punjabi" "russian" "serbian" "sinhala" "somali" "swahili" "tamil" "telugu" "thai" "tigrinya" "turkce" "ukchina" "ukrainian" "urdu" "uzbek" "vietnamese" "yoruba" "zhongwen" )
