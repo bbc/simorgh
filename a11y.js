ce35d533937491a1ac508633dc11b5579fa61efd
Einstein <forensicslabnet@gmail.com>
2019-08-08 15:46:39 +0300

    Run a11y on articles in all services

--- a11y.js
+++ a11y.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
+const path = require('path');
 
 const dataDir = `${__dirname}/data`;
 const dirNames = fs.readdirSync(dataDir);
 const getServiceDir = service => fs.readdirSync(`${dataDir}/${service}`);
 const getServiceNames = () =>
   dirNames.filter(file => fs.statSync(`${dataDir}/${file}`).isDirectory());
+
+const serviceHasArticles = service =>
+  getServiceDir(service).find(file => file === 'articles');
 
 const serviceHasFrontPage = service =>
   getServiceDir(service).find(file => file === 'frontpage');
@@ -13,9 +17,22 @@ const pageWidths = [320, 400, 600, 1008, 1280];
 const baseUrl = 'http://localhost.bbc.com:7080/';
 const services = getServiceNames();
 
-const urls = services
+const frontPagesUrls = services
   .filter(serviceHasFrontPage)
   .map(service => `${baseUrl}${service}`);
+
+const getArticleUrls = service => {
+  const files = fs.readdirSync(`${dataDir}/${service}/articles`);
+  const articleFile = files.find(file => !file.includes('0000'));
+  const article = path.basename(articleFile, path.extname(articleFile));
+  return `${baseUrl}${service}/articles/${article}`;
+};
+
+const articlePagesUrls = services
+  .filter(serviceHasArticles)
+  .map(getArticleUrls);
+
+const urls = [...frontPagesUrls, ...articlePagesUrls];
 
 // Added to prevent false negatives from mPulse beacon
 // which creates iframe in document head
