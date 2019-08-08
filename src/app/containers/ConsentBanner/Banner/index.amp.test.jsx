e0dcfe6e9ad074c19cd37ed9c72f293d92ed7f92
sareh <sareh.heidari@gmail.com>
2019-08-08 13:50:11 +0100

    Add snapshot tests for RTL consent banner layout

--- src/app/containers/ConsentBanner/Banner/index.amp.test.jsx
+++ src/app/containers/ConsentBanner/Banner/index.amp.test.jsx
@@ -5,7 +5,7 @@ import Banner from './index.amp';
 
 describe('Amp Consent Banner Container - Banner', () => {
   shouldMatchSnapshot(
-    'should correctly render privacy banner',
+    'should correctly render privacy banner - LTR layout',
     <ServiceContextProvider service="news">
       <Banner
         type="privacy"
@@ -18,8 +18,34 @@ describe('Amp Consent Banner Container - Banner', () => {
   );
 
   shouldMatchSnapshot(
-    'should correctly render cookie banner',
+    'should correctly render privacy banner - RTL layout',
+    <ServiceContextProvider service="arabic">
+      <Banner
+        type="privacy"
+        acceptAction="tap:cookieId.show, privacyId.hide"
+        rejectAction="tap:cookieId.show, privacyId.hide"
+        promptId="promptId"
+        hidden
+      />
+    </ServiceContextProvider>,
+  );
+
+  shouldMatchSnapshot(
+    'should correctly render cookie banner - LTR layout',
     <ServiceContextProvider service="news">
+      <Banner
+        type="cookie"
+        acceptAction="tap:parentId.accept"
+        rejectAction="tap:parentId.reject"
+        promptId="promptId"
+        hidden
+      />
+    </ServiceContextProvider>,
+  );
+
+  shouldMatchSnapshot(
+    'should correctly render cookie banner - RTL layout',
+    <ServiceContextProvider service="arabic">
       <Banner
         type="cookie"
         acceptAction="tap:parentId.accept"
--- src/app/containers/ConsentBanner/Banner/index.canonical.test.jsx
+++ src/app/containers/ConsentBanner/Banner/index.canonical.test.jsx
@@ -5,15 +5,29 @@ import Banner from './index.canonical';
 
 describe('Canonical Consent Banner Container - Banner', () => {
   shouldMatchSnapshot(
-    'should correctly render privacy banner',
+    'should correctly render privacy banner - LTR layout',
     <ServiceContextProvider service="news">
       <Banner type="privacy" onAccept={() => {}} onReject={() => {}} />
     </ServiceContextProvider>,
   );
 
   shouldMatchSnapshot(
-    'should correctly render cookie banner',
+    'should correctly render cookie banner - LTR layout',
     <ServiceContextProvider service="news">
+      <Banner type="cookie" onAccept={() => {}} onReject={() => {}} />
+    </ServiceContextProvider>,
+  );
+
+  shouldMatchSnapshot(
+    'should correctly render privacy banner - RTL layout',
+    <ServiceContextProvider service="arabic">
+      <Banner type="privacy" onAccept={() => {}} onReject={() => {}} />
+    </ServiceContextProvider>,
+  );
+
+  shouldMatchSnapshot(
+    'should correctly render cookie banner - RTL layout',
+    <ServiceContextProvider service="arabic">
       <Banner type="cookie" onAccept={() => {}} onReject={() => {}} />
     </ServiceContextProvider>,
   );
