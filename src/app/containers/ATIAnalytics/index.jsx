982d48f4d010e2d4fe3441ba7b7ec5e58fb3fdbd
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2019-08-09 15:05:41 +0100

    Refactor

--- src/app/containers/ATIAnalytics/index.jsx
+++ src/app/containers/ATIAnalytics/index.jsx
@@ -3,24 +3,23 @@ import { oneOfType } from 'prop-types';
 import { RequestContext } from '../../contexts/RequestContext';
 import CanonicalATIAnalytics from './canonical';
 import AmpATIAnalytics from './amp';
-import ArticleAtiParams from './params/article';
-import FrontPageAtiParams from './params/frontpage';
-import MediaPageAtiParams from './params/media';
+import getArticleAtiParams from './params/article';
+import getFrontPageAtiParams from './params/frontpage';
+import getMediaPageAtiParams from './params/media';
 import { articleDataPropTypes } from '../../models/propTypes/article';
 import { frontPageDataPropTypes } from '../../models/propTypes/frontPage';
 
 const ATIAnalytics = ({ data }) => {
   const { pageType, platform } = React.useContext(RequestContext);
 
-  // The functions that converts pageData to ATI params
   const pageTypeHandlers = {
-    article: ArticleAtiParams,
-    frontPage: FrontPageAtiParams,
-    media: MediaPageAtiParams,
+    article: getArticleAtiParams,
+    frontPage: getFrontPageAtiParams,
+    media: getMediaPageAtiParams,
   };
 
-  // Eject if the page type is not recognised
-  if (!Object.keys(pageTypeHandlers).includes(pageType)) {
+  const isValidPageType = Object.keys(pageTypeHandlers).includes(pageType)
+  if (!isValidPageType) {
     return null;
   }
 
