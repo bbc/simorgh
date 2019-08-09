ef95cb579cab2d648b5e339354cb709181d306af
Andrew Nowak <andrew.nowak@bbc.co.uk>
2019-08-09 09:25:52 +0100

    pull in story-promo-list@2

--- src/app/containers/FrontPageMain/index.jsx
+++ src/app/containers/FrontPageMain/index.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable jsx-a11y/aria-role */
 import React, { Fragment, useContext } from 'react';
+import styled from 'styled-components';
 import VisuallyHiddenText from '@bbc/psammead-visually-hidden-text';
+import { GEL_GROUP_2_SCREEN_WIDTH_MIN } from '@bbc/gel-foundations/breakpoints';
+import { GEL_SPACING, GEL_SPACING_DBL } from '@bbc/gel-foundations/spacings';
 import pathOr from 'ramda/src/pathOr';
 import { frontPageDataPropTypes } from '../../models/propTypes/frontPage';
 import { Grid, GridItemConstrainedLarge } from '../../lib/styledGrid';
@@ -8,6 +11,14 @@ import { ServiceContext } from '../../contexts/ServiceContext';
 import FrontPageSection from '../FrontPageSection';
 import MetadataContainer from '../Metadata';
 import ATIAnalytics from '../ATIAnalytics';
+
+const GridItemConstrainedLargeWithTopMargin = styled(GridItemConstrainedLarge)`
+  margin-top: ${GEL_SPACING};
+
+  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
+    margin-top: ${GEL_SPACING_DBL};
+  }
+`;
 
 const FrontPageMain = ({ frontPageData }) => {
   const { product, serviceLocalizedName, translations } = useContext(
@@ -36,7 +47,7 @@ const FrontPageMain = ({ frontPageData }) => {
           {offScreenText}
         </VisuallyHiddenText>
         <Grid>
-          <GridItemConstrainedLarge>
+          <GridItemConstrainedLargeWithTopMargin>
             {groups.map((group, index) => (
               <FrontPageSection
                 key={group.title}
@@ -44,7 +55,7 @@ const FrontPageMain = ({ frontPageData }) => {
                 sectionNumber={index}
               />
             ))}
-          </GridItemConstrainedLarge>
+          </GridItemConstrainedLargeWithTopMargin>
         </Grid>
       </main>
     </Fragment>
