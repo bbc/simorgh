af659de6e994fc94a9702bb623ddff631d4c8ae3
PriyaKR <Priyatharsini.kuduvarajan@bbc.co.uk>
2019-08-09 16:11:17 +0100

    added tests for header links

--- cypress/integration/pages/article/amp.js
+++ cypress/integration/pages/article/amp.js
@@ -166,7 +166,16 @@ Object.keys(config)
       });
 
       describe('Assert for broken links', () => {
-        it('should not contain any broken links - no 404s links', () => {
+        it('should not contain any broken header links - no 404s links', () => {
+          cy.get('header a').each(element => {
+            const href = element.attr('href');
+            console.log(href);
+            cy.request(href).then(resp => {
+              expect(resp.status).to.not.equal(404);
+            });
+          });
+        });
+        it('should not contain any broken footer links - no 404s links', () => {
           cy.get('footer a').each(element => {
             const href = element.attr('href');
             console.log(href);
