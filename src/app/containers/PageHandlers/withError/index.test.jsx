b61b78082b43cdae25d849e47485bf7d46b37c7a
thekp <khoa.phan@bbc.co.uk>
2019-08-12 17:52:19 +0100

    pass boolean, not string

--- src/app/containers/PageHandlers/withError/index.test.jsx
+++ src/app/containers/PageHandlers/withError/index.test.jsx
@@ -9,14 +9,14 @@ describe('withError HOC', () => {
   describe('with error', () => {
     shouldShallowMatchSnapshot(
       `should return the errorMain component`,
-      <ErrorHOC error="error handling at its best" />,
+      <ErrorHOC error />,
     );
   });
 
   describe('with no error', () => {
     shouldShallowMatchSnapshot(
       `should return the passed in component`,
-      <ErrorHOC error={null} />,
+      <ErrorHOC error={false} />,
     );
   });
 });
