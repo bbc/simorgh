45a433b7c6bbab6444d01e486b9600d7204625f8
James Brumpton <james.brumpton@bbc.co.uk>
2019-08-12 15:04:19 +0100

    Updating

--- cypress/integration/pages/article/canonical.js
+++ cypress/integration/pages/article/canonical.js
@@ -321,9 +321,8 @@ Object.keys(config)
 
           it('should have an image copyright label with styling', () => {
             cy.copyrightDataWindow();
-          })
+          });
         }
-        
 
         it('should render a title', () => {
           cy.window().then(win => {
