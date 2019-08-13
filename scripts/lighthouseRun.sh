e85dc132aed86fbc5ec0c2c8413d78e5b7dba722
Ho TIng Lok <53564281+lokh01@users.noreply.github.com>
2019-08-13 14:35:33 +0100

    Update package.json
    Co-Authored-By: Andrew Nowak <10963046+aacn500@users.noreply.github.com>
    
    Moved lighthouse command into ./scripts

---
+++ scripts/lighthouseRun.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+lighthouse http://localhost.bbc.com:7080/news/articles/c6v11qzyv8po --chrome-flags=\"--no-sandbox --headless --disable-gpu\" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run && lighthouse http://localhost.bbc.com:7080/igbo --chrome-flags=\"--no-sandbox --headless --disable-gpu\" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
