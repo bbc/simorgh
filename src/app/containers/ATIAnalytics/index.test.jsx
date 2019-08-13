388c10f2ecb47a0336875dc6616aaab019bacd5b
Fahad Khan <fk17abb@protonmail.com>
2019-08-13 17:12:08 +0100

    change pageType

--- src/app/containers/ATIAnalytics/index.test.jsx
+++ src/app/containers/ATIAnalytics/index.test.jsx
@@ -142,7 +142,7 @@ describe('ATI Analytics Container', () => {
   describe('pageType neither article nor frontPage', () => {
     isNull(
       'should render null',
-      <ContextWrap platform="canonical" pageType="randomvalue">
+      <ContextWrap platform="canonical" pageType="media">
         <ATIAnalytics data={articleDataNews} />
       </ContextWrap>,
     );
