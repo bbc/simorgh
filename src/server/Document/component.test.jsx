fea029849a5e6b1b45af7d5e973b45b6c8240777
Fahad Khan <fk17abb@protonmail.com>
2019-08-13 12:06:13 +0100

    Remove changes

--- src/server/Document/component.test.jsx
+++ src/server/Document/component.test.jsx
@@ -4,7 +4,6 @@ import DocumentComponent from './component';
 
 describe('Document Component', () => {
   const assets = ['http://example.com/file.js'];
-  const assetOrigins = ['http://example.com'];
   const data = { test: 'data' };
   const dials = { dial: true };
 
@@ -40,7 +39,6 @@ describe('Document Component', () => {
     shallowRender(
       <DocumentComponent
         assets={assets}
-        assetOrigins={assetOrigins}
         app={'<h1>App!</h1>'}
         data={{ ...data }}
         helmet={helmet}
