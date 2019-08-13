b54ec785c02dd11d2626dcf155fa5ca7482e47d5
Ho Ting Lok <hoting.lok@bbc.co.uk>
2019-08-13 15:35:11 +0100

    Updated package.json with lighthouse changes

--- scripts/lighthouseRun.sh
+++ scripts/lighthouseRun.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-lighthouse http://localhost.bbc.com:7080/news/articles/c6v11qzyv8po --chrome-flags=\"--no-sandbox --headless --disable-gpu\" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run && lighthouse http://localhost.bbc.com:7080/igbo --chrome-flags=\"--no-sandbox --headless --disable-gpu\" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
+npx -p lighthouse@5 lighthouse http://localhost.bbc.com:7080/news/articles/c6v11qzyv8po --chrome-flags="--no-sandbox --headless --disable-gpu" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
+npx -p lighthouse@5 lighthouse http://localhost.bbc.com:7080/igbo --chrome-flags="--no-sandbox --headless --disable-gpu" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
+
