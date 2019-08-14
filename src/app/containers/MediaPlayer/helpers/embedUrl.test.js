bf4229d069eecf02293b29da12b43b3ceaea5773
Rhys Hellyar <rhys.hellyar@bbc.co.uk>
2019-08-14 16:50:22 +0100

    DRY-up embedUrl tests

--- src/app/containers/MediaPlayer/helpers/embedUrl.test.js
+++ src/app/containers/MediaPlayer/helpers/embedUrl.test.js
@@ -1,90 +1,81 @@
 import embedUrl from './embedUrl';
 
-const ASSET_ID = 'foo';
-const VPID = 'bar';
+const assetId = 'foo';
+const vpid = 'bar';
 
-describe('Media Player: Embed URL', () => {
-  describe('Canonical', () => {
-    it('builds a URL for LIVE environment on .co.uk', () => {
-      const expectedUrl = `https://www.bbc.co.uk/ws/av-embeds/${ASSET_ID}/${VPID}`;
-      const embedObject = {
-        origin: 'https://www.bbc.co.uk',
-        assetId: ASSET_ID,
-        vpid: VPID,
-      };
+const testCases = [
+  {
+    description: 'CANONICAL: builds a URL for LIVE environment on .co.uk',
+    expected: `https://www.bbc.co.uk/ws/av-embeds/${assetId}/${vpid}`,
+    embedObject: {
+      origin: 'https://www.bbc.co.uk',
+      assetId,
+      vpid,
+    },
+  },
+  {
+    description: 'CANONICAL: builds a URL for TEST environment on .com',
+    expected: `https://www.test.bbc.com/ws/av-embeds/${assetId}/${vpid}`,
+    embedObject: {
+      origin: 'https://www.test.bbc.com',
+      assetId,
+      vpid,
+    },
+  },
+  {
+    description:
+      'CANONICAL: builds a URL for LOCAL environment that has a base of test.bbc.co.uk',
+    expected: `https://www.test.bbc.co.uk/ws/av-embeds/${assetId}/${vpid}`,
+    embedObject: {
+      origin: 'http://localhost.bbc.co.uk:7080',
+      assetId,
+      vpid,
+    },
+  },
+  {
+    description:
+      'CANONICAL: builds a URL for LOCAL environment that has a base of test.bbc.com',
+    expected: `https://www.test.bbc.com/ws/av-embeds/${assetId}/${vpid}`,
+    embedObject: {
+      origin: 'http://localhost.bbc.com:7080',
+      assetId,
+      vpid,
+    },
+  },
+  {
+    description: 'AMP: builds a URL for LIVE environment on .co.uk',
+    expected: `https://www.bbc.co.uk/ws/av-embeds/${assetId}/${vpid}/amp`,
+    embedObject: {
+      origin: 'https://www.bbc.co.uk',
+      amp: true,
+      assetId,
+      vpid,
+    },
+  },
+  {
+    description: 'AMP: builds a URL for TEST environment on .com',
+    expected: `https://www.test.bbc.com/ws/av-embeds/${assetId}/${vpid}/amp`,
+    embedObject: {
+      origin: 'https://www.test.bbc.com',
+      amp: true,
+      assetId,
+      vpid,
+    },
+  },
+  {
+    description:
+      'AMP: builds a URL for LOCAL environment that has a base of test.bbc.co.uk',
+    expected: `https://www.test.bbc.co.uk/ws/av-embeds/${assetId}/${vpid}/amp`,
+    embedObject: {
+      origin: 'http://localhost.bbc.co.uk:7080',
+      amp: true,
+      assetId,
+      vpid,
+    },
+  },
+];
 
-      expect(embedUrl(embedObject)).toEqual(expectedUrl);
-    });
-
-    it('builds a URL for TEST environment on .com', () => {
-      const expectedUrl = `https://www.test.bbc.com/ws/av-embeds/${ASSET_ID}/${VPID}`;
-      const embedObject = {
-        origin: 'https://www.test.bbc.com',
-        assetId: ASSET_ID,
-        vpid: VPID,
-      };
-
-      expect(embedUrl(embedObject)).toEqual(expectedUrl);
-    });
-
-    it('builds a URL for LOCAL environment that has a base of test.bbc.co.uk', () => {
-      const expectedUrl = `https://www.test.bbc.co.uk/ws/av-embeds/${ASSET_ID}/${VPID}`;
-      const embedObject = {
-        origin: 'http://localhost.bbc.co.uk:7080',
-        assetId: ASSET_ID,
-        vpid: VPID,
-      };
-
-      expect(embedUrl(embedObject)).toEqual(expectedUrl);
-    });
-
-    it('builds a URL for LOCAL environment that has a base of test.bbc.com', () => {
-      const expectedUrl = `https://www.test.bbc.com/ws/av-embeds/${ASSET_ID}/${VPID}`;
-      const embedObject = {
-        origin: 'https://www.localhost.bbc.com:7080',
-        assetId: ASSET_ID,
-        vpid: VPID,
-      };
-
-      expect(embedUrl(embedObject)).toEqual(expectedUrl);
-    });
-  });
-
-  describe('AMP', () => {
-    it('builds a URL for LIVE environment on .co.uk', () => {
-      const expectedUrl = `https://www.bbc.co.uk/ws/av-embeds/${ASSET_ID}/${VPID}/amp`;
-      const embedObject = {
-        origin: 'https://www.bbc.co.uk',
-        assetId: ASSET_ID,
-        vpid: VPID,
-        amp: true,
-      };
-
-      expect(embedUrl(embedObject)).toEqual(expectedUrl);
-    });
-
-    it('builds a URL for TEST environment on .com', () => {
-      const expectedUrl = `https://www.test.bbc.com/ws/av-embeds/${ASSET_ID}/${VPID}/amp`;
-      const embedObject = {
-        origin: 'https://www.test.bbc.com',
-        assetId: ASSET_ID,
-        vpid: VPID,
-        amp: true,
-      };
-
-      expect(embedUrl(embedObject)).toEqual(expectedUrl);
-    });
-
-    it('builds a URL for LOCAL environment that has a base of test.bbc.co.uk', () => {
-      const expectedUrl = `https://www.test.bbc.co.uk/ws/av-embeds/${ASSET_ID}/${VPID}/amp`;
-      const embedObject = {
-        origin: 'http://localhost.bbc.co.uk:7080',
-        assetId: ASSET_ID,
-        vpid: VPID,
-        amp: true,
-      };
-
-      expect(embedUrl(embedObject)).toEqual(expectedUrl);
-    });
-  });
-});
+describe('Media Player: Embed URL', () =>
+  testCases.forEach(({ description, expected, embedObject }) =>
+    it(description, () => expect(embedUrl(embedObject)).toEqual(expected)),
+  ));
