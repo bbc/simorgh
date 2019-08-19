e7af4f81e97bcd1e08c305924b75aa93dec681ac
Sareh Heidari <sareh@users.noreply.github.com>
2019-08-19 11:35:38 +0100

    Use navOnArticles toggle within the Header container

--- src/app/containers/Header/index.jsx
+++ src/app/containers/Header/index.jsx
@@ -3,16 +3,20 @@ import BrandContainer from '../Brand';
 import NavigationContainer from '../Navigation';
 import { RequestContext } from '../../contexts/RequestContext';
 import ConsentBanner from '../ConsentBanner';
+import useToggle from '../Toggle/useToggle';
 
 const HeaderContainer = () => {
   const { pageType } = useContext(RequestContext);
   const borderBottom = pageType !== 'frontPage';
+  const { enabled } = useToggle('navOnArticles');
+  const showNavBar =
+    ['frontPage', 'media', 'error'].includes(pageType) || enabled;
 
   return (
     <header role="banner">
       <ConsentBanner />
       <BrandContainer borderBottom={borderBottom} />
-      <NavigationContainer />
+      {showNavBar && <NavigationContainer />}
     </header>
   );
 };
