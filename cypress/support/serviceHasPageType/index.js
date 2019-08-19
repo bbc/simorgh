ba57d1114f59289fad984e66fc9e2fbbaccbd81b
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2019-08-19 11:23:42 +0100

    refactor serviceHasPageType

--- cypress/support/serviceHasPageType/index.js
+++ cypress/support/serviceHasPageType/index.js
@@ -1,17 +1,11 @@
+import curry from 'ramda/src/curry';
 import pathSatisfies from 'ramda/src/pathSatisfies';
 import servicesConfig from '../config/services';
 
-const serviceHasPageType = pageType => service =>
+export default curry((pageType, service) =>
   pathSatisfies(
     Boolean,
     [service, 'pageTypes', pageType, 'path'],
     servicesConfig,
-  );
-
-export const hasFrontPage = serviceHasPageType('frontPage');
-
-export const hasArticlePage = serviceHasPageType('articles');
-
-export const hasLiveRadioPage = serviceHasPageType('liveRadio');
-
-export const hasErrorPage = serviceHasPageType('errorPage404');
+  ),
+);
