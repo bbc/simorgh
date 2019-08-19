738946a18a3b413e270a4226de4836fdcdfce48c
Harvey Peachey <harvey.peachey@bbc.co.uk>
2019-08-19 11:18:04 +0100

    Added and amended logic for all destination cases

--- src/app/contexts/RequestContext/getStatsDestination/index.js
+++ src/app/contexts/RequestContext/getStatsDestination/index.js
@@ -2,11 +2,14 @@ const getStatsDestination = ({ isUK = true, env = 'test', service }) => {
   let destination = '';
 
   if (service === 'news') {
-    destination = isUK !== false ? 'NEWS_PS' : 'NEWS_GNL';
+    destination = isUK !== false ? 'PS_NEWS' : 'GNL_NEWS';
+  } else if (service === 'cymrufyw' || service === 'naidheachdan') {
+    destination = isUK !== false ? 'PS_NEWS_LANGUAGES' : 'GNL_NEWS_LANGUAGES';
+  } else if (service === 'japanese') {
+    destination = 'GNL_NEWS_LANGUAGES';
   } else {
     destination = 'WS_NEWS_LANGUAGES';
   }
-
   return env === 'live' ? destination : `${destination}_TEST`;
 };
 
