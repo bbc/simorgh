459de09c75b493b5c06a6027e692326037b11546
Ho Ting Lok <hoting.lok@bbc.co.uk>
2019-08-20 14:03:25 +0100

    Fixed react warnings

--- src/app/components/InlineSpan/index.test.jsx
+++ src/app/components/InlineSpan/index.test.jsx
@@ -6,14 +6,14 @@ import Inline from './index';
 describe('Inline', () => {
   shouldMatchSnapshot(
     'english inline span in persian service article',
-    <Inline language="en" script={arabic} service="persian">
+    <Inline lang="en" script={arabic} service="persian">
       This is text in a Inline.
     </Inline>,
   );
 
   shouldMatchSnapshot(
     'persian inline span in english service article',
-    <Inline language="fa" script={latin} service="news">
+    <Inline lang="fa" script={latin} service="news">
       بعض محتوى النص
     </Inline>,
   );
