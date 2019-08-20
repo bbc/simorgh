d2e99333442cef2ce8d50c2e10db7c8e754ba91d
Harvey Peachey <harvey.peachey@bbc.co.uk>
2019-08-20 14:46:49 +0100

    Changed to switch statement for logic to see if it reduces complexity

--- src/app/contexts/RequestContext/getStatsDestination/index.js
+++ src/app/contexts/RequestContext/getStatsDestination/index.js
@@ -1,14 +1,18 @@
 const getStatsDestination = ({ isUK = true, env = 'test', service }) => {
   let destination = '';
-
-  if (service === 'news') {
-    destination = isUK !== false ? 'NEWS_PS' : 'NEWS_GNL';
-  } else if (service === 'cymrufyw' || service === 'naidheachdan') {
-    destination = isUK !== false ? 'NEWS_LANGUAGES_PS' : 'NEWS_LANGUAGES_GNL';
-  } else if (service === 'japanese') {
-    destination = 'NEWS_LANGUAGES_GNL';
-  } else {
-    destination = 'WS_NEWS_LANGUAGES';
+  switch (service) {
+    case 'news':
+      destination = isUK !== false ? 'NEWS_PS' : 'NEWS_GNL';
+      break;
+    case 'cymrufyw':
+    case 'naidheachdan':
+      destination = isUK !== false ? 'NEWS_LANGUAGES_PS' : 'NEWS_LANGUAGES_GNL';
+      break;
+    case 'japanese':
+      destination = 'NEWS_LANGUAGES_GNL';
+      break;
+    default:
+      destination = 'WS_NEWS_LANGUAGES';
   }
   return env === 'live' ? destination : `${destination}_TEST`;
 };
