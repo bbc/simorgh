736eb5b45eb5f84366777ca619a30c50ad11b54a
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2019-08-21 13:06:44 +0100

    remove currying from serviceHasPageType

--- cypress/support/serviceHasPageType/index.js
+++ cypress/support/serviceHasPageType/index.js
@@ -1,11 +1,9 @@
-import curry from 'ramda/src/curry';
 import pathSatisfies from 'ramda/src/pathSatisfies';
 import servicesConfig from '../config/services';
 
-export default curry((pageType, service) =>
+export default (pageType, service) =>
   pathSatisfies(
     Boolean,
     [service, 'pageTypes', pageType, 'path'],
     servicesConfig,
-  ),
-);
+  );
