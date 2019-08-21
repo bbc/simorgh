df2ed6d52ef870a8296ff086b26ca8af8ee72e74
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2019-08-21 10:06:54 +0100

    add currying back in to shouldSmokeTest

--- cypress/support/shouldSmokeTest/index.js
+++ cypress/support/shouldSmokeTest/index.js
@@ -1,9 +1,11 @@
+import curry from 'ramda/src/curry';
 import path from 'ramda/src/path';
 import config from '../config/services';
 
 const DEFAULT_SMOKE_VALUE = true;
 
-export default pageType => service =>
+export default curry((pageType, service) =>
   Cypress.env('SMOKE')
     ? path([service, 'pageTypes', pageType, 'smoke'], config)
-    : DEFAULT_SMOKE_VALUE;
+    : DEFAULT_SMOKE_VALUE,
+);
