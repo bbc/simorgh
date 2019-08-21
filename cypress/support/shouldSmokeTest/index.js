96389b058057a2967b206911432d8a1116ca1110
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2019-08-21 09:32:55 +0100

    make shouldSmokeTest only a curried function

--- cypress/support/shouldSmokeTest/index.js
+++ cypress/support/shouldSmokeTest/index.js
@@ -1,11 +1,9 @@
-import curry from 'ramda/src/curry';
 import path from 'ramda/src/path';
 import config from '../config/services';
 
 const DEFAULT_SMOKE_VALUE = true;
 
-export default curry((pageType, service) =>
+export default pageType => service =>
   Cypress.env('SMOKE')
     ? path([service, 'pageTypes', pageType, 'smoke'], config)
-    : DEFAULT_SMOKE_VALUE,
-);
+    : DEFAULT_SMOKE_VALUE;
