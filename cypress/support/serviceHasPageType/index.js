ae257dab6b3cb7846cab377944056db26157b128
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2019-08-22 08:51:35 +0100

    correc t serviceHasPageType arg order

--- cypress/support/serviceHasPageType/index.js
+++ cypress/support/serviceHasPageType/index.js
@@ -1,7 +1,7 @@
 import pathSatisfies from 'ramda/src/pathSatisfies';
 import servicesConfig from '../config/services';
 
-export default (pageType, service) =>
+export default (service, pageType) =>
   pathSatisfies(
     Boolean,
     [service, 'pageTypes', pageType, 'path'],
--- cypress/support/serviceHasPageType/index.test.js
+++ cypress/support/serviceHasPageType/index.test.js
@@ -2,11 +2,11 @@ import serviceHasPageType from '.';
 
 describe('serviceHasPageType', () => {
   it('should return true when a service has a page type', () => {
-    expect(serviceHasPageType('frontPage', 'afaanoromoo')).toBe(true);
+    expect(serviceHasPageType('afaanoromoo', 'frontPage')).toBe(true);
   });
 
   it('should return false when a service does not have a page type', () => {
-    expect(serviceHasPageType('frontPage', 'dummyService')).toBe(false);
-    expect(serviceHasPageType('dummyPage', 'dummyService')).toBe(false);
+    expect(serviceHasPageType('dummyService', 'frontPage')).toBe(false);
+    expect(serviceHasPageType('dummyService', 'dummyPage')).toBe(false);
   });
 });
