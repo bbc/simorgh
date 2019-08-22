0d9a08ccab145259f0bb273a3daf76d1d322a173
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-08-22 10:04:00 +0100

    :white_check_mark: UserContext test

--- src/app/contexts/UserContext/index.test.jsx
+++ src/app/contexts/UserContext/index.test.jsx
@@ -1,1 +1,57 @@
 // TODO
+import React, { useContext, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { UserContext, UserContextProvider } from '.';
+import { getCookiePolicy, personalisationEnabled } from './cookies';
+
+jest.mock('react', () => {
+  const original = jest.requireActual('react');
+  return {
+    ...original,
+    useContext: jest.fn().mockImplementation(original.useContext),
+  };
+});
+
+jest.mock('./cookies', () => ({
+  getCookiePolicy: jest.fn(),
+  personalisationEnabled: jest.fn(),
+}));
+
+let DummyComponent = () => {
+  useContext(UserContext);
+  return null;
+};
+
+const DummyComponentWithContext = () => (
+  <UserContextProvider>
+    <DummyComponent />
+  </UserContextProvider>
+);
+
+let container;
+
+describe('UserContext', () => {
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    getCookiePolicy.mockReturnValue('111');
+    personalisationEnabled.mockReturnValue(true);
+    jest.clearAllMocks();
+  });
+
+  it('should', () => {
+    act(() => {
+      ReactDOM.render(<DummyComponentWithContext />, container);
+    });
+
+    expect(personalisationEnabled).toHaveBeenCalledWith('111');
+
+    expect(React.useContext).toHaveBeenCalledTimes(1);
+    expect(React.useContext).toHaveReturnedWith({
+      cookiePolicy: '111',
+      personalisationEnabled: true,
+      updateCookiePolicy: expect.any(Function),
+    });
+  });
+});
