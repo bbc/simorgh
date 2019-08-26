16048bf91207d72d413cfa9708b5a51e35b7e5b2
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-08-26 20:06:07 +0100

    :white_check_mark: Tests WIP

--- src/app/containers/PageHandlers/withPageWrapper/index.test.jsx
+++ src/app/containers/PageHandlers/withPageWrapper/index.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '../../../../testHelpers';
 import { ServiceContextProvider } from '../../../contexts/ServiceContext';
+import { UserContextProvider } from '../../../contexts/UserContext';
 import WithPageWrapper from '.';
 
 const dataProps = {
@@ -9,13 +10,20 @@ const dataProps = {
   route: { pageType: 'article' },
 };
 
+// eslint-disable-next-line react/prop-types
+jest.mock('../../../Layouts/defaultPageWrapper', () => ({ children }) => (
+  <div id="defaultPageWrapper">{children}</div>
+));
+
 describe('with pageWrapper', () => {
   const PageWrapperContainer = () => <h1>Holla</h1>;
   const PageWrapperHOC = WithPageWrapper(PageWrapperContainer);
   shouldMatchSnapshot(
     `should render correctly`,
     <ServiceContextProvider service="news">
-      <PageWrapperHOC {...dataProps} />,
+      <UserContextProvider>
+        <PageWrapperHOC {...dataProps} />
+      </UserContextProvider>
     </ServiceContextProvider>,
   );
 });
