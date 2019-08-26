12868addeb5bd165556ff545b583c70995d3e26d
Benjamin Hobbs <bjhobbs06@gmail.com>
2019-08-26 17:18:51 +0100

    Stop running tests we don't use in CI

--- scripts/lighthouseConfig.js
+++ scripts/lighthouseConfig.js
@@ -1,9 +1,7 @@
 module.exports = {
-    extends: 'lighthouse:default',
-    settings: {
-      skipAudits: [
-        'is-on-https',
-      ],
-    },
-  };
-  
+  extends: 'lighthouse:default',
+  settings: {
+    onlyCategories: ['ally', 'bestPractises', 'seo'],
+    skipAudits: ['is-on-https'],
+  },
+};
