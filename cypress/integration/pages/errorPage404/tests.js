18cbcbcbadbe39117ca15f500aae5283911c950a
Sareh Heidari <sareh@users.noreply.github.com>
2019-08-30 08:13:03 +0100

    Fix typo

--- cypress/integration/pages/errorPage404/tests.js
+++ cypress/integration/pages/errorPage404/tests.js
@@ -7,7 +7,7 @@ export const testsThatAlwaysRun = ({ service, pageType }) => {
   describe(`No testsToAlwaysRun to run for ${service} ${pageType}`, () => {});
 };
 
-// For testing feastures that may differ across services but share a common logic e.g. translated strings.
+// For testing features that may differ across services but share a common logic e.g. translated strings.
 export const testsThatFollowSmokeTestConfig = ({ service, pageType }) =>
   describe(`Tests for ${service} ${pageType}`, () => {
     describe(`${service} Test we get a 404`, () => {
