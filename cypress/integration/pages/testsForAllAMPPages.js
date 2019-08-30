a34a9ff6f7b7fabcf0479baa545848f533d461d4
Harvey Peachey <harvey.peachey@bbc.co.uk>
2019-08-30 14:45:59 +0100

    Implemented more efficient logic as pointed out in code review of the PR

--- cypress/integration/pages/testsForAllAMPPages.js
+++ cypress/integration/pages/testsForAllAMPPages.js
@@ -2,6 +2,8 @@ import envConfig from '../../support/config/envs';
 import config from '../../support/config/services';
 
 const serviceIsGNL = service => service === 'japanese';
+const serviceIsWS = service => service === 'persian';
+const serviceIsPS = service => service === 'news';
 
 // For testing important features that differ between services, e.g. Timestamps.
 // We recommend using inline conditional logic to limit tests to services which differ.
@@ -19,23 +21,45 @@ export const testsThatFollowSmokeTestConfigForAllAMPPages = ({
       it('should have an AMP attribute on the page', () => {
         cy.get('html').should('have.attr', 'amp');
       });
-      describe('ATI', () => {
-        it('should have an amp-analytics tag with the ati url', () => {
-          if (service === serviceIsGNL) {
+      if (Cypress.env('SMOKE')) {
+        describe('ATI', () => {
+          it('should have an amp-analytics tag with the ati url smoke', () => {
+            if (serviceIsGNL(service)) {
+              cy.hasAmpAnalyticsAtiUrl(
+                envConfig.atiUrl,
+                23453464573456435634563456,
+              );
+            } else if (serviceIsWS(service)) {
+              cy.hasAmpAnalyticsAtiUrl(
+                envConfig.atiUrl,
+                envConfig.atiAnalyticsWSBucket,
+              );
+            } else if (serviceIsPS(service)) {
+              cy.hasAmpAnalyticsAtiUrl(envConfig.atiUrl, '');
+            }
+          });
+        });
+      } else if (serviceIsGNL(service)) {
+        describe('ATI', () => {
+          it('should have an amp-analytics tag with the ati url', () => {
             cy.hasAmpAnalyticsAtiUrl(
               envConfig.atiUrl,
               envConfig.atiAnalyticsGNLBucket,
             );
-          } else if (config[service].isWorldService) {
-            cy.hasAmpAnalyticsAtiUrl(
-              envConfig.atiUrl,
-              envConfig.atiAnalyticsWSBucket,
-            );
-          } else {
-            cy.hasAmpAnalyticsAtiUrl(envConfig.atiUrl, '');
-          }
+          });
         });
-      });
+      } else if (config[service].isWorldService) {
+        it('should have an amp-analytics tag with the ati url', () => {
+          cy.hasAmpAnalyticsAtiUrl(
+            envConfig.atiUrl,
+            envConfig.atiAnalyticsWSBucket,
+          );
+        });
+      } else {
+        it('should have an amp-analytics tag with the ati url', () => {
+          cy.hasAmpAnalyticsAtiUrl(envConfig.atiUrl, '');
+        });
+      }
     }
   });
 };
