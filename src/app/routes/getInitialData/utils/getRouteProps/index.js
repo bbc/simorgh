e6b74729e8cf2c9a1feddc0240db25a4651e951d
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-09-02 09:40:03 +0100

    :bath: Better use ramda

--- src/app/routes/getInitialData/utils/getRouteProps/index.js
+++ src/app/routes/getInitialData/utils/getRouteProps/index.js
@@ -1,17 +1,18 @@
 import { matchRoutes } from 'react-router-config';
+import path from 'ramda/src/path';
 import pathOr from 'ramda/src/pathOr';
 import { fallbackAmpParam, fallbackServiceParam } from './routeFallbackParams';
 
 const getRouteProps = (routes, url) => {
   const matchedRoutes = matchRoutes(routes, url);
 
-  const route = pathOr(undefined, [0, 'route'], matchedRoutes);
-  const match = pathOr(undefined, [0, 'match'], matchedRoutes);
+  const route = path([0, 'route'], matchedRoutes);
+  const match = path([0, 'match'], matchedRoutes);
   const params = pathOr({}, ['params'], match);
 
-  const amp = pathOr(undefined, ['amp'], params);
-  const service = pathOr(undefined, ['service'], params);
-  const id = pathOr(undefined, ['id'], params);
+  const amp = path(['amp'], params);
+  const service = path(['service'], params);
+  const id = path(['id'], params);
 
   return {
     isAmp: 'amp' in params ? !!amp : fallbackAmpParam(url),
