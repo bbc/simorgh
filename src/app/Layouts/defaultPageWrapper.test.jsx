a33a492170ff88ba258cb811651124a3a577b784
Fahad Khan <fk17abb@protonmail.com>
2019-09-09 10:05:43 +0100

    resolve react warning

--- src/app/Layouts/defaultPageWrapper.test.jsx
+++ src/app/Layouts/defaultPageWrapper.test.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import DefaultPageWrapper from './defaultPageWrapper';
+import { ServiceContextProvider } from '../contexts/ServiceContext';
+import { ToggleContext } from '../contexts/ToggleContext';
+import { RequestContext } from '../contexts/RequestContext';
 
 describe('defaultPageWrapper', () => {
   const propsWithChildren = {
     children: <h2>Child element</h2>,
   };
 
+  const defaultToggles = {
+    test: {
+      mpulse: {
+        enabled: true, // or maybe false
+      },
+    },
+  };
+
   shouldMatchSnapshot(
     'should render default page wrapper with children',
-    <DefaultPageWrapper {...propsWithChildren} />,
+    <ServiceContextProvider service="news">
+      <RequestContext.Provider value={{ env: 'test' }}>
+        <ToggleContext.Provider value={{ toggleState: defaultToggles }}>
+          <DefaultPageWrapper {...propsWithChildren} />,
+        </ToggleContext.Provider>
+      </RequestContext.Provider>
+    </ServiceContextProvider>,
   );
 });
