95ca44dcf4f5c7017ca3a5ac0d895d7f99b73f33
Einstein <forensicslabnet@gmail.com>
2019-09-10 17:00:41 +0300

    Expose front pages main without analytics

--- src/app/containers/FrontPageMain/index.jsx
+++ src/app/containers/FrontPageMain/index.jsx
@@ -13,45 +13,48 @@ import MetadataContainer from '../Metadata';
 import ATIAnalytics from '../ATIAnalytics';
 import ChartbeatAnalytics from '../ChartbeatAnalytics';
 
-const FrontPageMain = ({ frontPageData }) => {
+export const FrontPageMain = ({ frontPageData }) => {
+  const groups = pathOr(null, ['content', 'groups'], frontPageData);
   const { product, serviceLocalizedName, translations } = useContext(
     ServiceContext,
   );
   const { home } = translations;
 
-  const groups = pathOr(null, ['content', 'groups'], frontPageData);
-  const { metadata, promo } = frontPageData;
-
-  // eslint-disable-next-line jsx-a11y/aria-role
   const offScreenText = (
-    <>
-      <span role="text">
-        <span lang="en-GB">{product}</span>, {serviceLocalizedName} - {home}
-      </span>
-    </>
+    <span role="text">
+      <span lang="en-GB">{product}</span>, {serviceLocalizedName} - {home}
+    </span>
   );
+
+  return (
+    <main role="main">
+      <VisuallyHiddenText id="content" tabIndex="-1" as="h1">
+        {offScreenText}
+      </VisuallyHiddenText>
+      <Grid>
+        <GridItemConstrainedLargeWithTopMargin>
+          {groups.map((group, index) => (
+            <FrontPageSection
+              key={group.title}
+              group={group}
+              sectionNumber={index}
+            />
+          ))}
+        </GridItemConstrainedLargeWithTopMargin>
+      </Grid>
+    </main>
+  );
+};
+
+const FrontPageMainWithAnalytics = ({ frontPageData }) => {
+  const { metadata, promo } = frontPageData;
 
   return (
     <>
       <ATIAnalytics data={frontPageData} />
       <ChartbeatAnalytics data={frontPageData} />
       <MetadataContainer metadata={metadata} promo={promo} />
-      <main role="main">
-        <VisuallyHiddenText id="content" tabIndex="-1" as="h1">
-          {offScreenText}
-        </VisuallyHiddenText>
-        <Grid>
-          <GridItemConstrainedLargeWithTopMargin>
-            {groups.map((group, index) => (
-              <FrontPageSection
-                key={group.title}
-                group={group}
-                sectionNumber={index}
-              />
-            ))}
-          </GridItemConstrainedLargeWithTopMargin>
-        </Grid>
-      </main>
+      <FrontPageMain frontPageData={frontPageData} />
     </>
   );
 };
@@ -60,4 +63,8 @@ FrontPageMain.propTypes = {
   frontPageData: frontPageDataPropTypes.isRequired,
 };
 
-export default FrontPageMain;
+FrontPageMainWithAnalytics.propTypes = {
+  frontPageData: frontPageDataPropTypes.isRequired,
+};
+
+export default FrontPageMainWithAnalytics;
---
+++ src/app/containers/FrontPageMain/index.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import newsData from '../../../../data/news/frontpage';
+import { FrontPageMain } from './index';
+import { ServiceContextProvider } from '../../contexts/ServiceContext';
+
+storiesOf('Containers|Front Page Main', module).add('Grid Example', () => (
+  <ServiceContextProvider service="news">
+    <FrontPageMain frontPageData={newsData} />
+  </ServiceContextProvider>
+));
