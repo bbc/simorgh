2ca9124d9475d678e0873820acff708fd9a4e22a
ibMadbouly <ibrahim.madbouly@andela.com>
2019-09-11 17:59:21 +0200

    fix linting issues

--- cypress/integration/pages/mediaAssetPage/testsForCanonicalOnly.js
+++ cypress/integration/pages/mediaAssetPage/testsForCanonicalOnly.js
@@ -1,5 +1,3 @@
-import envConfig from '../../../support/config/envs';
-
 // For testing important features that differ between services, e.g. Timestamps.
 // We recommend using inline conditional logic to limit tests to services which differ.
 export const testsThatAlwaysRunForCanonicalOnly = ({ service, pageType }) => {
@@ -10,9 +8,7 @@ export const testsThatAlwaysRunForCanonicalOnly = ({ service, pageType }) => {
 export const testsThatFollowSmokeTestConfigForCanonicalOnly = ({
   service,
   pageType,
-}) =>
-  describe(`Canonical Tests for ${service} ${pageType}`, () => {
-  });
+}) => describe(`Canonical Tests for ${service} ${pageType}`, () => {});
 
 // For testing low priority things e.g. cosmetic differences, and a safe place to put slow tests.
 export const testsThatNeverRunDuringSmokeTestingForCanonicalOnly = ({
