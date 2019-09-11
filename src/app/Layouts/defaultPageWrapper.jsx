65379aee24ec22a00380c10c08fbec3ddb775cb3
Fahad Khan <fk17abb@protonmail.com>
2019-09-11 17:15:47 +0100

    defaultPageWrapper import

--- src/app/Layouts/defaultPageWrapper.jsx
+++ src/app/Layouts/defaultPageWrapper.jsx
@@ -2,17 +2,9 @@ import React from 'react';
 import { node } from 'prop-types';
 import HeaderContainer from '../containers/Header';
 import FooterContainer from '../containers/Footer';
-import ManifestContainer from '../containers/Manifest';
-import ServiceWorkerContainer from '../containers/ServiceWorker';
-import GlobalStyle from '../lib/globalStyles';
-import MPulseBeacon from '../containers/MPulseBeacon';
 
 const PageWrapper = ({ children }) => (
   <>
-    <GlobalStyle />
-    <ServiceWorkerContainer />
-    <ManifestContainer />
-    <MPulseBeacon />
     <HeaderContainer />
     {children}
     <FooterContainer />
--- src/app/Layouts/defaultPageWrapper.test.jsx
+++ src/app/Layouts/defaultPageWrapper.test.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import DefaultPageWrapper from './defaultPageWrapper';
-import { shouldShallowMatchSnapshot } from '../../testHelpers';
+import { ServiceContextProvider } from '../contexts/ServiceContext';
+import { ToggleContext } from '../contexts/ToggleContext';
+import { RequestContext } from '../contexts/RequestContext';
 
 describe('defaultPageWrapper', () => {
   const propsWithChildren = {
     children: <h2>Child element</h2>,
   };
 
-  shouldShallowMatchSnapshot(
+  const defaultToggles = {
+    test: {
+      mpulse: {
+        enabled: true, // or maybe false
+      },
+    },
+  };
+
+  shouldMatchSnapshot(
     'should render default page wrapper with children',
-    <DefaultPageWrapper {...propsWithChildren} />,
+    <ServiceContextProvider service="news">
+      <RequestContext.Provider value={{ env: 'test' }}>
+        <ToggleContext.Provider value={{ toggleState: defaultToggles }}>
+          <DefaultPageWrapper {...propsWithChildren} />,
+        </ToggleContext.Provider>
+      </RequestContext.Provider>
+    </ServiceContextProvider>,
   );
 });
