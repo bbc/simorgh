81e2d7a27dba3b3fcb02a7e579faa65c282fbb5f
Einstein <forensicslabnet@gmail.com>
2019-09-12 15:28:58 +0300

    Add services to grid example

--- src/app/containers/FrontPageMain/index.stories.jsx
+++ src/app/containers/FrontPageMain/index.stories.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import newsData from '../../../../data/news/frontpage';
+import igboData from '../../../../data/igbo/frontpage';
+import pidginData from '../../../../data/pidgin/frontpage';
+import thaiData from '../../../../data/thai/frontpage';
+import yorubaData from '../../../../data/yoruba/frontpage';
 import FrontPageMain from '.';
 import { ServiceContextProvider } from '../../contexts/ServiceContext';
 import { ToggleContextProvider } from '../../contexts/ToggleContext';
 import { RequestContextProvider } from '../../contexts/RequestContext';
 
-storiesOf('Containers|Main|Front Page', module).add('Front Page Main', () => (
-  <ToggleContextProvider>
-    <ServiceContextProvider service="news">
-      <RequestContextProvider isAmp={false} pageType="frontPage" service="news">
-        <FrontPageMain frontPageData={newsData} />
-      </RequestContextProvider>
-    </ServiceContextProvider>
-  </ToggleContextProvider>
-));
+const serviceDataSets = {
+  news: newsData,
+  igbo: igboData,
+  yoruba: yorubaData,
+  pidgin: pidginData,
+  thai: thaiData,
+};
+
+const stories = storiesOf('Containers|Main|Front Page', module);
+
+Object.keys(serviceDataSets).forEach(service => {
+  stories.add(`Frontpage main - ${service}`, () => (
+    <ToggleContextProvider>
+      <ServiceContextProvider service={service}>
+        <RequestContextProvider
+          isAmp={false}
+          pageType="frontPage"
+          service={service}
+        >
+          <FrontPageMain frontPageData={serviceDataSets[service]} />
+        </RequestContextProvider>
+      </ServiceContextProvider>
+    </ToggleContextProvider>
+  ));
+});
