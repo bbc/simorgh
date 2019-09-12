a4124bcd10de9ab5e2c85285ba2800ba9d848049
Einstein <forensicslabnet@gmail.com>
2019-09-12 13:56:01 +0300

    Group main containers in storybook

--- src/app/containers/ArticleMain/index.stories.jsx
+++ src/app/containers/ArticleMain/index.stories.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { inputProvider } from '@bbc/psammead-storybook-helpers';
 import { withKnobs } from '@storybook/addon-knobs';
 import { ServiceContextProvider } from '../../contexts/ServiceContext';
 import { ToggleContextProvider } from '../../contexts/ToggleContext';
 import { RequestContextProvider } from '../../contexts/RequestContext';
 import ArticleMain from '.';
-import { articleDataNews, articleDataPersian } from '../Article/fixtureData';
 
 // article c5jje4ejkqvo contains a Headline, a Paragraph, a timestamp
 // a Portrait Image with Caption, a Landscape Image with Caption and Square Image with Caption.
@@ -14,36 +12,10 @@ import articleData from '../../../../data/news/articles/c5jje4ejkqvo';
 
 // Not all services have fixtures for article data yet
 // the service selector will be constrained to services that have article fixtures:
-const availableFixtures = {
-  news: articleDataNews,
-  persian: articleDataPersian,
-};
 
-storiesOf('Containers|Article/Article Main', module)
+storiesOf('Containers|Main|Article', module)
   .addDecorator(withKnobs)
-  .add(
-    'default',
-    inputProvider({
-      // eslint-disable-next-line react/prop-types
-      componentFunction: ({ service }) => {
-        return (
-          <ToggleContextProvider>
-            <ServiceContextProvider service={service}>
-              <RequestContextProvider
-                isAmp={false}
-                pageType="article"
-                service={service}
-              >
-                <ArticleMain articleData={availableFixtures[service]} />
-              </RequestContextProvider>
-            </ServiceContextProvider>
-          </ToggleContextProvider>
-        );
-      },
-      services: Object.keys(availableFixtures),
-    }),
-  )
-  .add('Grid Layout', () => (
+  .add('Articles Main', () => (
     <ToggleContextProvider>
       <ServiceContextProvider service="news">
         <RequestContextProvider isAmp={false} pageType="article" service="news">
--- src/app/containers/FrontPageMain/index.stories.jsx
+++ src/app/containers/FrontPageMain/index.stories.jsx
@@ -4,11 +4,14 @@ import newsData from '../../../../data/news/frontpage';
 import FrontPageMain from '.';
 import { ServiceContextProvider } from '../../contexts/ServiceContext';
 import { ToggleContextProvider } from '../../contexts/ToggleContext';
+import { RequestContextProvider } from '../../contexts/RequestContext';
 
-storiesOf('Containers|Front Page Main', module).add('Grid Layout', () => (
+storiesOf('Containers|Main|Front Page', module).add('Front Page Main', () => (
   <ToggleContextProvider>
     <ServiceContextProvider service="news">
-      <FrontPageMain frontPageData={newsData} />
+      <RequestContextProvider isAmp={false} pageType="frontPage" service="news">
+        <FrontPageMain frontPageData={newsData} />
+      </RequestContextProvider>
     </ServiceContextProvider>
   </ToggleContextProvider>
 ));
