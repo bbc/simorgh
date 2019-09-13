91ec7b4bac86ec881907bc73a210fc1f074ce4ae
Chris Hinds <chris.hinds@bbc.co.uk>
2019-09-13 10:44:11 +0100

    Update src/app/containers/MediaPageMain/blocks/image.jsx
    Co-Authored-By: Drew McMillan <drew.mcmillan@bbc.co.uk>

--- src/app/containers/MediaPageMain/blocks/image.jsx
+++ src/app/containers/MediaPageMain/blocks/image.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StoryPromoImage } from '../../StoryPromo/index.jsx'
 
 // eslint-disable-next-line react/prop-types
-const HeadingContainer = ({ uuid, path, height, width }) => {
+const ImageContainer = ({ uuid, path, height, width }) => {
     const imageValues = {height, width, path}
     return (
         <StoryPromoImage key={uuid} topStory="false" imageValues={imageValues} lazyLoad="true"/>
