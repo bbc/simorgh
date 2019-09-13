403b9b27a79ea24829ea79052d69ca9b22ced894
Chris <chris.hinds@bbc.co.uk>
2019-09-13 12:27:11 +0100

    Add some basic regex to pull out the tweet id from the embed url

--- src/app/containers/MediaPageMain/blocks/twitterEmbed.jsx
+++ src/app/containers/MediaPageMain/blocks/twitterEmbed.jsx
@@ -3,7 +3,8 @@ import { TwitterTweetEmbed } from 'react-twitter-embed';
 
 // eslint-disable-next-line react/prop-types
 const TwitterEmbedContainer = ({ href }) => {
-  return <TwitterTweetEmbed tweetId={href} />;
+  const tweetId = href.match(/[0-9]+$/g);
+  return <TwitterTweetEmbed tweetId={tweetId[0]} />;
 };
 
 export default TwitterEmbedContainer;
