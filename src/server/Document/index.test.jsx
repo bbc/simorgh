034cdddcb9705354d30950b8a61e9817cc5e4fc0
Olu Niyi-Awosusi <o.niyiawosusi@gmail.com>
2019-09-16 12:09:08 +0100

    more mocking

--- src/server/Document/index.test.jsx
+++ src/server/Document/index.test.jsx
@@ -10,7 +10,8 @@ const sheet = new ServerStyleSheet();
 jest.mock('styled-components', () => {
   return {
     ServerStyleSheet: jest.fn().mockImplementation(() => ({
-      collectStyles: () => {
+      collectStyles: jest.fn(),
+      getStyleElement: () => {
         jest.fn();
       },
     })),
@@ -26,8 +27,14 @@ jest.mock('react-helmet', () => ({
   },
 }));
 
-jest.spyOn(sheet, 'collectStyles');
+jest.mock('react-dom/server', () => ({
+  renderToString: jest.fn(),
+  renderToStaticMarkup: jest
+    .fn()
+    .mockImplementation(() => '<html lang="en-GB"></html>'),
+}));
 
+jest.spyOn(sheet, 'collectStyles');
 jest.spyOn(server, 'renderToString');
 jest.spyOn(server, 'renderToStaticMarkup');
 
@@ -35,7 +42,7 @@ ServerApp.mockImplementation(() => <div />);
 DocumentComponent.mockImplementation(() => <html lang="en-GB" />);
 
 describe('Render Document', () => {
-  it('should render corretly', () => {
+  it('should render corretly', done => {
     renderDocument({
       bbcOrigin: 'https://www.test.bbc.co.uk',
       data: { test: 'data' },
@@ -45,9 +52,10 @@ describe('Render Document', () => {
       url: '/',
     }).then(document => {
       expect(document).toEqual('<!doctype html><html lang="en-GB"></html>');
-      expect(sheet.collectStyles).toHaveBeenCalledWith('lol');
-      expect(server.renderStatic).toHaveBeenCalledWith('ha ha');
+      expect(sheet.collectStyles).toHaveBeenCalled();
+      expect(server.renderToStaticMarkup).toHaveBeenCalledWith('ha ha');
       expect(server.renderToString).toHaveBeenCalledWith('no');
+      done();
     });
   });
 });
