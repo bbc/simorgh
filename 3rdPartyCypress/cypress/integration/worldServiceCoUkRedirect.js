b242a9d9182218cf0ebb2bc62f65a6d60ec65c98
Chris <chris.hinds@bbc.co.uk>
2019-09-17 16:38:09 +0100

    Add env specific logic to ws redirect tests

--- 3rdPartyCypress/cypress/integration/worldServiceCoUkRedirect.js
+++ 3rdPartyCypress/cypress/integration/worldServiceCoUkRedirect.js
@@ -1,4 +1,10 @@
 import services from '../../../src/app/lib/config/services';
+import {
+  ukBasePath,
+  ukBasePathTest,
+  basePath,
+  basePathTest,
+} from '../support/env';
 
 Object.keys(services).forEach(service => {
   const notWSServices = ['news', 'cymrufyw', 'naidheachdan', 'default']; // Not WS
@@ -6,13 +12,31 @@ Object.keys(services).forEach(service => {
   if (notWSServices.includes(service)) {
     return;
   }
+
+  // Do not run the redirect tests on the local environment
+  if (Cypress.env('APP_ENV') === 'local') {
+    return;
+  }
+
   describe('WS Redirects', () => {
     it(`should redirect *bbc.com/${service}`, () => {
-      const urlsTotest = [
-        `https://www.bbc.co.uk/${service}`,
-        `https://www.bbc.co.uk/${service}/articles/a0000000000o`,
-        `https://www.bbc.co.uk/${service}/articles/a0000000000o.amp`,
+      let urlsTotest = [
+        `${ukBasePathTest}${service}`,
+        `${ukBasePathTest}${service}/articles/a0000000000o`,
+        `${ukBasePathTest}${service}/articles/a0000000000o.amp`,
       ];
+      let redirectTo = basePathTest;
+      let substringCharCount = 25;
+
+      if (Cypress.env('APP_ENV') === 'live') {
+        urlsTotest = [
+          `${ukBasePath}${service}`,
+          `${ukBasePath}${service}/articles/a0000000000o`,
+          `${ukBasePath}${service}/articles/a0000000000o.amp`,
+        ];
+        redirectTo = basePath;
+        substringCharCount = 20;
+      }
 
       urlsTotest.forEach(urlToTest => {
         const slashLoc = urlToTest.indexOf('/', 8);
@@ -22,11 +46,11 @@ Object.keys(services).forEach(service => {
         }).then(resp => {
           expect(resp.status).to.eq(301);
           // expect first slice to equal https://www.bbc.com/
-          expect(resp.redirectedToUrl.substring(0, 20)).to.eq(
-            'https://www.bbc.com/',
+          expect(resp.redirectedToUrl.substring(0, substringCharCount)).to.eq(
+            redirectTo,
           );
           // expect second slice to equal whatever came in
-          expect(resp.redirectedToUrl.substring(20)).to.eq(
+          expect(resp.redirectedToUrl.substring(substringCharCount)).to.eq(
             urlToTest.substring(slashLoc + 1),
           );
         });
---
+++ 3rdPartyCypress/cypress/support/env.js
@@ -0,0 +1,4 @@
+export const ukBasePath = `https://www.bbc.co.uk/`;
+export const ukBasePathTest = `https://www.test.bbc.co.uk/`;
+export const basePath = `https://www.bbc.com/`;
+export const basePathTest = `https://www.test.bbc.com/`;
