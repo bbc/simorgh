4258dded29e8353e74c7118035e26f13b86a4915
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2019-09-17 16:11:33 +0100

    remove react-helmet mock from metadata container tests

--- src/app/containers/Metadata/index.test.jsx
+++ src/app/containers/Metadata/index.test.jsx
@@ -11,14 +11,6 @@ import { RequestContextProvider } from '../../contexts/RequestContext';
 import frontPageData from '../../../../data/igbo/frontpage/index.json';
 import liveRadioPageData from '../../../../data/korean/bbc_korean_radio/liveradio.json';
 
-// eslint-disable-next-line react/prop-types
-jest.mock('react-helmet', () => ({ htmlAttributes, ...props }) => (
-  <>
-    {htmlAttributes && <helmet-html-attributes {...htmlAttributes} />}
-    <helmet-head {...props} />
-  </>
-));
-
 const dotComOrigin = 'https://www.bbc.com';
 const dotCoDotUKOrigin = 'https://www.bbc.co.uk';
 
