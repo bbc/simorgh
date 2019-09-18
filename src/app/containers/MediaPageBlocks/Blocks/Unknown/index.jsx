8b6aecfd62b7340f83139ff83c791568381c5865
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-09-18 15:37:43 +0100

    :rocket: Props

--- src/app/containers/MediaPageBlocks/Blocks/Unknown/index.jsx
+++ src/app/containers/MediaPageBlocks/Blocks/Unknown/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { string } from 'prop-types';
 
-/* eslint-disable react/prop-types, react/no-danger */
+/* eslint-disable react/no-danger */
 const UnknownBlockContainer = ({ blockType }) => (
   <div
     dangerouslySetInnerHTML={{ __html: `<!-- unknown block ${blockType} -->` }}
   />
 );
+
+UnknownBlockContainer.propTypes = {
+  blockType: string.isRequired,
+};
 
 export default UnknownBlockContainer;
