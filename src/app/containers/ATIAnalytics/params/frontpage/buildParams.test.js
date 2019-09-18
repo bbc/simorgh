05a520710073dcfd3734cba7c741626460a70f02
Ikechi Michael <mykehell123@gmail.com>
2019-09-18 15:49:51 +0100

    test: move frontpage buildParamms

--- src/app/containers/ATIAnalytics/params/frontpage/buildParams.test.js
+++ src/app/containers/ATIAnalytics/params/frontpage/buildParams.test.js
@@ -30,6 +30,7 @@ const serviceContext = {
   atiAnalyticsAppName: 'atiAnalyticsAppName',
   atiAnalyticsProducerId: 'atiAnalyticsProducerId',
   service: 'service',
+  brandName: 'brandName',
 };
 
 const validURLParams = {
@@ -47,20 +48,63 @@ const validURLParams = {
   statsDestination: requestContext.statsDestination,
 };
 
-describe('buildFrontPageATIParams', () => {
-  it('should return the right object', () => {
-    const result = buildFrontPageATIParams({}, requestContext, serviceContext);
-    expect(result).toEqual(validURLParams);
+describe('frontpage buildParams', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
   });
-});
 
-describe('buildFrontPageATIUrl', () => {
-  it('should call buildATIPageTrackPath exactly once', () => {
-    const mockBuildATIPageTrackPath = jest.fn();
-    buildATIPageTrackPath.mockImplementation(mockBuildATIPageTrackPath);
+  describe('buildFrontPageATIParams', () => {
+    it('should return the right object', () => {
+      const result = buildFrontPageATIParams(
+        {},
+        requestContext,
+        serviceContext,
+      );
+      expect(result).toEqual(validURLParams);
+    });
+  });
 
-    buildFrontPageATIUrl({}, requestContext, serviceContext);
-    expect(mockBuildATIPageTrackPath).toHaveBeenCalledTimes(1);
-    expect(mockBuildATIPageTrackPath).toHaveBeenCalledWith(validURLParams);
+  describe('buildFrontPageATIUrl', () => {
+    it('should call buildATIPageTrackPath exactly once', () => {
+      const mockBuildATIPageTrackPath = jest.fn();
+      buildATIPageTrackPath.mockImplementation(mockBuildATIPageTrackPath);
+
+      buildFrontPageATIUrl({}, requestContext, serviceContext);
+      expect(mockBuildATIPageTrackPath).toHaveBeenCalledTimes(1);
+      expect(mockBuildATIPageTrackPath).toHaveBeenCalledWith(validURLParams);
+    });
+
+    it('should call frontpage utility functions with arguments', () => {
+      const mockFrontPageData = {};
+      const mockServiceContext = {
+        ...serviceContext,
+        service: 'news',
+      };
+      buildFrontPageATIUrl({}, requestContext, mockServiceContext);
+
+      expect(getContentId).toHaveBeenCalledTimes(1);
+      expect(getContentId).toHaveBeenCalledWith(mockFrontPageData);
+      expect(getLanguage).toHaveBeenCalledTimes(1);
+      expect(getLanguage).toHaveBeenCalledWith(mockFrontPageData);
+      expect(getPageIdentifier).toHaveBeenCalledTimes(1);
+      expect(getPageIdentifier).toHaveBeenCalledWith(
+        mockFrontPageData,
+        mockServiceContext.service,
+      );
+      expect(getPageTitle).toHaveBeenCalledTimes(1);
+      expect(getPageTitle).toHaveBeenCalledWith(
+        mockFrontPageData,
+        mockServiceContext.brandName,
+      );
+      expect(getPublishedDatetime).toHaveBeenCalledTimes(2);
+      expect(getPublishedDatetime).toHaveBeenCalledWith(
+        'firstPublished',
+        mockFrontPageData,
+      );
+      expect(getPublishedDatetime).toHaveBeenCalledWith(
+        'lastPublished',
+        mockFrontPageData,
+      );
+    });
   });
 });
--- src/app/containers/ATIAnalytics/params/frontpage/index.test.jsx
+++ src/app/containers/ATIAnalytics/params/frontpage/index.test.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import * as atiUrlBuilder from '../../atiUrl';
-import * as commonTestUtils from '../../../../lib/analyticsUtils';
-import * as testUtils from '../../../../lib/analyticsUtils/frontpage';
 import { ServiceContext } from '../../../../contexts/ServiceContext';
-import { RequestContextProvider } from '../../../../contexts/RequestContext';
-
+import { RequestContext } from '../../../../contexts/RequestContext';
+import { buildFrontPageATIUrl } from './buildParams';
 import FrontPageAtiParams from '.';
+
+jest.mock('./buildParams', () => {
+  const buildParams = jest.requireActual('./buildParams');
+
+  return {
+    ...buildParams,
+    buildFrontPageATIUrl: jest.fn(),
+  };
+});
+
+const mockBuildArticleATIUrl = jest.fn().mockReturnValue(null);
+buildFrontPageATIUrl.mockImplementation(mockBuildArticleATIUrl);
 
 describe('FrontPageAtiParams', () => {
   const mockFPData = {
@@ -28,9 +37,9 @@ describe('FrontPageAtiParams', () => {
 
   const Component = (serviceContextStub, requestContextStub) => (
     <ServiceContext.Provider value={serviceContextStub}>
-      <RequestContextProvider {...requestContextStub}>
+      <RequestContext.Provider value={requestContextStub}>
         <FrontPageAtiParams {...mockFPData} />
-      </RequestContextProvider>
+      </RequestContext.Provider>
     </ServiceContext.Provider>
   );
   const requestContextStub = {
@@ -54,63 +63,13 @@ describe('FrontPageAtiParams', () => {
     });
 
     it('should call atiPageViewParams with the params from the Contexts', () => {
-      const mock = jest.fn().mockReturnValue('key=value&key2=value2');
-      atiUrlBuilder.buildATIPageTrackPath = mock;
-
       renderer.create(Component(serviceContextStub, requestContextStub));
 
-      expect(mock).toHaveBeenCalledTimes(1);
-      expect(mock).toHaveBeenCalledWith({
-        appName: 'news-SERVICE',
-        contentId: 'urn:bbc:cps:b2ce8e02-168f-42c4-b78b-4780807445b4',
-        contentType: 'index-home',
-        language: 'LANGUAGE',
-        pageIdentifier: 'SERVICE.page',
-        pageTitle: 'PAGE TITLE - BBC News SERVICE',
-        producerId: 0,
-        platform: 'canonical',
-        service: 'SERVICE',
-        statsDestination: 'WS_NEWS_LANGUAGES_TEST',
-        timePublished: '2000-01-01T00:00:00.000Z',
-        timeUpdated: '2001-01-01T00:00:00.000Z',
-      });
-    });
-
-    it('should call front page utility functions with arguments', () => {
-      testUtils.getContentId = jest.fn();
-      testUtils.getLanguage = jest.fn();
-      testUtils.getPageIdentifier = jest.fn();
-      testUtils.getPageTitle = jest.fn();
-      commonTestUtils.getPublishedDatetime = jest.fn();
-
-      renderer.create(Component(serviceContextStub, requestContextStub));
-
-      expect(testUtils.getContentId).toHaveBeenCalledTimes(1);
-      expect(testUtils.getContentId).toHaveBeenCalledWith(mockFPData);
-
-      expect(testUtils.getLanguage).toHaveBeenCalledTimes(1);
-      expect(testUtils.getLanguage).toHaveBeenCalledWith(mockFPData);
-
-      expect(testUtils.getPageIdentifier).toHaveBeenCalledTimes(1);
-      expect(testUtils.getPageIdentifier).toHaveBeenCalledWith(
+      expect(mockBuildArticleATIUrl).toHaveBeenCalledTimes(1);
+      expect(mockBuildArticleATIUrl).toHaveBeenCalledWith(
         mockFPData,
-        'SERVICE',
-      );
-
-      expect(testUtils.getPageTitle).toHaveBeenCalledTimes(1);
-      expect(testUtils.getPageTitle).toHaveBeenCalledWith(
-        mockFPData,
-        'BBC News SERVICE',
-      );
-
-      expect(commonTestUtils.getPublishedDatetime).toHaveBeenCalledTimes(2);
-      expect(commonTestUtils.getPublishedDatetime).toHaveBeenCalledWith(
-        'firstPublished',
-        mockFPData,
-      );
-      expect(commonTestUtils.getPublishedDatetime).toHaveBeenCalledWith(
-        'lastPublished',
-        mockFPData,
+        requestContextStub,
+        serviceContextStub,
       );
     });
   });
