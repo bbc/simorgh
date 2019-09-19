1ba715cdd984eaf48c9a69fcaee8585fe66f4697
Ikechi Michael <mykehell123@gmail.com>
2019-09-19 14:09:28 +0100

    fix: separate shouldRender

--- src/app/containers/PageHandlers/withData/index.jsx
+++ src/app/containers/PageHandlers/withData/index.jsx
@@ -1,47 +1,11 @@
 import React, { useContext } from 'react';
 import { element, string } from 'prop-types';
-import pathOr from 'ramda/src/pathOr';
 import { dataPropType } from '../../../models/propTypes/data';
 import ErrorMain from '../../ErrorMain';
-import {
-  getPassportHome,
-  isValidPassportHome,
-} from '../../../lib/utilities/passport';
 import { ServiceContext } from '../../../contexts/ServiceContext';
+import shouldRender from './shouldRender';
 
-// checks for data, status, setting default status if not found
-const constructRenderObject = data => ({
-  status: pathOr(null, ['status'], data) || 500,
-  pageData: pathOr(null, ['pageData'], data),
-});
-
-// checks for pageData, 200 status and if home service from article data fits the service locale
-export const shouldRender = (data, service, passportHomesOverride = []) => {
-  const { status, pageData } = constructRenderObject(data);
-
-  let statusCode = status;
-  let isCorrectService;
-
-  const hasDataAnd200Status = pageData && status === 200;
-  if (hasDataAnd200Status) {
-    const passportHome = getPassportHome(pageData);
-    isCorrectService = isValidPassportHome(passportHome, service);
-    if (!isCorrectService) {
-      (passportHomesOverride || []).forEach(serviceHome => {
-        isCorrectService = isValidPassportHome(passportHome, serviceHome);
-      });
-    }
-    statusCode = !isCorrectService ? 404 : status;
-  }
-
-  return {
-    hasData200StatusAndCorrectService: hasDataAnd200Status && isCorrectService,
-    status: statusCode,
-    pageData,
-  };
-};
-
-export const WithData = Component => {
+const WithData = Component => {
   const DataContainer = ({ data, ...props }) => {
     const { service } = props;
     const { passportHomes } = useContext(ServiceContext) || {};
--- src/app/containers/PageHandlers/withData/index.test.jsx
+++ src/app/containers/PageHandlers/withData/index.test.jsx
@@ -4,7 +4,7 @@ import {
   suppressPropWarnings,
 } from '../../../../testHelpers';
 import { articleDataNews, articleDataPersian } from '../../Article/fixtureData';
-import { WithData, shouldRender } from '.';
+import WithData from '.';
 import frontPageDataPidgin from '../../../../../data/pidgin/frontpage';
 
 describe('withData HOC', () => {
@@ -55,28 +55,6 @@ describe('withData HOC', () => {
     },
   };
 
-  const validPortugueseData = {
-    pageData: {
-      metadata: {
-        passport: {
-          home: 'brasil',
-        },
-      },
-    },
-    status: 200,
-  };
-
-  const invalidPortugueseData = {
-    pageData: {
-      metadata: {
-        passport: {
-          home: 'brasil',
-        },
-      },
-    },
-    status: 404,
-  };
-
   describe('with no data', () => {
     suppressPropWarnings(['data.dials', 'undefined']);
     shouldShallowMatchSnapshot(
@@ -119,74 +97,5 @@ describe('withData HOC', () => {
       'should return the errorMain component',
       <WithDataHOC {...non200StatusProps} />,
     );
-  });
-
-  describe('passport home override', () => {
-    jest.mock('../../../contexts/ServiceContext', () => {
-      const mockReact = jest.requireActual('react');
-      return jest.fn().mockImplementation(
-        mockReact.createContext({
-          passportHomes: ['brasil'],
-        }),
-      );
-    });
-
-    it('should match passport home override', () => {
-      const service = 'portuguese';
-      const { hasData200StatusAndCorrectService, status } = shouldRender(
-        validPortugueseData,
-        service,
-        ['brasil'],
-      );
-      expect(status).toEqual(200);
-      expect(hasData200StatusAndCorrectService).toEqual(true);
-    });
-
-    it('should NOT match passport home override', () => {
-      const service = 'portuguese';
-      const { hasData200StatusAndCorrectService, status } = shouldRender(
-        validPortugueseData,
-        service,
-        ['xyz'],
-      );
-      expect(status).toEqual(404);
-      expect(hasData200StatusAndCorrectService).toEqual(false);
-    });
-
-    describe('no passportHomeOverride', () => {
-      it('should NOT match passport home override', () => {
-        const service = 'portuguese';
-        const { hasData200StatusAndCorrectService, status } = shouldRender(
-          validPortugueseData,
-          service,
-        );
-        expect(status).toEqual(404);
-        expect(hasData200StatusAndCorrectService).toEqual(false);
-      });
-    });
-
-    describe('null passportHomeOverride', () => {
-      it('should NOT match passport home override', () => {
-        const service = 'portuguese';
-        const { hasData200StatusAndCorrectService, status } = shouldRender(
-          validPortugueseData,
-          service,
-          null,
-        );
-        expect(status).toEqual(404);
-        expect(hasData200StatusAndCorrectService).toEqual(false);
-      });
-    });
-
-    it('should return 404 status', () => {
-      const service = 'portuguese';
-      const { hasData200StatusAndCorrectService, status } = shouldRender(
-        invalidPortugueseData,
-        service,
-        ['brasil'],
-      );
-      expect(status).toEqual(404);
-      expect(hasData200StatusAndCorrectService).toEqual(false);
-    });
   });
 });
