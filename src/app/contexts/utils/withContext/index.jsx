d6c772e113814dc5beee4b05be2455bc9bea9f75
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-09-23 01:37:13 +0100

    :white_check_mark: WithContext tests

--- src/app/contexts/utils/withContext/index.jsx
+++ src/app/contexts/utils/withContext/index.jsx
@@ -3,7 +3,9 @@ import { node, string, elementType } from 'prop-types';
 
 const withContext = data => {
   const LoadedContextProvider = ({ Context, dataKey, children }) => (
-    <Context.Provider value={data[dataKey]}>{children}</Context.Provider>
+    <Context.Provider value={dataKey ? data[dataKey] : data}>
+      {children}
+    </Context.Provider>
   );
 
   LoadedContextProvider.propTypes = {
