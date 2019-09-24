10397dafe87c650e163427fe9d017248ba1dab86
Fahad Khan <fahad.khan01@bbc.co.uk>
2019-09-24 11:23:24 +0100

    ATIAnalytics import

--- src/app/containers/ATIAnalytics/canonical/index.test.jsx
+++ src/app/containers/ATIAnalytics/canonical/index.test.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { create, act } from 'react-test-renderer';
 import { render } from 'enzyme';
+import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import CanonicalATIAnalytics from '.';
-import { shouldMatchSnapshot } from '#testHelpers';
 import * as beacon from '#lib/analyticsUtils/sendBeacon';
 
 describe('Canonical ATI Analytics', () => {
@@ -32,11 +32,10 @@ describe('Canonical ATI Analytics', () => {
     const renderedATI = render(
       <CanonicalATIAnalytics pageviewParams={mockPageviewParams} />,
     );
-
-    expect(renderedATI.html()).toBe(
-      `<img height="1px" width="1px" alt="" style="position:absolute" src="https://foobar.com?key=value&amp;key2=value2"/>`,
-    );
+    expect(renderedATI).toMatchSnapshot();
   });
+
+  // `<img height="1px" width="1px" alt="" style="position:absolute" src="https://foobar.com?key=value&amp;key2=value2"/>`,
 
   shouldMatchSnapshot(
     'should render correctly',
