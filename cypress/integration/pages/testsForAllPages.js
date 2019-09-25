a016e08a67e92b3c57d48a9ead8855b88438ac1e
jblaut <jagoda.blaut@bbc.co.uk>
2019-09-25 11:13:07 +0100

    more specific targeting of components

--- cypress/integration/pages/testsForAllPages.js
+++ cypress/integration/pages/testsForAllPages.js
@@ -244,17 +244,23 @@ export const testsThatFollowSmokeTestConfigforAllPages = ({
 
       if (hasLocalisedName) {
         it("should have offscreen text with product's language code set to English", () => {
-          cy.get('header a span span').should('have.attr', 'lang', 'en-GB');
+          cy.get(
+            'header div[class^="Banner"] span[class^="VisuallyHiddenText"] span',
+          ).should('have.attr', 'lang', 'en-GB');
         });
 
         it('should not set the language code for localised name', () => {
-          cy.get('header a span')
+          cy.get(
+            'header div[class^="Banner"] span[class^="VisuallyHiddenText"]',
+          )
             .eq(0)
             .should('not.have.attr', 'lang', 'en-GB');
         });
       } else {
         it('should not have a language attribute if no serviceLocalizedName set', () => {
-          cy.get('header a span').should('not.have.attr', 'lang', 'en-GB');
+          cy.get(
+            'header div[class^="Banner"] span[class^="VisuallyHiddenText"]',
+          ).should('not.have.attr', 'lang', 'en-GB');
         });
       }
 
