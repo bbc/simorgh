f4a401277a18a4137f741808e04afa748c991e9c
Fahad Khan <fahad.khan01@bbc.co.uk>
2019-09-25 12:34:52 +0100

    App index test import

--- src/app/containers/App/index.test.jsx
+++ src/app/containers/App/index.test.jsx
@@ -3,36 +3,68 @@
  * https://github.com/jtart/react-universal-app
  */
 import React from 'react';
-import { shouldShallowMatchSnapshot } from '#testHelpers';
+import { render } from '@testing-library/react';
+import { StaticRouter, BrowserRouter } from 'react-router-dom';
 import { ClientApp, ServerApp } from '.';
+// eslint-disable-next-line import/no-named-as-default
+import App from './App';
+
+jest.mock('./App', () => jest.fn(() => <div />));
+
+jest.mock('react-router-dom', () => ({
+  BrowserRouter: jest.fn(),
+  StaticRouter: jest.fn(),
+}));
+BrowserRouter.mockImplementation(({ children }) => <div>{children}</div>);
+StaticRouter.mockImplementation(({ children }) => <div>{children}</div>);
 
 describe('ClientApp', () => {
-  shouldShallowMatchSnapshot(
-    'should render correctly',
-    <ClientApp data="someData!" routes={['someRoute']} />,
-  );
+  it('should render correctly', () => {
+    render(<ClientApp data="someData!" routes={['someRoute']} />);
+    expect(App).toHaveBeenCalledWith(
+      { initialData: 'someData!', routes: ['someRoute'] },
+      {},
+    );
+    expect(BrowserRouter).toHaveBeenCalledWith(
+      {
+        children: expect.anything(),
+        data: 'someData!',
+        routes: ['someRoute'],
+      },
+      {},
+    );
+  });
 });
 
 describe('ServerApp', () => {
-  describe('no passed routerContext', () => {
-    shouldShallowMatchSnapshot(
-      'should render correctly',
-      <ServerApp
-        location="someUrl"
-        routes={['someRoute']}
-        data="somePassedData"
-        context={{}}
-      />,
-    );
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
-  shouldShallowMatchSnapshot(
-    'should render correctly',
-    <ServerApp
-      location="someUrl"
-      routes={['someRoute']}
-      data="somePassedData"
-      context={{ context: 'someRouterContext' }}
-    />,
-  );
+  it('should render correctly', () => {
+    render(
+      <ServerApp
+        data="somePassedData"
+        routes={['someRoute']}
+        bbcOrigin="https://www.bbc.com"
+      />,
+    );
+    expect(App).toHaveBeenCalledWith(
+      {
+        initialData: 'somePassedData',
+        routes: ['someRoute'],
+        bbcOrigin: 'https://www.bbc.com',
+      },
+      {},
+    );
+    expect(StaticRouter).toHaveBeenCalledWith(
+      {
+        children: expect.anything(),
+        data: 'somePassedData',
+        routes: ['someRoute'],
+        bbcOrigin: 'https://www.bbc.com',
+      },
+      {},
+    );
+  });
 });
