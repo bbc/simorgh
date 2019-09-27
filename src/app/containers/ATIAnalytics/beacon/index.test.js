5d3b53210fe68c3b2703651d73df7a826d59fecb
Ikechi Michael <mykehell123@gmail.com>
2019-09-27 12:39:23 +0100

    fix: use spy in beacon tests

--- src/app/containers/ATIAnalytics/beacon/index.test.js
+++ src/app/containers/ATIAnalytics/beacon/index.test.js
@@ -1,24 +1,16 @@
 import { sendEventBeacon, sendViewBeacon } from '.';
-import sendBeacon from '../../../lib/analyticsUtils/sendBeacon';
+import * as sendBeacon from '../../../lib/analyticsUtils/sendBeacon';
+import * as analyticsUtils from '../../../lib/analyticsUtils';
 
-let mockSendBeacon = jest.fn();
-
-jest.mock('../../../lib/analyticsUtils/sendBeacon');
-jest.mock('../../../lib/analyticsUtils', () => {
-  const analyticsUtils = jest.requireActual('../../../lib/analyticsUtils');
-
-  return {
-    ...analyticsUtils,
-    getCurrentTime: () => '00x00x00',
-  };
-});
+const sendBeaconSpy = jest.spyOn(sendBeacon, 'default');
+analyticsUtils.getCurrentTime = jest.fn().mockReturnValue('00-00-00');
 
 describe('beacon', () => {
   const atiBaseUrl = 'https://foobar.com?';
   process.env.SIMORGH_ATI_BASE_URL = atiBaseUrl;
-  beforeEach(() => {
-    mockSendBeacon = jest.fn();
-    sendBeacon.mockImplementation(mockSendBeacon);
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   describe('event', () => {
@@ -30,13 +22,13 @@ describe('beacon', () => {
         service: 'service',
         component: 'component',
       });
-      expect(mockSendBeacon).toHaveBeenCalledTimes(2);
-      expect(mockSendBeacon.mock.calls).toEqual([
+      expect(sendBeaconSpy).toHaveBeenCalledTimes(2);
+      expect(sendBeaconSpy.mock.calls).toEqual([
         [
-          'https://foobar.com?s=598285&r=0x0x24x24&re=1024x768&hl=00x00x00&lng=en-US&ati=PUB-[service-component]-[=]-[]-[PAR=container-component::name~CHD=brand-top]-[]-[]-[]-[/]',
+          'https://foobar.com?s=598285&r=0x0x24x24&re=1024x768&hl=00-00-00&lng=en-US&ati=PUB-[service-component]-[=]-[]-[PAR=container-component::name~CHD=brand-top]-[]-[]-[]-[/]',
         ],
         [
-          'https://foobar.com?s=598285&r=0x0x24x24&re=1024x768&hl=00x00x00&lng=en-US&ati=PUB-[service-component]-[=click]-[test]-[PAR=container-component::name~CHD=brand-top]-[]-[]-[]-[/]',
+          'https://foobar.com?s=598285&r=0x0x24x24&re=1024x768&hl=00-00-00&lng=en-US&ati=PUB-[service-component]-[=click]-[test]-[PAR=container-component::name~CHD=brand-top]-[]-[]-[]-[/]',
         ],
       ]);
     });
@@ -50,10 +42,10 @@ describe('beacon', () => {
         service: 'service',
         component: 'component',
       });
-      expect(mockSendBeacon).toHaveBeenCalledTimes(1);
-      expect(mockSendBeacon.mock.calls).toEqual([
+      expect(sendBeaconSpy).toHaveBeenCalledTimes(1);
+      expect(sendBeaconSpy.mock.calls).toEqual([
         [
-          'https://foobar.com?s=598285&r=0x0x24x24&re=1024x768&hl=00x00x00&lng=en-US&ati=PUB-[service-component]-[=viewed]-[]-[PAR=container-component::name~CHD=brand-top]-[]-[]-[]-[/]',
+          'https://foobar.com?s=598285&r=0x0x24x24&re=1024x768&hl=00-00-00&lng=en-US&ati=PUB-[service-component]-[=viewed]-[]-[PAR=container-component::name~CHD=brand-top]-[]-[]-[]-[/]',
         ],
       ]);
     });
