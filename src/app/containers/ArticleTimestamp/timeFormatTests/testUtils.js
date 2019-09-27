2ef42d26b1fb99b4c39d407e2546178714f191ed
rowland <rowland.henshaw@andela.com>
2019-09-27 06:46:07 +0100

    changed expectations

--- src/app/containers/ArticleTimestamp/timeFormatTests/testUtils.js
+++ src/app/containers/ArticleTimestamp/timeFormatTests/testUtils.js
@@ -1,42 +1,33 @@
 import * as moment from 'moment-timezone';
 import { timestampGenerator } from '../testHelpers';
-import { formatDate } from '../timeFormats';
+import { formatDate, formatDateAndTime } from '../timeFormats';
 
 export const timestampsFixtures = {
-  'exact date': {
-    time: 1539188425274,
-    isRelative: false,
+  'one minute ago': {
+    time: timestampGenerator({ minutes: 1 }),
+    isRelative: true,
   },
-  'three hours ago': {
-    time: timestampGenerator({ hours: 3 }),
+  'five minutes ago': {
+    time: timestampGenerator({ minutes: 5 }),
+    isRelative: true,
+  },
+  'one hour ago': {
+    time: timestampGenerator({ hours: 1 }),
     isRelative: true,
   },
   'five hours ago': {
     time: timestampGenerator({ hours: 5 }),
     isRelative: true,
   },
-  'eleven hours ago': {
-    time: timestampGenerator({ hours: 11 }),
-    isRelative: true,
+  'exact date': {
+    time: 1539188425274,
+    isRelative: false,
+    dateTimeFormat: formatDate,
   },
-  'twelve hours ago': {
-    time: timestampGenerator({ hours: 12 }),
-    isRelative: true,
-  },
-  'twenty four hours ago': {
-    time: timestampGenerator({
-      hours: 24,
-      seconds: 1,
-    }),
-    isRelative: true,
-  },
-  'two days ago': {
-    time: timestampGenerator({ days: 2 }),
-    isRelative: true,
-  },
-  'three days ago': {
-    time: timestampGenerator({ days: 3 }),
-    isRelative: true,
+  'exact date with timezone': {
+    time: 1562936158365,
+    isRelative: false,
+    dateTimeFormat: formatDateAndTime,
   },
 };
 
@@ -49,5 +40,5 @@ export const format = (datetimeLocale, timezone, timeStamp) => {
     : moment
         .tz(timestampsFixtures[timeStamp].time, timezone)
         .locale(datetimeLocale)
-        .format(formatDate);
+        .format(timestampsFixtures[timeStamp].dateTimeFormat);
 };
