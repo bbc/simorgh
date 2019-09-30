7c2d2cde253f8f81a4d2478b29f75864704906c1
Fahad Khan <fahad.khan01@bbc.co.uk>
2019-09-30 12:52:17 +0100

    mock div

--- src/app/containers/App/index.test.jsx
+++ src/app/containers/App/index.test.jsx
@@ -9,7 +9,7 @@ import { ClientApp, ServerApp } from '.';
 // eslint-disable-next-line import/no-named-as-default
 import App from './App';
 
-jest.mock('./App', () => jest.fn(() => <div />));
+jest.mock('./App', () => jest.fn(() => <div>Client App</div>));
 
 jest.mock('react-router-dom', () => ({
   BrowserRouter: jest.fn(),
@@ -32,11 +32,6 @@ describe('ClientApp', () => {
         routes: ['someRoute'],
       },
       {},
-    );
-    expect(BrowserRouter.mock.calls[0][0].children).not.toBeInstanceOf(Array);
-    // eslint-disable-next-line no-underscore-dangle
-    expect(BrowserRouter.mock.calls[0][0].children._owner.type.name).toEqual(
-      'ClientApp',
     );
   });
 });
