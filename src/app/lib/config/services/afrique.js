f86e34977a586bd44dff25965f1dcea8405bab73
Liz Cameron <52247014+lizcameron@users.noreply.github.com>
2019-10-01 16:31:44 +0100

    Import moment locale 'fr'

--- src/app/lib/config/services/afrique.js
+++ src/app/lib/config/services/afrique.js
@@ -2,6 +2,7 @@ import { C_POSTBOX, C_WHITE } from '@bbc/psammead-styles/colours';
 import { latinDiacritics } from '@bbc/gel-foundations/scripts';
 import { afrique as brandSVG } from '@bbc/psammead-assets/svgs';
 import '@bbc/moment-timezone-include/tz/GMT';
+import 'moment/locale/fr';
 import withContext from '../../../contexts/utils/withContext';
 
 export const service = {
