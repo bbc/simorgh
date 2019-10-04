07533c0a6b9521e5639c74a68dcbe028b3776a8b
Ho Ting Lok <hoting.lok@bbc.co.uk>
2019-10-04 16:05:58 +0100

    Updated base URL

--- .storybook/config.js
+++ .storybook/config.js
@@ -23,7 +23,7 @@ addDecorator(story => (
   <>
     <GlobalStyles
       fonts={Object.values(fontFaces).map(fontFace => {
-        return fontFace();
+        return fontFace('./.storybook/static/fonts/');
       })}
     />
     {story()}
