fd938c0ac696055a9d955ac0863d615bf5053bb5
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-10-04 17:05:39 +0100

    AMP page snapshot

--- src/app/containers/RadioPageMain/index.test.jsx
+++ src/app/containers/RadioPageMain/index.test.jsx
@@ -10,11 +10,27 @@ const pageData = addIdsToBlocks(amharicPageData);
 
 describe('Radio Page Main', () => {
   shouldMatchSnapshot(
-    'should match snapshot',
+    'should match snapshot for Canonical',
     <ServiceContextProvider service="amharic">
       <RequestContextProvider
         bbcOrigin="https://www.test.bbc.co.uk"
         isAmp={false}
+        pageType="media"
+        pathname="/pathname"
+        service="amharic"
+        statusCode={200}
+      >
+        <RadioPageMain service="amharic" pageData={pageData} />
+      </RequestContextProvider>
+    </ServiceContextProvider>,
+  );
+
+  shouldMatchSnapshot(
+    'should match snapshot for AMP',
+    <ServiceContextProvider service="amharic">
+      <RequestContextProvider
+        bbcOrigin="https://www.test.bbc.co.uk"
+        isAmp
         pageType="media"
         pathname="/pathname"
         service="amharic"
