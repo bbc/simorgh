6be9fb7fd914d6f1a861154291168afb0991cc93
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-10-07 14:42:31 +0100

    ensure domain is only swapped for AMP + changes from https://github.com/bbc/simorgh/pull/4147/files

--- src/app/containers/MediaPlayer/helpers/embedUrl.js
+++ src/app/containers/MediaPlayer/helpers/embedUrl.js
@@ -1,26 +1,25 @@
-const buildBaseUrl = origin => {
-  /*
-    Explicitly request the embed from the opposite domain to ensure valid playback on traffic direct to `.amp` pages.
-    
-    EG: 
-    www.bbc.com/bbc_korean_radio/liveradio requests www.bbc.co.uk/ws/av-embeds/media/bbc_korean_radio/liveradio
-  */
-  const swappedOrigin = origin.includes('.co.uk')
-    ? origin.replace('.co.uk', '.com')
-    : origin.replace('.com', '.co.uk');
-
-  if (!origin.includes('local')) {
-    return swappedOrigin;
+const buildBaseUrl = (origin, isAmp) => {
+  if (origin.includes('local') || origin.includes('bbc.github.io')) {
+    const tld = origin.includes('.co.uk') ? '.co.uk' : '.com';
+    return `https://www.test.bbc${tld}`;
   }
 
-  const tld = origin.includes('.co.uk') ? '.co.uk' : '.com';
+  /*
+    Explicitly request the embed from the opposite domain to ensure valid playback on traffic direct to `.amp` pages.
+    EG: www.bbc.com/bbc_korean_radio/liveradio requests www.bbc.co.uk/ws/av-embeds/media/bbc_korean_radio/liveradio
+  */
+  if (isAmp) {
+    return origin.includes('.co.uk')
+      ? origin.replace('.co.uk', '.com')
+      : origin.replace('.com', '.co.uk');
+  }
 
-  return `https://www.test.bbc${tld}`;
+  return origin;
 };
 
 const embedUrl = ({ origin, type, requestUrl, isAmp = false }) => {
   const avRoute = 'ws/av-embeds';
-  const baseUrl = buildBaseUrl(origin);
+  const baseUrl = buildBaseUrl(origin, isAmp);
   const parts = [baseUrl, avRoute, type, requestUrl];
 
   if (isAmp) {
