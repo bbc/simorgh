fbd79b697d747ad8273544a2d34fab676cbc1a57
rowland <rowland.henshaw@andela.com>
2019-10-07 08:28:35 +0100

    incorporated changes from pr feedback

--- src/app/containers/ArticleTimestamp/helpers.test.js
+++ src/app/containers/ArticleTimestamp/helpers.test.js
@@ -14,16 +14,17 @@ describe('ArticleTimestamp helper functions', () => {
     it('should return true if timestamp is valid', () => {
       const timestamp = 1539969006000; // 19 October 2018
 
-      expect(isValidDateTime(timestamp)).toEqual(true);
-      expect(isValidDateTime(0)).toEqual(true);
-      expect(isValidDateTime(-30000000)).toEqual(true);
+      expect(isValidDateTime(timestamp)).toBe(true);
+      expect(isValidDateTime(0)).toBe(true);
+      expect(isValidDateTime(-30000000)).toBe(true);
     });
 
     it('should return false if timestamp is invalid or missing', () => {
-      expect(isValidDateTime('foo')).toEqual(false);
-      expect(isValidDateTime(null)).toEqual(false);
-      expect(isValidDateTime(undefined)).toEqual(false);
-      expect(isValidDateTime()).toEqual(false);
+      expect(isValidDateTime('foo')).toBe(false);
+      expect(isValidDateTime(null)).toBe(false);
+      expect(isValidDateTime(undefined)).toBe(false);
+      expect(isValidDateTime()).toBe(false);
+      expect(isValidDateTime(NaN)).toBe(false);
     });
   });
 
@@ -33,7 +34,7 @@ describe('ArticleTimestamp helper functions', () => {
       const firstPublished = oneHourAgo;
       const lastPublished = oneHourAgo;
 
-      expect(isFirstRelative(lastPublished, firstPublished)).toEqual(true);
+      expect(isFirstRelative(lastPublished, firstPublished)).toBe(true);
     });
 
     it(`should return false when lastPublished === firstPublished and firstPublished > 10 hours ago`, () => {
@@ -41,52 +42,52 @@ describe('ArticleTimestamp helper functions', () => {
       const firstPublished = fiveDaysAgo;
       const lastPublished = fiveDaysAgo;
 
-      expect(isFirstRelative(lastPublished, firstPublished)).toEqual(false);
+      expect(isFirstRelative(lastPublished, firstPublished)).toBe(false);
     });
 
     it(`should return false when lastPublished !== firstPublished and firstPublished < 10 hours ago`, () => {
       const firstPublished = timestampGenerator({ hours: 2 });
       const lastPublished = timestampGenerator({ days: 3 });
 
-      expect(isFirstRelative(lastPublished, firstPublished)).toEqual(false);
+      expect(isFirstRelative(lastPublished, firstPublished)).toBe(false);
     });
   });
 
   describe('isLastRelative', () => {
     it('should return true when lastPublished < 10 hours ago', () => {
       const lastPublished = timestampGenerator({ hours: 8 });
-      expect(isLastRelative(lastPublished)).toEqual(true);
+      expect(isLastRelative(lastPublished)).toBe(true);
     });
 
     it('should return false when lastPublished > 10 hours ago', () => {
       const lastPublished = timestampGenerator({ days: 1 });
-      expect(isLastRelative(lastPublished)).toEqual(false);
+      expect(isLastRelative(lastPublished)).toBe(false);
     });
   });
 
   describe('isSameDay', () => {
-    it('should return true if both timestamps are within 24 hours of each other', () => {
+    it('should return true if both timestamps are on the same day', () => {
       const firstPublished = 1569962265000;
       const lastPublished = 1569962145008;
-      expect(isSameDay(firstPublished, lastPublished)).toEqual(true);
+      expect(isSameDay(firstPublished, lastPublished)).toBe(true);
     });
 
-    it('should return false if both timestamps are not within 24 hours of each other', () => {
+    it('should return false if both timestamps are not on the same day', () => {
       const fourHoursAgo = timestampGenerator({ hours: 4 });
       const thirtySixHoursAgo = timestampGenerator({ hours: 36 });
-      expect(isSameDay(fourHoursAgo, thirtySixHoursAgo)).toEqual(false);
+      expect(isSameDay(fourHoursAgo, thirtySixHoursAgo)).toBe(false);
     });
   });
 
   describe('isToday', () => {
     it('should return true if timestamp is today (3 minutes ago)', () => {
       const timestamp = timestampGenerator({ minutes: 3 });
-      expect(isToday(timestamp)).toEqual(true);
+      expect(isToday(timestamp)).toBe(true);
     });
 
     it('should return false if timestamp is not today (32 hours ago)', () => {
       const timestamp = timestampGenerator({ hours: 32 });
-      expect(isToday(timestamp)).toEqual(false);
+      expect(isToday(timestamp)).toBe(false);
     });
   });
 
@@ -95,25 +96,35 @@ describe('ArticleTimestamp helper functions', () => {
       date: 'D MMMM YYYY',
       dateTimeTimezone: 'D MMMM YYYY, HH:mm z',
     };
-    it(`should return default date format when firstPublished is > 10 hours ago`, () => {
+    it(`should return default date format when firstPublished is not today`, () => {
       const firstPublished = timestampGenerator({ days: 5 });
-      expect(formatType({ firstPublished })).toEqual(dateFormats.date);
+      expect(formatType({ firstPublished })).toBe(dateFormats.date);
     });
 
-    it(`should return dateTimeTimezone format when firstPublished and lastPublished are < 10 hours ago`, () => {
-      const firstPublished = timestampGenerator({ hours: 8 });
+    it(`should return dateTimeTimezone format when firstPublished is today`, () => {
+      const firstPublished = timestampGenerator({ hours: 4 });
+      expect(formatType({ firstPublished })).toBe(dateFormats.dateTimeTimezone);
+    });
+
+    it(`should return date format when lastPublished and firstPublished are not on the same day`, () => {
+      const firstPublished = timestampGenerator({ hours: 50 });
+      const lastPublished = timestampGenerator({ hours: 20 });
+      expect(formatType({ firstPublished, lastPublished })).toBe(
+        dateFormats.date,
+      );
+    });
+
+    it(`should return dateTimeTimezone format when firstPublished and lastPublished are on the same day and today`, () => {
+      const firstPublished = timestampGenerator({ hours: 4 });
       const lastPublished = timestampGenerator({ hours: 2 });
-      expect(formatType({ firstPublished, lastPublished })).toEqual(
+      expect(formatType({ firstPublished, lastPublished })).toBe(
         dateFormats.dateTimeTimezone,
       );
     });
 
-    it(`should return date format when firstPublished and lastPublished are > 10 hours ago`, () => {
-      const firstPublished = timestampGenerator({ hours: 50 });
-      const lastPublished = timestampGenerator({ hours: 20 });
-      expect(formatType({ firstPublished, lastPublished })).toEqual(
-        dateFormats.date,
-      );
+    it(`should return default date format when firstPublished is null`, () => {
+      const firstPublished = null;
+      expect(formatType({ firstPublished })).toBe(dateFormats.date);
     });
   });
 });
