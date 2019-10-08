3e54bf5819378129b1de5e6e45b85687b8d3433b
ibMadbouly <ibrahim.madbouly@andela.com>
2019-10-08 05:38:45 +0200

    CpsAssetPage main tests

---
+++ src/app/containers/CpsAssetPageMain/index.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import { ServiceContextProvider } from '#contexts/ServiceContext';
+import { RequestContextProvider } from '#contexts/RequestContext';
+import CpsAssetPageMain from '.';
+import pidginPageData from '#data/pidgin/cpsAssets/tori-49450859';
+import addIdsToBlocks from '../../routes/getInitialData/radioPage/addIdsToBlocks';
+
+const pageData = addIdsToBlocks(pidginPageData);
+
+describe('CpsAssetPageMain', () => {
+  shouldMatchSnapshot(
+    'should match snapshot',
+    <ServiceContextProvider service="news">
+      <RequestContextProvider
+        bbcOrigin="https://www.test.bbc.co.uk"
+        isAmp={false}
+        pageType="media"
+        pathname="/pathname"
+        service="news"
+        statusCode={200}
+      >
+        <CpsAssetPageMain service="pidgin" pageData={pageData} />
+      </RequestContextProvider>
+    </ServiceContextProvider>,
+  );
+});
