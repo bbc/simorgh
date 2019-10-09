2df661197b90f9bae374f6c319f33fdbd12004e5
Ikechi Michael <mykehell123@gmail.com>
2019-10-09 16:59:51 +0100

    feat: use decorator with timemachine

---
+++ src/app/containers/CpsAssetPageMain/index.test.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import { ServiceContextProvider } from '#contexts/ServiceContext';
+import { RequestContextProvider } from '#contexts/RequestContext';
+import CpsAssetPageMain from '.';
+import pidginPageData from '#data/pidgin/cpsAssets/tori-49450859';
+
+describe('CpsAssetPageMain', () => {
+  shouldMatchSnapshot(
+    'should match snapshot',
+    <ServiceContextProvider service="pidgin">
+      <RequestContextProvider
+        bbcOrigin="https://www.test.bbc.co.uk"
+        isAmp={false}
+        pageType="MAP"
+        pathname="/pathname"
+        service="pidgin"
+        statusCode={200}
+      >
+        <CpsAssetPageMain service="pidgin" pageData={pidginPageData} />
+      </RequestContextProvider>
+    </ServiceContextProvider>,
+  );
+});
