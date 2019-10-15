035232e09d9beee74879f8a4e045e7e98b9bae51
thekp <khoa.phan@bbc.co.uk>
2019-10-15 14:30:21 +0100

    add logic to stop tests when cypress fails

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -10,3 +10,10 @@ Cypress.on(`window:before:load`, win => {
     throw new Error(msg);
   });
 });
+
+Cypress.on('fail', error => {
+  // eslint-disable-next-line no-console
+  console.log('One Cypress test has failed, this is the:', error);
+  Cypress.runner.stop();
+  throw error; // throw error to have test fail
+});
