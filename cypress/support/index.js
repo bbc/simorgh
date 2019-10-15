63335dbc102c1b57239a7f09457876542b640c69
thekp <khoa.phan@bbc.co.uk>
2019-10-15 16:29:06 +0100

    add process exit

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,5 +15,6 @@ Cypress.on('fail', error => {
   // eslint-disable-next-line no-console
   console.log('One Cypress test has failed, this is the:', error);
   Cypress.runner.stop();
+  process.exit(1);
   throw error; // throw error to have test fail
 });
