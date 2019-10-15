13ea76f2dcb760896dd5251893399d3f6de33722
PriyaKR <Priyatharsini.kuduvarajan@bbc.co.uk>
2019-10-15 14:46:24 +0100

    testing cypress test failure

--- cypress/integration/pages/testsForAllPages.js
+++ cypress/integration/pages/testsForAllPages.js
@@ -183,7 +183,7 @@ export const testsThatFollowSmokeTestConfigforAllPages = ({
                   cy.get('meta[name="og:description"]').should(
                     'have.attr',
                     'content',
-                    description,
+                    Priya,
                   );
                   cy.get('meta[name="og:title"]').should(
                     'have.attr',
