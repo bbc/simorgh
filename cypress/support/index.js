bdeabc826eff8a9d20bea509e95fb463e1297fa3
PriyaKR <Priyatharsini.kuduvarajan@bbc.co.uk>
2019-10-15 15:16:38 +0100

    testing

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,5 +15,6 @@ Cypress.on('fail', error => {
   // eslint-disable-next-line no-console
   console.log('One Cypress test has failed, this is the:', error);
   Cypress.runner.abort();
+  Cypress.runner.stop()
   throw error; // throw error to have test fail
 });
