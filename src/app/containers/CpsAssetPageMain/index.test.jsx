83ef3ec20e21b5f324ba12a1ccf228c75695a8f0
Phil Lee <phil.lee02@bbc.co.uk>
2019-10-16 18:07:03 +0100

    wrap in static router for <Link> to work and use new async snapshot test helper

--- src/app/containers/CpsAssetPageMain/index.test.jsx
+++ src/app/containers/CpsAssetPageMain/index.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import { StaticRouter } from 'react-router-dom';
+import matchSnapshot from './testHelpers';
 import { ServiceContextProvider } from '#contexts/ServiceContext';
 import { RequestContextProvider } from '#contexts/RequestContext';
 import CpsAssetPageMain from '.';
@@ -7,25 +8,30 @@ import preprocessor from '#lib/utilities/preprocessor';
 import convertToOptimoBlocks from '#lib/utilities/preprocessor/rules/cpsAssetPage/convertToOptimoBlocks';
 import pidginPageData from '#data/pidgin/cpsAssets/tori-49450859';
 
-const processedPidgin = async () => {
-  const page = await preprocessor(pidginPageData, [convertToOptimoBlocks]);
-  return page;
-};
+const pageData = preprocessor(pidginPageData, [convertToOptimoBlocks]);
 
-describe('CpsAssetPageMain', async () => {
-  shouldMatchSnapshot(
-    'should match snapshot',
-    <ServiceContextProvider service="pidgin">
-      <RequestContextProvider
-        bbcOrigin="https://www.test.bbc.co.uk"
-        isAmp={false}
-        pageType="MAP"
-        pathname="/pidgin/tori-49450859"
-        service="pidgin"
-        statusCode={200}
-      >
-        <CpsAssetPageMain service="pidgin" pageData={await processedPidgin} />
-      </RequestContextProvider>
-    </ServiceContextProvider>,
-  );
+describe('CpsAssetPageMain', () => {
+  it('should match snapshot', async done => {
+    const component = (
+      /*
+        for the value it would bring, it is much simpler to wrap a react-router Link in a Router, rather than mock a Router or pass some mocked context.
+      */
+      <StaticRouter>
+        <ServiceContextProvider service="pidgin">
+          <RequestContextProvider
+            bbcOrigin="https://www.test.bbc.co.uk"
+            isAmp={false}
+            pageType="MAP"
+            pathname="/pidgin/tori-49450859"
+            service="pidgin"
+            statusCode={200}
+          >
+            <CpsAssetPageMain service="pidgin" pageData={await pageData} />
+          </RequestContextProvider>
+        </ServiceContextProvider>
+      </StaticRouter>
+    );
+
+    matchSnapshot(component, done);
+  });
 });
