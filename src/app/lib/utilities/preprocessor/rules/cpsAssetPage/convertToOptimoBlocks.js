21399950275732c62c9f0004c8d1e19864c61512
Phil Lee <phil.lee02@bbc.co.uk>
2019-10-16 19:39:32 +0100

    move functions out of convertToOptimoBlocks for readability

--- src/app/lib/utilities/preprocessor/rules/cpsAssetPage/convertToOptimoBlocks.js
+++ src/app/lib/utilities/preprocessor/rules/cpsAssetPage/convertToOptimoBlocks.js
@@ -1,29 +1,29 @@
 import { clone, pathOr } from 'ramda';
 import convertParagraph from './convertParagraph';
 
+const handleMissingType = block =>
+  console.log(`Missing type field on block ${block.type}`); // eslint-disable-line no-console
+
+const typesToConvert = {
+  paragraph: convertParagraph,
+};
+
+const parseBlockByType = block => {
+  if (!block || !block.type) {
+    return null;
+  }
+  const { type } = block;
+
+  const parsedBlock = (typesToConvert[type] || handleMissingType)(block);
+
+  if (!parsedBlock) {
+    return null;
+  }
+
+  return parsedBlock;
+};
+
 const convertToOptimoBlocks = async jsonRaw => {
-  const handleMissingType = block =>
-    console.log(`Missing type field on block ${block.type}`); // eslint-disable-line no-console
-
-  const typesToConvert = {
-    paragraph: convertParagraph,
-  };
-
-  const parseBlockByType = async block => {
-    if (!block || !block.type) {
-      return null;
-    }
-    const { type } = block;
-
-    const parsedBlock = (typesToConvert[type] || handleMissingType)(block);
-
-    if (!parsedBlock) {
-      return null;
-    }
-
-    return parsedBlock;
-  };
-
   const json = clone(jsonRaw);
   const blocks = pathOr([], ['content', 'blocks'], json);
 
