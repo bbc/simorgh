772c9b7c73e6228f04555abe4f16e1d24058f6a2
Phil Lee <phil.lee02@bbc.co.uk>
2019-10-17 22:18:40 +0100

    <h1> should have id of content and tab index

--- src/app/containers/CpsAssetPageMain/index.jsx
+++ src/app/containers/CpsAssetPageMain/index.jsx
@@ -24,7 +24,9 @@ const CpsAssetPageMain = ({ pageData }) => {
       <ATIAnalytics data={pageData} />
       <Grid as="main" role="main">
         <GridItemConstrainedMedium>
-          <h1> Placeholder content for MAP page skeleton</h1>
+          <h1 id="content" tabIndex="-1">
+            Placeholder content for MAP page skeleton
+          </h1>
           <Link to="/pidgin/23248703" data-e2e="cpsAssetDummyLink">
             Test MAP to MAP inline link
           </Link>
