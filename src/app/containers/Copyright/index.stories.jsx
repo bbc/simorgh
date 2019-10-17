bff3d59e723b6d67beee81879c2432dc196dcba8
Einstein <forensicslabnet@gmail.com>
2019-10-17 14:39:21 +0300

    Conditionally load Resource Hints for font domains

--- src/app/containers/Copyright/index.stories.jsx
+++ src/app/containers/Copyright/index.stories.jsx
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { inputProvider } from '@bbc/psammead-storybook-helpers';
 import { withKnobs } from '@storybook/addon-knobs';
 import CopyrightContainer from '.';
-import services from '#testHelpers/serviceConfigs';
+import services from '../../../server/utilities/serviceConfigs';
 import { ServiceContext } from '#contexts/ServiceContext';
 
 storiesOf('Containers|Copyright', module)
