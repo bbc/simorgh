118cd697e56177d3d78489da2b9aefb6c81887f4
Phil Lee <phil.lee02@bbc.co.uk>
2019-10-17 10:26:04 +0100

    move <link> under h1

--- src/app/containers/CpsAssetPageMain/index.jsx
+++ src/app/containers/CpsAssetPageMain/index.jsx
@@ -23,10 +23,10 @@ const CpsAssetPageMain = ({ pageData }) => {
       <LinkedData type="Article" seoTitle={title} />
       <Grid as="main" role="main">
         <GridItemConstrainedMedium>
+          <h1> Placeholder content for MAP page skeleton</h1>
           <Link to="/pidgin/test-12345678" data-e2e="cpsAssetDummyLink">
             Test MAP to MAP inline link
           </Link>
-          <h1> Placeholder content for MAP page skeleton</h1>
         </GridItemConstrainedMedium>
       </Grid>
     </>
