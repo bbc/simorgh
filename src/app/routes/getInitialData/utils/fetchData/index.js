237a5a498f4ed48fc72e00dd25c55aa4e23dfa85
Phil Lee <phil.lee02@bbc.co.uk>
2019-10-17 17:46:12 +0100

    whitespace

--- src/app/routes/getInitialData/utils/fetchData/index.js
+++ src/app/routes/getInitialData/utils/fetchData/index.js
@@ -3,7 +3,6 @@ import nodeLogger from '#lib/logger.node';
 import preprocess from '#lib/utilities/preprocessor';
 
 const logger = nodeLogger(__filename);
-
 const STATUS_CODE_OK = 200;
 const STATUS_CODE_BAD_GATEWAY = 502;
 const STATUS_CODE_NOT_FOUND = 404;
