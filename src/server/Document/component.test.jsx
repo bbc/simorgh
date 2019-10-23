fd82cc865e2e22b65b86d54c00dd84f368291f90
Olu Niyi-Awosusi <o.niyiawosusi@gmail.com>
2019-10-23 14:21:02 +0100

    fixing console error

--- src/server/Document/component.test.jsx
+++ src/server/Document/component.test.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
-import { string, bool } from 'prop-types';
+import { Helmet } from 'react-helmet';
+import { renderToString } from 'react-dom/server';
+import { JSDOM } from 'jsdom';
+
 import DocumentComponent from './component';
+
+Helmet.canUseDOM = false;
 
 describe('Document Component', () => {
   const assetOrigins = ['http://example.com'];
   const data = { test: 'data' };
-
-  const mockHelmetToComponent = element => ({
-    toComponent: jest.fn().mockImplementation(() => element),
-  });
-
   const scripts = (
     <>
       <script src="main.js" />
@@ -18,37 +17,34 @@ describe('Document Component', () => {
       <script src="igbo.js" />
     </>
   );
-
-  const helmet = {
-    htmlAttributes: mockHelmetToComponent({
-      lang: 'test',
-    }),
-    link: mockHelmetToComponent(
-      <link
-        rel="canonical"
-        href="https://www.bbc.com/news/articles/c6v11qzyv8po"
-      />,
-    ),
-    meta: mockHelmetToComponent(
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, minimum-scale=1"
-      />,
-    ),
-    script: mockHelmetToComponent(<script type="application/ld+json" />),
-    title: mockHelmetToComponent(<title>Test title</title>),
-  };
   const styleTags = (
     <style data-styled-components="abc">{'html { color: red; }'}</style>
   );
   const styleTagsAmp = <style amp-custom="">{'html { color: red; }'}</style>;
 
-  const testDocumentComponent = ({ service, isAmp }) => (
+  // eslint-disable-next-line react/prop-types
+  const TestDocumentComponent = ({ service, isAmp }) => (
     <DocumentComponent
       assetOrigins={assetOrigins}
-      app={'<h1>App!</h1>'}
+      app={renderToString(
+        <>
+          <Helmet htmlAttributes={{ lang: 'test' }}>
+            <title>Test title</title>
+            <link
+              rel="canonical"
+              href="https://www.bbc.com/news/articles/c6v11qzyv8po"
+            />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, minimum-scale=1"
+            />
+            <script type="application/ld+json" />
+          </Helmet>
+          <h1>App!</h1>
+        </>,
+      )}
       data={{ ...data }}
-      helmet={helmet}
+      helmet={Helmet.renderStatic()}
       styleTags={isAmp ? styleTagsAmp : styleTags}
       scripts={scripts}
       service={service}
@@ -56,18 +52,17 @@ describe('Document Component', () => {
     />
   );
 
-  testDocumentComponent.propTypes = {
-    service: string.isRequired,
-    isAmp: bool.isRequired,
-  };
+  it('should render correctly', async () => {
+    const dom = new JSDOM(
+      renderToString(<TestDocumentComponent service="news" isAmp={false} />),
+    );
+    expect(dom.window.document.documentElement).toMatchSnapshot();
+  });
 
-  shouldMatchSnapshot(
-    'should render correctly',
-    testDocumentComponent('news', false),
-  );
-
-  shouldMatchSnapshot(
-    'should render AMP version correctly',
-    testDocumentComponent('news', true),
-  );
+  it('should render AMP version correctly', async () => {
+    const dom = new JSDOM(
+      renderToString(<TestDocumentComponent service="news" isAmp />),
+    );
+    expect(dom.window.document.documentElement).toMatchSnapshot();
+  });
 });
