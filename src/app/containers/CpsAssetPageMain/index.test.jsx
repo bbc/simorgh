63c2c8f17644090f3da5fce0257b5e34aaadffa0
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-10-24 00:15:17 +0100

    :white_check_mark: Tweak tests

--- src/app/containers/CpsAssetPageMain/index.test.jsx
+++ src/app/containers/CpsAssetPageMain/index.test.jsx
@@ -1,31 +1,42 @@
 import React from 'react';
 import { StaticRouter } from 'react-router-dom';
-import { matchSnapshotAsync } from '@bbc/psammead-test-helpers';
+import {
+  matchSnapshotAsync,
+  suppressPropWarnings,
+} from '@bbc/psammead-test-helpers';
 import { ServiceContextProvider } from '#contexts/ServiceContext';
 import { RequestContextProvider } from '#contexts/RequestContext';
 import CpsAssetPageMain from '.';
 import preprocessor from '#lib/utilities/preprocessor';
-import pidginPageData from '#data/pidgin/cpsAssets/tori-49450859';
+import pidginPageData from '#data/igbo/cpsAssets/afirika-23252735';
 import { preprocessorRules } from '#app/routes/getInitialData/cpsAsset';
 
 describe('CpsAssetPageMain', () => {
   it('should match snapshot', async () => {
+    // TODO: Remove the prop warnings
+    // https://github.com/bbc/simorgh/issues/4396
+    suppressPropWarnings(['TextContainer']);
+    suppressPropWarnings(['ParagraphContainer']);
+    suppressPropWarnings(['isExternal', 'InlineLinkContainer']);
+    suppressPropWarnings(['ParagraphContainer']);
+
     const pageData = await preprocessor(pidginPageData, preprocessorRules);
+
     await matchSnapshotAsync(
       /*
         for the value it would bring, it is much simpler to wrap a react-router Link in a Router, rather than mock a Router or pass some mocked context.
       */
       <StaticRouter>
-        <ServiceContextProvider service="pidgin">
+        <ServiceContextProvider service="igbo">
           <RequestContextProvider
             bbcOrigin="https://www.test.bbc.co.uk"
             isAmp={false}
             pageType="MAP"
-            pathname="/pidgin/tori-49450859"
-            service="pidgin"
+            pathname="/igbo/afirika-23252735"
+            service="igbo"
             statusCode={200}
           >
-            <CpsAssetPageMain service="pidgin" pageData={pageData} />
+            <CpsAssetPageMain service="igbo" pageData={pageData} />
           </RequestContextProvider>
         </ServiceContextProvider>
       </StaticRouter>,
