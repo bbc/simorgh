6fa86c94f3cd9712aaad5f05afc9bd5f17b24d17
Ryan McCombe <ryanmccombe@gmail.com>
2019-10-24 16:37:43 +0100

    Update unit tests

--- src/app/containers/CpsAssetPageMain/Blocks/Video/index.test.jsx
+++ src/app/containers/CpsAssetPageMain/Blocks/Video/index.test.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import { ServiceContextProvider } from '#contexts/ServiceContext';
+import { RequestContextProvider } from '#contexts/RequestContext';
+import { ToggleContextProvider } from '#contexts/ToggleContext';
+
+import { optimoVideoBlock } from '#lib/utilities/preprocessor/rules/cpsAssetPage/convertToOptimoBlocks/blocks/media/fixtures';
+
 import CPSVideoBlock from '.';
+
+// eslint-disable-next-line react/prop-types
+const Contexts = ({ children }) => (
+  <ToggleContextProvider>
+    <ServiceContextProvider service="igbo">
+      <RequestContextProvider
+        bbcOrigin="https://www.test.bbc.co.uk"
+        isAmp={false}
+        pageType="STY"
+        pathname="/igbo/afirika-23252735"
+        service="igbo"
+        statusCode={200}
+      >
+        {children}
+      </RequestContextProvider>
+    </ServiceContextProvider>
+  </ToggleContextProvider>
+);
 
 describe('CPSVideoBlock', () => {
   shouldMatchSnapshot(
     'no placeholder',
-    <CPSVideoBlock assetUri="/pidgin/12345678" />,
+    <Contexts>
+      <CPSVideoBlock
+        {...optimoVideoBlock.model}
+        assetUri="/pidgin/12345678"
+        showPlaceholder={false}
+      />
+    </Contexts>,
   );
   shouldMatchSnapshot(
     'with placeholder',
-    <CPSVideoBlock assetUri="/pidgin/12345678" showPlaceholder />,
+    <Contexts>
+      <CPSVideoBlock
+        {...optimoVideoBlock.model}
+        assetUri="/pidgin/12345678"
+        showPlaceholder
+      />
+      ,
+    </Contexts>,
   );
 });
