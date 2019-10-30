be9204021dd8d194ecfe1675265b5ef59506defb
Drew McMillan <drew.mcmillan@bbc.co.uk>
2019-10-30 15:23:57 +0000

    :white_check_mark: Test fix

--- src/app/containers/CpsAssetPageMain/index.test.jsx
+++ src/app/containers/CpsAssetPageMain/index.test.jsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import { StaticRouter } from 'react-router-dom';
-import {
-  matchSnapshotAsync,
-  suppressPropWarnings,
-} from '@bbc/psammead-test-helpers';
+import { matchSnapshotAsync } from '@bbc/psammead-test-helpers';
 import { ServiceContextProvider } from '#contexts/ServiceContext';
 import { RequestContextProvider } from '#contexts/RequestContext';
+import { ToggleContext } from '#contexts/ToggleContext';
 import CpsAssetPageMain from '.';
 import preprocessor from '#lib/utilities/preprocessor';
 import igboPageData from '#data/igbo/cpsAssets/afirika-23252735';
 import pidginPageData from '#data/pidgin/cpsAssets/tori-49450859';
 import { preprocessorRules } from '#app/routes/getInitialData/cpsAsset';
 
+const toggleState = {
+  local: {
+    mediaPlayer: {
+      enabled: true,
+    },
+  },
+  test: {
+    mediaPlayer: {
+      enabled: true,
+    },
+  },
+  live: {
+    mediaPlayer: {
+      enabled: false,
+    },
+  },
+};
+
 describe('CpsAssetPageMain', () => {
   it('should match snapshot for STY', async () => {
-    // TODO: Remove the prop warnings
-    // https://github.com/bbc/simorgh/issues/4396
-    // https://github.com/bbc/simorgh/issues/4397
-    suppressPropWarnings(['TextContainer']);
-    suppressPropWarnings(['ParagraphContainer']);
-    suppressPropWarnings(['isExternal', 'InlineLinkContainer']);
-    suppressPropWarnings(['ParagraphContainer']);
-
     const pageData = await preprocessor(igboPageData, preprocessorRules);
 
     await matchSnapshotAsync(
@@ -29,18 +37,22 @@ describe('CpsAssetPageMain', () => {
         for the value it would bring, it is much simpler to wrap a react-router Link in a Router, rather than mock a Router or pass some mocked context.
       */
       <StaticRouter>
-        <ServiceContextProvider service="igbo">
-          <RequestContextProvider
-            bbcOrigin="https://www.test.bbc.co.uk"
-            isAmp={false}
-            pageType="STY"
-            pathname="/igbo/afirika-23252735"
-            service="igbo"
-            statusCode={200}
-          >
-            <CpsAssetPageMain service="igbo" pageData={pageData} />
-          </RequestContextProvider>
-        </ServiceContextProvider>
+        <ToggleContext.Provider
+          value={{ toggleState, toggleDispatch: jest.fn() }}
+        >
+          <ServiceContextProvider service="igbo">
+            <RequestContextProvider
+              bbcOrigin="https://www.test.bbc.co.uk"
+              isAmp={false}
+              pageType="STY"
+              pathname="/igbo/afirika-23252735"
+              service="igbo"
+              statusCode={200}
+            >
+              <CpsAssetPageMain service="igbo" pageData={pageData} />
+            </RequestContextProvider>
+          </ServiceContextProvider>
+        </ToggleContext.Provider>
       </StaticRouter>,
     );
   });
@@ -50,18 +62,22 @@ describe('CpsAssetPageMain', () => {
 
     await matchSnapshotAsync(
       <StaticRouter>
-        <ServiceContextProvider service="igbo">
-          <RequestContextProvider
-            bbcOrigin="https://www.test.bbc.co.uk"
-            isAmp={false}
-            pageType="MAP"
-            pathname="/pidgin/tori-49450859"
-            service="pidgin"
-            statusCode={200}
-          >
-            <CpsAssetPageMain service="pidgin" pageData={pageData} />
-          </RequestContextProvider>
-        </ServiceContextProvider>
+        <ToggleContext.Provider
+          value={{ toggleState, toggleDispatch: jest.fn() }}
+        >
+          <ServiceContextProvider service="igbo">
+            <RequestContextProvider
+              bbcOrigin="https://www.test.bbc.co.uk"
+              isAmp={false}
+              pageType="MAP"
+              pathname="/pidgin/tori-49450859"
+              service="pidgin"
+              statusCode={200}
+            >
+              <CpsAssetPageMain service="pidgin" pageData={pageData} />
+            </RequestContextProvider>
+          </ServiceContextProvider>
+        </ToggleContext.Provider>
       </StaticRouter>,
     );
   });
