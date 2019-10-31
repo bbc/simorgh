201281266d6e8fa99dac9decf9d6b3af752fc8d9
Sareh Heidari <sareh@users.noreply.github.com>
2019-10-31 13:33:04 +0000

    Update loading page to use Psammead Grid

--- src/app/containers/PageHandlers/withLoading/index.jsx
+++ src/app/containers/PageHandlers/withLoading/index.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import { bool, element } from 'prop-types';
-import { GhostGrid, GridItemConstrainedMedium } from '#lib/styledGrid';
+import Grid, { GelPageGridGhost } from '#app/components/Grid';
 
 const WithLoading = Component => {
   const LoadingContainer = ({ loading, ...props }) => {
     if (!loading) return <Component {...props} />;
     return (
-      <main role="main">
-        <GhostGrid>
-          <GridItemConstrainedMedium />
-        </GhostGrid>
-      </main>
+      <GelPageGridGhost
+        as="main"
+        role="main"
+        columns={{
+          group0: 6,
+          group1: 6,
+          group2: 6,
+          group3: 6,
+          group4: 8,
+          group5: 20,
+        }}
+        enableGelGutters
+        enableGelMargins
+      >
+        <Grid
+          item
+          startOffset={{
+            group0: 1,
+            group1: 1,
+            group2: 1,
+            group3: 1,
+            group4: 2,
+            group5: 5,
+          }}
+          columns={{
+            group0: 6,
+            group1: 6,
+            group2: 6,
+            group3: 6,
+            group4: 6,
+            group5: 12,
+          }}
+        >
+          <div />
+        </Grid>
+      </GelPageGridGhost>
     );
   };
 
