96baf49b254d13c9a4140e8115bf14ebc7f4a429
Sareh Heidari <sareh@users.noreply.github.com>
2019-10-31 13:23:40 +0000

    Update CPS Asset Page Main to use Psammead Grid

--- src/app/containers/CpsAssetPageMain/index.jsx
+++ src/app/containers/CpsAssetPageMain/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import path from 'ramda/src/path';
 import pathOr from 'ramda/src/pathOr';
 import { Link } from 'react-router-dom';
-import { GhostGrid, GridItemConstrainedMedium } from '#lib/styledGrid';
 import MetadataContainer from '../Metadata';
 import LinkedData from '../LinkedData';
 import headings from '../Headings';
@@ -12,6 +11,7 @@ import image from '../Image';
 import Blocks from '../Blocks';
 import ATIAnalytics from '../ATIAnalytics';
 import cpsAssetPagePropTypes from '../../models/propTypes/cpsAssetPage';
+import Grid, { GelPageGridGhost } from '#app/components/Grid';
 
 const componentsToRender = {
   headline: headings,
@@ -36,14 +36,45 @@ const CpsAssetPageMain = ({ pageData }) => {
       />
       <LinkedData type="Article" seoTitle={title} />
       <ATIAnalytics data={pageData} />
-      <GhostGrid as="main" role="main">
-        <GridItemConstrainedMedium>
+      <GelPageGridGhost
+        as="main"
+        role="main"
+        columns={{
+          group0: 6,
+          group1: 6,
+          group2: 6,
+          group3: 6,
+          group4: 8,
+          group5: 20,
+        }}
+        enableGelGutters
+        enableGelMargins
+      >
+        <Grid
+          item
+          startOffset={{
+            group0: 1,
+            group1: 1,
+            group2: 1,
+            group3: 1,
+            group4: 2,
+            group5: 5,
+          }}
+          columns={{
+            group0: 6,
+            group1: 6,
+            group2: 6,
+            group3: 6,
+            group4: 6,
+            group5: 12,
+          }}
+        >
           <Link to="/pidgin/23248703" data-e2e="cpsAssetDummyLink">
             Test MAP to MAP inline link
           </Link>
-        </GridItemConstrainedMedium>
+        </Grid>
         <Blocks blocks={blocks} componentsToRender={componentsToRender} />
-      </GhostGrid>
+      </GelPageGridGhost>
     </>
   );
 };
