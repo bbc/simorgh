0ecff4b7ffe2d7a7cd2d4d6b9269bc7ab729472a
Sareh Heidari <sareh@users.noreply.github.com>
2019-10-31 10:34:23 +0000

    Update bundle min/max sizes

--- scripts/bundleSizeConfig.js
+++ scripts/bundleSizeConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Size limit for all bundles used by each service (K)
   // Keep these +/- 5K and update frequently!
-  MIN_SIZE: 559,
-  MAX_SIZE: 605,
+  MIN_SIZE: 562,
+  MAX_SIZE: 608,
 };
