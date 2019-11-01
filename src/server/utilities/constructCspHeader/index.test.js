0c5259364906334af722d8953857469019d4c4f0
DemonusPC <andrzej.pogonowski@bbc.co.uk>
2019-11-01 11:02:34 +0000

    Final testing done

--- src/server/utilities/constructCspHeader/index.test.js
+++ src/server/utilities/constructCspHeader/index.test.js
@@ -44,40 +44,24 @@ describe('Construct CSP Header', () => {
     expect(result).toEqual(expected);
   });
 
-  it('should be able to generate the default live amp script src', async () => {
+  const testAmpScript = (isAmp, isLive) => {
     const expected = [
       'https://cdn.ampproject.org',
       'https://*.chartbeat.com',
       'https://*.go-mpulse.net',
       "'unsafe-inline'",
     ];
-    const result = generateScriptSrc(true, true);
+    const result = generateScriptSrc(isAmp, isLive);
 
     expect(result).toEqual(expected);
+  };
+
+  it('should be able to generate the default live amp script src', async () => {
+    testAmpScript(true, true);
   });
 
   it('should be able to generate the default test amp script src', async () => {
-    const expected = [
-      'https://cdn.ampproject.org',
-      'https://*.chartbeat.com',
-      'https://*.go-mpulse.net',
-      "'unsafe-inline'",
-    ];
-    const result = generateScriptSrc(true, false);
-
-    expect(result).toEqual(expected);
-  });
-
-  it('should be able to generate the amp script src', async () => {
-    const expected = [
-      'https://cdn.ampproject.org',
-      'https://*.chartbeat.com',
-      'https://*.go-mpulse.net',
-      "'unsafe-inline'",
-    ];
-    const result = generateScriptSrc(true, true);
-
-    expect(result).toEqual(expected);
+    testAmpScript(true, false);
   });
 
   it('should be able to generate the live img src', async () => {
@@ -126,7 +110,7 @@ describe('Construct CSP Header', () => {
     expect(result).toEqual(expected);
   };
 
-  it('should be able to generate the live connect src when in the uk', async () => {
+  it('should be able to generate the live connect src when in the uk on cannonical', async () => {
     testConnect(
       [
         'https://a1.api.bbc.co.uk/hit.xiti',
@@ -139,7 +123,7 @@ describe('Construct CSP Header', () => {
     );
   });
 
-  it('should be able to generate the test connect src when in the uk', async () => {
+  it('should be able to generate the test connect src when in the uk on cannonical', async () => {
     testConnect(
       [
         'https://logws1363.ati-host.net',
@@ -153,7 +137,7 @@ describe('Construct CSP Header', () => {
     );
   });
 
-  it('should be able to generate the live connect src when not in the uk', async () => {
+  it('should be able to generate the live connect src when not in the uk on cannonical', async () => {
     testConnect(
       [
         'https://a1.api.bbc.co.uk/hit.xiti',
@@ -166,7 +150,7 @@ describe('Construct CSP Header', () => {
     );
   });
 
-  it('should be able to generate the test connect src when not in the uk', async () => {
+  it('should be able to generate the test connect src when not in the uk on cannonical', async () => {
     testConnect(
       [
         'https://logws1363.ati-host.net',
@@ -181,16 +165,47 @@ describe('Construct CSP Header', () => {
   });
 
   // Amp, NonUk, Test
-
-  it('connect src: amp, Nonuk test', async () => {
+  it('should be able to generate the test connect src when not in the uk on amp', async () => {
     testConnect(['https://logws1363.ati-host.net'], true, false, false);
   });
 
   // Amp NonUk, Live
-  it('connect src: amp, Nonuk live', async () => {
+  it('should be able to generate the live connect src when not in the uk on amp', async () => {
     testConnect(['https://a1.api.bbc.co.uk/hit.xiti'], true, false, true);
   });
+
+  it('should be able to generate the test connect src when in the uk on amp', async () => {
+    testConnect(['https://logws1363.ati-host.net'], true, true, false);
+  });
+
+  it('should be able to generate the live connect src when in the uk on amp', async () => {
+    testConnect(['https://a1.api.bbc.co.uk/hit.xiti'], true, true, true);
+  });
 });
+
+const testMiddleware = (origin, path, cspString) => {
+  const req = {
+    url: origin + path,
+    headers: {
+      'user-agent': 'local-agent',
+      'bbc-origin': origin,
+    },
+  };
+
+  const headers = {};
+
+  const res = {
+    setHeader: (key, value) => {
+      headers[key] = value;
+    },
+  };
+
+  injectCspHeader(req, res, next);
+
+  expect(next).toHaveBeenCalled();
+
+  expect(headers['Content-Security-Policy']).toEqual(cspString);
+};
 
 describe('CSP Header Middleware', () => {
   afterEach(() => {
@@ -198,108 +213,70 @@ describe('CSP Header Middleware', () => {
   });
   // CSP header for live, in the uk on cannonical
   it('should be able to inject the csp header for live in the uk on cannon', async () => {
-    const req = {
-      url: 'https://bbc.co.uk/igbo',
-      headers: {
-        'user-agent': 'local-agent',
-        'bbc-origin': 'https://bbc.co.uk',
-      },
-    };
-
-    const headers = {};
-
-    const res = {
-      setHeader: (key, value) => {
-        headers[key] = value;
-      },
-    };
-
-    injectCspHeader(req, res, next);
-
-    expect(next).toHaveBeenCalled();
-
-    expect(headers['Content-Security-Policy']).toEqual(
+    testMiddleware(
+      'https://bbc.co.uk',
+      '/igbo',
       "default-src 'self'; font-src https://gel.files.bbci.co.uk https://ws-downloads.files.bbci.co.uk; style-src 'unsafe-inline'; img-src https://ichef.bbci.co.uk https://ping.chartbeat.net https://a1.api.bbc.co.uk/hit.xiti https://news.files.bbci.co.uk https://*.akstat.io https://r.bbci.co.uk data: 'self'; script-src https://news.files.bbci.co.uk https://*.chartbeat.com https://*.go-mpulse.net https://mybbc-analytics.files.bbci.co.uk https://emp.bbci.co.uk https://static.bbci.co.uk 'self' 'unsafe-inline'; connect-src https://*.akstat.io https://*.akamaihd.net https://c.go-mpulse.net https://a1.api.bbc.co.uk/hit.xiti 'self' https://cookie-oven.api.bbc.co.uk; frame-src 'self' https://emp.bbc.com https://emp.bbc.co.uk https://chartbeat.com https://*.chartbeat.com",
     );
   });
 
   // CSP header for live, in the uk on amp
   it('should be able to inject the csp header for live in the uk on amp', async () => {
-    const req = {
-      url: 'https://bbc.co.uk/igbo.amp',
-      headers: {
-        'user-agent': 'local-agent',
-        'bbc-origin': 'https://bbc.co.uk',
-      },
-    };
+    testMiddleware(
+      'https://bbc.co.uk',
+      '/igbo.amp',
+      "default-src 'self'; font-src https://gel.files.bbci.co.uk https://ws-downloads.files.bbci.co.uk; style-src 'unsafe-inline'; img-src https://ichef.bbci.co.uk https://ping.chartbeat.net https://a1.api.bbc.co.uk/hit.xiti https://news.files.bbci.co.uk https://*.akstat.io https://r.bbci.co.uk data: 'self'; script-src https://cdn.ampproject.org https://*.chartbeat.com https://*.go-mpulse.net 'unsafe-inline'; connect-src https://*.akstat.io https://*.akamaihd.net https://c.go-mpulse.net https://a1.api.bbc.co.uk/hit.xiti; frame-src 'self' https://emp.bbc.com https://emp.bbc.co.uk https://chartbeat.com https://*.chartbeat.com",
+    );
+  });
 
-    const headers = {};
+  it('should be able to inject the csp header for live outside the uk on cannon', async () => {
+    testMiddleware(
+      'https://bbc.com',
+      '/igbo',
+      "default-src 'self'; font-src https://gel.files.bbci.co.uk https://ws-downloads.files.bbci.co.uk; style-src 'unsafe-inline'; img-src https://ichef.bbci.co.uk https://ping.chartbeat.net https://a1.api.bbc.co.uk/hit.xiti https://news.files.bbci.co.uk https://*.akstat.io https://r.bbci.co.uk data: 'self'; script-src https://news.files.bbci.co.uk https://*.chartbeat.com https://*.go-mpulse.net https://mybbc-analytics.files.bbci.co.uk https://emp.bbci.co.uk https://static.bbci.co.uk 'self' 'unsafe-inline'; connect-src https://*.akstat.io https://*.akamaihd.net https://c.go-mpulse.net https://a1.api.bbc.co.uk/hit.xiti 'self' https://cookie-oven.api.bbc.com; frame-src 'self' https://emp.bbc.com https://emp.bbc.co.uk https://chartbeat.com https://*.chartbeat.com",
+    );
+  });
 
-    const res = {
-      setHeader: (key, value) => {
-        headers[key] = value;
-      },
-    };
-
-    injectCspHeader(req, res, next);
-
-    expect(next).toHaveBeenCalled();
-
-    expect(headers['Content-Security-Policy']).toEqual(
+  // CSP header for live, in the uk on amp
+  it('should be able to inject the csp header for live outside the uk on amp', async () => {
+    testMiddleware(
+      'https://bbc.com',
+      '/igbo.amp',
       "default-src 'self'; font-src https://gel.files.bbci.co.uk https://ws-downloads.files.bbci.co.uk; style-src 'unsafe-inline'; img-src https://ichef.bbci.co.uk https://ping.chartbeat.net https://a1.api.bbc.co.uk/hit.xiti https://news.files.bbci.co.uk https://*.akstat.io https://r.bbci.co.uk data: 'self'; script-src https://cdn.ampproject.org https://*.chartbeat.com https://*.go-mpulse.net 'unsafe-inline'; connect-src https://*.akstat.io https://*.akamaihd.net https://c.go-mpulse.net https://a1.api.bbc.co.uk/hit.xiti; frame-src 'self' https://emp.bbc.com https://emp.bbc.co.uk https://chartbeat.com https://*.chartbeat.com",
     );
   });
 
   // CSP header for test, in the uk on cannon
   it('should be able to inject the csp header for test in the uk on cannon', async () => {
-    const req = {
-      url: 'https://www.test.bbc.co.uk/igbo',
-      headers: {
-        'user-agent': 'local-agent',
-        'bbc-origin': 'https://www.test.bbc.co.uk/',
-      },
-    };
-
-    const headers = {};
-
-    const res = {
-      setHeader: (key, value) => {
-        headers[key] = value;
-      },
-    };
-
-    injectCspHeader(req, res, next);
-
-    expect(next).toHaveBeenCalled();
-
-    expect(headers['Content-Security-Policy']).toEqual(
+    testMiddleware(
+      'https://www.test.bbc.co.uk',
+      '/igbo',
       "default-src 'self'; font-src https://gel.files.bbci.co.uk https://ws-downloads.files.bbci.co.uk; style-src 'unsafe-inline'; img-src https://ichef.bbci.co.uk https://ping.chartbeat.net https://a1.api.bbc.co.uk/hit.xiti https://news.files.bbci.co.uk https://*.akstat.io https://r.bbci.co.uk https://ichef.test.bbci.co.uk https://news.test.files.bbci.co.uk https://logws1363.ati-host.net data: 'self'; script-src https://news.files.bbci.co.uk https://*.chartbeat.com https://*.go-mpulse.net https://mybbc-analytics.files.bbci.co.uk https://emp.bbci.co.uk https://static.bbci.co.uk 'self' 'unsafe-inline' https://news.test.files.bbci.co.uk; connect-src https://*.akstat.io https://*.akamaihd.net https://c.go-mpulse.net https://logws1363.ati-host.net 'self' https://cookie-oven.api.bbc.co.uk https://cookie-oven.test.api.bbc.co.uk; frame-src 'self' https://emp.bbc.com https://emp.bbc.co.uk https://chartbeat.com https://*.chartbeat.com",
     );
   });
 
   // CSP header for test, in the uk on amp
   it('should be able to inject the csp header for test in the uk on amp', async () => {
-    const req = {
-      url: 'https://www.test.bbc.co.uk/igbo.amp',
-      headers: {
-        'user-agent': 'local-agent',
-        'bbc-origin': 'https://www.test.bbc.co.uk/',
-      },
-    };
+    testMiddleware(
+      'https://www.test.bbc.co.uk',
+      '/igbo.amp',
+      "default-src 'self'; font-src https://gel.files.bbci.co.uk https://ws-downloads.files.bbci.co.uk; style-src 'unsafe-inline'; img-src https://ichef.bbci.co.uk https://ping.chartbeat.net https://a1.api.bbc.co.uk/hit.xiti https://news.files.bbci.co.uk https://*.akstat.io https://r.bbci.co.uk https://ichef.test.bbci.co.uk https://news.test.files.bbci.co.uk https://logws1363.ati-host.net data: 'self'; script-src https://cdn.ampproject.org https://*.chartbeat.com https://*.go-mpulse.net 'unsafe-inline'; connect-src https://*.akstat.io https://*.akamaihd.net https://c.go-mpulse.net https://logws1363.ati-host.net; frame-src 'self' https://emp.bbc.com https://emp.bbc.co.uk https://chartbeat.com https://*.chartbeat.com",
+    );
+  });
 
-    const headers = {};
+  it('should be able to inject the csp header for test in the uk on cannon', async () => {
+    testMiddleware(
+      'https://www.test.bbc.com',
+      '/igbo',
+      "default-src 'self'; font-src https://gel.files.bbci.co.uk https://ws-downloads.files.bbci.co.uk; style-src 'unsafe-inline'; img-src https://ichef.bbci.co.uk https://ping.chartbeat.net https://a1.api.bbc.co.uk/hit.xiti https://news.files.bbci.co.uk https://*.akstat.io https://r.bbci.co.uk https://ichef.test.bbci.co.uk https://news.test.files.bbci.co.uk https://logws1363.ati-host.net data: 'self'; script-src https://news.files.bbci.co.uk https://*.chartbeat.com https://*.go-mpulse.net https://mybbc-analytics.files.bbci.co.uk https://emp.bbci.co.uk https://static.bbci.co.uk 'self' 'unsafe-inline' https://news.test.files.bbci.co.uk; connect-src https://*.akstat.io https://*.akamaihd.net https://c.go-mpulse.net https://logws1363.ati-host.net 'self' https://cookie-oven.api.bbc.com https://cookie-oven.test.api.bbc.com; frame-src 'self' https://emp.bbc.com https://emp.bbc.co.uk https://chartbeat.com https://*.chartbeat.com",
+    );
+  });
 
-    const res = {
-      setHeader: (key, value) => {
-        headers[key] = value;
-      },
-    };
-
-    injectCspHeader(req, res, next);
-
-    expect(next).toHaveBeenCalled();
-
-    expect(headers['Content-Security-Policy']).toEqual(
+  // CSP header for test, in the uk on amp
+  it('should be able to inject the csp header for test in the uk on amp', async () => {
+    testMiddleware(
+      'https://www.test.bbc.com',
+      '/igbo.amp',
       "default-src 'self'; font-src https://gel.files.bbci.co.uk https://ws-downloads.files.bbci.co.uk; style-src 'unsafe-inline'; img-src https://ichef.bbci.co.uk https://ping.chartbeat.net https://a1.api.bbc.co.uk/hit.xiti https://news.files.bbci.co.uk https://*.akstat.io https://r.bbci.co.uk https://ichef.test.bbci.co.uk https://news.test.files.bbci.co.uk https://logws1363.ati-host.net data: 'self'; script-src https://cdn.ampproject.org https://*.chartbeat.com https://*.go-mpulse.net 'unsafe-inline'; connect-src https://*.akstat.io https://*.akamaihd.net https://c.go-mpulse.net https://logws1363.ati-host.net; frame-src 'self' https://emp.bbc.com https://emp.bbc.co.uk https://chartbeat.com https://*.chartbeat.com",
     );
   });
