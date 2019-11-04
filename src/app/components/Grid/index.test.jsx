c9941d4466a3f23fbc5a9fa979873bb0afb26b10
Sareh Heidari <sareh@users.noreply.github.com>
2019-11-04 14:08:28 +0000

    Add snapshot tests and stories to Grid component

---
+++ src/app/components/Grid/index.test.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import Grid, { GelPageGrid, GridMaxWidthGroup4 } from '.';
+
+const outerGridProps = {
+  columns: {
+    group0: 6,
+    group1: 6,
+    group2: 6,
+    group3: 6,
+    group4: 6,
+    group5: 6,
+  },
+};
+
+const gridItemProps = {
+  item: true,
+  columns: {
+    group0: 2,
+    group1: 2,
+    group2: 2,
+    group3: 2,
+    group4: 2,
+    group5: 2,
+  },
+};
+
+describe('Grid', () => {
+  shouldMatchSnapshot(
+    'should render Grid with columns',
+    <Grid {...outerGridProps}>
+      <Grid {...gridItemProps}>
+        <p>Test</p>
+      </Grid>
+    </Grid>,
+  );
+
+  shouldMatchSnapshot(
+    'should render GelPageGrid with columns',
+    <GelPageGrid {...outerGridProps}>
+      <Grid {...gridItemProps}>
+        <p>Test</p>
+      </Grid>
+    </GelPageGrid>,
+  );
+
+  shouldMatchSnapshot(
+    'should render GridMaxWidthGroup4 with columns',
+    <GridMaxWidthGroup4 {...outerGridProps}>
+      <Grid {...gridItemProps}>
+        <p>Test</p>
+      </Grid>
+    </GridMaxWidthGroup4>,
+  );
+});
