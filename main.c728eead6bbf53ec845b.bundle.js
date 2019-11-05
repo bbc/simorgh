46925fd8b2f812db1c2a695752e5ef67d2fb1139
BBC News CI <foo@bar.com>
2019-11-05 15:58:34 +0000

    Updates

--- main.c728eead6bbf53ec845b.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{438:function(n,o,c){c(439),c(570),n.exports=c(893)},495:function(n,o){},570:function(n,o,c){"use strict";c.r(o);c(571)}},[[438,1,2]]]);
