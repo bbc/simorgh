02f549f654f197586a577ce7c2d08b9e095df8df
RayNjeri <rachael.njeri@andela.com>
2019-11-07 09:06:54 +0300

    Add guidance prop to mediaInfo
    Signed-off-by: RayNjeri <rachael.njeri@andela.com>

--- src/app/containers/MediaPlayer/index.jsx
+++ src/app/containers/MediaPlayer/index.jsx
@@ -70,6 +70,11 @@ const MediaPlayerContainer = ({
       aresMediaBlock,
     ),
     type: format === 'audio' ? 'audio' : 'video',
+    guidanceMessage: pathOr(
+      null,
+      ['model', 'blocks', 0, 'model', 'versions', 0, 'warnings', 'short'],
+      aresMediaBlock,
+    ),
   };
 
   if (!versionId) {
