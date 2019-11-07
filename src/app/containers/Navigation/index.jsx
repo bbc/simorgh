980cf15521182813fada9438e9449e599df9a35e
J Blaut <jagoda.blaut@bbc.co.uk>
2019-11-07 15:40:18 +0000

    add nav example

--- src/app/containers/Navigation/index.jsx
+++ src/app/containers/Navigation/index.jsx
@@ -4,13 +4,35 @@ import Navigation, {
   NavigationLi,
 } from '@bbc/psammead-navigation';
 import { ServiceContext } from '#contexts/ServiceContext';
+import { RequestContext } from '#contexts/RequestContext';
+import { EventContext } from '#contexts/EventContext';
+import { sendEventBeacon } from '../ATIAnalytics/beacon';
+import { buildATIClickParams } from '../ATIAnalytics/params';
 
 const NavigationContainer = () => {
   const { script, translations, navigation, service, dir } = useContext(
     ServiceContext,
   );
-
+  const eventTrackingProps = buildATIClickParams(
+    useContext(RequestContext),
+    useContext(ServiceContext),
+  );
   const { currentPage, skipLinkText } = translations;
+
+  const { useClickTracker } = useContext(EventContext);
+
+  useClickTracker('[data-navigation]', event => {
+    const componentName = 'navigation';
+    const eventData = event.srcElement.dataset[componentName];
+
+    sendEventBeacon({
+      ...eventTrackingProps,
+      element: event.target,
+      componentName,
+      type: 'click',
+      componentInfo: `${componentName}-${eventData}`,
+    });
+  });
 
   if (!navigation || navigation.length === 0) {
     return null;
@@ -36,6 +58,7 @@ const NavigationContainer = () => {
               active={active}
               currentPageText={currentPage}
               service={service}
+              data-navigation={`${title.split(' ').join('-')}_${index}`}
             >
               {title}
             </NavigationLi>
