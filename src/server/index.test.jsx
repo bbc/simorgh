038de56548550ae1f6bd76843390c7e1943d9ed6
Ryan McCombe <ryanmccombe@gmail.com>
2019-11-11 16:28:08 +0000

    Change unit test service

--- src/server/index.test.jsx
+++ src/server/index.test.jsx
@@ -582,7 +582,7 @@ describe('Server', () => {
 
     describe('for front pages', () => {
       it('should respond with JSON', async () => {
-        const { body } = await makeRequest('/igbo.json');
+        const { body } = await makeRequest('/pidgin.json');
         expect(body).toEqual(
           expect.objectContaining({ content: expect.any(Object) }),
         );
