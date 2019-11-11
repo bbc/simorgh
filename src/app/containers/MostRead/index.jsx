df2b28c690152f62111a43b85fdef300dd00df7e
thekp <khoa.phan@bbc.co.uk>
2019-11-11 21:08:34 +0000

    return some data

--- src/app/containers/MostRead/index.jsx
+++ src/app/containers/MostRead/index.jsx
@@ -5,19 +5,35 @@ import { ServiceContext } from '#contexts/ServiceContext';
 const MostReadContainer = () => {
   const { service, variant } = useContext(ServiceContext);
 
-  const [data, setData] = useState();
+  const [data, setData] = useState({ records: [] });
+  // const [promos, setPromos] = useState([]);
 
-  const localEndpoint = variant
+  const localMostReadData = variant
     ? `http://localhost:7080/${service}/most_read/${variant}.json`
     : `http://localhost:7080/news/most_read.json`;
 
   useEffect(() => {
-    axios.get(localEndpoint).then(response => {
-      setData(response.data);
-    });
-  }, [localEndpoint]);
+    const fetchData = async () => {
+      const result = await axios(localMostReadData);
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      <p>Generated: {data.generated}</p>
+      <ul>
+        {data.records.slice(0, 10).map(record => (
+          <li key={record.id}>
+            <p>{record.promo.timestamp}</p>
+            <p>{record.promo.headlines.headline}</p>
+            <p>{record.promo.locators.assetUri}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default MostReadContainer;
