50248d5d9eb298e74ec2a0886503cc4f0d999906
Einstein <forensicslabnet@gmail.com>
2019-11-12 20:33:05 +0300

    old content timestamps
    Signed-off-by: Einstein <forensicslabnet@gmail.com>

--- src/app/containers/MostRead/utilities/index.js
+++ src/app/containers/MostRead/utilities/index.js
@@ -1,8 +1,12 @@
-const mostReadRecordIsFresh = lastRecordTimeStamp => {
+export const mostReadRecordIsFresh = lastRecordTimeStamp => {
   const now = Date.now();
   const utcTimestamp = Date.parse(lastRecordTimeStamp);
   const mostReadLifeSpan = 35 * 60 * 1000; // 35 minutes
   return now - utcTimestamp <= mostReadLifeSpan;
 };
 
-export default mostReadRecordIsFresh;
+const isMoreThan60Days = lastUpdated =>
+  lastUpdated < Date.now() - 60 * 24 * 60 * 60 * 1000; // 60 days
+
+export const shouldRenderLastUpdated = lastUpdated =>
+  lastUpdated && isMoreThan60Days(lastUpdated) ? lastUpdated : null;
--- src/app/containers/MostRead/utilities/index.test.js
+++ src/app/containers/MostRead/utilities/index.test.js
@@ -1,4 +1,4 @@
-import mostReadRecordIsFresh from '.';
+import { mostReadRecordIsFresh, shouldRenderLastUpdated } from '.';
 
 describe('mostReadRecordIsFresh', () => {
   it('should return true if 35 minutes ago or less', () => {
@@ -17,5 +17,21 @@ describe('mostReadRecordIsFresh', () => {
       false,
     );
     expect(mostReadRecordIsFresh(oneDayAgo.toUTCString())).toEqual(false);
+  });
+});
+
+describe('shouldRenderLastUpdated', () => {
+  it('should return lastUpdated time if older than 60 days', () => {
+    const sixtyOneDaysAgo = new Date(Date.now() - 61 * 24 * 60 * 60 * 1000);
+    const hundredDaysAgo = new Date(Date.now() - 100 * 24 * 60 * 60 * 1000);
+    expect(shouldRenderLastUpdated(sixtyOneDaysAgo)).toEqual(sixtyOneDaysAgo);
+    expect(shouldRenderLastUpdated(hundredDaysAgo)).toEqual(hundredDaysAgo);
+  });
+
+  it('should return null if less than 60 days old', () => {
+    const currentTime = new Date();
+    const fiftyNineDaysAgo = new Date(Date.now() - 59 * 24 * 60 * 60 * 1000);
+    expect(shouldRenderLastUpdated(currentTime)).toEqual(null);
+    expect(shouldRenderLastUpdated(fiftyNineDaysAgo)).toEqual(null);
   });
 });
