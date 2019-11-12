5ef3806ddb574bf33d7d20087b699c4efa5a7c39
sadickisaac <isaac.odhiambo@andela.com>
2019-11-12 12:16:12 +0300

    Add container to wrap frontpage

--- src/app/containers/FrontPageMain/index.jsx
+++ src/app/containers/FrontPageMain/index.jsx
@@ -1,15 +1,25 @@
 /* eslint-disable jsx-a11y/aria-role */
 import React, { useContext } from 'react';
 import VisuallyHiddenText from '@bbc/psammead-visually-hidden-text';
+import styled from 'styled-components';
+import { GEL_GROUP_4_SCREEN_WIDTH_MAX } from '@bbc/gel-foundations/breakpoints';
 import path from 'ramda/src/path';
 import { frontPageDataPropTypes } from '#models/propTypes/frontPage';
-import { Grid, GridItemConstrainedLargeWithTopMargin } from '#lib/styledGrid';
 import { ServiceContext } from '#contexts/ServiceContext';
 import FrontPageSection from '../FrontPageSection';
 import MetadataContainer from '../Metadata';
 import LinkedData from '../LinkedData';
 import ATIAnalytics from '../ATIAnalytics';
 import ChartbeatAnalytics from '../ChartbeatAnalytics';
+
+const FrontPageWrapper = styled.div`
+  padding-bottom: 2rem;
+  max-width: 80rem;
+  margin: 0 auto;
+  @media (max-width: ${GEL_GROUP_4_SCREEN_WIDTH_MAX}) {
+    padding: 0 1rem;
+  }
+`;
 
 const FrontPageMain = ({ frontPageData }) => {
   const {
@@ -48,17 +58,15 @@ const FrontPageMain = ({ frontPageData }) => {
         <VisuallyHiddenText id="content" tabIndex="-1" as="h1">
           {offScreenText}
         </VisuallyHiddenText>
-        <Grid>
-          <GridItemConstrainedLargeWithTopMargin>
-            {groups.map((group, index) => (
-              <FrontPageSection
-                key={group.title}
-                group={group}
-                sectionNumber={index}
-              />
-            ))}
-          </GridItemConstrainedLargeWithTopMargin>
-        </Grid>
+        <FrontPageWrapper>
+          {groups.map((group, index) => (
+            <FrontPageSection
+              key={group.title}
+              group={group}
+              sectionNumber={index}
+            />
+          ))}
+        </FrontPageWrapper>
       </main>
     </>
   );
