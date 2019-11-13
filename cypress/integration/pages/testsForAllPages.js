576f9f2cc0d4302a57bea1c591fb34ff11d527ee
PriyaKR <Priyatharsini.kuduvarajan@bbc.co.uk>
2019-11-13 14:40:55 +0000

    added cypress log text for testing

--- cypress/integration/pages/testsForAllPages.js
+++ cypress/integration/pages/testsForAllPages.js
@@ -390,7 +390,7 @@ export const testsThatNeverRunDuringSmokeTestingForAllPageTypes = ({
               failOnStatusCode: false,
             }).then(resp => {
               if (resp.status !== 200) {
-                Cypress.log('test');
+                Cypress.log('testdebugging');
               }
             });
           });
