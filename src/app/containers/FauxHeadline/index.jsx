12ef63160f3ae04ec6fa0463d3bcd3300a934350
Sareh Heidari <sareh@users.noreply.github.com>
2019-11-13 12:26:27 +0000

    Merge FauxHeadline from latest & use Grid

---
+++ src/app/containers/FauxHeadline/index.jsx
@@ -0,0 +1,64 @@
+import React, { useContext } from 'react';
+import path from 'ramda/src/path';
+import { Headline } from '@bbc/psammead-headings';
+import { textDefaultPropTypes } from '#models/propTypes';
+import { ServiceContext } from '#contexts/ServiceContext';
+import { headlineModelPropTypes } from '#models/propTypes/headline';
+import Fragment from '../Fragment';
+import Blocks from '../Blocks';
+import Grid from '#app/components/Grid';
+
+// missing CSS 'display: block;' in psammead branch `explicitly-set-h1-styles-display-and-font-weight`
+const FauxHeadline = props => <Headline as="strong" {...props} />;
+
+const FauxHeadlineContainer = ({ blocks }) => {
+  const { script, service } = useContext(ServiceContext);
+
+  const arrayOfFragments = path(
+    ['0', 'model', 'blocks', '0', 'model', 'blocks'],
+    blocks,
+  );
+
+  if (!arrayOfFragments || !Array.isArray(arrayOfFragments)) {
+    return null;
+  }
+  const componentsToRender = { fragment: Fragment };
+
+  const renderText = () => (
+    <Blocks blocks={arrayOfFragments} componentsToRender={componentsToRender} />
+  );
+
+  return (
+    <Grid
+      item
+      startOffset={{
+        group0: 1,
+        group1: 1,
+        group2: 1,
+        group3: 1,
+        group4: 2,
+        group5: 5,
+      }}
+      columns={{
+        group0: 6,
+        group1: 6,
+        group2: 6,
+        group3: 6,
+        group4: 6,
+        group5: 12,
+      }}
+    >
+      <FauxHeadline script={script} service={service} aria-hidden="true">
+        {renderText()}
+      </FauxHeadline>
+    </Grid>
+  );
+};
+
+FauxHeadlineContainer.propTypes = {
+  ...headlineModelPropTypes,
+};
+
+FauxHeadlineContainer.defaultProps = textDefaultPropTypes;
+
+export default FauxHeadlineContainer;
