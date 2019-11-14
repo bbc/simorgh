ab3e37df2ec62264c79da3a023f2a3edc5163291
thekp <khoa.phan@bbc.co.uk>
2019-11-14 16:28:30 +0000

    add test for Zhongwen

--- src/app/containers/MostRead/index.test.jsx
+++ src/app/containers/MostRead/index.test.jsx
@@ -5,46 +5,53 @@ import MostReadContainer from '.';
 import { RequestContextProvider } from '#contexts/RequestContext';
 import { ServiceContextProvider } from '#contexts/ServiceContext';
 import newsMostReadData from '#data/news/mostRead';
+import zhongwenSimpMostReadData from '#data/zhongwen/mostRead/simp';
 
 let container;
 
-const mostReadContainerWithContext = (service, variant = null) => (
-  <RequestContextProvider
-    bbcOrigin={`http://localhost:7080/${service}/articles/c0000000000o`}
-    id="c0000000000o"
-    isAmp={false}
-    pageType="article"
-    service={service}
-    statusCode={200}
-    pathname={`/${service}`}
-    variant={variant}
-  >
-    <ServiceContextProvider service={service}>
-      <MostReadContainer />,
-    </ServiceContextProvider>
-  </RequestContextProvider>
-);
+const renderMostReadContainer = async ({ service, variant = null }) =>
+  act(async () => {
+    ReactDOM.render(
+      <RequestContextProvider
+        bbcOrigin={`http://localhost:7080/${service}/articles/c0000000000o`}
+        id="c0000000000o"
+        isAmp={false}
+        pageType="article"
+        service={service}
+        statusCode={200}
+        pathname={`/${service}`}
+        variant={variant}
+      >
+        <ServiceContextProvider service={service} variant={variant}>
+          <MostReadContainer />,
+        </ServiceContextProvider>
+      </RequestContextProvider>,
+      container,
+    );
+  });
 
 describe('MostReadContainer', () => {
-  beforeEach(() => {
+  beforeAll(() => {
+    container = null;
     container = document.createElement('div');
     document.body.appendChild(container);
     fetch.resetMocks();
   });
 
-  afterEach(() => {
-    document.body.removeChild(container);
-    container = null;
+  it('test data returns as expected on canonical for News', async () => {
+    fetch.mockResponse(JSON.stringify(newsMostReadData));
+    await renderMostReadContainer({ service: 'news' });
+    expect(container.querySelector('p').textContent).toEqual(
+      `Generated: ${newsMostReadData.generated}`,
+    );
+    expect(container.querySelectorAll('ul').length).toEqual(10);
   });
 
-  it('test data returns as expected on canonical for News', async () => {
-    await act(async () => {
-      fetch.mockResponse(JSON.stringify(newsMostReadData));
-      ReactDOM.render(mostReadContainerWithContext('news'), container);
-    });
-
-    expect(container.querySelector('p').textContent).toBe(
-      `Generated: ${newsMostReadData.generated}`,
+  it('test data returns as expected on canonical for Zhongwen Simp', async () => {
+    fetch.mockResponse(JSON.stringify(zhongwenSimpMostReadData));
+    await renderMostReadContainer({ service: 'zhongwen', variant: 'simp' });
+    expect(container.querySelector('p').textContent).toEqual(
+      `Generated: ${zhongwenSimpMostReadData.generated}`,
     );
     expect(container.querySelectorAll('ul').length).toEqual(10);
   });
