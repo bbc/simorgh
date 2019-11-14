b4ccf9c7af7e1c601b8ebc66f30ef8507b4001d5
thekp <khoa.phan@bbc.co.uk>
2019-11-14 14:57:40 +0000

    wip

--- src/app/containers/MostRead/index.test.jsx
+++ src/app/containers/MostRead/index.test.jsx
@@ -6,9 +6,26 @@ import { RequestContextProvider } from '#contexts/RequestContext';
 import { ServiceContextProvider } from '#contexts/ServiceContext';
 import newsMostReadData from '#data/news/mostRead';
 
-describe('MostReadContainer', () => {
-  let container;
+let container;
 
+const mostReadContainerWithContext = (service, variant = null) => (
+  <RequestContextProvider
+    bbcOrigin={`http://localhost:7080/${service}/articles/c0000000000o`}
+    id="c0000000000o"
+    isAmp={false}
+    pageType="article"
+    service={service}
+    statusCode={200}
+    pathname={`/${service}`}
+    variant={variant}
+  >
+    <ServiceContextProvider service={service}>
+      <MostReadContainer />,
+    </ServiceContextProvider>
+  </RequestContextProvider>
+);
+
+describe('MostReadContainer', () => {
   beforeEach(() => {
     container = document.createElement('div');
     document.body.appendChild(container);
@@ -19,23 +36,6 @@ describe('MostReadContainer', () => {
     document.body.removeChild(container);
     container = null;
   });
-
-  const mostReadContainerWithContext = (service, variant = null) => (
-    <RequestContextProvider
-      bbcOrigin={`http://localhost:7080/${service}/articles/c0000000000o`}
-      id="c0000000000o"
-      isAmp={false}
-      pageType="article"
-      service={service}
-      statusCode={200}
-      pathname={`/${service}`}
-      variant={variant}
-    >
-      <ServiceContextProvider service={service}>
-        <MostReadContainer />,
-      </ServiceContextProvider>
-    </RequestContextProvider>
-  );
 
   it('test data returns as expected on canonical for News', async () => {
     await act(async () => {
