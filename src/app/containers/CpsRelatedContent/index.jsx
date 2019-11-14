4951d1affc5d8ac02509ff308b52877deb16fce4
Ryan McCombe <ryanmccombe@gmail.com>
2019-11-14 15:19:00 +0000

    Fix warnings

--- src/app/containers/CpsRelatedContent/index.jsx
+++ src/app/containers/CpsRelatedContent/index.jsx
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react';
+import { arrayOf, shape } from 'prop-types';
 import SectionLabel from '@bbc/psammead-section-label';
 import styled from 'styled-components';
 import { StoryPromoLi, StoryPromoUl } from '@bbc/psammead-story-promo-list';
+
+import { storyItem } from '#models/propTypes/storyItem';
+import { ServiceContext } from '#contexts/ServiceContext';
 import { GridItemConstrainedLarge } from '#lib/styledGrid';
 import StoryPromo from '../StoryPromo';
-import { ServiceContext } from '#contexts/ServiceContext';
 
 // This is required as the psammead section label component uses negative z-indices
 const Wrapper = styled(GridItemConstrainedLarge)`
@@ -16,9 +19,8 @@ const isValidItem = item => {
 };
 
 const CpsRelatedContent = ({ content }) => {
-  if (!content.length) return null;
-
   const { script, service, dir, translations } = useContext(ServiceContext);
+  if (!content.length) return null;
 
   return (
     <Wrapper aria-labelledby="related-content-heading">
@@ -33,8 +35,8 @@ const CpsRelatedContent = ({ content }) => {
 
       <StoryPromoUl>
         {content.filter(isValidItem).map(item => (
-          <StoryPromoLi>
-            <StoryPromo key={item.id} item={item} />
+          <StoryPromoLi key={item.id}>
+            <StoryPromo item={item} />
           </StoryPromoLi>
         ))}
       </StoryPromoUl>
@@ -42,6 +44,12 @@ const CpsRelatedContent = ({ content }) => {
   );
 };
 
-CpsRelatedContent.propTypes = {};
+CpsRelatedContent.propTypes = {
+  content: arrayOf(shape(storyItem)),
+};
+
+CpsRelatedContent.defaultProps = {
+  content: [],
+};
 
 export default CpsRelatedContent;
