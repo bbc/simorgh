0bc4895e39bb9875a6cbbd402d4b8a037f58ba8c
Sareh Heidari <sareh@users.noreply.github.com>
2019-11-19 12:47:05 +0000

    Create ArticlePageGrid export

--- src/app/components/Grid/index.jsx
+++ src/app/components/Grid/index.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { node, oneOf } from 'prop-types';
 import styled, { css } from 'styled-components';
 import Grid from '@bbc/psammead-grid';
 import {
@@ -11,11 +13,12 @@ const group4WrapperMaxWidth = `63rem`; // 1008px
 const group5WrapperMaxWidth = `80rem`; // 1280px
 
 const gelMaxWidths = css`
-  margin: 0 auto;
   @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_4_SCREEN_WIDTH_MAX}) {
+    margin: 0 auto;
     max-width: ${group4WrapperMaxWidth};
   }
   @media (min-width: ${GEL_GROUP_5_SCREEN_WIDTH_MIN}) {
+    margin: 0 auto;
     max-width: ${group5WrapperMaxWidth};
   }
 `;
@@ -37,3 +40,28 @@ export const GridMaxWidthGroup4 = styled(Grid)`
 `;
 
 export default Grid;
+
+export const ArticlePageGrid = ({ children, dir }) => (
+  <GelPageGrid
+    dir={dir}
+    columns={{
+      group0: 6,
+      group1: 6,
+      group2: 6,
+      group3: 6,
+      group4: 8,
+      group5: 20,
+    }}
+    enableGelGutters
+  >
+    {children}
+  </GelPageGrid>
+);
+
+ArticlePageGrid.propTypes = {
+  children: node.isRequired,
+  dir: oneOf[('ltr', 'rtl')],
+};
+ArticlePageGrid.defaultProps = {
+  dir: 'ltr',
+};
