fdc23fd1284b1cb5709f2073194a79f2180a51c1
Ryan McCombe <ryanmccombe@gmail.com>
2019-11-19 18:05:33 +0000

    Initial commit (todo: tests & refactor)

--- src/app/lib/utilities/preprocessor/rules/cpsAssetPage/convertToOptimoBlocks/blocks/paragraph/helpers.js
+++ src/app/lib/utilities/preprocessor/rules/cpsAssetPage/convertToOptimoBlocks/blocks/paragraph/helpers.js
@@ -3,14 +3,23 @@ import path from 'ramda/src/path';
 
 const boldWrap = text => `<bold>${text}</bold>`;
 
-const escapeDoubleQuotes = text => text.replace(/&quot;/g, '"');
+const replacements = {
+  '&quot;': '"',
+  '&amp;': '&',
+  '&lt;': '<',
+  '&gt;': '>',
+};
+const replacementsRegex = new RegExp(Object.keys(replacements).join('|'), 'gi');
+
+const parseReplacements = text =>
+  text.replace(replacementsRegex, match => replacements[match]);
 
 export const processBlock = _block => {
   const block = _block;
 
   if (path(['text'], block)) {
     // escape quotes in all text
-    block.text = escapeDoubleQuotes(block.text);
+    block.text = parseReplacements(block.text);
 
     // make introductions bold
     if (block.role === 'introduction') {
