d4e8058425ce0ef68858f5aa0ee8743f8c80cf35
Fahad Khan <fahad.khan01@bbc.co.uk>
2019-11-20 15:47:23 +0000

    Arabic LIVE label translation

--- src/app/lib/config/services/arabic.js
+++ src/app/lib/config/services/arabic.js
@@ -151,7 +151,7 @@ export const service = {
         },
         listen: 'Listen',
         watch: 'Watch',
-        liveLabel: 'LIVE',
+        liveLabel: 'مباشر',
         previousRadioShow: 'Previous radio show',
         nextRadioShow: 'Next radio show',
         duration: 'Duration',
