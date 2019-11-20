58d81c6503863e7835102c6e31d49ddb92d357fa
Fahad Khan <fahad.khan01@bbc.co.uk>
2019-11-20 17:36:41 +0000

    Yoruba liveLabel translation

--- src/app/lib/config/services/yoruba.js
+++ src/app/lib/config/services/yoruba.js
@@ -146,7 +146,7 @@ export const service = {
         },
         listen: 'Listen',
         watch: 'Watch',
-        liveLabel: 'LIVE',
+        liveLabel: 'NÍ YÀJÓYÀJÓ',
         previousRadioShow: 'Previous radio show',
         nextRadioShow: 'Next radio show',
         duration: 'Duration',
