98e94d964cb3d189b58da537e1db69352cb7b507
James Donohue <james.donohue@bbc.co.uk>
2019-11-24 08:13:49 +0000

    Fix more test naming

--- cypress/integration/pages/testsForAllAMPPages.js
+++ cypress/integration/pages/testsForAllAMPPages.js
@@ -12,37 +12,39 @@ export const testsThatFollowSmokeTestConfigForAllAMPPages = ({
   pageType,
 }) => {
   describe(service, () => {
-    if (pageType !== 'errorPage404') {
-      it('should have an AMP attribute on the page', () => {
-        cy.get('html').should('have.attr', 'amp');
-      });
-
-      it('should load the core AMP scripts in the head', () => {
-        const ampScripts = [
-          'https://cdn.ampproject.org/v0.js',
-          'https://cdn.ampproject.org/v0/amp-geo-0.1.js',
-          'https://cdn.ampproject.org/v0/amp-consent-0.1.js',
-          'https://cdn.ampproject.org/v0/amp-analytics-0.1.js',
-        ];
-
-        ampScripts.forEach(script => {
-          cy.get(`head > script[src="${script}"]`);
+    describe('General', () => {
+      if (pageType !== 'errorPage404') {
+        it('should have an AMP attribute on the page', () => {
+          cy.get('html').should('have.attr', 'amp');
         });
-      });
 
-      it('should include AMP elements with JSON configuration in the body', () => {
-        cy.get('body amp-geo > script[type="application/json"]');
-        cy.get('body amp-consent > script[type="application/json"]');
-      });
+        it('should load the core AMP scripts in the head', () => {
+          const ampScripts = [
+            'https://cdn.ampproject.org/v0.js',
+            'https://cdn.ampproject.org/v0/amp-geo-0.1.js',
+            'https://cdn.ampproject.org/v0/amp-consent-0.1.js',
+            'https://cdn.ampproject.org/v0/amp-analytics-0.1.js',
+          ];
 
-      if (Cypress.env('SMOKE')) {
-        describe('ATI', () => {
-          it('should have an amp-analytics tag with the ati url', () => {
-            cy.hasAmpAnalyticsAtiUrl(envConfig.atiUrl);
+          ampScripts.forEach(script => {
+            cy.get(`head > script[src="${script}"]`);
           });
         });
+
+        it('should include AMP elements with JSON configuration in the body', () => {
+          cy.get('body amp-geo > script[type="application/json"]');
+          cy.get('body amp-consent > script[type="application/json"]');
+        });
+
+        if (Cypress.env('SMOKE')) {
+          describe('ATI', () => {
+            it('should have an amp-analytics tag with the ati url', () => {
+              cy.hasAmpAnalyticsAtiUrl(envConfig.atiUrl);
+            });
+          });
+        }
       }
-    }
+    });
   });
 };
 
