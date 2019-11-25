4d4b2bf9e603f142d5c54cbf310a6e2f09e7de6f
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2019-11-25 19:33:56 +0000

    Move fixtures

--- src/app/containers/BulletedList/index.test.jsx
+++ src/app/containers/BulletedList/index.test.jsx
@@ -3,152 +3,16 @@ import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import { latin, arabic } from '@bbc/gel-foundations/scripts';
 import BulletedListContainer from './index';
 import { ServiceContext } from '#contexts/ServiceContext';
+import { orderedList } from './fixtures';
 
 describe('BulletedListContainer', () => {
   describe('with data', () => {
-    const ltrData = {
-      blocks: [
-        {
-          id: 'mock-id-1',
-          type: 'listItem',
-          model: {
-            blocks: [
-              {
-                id: 'mock-id-a',
-                type: 'paragraph',
-                model: {
-                  text: 'Here is a list',
-                  blocks: [
-                    {
-                      id: 'mock-id-i',
-                      type: 'fragment',
-                      model: {
-                        text: 'Here is a list',
-                        attributes: [],
-                      },
-                    },
-                  ],
-                },
-              },
-            ],
-          },
-        },
-        {
-          id: 'mock-id-2',
-          type: 'listItem',
-          model: {
-            blocks: [
-              {
-                id: 'mock-id-b',
-                type: 'paragraph',
-                model: {
-                  text: 'It is unordered',
-                  blocks: [
-                    {
-                      id: 'mock-id-i',
-                      type: 'fragment',
-                      model: {
-                        text: 'It is unordered',
-                        attributes: [],
-                      },
-                    },
-                  ],
-                },
-              },
-            ],
-          },
-        },
-        {
-          id: 'mock-id-3',
-          type: 'listItem',
-          model: {
-            blocks: [
-              {
-                id: 'mock-id-c',
-                type: 'paragraph',
-                model: {
-                  text: 'It has three list items',
-                  blocks: [
-                    {
-                      id: 'mock-id-i',
-                      type: 'fragment',
-                      model: {
-                        text: 'It has three list items',
-                        attributes: [],
-                      },
-                    },
-                  ],
-                },
-              },
-            ],
-          },
-        },
-      ],
-    };
-
-    const rtlData = {
-      blocks: [
-        {
-          id: 'mock-id-4',
-          type: 'listItem',
-          model: {
-            blocks: [
-              {
-                id: 'mock-id-a',
-                type: 'paragraph',
-                model: {
-                  text: 'It is unordered',
-                  blocks: [
-                    {
-                      id: 'mock-id-i',
-                      type: 'fragment',
-                      model: {
-                        text: 'It is unordered',
-                        attributes: [],
-                      },
-                    },
-                  ],
-                },
-              },
-            ],
-          },
-        },
-        {
-          id: 'mock-id-5',
-          type: 'listItem',
-          model: {
-            blocks: [
-              {
-                id: 'mock-id-b',
-                type: 'paragraph',
-                model: {
-                  text: 'It has three list items',
-                  blocks: [
-                    {
-                      id: 'mock-id-i',
-                      type: 'fragment',
-                      model: {
-                        text: 'It has three list items',
-                        attributes: [],
-                      },
-                    },
-                  ],
-                },
-              },
-            ],
-          },
-        },
-      ],
-    };
-
-    // console.log(BulletedListContainer.propTypes);
-
     shouldMatchSnapshot(
       'should render ltr correctly',
       <ServiceContext.Provider
         value={{ script: latin, service: 'news', dir: 'ltr' }}
       >
-        <BulletedListContainer {...ltrData} />
+        <BulletedListContainer blocks={orderedList.model.blocks} />
       </ServiceContext.Provider>,
     );
 
@@ -157,7 +21,7 @@ describe('BulletedListContainer', () => {
       <ServiceContext.Provider
         value={{ script: arabic, service: 'arabic', dir: 'rtl' }}
       >
-        <BulletedListContainer {...rtlData} />
+        <BulletedListContainer blocks={orderedList.model.blocks} />
       </ServiceContext.Provider>,
     );
   });
