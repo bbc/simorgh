2b656632a143e396e1b5361e77b104d4f09f3b1d
Alistair Gempf <alistair.gempf@bbc.co.uk>
2019-11-28 10:42:57 +0000

    Revert navigation changes

--- src/app/containers/Navigation/index.jsx
+++ src/app/containers/Navigation/index.jsx
@@ -4,52 +4,13 @@ import Navigation, {
   NavigationLi,
 } from '@bbc/psammead-navigation';
 import { ServiceContext } from '#contexts/ServiceContext';
-import { RequestContext } from '#contexts/RequestContext';
-import { EventContext } from '#contexts/EventContext';
-import { sendEventBeacon } from '../ATIAnalytics/beacon';
-import { buildATIClickParams } from '../ATIAnalytics/params';
-import { getComponentInfo } from '../../lib/analyticsUtils/index';
 
 const NavigationContainer = () => {
   const { script, translations, navigation, service, dir } = useContext(
     ServiceContext,
   );
-  const eventTrackingProps = buildATIClickParams(
-    {
-      metadata: {
-        id: 'id',
-        language: 'lang',
-      },
-    },
-    useContext(RequestContext),
-    useContext(ServiceContext),
-  );
+
   const { currentPage, skipLinkText } = translations;
-
-  const { useClickTracker } = useContext(EventContext);
-
-  useClickTracker('[data-navigation]', event => {
-    const componentName = 'navigation';
-    const eventData = event.srcElement.dataset[componentName];
-    const componentDataSplit = eventData.split('_');
-    const componentData = {
-      creationLabel: componentDataSplit[0],
-      child: componentDataSplit[1],
-    };
-
-    const componentInfo = getComponentInfo({
-      result: event.target.href,
-      componentName,
-      componentData,
-    });
-
-    sendEventBeacon({
-      componentName,
-      type: 'click',
-      componentInfo,
-      ...eventTrackingProps,
-    });
-  });
 
   if (!navigation || navigation.length === 0) {
     return null;
@@ -75,7 +36,6 @@ const NavigationContainer = () => {
               active={active}
               currentPageText={currentPage}
               service={service}
-              data-navigation={`${title.split(' ').join('-')}_${index}`}
             >
               {title}
             </NavigationLi>
