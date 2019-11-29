1e01dd6b5f1cd04db377442cb88feca413c1bd69
Alistair Gempf <alistair.gempf@bbc.co.uk>
2019-11-29 17:00:03 +0000

    Split into Amp and canonical scrolling nav

---
+++ src/app/containers/Navigation/index.amp.jsx
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react';
+import Navigation, {
+  NavigationUl,
+  NavigationLi,
+} from '@bbc/psammead-navigation';
+import { AmpScrollableNavigation } from '@bbc/psammead-navigation/scrollable';
+import { ServiceContext } from '#contexts/ServiceContext';
+
+const AmpNavigationContainer = () => {
+  const { script, translations, navigation, service, dir } = useContext(
+    ServiceContext,
+  );
+
+  const { currentPage, skipLinkText } = translations;
+
+  if (!navigation || navigation.length === 0) {
+    return null;
+  }
+
+  return (
+    <Navigation
+      script={script}
+      skipLinkText={skipLinkText}
+      service={service}
+      dir={dir}
+    >
+      <AmpScrollableNavigation dir={dir}>
+        <NavigationUl>
+          {navigation.map((item, index) => {
+            const { title, url } = item;
+            const active = index === 0;
+
+            return (
+              <NavigationLi
+                key={title}
+                url={url}
+                script={script}
+                active={active}
+                currentPageText={currentPage}
+                service={service}
+              >
+                {title}
+              </NavigationLi>
+            );
+          })}
+        </NavigationUl>
+      </AmpScrollableNavigation>
+    </Navigation>
+  );
+};
+
+export default AmpNavigationContainer;
--- src/app/containers/Navigation/index.jsx
+++ src/app/containers/Navigation/index.jsx
@@ -1,52 +1,11 @@
 import React, { useContext } from 'react';
-import Navigation, {
-  NavigationUl,
-  NavigationLi,
-} from '@bbc/psammead-navigation';
-import { CanonicalScrollableNavigation } from '@bbc/psammead-navigation/scrollable';
-import { ServiceContext } from '#contexts/ServiceContext';
+import { RequestContext } from '#contexts/RequestContext';
+import Canonical from './index.canonical';
+import Amp from './index.amp';
 
-const NavigationContainer = () => {
-  const { script, translations, navigation, service, dir } = useContext(
-    ServiceContext,
-  );
-
-  const { currentPage, skipLinkText } = translations;
-
-  if (!navigation || navigation.length === 0) {
-    return null;
-  }
-
-  return (
-    <Navigation
-      script={script}
-      skipLinkText={skipLinkText}
-      service={service}
-      dir={dir}
-    >
-      <CanonicalScrollableNavigation>
-        <NavigationUl>
-          {navigation.map((item, index) => {
-            const { title, url } = item;
-            const active = index === 0;
-
-            return (
-              <NavigationLi
-                key={title}
-                url={url}
-                script={script}
-                active={active}
-                currentPageText={currentPage}
-                service={service}
-              >
-                {title}
-              </NavigationLi>
-            );
-          })}
-        </NavigationUl>
-      </CanonicalScrollableNavigation>
-    </Navigation>
-  );
+const Navigation = () => {
+  const { platform } = useContext(RequestContext);
+  return platform === 'amp' ? <Amp /> : <Canonical />;
 };
 
-export default NavigationContainer;
+export default Navigation;
