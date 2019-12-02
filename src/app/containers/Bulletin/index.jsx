2f2b440b7d395063a427d96672f67cede976f056
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2019-12-02 14:34:27 +0000

    Remove unused test data

--- src/app/containers/Bulletin/index.jsx
+++ src/app/containers/Bulletin/index.jsx
@@ -9,25 +9,26 @@ import { ServiceContext } from '#contexts/ServiceContext';
 const BulletinContainer = ({ item }) => {
   const { script, service, dir, translations } = useContext(ServiceContext);
 
-  const contentType = pathOr(null, ['contentType'], item);
-  const isLive = pathOr(null, ['isLive'], item);
   const headline = pathOr(null, ['name'], item);
-  const summary = pathOr(null, ['summary'], item);
   const ctaLink = pathOr(null, ['uri'], item);
+
+  if (!headline || !ctaLink) {
+    return null;
+  }
+
+  const summary = pathOr(null, ['summary'], item);
   const imageSrc = pathOr(null, ['indexImage', 'href'], item);
   const imageAlt = pathOr(null, ['indexImage', 'altText'], item);
   const bulletinImage = <Image src={imageSrc} alt={imageAlt} />;
 
   const watchText = pathOr('Watch', ['media', 'watch'], translations);
   const listenText = pathOr('Listen', ['media', 'listen'], translations);
-
+  const contentType = pathOr(null, ['contentType'], item);
   const ctaText = contentType === 'TVBulletin' ? watchText : listenText;
   const type = contentType === 'TVBulletin' ? 'video' : 'audio';
-  const offScreenText = isLive ? `${ctaText} LIVE` : ctaText;
 
-  if (!headline || !ctaLink) {
-    return null;
-  }
+  const isLive = pathOr(null, ['isLive'], item);
+  const offScreenText = isLive ? `${ctaText} LIVE` : ctaText;
 
   return (
     <Bulletin
