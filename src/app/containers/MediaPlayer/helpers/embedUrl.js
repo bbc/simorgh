8ad8ca05a0080f533ecec82cbfd1f8e3dee789c5
rowland <rowland.henshaw@andela.com>
2019-12-03 15:36:08 +0100

    changed embed url

--- src/app/containers/MediaPlayer/helpers/embedUrl.js
+++ src/app/containers/MediaPlayer/helpers/embedUrl.js
@@ -1,16 +1,6 @@
-// Local embeds do not work, therefore, we use the test server when running locally or in storybook
-const buildBaseUrl = origin => {
-  if (origin.includes('local') || origin.includes('bbc.github.io')) {
-    const tld = origin.includes('.co.uk') ? '.co.uk' : '.com';
-    return `https://www.test.bbc${tld}`;
-  }
-
-  return origin;
-};
-
-const embedUrl = ({ origin, type, requestUrl, isAmp = false }) => {
+const embedUrl = ({ type, requestUrl, isAmp = false }) => {
   const avRoute = 'ws/av-embeds';
-  const baseUrl = buildBaseUrl(origin);
+  const baseUrl = process.env.SIMORGH_EMBEDS_BASE_URL;
   const parts = [baseUrl, avRoute, type, requestUrl];
 
   if (isAmp) {
