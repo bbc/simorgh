436003203d2a09148c453fd408f4fda669d16cc8
RayNjeri <rachael.njeri@andela.com>
2019-12-03 15:38:51 +0300

    remove test
    Signed-off-by: RayNjeri <rachael.njeri@andela.com>

--- src/app/containers/MediaPlayer/Metadata.test.js
+++
@@ -1,7 +0,0 @@
-import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
-
-import Metadata from './Metadata';
-
-describe('MediaPlayer', () => {
-  shouldMatchSnapshot('should render Metadata correctly ', Metadata);
-});
