5e9dcf6019d34d83d311aee6d1e3ce4463549aa6
rowland <rowland.henshaw@andela.com>
2019-12-05 17:22:55 +0100

    updated test

--- src/app/components/HtmlClassNameModifier/index.test.jsx
+++ src/app/components/HtmlClassNameModifier/index.test.jsx
@@ -1,21 +1,39 @@
 /* eslint-disable jsx-a11y/html-has-lang */
 import React from 'react';
 import { render, waitForDomChange } from '@testing-library/react';
-import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import { renderToStaticMarkup } from 'react-dom/server';
 import Helmet from 'react-helmet';
 import HtmlClassNameModifier from '.';
 
 describe('HtmlClassNameModifier', () => {
+  afterEach(() => {
+    Helmet.canUseDOM = true;
+  });
   it('should return a mounted component with the html class attribute as js', async () => {
-    render(<HtmlClassNameModifier />);
-    await waitForDomChange();
-    const { htmlAttributes } = Helmet.peek();
-    expect(htmlAttributes.class).toBe('js');
-    expect(document.querySelector('html')).toMatchSnapshot();
+    render(
+      <div>
+        <HtmlClassNameModifier />
+      </div>,
+    );
+
+    await waitForDomChange({
+      container: document.querySelector('html'),
+    });
+
+    const htmlClassName = document.querySelector('html').className;
+    expect(htmlClassName).toBe('js');
   });
 
-  shouldMatchSnapshot(
-    'should return the markup with the html class attribute as no-js',
-    <HtmlClassNameModifier />,
-  );
+  it('should return the markup with the html class attribute as no-js', async () => {
+    Helmet.canUseDOM = false;
+    renderToStaticMarkup(
+      <html>
+        <HtmlClassNameModifier />
+      </html>,
+    );
+
+    const htmlClassName = Helmet.renderStatic().htmlAttributes.toComponent()
+      .className;
+    expect(htmlClassName).toEqual('no-js');
+  });
 });
