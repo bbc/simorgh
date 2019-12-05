30c77a14197dd07feb7e10682f0d3ed50e1dadb5
rowland <rowland.henshaw@andela.com>
2019-12-05 13:32:06 +0100

    modified tests

--- src/app/components/HtmlClassNameModifier/index.test.jsx
+++ src/app/components/HtmlClassNameModifier/index.test.jsx
@@ -1,20 +1,21 @@
 /* eslint-disable jsx-a11y/html-has-lang */
 import React from 'react';
-import { render as mount } from '@testing-library/react';
-import { render } from 'enzyme';
+import { render, waitForDomChange } from '@testing-library/react';
+import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import Helmet from 'react-helmet';
 import HtmlClassNameModifier from '.';
 
 describe('HtmlClassNameModifier', () => {
-  it('should return a mounted component with the html class attribute as js', () => {
-    mount(<HtmlClassNameModifier />);
+  it('should return a mounted component with the html class attribute as js', async () => {
+    render(<HtmlClassNameModifier />);
+    await waitForDomChange();
     const { htmlAttributes } = Helmet.peek();
     expect(htmlAttributes.class).toBe('js');
+    expect(document.querySelector('html')).toMatchSnapshot();
   });
 
-  it('should return the markup with the html class attribute as no-js', () => {
-    render(<HtmlClassNameModifier />);
-    const { htmlAttributes } = Helmet.peek();
-    expect(htmlAttributes.class).toBe('no-js');
-  });
+  shouldMatchSnapshot(
+    'should return the markup with the html class attribute as no-js',
+    <HtmlClassNameModifier />,
+  );
 });
