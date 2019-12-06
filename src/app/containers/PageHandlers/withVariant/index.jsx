f611b773c1920ac545b3b228d95f93622bc90f38
Ikechi Michael <michael.ikechi.ext@bbc.co.uk>
2019-12-06 14:03:53 +0100

    fix: client-side redirect to preferred cookie variant was not happening.
    - updated the redirect condition
    - compute the new url with path-to-regexp

--- src/app/containers/PageHandlers/withVariant/index.jsx
+++ src/app/containers/PageHandlers/withVariant/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import pathToRegexp from 'path-to-regexp';
+import { pathOr } from 'ramda';
 import { useParams, useLocation, Redirect } from 'react-router-dom';
 import { variantSanitiser, getVariant } from '#lib/utilities/variantHandler';
 import {
@@ -6,14 +8,28 @@ import {
   setPreferredVariantCookie,
 } from '#contexts/UserContext/cookies';
 
-const redirect = (location, variant) => {
+const redirect = (location, pathname) => {
   return (
     <Redirect
       to={{
         ...location,
-        pathname: `${location.pathname}/${variant}`,
+        pathname,
       }}
     />
+  );
+};
+
+const getRedirectUrl = (path, params, variant) => {
+  const pathTo = pathToRegexp.compile(path);
+
+  return pathTo(
+    {
+      ...params,
+      variant: `/${variant}`,
+    },
+    {
+      encode: value => value,
+    },
   );
 };
 
@@ -24,13 +40,24 @@ const withVariant = Component => props => {
   const sanitizedVariant = variantSanitiser(variant);
   const cookieVariant = getPreferredVariantCookie(service);
   const isNotDefaultVariant = defaultVariant !== 'default';
+  const { path } = pathOr({}, ['match'], props);
 
-  if (!sanitizedVariant && cookieVariant) {
-    return redirect(location, cookieVariant);
+  const getRedirectUrlFromVariant = variantOverride =>
+    getRedirectUrl(
+      path,
+      {
+        service,
+        variant,
+      },
+      variantOverride,
+    );
+
+  if (sanitizedVariant && cookieVariant && sanitizedVariant !== cookieVariant) {
+    return redirect(location, getRedirectUrlFromVariant(cookieVariant));
   }
 
   if (!sanitizedVariant && isNotDefaultVariant) {
-    return redirect(location, defaultVariant);
+    return redirect(location, getRedirectUrlFromVariant(defaultVariant));
   }
 
   if (sanitizedVariant) setPreferredVariantCookie(service, sanitizedVariant);
