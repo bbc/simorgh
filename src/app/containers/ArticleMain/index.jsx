7b30efad5a8a7d99fb02223fe2e5a23c24ae3d31
thekp <khoa.phan@bbc.co.uk>
2019-12-12 17:05:31 +0000

    add example of amp mustache and amp scripting fetching data

--- src/app/containers/ArticleMain/index.jsx
+++ src/app/containers/ArticleMain/index.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import * as Amp from 'react-amphtml';
+import styled from 'styled-components';
+// import MostRead fro
+
+const Headline = styled.h1`
+  color: red;
+  ${({ text }) => text && `color: blue;`}
+  font-size: 100px
+  display: block; /* Explicitly set */
+`;
+
+const AMP_ACCESS_DATA = endpoint => ({
+  authorization: endpoint,
+  noPingback: true,
+});
+const AMP_ACCESS_FETCH = endpoint => (
+  <script id="amp-access" type="application/json">
+    {JSON.stringify(AMP_ACCESS_DATA(endpoint))}
+  </script>
+);
+const HeadlineWrapper = ({ records }) => {
+  return <Headline>{records}</Headline>;
+};
 
 const ArticleMain = ({ articleData: data }) => {
   return (
@@ -10,21 +33,48 @@ const ArticleMain = ({ articleData: data }) => {
           custom-element="amp-script"
           src="https://cdn.ampproject.org/v0/amp-script-0.1.js"
         ></script>
-        <meta name="simorghstuff" content="Helmet khoaphanmetadata" />
+        <script
+          async
+          custom-element="amp-list"
+          src="https://cdn.ampproject.org/v0/amp-list-0.1.js"
+        ></script>
+        <script
+          async
+          custom-template="amp-mustache"
+          src="https://cdn.ampproject.org/v0/amp-mustache-0.2.js"
+        ></script>
+        <script
+          async
+          custom-element="amp-access"
+          src="https://cdn.ampproject.org/v0/amp-access-0.1.js"
+        ></script>
+        {AMP_ACCESS_FETCH(
+          'http://localhost:7080/amp-script/mostReadLocal.json',
+        )}
 
-        <style type="text/css" amp-custom>{`
-          .khoaphan {
-              background-color: blue;
-          }
-        `}</style>
-        <script></script>
+        <meta name="simorghstuff" content="Helmet khoaphanmetadata" />
       </Helmet>
 
+      <section amp-access="totalRecords > 0">
+        <h2>totalRecods exists and is available</h2>
+        <Amp.Template
+          specName="default"
+          type="amp-mustache"
+          amp-access-template="true"
+        >
+          <Headline text="{{lastRecordTimeStamp}}">
+            lastRecordTimeStamp: {typeof '{{lastRecordTimeStamp}}'}
+          </Headline>
+
+          <HeadlineWrapper records="{{records}}" />
+          <Headline>generated: {'{{generated}}'}</Headline>
+        </Amp.Template>
+      </section>
       <amp-script
         layout="container"
         data-ampdevmode="true" // bypasses amp checks such as is on https and js size
         sandbox="allow-forms" // allows form input like buttons
-        src={`http://localhost:7080/amp-script/testingStyledComp.js`}
+        src={`http://localhost:7080/amp-script/testingFetch.js`}
       >
         <button>testing my lazers</button>
         {/* Root Div for react app to render into */}
