9e30116aa04ed9f3f4fda53107536421c64d34d0
karinathomas <karina.mp.thomas@gmail.com>
2019-12-19 13:07:04 +0000

    Update comment for getUrlPath to better explain what the function does

--- src/app/lib/utilities/urlParser/index.js
+++ src/app/lib/utilities/urlParser/index.js
@@ -2,7 +2,12 @@ import Url from 'url-parse';
 
 // Returns the path, excluding any query string params
 export const getUrlPath = url => {
-  // Remove trailing /
+  /* Remove leading /
+   * According to https://nodejs.org/api/url.html#url_url_format_urlobject
+   * If the urlObject.pathname property is a string that is not an empty string:
+   * If the urlObject.pathname does not start with an ASCII forward slash (/),
+   * then the literal string '/' is appended to result.
+   */
   return new Url(url).pathname.substring(1);
 };
 
