f2cdeca30937228fca18505774c3e0114d5d154c
karinathomas <karina.mp.thomas@gmail.com>
2019-12-19 10:12:18 +0000

    Do not append query string params to the embed URL

--- src/app/containers/MediaPlayer/helpers/embedUrl.js
+++ src/app/containers/MediaPlayer/helpers/embedUrl.js
@@ -1,5 +1,4 @@
 import Url from 'url-parse';
-import QS from 'querystringify';
 
 const AV_ROUTE = 'ws/av-embeds';
 
@@ -27,7 +26,7 @@ const getBaseUrl = requestUrl => {
   return process.env.SIMORGH_EMBEDS_BASE_URL;
 };
 
-const getUrlParts = (type, requestUrl, isAmp) => {
+const embedUrl = ({ type, requestUrl, isAmp = false }) => {
   const urlParts = [
     getBaseUrl(requestUrl),
     AV_ROUTE,
@@ -39,22 +38,7 @@ const getUrlParts = (type, requestUrl, isAmp) => {
     urlParts.push('amp');
   }
 
-  return urlParts;
-};
-
-const getUrl = (type, requestUrl, isAmp) => {
-  const urlParts = getUrlParts(type, requestUrl, isAmp);
   return urlParts.join('/');
-};
-
-const embedUrl = ({ type, requestUrl, isAmp = false }) => {
-  let url = getUrl(type, requestUrl, isAmp);
-
-  if (overrideExists(requestUrl)) {
-    url += QS.stringify(getQueryParams(requestUrl), true);
-  }
-
-  return url;
 };
 
 export default embedUrl;
--- src/app/containers/MediaPlayer/helpers/embedUrl.test.js
+++ src/app/containers/MediaPlayer/helpers/embedUrl.test.js
@@ -3,8 +3,10 @@ import embedUrl from './embedUrl';
 const requestUrl = 'foo/bar';
 const overrideParam = '?renderer_env=live';
 const requestUrlWithOverride = `${requestUrl}${overrideParam}`;
+const mediaRequestUrl = 'bbc_korean_radio/liveradio';
+const mediaRequestUrlWithOverride = `${mediaRequestUrl}${overrideParam}`;
 
-const testCases = [
+const defaultTestCases = [
   {
     description: 'CANONICAL: builds a URL for articles',
     expected: `https://embed-host.bbc.com/ws/av-embeds/articles/${requestUrl}`,
@@ -14,15 +16,7 @@ const testCases = [
     },
   },
   {
-    description: 'CANONICAL: builds a URL with override',
-    expected: `https://embed-host.override.bbc.com/ws/av-embeds/articles/${requestUrlWithOverride}`,
-    embedObject: {
-      requestUrl: requestUrlWithOverride,
-      type: 'articles',
-    },
-  },
-  {
-    description: 'AMP: builds a URL',
+    description: 'AMP: builds a URL for articles',
     expected: `https://embed-host.bbc.com/ws/av-embeds/articles/${requestUrl}/amp`,
     embedObject: {
       isAmp: true,
@@ -31,8 +25,36 @@ const testCases = [
     },
   },
   {
-    description: 'AMP: builds a URL with override',
-    expected: `https://embed-host.override.bbc.com/ws/av-embeds/articles/${requestUrl}/amp${overrideParam}`,
+    description: 'CANONICAL: builds a URL for media',
+    expected: `https://embed-host.bbc.com/ws/av-embeds/media/${mediaRequestUrl}`,
+    embedObject: {
+      requestUrl: mediaRequestUrl,
+      type: 'media',
+    },
+  },
+  {
+    description: 'AMP: builds a URL for media',
+    expected: `https://embed-host.bbc.com/ws/av-embeds/media/${mediaRequestUrl}/amp`,
+    embedObject: {
+      isAmp: true,
+      requestUrl: mediaRequestUrl,
+      type: 'media',
+    },
+  },
+];
+
+const nonLiveEnvWithOverrideTestCases = [
+  {
+    description: 'CANONICAL: builds a URL for articles with override',
+    expected: `https://embed-host.override.bbc.com/ws/av-embeds/articles/${requestUrl}`,
+    embedObject: {
+      requestUrl: requestUrlWithOverride,
+      type: 'articles',
+    },
+  },
+  {
+    description: 'AMP: builds a URL for articles with override',
+    expected: `https://embed-host.override.bbc.com/ws/av-embeds/articles/${requestUrl}/amp`,
     embedObject: {
       isAmp: true,
       requestUrl: requestUrlWithOverride,
@@ -40,32 +62,89 @@ const testCases = [
     },
   },
   {
-    description: 'AMP: builds a URL for media',
-    expected: `https://embed-host.bbc.com/ws/av-embeds/media/bbc_korean_radio/liveradio/amp`,
+    description: 'CANONICAL: builds a URL for media with override',
+    expected: `https://embed-host.override.bbc.com/ws/av-embeds/media/${mediaRequestUrl}`,
     embedObject: {
-      isAmp: true,
-      requestUrl: 'bbc_korean_radio/liveradio',
+      requestUrl: mediaRequestUrlWithOverride,
       type: 'media',
     },
   },
   {
     description: 'AMP: builds a URL for media with override',
-    expected: `https://embed-host.override.bbc.com/ws/av-embeds/media/bbc_korean_radio/liveradio/amp${overrideParam}`,
+    expected: `https://embed-host.override.bbc.com/ws/av-embeds/media/${mediaRequestUrl}/amp`,
     embedObject: {
       isAmp: true,
-      requestUrl: `bbc_korean_radio/liveradio${overrideParam}`,
+      requestUrl: mediaRequestUrlWithOverride,
       type: 'media',
     },
   },
 ];
 
-describe('Media Player: Embed URL', () => {
+const liveEnvWithOverrideTestCases = [
+  {
+    description: 'CANONICAL: builds a URL for articles with override',
+    expected: `https://embed-host.bbc.com/ws/av-embeds/articles/${requestUrl}`,
+    embedObject: {
+      requestUrl: requestUrlWithOverride,
+      type: 'articles',
+    },
+  },
+  {
+    description: 'AMP: builds a URL for articles with override',
+    expected: `https://embed-host.bbc.com/ws/av-embeds/articles/${requestUrl}/amp`,
+    embedObject: {
+      isAmp: true,
+      requestUrl: requestUrlWithOverride,
+      type: 'articles',
+    },
+  },
+  {
+    description: 'CANONICAL: builds a URL for media with override',
+    expected: `https://embed-host.bbc.com/ws/av-embeds/media/${mediaRequestUrl}`,
+    embedObject: {
+      requestUrl: mediaRequestUrlWithOverride,
+      type: 'media',
+    },
+  },
+  {
+    description: 'AMP: builds a URL for media with override',
+    expected: `https://embed-host.bbc.com/ws/av-embeds/media/${mediaRequestUrl}/amp`,
+    embedObject: {
+      isAmp: true,
+      requestUrl: mediaRequestUrlWithOverride,
+      type: 'media',
+    },
+  },
+];
+
+describe('Media Player: Embed URL in non-live environment', () => {
+  const environment = process.env.APP_ENV;
   beforeEach(() => {
+    process.env.APP_ENV = 'test';
     process.env.SIMORGH_EMBEDS_BASE_URL = 'https://embed-host.bbc.com';
     process.env.SIMORGH_EMBEDS_BASE_URL_OVERRIDE =
       'https://embed-host.override.bbc.com';
   });
+  const testCases = defaultTestCases.concat(nonLiveEnvWithOverrideTestCases);
   testCases.forEach(({ description, expected, embedObject }) =>
     it(description, () => expect(embedUrl(embedObject)).toEqual(expected)),
   );
+  afterEach(() => {
+    process.env.APP_ENV = environment;
+  });
+});
+
+describe('Media Player: Embed URL in live environment', () => {
+  const environment = process.env.APP_ENV;
+  beforeEach(() => {
+    process.env.APP_ENV = 'live';
+    process.env.SIMORGH_EMBEDS_BASE_URL = 'https://embed-host.bbc.com';
+  });
+  const testCases = defaultTestCases.concat(liveEnvWithOverrideTestCases);
+  testCases.forEach(({ description, expected, embedObject }) =>
+    it(description, () => expect(embedUrl(embedObject)).toEqual(expected)),
+  );
+  afterEach(() => {
+    process.env.APP_ENV = environment;
+  });
 });
