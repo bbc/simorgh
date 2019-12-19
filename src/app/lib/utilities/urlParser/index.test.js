f703fc1a9d55b90d2e0442fe48f602d974ad81d3
Karina Thomas <karina.thomas@bbc.co.uk>
2019-12-19 17:41:59 +0000

    Only remove the trailing / if it exists

--- src/app/lib/utilities/urlParser/index.test.js
+++ src/app/lib/utilities/urlParser/index.test.js
@@ -1,42 +1,69 @@
 // eslint-disable-next-line import/named
 import { getUrlPath, getQueryString, getParsedQueryString } from '.';
 
-const requestUrl = 'foo/bar';
 const queryString = '?param=test&qs=1';
-const requestUrlWithQueryString = `${requestUrl}${queryString}`;
+const partialRequestUrl = 'foo/bar';
+const partialRequestUrlWithQueryString = `${partialRequestUrl}${queryString}`;
+const url = `https://www.test.com/${partialRequestUrl}`;
+const urlWithQueryString = `${url}${queryString}`;
 
 describe('getUrlPath', () => {
-  it('should return path', () => {
-    expect(getUrlPath(requestUrl)).toBe(requestUrl);
+  it('should return path from url', () => {
+    expect(getUrlPath(url)).toBe(partialRequestUrl);
+  });
+  it('should return path from partial url', () => {
+    expect(getUrlPath(partialRequestUrl)).toBe(partialRequestUrl);
   });
 
-  it('should return path when query string exists', () => {
-    expect(getUrlPath(requestUrlWithQueryString)).toBe(requestUrl);
+  it('should return path from url when query string exists', () => {
+    expect(getUrlPath(urlWithQueryString)).toBe(partialRequestUrl);
+  });
+  it('should return path from partial url when query string exists', () => {
+    expect(getUrlPath(partialRequestUrlWithQueryString)).toBe(
+      partialRequestUrl,
+    );
   });
 });
 
 describe('getQueryString', () => {
-  it('should not return query string when none exists ', () => {
-    expect(getQueryString(requestUrl)).toBe('');
+  it('should not return query string from url when no query string exists ', () => {
+    expect(getQueryString(url)).toBe('');
+  });
+  it('should not return query string from partial url when no query string exists ', () => {
+    expect(getQueryString(partialRequestUrl)).toBe('');
   });
 
-  it('should return query string when query string exists', () => {
-    expect(getQueryString(requestUrlWithQueryString)).toBe(queryString);
+  it('should return query string from url when query string exists', () => {
+    expect(getQueryString(urlWithQueryString)).toBe(queryString);
+  });
+  it('should return query string from partial url when query string exists', () => {
+    expect(getQueryString(partialRequestUrlWithQueryString)).toBe(queryString);
   });
 });
 
 describe('getParsedQueryString', () => {
-  it('should return empty query string object when none exists ', () => {
-    expect(getParsedQueryString(requestUrl)).toEqual({});
+  it('should return empty query string object from url when no query string exists ', () => {
+    expect(getParsedQueryString(url)).toEqual({});
+  });
+  it('should return empty query string object from partial url when no query string exists ', () => {
+    expect(getParsedQueryString(partialRequestUrl)).toEqual({});
   });
 
-  it('should return parsed query string object when query string exists', () => {
+  describe('parsed query string object', () => {
     const parsedQueryString = {
       param: 'test',
       qs: '1',
     };
-    expect(getParsedQueryString(requestUrlWithQueryString)).toEqual(
-      parsedQueryString,
-    );
+
+    it('should be returned from url when query string exists', () => {
+      expect(getParsedQueryString(urlWithQueryString)).toEqual(
+        parsedQueryString,
+      );
+    });
+    it('should be returned from partial url when query string exists', () => {
+      expect(getParsedQueryString(partialRequestUrlWithQueryString)).toEqual(
+        parsedQueryString,
+      );
+    });
   });
 });
