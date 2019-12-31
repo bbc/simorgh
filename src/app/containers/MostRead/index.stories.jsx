09451b85b3cf152d5447543d3fe952d1a5df7313
thekp <khoa.phan@bbc.co.uk>
2019-12-31 11:38:48 +0000

    add toggle provider

--- src/app/containers/MostRead/index.stories.jsx
+++ src/app/containers/MostRead/index.stories.jsx
@@ -5,29 +5,36 @@ import { withServicesKnob } from '@bbc/psammead-storybook-helpers';
 import MostReadContainer from '.';
 import { RequestContextProvider } from '#contexts/RequestContext';
 import { ServiceContextProvider } from '#contexts/ServiceContext';
+import { ToggleContext } from '#contexts/ToggleContext';
 
 const staticMostReadURL = (service, variant) =>
   variant !== 'default'
     ? `/data/${service}/mostRead/${variant}.json`
     : `/data/${service}/mostRead/index.json`;
 
+const getToggleState = enabled => ({
+  local: { mostRead: { enabled } },
+});
+
 const renderMostReadContainer = (service, variant) => (
-  <RequestContextProvider
-    bbcOrigin={`/${service}/articles/c0000000000o`}
-    id="c0000000000o"
-    isAmp={false}
-    pageType="article"
-    service={service}
-    statusCode={200}
-    pathname={`/${service}`}
-    variant={variant}
-  >
-    <ServiceContextProvider service={service} variant={variant}>
-      <MostReadContainer
-        endpointOverride={staticMostReadURL(service, variant)}
-      />
-    </ServiceContextProvider>
-  </RequestContextProvider>
+  <ToggleContext.Provider value={{ toggleState: getToggleState(true) }}>
+    <RequestContextProvider
+      bbcOrigin={`http://localhost/${service}/articles/c0000000000o`}
+      id="c0000000000o"
+      isAmp={false}
+      pageType="article"
+      service={service}
+      statusCode={200}
+      pathname={`/${service}`}
+      variant={variant}
+    >
+      <ServiceContextProvider service={service} variant={variant}>
+        <MostReadContainer
+          endpointOverride={staticMostReadURL(service, variant)}
+        />
+      </ServiceContextProvider>
+    </RequestContextProvider>
+  </ToggleContext.Provider>
 );
 
 const stories = storiesOf('Containers|MostRead', module)
