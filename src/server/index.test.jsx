2b2284c2637faae3ff7eed1d3c02bbceac274b48
Ikechi Michael <michael.ikechi.ext@bbc.co.uk>
2019-12-31 09:37:12 +0100

    Change isUK to isNotUK, and update logic to use BBC-Country header

--- src/server/index.test.jsx
+++ src/server/index.test.jsx
@@ -50,6 +50,11 @@ jest.mock('@loadable/server', () => ({
 
 jest.mock('../app/routes/getInitialData/utils/getRouteProps');
 
+const bbcHeaders = {
+  bbcOrigin: undefined,
+  bbcCountry: undefined,
+};
+
 const mockRouteProps = ({
   id,
   service,
@@ -130,16 +135,17 @@ const testRenderedData = ({
       service={service}
       scripts="__mock_script_elements__"
       styleTags={<style />}
+      headers={bbcHeaders}
     />,
   );
 
   const expectedProps = {
-    bbcOrigin: undefined,
     data: successDataResponse,
     isAmp,
     service,
     routes,
     url,
+    headers: expect.anything(),
   };
 
   if (variant) {
@@ -825,6 +831,7 @@ describe('Server', () => {
             service={service}
             scripts="__mock_script_elements__"
             styleTags={<style />}
+            headers={bbcHeaders}
           />,
         );
 
