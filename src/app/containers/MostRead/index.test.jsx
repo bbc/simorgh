58f03312f0efda99d4eb58e8096c953d7ca03005
thekp <khoa.phan@bbc.co.uk>
2020-01-03 10:09:25 +0000

    add snapshot tests

--- src/app/containers/MostRead/index.test.jsx
+++ src/app/containers/MostRead/index.test.jsx
@@ -7,17 +7,18 @@ import { ServiceContextProvider } from '#contexts/ServiceContext';
 import { ToggleContext } from '#contexts/ToggleContext';
 import newsMostReadData from '#data/news/mostRead';
 import zhongwenSimpMostReadData from '#data/zhongwen/mostRead/simp';
-import { service as newsConfig } from '#app/lib/config/services/news';
-import { service as zhongwenConfig } from '#app/lib/config/services/zhongwen';
+import { setFreshLastRecordTimestamp } from './utilities/testHelper';
 
 let container;
 
 const services = {
-  news: { variant: null, data: newsMostReadData, config: newsConfig.default },
+  news: {
+    variant: null,
+    data: newsMostReadData,
+  },
   zhongwen: {
     variant: 'simp',
     data: zhongwenSimpMostReadData,
-    config: zhongwenConfig.simp,
   },
 };
 
@@ -68,24 +69,25 @@ describe('MostReadContainerCanonical', () => {
   });
 
   Object.keys(services).forEach(service => {
-    it(`test data returns as expected on canonical for ${service}`, async () => {
-      const { variant, data: mostReadData, config } = services[service];
-      const mostReadHeader = config.mostRead.header;
+    it(`test most read renders as expected on canonical for ${service}`, async () => {
+      const { variant, data: mostReadData } = services[service];
 
-      fetch.mockResponse(JSON.stringify(mostReadData));
+      fetch.mockResponse(
+        JSON.stringify(setFreshLastRecordTimestamp(mostReadData)),
+      );
+
       await renderMostReadContainer({
         isAmp: false,
         service,
         variant,
         mostReadToggle: true,
       });
-      expect(container.querySelector('h2').textContent).toEqual(mostReadHeader);
-      expect(container.querySelectorAll('li').length).toEqual(10);
+
+      expect(container).toMatchSnapshot();
     });
 
     it(`should return empty string when mostRead toggle is disabled - ${service}`, async () => {
       const { variant, data: mostReadData } = services[service];
-
       fetch.mockResponse(JSON.stringify(mostReadData));
       await renderMostReadContainer({ isAmp: false, service, variant });
       expect(container.innerHTML).toEqual('');
