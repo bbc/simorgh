8c4c80c6f23f31d6e81598c7f2a46a72aee6ba70
Karina Thomas <karina.thomas@bbc.co.uk>
2020-01-06 12:14:05 +0000

    Code review feedback - don't mutate the url parts array & run the isLive() check first, so that the hasLiveOverride function does not run in production environment

--- src/app/containers/MediaPlayer/helpers/embedUrl.js
+++ src/app/containers/MediaPlayer/helpers/embedUrl.js
@@ -1,29 +1,20 @@
 import isLive from '#lib/utilities/envHelper';
 import hasLiveOverride from '#lib/utilities/rendererEnvHelper';
-import nodeLogger from '#lib/logger.node';
-
-const logger = nodeLogger(__filename);
 
 const AV_ROUTE = 'ws/av-embeds';
 
 const getBaseUrl = url => {
-  if (hasLiveOverride(url) || isLive()) {
+  if (isLive() || hasLiveOverride(url)) {
     return process.env.SIMORGH_EMBEDS_BASE_URL_LIVE;
   }
   return process.env.SIMORGH_EMBEDS_BASE_URL_TEST;
 };
 
 const embedUrl = ({ type, mediaId, isAmp = false, pageUrl }) => {
-  const urlParts = [getBaseUrl(pageUrl), AV_ROUTE, type, mediaId];
+  const baseUrl = getBaseUrl(pageUrl);
+  const url = `${baseUrl}/${AV_ROUTE}/${type}/${mediaId}`;
 
-  if (isAmp) {
-    urlParts.push('amp');
-  }
-
-  const urlEmbed = urlParts.join('/');
-  logger.info(`Embed URL: ${urlEmbed}`);
-
-  return urlEmbed;
+  return isAmp ? `${url}/amp` : url;
 };
 
 export default embedUrl;
