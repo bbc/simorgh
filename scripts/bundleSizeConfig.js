df1e0a0731e516e73387f5298380d700347200ae
Olha Lyubinets <olha.lyubinets@bbc.co.uk>
2020-01-08 16:26:47 +0000

    Update bundle size

--- scripts/bundleSizeConfig.js
+++ scripts/bundleSizeConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Size limit for all bundles used by each service (K)
   // Keep these +/- 5K and update frequently!
-  MIN_SIZE: 636,
-  MAX_SIZE: 680,
+  MIN_SIZE: 641,
+  MAX_SIZE: 689,
 };
