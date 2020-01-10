20d35bf28d7f596053bb3774e63e609b629c56e6
Karina Thomas <karina.thomas@bbc.co.uk>
2020-01-10 16:12:16 +0000

    Tidy up test

--- src/app/lib/utilities/isLive/index.test.js
+++ src/app/lib/utilities/isLive/index.test.js
@@ -1,21 +1,19 @@
 import isLive from '.';
 
-describe('envHelper', () => {
-  describe('isLive', () => {
-    const environment = process.env.SIMORGH_APP_ENV;
+describe('isLive', () => {
+  const environment = process.env.SIMORGH_APP_ENV;
 
-    it('should return true when APP_ENV is live', () => {
-      process.env.SIMORGH_APP_ENV = 'live';
-      expect(isLive()).toBe(true);
-    });
+  it('should return true when APP_ENV is live', () => {
+    process.env.SIMORGH_APP_ENV = 'live';
+    expect(isLive()).toBe(true);
+  });
 
-    it('should return false when APP_ENV is not live', () => {
-      process.env.SIMORGH_APP_ENV = 'non-live';
-      expect(isLive()).toBe(false);
-    });
+  it('should return false when APP_ENV is not live', () => {
+    process.env.SIMORGH_APP_ENV = 'non-live';
+    expect(isLive()).toBe(false);
+  });
 
-    afterEach(() => {
-      process.env.SIMORGH_APP_ENV = environment;
-    });
+  afterEach(() => {
+    process.env.SIMORGH_APP_ENV = environment;
   });
 });
