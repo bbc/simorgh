9adf19d4ea8c1bfe02f36ba82b9f37237b2b7f39
LilyL0u <lily.higham@bbc.co.uk>
2020-01-14 17:26:23 +0000

    removed thai url from map test

--- cypress/support/config/services.js
+++ cypress/support/config/services.js
@@ -1212,7 +1212,7 @@ const genServices = appEnv => ({
       },
       liveRadio: { path: undefined, smoke: false },
       mediaAssetPage: {
-        path: isLive(appEnv) ? undefined : '/thai/thailand-23248713',
+        path: undefined,
         smoke: false,
       },
     },
