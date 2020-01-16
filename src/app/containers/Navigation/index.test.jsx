1e15aaf113f804a4c4aa354d97a08b87c18b6299
Tom Williams <tom.williams07@bbc.co.uk>
2020-01-16 15:55:34 +0000

    Unit test for navigation links

--- src/app/containers/Navigation/index.test.jsx
+++ src/app/containers/Navigation/index.test.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import { render } from '@testing-library/react';
+import { service as newsConfig } from '#lib/config/services/news';
 import { RequestContextProvider } from '#contexts/RequestContext';
 import { ServiceContextProvider } from '#contexts/ServiceContext';
 import Navigation from './index';
@@ -47,4 +49,33 @@ describe('Navigation Container', () => {
       </RequestContextProvider>
     </ServiceContextProvider>,
   );
+
+  it('should render navigation links in the correct format', () => {
+    const { navigation } = newsConfig.default;
+
+    const navigationComponent = (
+      <ServiceContextProvider service="news">
+        <RequestContextProvider
+          bbcOrigin="https://www.test.bbc.co.uk"
+          id="c0000000000o"
+          isAmp={false}
+          pageType="article"
+          service="news"
+          statusCode={200}
+          pathname="/pathname"
+        >
+          <Navigation />
+        </RequestContextProvider>
+      </ServiceContextProvider>
+    );
+
+    const { getAllByRole } = render(navigationComponent);
+    const listItems = getAllByRole('listitem');
+
+    navigation.forEach((navItem, index) => {
+      const link = listItems[index].querySelector('a');
+      const href = link.getAttribute('href');
+      expect(href).toEqual(navItem.url);
+    });
+  });
 });
