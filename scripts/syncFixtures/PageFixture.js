ac45ce4da09cc8268f3f3ae3b0cf901d9ed27fc4
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2020-01-17 15:29:38 +0000

    CURL error reporting

--- scripts/syncFixtures/PageFixture.js
+++ scripts/syncFixtures/PageFixture.js
@@ -28,12 +28,18 @@ class PageFixture {
 
   // TODO: reporting, error handling
   downloadFixture() {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
       const curlCmd = `curl ${this.aresUrl} -E ${CERTS.DEV} --cacert ${CERTS.CA}`;
       cmd.get(curlCmd, (curlError, data) => {
+        if (curlError) {
+          console.error(
+            `HTTPS Error - did you set the correct dev cert and CA within ./config.js?`,
+          );
+          return reject(curlError);
+        }
         const prettifiedData = beautify(JSON.parse(data), null, 2, 100);
         fs.writeFile(this.outputPath, prettifiedData, () => {
-          resolve(data);
+          return resolve(data);
         });
       });
     });
