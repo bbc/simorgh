4701f6dafd63bef6f102f85eb1ee9f28a1f384e2
Alistair Gempf <alistair.gempf@bbc.co.uk>
2020-01-17 10:19:35 +0000

    Test changing host properly

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -24,14 +24,14 @@ module.exports = ({ resolvePath, IS_CI, IS_PROD, START_DEV_SERVER }) => {
     target: 'web', // compile for browser environment
     entry: START_DEV_SERVER
       ? [
-          `webpack-dev-server/client?http://alistair-simorgh-test:${webpackDevServerPort}`,
+          `webpack-dev-server/client?http://alistair-simorgh-test.herokuapp.com:${webpackDevServerPort}`,
           'webpack/hot/only-dev-server',
           './src/poly',
           './src/client',
         ]
       : ['./src/poly', './src/client'],
     devServer: {
-      host: 'alistair-simorgh-test',
+      host: 'alistair-simorgh-test.herokuapp.com',
       port: webpackDevServerPort,
       historyApiFallback: true,
       hot: true,
@@ -52,7 +52,7 @@ module.exports = ({ resolvePath, IS_CI, IS_PROD, START_DEV_SERVER }) => {
         : 'static/js/[name].[chunkhash:8].js', // hash based on the contents of the file
       // need full URL for dev server & HMR: https://github.com/webpack/docs/wiki/webpack-dev-server#combining-with-an-existing-server
       publicPath: START_DEV_SERVER
-        ? `http://alistair-simorgh-test:${webpackDevServerPort}/`
+        ? `http://alistair-simorgh-test.herokuapp.com:${webpackDevServerPort}/`
         : prodPublicPath,
     },
     optimization: {
