fd0c2a83c7bd33e2e2f792f0a1489b5c833dbec0
rowland <rowland.henshaw@andela.com>
2020-01-20 16:31:26 +0100

    updated tests for CpsMap

--- src/app/pages/CpsMap/index.test.jsx
+++ src/app/pages/CpsMap/index.test.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import { StaticRouter } from 'react-router-dom';
-import { matchSnapshotAsync } from '@bbc/psammead-test-helpers';
+import path from 'ramda/src/path';
 import { ServiceContextProvider } from '#contexts/ServiceContext';
 import { RequestContextProvider } from '#contexts/RequestContext';
 import { ToggleContext } from '#contexts/ToggleContext';
 import CpsMapPage from '.';
-import pidginPageData from '#data/pidgin/cpsAssets/tori-49450859';
+import mapPageData from '#data/pidgin/cpsAssets/23248703';
+import uzbekPageData from '#data/uzbek/cpsAssets/sport-23248721';
 import preprocessor from '#lib/utilities/preprocessor';
 import { cpsAssetPreprocessorRules } from '#app/routes/getInitialData/utils/preprocessorRulesConfig';
 
@@ -97,15 +100,149 @@ jest.mock('../../containers/PageHandlers/withContexts', () => Component => {
   return ContextsContainer;
 });
 
+const escapedText = text => {
+  const textReplacements = {
+    '&quot;': '"',
+    '&amp;': '&',
+    '&lt;': '<',
+    '&gt;': '>',
+  };
+
+  const replacementsRegex = new RegExp(
+    Object.keys(textReplacements).join('|'),
+    'gi',
+  );
+
+  return text.replace(replacementsRegex, match => textReplacements[match]);
+};
+
+const getBlockTextAtIndex = (index, originalPageData) => {
+  return path(['content', 'blocks', index, 'text'], originalPageData);
+};
+
 describe('CPS MAP Page', () => {
-  describe('snapshots', () => {
-    it('should match snapshot for MAP', async () => {
-      const pageData = await preprocessor(
-        pidginPageData,
-        cpsAssetPreprocessorRules,
+  let pageData;
+  let asFragment;
+  let getByText;
+  beforeEach(async () => {
+    pageData = await preprocessor(mapPageData, cpsAssetPreprocessorRules);
+
+    ({ asFragment, getByText } = render(
+      createAssetPage({ pageData }, 'pidgin'),
+    ));
+  });
+
+  it('should render component', () => {
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render paragraph', () => {
+    const paragraphText = getBlockTextAtIndex(1, mapPageData);
+    expect(getByText(escapedText(paragraphText))).toBeInTheDocument();
+  });
+
+  it('should render image', () => {
+    const imageCaption = path(
+      ['content', 'blocks', 25, 'caption'],
+      mapPageData,
+    );
+    // Images not rendered properly due to lazyload, therefore can only check caption text
+    expect(getByText(escapedText(imageCaption))).toBeInTheDocument();
+  });
+
+  describe('AV player', () => {
+    let liveStreamSource;
+
+    const getLiveStreamBlock = processedPageData => {
+      return path(['content', 'model', 'blocks', 1], processedPageData);
+    };
+
+    const getLiveStreamSource = liveStreamBlock => {
+      return path(
+        [
+          'model',
+          'blocks',
+          '0',
+          'model',
+          'blocks',
+          0,
+          'model',
+          'versions',
+          0,
+          'versionId',
+        ],
+        liveStreamBlock,
       );
-      const page = createAssetPage({ pageData }, 'pidgin');
-      await matchSnapshotAsync(page);
+    };
+
+    it('should render version (live audio stream)', async () => {
+      pageData = await preprocessor(uzbekPageData, cpsAssetPreprocessorRules);
+      const liveStreamBlock = getLiveStreamBlock(pageData);
+      liveStreamSource = getLiveStreamSource(liveStreamBlock);
+      expect(liveStreamBlock.type).toBe('version');
+
+      ({ asFragment } = render(createAssetPage({ pageData }, 'uzbek')));
+
+      expect(
+        document.querySelector(`iframe[src*=${liveStreamSource}]`),
+      ).not.toBeNull();
+      expect(asFragment()).toMatchSnapshot();
     });
+
+    it('should render video', () => {
+      const liveStreamBlock = getLiveStreamBlock(pageData);
+      liveStreamSource = getLiveStreamSource(liveStreamBlock);
+      expect(liveStreamBlock.type).toBe('video');
+
+      expect(
+        document.querySelector(`iframe[src*=${liveStreamSource}]`),
+      ).not.toBeNull();
+    });
+  });
+
+  describe('heading', () => {
+    let headingText;
+
+    const getBlockAtIndex = (index, processedPageData) => {
+      return path(['content', 'model', 'blocks', index], processedPageData);
+    };
+
+    beforeAll(() => {
+      headingText = getBlockTextAtIndex(2, mapPageData);
+    });
+
+    it('should render faux headline', () => {
+      const fauxHeadlineBlock = getBlockAtIndex(2, pageData);
+
+      expect(fauxHeadlineBlock.type).toBe('fauxHeadline');
+      expect(getByText(escapedText(headingText))).toBeInTheDocument();
+    });
+
+    it('should render visually hidden headline', () => {
+      const hiddenHeadline = getBlockAtIndex(0, pageData);
+
+      expect(hiddenHeadline.type).toBe('visuallyHiddenHeadline');
+      expect(getByText(escapedText(headingText))).toBeInTheDocument();
+    });
+  });
+
+  it('should render sub heading', () => {
+    const subHeadingText = getBlockTextAtIndex(3, mapPageData);
+
+    expect(getByText(escapedText(subHeadingText))).toBeInTheDocument();
+  });
+
+  it('should render crosshead', () => {
+    const crossHeadText = getBlockTextAtIndex(4, mapPageData);
+
+    expect(getByText(escapedText(crossHeadText))).toBeInTheDocument();
+  });
+
+  it('should render firstPublished timestamp for Pidgin', () => {
+    expect(getByText('13 September 2019')).toBeInTheDocument();
+  });
+
+  it('should render lastPublished timestamp for Pidgin', () => {
+    expect(getByText('New Informate 20 November 2019')).toBeInTheDocument();
   });
 });
