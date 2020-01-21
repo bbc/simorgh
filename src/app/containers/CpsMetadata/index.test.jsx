a42a0b0bcd6475faa9dc963a9a6d8e0ebaae0b3b
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2020-01-21 14:23:51 +0000

    Add tests for CpsMetadata

---
+++ src/app/containers/CpsMetadata/index.test.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { render, waitForDomChange } from '@testing-library/react';
+import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import { RequestContextProvider } from '#contexts/RequestContext';
+import { ServiceContextProvider } from '#contexts/ServiceContext';
+import CpsMetadata from './index';
+import { articleDataNews } from '../../pages/Article/fixtureData';
+
+const getISOStringDate = date => new Date(date).toISOString();
+
+// eslint-disable-next-line react/prop-types
+const Context = ({ service, children }) => (
+  <ServiceContextProvider service={service}>
+    <RequestContextProvider
+      bbcOrigin="https://www.test.bbc.co.uk"
+      id="c0000000000o"
+      isAmp={false}
+      pageType="article"
+      pathname="/pathname"
+      service={service}
+      statusCode={200}
+    >
+      {children}
+    </RequestContextProvider>
+  </ServiceContextProvider>
+);
+
+const mapProps = {
+  title: articleDataNews.promo.headlines.seoHeadline,
+  language: articleDataNews.metadata.passport.language,
+  description: articleDataNews.promo.headlines.seoHeadline,
+  firstPublished: getISOStringDate(articleDataNews.metadata.firstPublished),
+  lastPublished: getISOStringDate(articleDataNews.metadata.lastPublished),
+  indexImage: 'http://b.files.bbci.co.uk/6FC4/test/_63721682_p01kx435.jpg',
+  indexImageAltText: 'connectionAltText',
+};
+
+const renderMetadataToDocument = async Component => {
+  render(Component);
+
+  await waitForDomChange({
+    container: document.querySelector('head'),
+  });
+};
+
+it('should render the expected metadata tags', async () => {
+  await renderMetadataToDocument(
+    <Context service="news">
+      <CpsMetadata {...mapProps} />
+    </Context>,
+  );
+
+  const actual = Array.from(
+    document.querySelectorAll(
+      'head > meta[name^="article:"], head > meta[name*="image"]',
+    ),
+  ).map(tag => ({
+    name: tag.getAttribute('name'),
+    content: tag.getAttribute('content'),
+  }));
+
+  const expected = [
+    {
+      name: 'og:image',
+      content: 'http://b.files.bbci.co.uk/6FC4/test/_63721682_p01kx435.jpg',
+    },
+    { name: 'og:image:alt', content: 'connectionAltText' },
+    { name: 'twitter:image:alt', content: 'connectionAltText' },
+    {
+      name: 'twitter:image:src',
+      content: 'http://b.files.bbci.co.uk/6FC4/test/_63721682_p01kx435.jpg',
+    },
+    { content: 'https://www.facebook.com/bbcnews', name: 'article:author' },
+    { content: '2018-01-01T12:01:00.000Z', name: 'article:published_time' },
+    { content: '2018-01-01T13:00:00.000Z', name: 'article:modified_time' },
+  ];
+
+  expect(actual).toEqual(expected);
+});
+
+shouldMatchSnapshot(
+  'should match snapshot for News & International',
+  <Context service="news">
+    <CpsMetadata {...mapProps} />
+  </Context>,
+);
