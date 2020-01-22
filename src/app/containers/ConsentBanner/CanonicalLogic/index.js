d401373d7dae02cea8c083b4ce1f9b3f86b9f40c
RichardPK <r.phillips.kerr@gmail.com>
2020-01-22 12:21:17 +0000

    Added domain key to Cookies.set to match .bbc.com/.bbc.co.uk pattern

--- src/app/containers/ConsentBanner/CanonicalLogic/index.js
+++ src/app/containers/ConsentBanner/CanonicalLogic/index.js
@@ -15,7 +15,10 @@ const PRIVACY_COOKIE_PREVIOUS_VALUES = ['0', '1'];
 const onClient = typeof window !== 'undefined';
 
 const setCookie = (name, value) =>
-  Cookie.set(name, value, { expires: COOKIE_EXPIRY });
+  Cookie.set(name, value, {
+    expires: COOKIE_EXPIRY,
+    domain: `.${document.domain}`,
+  });
 
 const setPolicyCookie = (value, logger) => {
   setCookie(POLICY_COOKIE, value);
