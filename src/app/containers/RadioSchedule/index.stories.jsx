c7bbd44abbaaa7ba16434c0ff8014570b53fa5cd
Ruth Ogendi <ruth.ogendi@andela.com>
2020-01-22 16:10:08 +0300

    Add test.

--- src/app/containers/RadioSchedule/index.stories.jsx
+++ src/app/containers/RadioSchedule/index.stories.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withServicesKnob } from '@bbc/psammead-storybook-helpers';
-import { ServiceContextProvider } from '#contexts/ServiceContext';
-import { RequestContextProvider } from '#contexts/RequestContext';
 import RadioScheduleContainer from '.';
 import validServices from './helpers/validServices';
 
@@ -11,16 +9,7 @@ const staticRadioScheduleURL = service =>
   `/data/${service}/bbc_${service}_radio/radioschedule.json`;
 
 const renderRadioScheduleContainer = service => (
-  <RequestContextProvider
-    isAmp={false}
-    pageType="frontPage"
-    service={service}
-    pathname={`/${service}`}
-  >
-    <ServiceContextProvider service={service}>
-      <RadioScheduleContainer endpoint={staticRadioScheduleURL(service)} />
-    </ServiceContextProvider>
-  </RequestContextProvider>
+  <RadioScheduleContainer endpoint={staticRadioScheduleURL(service)} />
 );
 
 const stories = storiesOf('Containers|RadioSchedule', module)
