b8ffeb21e078b62a020fb4255c77d5b0e4abba25
Alistair Gempf <alistair.gempf@bbc.co.uk>
2020-01-22 12:35:52 +0000

    Update some naming as per feedback

--- src/app/containers/FrontPageSection/utilities/storySplitter.jsx
+++ src/app/containers/FrontPageSection/utilities/storySplitter.jsx
@@ -1,10 +1,10 @@
 import { splitEvery, splitAt, take } from 'ramda';
 
-const getItemLimit = (items, isFirstSection) =>
+const getAllowedItems = (items, isFirstSection) =>
   isFirstSection ? take(13, items) : take(10, items);
 
 // Split the top row stories out into their own list
-const splitTopRowSlice = (items, isFirstSection) => {
+const splitTopRow = (items, isFirstSection) => {
   // First section always has a single top story
   if (isFirstSection || items.length % 4 === 1) {
     return splitAt(1, items);
@@ -16,28 +16,28 @@ const splitTopRowSlice = (items, isFirstSection) => {
 };
 
 // Split into fours and make sure slices only go through if a four
-const splitStandardSlices = items =>
+const splitStandardRows = items =>
   splitEvery(4, items).filter(itemList => itemList.length === 4);
 
 // Anything beyond the first 2 fours goes into an imageless slice
-const splitNoImageSlices = standardSlices => splitAt(2, standardSlices);
+const splitNoImageRow = standardSlices => splitAt(2, standardSlices);
 
-const getItems = (items, isFirstSection) => {
-  const presentStories = getItemLimit(items, isFirstSection);
+const getRows = (items, isFirstSection) => {
+  const allowedItems = getAllowedItems(items, isFirstSection);
 
-  const [featuredSliceItems, unsplitregularItems] = splitTopRowSlice(
-    presentStories,
+  const [topRow, unsplitregularItems] = splitTopRow(
+    allowedItems,
     isFirstSection,
   );
-  const [regularItems, [noImageItems]] = splitNoImageSlices(
-    splitStandardSlices(unsplitregularItems),
+  const [regularRows, [noImageRow]] = splitNoImageRow(
+    splitStandardRows(unsplitregularItems),
   );
 
   return {
-    featuredSliceItems,
-    regularItems,
-    noImageItems: noImageItems || [],
+    topRow,
+    regularRows,
+    noImageRow: noImageRow || [],
   };
 };
 
-export default getItems;
+export default getRows;
--- src/app/containers/FrontPageSection/utilities/storySplitter.test.jsx
+++ src/app/containers/FrontPageSection/utilities/storySplitter.test.jsx
@@ -1,258 +1,258 @@
 import { range } from 'ramda';
-import getItems from './storySplitter';
+import getRows from './storySplitter';
 
 const numberOfStories = {
   0: {
     input: [],
     expectedFirstSection: {
-      featuredSliceItems: [],
-      regularItems: [],
-      noImageItems: [],
+      topRow: [],
+      regularRows: [],
+      noImageRow: [],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [],
-      regularItems: [],
-      noImageItems: [],
+      topRow: [],
+      regularRows: [],
+      noImageRow: [],
     },
   },
   1: {
     input: range(0, 1),
     expectedFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [],
-      noImageItems: [],
+      topRow: [0],
+      regularRows: [],
+      noImageRow: [],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [],
-      noImageItems: [],
+      topRow: [0],
+      regularRows: [],
+      noImageRow: [],
     },
   },
   2: {
     input: range(0, 2),
     expectedFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [],
-      noImageItems: [],
+      topRow: [0],
+      regularRows: [],
+      noImageRow: [],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [0, 1],
-      regularItems: [],
-      noImageItems: [],
+      topRow: [0, 1],
+      regularRows: [],
+      noImageRow: [],
     },
   },
   3: {
     input: range(0, 3),
     expectedFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [],
-      noImageItems: [],
+      topRow: [0],
+      regularRows: [],
+      noImageRow: [],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [0, 1],
-      regularItems: [],
-      noImageItems: [],
+      topRow: [0, 1],
+      regularRows: [],
+      noImageRow: [],
     },
   },
   4: {
     input: range(0, 4),
     expectedFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [],
-      noImageItems: [],
+      topRow: [0],
+      regularRows: [],
+      noImageRow: [],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [],
-      regularItems: [[0, 1, 2, 3]],
-      noImageItems: [],
+      topRow: [],
+      regularRows: [[0, 1, 2, 3]],
+      noImageRow: [],
     },
   },
   5: {
     input: range(0, 5),
     expectedFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [[1, 2, 3, 4]],
-      noImageItems: [],
+      topRow: [0],
+      regularRows: [[1, 2, 3, 4]],
+      noImageRow: [],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [[1, 2, 3, 4]],
-      noImageItems: [],
+      topRow: [0],
+      regularRows: [[1, 2, 3, 4]],
+      noImageRow: [],
     },
   },
   6: {
     input: range(0, 6),
     expectedFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [[1, 2, 3, 4]],
-      noImageItems: [],
+      topRow: [0],
+      regularRows: [[1, 2, 3, 4]],
+      noImageRow: [],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [0, 1],
-      regularItems: [[2, 3, 4, 5]],
-      noImageItems: [],
+      topRow: [0, 1],
+      regularRows: [[2, 3, 4, 5]],
+      noImageRow: [],
     },
   },
   7: {
     input: range(0, 7),
     expectedFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [[1, 2, 3, 4]],
-      noImageItems: [],
+      topRow: [0],
+      regularRows: [[1, 2, 3, 4]],
+      noImageRow: [],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [0, 1],
-      regularItems: [[2, 3, 4, 5]],
-      noImageItems: [],
+      topRow: [0, 1],
+      regularRows: [[2, 3, 4, 5]],
+      noImageRow: [],
     },
   },
   8: {
     input: range(0, 8),
     expectedFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [[1, 2, 3, 4]],
-      noImageItems: [],
+      topRow: [0],
+      regularRows: [[1, 2, 3, 4]],
+      noImageRow: [],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [],
-      regularItems: [
+      topRow: [],
+      regularRows: [
         [0, 1, 2, 3],
         [4, 5, 6, 7],
       ],
-      noImageItems: [],
+      noImageRow: [],
     },
   },
   9: {
     input: range(0, 9),
     expectedFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [
+      topRow: [0],
+      regularRows: [
         [1, 2, 3, 4],
         [5, 6, 7, 8],
       ],
-      noImageItems: [],
+      noImageRow: [],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [
+      topRow: [0],
+      regularRows: [
         [1, 2, 3, 4],
         [5, 6, 7, 8],
       ],
-      noImageItems: [],
+      noImageRow: [],
     },
   },
   10: {
     input: range(0, 10),
     expectedFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [
+      topRow: [0],
+      regularRows: [
         [1, 2, 3, 4],
         [5, 6, 7, 8],
       ],
-      noImageItems: [],
+      noImageRow: [],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [0, 1],
-      regularItems: [
+      topRow: [0, 1],
+      regularRows: [
         [2, 3, 4, 5],
         [6, 7, 8, 9],
       ],
-      noImageItems: [],
+      noImageRow: [],
     },
   },
   11: {
     input: range(0, 11),
     expectedFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [
+      topRow: [0],
+      regularRows: [
         [1, 2, 3, 4],
         [5, 6, 7, 8],
       ],
-      noImageItems: [],
+      noImageRow: [],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [0, 1],
-      regularItems: [
+      topRow: [0, 1],
+      regularRows: [
         [2, 3, 4, 5],
         [6, 7, 8, 9],
       ],
-      noImageItems: [],
+      noImageRow: [],
     },
   },
   12: {
     input: range(0, 12),
     expectedFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [
+      topRow: [0],
+      regularRows: [
         [1, 2, 3, 4],
         [5, 6, 7, 8],
       ],
-      noImageItems: [],
+      noImageRow: [],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [0, 1],
-      regularItems: [
+      topRow: [0, 1],
+      regularRows: [
         [2, 3, 4, 5],
         [6, 7, 8, 9],
       ],
-      noImageItems: [],
+      noImageRow: [],
     },
   },
   13: {
     input: range(0, 13),
     expectedFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [
+      topRow: [0],
+      regularRows: [
         [1, 2, 3, 4],
         [5, 6, 7, 8],
       ],
-      noImageItems: [9, 10, 11, 12],
+      noImageRow: [9, 10, 11, 12],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [0, 1],
-      regularItems: [
+      topRow: [0, 1],
+      regularRows: [
         [2, 3, 4, 5],
         [6, 7, 8, 9],
       ],
-      noImageItems: [],
+      noImageRow: [],
     },
   },
   14: {
     input: range(0, 14),
     expectedFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [
+      topRow: [0],
+      regularRows: [
         [1, 2, 3, 4],
         [5, 6, 7, 8],
       ],
-      noImageItems: [9, 10, 11, 12],
+      noImageRow: [9, 10, 11, 12],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [0, 1],
-      regularItems: [
+      topRow: [0, 1],
+      regularRows: [
         [2, 3, 4, 5],
         [6, 7, 8, 9],
       ],
-      noImageItems: [],
+      noImageRow: [],
     },
   },
   20: {
     input: range(0, 20),
     expectedFirstSection: {
-      featuredSliceItems: [0],
-      regularItems: [
+      topRow: [0],
+      regularRows: [
         [1, 2, 3, 4],
         [5, 6, 7, 8],
       ],
-      noImageItems: [9, 10, 11, 12],
+      noImageRow: [9, 10, 11, 12],
     },
     expectedNotFirstSection: {
-      featuredSliceItems: [0, 1],
-      regularItems: [
+      topRow: [0, 1],
+      regularRows: [
         [2, 3, 4, 5],
         [6, 7, 8, 9],
       ],
-      noImageItems: [],
+      noImageRow: [],
     },
   },
 };
@@ -261,7 +261,7 @@ describe('getItems for first section', () => {
   describe('assertions', () => {
     Object.keys(numberOfStories).forEach(value =>
       it(`should return right splitting for ${value} stories`, () => {
-        expect(getItems(numberOfStories[value].input, true)).toStrictEqual(
+        expect(getRows(numberOfStories[value].input, true)).toStrictEqual(
           numberOfStories[value].expectedFirstSection,
         );
       }),
@@ -273,7 +273,7 @@ describe('getItems for non-first section', () => {
   describe('assertions', () => {
     Object.keys(numberOfStories).forEach(value =>
       it(`should return right splitting for ${value} stories`, () => {
-        expect(getItems(numberOfStories[value].input, false)).toStrictEqual(
+        expect(getRows(numberOfStories[value].input, false)).toStrictEqual(
           numberOfStories[value].expectedNotFirstSection,
         );
       }),
