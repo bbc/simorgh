61848039df1cc3c0c384a770eb67243aa5b4eb30
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2020-01-23 16:37:18 +0000

    Update bundle sizes

--- scripts/bundleSizeConfig.js
+++ scripts/bundleSizeConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Size limit for all bundles used by each service (K)
   // Keep these +/- 5K and update frequently!
-  MIN_SIZE: 629,
-  MAX_SIZE: 681,
+  MIN_SIZE: 634,
+  MAX_SIZE: 686,
 };
