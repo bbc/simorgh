86b9e485442bf7905dc5ff1bbafa758c0127637b
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2020-01-23 16:00:42 +0000

    Fix simorgh production url

--- src/app/containers/CpsMetadata/utils/getBrandedImage/index.js
+++ src/app/containers/CpsMetadata/utils/getBrandedImage/index.js
@@ -1,6 +1,6 @@
 const getBrandedImage = (locator, service) => {
   const iChefHost =
-    process.env.SIMORGH_APP_ENV === 'prod'
+    process.env.SIMORGH_APP_ENV === 'live'
       ? 'http://ichef.bbci.co.uk'
       : 'http://ichef.test.bbci.co.uk';
   return `${iChefHost}/news/1024/branded_${service}/${locator}`;
