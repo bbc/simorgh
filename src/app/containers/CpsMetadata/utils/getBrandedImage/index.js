7d8aadc1a5724fd437862e98f6c244136c0edc8a
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2020-01-23 14:44:25 +0000

    Get branded image from ichef

---
+++ src/app/containers/CpsMetadata/utils/getBrandedImage/index.js
@@ -0,0 +1,9 @@
+const getBrandedImage = (locator, service) => {
+  const iChefHost =
+    process.env.SIMORGH_APP_ENV === 'prod'
+      ? 'http://ichef.bbci.co.uk'
+      : 'http://ichef.test.bbci.co.uk';
+  return `${iChefHost}/news/1024/branded_${service}/${locator}`;
+};
+
+export default getBrandedImage;
