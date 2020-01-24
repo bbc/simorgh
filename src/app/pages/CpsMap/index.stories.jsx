526b6c94115b5519f1095e6458e68ae4dd1139bc
Karina Thomas <karina.thomas@bbc.co.uk>
2020-01-24 19:08:16 +0000

    Add time machine to MAP storybook

--- src/app/pages/CpsMap/index.stories.jsx
+++ src/app/pages/CpsMap/index.stories.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { BrowserRouter } from 'react-router-dom';
 import AmpDecorator from '../../../../.storybook/helpers/ampDecorator';
+import WithTimeMachine from '#testHelpers/withTimeMachine';
+
 import { ToggleContextProvider } from '#contexts/ToggleContext';
 import CPSMap from '.';
 import pageData from './fixtureData';
@@ -14,22 +16,25 @@ const defaultToggles = {
   },
 };
 
-storiesOf('Pages|Media Asset Page/Canonical', module).add('default', () => (
-  <ToggleContextProvider value={{ toggleState: defaultToggles }}>
-    <BrowserRouter>
-      <CPSMap
-        pageType="MAP"
-        isAmp={false}
-        pathname="/pathname"
-        status={200}
-        pageData={pageData}
-        service="pidgin"
-      />
-    </BrowserRouter>
-  </ToggleContextProvider>
-));
+storiesOf('Pages|Media Asset Page/Canonical', module)
+  .addDecorator(story => <WithTimeMachine>{story()}</WithTimeMachine>)
+  .add('default', () => (
+    <ToggleContextProvider value={{ toggleState: defaultToggles }}>
+      <BrowserRouter>
+        <CPSMap
+          pageType="MAP"
+          isAmp={false}
+          pathname="/pathname"
+          status={200}
+          pageData={pageData}
+          service="pidgin"
+        />
+      </BrowserRouter>
+    </ToggleContextProvider>
+  ));
 
 storiesOf('Pages|Media Asset Page/AMP', module)
+  .addDecorator(story => <WithTimeMachine>{story()}</WithTimeMachine>)
   .addDecorator(AmpDecorator)
   .add('default', () => (
     <ToggleContextProvider value={{ toggleState: defaultToggles }}>
