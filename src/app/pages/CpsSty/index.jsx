c54a1dc552f8a133042b99b5b75388d4a8a72394
Rebecca McGinn <mcginr02@mc-n362841.local>
2020-01-24 16:25:28 +0000

    Updating STYs and PGLs to fix timestamp popout

--- src/app/pages/CpsSty/index.jsx
+++ src/app/pages/CpsSty/index.jsx
@@ -58,7 +58,7 @@ const CpsStyContainer = ({ pageData }) => {
     image,
     timestamp: props =>
       allowDateStamp ? (
-        <StyledTimestamp {...props} minutesTolerance={1} />
+        <StyledTimestamp {...props} popOut={false} minutesTolerance={1} />
       ) : null,
     video: props => <MediaPlayer {...props} assetUri={assetUri} />,
     version: props => <MediaPlayer {...props} assetUri={assetUri} />,
