f6e08e018fc630b8d85ab482b572ec8cd6fc91e9
Harvey Peachey <harvey.peachey@bbc.co.uk>
2020-01-28 10:32:36 +0000

    Re-added context for pgl and map specific test

--- cypress/integration/pages/testsForAllPages.js
+++ cypress/integration/pages/testsForAllPages.js
@@ -397,9 +397,10 @@ export const testsThatFollowSmokeTestConfigforAllPages = ({
           );
       });
     });
-
     if (pageType === 'mediaAssetPage' || pageType === 'photoGalleryPage') {
       describe('CPS PGL and MAP Tests', () => {
+        // Expects a second timestamp only if lastPublished is 1 minute later than firstPublished.
+        // This is due to a CPS asset bug, see issue simorgh#5065
         it('should render a timestamp', () => {
           cy.request(`${config[service].pageTypes[pageType].path}.json`).then(
             ({ body }) => {
@@ -431,6 +432,7 @@ export const testsThatFollowSmokeTestConfigforAllPages = ({
         });
       });
     }
+    // End of block (pageType === 'mediaAssetPage' || pageType === 'photoGalleryPage')
   });
 };
 
