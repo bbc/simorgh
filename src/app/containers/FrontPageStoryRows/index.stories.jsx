e818180b38db4c7bf6735a1aa57c8e9e88d70a05
Alistair Gempf <alistair.gempf@bbc.co.uk>
2020-01-28 09:12:11 +0000

    Add no image row story

--- src/app/containers/FrontPageStoryRows/index.stories.jsx
+++ src/app/containers/FrontPageStoryRows/index.stories.jsx
@@ -1,33 +1,41 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Grid from '@bbc/psammead-grid';
-import { take } from 'ramda';
 import { withKnobs } from '@storybook/addon-knobs';
-import { withServicesKnob } from '@bbc/psammead-storybook-helpers';
 import { ServiceContextProvider } from '#contexts/ServiceContext';
 import { RequestContextProvider } from '#contexts/RequestContext';
 import { topStoryColumns } from './storyColumns';
 import { TopRow, LeadingRow, RegularRow } from '.';
-import getPromoFixtures from './testHelpers';
-
-const standardPromos = (dir, number) => take(number, getPromoFixtures(dir));
+import getNumberPromoFixtures from './testHelpers';
 
 // eslint-disable-next-line react/prop-types
-const TopRowStory = ({ dir }) => (
-  <TopRow stories={standardPromos(dir, 1)} dir={dir} />
+const TopRowStory = ({ dir, displayImages }) => (
+  <TopRow
+    stories={getNumberPromoFixtures(dir, 1)}
+    dir={dir}
+    displayImages={displayImages}
+  />
 );
 
 // eslint-disable-next-line react/prop-types
-const LeadingRowStory = ({ dir }) => (
-  <LeadingRow stories={standardPromos(dir, 2)} dir={dir} />
+const LeadingRowStory = ({ dir, displayImages }) => (
+  <LeadingRow
+    stories={getNumberPromoFixtures(dir, 2)}
+    dir={dir}
+    displayImages={displayImages}
+  />
 );
 
 // eslint-disable-next-line react/prop-types
-const RegularRowStory = ({ dir }) => (
-  <RegularRow stories={standardPromos(dir, 4)} displayImages dir={dir} />
+const RegularRowStory = ({ dir, displayImages }) => (
+  <RegularRow
+    stories={getNumberPromoFixtures(dir, 4)}
+    displayImages={displayImages}
+    dir={dir}
+  />
 );
 
-const getRow = (RowType, dir = 'ltr') => {
+const getRow = (RowType, dir = 'ltr', displayImages = true) => {
   return (
     <ServiceContextProvider service="news">
       <RequestContextProvider
@@ -38,7 +46,7 @@ const getRow = (RowType, dir = 'ltr') => {
         service="news"
       >
         <Grid enableGelGutters columns={topStoryColumns} dir={dir}>
-          <RowType dir={dir} />
+          <RowType dir={dir} displayImages={displayImages} />
         </Grid>
       </RequestContextProvider>
     </ServiceContextProvider>
@@ -50,10 +58,11 @@ storiesOf('Containers|Front Page Story Row', module)
     chromatic: { disable: true },
   })
   .addDecorator(withKnobs)
-  .addDecorator(withServicesKnob())
   .add('Top Row', () => getRow(TopRowStory))
   .add('Leading Row', () => getRow(LeadingRowStory))
   .add('Regular Row', () => getRow(RegularRowStory))
+  .add('NoImage Row', () => getRow(RegularRowStory, 'ltr', false))
   .add('Top Row RTL', () => getRow(TopRowStory, 'rtl'))
   .add('Leading Row RTL', () => getRow(LeadingRowStory, 'rtl'))
-  .add('Regular Row RTL', () => getRow(RegularRowStory, 'rtl'));
+  .add('Regular Row RTL', () => getRow(RegularRowStory, 'rtl'))
+  .add('NoImage Row RTL', () => getRow(RegularRowStory, 'rtl', false));
