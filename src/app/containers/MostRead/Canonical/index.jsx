63971ba58607ca25d895aa96620406f9bfcd035b
Ho Ting Lok <hoting.lok@bbc.co.uk>
2020-02-03 15:46:53 +0000

    Refactor of mostReadRecordIsFresh check

--- src/app/containers/MostRead/Canonical/index.jsx
+++ src/app/containers/MostRead/Canonical/index.jsx
@@ -22,37 +22,32 @@ const CanonicalMostRead = ({ endpoint }) => {
   useEffect(() => {
     const handleResponse = async response => {
       const mostReadData = await response.json();
-
       // do not show most read if lastRecordUpdated is greater than 35min as this means PopAPI has failed twice
       // in succession. This suggests ATI may be having issues, hence risk of stale data.
-      if (!mostReadRecordIsFresh(mostReadData.lastRecordTimeStamp)) {
-        return;
+      if (mostReadRecordIsFresh(mostReadData.lastRecordTimeStamp)) {
+        const mostReadItems = mostReadData.records
+          .slice(0, numberOfItems)
+          .map(({ id, promo: { headlines, locators, timestamp } }) => ({
+            id,
+            title: headlines.shortHeadline,
+            href: locators.assetUri,
+            timestamp: (
+              <LastUpdated
+                prefix={lastUpdated}
+                script={script}
+                service={service}
+                timestamp={timestamp}
+                locale={datetimeLocale}
+              />
+            ),
+          }));
+        setItems(mostReadItems);
       }
-
-      const mostReadItems = mostReadData.records
-        .slice(0, numberOfItems)
-        .map(({ id, promo: { headlines, locators, timestamp } }) => ({
-          id,
-          title: headlines.shortHeadline,
-          href: locators.assetUri,
-          timestamp: (
-            <LastUpdated
-              prefix={lastUpdated}
-              script={script}
-              service={service}
-              timestamp={timestamp}
-              locale={datetimeLocale}
-            />
-          ),
-        }));
-      setItems(mostReadItems);
     };
-
     const fetchMostReadData = pathname =>
       fetch(pathname, { mode: 'no-cors' })
         .then(handleResponse)
         .catch(e => logger.error(`HTTP Error: "${e}"`));
-
     fetchMostReadData(endpoint);
   }, [endpoint, numberOfItems, datetimeLocale, lastUpdated, script, service]);
 
