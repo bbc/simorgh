443fb41b3d4c5b28bcbc7419bc0e2b5946c7eb9b
rowland <rowland.henshaw@andela.com>
2020-02-05 17:23:56 +0100

    added test for byline rules

--- src/app/lib/utilities/preprocessor/rules/cpsAssetPage/addBylineBlock/index.js
+++ src/app/lib/utilities/preprocessor/rules/cpsAssetPage/addBylineBlock/index.js
@@ -1,35 +1,34 @@
 import pathOr from 'ramda/src/pathOr';
 import deepClone from 'ramda/src/clone';
-import path from 'ramda/src/path';
 import convertToBylineBlock from './models';
 
 const getBylineBlock = json => {
   const byline = pathOr(null, ['promo', 'byline'], json);
 
   if (!byline) {
-    return json;
+    return null;
   }
 
   return byline;
 };
 
-const insertBylineBlock = originalJson => {
+const addBylineBlock = originalJson => {
   const json = deepClone(originalJson);
-  const type = path(['metadata', 'type'], json);
+  const type = pathOr(null, ['metadata', 'type'], json);
 
-  const blocks = path(['content', 'model', 'blocks'], json);
+  const blocks = pathOr(null, ['content', 'model', 'blocks'], json);
 
-  if (!blocks) {
+  if (!blocks || type !== 'STY') {
     return json;
   }
 
-  if (type === 'STY') {
-    // add after headline block
-    const bylineBlock = convertToBylineBlock(getBylineBlock(json));
+  const byline = getBylineBlock(json);
+  if (byline) {
+    const bylineBlock = convertToBylineBlock(byline);
     json.content.model.blocks = [blocks.shift(), bylineBlock, ...blocks];
   }
 
   return json;
 };
 
-export default insertBylineBlock;
+export default addBylineBlock;
