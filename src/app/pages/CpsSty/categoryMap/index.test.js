6b7601ea3f94a60180d8a47dca60c10dc4191ec0
Fahad <34196381+FK78@users.noreply.github.com>
2020-02-10 16:44:27 +0000

    Update index.test.js

--- src/app/pages/CpsSty/categoryMap/index.test.js
+++ src/app/pages/CpsSty/categoryMap/index.test.js
@@ -1,13 +1,13 @@
 import categoryMap from './index';
 
-test('should return the correct mapping when category recognised', () => {
+test('should return the correct mapping when the category is recognised', () => {
   expect(categoryMap('Analysis')).toEqual('AnalysisNewsArticle');
 });
 
-test('should return NewsArticle when category is null', () => {
+test('should return NewsArticle when the category is null', () => {
   expect(categoryMap()).toEqual('NewsArticle');
 });
 
-test('should return NewsArticle when category is not recognised', () => {
+test('should return NewsArticle when the category is not recognised', () => {
   expect(categoryMap('Random Category')).toEqual('NewsArticle');
 });
