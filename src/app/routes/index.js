915a62f0088f6afd38299d3d66d1c640c088288c
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-02-10 14:55:05 +0000

    error routes need to be last

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,8 +1,8 @@
 import article from './article';
 import cpsAsset from './cpsAsset';
-import error200 from './error200';
-import error404 from './error404';
 import home from './home';
 import radio from './radio';
+import error200 from './error200';
+import error404 from './error404';
 
-export default [article, cpsAsset, error200, error404, home, radio];
+export default [article, cpsAsset, home, radio, error200, error404];
