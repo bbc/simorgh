3024b6259bcbdc964f114dd67ec5a6dd69c1c475
J Blaut <jagoda.blaut@bbc.co.uk>
2020-02-11 09:06:40 +0000

    update links

--- src/app/lib/config/services/hindi.js
+++ src/app/lib/config/services/hindi.js
@@ -168,7 +168,7 @@ export const service = {
     },
     footer: {
       trustProjectLink: {
-        href: 'https://www.bbc.com/news/help-41670342',
+        href: 'https://www.bbc.com/hindi/institutional-50223932',
         text: 'आप बीबीसी पर क्यों भरोसा कर सकते हैं',
       },
       externalLink: {
