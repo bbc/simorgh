4052bb20b0446ef4f4fe540b6766835f4f2cfa8d
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-02-13 11:25:06 +0000

    use correct processor functions in home getInitialData

--- src/app/routes/home/getInitialData.js
+++ src/app/routes/home/getInitialData.js
@@ -1,13 +1,22 @@
 import pipe from 'ramda/src/pipe';
 import fetchPageData from '../fetchPageData';
-import applyTimestampRules from '#lib/utilities/preprocessor/rules/timestamp';
-import addIdsToBlocks from '#lib/utilities/preprocessor/rules/addIdsToBlocks';
-import applyBlockPositioning from '#lib/utilities/preprocessor/rules/blockPositioning';
+import filterUnknownContentTypes from '#lib/utilities/preprocessor/rules/filterContentType';
+import filterEmptyGroupItems from '#lib/utilities/preprocessor/rules/filterEmptyGroupItems';
+import applySquashTopstories from '#lib/utilities/preprocessor/rules/topstories';
+import addIdsToItems from '#lib/utilities/preprocessor/rules/addIdsToItems';
+import filterGroupsWithoutStraplines from '#lib/utilities/preprocessor/rules/filterGroupsWithoutStraplines';
 
 const processPageData = pipe(
-  applyTimestampRules,
-  addIdsToBlocks,
-  applyBlockPositioning,
+  filterUnknownContentTypes,
+  filterEmptyGroupItems,
+  addIdsToItems,
+  applySquashTopstories,
+  filterGroupsWithoutStraplines,
 );
 
-export default path => fetchPageData(path).then(processPageData);
+export default async path => {
+  const { pageData: rawPageData, ...rest } = await fetchPageData(path);
+  const processedPageData = processPageData(rawPageData);
+
+  return { pageData: processedPageData, ...rest };
+};
