7e9cf4927cda90aee553ac6ec778f228164dfb56
Ikechi Michael <michael.ikechi.ext@bbc.co.uk>
2020-02-14 08:38:27 +0100

    Update cypress/integration/pages/storyPage/tests.js
    Co-Authored-By: Harvey Peachey <harvey.peachey@bbc.co.uk>

--- cypress/integration/pages/storyPage/tests.js
+++ cypress/integration/pages/storyPage/tests.js
@@ -6,7 +6,7 @@ export const testsThatAlwaysRun = ({ service, pageType }) => {
 
 // For testing features that may differ across services but share a common logic e.g. translated strings.
 export const testsThatFollowSmokeTestConfig = ({ service, pageType }) => {
-  describe(`testsThatFollowSmokeTestConfig to run for ${service} ${pageType}`, () => {});
+  describe(`No testsThatFollowSmokeTestConfig to run for ${service} ${pageType}`, () => {});
 };
 
 // For testing low priority things e.g. cosmetic differences, and a safe place to put slow tests.
