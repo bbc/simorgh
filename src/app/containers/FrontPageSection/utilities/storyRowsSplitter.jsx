ef5bb5ee06dc2eecf3d305b6cca939f51f1a99ed
Alistair Gempf <alistair.gempf@bbc.co.uk>
2020-02-17 16:01:01 +0000

    Replace ramda splitAt with raw JS slice

--- src/app/containers/FrontPageSection/utilities/storyRowsSplitter.jsx
+++ src/app/containers/FrontPageSection/utilities/storyRowsSplitter.jsx
@@ -1,17 +1,16 @@
 import splitEvery from 'ramda/src/splitEvery';
-import splitAt from 'ramda/src/splitAt';
 
 // Split the top row stories out into their own list
 const splitFirstRow = (items, isFirstSection) => {
   const shouldHaveOneItemInFirstRow = isFirstSection || items.length % 4 === 1;
   const shouldHaveTwoItemsInFirstRow = items.length % 4 > 1;
   if (shouldHaveOneItemInFirstRow) {
-    return splitAt(1, items);
+    return [items.slice(0, 1), items.slice(1)];
   }
   if (shouldHaveTwoItemsInFirstRow) {
-    return splitAt(2, items);
+    return [items.slice(0, 2), items.slice(2)];
   }
-  return splitAt(0, items); // 0 items in first row
+  return [items.slice(0, 0), items.slice(0)]; // 0 items in first row
 };
 
 // Split into fours and make sure slices only go through if a four
@@ -19,7 +18,10 @@ const splitStandardRows = items =>
   splitEvery(4, items).filter(itemList => itemList.length === 4);
 
 // Anything beyond the first 2 fours goes into an imageless slice
-const splitNoImageRow = standardSlices => splitAt(2, standardSlices);
+const splitNoImageRow = standardSlices => [
+  standardSlices.slice(0, 2),
+  standardSlices.slice(2),
+];
 
 const getRows = (items, isFirstSection) => {
   const [firstRow, unsplitregularItems] = splitFirstRow(items, isFirstSection);
