23021e3bda0daa47f9abaf4331b5b2b12d337b7a
Mykeels <mykehell123@gmail.com>
2020-02-17 03:45:10 +0300

    test: add tests for STY page description

--- cypress/integration/pages/storyPage/tests.js
+++ cypress/integration/pages/storyPage/tests.js
@@ -1,3 +1,6 @@
+import { pathOr } from 'ramda';
+import config from '../../../support/config/services';
+
 // For testing important features that differ between services, e.g. Timestamps.
 // We recommend using inline conditional logic to limit tests to services which differ.
 export const testsThatAlwaysRun = ({ service, pageType }) => {
@@ -6,7 +9,24 @@ export const testsThatAlwaysRun = ({ service, pageType }) => {
 
 // For testing features that may differ across services but share a common logic e.g. translated strings.
 export const testsThatFollowSmokeTestConfig = ({ service, pageType }) => {
-  describe(`No testsThatFollowSmokeTestConfig to run for ${service} ${pageType}`, () => {});
+  describe(`testsThatFollowSmokeTestConfig to run for ${service} ${pageType}`, () => {
+    it('should render a description for the page', () => {
+      cy.request(`${config[service].pageTypes[pageType].path}.json`).then(
+        ({ body }) => {
+          const descriptionBlock = body.content.blocks.find(
+            block => block.role === 'introduction',
+          );
+          const descriptionHtml = pathOr({}, ['text'], descriptionBlock);
+
+          // strip html from the description, so we get description as plain text
+          const elem = document.createElement('div');
+          elem.innerHTML = descriptionHtml;
+          const description = elem.innerText;
+          cy.get('main p').should('contain', description);
+        },
+      );
+    });
+  });
 };
 
 // For testing low priority things e.g. cosmetic differences, and a safe place to put slow tests.
