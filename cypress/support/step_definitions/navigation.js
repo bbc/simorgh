d9fe7e453fa7b4ecd6eff314f7d9a241b40c4913
Karina Thomas <karina.thomas@bbc.co.uk>
2020-02-19 18:20:22 +0000

    Set service as Cypress environment variable, so that it can be used in subsequent step definitions

--- cypress/support/step_definitions/navigation.js
+++ cypress/support/step_definitions/navigation.js
@@ -7,6 +7,8 @@ Given('I navigate to {}', url => {
   const path = url.split('.amp')[0];
   const pageUrl = `${envConfig.baseUrl}/${path}`;
 
+  Cypress.env('service', path.split('/')[0]);
+
   // Retrieve the page data file, so that it can used in later step definitions
   downloadPageData(path);
   cy.visit(pageUrl);
