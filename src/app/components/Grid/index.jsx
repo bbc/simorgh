996e559dccfaf9c8267cecbe6be5ef1221149d84
thekp <khoa.phan@bbc.co.uk>
2020-02-21 16:45:40 +0000

    update padding bottom for front page grid

--- src/app/components/Grid/index.jsx
+++ src/app/components/Grid/index.jsx
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 import Grid from '@bbc/psammead-grid';
 import {
   GEL_GROUP_2_SCREEN_WIDTH_MIN,
+  GEL_GROUP_2_SCREEN_WIDTH_MAX,
   GEL_GROUP_3_SCREEN_WIDTH_MIN,
   GEL_GROUP_4_SCREEN_WIDTH_MIN,
   GEL_GROUP_4_SCREEN_WIDTH_MAX,
@@ -10,6 +11,7 @@ import {
 import {
   GEL_SPACING,
   GEL_SPACING_DBL,
+  GEL_SPACING_TRPL,
   GEL_SPACING_QUAD,
 } from '@bbc/gel-foundations/spacings';
 import { C_GHOST } from '@bbc/psammead-styles/colours';
@@ -53,8 +55,14 @@ export const FrontPageGrid = styled(GridMaxWidthGroup5)`
   @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
     margin-top: ${GEL_SPACING_DBL};
   }
+  @media (max-width: ${GEL_GROUP_2_SCREEN_WIDTH_MAX}) {
+    padding-bottom: ${GEL_SPACING_TRPL};
+  }
   @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
     margin-top: 0;
+  }
+  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
+    padding-bottom: ${GEL_SPACING_QUAD};
   }
 `;
 
