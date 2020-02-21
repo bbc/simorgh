f76fbbd6d22ba50fbcb1dad81ee24c6e52db60ce
Simon Sinclair <simon.sinclair@bbc.co.uk>
2020-02-21 13:09:04 +0000

    Append provider's widget-js for styles etc.

--- src/app/components/OEmbed/index.jsx
+++ src/app/components/OEmbed/index.jsx
@@ -3,41 +3,11 @@ import React from 'react';
 import { string, number } from 'prop-types';
 import DOMPurify from 'dompurify';
 import styled from 'styled-components';
-import { getSansRegular } from '@bbc/psammead-styles/font-styles';
-import {
-  GEL_SPACING_TRPL,
-  GEL_SPACING_DBL,
-  GEL_SPACING,
-} from '@bbc/gel-foundations/spacings';
-import { C_SHADOW } from '@bbc/psammead-styles/colours';
-import { GEL_BODY_COPY } from '@bbc/gel-foundations/typography';
-
-const twitterStyles = `
-  .twitter-tweet {
-    margin: 0;
-    padding: ${GEL_SPACING_DBL};
-
-    > p {
-      margin-top: 0;
-      margin-bottom: ${GEL_SPACING};
-    }
-
-    > a {
-      display: block;
-      margin-top: ${GEL_SPACING};
-    }
-  }
-`;
+import { GEL_SPACING_TRPL } from '@bbc/gel-foundations/spacings';
 
 const OEmbedWrapper = styled.div`
-  border: 1px solid #eee;
-  border-radius: 5px;
-  color: ${C_SHADOW};
   margin-bottom: ${GEL_SPACING_TRPL};
   max-width: ${({ providerWidth }) => `${providerWidth}px`};
-  ${({ service }) => getSansRegular(service)}
-  ${GEL_BODY_COPY}
-  ${twitterStyles}
 `;
 
 const OEmbed = ({ service, width, html }) => {
--- src/app/containers/SocialEmbed/index.jsx
+++ src/app/containers/SocialEmbed/index.jsx
@@ -1,22 +1,39 @@
 import React, { useContext, useState, useEffect } from 'react';
+import Helmet from 'react-helmet';
 import { string } from 'prop-types';
 import OEmbed from '#app/components/OEmbed';
 import { GridItemConstrainedMedium } from '#lib/styledGrid';
 import { ServiceContext } from '#contexts/ServiceContext';
 
 import twitter from './fixtures/twitter.json';
+import youtube from './fixtures/youtube.json';
+import instagram from './fixtures/instagram.json';
 
 const SocialEmbed = ({ source, className }) => {
   const { service } = useContext(ServiceContext);
   const [embed, setEmbed] = useState(null);
+  const [widgetUrl, setWidgetUrl] = useState(null);
 
   useEffect(() => {
-    const getEmbedephantData = provider => ({ twitter }[provider]);
+    const getEmbedephantData = provider =>
+      ({ twitter, youtube, instagram }[provider]);
+    const getWidgetUrl = provider =>
+      ({
+        twitter: 'https://platform.twitter.com/widgets.js',
+        youtube: null,
+        instagram: null,
+      }[provider]);
+    setWidgetUrl(getWidgetUrl(source));
     setEmbed(getEmbedephantData(source));
   }, [source]);
 
   return embed ? (
     <GridItemConstrainedMedium className={className}>
+      {widgetUrl && (
+        <Helmet>
+          <script async="" src={widgetUrl} />
+        </Helmet>
+      )}
       <OEmbed service={service} {...embed} />
     </GridItemConstrainedMedium>
   ) : null;
