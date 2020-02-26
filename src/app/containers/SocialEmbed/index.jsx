3c6404f82ba84f5913b33ed4461696f16e2d4f7d
Harvey Peachey <harvey.peachey@bbc.co.uk>
2020-02-26 08:30:20 +0000

    Added switching for amp scripts

--- src/app/containers/SocialEmbed/index.jsx
+++ src/app/containers/SocialEmbed/index.jsx
@@ -4,6 +4,7 @@ import { string } from 'prop-types';
 import OEmbed from '#app/components/OEmbed';
 import { GridItemConstrainedMedium } from '#lib/styledGrid';
 import { ServiceContext } from '#contexts/ServiceContext';
+import { RequestContext } from '#contexts/RequestContext';
 
 import twitter from './fixtures/twitter.json';
 import youtube from './fixtures/youtube.json';
@@ -11,6 +12,7 @@ import instagram from './fixtures/instagram.json';
 
 const SocialEmbed = ({ source, className }) => {
   const { service } = useContext(ServiceContext);
+  const { isAmp } = useContext(RequestContext);
   const [embed, setEmbed] = useState(null);
   const [widgetUrl, setWidgetUrl] = useState(null);
 
@@ -19,22 +21,39 @@ const SocialEmbed = ({ source, className }) => {
       ({ twitter, youtube, instagram }[provider]);
     const getWidgetUrl = provider =>
       ({
-        twitter: 'https://platform.twitter.com/widgets.js',
-        youtube: null,
-        instagram: null,
+        twitter: isAmp
+          ? 'https://cdn.ampproject.org/v0/amp-twitter-0.1.js'
+          : 'https://platform.twitter.com/widgets.js',
+        youtube: isAmp
+          ? 'https://cdn.ampproject.org/v0/amp-youtube-0.1.js'
+          : null,
+        instagram: isAmp
+          ? 'https://cdn.ampproject.org/v0/amp-instagram-0.1.js'
+          : null,
       }[provider]);
     setWidgetUrl(getWidgetUrl(source));
     setEmbed(getEmbedephantData(source));
-  }, [source]);
+  }, [isAmp, source]);
+
+  const ampAttribute = isAmp && { 'custom-element': `amp-${source}` };
 
   return embed ? (
     <GridItemConstrainedMedium className={className}>
       {widgetUrl && (
         <Helmet>
-          <script async="" src={widgetUrl} />
+          <script async="" {...ampAttribute} src={widgetUrl} />
         </Helmet>
       )}
-      <OEmbed service={service} {...embed} />
+      {isAmp ? (
+        <amp-twitter
+          width="375"
+          height="472"
+          layout="responsive"
+          data-tweetid="1164036827667238912"
+        />
+      ) : (
+        <OEmbed service={service} {...embed} />
+      )}
     </GridItemConstrainedMedium>
   ) : null;
 };
