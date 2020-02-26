cb73323aea01895cfe5daf43d075a7531f009b9d
Simon Sinclair <simon.sinclair@bbc.co.uk>
2020-02-26 22:29:10 +0000

    Refactor SocialEmbed container to utilise AmpSocialEmbed

--- src/app/containers/SocialEmbed/index.jsx
+++ src/app/containers/SocialEmbed/index.jsx
@@ -2,69 +2,55 @@ import React, { useContext, useState, useEffect } from 'react';
 import Helmet from 'react-helmet';
 import { string } from 'prop-types';
 import OEmbed from '#app/components/OEmbed';
+import AmpSocialEmbed from '#app/components/AmpSocialEmbed';
 import { GridItemConstrainedMedium } from '#lib/styledGrid';
-import { ServiceContext } from '#contexts/ServiceContext';
 import { RequestContext } from '#contexts/RequestContext';
 
 import twitter from './fixtures/twitter.json';
 import youtube from './fixtures/youtube.json';
 import instagram from './fixtures/instagram.json';
 
-const SocialEmbed = ({ source, className }) => {
-  const { service } = useContext(ServiceContext);
+const SocialEmbedContainer = ({ className, source, id }) => {
   const { isAmp } = useContext(RequestContext);
-  const [embed, setEmbed] = useState(null);
-  const [widgetUrl, setWidgetUrl] = useState(null);
 
+  const getWidgetUrl = provider =>
+    ({
+      twitter: 'https://platform.twitter.com/widgets.js',
+      youtube: null,
+      instagram: 'https://platform.instagram.com/en_US/embeds.js',
+    }[provider]);
+
+  const [embed, setEmbed] = useState(null);
   useEffect(() => {
     const getEmbedephantData = provider =>
       ({ twitter, youtube, instagram }[provider]);
-    const getWidgetUrl = provider =>
-      ({
-        twitter: isAmp
-          ? 'https://cdn.ampproject.org/v0/amp-twitter-0.1.js'
-          : 'https://platform.twitter.com/widgets.js',
-        youtube: isAmp
-          ? 'https://cdn.ampproject.org/v0/amp-youtube-0.1.js'
-          : null,
-        instagram: isAmp
-          ? 'https://cdn.ampproject.org/v0/amp-instagram-0.1.js'
-          : null,
-      }[provider]);
-    setWidgetUrl(getWidgetUrl(source));
-    setEmbed(getEmbedephantData(source));
+    if (!isAmp) setEmbed(getEmbedephantData(source));
   }, [isAmp, source]);
 
-  const ampAttribute = isAmp && { 'custom-element': `amp-${source}` };
-
-  return embed ? (
+  return (
     <GridItemConstrainedMedium className={className}>
-      {widgetUrl && (
-        <Helmet>
-          <script async="" {...ampAttribute} src={widgetUrl} />
-        </Helmet>
-      )}
-      {isAmp ? (
-        <amp-twitter
-          width="375"
-          height="472"
-          layout="responsive"
-          data-tweetid="1164036827667238912"
-        />
-      ) : (
-        <OEmbed service={service} {...embed} />
+      {isAmp && <AmpSocialEmbed source={source} id={id} />}
+
+      {!isAmp && embed && (
+        <>
+          <Helmet>
+            <script async src={getWidgetUrl(source)} />
+          </Helmet>
+          <OEmbed {...embed} />
+        </>
       )}
     </GridItemConstrainedMedium>
-  ) : null;
+  );
 };
 
-SocialEmbed.defaultProps = {
+SocialEmbedContainer.defaultProps = {
   className: '',
 };
 
-SocialEmbed.propTypes = {
+SocialEmbedContainer.propTypes = {
   source: string.isRequired,
+  id: string.isRequired,
   className: string,
 };
 
-export default SocialEmbed;
+export default SocialEmbedContainer;
