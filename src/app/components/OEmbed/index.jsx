269fa599ea96524c8fbb29f51c2c1ee34b4adfcb
Simon Sinclair <simon.sinclair@bbc.co.uk>
2020-02-26 22:28:34 +0000

    Disable DOMPurify until it's configured properly

--- src/app/components/OEmbed/index.jsx
+++ src/app/components/OEmbed/index.jsx
@@ -1,20 +1,20 @@
 /* eslint-disable react/no-danger */
 import React from 'react';
 import { string, number } from 'prop-types';
-import DOMPurify from 'dompurify';
+// import DOMPurify from 'dompurify';
 import styled from 'styled-components';
 import { GEL_SPACING_TRPL } from '@bbc/gel-foundations/spacings';
 
 const OEmbedWrapper = styled.div`
   margin-bottom: ${GEL_SPACING_TRPL};
-  max-width: ${({ providerWidth }) => `${providerWidth}px`};
+  max-width: ${({ width }) => `${width}px`};
 `;
 
-const OEmbed = ({ service, width, html }) => {
-  const sanitizedHtml = DOMPurify.sanitize(html, { FORBID_TAGS: ['script'] });
+const OEmbed = ({ width, html }) => {
+  // const sanitizedHtml = DOMPurify.sanitize(html, { FORBID_TAGS: ['script'] });
   return (
-    <OEmbedWrapper service={service} providerWidth={width}>
-      <div dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />
+    <OEmbedWrapper width={width}>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </OEmbedWrapper>
   );
 };
@@ -22,7 +22,6 @@ const OEmbed = ({ service, width, html }) => {
 OEmbed.defaultProps = {};
 
 OEmbed.propTypes = {
-  service: string.isRequired,
   width: number.isRequired,
   html: string.isRequired,
 };
