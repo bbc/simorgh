8d6dcfc428e66ea4fecc04b20ac5a88b87af69c0
Simon Sinclair <simon.sinclair@bbc.co.uk>
2020-02-26 23:18:22 +0000

    Get ID from href/URL

--- src/app/routes/cpsAsset/getInitialData/convertToOptimoBlocks/blocks/social_embed/index.js
+++ src/app/routes/cpsAsset/getInitialData/convertToOptimoBlocks/blocks/social_embed/index.js
@@ -1,11 +1,28 @@
 const convertSocialEmbed = block => {
   const { type, source, href } = block;
+  const sourceLowerCase = source.toLowerCase();
+
+  // https://twitter.com/BBCWorld/status/1224253149176791040
+  // https://www.instagram.com/p/B8FPf4ZphHi/?utm_source=ig_web_copy_link
+  // https://youtu.be/J4KzO1P6gW4
+
+  const getIdFromUrl = (provider, url) => {
+    const getUrlIdIndex = p =>
+      ({
+        twitter: 5,
+        instagram: 4,
+        youtube: 3,
+      }[p]);
+
+    return url.split('/')[getUrlIdIndex(provider)];
+  };
 
   return {
     type,
     model: {
-      source,
+      source: sourceLowerCase,
       href,
+      id: getIdFromUrl(source, href),
     },
   };
 };
