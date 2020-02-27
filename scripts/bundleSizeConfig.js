bc3586747d787ea915b223cdedc5ed3221951c75
Alistair Gempf <alistair.gempf@bbc.co.uk>
2020-02-27 12:44:20 +0000

    Update bundle sizes a little

--- scripts/bundleSizeConfig.js
+++ scripts/bundleSizeConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Size limit for all bundles used by each service (K)
   // Keep these +/- 5K and update frequently!
-  MIN_SIZE: 715,
-  MAX_SIZE: 758,
+  MIN_SIZE: 717,
+  MAX_SIZE: 760,
 };
