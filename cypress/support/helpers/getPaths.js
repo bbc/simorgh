db6222f4d38e47673ac409e45db98a5f3f6a8227
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-02 14:29:01 +0000

    Code review feedback - don't use let

--- cypress/support/helpers/getPaths.js
+++ cypress/support/helpers/getPaths.js
@@ -2,13 +2,7 @@ import config from '../config/services';
 import getAppEnv from './getAppEnv';
 
 export default (service, pageType) => {
-  const { paths } = config[service].pageTypes[pageType];
+  const { paths = [] } = config[service].pageTypes[pageType];
 
-  let environmentPaths = [];
-
-  if (paths) {
-    environmentPaths = paths[getAppEnv()] || [];
-  }
-
-  return environmentPaths;
+  return paths[getAppEnv()] || [];
 };
