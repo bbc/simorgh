e899866638df55a4d504b3e9e99059c7b6810810
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-03 09:54:19 +0000

    Paths is an object, not an array, therefore default to {} when destructuring

--- cypress/support/helpers/getPaths.js
+++ cypress/support/helpers/getPaths.js
@@ -2,7 +2,6 @@ import config from '../config/services';
 import getAppEnv from './getAppEnv';
 
 export default (service, pageType) => {
-  const { paths = [] } = config[service].pageTypes[pageType];
-
+  const { paths = {} } = config[service].pageTypes[pageType];
   return paths[getAppEnv()] || [];
 };
