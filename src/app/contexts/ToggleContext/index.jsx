f4b24a466d39ddc266aac4d37ff771f5f0aa14d1
Chris <chris.hinds@bbc.co.uk>
2020-03-04 12:47:58 +0000

    use the toggles smimorgh application instead of AMP

--- src/app/contexts/ToggleContext/index.jsx
+++ src/app/contexts/ToggleContext/index.jsx
@@ -8,7 +8,7 @@ const ToggleContext = createContext({});
 
 const getTogglesEndpoint = (service, origin) => {
   const requestOrigin = origin ? origin : 'https://www.test.bbc.com';
-  const baseTogglesUrl = `${process.env.SIMORGH_TOGGLES_URL}/toggles?application=amp&service=${service}&__amp_source_origin=${requestOrigin}`;
+  const baseTogglesUrl = `${process.env.SIMORGH_TOGGLES_URL}/toggles?application=simorgh&service=${service}&__amp_source_origin=${requestOrigin}`;
 
   // client side renders should trigger a geoIPLookup
   if (onClient()) {
