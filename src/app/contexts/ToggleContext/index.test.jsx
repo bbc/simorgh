d423e37d56a304f27cad64730f52686b9cf2775e
Chris <chris.hinds@bbc.co.uk>
2020-03-04 21:19:54 +0000

    Mock fetch call to toggles api, add new snapshot

--- src/app/contexts/ToggleContext/index.test.jsx
+++ src/app/contexts/ToggleContext/index.test.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import fetchMock from 'fetch-mock';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import { ComponentUsingContext } from '#testHelpers/mockComponents';
+import constructTogglesEndpoint from './utils/constructTogglesEndpoint';
 
 const mockToggleValues = {
   local: {
     foobarFeature: {
       enabled: true,
+    },
+    remoteFeatureToggles: {
+      enabled: false,
     },
   },
   test: {
@@ -20,16 +25,67 @@ const mockToggleValues = {
   },
 };
 
+const mockToggleValuesRemoteFeatureToglles = {
+  local: {
+    remoteFeatureToggles: {
+      enabled: true,
+    },
+  },
+};
+
+const mockTogglesResponse = {
+  toggles: {
+    toggleName: {
+      enabled: true,
+      value: '',
+    },
+  },
+};
+
 jest.mock('#lib/config/toggles', () => mockToggleValues);
+jest.mock('./utils/constructTogglesEndpoint', () =>
+  jest.fn().mockReturnValue('test.com'),
+);
+
+beforeEach(() => {
+  process.env.SIMORGH_APP_ENV = 'local';
+  global.fetch = fetch;
+});
+
+afterEach(() => {
+  fetchMock.restore();
+});
 
 // Require after mock to allow mocking of JS object
 const { ToggleContext, ToggleContextProvider } = require('./index');
 
-describe('ToggleContext', () => {
+describe('ToggleContext without remote feature toggles', () => {
+  // fetchMock.mock('test.com', mockTogglesResponse);
+
   shouldMatchSnapshot(
     `should provide a toggles object`,
-    <ToggleContextProvider>
+    <ToggleContextProvider service="mundo">
       <ComponentUsingContext context={ToggleContext} />
     </ToggleContextProvider>,
   );
+});
+
+describe('ToggleContext with remote feature toggles', () => {
+  jest.mock('#lib/config/toggles', () => mockToggleValuesRemoteFeatureToglles);
+  fetchMock.mock('test.com', mockTogglesResponse);
+
+  shouldMatchSnapshot(
+    `should provide a toggles object`,
+    <ToggleContextProvider service="mundo">
+      <ComponentUsingContext context={ToggleContext} />
+    </ToggleContextProvider>,
+  );
+
+  it('should trigger useEffect', () => {
+    // TODO
+  });
+
+  it('should dispatch the updateToggles method', () => {
+    // TODO
+  });
 });
