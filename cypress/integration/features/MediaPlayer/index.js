187163bb0bfda7471528be932eff6dddd9346ad2
Karina Thomas <58214768+karinathomasbbc@users.noreply.github.com>
2020-03-06 01:18:05 +0000

    Remove unnecessary step definitions

--- cypress/integration/features/MediaPlayer/index.js
+++ cypress/integration/features/MediaPlayer/index.js
@@ -1,13 +1,5 @@
 import { When } from 'cypress-cucumber-preprocessor/steps';
-import { playMedia, playMediaWithPlaceholder } from '../../common/media';
-
-When('I click the article play video button', () => {
-  playMediaWithPlaceholder('StyledVideoContainer', 'button');
-});
-
-When('I click the article play audio button', () => {
-  playMediaWithPlaceholder('StyledVideoContainer', 'button');
-});
+import { playMedia } from '../../common/media';
 
 When('I click the play video button', () => {
   playMedia('StyledVideoContainer', 'button.p_cta');
