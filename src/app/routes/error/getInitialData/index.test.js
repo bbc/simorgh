de0adee0e471233b5a48e660158b10a6c06c5ce7
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-03-06 15:57:27 +0000

    fix test

--- src/app/routes/error/getInitialData/index.test.js
+++ src/app/routes/error/getInitialData/index.test.js
@@ -1,8 +1,9 @@
 import getInitialData from '.';
+import { errorPagePath } from '../../utils/regex';
 
 it('should resolve with status 200 and errorCode', async () => {
   const expected = { status: 200, errorCode: 404 };
-  const actual = await getInitialData('/pidgin/404');
+  const actual = await getInitialData(errorPagePath)('/pidgin/404');
 
   expect(expected).toEqual(actual);
 });
