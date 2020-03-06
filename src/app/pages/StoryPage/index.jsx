a6d3885fcd60d9b0de0eea3524116324ac8fe5f4
Ikechi Michael <michael.ikechi.ext@bbc.co.uk>
2020-03-06 14:13:18 +0100

    fix: rm gray background

--- src/app/pages/StoryPage/index.jsx
+++ src/app/pages/StoryPage/index.jsx
@@ -5,7 +5,6 @@ import {
   GEL_SPACING_TRPL,
   GEL_SPACING_QUAD,
 } from '@bbc/gel-foundations/spacings';
-import { C_LUNAR } from '@bbc/psammead-styles/colours';
 import { GEL_GROUP_4_SCREEN_WIDTH_MIN } from '@bbc/gel-foundations/breakpoints';
 import path from 'ramda/src/path';
 import pathOr from 'ramda/src/pathOr';
@@ -101,7 +100,6 @@ const StoryPage = ({ pageData }) => {
   `;
 
   const ComponentWrapper = styled.div`
-    background: ${C_LUNAR};
     margin-bottom: ${GEL_SPACING_TRPL};
     padding: ${GEL_SPACING_DBL};
   `;
