732117e24f9303a478e690db6e979e794b7bc5e7
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2020-03-07 17:17:43 +0000

    Adding metadata ID validation to onDemand radio test

--- src/app/routes/onDemandRadio/getInitialData/index.test.js
+++ src/app/routes/onDemandRadio/getInitialData/index.test.js
@@ -7,6 +7,9 @@ it('should return essential data for a page to render', async () => {
   const { pageData } = await getInitialData('mock-live-radio-path');
 
   expect(pageData.promo.headlines.headline).toEqual('وروستي خبرونه');
+  expect(pageData.metadata.id).toEqual(
+    'urn:bbc:ares:ws_media:page:bbc_pashto_radio/w172x8nvf4bchz5',
+  );
   expect(pageData.metadata.language).toEqual('ps');
   expect(pageData.content.blocks.length).toBeTruthy();
 });
