4702dbacde992d99621353cf54cae95a66a2c830
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-08 22:23:26 +0000

    Delete live radio cypress tests as they have been replaced with cucumber tests

--- cypress/integration/pages/liveRadio/helpers.js
+++
@@ -1,5 +0,0 @@
-// the externalId `bbc_oromo_radio` is overriden to `bbc_afaanoromoo` in production code
-const getMappedServiceId = externalId =>
-  externalId === 'bbc_oromo_radio' ? 'bbc_afaanoromoo_radio' : externalId;
-
-export default getMappedServiceId;
