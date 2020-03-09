e154bf26ffdca9bb3b8245d18a1b45052c664f4f
Fahad Khan <fahad.khan01@bbc.co.uk>
2020-03-09 17:08:52 +0000

    refactor: add CCPA link for more services.

--- src/app/lib/config/services/ukrainian.js
+++ src/app/lib/config/services/ukrainian.js
@@ -193,6 +193,12 @@ export const service = {
           href: 'https://www.bbc.com/ukrainian/institutional-38146278',
           text: "Зв'язатися з BBC",
         },
+        {
+          href:
+            'https://www.bbc.com/usingthebbc/cookies/how-can-i-change-my-bbc-cookie-settings/',
+          text: 'AdChoices / Do Not Sell My Info',
+          lang: 'en-GB',
+        },
       ],
       copyrightText:
         'BBC. ВВС не несе відповідальності за контент інших сайтів.',
