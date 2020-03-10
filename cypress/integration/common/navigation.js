0b8babe486953ec3569364afb7382344a4da8544
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-10 23:47:06 +0000

    Add isAmp to the cypress env variables

--- cypress/integration/common/navigation.js
+++ cypress/integration/common/navigation.js
@@ -4,9 +4,9 @@ import { Given } from 'cypress-cucumber-preprocessor/steps';
 Given('I navigate to {}', pageUrl => {
   const url = new Url(pageUrl);
 
-  Cypress.env('url', url);
   Cypress.env('currentPath', url.pathname);
   Cypress.env('service', url.pathname.split('/')[0]);
+  Cypress.env('isAmp', url.pathname.split('.amp').length > 1);
 
   cy.visit(url.toString());
 });
