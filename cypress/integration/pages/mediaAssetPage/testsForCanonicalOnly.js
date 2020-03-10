e1bc008aeb234bb115d3eef9a4da8e40c756efdc
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-10 22:06:06 +0000

    Revert "Delete Cypress MAP tests - these are replaced by cucumber tests"
    This reverts commit ea5e2c3360f6d0b5196b95c68208d3e96f3a1680.

---
+++ cypress/integration/pages/mediaAssetPage/testsForCanonicalOnly.js
@@ -0,0 +1,91 @@
+import config from '../../../support/config/services';
+import envConfig from '../../../support/config/envs';
+import appConfig from '../../../../src/server/utilities/serviceConfigs';
+import { getBlockData } from './helpers';
+
+// For testing important features that differ between services, e.g. Timestamps.
+// We recommend using inline conditional logic to limit tests to services which differ.
+export const testsThatAlwaysRunForCanonicalOnly = ({ service, pageType }) => {
+  describe(`No testsToAlwaysRunForCanonicalOnly to run for ${service} ${pageType}`, () => {});
+};
+
+// For testing feastures that may differ across services but share a common logic e.g. translated strings.
+export const testsThatFollowSmokeTestConfigForCanonicalOnly = ({
+  service,
+  pageType,
+  variant,
+}) =>
+  describe(`testsThatFollowSmokeTestConfigForCanonicalOnly for ${service} ${pageType}`, () => {
+    it('should render a media player', () => {
+      cy.request(`${Cypress.env('currentPath')}.json`).then(({ body }) => {
+        const assetUriString = body.metadata.locators.assetUri;
+        if (assetUriString.split('/').length > 4) {
+          cy.log('Test skipped because legacy MAP');
+        } else {
+          const mediaBlock = body.content.blocks[0];
+          const isLiveStream = mediaBlock.type === 'version';
+          const serviceId = isLiveStream
+            ? mediaBlock.externalId
+            : mediaBlock.versions[0].versionId;
+          const language = appConfig[config[service].name][variant].lang;
+          const { assetUri } = body.metadata.locators;
+          cy.get(
+            `iframe[src*="${envConfig.avEmbedBaseUrl}/ws/av-embeds/cps${assetUri}/${serviceId}/${language}"]`,
+          ).should('be.visible');
+        }
+      });
+    });
+
+    it('should play media', () => {
+      cy.window().then(win => {
+        const assetUriString =
+          win.SIMORGH_DATA.pageData.metadata.locators.assetUri;
+        if (assetUriString.split('/').length > 4) {
+          cy.log('Test skipped because legacy MAP');
+        } else {
+          const media =
+            getBlockData('video', win.SIMORGH_DATA.pageData) ||
+            getBlockData('version', win.SIMORGH_DATA.pageData);
+
+          if (!media) throw new Error('no media');
+
+          // Ensure media player is ready
+          cy.get(
+            'div[class^="StyledVideoContainer"] iframe[class^="StyledIframe"]',
+          ).then($iframe => {
+            cy.wrap($iframe.prop('contentWindow'), {
+              timeout: 8000,
+            })
+              .its('embeddedMedia.playerInstances.mediaPlayer.ready')
+              .should('eq', true);
+          });
+
+          const playButton = 'button.p_cta';
+
+          cy.get('iframe').then(iframe => {
+            cy.wrap(iframe.contents().find('iframe'))
+              .should(
+                inner => expect(inner.contents().find(playButton)).to.exist,
+              )
+              .then(inner => cy.wrap(inner.contents().find(playButton)).click())
+              .then(() => {
+                cy.wrap(iframe.prop('contentWindow'), {
+                  timeout: 8000,
+                })
+                  .its('embeddedMedia.playerInstances.mediaPlayer')
+                  .invoke('currentTime')
+                  .should('be.gt', 0);
+              });
+          });
+        }
+      });
+    });
+  });
+
+// For testing low priority things e.g. cosmetic differences, and a safe place to put slow tests.
+export const testsThatNeverRunDuringSmokeTestingForCanonicalOnly = ({
+  service,
+  pageType,
+}) => {
+  describe(`No testsToNeverSmokeTestForCanonicalOnly to run for ${service} ${pageType}`, () => {});
+};
