ea993a7e9bc8de1e2f97045f3ed8830008b294da
Sareh Heidari <sareh@users.noreply.github.com>
2020-03-11 14:43:59 +0000

    Update tests to include case where local toggle is off

--- src/app/contexts/ToggleContext/index.test.jsx
+++ src/app/contexts/ToggleContext/index.test.jsx
@@ -2,14 +2,15 @@ import React, { useContext } from 'react';
 import { render, act } from '@testing-library/react';
 import fetchMock from 'fetch-mock';
 import '@testing-library/jest-dom/extend-expect';
+import { queryByText } from '@testing-library/dom';
 import togglesConfig from '#lib/config/toggles';
 
-const mockTogglesResponseAdsOn = {
-  toggles: { ads: { enabled: true, value: '' } },
-};
-const mockTogglesResponseAdsOff = {
-  toggles: { ads: { enabled: false, value: '' } },
-};
+// const mockTogglesResponseAdsOn = {
+//   toggles: { ads: { enabled: true, value: '' } },
+// };
+// const mockTogglesResponseAdsOff = {
+//   toggles: { ads: { enabled: false, value: '' } },
+// };
 
 beforeEach(() => {
   process.env.SIMORGH_APP_ENV = 'local';
@@ -24,9 +25,12 @@ afterEach(() => {
 // Require after mock to allow mocking of JS object
 const { ToggleContext, ToggleContextProvider } = require('./index');
 
-describe('ToggleContext with remote feature toggles', () => {
+describe('ToggleContext with feature toggles', () => {
   describe('given the feature toggle is enabled', () => {
-    togglesConfig.local.chartbeatAnalytics.enabled = true;
+    beforeEach(() => {
+      togglesConfig.local.chartbeatAnalytics.enabled = true;
+      togglesConfig.local.remoteFeatureToggles.enabled = false;
+    });
 
     it('should render the feature component', () => {
       const TestComponent = () => {
@@ -49,40 +53,78 @@ describe('ToggleContext with remote feature toggles', () => {
     });
   });
 
+  describe('given the feature toggle is disabled', () => {
+    beforeEach(() => {
+      togglesConfig.local.chartbeatAnalytics.enabled = false;
+      togglesConfig.local.remoteFeatureToggles.enabled = false;
+    });
+
+    it('should render the feature component', () => {
+      const TestComponent = () => {
+        const chartbeatIsEnabled = useContext(ToggleContext).toggleState
+          .chartbeatAnalytics.enabled;
+
+        return (
+          chartbeatIsEnabled && <div>Dummy Chartbeat Analytics Component</div>
+        );
+      };
+      const { container } = render(
+        <ToggleContextProvider service="mundo">
+          <TestComponent />
+        </ToggleContextProvider>,
+      );
+
+      expect(
+        queryByText(container, 'Dummy Chartbeat Analytics Component'),
+      ).not.toBeInTheDocument();
+    });
+  });
+
   describe('given feature toggle is disabled locally and enabled remotely i.e. enabled in iSite', () => {
-    togglesConfig.local.ads.enabled = false;
-    fetchMock.mock(
-      'https://mock-toggles-endpoint.bbc.co.uk/toggles?application=simorgh&service=mundo&__amp_source_origin=https://www.test.bbc.com&geoiplookup=true',
-      JSON.stringify({
-        toggles: {
-          ads: {
-            enabled: true,
-            value: '',
+    let container;
+
+    beforeEach(() => {
+      container = document.createElement('div');
+      document.body.appendChild(container);
+      togglesConfig.local.ads.enabled = false; // local ads toggle is false
+      togglesConfig.local.remoteFeatureToggles.enabled = true;
+      fetchMock.mock(
+        'https://mock-toggles-endpoint.bbc.co.uk/toggles?application=simorgh&service=mundo&__amp_source_origin=https://www.test.bbc.com&geoiplookup=true',
+        JSON.stringify({
+          toggles: {
+            ads: {
+              enabled: true, // remote ads toggle is true
+              value: '',
+            },
           },
-        },
-      }),
-    );
+        }),
+      );
+    });
+
+    afterEach(() => {
+      document.body.removeChild(container);
+      container = null;
+    });
 
     it('should render the feature component', async () => {
-      let getByText;
-
-      const TestComponent = () => {
-        const adsIsEnabled = useContext(ToggleContext).toggleState.ads.enabled;
-
-        return adsIsEnabled && <div>Dummy Ad Component</div>;
-      };
-
       act(() => {
+        const TestComponent = () => {
+          const adsIsEnabled = useContext(ToggleContext).toggleState.ads
+            .enabled;
+          console.log('adsIsEnabled', adsIsEnabled);
+          return adsIsEnabled && <div>Dummy Ad Component</div>;
+        };
         // todo not sure this is working
-        const thing = render(
+        const renderResult = render(
           <ToggleContextProvider service="mundo">
             <TestComponent />
           </ToggleContextProvider>,
         );
-        getByText = thing.getByText;
+        container = renderResult.container;
       });
-
-      expect(getByText('Dummy Ad Component')).toBeInTheDocument();
+      console.log('just before expect');
+      // console.log('querybytext', queryByText(container, 'Dummy Ad Component'));
+      expect(queryByText(container, 'Dummy Ad Component')).toBeInTheDocument();
     });
   });
 });
