b6bf3d286cab195dc0102ed7b9ce2739ffb00b99
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-11 01:53:52 +0000

    Allow domain to be included in the url. If not, use the default baseURL from the environment config

--- cypress/integration/common/navigation.js
+++ cypress/integration/common/navigation.js
@@ -1,8 +1,12 @@
 import Url from 'url-parse';
 import { Given } from 'cypress-cucumber-preprocessor/steps';
+import envConfig from '../../support/config/envs';
 
 Given('I navigate to {}', pageUrl => {
-  const url = new Url(pageUrl);
+  const path = pageUrl.startsWith('http')
+    ? pageUrl
+    : `${envConfig.baseUrl}/${pageUrl}`;
+  const url = new Url(path);
 
   Cypress.env('currentPath', url.pathname);
   Cypress.env('service', url.pathname.split('/')[0]);
