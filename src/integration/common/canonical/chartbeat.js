347391801934d3311ba91da9a591bea9926dcc9b
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-13 17:03:39 +0000

    Placeholder for chartbeat

---
+++ src/integration/common/canonical/chartbeat.js
@@ -0,0 +1,19 @@
+import '@testing-library/jest-dom/extend-expect';
+import { renderFullHTML } from '../../renderApp';
+
+export default ({ pageUrl }) => {
+  describe('Chartbeat', () => {
+    it('should have a noscript img tag with the ati url', async () => {
+      process.env.SIMORGH_ATI_BASE_URL = 'https://logws1363.ati-host.net?';
+      const { document } = await renderFullHTML(pageUrl);
+      const noscriptImage = document.querySelector('noscript img');
+
+      expect(noscriptImage.tagName).toEqual('IMG');
+      expect(noscriptImage.getAttribute('width')).toEqual('1px');
+      expect(noscriptImage.getAttribute('height')).toEqual('1px');
+      expect(noscriptImage.getAttribute('src')).toMatch(
+        'https://logws1363.ati-host.net?',
+      );
+    });
+  });
+};
