4094ca6b82576e60c57406e7d20053c8c4312d92
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-03-13 12:29:12 +0000

    remove describe block

--- src/integration/common/canonical/footer.js
+++ src/integration/common/canonical/footer.js
@@ -9,33 +9,29 @@ export default ({ pageUrl, copyrightText, brandingText, brandingLink }) => {
       app = await renderApp(pageUrl);
     });
 
-    describe('User tests', () => {
-      it('should render footer copyright text', () => {
-        const footerEl = document.querySelector('footer');
-        const copyrightEl = app
-          .within(footerEl)
-          .getByTextSpecial(copyrightText);
+    it('should render footer copyright text', () => {
+      const footerEl = document.querySelector('footer');
+      const copyrightEl = app.within(footerEl).getByTextSpecial(copyrightText);
 
-        expect(copyrightEl).toBeInTheDocument();
-      });
+      expect(copyrightEl).toBeInTheDocument();
+    });
 
-      it('should render BBC branding', () => {
-        const footerEl = document.querySelector('footer');
-        const brandingEl = app.within(footerEl).getByTextSpecial(brandingText);
+    it('should render BBC branding', () => {
+      const footerEl = document.querySelector('footer');
+      const brandingEl = app.within(footerEl).getByTextSpecial(brandingText);
 
-        expect(brandingEl).toBeInTheDocument();
-      });
+      expect(brandingEl).toBeInTheDocument();
+    });
 
-      it('should wrap BBC branding with an anchor that links to the homepage', () => {
-        const footerEl = document.querySelector('footer');
-        const brandingEl = app.within(footerEl).getByTextSpecial(brandingText);
-        const brandingLinkEl = brandingEl.parentNode;
-        const brandingImageEl = brandingLinkEl.querySelector('svg');
+    it('should wrap BBC branding with an anchor that links to the homepage', () => {
+      const footerEl = document.querySelector('footer');
+      const brandingEl = app.within(footerEl).getByTextSpecial(brandingText);
+      const brandingLinkEl = brandingEl.parentNode;
+      const brandingImageEl = brandingLinkEl.querySelector('svg');
 
-        expect(brandingLinkEl.tagName).toEqual('A');
-        expect(brandingLinkEl.getAttribute('href')).toEqual(brandingLink);
-        expect(brandingImageEl).toBeInTheDocument();
-      });
+      expect(brandingLinkEl.tagName).toEqual('A');
+      expect(brandingLinkEl.getAttribute('href')).toEqual(brandingLink);
+      expect(brandingImageEl).toBeInTheDocument();
     });
   });
 };
