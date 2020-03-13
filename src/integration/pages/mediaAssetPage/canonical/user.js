e6463909260c9e2413d0af992f94a7b13d25154e
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-13 17:04:01 +0000

    Adding more examples to user tests

--- src/integration/pages/mediaAssetPage/canonical/user.js
+++ src/integration/pages/mediaAssetPage/canonical/user.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import renderApp from '../../../renderApp';
 import { MEDIA_ASSET_PAGE_URL } from '../../../pageUrls';
+import getHeadlineElement from '../../../helpers/getHeadlineElement';
 
 export default () => {
   describe('User', () => {
@@ -11,12 +12,44 @@ export default () => {
     });
 
     it('should render the headline', () => {
-      // Need to getAllByText because of the VisuallyHiddenHeadline
-      const headlineEl = app.getAllByText(
-        'Konflik Iran-AS: Lima penyebab krisis tak kunjung usai',
+      const headlineEl = getHeadlineElement();
+      const headline = app
+        .within(headlineEl)
+        .getByText(
+          'Hari perempuan internasional: Bergulat demi kebebasan perempuan',
+        );
+
+      expect(headline).toBeInTheDocument();
+    });
+
+    it('should render a paragraph', () => {
+      const paragraphEl = app.getByText(
+        'Vinesh berasal dari Haryana, kota kecil di utara India. Gulat, yang kerap diasosiasikan sebagai olahraga maskulin, kini bergeser berkat Vinesh dan pegulat perempuan lainnya.',
       );
 
-      expect(headlineEl).toBeInTheDocument();
+      expect(paragraphEl).toBeInTheDocument();
+    });
+
+    it('should render a bulleted list', () => {
+      const bulletedListItemEl = document.querySelector('[role="listitem"]');
+      const listIem = app
+        .within(bulletedListItemEl)
+        .getByTextSpecial(
+          `Tara Basro: Hak perempuan atas tubuhnya dan batasan pornografi`,
+        );
+
+      expect(listIem).toBeInTheDocument();
+    });
+
+    it('should render related content', () => {
+      const relatedContentListEl = document.querySelector('[role="listitem"]');
+      const relatedContentItem = app
+        .within(relatedContentListEl)
+        .getByTextSpecial(
+          `Foto Tara Basro: Hak perempuan atas tubuhnya dan batasan pornografi`,
+        );
+
+      expect(relatedContentItem).toBeInTheDocument();
     });
   });
 };
