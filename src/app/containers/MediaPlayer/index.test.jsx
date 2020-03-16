a7ed0bba43a88d1279c1f97f47f6b4df0df59614
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2020-03-16 09:18:42 +0000

    Removing two snapshot tests that are now covered by assertions

--- src/app/containers/MediaPlayer/index.test.jsx
+++ src/app/containers/MediaPlayer/index.test.jsx
@@ -24,16 +24,6 @@ describe('MediaPlayer', () => {
 
   shouldMatchSnapshot('Renders the AMP player when platform is AMP', VideoAmp);
 
-  shouldMatchSnapshot(
-    'Renders the canonical player with a caption',
-    VideoCanonicalWithCaption,
-  );
-
-  shouldMatchSnapshot(
-    'Renders the AMP player with a caption',
-    VideoAmpWithCaption,
-  );
-
   describe('Fails and returns early when', () => {
     isNull('there is no versionId', VideoCanonicalNoVersionId);
     isNull('component is toggled off', VideoCanonicalToggledOff);
