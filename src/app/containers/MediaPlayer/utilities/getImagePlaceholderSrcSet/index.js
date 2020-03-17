29c57ea578d0b5bcf31b11a0a2d0242e03f09759
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-17 10:03:56 +0000

    Simplifying logic

--- src/app/containers/MediaPlayer/utilities/getImagePlaceholderSrcSet/index.js
+++ src/app/containers/MediaPlayer/utilities/getImagePlaceholderSrcSet/index.js
@@ -3,17 +3,13 @@ import getDefaultImage from '../getDefaultImage';
 
 const DEFAULT_RESOLUTIONS = [240, 320, 480, 624, 800];
 
-const useDefaultImage = (originCode, locator) => {
-  return !(originCode && locator);
-};
-
 export default (originCode, locator) => {
   return DEFAULT_RESOLUTIONS.map(resolution => {
-    let image = buildIChefURL({ originCode, locator, resolution });
+    const useDefault = !(originCode && locator);
 
-    if (useDefaultImage(originCode, locator)) {
-      image = getDefaultImage();
-    }
+    const image = useDefault
+      ? getDefaultImage()
+      : buildIChefURL({ originCode, locator, resolution });
 
     return `${image} ${resolution}w`;
   }).join(', ');
