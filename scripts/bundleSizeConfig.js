5dc0e3b2bb2fea42d46f536c3ab6133a3b8d9906
Ikechi Michael <michael.ikechi.ext@bbc.co.uk>
2020-03-17 14:28:33 +0100

    chore: update bundle size config

--- scripts/bundleSizeConfig.js
+++ scripts/bundleSizeConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Size limit for all bundles used by each service (K)
   // Keep these +/- 5K and update frequently!
-  MIN_SIZE: 702,
-  MAX_SIZE: 765,
+  MIN_SIZE: 724,
+  MAX_SIZE: 770,
 };
