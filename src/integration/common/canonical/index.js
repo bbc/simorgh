87b1aedc9f8e715ca85f24e7d45509724e619e1e
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-17 17:11:35 +0000

    Adding chartbeat tests

--- src/integration/common/canonical/index.js
+++ src/integration/common/canonical/index.js
@@ -2,10 +2,12 @@ import runCommonAnalyticsTests from './analytics';
 import runFooterTests from './footer';
 import runCommonSeoTests from './SEO';
 import runCommonA11yTests from './a11y';
+import runChartbeatTests from './chartbeat';
 
 export {
   runFooterTests,
   runCommonSeoTests,
   runCommonA11yTests,
   runCommonAnalyticsTests,
+  runChartbeatTests,
 };
