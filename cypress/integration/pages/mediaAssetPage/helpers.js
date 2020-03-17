373cf31443a54f877c00a66b8199103e8c27034e
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-17 13:34:01 +0000

    Ability to play media on canonical for CPS and legacy MAPs

--- cypress/integration/pages/mediaAssetPage/helpers.js
+++ cypress/integration/pages/mediaAssetPage/helpers.js
@@ -1,3 +1,5 @@
+import envConfig from '../../../support/config/envs';
+
 export const getBlockByType = (blocks, blockType) => {
   let blockData;
 
@@ -13,4 +15,30 @@ export const getBlockData = (blockType, body) => {
   const { blocks } = body.content.model;
 
   return getBlockByType(blocks, blockType);
+};
+
+export const hasMedia = body => {
+  const mediaTypes = ['video', 'version', 'media', 'legacyMedia'];
+  return mediaTypes.some(type => body.metadata.blockTypes.includes(type));
+};
+
+const getServiceId = body => {
+  const mediaBlock = body.promo.media;
+  const versionId = mediaBlock.versions && mediaBlock.versions[0].versionId;
+  const id = mediaBlock.externalId || mediaBlock.id;
+
+  return versionId || id;
+};
+
+export const getEmbedUrl = (body, language) => {
+  const isLegacyMedia = body.promo.media.type === 'legacyMedia';
+  const prefix = isLegacyMedia ? 'legacy' : 'cps';
+
+  return [
+    envConfig.avEmbedBaseUrl,
+    'ws/av-embeds',
+    `${prefix}${body.metadata.locators.assetUri}`,
+    getServiceId(body),
+    language,
+  ].join('/');
 };
