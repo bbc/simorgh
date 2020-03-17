cef871a8b6570c6a70af3522b1d4a466e291d254
Ryan McCombe <ryanmccombe@gmail.com>
2020-03-17 18:11:27 +0000

    eslint

--- src/app/routes/utils/pageIsSame/index.js
+++ src/app/routes/utils/pageIsSame/index.js
@@ -2,8 +2,5 @@ import path from 'ramda/src/path';
 
 const getPageId = path(['pageData', 'metadata', 'id']);
 
-export default (prevProps, nextProps) => {
-    console.log('memo eval', prevProps.pageData.metadata.id, nextProps.pageData.metadata.id);
-    return getPageId(prevProps) === getPageId(nextProps);
-}
-  
+export default (prevProps, nextProps) =>
+  getPageId(prevProps) === getPageId(nextProps);
