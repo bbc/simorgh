d473a004923765e41db360f4401de88c23d6956f
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-03-19 16:27:27 +0000

    fix common a11y tests

--- src/integration/common/a11y.js
+++ src/integration/common/a11y.js
@@ -1,12 +1,14 @@
+const { amp, canonical } = global;
+
 export default ({ skipToContentText, headlineText }) => {
-  let app;
+  [amp, canonical].forEach(app => {
+    it('I can see a skip to content link that links to the main content of the page', () => {
+      const skipToContentEl = app.getByText(skipToContentText);
+      const mainContentEl = app.getByText(headlineText);
 
-  it('I can see a skip to content link that links to the main content of the page', () => {
-    const skipToContentEl = app.getByText(skipToContentText);
-    const mainContentEl = app.getByText(headlineText);
-
-    expect(skipToContentEl.getAttribute('href')).toBe('#content');
-    expect(mainContentEl.getAttribute('id')).toBe('content');
-    expect(mainContentEl.getAttribute('tabindex')).toBe('-1');
+      expect(skipToContentEl.getAttribute('href')).toBe('#content');
+      expect(mainContentEl.getAttribute('id')).toBe('content');
+      expect(mainContentEl.getAttribute('tabindex')).toBe('-1');
+    });
   });
 };
