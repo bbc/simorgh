db4d547fd56962270d870e7ba36c85f0f7ff2d58
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-03-19 15:08:32 +0000

    tidier export default

--- src/app/contexts/ToggleContext/utils/constructTogglesEndpoint/index.js
+++ src/app/contexts/ToggleContext/utils/constructTogglesEndpoint/index.js
@@ -1,8 +1,8 @@
 import onClient from '#lib/utilities/onClient';
 
-const getTogglesEndpoint = (service, origin) => {
+export default (service, origin) => {
   const requestOrigin = origin || 'https://www.test.bbc.com';
-  const baseTogglesUrl = `${process.env.SIMORGH_TOGGLES_URL}/toggles?application=simorgh&service=${service}&__amp_source_origin=${requestOrigin}`;
+  const baseTogglesUrl = `${process.env.SIMORGH_TOGGLES_URL}/toggles?application=simorgh&service=${service}&__amp_source_origin=${requestOrigin}`; // __amp_source_origin is relevant to both canonical and amp
 
   // client side renders should trigger a geoIPLookup
   if (onClient()) {
@@ -11,5 +11,3 @@ const getTogglesEndpoint = (service, origin) => {
 
   return baseTogglesUrl;
 };
-
-export default getTogglesEndpoint;
