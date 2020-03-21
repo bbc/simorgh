26fa55cbf4a59f3a6152532cd8ba1ddecf7c6345
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-03-21 15:34:05 +0000

    add test for service bundle

--- src/integration/common/core.canonical.js
+++ src/integration/common/core.canonical.js
@@ -16,4 +16,21 @@ export default ({ service }) => {
       expect(bbcOriginScript.getAttribute('src')).toMatch(bundleScriptMatcher);
     });
   });
+
+  it('The service bundle is loaded', () => {
+    const bundleScriptMatcher = new RegExp(
+      `(\\/static\\/js\\/(${service})-\\w+\\.\\w+\\.js)`,
+      'g',
+    );
+    const bbcOriginScripts = Array.from(
+      canonical.document.querySelectorAll('script[src]'),
+    ).filter(script =>
+      script.getAttribute('src').startsWith('http://localhost:7080'),
+    );
+    const serviceScripts = bbcOriginScripts.filter(script =>
+      bundleScriptMatcher.test(script.getAttribute('src')),
+    );
+
+    expect(serviceScripts.length).toBe(1);
+  });
 };
