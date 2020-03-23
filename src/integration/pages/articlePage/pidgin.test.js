6f9c0a39dc35289fbde6d185935cf43d4525e96f
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-23 15:04:45 +0000

    Moving performance tests to correct location

--- src/integration/pages/articlePage/pidgin.test.js
+++ src/integration/pages/articlePage/pidgin.test.js
@@ -91,10 +91,10 @@ describe('Given I am on a Pidgin Article page', () => {
       linkedData:
         '{"@context":"http://schema.org","@type":"Article","url":"http://localhost:7080/pidgin/articles/cwl08rd38l6o","publisher":{"@type":"NewsMediaOrganization","name":"BBC News Pidgin","publishingPrinciples":"https://www.bbc.com/pidgin/institutional-48528766","logo":{"@type":"ImageObject","width":1024,"height":576,"url":"https://news.files.bbci.co.uk/ws/img/logos/og/pidgin.png"}},"image":{"@type":"ImageObject","width":1024,"height":576,"url":"https://news.files.bbci.co.uk/ws/img/logos/og/pidgin.png"},"thumbnailUrl":"https://news.files.bbci.co.uk/ws/img/logos/og/pidgin.png","mainEntityOfPage":{"@type":"WebPage","@id":"http://localhost:7080/pidgin/articles/cwl08rd38l6o","name":"This is the SEO headline of this test article"},"headline":"This is the SEO headline of this test article","datePublished":"2019-07-12T13:00:04.940Z","dateModified":"2019-09-27T15:56:33.775Z","author":{"@type":"NewsMediaOrganization","name":"BBC News Pidgin","logo":{"@type":"ImageObject","width":1024,"height":576,"url":"https://news.files.bbci.co.uk/ws/img/logos/og/pidgin.png"},"noBylinesPolicy":"https://www.bbc.com/pidgin/institutional-48528766#authorexpertise"}}',
     });
+  });
 
-    describe('When optimising the application performance', () => {
-      runPerformanceTests();
-    });
+  describe('When optimising the application performance', () => {
+    runPerformanceTests();
   });
 
   describe('When I am using assistive technology', () => {
