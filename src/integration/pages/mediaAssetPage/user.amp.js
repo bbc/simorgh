c4ba7698404e8dd8d5839df34afa809463827a2b
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-23 12:29:15 +0000

    Use page variable for consistency

--- src/integration/pages/mediaAssetPage/user.amp.js
+++ src/integration/pages/mediaAssetPage/user.amp.js
@@ -1,8 +1,8 @@
-const { amp } = global;
+const { amp: page } = global;
 
 export default () => {
   it('should render the media player', () => {
-    const mediaPlayer = amp.document.querySelector('amp-iframe'); // TODO: need a more accurate selector
+    const mediaPlayer = page.document.querySelector('amp-iframe'); // TODO: need a more accurate selector
 
     expect(mediaPlayer).toBeInTheDocument();
   });
--- src/integration/pages/mediaAssetPage/user.canonical.js
+++ src/integration/pages/mediaAssetPage/user.canonical.js
@@ -1,8 +1,8 @@
-const { canonical } = global;
+const { canonical: page } = global;
 
 export default () => {
   it('should render the media player', () => {
-    const mediaPlayer = canonical.document.querySelector('iframe'); // TODO: need a more accurate selector
+    const mediaPlayer = page.document.querySelector('iframe'); // TODO: need a more accurate selector
 
     expect(mediaPlayer).toBeInTheDocument();
   });
