37b2fc0717dc567a6e0511aee298540c9747f9fa
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2020-03-23 14:31:38 +0000

    Not overriding counterName if already exists

--- src/app/routes/cpsAsset/getInitialData/addAnalyticsCounterName/index.js
+++ src/app/routes/cpsAsset/getInitialData/addAnalyticsCounterName/index.js
@@ -1,7 +1,12 @@
 import assocPath from 'ramda/src/assocPath';
 import path from 'ramda/src/path';
 
+const hasCounterName = pagedata => {
+  return path(['metadata', 'analyticsLabels', 'counterName'], pagedata);
+};
+
 const transformer = pagedata => {
+  if (hasCounterName(pagedata)) return pagedata;
   const id = path(['metadata', 'id'], pagedata);
   const counterName = id
     .split(':')
