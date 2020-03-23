5ea0aa0852214cf1993294b080973b6312f16af5
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-23 15:08:54 +0000

    Adding missing tests

--- src/integration/pages/mediaAssetPage/pidgin.test.js
+++ src/integration/pages/mediaAssetPage/pidgin.test.js
@@ -7,9 +7,12 @@ import runAmpUserTests from './user.amp';
 import runUserTests from './user';
 
 import {
+  runHeaderTests,
   runFooterTests,
   runCommonSeoTests,
   runCommonCanonicalAnalyticsTests,
+  runCoreCanonicalTests,
+  runCoreAmpTests,
   runCommonA11yTests,
   runPerformanceTests,
 } from '../../common';
@@ -21,6 +24,10 @@ describe('Given I am on a Pidgin AMP Media Asset Page', () => {
       mediaEmbedUrl:
         'https://polling.test.bbc.co.uk/ws/av-embeds/cps/pidgin/23248703/p01kx42v/pcm/amp',
     });
+  });
+
+  describe('When the application starts', () => {
+    runCoreAmpTests({ service: 'pidgin' });
   });
 });
 
@@ -35,10 +42,18 @@ describe('Given I am on a Pidgin Canonical Media Asset Page', () => {
   describe('When I am analysing user/performance metrics', () => {
     runCommonCanonicalAnalyticsTests();
   });
+
+  describe('When the application starts', () => {
+    runCoreCanonicalTests({ service: 'pidgin' });
+  });
 });
 
 describe('Given I am on a Pidgin Media Asset Page', () => {
   describe('When I am using the website', () => {
+    runHeaderTests({
+      skipToContentText: 'Waka go wetin de inside',
+    });
+
     runUserTests({
       headlineText:
         'Simorgh: Media Pod Build First CPS Media Asset Page in Simorgh with the Help of Drew & < >',
@@ -88,10 +103,10 @@ describe('Given I am on a Pidgin Media Asset Page', () => {
       linkedData:
         '{"@context":"http://schema.org","@type":"Article","url":"http://localhost:7080/pidgin/23248703","publisher":{"@type":"NewsMediaOrganization","name":"BBC News Pidgin","publishingPrinciples":"https://www.bbc.com/pidgin/institutional-48528766","logo":{"@type":"ImageObject","width":1024,"height":576,"url":"https://news.files.bbci.co.uk/ws/img/logos/og/pidgin.png"}},"image":{"@type":"ImageObject","width":1024,"height":576,"url":"https://news.files.bbci.co.uk/ws/img/logos/og/pidgin.png"},"thumbnailUrl":"https://news.files.bbci.co.uk/ws/img/logos/og/pidgin.png","mainEntityOfPage":{"@type":"WebPage","@id":"http://localhost:7080/pidgin/23248703","name":"Simorgh: Media Pod Build First CPS Media Asset Page in Simorgh with the Help of Drew & < >"},"headline":"Simorgh: Media Pod Build First CPS Media Asset Page in Simorgh with the Help of Drew & < >","datePublished":"2019-09-13T15:31:44.000Z","dateModified":"2019-11-20T12:24:25.000Z","author":{"@type":"NewsMediaOrganization","name":"BBC News Pidgin","logo":{"@type":"ImageObject","width":1024,"height":576,"url":"https://news.files.bbci.co.uk/ws/img/logos/og/pidgin.png"},"noBylinesPolicy":"https://www.bbc.com/pidgin/institutional-48528766#authorexpertise"}}',
     });
+  });
 
-    describe('When optimising the application performance', () => {
-      runPerformanceTests();
-    });
+  describe('When optimising the application performance', () => {
+    runPerformanceTests();
   });
 
   describe('When I am using assistive technology', () => {
