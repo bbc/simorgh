b189b15855739c4f2dce55ee205e94bdad2589a3
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-23 12:30:18 +0000

    Further integration tests for MAPs

--- src/integration/pages/mediaAssetPage/user.js
+++ src/integration/pages/mediaAssetPage/user.js
@@ -9,32 +9,38 @@ export default ({
   relatedContentHeadline,
 }) => {
   [amp, canonical].forEach(page => {
-    it('I can see the headline', () => {
-      const { getByText } = within(
-        page.document.querySelector('h1[id="content"]'),
-      );
+    describe(`And using ${page.platform}`, () => {
+      it('I can see the headline', () => {
+        const { getByText } = within(
+          page.document.querySelector('h1[id="content"]'),
+        );
 
-      const headline = getByText(headlineText);
-      expect(headline).toBeInTheDocument();
-    });
+        const headline = getByText(headlineText);
+        expect(headline).toBeInTheDocument();
+      });
 
-    it('I can see the timestamp', () => {
-      const { getByText } = within(page.document.querySelector('time'));
+      it('I can see the timestamp', () => {
+        const { getByText } = within(page.document.querySelector('time'));
 
-      const timestampEl = getByText(timestamp);
+        const timestampEl = getByText(timestamp);
 
-      expect(timestampEl).toBeInTheDocument();
-    });
+        expect(timestampEl).toBeInTheDocument();
+      });
 
-    it('I can see the bulleted list item', () => {
-      const [bulletedListItemEl] = page.getAllByText(bulletedListItem);
-      expect(bulletedListItemEl).toBeInTheDocument();
-      expect(bulletedListItemEl.getAttribute('class')).toBe('BulletedListItem');
-    });
+      it('I can see the bulleted list item', () => {
+        const [bulletedListItemEl] = page.getAllByText(bulletedListItem);
+        expect(bulletedListItemEl).toBeInTheDocument();
+        expect(bulletedListItemEl.getAttribute('class')).toContain(
+          'BulletedListItem',
+        );
+      });
 
-    it('I can see related content', () => {
-      const [relatedContentItemEl] = page.getAllByText(relatedContentHeadline);
-      expect(relatedContentItemEl).toBeInTheDocument();
+      it('I can see related content', () => {
+        const [relatedContentItemEl] = page.getAllByText(
+          relatedContentHeadline,
+        );
+        expect(relatedContentItemEl).toBeInTheDocument();
+      });
     });
   });
 };
