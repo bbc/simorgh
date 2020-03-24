135cf6ce0a588385c6263427aeb2204898fb13a1
Sareh Heidari <sareh@users.noreply.github.com>
2020-03-24 10:01:51 +0000

    Remove comments

--- src/app/contexts/ToggleContext/reducer.jsx
+++ src/app/contexts/ToggleContext/reducer.jsx
@@ -1,4 +1,3 @@
-// toggle actions
 export const updateToggles = data => ({
   type: 'UPDATE_TOGGLE_STATE',
   data,
@@ -8,8 +7,6 @@ export const toggleReducer = (toggleState, action) => {
   const { type, data } = action;
   switch (type) {
     case 'UPDATE_TOGGLE_STATE':
-      // TODO: In future update toggles with the data passed in, this would be from a 3rd party API.
-
       return { ...toggleState, ...data.toggles };
     default:
       return toggleState;
