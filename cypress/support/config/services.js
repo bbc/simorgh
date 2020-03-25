aabb9f0b85d398627e548555ddfc4893d62de895
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-25 13:57:19 +0000

    Disable pidgin MAP checks in Test / Live environments

--- cypress/support/config/services.js
+++ cypress/support/config/services.js
@@ -2898,11 +2898,11 @@ const genServices = {
         environments: {
           live: {
             paths: ['/pidgin/tori-50974590'], // CPS MAP with video clip
-            enabled: true,
+            enabled: false,
           },
           test: {
             paths: ['/pidgin/23248703'], // CPS MAP with video clip
-            enabled: true,
+            enabled: false,
           },
           local: {
             paths: ['/pidgin/23248703'], // CPS MAP with video clip
