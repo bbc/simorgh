0d000f868f6ffdbbe89b8625ef27bde6d52fa9a4
J Blaut <jagoda.blaut@bbc.co.uk>
2020-03-26 12:19:44 +0000

    :camera: + update tests

--- src/app/containers/Bulletin/index.test.jsx
+++ src/app/containers/Bulletin/index.test.jsx
@@ -82,19 +82,19 @@ describe('Bulletin Container', () => {
       const { container } = render(BulletinWithContext(liveTvBulletinItem));
       const span = container.getElementsByTagName('span')[1];
 
-      expect(span.textContent).toEqual('Lee Live, ');
+      expect(span.textContent).toEqual('NA EME UGBU A ');
     });
 
     it('should render the Live Radio offscreen text', () => {
       const { container } = render(BulletinWithContext(liveRadioBulletinItem));
       const span = container.getElementsByTagName('span')[1];
 
-      expect(span.textContent).toEqual('Gee ntị Live, ');
+      expect(span.textContent).toEqual('NA EME UGBU A ');
     });
 
     it('should render the LIVE label', () => {
       const { container } = render(BulletinWithContext(liveTvBulletinItem));
-      const span = container.getElementsByTagName('span')[2];
+      const span = container.getElementsByTagName('span')[1];
 
       expect(span.getAttribute('aria-hidden')).toBeDefined();
       expect(span.getAttribute('aria-hidden')).toEqual('true');
