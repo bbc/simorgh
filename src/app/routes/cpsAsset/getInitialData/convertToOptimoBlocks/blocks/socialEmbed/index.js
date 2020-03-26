4728484e7ed9a4dfdbb07bf14ca4a88182f5645a
Simon Sinclair <simon@simonsinclair.com>
2020-03-26 14:24:58 +0000

    Escape HTML special characters in embed.oembed.html

--- src/app/routes/cpsAsset/getInitialData/convertToOptimoBlocks/blocks/socialEmbed/index.js
+++ src/app/routes/cpsAsset/getInitialData/convertToOptimoBlocks/blocks/socialEmbed/index.js
@@ -1,5 +1,18 @@
+import path from 'ramda/src/path';
+import mergeDeepLeft from 'ramda/src/mergeDeepLeft';
+
+const htmlEscape = string =>
+  string
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+
 const convertSocialEmbed = block => {
-  const { type, source, ...rest } = block;
+  const { type, source, embed, ...rest } = block;
+  const html = path(['oembed', 'html'], embed);
+
   return {
     type,
     model: {
@@ -8,6 +21,18 @@ const convertSocialEmbed = block => {
           type: source,
           model: {
             ...rest,
+            // If 'embed.oembed.html' exists, merge an htmlEscape'd version into
+            // a copy of the original object and then spread it into 'model'.
+            ...(html && {
+              embed: mergeDeepLeft(
+                {
+                  oembed: {
+                    html: htmlEscape(html),
+                  },
+                },
+                embed,
+              ),
+            }),
           },
         },
       ],
