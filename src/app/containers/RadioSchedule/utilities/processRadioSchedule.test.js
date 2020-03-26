41ca2f1e42378961dfa800880283940ed9bc0ee1
Richard Itimi <itimirichard@gmail.com>
2020-03-26 11:56:08 +0100

    add tests for processRadioSchedules

--- src/app/containers/RadioSchedule/utilities/processRadioSchedule.test.js
+++ src/app/containers/RadioSchedule/utilities/processRadioSchedule.test.js
@@ -4,13 +4,17 @@ import processRadioSchedule, {
 } from './processRadioSchedule';
 import persianRadioScheduleData from '#data/persian/bbc_persian_radio/schedule.json';
 
-const service = 'persian';
-const program = {
-  serviceId: 'bbc_dari_radio',
-  episode: { pid: 'p07zbtbf' },
-};
-
 describe('getLink', () => {
+  let program;
+  let service;
+
+  beforeAll(() => {
+    service = 'persian';
+    program = {
+      serviceId: 'bbc_dari_radio',
+      episode: { pid: 'p07zbtbf' },
+    };
+  });
   it('should return liveradio link when state is live', () => {
     expect(getLink('live', program, service)).toBe(
       '/persian/bbc_dari_radio/liveradio',
@@ -48,13 +52,61 @@ describe('getProgramState', () => {
 });
 
 describe('processRadioSchedule', () => {
-  // it('should return `live` when currentTime is greater than startTime but less than endTime', () => {
-  //   const currentTime = Date.now();
-  //   const startTime = Date.now() - 1000;
-  //   const endTime = Date.now() + 1000;
+  let programs;
+  const service = 'persian';
+  describe('Complete schedule data', () => {
+    beforeAll(() => {
+      programs = processRadioSchedule(
+        persianRadioScheduleData,
+        service,
+        Date.now(),
+      );
+    });
 
-  //   expect(getProgramState(currentTime, startTime, endTime)).toBe('live');
-  // });
-  processRadioSchedule(persianRadioScheduleData, service, Date.now());
-  // console.log(programs);
+    it('should return an array of four programs', () => {
+      expect(programs).toHaveLength(4);
+    });
+
+    it('should return programs in the right order', () => {
+      // Checks the order of live and onDemand programs
+      expect(programs[0].startTime).toBeGreaterThan(programs[1].startTime);
+      expect(programs[1].startTime).toBeGreaterThan(programs[2].startTime);
+      // Checks that the next program is last item on the array
+      expect(programs[3].startTime).toBeGreaterThan(programs[0].startTime);
+    });
+
+    it('should return a program that has the right fields', () => {
+      programs.forEach(program => {
+        expect(program).toHaveProperty('id');
+        expect(program).toHaveProperty('state');
+        expect(program).toHaveProperty('stateLabel');
+        expect(program).toHaveProperty('startTime');
+        expect(program).toHaveProperty('link');
+        expect(program).toHaveProperty('brandTitle');
+        expect(program).toHaveProperty('episodeTitle');
+        expect(program).toHaveProperty('summary');
+        expect(program).toHaveProperty('duration');
+        expect(program).toHaveProperty('durationLabel');
+      });
+    });
+  });
+
+  describe('Incomplete schedule data', () => {
+    // Reduce the number of schedules to less than 4
+    beforeEach(() => {
+      persianRadioScheduleData.schedules.splice(
+        3,
+        persianRadioScheduleData.schedules.length - 1,
+      );
+      programs = processRadioSchedule(
+        persianRadioScheduleData,
+        service,
+        Date.now(),
+      );
+    });
+
+    it('should return undefined when schedule data is incomplete', () => {
+      expect(programs).toBeUndefined();
+    });
+  });
 });
