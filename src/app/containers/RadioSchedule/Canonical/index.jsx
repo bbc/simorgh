5f44697fba522a2059fecb8ef18b8d564078d615
Tom Williams <tom.williams07@bbc.co.uk>
2020-03-27 09:14:17 +0000

    Remove before pseudoelement from radio schedules link

--- src/app/containers/RadioSchedule/Canonical/index.jsx
+++ src/app/containers/RadioSchedule/Canonical/index.jsx
@@ -22,8 +22,7 @@ import { getLongPrimer } from '@bbc/gel-foundations/typography';
 import { getSansRegular } from '@bbc/psammead-styles/font-styles';
 import RadioSchedule from '@bbc/psammead-radio-schedule';
 import SectionLabel from '@bbc/psammead-section-label';
-import { Link } from '@bbc/psammead-story-promo';
-import { C_LUNAR } from '@bbc/psammead-styles/colours';
+import { C_LUNAR, C_EBON, C_METAL } from '@bbc/psammead-styles/colours';
 import { ServiceContext } from '#contexts/ServiceContext';
 import { RequestContext } from '#contexts/RequestContext';
 import processRadioSchedule from '../utilities/processRadioSchedule';
@@ -76,9 +75,20 @@ const RadioScheduleSectionLabel = styled(SectionLabel)`
   }
 `;
 
-const RadioFrequencyLink = styled(Link)`
+const RadioFrequencyLink = styled.a`
   ${({ script }) => script && getLongPrimer(script)};
   ${({ service }) => service && getSansRegular(service)};
+  color: ${C_EBON};
+  text-decoration: none;
+
+  &:hover,
+  &:focus {
+    text-decoration: underline;
+  }
+
+  &:visited {
+    color: ${C_METAL};
+  }
 `;
 
 const CanonicalRadioSchedule = ({ endpoint }) => {
