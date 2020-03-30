275e0a43556509078fff62f33074911e4e61bfb2
Liz Cameron <52247014+lizcameron@users.noreply.github.com>
2020-03-30 17:21:08 +0100

    Marathi translations

--- src/app/lib/config/services/marathi.js
+++ src/app/lib/config/services/marathi.js
@@ -205,6 +205,22 @@ export const service = {
       ],
       copyrightText:
         'बीबीसी बाह्य इंटरनेट साइट्सच्या सामग्रीसाठी बीबीसी जबाबदार नाही.',
+      socialEmbed: {
+        caption: {
+          textPrefixVisuallyHidden: 'Video caption,',
+          text: 'Warning: Third party content may contain adverts',
+        },
+        fallback: {
+          text: "Content is not available",
+          linkText: 'View content on %provider_name%',
+          linkTextSuffixVisuallyHidden: ', external',
+          warningText: "बीबीसी बाह्य इंटरनेट साइट्सच्या सामग्रीसाठी बीबीसी जबाबदार नाही. बाह्य लिंक्सबद्दल आम्हाल काय वाटतं? इथे वाचा.",
+        },
+        skipLink: {
+          text: 'Skip %provider_name% post',
+          endTextVisuallyHidden: 'End of %provider_name% post',
+        },
+      },
     },
     fonts: [],
     timezone: 'Asia/Kolkata',
