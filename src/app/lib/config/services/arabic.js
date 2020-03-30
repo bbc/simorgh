85f6744583ec00b5180758a60690bd873ba638b8
Liz Cameron <52247014+lizcameron@users.noreply.github.com>
2020-03-30 15:05:57 +0100

    Arabic translations

--- src/app/lib/config/services/arabic.js
+++ src/app/lib/config/services/arabic.js
@@ -219,6 +219,22 @@ export const service = {
       ],
       copyrightText:
         'بي بي سي. بي بي سي ليست مسؤولة عن محتوى المواقع الخارجية.',
+      socialEmbed: {
+        caption: {
+          textPrefixVisuallyHidden: 'التعليق على الفيديو،',
+          text: 'تحذير: المحتوى من طرف ثالث قد يتضمن إعلانات',
+        },
+        fallback: {
+          text: "المحتوى غير متاح",
+          linkText: '%provider_name% اطلع على المزيد في',
+          linkTextSuffixVisuallyHidden: '، خارجي',
+          warningText: "بي بي سي ليست مسؤولة عن محتوى المواقع الخارجية.",
+        },
+        skipLink: {
+          text: 'أهمل %provider_name% مشاركة',
+          endTextVisuallyHidden: 'نهاية %provider_name% مشاركة',
+        },
+      },
     },
     fonts: [F_NASSIM_ARABIC_REGULAR, F_NASSIM_ARABIC_BOLD],
     timezone: 'GMT',
