dfe74b4e279d6c409471618444c42dced5584ad3
James Donohue <james.donohue@bbc.co.uk>
2020-03-30 14:47:32 +0100

    Clarify description in comment

--- src/app/lib/utilities/serialiseForScript/index.js
+++ src/app/lib/utilities/serialiseForScript/index.js
@@ -1,8 +1,11 @@
 // Serialises a JavaScript object to a JSON string in a way that is safe for
-// output in an HTML `script` tag, suggested by
+// output in a `script` tag without confusing the HTML parser
+//
+// The approach used is simply to replace literal less-than characters ('<') with
+// an equivalent Unicode escape sequence. For more detail see this blog post:
 // https://uploadcare.com/blog/vulnerability-in-html-design/
 //
 // See also:
 // https://html.spec.whatwg.org/multipage/scripting.html#restrictions-for-contents-of-script-elements
 
-export default obj => JSON.stringify(obj).replace(/</g, '\\u003c');
+export default obj => obj && JSON.stringify(obj).replace(/</g, '\\u003c');
