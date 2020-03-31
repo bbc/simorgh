41a8df6392e3fd1b118fb8b541b7d36ccd6a0b71
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-31 18:58:05 +0100

    Ensure AMP tests run on correct paths

--- cypress/integration/specialFeatures/scriptSwitching/testsForAMPOnly.js
+++ cypress/integration/specialFeatures/scriptSwitching/testsForAMPOnly.js
@@ -29,49 +29,51 @@ Object.keys(config)
       )
       .forEach(pageType => {
         const paths = getPaths(service, pageType);
-        paths.forEach(path => {
-          const { variant } = config[service];
-          const product = config[service].name;
-          const otherVariant = appConfig[product][variant].scriptLink.variant;
+        paths
+          .map(path => `${path}.amp`)
+          .forEach(path => {
+            const { variant } = config[service];
+            const product = config[service].name;
+            const otherVariant = appConfig[product][variant].scriptLink.variant;
 
-          describe(`Script Switching - ${service} - ${pageType} - ${path}`, () => {
-            beforeEach(() => {
-              cy.clearCookies();
-              visitPage(path, pageType);
-            });
+            describe(`Script Switching - ${service} - ${pageType} - ${path}`, () => {
+              beforeEach(() => {
+                cy.clearCookies();
+                visitPage(path, pageType);
+              });
 
-            it(`should change to the correct script when switching script between ${variant} and ${otherVariant}`, () => {
-              // Accept privacy banner
-              getPrivacyBannerAccept(service, variant).click();
+              it(`should change to the correct script when switching script between ${variant} and ${otherVariant}`, () => {
+                // Accept privacy banner
+                getPrivacyBannerAccept(service, variant).click();
 
-              // Accept cookie banner
-              getCookieBannerAccept(service, variant).click();
+                // Accept cookie banner
+                getCookieBannerAccept(service, variant).click();
 
-              // Checks URL is in current variant
-              assertURLContains(product, variant);
+                // Checks URL is in current variant
+                assertURLContains(product, variant);
 
-              // Clicks script switcher
-              clickScriptSwitcher(otherVariant);
+                // Clicks script switcher
+                clickScriptSwitcher(otherVariant);
 
-              // Checks URL is in other variant
-              assertURLContains(product, otherVariant);
+                // Checks URL is in other variant
+                assertURLContains(product, otherVariant);
 
-              // Navigate to home page
-              visitPage(`${product}/${otherVariant}`, pageType);
+                // Navigate to home page
+                visitPage(`${product}/${otherVariant}.amp`, pageType);
 
-              // Navigates back to the original page, but for other variant
-              visitPage(
-                path.replace(`/${variant}`, `/${otherVariant}`),
-                pageType,
-              );
+                // Navigates back to the original page, but for other variant
+                visitPage(
+                  path.replace(`/${variant}`, `/${otherVariant}`),
+                  pageType,
+                );
 
-              // Clicks script switcher to original variant
-              clickScriptSwitcher(variant);
+                // Clicks script switcher to original variant
+                clickScriptSwitcher(variant);
 
-              // Checks URL is correct variant
-              assertURLContains(product, variant);
+                // Checks URL is correct variant
+                assertURLContains(product, variant);
+              });
             });
           });
-        });
       });
   });
