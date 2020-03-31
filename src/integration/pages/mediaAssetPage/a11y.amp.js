af14e427bf5d63ee1e0cd000a787ff9795e13bba
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-31 13:08:14 +0100

    Rename the a11y test name to reflect behaviour of screenreader

--- src/integration/pages/mediaAssetPage/a11y.amp.js
+++ src/integration/pages/mediaAssetPage/a11y.amp.js
@@ -1,6 +1,6 @@
 export default ({ mediaPlayerTitle }) => {
   describe(`And using ${amp.platform}`, () => {
-    it('I can see the media player title', () => {
+    it('It can read the media player title', () => {
       const mediaPlayer = amp.document.querySelector(
         `amp-iframe[title="${mediaPlayerTitle}"]`,
       );
--- src/integration/pages/mediaAssetPage/a11y.canonical.js
+++ src/integration/pages/mediaAssetPage/a11y.canonical.js
@@ -1,6 +1,6 @@
 export default ({ mediaPlayerTitle }) => {
   describe(`And using ${canonical.platform}`, () => {
-    it('I can see the media player title', () => {
+    it('It can read the media player title', () => {
       const mediaPlayer = canonical.document.querySelector(
         `iframe[title="${mediaPlayerTitle}"]`,
       );
