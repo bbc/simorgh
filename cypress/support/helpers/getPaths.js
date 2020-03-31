69cea31af89e2a0c600295329fb01ecf6ccdb16f
Karina Thomas <karina.thomas@bbc.co.uk>
2020-03-31 21:58:53 +0100

    Use smoke variable to determine which paths to run tests on

--- cypress/support/helpers/getPaths.js
+++ cypress/support/helpers/getPaths.js
@@ -5,7 +5,9 @@ export default (service, pageType) => {
   const { environments = {}, smoke } = config[service].pageTypes[pageType];
   const environment = environments[getAppEnv()];
 
-  return environment && environment.enabled && smoke === Cypress.env('SMOKE')
+  return environment &&
+    environment.enabled &&
+    smoke === Boolean(Cypress.env('SMOKE'))
     ? environment.paths
     : [];
 };
