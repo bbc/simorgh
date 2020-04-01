9e2fbbe54c3ff73481722c5a3de7d579aa84df13
Sareh Heidari <sareh@users.noreply.github.com>
2020-04-01 13:47:29 +0100

    Remove localInjectHostCspHeader & ensure injectCspHeader is checking correct origin values

--- src/server/utilities/cspHeader/middleware.js
+++ src/server/utilities/cspHeader/middleware.js
@@ -4,16 +4,13 @@ import routes from '#app/routes';
 import getOriginContext from '#contexts/RequestContext/getOriginContext';
 import { constructCspHeader } from './index';
 
-export const localInjectHostCspHeader = (_req, _res, next) => {
-  next();
-};
-
 const injectCspHeader = (req, res, next) => {
   const { isAmp } = getRouteProps(routes, req.url);
   const originHeader = req.headers['bbc-origin'];
   const { origin, isUK } = getOriginContext(originHeader);
 
-  const isLive = origin === 'https://bbc.co.uk' || origin === 'https://bbc.com';
+  const isLive =
+    origin === 'https://www.bbc.co.uk' || origin === 'https://www.bbc.com';
 
   const middleware = csp(constructCspHeader({ isAmp, isLive, isUK }));
   middleware(req, res, next);
