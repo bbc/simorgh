e684afe3562824d3adb03ea8e0d348ff694e06f4
Karina Thomas <karina.thomas@bbc.co.uk>
2020-04-02 11:49:02 +0100

    Reorder alphabetically

--- src/app/lib/logger.const.js
+++ src/app/lib/logger.const.js
@@ -1,16 +1,16 @@
 const logCodes = {
-  NO_TRANSLATION_FOUND: 'no_translation_found',
-  NO_MEDIA_BLOCK: 'no_media_block',
+  DATA_FETCH_ERROR: 'data_fetch_error',
   DATA_NOT_FOUND: 'data_response_404',
-  SERVER_LISTEN_ERROR: 'server_listen_error',
-  SERVICE_WORKER_SENDFILE_ERROR: 'server_sendfile_error_sw',
+  DATA_REQUEST_RECEIVED: 'data_request_received',
+  LOCAL_SENDFILE_ERROR: 'local_sendfile_error',
   MANIFEST_SENDFILE_ERROR: 'server_sendfile_error_manifest',
+  NO_MEDIA_BLOCK: 'no_media_block',
+  NO_TRANSLATION_FOUND: 'no_translation_found',
+  ROUTING_INFORMATION: 'routing_info',
+  SERVER_LISTEN_ERROR: 'server_listen_error',
   SERVER_SIDE_RENDER_REQUEST_RECEIVED: 'ssr_request_received',
   SERVER_SIDE_REQUEST_FAILED: 'ssr_request_failed',
-  LOCAL_SENDFILE_ERROR: 'local_sendfile_error',
-  DATA_REQUEST_RECEIVED: 'data_request_received',
-  DATA_FETCH_ERROR: 'data_fetch_error',
-  ROUTING_INFORMATION: 'routing_info',
+  SERVICE_WORKER_SENDFILE_ERROR: 'server_sendfile_error_sw',
 };
 
 module.exports = logCodes;
