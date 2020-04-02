0ae1b25ae41a2d953a4df4287c3538bac2b47f89
thekp <khoa.phan@bbc.co.uk>
2020-04-02 16:15:18 +0100

    add styledAd, add amp access to conditionally load ads client side

--- src/app/containers/Ad/Amp/index.jsx
+++ src/app/containers/Ad/Amp/index.jsx
@@ -1,4 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Helmet } from 'react-helmet';
+import { AMP_ACCESS_JS } from '@bbc/psammead-assets/amp-boilerplate';
+import {
+  GEL_GROUP_3_SCREEN_WIDTH_MIN,
+  GEL_GROUP_4_SCREEN_WIDTH_MIN,
+} from '@bbc/gel-foundations/breakpoints';
+import {
+  GEL_SPACING_TRPL,
+  GEL_SPACING_QUAD,
+} from '@bbc/gel-foundations/spacings';
+
+const StyledAd = styled.div`
+  /* To centre page layout for Group 4+ */
+  margin: 0 auto;
+  width: 100%; /* Needed for IE11 */
+  text-align: center;
+
+  @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
+    margin-top: ${GEL_SPACING_TRPL};
+  }
+
+  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
+    margin-top: ${GEL_SPACING_QUAD};
+    max-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN};
+  }
+`;
 
 const constructAdJsonData = ({ service }) => {
   const data = {
@@ -36,29 +63,54 @@ const ampAdPropsDesktop = ({ service }) => ({
   json: JSON.stringify(constructAdJsonData({ service })),
 });
 
+const AMP_ACCESS_DATA = (endpoint) => ({
+  authorization: endpoint,
+  noPingback: true,
+});
+
+const AMP_ACCESS_FETCH = (service) => {
+  const togglesEndpoint = `https://toggles.test.api.bbci.co.uk/toggles?application=simorgh&service=${service}&geoiplookup=true`;
+
+  return (
+    <script id="amp-access" type="application/json">
+      {JSON.stringify(AMP_ACCESS_DATA(togglesEndpoint))}
+    </script>
+  );
+};
+
 // eslint-disable-next-line react/prop-types
 const AmpAd = ({ service }) => {
   return (
     <>
-      <amp-ad {...ampAdPropsMobile({ service })}>
-        <amp-img
-          placeholder
-          width="320"
-          height="50"
-          src="https://via.placeholder.com/320x50"
-          layout="responsive"
-        />
-      </amp-ad>
-
-      <amp-ad {...ampAdPropsDesktop({ service })}>
-        <amp-img
-          placeholder
-          width="970"
-          height="250"
-          src="https://via.placeholder.com/970x250"
-          layout="responsive"
-        />
-      </amp-ad>
+      <Helmet>
+        {AMP_ACCESS_JS}
+        {AMP_ACCESS_FETCH(service)}
+      </Helmet>
+      <div
+        amp-access="toggles.ads.enabled  AND geoIp.advertiseCombined"
+        amp-access-hide
+      >
+        <StyledAd>
+          <amp-ad {...ampAdPropsMobile({ service })}>
+            <amp-img
+              placeholder
+              width="320"
+              height="50"
+              src="https://via.placeholder.com/320x50"
+              layout="responsive"
+            />
+          </amp-ad>
+          <amp-ad {...ampAdPropsDesktop({ service })}>
+            <amp-img
+              placeholder
+              width="970"
+              height="250"
+              src="https://via.placeholder.com/970x250"
+              layout="responsive"
+            />
+          </amp-ad>
+        </StyledAd>
+      </div>
     </>
   );
 };
