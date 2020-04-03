962bf256cbb0d294f64c9e921db5fee8a881f6d5
Simon Sinclair <simon@simonsinclair.com>
2020-04-03 15:42:19 +0100

    Add convert social_media to Optimo block tests

---
+++ src/app/routes/cpsAsset/getInitialData/convertToOptimoBlocks/blocks/socialEmbed/fixtures.js
@@ -0,0 +1,35 @@
+export const twitterNoEmbed = {
+  href: 'https://twitter.com/MileyCyrus/status/1237210910835392512',
+  source: 'twitter',
+  type: 'social_embed',
+  id: '1237210910835392512',
+};
+
+export const twitter = {
+  href: 'https://twitter.com/MileyCyrus/status/1237210910835392512',
+  source: 'twitter',
+  type: 'social_embed',
+  id: '1237210910835392512',
+  embed: {
+    oembed: {
+      url: 'https://twitter.com/MileyCyrus/status/1237210910835392512',
+      author_name: 'Miley Ray Cyrus',
+      author_url: 'https://twitter.com/MileyCyrus',
+      html:
+        '<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Australia: Due to the recommendations of local, state, federal and international government authorities, including the Center for Disease Control, to reduce potential health risks in response to the current global health crisis, we are no longer traveling to Aus for the show.</p>&mdash; Miley Ray Cyrus (@MileyCyrus) <a href="https://twitter.com/MileyCyrus/status/1237210910835392512?ref_src=twsrc%5Etfw">March 10, 2020</a></blockquote>',
+      width: 550,
+      height: null,
+      type: 'rich',
+      cache_age: '3153600000',
+      provider_name: 'Twitter',
+      provider_url: 'https://twitter.com',
+      version: '1.0',
+    },
+    fallback_image: {
+      fallback_image_width: 465,
+      fallback_image_height: 279,
+      alt_text:
+        'Twitter post by Miley Ray Cyrus: Australia: Due to the recommendations of local, state, federal and international government authorities, including the Center for Disease Control, to reduce potential health risks in response to the current global health crisis, we are no longer traveling to Aus for the show.',
+    },
+  },
+};
