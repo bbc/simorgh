e8a3379c1020cda6e109498d006855ace3af4a62
Simon Sinclair <simon.sinclair@bbc.co.uk>
2020-04-06 12:19:19 +0100

    Update min/max bundle size values

--- scripts/bundleSizeConfig.js
+++ scripts/bundleSizeConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Size limit for all bundles used by each service (K)
   // Keep these +/- 5K and update frequently!
-  MIN_SIZE: 762,
-  MAX_SIZE: 806,
+  MIN_SIZE: 769,
+  MAX_SIZE: 813,
 };
