12e491f7781b8b8bbde8f22249edef5b28267381
Ikechi Michael <michael.ikechi.ext@bbc.co.uk>
2020-04-06 15:57:39 +0100

    fix: inject scripts only when the previous is loaded

--- src/app/containers/Include/index.jsx
+++ src/app/containers/Include/index.jsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 import { string } from 'prop-types';
 import { GridItemConstrainedMedium } from '#lib/styledGrid';
 import useToggle from '#hooks/useToggle';
-import { create } from '@storybook/theming';
 
 /* The Include html which we are getting would be encoded
    so that html characters are escaped when serializing the page data.
@@ -36,14 +35,23 @@ const IncludeContainer = ({ html, type, href }) => {
   };
 
   const createAppendScriptTag = (code, src) => {
-    const script = document.createElement('script');
-    if (src) {
-      script.src = src;
-    } else if (code) {
-      script.appendChild(document.createTextNode(code));
-    }
-    document.body.append(script);
-    // console.log('script: ', script)
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      if (src) {
+        script.src = src;
+      } else if (code) {
+        script.appendChild(document.createTextNode(code));
+      }
+      // eslint-disable-next-line func-names
+      script.onload = function () {
+        resolve();
+      };
+      // eslint-disable-next-line func-names
+      script.onerror = function () {
+        reject();
+      };
+      document.body.append(script);
+    });
   };
 
   useEffect(() => {
@@ -56,36 +64,34 @@ const IncludeContainer = ({ html, type, href }) => {
   }, [href]);
 
   useEffect(() => {
-
     const scriptTagRegExp = new RegExp(/<script\b[^>]*>([\s\S]*?)<\/script>/gm);
     const scriptTagMatches = includeHtml.matchAll(scriptTagRegExp);
 
     setScriptsTags(Array.from(scriptTagMatches));
     setIncludeHtml(includeHtml.replace(scriptTagRegExp, ''));
-
   }, [includeHtml]);
 
   // Keep the DOM up to date with our script tags.
   useEffect(() => {
-    scriptTags.forEach((scriptTag) => {
-      const [textContent, contents] = scriptTag;
-      console.log('scriptTag: ', scriptTag)
-      const srcRegex = new RegExp(/src="(.*?)"/gm);
-      // console.log(Array.from(textContent.matchAll(srcRegex)));
-      const [srcContent] = Array.from(textContent.matchAll(srcRegex))
-      if (srcContent) {
-        console.log('srcContent ', srcContent)
-        const [srcContent2] = srcContent;
-        console.log('srcContent2', srcContent2)
+    async function placeScriptsOneAfterTheOther() {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const scriptTag of scriptTags) {
+        const [textContent, contents] = scriptTag;
+        const srcRegex = new RegExp(/src="(.*?)"/gm);
+        // console.log(Array.from(textContent.matchAll(srcRegex)));
+        const [srcContent] = Array.from(textContent.matchAll(srcRegex));
+        if (srcContent) {
+          const [src] = srcContent.slice(-1);
 
-        createAppendScriptTag(
-          '',
-          srcContent2.replace('src="', '').replace('"', ''),
-        );
-      } else {
-        createAppendScriptTag(contents);
+          // eslint-disable-next-line no-await-in-loop
+          await createAppendScriptTag('', src);
+        } else {
+          // eslint-disable-next-line no-await-in-loop
+          await createAppendScriptTag(contents);
+        }
       }
-    });
+    }
+    placeScriptsOneAfterTheOther();
   }, [scriptTags]);
 
   const shouldNotRenderInclude = !enabled || !html || !supportedTypes[type];
