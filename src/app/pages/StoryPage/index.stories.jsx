363e9dd14f109adbe0ca8acc991408d0f0bbbab1
Sareh Heidari <sareh@users.noreply.github.com>
2020-04-06 18:12:18 +0100

    Update stories to show a RTL service example

--- src/app/pages/StoryPage/index.stories.jsx
+++ src/app/pages/StoryPage/index.stories.jsx
@@ -4,7 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import WithTimeMachine from '#testHelpers/withTimeMachine';
 
 import { StoryPage } from '..';
-import pageData from './fixtureData';
+import mundoPageData from './fixtureData/mundo';
+import persianPageData from './fixtureData/persian';
 
 const styStories = storiesOf('Pages|Story Page', module);
 
@@ -12,17 +13,28 @@ styStories.addDecorator((story) => (
   <WithTimeMachine>{story()}</WithTimeMachine>
 ));
 
-styStories.add('Mundo', () => {
-  return (
-    <BrowserRouter>
-      <StoryPage
-        pageType="STY"
-        isAmp={false}
-        pathname="/path"
-        status={200}
-        pageData={pageData}
-        service="mundo"
-      />
-    </BrowserRouter>
-  );
+[
+  {
+    service: 'mundo',
+    pageData: mundoPageData,
+  },
+  {
+    service: 'persian',
+    pageData: persianPageData,
+  },
+].forEach(({ service, pageData }) => {
+  styStories.add(service, () => {
+    return (
+      <BrowserRouter>
+        <StoryPage
+          pageType="STY"
+          isAmp={false}
+          pathname="/path"
+          status={200}
+          pageData={pageData}
+          service={service}
+        />
+      </BrowserRouter>
+    );
+  });
 });
