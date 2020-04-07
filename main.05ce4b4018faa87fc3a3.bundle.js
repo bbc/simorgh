8235e0972b7e4b343ab0376856d1e1b5c92a429d
BBC News CI <foo@bar.com>
2020-04-07 11:24:37 +0000

    Updates

---
+++ main.05ce4b4018faa87fc3a3.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1247:function(n,o,c){"use strict";c.r(o);c(1248)},454:function(n,o,c){c(455),c(598),n.exports=c(1247)},517:function(n,o){}},[[454,1,2]]]);
--- main.c973c735dd4d9bbb3f29.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1481:function(n,o,c){"use strict";c.r(o);c(1482)},508:function(n,o,c){c(509),c(827),n.exports=c(1481)},591:function(n,o){}},[[508,1,2]]]);
