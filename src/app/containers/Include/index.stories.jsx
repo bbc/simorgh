681bbbc79b867a1b1bfd6b128c31f77203297919
Fahad Khan <fahad.khan01@bbc.co.uk>
2020-04-08 15:41:57 +0100

    test: use storybook to check if same include can render
          We want to see if the same include can render multiple times on the same page.

--- src/app/containers/Include/index.stories.jsx
+++ src/app/containers/Include/index.stories.jsx
@@ -21,6 +21,14 @@ const defaultValue =
   'https://simorgh-include-test.s3-eu-west-1.amazonaws.com/idt2.html';
 const groupId = 'GROUP-ID1';
 
+const multipleOptions = {
+  vjOneTest:
+    'https://www.test.bbc.com/ws/includes/include/newsspec/22766-democrats/mundo/app?responsive=true&newsapps=true&app-image=https://news.files.bbci.co.uk/vj/live/idt-images/image-slider-test/app_launcher2_640-mundo_aldmj.png&app-clickable=true&amp-clickable=true&amp-image-height=360&amp-image-width=640&amp-image=https://news.files.bbci.co.uk/vj/live/idt-images/image-slider-test/app_launcher2_640-mundo_aldmj.png',
+};
+
+const defaultValue1 =
+  'https://www.test.bbc.com/ws/includes/include/vjeastasia/185-timeline-generator/nepali/china_4th_june_nepali?responsive=true&newsapps=true&app-image=https://news.files.bbci.co.uk/include/vjeastasia/185-timeline-generator/assets/china_4th_june_burmese-project-assets/img/100.jpg&app-clickable=true&amp-clickable=true&amp-image-height=689&amp-image-width=1024&amp-image=https://news.files.bbci.co.uk/include/vjeastasia/185-timeline-generator/assets/china_4th_june_burmese-project-assets/img/100.jpg';
+
 storiesOf('Containers|Include', module)
   .addParameters({ chromatic: { disable: true } })
   .addDecorator(withKnobs)
@@ -43,6 +51,30 @@ storiesOf('Containers|Include', module)
         service="mundo"
         origin="https://www.test.bbc.com"
       >
+        <Include html={html} type="idt2" />
+      </ToggleContextProvider>
+    );
+  })
+  .add('multiple includes', () => {
+    const href = select(label, multipleOptions, defaultValue1, groupId);
+
+    const [html, setHtml] = useState('');
+
+    useEffect(() => {
+      const fetchInclude = async () => {
+        const response = await fetch(href);
+        setHtml(await response.text());
+      };
+      fetchInclude();
+    }, [href]);
+
+    return (
+      <ToggleContextProvider
+        value={{ toggleState: defaultToggles }}
+        service="mundo"
+        origin="https://www.test.bbc.com"
+      >
+        <Include html={html} type="idt2" />
         <Include html={html} type="idt2" />
       </ToggleContextProvider>
     );
