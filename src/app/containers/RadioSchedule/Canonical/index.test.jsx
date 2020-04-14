0153ef2f233743fe3779622f0620032c03a27368
Ben Fellows <benjamin.fellows@bbc.co.uk>
2020-04-14 15:16:40 +0100

    Canonical RadioSchedule added tests with and without initial data

--- src/app/containers/RadioSchedule/Canonical/index.test.jsx
+++ src/app/containers/RadioSchedule/Canonical/index.test.jsx
@@ -11,62 +11,127 @@ describe('Canonical RadioSchedule', () => {
     fetch.resetMocks();
   });
 
-  it('renders correctly for a service with a radio schedule and page frequency URL', async () => {
-    fetch.mockResponse(JSON.stringify(arabicRadioScheduleData));
+  describe('With initial data', () => {
+    it('renders correctly for a service with initial data', async () => {
+      fetch.mockResponse(JSON.stringify(arabicRadioScheduleData));
+      const initialData = processRadioSchedule(
+        arabicRadioScheduleData,
+        'arabic',
+        Date.now(),
+      );
+      await matchSnapshotAsync(
+        <RadioSchedulesWithContext
+          service="arabic"
+          initialData={initialData}
+          radioScheduleToggle
+        />,
+      );
+      expect(fetch).not.toBeCalled();
+    });
 
-    await matchSnapshotAsync(
-      <RadioSchedulesWithContext service="arabic" radioScheduleToggle />,
-    );
-  });
+    it('contains four programs for a service with a radio schedule', async () => {
+      const initialData = processRadioSchedule(
+        arabicRadioScheduleData,
+        'arabic',
+        Date.now(),
+      );
+      const { container } = render(
+        <RadioSchedulesWithContext
+          service="arabic"
+          initialData={initialData}
+          radioScheduleToggle
+        />,
+      );
 
-  it('renders correctly for a service with initial data', async () => {
-    fetch.mockResponse(JSON.stringify(arabicRadioScheduleData));
-    const initialData = processRadioSchedule(
-      arabicRadioScheduleData,
-      'arabic',
-      Date.now(),
-    );
-    await matchSnapshotAsync(
-      <RadioSchedulesWithContext
-        service="arabic"
-        initialData={initialData}
-        radioScheduleToggle
-      />,
-    );
-  });
+      await wait(() => {
+        expect(container.querySelectorAll('li').length).toEqual(4);
+      });
+    });
 
-  it('contains four programs for a service with a radio schedule', async () => {
-    fetch.mockResponse(JSON.stringify(arabicRadioScheduleData));
-    const { container } = render(
-      <RadioSchedulesWithContext service="arabic" radioScheduleToggle />,
-    );
+    it('does not render when data contains less than 4 programs', async () => {
+      const initialData = processRadioSchedule(
+        { schedules: arabicRadioScheduleData.schedules.slice(0, 2) },
+        'arabic',
+        Date.now(),
+      );
 
-    await wait(() => {
-      expect(container.querySelectorAll('li').length).toEqual(4);
+      const { container } = render(
+        <RadioSchedulesWithContext
+          service="arabic"
+          initialData={initialData}
+        />,
+      );
+      await wait(() => {
+        expect(container).toBeEmpty();
+      });
+    });
+
+    it('does not render when data contains no programs', async () => {
+      const initialData = processRadioSchedule(
+        { schedules: [] },
+        'arabic',
+        Date.now(),
+      );
+
+      const { container } = render(
+        <RadioSchedulesWithContext
+          service="arabic"
+          initialData={initialData}
+        />,
+      );
+      await wait(() => {
+        expect(container).toBeEmpty();
+      });
     });
   });
 
-  it('does not render when data contains less than 4 programs', async () => {
-    fetch.mockResponse(
-      JSON.stringify(arabicRadioScheduleData.schedules.slice(0, 2)),
-    );
-    const { container } = render(
-      <RadioSchedulesWithContext service="arabic" />,
-    );
-    await wait(() => {
-      expect(container).toBeEmpty();
-    });
-  });
+  describe('Without initial data', () => {
+    it('renders correctly for a service with a radio schedule and page frequency URL', async () => {
+      fetch.mockResponse(JSON.stringify(arabicRadioScheduleData));
 
-  it('does not render when data contains no programs', async () => {
-    fetch.mockResponse(
-      JSON.stringify(arabicRadioScheduleData.schedules.slice(0, 0)),
-    );
-    const { container } = render(
-      <RadioSchedulesWithContext service="arabic" />,
-    );
-    await wait(() => {
-      expect(container).toBeEmpty();
+      await matchSnapshotAsync(
+        <RadioSchedulesWithContext service="arabic" radioScheduleToggle />,
+      );
+      expect(fetch).toBeCalled();
+    });
+
+    it('contains four programs for a service with a radio schedule', async () => {
+      fetch.mockResponse(JSON.stringify(arabicRadioScheduleData));
+      const { container } = render(
+        <RadioSchedulesWithContext service="arabic" radioScheduleToggle />,
+      );
+
+      await wait(() => {
+        expect(container.querySelectorAll('li').length).toEqual(4);
+      });
+    });
+
+    it('does not render when data contains less than 4 programs', async () => {
+      fetch.mockResponse(
+        JSON.stringify({
+          schedules: arabicRadioScheduleData.schedules.slice(0, 2),
+        }),
+      );
+      const { container } = render(
+        <RadioSchedulesWithContext service="arabic" />,
+      );
+      await wait(() => {
+        expect(container).toBeEmpty();
+      });
+    });
+
+    it('does not render when data contains no programs', async () => {
+      fetch.mockResponse(
+        JSON.stringify({
+          schedules: [],
+        }),
+      );
+      const { container } = render(
+        <RadioSchedulesWithContext service="arabic" />,
+      );
+      await wait(() => {
+        expect(container).toBeEmpty();
+      });
     });
   });
 });
