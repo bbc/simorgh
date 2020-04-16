7ce12c892b925380670d25b145ae8fdeb4ff7f01
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-04-16 08:52:51 +0100

    retry strategy for dom fetch in integration tests

--- src/integration/render.js
+++ src/integration/render.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-console */
+
 const { JSDOM } = require('jsdom');
 const { within } = require('@testing-library/dom');
+const retry = require('retry');
 
 const enhanceGetByText = (getByText) => (text) =>
   getByText((content, node) => {
@@ -12,15 +15,50 @@ const enhanceGetByText = (getByText) => (text) =>
     return nodeHasText && childrenDontHaveText;
   });
 
+const faultTolerantDomFetch = (url) =>
+  new Promise((resolve, reject) => {
+    const oneSecond = 1000;
+    const operation = retry.operation({
+      retries: 10,
+      factor: 1.5,
+      minTimeout: oneSecond,
+      maxTimeout: 10 * oneSecond,
+    });
+
+    operation.attempt(async (currentAttempt) => {
+      if (currentAttempt > 1) {
+        console.warn(
+          `Error getting DOM from ${url}`,
+          `Retry attempts: ${currentAttempt - 1}`,
+        );
+      }
+
+      try {
+        const dom = await JSDOM.fromURL(url);
+
+        resolve(dom);
+      } catch (error) {
+        if (operation.retry(error)) {
+          return;
+        }
+
+        reject(operation.mainError());
+      }
+    });
+  });
+
 module.exports = async (path) => {
-  const dom = await JSDOM.fromURL(`http://localhost:7080${path}`);
+  try {
+    const dom = await faultTolerantDomFetch(`http://localhost:7080${path}`);
+    const queries = within(dom.window.document.body);
 
-  const queries = within(dom.window.document.body);
-
-  return {
-    window: dom.window,
-    document: dom.window.document,
-    getByTextMultiElement: enhanceGetByText(queries.getByText),
-    ...queries,
-  };
+    return {
+      window: dom.window,
+      document: dom.window.document,
+      getByTextMultiElement: enhanceGetByText(queries.getByText),
+      ...queries,
+    };
+  } catch (error) {
+    throw new Error(error);
+  }
 };
