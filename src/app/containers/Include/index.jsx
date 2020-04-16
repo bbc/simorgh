3bcc088f944f5ca09904b01a92a21f3348a4e4ec
Ikechi Michael <michael.ikechi.ext@bbc.co.uk>
2020-04-16 11:56:45 +0100

    fix: prevent multiple re-render

--- src/app/containers/Include/index.jsx
+++ src/app/containers/Include/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-danger */
-import React, { useState, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { string } from 'prop-types';
 import { GridItemConstrainedMedium } from '#lib/styledGrid';
 import useToggle from '#hooks/useToggle';
@@ -7,9 +7,7 @@ import useToggle from '#hooks/useToggle';
 const IncludeContainer = ({ html = '', type }) => {
   const scriptTagRegExp = new RegExp(/<script\b[^>]*>([\s\S]*?)<\/script>/gm);
   const { enabled } = useToggle('include');
-  const [ssrHtml, setSsrHtml] = useState(
-    (html || '').replace(scriptTagRegExp, ''),
-  );
+  const isInitialMount = useRef(true);
 
   const supportedTypes = {
     idt2: 'idt2',
@@ -44,10 +42,10 @@ const IncludeContainer = ({ html = '', type }) => {
   // Keep the DOM up to date with our script tags.
   useEffect(() => {
     const originalHtml = html || '';
-    setSsrHtml(originalHtml.replace(scriptTagRegExp, ''));
     const scriptTagMatches = originalHtml.matchAll(scriptTagRegExp);
     const scriptTags = Array.from(scriptTagMatches);
     async function placeScriptsOneAfterTheOther() {
+      isInitialMount.current = false;
       // eslint-disable-next-line no-restricted-syntax
       for (const scriptTag of scriptTags) {
         const [textContent, contents] = scriptTag;
@@ -63,8 +61,10 @@ const IncludeContainer = ({ html = '', type }) => {
         }
       }
     }
-    placeScriptsOneAfterTheOther();
-  }, [html, ssrHtml, scriptTagRegExp]);
+    if (isInitialMount.current) {
+      placeScriptsOneAfterTheOther();
+    }
+  }, [html, type, scriptTagRegExp]);
 
   if (shouldNotRenderInclude) {
     return null;
@@ -74,7 +74,9 @@ const IncludeContainer = ({ html = '', type }) => {
     <GridItemConstrainedMedium>
       <div
         suppressHydrationWarning
-        dangerouslySetInnerHTML={{ __html: ssrHtml }}
+        dangerouslySetInnerHTML={{
+          __html: (html || '').replace(scriptTagRegExp, ''),
+        }}
       />
     </GridItemConstrainedMedium>
   );
