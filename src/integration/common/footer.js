46c76c2c1815bdb4e710e1c961951f0aa2e20214
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-04-17 15:01:00 +0100

    use correct footer copyright test title

--- src/integration/common/footer.js
+++ src/integration/common/footer.js
@@ -1,6 +1,6 @@
 export default () => {
   describe('Footer', () => {
-    it('I can see the copyright text', () => {
+    it('I can see the footer copyright and external linking text', () => {
       const footerCopyrightEl = document.querySelector('footer div p');
 
       expect(footerCopyrightEl).toBeInTheDocument();
