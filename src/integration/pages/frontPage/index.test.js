720995f1c49774372d4339b97f77aec2989701b1
Karina Thomas <karina.thomas@bbc.co.uk>
2020-04-17 23:34:20 +0100

    Update front page snapshots

---
+++ src/integration/pages/frontPage/index.test.js
@@ -0,0 +1,26 @@
+/**
+ * @service arabic
+ * @pathname /arabic
+ */
+
+import runCrossPlatformTests from './crossPlatformTests';
+import runAmpTests from './ampTests';
+import runCanonicalTests from './canonicalTests';
+
+describe('Front Page', () => {
+  describe(platform, () => {
+    runCrossPlatformTests();
+  });
+
+  if (platform === 'amp') {
+    describe(platform, () => {
+      runAmpTests();
+    });
+  }
+
+  if (platform === 'canonical') {
+    describe(platform, () => {
+      runCanonicalTests();
+    });
+  }
+});
