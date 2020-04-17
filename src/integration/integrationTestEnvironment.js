1fea0d98fb4a1c20631c15c1fc0983ee6a0934f8
Karina Thomas <karina.thomas@bbc.co.uk>
2020-04-17 21:58:09 +0100

    Remove test file since render.js has been deleted

--- src/integration/integrationTestEnvironment.js
+++ src/integration/integrationTestEnvironment.js
@@ -1,5 +1,44 @@
 const JsdomEnvironment = require('jest-environment-jsdom');
 const { JSDOM } = require('jsdom');
+const retry = require('retry');
+
+const faultTolerantDomFetch = (url) =>
+  new Promise((resolve, reject) => {
+    const oneSecond = 1000;
+    const operation = retry.operation({
+      retries: 5,
+      factor: 1,
+      minTimeout: oneSecond,
+      maxTimeout: oneSecond,
+    });
+
+    operation.attempt(async (currentAttempt) => {
+      if (currentAttempt > 1) {
+        console.warn(
+          `Error getting DOM from ${url}`,
+          `Retry attempts: ${currentAttempt - 1}`,
+        );
+      }
+
+      try {
+        const dom = await JSDOM.fromURL(url);
+
+        resolve(dom);
+      } catch (error) {
+        const isSocketHangUpError = error
+          .toString()
+          .includes('Error: socket hang up');
+
+        if (isSocketHangUpError) {
+          if (operation.retry(error)) {
+            return;
+          }
+        }
+
+        reject(error);
+      }
+    });
+  });
 
 class IntegrationTestEnvironment extends JsdomEnvironment {
   constructor(config, context) {
@@ -17,7 +56,7 @@ class IntegrationTestEnvironment extends JsdomEnvironment {
     await super.setup();
 
     try {
-      const dom = await JSDOM.fromURL(this.url);
+      const dom = await faultTolerantDomFetch(this.url);
 
       Object.defineProperties(this.global, {
         service: { value: this.service },
