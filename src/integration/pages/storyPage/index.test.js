819abc19e8dbad4094144aacd59d4a729e83affd
Karina Thomas <karina.thomas@bbc.co.uk>
2020-04-18 03:45:39 +0100

    Add line break

--- src/integration/pages/storyPage/index.test.js
+++ src/integration/pages/storyPage/index.test.js
@@ -2,6 +2,7 @@
  * @service mundo
  * @pathname /mundo/noticias-internacional-51266689
  */
+
 import runCrossPlatformTests from './crossPlatformTests';
 import runAmpTests from './ampTests';
 import runCanonicalTests from './canonicalTests';
