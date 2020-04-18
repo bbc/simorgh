67da5e164038ce5859778a50f1ff9adbe526f238
Karina Thomas <karina.thomas@bbc.co.uk>
2020-04-18 15:16:00 +0100

    Updating header tests

--- src/integration/common/header.js
+++ src/integration/common/header.js
@@ -11,8 +11,26 @@ export default () => {
     const navigationItemEls = document.querySelector('header nav ul > li > a');
 
     if (navigationItemEls) {
-      it('I can see the navigation', () => {
+      it('I can see the navigation bar', () => {
         expect(navigationItemEls).toBeInTheDocument();
+      });
+
+      const navigationLinks = document.querySelectorAll(
+        'header nav ul > li > a',
+      );
+      navigationLinks.forEach((navigationLink) => {
+        describe('I can see a navigation link', () => {
+          beforeEach(() => {
+            expect(navigationLink).toBeInTheDocument();
+          });
+          it('with title', () => {
+            expect(navigationLink.textContent).toBeTruthy();
+            expect(navigationLink.textContent).toMatchSnapshot();
+          });
+          it('with url', () => {
+            expect(navigationLink.getAttribute('href')).toMatchSnapshot();
+          });
+        });
       });
     }
 
