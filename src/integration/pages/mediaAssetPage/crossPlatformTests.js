33b3f399b947e8aea8be92be0ce3490fb48ad176
Karina Thomas <karina.thomas@bbc.co.uk>
2020-04-18 03:36:28 +0100

    Removing inline snapshots

--- src/integration/pages/mediaAssetPage/crossPlatformTests.js
+++ src/integration/pages/mediaAssetPage/crossPlatformTests.js
@@ -9,9 +9,7 @@ export default () => {
 
     expect(headlineEl).toBeInTheDocument();
     expect(headlineEl.textContent).toBeTruthy();
-    expect(headlineEl.textContent).toMatchInlineSnapshot(
-      `"Simorgh: Media Pod Build First CPS Media Asset Page in Simorgh with the Help of Drew & < >"`,
-    );
+    expect(headlineEl.textContent).toMatchSnapshot();
   });
 
   it('I can see the timestamp', () => {
@@ -19,9 +17,7 @@ export default () => {
 
     expect(timestampEl).toBeInTheDocument();
     expect(timestampEl.textContent).toBeTruthy();
-    expect(timestampEl.textContent).toMatchInlineSnapshot(
-      `"13 September 2019"`,
-    );
+    expect(timestampEl.textContent).toMatchSnapshot();
   });
 
   const bulletedListEl = document.querySelector('main ul[role="list"]');
@@ -30,9 +26,7 @@ export default () => {
     it('I can see the bulleted list item', () => {
       expect(bulletedListEl).toBeInTheDocument();
       expect(bulletedListEl.textContent).toBeTruthy();
-      expect(bulletedListEl.textContent).toMatchInlineSnapshot(
-        `"<Media Asset Page><Media Asset Page><Media Asset Page>this is the link textthis is the link textthis is the link text"`,
-      );
+      expect(bulletedListEl.textContent).toMatchSnapshot();
     });
   }
 
@@ -42,9 +36,7 @@ export default () => {
     it('I can see the related content', () => {
       expect(releatedContentEl).toBeInTheDocument();
       expect(releatedContentEl.textContent).toBeTruthy();
-      expect(releatedContentEl.textContent).toMatchInlineSnapshot(
-        `"Police Arrest 3 ontop Anambra Church AttackGovernor Willie Obiano of Anambra State bin don promise say government go handle the matter sharp, sharp8th August 2017Lagos 'Gay' men Appear for Court, say Dem no dey GuiltyThe men wey dem arrest almost one week ago don say dem no dey guilty, as court order dem to go for sexual rehabilitation.8th August 2017Image gallery, Kenya Elections Pictures: 8 August 2017This na some of the picture wey our eye see as Kenya people go vote for election today8th August 20170:15Audio, BBC News Pidgin One Minute News, 0,15All di local and world tori wey you suppose know in 60 seconds!17th August 2017"`,
-      );
+      expect(releatedContentEl.textContent).toMatchSnapshot();
     });
   }
 };
