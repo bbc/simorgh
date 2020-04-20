d08fc77bba0c84b51eba08da03a49fca00dd0d02
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2020-04-20 15:10:11 +0000

    Undo bundleSize changes after removing URLSearchParams

--- scripts/bundleSizeConfig.js
+++ scripts/bundleSizeConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Size limit for all bundles used by each service (K)
   // Keep these +/- 5K and update frequently!
-  MIN_SIZE: 784,
-  MAX_SIZE: 828,
+  MIN_SIZE: 769,
+  MAX_SIZE: 813,
 };
