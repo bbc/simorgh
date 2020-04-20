1e1893e77bfb228cdcdce5d7ed0c1e9166a1082a
thekp <khoa.phan@bbc.co.uk>
2020-04-20 15:34:17 +0100

    remove appended json at the end of url

--- src/app/routes/mostRead/getInitialData/index.js
+++ src/app/routes/mostRead/getInitialData/index.js
@@ -2,9 +2,8 @@ import fetchPageData from '../../utils/fetchPageData';
 import { getMostReadEndpoint } from '#lib/utilities/getMostReadUrls';
 
 export default async ({ service, variant }) => {
-  const { json, ...rest } = await fetchPageData(
-    getMostReadEndpoint({ service, variant }),
-  );
+  const mostReadUrl = getMostReadEndpoint({ service, variant }).split('.')[0];
+  const { json, ...rest } = await fetchPageData(mostReadUrl);
 
   return {
     ...rest,
