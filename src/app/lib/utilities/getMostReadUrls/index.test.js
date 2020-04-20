52cbca753e7ca408fe169ea5ce970997f58b0e22
thekp <khoa.phan@bbc.co.uk>
2020-04-20 15:18:08 +0100

    add getMostReadUrls function to lib utils

---
+++ src/app/lib/utilities/getMostReadUrls/index.test.js
@@ -0,0 +1,29 @@
+import { getMostReadEndpoint, getLocalMostReadEndpoint } from '.';
+
+describe('getRadioScheduleEndpoint', () => {
+  it('should return endpoint when passed service', () => {
+    expect(getMostReadEndpoint({ service: 'hausa' })).toBe(
+      '/hausa/mostread.json',
+    );
+  });
+  it('should return endpoint when passed service and variant', () => {
+    expect(getMostReadEndpoint({ service: 'serbian', variant: 'lat' })).toBe(
+      '/serbian/mostread/lat.json',
+    );
+  });
+});
+describe('getLocalRadioScheduleEndpoint', () => {
+  it('should return endpoint when passed service', () => {
+    expect(getLocalMostReadEndpoint({ service: 'hausa' })).toBe(
+      './data/hausa/mostRead/index.json',
+    );
+  });
+  it('should return endpoint when passed service & radioService', () => {
+    expect(
+      getLocalMostReadEndpoint({
+        service: 'serbian',
+        variant: 'lat',
+      }),
+    ).toBe('./data/serbian/mostRead/lat.json');
+  });
+});
