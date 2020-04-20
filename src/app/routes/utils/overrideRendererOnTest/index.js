f69094584642ffbe1d1be4790dffe3b39bf6b7db
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2020-04-20 14:34:05 +0000

    Remove unsafe URLSearchParams

--- src/app/routes/utils/overrideRendererOnTest/index.js
+++ src/app/routes/utils/overrideRendererOnTest/index.js
@@ -1,12 +1,15 @@
 import Url from 'url-parse';
 import isLive from '#lib/utilities/isLive';
 
+const getQueryString = (query) => {
+  const params = Object.keys(query).map((key) => `${key}=${query[key]}`);
+  return params.join('&');
+};
+
 export const addOverrideQuery = (path) => {
-  const url = new Url(path);
+  const url = new Url(path, true);
   const { query } = url;
-  const searchParams = new URLSearchParams(query);
-  searchParams.set('renderer_env', 'live');
-  const searchString = searchParams.toString();
+  const searchString = getQueryString({ ...query, renderer_env: 'live' });
   const pathName = path ? path.split('?')[0] : path;
 
   return `${pathName}?${searchString}`;
