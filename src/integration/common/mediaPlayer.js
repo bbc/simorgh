a0af33c140f21f9d210561c19720264f4dcca1d6
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-04-21 10:09:32 +0100

    read title of media player rather than src

--- src/integration/common/mediaPlayer.js
+++ src/integration/common/mediaPlayer.js
@@ -5,8 +5,8 @@ export default () => {
         const mediaPlayerEl = document.querySelector('iframe, amp-iframe');
 
         expect(mediaPlayerEl).toBeInTheDocument();
-        expect(mediaPlayerEl.getAttribute('src')).toBeTruthy();
-        expect(mediaPlayerEl.getAttribute('src')).toMatchSnapshot();
+        expect(mediaPlayerEl.getAttribute('title')).toBeTruthy();
+        expect(mediaPlayerEl.getAttribute('title')).toMatchSnapshot();
       });
     });
   });
