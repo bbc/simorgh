39c67ee11a6e19092413fe004db581a44307e91e
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-04-22 09:55:56 +0100

    make test canonical only

--- src/integration/pages/frontPage/crossPlatformTests.js
+++ src/integration/pages/frontPage/crossPlatformTests.js
@@ -8,16 +8,4 @@ export default () => {
 
     expect(sect).toBeInTheDocument();
   });
-
-  it('I can see a radio schedule component with an id', () => {
-    const hasRadioSchedule = service === 'arabic';
-
-    const id = document.getElementById('Radio-Schedule');
-
-    if (hasRadioSchedule) {
-      expect(id).toBeInTheDocument();
-    } else {
-      expect(id).not.toBeInTheDocument();
-    }
-  });
 };
