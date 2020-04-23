6938268ea3ca31776b1d5f0530beb2c33a5c023b
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2020-04-23 18:01:37 +0000

    Log unavailable media

--- src/app/routes/cpsAsset/getInitialData/processUnavailableMedia/index.js
+++ src/app/routes/cpsAsset/getInitialData/processUnavailableMedia/index.js
@@ -2,21 +2,44 @@ import pathOr from 'ramda/src/pathOr';
 import assocPath from 'ramda/src/assocPath';
 import path from 'ramda/src/path';
 import nodeLogger from '#lib/logger.node';
-import { NO_MEDIA_BLOCK } from '#lib/logger.const';
+import {
+  NO_MEDIA_BLOCK,
+  MEDIA_ASSET_EXPIRED,
+  MEDIA_ASSET_REVOKED,
+  MEDIA_METADATA_UNAVAILABLE,
+} from '#lib/logger.const';
 
 const logger = nodeLogger(__filename);
 
-const UNAVAILABLE_MEDIA_TEXT = 'unavailableMedia';
-const REVOKED_MEDIA = 'external_vpid';
+export const UNAVAILABLE_MEDIA_TEXT = 'unavailableMedia';
+export const EXTERNAL_VPID = 'external_vpid';
 export const unavailableMediaBlock = {
   type: UNAVAILABLE_MEDIA_TEXT,
   model: {},
   id: UNAVAILABLE_MEDIA_TEXT,
 };
 
+export const logUnavailableMedia = blocks => {
+  const mediaBlock = blocks.find(block => block.type === EXTERNAL_VPID);
+  if (mediaBlock) {
+    const statusCode = path(['statusCode'], mediaBlock);
+    switch (statusCode) {
+      case 404:
+        logger.warn(MEDIA_ASSET_REVOKED);
+        break;
+      case 410:
+        logger.warn(MEDIA_ASSET_EXPIRED);
+        break;
+      default:
+        logger.error(MEDIA_METADATA_UNAVAILABLE);
+    }
+  }
+};
+
 export const addUnavailableMediaBlock = pageData => {
   const blocks = pathOr([], ['content', 'model', 'blocks'], pageData);
-  const filteredBlocks = blocks.filter(block => block.type !== REVOKED_MEDIA);
+  logUnavailableMedia(blocks);
+  const filteredBlocks = blocks.filter(block => block.type !== EXTERNAL_VPID);
   return assocPath(
     ['content', 'model', 'blocks'],
     [unavailableMediaBlock, ...filteredBlocks],
@@ -25,7 +48,7 @@ export const addUnavailableMediaBlock = pageData => {
 };
 
 const logIfNoMedia = (blockTypes, pageData) => {
-  if (!blockTypes.includes(REVOKED_MEDIA)) {
+  if (!blockTypes.includes(EXTERNAL_VPID)) {
     logger.warn(NO_MEDIA_BLOCK, {
       url: path(['metadata', 'locators', 'assetUri'], pageData),
     });
