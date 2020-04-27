ce488cb34eee9b0c4fe06887ede16c733f1f6553
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2020-04-27 14:47:59 +0100

    Update Pashto Radio Schedule position to Interactivity

--- src/app/lib/config/services/pashto.js
+++ src/app/lib/config/services/pashto.js
@@ -194,7 +194,7 @@ export const service = {
     radioSchedule: {
       hasRadioSchedule: true,
       onFrontPage: true,
-      frontPagePosition: 'Showcase 1',
+      frontPagePosition: 'Interactivity',
       frequenciesPageUrl: '/pashto/institutional/2012/03/000001_frequencies',
       frequenciesPageLabel: 'راديويي څپې',
       header: 'تازه خبرونه واورئ',
