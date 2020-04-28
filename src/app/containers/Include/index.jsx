3c3c7f6aa65fc03030fd4776a7a4b7466bc9da32
sadickisaac <isaac.odhiambo@andela.com>
2020-04-28 16:58:23 +0300

    Cleanup

--- src/app/containers/Include/index.jsx
+++ src/app/containers/Include/index.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/no-danger */
 import React from 'react';
 import { string } from 'prop-types';
+import { Helmet } from 'react-helmet';
 import { GridItemConstrainedMedium } from '#lib/styledGrid';
 import useToggle from '#hooks/useToggle';
-import { Helmet } from 'react-helmet';
 
 const IncludeContainer = ({ html, type }) => {
   const { enabled } = useToggle('include');
@@ -22,8 +22,14 @@ const IncludeContainer = ({ html, type }) => {
 
   const requireIncludeTypes = ['vj', 'idt1'];
 
-  const configureJQuery = `const paths = { "jquery-1": "https:\/\/static.bbc.co.uk\/frameworks\/jquery\/0.4.1\/sharedmodules\/jquery-1.7.2" };
-  require.config({ paths })`;
+  const paths = `{
+    'jquery-1':
+      'https://static.bbc.co.uk/frameworks/jquery/0.4.1/sharedmodules/jquery-1.7.2',
+    'istats-1':
+      'https://nav.files.bbci.co.uk/orbit-webmodules/0.0.2-448.45dcd56/istats/istats-1',
+  }`;
+
+  const configureAdditionalScripts = `require.config({ paths:${paths} });`;
 
   return (
     <>
@@ -32,20 +38,8 @@ const IncludeContainer = ({ html, type }) => {
           <script
             type="text/javascript"
             src="https://news.files.bbci.co.uk/include/vjassets/js/vendor/require-2.1.20b.min.js"
-          ></script>
-          <script
-            type="text/javascript"
-            src="https://nav.files.bbci.co.uk/orbit-webmodules/0.0.2-448.45dcd56/istats/istats-1.js"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{ __html: configureJQuery }}
-          ></script>
-          <script>
-          const paths = { 
-          "jquery-1": "https:\/\/static.bbc.co.uk\/frameworks\/jquery\/0.4.1\/sharedmodules\/jquery-1.7.2"
-           };
-          require.config({ paths })
-          </script>
+          />
+          <script>{configureAdditionalScripts}</script>
         </Helmet>
       ) : null}
       <GridItemConstrainedMedium>
