cf93798a0355a3ed3c8e801e0041db96867d2c6a
bennea27 <andrew.bennett07@bbc.co.uk>
2020-04-28 13:58:54 +0100

    Initial work to stub require (and other libraries)

--- src/app/containers/Include/index.jsx
+++ src/app/containers/Include/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { string } from 'prop-types';
 import { GridItemConstrainedMedium } from '#lib/styledGrid';
 import useToggle from '#hooks/useToggle';
+import { Helmet } from 'react-helmet';
 
 const IncludeContainer = ({ html, type }) => {
   const { enabled } = useToggle('include');
@@ -10,6 +11,7 @@ const IncludeContainer = ({ html, type }) => {
   const supportedTypes = {
     idt2: 'idt2',
     vj: 'vj',
+    idt1: 'idt1',
   };
 
   const shouldNotRenderInclude = !enabled || !html || !supportedTypes[type];
@@ -18,13 +20,41 @@ const IncludeContainer = ({ html, type }) => {
     return null;
   }
 
+  const requireIncludeTypes = ['vj', 'idt1'];
+
+  const configureJQuery = `const paths = { "jquery-1": "https:\/\/static.bbc.co.uk\/frameworks\/jquery\/0.4.1\/sharedmodules\/jquery-1.7.2" };
+  require.config({ paths })`;
+
   return (
-    <GridItemConstrainedMedium>
-      <div
-        suppressHydrationWarning
-        dangerouslySetInnerHTML={{ __html: html }}
-      />
-    </GridItemConstrainedMedium>
+    <>
+      {requireIncludeTypes.includes(type) ? (
+        <Helmet>
+          <script
+            type="text/javascript"
+            src="https://news.files.bbci.co.uk/include/vjassets/js/vendor/require-2.1.20b.min.js"
+          ></script>
+          <script
+            type="text/javascript"
+            src="https://nav.files.bbci.co.uk/orbit-webmodules/0.0.2-448.45dcd56/istats/istats-1.js"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{ __html: configureJQuery }}
+          ></script>
+          <script>
+          const paths = { 
+          "jquery-1": "https:\/\/static.bbc.co.uk\/frameworks\/jquery\/0.4.1\/sharedmodules\/jquery-1.7.2"
+           };
+          require.config({ paths })
+          </script>
+        </Helmet>
+      ) : null}
+      <GridItemConstrainedMedium>
+        <div
+          suppressHydrationWarning
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </GridItemConstrainedMedium>
+    </>
   );
 };
 
