80d06fa39e2db127e6e1ceaaa7b0c1d58c30b8c6
Einstein <forensicslabnet@gmail.com>
2020-04-29 13:20:11 +0300

    Bump psammead-bulletin

--- src/app/containers/Bulletin/index.jsx
+++ src/app/containers/Bulletin/index.jsx
@@ -56,9 +56,7 @@ BulletinImage.defaultProps = {
 };
 
 const BulletinContainer = ({ item, lazyLoadImage }) => {
-  const { script, service, dir, translations, lang } = useContext(
-    ServiceContext,
-  );
+  const { script, service, dir, translations } = useContext(ServiceContext);
 
   const headline = pathOr(null, ['name'], item);
   const ctaLink = pathOr(null, ['uri'], item);
@@ -103,7 +101,7 @@ const BulletinContainer = ({ item, lazyLoadImage }) => {
       isLive={isLive}
       liveText={liveText}
       offScreenText={offScreenText}
-      lang={ctaTextIsEnglish && 'en-GB'}
+      lang={ctaTextIsEnglish ? 'en-GB' : null}
     />
   );
 };
