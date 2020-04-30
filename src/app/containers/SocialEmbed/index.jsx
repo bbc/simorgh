d9289bea8648fa6d87d0c642d4cc1d99dab2f871
Simon Sinclair <simon@simonsinclair.com>
2020-04-30 14:48:21 +0100

    Continue to render, even if `id` is not present

--- src/app/containers/SocialEmbed/index.jsx
+++ src/app/containers/SocialEmbed/index.jsx
@@ -54,7 +54,7 @@ const SocialEmbedContainer = ({ blocks }) => {
 
   const id = path(['id'], model);
   const href = path(['href'], model);
-  if (!id || !href) return null;
+  if (!href) return null;
 
   const oEmbed = path(['embed', 'oembed'], model);
 
