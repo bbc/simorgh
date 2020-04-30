0e96611b6036004e4f2793846fbe8041a19c98cf
Simon Sinclair <simon.sinclair@bbc.co.uk>
2020-04-30 15:30:03 +0100

    Amend comment

--- src/app/models/propTypes/socialEmbed/index.js
+++ src/app/models/propTypes/socialEmbed/index.js
@@ -8,7 +8,7 @@ const socialEmbedBlockPropTypes = {
       model: shape({
         // `id` is provided by Ares on the basis it knows how to extract it
         // from the URL. For unsupported providers (e.g. Facebook), this will
-        // not be supplied - for that reason it is no required.
+        // not be supplied - for that reason it is not required.
         id: string,
         href: string.isRequired,
         // 'embed' is not required, however if it is included it must contain
