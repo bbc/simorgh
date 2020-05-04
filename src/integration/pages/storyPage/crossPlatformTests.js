d2ca55ca111d89b4fc402dc9fc246ced2f78e449
Simon Sinclair <simon@simonsinclair.com>
2020-05-04 15:19:01 +0100

    Nest Social Embed tests in a describe block

--- src/integration/pages/storyPage/crossPlatformTests.js
+++ src/integration/pages/storyPage/crossPlatformTests.js
@@ -15,59 +15,62 @@ export default () => {
     expect(imageCaptionEl.textContent).toMatchSnapshot();
   });
 
-  /**
-   * Social Embeds.
-   */
-  it('I can see the skip social embed link', () => {
-    const skipLinkEl = document.querySelector(
-      'a[href="#skip-youtube-content-1"]',
-    );
-
-    expect(skipLinkEl).toBeInTheDocument();
-    expect(skipLinkEl.textContent).toBeTruthy();
-    expect(skipLinkEl.textContent).toMatchSnapshot();
-  });
-
-  it('I can see the skip social embed link destination', () => {
-    const skipLinkDestinationEl = document.getElementById(
-      'skip-youtube-content-1',
-    );
-
-    expect(skipLinkDestinationEl).toBeInTheDocument();
-    expect(skipLinkDestinationEl.textContent).toBeTruthy();
-    expect(skipLinkDestinationEl.textContent).toMatchSnapshot();
-  });
-
-  // Social Embeds - Rich.
-  const hasRichYouTubeEmbed = [
-    document.querySelector('iframe[src*="https://www.youtube.com/embed/"]'),
-    document.querySelector('amp-youtube'),
-  ].find(el => !!el);
-
-  if (hasRichYouTubeEmbed) {
-    it('I can see the caption accompanying a rich YouTube embed', () => {
-      const figCaptionEl = document.querySelector(
-        'a[href="#skip-youtube-content-1"] + figure > figcaption',
+  describe('Social Embeds', () => {
+    it('I can see the skip link', () => {
+      const skipLinkEl = document.querySelector(
+        'a[href="#skip-youtube-content-1"]',
       );
 
-      expect(figCaptionEl).toBeInTheDocument();
-      expect(figCaptionEl.textContent).toBeTruthy();
-      expect(figCaptionEl.textContent).toMatchSnapshot();
+      expect(skipLinkEl).toBeInTheDocument();
+      expect(skipLinkEl.textContent).toBeTruthy();
+      expect(skipLinkEl.textContent).toMatchSnapshot();
     });
-  }
 
-  // Social Embeds - Fallback.
-  const hasFallback = !!document.getElementById('skip-facebook-content-1');
-
-  if (hasFallback) {
-    it('I can see a link to the original content in a social embed fallback', () => {
-      const externalLinkEl = document.querySelector(
-        'a[href="https://www.facebook.com/228735667216/posts/10157712846517217/?sfnsn=scwspmo&extid=ixkwkArH1fy0g2GP"]',
+    it('I can see the skip link destination', () => {
+      const skipLinkDestinationEl = document.getElementById(
+        'skip-youtube-content-1',
       );
 
-      expect(externalLinkEl).toBeInTheDocument();
-      expect(externalLinkEl.textContent).toBeTruthy();
-      expect(externalLinkEl.textContent).toMatchSnapshot();
+      expect(skipLinkDestinationEl).toBeInTheDocument();
+      expect(skipLinkDestinationEl.textContent).toBeTruthy();
+      expect(skipLinkDestinationEl.textContent).toMatchSnapshot();
     });
-  }
+
+    /**
+     * Rich embeds.
+     */
+    const hasRichYouTubeEmbed = [
+      document.querySelector('iframe[src*="https://www.youtube.com/embed/"]'),
+      document.querySelector('amp-youtube'),
+    ].find(el => !!el);
+
+    if (hasRichYouTubeEmbed) {
+      it('I can see the caption accompanying a YouTube embed', () => {
+        const figCaptionEl = document.querySelector(
+          'a[href="#skip-youtube-content-1"] + figure > figcaption',
+        );
+
+        expect(figCaptionEl).toBeInTheDocument();
+        expect(figCaptionEl.textContent).toBeTruthy();
+        expect(figCaptionEl.textContent).toMatchSnapshot();
+      });
+    }
+
+    /**
+     * Fallbacks.
+     */
+    const hasFallback = !!document.getElementById('skip-facebook-content-1');
+
+    if (hasFallback) {
+      it('I can see a link to the original content in a fallback', () => {
+        const externalLinkEl = document.querySelector(
+          'a[href="https://www.facebook.com/228735667216/posts/10157712846517217/?sfnsn=scwspmo&extid=ixkwkArH1fy0g2GP"]',
+        );
+
+        expect(externalLinkEl).toBeInTheDocument();
+        expect(externalLinkEl.textContent).toBeTruthy();
+        expect(externalLinkEl.textContent).toMatchSnapshot();
+      });
+    }
+  });
 };
