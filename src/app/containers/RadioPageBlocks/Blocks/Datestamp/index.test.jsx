c149c3b24c69533745987aab33e652ff77a5abbd
RichardPK <r.phillips.kerr@gmail.com>
2020-05-04 12:52:59 +0100

    Added snapshot test for datestamp

--- src/app/containers/RadioPageBlocks/Blocks/Datestamp/index.test.jsx
+++ src/app/containers/RadioPageBlocks/Blocks/Datestamp/index.test.jsx
@@ -1,28 +1,28 @@
-// import React from 'react';
-// import {
-//   shouldMatchSnapshot,
-//   isNull,
-//   suppressPropWarnings,
-// } from '@bbc/psammead-test-helpers';
-// import { ServiceContextProvider } from '#contexts/ServiceContext';
-// import Datestamp from '.';
+import React from 'react';
+import {
+  shouldMatchSnapshot,
+  isNull,
+  suppressPropWarnings,
+} from '@bbc/psammead-test-helpers';
+import { ServiceContextProvider } from '#contexts/ServiceContext';
+import Datestamp from '.';
 
-// describe('AudioPlayer blocks DateStamp', () => {
-//   shouldMatchSnapshot(
-//     'should render correctly',
-//     <ServiceContextProvider service="news">
+describe('AudioPlayer blocks DateStamp', () => {
+  shouldMatchSnapshot(
+    'should render correctly',
+    <ServiceContextProvider service="news">
+      <Datestamp timestamp={1587945600000} uuid="uuid" idAttr="idAttr" />
+    </ServiceContextProvider>,
+  );
 
-//     </ServiceContextProvider>,
-//   );
+  describe('when timestamp isnt provided', () => {
+    suppressPropWarnings(['timestamp', 'undefined']);
 
-//   describe('when timestamp isnt provided', () => {
-//     suppressPropWarnings(['text', 'undefined']);
-
-//     isNull(
-//       'should render null',
-//       <ServiceContextProvider service="news">
-//         <Datestamp uuid="uuid" idAttr="idAttr" />
-//       </ServiceContextProvider>,
-//     );
-//   });
-// });
+    isNull(
+      'should render null',
+      <ServiceContextProvider service="news">
+        <Datestamp uuid="uuid" idAttr="idAttr" />
+      </ServiceContextProvider>,
+    );
+  });
+});
