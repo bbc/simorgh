5511f2c99f5732ec2dabe0a74865efb3b66ec753
Simon Sinclair <simon@simonsinclair.com>
2020-05-04 16:06:37 +0100

    Add third-party JavaScript tests

--- src/integration/pages/storyPage/canonicalTests.js
+++ src/integration/pages/storyPage/canonicalTests.js
@@ -6,4 +6,20 @@ import {
 export default () => {
   runCoreCanonicalTests();
   runCanonicalAnalyticsTests();
+
+  describe('Social Embeds', () => {
+    const hasRichInstagramEmbed = !!document.querySelector(
+      'iframe.instagram-media',
+    );
+
+    if (hasRichInstagramEmbed) {
+      it("I can see the social media provider's JavaScript", () => {
+        expect(
+          document.querySelector(
+            'head > script[src="https://www.instagram.com/embed.js"]',
+          ),
+        ).toBeInTheDocument();
+      });
+    }
+  });
 };
