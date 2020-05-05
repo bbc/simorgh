5ac2619fd1a2454622d19d911a0e5a941a18248e
Karina Thomas <karina.thomas@bbc.co.uk>
2020-05-05 18:36:41 +0100

    Tidy up mock services - remove fields which are no longer required

--- scripts/structuredData/mocks/services.js
+++ scripts/structuredData/mocks/services.js
@@ -5,14 +5,6 @@ jest.mock('../../../cypress/support/config/services', () => {
       pageTypes: {
         articles: {
           environments: {
-            live: {
-              paths: ['/afaanoromoo/articles/ce3nlgrelv1o'],
-              enabled: true,
-            },
-            test: {
-              paths: ['/afaanoromoo/articles/c4g19kgl85ko'],
-              enabled: true,
-            },
             local: {
               paths: ['/afaanoromoo/articles/c4g19kgl85ko'],
               enabled: true,
@@ -21,14 +13,6 @@ jest.mock('../../../cypress/support/config/services', () => {
         },
         frontPage: {
           environments: {
-            live: {
-              paths: ['/afaanoromoo'],
-              enabled: true,
-            },
-            test: {
-              paths: ['/afaanoromoo'],
-              enabled: true,
-            },
             local: {
               paths: ['/afaanoromoo'],
               enabled: true,
@@ -37,75 +21,35 @@ jest.mock('../../../cypress/support/config/services', () => {
         },
         liveRadio: {
           environments: {
-            live: {
-              paths: ['/afaanoromoo/bbc_afaanoromoo_radio/liveradio'],
-              enabled: true,
-            },
-            test: {
-              paths: ['/afaanoromoo/bbc_afaanoromoo_radio/liveradio'],
-              enabled: true,
-            },
             local: {
               paths: ['/afaanoromoo/bbc_afaanoromoo_radio/liveradio'],
               enabled: true,
             },
           },
-          smoke: false,
         },
         onDemandRadioEpisode: {
           environments: {
-            live: {
-              paths: [],
-              enabled: false,
-            },
-            test: {
-              paths: [],
-              enabled: false,
-            },
             local: {
               paths: ['/afaanoromoo/bbc_afaanoromoo_radio/w3cszx1y'],
               enabled: true,
             },
           },
-          smoke: false,
         },
         mediaAssetPage: {
           environments: {
-            live: {
-              paths: [
-                '/afaanoromoo/oduu-51248626', // CPS MAP with video clip
-              ],
-              enabled: false,
-            },
-            test: {
-              paths: [
-                '/afaanoromoo/23149891', // CPS MAP with video clip
-              ],
-              enabled: true,
-            },
             local: {
-              paths: ['/afaanoromoo/23149891'], // CPS MAP with video clip
+              paths: ['/afaanoromoo/23149891'],
               enabled: true,
             },
           },
-          smoke: false,
         },
         photoGalleryPage: {
           environments: {
-            live: {
-              paths: ['/afaanoromoo/oduu-50716382'],
-              enabled: false,
-            },
-            test: {
-              paths: ['/afaanoromoo/oduu-23141286'],
-              enabled: false,
-            },
             local: {
               paths: ['/afaanoromoo/oduu-41217768'],
               enabled: true,
             },
           },
-          smoke: false,
         },
       },
     },
@@ -114,14 +58,6 @@ jest.mock('../../../cypress/support/config/services', () => {
       pageTypes: {
         articles: {
           environments: {
-            live: {
-              paths: ['/amharic/articles/c0lgxqknqkdo'],
-              enabled: true,
-            },
-            test: {
-              paths: ['/amharic/articles/czqverekrldo'],
-              enabled: true,
-            },
             local: {
               paths: ['/amharic/articles/czqverekrldo'],
               enabled: true,
@@ -130,14 +66,6 @@ jest.mock('../../../cypress/support/config/services', () => {
         },
         errorPage404: {
           environments: {
-            live: {
-              paths: [],
-              enabled: false,
-            },
-            test: {
-              paths: [],
-              enabled: false,
-            },
             local: {
               paths: ['/amharic/articles/c123456abcdo'],
               enabled: true,
@@ -146,14 +74,6 @@ jest.mock('../../../cypress/support/config/services', () => {
         },
         frontPage: {
           environments: {
-            live: {
-              paths: ['/amharic'],
-              enabled: true,
-            },
-            test: {
-              paths: ['/amharic'],
-              enabled: true,
-            },
             local: {
               paths: ['/amharic'],
               enabled: true,
@@ -162,14 +82,6 @@ jest.mock('../../../cypress/support/config/services', () => {
         },
         liveRadio: {
           environments: {
-            live: {
-              paths: ['/amharic/bbc_amharic_radio/liveradio'],
-              enabled: true,
-            },
-            test: {
-              paths: ['/amharic/bbc_amharic_radio/liveradio'],
-              enabled: true,
-            },
             local: {
               paths: ['/amharic/bbc_amharic_radio/liveradio'],
               enabled: true,
@@ -178,14 +90,6 @@ jest.mock('../../../cypress/support/config/services', () => {
         },
         onDemandRadioEpisode: {
           environments: {
-            live: {
-              paths: [],
-              enabled: false,
-            },
-            test: {
-              paths: [],
-              enabled: false,
-            },
             local: {
               paths: ['/amharic/bbc_amharic_radio/w3csz5r9'],
               enabled: true,
@@ -194,34 +98,14 @@ jest.mock('../../../cypress/support/config/services', () => {
         },
         mediaAssetPage: {
           environments: {
-            live: {
-              paths: [
-                '/amharic/news-51270657', // CPS MAP with video clip
-              ],
-              enabled: false,
-            },
-            test: {
-              paths: [
-                '/amharic/news-23263266', // CPS MAP with video clip
-              ],
-              enabled: true,
-            },
             local: {
-              paths: ['/amharic/news-23263266'], // CPS MAP with video clip
+              paths: ['/amharic/news-23263266'],
               enabled: true,
             },
           },
         },
         photoGalleryPage: {
           environments: {
-            live: {
-              paths: ['/amharic/42743191'],
-              enabled: false,
-            },
-            test: {
-              paths: ['/amharic/23194496'],
-              enabled: false,
-            },
             local: {
               paths: ['/amharic/42743191'],
               enabled: true,
