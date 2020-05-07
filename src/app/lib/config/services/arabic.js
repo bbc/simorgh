007a67aa4b37bdd8f5cf6c107a406212a5c2564b
Ho Ting Lok <hoting.lok@bbc.co.uk>
2020-05-07 14:21:31 +0100

    Re-added durationLabel with text/number swapped

--- src/app/lib/config/services/arabic.js
+++ src/app/lib/config/services/arabic.js
@@ -198,6 +198,7 @@ export const service = {
         '/arabic/institutional/2011/01/000000_frequencies_radio',
       frequenciesPageLabel: 'استقبال البث',
       header: 'أحدث نشرة أخبار',
+      durationLabel: '%duration% المدة',
     },
     recommendations: {
       hasStoryRecommendations: true,
