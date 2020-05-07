45b74bad4899c2ef3a511913747f9cb5e2208d5a
RichardPK <r.phillips.kerr@gmail.com>
2020-05-07 16:29:58 +0100

    Move datestamp into OnDemandHeading

--- src/app/containers/RadioPageBlocks/Blocks/OnDemandHeading/index.jsx
+++ src/app/containers/RadioPageBlocks/Blocks/OnDemandHeading/index.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { string } from 'prop-types';
+import { C_SHADOW } from '@bbc/psammead-styles/colours';
 import styled from 'styled-components';
 import { Headline } from '@bbc/psammead-headings';
 import VisuallyHiddenText from '@bbc/psammead-visually-hidden-text';
@@ -9,8 +10,16 @@ import {
   GEL_SPACING_TRPL,
 } from '@bbc/gel-foundations/spacings';
 import { MEDIA_QUERY_TYPOGRAPHY } from '@bbc/gel-foundations/breakpoints';
+import { formatUnixTimestamp } from '@bbc/psammead-timestamp-container/utilities';
+import { getBodyCopy } from '@bbc/gel-foundations/typography';
+import { getSansRegular } from '@bbc/psammead-styles/font-styles';
 import { ServiceContext } from '#contexts/ServiceContext';
-import Datestamp from '#containers/RadioPageBlocks/Blocks/Datestamp';
+
+const StyledHeadline = styled(Headline)`
+  @media (min-width: 1px) {
+    padding: ${GEL_SPACING_QUAD} 0 ${GEL_SPACING_TRPL};
+  }
+`;
 
 const BrandTitle = styled.span`
   display: block;
@@ -20,16 +29,24 @@ const BrandTitle = styled.span`
   }
 `;
 
-const StyledHeadline = styled(Headline)`
-  @media (min-width: 1px) {
-    padding: ${GEL_SPACING_QUAD} 0 ${GEL_SPACING_TRPL};
-  }
+const Datestamp = styled.span`
+  ${({ script }) => script && getBodyCopy(script)};
+  ${({ service }) => getSansRegular(service)};
+  color: ${C_SHADOW};
+  /* padding-bottom: ${GEL_SPACING_TRPL}; */
+  margin: 0; /* Reset */
 `;
 
 const HeadingContainer = ({ idAttr, brandTitle, releaseDateTimeStamp }) => {
-  const { script, service } = useContext(ServiceContext);
+  const { script, service, timezone, locale } = useContext(ServiceContext);
 
-  // if (!brandTitle) return null;
+  const formattedTimestamp = formatUnixTimestamp({
+    releaseDateTimeStamp,
+    format: 'LL',
+    timezone,
+    locale,
+    isRelative: false,
+  });
 
   return (
     <StyledHeadline script={script} service={service} id={idAttr} tabIndex="-1">
@@ -39,7 +56,13 @@ const HeadingContainer = ({ idAttr, brandTitle, releaseDateTimeStamp }) => {
       >
         <BrandTitle>{brandTitle}</BrandTitle>
         <VisuallyHiddenText tabIndex="-1">, </VisuallyHiddenText>
-        <Datestamp releaseDateTimeStamp={releaseDateTimeStamp} />
+        <Datestamp
+          script={script}
+          service={service}
+          releaseDateTimeStamp={releaseDateTimeStamp}
+        >
+          {formattedTimestamp}
+        </Datestamp>
       </span>
     </StyledHeadline>
   );
