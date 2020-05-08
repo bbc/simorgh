e2c5c95ef7899f4e7f14f76ef51c6a440ba46d7a
Ruth Ogendi <ruth.ogendi@andela.com>
2020-05-08 20:31:44 +0300

    Update processMostRead to handle optimo data

--- src/app/containers/MostRead/utilities/processMostRead.js
+++ src/app/containers/MostRead/utilities/processMostRead.js
@@ -1,11 +1,12 @@
 import pathOr from 'ramda/src/pathOr';
 import { mostReadRecordIsFresh } from '.';
 
+// const getMostReadItemData = record 
+
 const mostReadItems = ({ data, numberOfItems }) => {
   if (!data) {
     return null;
   }
-
   const records = pathOr([], ['records'], data);
 
   // The ARES test endpoint for most read renders fixture data, so the data is stale
@@ -14,16 +15,35 @@ const mostReadItems = ({ data, numberOfItems }) => {
   // Do not show most read if lastRecordUpdated is greater than 35min as this means PopAPI has failed twice
   // in succession. This suggests ATI may be having issues, hence risk of stale data.
   if (isTest || mostReadRecordIsFresh(data.lastRecordTimeStamp)) {
-    return records
-      .slice(0, numberOfItems)
-      .map(({ id, promo: { headlines, locators, timestamp } }) => ({
-        id,
-        title:
-          headlines.shortHeadline ||
-          headlines.promoHeadline.blocks[0].model.blocks[0].model.text,
-        href: locators.assetUri || locators.canonicalUrl,
-        timestamp,
-      }));
+    records.slice(0, numberOfItems).map(record => {
+      const cpsHeadline = pathOr(
+        null,
+        ['headlines', 'shortHeadline'],
+        record.promo,
+      );
+      const cpsLocator = pathOr(null, ['locators', 'assetUri'], record.promo);
+      const optimoHeadline = pathOr(
+        null,
+        [
+          'headlines',
+          'promoHeadline',
+          'blocks[0]',
+          'model',
+          'blocks[0]',
+          'model',
+          'text',
+        ],
+        record,
+      );
+      const optimoLocator = pathOr(null, ['locators', 'canonicalUrl'], record);
+      console.log('DETS---', cpsHeadline, cpsLocator);
+
+      return {
+        id: record.id,
+        title: cpsHeadline || optimoHeadline,
+        href: cpsLocator || optimoLocator,
+      };
+    });
   }
   return null;
 };
