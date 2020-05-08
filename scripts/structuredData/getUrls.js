e5bb1d546c527ee0d3ea09a70f6677ce2d4c43cb
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2020-05-08 15:27:23 +0000

    Select paths and url host based on environment

--- scripts/structuredData/getUrls.js
+++ scripts/structuredData/getUrls.js
@@ -1,20 +1,31 @@
 global.Cypress = {
-  env: () => {
-    return 'local';
-  },
+  env: () => 'local',
 };
 
 const getPaths = require('../../cypress/support/helpers/getPaths');
 const services = require('../../cypress/support/config/services');
 
+const baseUrls = {
+  local: 'http://localhost:7080',
+  test: 'https://www.test.bbc.com',
+  live: 'https://www.bbc.com',
+};
+
+const getHost = () => {
+  const env = process.env.SIMORGH_APP_ENV || 'local';
+  global.Cypress.env = () => env;
+  return baseUrls[env];
+};
+
 const getUrls = () => {
   const urlsToValidate = [];
+  const host = getHost();
   Object.keys(services).forEach(service => {
     Object.keys(services[service].pageTypes)
       .filter(pageType => !pageType.startsWith('error'))
       .forEach(pageType => {
         const paths = getPaths(service, pageType);
-        const urls = paths.map(path => `http://localhost:7080${path}`);
+        const urls = paths.map(path => `${host}${path}`);
 
         urlsToValidate.push(...urls);
       });
