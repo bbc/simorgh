bae4229de924d998cf88349d8f5dd708dc334889
Khoa Thế Phan <30599794+thekp@users.noreply.github.com>
2020-05-11 14:58:08 +0100

    Update src/integration/pages/mostReadPage/crossPlatformTests.js
    Co-authored-by: Tom Williams <tom.williams07@bbc.co.uk>

--- src/integration/pages/mostReadPage/crossPlatformTests.js
+++ src/integration/pages/mostReadPage/crossPlatformTests.js
@@ -14,7 +14,7 @@ export default () => {
   it('I can see the list items', () => {
     const listItemElements = document.querySelectorAll('main ol li');
 
-    listItemtElements.forEach(itemEl => {
+    listItemElements.forEach(itemEl => {
       expect(itemEl).toBeInTheDocument();
       expect(itemEl).toBeTruthy();
       expect(itemEl.textContent).toMatchSnapshot();
