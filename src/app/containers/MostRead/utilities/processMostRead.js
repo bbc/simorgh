aba06a886076baebd7408e8f51648bfaa15a102a
Ruth Ogendi <ruth.ogendi@andela.com>
2020-05-12 08:45:22 +0300

    Remove incolpete data

--- src/app/containers/MostRead/utilities/processMostRead.js
+++ src/app/containers/MostRead/utilities/processMostRead.js
@@ -1,6 +1,40 @@
 import pathOr from 'ramda/src/pathOr';
 import { mostReadRecordIsFresh } from '.';
 
+const getMostReadItemData = record => {
+  const cpsHeadline = pathOr(
+    null,
+    ['promo', 'headlines', 'shortHeadline'],
+    record,
+  );
+  const optimoHeadline = pathOr(
+    null,
+    [
+      'headlines',
+      'promoHeadline',
+      'blocks',
+      0,
+      'model',
+      'blocks',
+      0,
+      'model',
+      'text',
+    ],
+    record,
+  );
+  const cpsLocator = pathOr(null, ['promo', 'locators', 'assetUri'], record);
+  const optimoLocator = pathOr(null, ['locators', 'canonicalUrl'], record);
+  const cpsTimestamp = pathOr(null, ['promo', 'timestamp'], record);
+  const optimoTimestamp = pathOr(null, ['timestamp'], record);
+
+  return {
+    id: record.id,
+    title: cpsHeadline || optimoHeadline,
+    href: cpsLocator || optimoLocator,
+    timestamp: cpsTimestamp || optimoTimestamp,
+  };
+};
+
 const mostReadItems = ({ data, numberOfItems }) => {
   if (!data) {
     return null;
@@ -13,40 +47,10 @@ const mostReadItems = ({ data, numberOfItems }) => {
   // Do not show most read if lastRecordUpdated is greater than 35min as this means PopAPI has failed twice
   // in succession. This suggests ATI may be having issues, hence risk of stale data.
   if (isTest || mostReadRecordIsFresh(data.lastRecordTimeStamp)) {
-    const items = records.slice(0, numberOfItems).map(record => {
-      const cpsHeadline = pathOr(
-        null,
-        ['headlines', 'shortHeadline'],
-        record.promo,
-      );
-      // const contentBlocks = blocks.map(block => block.model.blocks).flat();
-      const optimoHeadline = pathOr(
-        null,
-        [
-          'headlines',
-          'promoHeadline',
-          'blocks',
-          0,
-          'model',
-          'blocks',
-          0,
-          'model',
-          'text',
-        ],
-        record,
-      );
-      const cpsLocator = pathOr(null, ['locators', 'assetUri'], record.promo);
-      const optimoLocator = pathOr(null, ['locators', 'canonicalUrl'], record);
-      const cpsTimestamp = pathOr(null, ['promo', 'timestamp'], record);
-      const optimoTimestamp = pathOr(null, ['timestamp'], record);
-
-      return {
-        id: record.id,
-        title: cpsHeadline || optimoHeadline,
-        href: cpsLocator || optimoLocator,
-        timestamp: cpsTimestamp || optimoTimestamp,
-      };
-    });
+    const items = records
+      .map(record => getMostReadItemData(record))
+      .filter(record => record.title && record.href)
+      .slice(0, numberOfItems);
     return items;
   }
   return null;
