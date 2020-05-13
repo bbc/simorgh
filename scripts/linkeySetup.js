194729d4915d553641df5744e1a2e65ef81505bd
J Blaut <jagoda.blaut@bbc.co.uk>
2020-05-13 15:55:11 +0100

    add tests for services with variants

--- scripts/linkeySetup.js
+++ scripts/linkeySetup.js
@@ -7,9 +7,6 @@ const excludes = [
   'loadableConfig',
   'naidheachdan',
   'scotland',
-  'serbian', // service with variant
-  'ukchina', // service with variant
-  'zhongwen', // service with variant
 ];
 
 fs.readdir(dir, (err, files) => {
@@ -21,8 +18,6 @@ fs.readdir(dir, (err, files) => {
     const noExtension = configFileName.split('.');
     const fileName = `${dir}/${noExtension[0]}.test.js`;
 
-    // TO DO: footer links
-    // TO DO: services with variants
     // TO DO: what if no navigation/footer links available
     const importConfig = `import { service } from '#lib/config/services/${noExtension[0]}';\n`;
     const testFile = fs.readFileSync('scripts/linkeyTest.js', (error, data) => {
--- scripts/linkeyTest.js
+++ scripts/linkeyTest.js
@@ -1,13 +1,37 @@
 import fetch from 'node-fetch';
 
 /* eslint-disable no-undef */
+const getServiceName = serviceConfig => {
+  if (serviceConfig.length === 1) {
+    return service.default.service;
+  }
+
+  const variants = Object.keys(serviceConfig);
+  return service[variants[0]].service;
+};
+
+const getLinks = (serviceConfig, type) => {
+  if (serviceConfig.length === 1) {
+    return serviceConfig.default;
+  }
+
+  const variants = Object.keys(serviceConfig);
+  const serviceConfigVariantOne = serviceConfig[variants[0]][type];
+  const serviceConfigVariantTwo = serviceConfig[variants[1]][type];
+
+  return type === 'navigation'
+    ? serviceConfigVariantOne.concat(serviceConfigVariantTwo)
+    : { ...serviceConfigVariantOne, ...serviceConfigVariantTwo };
+};
+
 const fetchResponse = async link => {
   const fetchStatus = await fetch(link);
   return fetchStatus.status;
 };
 
-describe(`${service.default.service} navigation links`, () => {
-  const { navigation } = service.default;
+describe(`${getServiceName(service)} navigation links`, () => {
+  const navigation = getLinks(service, 'navigation');
+
   navigation.map(nav => {
     const fullUrl = `https://www.bbc.com${nav.url}`;
     return it(`should return 200 for ${fullUrl}`, async () => {
@@ -16,8 +40,8 @@ describe(`${service.default.service} navigation links`, () => {
   });
 });
 
-describe(`${service.default.service} footer links`, () => {
-  const { footer } = service.default;
+describe(`${getServiceName(service)} footer links`, () => {
+  const footer = getLinks(service, 'footer');
   const keys = Object.keys(footer);
   const hrefsArray = [];
 
