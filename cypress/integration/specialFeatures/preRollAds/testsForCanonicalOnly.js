cceccf02a8e13fe8d89edc30053024962be171fa
LilyL0u <lily.higham@bbc.co.uk>
2020-05-13 13:58:31 +0100

    removed some logs

--- cypress/integration/specialFeatures/preRollAds/testsForCanonicalOnly.js
+++ cypress/integration/specialFeatures/preRollAds/testsForCanonicalOnly.js
@@ -29,13 +29,9 @@ export default ({ serviceName, pageType, path }) => {
     it('should load pre-roll ad plugin', () => {
       cy.request(`${path}.json`).then(({ body: jsonData }) => {
         if (hasMedia(jsonData)) {
-          cy.log('has media');
           if (shouldAllowAdsForAssetType(jsonData)) {
-            cy.log('cps asset');
             if (getAdSetting(jsonData)) {
-              cy.log('ads allowed');
               if (shouldAllowAdsForVideoDuration(jsonData)) {
-                cy.log('Duration condition met');
                 cy.get(
                   `script[src="https://gn-web-assets.api.bbc.com/ngas/dotcom-preroll.js"]`,
                 ).should('exist');
