a12337e578e883e1b81ffce266bca56edc069939
Karina Thomas <karina.thomas@bbc.co.uk>
2020-05-14 17:06:46 +0100

    Update test for brand / episode title

--- src/integration/pages/onDemandRadioEpisode/crossPlatformTests.js
+++ src/integration/pages/onDemandRadioEpisode/crossPlatformTests.js
@@ -7,7 +7,7 @@ export default () => {
   runCommonCrossPlatformTests();
   runMediaPlayerEmbedTests();
 
-  it('I can see the brand title', () => {
+  it('I can see the brand and episode title', () => {
     const brandTitleEl = document.querySelector('h1[id="content"]');
 
     expect(brandTitleEl).toBeInTheDocument();
@@ -15,16 +15,8 @@ export default () => {
     expect(brandTitleEl.textContent).toMatchSnapshot();
   });
 
-  it('I can see the episode title', () => {
-    const episodeTitleEl = document.querySelectorAll('main p')[0];
-
-    expect(episodeTitleEl).toBeInTheDocument();
-    expect(episodeTitleEl.textContent).toBeTruthy();
-    expect(episodeTitleEl.textContent).toMatchSnapshot();
-  });
-
   it('I can see the episode summary', () => {
-    const episodeSummaryEl = document.querySelectorAll('main p')[1];
+    const episodeSummaryEl = document.querySelector('main p');
 
     expect(episodeSummaryEl).toBeInTheDocument();
     expect(episodeSummaryEl.textContent).toBeTruthy();
