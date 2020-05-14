c6d770d5685c16b2e7d8d95437c4cf61d0562f80
Michael McCulloch <michael.mcculloch@bbc.co.uk>
2020-05-14 09:51:20 +0100

    Move toggle check into container

--- src/app/containers/Ad/Canonical/index.jsx
+++ src/app/containers/Ad/Canonical/index.jsx
@@ -1,12 +1,4 @@
-import useToggle from '#hooks/useToggle';
-
 const CanonicalAd = () => {
-  const { enabled: adsEnabled } = useToggle('ads');
-
-  if (!adsEnabled) {
-    return null;
-  }
-
   // To be implemented in the next feature ticket: https://github.com/bbc/simorgh/issues/6182
   return null;
 };
