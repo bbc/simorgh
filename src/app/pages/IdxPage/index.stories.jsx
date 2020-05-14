73f2c1b5f88993224011511d5410cd6fc57935ac
Ho Ting Lok <hoting.lok@bbc.co.uk>
2020-05-14 16:53:34 +0100

    Updated stories and regex snapshots with renamed components

--- src/app/pages/IdxPage/index.stories.jsx
+++ src/app/pages/IdxPage/index.stories.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
-import IDXPage from '.';
+import IdxPage from '.';
 
-storiesOf('Pages|IDX Page', module)
+storiesOf('Pages|Idx Page', module)
   .addDecorator(withKnobs)
-  .add('default', () => <IDXPage />);
+  .add('default', () => <IdxPage />);
