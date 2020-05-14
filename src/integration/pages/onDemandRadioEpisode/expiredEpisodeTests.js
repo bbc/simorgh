ae27d58e115d1dcfe9297074b51cf486502cf232
Karina Thomas <karina.thomas@bbc.co.uk>
2020-05-14 19:26:10 +0100

    Moving common episode tests to a separate file and reusing for amp, canonical and expired episodes

--- src/integration/pages/onDemandRadioEpisode/expiredEpisodeTests.js
+++ src/integration/pages/onDemandRadioEpisode/expiredEpisodeTests.js
@@ -1,28 +1,8 @@
+import runCommonEpisodeTests from './commonEpisodeTests';
+
 export default () => {
   describe('Expired Episode', () => {
-    it('I can see the brand title', () => {
-      const brandTitleEl = document.querySelector('h1 span span:first-child');
-
-      expect(brandTitleEl).toBeInTheDocument();
-      expect(brandTitleEl.textContent).toBeTruthy();
-      expect(brandTitleEl.textContent).toMatchSnapshot();
-    });
-
-    it('I can see the episode title', () => {
-      const brandTitleEl = document.querySelector('h1 span span:last-child');
-
-      expect(brandTitleEl).toBeInTheDocument();
-      expect(brandTitleEl.textContent).toBeTruthy();
-      expect(brandTitleEl.textContent).toMatchSnapshot();
-    });
-
-    it('I can see the episode summary', () => {
-      const episodeSummaryEl = document.querySelector('main p');
-
-      expect(episodeSummaryEl).toBeInTheDocument();
-      expect(episodeSummaryEl.textContent).toBeTruthy();
-      expect(episodeSummaryEl.textContent).toMatchSnapshot();
-    });
+    runCommonEpisodeTests();
 
     it(`I can see the 'Content is not available' placeholder`, () => {
       const contentNotAvailableEl = document.querySelector('main div strong');
