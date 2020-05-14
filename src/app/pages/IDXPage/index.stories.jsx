1f92055be85d38968ef4c37816cbe0bd18f286b8
Olha Lyubinets <olha.lyubinets@bbc.co.uk>
2020-05-14 11:10:29 +0100

    Remove withServicesKnob from storybook

--- src/app/pages/IDXPage/index.stories.jsx
+++ src/app/pages/IDXPage/index.stories.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
-import { withServicesKnob } from '@bbc/psammead-storybook-helpers';
 import IDXPage from '.';
 
 storiesOf('Pages|IDX Page', module)
   .addDecorator(withKnobs)
-  .addDecorator(withServicesKnob())
   .add('default', () => <IDXPage />);
