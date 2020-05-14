14168d39fe3ac67dad8528e42742d99985f34a6a
Einstein <forensicslabnet@gmail.com>
2020-05-14 11:20:48 +0300

    Add assertion test

--- src/app/containers/Ad/Amp/index.test.jsx
+++ src/app/containers/Ad/Amp/index.test.jsx
@@ -3,6 +3,7 @@ import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import AmpAd, { AMP_ACCESS_FETCH } from './index';
+import { ServiceContextProvider } from '#contexts/ServiceContext';
 
 const adJsonAttributes = {
   targeting: {
@@ -11,6 +12,13 @@ const adJsonAttributes = {
     channel: 'pidgin',
   },
 };
+
+const renderAmpAd = () =>
+  render(
+    <ServiceContextProvider service="pidgin">
+      <AmpAd service="pidgin" />
+    </ServiceContextProvider>,
+  );
 
 describe('AMP Ads', () => {
   beforeAll(() => {
@@ -30,14 +38,14 @@ describe('AMP Ads', () => {
 
   describe('Assertions', () => {
     it('should render two leaderboard ads', () => {
-      const { container } = render(<AmpAd service="pidgin" />);
+      const { container } = renderAmpAd();
       const ampAd = container.querySelectorAll('amp-ad');
 
       expect(ampAd.length).toBe(2);
     });
 
     it('should display ad with values for all of the needed attributes', () => {
-      const { container } = render(<AmpAd service="pidgin" />);
+      const { container } = renderAmpAd();
 
       const ampAd = container.querySelectorAll('amp-ad');
       ampAd.forEach(ad => {
@@ -53,6 +61,14 @@ describe('AMP Ads', () => {
         expect(ad).toHaveAttribute('data-a4a-upgrade-type');
         expect(ad).toHaveAttribute('json', JSON.stringify(adJsonAttributes));
       });
+    });
+
+    it('should render an `advertisement` label', () => {
+      const { container } = renderAmpAd();
+      const p = container.querySelectorAll('p');
+
+      expect(p.length).toBeGreaterThanOrEqual(1);
+      expect(p[0].textContent).toEqual('Tori we dem pay for');
     });
 
     describe('AMP_ACCESS_FETCH', () => {
