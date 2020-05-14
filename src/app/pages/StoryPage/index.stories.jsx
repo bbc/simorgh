7e7e61aa85011a493271d2bbec9f6c842ae1ced0
Fahad Khan <fahad.khan01@bbc.co.uk>
2020-05-14 18:15:49 +0100

    revert: remove override import

--- src/app/pages/StoryPage/index.stories.jsx
+++ src/app/pages/StoryPage/index.stories.jsx
@@ -6,8 +6,6 @@ import WithTimeMachine from '#testHelpers/withTimeMachine';
 import { StoryPage } from '..';
 import mundoPageData from './fixtureData/mundo';
 import persianPageData from './fixtureData/persian';
-import topStoriesData from './topStories.json';
-import featuresAnalysisData from './featuresAnalysis.json';
 
 const styStories = storiesOf('Pages|Story Page', module);
 
@@ -33,8 +31,6 @@ styStories.addDecorator(story => <WithTimeMachine>{story()}</WithTimeMachine>);
           status={200}
           pageData={pageData}
           service={service}
-          topStoriesOverride={topStoriesData}
-          featuresAnalysisOverride={featuresAnalysisData}
           mostReadEndpointOverride="./data/mundo/mostRead/index.json"
         />
       </BrowserRouter>
