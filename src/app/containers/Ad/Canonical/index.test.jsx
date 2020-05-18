098faf60b771b6238f83eedfad32259ca658d024
Michael McCulloch <michael.mcculloch@bbc.co.uk>
2020-05-18 15:16:57 +0100

    Add canonical test for useEffect

--- src/app/containers/Ad/Canonical/index.test.jsx
+++ src/app/containers/Ad/Canonical/index.test.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
 import { BrowserRouter } from 'react-router-dom';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import '@testing-library/jest-dom/extend-expect';
@@ -12,5 +14,40 @@ describe('CanonicalAds Ads', () => {
         <CanonicalAd service="pidgin" />
       </BrowserRouter>,
     );
+  });
+
+  describe('Assertions', () => {
+    const bootstrap = jest.fn();
+
+    beforeEach(() => {
+      bootstrap.mockClear();
+      window.dotcom = {
+        bootstrap,
+        // Used in AdSlot so this is tested there
+        cmd: { push: jest.fn() },
+      };
+    });
+
+    afterEach(() => {
+      window.dotcom = undefined;
+    });
+
+    it('should call dotcom bootstrap to enable pageAds and disable playerAds', () => {
+      act(() => {
+        const container = document.createElement('div');
+        ReactDOM.render(
+          <BrowserRouter>
+            <CanonicalAd />
+          </BrowserRouter>,
+          container,
+        );
+      });
+
+      expect(bootstrap).toHaveBeenCalledTimes(1);
+      expect(bootstrap).toHaveBeenCalledWith({
+        pageAds: true,
+        playerAds: false,
+      });
+    });
   });
 });
