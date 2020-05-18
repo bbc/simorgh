9c259b1612cdd79c291951a18c653cfebc10fc1d
Michael McCulloch <michael.mcculloch@bbc.co.uk>
2020-05-18 16:35:35 +0100

    Add AdSlock assertion test

--- src/app/containers/Ad/Canonical/AdSlot.test.jsx
+++ src/app/containers/Ad/Canonical/AdSlot.test.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
 import { BrowserRouter } from 'react-router-dom';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import '@testing-library/jest-dom/extend-expect';
@@ -12,5 +14,37 @@ describe('CanonicalAds Ads', () => {
         <AdSlot type="leaderboard" />
       </BrowserRouter>,
     );
+  });
+
+  describe('Assertions', () => {
+    const push = jest.fn();
+
+    beforeEach(() => {
+      push.mockClear();
+      window.dotcom = {
+        // Used in index.jsx so this is tested there
+        bootstrap: jest.fn(),
+        cmd: { push },
+      };
+    });
+
+    afterEach(() => {
+      window.dotcom = undefined;
+    });
+
+    it('should call dotcom cmd.push with ad registration', () => {
+      act(() => {
+        const container = document.createElement('div');
+        ReactDOM.render(
+          <BrowserRouter>
+            <AdSlot type="leaderboard" />
+          </BrowserRouter>,
+          container,
+        );
+      });
+
+      expect(push).toHaveBeenCalledTimes(1);
+      expect(push).toHaveBeenCalledWith(expect.any(Function));
+    });
   });
 });
