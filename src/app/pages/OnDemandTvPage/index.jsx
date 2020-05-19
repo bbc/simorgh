3645d11d74963fd24353e7c31c64eda11ac5bf1e
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2020-05-19 17:08:24 +0100

    Adding Afrique fixture data, removing grid

--- src/app/pages/OnDemandTvPage/index.jsx
+++ src/app/pages/OnDemandTvPage/index.jsx
@@ -1,13 +1,9 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { shape, string, number } from 'prop-types';
-import { GEL_SPACING_TRPL } from '@bbc/gel-foundations/spacings';
 import MetadataContainer from '../../containers/Metadata';
-import Grid, { GelPageGrid } from '#app/components/Grid';
+import { GelPageGrid } from '#app/components/Grid';
 import { ServiceContext } from '../../contexts/ServiceContext';
-import OnDemandHeadingBlock from '#containers/RadioPageBlocks/Blocks/OnDemandHeading';
-
-const SKIP_LINK_ANCHOR_ID = 'content';
 
 const getGroups = (zero, one, two, three, four, five) => ({
   group0: zero,
@@ -23,22 +19,10 @@ const StyledGelPageGrid = styled(GelPageGrid)`
   flex-grow: 1; /* needed to ensure footer positions at bottom of viewport */
 `;
 
-const StyledGelWrapperGrid = styled(GelPageGrid)`
-  padding-top: ${GEL_SPACING_TRPL};
-`;
-
 const OnDemandTvPage = ({ pageData }) => {
-  const idAttr = SKIP_LINK_ANCHOR_ID;
-  const {
-    language,
-    brandTitle,
-    headline,
-    shortSynopsis,
-    releaseDateTimeStamp,
-  } = pageData;
+  const { language, headline, shortSynopsis } = pageData;
 
   const { dir } = useContext(ServiceContext);
-  const oppDir = dir === 'rtl' ? 'ltr' : 'rtl';
 
   return (
     <>
@@ -56,27 +40,7 @@ const OnDemandTvPage = ({ pageData }) => {
         columns={getGroups(6, 6, 6, 6, 8, 20)}
         enableGelGutters
       >
-        <Grid
-          item
-          dir={dir}
-          startOffset={getGroups(1, 1, 1, 1, 2, 5)}
-          columns={getGroups(6, 6, 6, 6, 6, 12)}
-          margins={getGroups(true, true, true, true, false, false)}
-        >
-          <StyledGelWrapperGrid
-            dir={oppDir}
-            columns={getGroups(6, 6, 6, 6, 6, 6)}
-            enableGelGutters
-          >
-            <Grid dir={dir} item columns={getGroups(6, 6, 4, 4, 4, 4)}>
-              <OnDemandHeadingBlock
-                idAttr={idAttr}
-                brandTitle={brandTitle}
-                releaseDateTimeStamp={releaseDateTimeStamp}
-              />
-            </Grid>
-          </StyledGelWrapperGrid>
-        </Grid>
+        <h1>On Demand TV</h1>
       </StyledGelPageGrid>
     </>
   );
