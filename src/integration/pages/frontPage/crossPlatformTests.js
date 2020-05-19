3d20f366c3834f9cbea96132b0f743b78c19e5ec
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2020-05-19 10:17:06 +0100

    Add Index Alsos and Useful Links integration tests

--- src/integration/pages/frontPage/crossPlatformTests.js
+++ src/integration/pages/frontPage/crossPlatformTests.js
@@ -8,4 +8,37 @@ export default () => {
 
     expect(sect).toBeInTheDocument();
   });
+
+  it('I can see Index Alsos', () => {
+    const serviceHasIndexAlsos = service === 'thai';
+
+    if (serviceHasIndexAlsos) {
+      const topStories = document.querySelector(
+        '[aria-labelledby="Top-stories"]',
+      );
+
+      if (topStories) {
+        const indexAlsos = topStories.querySelector('[data-e2e=index-alsos]');
+
+        if (indexAlsos) {
+          const h4 = indexAlsos.querySelector('h4');
+          expect(h4.textContent).toEqual('อ่านข่าวนี้เพิ่มเติม');
+        }
+      }
+    }
+  });
+
+  it('I can see Useful Links', () => {
+    const usefulLinks = document.querySelector('[data-e2e=useful-links]');
+
+    if (usefulLinks) {
+      const links = usefulLinks.querySelectorAll('a');
+
+      links.forEach(linkEl => {
+        expect(linkEl).toBeInTheDocument();
+        expect(linkEl.textContent).toBeTruthy();
+        expect(linkEl.textContent).toMatchSnapshot();
+      });
+    }
+  });
 };
