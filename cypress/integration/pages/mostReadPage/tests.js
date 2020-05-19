c8f7269ed36b9eb070806839e82c62f248f1470a
thekp <khoa.phan@bbc.co.uk>
2020-05-19 11:08:45 +0100

    remove test for url in most read page

--- cypress/integration/pages/mostReadPage/tests.js
+++ cypress/integration/pages/mostReadPage/tests.js
@@ -6,31 +6,13 @@ export const testsThatAlwaysRun = ({ service, pageType }) => {
 export const testsThatFollowSmokeTestConfig = ({ service, pageType }) => {
   describe(`No testsThatFollowSmokeTestConfig to run for ${service} ${pageType}`, () => {
     describe(`Tests for ${service} ${pageType}`, () => {
-      it('should have a visually hidden top-level header', () => {
+      it('should have a top-level header', () => {
         cy.get('h1').should('have.length', 1);
       });
 
       it('should have mostread component rendered ', () => {
         cy.get('main').should('have.attr', 'data-e2e', 'most-read');
       });
-      if (Cypress.env('APP_ENV') !== 'local') {
-        it('should have articles list each linking to article page', () => {
-          cy.get('main ol[role="list"]>li')
-            .eq(0)
-            .within(() => {
-              cy.get('a')
-                .should('have.attr', 'href')
-                .then(href => {
-                  cy.request({
-                    url: href,
-                    failOnStatusCode: false,
-                  }).then(resp => {
-                    expect(resp.status).to.not.equal(404);
-                  });
-                });
-            });
-        });
-      }
     });
   });
 };
