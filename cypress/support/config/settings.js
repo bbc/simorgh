ea3a82e0adf207f4d2ffcc5e0a45ed5160a3aee5
Karina Thomas <58214768+karinathomasbbc@users.noreply.github.com>
2020-05-20 21:26:48 +0100

    Revert local indonesian on demand radio episode ID

--- cypress/support/config/settings.js
+++ cypress/support/config/settings.js
@@ -2034,7 +2034,7 @@ module.exports = () => ({
             enabled: true,
           },
           local: {
-            paths: ['/indonesia/bbc_indonesian_radio/w172x6r5000f38s'],
+            paths: ['/indonesia/bbc_indonesian_radio/w172xh267fpn19l'],
             enabled: true,
           },
         },
