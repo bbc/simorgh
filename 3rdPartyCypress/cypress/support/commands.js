78f114a783591cf5c9bbb9c23c8b2adb906e26f3
James Donohue <james.donohue@bbc.co.uk>
2020-05-20 15:58:27 +0100

    Revert "Remove mozart fallback checks from E2Es"
    This reverts commit 7345572a98050bd0a78bcf59bf4c8ddadf70ad21.

--- 3rdPartyCypress/cypress/support/commands.js
+++ 3rdPartyCypress/cypress/support/commands.js
@@ -7,5 +7,10 @@ Cypress.Commands.add('testResponseCodeAndType', (path, responseCode, type) => {
   }).then(({ status, headers }) => {
     expect(status).to.eq(responseCode);
     expect(headers['content-type']).to.include(type);
+    // Always ensure we're not seeing the Mozart fallback
+    expect(
+      headers,
+      `Mozart fallback response detected for ${path}`,
+    ).not.to.have.property('x-mfa');
   });
 });
