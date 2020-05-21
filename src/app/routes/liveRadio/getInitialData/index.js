962839b0eb9b8b57469a8a2f32d1d5b762da31ba
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-05-21 14:58:57 +0100

    refactor - remove live radio block mapping logic

--- src/app/routes/liveRadio/getInitialData/index.js
+++ src/app/routes/liveRadio/getInitialData/index.js
@@ -1,6 +1,5 @@
 import path from 'ramda/src/path';
 import fetchPageData from '../../utils/fetchPageData';
-import addIdsToBlocks from './addIdsToBlocks';
 import overrideRendererOnTest from '../../utils/overrideRendererOnTest';
 
 const getLanguage = path(['metadata', 'language']);
@@ -15,18 +14,23 @@ const getPageIdentifier = path([
   'pageIdentifier',
 ]);
 
+const getHeading = path(['content', 'blocks', 0, 'text']);
+const getBodySummary = path(['content', 'blocks', 1, 'text']);
+const getMasterBrand = path(['content', 'blocks', 2, 'externalId']);
+
 export default async ({ path: pathname }) => {
   const liveRadioDataPath = overrideRendererOnTest(pathname);
   const { json, ...rest } = await fetchPageData(liveRadioDataPath);
-  const contentData = path(['content'], json);
   const pageType = { metadata: { type: 'Live Radio' } };
 
   return {
     ...rest,
     ...(json && {
       pageData: {
-        content: addIdsToBlocks(contentData),
+        heading: getHeading(json),
+        bodySummary: getBodySummary(json),
         language: getLanguage(json),
+        masterBrand: getMasterBrand(json),
         id: getMetaDataId(json),
         name: getPromoName(json),
         summary: getPromoSummary(json),
