48a3becf5a4afc8e58645a06ca1fdbed65e8681d
RichardPK <r.phillips.kerr@gmail.com>
2020-05-21 16:17:24 +0100

    Fixed amp img queryselector

--- src/integration/pages/onDemandRadioEpisode/ampOnlyEpisodeTests.js
+++ src/integration/pages/onDemandRadioEpisode/ampOnlyEpisodeTests.js
@@ -1,6 +1,6 @@
 export default () => {
   it('I can see the hero image', () => {
-    const imageEl = document.querySelector('main div amp-img img');
+    const imageEl = document.querySelector('main amp-img');
 
     expect(imageEl).toBeInTheDocument();
     expect(imageEl).toBeTruthy();
