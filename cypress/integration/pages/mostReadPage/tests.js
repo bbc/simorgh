8cf7dea20e424b8609c1f57dbec362073a4aa24f
thekp <khoa.phan@bbc.co.uk>
2020-05-21 13:59:08 +0100

    remove tests that are already in integration tests

--- cypress/integration/pages/mostReadPage/tests.js
+++ cypress/integration/pages/mostReadPage/tests.js
@@ -4,17 +4,7 @@ export const testsThatAlwaysRun = ({ service, pageType }) => {
 
 // For testing features that may differ across services but share a common logic e.g. translated strings.
 export const testsThatFollowSmokeTestConfig = ({ service, pageType }) => {
-  describe(`No testsThatFollowSmokeTestConfig to run for ${service} ${pageType}`, () => {
-    describe(`Tests for ${service} ${pageType}`, () => {
-      it('should have a top-level header', () => {
-        cy.get('h1').should('have.length', 1);
-      });
-
-      it('should have mostread component rendered ', () => {
-        cy.get('main').should('have.attr', 'data-e2e', 'most-read');
-      });
-    });
-  });
+  describe(`No testsThatFollowSmokeTestConfig to run for ${service} ${pageType}`, () => {});
 };
 
 // For testing low priority things e.g. cosmetic differences, and a safe place to put slow tests.
