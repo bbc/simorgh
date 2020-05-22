e592f6e62aced28892e0a10815b59e3986d4d582
LilyL0u <lily.higham@bbc.co.uk>
2020-05-22 11:26:11 +0100

    override

--- cypress/integration/pages/onDemandRadio/tests.js
+++ cypress/integration/pages/onDemandRadio/tests.js
@@ -9,14 +9,14 @@ export const testsThatFollowSmokeTestConfig = ({ service, pageType }) =>
   describe(`Tests for ${service} ${pageType}`, () => {
     describe('Brand image visible above 400px, not visible below 400px', () => {
       it(`Should display image on default viewport (1000x660))`, () => {
-        cy.request(`${Cypress.env('currentPath')}.json`);
+        cy.request(`${Cypress.env('currentPath')}.json?renderer_env=live`);
         cy.get('div[class^="ImageContainer"]').find('img');
       });
 
       it(`Should not display image on iphone-6 screen (375x667)`, () => {
         cy.viewport('iphone-6');
 
-        cy.request(`${Cypress.env('currentPath')}.json`);
+        cy.request(`${Cypress.env('currentPath')}.json?renderer_env=live`);
         cy.get('div[class^="ImageContainer"]')
           .find('img')
           .should('not.be.visible');
