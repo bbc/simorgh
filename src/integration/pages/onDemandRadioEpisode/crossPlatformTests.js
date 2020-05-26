0cb98a1f7e837e0af1909a317f378024165c269c
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-05-26 12:03:19 +0100

    run tests for expired

--- src/integration/pages/onDemandRadioEpisode/crossPlatformTests.js
+++ src/integration/pages/onDemandRadioEpisode/crossPlatformTests.js
@@ -6,6 +6,42 @@ import {
 export default () => {
   // runCommonCrossPlatformTests(); linked data missing
 
+  it('I can see the brand title', () => {
+    const brandTitleEl = document.querySelector('h1 span span:first-child');
+
+    expect(brandTitleEl).toBeInTheDocument();
+    expect(brandTitleEl.textContent).toBeTruthy();
+    expect(brandTitleEl.textContent).toMatchSnapshot();
+  });
+
+  it('I can see the episode title', () => {
+    const episodeTitleEl = document.querySelector('h1 span span:last-child');
+
+    expect(episodeTitleEl).toBeInTheDocument();
+    expect(episodeTitleEl.textContent).toBeTruthy();
+    expect(episodeTitleEl.textContent).toMatchSnapshot();
+  });
+
+  it('I can see the episode summary', () => {
+    const episodeSummaryEl = document.querySelector('main p');
+
+    expect(episodeSummaryEl).toBeInTheDocument();
+    expect(episodeSummaryEl.textContent).toBeTruthy();
+    expect(episodeSummaryEl.textContent).toMatchSnapshot();
+  });
+
+  describe('a11y', () => {
+    it('Assistive technology reads the brand and episode title as the headline', () => {
+      const headlineEl = document.querySelector(
+        'h1[id="content"][tabindex="-1"]',
+      );
+
+      expect(headlineEl).toBeInTheDocument();
+      expect(headlineEl.textContent).toBeTruthy();
+      expect(headlineEl.textContent).toMatchSnapshot();
+    });
+  });
+
   if (global.isExpired) {
     describe('Expired Episode', () => {
       it(`I can see the 'Content is not available' placeholder`, () => {
@@ -18,41 +54,5 @@ export default () => {
     });
   } else {
     runMediaPlayerEmbedTests();
-
-    it('I can see the brand title', () => {
-      const brandTitleEl = document.querySelector('h1 span span:first-child');
-
-      expect(brandTitleEl).toBeInTheDocument();
-      expect(brandTitleEl.textContent).toBeTruthy();
-      expect(brandTitleEl.textContent).toMatchSnapshot();
-    });
-
-    it('I can see the episode title', () => {
-      const episodeTitleEl = document.querySelector('h1 span span:last-child');
-
-      expect(episodeTitleEl).toBeInTheDocument();
-      expect(episodeTitleEl.textContent).toBeTruthy();
-      expect(episodeTitleEl.textContent).toMatchSnapshot();
-    });
-
-    it('I can see the episode summary', () => {
-      const episodeSummaryEl = document.querySelector('main p');
-
-      expect(episodeSummaryEl).toBeInTheDocument();
-      expect(episodeSummaryEl.textContent).toBeTruthy();
-      expect(episodeSummaryEl.textContent).toMatchSnapshot();
-    });
-
-    describe('a11y', () => {
-      it('Assistive technology reads the brand and episode title as the headline', () => {
-        const headlineEl = document.querySelector(
-          'h1[id="content"][tabindex="-1"]',
-        );
-
-        expect(headlineEl).toBeInTheDocument();
-        expect(headlineEl.textContent).toBeTruthy();
-        expect(headlineEl.textContent).toMatchSnapshot();
-      });
-    });
   }
 };
