fbc338b094efcd2075ffb6e18571387556ae9c2d
Einstein <forensicslabnet@gmail.com>
2020-05-27 16:09:59 +0300

    Update snapshots

--- src/app/pages/IdxPage/index.test.jsx
+++ src/app/pages/IdxPage/index.test.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import { act, render } from '@testing-library/react';
 import IdxPage from '.';
+import { ServiceContextProvider } from '#contexts/ServiceContext';
+import { ToggleContextProvider } from '#contexts/ToggleContext';
+import { RequestContextProvider } from '#contexts/RequestContext';
+import persianMostReadData from '#data/persian/mostRead';
+
+const IDXPageWithContext = () => (
+  <ToggleContextProvider service="pidgin" origin="https://www.test.bbc.com">
+    <RequestContextProvider
+      pageType="IDX"
+      service="persian"
+      pathname="/pathname"
+      data={{ status: 200 }}
+      isAmp={false}
+    >
+      <ServiceContextProvider service="persian">
+        <IdxPage />
+      </ServiceContextProvider>
+    </RequestContextProvider>
+  </ToggleContextProvider>
+);
 
 describe('IdxPage', () => {
+  beforeEach(async () => {
+    fetch.mockResponse(JSON.stringify(persianMostReadData));
+  });
+
   describe('Snapshots', () => {
-    shouldMatchSnapshot(
-      'should correctly render a blank page with HOC components',
-      <IdxPage />,
-    );
+    it('should render a persian idx page correctly with most read', async () => {
+      let container = null;
+      await act(async () => {
+        container = render(<IDXPageWithContext />).container;
+      });
+
+      expect(container).toMatchSnapshot();
+    });
   });
 });
