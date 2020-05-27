48d0b22235a2213ff5d1123c564de54993bc7d8c
LilyL0u <lily.higham@bbc.co.uk>
2020-05-27 15:23:35 +0100

    removed unecessary json requests in viewport test

--- cypress/integration/pages/onDemandRadio/tests.js
+++ cypress/integration/pages/onDemandRadio/tests.js
@@ -8,21 +8,13 @@ export const testsThatAlwaysRun = ({ service, pageType }) => {
 export const testsThatFollowSmokeTestConfig = ({ service, pageType }) =>
   describe(`Tests for ${service} ${pageType}`, () => {
     describe('Brand image visible above 400px, not visible below 400px', () => {
-      let override;
       it(`Should display image on default viewport (1000x660))`, () => {
-        if (Cypress.env('APP_ENV')) {
-          override = '?renderer_env=live';
-        } else {
-          override = '';
-        }
-        cy.request(`${Cypress.env('currentPath')}.json${override}`);
         cy.get('div[class^="ImageContainer"]').find('img');
       });
 
       it(`Should not display image on iphone-6 screen (375x667)`, () => {
         cy.viewport('iphone-6');
 
-        cy.request(`${Cypress.env('currentPath')}.json${override}`);
         cy.get('div[class^="ImageContainer"]')
           .find('img')
           .should('not.be.visible');
