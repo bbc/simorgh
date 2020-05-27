d17a6b385f588d0c95a1786a6adac2ac33ffd7b2
RichardPK <r.phillips.kerr@gmail.com>
2020-05-27 11:56:46 +0100

    Moved amp/canonical specific tests into common & updated snapshots

--- src/integration/pages/onDemandRadioEpisode/ampTests.js
+++ src/integration/pages/onDemandRadioEpisode/ampTests.js
@@ -3,11 +3,4 @@ import { runCoreAmpTests, runAmpAnalyticsTests } from '../../common';
 export default () => {
   runCoreAmpTests();
   runAmpAnalyticsTests();
-  it('I can see the hero image on amp', () => {
-    const imageEl = document.querySelector('main amp-img');
-
-    expect(imageEl).toBeInTheDocument();
-    expect(imageEl).toBeTruthy();
-    expect(imageEl.getAttribute('src')).toMatchSnapshot();
-  });
 };
--- src/integration/pages/onDemandRadioEpisode/canonicalTests.js
+++ src/integration/pages/onDemandRadioEpisode/canonicalTests.js
@@ -6,11 +6,4 @@ import {
 export default () => {
   runCoreCanonicalTests();
   runCanonicalAnalyticsTests();
-  it('I can see the hero image on canonical', () => {
-    const imageEl = document.querySelector('main img');
-
-    expect(imageEl).toBeInTheDocument();
-    expect(imageEl).toBeTruthy();
-    expect(imageEl.getAttribute('src')).toMatchSnapshot();
-  });
 };
