72bc9f046853886a8e20c82f17935181609d9318
Ruth Ogendi <ruth.ogendi@andela.com>
2020-05-28 15:19:21 +0300

    Add getInitialData test

--- src/app/routes/idx/getInitialData/index.test.js
+++ src/app/routes/idx/getInitialData/index.test.js
@@ -1,13 +1,19 @@
 import getInitialData from '.';
 import idxPageJson from '#data/persian/afghanistan/index.json';
 
-jest.mock('../../utils/getConfig', () => jest.fn());
+fetch.mockResponse(JSON.stringify(idxPageJson));
 
 describe('Get intial data from IDX page', () => {
-  beforeEach(() => {
-    process.env.SIMORGH_BASE_URL = 'http://localhost';
-    fetchMock.restore();
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
+  it('should return essential data for an IDX page to render', async () => {
+    const { pageData } = await getInitialData({
+      path: 'mock-idx-page',
+    });
 
+    expect(pageData.metadata.type).toEqual('IDX');
+    expect(pageData.content.groups.length).toBeGreaterThan(1);
+  });
 });
