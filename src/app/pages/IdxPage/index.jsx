30e122b2ed414d027724b54a6a3eaa25286739a4
Richard Itimi <itimirichard@gmail.com>
2020-05-28 09:51:05 +0100

    import metadatacontainer and context

--- src/app/pages/IdxPage/index.jsx
+++ src/app/pages/IdxPage/index.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ServiceContext } from '#contexts/ServiceContext';
+// import MetadataContainer from '#containers/Metadata';
 
 const IdxPage = () => {
+  const { brandName, service, script, dir, lang, ...others } = useContext(
+    ServiceContext,
+  );
+  console.log({ brandName, service, script, dir, lang, others });
+
   return (
-    <main role="main">
-      <h1 id="content">IDX Page</h1>
-    </main>
+    <>
+      {/* <MetadataContainer
+        title={header}
+        lang={lang}
+        description={`${header} - ${brandName}`}
+        openGraphType="website"
+      /> */}
+      <main role="main">
+        <h1 id="content">IDX Page</h1>
+      </main>
+    </>
   );
 };
 
