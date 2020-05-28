9ddb2a89c2764aeb486628195a11400e5a47f5bc
Ruth Ogendi <ruth.ogendi@andela.com>
2020-05-28 15:40:57 +0300

    Update IdxPage test

--- src/app/pages/IdxPage/index.test.jsx
+++ src/app/pages/IdxPage/index.test.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import { render } from '@testing-library/react';
 import IdxPage from '.';
+import persianAfghanistanIdxData from '#data/persian/afghanistan';
+import { RequestContextProvider } from '#contexts/RequestContext';
+import { ServiceContextProvider } from '#contexts/ServiceContext';
+
+const requestContextData = {
+  isAmp: false,
+  pageType: 'IDX',
+  service: 'persian',
+  pathname: '/pathname',
+  data: { status: 200 },
+};
+
+const IdxPageWithContext = () => (
+  <RequestContextProvider {...requestContextData}>
+    <ServiceContextProvider service="pidgin">
+      <IdxPage pageData={persianAfghanistanIdxData} />
+    </ServiceContextProvider>
+  </RequestContextProvider>
+);
 
 describe('IdxPage', () => {
   describe('Snapshots', () => {
-    shouldMatchSnapshot(
-      'should correctly render a blank page with HOC components',
-      <IdxPage />,
-    );
+    it('should render an IDX page correctly', () => {
+      const container = render(<IdxPageWithContext />);
+      expect(container).toMatchSnapshot();
+    });
   });
 });
