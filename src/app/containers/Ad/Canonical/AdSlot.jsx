8b53359e038d565602c3101f7904c7c2e9fda3bf
Michael McCulloch <michael.mcculloch@bbc.co.uk>
2020-05-28 15:58:03 +0100

    Boostrap GNL Earlier

--- src/app/containers/Ad/Canonical/AdSlot.jsx
+++ src/app/containers/Ad/Canonical/AdSlot.jsx
@@ -1,25 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { string } from 'prop-types';
-import { Helmet } from 'react-helmet';
 
-const AdSlot = ({ uniqueId }) => (
-  <>
-    <Helmet
-      script={[
-        {
-          type: 'text/javascript',
-          // Once the Ad script has loaded, ads pushed to `cmd` are rendered
-          innerHTML: `
-            window.dotcom.cmd.push(() => {
-              window.dotcom.ads.registerSlot('${uniqueId}');
-            });
-          `,
-        },
-      ]}
-    />
-    <div id={`dotcom-${uniqueId}`} className="dotcom-ad" />
-  </>
-);
+const AdSlot = ({ uniqueId }) => {
+  useEffect(() => {
+    window.dotcom.cmd.push(() => {
+      window.dotcom.ads.registerSlot(uniqueId);
+    });
+  }, [uniqueId]);
+
+  return <div id={`dotcom-${uniqueId}`} className="dotcom-ad" />;
+};
 
 AdSlot.propTypes = {
   uniqueId: string.isRequired,
