d4c1990e06f1cc293f63f589e6af59cfa428d39b
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2020-05-28 20:54:27 +0100

    Adding integration tests for TV

---
+++ src/integration/pages/onDemandTVPage/pashto/amp.test.js
@@ -0,0 +1,14 @@
+/**
+ * @service pashto
+ * @pathname /pashto/bbc_pashto_tv/w13xttn4
+ */
+
+import runCrossPlatformTests from '../crossPlatformTests';
+import runAmpTests from '../ampTests';
+
+describe('AMP', () => {
+  describe(pageType, () => {
+    runCrossPlatformTests();
+    runAmpTests();
+  });
+});
---
+++ src/integration/pages/onDemandTVPage/pashto/canonical.test.js
@@ -0,0 +1,14 @@
+/**
+ * @service pashto
+ * @pathname /pashto/bbc_pashto_tv/w13xttn4
+ */
+
+import runCrossPlatformTests from '../crossPlatformTests';
+import runCanonicalTests from '../canonicalTests';
+
+describe('Canonical', () => {
+  describe(pageType, () => {
+    runCrossPlatformTests();
+    runCanonicalTests();
+  });
+});
