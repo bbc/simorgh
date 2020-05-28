c7a1f6a898d3e4fb41078f81ef2e031de2d0a2bf
Michael McCulloch <michael.mcculloch@bbc.co.uk>
2020-05-28 13:47:54 +0100

    Fix ad race conditions with inline script

--- src/app/containers/Ad/Canonical/AdSlot.jsx
+++ src/app/containers/Ad/Canonical/AdSlot.jsx
@@ -1,15 +1,25 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { string } from 'prop-types';
+import { Helmet } from 'react-helmet';
 
-const AdSlot = ({ uniqueId }) => {
-  useEffect(() => {
-    window.dotcom.cmd.push(() => {
-      window.dotcom.ads.registerSlot(uniqueId);
-    });
-  }, [uniqueId]);
-
-  return <div id={`dotcom-${uniqueId}`} className="dotcom-ad" />;
-};
+const AdSlot = ({ uniqueId }) => (
+  <>
+    <Helmet
+      script={[
+        {
+          type: 'text/javascript',
+          // Once the Ad script has loaded, ads pushed to `cmd` are rendered
+          innerHTML: `
+            window.dotcom.cmd.push(() => {
+              window.dotcom.ads.registerSlot('${uniqueId}');
+            });
+          `,
+        },
+      ]}
+    />
+    <div id={`dotcom-${uniqueId}`} className="dotcom-ad" />
+  </>
+);
 
 AdSlot.propTypes = {
   uniqueId: string.isRequired,
