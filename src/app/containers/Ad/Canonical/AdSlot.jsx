42a39d202712c2d88ee57e9bbf3d87ea937ccf3b
Michael McCulloch <michael.mcculloch@bbc.co.uk>
2020-05-29 10:46:02 +0100

    Check if adding location solves the issue

--- src/app/containers/Ad/Canonical/AdSlot.jsx
+++ src/app/containers/Ad/Canonical/AdSlot.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
 import { string } from 'prop-types';
+import { useLocation } from 'react-router-dom';
 
 const AdSlot = ({ uniqueId }) => {
+  const location = useLocation();
   useEffect(() => {
     window.dotcom.cmd.push(() => {
       window.dotcom.ads.registerSlot(uniqueId);
     });
-  }, [uniqueId]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
 
   return <div id={`dotcom-${uniqueId}`} className="dotcom-ad" />;
 };
