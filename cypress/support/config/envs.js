2186ca6157aaf0dd0ae83414602b5bee727d0206
Karina Thomas <karina.thomas@bbc.co.uk>
2020-06-01 18:29:25 +0100

    Revert "Disable chartbeat locally as it is failing for on demand radio"
    This reverts commit 716f53bfef95ff8ae5f391e4e559226e7210359d.

--- cypress/support/config/envs.js
+++ cypress/support/config/envs.js
@@ -27,7 +27,7 @@ const config = {
     assetUrl: 'http://localhost:7080',
     assetOrigin: 'http://localhost:7080',
     atiUrl: 'https://logws1363.ati-host.net?',
-    chartbeatEnabled: false,
+    chartbeatEnabled: true,
     avEmbedBaseUrl: 'https://polling.test.bbc.co.uk',
     standaloneErrorPages: true,
     alwaysCheckForFallback: false,
