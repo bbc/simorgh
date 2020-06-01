e83b7f0b656f88f42e543f1544f0c10f029418db
Olha Lyubinets <olha.lyubinets@bbc.co.uk>
2020-06-01 08:52:09 +0100

    Add radio schedule to idx page

--- src/app/pages/IdxPage/index.jsx
+++ src/app/pages/IdxPage/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import pathOr from 'ramda/src/path';
+import { ServiceContext } from '#contexts/ServiceContext';
+import RadioScheduleContainer from '#containers/RadioSchedule';
 
-const IdxPage = () => {
+const IdxPage = ({ pageData }) => {
+  const { idxPage } = useContext(ServiceContext);
+
+  const radioScheduleData = pathOr(['radioScheduleData'], pageData);
+  const radioScheduleOnIdxPage = pathOr(['hasRadioSchedule'], idxPage);
+  const radioScheduleIdxPosition = pathOr(['idxPagePosition'], idxPage);
+  const idxRadioScheduleOverride =
+    './data/persian/bbc_dari_radio/schedule.json';
+
   return (
     <main role="main">
       <h1 id="content">IDX Page</h1>
+      {radioScheduleOnIdxPage && (
+        <RadioScheduleContainer
+          initialData={radioScheduleData}
+          radioScheduleEndpointOverride={idxRadioScheduleOverride}
+        />
+      )}
     </main>
   );
 };
