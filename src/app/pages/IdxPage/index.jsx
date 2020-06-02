65b1bab4aa7dfc61bb65ebe2666d1807f6a8be75
Richard Itimi <itimirichard@gmail.com>
2020-06-02 23:37:05 +0100

    add LinkedData to IDX

--- src/app/pages/IdxPage/index.jsx
+++ src/app/pages/IdxPage/index.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import path from 'ramda/src/path';
 import PageContainer from '#lib/pageStyles/PageContainer';
 import MetadataContainer from '#containers/Metadata';
+import LinkedData from '#containers/LinkedData';
 import FrontPageSection from '#containers/FrontPageSection';
 import idxPageDataPropTypes from '#models/propTypes/idxPage';
 
@@ -10,6 +11,7 @@ const IdxPage = ({ pageData }) => {
   const summary = path(['metadata', 'summary'], pageData);
   const title = path(['metadata', 'title'], pageData);
   const lang = path(['metadata', 'language'], pageData);
+  const seoTitle = path(['promo', 'name'], pageData);
 
   return (
     <>
@@ -19,6 +21,7 @@ const IdxPage = ({ pageData }) => {
         description={summary}
         openGraphType="website"
       />
+      <LinkedData type="WebPage" seoTitle={seoTitle} />
       <main role="main">
         <h1 id="content">IDX Page</h1>
         <PageContainer>
