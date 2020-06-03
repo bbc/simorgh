c610e8517e27816b44529f3ef1c241e6fb847cf1
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2020-06-03 12:51:14 +0100

    Update heading paddings

--- src/app/containers/IndexHeading/index.jsx
+++ src/app/containers/IndexHeading/index.jsx
@@ -8,20 +8,18 @@ import {
   GEL_GROUP_4_SCREEN_WIDTH_MIN,
   GEL_GROUP_5_SCREEN_WIDTH_MIN,
 } from '@bbc/gel-foundations/breakpoints';
-import {
-  GEL_SPACING,
-  GEL_SPACING_DBL,
-  GEL_SPACING_TRPL,
-} from '@bbc/gel-foundations/spacings';
+import { GEL_SPACING, GEL_SPACING_TRPL } from '@bbc/gel-foundations/spacings';
 import { ServiceContext } from '#contexts/ServiceContext';
 
 const StyledIndexHeading = styled(IndexHeading)`
-  padding: ${GEL_SPACING_DBL} 0 ${GEL_SPACING_TRPL};
+  padding-bottom: ${GEL_SPACING_TRPL};
+
   @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
-    padding: ${GEL_SPACING_TRPL} 0 ${GEL_SPACING_DBL};
-  }
-  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
     padding: ${GEL_SPACING_TRPL} 0 ${GEL_SPACING};
+  }
+
+  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
+    padding: ${GEL_SPACING_TRPL} 0 0;
   }
 
   @media (min-width: ${GEL_GROUP_5_SCREEN_WIDTH_MIN}) {
