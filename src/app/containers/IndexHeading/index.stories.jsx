0fc0fb8970cb2a28b78dd03675b0c9228663c36d
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2020-06-03 16:35:07 +0100

    Add service knob to IndexHeading stories

--- src/app/containers/IndexHeading/index.stories.jsx
+++ src/app/containers/IndexHeading/index.stories.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { cyrillicAndLatin } from '@bbc/gel-foundations/scripts';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withServicesKnob } from '@bbc/psammead-storybook-helpers';
 import IndexHeadingContainer from '.';
 
 storiesOf('Containers|Index Heading', module)
   .addParameters({ chromatic: { disable: true } })
-  .add('default', () => (
-    <IndexHeadingContainer script={cyrillicAndLatin} service="ukrainian">
+  .addDecorator(withKnobs)
+  .addDecorator(withServicesKnob())
+  .add('default', ({ script, service }) => (
+    <IndexHeadingContainer script={script} service={service}>
       Index Heading
     </IndexHeadingContainer>
   ));
