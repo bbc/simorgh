68786c6026702b46188305db0fbc6322434f2993
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2020-06-03 20:08:17 +0100

    Remove ServiceContext from IDX page

--- src/app/pages/IdxPage/index.jsx
+++ src/app/pages/IdxPage/index.jsx
@@ -1,22 +1,18 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment } from 'react';
 import path from 'ramda/src/path';
 import IndexHeading from '#containers/IndexHeading';
 import IndexPageContainer from '#app/components/PageLayout/IndexPageContainer';
 import FrontPageSection from '#containers/FrontPageSection';
 import idxPageDataPropTypes from '#models/propTypes/idxPage';
-import { ServiceContext } from '#contexts/ServiceContext';
 
 const IdxPage = ({ pageData }) => {
-  const { script, service, dir } = useContext(ServiceContext);
   const groups = path(['content', 'groups'], pageData);
   const heading = path(['metadata', 'title'], pageData);
 
   return (
     <main role="main">
       <IndexPageContainer>
-        <IndexHeading id="content" script={script} service={service} dir={dir}>
-          {heading}
-        </IndexHeading>
+        <IndexHeading id="content">{heading}</IndexHeading>
         {groups.map((group, index) => (
           <Fragment key={group.title}>
             <FrontPageSection group={group} sectionNumber={index} />
