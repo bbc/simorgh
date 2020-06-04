73df96698add1c1e38c20928c604f759b6122eca
Ruth Ogendi <ruth.ogendi@andela.com>
2020-06-04 21:11:50 +0300

    Update unit tests.

--- src/app/pages/IdxPage/index.test.jsx
+++ src/app/pages/IdxPage/index.test.jsx
@@ -4,32 +4,34 @@ import persianMostReadData from '#data/persian/mostRead';
 import IdxPageWithContext from './testHelpers';
 import persianAfghanistanIdxData from '#data/persian/afghanistan';
 
+jest.mock('#containers/ChartbeatAnalytics', () => {
+  return () => <div>chartbeat</div>;
+});
+
+let container;
+
 describe('IdxPage', () => {
   beforeEach(async () => {
     fetch.mockResponse(JSON.stringify(persianMostReadData));
+
+    container = null;
+    await act(async () => {
+      container = render(
+        <IdxPageWithContext pageData={persianAfghanistanIdxData} />,
+      ).container;
+    });
   });
 
   describe('Snapshots', () => {
     it('should render a persian idx page correctly with most read', async () => {
-      let container = null;
-      await act(async () => {
-        container = render(
-          <IdxPageWithContext pageData={persianAfghanistanIdxData} />,
-        ).container;
-      });
-
       expect(container).toMatchSnapshot();
     });
   });
 
   describe('Assertions', () => {
     it('should render idx page sections', async () => {
-      const { container } = render(
-        <IdxPageWithContext pageData={persianAfghanistanIdxData} />,
-      );
-
       const sections = container.querySelectorAll('section');
-      expect(sections).toHaveLength(2);
+      expect(sections).toHaveLength(3);
       sections.forEach(section => {
         expect(section.getAttribute('role')).toEqual('region');
       });
