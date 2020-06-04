bd345bf0fa71460a1b01033394e213778b8e38c4
Ruth Ogendi <ruth.ogendi@andela.com>
2020-06-04 10:28:48 +0300

    Add ATI and Chartbeat to IDX page

--- src/app/pages/IdxPage/index.jsx
+++ src/app/pages/IdxPage/index.jsx
@@ -3,21 +3,27 @@ import path from 'ramda/src/path';
 import PageContainer from '#lib/pageStyles/PageContainer';
 import FrontPageSection from '#containers/FrontPageSection';
 import idxPageDataPropTypes from '#models/propTypes/idxPage';
+import ATIAnalytics from '#containers/ATIAnalytics';
+import ChartbeatAnalytics from '#containers/ChartbeatAnalytics';
 
 const IdxPage = ({ pageData }) => {
   const groups = path(['content', 'groups'], pageData);
 
   return (
-    <main role="main">
-      <h1 id="content">IDX Page</h1>
-      <PageContainer>
-        {groups.map((group, index) => (
-          <Fragment key={group.title}>
-            <FrontPageSection group={group} sectionNumber={index} />
-          </Fragment>
-        ))}
-      </PageContainer>
-    </main>
+    <>
+      <ATIAnalytics data={pageData} />
+      <ChartbeatAnalytics data={pageData} />
+      <main role="main">
+        <h1 id="content">IDX Page</h1>
+        <PageContainer>
+          {groups.map((group, index) => (
+            <Fragment key={group.title}>
+              <FrontPageSection group={group} sectionNumber={index} />
+            </Fragment>
+          ))}
+        </PageContainer>
+      </main>
+    </>
   );
 };
 
