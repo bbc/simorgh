607a611d289306d208394a693fec12ad2a7253f2
Michael McCulloch <michael.mcculloch@bbc.co.uk>
2020-06-05 15:23:12 +0100

    Add comment

--- src/app/containers/Ad/Canonical/index.jsx
+++ src/app/containers/Ad/Canonical/index.jsx
@@ -4,6 +4,8 @@ import AdSlot from './AdSlot';
 
 const CanonicalAd = () => (
   <>
+    {/* Loading dotcom-bootstrap.js here instead of CanonicalAdBootstrapJs to avoid it loading on live */}
+    {/* This can be moved once we allow the script to load on live */}
     <Helmet>
       <script src="https://gn-web-assets.api.bbc.com/ngas/test/dotcom-bootstrap.js" />
     </Helmet>
