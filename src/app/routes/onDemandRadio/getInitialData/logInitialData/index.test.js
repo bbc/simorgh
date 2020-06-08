87eeed7d525eefd89f2e8296d33ab446ec158d4a
RichardPK <r.phillips.kerr@gmail.com>
2020-06-08 15:38:05 +0100

    Drafted logging test

--- src/app/routes/onDemandRadio/getInitialData/logInitialData/index.test.js
+++ src/app/routes/onDemandRadio/getInitialData/logInitialData/index.test.js
@@ -1,5 +1,9 @@
 import path from 'ramda/src/path';
+import assocPath from 'ramda/src/assocPath';
+import loggerMock from '#testHelpers/loggerMock';
 import { parsePageIdentifier } from '.';
+import { RADIO_EPISODE_EXPIRED } from '#lib/logger.const';
+import getInitialData from '..';
 import onDemandRadioEpisodeJson from '#data/pashto/bbc_pashto_radio/w3ct0lz1';
 import onDemandRadioBrandJson from '#data/indonesia/bbc_indonesian_radio/w13xtt0s';
 
@@ -39,5 +43,18 @@ describe('Logging get initial data', () => {
     const pid = path(['promo', 'locators', 'brandPid'], onDemandRadioBrandJson);
 
     expect(url).toEqual(`${service}/${masterBrand}/programmes/${pid}`);
+  });
+
+  it('logs the correct message when the media asset is revoked', async () => {
+    const responseWithoutVersions = assocPath(
+      ['content', 'blocks', 0, 'versions'],
+      [],
+      onDemandRadioEpisodeJson,
+    );
+    fetch.mockResponse(JSON.stringify(responseWithoutVersions));
+    await getInitialData('mock-on-demand-radio-path');
+    expect(loggerMock.info).toHaveBeenCalledWith(RADIO_EPISODE_EXPIRED, {
+      url: 'pashto/bbc_pashto_radio/w3ct0lz1',
+    });
   });
 });
