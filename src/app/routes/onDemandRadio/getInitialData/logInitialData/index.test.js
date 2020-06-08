245a19eb704483756b033d8dcfaf7132e03b9ed1
RichardPK <r.phillips.kerr@gmail.com>
2020-06-08 12:38:06 +0100

    Added test for brand page valid uri

--- src/app/routes/onDemandRadio/getInitialData/logInitialData/index.test.js
+++ src/app/routes/onDemandRadio/getInitialData/logInitialData/index.test.js
@@ -1,8 +1,9 @@
 import path from 'ramda/src/path';
 import { parsePageIdentifier } from '.';
 import onDemandRadioEpisodeJson from '#data/pashto/bbc_pashto_radio/w3ct0lz1';
+import onDemandRadioBrandJson from '#data/indonesia/bbc_indonesian_radio/w13xtt0s';
 
-describe('Logging getinitial data', () => {
+describe('Logging get initial data', () => {
   it('should return a valid uri for episode page', () => {
     const pageIdentifier = path(
       ['metadata', 'analyticsLabels', 'pageIdentifier'],
@@ -21,5 +22,22 @@ describe('Logging getinitial data', () => {
     const pid = path(['promo', 'locators', 'pid'], onDemandRadioEpisodeJson);
 
     expect(url).toEqual(`${service}/${masterBrand}/${pid}`);
+  });
+
+  it('should return a valid uri for brand page', () => {
+    const pageIdentifier = path(
+      ['metadata', 'analyticsLabels', 'pageIdentifier'],
+      onDemandRadioBrandJson,
+    );
+    const url = parsePageIdentifier(pageIdentifier);
+
+    const service = path(
+      ['relatedContent', 'site', 'name'],
+      onDemandRadioBrandJson,
+    ).toLowerCase();
+    const masterBrand = path(['metadata', 'createdBy'], onDemandRadioBrandJson);
+    const pid = path(['promo', 'locators', 'brandPid'], onDemandRadioBrandJson);
+
+    expect(url).toEqual(`${service}/${masterBrand}/programmes/${pid}`);
   });
 });
