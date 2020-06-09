50171df382c239091640db03ce8183f68f0abea4
Ruth Ogendi <ruth.ogendi@andela.com>
2020-06-09 12:42:49 +0300

    Update bundleSizeConfig

--- scripts/bundleSizeConfig.js
+++ scripts/bundleSizeConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Size limit for all bundles used by each service (K)
   // Keep these +/- 5K and update frequently!
-  MIN_SIZE: 801,
-  MAX_SIZE: 851,
+  MIN_SIZE: 813,
+  MAX_SIZE: 857,
 };
