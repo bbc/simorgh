4d70cd826d829693775b28114e7484f6bd3bb39e
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-06-11 14:43:46 +0100

    wip

--- cypress/integration/specialFeatures/preRollAds/index.js
+++ cypress/integration/specialFeatures/preRollAds/index.js
@@ -1,38 +1,7 @@
-import config from '../../../support/config/services';
-import getPaths from '../../../support/helpers/getPaths';
 import testsForCanonicalOnly from './testsForCanonicalOnly';
-// import testsForAMPOnly from './testsForAMPOnly';
 
-// eslint-disable-next-line no-unused-vars
-const hasAds = serviceName => {
-  return true;
-};
-
-Object.keys(config)
-  .filter(hasAds)
-  .forEach(serviceId => {
-    const serviceName = config[serviceId].name;
-    Object.keys(config[serviceId].pageTypes)
-      .filter(pageType => pageType.includes('mediaAssetPage'))
-      .forEach(pageType => {
-        const paths = getPaths(serviceId, pageType);
-        paths.forEach(path => {
-          testsForCanonicalOnly({
-            serviceId,
-            serviceName,
-            pageType,
-            path,
-          });
-        });
-        // paths
-        //   .map(path => `${path}.amp`)
-        //   .forEach(path => {
-        //
-        //     testsForAMPOnly({
-        //       serviceName,
-        //       pageType,
-        //       path,
-        //     });
-        //   });
-      });
-  });
+testsForCanonicalOnly({
+  serviceName: 'bengali',
+  pageType: 'mediaAssetPage',
+  path: 'https://www.test.bbc.com/bengali/media-23269006',
+});
--- cypress/integration/specialFeatures/preRollAds/testsForCanonicalOnly.js
+++ cypress/integration/specialFeatures/preRollAds/testsForCanonicalOnly.js
@@ -1,63 +1,11 @@
-import { hasMedia } from '../../pages/mediaAssetPage/helpers';
-
-// const R = require('ramda');
-// const requestAdsToggle = require('./requestAdsToggle');
-
-const MIN_VIDEO_DURATION_FOR_PREROLLS = 30; // seconds
-
-const getDuration = jsonData => jsonData.promo.media.versions[0].duration;
-
-const shouldAllowAdsForAssetType = jsonData =>
-  !jsonData.metadata.blockTypes.includes('legacyMedia');
-
-const shouldAllowAdsForVideoDuration = jsonData => {
-  if (jsonData.promo.media.live) {
-    return true;
-  }
-  return getDuration(jsonData) >= MIN_VIDEO_DURATION_FOR_PREROLLS;
-};
-
-const getAdSetting = jsonData => {
-  if (jsonData.promo.media.live) {
-    return jsonData.metadata.options.allowAdvertising;
-  }
-  return jsonData.promo.media.advertising;
-};
+import visitPage from '../../../support/helpers/visitPage';
 
 export default ({ serviceName, pageType, path }) => {
   describe(`Canonical tests for PreRoll ads for ${serviceName} ${pageType} ${path}`, () => {
     it('should load pre-roll ad plugin', () => {
-      cy.request(`${path}.json`).then(({ body: jsonData }) => {
-        if (hasMedia(jsonData)) {
-          if (shouldAllowAdsForAssetType(jsonData)) {
-            if (getAdSetting(jsonData)) {
-              if (shouldAllowAdsForVideoDuration(jsonData)) {
-                cy.get(
-                  `script[src="https://gn-web-assets.api.bbc.com/ngas/dotcom-preroll.js"]`,
-                ).should('exist');
-              } else {
-                cy.log(
-                  `Duration is below ${MIN_VIDEO_DURATION_FOR_PREROLLS} on ${path}
-                  `,
-                );
-              }
-            } else {
-              cy.log(
-                `No ads allowed in ad settings on ${pageType} ${path}
-                `,
-              );
-            }
-          } else {
-            cy.log(
-              `Not a CPS asset on ${pageType} ${path}
-              `,
-            );
-          }
-        } else {
-          cy.log(`No media on ${pageType} ${path}
-          `);
-        }
-      });
+      visitPage(path, pageType);
+
+      cy.get(`script[src*="dotcom-preroll.js"]`).should('exist');
     });
   });
 };
