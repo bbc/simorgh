cd4d67df8980f38a90b73014a07844a17af2a3fa
thekp <khoa.phan@bbc.co.uk>
2020-06-11 18:37:52 +0100

    remove console log

--- cypress/integration/specialFeatures/cookieBanner/testsForCanonicalOnly.js
+++ cypress/integration/specialFeatures/cookieBanner/testsForCanonicalOnly.js
@@ -16,9 +16,6 @@ const serviceFilter = service =>
 
 const assertCookieValue = (cookieName, value) => {
   cy.getCookie(cookieName).should('have.property', 'value', value);
-  cy.getCookie(cookieName).then(c => {
-    console.log(c);
-  });
 };
 
 const assertCookieExpiryDate = (cookieName, timestamp) => {
