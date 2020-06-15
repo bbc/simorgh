fa89008632b347445c5772f10214601d4d53224c
Richard Itimi <itimirichard@gmail.com>
2020-06-15 18:26:03 +0100

    update toggle service endpoint

--- src/app/contexts/ToggleContext/utils/constructTogglesEndpoint/index.test.js
+++ src/app/contexts/ToggleContext/utils/constructTogglesEndpoint/index.test.js
@@ -1,7 +1,7 @@
 import getTogglesEndpoint from './index';
 
 beforeEach(() => {
-  process.env.SIMORGH_TOGGLES_URL = 'https://toggles.test.api.bbci.co.uk';
+  process.env.SIMORGH_TOGGLES_URL = 'https://config.test.api.bbci.co.uk';
 });
 
 afterEach(() => {
@@ -11,15 +11,15 @@ afterEach(() => {
 describe('Toggles endpoint constructor', () => {
   it('returns correct endpoint during ssr', () => {
     expect(getTogglesEndpoint('mundo', null)).toEqual(
-      `https://toggles.test.api.bbci.co.uk/toggles?application=simorgh&service=mundo&__amp_source_origin=https://www.test.bbc.com`,
+      `https://config.test.api.bbci.co.uk/toggles?application=simorgh&service=mundo&__amp_source_origin=https://www.test.bbc.com`,
     );
   });
 
   it('returns correct endpoint when on live', () => {
-    process.env.SIMORGH_TOGGLES_URL = 'https://toggles.api.bbci.co.uk';
+    process.env.SIMORGH_TOGGLES_URL = 'https://config.api.bbci.co.uk';
 
     expect(getTogglesEndpoint('mundo', 'https://www.bbc.com')).toEqual(
-      `https://toggles.api.bbci.co.uk/toggles?application=simorgh&service=mundo&__amp_source_origin=https://www.bbc.com`,
+      `https://config.api.bbci.co.uk/toggles?application=simorgh&service=mundo&__amp_source_origin=https://www.bbc.com`,
     );
   });
 });
