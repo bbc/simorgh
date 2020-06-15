5e6766b4b1d059c4729c031867f64e441e3f61b7
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2020-06-15 09:56:55 +0100

    Update Front Page integration tests

--- src/integration/pages/frontPage/canonicalTests.js
+++ src/integration/pages/frontPage/canonicalTests.js
@@ -1,9 +1,11 @@
+import runCrossPlatformTests from './crossPlatformTests';
 import {
   runCoreCanonicalTests,
   runCanonicalAnalyticsTests,
 } from '../../common';
 
 export default () => {
+  runCrossPlatformTests();
   runCoreCanonicalTests();
   runCanonicalAnalyticsTests();
 
@@ -20,32 +22,5 @@ export default () => {
         expect(id).not.toBeInTheDocument();
       });
     }
-  });
-
-  describe('Story Promo', () => {
-    it('I can see an image', () => {
-      const section = document.querySelector('section');
-
-      if (section) {
-        expect(section).toBeInTheDocument();
-
-        const imageEl = section.getElementsByTagName('img')[0];
-        expect(imageEl).toBeInTheDocument();
-        expect(imageEl).toMatchSnapshot();
-      }
-    });
-
-    it('I can see a headline', () => {
-      const section = document.querySelector('section');
-
-      if (section) {
-        expect(section).toBeInTheDocument();
-
-        const h3El = section.querySelector('h3');
-        expect(h3El).toBeInTheDocument();
-        expect(h3El.textContent).toBeTruthy();
-        expect(h3El.textContent).toMatchSnapshot();
-      }
-    });
   });
 };
