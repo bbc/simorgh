ff4ddb73665c5aad664a8572f393cfb643a11f47
Simon Tanner <simon.tanner01@bbc.co.uk>
2020-06-16 15:37:13 +0100

    Add base amp url variants to environments

--- src/app/routes/cpsAsset/getInitialData/convertToOptimoBlocks/blocks/include/ampSrcBuilder.js
+++ src/app/routes/cpsAsset/getInitialData/convertToOptimoBlocks/blocks/include/ampSrcBuilder.js
@@ -1,7 +1,7 @@
 import Url from 'url-parse';
 
 const ampSrcBuilder = href => {
-  const ampSrcPrefix = 'https://news.files.bbci.co.uk';
+  const ampSrcPrefix = process.env.SIMORGH_INCLUDES_BASE_AMP_URL;
   const includePathname = new Url(href, true).pathname;
   const includeQueryString = new Url(href, false).query;
 
