66078bd79a0427d44d5209dc5580cc94ec4353de
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2020-06-19 16:48:58 +0100

    Removed unncessary prop import

--- src/app/containers/AudioPlayer/index.test.jsx
+++ src/app/containers/AudioPlayer/index.test.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import { string } from 'prop-types';
 import { RequestContextProvider } from '#contexts/RequestContext';
 import { ServiceContextProvider } from '#contexts/ServiceContext';
 import AudioPlayer from '.';
