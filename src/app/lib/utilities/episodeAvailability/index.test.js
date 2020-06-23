16f0fdfdec82ecc1db48c960c9352c786abb705d
RichardPK <r.phillips.kerr@gmail.com>
2020-06-23 14:37:10 +0100

    Updated episode availability tests

--- src/app/lib/utilities/episodeAvailability/index.test.js
+++ src/app/lib/utilities/episodeAvailability/index.test.js
@@ -1,7 +1,7 @@
 import path from 'ramda/src/path';
 import assocPath from 'ramda/src/assocPath';
 import loggerMock from '#testHelpers/loggerMock';
-import { EPISODE_EXPIRED } from '#lib/logger.const';
+import { EPISODE_EXPIRED, EPISODE_NOT_YET_AVAILABLE } from '#lib/logger.const';
 import getEpisodeAvailability, { getUrl } from '.';
 import onDemandRadioEpisodeJson from '#data/pashto/bbc_pashto_radio/w3ct0lz1';
 import onDemandRadioBrandJson from '#data/indonesia/bbc_indonesian_radio/w13xtt0s';
@@ -15,7 +15,6 @@ describe('Logging episodeAvailability', () => {
 
   it('should return a valid url for radio episode page', () => {
     const url = getUrl(onDemandRadioEpisodeJson);
-
     const service = path(
       ['relatedContent', 'site', 'name'],
       onDemandRadioEpisodeJson,
@@ -25,50 +24,41 @@ describe('Logging episodeAvailability', () => {
       onDemandRadioEpisodeJson,
     );
     const pid = path(['promo', 'locators', 'pid'], onDemandRadioEpisodeJson);
-
     expect(url).toEqual(`${service}/${masterBrand}/${pid}`);
   });
 
   it('should return a valid url for radio brand page', () => {
     const url = getUrl(onDemandRadioBrandJson);
-
     const service = path(
       ['relatedContent', 'site', 'name'],
       onDemandRadioBrandJson,
     ).toLowerCase();
     const masterBrand = path(['metadata', 'createdBy'], onDemandRadioBrandJson);
     const pid = path(['promo', 'locators', 'brandPid'], onDemandRadioBrandJson);
-
     expect(url).toEqual(`${service}/${masterBrand}/programmes/${pid}`);
   });
 
   it('should return a valid url for tv episode page', () => {
     const url = getUrl(onDemandTvEpisodeJson);
-
     const service = path(
       ['relatedContent', 'site', 'name'],
       onDemandTvEpisodeJson,
     ).toLowerCase();
     const masterBrand = path(['metadata', 'createdBy'], onDemandTvEpisodeJson);
     const pid = path(['promo', 'locators', 'pid'], onDemandTvEpisodeJson);
-
     expect(url).toEqual(`${service}/${masterBrand}/${pid}`);
   });
 
   it('should return a valid url for tv brand page', () => {
     const url = getUrl(onDemandTvBrandJson);
-
     const service = path(
       ['relatedContent', 'site', 'name'],
       onDemandTvBrandJson,
     ).toLowerCase();
     const masterBrand = path(['metadata', 'createdBy'], onDemandTvBrandJson);
     const pid = path(['promo', 'locators', 'brandPid'], onDemandTvBrandJson);
-
     expect(url).toEqual(`${service}/${masterBrand}/tv_programmes/${pid}`);
   });
-
-  //   add tests for tv URLs
 
   it('logs the correct message when the on demand radio episode is expired', async () => {
     const dataWithoutVersions = assocPath(
@@ -83,6 +73,22 @@ describe('Logging episodeAvailability', () => {
     });
   });
 
+  it('logs the correct message when the on demand tv episode is not yet available', async () => {
+    const oneMinuteFromNow = Date.now() + 60 * 1000;
+    const responseWithEpisodeAvailableInOneMinute = assocPath(
+      ['content', 'blocks', '0', 'versions', '0', 'availableFrom'],
+      oneMinuteFromNow,
+      onDemandTvEpisodeJson,
+    );
+    const episodeAvailability = getEpisodeAvailability(
+      responseWithEpisodeAvailableInOneMinute,
+    );
+    expect(episodeAvailability).toEqual('not-yet-available');
+    expect(loggerMock.info).toHaveBeenCalledWith(EPISODE_NOT_YET_AVAILABLE, {
+      url: 'afrique/bbc_afrique_tv/w172xc9mmckr8m6',
+    });
+  });
+
   it('does not log anything when a radio episode is available', async () => {
     const oneMinuteAgo = Date.now() - 60 * 1000;
     const responseWithEpisodeAvailableOneMinuteAgo = assocPath(
@@ -90,10 +96,10 @@ describe('Logging episodeAvailability', () => {
       oneMinuteAgo,
       onDemandRadioEpisodeJson,
     );
-    fetch.mockResponse(
-      JSON.stringify(responseWithEpisodeAvailableOneMinuteAgo),
+    const episodeAvailability = getEpisodeAvailability(
+      responseWithEpisodeAvailableOneMinuteAgo,
     );
-
+    expect(episodeAvailability).toEqual('available');
     expect(loggerMock.info).not.toHaveBeenCalled();
   });
 });
