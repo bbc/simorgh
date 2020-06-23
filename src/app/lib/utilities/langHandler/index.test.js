1d472e47f960c7353b4ee21077b480980f618041
thekp <khoa.phan@bbc.co.uk>
2020-06-23 10:38:12 +0100

    update langHandler to check pathname includes rather than equals to catch amp extension

--- src/app/lib/utilities/langHandler/index.test.js
+++ src/app/lib/utilities/langHandler/index.test.js
@@ -5,6 +5,9 @@ describe('getLangOverride', () => {
     expect(
       getLangOverride({ pathname: '/ukrainian/ukraine_in_russian' }),
     ).toEqual('ru-UA');
+    expect(
+      getLangOverride({ pathname: '/ukrainian/ukraine_in_russian.amp' }),
+    ).toEqual('ru-UA');
   });
 
   it('should return null if pathname is incorrect', () => {
