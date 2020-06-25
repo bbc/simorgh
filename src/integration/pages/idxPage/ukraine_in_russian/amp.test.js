0770275fdfc894dc3f7de8fc58b55b62f7d24942
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2020-06-25 17:22:23 +0100

    Add Integration tests to check the header and lang attribute for the Ukraine in Russian IDX page

--- src/integration/pages/idxPage/ukraine_in_russian/amp.test.js
+++ src/integration/pages/idxPage/ukraine_in_russian/amp.test.js
@@ -4,5 +4,10 @@
  */
 
 import runAmpTests from '../ampTests';
+import runHeaderTests from './header.test';
+import runFooterTests from './footer.test';
 
 describe('AMP ukrainian/ukraine_in_russian IDX page', runAmpTests);
+
+runHeaderTests();
+runFooterTests();
--- src/integration/pages/idxPage/ukraine_in_russian/canonical.test.js
+++ src/integration/pages/idxPage/ukraine_in_russian/canonical.test.js
@@ -4,5 +4,10 @@
  */
 
 import runCanonicalTests from '../canonicalTests';
+import runHeaderTests from './header.test';
+import runFooterTests from './footer.test';
 
 describe('Canonical ukrainian/ukraine_in_russian IDX page', runCanonicalTests);
+
+runHeaderTests();
+runFooterTests();
