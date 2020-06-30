6e6de313881777e1fdc94570b90920810b69605b
Karina Thomas <karina.thomas@bbc.co.uk>
2020-06-30 16:14:46 +0100

    Failing tests for episode availability

--- src/app/lib/utilities/episodeAvailability/index.test.js
+++ src/app/lib/utilities/episodeAvailability/index.test.js
@@ -1,5 +1,6 @@
 import path from 'ramda/src/path';
 import assocPath from 'ramda/src/assocPath';
+import dissocPath from 'ramda/src/dissocPath';
 import loggerMock from '#testHelpers/loggerMock';
 import { EPISODE_EXPIRED, EPISODE_NOT_YET_AVAILABLE } from '#lib/logger.const';
 import getEpisodeAvailability, { getUrl } from '.';
@@ -8,98 +9,214 @@ import onDemandRadioBrandJson from '#data/indonesia/bbc_indonesian_radio/w13xtt0
 import onDemandTvEpisodeJson from '#data/afrique/bbc_afrique_tv/w13xttmz.json';
 import onDemandTvBrandJson from '#data/somali/bbc_somali_tv/tv_programmes/w13xttqt.json';
 
-describe('Logging episodeAvailability', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+describe('Episode Availability', () => {
+  describe('getUrl', () => {
+    it('should return a valid url for radio episode page', () => {
+      const url = getUrl(onDemandRadioEpisodeJson);
+      const service = path(
+        ['relatedContent', 'site', 'name'],
+        onDemandRadioEpisodeJson,
+      ).toLowerCase();
+      const masterBrand = path(
+        ['metadata', 'createdBy'],
+        onDemandRadioEpisodeJson,
+      );
+      const pid = path(['promo', 'locators', 'pid'], onDemandRadioEpisodeJson);
+      expect(url).toEqual(`${service}/${masterBrand}/${pid}`);
+    });
 
-  it('should return a valid url for radio episode page', () => {
-    const url = getUrl(onDemandRadioEpisodeJson);
-    const service = path(
-      ['relatedContent', 'site', 'name'],
-      onDemandRadioEpisodeJson,
-    ).toLowerCase();
-    const masterBrand = path(
-      ['metadata', 'createdBy'],
-      onDemandRadioEpisodeJson,
-    );
-    const pid = path(['promo', 'locators', 'pid'], onDemandRadioEpisodeJson);
-    expect(url).toEqual(`${service}/${masterBrand}/${pid}`);
-  });
+    it('should return a valid url for radio brand page', () => {
+      const url = getUrl(onDemandRadioBrandJson);
+      const service = path(
+        ['relatedContent', 'site', 'name'],
+        onDemandRadioBrandJson,
+      ).toLowerCase();
+      const masterBrand = path(
+        ['metadata', 'createdBy'],
+        onDemandRadioBrandJson,
+      );
+      const pid = path(
+        ['promo', 'locators', 'brandPid'],
+        onDemandRadioBrandJson,
+      );
+      expect(url).toEqual(`${service}/${masterBrand}/programmes/${pid}`);
+    });
 
-  it('should return a valid url for radio brand page', () => {
-    const url = getUrl(onDemandRadioBrandJson);
-    const service = path(
-      ['relatedContent', 'site', 'name'],
-      onDemandRadioBrandJson,
-    ).toLowerCase();
-    const masterBrand = path(['metadata', 'createdBy'], onDemandRadioBrandJson);
-    const pid = path(['promo', 'locators', 'brandPid'], onDemandRadioBrandJson);
-    expect(url).toEqual(`${service}/${masterBrand}/programmes/${pid}`);
-  });
+    it('should return a valid url for tv episode page', () => {
+      const url = getUrl(onDemandTvEpisodeJson);
+      const service = path(
+        ['relatedContent', 'site', 'name'],
+        onDemandTvEpisodeJson,
+      ).toLowerCase();
+      const masterBrand = path(
+        ['metadata', 'createdBy'],
+        onDemandTvEpisodeJson,
+      );
+      const pid = path(['promo', 'locators', 'pid'], onDemandTvEpisodeJson);
+      expect(url).toEqual(`${service}/${masterBrand}/${pid}`);
+    });
 
-  it('should return a valid url for tv episode page', () => {
-    const url = getUrl(onDemandTvEpisodeJson);
-    const service = path(
-      ['relatedContent', 'site', 'name'],
-      onDemandTvEpisodeJson,
-    ).toLowerCase();
-    const masterBrand = path(['metadata', 'createdBy'], onDemandTvEpisodeJson);
-    const pid = path(['promo', 'locators', 'pid'], onDemandTvEpisodeJson);
-    expect(url).toEqual(`${service}/${masterBrand}/${pid}`);
-  });
-
-  it('should return a valid url for tv brand page', () => {
-    const url = getUrl(onDemandTvBrandJson);
-    const service = path(
-      ['relatedContent', 'site', 'name'],
-      onDemandTvBrandJson,
-    ).toLowerCase();
-    const masterBrand = path(['metadata', 'createdBy'], onDemandTvBrandJson);
-    const pid = path(['promo', 'locators', 'brandPid'], onDemandTvBrandJson);
-    expect(url).toEqual(`${service}/${masterBrand}/tv_programmes/${pid}`);
-  });
-
-  it('logs the correct message when the on demand radio episode is expired', async () => {
-    const dataWithoutVersions = assocPath(
-      ['content', 'blocks', 0, 'versions'],
-      [],
-      onDemandRadioEpisodeJson,
-    );
-    const episodeAvailability = getEpisodeAvailability(dataWithoutVersions);
-    expect(episodeAvailability).toEqual('expired');
-    expect(loggerMock.info).toHaveBeenCalledWith(EPISODE_EXPIRED, {
-      url: 'pashto/bbc_pashto_radio/w3ct0lz1',
+    it('should return a valid url for tv brand page', () => {
+      const url = getUrl(onDemandTvBrandJson);
+      const service = path(
+        ['relatedContent', 'site', 'name'],
+        onDemandTvBrandJson,
+      ).toLowerCase();
+      const masterBrand = path(['metadata', 'createdBy'], onDemandTvBrandJson);
+      const pid = path(['promo', 'locators', 'brandPid'], onDemandTvBrandJson);
+      expect(url).toEqual(`${service}/${masterBrand}/tv_programmes/${pid}`);
     });
   });
 
-  it('logs the correct message when the on demand tv episode is not yet available', async () => {
-    const oneMinuteFromNow = Date.now() + 60 * 1000;
-    const responseWithEpisodeAvailableInOneMinute = assocPath(
-      ['content', 'blocks', '0', 'versions', '0', 'availableFrom'],
-      oneMinuteFromNow,
-      onDemandTvEpisodeJson,
-    );
-    const episodeAvailability = getEpisodeAvailability(
-      responseWithEpisodeAvailableInOneMinute,
-    );
-    expect(episodeAvailability).toEqual('not-yet-available');
-    expect(loggerMock.info).toHaveBeenCalledWith(EPISODE_NOT_YET_AVAILABLE, {
-      url: 'afrique/bbc_afrique_tv/w172xc9mmckr8m6',
-    });
-  });
+  const oneMinuteFromNow = Date.now() + 60 * 1000;
+  const sixtyMinutesFromNow = Date.now() + 60 * 60 * 1000;
 
-  it('does not log anything when a radio episode is available', async () => {
-    const oneMinuteAgo = Date.now() - 60 * 1000;
-    const responseWithEpisodeAvailableOneMinuteAgo = assocPath(
-      ['content', 'blocks', '0', 'versions', '0', 'availableFrom'],
-      oneMinuteAgo,
-      onDemandRadioEpisodeJson,
-    );
-    const episodeAvailability = getEpisodeAvailability(
-      responseWithEpisodeAvailableOneMinuteAgo,
-    );
-    expect(episodeAvailability).toEqual('available');
-    expect(loggerMock.info).not.toHaveBeenCalled();
+  const oneMinuteAgo = Date.now() - 60 * 1000;
+  const sixtyMinutesAgo = Date.now() - 60 * 60 * 1000;
+
+  const episodeData = onDemandRadioEpisodeJson;
+
+  describe('getEpisodeAvailability', () => {
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    describe('episode is expired', () => {
+      describe('because versions is empty', () => {
+        const dataWithoutVersions = assocPath(
+          ['content', 'blocks', 0, 'versions'],
+          [],
+          episodeData,
+        );
+
+        it(`should return 'expired'`, () => {
+          const episodeAvailability = getEpisodeAvailability(
+            dataWithoutVersions,
+          );
+          expect(episodeAvailability).toEqual('expired');
+        });
+
+        it(`should log expired message`, () => {
+          getEpisodeAvailability(dataWithoutVersions);
+          expect(loggerMock.info).toHaveBeenCalledWith(EPISODE_EXPIRED, {
+            url: 'pashto/bbc_pashto_radio/w3ct0lz1',
+          });
+        });
+      });
+
+      describe('because availableFrom and availableUntil are in the past', () => {});
+    });
+
+    describe('episode is available', () => {
+      const episodeAvailableFromOneMinuteAgo = assocPath(
+        ['content', 'blocks', '0', 'versions', '0', 'availableFrom'],
+        oneMinuteAgo,
+        episodeData,
+      );
+
+      describe('because availableFrom is in the past, and availableUntil does not exist', () => {
+        const episodeWithAvailableFromInPastNoAvailableUntil = dissocPath(
+          ['content', 'blocks', '0', 'versions', '0', 'availableUntil'],
+          episodeAvailableFromOneMinuteAgo,
+        );
+
+        it(`should return 'available'`, () => {
+          const episodeAvailability = getEpisodeAvailability(
+            episodeWithAvailableFromInPastNoAvailableUntil,
+          );
+          expect(episodeAvailability).toEqual('available');
+        });
+
+        it('should not log available message', () => {
+          getEpisodeAvailability(
+            episodeWithAvailableFromInPastNoAvailableUntil,
+          );
+          expect(loggerMock.info).not.toHaveBeenCalled();
+        });
+      });
+
+      describe('because availableFrom is in the past, and availableUntil is in the future', () => {
+        const episodeWithAvailableFromInPastAvailableUntilInFuture = assocPath(
+          ['content', 'blocks', '0', 'versions', '0', 'availableUntil'],
+          sixtyMinutesFromNow,
+          episodeAvailableFromOneMinuteAgo,
+        );
+
+        it(`should return 'available'`, () => {
+          const episodeAvailability = getEpisodeAvailability(
+            episodeWithAvailableFromInPastAvailableUntilInFuture,
+          );
+          expect(episodeAvailability).toEqual('available');
+        });
+
+        it('should not log available message', () => {
+          getEpisodeAvailability(
+            episodeWithAvailableFromInPastAvailableUntilInFuture,
+          );
+          expect(loggerMock.info).not.toHaveBeenCalled();
+        });
+      });
+    });
+
+    describe('episode is not yet available', () => {
+      const episodeAvailableFromOneMinuteInFuture = assocPath(
+        ['content', 'blocks', '0', 'versions', '0', 'availableFrom'],
+        oneMinuteFromNow,
+        episodeData,
+      );
+
+      describe('because availableFrom is in the future, and availableUntil does not exist', () => {
+        const episodeWithAvailableFromInFutureNoAvailableUntil = dissocPath(
+          ['content', 'blocks', '0', 'versions', '0', 'availableUntil'],
+          episodeAvailableFromOneMinuteInFuture,
+        );
+
+        it(`should return 'not-yet-available'`, () => {
+          const episodeAvailability = getEpisodeAvailability(
+            episodeWithAvailableFromInFutureNoAvailableUntil,
+          );
+          expect(episodeAvailability).toEqual('not-yet-available');
+        });
+
+        it('should log not yet available message', () => {
+          getEpisodeAvailability(
+            episodeWithAvailableFromInFutureNoAvailableUntil,
+          );
+          expect(loggerMock.info).toHaveBeenCalledWith(
+            EPISODE_NOT_YET_AVAILABLE,
+            {
+              url: 'pashto/bbc_pashto_radio/w3ct0lz1',
+            },
+          );
+        });
+      });
+
+      describe('because availableFrom and availableUntil are in the future', () => {
+        const episodeWithAvailableFromAndAvailableUntilInFuture = assocPath(
+          ['content', 'blocks', '0', 'versions', '0', 'availableUntil'],
+          sixtyMinutesFromNow,
+          episodeAvailableFromOneMinuteInFuture,
+        );
+
+        it(`should return 'not-yet-available'`, () => {
+          const episodeAvailability = getEpisodeAvailability(
+            episodeWithAvailableFromAndAvailableUntilInFuture,
+          );
+          expect(episodeAvailability).toEqual('not-yet-available');
+        });
+
+        it('should log not available yet message', () => {
+          getEpisodeAvailability(
+            episodeWithAvailableFromAndAvailableUntilInFuture,
+          );
+          expect(loggerMock.info).toHaveBeenCalledWith(
+            EPISODE_NOT_YET_AVAILABLE,
+            {
+              url: 'pashto/bbc_pashto_radio/w3ct0lz1',
+            },
+          );
+        });
+      });
+    });
   });
 });
