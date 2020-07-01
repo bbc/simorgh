8670cf361ff7184454a853ebaa91e940d5f391d5
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-07-01 16:02:59 +0100

    use async await

--- src/app/routes/utils/fetchPageData/index.js
+++ src/app/routes/utils/fetchPageData/index.js
@@ -11,9 +11,7 @@ const OK = 200;
 const BAD_GATEWAY = 502;
 const INTERNAL_SERVER_ERROR = 500;
 const NOT_FOUND = 404;
-
 const ampRegex = /.amp$/;
-
 const baseUrl = onClient()
   ? getBaseUrl(window.location.origin)
   : process.env.SIMORGH_BASE_URL;
@@ -27,49 +25,44 @@ export const getUrl = pathname => {
   return `${baseUrl}${basePath.replace(ampRegex, '')}.json${params}`; // Remove .amp at the end of pathnames for AMP pages.
 };
 
-const handleResponse = url => async response => {
-  const { status } = response;
-  const json = await response.json();
-
-  if (status === OK) {
-    return {
-      status,
-      json,
-    };
-  }
-
-  const error = new Error();
-
-  if (status === NOT_FOUND) {
-    error.message = DATA_NOT_FOUND;
-    error.status = NOT_FOUND;
-
-    throw error;
-  }
-
-  error.message = `Unexpected upstream response (HTTP status code ${status}) when requesting ${url}`;
-
-  throw error;
-};
-
-const handleError = url => ({ message, status }) => {
-  const error = new Error(message);
-  error.status = status || (onClient() ? BAD_GATEWAY : INTERNAL_SERVER_ERROR);
-
-  logger.error(message, {
-    url,
-    status,
-  });
-
-  throw error;
-};
-
-const fetchData = pathname => {
+export default async pathname => {
   const url = getUrl(pathname);
 
   logger.info(DATA_REQUEST_RECEIVED, { url });
 
-  return fetch(url).then(handleResponse(url)).catch(handleError(url));
-};
+  try {
+    const response = await fetch(url);
+    const { status } = response;
+    const json = await response.json();
 
-export default fetchData;
+    if (status === OK) {
+      return {
+        status,
+        json,
+      };
+    }
+
+    const error = new Error();
+
+    if (status === NOT_FOUND) {
+      error.message = DATA_NOT_FOUND;
+      error.status = NOT_FOUND;
+
+      throw error;
+    }
+
+    error.message = `Unexpected upstream response (HTTP status code ${status}) when requesting ${url}`;
+
+    throw error;
+  } catch ({ message, status }) {
+    const error = new Error(message);
+    error.status = status || (onClient() ? BAD_GATEWAY : INTERNAL_SERVER_ERROR);
+
+    logger.error(message, {
+      url,
+      status,
+    });
+
+    throw error;
+  }
+};
