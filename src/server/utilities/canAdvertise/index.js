d2b6f65fb713bc534a01c0d31c361bfc28d60be9
Richard Itimi <itimirichard@gmail.com>
2020-07-02 17:33:00 +0100

    remove unnecessary !!

--- src/server/utilities/canAdvertise/index.js
+++ src/server/utilities/canAdvertise/index.js
@@ -2,7 +2,7 @@ const canAdvertise = headers => {
   const isAdvertiseCombine = headers['x-ip_is_advertise_combined'];
   const isUK = headers['x-bbc-edge-isuk'];
 
-  return !!(isAdvertiseCombine === 'yes' || isUK === 'no');
+  return isAdvertiseCombine === 'yes' || isUK === 'no';
 };
 
 export default canAdvertise;
