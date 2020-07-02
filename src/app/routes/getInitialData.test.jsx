454979aefa0e8537a77de4a9b8668f2c364a9a5f
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-07-02 14:43:38 +0100

    test getInitialData 202 handling

--- src/app/routes/getInitialData.test.jsx
+++ src/app/routes/getInitialData.test.jsx
@@ -17,6 +17,17 @@ routes
       expect(actual).toEqual(expected);
     });
 
+    it(`${pageType} - should handle Ares 202`, async () => {
+      global.fetch.mockResponseOnce(JSON.stringify({}), { status: 202 });
+
+      const actual = await getInitialData(MOCK_PATH);
+
+      expect(actual.status).toEqual(502);
+      expect(actual.error).toMatch(
+        'Unexpected upstream response (HTTP status code 202) when requesting',
+      );
+    });
+
     it(`${pageType} - should handle Ares 500`, async () => {
       global.fetch.mockResponseOnce(JSON.stringify({}), { status: 500 });
 
