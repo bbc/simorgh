f2ae8588ecc0c5041f7803ce3fa81c0927862298
Richard Itimi <itimirichard@gmail.com>
2020-07-02 15:24:57 +0100

    add test for when advert headers are not present update canAdvertise logic to not conside isUkCombined

--- src/server/utilities/canAdvertise/index.js
+++ src/server/utilities/canAdvertise/index.js
@@ -1,13 +1,8 @@
 const canAdvertise = headers => {
   const isAdvertiseCombine = headers['x-ip_is_advertise_combined'];
-  const isUkCombined = headers['x-ip_is_uk_combined'];
   const isUK = headers['x-bbc-edge-isuk'];
 
-  const hasIpHeaders = isAdvertiseCombine && isUkCombined;
-
-  return hasIpHeaders
-    ? isAdvertiseCombine === 'yes' && isUkCombined === 'no'
-    : isUK === 'no';
+  return !!(isAdvertiseCombine === 'yes' || isUK === 'no');
 };
 
 export default canAdvertise;
--- src/server/utilities/canAdvertise/index.test.js
+++ src/server/utilities/canAdvertise/index.test.js
@@ -1,26 +1,16 @@
 import canAdvertise from '.';
 
 describe('canAdvertise', () => {
-  it('should return true when isAdvertiseCombine equals yes and isUkCombined equals no', () => {
+  it('should return true when isAdvertiseCombine equals `yes`', () => {
     const headers = {
       'x-ip_is_advertise_combined': 'yes',
-      'x-ip_is_uk_combined': 'no',
     };
     expect(canAdvertise(headers)).toBeTruthy();
   });
 
-  it('should return false when isUkCombined equals yes', () => {
-    const headers = {
-      'x-ip_is_advertise_combined': 'yes',
-      'x-ip_is_uk_combined': 'yes',
-    };
-    expect(canAdvertise(headers)).toBeFalsy();
-  });
-
-  it('should return false isAdvertiseCombined equals no', () => {
+  it('should return false isAdvertiseCombined equals `no`', () => {
     const headers = {
       'x-ip_is_advertise_combined': 'no',
-      'x-ip_is_uk_combined': 'yes',
     };
     expect(canAdvertise(headers)).toBeFalsy();
   });
@@ -32,10 +22,15 @@ describe('canAdvertise', () => {
     expect(canAdvertise(headers)).toBeFalsy();
   });
 
-  it('should return false when isUk equals no', () => {
+  it('should return true when isUk equals no', () => {
     const headers = {
       'x-bbc-edge-isuk': 'no',
     };
     expect(canAdvertise(headers)).toBeTruthy();
+  });
+
+  it('should return false when neither isUk nor isAdvertiseCombined header is present', () => {
+    const headers = {};
+    expect(canAdvertise(headers)).toBeFalsy();
   });
 });
