bbe75e0c7337bc68cff091781c576850e9d4c16e
LilyL0u <lily.higham@bbc.co.uk>
2020-07-03 16:07:43 +0100

    removed afaan oromo replacement in helpers as it doesnt have ODTV

--- cypress/integration/pages/onDemandTV/helpers.js
+++ cypress/integration/pages/onDemandTV/helpers.js
@@ -2,15 +2,13 @@ import path from 'ramda/src/path';
 import envConfig from '../../../support/config/envs';
 
 // the externalId `bbc_oromo_radio` is overriden to `bbc_afaanoromoo` in production code
-const getBrandId = externalId =>
-  externalId.replace('bbc_oromo_radio', 'bbc_afaanoromoo_radio');
 
 const getServiceName = producerName =>
   producerName.toLowerCase().replace('indonesian', 'indonesia');
 
 export const getEmbedUrl = (body, language) => {
   const externalId = body.metadata.createdBy;
-  const brandId = getBrandId(externalId);
+  const brandId = externalId;
   const producerName = body.metadata.analyticsLabels.producer;
   const serviceName = getServiceName(producerName);
   const { pid } = body.metadata.locators;
