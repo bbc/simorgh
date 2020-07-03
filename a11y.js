25e9a253b321ed4630cd6b3f6f6aaba7a2a80e65
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2020-07-03 11:56:31 +0000

    Add on demand tv to a11y tests

--- a11y.js
+++ a11y.js
@@ -36,6 +36,9 @@ const pageTypes = {
     "//div[@id='root']/header/nav/div/div[1]/div/ul",
   ],
   idxPage: ["//div[@id='root']/header/nav/div/div[1]/div/ul"],
+  onDemandTV: [
+    '/iframe', // Can be removed once https://github.com/bbc/bbc-a11y/issues/298 is resolved
+  ],
 };
 
 Object.keys(pageTypes).forEach(pageType => {
