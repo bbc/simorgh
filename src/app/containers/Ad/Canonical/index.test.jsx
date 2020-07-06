eb7ccef54bc8b02891a595f60769b05f2e0df6ea
thekp <khoa.phan@bbc.co.uk>
2020-07-06 11:19:11 +0100

    use suggested test query string

--- src/app/containers/Ad/Canonical/index.test.jsx
+++ src/app/containers/Ad/Canonical/index.test.jsx
@@ -42,7 +42,7 @@ describe('getBootsrapSrc', () => {
         'https://gn-web-assets.api.bbc.com/ngas/dotcom-bootstrap.js',
     },
     {
-      queryString: '?test_query_string',
+      queryString: '?invalid-string',
       isLiveValue: true,
       bootstrapSrc:
         'https://gn-web-assets.api.bbc.com/ngas/dotcom-bootstrap.js',
