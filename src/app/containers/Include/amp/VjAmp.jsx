812e05a2b3165ec4ea5c4118cf72857d81c7591c
Simon Sinclair <simon.sinclair@bbc.co.uk>
2020-07-08 17:10:12 +0100

    Refactor: wrap amp-iframe to control its styles

--- src/app/containers/Include/amp/VjAmp.jsx
+++ src/app/containers/Include/amp/VjAmp.jsx
@@ -1,72 +1,96 @@
 import React from 'react';
 import styled from 'styled-components';
-import { shape, string } from 'prop-types';
+import { shape, string, node } from 'prop-types';
+import {
+  GEL_SPACING,
+  GEL_SPACING_DBL,
+} from '@bbc/gel-foundations/dist/spacings';
 import { GridItemConstrainedMedium } from '#lib/styledGrid';
+
+const KEYLINE_WIDTH = '0.125rem';
 
 const IncludeGrid = styled(GridItemConstrainedMedium)`
   display: grid;
 `;
 
-const PocketTapArea = styled.div`
-  &&& {
-    display: block;
-    height: 70px;
+const AmpIframe = ({ children, className, width, height, src }) => (
+  <amp-iframe
+    class={className}
+    width={width}
+    height={height}
+    layout="responsive"
+    sandbox="allow-scripts allow-same-origin allow-top-navigation-by-user-activation allow-forms"
+    resizable
+    src={src}
+  >
+    {children}
+  </amp-iframe>
+);
+
+const StyledAmpIframe = styled(AmpIframe)`
+  > [overflow] {
     background: linear-gradient(
       0deg,
       #fff 0%,
-      rgba(255, 255, 255, 0.816947) 66%,
-      rgba(255, 255, 255, 0.0718488) 100%
+      rgba(255, 255, 255, 1) 75%,
+      rgba(255, 255, 255, 0) 100%
     );
-    &:after {
-      display: inline-block;
+    display: flex;
+    justify-content: center;
+
+    &::after {
+      background-color: #fff;
+      border-top: ${KEYLINE_WIDTH} solid #1380a1;
       content: '';
-      width: 100vw;
-      border-bottom: 2px solid #1380a1;
-      transform: translateY(2.4rem);
+      display: block;
+      height: 50%;
+      left: 0;
+      position: absolute;
+      top: 50%;
+      width: 100%;
+      z-index: -10;
+    }
+
+    button {
+      background-color: #1380a1;
+      border: none;
+      color: #fff;
+      cursor: pointer;
+      display: block;
+      padding: ${GEL_SPACING} ${GEL_SPACING_DBL};
     }
   }
-`;
-
-const PocketButton = styled.button`
-  background: #1380a1;
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-  padding: 1rem 2rem;
-  font-size: 1rem;
-  color: #fff;
-  top: 1.2rem;
-  border: 0;
-  cursor: pointer;
 `;
 
 const VjAmp = ({ ampMetadata: { imageWidth, imageHeight, image, src } }) => {
   return (
     <IncludeGrid>
-      <amp-iframe
-        width={imageWidth}
-        height={imageHeight}
-        layout="responsive"
-        sandbox="allow-scripts allow-same-origin allow-top-navigation-by-user-activation allow-forms"
-        resizable
-        src={src}
-      >
-        <PocketTapArea overflow="true">
-          <PocketButton aria-label="Read more">Show more</PocketButton>
-        </PocketTapArea>
-
+      <StyledAmpIframe width={imageWidth} height={imageHeight} src={src}>
+        <div overflow="true">
+          <button type="button" aria-label="Show More">
+            Show More
+          </button>
+        </div>
         <amp-img layout="fill" src={image} placeholder />
-      </amp-iframe>
+      </StyledAmpIframe>
     </IncludeGrid>
   );
 };
 
+AmpIframe.propTypes = {
+  children: node.isRequired,
+  width: string.isRequired,
+  height: string.isRequired,
+  src: string.isRequired,
+  className: string.isRequired,
+};
+
 VjAmp.propTypes = {
   ampMetadata: shape({
-    imageWidth: string,
-    imageHeight: string,
-    image: string,
-    src: string,
+    imageWidth: string.isRequired,
+    imageHeight: string.isRequired,
+    image: string.isRequired,
+    src: string.isRequired,
   }).isRequired,
 };
 
