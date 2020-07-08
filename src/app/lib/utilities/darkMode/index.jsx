24a24f66fb4a960bd2e7faf09d36d641c52911d4
Ryan McCombe <ryanmccombe@gmail.com>
2020-07-09 00:44:09 +0100

    Dark mode plumbing

---
+++ src/app/lib/utilities/darkMode/index.jsx
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components';
+
+// TODO: #121212 is a new colour not in psammead - add it?
+export default createGlobalStyle`
+#root {
+  background-color: #121212;
+}
+`;
