f80156e7927980da397df4adebcebebcea0632a2
LilyL0u <lily.higham@bbc.co.uk>
2020-07-16 17:57:37 +0100

    tests for href, bitrate, length, undefined url and image

--- AdHocCypress/cypress/integration/topcat2MediaPlayback/config.js
+++ AdHocCypress/cypress/integration/topcat2MediaPlayback/config.js
@@ -1,5 +1,11 @@
 export default {
-  local: [],
-  test: [],
-  live: [],
+  local: [
+    'https://www.test.bbc.com/arabic/worldnews/2015/11/151120_t_arabic_av',
+  ],
+  test: [
+    'https://www.test.bbc.com/arabic/worldnews/2015/11/151120_t_arabic_av',
+  ],
+  live: [
+    'https://www.bbc.com/arabic/multimedia/2013/10/131020_belfast_land_art_girl_face_',
+  ],
 };
--- AdHocCypress/cypress/integration/topcat2MediaPlayback/index.js
+++ AdHocCypress/cypress/integration/topcat2MediaPlayback/index.js
@@ -1,6 +1,5 @@
 import legacyURLs from './config';
 import getAppEnv from '../../../../cypress/support/helpers/getAppEnv';
-import { getEmbedUrl } from '../../../../cypress/integration/pages/mediaAssetPage/helpers';
 
 const environment = getAppEnv();
 
@@ -9,11 +8,62 @@ describe('Legacy MAP media playback', () => {
 
   if (paths.length > 0) {
     paths.forEach(path => {
-      it(path, () => {
+      it(`SMP playlist has expected hrefs, bitrates and undefined url - ${path}`, () => {
         cy.request(`${path}.json`).then(({ body: jsonData }) => {
-          const embedUrl = getEmbedUrl(jsonData, jsonData.metadata.language);
-          cy.visit(embedUrl);
-          // cy.get(`script[src*="dotcom-bootstrap.js"]`).should('exist');
+          const mediaUrlsFromData = jsonData.content.blocks[0].playlist.map(
+            item => item.url,
+          );
+          const bitratesFromData = jsonData.content.blocks[0].playlist.map(
+            item => (item.bitrate / 1000).toString(),
+          );
+          cy.visit(path);
+          cy.get(
+            'div[class^="StyledVideoContainer"] iframe[class^="StyledIframe"]',
+          ).then($iframe => {
+            cy.wrap($iframe.prop('contentWindow'), {
+              timeout: 8000,
+            })
+              .its('embeddedMedia.playerInstances.mediaPlayer.playlist')
+              .then(playlist => {
+                const { originalConnections } = playlist.items[0];
+                const mediaHrefs = originalConnections.map(item => item.href);
+                const bitrates = originalConnections.map(item => item.bitrate);
+                // eslint-disable-next-line no-unused-expressions
+                expect(playlist.url, 'Playlist URL should be undefined').to.be
+                  .undefined;
+                expect(
+                  originalConnections.length,
+                  'SMP playlist length matches json playlist length',
+                ).equal(mediaUrlsFromData.length);
+                expect(
+                  mediaHrefs,
+                  'Hrefs of media should match json',
+                ).to.deep.equal(mediaUrlsFromData); // Array checking
+                expect(bitrates, 'Bitrates should match json').to.deep.equal(
+                  bitratesFromData,
+                );
+              });
+          });
+        });
+      });
+
+      it(`Player has expected image - ${path}`, () => {
+        cy.request(`${path}.json`).then(({ body: jsonData }) => {
+          cy.visit(path);
+          cy.get(
+            'div[class^="StyledVideoContainer"] iframe[class^="StyledIframe"]',
+          ).then($iframe => {
+            cy.wrap($iframe.prop('contentWindow'), {
+              timeout: 8000,
+            })
+              .its(
+                'embeddedMedia.playerInstances.mediaPlayer.playlist.holdingImageURL',
+              )
+              .should(
+                'eq',
+                jsonData.content.blocks[0].image.href.replace('http', 'https'),
+              );
+          });
         });
       });
     });
