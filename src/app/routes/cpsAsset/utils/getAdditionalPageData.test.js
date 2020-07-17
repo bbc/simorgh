ac8cbc106ac46962719ae8d456773026a4c3eb9d
Harvey Peachey <harvey.peachey@bbc.co.uk>
2020-07-17 13:32:56 +0100

    Added additional test case for service which has recommedations disabled

--- src/app/routes/cpsAsset/utils/getAdditionalPageData.test.js
+++ src/app/routes/cpsAsset/utils/getAdditionalPageData.test.js
@@ -7,6 +7,7 @@ import getAdditionalPageData from './getAdditionalPageData';
 // mock data
 import mapJson from '#data/pidgin/cpsAssets/media-23256549.json';
 import styJson from '#data/mundo/cpsAssets/23263889.json';
+import noRecommendationsStyJson from '#data/pidgin/cpsAssets/world-23252817.json';
 import mostReadJson from '#data/mundo/mostRead/index.json';
 import secondaryColumnJson from '#data/mundo/secondaryColumn/index.json';
 import recommendationsJson from '#data/mundo/recommendations/index.json';
@@ -24,7 +25,7 @@ describe('getAdditionalPageData', () => {
     expect(additionalPageData).toBe(null);
   });
 
-  it('should return additonal data for an STY asset', async () => {
+  it('should return additonal data for an STY with recommendations for service with hasStoryRecommendations set to true', async () => {
     fetchMock.mock('http://localhost/mundo/mostread.json', mostReadJson);
     fetchMock.mock(
       'http://localhost/mundo/sty-secondary-column.json',
@@ -40,6 +41,28 @@ describe('getAdditionalPageData', () => {
       mostRead: mostReadJson,
       secondaryColumn: secondaryColumnJson,
       recommendations: recommendationsJson,
+    };
+
+    expect(additionalPageData).toEqual(expectedOutput);
+  });
+
+  it('should return additonal data for an STY with no recommendations for service with hasStoryRecommendations set to false', async () => {
+    fetchMock.mock('http://localhost/pidgin/mostread.json', { foo: 'bar' });
+    fetchMock.mock('http://localhost/pidgin/sty-secondary-column.json', {
+      foo: 'bar',
+    });
+    fetchMock.mock(
+      'http://localhost/pidgin/world-23252817/recommendations.json',
+      { foo: 'bar' },
+    );
+    const additionalPageData = await getAdditionalPageData(
+      noRecommendationsStyJson,
+      'pidgin',
+    );
+
+    const expectedOutput = {
+      mostRead: { foo: 'bar' },
+      secondaryColumn: { foo: 'bar' },
     };
 
     expect(additionalPageData).toEqual(expectedOutput);
