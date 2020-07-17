966a4a3caa6ef56aadda72e85eabbee38680ea9b
rowland <rowland.henshaw@andela.com>
2020-07-17 14:28:55 +0100

    reused simorgh storypromo component

--- src/app/containers/CpsRecommendations/index.jsx
+++ src/app/containers/CpsRecommendations/index.jsx
@@ -1,11 +1,8 @@
 import React, { useContext } from 'react';
 import { arrayOf, shape, number } from 'prop-types';
 import styled from 'styled-components';
-import path from 'ramda/src/path';
-import StoryPromo, { Headline, Link } from '@bbc/psammead-story-promo';
 import { StoryPromoLiBase, StoryPromoUl } from '@bbc/psammead-story-promo-list';
 import { C_LUNAR, C_GHOST } from '@bbc/psammead-styles/colours';
-import Image from '@bbc/psammead-image';
 import {
   GEL_SPACING,
   GEL_SPACING_HLF,
@@ -13,18 +10,21 @@ import {
 } from '@bbc/gel-foundations/spacings';
 import { GEL_GROUP_3_SCREEN_WIDTH_MAX } from '@bbc/gel-foundations/breakpoints';
 
+import StoryPromo from '../StoryPromo';
 import { storyItem } from '#models/propTypes/storyItem';
 import { ServiceContext } from '#contexts/ServiceContext';
 import useToggle from '#hooks/useToggle';
 import CpsOnwardJourney from '../CpsOnwardJourney';
 import Grid from '../../components/Grid';
 
-const StoryPromoWrapper = styled(StoryPromo)`
-  display: grid;
-  margin: ${GEL_SPACING_HLF} ${GEL_SPACING_DBL};
-  background-color: ${C_GHOST};
-  @media (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
-    margin: ${GEL_SPACING_HLF} 0;
+const StyledStoryPromoWrapper = styled.div`
+  > div {
+    display: grid;
+    margin: ${GEL_SPACING_HLF} ${GEL_SPACING_DBL};
+    background-color: ${C_GHOST};
+    @media (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
+      margin: ${GEL_SPACING_HLF} 0;
+    }
   }
 `;
 
@@ -33,15 +33,8 @@ const RecommendationsWrapper = styled.div`
   padding-bottom: ${GEL_SPACING};
 `;
 
-const getPromoItemProps = item => ({
-  imgSrc: path(['indexImage', 'path'], item),
-  href: path(['locators', 'assetUri'], item),
-  headline: path(['headlines', 'headline'], item),
-  alt: path(['indexImage', 'altText'], item),
-});
-
 const CpsRecommendations = ({ items, parentColumns }) => {
-  const { recommendations, script, service, dir } = useContext(ServiceContext);
+  const { recommendations, dir } = useContext(ServiceContext);
   const { enabled } = useToggle('cpsRecommendations');
 
   const { hasStoryRecommendations } = recommendations;
@@ -49,20 +42,6 @@ const CpsRecommendations = ({ items, parentColumns }) => {
   if (!hasStoryRecommendations || !enabled || !items.length) return null;
 
   const singleTransform = item => {
-    const { imgSrc, href, headline, alt } = getPromoItemProps(item);
-
-    const DEFAULT_IMAGE_RES = 660;
-    const ichefPath = imgSrc.replace('http://c.files.bbci.co.uk', '/cpsprodpb');
-    const ichefSrc = `https://ichef.bbci.co.uk/news/${DEFAULT_IMAGE_RES}${ichefPath}`;
-
-    const Img = <Image alt={alt} src={ichefSrc} />;
-
-    const Info = (
-      <Headline script={script} service={service} promoHasImage as="span">
-        <Link href={href}>{headline}</Link>
-      </Headline>
-    );
-
     return (
       <Grid
         columns={{
@@ -76,7 +55,14 @@ const CpsRecommendations = ({ items, parentColumns }) => {
         enableGelGutters
         dir={dir}
       >
-        <StoryPromoWrapper image={Img} info={Info} />
+        <StyledStoryPromoWrapper>
+          <StoryPromo
+            item={item}
+            dir={dir}
+            isRecommendation
+            displaySummary={false}
+          />
+        </StyledStoryPromoWrapper>
       </Grid>
     );
   };
