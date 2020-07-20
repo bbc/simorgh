02d27eda2b86f35ae682ec6e910ffb190831fb49
rowland <rowland.henshaw@andela.com>
2020-07-20 07:28:21 +0100

    updated snapshots

--- src/app/containers/CpsRecommendations/index.test.jsx
+++ src/app/containers/CpsRecommendations/index.test.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { latinDiacritics } from '@bbc/gel-foundations/scripts';
-import { ServiceContext } from '#contexts/ServiceContext';
+import { ServiceContextProvider } from '#contexts/ServiceContext';
 import { RequestContextProvider } from '#contexts/RequestContext';
 import { ToggleContext } from '#contexts/ToggleContext';
 
@@ -18,29 +17,20 @@ const parentColumns = {
   group5: 12,
 };
 
-const renderContainer = (items, hasStoryRecommendations, toggleEnabled) => {
+const renderContainer = (items, service, toggleEnabled) => {
   const toggleState = {
     cpsRecommendations: {
       enabled: toggleEnabled,
     },
   };
   return render(
-    <ServiceContext.Provider
-      value={{
-        script: latinDiacritics,
-        service: 'mundo',
-        dir: 'ltr',
-        recommendations: {
-          hasStoryRecommendations,
-        },
-      }}
-    >
+    <ServiceContextProvider service={service}>
       <RequestContextProvider
         bbcOrigin="https://www.test.bbc.co.uk"
         isAmp={false}
         pageType="STY"
-        pathname="/mundo/085965"
-        service="mundo"
+        pathname="/service/085965"
+        service={service}
         statusCode={200}
       >
         <ToggleContext.Provider
@@ -49,54 +39,46 @@ const renderContainer = (items, hasStoryRecommendations, toggleEnabled) => {
           <CpsRecommendations items={items} parentColumns={parentColumns} />
         </ToggleContext.Provider>
       </RequestContextProvider>
-    </ServiceContext.Provider>,
+    </ServiceContextProvider>,
   );
 };
 
 describe('CpsRecommendations', () => {
   it('should not render when cpsRecommendations toggle is disabled', () => {
     const toggleEnabled = false;
-    const hasStoryRecommendations = true;
 
     const { container } = renderContainer(
       recommendationsData,
-      hasStoryRecommendations,
+      'mundo',
       toggleEnabled,
     );
     expect(container).toMatchSnapshot();
   });
-  it('should not render when the hasStoryRecommendations flag is disabled for the service', () => {
-    const hasStoryRecommendations = false;
+  it('should not render when the hasStoryRecommendations is disabled for the service', () => {
     const toggleEnabled = true;
 
     const { container } = renderContainer(
       recommendationsData,
-      hasStoryRecommendations,
+      'news',
       toggleEnabled,
     );
     expect(container).toMatchSnapshot();
   });
 
   it('should not render when there is no recommendations data', () => {
-    const hasStoryRecommendations = false;
     const toggleEnabled = true;
 
-    const { container } = renderContainer(
-      [],
-      hasStoryRecommendations,
-      toggleEnabled,
-    );
+    const { container } = renderContainer([], 'mundo', toggleEnabled);
     expect(container).toMatchSnapshot();
   });
 
-  describe('should render when cpsRecommendations toggle and hasStoryRecommendations flag are both true', () => {
+  describe('should render when cpsRecommendations toggle is enabled and hasStoryRecommendations flag is enabled for the service', () => {
     const toggleEnabled = true;
-    const hasStoryRecommendations = true;
 
     it('for multiple items', () => {
       const { container } = renderContainer(
         recommendationsData,
-        hasStoryRecommendations,
+        'mundo',
         toggleEnabled,
       );
       expect(container).toMatchSnapshot();
@@ -104,7 +86,7 @@ describe('CpsRecommendations', () => {
     it('for a single item', () => {
       const { container } = renderContainer(
         [recommendationsData[0]],
-        hasStoryRecommendations,
+        'mundo',
         toggleEnabled,
       );
       expect(container).toMatchSnapshot();
