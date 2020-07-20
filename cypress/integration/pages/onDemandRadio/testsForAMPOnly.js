edb738e0a5fc6a1163224f425814e88b23218e76
karinathomasbbc <karina.thomas@bbc.co.uk>
2020-07-20 18:18:29 +0100

    Reinstate tests for audio placeholder image

--- cypress/integration/pages/onDemandRadio/testsForAMPOnly.js
+++ cypress/integration/pages/onDemandRadio/testsForAMPOnly.js
@@ -1,1 +1,28 @@
-export default () => {};
+/* eslint-disable consistent-return */
+import envConfig from '../../../support/config/envs';
+import {
+  isExpired,
+  dataEndpointOverride,
+} from '../../../support/helpers/onDemandRadioTv';
+
+export default ({ service, pageType }) => {
+  describe(`testsForAMPOnly for ${service} ${pageType}`, () => {
+    describe('Audio Player', () => {
+      it('should render an image placeholder', () => {
+        cy.request(
+          `${Cypress.env('currentPath')}.json${dataEndpointOverride()}`,
+        ).then(({ body: jsonData }) => {
+          const isExpiredEpisode = isExpired(jsonData);
+
+          if (isExpiredEpisode) {
+            return cy.log(`Episode is expired: ${Cypress.env('currentPath')}`);
+          }
+
+          cy.get(
+            `amp-img[src="${envConfig.assetUrl}/images/amp_audio_placeholder.png"]`,
+          ).should('exist');
+        });
+      });
+    });
+  });
+};
