317c13c1684d9c22f37e1153fc28b32fef536e5b
karinathomasbbc <karina.thomas@bbc.co.uk>
2020-07-20 21:42:33 +0100

    Move constructDataFilePath to separate file + add tests

---
+++ src/server/local/constructDataFilePath/index.js
@@ -0,0 +1,21 @@
+import path from 'path';
+
+export default ({ pageType, service, id, variant = '', assetUri }) => {
+  let dataPath;
+
+  switch (pageType) {
+    case 'frontpage':
+    case 'mostRead':
+    case 'secondaryColumn':
+      dataPath = `${variant || 'index'}.json`;
+      break;
+    case 'cpsAssets':
+    case 'legacyAssets':
+      dataPath = `${variant}/${assetUri}.json`;
+      break;
+    default:
+      dataPath = `${id}${variant}.json`;
+  }
+
+  return path.join(process.cwd(), 'data', service, pageType, dataPath);
+};
