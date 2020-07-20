c18e3565f4ff4bd8718190511ba1233a0914ddef
karinathomasbbc <karina.thomas@bbc.co.uk>
2020-07-20 12:49:24 +0100

    Adding test URLs

--- AdHocCypress/cypress/integration/topcat2MediaPlayback/config.js
+++ AdHocCypress/cypress/integration/topcat2MediaPlayback/config.js
@@ -27,7 +27,36 @@ export default {
     'http://localhost:7080/vietnamese/sport/2016/09/160922_tc2_testmap2', // TC2 MAP with video clip
   ],
   test: [
-    'https://www.test.bbc.com/arabic/worldnews/2015/11/151120_t_arabic_av',
+    'https://www.test.bbc.com/afrique/nos_emissions/2016/06/160622_tc2_testmap1', // TC2 MAP video
+    'https://www.test.bbc.com/arabic/worldnews/2015/11/151120_t_arabic_av', // TC2 video
+    'https://www.test.bbc.com/azeri/multimedia/2015/08/150804_azeri_test', // TC2 MAP with video clip
+    'https://www.test.bbc.com/bengali/multimedia/2016/08/160803_tc2_testmap1', // TC2 MAP with video clip
+    'https://www.test.bbc.com/burmese/sport/2016/08/160811_tc2_testmap1', // TC2 MAP with video clip
+    'https://www.test.bbc.com/gahuza/amakuru/2016/02/160215_map_amakuru_test1', // TC2 MAP with video clip
+    'https://www.test.bbc.com/hausa/multimedia/2016/07/160714_tc2_audiomap', // TC2 MAP with audio clip
+    'https://www.test.bbc.com/hindi/sport/2016/08/160822_tc2_testmap1', // TC2 video
+    'https://www.test.bbc.com/indonesia/olahraga/2016/10/161003_tc2_videomap1', // TC2 MAP with video clip
+    'https://www.test.bbc.com/kyrgyz/multimedia/2015/03/150330_map_test', // TC2 MAP with audio clip
+    'https://www.test.bbc.com/mundo/noticias/2016/04/160427_tc2_testmap1', // TC2 MAP video
+    'https://www.test.bbc.com/nepali/multimedia/2016/07/160725_tc2_testmap1', // TC2 MAP with video clip
+    'https://www.test.bbc.com/pashto/world/2016/09/160921_tc2_testmap1', // TC2 MAP
+    'https://www.test.bbc.com/persian/iran/2016/09/160907_tc2_testmap1', // TC2 MAP
+    'https://www.test.bbc.com/portuguese/revista/2016/05/160506_tc2_map_0605', // TC2 MAP video
+    'https://www.test.bbc.com/russian/news/2016/05/160510_tc2_testmap3', // TC2 video
+    'https://www.test.bbc.com/sinhala/multimedia/2016/03/160323_si_test_audio_map', // TC2 MAP with audio clip
+    'https://www.test.bbc.com/somali/maqal_iyo_muuqaal/2016/06/160628_tc2_testmap1', // TC2 MAP with video clip
+    'https://www.test.bbc.com/swahili/michezo/2016/07/160713_tc2_testmap2', // TC2 MAP with audio clip
+    'https://www.test.bbc.com/swahili/michezo/2016/07/160712_tc2_testmap1', // TC2 MAP with video clip
+    'https://www.test.bbc.com/tamil/global/2016/08/160822_tc2_testmap1', // TC2 MAP with video clip
+    'https://www.test.bbc.com/turkce/multimedya/2016/05/160524_tc2_testmap1', // TC2 MAP with video clip
+    'https://www.test.bbc.com/ukchina/simp/multimedia/2016/11/161104_tc2_testmap1', // TC2 video
+    'https://www.test.bbc.com/ukchina/trad/multimedia/2016/11/161104_tc2_testmap1', // TC2 video
+    'https://www.test.bbc.com/ukrainian/entertainment/2016/10/161025_tc2_testmap1', // TC2 Ukrainian video (no Russian TC2 MAP)
+    'https://www.test.bbc.com/urdu/sport/2016/09/160902_tc2_testmap2', // TC2 MAP with video clip
+    'https://www.test.bbc.com/uzbek/multimedia/2016/06/160610_tc2_testmap1', // TC2 MAP Cyrillic
+    'https://www.test.bbc.com/vietnamese/sport/2016/09/160922_tc2_testmap2', // TC2 MAP with video clip
+    'https://www.test.bbc.com/zhongwen/simp/multimedia/2016/11/161107_tc2_testmap1', // TC2 Video
+    'https://www.test.bbc.com/zhongwen/trad/multimedia/2016/11/161107_tc2_testmap1', // TC2 Video
   ],
   live: [
     'https://www.bbc.com/arabic/multimedia/2013/10/131020_belfast_land_art_girl_face_',
