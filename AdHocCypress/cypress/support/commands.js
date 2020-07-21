dca86e428f49836f2d3d58d65a1591a869ac6578
karinathomasbbc <karina.thomas@bbc.co.uk>
2020-07-21 18:22:42 +0100

    Adding a custom command to check that a URL returns the expected status code

--- AdHocCypress/cypress/support/commands.js
+++ AdHocCypress/cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('testResponseCodeAndType', (path, responseCode) => {
+  cy.request({ url: path, failOnStatusCode: false }).then(({ status }) => {
+    expect(status, `Status code for ${path}`).to.equal(responseCode);
+  });
+});
