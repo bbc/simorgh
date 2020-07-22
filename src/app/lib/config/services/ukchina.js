7438e974004f02f2006e3fa3ff2ae0788e0c16c1
Tom Williams <tom.williams07@bbc.co.uk>
2020-07-22 13:42:42 +0100

    Revert "Revert "Remove hasAds from service configs""

--- src/app/lib/config/services/ukchina.js
+++ src/app/lib/config/services/ukchina.js
@@ -40,10 +40,6 @@ const baseServiceConfig = {
 export const service = {
   simp: {
     ...baseServiceConfig,
-    ads: {
-      hasAds: false,
-      advertisementLabel: '广告',
-    },
     externalLinkText: ', 外部链接',
     frontPageTitle: '主页',
     lang: `zh-hans`,
@@ -123,6 +119,9 @@ export const service = {
       variant: 'trad',
     },
     translations: {
+      ads: {
+        advertisementLabel: '广告',
+      },
       seeAll: '浏览全部',
       home: '主页',
       currentPage: '目前页面',
@@ -245,10 +244,6 @@ export const service = {
   },
   trad: {
     ...baseServiceConfig,
-    ads: {
-      hasAds: false,
-      advertisementLabel: '廣告',
-    },
     lang: `zh-hant`,
     locale: `zh-hant`,
     externalLinkText: ', 外部鏈接',
@@ -325,6 +320,9 @@ export const service = {
       variant: 'simp',
     },
     translations: {
+      ads: {
+        advertisementLabel: '廣告',
+      },
       seeAll: '瀏覽全部',
       home: '主頁',
       currentPage: '目前頁面',
