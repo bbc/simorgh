b159f3b6aaaa6a0357bd1b705c6368bb6f6030dd
karinathomasbbc <karina.thomas@bbc.co.uk>
2020-07-24 19:13:09 +0100

    Toggles in alphabetical order

--- src/app/lib/config/toggles/live/index.js
+++ src/app/lib/config/toggles/live/index.js
@@ -5,11 +5,28 @@ export default {
   chartbeatAnalytics: {
     enabled: true,
   },
+  cinemaModeTV: {
+    enabled: false,
+  },
   comscoreAnalytics: {
     enabled: false,
   },
+  cpsRecommendations: {
+    enabled: false,
+  },
+  enableFetchingToggles: {
+    enabled: true,
+    value:
+      '(afaanoromoo|afrique|amharic|arabic|archive|azeri|bengali|burmese|cymrufyw|gahuza|gujarati|hausa|hindi|igbo|indonesia|japanese|korean|kyrgyz|marathi|mundo|naidheachdan|nepali|news|pashto|persian|pidgin|portuguese|punjabi|russian|scotland|serbian|sinhala|somali|swahili|tamil|telugu|thai|tigrinya|turkce|ukchina|ukrainian|urdu|uzbek|vietnamese|yoruba|zhongwen)',
+  },
   include: {
     enabled: true,
+  },
+  liveRadioSchedule: {
+    enabled: false,
+  },
+  logMediaPlayerStatus: {
+    enabled: false,
   },
   mediaPlayer: {
     enabled: true,
@@ -20,33 +37,19 @@ export default {
   navOnArticles: {
     enabled: true,
   },
+  prerollAds: {
+    enabled: true,
+  },
   radioSchedule: {
     enabled: true,
   },
   scriptLink: {
     enabled: true,
   },
-  variantCookie: {
-    enabled: true,
-  },
-  enableFetchingToggles: {
-    enabled: false,
-    value:
-      '(afaanoromoo|afrique|amharic|arabic|archive|azeri|bengali|burmese|cymrufyw|gahuza|gujarati|hausa|hindi|igbo|indonesia|japanese|korean|kyrgyz|marathi|mundo|naidheachdan|nepali|news|pashto|persian|pidgin|portuguese|punjabi|russian|scotland|serbian|sinhala|somali|swahili|tamil|telugu|thai|tigrinya|turkce|ukchina|ukrainian|urdu|uzbek|vietnamese|yoruba|zhongwen)',
-  },
   socialEmbed: {
     enabled: true,
   },
-  logMediaPlayerStatus: {
-    enabled: false,
-  },
-  prerollAds: {
+  variantCookie: {
     enabled: true,
-  },
-  cpsRecommendations: {
-    enabled: false,
-  },
-  cinemaModeTV: {
-    enabled: false,
   },
 };
