3f81bc8493328df180711bf928e085217eb73809
karinathomasbbc <karina.thomas@bbc.co.uk>
2020-07-28 16:17:20 +0100

    Refactor radio schedule utilities for reusing in cypress tests

--- src/app/containers/RadioSchedule/utilities/processRadioSchedule.js
+++ src/app/containers/RadioSchedule/utilities/processRadioSchedule.js
@@ -1,8 +1,7 @@
-import findLastIndex from 'ramda/src/findLastIndex';
-import propSatisfies from 'ramda/src/propSatisfies';
 import path from 'ramda/src/path';
 import nodeLogger from '#lib/logger.node';
 import { RADIO_SCHEDULE_DATA_INCOMPLETE_ERROR } from '#lib/logger.const';
+import { getLastProgramIndex, isScheduleDataComplete } from '.';
 
 const logger = nodeLogger(__filename);
 
@@ -29,23 +28,6 @@ const logProgramError = ({ error, service }) => {
     error,
     service,
   });
-};
-
-const getLastProgramIndex = ({ schedules, currentTime }) => {
-  return findLastIndex(
-    propSatisfies(time => time < currentTime, 'publishedTimeStart'),
-  )(schedules);
-};
-
-export const isScheduleDataComplete = ({
-  schedules,
-  currentTime = Date.now(),
-}) => {
-  // finding latest program, that may or may not still be live. this is because there isn't
-  // always a live program, in which case we show the most recently played program on demand.
-  const latestProgramIndex = getLastProgramIndex({ schedules, currentTime });
-
-  return schedules[latestProgramIndex - 2] && schedules[latestProgramIndex + 1];
 };
 
 export default (data, service, currentTime) => {
