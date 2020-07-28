e85e36f58d781092b71bb468a05ea45851109355
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-07-28 09:19:23 +0100

    fix error page tests

--- src/app/pages/ErrorPage/index.test.jsx
+++ src/app/pages/ErrorPage/index.test.jsx
@@ -1,41 +1,42 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
-import { ServiceContextProvider } from '#contexts/ServiceContext';
-import ErrorPage from '.';
+import ERROR_PAGE from '.';
+
+// eslint-disable-next-line react/prop-types
+const ErrorPage = ({ errorCode, service }) => (
+  <ERROR_PAGE
+    errorCode={errorCode}
+    service={service}
+    bbcOrigin="https://www.bbc.co.uk"
+    pathname={`/${service}`}
+    pageType="error"
+    isAmp={false}
+  />
+);
 
 describe('ErrorPage', () => {
   shouldMatchSnapshot(
     'should correctly render for 404',
-    <ServiceContextProvider service="news">
-      <ErrorPage errorCode={404} />
-    </ServiceContextProvider>,
+    <ErrorPage service="news" errorCode={404} />,
   );
 
   shouldMatchSnapshot(
     'should correctly render for 500',
-    <ServiceContextProvider service="news">
-      <ErrorPage errorCode={500} />
-    </ServiceContextProvider>,
+    <ErrorPage service="news" errorCode={500} />,
   );
 
   shouldMatchSnapshot(
     'should correctly render for other status code',
-    <ServiceContextProvider service="news">
-      <ErrorPage errorCode={123} />
-    </ServiceContextProvider>,
+    <ErrorPage service="news" errorCode={123} />,
   );
 
   shouldMatchSnapshot(
     'should correctly render for 404 for persian',
-    <ServiceContextProvider service="persian">
-      <ErrorPage errorCode={404} />
-    </ServiceContextProvider>,
+    <ErrorPage service="persian" errorCode={404} />,
   );
 
   shouldMatchSnapshot(
     'should correctly render for 500 for persian',
-    <ServiceContextProvider service="persian">
-      <ErrorPage errorCode={500} />
-    </ServiceContextProvider>,
+    <ErrorPage service="news" errorCode={500} />,
   );
 });
