854735beb1d1045ac62bff8ab7ac81aaa15fe529
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-07-28 15:56:55 +0100

    mock withPageWrapper in PhotoGalleryPage tests

--- src/app/pages/PhotoGalleryPage/index.test.jsx
+++ src/app/pages/PhotoGalleryPage/index.test.jsx
@@ -8,93 +8,39 @@ import assocPath from 'ramda/src/assocPath';
 import { ServiceContextProvider } from '#contexts/ServiceContext';
 import { RequestContextProvider } from '#contexts/RequestContext';
 import { ToggleContext } from '#contexts/ToggleContext';
-import { PhotoGalleryPage as AsyncPhotoGalleryPage } from '..';
+import PhotoGalleryPage from '.';
 import noOnwardJourneys from '#data/pidgin/cpsAssets/sport-23252855';
 import someCpsOnwardJourneys from '#data/azeri/cpsAssets/azerbaijan-44208474.json';
 import allCpsOnwardJourneys from '#data/pidgin/cpsAssets/tori-49221071.json';
 import pglAboutData from '#data/afaanoromoo/cpsAssets/oduu-41217768';
 import getInitialData from '#app/routes/cpsAsset/getInitialData';
 
-let PhotoGalleryPage;
-
-beforeAll(async () => {
-  PhotoGalleryPage = (await AsyncPhotoGalleryPage.load()).default;
+jest.mock('#containers/PageHandlers/withPageWrapper', () => Component => {
+  return props => <Component {...props} />;
 });
 
-const toggleState = {
-  mediaPlayer: {
-    enabled: true,
+jest.mock('#lib/config/toggles', () => ({
+  local: {
+    enableFetchingToggles: { enabled: false },
+    mediaPlayer: {
+      enabled: true,
+    },
   },
-};
+}));
 
 const createAssetPage = ({ pageData }, service) => (
   <StaticRouter>
-    <ToggleContext.Provider value={{ toggleState, toggleDispatch: jest.fn() }}>
-      <ServiceContextProvider service={service}>
-        <RequestContextProvider
-          bbcOrigin="https://www.test.bbc.co.uk"
-          isAmp={false}
-          pageType={pageData.metadata.type}
-          pathname={pageData.metadata.locators.assetUri}
-          service={service}
-          statusCode={200}
-        >
-          <PhotoGalleryPage service={service} pageData={pageData} />
-        </RequestContextProvider>
-      </ServiceContextProvider>
-    </ToggleContext.Provider>
+    <PhotoGalleryPage
+      service={service}
+      pageData={pageData}
+      bbcOrigin="https://www.test.bbc.co.uk"
+      isAmp={false}
+      pageType={pageData.metadata.type}
+      pathname={pageData.metadata.locators.assetUri}
+      status={200}
+    />
   </StaticRouter>
 );
-
-jest.mock('#containers/PageHandlers/withPageWrapper', () => Component => {
-  const PageWrapperContainer = props => (
-    <div id="PageWrapperContainer">
-      <Component {...props} />
-    </div>
-  );
-
-  return PageWrapperContainer;
-});
-
-jest.mock('#containers/PageHandlers/withLoading', () => Component => {
-  const LoadingContainer = props => (
-    <div id="LoadingContainer">
-      <Component {...props} />
-    </div>
-  );
-
-  return LoadingContainer;
-});
-
-jest.mock('#containers/PageHandlers/withError', () => Component => {
-  const ErrorContainer = props => (
-    <div id="ErrorContainer">
-      <Component {...props} />
-    </div>
-  );
-
-  return ErrorContainer;
-});
-
-jest.mock('#containers/PageHandlers/withData', () => Component => {
-  const DataContainer = props => (
-    <div id="DataContainer">
-      <Component {...props} />
-    </div>
-  );
-
-  return DataContainer;
-});
-
-jest.mock('#containers/PageHandlers/withContexts', () => Component => {
-  const ContextsContainer = props => (
-    <div id="ContextsContainer">
-      <Component {...props} />
-    </div>
-  );
-
-  return ContextsContainer;
-});
 
 describe('Photo Gallery Page', () => {
   describe('snapshots', () => {
