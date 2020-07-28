77d0df105055ab855b1100aa02c14f6a3b520441
karinathomasbbc <karina.thomas@bbc.co.uk>
2020-07-28 19:32:59 +0100

    Tidy up config file - remove URLs which don't work

--- AdHocCypress/cypress/integration/topcat2MediaPlayback/config.js
+++ AdHocCypress/cypress/integration/topcat2MediaPlayback/config.js
@@ -2,29 +2,14 @@ export default {
   local: [
     'http://localhost:7080/afrique/nos_emissions/2016/06/160622_tc2_testmap1', // TC2 MAP video
     'http://localhost:7080/arabic/worldnews/2015/11/151120_t_arabic_av', // TC2 video
-    // 'http://localhost:7080/azeri/multimedia/2015/08/150804_azeri_test', // TC2 MAP with video clip
-    // 'http://localhost:7080/hausa/multimedia/2016/07/160714_tc2_audiomap', // TC2 MAP with audio clip
     'http://localhost:7080/hindi/sport/2016/08/160822_tc2_testmap1', // TC2 video
-    // 'http://localhost:7080/indonesia/olahraga/2016/10/161003_tc2_videomap1', // TC2 MAP with video clip
-    // 'http://localhost:7080/kyrgyz/multimedia/2015/03/150330_map_test', // TC2 MAP with audio clip
     'http://localhost:7080/mundo/noticias/2016/04/160427_tc2_testmap1', // TC2 MAP
-    // 'http://localhost:7080/nepali/multimedia/2016/07/160725_tc2_testmap1', // TC2 MAP with video clip
-    // 'http://localhost:7080/pashto/world/2016/09/160921_tc2_testmap1', // TC2 MAP with video clip
-    // 'http://localhost:7080/persian/iran/2016/09/160907_tc2_testmap1', // TC2 MAP with video clip
     'http://localhost:7080/portuguese/revista/2016/05/160506_tc2_map_0605', // TC2 MAP
-    // 'http://localhost:7080/russian/multimedia/2016/05/160505_v_diving_record', // TC2 MAP with video
-    // 'http://localhost:7080/sinhala/multimedia/2016/03/160323_si_test_audio_map', // TC2 MAP with audio clip
-    // 'http://localhost:7080/somali/maqal_iyo_muuqaal/2016/06/160628_tc2_testmap1', // TC2 MAP with video clip
-    // 'http://localhost:7080/swahili/michezo/2016/07/160713_tc2_testmap2', // TC2 MAP with audio clip
     'http://localhost:7080/swahili/michezo/2016/07/160712_tc2_testmap1', // TC2 MAP with video clip
-    // 'http://localhost:7080/tamil/global/2016/08/160822_tc2_testmap1', // TC2 MAP with video clip
-    // 'http://localhost:7080/turkce/multimedya/2016/05/160524_tc2_testmap1', // TC2 MAP with video clip
     'http://localhost:7080/ukchina/simp/multimedia/2016/11/161104_tc2_testmap1', // TC2 video
     'http://localhost:7080/ukchina/trad/multimedia/2016/11/161104_tc2_testmap1', // TC2 video
     'http://localhost:7080/ukrainian/entertainment/2016/10/161025_tc2_testmap1', // TC2 Ukrainian (no Russian TC2 MAP)
-    // 'http://localhost:7080/urdu/sport/2016/09/160902_tc2_testmap2', // TC2 MAP with video clip
     'http://localhost:7080/uzbek/multimedia/2016/06/160610_tc2_testmap1', // TC2 MAP Cyrillic
-    // 'http://localhost:7080/vietnamese/sport/2016/09/160922_tc2_testmap2', // TC2 MAP with video clip
   ],
   test: [
     'https://www.test.bbc.com/afrique/nos_emissions/2016/06/160622_tc2_testmap1.amp', // TC2 MAP video
@@ -67,7 +52,6 @@ export default {
     'https://www.test.bbc.com/burmese/multimedia/2016/01/160108_korean_cook?renderer_env=live',
     'https://www.test.bbc.com/burmese/multimedia/2016/07/160706_cooltech_zawgyi_facebook_unicode?renderer_env=live',
     'https://www.test.bbc.com/gahuza/video/2015/12/151201_100womenburundi?renderer_env=live',
-    // 'https://www.test.bbc.com/gahuza/ibindi_wifashisha/2010/10/101006_kidum_on_east_africa?renderer_env=live',
     'https://www.test.bbc.com/hausa/multimedia/2012/07/120712_click?renderer_env=live',
     'https://www.test.bbc.com/hausa/multimedia/2014/09/140922_ebola_sierraleone_lockdown?renderer_env=live',
     'https://www.test.bbc.com/hindi/multimedia/2015/07/150703_jharkhand_maoist_fake_surrender_sr?renderer_env=live',
