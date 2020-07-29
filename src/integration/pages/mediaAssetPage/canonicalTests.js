d14d68137593040d1b0468db78ec7dd01265f941
RichardPK <r.phillips.kerr@gmail.com>
2020-07-29 12:19:09 +0100

    Added integration test

--- src/integration/pages/mediaAssetPage/canonicalTests.js
+++ src/integration/pages/mediaAssetPage/canonicalTests.js
@@ -8,9 +8,19 @@ export default () => {
   runCanonicalAnalyticsTests();
 
   it('I can see the placeholder loading image', () => {
-    // const headlineEl = document.querySelector('h1[id="content"]');
-    // expect(headlineEl).toBeInTheDocument();
-    // expect(headlineEl.textContent).toBeTruthy();
-    // expect(headlineEl.textContent).toMatchSnapshot();
+    const loadingImageWrapper = document.querySelector(
+      "div[class^='LoadingImageWrapper']",
+    );
+    expect(loadingImageWrapper).toBeInTheDocument();
+    expect(loadingImageWrapper).toMatchSnapshot();
+  });
+
+  it('outer iframe has z-index of 1', () => {
+    const iframe = document.querySelector('iframe');
+    expect(iframe).toBeInTheDocument();
+    expect(window.getComputedStyle(iframe).getPropertyValue('z-index')).toEqual(
+      '1',
+    );
+    expect(iframe).toMatchSnapshot();
   });
 };
