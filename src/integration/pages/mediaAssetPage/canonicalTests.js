828e15a7d48f1cc58dfb2186324830d591fc1037
RichardPK <r.phillips.kerr@gmail.com>
2020-07-29 08:32:57 +0100

    Added placeholder loading image integration test for map

--- src/integration/pages/mediaAssetPage/canonicalTests.js
+++ src/integration/pages/mediaAssetPage/canonicalTests.js
@@ -6,4 +6,11 @@ import {
 export default () => {
   runCoreCanonicalTests();
   runCanonicalAnalyticsTests();
+
+  it('I can see the placeholder loading image', () => {
+    // const headlineEl = document.querySelector('h1[id="content"]');
+    // expect(headlineEl).toBeInTheDocument();
+    // expect(headlineEl.textContent).toBeTruthy();
+    // expect(headlineEl.textContent).toMatchSnapshot();
+  });
 };
