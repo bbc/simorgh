845eb4df822b2a182d99ef9633f0b476081fba88
thekp <khoa.phan@bbc.co.uk>
2020-08-03 16:21:43 +0100

    move comscore to public/static/js

--- src/server/local/index.js
+++ src/server/local/index.js
@@ -155,8 +155,11 @@ export default server => {
       const dataFilePath = path.join(process.cwd(), 'data', idx, 'index.json');
       sendDataFile(res, dataFilePath, next);
     })
-    .get('/comscore.js', (req, res, next) => {
-      const localComscorePath = path.join(process.cwd(), '/public/comscore.js');
+    .get('/static/js/comscore.js', (req, res, next) => {
+      const localComscorePath = path.join(
+        process.cwd(),
+        '/public/static/js/comscore.js',
+      );
       sendDataFile(res, localComscorePath, next);
     })
     .get('/ckns_policy/*', (req, res) => {
