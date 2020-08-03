8b351f2326f583d57b536bca0411fa4c41811926
Fahad Khan <fahad.khan01@bbc.co.uk>
2020-08-03 17:28:20 +0100

    replace GridItem with Grid

--- src/app/containers/Paragraph/index.jsx
+++ src/app/containers/Paragraph/index.jsx
@@ -1,12 +1,20 @@
 import React, { useContext } from 'react';
 import Paragraph from '@bbc/psammead-paragraph';
+import Grid from '@bbc/psammead-grid';
 import { ServiceContext } from '#contexts/ServiceContext';
 import Blocks from '../Blocks';
 import fragment from '../Fragment';
 import InlineLink from '../InlineLink';
 import Inline from '../InlineContainer';
 import { paragraphModelPropTypes } from '#models/propTypes/paragraph';
-import { GridItemConstrainedMedium } from '#lib/styledGrid';
+
+const parentsGridColumns = {
+  group1: 6,
+  group2: 6,
+  group3: 6,
+  group4: 8,
+  group5: 10,
+};
 
 const componentsToRender = { fragment, urlLink: InlineLink, inline: Inline };
 
@@ -14,14 +22,29 @@ const ParagraphContainer = ({ blocks }) => {
   const { script, service } = useContext(ServiceContext);
 
   return (
-    <GridItemConstrainedMedium>
-      <Paragraph script={script} service={service}>
-        <Blocks blocks={blocks} componentsToRender={componentsToRender} />
-      </Paragraph>
-    </GridItemConstrainedMedium>
+    <Grid columns={parentsGridColumns}>
+      <Grid
+        item
+        columns={{ group1: 6, group2: 6, group3: 6, group4: 8, group5: 10 }}
+      >
+        <Paragraph script={script} service={service}>
+          <Blocks blocks={blocks} componentsToRender={componentsToRender} />
+        </Paragraph>
+      </Grid>
+    </Grid>
   );
 };
 
 ParagraphContainer.propTypes = paragraphModelPropTypes;
+
+ParagraphContainer.defaultProps = {
+  parentsGridColumns: {
+    group1: 6,
+    group2: 6,
+    group3: 6,
+    group4: 8,
+    group5: 10,
+  },
+};
 
 export default ParagraphContainer;
