b43131e5e041a669d3a722bc11c9841c6fbd94b3
bennea27 <andrew.bennett07@bbc.co.uk>
2020-08-04 15:40:32 +0100

    update bundleSizeConfig comment to make clearer the expectation on maintaining these thresholds

--- scripts/bundleSize/bundleSizeConfig.js
+++ scripts/bundleSize/bundleSizeConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Size limit for all bundles used by each service (K)
-  // Keep these +/- 5K and update frequently!
+  // Keep the MAX_SIZE +5 above largest value and MIN_SIZE -5 below smallest value
   MIN_SIZE: 686,
   MAX_SIZE: 833,
 };
