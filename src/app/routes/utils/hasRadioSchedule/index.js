98ffe9d852bd96549a14ab3d6323071c04fb1b42
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-08-07 14:38:07 +0100

    remove accidentally comitted persian checks

--- src/app/routes/utils/hasRadioSchedule/index.js
+++ src/app/routes/utils/hasRadioSchedule/index.js
@@ -1,12 +1,8 @@
 import path from 'ramda/src/path';
 import getConfig from '#app/routes/utils/getConfig';
 
-export default async ({ service, pageType, pathname }) => {
+export default async ({ service, pageType }) => {
   const config = await getConfig(service);
-
-  if (service === 'persian' && pathname.includes('bbc_persian_radio')) {
-    return 'persian';
-  }
 
   const configKeyName = {
     liveRadio: 'onLiveRadioPage',
