571a83bc74e89a20bdf9346dac834d9ec81052a0
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-08-10 16:55:51 +0100

    update bundle sizes

--- scripts/bundleSize/bundleSizeConfig.js
+++ scripts/bundleSize/bundleSizeConfig.js
@@ -4,6 +4,6 @@ module.exports = {
   // Keep the MAX_SIZE +5 above the largest value and MIN_SIZE -5
   // below the smallest value in the build output; this avoids the
   // need for frequent changes as bundle sizes fluctuate.
-  MIN_SIZE: 681,
-  MAX_SIZE: 848,
+  MIN_SIZE: 657,
+  MAX_SIZE: 823,
 };
