10373f4c0c16e292e65e841bdf2dd36cdaba6e97
LilyL0u <lily.higham@bbc.co.uk>
2020-08-11 12:43:21 +0100

    added schedule test, updated settings

--- cypress/integration/pages/onDemandRadio/testsForCanonicalOnly.js
+++ cypress/integration/pages/onDemandRadio/testsForCanonicalOnly.js
@@ -1,7 +1,9 @@
 import envConfig from '../../../support/config/envs';
+import { serviceHasRadioSchedule } from '../liveRadio/helper';
+import { isScheduleDataComplete } from '../../../../src/app/containers/RadioSchedule/utilities/evaluateScheduleData';
 
-export default ({ service, pageType }) => {
-  describe(`testsForCanonicalOnly for ${service} ${pageType}`, () => {
+export default ({ service, pageType, variant }) => {
+  describe(`testsForCanonicalOnly for ${service} ${pageType} ${variant}`, () => {
     describe('Chartbeat', () => {
       if (envConfig.chartbeatEnabled) {
         it('should have a script with src value set to chartbeat source', () => {
@@ -11,6 +13,46 @@ export default ({ service, pageType }) => {
           cy.hasGlobalChartbeatConfig();
         });
       }
+    });
+    describe('Radio Schedule', () => {
+      it('should be displayed if there is enough schedule data', () => {
+        const isRadioScheduleOnPage = serviceHasRadioSchedule({
+          service,
+          variant,
+        });
+        cy.log(
+          `On Demand Radio Page configured for Radio Schedule? ${isRadioScheduleOnPage}`,
+        );
+
+        if (isRadioScheduleOnPage) {
+          const schedulePath = Cypress.env('currentPath');
+          const index = schedulePath.lastIndexOf('_radio/');
+          let newUrl = `${schedulePath.substring(
+            0,
+            index + 6,
+          )}/schedule.json?renderer_env=live`;
+          newUrl = newUrl.replace('bbc_afaanoromoo_radio', 'bbc_oromo_radio');
+          cy.log(newUrl);
+
+          cy.request(newUrl).then(({ body: scheduleJson }) => {
+            const { schedules } = scheduleJson;
+
+            const isRadioScheduleDataComplete = isScheduleDataComplete({
+              schedules,
+            });
+
+            cy.log(
+              `Radio Schedule is displayed? ${isRadioScheduleDataComplete}`,
+            );
+            if (isRadioScheduleOnPage && isRadioScheduleDataComplete) {
+              cy.log('Schedule has enough data');
+              cy.get('[data-e2e=radio-schedule]').should('exist');
+            } else {
+              cy.get('[data-e2e=radio-schedule]').should('not.exist');
+            }
+          });
+        }
+      });
     });
   });
 };
