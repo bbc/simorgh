201a4a6f660a8a38a22c0a7c2e52b2da68216b45
BBC News CI <foo@bar.com>
2020-08-13 11:45:07 +0000

    Updates

--- ArticlePage.6dbfd419dda4903e71a8.bundle.js
+++ ArticlePage.6dbfd419dda4903e71a8.bundle.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[0],{826:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var _ArticlePage__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(751),_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__(248);__webpack_exports__.default=Object(_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__.a)({addVariantHandling:!0})(_ArticlePage__WEBPACK_IMPORTED_MODULE_0__.a)}}]);
-//# sourceMappingURL=ArticlePage.dc95e3889386b5aa9bfb.bundle.js.map
+//# sourceMappingURL=ArticlePage.6dbfd419dda4903e71a8.bundle.js.map
