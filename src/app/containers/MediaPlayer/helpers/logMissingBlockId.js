e36efc633d1ed3db10be27d25b42a8460bf9a564
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2020-08-13 16:58:10 +0100

    Update warning message

--- src/app/containers/MediaPlayer/helpers/logMissingBlockId.js
+++ src/app/containers/MediaPlayer/helpers/logMissingBlockId.js
@@ -1,12 +1,12 @@
 import nodeLogger from '#lib/logger.node';
-import { MEDIA_PLAYER_STATUS } from '#lib/logger.const';
+import { MEDIA_MISSING_FIELD } from '#lib/logger.const';
 
 const logger = nodeLogger(__filename);
 
 const logMissingBlockId = ({ url, assetType }) => {
   const blockIdError = 'Missing Block ID';
   const message = { url, assetType, blockIdError };
-  logger.warn(MEDIA_PLAYER_STATUS, message);
+  logger.warn(MEDIA_MISSING_FIELD, message);
 };
 
 export default logMissingBlockId;
