3d40447c7ab3c2a4b8a0cde622d010a978661a17
PriyaKR <Priyatharsini.Kuduvarajan@bbc.co.uk>
2020-08-17 15:55:53 +0100

    added integration test for ads

--- src/integration/pages/frontPage/canonicalTests.js
+++ src/integration/pages/frontPage/canonicalTests.js
@@ -1,4 +1,5 @@
 import runCrossPlatformTests from './crossPlatformTests';
+import AdContainer from '../../../../src/app/containers/Ad';
 import {
   runCoreCanonicalTests,
   runCanonicalAnalyticsTests,
@@ -19,6 +20,25 @@ export default () => {
       });
     } else {
       it('should not be in the document', () => {
+        expect(id).not.toBeInTheDocument();
+      });
+    }
+  });
+
+  describe('Ads', () => {
+    const hasAds = service === 'arabic';
+    const id = document.getElementById('google_image_div');
+    jest.mock('AdContainer', () => ({ leaderboard }) => ({
+      isAmp: '1',
+      adsEnabled: true,
+    }));
+
+    if (hasAds) {
+      it('leaderboard ad should be in the document', () => {
+        expect(id).toBeInTheDocument();
+      });
+    } else {
+      it('leaderboard ad should not be in the document', () => {
         expect(id).not.toBeInTheDocument();
       });
     }
