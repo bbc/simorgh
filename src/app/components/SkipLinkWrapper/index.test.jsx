7efa38257f24f0a8fcae0826bb266cc3e1a281b0
rowland <rowland.henshaw@andela.com>
2020-08-18 12:55:51 +0100

    updated test for skip link wrapper

--- src/app/components/SkipLinkWrapper/index.test.jsx
+++ src/app/components/SkipLinkWrapper/index.test.jsx
@@ -25,4 +25,19 @@ describe('SkipLinkWrapper', () => {
     expect(skipLink.getAttribute('href')).toEqual('#end-of-content');
     expect(skipLink.textContent).toEqual('Skip Foo Bar content');
   });
+
+  it('should return child elements without skip link when a skip text prop is not passed in', () => {
+    const terms = { '%foo_bar%': 'Foo Bar' };
+    const { container } = render(
+      <SkipLinkWrapper service="news" terms={terms} endTextId="end-of-content">
+        <span>Foo Bar</span>
+      </SkipLinkWrapper>,
+    );
+
+    const links = container.querySelectorAll('a');
+    const spans = container.querySelectorAll('span');
+
+    expect(links).toHaveLength(0);
+    expect(spans[0].textContent).toEqual('Foo Bar');
+  });
 });
