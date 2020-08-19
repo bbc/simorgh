1954af335462e0afa5e8ad2f7d22c9c305debf28
rowland <rowland.henshaw@andela.com>
2020-08-19 11:18:46 +0100

    fixed prop issues after dependency bump

--- src/app/lib/utilities/episodeAvailability/withMediaError/index.jsx
+++ src/app/lib/utilities/episodeAvailability/withMediaError/index.jsx
@@ -1,33 +1,51 @@
 import React from 'react';
-import { shape, oneOf } from 'prop-types';
+import { element, shape, oneOf } from 'prop-types';
 import { EPISODE_STATUS } from '..';
 import ErrorMessage from '../ErrorMessage';
 
-const withMediaError = PageComponent => props => {
-  const {
-    pageData: { episodeAvailability },
-  } = props;
+const withMediaError = PageComponent => {
+  const MediaErrorComponent = props => {
+    const {
+      pageData: { episodeAvailability },
+    } = props;
+    const mediaIsAvailable =
+      episodeAvailability === EPISODE_STATUS.EPISODE_IS_AVAILABLE;
 
-  const mediaIsAvailable =
-    episodeAvailability === EPISODE_STATUS.EPISODE_IS_AVAILABLE;
+    const ErrorComponent = errorProps => (
+      <ErrorMessage {...errorProps} episodeAvailability={episodeAvailability} />
+    );
 
-  const ErrorComponent = errorProps => (
-    <ErrorMessage {...errorProps} episodeAvailability={episodeAvailability} />
-  );
+    ErrorComponent.propTypes = {
+      pageData: shape({
+        episodeAvailability: oneOf(Object.values(EPISODE_STATUS)).isRequired,
+      }),
+    };
 
-  return (
-    <PageComponent
-      {...props}
-      mediaIsAvailable={mediaIsAvailable}
-      MediaError={mediaIsAvailable ? () => null : ErrorComponent}
-    />
-  );
+    ErrorComponent.defaultProps = {
+      pageData: null,
+    };
+    return (
+      <PageComponent
+        {...props}
+        mediaIsAvailable={mediaIsAvailable}
+        MediaError={mediaIsAvailable ? () => null : ErrorComponent}
+      />
+    );
+  };
+
+  MediaErrorComponent.propTypes = {
+    pageData: shape({
+      episodeAvailability: oneOf(Object.values(EPISODE_STATUS)).isRequired,
+    }),
+  };
+
+  MediaErrorComponent.defaultProps = {
+    pageData: null,
+  };
+  return MediaErrorComponent;
 };
 
 withMediaError.propTypes = {
-  pageData: shape({
-    episodeAvailability: oneOf(Object.values(EPISODE_STATUS)).isRequired,
-  }),
+  PageComponent: element,
 };
-
 export default withMediaError;
