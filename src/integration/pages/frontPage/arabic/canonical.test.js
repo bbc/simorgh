dbf5e620421188d38d99a850ea352488e384f25a
PriyaKR <Priyatharsini.Kuduvarajan@bbc.co.uk>
2020-08-21 15:53:29 +0100

    added integration test for ads

--- src/integration/pages/frontPage/arabic/canonical.test.js
+++ src/integration/pages/frontPage/arabic/canonical.test.js
@@ -1,6 +1,7 @@
 /**
  * @service arabic
  * @pathname /arabic
+ * @displayAds true
  */
 
 import runCrossPlatformTests from '../crossPlatformTests';
@@ -11,4 +12,18 @@ describe('Canonical', () => {
     runCrossPlatformTests();
     runCanonicalTests();
   });
+});
+
+describe('Canonical Ads', () => {
+  const hasAds = service === 'arabic';
+  const id = document.getElementById('dotcom-leaderboard');
+  if (hasAds) {
+    it('ad should be in the document', () => {
+      expect(id).toBeInTheDocument();
+    });
+  } else {
+    it('ad should not be in the document', () => {
+      expect(id).not.toBeInTheDocument();
+    });
+  }
 });
