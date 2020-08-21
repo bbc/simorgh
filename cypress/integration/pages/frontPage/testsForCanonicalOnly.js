04c78f73eba6a8d91150b0dc6b90e0118e234ee0
PriyaKR <Priyatharsini.Kuduvarajan@bbc.co.uk>
2020-08-21 17:43:29 +0100

    linting fixes

--- cypress/integration/pages/frontPage/testsForCanonicalOnly.js
+++ cypress/integration/pages/frontPage/testsForCanonicalOnly.js
@@ -8,13 +8,12 @@ export const testsThatAlwaysRunForCanonicalOnly = ({ service, pageType }) => {
 export const testsThatFollowSmokeTestConfigForCanonicalOnly = ({
   service,
   pageType,
-}) => {
+}) =>
   describe(`Canonical Tests for ${service} ${pageType}`, () => {
     it('should not have an AMP attribute', () => {
       cy.get('html').should('not.have.attr', 'amp');
     });
   });
-};
 
 // For testing low priority things e.g. cosmetic differences, and a safe place to put slow tests.
 export const testsThatNeverRunDuringSmokeTestingForCanonicalOnly = ({
