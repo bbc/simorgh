6d6263cb1d249b56253044f3342805fe28a115ab
Denis Hernandez <denis.hernandezesteban@bbc.co.uk>
2020-08-24 10:39:29 +0100

    Move tests

--- src/integration/pages/frontPage/arabic/canonical.test.js
+++ src/integration/pages/frontPage/arabic/canonical.test.js
@@ -13,20 +13,3 @@ describe('Canonical', () => {
     runCanonicalTests();
   });
 });
-
-describe('Canonical Ads', () => {
-  const hasAds = service === 'arabic';
-  const leaderboardId = document.getElementById('dotcom-leaderboard');
-  const mpuId = document.getElementById('dotcom-mpu');
-  if (hasAds) {
-    it('ad should be in the document', () => {
-      expect(leaderboardId).toBeInTheDocument();
-      expect(mpuId).toBeInTheDocument();
-    });
-  } else {
-    it('ad should not be in the document', () => {
-      expect(leaderboardId).not.toBeInTheDocument();
-      expect(mpuId).not.toBeInTheDocument();
-    });
-  }
-});
