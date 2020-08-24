4ace5a1ddb6f2cae12ef2950beea3739e5e5825b
Gerald Pharin <gerald.pharin.ext@bbc.co.uk>
2020-08-24 09:57:34 +0000

    Update log event name

--- src/app/lib/utilities/filterPopularStaleData/index.js
+++ src/app/lib/utilities/filterPopularStaleData/index.js
@@ -19,12 +19,12 @@ const filterPopularStaleData = ({
   }
   // The ARES test endpoint for most read/watched renders fixture data, so the data is stale
   const isTest = process.env.SIMORGH_APP_ENV === 'test';
-  const logConstant =
+  const logEvent =
     popularType === 'mostRead' ? MOST_READ_STALE_DATA : MOST_WATCHED_STALE_DATA;
   // Do not show most read if lastRecordUpdated is greater than 60min as this means PopAPI has failed twice
   // in succession. This suggests ATI may be having issues, hence risk of stale data.
   if (!isTest && isDataStale(data.lastRecordTimeStamp)) {
-    logger.warn(logConstant, {
+    logger.warn(logEvent, {
       message: 'lastRecordTimeStamp is greater than 60min',
       lastRecordTimeStamp: data.lastRecordTimeStamp,
       generated: data.generated,
