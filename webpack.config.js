481606df998d6d8ad749f9966bf2816f18ba57a2
Chris <chris.hinds@bbc.co.uk>
2020-08-25 08:37:23 +0100

    Create docker-compose file for building and running simorgh

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,12 @@ module.exports = (shell = {}) => {
     },
     devServer: {
       stats,
+      port: 80, // use any port suitable for your configuration
+      host: '0.0.0.0', // to accept connections from outside container
+      watchOptions: {
+        aggregateTimeout: 500, // delay before reloading
+        poll: 1000, // enable polling since fsevents are not supported in docker
+      },
     },
     stats,
     node: {
