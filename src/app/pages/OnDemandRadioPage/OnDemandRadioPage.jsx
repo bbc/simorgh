1a7a2bbb43f341afe2372e59d1b3f485de75da08
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2020-08-28 15:42:16 +0100

    Fixing scrub bar icon

--- src/app/pages/OnDemandRadioPage/OnDemandRadioPage.jsx
+++ src/app/pages/OnDemandRadioPage/OnDemandRadioPage.jsx
@@ -60,11 +60,13 @@ const StyledGelWrapperGrid = styled(GelPageGrid)`
 const StyledAudioPlayer = styled(AVPlayer)`
   amp-iframe {
     width: calc(100% + ${GEL_SPACING_DBL});
+    margin: 0 -${GEL_SPACING};
     @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
       width: calc(100% + ${GEL_SPACING_QUAD});
+      margin: 0 -${GEL_SPACING_DBL};
     }
   }
-  iframe {
+  div > iframe {
     width: calc(100% + ${GEL_SPACING_DBL});
     margin: 0 -${GEL_SPACING};
     @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
