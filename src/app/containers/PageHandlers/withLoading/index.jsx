d6074d6ebaf0ddd081d3f9167a1821cdf2f74a35
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-09-04 13:35:59 +0100

    add animation to transitioning pages

--- src/app/containers/PageHandlers/withLoading/index.jsx
+++ src/app/containers/PageHandlers/withLoading/index.jsx
@@ -3,6 +3,7 @@ import { bool, element } from 'prop-types';
 import styled from 'styled-components';
 import { Headline } from '@bbc/psammead-headings';
 import Paragraph from '@bbc/psammead-paragraph';
+import { useSpring, useTransition, animated } from 'react-spring';
 import Skeleton from 'react-loading-skeleton';
 import { GridWrapper, GridItemConstrainedMedium } from '#lib/styledGrid';
 import { ServiceContext } from '#contexts/ServiceContext';
@@ -17,6 +18,14 @@ const WithLoading = Component => {
   const LoadingContainer = ({ loading, ...props }) => {
     const [showSkeleton, setShowSkeleton] = useState(false);
     const { script, service } = useContext(ServiceContext);
+    const transitions = useTransition(loading, null, {
+      from: { opacity: 0 },
+      enter: { opacity: 1 },
+      leave: { display: 'none' },
+    });
+    const skeletonTransition = useSpring({
+      opacity: showSkeleton ? 1 : 0,
+    });
 
     useEffect(() => {
       timeout = setTimeout(() => {
@@ -25,32 +34,36 @@ const WithLoading = Component => {
       return () => clearTimeout(timeout);
     }, []);
 
-    if (!loading) {
-      return <Component {...props} />;
-    }
-
-    return (
-      <StyledMain role="main">
-        <GridWrapper>
-          <GridItemConstrainedMedium>
-            {showSkeleton && (
-              <>
-                <Headline
-                  script={script}
-                  service={service}
-                  id="content"
-                  tabIndex="0"
-                >
-                  <Skeleton count="2" />
-                </Headline>
-                <Paragraph script={script} service={service} tabIndex="0">
-                  <Skeleton count="4" />
-                </Paragraph>
-              </>
-            )}
-          </GridItemConstrainedMedium>
-        </GridWrapper>
-      </StyledMain>
+    return transitions.map(({ item, props: animProps }) =>
+      item ? (
+        <animated.div style={animProps}>
+          <StyledMain role="main">
+            <GridWrapper>
+              <GridItemConstrainedMedium>
+                {showSkeleton && (
+                  <animated.div style={skeletonTransition}>
+                    <Headline
+                      script={script}
+                      service={service}
+                      id="content"
+                      tabIndex="0"
+                    >
+                      <Skeleton count="2" />
+                    </Headline>
+                    <Paragraph script={script} service={service} tabIndex="0">
+                      <Skeleton count="4" />
+                    </Paragraph>
+                  </animated.div>
+                )}
+              </GridItemConstrainedMedium>
+            </GridWrapper>
+          </StyledMain>
+        </animated.div>
+      ) : (
+        <animated.div style={animProps}>
+          <Component {...props} />
+        </animated.div>
+      ),
     );
   };
 
