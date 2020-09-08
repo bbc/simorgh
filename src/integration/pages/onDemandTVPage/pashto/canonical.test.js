0f12d80a63adcc8cce1232ed1b269d7e7f9bb9f2
karinathomasbbc <karina.thomas@bbc.co.uk>
2020-09-08 10:46:24 +0100

    Tidy up tests for on demand TV page

--- src/integration/pages/onDemandTVPage/pashto/canonical.test.js
+++ src/integration/pages/onDemandTVPage/pashto/canonical.test.js
@@ -3,12 +3,10 @@
  * @pathname /pashto/bbc_pashto_tv/tv_programmes/w13xttn4
  */
 
-import runCrossPlatformTests from '../crossPlatformTests';
 import runCanonicalTests from '../canonicalTests';
 
 describe('Canonical', () => {
   describe(pageType, () => {
-    runCrossPlatformTests();
-    runCanonicalTests();
+    runCanonicalTests(service);
   });
 });
