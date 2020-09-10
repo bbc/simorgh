8f11a071552392c8b35d3be12c5508003561169b
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2020-09-10 13:49:25 +0100

    Bundle size update

--- scripts/bundleSize/bundleSizeConfig.js
+++ scripts/bundleSize/bundleSizeConfig.js
@@ -5,5 +5,5 @@ module.exports = {
   // below the smallest value in the build output; this avoids the
   // need for frequent changes as bundle sizes fluctuate.
   MIN_SIZE: 668,
-  MAX_SIZE: 843,
+  MAX_SIZE: 950,
 };
