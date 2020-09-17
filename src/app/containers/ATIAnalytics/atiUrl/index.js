e97a5839c06a5a4290f49c95e28ef9c965118504
Andrew Bennett <andrew_scfc@yahoo.co.uk>
2020-09-17 16:26:03 +0100

    Revert "Revert "Don't encode ref parameter""

--- src/app/containers/ATIAnalytics/atiUrl/index.js
+++ src/app/containers/ATIAnalytics/atiUrl/index.js
@@ -195,6 +195,9 @@ export const buildATIPageTrackPath = ({
       description: 'referrer url',
       value: getReferrer(platform, origin, previousPath),
       wrap: false,
+      // disable encoding for this parameter as ati does not appear to support
+      // decoding of the ref parameter
+      disableEncoding: true,
     },
   ];
 
