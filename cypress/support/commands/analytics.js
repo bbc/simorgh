120f48cfceb542e39791d6b2626919cfb1341582
karinathomasbbc <karina.thomas@bbc.co.uk>
2020-09-21 15:38:15 +0100

    Remove command as it is no longer required

--- cypress/support/commands/analytics.js
+++ cypress/support/commands/analytics.js
@@ -6,14 +6,6 @@ Cypress.Commands.add('hasNoscriptImgAtiUrl', atiUrl => {
   );
 });
 
-// Should be moved into integration/pages/index.js once all pages have ATI
-Cypress.Commands.add('hasAmpAnalyticsAtiUrl', atiUrl => {
-  cy.get('amp-analytics script[type="application/json"]').should(
-    'contain',
-    `${atiUrl}`,
-  );
-});
-
 // Should be moved into integration/pages/index.js once all pages have Chartbeat
 Cypress.Commands.add('hasScriptWithChartbeatSrc', () => {
   cy.get(`script[src="//static.chartbeat.com/js/chartbeat.js"]`).should(
