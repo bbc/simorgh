77bcf9d5b77df5916eca810c1777607f215d7380
karinathomasbbc <karina.thomas@bbc.co.uk>
2020-09-22 11:16:27 +0100

    Adding webVitals toggle

--- src/app/lib/config/toggles/liveConfig.js
+++ src/app/lib/config/toggles/liveConfig.js
@@ -44,4 +44,7 @@ export default {
   variantCookie: {
     enabled: true,
   },
+  webVitals: {
+    enabled: false,
+  },
 };
--- src/app/lib/config/toggles/localConfig.js
+++ src/app/lib/config/toggles/localConfig.js
@@ -47,4 +47,7 @@ export default {
   variantCookie: {
     enabled: true,
   },
+  webVitals: {
+    enabled: true,
+  },
 };
