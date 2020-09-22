482f811bd9fe4894ebc310d8a95cf15a12796af5
karinathomasbbc <karina.thomas@bbc.co.uk>
2020-09-22 11:17:08 +0100

    Only use web vitals if toggle is enabled and personalisation enabled (accepted via cookie banner)

--- src/app/Layouts/defaultPageWrapper.jsx
+++ src/app/Layouts/defaultPageWrapper.jsx
@@ -6,10 +6,19 @@ import FooterContainer from '../containers/Footer';
 import ManifestContainer from '../containers/Manifest';
 import ServiceWorkerContainer from '../containers/ServiceWorker';
 import { ServiceContext } from '../contexts/ServiceContext';
+import { UserContext } from '../contexts/UserContext';
 import useWebVitals from '#hooks/useWebVitals';
+import useToggle from '#hooks/useToggle';
 
 const PageWrapper = ({ children }) => {
-  useWebVitals();
+  const { enabled: webVitalsEnabled } = useToggle('webVitals');
+  const { personalisationEnabled } = useContext(UserContext);
+
+  if (webVitalsEnabled && personalisationEnabled) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useWebVitals();
+  }
+
   const { fonts: fontFunctions } = useContext(ServiceContext);
 
   const fonts = fontFunctions.map(getFonts => getFonts());
