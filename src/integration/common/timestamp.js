ae06749d8404127d24f5908090fb36f331adada9
karinathomasbbc <karina.thomas@bbc.co.uk>
2020-09-24 10:55:27 +0100

    Updating snapshots for timestamp tests

--- src/integration/common/timestamp.js
+++ src/integration/common/timestamp.js
@@ -2,18 +2,18 @@ export default () => {
   describe('Timestamp', () => {
     const timestampEl = document.querySelector('time');
     const date = timestampEl.getAttribute('datetime');
-    const timestamp = timestampEl.textContent;
+    const text = timestampEl.textContent;
 
     it('should be in the document', () => {
       expect(timestampEl).toBeInTheDocument();
     });
 
     it('should contain text', () => {
-      expect(timestamp).toBeTruthy();
+      expect(text).toBeTruthy();
     });
 
-    it('should match text', () => {
-      expect({ date, timestamp }).toMatchSnapshot();
+    it('should match text and date', () => {
+      expect({ text, date }).toMatchSnapshot();
     });
   });
 };
