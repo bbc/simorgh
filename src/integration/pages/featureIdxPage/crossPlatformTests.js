35d03311d20a74ce14ff603bf9ae11764f418c9a
Che Wu <che.wu@bbc.co.uk>
2020-09-24 17:23:59 +0100

    Update snapshot tests

--- src/integration/pages/featureIdxPage/crossPlatformTests.js
+++ src/integration/pages/featureIdxPage/crossPlatformTests.js
@@ -8,4 +8,22 @@ export default service => {
   runCommonCrossPlatformTests(service);
   runMainHeadingTests();
   runStoryPromoTests();
+
+  describe('Sections', () => {
+    const sections = document.querySelectorAll('section');
+
+    if (sections) {
+      sections.forEach(section => {
+        it('should be in the document', () => {
+          expect(section).toBeInTheDocument();
+        });
+
+        const h2El = section.querySelector('h2');
+
+        it('should have a headline', () => {
+          expect(h2El).toBeInTheDocument();
+        });
+      });
+    }
+  });
 };
