c44307739b0cdc6998e9cebc840c94a05027e6c0
BBC News CI <foo@bar.com>
2020-09-29 10:10:11 +0000

    Updates

--- ArticlePage.3d89997080727ca8f22a.bundle.js
+++ ArticlePage.3d89997080727ca8f22a.bundle.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[0],{854:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var _ArticlePage__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(776),_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__(171);__webpack_exports__.default=Object(_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__.a)({addVariantHandling:!0})(_ArticlePage__WEBPACK_IMPORTED_MODULE_0__.a)}}]);
-//# sourceMappingURL=ArticlePage.a61b52fb0e026d8d5459.bundle.js.map
+//# sourceMappingURL=ArticlePage.3d89997080727ca8f22a.bundle.js.map
