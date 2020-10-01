95649cae0e46fd2f86941d13141028fcb4c97fd0
karinathomasbbc <karina.thomas@bbc.co.uk>
2020-10-01 12:07:45 +0100

    Use common sections test on Front Page, IDX Page and Feature Index Page

--- src/integration/pages/featureIdxPage/canonicalTests.js
+++ src/integration/pages/featureIdxPage/canonicalTests.js
@@ -2,12 +2,10 @@ import runCrossPlatformTests from './crossPlatformTests';
 import {
   runCoreCanonicalTests,
   runCanonicalAnalyticsTests,
-  runSeoCanonicalTests,
 } from '../../common';
 
 export default service => {
   runCrossPlatformTests(service);
   runCoreCanonicalTests();
   runCanonicalAnalyticsTests();
-  runSeoCanonicalTests(service);
 };
--- src/integration/pages/featureIdxPage/crossPlatformTests.js
+++ src/integration/pages/featureIdxPage/crossPlatformTests.js
@@ -2,28 +2,12 @@ import {
   runCommonCrossPlatformTests,
   runMainHeadingTests,
   runStoryPromoTests,
+  runSectionTests,
 } from '../../common';
 
 export default service => {
   runCommonCrossPlatformTests(service);
   runMainHeadingTests();
   runStoryPromoTests();
-
-  describe('Sections', () => {
-    const sections = document.querySelectorAll('section');
-
-    if (sections) {
-      sections.forEach(section => {
-        it('should be in the document', () => {
-          expect(section).toBeInTheDocument();
-        });
-
-        const h2El = section.querySelector('h2');
-
-        it('should have a headline', () => {
-          expect(h2El).toBeInTheDocument();
-        });
-      });
-    }
-  });
+  runSectionTests();
 };
