599ef80853cfd328e371ce5382a60109e5cf4f06
karinathomasbbc <karina.thomas@bbc.co.uk>
2020-10-02 13:44:26 +0100

    Update test

--- src/app/pages/FeatureIdxPage/flattenGroups.test.js
+++ src/app/pages/FeatureIdxPage/flattenGroups.test.js
@@ -23,44 +23,50 @@ describe('flattenGroups', () => {
 
   it('should move items from a group without a strapline into a group with a strapline', () => {
     const groups = [
-      { strapline: { name: 'strapline 1' }, items: [{ id: 1 }, { id: 2 }] },
-      { items: [{ id: 3 }, { id: 4 }] },
+      { strapline: { name: 'strapline 1' }, items: [{ id: '1' }, { id: '2' }] },
+      { items: [{ id: '3' }, { id: '4' }] },
     ];
     expect(flattenGroups(groups)).toStrictEqual([
       {
         strapline: { name: 'strapline 1' },
-        items: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }],
+        items: [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }],
       },
     ]);
   });
 
   it('should move items from multiple groups without a strapline into groups with a strapline', () => {
     const groups = [
-      { items: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }] },
-      { items: [{ id: 4 }, { id: 5 }, { id: 6 }] },
-      { strapline: { name: 'strapline 3' }, items: [{ id: 10 }, { id: 15 }] },
-      { items: [{ id: 20 }, { id: 25 }] },
-      { strapline: { name: 'strapline 5' }, items: [{ id: 30 }, { id: 40 }] },
-      { items: [{ id: 100 }, { id: 200 }] },
+      { items: [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }] },
+      { items: [{ id: '4' }, { id: '5' }, { id: '6' }] },
+      {
+        strapline: { name: 'strapline 3' },
+        items: [{ id: '10' }, { id: '15' }],
+      },
+      { items: [{ id: '20' }, { id: '25' }] },
+      {
+        strapline: { name: 'strapline 5' },
+        items: [{ id: '30' }, { id: '40' }],
+      },
+      { items: [{ id: '100' }, { id: '200' }] },
     ];
     expect(flattenGroups(groups)).toStrictEqual([
       {
         items: [
-          { id: 1 },
-          { id: 2 },
-          { id: 3 },
-          { id: 4 },
-          { id: 5 },
-          { id: 6 },
+          { id: '1' },
+          { id: '2' },
+          { id: '3' },
+          { id: '4' },
+          { id: '5' },
+          { id: '6' },
         ],
       },
       {
         strapline: { name: 'strapline 3' },
-        items: [{ id: 10 }, { id: 15 }, { id: 20 }, { id: 25 }],
+        items: [{ id: '10' }, { id: '15' }, { id: '20' }, { id: '25' }],
       },
       {
         strapline: { name: 'strapline 5' },
-        items: [{ id: 30 }, { id: 40 }, { id: 100 }, { id: 200 }],
+        items: [{ id: '30' }, { id: '40' }, { id: '100' }, { id: '200' }],
       },
     ]);
   });
