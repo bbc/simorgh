7db9d42fca3c172a8d95af8d683939e0c776c000
BBC News CI <foo@bar.com>
2020-10-08 13:33:21 +0000

    Updates

---
+++ ArticlePage.9086778fe33eadd6f4da.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{846:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var _ArticlePage__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(769),_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__(172);__webpack_exports__.default=Object(_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__.a)({addVariantHandling:!0})(_ArticlePage__WEBPACK_IMPORTED_MODULE_0__.a)}}]);
+//# sourceMappingURL=ArticlePage.9086778fe33eadd6f4da.bundle.js.map
