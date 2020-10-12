f2c099c0a4fa71db663923e2801b43c6d476a7d2
BBC News CI <foo@bar.com>
2020-10-12 11:27:05 +0000

    Updates

--- ArticlePage.06177813f32022119d9d.bundle.js
+++ ArticlePage.06177813f32022119d9d.bundle.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[0],{846:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var _ArticlePage__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(769),_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__(172);__webpack_exports__.default=Object(_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__.a)({addVariantHandling:!0})(_ArticlePage__WEBPACK_IMPORTED_MODULE_0__.a)}}]);
-//# sourceMappingURL=ArticlePage.2475bc0643bf66745a5a.bundle.js.map
+//# sourceMappingURL=ArticlePage.06177813f32022119d9d.bundle.js.map
--- IdxPage.06177813f32022119d9d.bundle.js
+++ IdxPage.06177813f32022119d9d.bundle.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[2],{848:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var _IdxPage__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(766),_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__(172);__webpack_exports__.default=Object(_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__.a)({addVariantHandling:!1})(_IdxPage__WEBPACK_IMPORTED_MODULE_0__.a)}}]);
-//# sourceMappingURL=IdxPage.2475bc0643bf66745a5a.bundle.js.map
+//# sourceMappingURL=IdxPage.06177813f32022119d9d.bundle.js.map
