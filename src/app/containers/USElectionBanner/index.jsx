52a1610724d98b1088f86ed1cec0f46e2e0ed9e5
Chris <chris.hinds@bbc.co.uk>
2020-10-14 10:00:30 +0100

    Impliment feature toggle

--- src/app/containers/USElectionBanner/index.jsx
+++ src/app/containers/USElectionBanner/index.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+
+// Utilities
+import useToggle from '#hooks/useToggle';
+
+// Fixture Data
 import Fixture from './fixture.json';
 
 const getMarkup = () => {
   return { __html: Fixture.html };
 };
 
-const USElectionBanner = () => (
-  <>
-    <div dangerouslySetInnerHTML={getMarkup()} />
-  </>
-);
+const USElectionBanner = () => {
+  const { enabled } = useToggle('us2020ElectionBanner');
+
+  if (!enabled) return null;
+
+  return (
+    <>
+      <div dangerouslySetInnerHTML={getMarkup()} />
+    </>
+  );
+};
 
 export default USElectionBanner;
