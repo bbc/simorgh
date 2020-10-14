6742c631fe8bb0c08b465e4068dcb51651750e4c
LilyL0u <lily.higham@bbc.co.uk>
2020-10-14 17:01:34 +0100

    reduced mostwatched e2es, persian & amharic remain

--- cypress/integration/pages/mediaAssetPage/testsForCanonicalOnly.js
+++ cypress/integration/pages/mediaAssetPage/testsForCanonicalOnly.js
@@ -52,70 +52,82 @@ export const testsThatFollowSmokeTestConfigForCanonicalOnly = ({
       const mostWatchedPath = `/${config[service].name}/mostwatched${serviceVariant}.json`;
       it('should show/not show the Most Watched component if the toggle is enabled for the service', function test() {
         // Find if the service has component enabled in toggles
-        cy.fixture(`toggles/${config[service].name}.json`).then(toggles => {
-          const mostWatchedIsEnabled = path(
-            ['mostPopularMedia', 'enabled'],
-            toggles,
-          );
-          cy.log(`Service toggle enabled ${mostWatchedIsEnabled}`);
-          // If toggle is enabled for that service, also check there are more than 0 records to show
-          if (mostWatchedIsEnabled) {
-            cy.request(mostWatchedPath).then(({ body: mostWatchedJson }) => {
-              if (mostWatchedJson.totalRecords > 0) {
-                cy.log('Enough records to show component');
-                cy.get('[data-e2e=most-watched-heading]').should('exist');
-              } else {
-                cy.log('Not enough records to show component');
+        if (service === 'persian' || service === 'amharic') {
+          cy.fixture(`toggles/${config[service].name}.json`).then(toggles => {
+            const mostWatchedIsEnabled = path(
+              ['mostPopularMedia', 'enabled'],
+              toggles,
+            );
+            cy.log(`Service toggle enabled ${mostWatchedIsEnabled}`);
+            // If toggle is enabled for that service, also check there are more than 0 records to show
+            if (mostWatchedIsEnabled) {
+              cy.request(mostWatchedPath).then(({ body: mostWatchedJson }) => {
+                if (mostWatchedJson.totalRecords > 0) {
+                  cy.log('Enough records to show component');
+                  cy.get('[data-e2e=most-watched-heading]').should('exist');
+                } else {
+                  cy.log('Not enough records to show component');
 
-                cy.get('[data-e2e=most-watched-heading]').should('not.exist');
-              }
-            });
-          } else {
-            cy.get('[data-e2e=most-watched-heading]').should('not.exist');
-          }
-        });
-      });
-      it('should show correct number of items', function test() {
-        cy.fixture(`toggles/${config[service].name}.json`).then(toggles => {
-          const mostWatchedIsEnabled = path(
-            ['mostPopularMedia', 'enabled'],
-            toggles,
-          );
-
-          cy.request(mostWatchedPath).then(({ body: mostWatchedJson }) => {
-            if (mostWatchedIsEnabled && mostWatchedJson.totalRecords > 0) {
-              const maxNumberofItems = path(
-                ['mostPopularMedia', 'value'],
-                toggles,
-              );
-              cy.log(
-                `Max number of items is ${maxNumberofItems}. Number of items is ${mostWatchedJson.totalRecords}`,
-              );
-              // Compares number of items in json to number of items shown in the component
-              // If there is only one record, the item is not in a list
-              // If the max number of items is 5 and the records are >= 5, checks it shows 5
-              // If the max number of items is 10 and the records are >= 10, checks it shows 10
-              const expectedNumberOfItems = Math.min(
-                mostWatchedJson.totalRecords,
-                maxNumberofItems,
-              );
-              if (mostWatchedJson.totalRecords === 1) {
-                cy.get('[data-e2e=most-watched-heading]').within(() => {
-                  cy.get('[class^="StoryPromoWrapper"]')
-                    .its('length')
-                    .should('eq', 1);
-                });
-              } else {
-                cy.get('[data-e2e=most-watched-heading]').within(() => {
-                  cy.get('[class^="MostWatchedOl"]')
-                    .find('>li')
-                    .its('length')
-                    .should('eq', expectedNumberOfItems);
-                });
-              }
+                  cy.get('[data-e2e=most-watched-heading]').should('not.exist');
+                }
+              });
+            } else {
+              cy.get('[data-e2e=most-watched-heading]').should('not.exist');
             }
           });
-        });
+        } else if (service !== 'persian' || service !== 'amharic') {
+          cy.log(
+            'Skipping this test on services that are not Persian or Amharic',
+          );
+        }
+      });
+      it('should show correct number of items', function test() {
+        if (service === 'persian' || service === 'amharic') {
+          cy.fixture(`toggles/${config[service].name}.json`).then(toggles => {
+            const mostWatchedIsEnabled = path(
+              ['mostPopularMedia', 'enabled'],
+              toggles,
+            );
+
+            cy.request(mostWatchedPath).then(({ body: mostWatchedJson }) => {
+              if (mostWatchedIsEnabled && mostWatchedJson.totalRecords > 0) {
+                const maxNumberofItems = path(
+                  ['mostPopularMedia', 'value'],
+                  toggles,
+                );
+                cy.log(
+                  `Max number of items is ${maxNumberofItems}. Number of items is ${mostWatchedJson.totalRecords}`,
+                );
+                // Compares number of items in json to number of items shown in the component
+                // If there is only one record, the item is not in a list
+                // If the max number of items is 5 and the records are >= 5, checks it shows 5
+                // If the max number of items is 10 and the records are >= 10, checks it shows 10
+                const expectedNumberOfItems = Math.min(
+                  mostWatchedJson.totalRecords,
+                  maxNumberofItems,
+                );
+                if (mostWatchedJson.totalRecords === 1) {
+                  cy.get('[data-e2e=most-watched-heading]').within(() => {
+                    cy.get('[class^="StoryPromoWrapper"]')
+                      .its('length')
+                      .should('eq', 1);
+                  });
+                } else {
+                  cy.get('[data-e2e=most-watched-heading]').within(() => {
+                    cy.get('[class^="MostWatchedOl"]')
+                      .find('>li')
+                      .its('length')
+                      .should('eq', expectedNumberOfItems);
+                  });
+                }
+              }
+            });
+          });
+        } else if (service !== 'persian' || service !== 'amharic') {
+          cy.log(
+            'Skipping this test on services that are not Persian or Amharic',
+          );
+        }
       });
     });
     if (appToggles.chartbeatAnalytics.enabled && envConfig.chartbeatEnabled) {
