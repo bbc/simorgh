1c9a5237017e974714d34a0690082d9b8783549e
BBC News CI <foo@bar.com>
2020-10-15 09:34:27 +0000

    Updates

--- IdxPage.a396999acf9dc24882fa.bundle.js
+++ IdxPage.a396999acf9dc24882fa.bundle.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[2],{850:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var _IdxPage__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(767),_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__(172);__webpack_exports__.default=Object(_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__.a)({addVariantHandling:!1})(_IdxPage__WEBPACK_IMPORTED_MODULE_0__.a)}}]);
-//# sourceMappingURL=IdxPage.9ded8f14c46835b3a339.bundle.js.map
+//# sourceMappingURL=IdxPage.a396999acf9dc24882fa.bundle.js.map
