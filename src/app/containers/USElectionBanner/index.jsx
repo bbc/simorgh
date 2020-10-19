23a513b33638c412cb8201110f6468a6c52a755a
Chris <chris.hinds@bbc.co.uk>
2020-10-19 09:12:52 +0100

    Add wrapping conmtainer with gel spacing

--- src/app/containers/USElectionBanner/index.jsx
+++ src/app/containers/USElectionBanner/index.jsx
@@ -7,9 +7,26 @@ import useToggle from '#hooks/useToggle';
 // Components
 import FrontPageSection from '../../components/FrontPageSection';
 
+// Styling
+import {
+  GEL_SPACING,
+  GEL_SPACING_TRPL,
+  GEL_SPACING_QUAD,
+} from '@bbc/gel-foundations/spacings';
+import { GEL_GROUP_3_SCREEN_WIDTH_MIN } from '@bbc/gel-foundations/breakpoints';
+
 const createMarkup = oembed => {
   return { __html: oembed.html };
 };
+
+const StyledWrapper = styled.div`
+  margin-top: ${GEL_SPACING};
+  margin-bottom: ${GEL_SPACING_TRPL};
+
+  @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
+    margin-top: ${GEL_SPACING_QUAD};
+  }
+`;
 
 const USElectionBanner = ({ oembed }) => {
   const { enabled } = useToggle('us2020ElectionBanner');
@@ -18,7 +35,7 @@ const USElectionBanner = ({ oembed }) => {
 
   return (
     <FrontPageSection>
-      <div dangerouslySetInnerHTML={createMarkup(oembed)} />
+      <StyledWrapper dangerouslySetInnerHTML={createMarkup(oembed)} />
     </FrontPageSection>
   );
 };
---
+++ src/app/containers/USElectionBanner/index.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { isNull, shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import ElectionBanner from './index';
+import { ToggleContext } from '#contexts/ToggleContext';
+
+const MockOembedData = {
+  version: '1.0',
+  type: 'rich',
+  width: 320,
+  height: 240,
+  html: '<div>Hello</div>',
+};
+
+const ElectionsBannerWithContext = ({ oembed }) => (
+  <ToggleContext.Provider
+    value={{
+      toggleState: {
+        us2020ElectionBanner: { enabled: true },
+      },
+    }}
+  >
+    <ElectionBanner oembed={oembed} />
+  </ToggleContext.Provider>
+);
+
+describe('ElectionBanner', () => {
+  describe('with no oEmbed data', () => {
+    isNull('should return null', <ElectionsBannerWithContext />);
+  });
+
+  describe('with oEmbed data', () => {
+    shouldMatchSnapshot(
+      'should render correctly',
+      <ElectionsBannerWithContext oembed={MockOembedData} />,
+    );
+  });
+});
