4761b365b0458595d8e2c0fecd9d24b93640ae5e
bennea27 <andrew.bennett07@bbc.co.uk>
2020-10-19 17:00:08 +0100

    increase bundle size

--- scripts/bundleSize/bundleSizeConfig.js
+++ scripts/bundleSize/bundleSizeConfig.js
@@ -5,5 +5,5 @@ module.exports = {
   // below the smallest value in the build output; this avoids the
   // need for frequent changes as bundle sizes fluctuate.
   MIN_SIZE: 673,
-  MAX_SIZE: 851,
+  MAX_SIZE: 855,
 };
