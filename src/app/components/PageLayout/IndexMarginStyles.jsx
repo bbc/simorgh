03626bec7cc5d38c26a9496cfb8a71122c82520c
RichardPK <r.phillips.kerr@gmail.com>
2020-10-20 11:00:40 +0100

    PageLayout

--- src/app/components/PageLayout/IndexMarginStyles.jsx
+++ src/app/components/PageLayout/IndexMarginStyles.jsx
@@ -1,8 +1,8 @@
-import { css } from 'styled-components';
 import { GEL_SPACING, GEL_SPACING_DBL } from '@bbc/gel-foundations/spacings';
 import { GEL_GROUP_2_SCREEN_WIDTH_MIN } from '@bbc/gel-foundations/breakpoints';
 
-const IndexMarginStyles = css`
+// confirm css removal is OK in snapshots
+const IndexMarginStyles = `
   margin: 0 ${GEL_SPACING};
   @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
     margin: 0 ${GEL_SPACING_DBL};
