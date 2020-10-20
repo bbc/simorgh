4bfab21ca108f7d5a88bb1fb6e29149f73ab94ce
Simon Sinclair <simon.sinclair@bbc.co.uk>
2020-10-20 12:42:13 +0100

    Amend MostReadSection migration to Emotion

--- src/app/containers/MostRead/section.jsx
+++ src/app/containers/MostRead/section.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { node } from 'prop-types';
 
-const MostReadSection = children => (
+const MostReadSection = ({ children }) => (
   // eslint-disable-next-line jsx-a11y/no-redundant-roles
   <section role="region" aria-labelledby="Most-Read" data-e2e="most-read">
     {children}
   </section>
 );
+
+MostReadSection.propTypes = {
+  children: node.isRequired,
+};
 
 export default MostReadSection;
