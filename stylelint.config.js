5576b0b9e97d1faf7d021737c2b24c678f9a14e1
Simon Sinclair <simon.sinclair@bbc.co.uk>
2020-10-20 10:55:06 +0100

    Remove styled-components from stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,17 +1,6 @@
 module.exports = {
-  processors: [
-    [
-      'stylelint-processor-styled-components',
-      {
-        parserPlugins: ['dynamicImport', 'jsx', 'throwExpressions'],
-      },
-    ],
-  ],
-  extends: [
-    'stylelint-config-recommended',
-    'stylelint-config-styled-components',
-  ],
   rules: {
     'selector-type-no-unknown': [true, { ignoreTypes: ['/^amp-/'] }],
   },
+  extends: ['stylelint-config-recommended'],
 };
