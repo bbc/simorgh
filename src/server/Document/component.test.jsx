5f61d6010d1faaafe6a0a975c50dc0b0250b4013
Simon Sinclair <simon@simonsinclair.com>
2020-10-22 17:25:45 +0100

    Update app object

--- src/server/Document/component.test.jsx
+++ src/server/Document/component.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import { renderToString } from 'react-dom/server';
+import { Helmet } from 'react-helmet';
 import { JSDOM } from 'jsdom';
 
 import DocumentComponent from './component';
@@ -22,23 +22,27 @@ describe('Document Component', () => {
   const TestDocumentComponent = ({ service, isAmp }) => (
     <DocumentComponent
       assetOrigins={assetOrigins}
-      app={renderToString(
-        <>
-          <Helmet htmlAttributes={{ lang: 'test' }}>
-            <title>Test title</title>
-            <link
-              rel="canonical"
-              href="https://www.bbc.com/news/articles/c6v11qzyv8po"
-            />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1, minimum-scale=1"
-            />
-            <script type="application/ld+json" />
-          </Helmet>
-          <h1>App!</h1>
-        </>,
-      )}
+      app={{
+        css: '.css-7prgni-StyledLink{display:inline-block;}',
+        ids: ['7prgni-StyledLink'],
+        html: renderToString(
+          <>
+            <Helmet htmlAttributes={{ lang: 'test' }}>
+              <title>Test title</title>
+              <link
+                rel="canonical"
+                href="https://www.bbc.com/news/articles/c6v11qzyv8po"
+              />
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1, minimum-scale=1"
+              />
+              <script type="application/ld+json" />
+            </Helmet>
+            <h1>App!</h1>
+          </>,
+        ),
+      }}
       data={{ ...data }}
       helmet={Helmet.renderStatic()}
       scripts={scripts}
