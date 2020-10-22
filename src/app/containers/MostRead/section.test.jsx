39f5dc018d46028e94dcef9afba6fcbb48fd32aa
RichardPK <r.phillips.kerr@gmail.com>
2020-10-22 16:27:17 +0100

    Revert "Section test fix"
    This reverts commit 9b03401ff9fe129700a7b359e31a715d6397bf1c.

--- src/app/containers/MostRead/section.test.jsx
+++ src/app/containers/MostRead/section.test.jsx
@@ -4,9 +4,7 @@ import MostReadSection from './section';
 
 describe('MostReadSectionLabel assertion', () => {
   it('should render most-read section with correct attributes', async () => {
-    const { container } = render(
-      <MostReadSection>Some child content</MostReadSection>,
-    );
+    const { container } = render(<MostReadSection />);
     const section = container.getElementsByTagName('section')[0];
     expect(section).toHaveAttribute('aria-labelledby', 'Most-Read');
     expect(section).toHaveAttribute('role', 'region');
