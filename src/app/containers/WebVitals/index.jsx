2f0c21d71f2b081a347f777e319df2c016d1ff7f
Chris <chris.hinds@bbc.co.uk>
2020-10-22 17:48:17 +0100

    Add radix value

--- src/app/containers/WebVitals/index.jsx
+++ src/app/containers/WebVitals/index.jsx
@@ -10,15 +10,22 @@ import { RequestContext } from '#contexts/RequestContext';
 const WebVitals = () => {
   const { personalisationEnabled } = useContext(UserContext);
   const { pageType } = useContext(RequestContext);
-  const { enabled } = useToggle('webVitalsMonitoring');
+  const { enabled, value: toggleSampleRate } = useToggle('webVitalsMonitoring');
+
+  // console.log(sampleRate);
 
   // Checks if readers have opted into performance tracking and if the feature toggle is enabled
   const isWebVitalsEnabled = personalisationEnabled && enabled;
 
+  const sampleRate = parseInt(
+    toggleSampleRate || process.env.SIMORGH_WEBVITALS_DEFAULT_SAMPLING_RATE,
+    10,
+  );
+
   const webVitalsConfig = {
     enabled: isWebVitalsEnabled,
     reportingEndpoint: process.env.SIMORGH_WEBVITALS_REPORTING_ENDPOINT,
-    sampleRate: process.env.SIMORGH_WEBVITALS_SAMPLING_RATE,
+    sampleRate,
     reportParams: { pageType },
   };
 
--- src/app/containers/WebVitals/index.test.jsx
+++ src/app/containers/WebVitals/index.test.jsx
@@ -20,11 +20,12 @@ const WebVitalsWithContext = ({
   featureToggle,
   personalisationEnabled,
   pageType,
+  sampleRate,
 }) => (
   <ToggleContext.Provider
     value={{
       toggleState: {
-        webVitalsMonitoring: { enabled: featureToggle },
+        webVitalsMonitoring: { enabled: featureToggle, value: sampleRate },
       },
     }}
   >
@@ -44,20 +45,22 @@ describe('WebVitals', () => {
   describe('calls the useWebVitals hook', () => {
     beforeEach(() => {
       process.env.SIMORGH_WEBVITALS_REPORTING_ENDPOINT = 'endpoint';
-      process.env.SIMORGH_WEBVITALS_SAMPLING_RATE = '5';
+      process.env.SIMORGH_WEBVITALS_DEFAULT_SAMPLING_RATE = 20;
     });
 
-    afterAll(() => {
+    afterEach(() => {
+      jest.clearAllMocks();
       delete process.env.SIMORGH_WEBVITALS_REPORTING_ENDPOINT;
-      delete process.env.SIMORGH_WEBVITALS_SAMPLING_RATE;
+      delete process.env.SIMORGH_WEBVITALS_DEFAULT_SAMPLING_RATE;
     });
 
     it.each`
-      testDescription                                | testConfig                                                                  | webVitalsParams
-      ${'feature toggle and personalisation off'}    | ${{ featureToggle: false, personalisationEnabled: false, pageType: 'STY' }} | ${{ enabled: false, reportParams: { pageType: 'STY' }, reportingEndpoint: 'endpoint', sampleRate: '5' }}
-      ${'feature toggle on and personalisation off'} | ${{ featureToggle: true, personalisationEnabled: false, pageType: 'STY' }}  | ${{ enabled: false, reportParams: { pageType: 'STY' }, reportingEndpoint: 'endpoint', sampleRate: '5' }}
-      ${'feature toggle off and personalisation on'} | ${{ featureToggle: false, personalisationEnabled: true, pageType: 'STY' }}  | ${{ enabled: false, reportParams: { pageType: 'STY' }, reportingEndpoint: 'endpoint', sampleRate: '5' }}
-      ${'feature toggle and personalisation on'}     | ${{ featureToggle: true, personalisationEnabled: true, pageType: 'STY' }}   | ${{ enabled: true, reportParams: { pageType: 'STY' }, reportingEndpoint: 'endpoint', sampleRate: '5' }}
+      testDescription                                | testConfig                                                                                | webVitalsParams
+      ${'feature toggle and personalisation off'}    | ${{ featureToggle: false, personalisationEnabled: false, pageType: 'STY' }}               | ${{ enabled: false, reportParams: { pageType: 'STY' }, reportingEndpoint: 'endpoint', sampleRate: 20 }}
+      ${'feature toggle on and personalisation off'} | ${{ featureToggle: true, personalisationEnabled: false, pageType: 'STY' }}                | ${{ enabled: false, reportParams: { pageType: 'STY' }, reportingEndpoint: 'endpoint', sampleRate: 20 }}
+      ${'feature toggle off and personalisation on'} | ${{ featureToggle: false, personalisationEnabled: true, pageType: 'STY' }}                | ${{ enabled: false, reportParams: { pageType: 'STY' }, reportingEndpoint: 'endpoint', sampleRate: 20 }}
+      ${'feature toggle and personalisation on'}     | ${{ featureToggle: true, personalisationEnabled: true, pageType: 'STY' }}                 | ${{ enabled: true, reportParams: { pageType: 'STY' }, reportingEndpoint: 'endpoint', sampleRate: 20 }}
+      ${'sample rate override'}                      | ${{ featureToggle: true, personalisationEnabled: true, pageType: 'STY', sampleRate: 65 }} | ${{ enabled: true, reportParams: { pageType: 'STY' }, reportingEndpoint: 'endpoint', sampleRate: 65 }}
     `(`$testDescription`, ({ testConfig, webVitalsParams }) => {
       render(<WebVitalsWithContext {...testConfig} />);
 
