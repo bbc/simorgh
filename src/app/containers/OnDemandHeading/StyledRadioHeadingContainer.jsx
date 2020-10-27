19f2e51687525133ac1f73b2a20de4640d117f05
RichardPK <r.phillips.kerr@gmail.com>
2020-10-27 16:18:54 +0000

    OnDemandRadio heading padding fix

--- src/app/containers/OnDemandHeading/StyledRadioHeadingContainer.jsx
+++ src/app/containers/OnDemandHeading/StyledRadioHeadingContainer.jsx
@@ -3,10 +3,20 @@ import {
   GEL_SPACING_DBL,
   GEL_SPACING_QUAD,
 } from '@bbc/gel-foundations/spacings';
+import {
+  GEL_GROUP_3_SCREEN_WIDTH_MAX,
+  GEL_GROUP_4_SCREEN_WIDTH_MIN,
+} from '@bbc/gel-foundations/breakpoints';
 import OnDemandHeading from '.';
 
 const StyledRadioHeadingContainer = styled(OnDemandHeading)`
-  padding: ${GEL_SPACING_QUAD} 0 ${GEL_SPACING_DBL};
+  @media (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
+    padding: ${GEL_SPACING_QUAD} 0 ${GEL_SPACING_DBL};
+  }
+
+  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
+    padding: ${GEL_SPACING_QUAD} 0 ${GEL_SPACING_DBL};
+  }
 `;
 
 export default StyledRadioHeadingContainer;
