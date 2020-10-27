ea8845e5b72b15deded0a7b8fe31ca750ef4fa09
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-10-27 10:48:40 +0000

    Update src/app/components/Grid/index.jsx
    Co-authored-by: RichardPK <r.phillips.kerr@gmail.com>

--- src/app/components/Grid/index.jsx
+++ src/app/components/Grid/index.jsx
@@ -275,7 +275,7 @@ export const GridItemConstrainedLargeNoMargin = ({ as, dir, ...props }) => (
       group1: 1,
       group2: 1,
       group3: 1,
-      group4: 3,
+      group4: 2,
       group5: 5,
     }}
     columns={{
