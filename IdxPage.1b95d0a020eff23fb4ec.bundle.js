c346c95a9f09379ce35fa0b65a8803e4cff85d2d
simorgh-bbc <DENewsSimorghDev@bbc.co.uk>
2020-11-03 11:24:34 +0000

    Deploying to gh-pages from  @ 4cf4c0847f635dbfe81dd20fc6888ceec432a4fe 🚀

--- IdxPage.1b95d0a020eff23fb4ec.bundle.js
+++ IdxPage.1b95d0a020eff23fb4ec.bundle.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[2],{851:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var _IdxPage__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(766),_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__(170);__webpack_exports__.default=Object(_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__.a)({addVariantHandling:!1})(_IdxPage__WEBPACK_IMPORTED_MODULE_0__.a)}}]);
-//# sourceMappingURL=IdxPage.e16e27e345ba7bbcc306.bundle.js.map
+//# sourceMappingURL=IdxPage.1b95d0a020eff23fb4ec.bundle.js.map
