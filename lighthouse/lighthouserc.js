6b9648ae72af8548b5ac550fca55147e8909bb49
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-11-09 16:16:30 +0000

    add more test and live urls for lhci to audit

--- lighthouse/lighthouserc.js
+++ lighthouse/lighthouserc.js
@@ -1,18 +1,72 @@
+const AUDIT_URLS = {
+  FRONT_PAGE: {
+    live: ['https://test.bbc.com/arabic'],
+    test: ['https://www.test.bbc.com/arabic'],
+  },
+
+  IDX: {
+    live: [],
+    test: [
+      'https://www.test.bbc.com/persian/afghanistan',
+      'https://www.test.bbc.com/azeri/azerbaijan-23066612',
+    ],
+  },
+
+  LIVE_RADIO: {
+    live: ['https://www.bbc.com/korean/bbc_korean_radio/liveradio'],
+    test: ['https://www.test.bbc.com/korean/bbc_korean_radio/liveradio'],
+  },
+
+  OD_RADIO: {
+    live: [],
+    test: [
+      'https://www.test.bbc.com/pashto/bbc_pashto_radio/programmes/p056fcjb',
+    ],
+  },
+
+  OD_TV: {
+    live: [],
+    test: [
+      'https://www.test.bbc.com/pashto/bbc_pashto_tv/tv_programmes/w13xttn4',
+    ],
+  },
+
+  MAP: {
+    live: [],
+    test: ['https://www.test.bbc.com/pidgin/23248703'],
+  },
+
+  MOST_READ: {
+    live: ['https://www.bbc.com/vietnamese/popular/read'],
+    test: ['https://www.test.bbc.com/vietnamese/popular/read'],
+  },
+
+  MOST_WATCHED: {
+    live: ['https://www.bbc.com/afrique/media/video'],
+    test: ['https://test.bbc.com/afrique/media/video'],
+  },
+
+  PGL: {
+    live: [],
+    test: ['https://www.test.bbc.com/afaanoromoo/oduu-23141286'],
+  },
+
+  STY: {
+    live: [],
+    test: [
+      'https://www.test.bbc.com/pashto/afghanistan-52643309?renderer_env=live',
+    ],
+  },
+};
+
+const urlsToAudit = Object.entries(AUDIT_URLS)
+  .map(([, urls]) => urls[process.env.LIGHTHOUSE_APP_ENV])
+  .flat();
+
 module.exports = {
   ci: {
     collect: {
-      url:
-        process.env.LIGHTHOUSE_APP_ENV === 'live'
-          ? [
-              'https://www.bbc.com/arabic',
-              'https://www.bbc.com/persian/afghanistan',
-              'https://www.bbc.com/korean/bbc_korean_radio/liveradio',
-              'https://www.bbc.com/vietnamese/popular/read',
-              'https://www.bbc.com/afrique/media/video',
-              'https://www.bbc.com/pashto/bbc_pashto_radio/programmes/p056fcjb',
-              'https://www.bbc.com/pashto/bbc_pashto_tv/tv_programmes/w13xttn4',
-            ]
-          : ['https://test.bbc.com/korean/bbc_korean_radio/liveradio'],
+      url: urlsToAudit,
       numberOfRuns: 3,
       settings: {
         chromeFlags: '--no-sandbox --headless',
