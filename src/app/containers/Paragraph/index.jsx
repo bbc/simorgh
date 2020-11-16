c5dd88d772c5d39d9f0f350996986518fa9adaeb
emilysaffron <emiy.saffron@bbc.co.uk>
2020-11-16 15:54:07 +0000

    styled paragraph, 40 px right padding

--- src/app/containers/Paragraph/index.jsx
+++ src/app/containers/Paragraph/index.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import Paragraph from '@bbc/psammead-paragraph';
+import styled from '@emotion/styled';
+import { GEL_GROUP_4_SCREEN_WIDTH_MIN } from '@bbc/gel-foundations/breakpoints';
 import { ServiceContext } from '#contexts/ServiceContext';
 import Blocks from '../Blocks';
 import fragment from '../Fragment';
@@ -10,14 +12,20 @@ import { GridItemMedium } from '#app/components/Grid';
 
 const componentsToRender = { fragment, urlLink: InlineLink, inline: Inline };
 
+const StyledParagraph = styled(Paragraph)`
+  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
+    padding-right: 40px;
+  }
+`;
+
 const ParagraphContainer = ({ blocks }) => {
   const { script, service } = useContext(ServiceContext);
 
   return (
     <GridItemMedium>
-      <Paragraph script={script} service={service}>
+      <StyledParagraph script={script} service={service}>
         <Blocks blocks={blocks} componentsToRender={componentsToRender} />
-      </Paragraph>
+      </StyledParagraph>
     </GridItemMedium>
   );
 };
