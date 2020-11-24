c6701f91b1ba5ae3c96e49439120c5c42d310106
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2020-11-24 12:15:44 +0000

    Revert "Remove unused import"
    This reverts commit 93f6ce39e9da0db30df3cb5831740d953ac65c60.

--- src/app/routes/onDemandTV/getInitialData/index.js
+++ src/app/routes/onDemandTV/getInitialData/index.js
@@ -1,3 +1,4 @@
+import path from 'ramda/src/path';
 import fetchPageData from '../../utils/fetchPageData';
 import overrideRendererOnTest from '../../utils/overrideRendererOnTest';
 import getPlaceholderImageUrl from '../../utils/getPlaceholderImageUrl';
