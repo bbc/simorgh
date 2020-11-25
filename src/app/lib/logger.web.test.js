ce18365e78e156aeb27e0048cfcf8ba50fcb0bc2
Fahad Khan <fk17abb@protonmail.com>
2020-11-25 15:24:09 +0000

    test: test production/dev
    Ensure that we are logging everything on dev clients, but not in production

--- src/app/lib/logger.web.test.js
+++ src/app/lib/logger.web.test.js
@@ -16,12 +16,13 @@ const expectNotCalled = (object, keys) => {
   });
 };
 
-describe('Logger Web - for the client-side', () => {
+describe('Logger Web - for the client-side in production', () => {
   afterEach(() => {
     jest.clearAllMocks();
+    process.env.NODE_ENV = 'production';
   });
 
-  it('logger.error should run console.error', () => {
+  it('logger.error should run console.error in production', () => {
     const loggerInstance = loggerWeb('');
     loggerInstance.error(event, message);
 
@@ -29,38 +30,93 @@ describe('Logger Web - for the client-side', () => {
     expectNotCalled(global.console, ['debug', 'info', 'log', 'warn']);
   });
 
-  it('logger.warn should not run console.warn', () => {
+  it('logger.warn should not run console.warn in production', () => {
     const loggerInstance = loggerWeb('');
     loggerInstance.warn(message);
 
     expect(global.console.warn).not.toHaveBeenCalled();
   });
 
-  it('logger.info should not run console.info', () => {
+  it('logger.info should not run console.info in production', () => {
     const loggerInstance = loggerWeb('');
     loggerInstance.info(message);
 
     expect(global.console.info).not.toHaveBeenCalled();
   });
 
-  it('logger.debug should not run console.debug', () => {
+  it('logger.debug should not run console.debug in production', () => {
     const loggerInstance = loggerWeb('');
     loggerInstance.debug(message);
 
     expect(global.console.debug).not.toHaveBeenCalled();
   });
 
-  it('logger.verbose should not run console.log', () => {
+  it('logger.verbose should not run console.log in production', () => {
     const loggerInstance = loggerWeb('');
     loggerInstance.verbose(message);
 
     expect(global.console.log).not.toHaveBeenCalled();
   });
 
-  it('logger.silly should not run console.log', () => {
+  it('logger.silly should not run console.log in production', () => {
     const loggerInstance = loggerWeb('');
     loggerInstance.silly(message);
 
     expect(global.console.log).not.toHaveBeenCalled();
+  });
+});
+
+describe('Logger Web - for the client-side in development', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+    process.env.NODE_ENV = 'development';
+  });
+
+  it('logger.error should run console.error in development', () => {
+    const loggerInstance = loggerWeb('');
+    loggerInstance.error(event, message);
+
+    expect(global.console.error).toHaveBeenCalledWith({ event, message });
+    expectNotCalled(global.console, ['debug', 'info', 'log', 'warn']);
+  });
+
+  it('logger.warn should run console.warn in development', () => {
+    const loggerInstance = loggerWeb('');
+    loggerInstance.warn(event, message);
+
+    expect(global.console.warn).toHaveBeenCalledWith({ event, message });
+    expectNotCalled(global.console, ['debug', 'error', 'info', 'log']);
+  });
+
+  it('logger.info should run console.info in development', () => {
+    const loggerInstance = loggerWeb('');
+    loggerInstance.info(event, message);
+
+    expect(global.console.info).toHaveBeenCalledWith({ event, message });
+    expectNotCalled(global.console, ['debug', 'error', 'log', 'warn']);
+  });
+
+  it('logger.debug should run console.debug in development', () => {
+    const loggerInstance = loggerWeb('');
+    loggerInstance.debug(event, message);
+
+    expect(global.console.debug).toHaveBeenCalledWith({ event, message });
+    expectNotCalled(global.console, ['error', 'info', 'log', 'warn']);
+  });
+
+  it('logger.verbose should run console.log in development', () => {
+    const loggerInstance = loggerWeb('');
+    loggerInstance.verbose(event, message);
+
+    expect(global.console.log).toHaveBeenCalledWith({ event, message });
+    expectNotCalled(global.console, ['debug', 'error', 'info', 'warn']);
+  });
+
+  it('logger.silly should run console.log in development', () => {
+    const loggerInstance = loggerWeb('');
+    loggerInstance.silly(event, message);
+
+    expect(global.console.log).toHaveBeenCalledWith({ event, message });
+    expectNotCalled(global.console, ['debug', 'error', 'info', 'warn']);
   });
 });
