bf309603b4abe1bee7d6bd7ad64e5d38848c729d
Harvey Peachey <harvey.peachey@bbc.co.uk>
2020-11-26 18:00:04 +0000

    Imported and used pageType constants

--- src/app/routes/home/index.js
+++ src/app/routes/home/index.js
@@ -1,11 +1,12 @@
 import { FrontPage } from '#pages';
 import getInitialData from './getInitialData';
 import { frontPagePath } from '../utils/regex';
+import { FRONT_PAGE } from '../utils/pageTypes';
 
 export default {
   path: frontPagePath,
   exact: true,
   component: FrontPage,
   getInitialData,
-  pageType: 'frontPage',
+  pageType: FRONT_PAGE,
 };
