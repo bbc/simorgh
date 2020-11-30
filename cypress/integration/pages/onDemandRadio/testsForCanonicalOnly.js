3932c3d7fbc0ef51c3a5f4480cbd57286b42458b
LilyL0u <lily.higham@bbc.co.uk>
2020-11-30 17:57:12 +0000

    recent episodes test added with class locators

--- cypress/integration/pages/onDemandRadio/testsForCanonicalOnly.js
+++ cypress/integration/pages/onDemandRadio/testsForCanonicalOnly.js
@@ -1,4 +1,5 @@
 import path from 'ramda/src/path';
+import getDataUrl from '../../../support/helpers/getDataUrl';
 import config from '../../../support/config/services';
 import envConfig from '../../../support/config/envs';
 import { isScheduleDataComplete } from '../../../../src/app/containers/RadioSchedule/utilities/evaluateScheduleData';
@@ -63,6 +64,55 @@ export default ({ service, pageType, variant }) => {
             });
           } else {
             cy.get('[data-e2e=radio-schedule]').should('not.exist');
+          }
+        });
+      });
+    });
+    describe('Recent Episodes component', () => {
+      it('should be displayed if the toggle is on, and shows the expected number of items (max 4)', function test() {
+        cy.fixture(`toggles/${config[service].name}.json`).then(toggles => {
+          const recentEpisodesEnabled = path(
+            ['recentAudioEpisodes', 'enabled'],
+            toggles,
+          );
+          cy.log(`Recent Episodes component enabled? ${recentEpisodesEnabled}`);
+
+          if (recentEpisodesEnabled) {
+            const currentPath = Cypress.env('currentPath');
+            const url =
+              Cypress.env('APP_ENV') === 'test'
+                ? `${currentPath}?renderer_env=live`
+                : `${currentPath}`;
+
+            cy.request(getDataUrl(url)).then(({ body }) => {
+              const maxNumberofItems = 4;
+              const numberOfEpisodes =
+                body.relatedContent.groups[0].promos.length;
+
+              cy.log(`Number of recent episodes ? ${numberOfEpisodes}`);
+              if (numberOfEpisodes > 0) {
+                cy.get("ul[class*='css-1ddpce6-StyledEpisodeList']").should(
+                  'exist',
+                );
+
+                const expectedNumberOfEpisodes = Math.min(
+                  numberOfEpisodes,
+                  maxNumberofItems,
+                );
+                cy.get("ul[class*='css-1ddpce6-StyledEpisodeList']").within(
+                  () => {
+                    cy.get("li[class*='css-9kvqqh-StyledEpisodeListItem']")
+                      .its('length')
+                      .should('eq', expectedNumberOfEpisodes);
+                  },
+                );
+              } else {
+                cy.get("div[id*='recent-episodes']").should('not.exist');
+              }
+            });
+          } else {
+            cy.get("div[id*='recent-episodes']").should('not.exist');
+            cy.log('No recent episodes in data');
           }
         });
       });
