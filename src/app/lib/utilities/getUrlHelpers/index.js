91da452fcb2280b7c5e696e2339f785b99af3728
L0u155 <72620593+L0u155@users.noreply.github.com>
2020-12-02 16:05:12 +0000

    Removed non url utility imports from getUrlHelpers

--- src/app/lib/utilities/getUrlHelpers/index.js
+++ src/app/lib/utilities/getUrlHelpers/index.js
@@ -1,17 +1,11 @@
 import getEmbedUrl from './getEmbedUrl';
-import getMasterbrand from '../getMasterbrand';
-import getMediaId from '../getMediaId';
 import getMostReadUrls from './getMostReadUrls';
 import getRadioSchedulesUrls from './getRadioSchedulesUrls';
 import getSecondaryColumnUrl from './getSecondaryColumnUrl';
-import getStoryPromoInfo from '../getStoryPromoInfo';
 
 export default {
   getEmbedUrl,
-  getMasterbrand,
-  getMediaId,
   getMostReadUrls,
   getRadioSchedulesUrls,
   getSecondaryColumnUrl,
-  getStoryPromoInfo,
 };
