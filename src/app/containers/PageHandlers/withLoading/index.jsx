79a69e05199a0b9fc92299ddac17f5a20b481408
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2020-12-02 12:00:15 +0000

    set focus on loading message after 500ms

--- src/app/containers/PageHandlers/withLoading/index.jsx
+++ src/app/containers/PageHandlers/withLoading/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useLayoutEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { bool, element } from 'prop-types';
 import styled from '@emotion/styled';
 import VisuallyHiddenText from '@bbc/psammead-visually-hidden-text';
@@ -12,26 +12,21 @@ const LoadingMain = styled.main`
 
 const WithLoading = Component => {
   const LoadingContainer = ({ loading, ...props }) => {
-    const [showLoading, setShowLoading] = useState(false);
-    console.log('xxx hello');
+    const loadingMessageRef = useRef();
 
     useEffect(() => {
       if (loading) {
         timeout = setTimeout(() => {
-          console.log('xxx setShowLoading');
-
-          setShowLoading(true);
+          if (loadingMessageRef) {
+            loadingMessageRef.current.focus();
+          }
         }, 500);
       }
-      return () => clearTimeout(timeout);
-    }, [loading]);
 
-    useLayoutEffect(() => {
-      if (showLoading) {
-        document.querySelector('#loading').focus();
-        console.log('xxx showLoading is true');
-      }
-    }, [showLoading]);
+      return () => {
+        clearTimeout(timeout);
+      };
+    }, [loading]);
 
     if (!loading) return <Component {...props} />;
 
@@ -39,13 +34,9 @@ const WithLoading = Component => {
       <LoadingMain role="main">
         <GridWrapper>
           <GridItemMedium>
-            {showLoading && (
-              <div data-testid="loading">
-                <VisuallyHiddenText tabIndex="-1" id="loading">
-                  Loading next page.
-                </VisuallyHiddenText>
-              </div>
-            )}
+            <div tabIndex="-1" ref={loadingMessageRef}>
+              <VisuallyHiddenText>Loading next page.</VisuallyHiddenText>
+            </div>
           </GridItemMedium>
         </GridWrapper>
       </LoadingMain>
