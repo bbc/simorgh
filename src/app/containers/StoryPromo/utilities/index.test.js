3693348250d8f439e947294e856fbf169c93fd52
Fahad Khan <fk17abb@protonmail.com>
2021-01-06 11:38:11 +0000

    use constants in more files

--- src/app/containers/StoryPromo/utilities/index.test.js
+++ src/app/containers/StoryPromo/utilities/index.test.js
@@ -2,11 +2,12 @@ import { isMap, isPgl, getHeadingTagOverride } from '.';
 import {
   MOST_WATCHED_PAGE,
   PHOTO_GALLERY_PAGE,
+  MEDIA_ASSET_PAGE,
 } from '#app/routes/utils/pageTypes';
 
 describe('isMap', () => {
   it('should return true if cpsType is MAP', () => {
-    const map = { cpsType: 'MAP' };
+    const map = { cpsType: MEDIA_ASSET_PAGE };
     expect(isMap(map)).toBe(true);
   });
 
@@ -16,7 +17,7 @@ describe('isMap', () => {
   });
 
   it("should return false if media doesn't exist and cpsType isn't MAP", () => {
-    const pgl = { cpsType: 'PGL' };
+    const pgl = { cpsType: PHOTO_GALLERY_PAGE };
     expect(isMap(pgl)).toBe(false);
   });
 
@@ -28,12 +29,12 @@ describe('isMap', () => {
 
 describe('isPgl', () => {
   it('should return true if cpsType is PGL', () => {
-    const pgl = { cpsType: 'PGL' };
+    const pgl = { cpsType: PHOTO_GALLERY_PAGE };
     expect(isPgl(pgl)).toBe(true);
   });
 
   it("should return false if cpsType isn't PGL", () => {
-    const map = { cpsType: 'MAP' };
+    const map = { cpsType: MEDIA_ASSET_PAGE };
     expect(isPgl(map)).toBe(false);
   });
 
