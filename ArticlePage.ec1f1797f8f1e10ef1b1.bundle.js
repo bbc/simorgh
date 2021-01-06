e0f5ed1092338fe8b86c1325392b4f1dd59cdf31
jroebu14 <jonathan.roebuck@bbc.co.uk>
2021-01-06 11:05:49 +0000

    Deploying to gh-pages from  @ 421a7543ec99ffc9f2c933c66d046b5420251bac 🚀

--- ArticlePage.ec1f1797f8f1e10ef1b1.bundle.js
+++ ArticlePage.ec1f1797f8f1e10ef1b1.bundle.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[2],{867:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var _ArticlePage__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(792),_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__(176);__webpack_exports__.default=Object(_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__.a)({addVariantHandling:!0})(_ArticlePage__WEBPACK_IMPORTED_MODULE_0__.a)}}]);
-//# sourceMappingURL=ArticlePage.840f1d640d2b578dd543.bundle.js.map
+//# sourceMappingURL=ArticlePage.ec1f1797f8f1e10ef1b1.bundle.js.map
