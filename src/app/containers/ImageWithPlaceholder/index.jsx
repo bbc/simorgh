25b6cb94742365fc64133fd63ddca6455e2aadd3
Che Wu <che.wu@bbc.co.uk>
2021-01-12 15:07:36 +0000

    Add toggle to container

--- src/app/containers/ImageWithPlaceholder/index.jsx
+++ src/app/containers/ImageWithPlaceholder/index.jsx
@@ -5,6 +5,7 @@ import ImagePlaceholder from '@bbc/psammead-image-placeholder';
 import Image, { AmpImg } from '@bbc/psammead-image';
 import { Helmet } from 'react-helmet';
 import { RequestContext } from '#contexts/RequestContext';
+import useToggle from '#hooks/useToggle';
 
 const LAZYLOAD_OFFSET = 250; // amount of pixels below the viewport to begin loading the image
 
@@ -42,7 +43,9 @@ const ImageWithPlaceholder = ({
     <Image onLoad={() => setIsLoaded(true)} {...imageProps} />
   );
 
-  const shouldPreload = !isAmp && preload;
+  const { enabled: preloadLeadImageToggle } = useToggle('preloadLeadImage');
+
+  const shouldPreload = !isAmp && preload && preloadLeadImageToggle;
 
   return (
     <>
