567aee369da78c81bbd6665e43396ea63ceb02c1
Chris <chris.hinds@bbc.co.uk>
2021-01-21 12:17:22 +0000

    Fix spelling error

--- src/server/utilities/encodeChunkUri/index.js
+++ src/server/utilities/encodeChunkUri/index.js
@@ -1,6 +1,6 @@
 const encodeChunkFilename = ({ url: chunkUrl }) => {
   const pathParts = chunkUrl.split('/'); // Split full url at each `/`
-  const fileName = pathParts.pop(); // Feth the content after the last `/`
+  const fileName = pathParts.pop(); // Fetch the content after the last `/`
   const encodedFileName = encodeURIComponent(fileName); // Encode the filename
 
   return pathParts.join('/').concat(`/${encodedFileName}`); // reconstruct the full url with the encoded filename
