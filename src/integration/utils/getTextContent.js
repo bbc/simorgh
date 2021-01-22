bf4c0df1a2bf60777c2637776a308d2e9b5ffcea
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2021-01-22 15:45:13 +0000

    Revert "JoRo impl"
    This reverts commit f7eeb3550c4aed3bce2d9359306123e821ed1278.

--- src/integration/utils/getTextContent.js
+++ src/integration/utils/getTextContent.js
@@ -1,17 +1,2 @@
-const old = el =>
+module.exports = el =>
   el.textContent.replace(/[.@]([a-z].*?)}/g, '').replace(/}/g, '');
-
-const getTextContent = el => {
-  if (el.children && el.children.length) {
-    return Array.from(el.childNodes)
-      .map(node => getTextContent(node))
-      .flat()
-      .join('');
-  }
-  if (el.tagName !== 'STYLE') {
-    return el.textContent;
-  }
-  return '';
-};
-
-module.exports = getTextContent;
