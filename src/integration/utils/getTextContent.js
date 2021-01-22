f7eeb3550c4aed3bce2d9359306123e821ed1278
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2021-01-22 15:29:48 +0000

    JoRo impl

--- src/integration/utils/getTextContent.js
+++ src/integration/utils/getTextContent.js
@@ -1,2 +1,17 @@
-module.exports = el =>
+const old = el =>
   el.textContent.replace(/[.@]([a-z].*?)}/g, '').replace(/}/g, '');
+
+const getTextContent = el => {
+  if (el.children && el.children.length) {
+    return Array.from(el.childNodes)
+      .map(node => getTextContent(node))
+      .flat()
+      .join('');
+  }
+  if (el.tagName !== 'STYLE') {
+    return el.textContent;
+  }
+  return '';
+};
+
+module.exports = getTextContent;
