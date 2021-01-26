fc91dc129694b01e91c85ce8f725f6056d0dfa5e
bennea27 <andrew.bennett07@bbc.co.uk>
2021-01-26 11:26:32 +0000

    turn include test back on + disable console error failures

--- cypress/integration/pages/storyPage/testsForCanonicalOnly.js
+++ cypress/integration/pages/storyPage/testsForCanonicalOnly.js
@@ -11,7 +11,7 @@ export const testsThatAlwaysRunForCanonicalOnly = ({ service }) => {
     // our story pages should not do this. The test checks the core content has been removed
     // following progressive enhancement by the include's inline scripts.
     // This test specifically is targeted at this test asset: '/mundo/23263889'
-    it.skip('should load the eclipse VJ include successfully', () => {
+    it('should load the eclipse VJ include successfully', () => {
       if (service === 'mundo') {
         cy.get(
           '#responsive-embed-vjamericas-176-eclipse-lookup-app-core-content',
