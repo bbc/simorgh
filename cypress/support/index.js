7edab3184d0189cf834ffdec73aa899701573cbf
bennea27 <andrew.bennett07@bbc.co.uk>
2021-01-27 11:40:24 +0000

    replace exception catch for amp analytics bug with one for an unexplained error in an includes test

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -11,18 +11,17 @@ Cypress.on(`window:before:load`, win => {
   });
 });
 
-// Workaround for AMP Analytics bug - there is a pending fix here: https://github.com/ampproject/amphtml/pull/28448
-// Can be removed when this is resolved.
+// Catches an unexplained error experienced while running the following test:
+// https://github.com/bbc/simorgh/blob/49e5b72db84df57144a92963734bcd080938e45b/cypress/integration/pages/storyPage/testsForCanonicalOnly.js#L14
+// We decided we could not invest any more time in the unexplained error as the test
+// successfully functioned with this specific error caught and discarded.
 // eslint-disable-next-line consistent-return
-Cypress.on('uncaught:exception', () => {
+Cypress.on('uncaught:exception', err => {
   // returning false here prevents Cypress from failing the test
-  // if (
-  //   err.message &&
-  //   err.message.includes(
-  //     'Inline or remote config should not overwrite vendor transport settings',
-  //   )
-  // ) {
-  //   return false;
-  // }
-  return false;
+  if (
+    err.message &&
+    err.message.includes("Cannot read property 'postMessage' of undefined")
+  ) {
+    return false;
+  }
 });
