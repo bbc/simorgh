809aac08f69e87fea40e419e989bdc52ca959f7b
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-02-02 16:32:01 +0000

    remove console.log

--- scripts/check-package-manager.js
+++ scripts/check-package-manager.js
@@ -1,6 +1,5 @@
 const ensureYarn = () => {
   // If npm is being run, print a message and cause 'npm install' to fail.
-  console.log('process.env.npm_execpath', process.env.npm_execpath);
   if (!process.env.npm_execpath.includes('yarn')) {
     console.log(
       [
