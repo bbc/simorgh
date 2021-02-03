6e2dd03651f57b892dfd6cb8ae000656b26b0ebf
Gerald Prince Pharin <gerald@pharin.me>
2021-02-03 10:10:41 +0000

    Change ODParagraph non-dark mode colour

--- src/app/containers/OnDemandParagraph/index.jsx
+++ src/app/containers/OnDemandParagraph/index.jsx
@@ -4,9 +4,11 @@ import { string, bool } from 'prop-types';
 import { GEL_SPACING } from '@bbc/gel-foundations/spacings';
 import { GEL_GROUP_3_SCREEN_WIDTH_MAX } from '@bbc/gel-foundations/breakpoints';
 import ParagraphComponent from '@bbc/psammead-paragraph';
+import { C_METAL } from '@bbc/psammead-styles/colours';
 import { ServiceContext } from '#contexts/ServiceContext';
 
 const StyledParagraphComponent = styled(ParagraphComponent)`
+  ${({ darkMode }) => !darkMode && `color: ${C_METAL};`}
   @media (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
     padding-bottom: ${GEL_SPACING};
   }
