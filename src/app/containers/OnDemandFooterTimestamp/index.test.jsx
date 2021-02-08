475f369175ff6a49432548bf825909b87839d04a
Gerald Prince Pharin <gerald@pharin.me>
2021-02-08 21:39:17 +0000

    Update odfootertimestamp tests

--- src/app/containers/OnDemandFooterTimestamp/index.test.jsx
+++ src/app/containers/OnDemandFooterTimestamp/index.test.jsx
@@ -6,9 +6,12 @@ import { ServiceContextProvider } from '#contexts/ServiceContext';
 import FooterTimestamp from '.';
 
 // eslint-disable-next-line react/prop-types
-const RenderTimestamp = ({ releaseDateTimeStamp }) => (
+const RenderTimestamp = ({ releaseDateTimeStamp, darkMode = false }) => (
   <ServiceContextProvider service="afrique">
-    <FooterTimestamp releaseDateTimeStamp={releaseDateTimeStamp} />
+    <FooterTimestamp
+      releaseDateTimeStamp={releaseDateTimeStamp}
+      darkMode={darkMode}
+    />
   </ServiceContextProvider>
 );
 
@@ -22,11 +25,40 @@ describe('OnDemandFooterTimestamp', () => {
       locale: 'fr',
       isRelative: false,
     });
+    const dateTime = formatUnixTimestamp({
+      timestamp: releaseDateTimeStamp,
+      format: 'YYYY-MM-DD',
+      timezone: 'GMT',
+      locale: 'fr',
+      isRelative: false,
+    });
     const { getByText } = await render(
       <RenderTimestamp releaseDateTimeStamp={releaseDateTimeStamp} />,
     );
     const el = getByText(formattedTimestamp);
     const style = window.getComputedStyle(el);
+    expect(el.tagName).toBe('TIME');
+    expect(el.getAttribute('dateTime')).toBe(dateTime);
+    expect(style.color).toBe('rgb(110, 110, 115)');
+    expect(style.display).toBe('inline-block');
+    expect(style.marginTop).toBe('0.5rem');
+  });
+
+  it('should have the right styling on dark mode', async () => {
+    const releaseDateTimeStamp = 1603065600000;
+    const formattedTimestamp = formatUnixTimestamp({
+      timestamp: releaseDateTimeStamp,
+      format: 'LL',
+      timezone: 'GMT',
+      locale: 'fr',
+      isRelative: false,
+    });
+    const { getByText } = await render(
+      <RenderTimestamp releaseDateTimeStamp={releaseDateTimeStamp} darkMode />,
+    );
+    const el = getByText(formattedTimestamp);
+    const style = window.getComputedStyle(el);
+    expect(el.tagName).toBe('TIME');
     expect(style.color).toBe('rgb(174, 174, 181)');
     expect(style.display).toBe('inline-block');
     expect(style.marginTop).toBe('0.5rem');
