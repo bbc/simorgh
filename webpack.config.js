2914673e41e9d474bfeb234aa28f10bdd621565a
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-02-17 12:29:24 +0000

    remove storybook deps and emotion alasies in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -48,18 +48,6 @@ module.exports = (shell = {}) => {
           'scheduler/tracing': 'scheduler/tracing-profiling',
         }),
         'safe-buffer': path.resolve(__dirname, 'node_modules/safe-buffer'),
-        '@emotion/serialize': path.resolve(
-          __dirname,
-          'node_modules/@emotion/serialize',
-        ),
-        '@emotion/utils': path.resolve(
-          __dirname,
-          'node_modules/@emotion/utils',
-        ),
-        '@emotion/sheet': path.resolve(
-          __dirname,
-          'node_modules/@emotion/sheet',
-        ),
       },
     },
     devServer: {
