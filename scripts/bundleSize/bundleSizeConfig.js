589a55aec57b7f67b579e33d23327ea81eb398a6
bennea27 <andrew.bennett07@bbc.co.uk>
2021-02-17 12:01:34 +0000

    revert unexpected changes from E2E branch

--- scripts/bundleSize/bundleSizeConfig.js
+++ scripts/bundleSize/bundleSizeConfig.js
@@ -5,6 +5,6 @@ module.exports = {
   // below the smallest value in the build output; this avoids the
   // need for frequent changes as bundle sizes fluctuate.
 
-  MIN_SIZE: 574,
-  MAX_SIZE: 900,
+  MIN_SIZE: 580,
+  MAX_SIZE: 863,
 };
