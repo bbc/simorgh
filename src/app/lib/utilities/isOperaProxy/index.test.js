3136ba67652f89093faad6d4f0332d1a8c4f0283
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2021-02-25 15:00:46 +0000

    Fix broken test

--- src/app/lib/utilities/isOperaProxy/index.test.js
+++ src/app/lib/utilities/isOperaProxy/index.test.js
@@ -1,29 +1,27 @@
-import { setWindowValue, resetWindowValue } from '@bbc/psammead-test-helpers';
 import isOperaProxy from '.';
 
-const originalWindowOperaMini = window.OperaMini;
+const originalWindowOperaMini = window.operamini;
 
 describe('isOperaProxy', () => {
   afterEach(() => {
-    resetWindowValue('operamini', originalWindowOperaMini);
+    window.operamini = originalWindowOperaMini;
   });
 
   it('returns false when not invoked by the opera mini proxy', () => {
-    setWindowValue('operamini', null);
+    window.operamini = null;
 
     expect(isOperaProxy()).not.toBeTruthy();
   });
 
   it('returns true when invoked by the opera mini proxy', () => {
-    class Opera {
+    class OperaMiniMock {
       // eslint-disable-next-line class-methods-use-this
       get [Symbol.toStringTag]() {
         return 'OperaMini';
       }
     }
+    window.operamini = new OperaMiniMock();
 
-    setWindowValue('operamini', new Opera());
-
-    expect(isOperaProxy()).not.toBeTruthy();
+    expect(isOperaProxy()).toBeTruthy();
   });
 });
