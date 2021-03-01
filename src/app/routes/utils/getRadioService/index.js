1e03cf0f19976163a57e9efda78afdbfb82f1d7e
RichardPK <r.phillips.kerr@gmail.com>
2021-03-01 17:19:45 +0000

    Add correct persian service mapping when podcast (previously returning dari)

--- src/app/routes/utils/getRadioService/index.js
+++ src/app/routes/utils/getRadioService/index.js
@@ -6,8 +6,14 @@ const RADIO_SERVICE_MAPPINGS = {
 };
 
 export default ({ service, pathname }) => {
-  if (service === 'persian' && pathname.includes('bbc_persian_radio')) {
+  const isPodcast = pathname.includes('podcasts');
+
+  if (
+    service === 'persian' &&
+    (pathname.includes('bbc_persian_radio') || isPodcast)
+  ) {
     return 'persian';
   }
+
   return RADIO_SERVICE_MAPPINGS[service] || service;
 };
