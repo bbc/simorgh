bafe4c3872947c3d858a0d19235562a46e005338
RichardPK <r.phillips.kerr@gmail.com>
2021-03-04 12:47:21 +0000

    Remove is(Array) ramda check as Cypress doesn't like it...

--- src/app/routes/utils/processRecentEpisodes/index.jsx
+++ src/app/routes/utils/processRecentEpisodes/index.jsx
@@ -11,14 +11,10 @@ const validateEpisode = episode => {
       is(String, episode.brand.title),
       episode.media,
       is(String, episode.media.id),
-      is(Array, episode.media.versions),
       episode.media.versions[0],
       is(String, episode.media.versions[0].durationISO8601),
       is(Number, episode.releaseDateTimestamp),
     ];
-    console.log(episode.media.versions, '- versions');
-    console.log(checks, '- checks');
-
     return checks.every(Boolean);
   } catch {
     return false;
