bef94876a3977df0dadb915a5b10d4cdb660d4c6
Simon Sinclair <simon@simonsinclair.com>
2021-03-24 17:32:58 +0000

    Add pseudocode and comments

--- src/app/containers/ATIAnalytics/amp/ampAnalyticsJson.js
+++ src/app/containers/ATIAnalytics/amp/ampAnalyticsJson.js
@@ -1,18 +1,39 @@
 // The pageview request needs to have the ${base} value not interpolated.
 /* eslint-disable no-template-curly-in-string */
 /* eslint-disable prefer-template */
+import Url from 'url-parse';
 
-const ampAnalyticsJson = ({ baseUrl, pageviewParams }) => ({
-  transport: {
-    beacon: false,
-    xhrpost: false,
-    image: true,
-  },
-  requests: {
-    base: baseUrl,
-    pageview: '${base}' + pageviewParams,
-  },
-  triggers: { trackPageview: { on: 'visible', request: 'pageview' } },
-});
+const ampAnalyticsJson = ({ baseUrl, pageviewParams, ampGeoGroup }) => {
+  /**
+   * The 's' query param in `pageviewParams` should be set according to a
+   * user's geographic location. Currently it is set based on the TLD
+   * (.com or .co.uk), which is not an accurate way to determine a user's
+   * location. On AMP, we use amp-geo to set this value more accurately.
+   *
+   * EXAMPLE:
+   */
+  const url = new Url(`${baseUrl}${pageviewParams}`, true);
+
+  url.set('query', {
+    ...url.query,
+    s: 'derive this value using `ampGeoGroup` value',
+  });
+
+  // With `url` query params modified, they can be then be used instead of
+  // `pageviewParams` below.
+
+  return {
+    transport: {
+      beacon: false,
+      xhrpost: false,
+      image: true,
+    },
+    requests: {
+      base: baseUrl,
+      pageview: '${base}' + pageviewParams,
+    },
+    triggers: { trackPageview: { on: 'visible', request: 'pageview' } },
+  };
+};
 
 export default ampAnalyticsJson;
--- src/app/containers/ATIAnalytics/amp/index.jsx
+++ src/app/containers/ATIAnalytics/amp/index.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import { string } from 'prop-types';
+
 import getAmpAnalyticsJson from './ampAnalyticsJson';
 
-const JsonInlinedScript = data => (
-  <script
-    type="application/json"
-    /* eslint-disable-next-line react/no-danger */
-    dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
-  />
+const AmpATIAnalytics = ({ pageviewParams }) => (
+  <amp-analytics>
+    <script
+      type="application/json"
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(
+          getAmpAnalyticsJson({
+            baseUrl: process.env.SIMORGH_ATI_BASE_URL,
+            pageviewParams,
+            ampGeoGroup: 'capture amp-geo state here and pass it through?',
+          }),
+        ),
+      }}
+    />
+  </amp-analytics>
 );
-
-const AmpATIAnalytics = ({ pageviewParams }) => {
-  return (
-    <amp-analytics>
-      {JsonInlinedScript(
-        getAmpAnalyticsJson({
-          baseUrl: process.env.SIMORGH_ATI_BASE_URL,
-          pageviewParams,
-        }),
-      )}
-    </amp-analytics>
-  );
-};
 
 AmpATIAnalytics.propTypes = {
   pageviewParams: string.isRequired,
