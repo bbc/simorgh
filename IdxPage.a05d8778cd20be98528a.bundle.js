1b6bb6c74687a7479f7db51d32ae972c1b0677d4
mukama <mcaesar88@gmail.com>
2021-03-29 12:06:37 +0000

    Deploying to gh-pages from  @ c71cfefeaffac22f9490effb4315b03b6979d2ab 🚀

--- IdxPage.a05d8778cd20be98528a.bundle.js
+++ IdxPage.a05d8778cd20be98528a.bundle.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[3],{1551:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var _IdxPage__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(1473),_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__(241);__webpack_exports__.default=Object(_utils_applyBasicPageHandlers__WEBPACK_IMPORTED_MODULE_1__.a)({addVariantHandling:!1})(_IdxPage__WEBPACK_IMPORTED_MODULE_0__.a)}}]);
-//# sourceMappingURL=IdxPage.d2880f61f9bdbd179450.bundle.js.map
+//# sourceMappingURL=IdxPage.a05d8778cd20be98528a.bundle.js.map
