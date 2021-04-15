ae0a306b407eca3d2c5516760a7a244e76d180da
HarryVerhoef <harry.verhoef1999@gmail.com>
2021-04-15 23:43:55 +0100

    Remove use of for loop

--- src/app/components/TeamJellyHack/StoryNav/index.jsx
+++ src/app/components/TeamJellyHack/StoryNav/index.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
-import styled from '@emotion/styled';
 
 const NavButton = ({ isCurrentPage, index, setCurrentPage }) => {
   return <button onClick={() => setCurrentPage(index)} />;
 };
 
 const StoryNav = ({ numStories, currentPage, setCurrentPage }) => {
-  const navButtons = [];
-  for (let i = 0; i < numStories; i++) {
-    navButtons.push(
+  const navButtons = new Array(numStories).map((_, index) => {
+    return (
       <li>
         <NavButton
-          isCurrentPage={i === currentPage}
-          index={i}
+          isCurrentPage={index === currentPage}
+          index={index}
           setCurrentPage={setCurrentPage}
         />
-      </li>,
+      </li>
     );
-  }
+  });
+  console.log(navButtons);
+  // for (let i = 0; i < numStories; i++) {
+  //   navButtons.push(
+  // <li>
+  //   <NavButton
+  //     isCurrentPage={i === currentPage}
+  //     index={i}
+  //     setCurrentPage={setCurrentPage}
+  //   />
+  // </li>,
+  //   );
+  // }
   return <ol>{navButtons}</ol>;
 };
 
