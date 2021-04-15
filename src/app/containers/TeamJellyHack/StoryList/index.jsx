eb294755b5f852fd3c48fc6821970e7bb51b7ae2
HarryVerhoef <harry.verhoef1999@gmail.com>
2021-04-15 15:46:27 +0100

    StoryList stuff

--- src/app/containers/TeamJellyHack/StoryList/index.jsx
+++ src/app/containers/TeamJellyHack/StoryList/index.jsx
@@ -1,20 +1,34 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
 import styled from '@emotion/styled';
 
-
-const StoryItem = ({ id, name, src, hasViewed }) => {
+const StoryItem = ({ className, showStory, id, name, src, hasViewed }) => {
   return (
-    <li id={id} className={hasViewed && 'has-viewed'}>
+    <li
+      id={id}
+      onClick={() => showStory(id)}
+      className={`${className} ${hasViewed && 'has-viewed'}`}
+    >
       <img src={src} alt={name} />
       <h4>{name}</h4>
     </li>
   );
 };
 
-const StoryList = ({ data }) => {
+const StyledStoryItem = styled(StoryItem)``;
+
+const StoryList = ({ data, showStory }) => {
   const topics = data.map(({ id, name, src, hasViewed }) => {
     return (
-      <StoryItem key={id} id={id} name={name} src={src} hasViewed={hasViewed} />
+      <StoryItem
+        key={id}
+        id={id}
+        name={name}
+        src={src}
+        hasViewed={hasViewed}
+        showStory={showStory}
+      />
     );
   });
 
