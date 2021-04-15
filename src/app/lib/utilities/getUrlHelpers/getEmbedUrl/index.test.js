6ce00bf4f1e4d2dc15b6cd0d4d44997cd84d6731
TimHewitt <tim.hewitt@gmail.com>
2021-04-15 10:49:44 +0100

    Update TEST_CANONICAL_URL to have www in domain

--- src/app/lib/utilities/getUrlHelpers/getEmbedUrl/index.test.js
+++ src/app/lib/utilities/getUrlHelpers/getEmbedUrl/index.test.js
@@ -13,7 +13,7 @@ const setEnvironment = environment => {
 const testCases = [
   {
     description: `should build a CANONICAL url for articles in test environment`,
-    expected: `https://test.bbc.com/ws/av-embeds/articles/${mediaId}`,
+    expected: `https://www.test.bbc.com/ws/av-embeds/articles/${mediaId}`,
     environment: 'test',
     before: setEnvironment,
     embedObject: {
@@ -36,7 +36,7 @@ const testCases = [
   },
   {
     description: `should build a CANONICAL url for articles in test environment with test override`,
-    expected: `https://test.bbc.com/ws/av-embeds/articles/${mediaId}`,
+    expected: `https://www.test.bbc.com/ws/av-embeds/articles/${mediaId}`,
     environment: 'test',
     before: setEnvironment,
     embedObject: {
@@ -59,7 +59,7 @@ const testCases = [
   },
   {
     description: `should build a CANONICAL url for articles in test environment with live override`,
-    expected: `https://test.bbc.com/ws/av-embeds/articles/${mediaId}${embedUrlLiveOverride}`,
+    expected: `https://www.test.bbc.com/ws/av-embeds/articles/${mediaId}${embedUrlLiveOverride}`,
     environment: 'test',
     before: setEnvironment,
     embedObject: {
@@ -82,7 +82,7 @@ const testCases = [
   },
   {
     description: `should build a CANONICAL url for on-demand/live radio in test environment with live override`,
-    expected: `https://test.bbc.com/ws/av-embeds/media/${mediaId}${embedUrlLiveOverride}`,
+    expected: `https://www.test.bbc.com/ws/av-embeds/media/${mediaId}${embedUrlLiveOverride}`,
     environment: 'test',
     before: setEnvironment,
     embedObject: {
@@ -197,7 +197,7 @@ const testCases = [
   },
   {
     description: `should build a CANONICAL url for legacy media in test environment with no override`,
-    expected: `https://test.bbc.com/ws/av-embeds/legacy/${legacyId}`,
+    expected: `https://www.test.bbc.com/ws/av-embeds/legacy/${legacyId}`,
     environment: 'test',
     before: setEnvironment,
     embedObject: {
