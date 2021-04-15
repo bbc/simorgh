fdeb9e5c7ed7ccc01b130caafe2ba443fb4419fd
HarryVerhoef <harry.verhoef1999@gmail.com>
2021-04-15 20:43:56 +0100

    Some rearranging

--- src/app/containers/TeamJellyHack/StoryList/index.jsx
+++ src/app/containers/TeamJellyHack/StoryList/index.jsx
@@ -1,27 +1,38 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
+import StoryPlayer from '../StoryPlayer';
+import data from 'stories-content.json';
 
-const StoryItem = ({ className, showStory, id, name, src, hasViewed }) => {
+const StoryItem = ({ className, showStory, id, name, src }) => {
   return (
-    <li
-      id={id}
-      onClick={() => showStory(id)}
-      className={`${className} ${hasViewed && 'has-viewed'}`}
-    >
+    <li id={id} onClick={() => showStory(id)} className={className}>
       <img src={src} alt={name} />
       <h4>{name}</h4>
     </li>
   );
 };
 
-const StyledStoryItem = styled(StoryItem)``;
+const StyledStoryItem = styled(StoryItem)`
+  ${({ hasViewed }) => /* hasViewed styles */}
+`;
 
-const StoryList = ({ data, showStory }) => {
+const StoryList = () => {
+  const [isStoryOverlayVisible, setStoryOverlayVisible] = useState(false);
+  const [activeStoryId, setActiveStoryId] = useState(null);
+  const [storiesState, setStoriesState] = useState(data);
+
+
+  const showStory = (id) => {
+    setStoryOverlayVisible(true);
+    setActiveStoryId(id);
+
+  }
+
   const topics = data.map(({ id, name, src, hasViewed }) => {
     return (
-      <StoryItem
+      <StyledStoryItem
         key={id}
         id={id}
         name={name}
@@ -32,7 +43,12 @@ const StoryList = ({ data, showStory }) => {
     );
   });
 
-  return <ol>{topics}</ol>;
+  return (
+    <>
+      {isStoryOverlayVisible && <StoryPlayer stories={storiesState.activeStoryId}/>}
+      <ol>{topics}</ol>
+    </>
+  );
 };
 
 export default StoryList;
