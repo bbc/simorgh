bc5429737f5cfc7ddf58c8fd734662cdee3cc204
Caesar Mukama <mcaesar88@gmail.com>
2021-04-16 11:44:04 +0300

    Styling

--- src/app/components/TeamJellyHack/StoryNav/index.jsx
+++ src/app/components/TeamJellyHack/StoryNav/index.jsx
@@ -1,26 +1,72 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { number, func } from 'prop-types';
-import { GEL_SPACING } from '@bbc/gel-foundations/spacings';
+import {
+  GEL_SPACING,
+  GEL_SPACING_DBL,
+  GEL_SPACING_TRPL,
+} from '@bbc/gel-foundations/spacings';
+
+import { C_RHINO, C_WHITE } from '@bbc/psammead-styles/colours';
+
+const StyledNav = styled.ol`
+  display: grid;
+  grid-template-columns: repeat(${({ columns }) => columns}, 1fr);
+  gap: ${GEL_SPACING};
+  list-style: none;
+  padding: 0;
+  margin: ${GEL_SPACING_TRPL} ${GEL_SPACING_DBL};
+`;
+
+const StyledButtonWrapper = styled.li`
+  display: block;
+`;
 
 const StyledButton = styled.button`
+  display: block;
+  width: 100%;
+  height: 2px;
+  border: none;
+  margin: 0;
+  background: none;
   padding: ${GEL_SPACING};
+  outline: none;
+  cursor: pointer;
+`;
+
+const StyledProgressBar = styled.div`
+  display: block;
+  height: 3px;
+  background: ${C_RHINO};
+`;
+
+const StyledProgressBarIndicator = styled.div`
+  width: ${({ isCurrentPage }) => (isCurrentPage ? '100%' : '0%')};
+  height: 3px;
+  background: ${C_WHITE};
 `;
 
 const StoryNav = ({ numStories, currentStoryNum, setCurrentStoryNum }) => {
-  const navButtons = new Array(numStories).fill(null).map((_, index) => (
-    <li>
-      <StyledButton
-        type="button"
-        isCurrentPage={index === currentStoryNum}
-        index={index}
-        setCurrentStoryNum={setCurrentStoryNum}
-        onClick={() => setCurrentStoryNum(index)}
-      />
-    </li>
-  ));
+  const navButtons = new Array(numStories).fill(null).map((_, index) => {
+    const isCurrentPage = index === currentStoryNum;
+    return (
+      <StyledButtonWrapper>
+        <StyledButton
+          type="button"
+          isCurrentPage={isCurrentPage}
+          index={index}
+          setCurrentStoryNum={setCurrentStoryNum}
+          onClick={() => setCurrentStoryNum(index)}
+        >
+          <StyledProgressBar>
+            <StyledProgressBarIndicator isCurrentPage={isCurrentPage} />
+          </StyledProgressBar>
+        </StyledButton>
+      </StyledButtonWrapper>
+    );
+  });
 
-  return <ol>{navButtons}</ol>;
+  return <StyledNav columns={numStories}>{navButtons}</StyledNav>;
 };
 
 StoryNav.propTypes = {
