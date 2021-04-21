7b525a017ae6c38352922271f0ea718cbfbebb6e
Simon Sinclair <simon@simonsinclair.com>
2021-04-21 21:59:29 +0100

    Flesh out sourceHelpers implementation

--- src/app/containers/SocialEmbed/sourceHelper.js
+++
@@ -1,9 +0,0 @@
-export const getProviderFromSource = source => {
-  console.log('SOCIAL_EMBED_SOURCE', source);
-  return 'twitter';
-};
-
-export const getIdFromSource = source => {
-  console.log('SOCIAL_EMBED_SOURCE', source);
-  return '1384138850478346243';
-};
