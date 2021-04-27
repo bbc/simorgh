faadf4ec369b774c034688da358ef70dd6c5c526
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-04-27 12:10:41 +0100

    test buildATIEventTrackUrl is called and returns the correct url

--- src/app/hooks/useViewTracker/index.jsx
+++ src/app/hooks/useViewTracker/index.jsx
@@ -1,33 +1,68 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { useInView } from 'react-intersection-observer';
 // Polyfill IntersectionObsesrver, e.g. for IE11
 import 'intersection-observer';
 
+import { ServiceContext } from '#contexts/ServiceContext';
+import { RequestContext } from '#contexts/RequestContext';
+import { buildATIClickParams } from '#containers/ATIAnalytics/params';
+import { getComponentInfo } from '#app/lib/analyticsUtils/index';
 import { sendEventBeacon } from '#containers/ATIAnalytics/beacon';
 
 const VIEWED_DURATION_MS = 1000;
 
-const useImpression = data => {
-  const timeoutRef = useRef();
+// const {trackRef} = useImpression(data)
+
+const useImpression = ({ pageData, componentName }) => {
+  const requestContext = useContext(RequestContext);
+  const serviceContext = useContext(ServiceContext);
   const [viewSent, setViewSent] = useState(false);
-  const { ref, inView } = useInView({
+  const [ref, inView] = useInView({
     threshold: 0.5,
+  });
+  const eventTrackingProps = buildATIClickParams(
+    pageData,
+    requestContext,
+    serviceContext,
+  );
+  const componentInfo = getComponentInfo({
+    result: 'https://www.bbc.com/mundo/something',
+    componentName: 'mostRead',
+    componentData: {
+      actionLabel: 'most-read-navigate',
+      child: 'link',
+    },
   });
 
   useEffect(() => {
+    let timeout;
+
     if (inView) {
-      timeoutRef.current = setTimeout(() => {
+      timeout = setTimeout(() => {
         if (!viewSent) {
           setViewSent(true);
-          sendEventBeacon(data);
+          sendEventBeacon({
+            type: 'view',
+            componentName,
+            service: serviceContext.service,
+            componentInfo,
+            ...eventTrackingProps,
+          });
         }
       }, VIEWED_DURATION_MS);
     } else {
-      clearTimeout(timeoutRef.current);
+      clearTimeout(timeout);
     }
 
-    return () => clearTimeout(timeoutRef.current);
-  }, [inView, viewSent, data]);
+    return () => clearTimeout(timeout);
+  }, [
+    inView,
+    viewSent,
+    componentName,
+    componentInfo,
+    eventTrackingProps,
+    serviceContext.service,
+  ]);
 
   return { trackRef: ref };
 };
--- src/app/hooks/useViewTracker/index.test.jsx
+++ src/app/hooks/useViewTracker/index.test.jsx
@@ -1,54 +1,141 @@
+import React from 'react';
 import { renderHook, act } from '@testing-library/react-hooks';
 import { useInView } from 'react-intersection-observer';
 
-import { sendEventBeacon } from '#containers/ATIAnalytics/beacon';
+import { ServiceContextProvider } from '#contexts/ServiceContext';
+import { RequestContextProvider } from '#contexts/RequestContext';
+import * as atiUrl from '#containers/ATIAnalytics/atiUrl';
 import useViewTracker from '.';
 
-jest.mock('react-intersection-observer', () => ({
-  useInView: jest.fn(),
-}));
+import { CORRESPONDENT_STORY_PAGE } from '#app/routes/utils/pageTypes';
+import pageData from './pageData.json';
 
-jest.mock('#containers/ATIAnalytics/beacon', () => ({
-  sendEventBeacon: jest.fn(),
-}));
-
-beforeEach(jest.resetAllMocks);
-
-const REQUIRED_TIME_IN_VIEW = 1000;
+jest.mock('react-intersection-observer');
+// jest.mock('#containers/ATIAnalytics/atiUrl');
+// jest.useFakeTimers();
 const wait = duration => new Promise(resolve => setTimeout(resolve, duration));
 
-it('should return a ref used for tracking if an element is in view', async () => {
-  useInView.mockImplementation(() => ({
-    inView: false,
-    ref: 'mock-ref',
-  }));
-
-  const data = { blah: 'foo' };
-  const { result } = renderHook(() => useViewTracker(data));
-
-  expect(result.current.trackRef).toEqual('mock-ref');
+beforeEach(() => {
+  jest.resetAllMocks();
+  jest.clearAllTimers();
 });
 
-it(`should call sendEventBeacon when element is 50% or more in view for more than ${
+const REQUIRED_TIME_IN_VIEW = 1000;
+const elementRefFn = jest.fn();
+const setIntersectionObserved = () =>
+  useInView.mockReturnValue([elementRefFn, true]);
+const setIntersectionNotObserved = () =>
+  useInView.mockReturnValue([elementRefFn, false]);
+
+it('should return a ref used for tracking', async () => {
+  setIntersectionNotObserved();
+
+  const data = { componentName: 'mostRead', pageData };
+  const { result } = renderHook(() => useViewTracker(data));
+
+  expect(result.current.trackRef).toBe(elementRefFn);
+});
+
+it.only(`should call buildATIEventTrackUrl when element is 50% or more in view for more than ${
   REQUIRED_TIME_IN_VIEW / 1000
 } seconds`, async () => {
-  useInView.mockImplementation(() => ({
-    inView: false,
-    ref: 'mock-ref',
-  }));
+  setIntersectionNotObserved();
+  const spy = jest.spyOn(atiUrl, 'buildATIEventTrackUrl');
+  const data = { componentName: 'mostRead', pageData };
+  const wrapper = ({ children }) => (
+    <RequestContextProvider
+      bbcOrigin="https://www.test.bbc.com"
+      isAmp
+      pageType={CORRESPONDENT_STORY_PAGE}
+      service="afrique"
+      pathname="/blah"
+    >
+      <ServiceContextProvider service="afrique">
+        {children}
+      </ServiceContextProvider>
+    </RequestContextProvider>
+  );
+  const { rerender } = renderHook(() => useViewTracker(data), { wrapper });
 
-  const data = { blah: 'foo' };
-  const { rerender } = renderHook(() => useViewTracker(data));
+  setIntersectionObserved();
+  rerender();
 
-  await act(async () => {
-    useInView.mockImplementation(() => ({
-      inView: true,
-      ref: 'mock-ref',
-    }));
-    rerender();
-    await wait(REQUIRED_TIME_IN_VIEW + 100);
-  });
+  await act(() => wait(REQUIRED_TIME_IN_VIEW + 100));
 
   expect(useInView).toHaveBeenCalledWith({ threshold: 0.5 });
-  expect(sendEventBeacon).toBeCalledWith({ blah: 'foo' });
+  expect(spy).toHaveBeenCalledTimes(2);
+
+  const [event, view] = spy.mock.results;
+
+  expect(event.value).toMatchInlineSnapshot(
+    `"undefineds=598343&p=news%3A%3Apidgin.news.story.51745682.page&r=\${screenWidth}x\${screenHeight}x\${screenColorDepth}&re=\${availableScreenWidth}x\${availableScreenHeight}&hl=\${timestamp}&lng=\${browserLanguage}&ati=PUB-%5Bafrique-mostRead%5D-%5BmostRead-most-read-navigate~view%5D-%5B%5D-%5BPAR%3Dcontainer-mostRead~CHD%3Dlink%5D-%5Bnews%3A%3Apidgin.news.story.51745682.page%5D-%5B%5D-%5B%5D-%5Bhttps%3A%2F%2Fwww.bbc.com%2Fmundo%2Fsomething%5D&type=AT"`,
+  );
+  expect(view.value).toMatchInlineSnapshot(
+    `"undefineds=598343&p=news%3A%3Apidgin.news.story.51745682.page&r=\${screenWidth}x\${screenHeight}x\${screenColorDepth}&re=\${availableScreenWidth}x\${availableScreenHeight}&hl=\${timestamp}&lng=\${browserLanguage}&ati=PUB-%5Bafrique-mostRead%5D-%5BmostRead-most-read-navigate~view%5D-%5B%5D-%5BPAR%3Dcontainer-mostRead~CHD%3Dlink%5D-%5Bnews%3A%3Apidgin.news.story.51745682.page%5D-%5B%5D-%5B%5D-%5Bhttps%3A%2F%2Fwww.bbc.com%2Fmundo%2Fsomething%5D&type=AT"`,
+  );
+});
+
+it(`should not call buildATIEventTrackUrl when element is 50% or more in view for less than ${
+  REQUIRED_TIME_IN_VIEW / 1000
+} seconds`, async () => {
+  setIntersectionNotObserved();
+  const spy = jest.spyOn(atiUrl, 'buildATIEventTrackUrl');
+  const data = { componentName: 'mostRead', pageData };
+  const wrapper = ({ children }) => (
+    <RequestContextProvider
+      bbcOrigin="https://www.test.bbc.com"
+      isAmp
+      pageType={CORRESPONDENT_STORY_PAGE}
+      service="afrique"
+      pathname="/blah"
+    >
+      <ServiceContextProvider service="afrique">
+        {children}
+      </ServiceContextProvider>
+    </RequestContextProvider>
+  );
+  const { rerender } = renderHook(() => useViewTracker(data), { wrapper });
+
+  setIntersectionObserved();
+  rerender();
+
+  await act(() => wait(REQUIRED_TIME_IN_VIEW - 100));
+
+  expect(useInView).toHaveBeenCalledWith({ threshold: 0.5 });
+  expect(spy).not.toHaveBeenCalledWith();
+});
+
+it('should not call buildATIEventTrackUrl more than once', async () => {
+  setIntersectionNotObserved();
+  const spy = jest.spyOn(atiUrl, 'buildATIEventTrackUrl');
+  const data = { componentName: 'mostRead', pageData };
+  const wrapper = ({ children }) => (
+    <RequestContextProvider
+      bbcOrigin="https://www.test.bbc.com"
+      isAmp
+      pageType={CORRESPONDENT_STORY_PAGE}
+      service="afrique"
+      pathname="/blah"
+    >
+      <ServiceContextProvider service="afrique">
+        {children}
+      </ServiceContextProvider>
+    </RequestContextProvider>
+  );
+  const { rerender } = renderHook(() => useViewTracker(data), { wrapper });
+
+  setIntersectionObserved();
+  rerender();
+
+  await act(() => wait(REQUIRED_TIME_IN_VIEW + 100));
+
+  setIntersectionNotObserved();
+  rerender();
+  setIntersectionObserved();
+  rerender();
+  await act(() => wait(REQUIRED_TIME_IN_VIEW + 100));
+
+  expect(useInView).toHaveBeenCalledWith({ threshold: 0.5 });
+  expect(spy.mock.results[0].value).toHaveBeenCalledWith({ blah: 'foo' });
+  expect(spy).toHaveBeenCalledTimes(1);
 });
