830ee604dc59ba7c0b54ca994e4ba19dd4535d7f
HarryVerhoef <harry.verhoef1999@gmail.com>
2021-04-29 13:48:18 +0100

    Added buildATIClickParams call assertion

--- src/app/hooks/useClickTracker/index.test.jsx
+++ src/app/hooks/useClickTracker/index.test.jsx
@@ -1,12 +1,26 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useContext } from 'react';
 import { renderHook } from '@testing-library/react-hooks';
 import { render } from '@testing-library/react';
 import useClickTracker from '.';
 import pidginData from './fixtureData/tori-51745682.json';
-import { RequestContextProvider } from '#contexts/RequestContext';
-import { ServiceContextProvider } from '#contexts/ServiceContext';
+import {
+  RequestContext,
+  RequestContextProvider,
+} from '#contexts/RequestContext';
+import {
+  ServiceContext,
+  ServiceContextProvider,
+} from '#contexts/ServiceContext';
 import { STORY_PAGE } from '#app/routes/utils/pageTypes';
+import * as params from '#containers/ATIAnalytics/params';
+
+params.buildATIClickParams = jest.fn();
+
+jest.useFakeTimers();
+delete window.location;
+window.location = { href: 'http://bbc.com/pidgin/tori-51745682' };
+process.env.SIMORGH_ATI_BASE_URL = 'https://logws1363.ati-host.net?';
 
 const defaultProps = {
   pageData: pidginData,
@@ -37,6 +51,26 @@ const TestComponent = React.forwardRef((_, ref) => {
 const wrapper = ({ children }) => <WithContexts>{children}</WithContexts>;
 
 describe('Expected use', () => {
+  it('should call buildATIClickParams with page data, requestContext, serviceContext', () => {
+    renderHook(() => useClickTracker(defaultProps), {
+      wrapper,
+    });
+
+    const requestContext = renderHook(() => useContext(RequestContext), {
+      wrapper,
+    });
+
+    const serviceContext = renderHook(() => useContext(ServiceContext), {
+      wrapper,
+    });
+
+    expect(params.buildATIClickParams).toHaveBeenCalledWith(
+      defaultProps.pageData,
+      requestContext.result.current,
+      serviceContext.result.current,
+    );
+  });
+
   it('should return a ref', () => {
     const { result } = renderHook(() => useClickTracker(defaultProps), {
       wrapper,
