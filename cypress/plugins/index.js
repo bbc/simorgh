85ac58b6036a68b577825b226111bba10673e795
LilyL0u <lily.higham@bbc.co.uk>
2021-05-04 17:39:18 +0100

    plugin allow sconsole log to terminal on error

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,6 +12,23 @@ module.exports = (on, config) => {
   console.log('APP_ENV:', config.env.APP_ENV);
   console.log('UK:', config.env.UK);
   console.log('\n\n\n\n\n');
+  // eslint-disable-next-line global-require
+  require('cypress-log-to-output').install(on, (type, event) => {
+    // return true or false from this plugin to control if the event is logged
+    // `type` is either `console` or `browser`
+    // if `type` is `browser`, `event` is an object of the type `LogEntry`:
+    //  https://chromedevtools.github.io/devtools-protocol/tot/Log#type-LogEntry
+    // if `type` is `console`, `event` is an object of the type passed to `Runtime.consoleAPICalled`:
+    //  https://chromedevtools.github.io/devtools-protocol/tot/Runtime#event-consoleAPICalled
+
+    // for example, to only show error events:
+
+    if (event.level === 'error' || event.type === 'error') {
+      return true;
+    }
+
+    return false;
+  });
   /* eslint-enable no-console */
 
   return config;
