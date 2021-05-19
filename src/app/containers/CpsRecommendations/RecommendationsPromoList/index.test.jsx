98a9af4673536fdbc6ce2b379788ee64f5e11b47
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-05-19 09:36:47 +0100

    fix CpsRecommendations unit tests

--- src/app/containers/CpsRecommendations/RecommendationsPromoList/index.test.jsx
+++ src/app/containers/CpsRecommendations/RecommendationsPromoList/index.test.jsx
@@ -4,6 +4,7 @@ import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import pidginPageData from '#data/pidgin/cpsAssets/tori-49450859';
 import RecommendationsPromoList from './index';
 import { ServiceContextProvider } from '#contexts/ServiceContext';
+import { ToggleContextProvider } from '#contexts/ToggleContext';
 
 const promoItems = path(
   ['relatedContent', 'groups', 0, 'promos'],
@@ -14,7 +15,13 @@ describe('RecommendationsPromoList', () => {
   shouldMatchSnapshot(
     'it renders a list of Story Promos wrapped in Grid components',
     <ServiceContextProvider service="pidgin">
-      <RecommendationsPromoList promoItems={promoItems} dir="ltr" />,
+      <ToggleContextProvider
+        toggles={{
+          eventTracking: { enabled: true },
+        }}
+      >
+        <RecommendationsPromoList promoItems={promoItems} dir="ltr" />,
+      </ToggleContextProvider>
     </ServiceContextProvider>,
   );
 });
