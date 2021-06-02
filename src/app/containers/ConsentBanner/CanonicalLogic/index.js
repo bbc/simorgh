97d9feda6118d5d03206cd35c9677c731a75b4df
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-06-02 12:28:49 +0100

    revert consent banner changes

--- src/app/containers/ConsentBanner/CanonicalLogic/index.js
+++ src/app/containers/ConsentBanner/CanonicalLogic/index.js
@@ -13,7 +13,6 @@ const PRIVACY_COOKIE_CURRENT = 'july2019';
 const PRIVACY_COOKIE_PREVIOUS_VALUES = ['0', '1'];
 
 const onClient = typeof window !== 'undefined';
-const isStorybook = () => process.env.STORYBOOK;
 
 // We opted for the sameSite=None attribute below to maintain consistency with Orbit/cross-TLD browsing
 // Setting sameSite=None allows the cookie to be accessed and updated on `.co.uk` and `.com`
@@ -35,10 +34,6 @@ const showCookieBanner = () =>
 const policyCookieSet = () => !!Cookie.get(POLICY_COOKIE);
 
 const setSeenPrivacyBanner = () => {
-  if (isStorybook()) {
-    return;
-  }
-
   setCookie({
     name: PRIVACY_COOKIE,
     value: PRIVACY_COOKIE_CURRENT,
