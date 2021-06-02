8711ed271da288387054b5d33b260f73a62d2163
Ryan McCombe <ryan.mccombe@bbc.co.uk>
2021-06-02 14:51:33 +0100

    Client side error printer

--- src/client.js
+++ src/client.js
@@ -12,13 +12,39 @@ const logger = loggerNode();
 const data = window.SIMORGH_DATA || {};
 const root = document.getElementById('root');
 
+class Catcher extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidCatch(e) {
+    this.setState({ e });
+  }
+
+  /* eslint-disable react/destructuring-assignment */
+  renderError() {
+    return this.state.e.toString() + this.state.e.stack;
+  }
+
+  /* eslint-disable react/prop-types */
+  render() {
+    return this.state.e ? this.renderError() : this.props.children;
+  }
+}
+
 // Only hydrate the client if we're on the expected path
 // When on an unknown route, the SSR would be discarded and the user would only
 // see a blank screen. Avoid this by only hydrating when the embedded page data
 // and window location agree what the path is. Otherwise, fallback to the SSR.
 if (window.SIMORGH_DATA.path === window.location.pathname) {
   loadableReady(() => {
-    hydrate(<ClientApp data={data} routes={routes} />, root);
+    hydrate(
+      <Catcher>
+        <ClientApp data={data} routes={routes} />
+      </Catcher>,
+      root,
+    );
   });
 } else {
   logger.warn(`
