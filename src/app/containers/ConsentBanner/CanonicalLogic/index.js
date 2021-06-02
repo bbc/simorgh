95fac27c57b3adfdb132e402d5d025f6252470c5
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-06-02 11:48:53 +0100

    use STORYBOOK_ prefix in env var

--- src/app/containers/ConsentBanner/CanonicalLogic/index.js
+++ src/app/containers/ConsentBanner/CanonicalLogic/index.js
@@ -13,7 +13,7 @@ const PRIVACY_COOKIE_CURRENT = 'july2019';
 const PRIVACY_COOKIE_PREVIOUS_VALUES = ['0', '1'];
 
 const onClient = typeof window !== 'undefined';
-const isCI = process.env.CI;
+const isChromaticCI = process.env.STORYBOOK_CHROMATIC_CI;
 
 // We opted for the sameSite=None attribute below to maintain consistency with Orbit/cross-TLD browsing
 // Setting sameSite=None allows the cookie to be accessed and updated on `.co.uk` and `.com`
@@ -35,13 +35,15 @@ const showCookieBanner = () =>
 const policyCookieSet = () => !!Cookie.get(POLICY_COOKIE);
 
 const setSeenPrivacyBanner = () => {
-  if (!isCI) {
-    setCookie({
-      name: PRIVACY_COOKIE,
-      value: PRIVACY_COOKIE_CURRENT,
-      sameSite: SAME_SITE_VALUE,
-    });
+  if (!isChromaticCI) {
+    return;
   }
+
+  setCookie({
+    name: PRIVACY_COOKIE,
+    value: PRIVACY_COOKIE_CURRENT,
+    sameSite: SAME_SITE_VALUE,
+  });
 };
 const setDefaultPolicy = logger => setPolicyCookie(POLICY_DENIED, logger);
 const setApprovedPolicy = logger => setPolicyCookie(POLICY_APPROVED, logger);
