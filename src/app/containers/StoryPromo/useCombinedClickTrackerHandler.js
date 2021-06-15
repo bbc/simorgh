b03322e7cab45a4afdbd5812ca92ca351ff98aff
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-06-15 09:15:31 +0100

    rename vars in hook

--- src/app/containers/StoryPromo/useCombinedClickTrackerHandler.js
+++ src/app/containers/StoryPromo/useCombinedClickTrackerHandler.js
@@ -3,25 +3,29 @@ import path from 'ramda/src/path';
 import useClickTrackerHandler from '#hooks/useClickTrackerHandler';
 
 const useCombinedClickTrackerHandler = eventTrackingData => {
-  const blockLevelEventTrackingData = path(['block'], eventTrackingData);
-  const linkLevelEventTrackingData = path(['link'], eventTrackingData);
+  const blockData = path(['block'], eventTrackingData);
+  const linkData = path(['link'], eventTrackingData);
   const handleBlockLevelClick = useClickTrackerHandler({
-    ...blockLevelEventTrackingData,
-    ...(blockLevelEventTrackingData && { preventNavigation: true }),
+    ...(blockData && {
+      ...blockData,
+      preventNavigation: true,
+    }),
   });
   const handleLinkLevelClick = useClickTrackerHandler({
-    ...linkLevelEventTrackingData,
-    ...(linkLevelEventTrackingData && { preventNavigation: true }),
+    ...(linkData && {
+      ...linkData,
+      preventNavigation: true,
+    }),
   });
 
   return async event => {
     const nextPageUrl =
       path(['href'], eventTrackingData) || path(['target', 'href'], event);
 
-    if (blockLevelEventTrackingData) {
+    if (blockData) {
       await handleBlockLevelClick(event);
     }
-    if (linkLevelEventTrackingData) {
+    if (linkData) {
       await handleLinkLevelClick(event);
     }
     if (nextPageUrl) {
