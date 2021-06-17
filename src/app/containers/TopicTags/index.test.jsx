a4d3da8c5d62004cef698f0bfabae932bfe71c0a
HarryVerhoef <harry.verhoef@bbc.co.uk>
2021-06-17 16:38:29 +0100

    Add snapshot tests

--- src/app/containers/TopicTags/index.test.jsx
+++ src/app/containers/TopicTags/index.test.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
+import { ServiceContextProvider } from '#contexts/ServiceContext';
+import { ToggleContextProvider } from '#contexts/ToggleContext';
 import { EventTrackingContextProvider } from '#contexts/EventTrackingContext';
-import { TopicTags, TopicTag } from '#containers/TopicTags';
+import Topics from '#containers/TopicTags';
 
 const withContexts = ({ children }) => (
-  <EventTrackingContextProvider>{children}</EventTrackingContextProvider>
+  <ServiceContextProvider service="pidgin">
+    <ToggleContextProvider
+      toggles={{
+        eventTracking: {
+          enabled: true,
+        },
+      }}
+    >
+      <EventTrackingContextProvider>{children}</EventTrackingContextProvider>
+    </ToggleContextProvider>
+  </ServiceContextProvider>
 );
 
 describe('TopicTags', () => {
@@ -13,28 +25,24 @@ describe('TopicTags', () => {
 
   shouldMatchSnapshot(
     'should render correctly with no tags',
-    withContexts(<TopicTags />),
+    withContexts(<Topics />),
   );
 
   shouldMatchSnapshot(
     'should render correctly with a single tag',
-    withContexts(
-      <TopicTags>
-        <TopicTag />
-      </TopicTags>,
-    ),
+    withContexts(<Topics topics={[{ topicName: 'topic', topicId: '123' }]} />),
   );
 
   shouldMatchSnapshot(
     'should render correctly with multiple tags',
     withContexts(
-      <TopicTags>
-        <TopicTag />
-        <TopicTag />
-        <TopicTag />
-      </TopicTags>,
+      <Topics
+        topics={[
+          { topicName: 'topic1', topicId: '1' },
+          { topicName: 'topic2', topicId: '2' },
+          { topicName: 'topic3', topicId: '3' },
+        ]}
+      />,
     ),
   );
-
-
 });
