0e3959d08c3d03058e80c3c729bf1f2c60a7ccdc
HarryVerhoef <harry.verhoef@bbc.co.uk>
2021-07-02 09:56:14 +0100

    Add article page story with related topics

--- src/app/pages/ArticlePage/index.stories.jsx
+++ src/app/pages/ArticlePage/index.stories.jsx
@@ -12,6 +12,8 @@ import { ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
 // article c5jje4ejkqvo contains a Headline, a Paragraph, a timestamp
 // a Portrait Image with Caption, a Landscape Image with Caption and Square Image with Caption.
 import articleData from '#data/news/articles/c5jje4ejkqvo';
+import articleDataWithRelatedTopics from '#data/kyrgyz/articles/c41p9ll9n0po';
+import { EventTrackingContextProvider } from '#app/contexts/EventTrackingContext';
 
 // Not all services have fixtures for article data yet
 // the service selector will be constrained to services that have article fixtures:
@@ -35,6 +37,41 @@ storiesOf('Pages/Article Page', module)
               />
             </MemoryRouter>
           </UserContextProvider>
+        </RequestContextProvider>
+      </ServiceContextProvider>
+    </ToggleContextProvider>
+  ));
+
+storiesOf('Pages/Article Page', module)
+  .addDecorator(withKnobs)
+  .add('With related topics', () => (
+    <ToggleContextProvider
+      toggles={{
+        eventTracking: {
+          enabled: true,
+        },
+        topicsTags: {
+          enabled: true,
+        },
+      }}
+    >
+      {/* Service set to pidgin to enable most read. Article data is in english */}
+      <ServiceContextProvider service="kyrgyz">
+        <RequestContextProvider
+          isAmp={false}
+          pageType={ARTICLE_PAGE}
+          service="kyrgyz"
+        >
+          <EventTrackingContextProvider>
+            <UserContextProvider>
+              <MemoryRouter>
+                <ArticlePage
+                  pageData={articleDataWithRelatedTopics}
+                  mostReadEndpointOverride="./data/news/mostRead/index.json"
+                />
+              </MemoryRouter>
+            </UserContextProvider>
+          </EventTrackingContextProvider>
         </RequestContextProvider>
       </ServiceContextProvider>
     </ToggleContextProvider>
