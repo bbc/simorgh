b41f4d5a196235e2ddc0e0a3aa175d122a7035c8
Ryan McCombe <ryanmccombe@gmail.com>
2021-07-12 15:03:04 +0100

    Storybook Rewrite - Containers

--- src/app/containers/Brand/index.stories.jsx
+++ src/app/containers/Brand/index.stories.jsx
@@ -5,23 +5,27 @@ import { withKnobs } from '@storybook/addon-knobs';
 import { ServiceContextProvider } from '#contexts/ServiceContext';
 import BrandContainer from '.';
 
-storiesOf('Containers/Brand', module)
-  .addParameters({ chromatic: { disable: true } })
-  .addDecorator(withKnobs)
-  .addDecorator(withServicesKnob())
-  .add('default', ({ service, variant }) => {
-    // eslint-disable-next-line import/no-dynamic-require,global-require
-    const serviceConfig = require(`../../lib/config/services/${service}`)
-      .service;
+// eslint-disable-next-line react/prop-types
+const Component = ({ service, variant }) => {
+  // eslint-disable-next-line import/no-dynamic-require,global-require
+  const serviceConfig = require(`../../lib/config/services/${service}`).service;
 
-    const configVariant = serviceConfig[variant];
+  const configVariant = serviceConfig[variant];
+  return (
+    <ServiceContextProvider service={service}>
+      <BrandContainer
+        backgroundColour={configVariant.theming.brandBackgroundColour}
+        logoColour={configVariant.theming.brandLogoColour}
+      />
+    </ServiceContextProvider>
+  );
+};
 
-    return (
-      <ServiceContextProvider service={service}>
-        <BrandContainer
-          backgroundColour={configVariant.theming.brandBackgroundColour}
-          logoColour={configVariant.theming.brandLogoColour}
-        />
-      </ServiceContextProvider>
-    );
-  });
+export default {
+  title: 'Containers/Brand',
+  Component,
+  decorators: [withKnobs, withServicesKnob()],
+  parameters: { chromatic: { disable: true } },
+};
+
+export const Brand = props => <Component {...props} />;
