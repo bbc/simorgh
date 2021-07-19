39ed476548fde55c24769c826e55102769b9c10d
LilyL0u <lily.higham@bbc.co.uk>
2021-07-19 08:42:43 +0100

    test for topic tags in testsForAllPages

--- cypress/integration/pages/testsForAllPages.js
+++ cypress/integration/pages/testsForAllPages.js
@@ -1,7 +1,35 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
 // For testing important features that differ between services, e.g. Timestamps.
 // We recommend using inline conditional logic to limit tests to services which differ.
+import getDataUrl from '../../support/helpers/getDataUrl';
+
 export const testsThatAlwaysRunForAllPages = ({ service, pageType }) => {
-  describe(`No testsToAlwaysRunForAllPages to run for ${service} ${pageType}`, () => {});
+  describe(`No testsToAlwaysRunForAllPages to run for ${service} ${pageType}`, () => {
+    it('should render topic tags if they are in the json, and they should lead to topic page', () => {
+      cy.request(getDataUrl(Cypress.env('currentPath'))).then(({ body }) => {
+        const topicTagsPresent = body.metadata.topics;
+        let topicTagsLength = 0;
+        if (topicTagsPresent) {
+          topicTagsLength = topicTagsPresent.length;
+        }
+        cy.log(topicTagsPresent);
+        cy.log(topicTagsLength);
+        if (topicTagsPresent && topicTagsLength > 1) {
+          cy.get(`aside[aria-labelledby*='related-topics'] > ul > li > a`)
+            .first()
+            .click();
+          cy.go('back');
+          cy.wait(1000);
+        } else if (topicTagsPresent && topicTagsLength === 1) {
+          cy.get(`aside[aria-labelledby*='related-topics']`).find('a').click();
+          cy.go('back');
+          cy.wait(1000);
+        } else {
+          cy.log('No topic tags in json');
+        }
+      });
+    });
+  });
 };
 
 // For testing features that may differ across services but share a common logic e.g. translated strings.
