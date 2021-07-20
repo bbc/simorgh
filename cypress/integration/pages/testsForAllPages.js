15d1ac4ea4c859dda31b974ee54fa521a60dbf55
LilyL0u <lily.higham@bbc.co.uk>
2021-07-20 11:27:18 +0100

    do not run test on sport

--- cypress/integration/pages/testsForAllPages.js
+++ cypress/integration/pages/testsForAllPages.js
@@ -6,64 +6,68 @@ import getDataUrl from '../../support/helpers/getDataUrl';
 export const testsThatAlwaysRunForAllPages = ({ service, pageType }) => {
   describe(`testsToAlwaysRunForAllPages to run for ${service} ${pageType}`, () => {
     it('should render topic tags if they are in the json, and they should navigate to correct topic page', () => {
-      cy.url().then(url => {
-        const urlForData = url.includes('amp') ? url.slice(0, -4) : url;
+      if (service !== 'sport') {
+        cy.url().then(url => {
+          const urlForData = url.includes('amp') ? url.slice(0, -4) : url;
 
-        const firstVisitedPage = url;
-        cy.log(firstVisitedPage);
-        cy.request(getDataUrl(urlForData)).then(({ body }) => {
-          // Check if data has topic tags
-          const topicTagsPresent = body.metadata.topics;
-          let topicTagsLength = 0;
+          const firstVisitedPage = url;
+          cy.log(firstVisitedPage);
+          cy.request(getDataUrl(urlForData)).then(({ body }) => {
+            // Check if data has topic tags
+            const topicTagsPresent = body.metadata.topics;
+            let topicTagsLength = 0;
 
-          // Get number of topic tags expected
-          if (topicTagsPresent) {
-            topicTagsLength = topicTagsPresent.length;
-          }
+            // Get number of topic tags expected
+            if (topicTagsPresent) {
+              topicTagsLength = topicTagsPresent.length;
+            }
 
-          if (topicTagsPresent && topicTagsLength > 1) {
-            // Gets the Topic Tag name
-            cy.get(
-              `aside[aria-labelledby*='related-topics'] > ul > li:first > a`,
-            ).then($tag => {
-              const topicTitle = $tag.text();
-              cy.wrap(topicTitle).as('topicTitle');
-            });
-            // Clicks on the first topic tag
-            cy.get(`aside[aria-labelledby*='related-topics'] > ul > li > a`)
-              .first()
-              .click();
-
-            // Checks the page is of the Topic Tag clicked on by checking H1
-            cy.get('@topicTitle').then(title => {
-              cy.get('h1').should('contain', title);
-            });
-
-            // Needs to go back to the first page for the rest of the test suite
-            // cy.go('back') does not work on AMP as it returns to a canonical page
-            cy.visit(firstVisitedPage);
-          } else if (topicTagsPresent && topicTagsLength === 1) {
-            cy.get(`aside[aria-labelledby*='related-topics']`)
-              .find('a')
-              .then($tag => {
+            if (topicTagsPresent && topicTagsLength > 1) {
+              // Gets the Topic Tag name
+              cy.get(
+                `aside[aria-labelledby*='related-topics'] > ul > li:first > a`,
+              ).then($tag => {
                 const topicTitle = $tag.text();
                 cy.wrap(topicTitle).as('topicTitle');
               });
-            // If there is only one topic tag it is not in a list
-            cy.get(`aside[aria-labelledby*='related-topics']`)
-              .find('a')
-              .click();
-            // Checks the page is of the Topic Tag clicked on
-            cy.get('@topicTitle').then(title => {
-              cy.get('h1').should('contain', title);
-            });
+              // Clicks on the first topic tag
+              cy.get(`aside[aria-labelledby*='related-topics'] > ul > li > a`)
+                .first()
+                .click();
 
-            cy.visit(firstVisitedPage);
-          } else {
-            cy.log('No topic tags in json');
-          }
+              // Checks the page is of the Topic Tag clicked on by checking H1
+              cy.get('@topicTitle').then(title => {
+                cy.get('h1').should('contain', title);
+              });
+
+              // Needs to go back to the first page for the rest of the test suite
+              // cy.go('back') does not work on AMP as it returns to a canonical page
+              cy.visit(firstVisitedPage);
+            } else if (topicTagsPresent && topicTagsLength === 1) {
+              cy.get(`aside[aria-labelledby*='related-topics']`)
+                .find('a')
+                .then($tag => {
+                  const topicTitle = $tag.text();
+                  cy.wrap(topicTitle).as('topicTitle');
+                });
+              // If there is only one topic tag it is not in a list
+              cy.get(`aside[aria-labelledby*='related-topics']`)
+                .find('a')
+                .click();
+              // Checks the page is of the Topic Tag clicked on
+              cy.get('@topicTitle').then(title => {
+                cy.get('h1').should('contain', title);
+              });
+
+              cy.visit(firstVisitedPage);
+            } else {
+              cy.log('No topic tags in json');
+            }
+          });
         });
-      });
+      } else {
+        cy.log('Topic tags currently disabled on Sport');
+      }
     });
   });
 };
