4e5c7aea4ce368e276047e39875cfcc083faea51
Rebecca McGinn <59573468+rebeccamcginn@users.noreply.github.com>
2021-07-21 16:56:26 +0100

    Update src/app/components/RadioSchedule/StartTime/index.stories.jsx
    Co-authored-by: Ryan McCombe <ryanmccombe@gmail.com>

--- src/app/components/RadioSchedule/StartTime/index.stories.jsx
+++ src/app/components/RadioSchedule/StartTime/index.stories.jsx
@@ -7,19 +7,17 @@ import StartTime from '.';
 const storiesUnixTimestamp = 1566914061212;
 
 // eslint-disable-next-line react/prop-types
-const Component = ({ service, locale, script, dir, timezone }) => {
-  return (
-    <ServiceContextProvider
-      service={service}
-      locale={locale}
-      script={script}
-      dir={dir}
-      timezone={timezone}
-    >
-      <StartTime timestamp={storiesUnixTimestamp} />
-    </ServiceContextProvider>
-  );
-};
+const Component = ({ service, locale, script, dir, timezone }) => (
+  <ServiceContextProvider
+    service={service}
+    locale={locale}
+    script={script}
+    dir={dir}
+    timezone={timezone}
+  >
+    <StartTime timestamp={storiesUnixTimestamp} />
+  </ServiceContextProvider>
+);
 
 export default {
   title: 'Components/RadioSchedule/StartTime',
