15a045b3145c4e20e87ec20f8de002246a2ee0c9
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2021-07-29 11:34:26 +0100

    Add correct padding and font size

--- src/app/containers/Gist/index.jsx
+++ src/app/containers/Gist/index.jsx
@@ -4,11 +4,17 @@ import pathOr from 'ramda/src/pathOr';
 import styled from '@emotion/styled';
 import {
   GEL_SPACING_HLF,
+  GEL_SPACING,
   GEL_SPACING_DBL,
   GEL_SPACING_QUAD,
 } from '@bbc/gel-foundations/spacings';
 import { C_METAL, C_POSTBOX, C_WHITE } from '@bbc/psammead-styles/colours';
 import { getSansRegular, getSansBold } from '@bbc/psammead-styles/font-styles';
+import { getPica, getGreatPrimer } from '@bbc/gel-foundations/typography';
+import {
+  GEL_GROUP_2_SCREEN_WIDTH_MIN,
+  GEL_GROUP_4_SCREEN_WIDTH_MIN,
+} from '@bbc/gel-foundations/breakpoints';
 import { object } from 'prop-types';
 import { ServiceContext } from '#contexts/ServiceContext';
 import Blocks from '#containers/Blocks';
@@ -17,6 +23,7 @@ import Text from '#containers/CpsText';
 
 const GistWrapper = styled.div`
   ${({ service }) => getSansRegular(service)}
+  ${({ script }) => getPica(script)}
   color: ${C_METAL};
   border-top: ${GEL_SPACING_HLF} solid ${C_POSTBOX};
   background-color: ${C_WHITE};
@@ -24,12 +31,32 @@ const GistWrapper = styled.div`
   padding-bottom: ${GEL_SPACING_QUAD};
   ${({ dir }) =>
     dir === 'ltr'
-      ? `padding-left: ${GEL_SPACING_QUAD};`
-      : `padding-right: ${GEL_SPACING_QUAD};`}
+      ? `padding-left: ${GEL_SPACING_DBL};`
+      : `padding-right: ${GEL_SPACING_DBL};`}
+  ul {
+    padding-inline-start: ${GEL_SPACING};
+  }
+
+  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
+    ul {
+      padding-inline-start: 0;
+    }
+  }
+
+  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
+    ${({ dir }) =>
+      dir === 'ltr'
+        ? `padding-left: ${GEL_SPACING_QUAD};`
+        : `padding-right: ${GEL_SPACING_QUAD};`}
+    ul {
+      padding-inline-start: ${GEL_SPACING_DBL};
+    }
+  }
 `;
 
 const GistIntroduction = styled.strong`
   ${({ service }) => getSansBold(service)}
+  ${({ script }) => getGreatPrimer(script)}
   display: inline-block;
   padding-bottom: ${GEL_SPACING_DBL};
 `;
@@ -47,11 +74,13 @@ const componentsToRender = {
 };
 
 const Gist = ({ gistData }) => {
-  const { service, dir, translations } = useContext(ServiceContext);
+  const { service, script, dir, translations } = useContext(ServiceContext);
   const gistTitle = pathOr('At a glance', ['gist'], translations);
   return (
-    <GistWrapper service={service} dir={dir}>
-      <GistIntroduction service={service}>{gistTitle}</GistIntroduction>
+    <GistWrapper service={service} script={script} dir={dir}>
+      <GistIntroduction service={service} script={script}>
+        {gistTitle}
+      </GistIntroduction>
       <Blocks
         blocks={path(['model', 'blocks'], gistData)}
         componentsToRender={componentsToRender}
