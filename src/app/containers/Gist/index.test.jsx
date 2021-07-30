e49a979e4aa6255d0c0c3c2406709a3a175608f1
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2021-07-30 14:09:49 +0100

    Remove unneeded test

--- src/app/containers/Gist/index.test.jsx
+++ src/app/containers/Gist/index.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { ServiceContextProvider } from '#contexts/ServiceContext';
 import Gist from '.';
-import fixtureData, { fixtureDataOneItem } from './fixtures';
+import fixtureData from './fixtures';
 
 // eslint-disable-next-line react/prop-types
 const GistWithContext = ({ gistData = fixtureData }) => (
@@ -19,18 +19,9 @@ describe('Gist', () => {
     expect(gistCaption).toBeInTheDocument();
   });
 
-  it('should render an unordered list if there is more than one item', () => {
+  it('should render an unordered list', () => {
     const { container } = render(<GistWithContext />);
 
     expect(container.querySelectorAll('li').length).toEqual(2);
-  });
-
-  it.skip('should render the bullet point as a paragraph if there is only one item', () => {
-    const { queryByRole } = render(
-      <GistWithContext gistData={fixtureDataOneItem} />,
-    );
-
-    expect(queryByRole('list')).not.toBeInTheDocument();
-    expect(queryByRole('listitem')).not.toBeInTheDocument();
   });
 });
