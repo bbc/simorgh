4f02f73db3c1caed7239b2fa6607c3cc89b25cc4
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2021-08-10 17:18:23 +0100

    Add snapshot tests for text

--- src/integration/pages/articles/gistTests.js
+++ src/integration/pages/articles/gistTests.js
@@ -1,18 +1,28 @@
 export default () => {
-  describe('gist', () => {
-    it('should render the title', () => {
-      const gistTitle = document.getElementById('gist-title');
-      const gistTitleText = gistTitle.textContent;
+  describe('Gist', () => {
+    const gistTitle = document.getElementById('gist-title');
+    const gistTitleText = gistTitle.textContent;
+    const listItem = document.querySelector(
+      `div[aria-labelledby='gist-title'] ul[role="list"] > li`,
+    );
+    const listItemText = listItem.textContent;
 
+    it('title should be in the document', () => {
       expect(gistTitle).toBeInTheDocument();
       expect(gistTitleText).toBeTruthy();
     });
 
-    it('should render a list', () => {
-      const gistList = document.querySelector(
-        `div[aria-labelledby='gist-title'] ul[role="list"] > li`,
-      );
-      expect(gistList).toBeInTheDocument();
+    it('title should match text', () => {
+      expect(gistTitleText).toMatchSnapshot();
+    });
+
+    it('should contain a list item within an unordered list', () => {
+      expect(listItem).toBeInTheDocument();
+      expect(listItemText).toBeTruthy();
+    });
+
+    it('list item should match text', () => {
+      expect(listItemText).toMatchSnapshot();
     });
   });
 };
