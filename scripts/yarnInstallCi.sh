7eca52d8adc20560d2cf1ba70197a9aa7ee71aeb
Harvey Peachey <harvey.peachey@bbc.co.uk>
2021-08-13 16:10:44 +0100

    fixed production command, changed bash syntax to be more standard

--- scripts/yarnInstallCi.sh
+++ scripts/yarnInstallCi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ $1 = "--production"]; then
-  rm -rf node_modules | yarn workspaces focus --all --immutable --production
+if [ "$1" = "--production" ]; then
+  rm -rf node_modules | yarn workspaces focus --all --production
 else
   rm -rf node_modules | yarn install --immutable --check-cache
 fi
