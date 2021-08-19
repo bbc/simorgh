8b4acc701b174cb8f3d8304015fd3c3e7f9c459e
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2021-08-19 12:10:55 +0100

    Fix unclosed block error

--- src/app/containers/PodcastPromo/components/title.jsx
+++ src/app/containers/PodcastPromo/components/title.jsx
@@ -21,6 +21,7 @@ const Heading = styled.h2`
     position: relative;
     bottom: 0.3125rem;
     ${({ dir }) => (dir === 'ltr' ? `right: 0.1875rem;` : `left: 0.1875rem;`)}
+  }
 `;
 
 const Wrapper = styled.div`
