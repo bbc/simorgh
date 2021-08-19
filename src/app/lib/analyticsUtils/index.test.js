946075a52775d8ca19cb11bddd117a0b24f6ce1f
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-08-19 15:39:20 +0100

    better var name in unit test

--- src/app/lib/analyticsUtils/index.test.js
+++ src/app/lib/analyticsUtils/index.test.js
@@ -441,12 +441,12 @@ describe('getAtUserId', () => {
   it('should return AT user id when found and then set it in cookies again to update the expiraton date', () => {
     Cookie.set('atuserid', JSON.stringify({ val: 'uuid' }));
     cookieSetterSpy.mockClear();
-    const actual = getAtUserId();
+    const atUserId = getAtUserId();
     const [
       [cookieName, cookieValue, cookieOptions],
     ] = cookieSetterSpy.mock.calls;
 
-    expect(actual).toEqual('uuid');
+    expect(atUserId).toEqual('uuid');
     expect(cookieName).toEqual('atuserid');
     expect(cookieValue).toEqual({
       val: 'uuid',
@@ -457,12 +457,12 @@ describe('getAtUserId', () => {
 
   it('should create new user id if cookie does not exist and set the id in cookies', () => {
     const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-    const actual = getAtUserId();
+    const atUserId = getAtUserId();
     const [
       [cookieName, cookieValue, cookieOptions],
     ] = cookieSetterSpy.mock.calls;
 
-    expect(actual).toMatch(uuidRegex);
+    expect(atUserId).toMatch(uuidRegex);
     expect(cookieName).toEqual('atuserid');
     expect(cookieValue).toEqual({
       val: expect.stringMatching(uuidRegex),
