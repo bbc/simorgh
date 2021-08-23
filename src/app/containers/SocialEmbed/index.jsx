3a656cc22a6c6a310ebaca41e01fee85eb994e30
HarryVerhoef <harry.verhoef@bbc.co.uk>
2021-08-23 15:47:42 +0100

    Remove useState from imports

--- src/app/containers/SocialEmbed/index.jsx
+++ src/app/containers/SocialEmbed/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import path from 'ramda/src/path';
 import is from 'ramda/src/is';
 import Lazyload from 'react-lazyload';
