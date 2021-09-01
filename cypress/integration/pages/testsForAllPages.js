2468a760c3e15617945aeeffef947fa30a36e66a
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-09-01 15:46:25 +0100

    move runOnly to cy.configureAxe to prevent overriding

--- cypress/integration/pages/testsForAllPages.js
+++ cypress/integration/pages/testsForAllPages.js
@@ -28,6 +28,10 @@ export const testsThatAlwaysRunForAllPages = ({ service, pageType }) => {
     it('Has no detectable a11y violations on load', () => {
       cy.injectAxe();
       cy.configureAxe({
+        runOnly: {
+          type: 'tag',
+          values: ['wcag21a', 'wcag21aa', 'wcag2a', 'wcag2aa', 'best-practice'],
+        },
         rules: [
           {
             // we need to disable this rule because of the inner & outer double iframe setup we have with media players
@@ -38,22 +42,7 @@ export const testsThatAlwaysRunForAllPages = ({ service, pageType }) => {
           },
         ],
       });
-      cy.checkA11y(
-        null,
-        {
-          runOnly: {
-            type: 'tag',
-            values: [
-              'wcag21a',
-              'wcag21aa',
-              'wcag2a',
-              'wcag2aa',
-              'best-practice',
-            ],
-          },
-        },
-        logA11yViolations,
-      );
+      cy.checkA11y(null, null, logA11yViolations);
     });
 
     it('should render topic tags if they are in the json, and they should navigate to correct topic page', () => {
