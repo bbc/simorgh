6edcc69905345f67677b2a351f6a9ad1434f0cbe
Harvey Peachey <harvey.peachey@bbc.co.uk>
2021-09-08 18:10:53 +0100

    added new forwardStyle prop

--- src/app/containers/ImageWithPlaceholder/index.jsx
+++ src/app/containers/ImageWithPlaceholder/index.jsx
@@ -66,7 +66,7 @@ const ImageWithPlaceholder = ({
         </Helmet>
       )}
       <ImagePlaceholder
-        css={isLoaded ? { background: 'none' } : null}
+        forwardStyle={isLoaded ? { background: 'none' } : null}
         ratio={ratio}
       >
         {isAmp ? (
