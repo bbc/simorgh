71eb1c85fc094aa08dcb0d0c79da98733d14a19a
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2021-09-09 09:52:51 +0100

    Remove use of CPS onward journey component

--- src/app/containers/CpsRecommendations/index.jsx
+++ src/app/containers/CpsRecommendations/index.jsx
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react';
-import { arrayOf, shape, number } from 'prop-types';
+import { arrayOf, shape, node } from 'prop-types';
 import styled from '@emotion/styled';
 import {
+  GEL_GROUP_1_SCREEN_WIDTH_MAX,
   GEL_GROUP_2_SCREEN_WIDTH_MIN,
   GEL_GROUP_3_SCREEN_WIDTH_MAX,
   GEL_GROUP_3_SCREEN_WIDTH_MIN,
@@ -9,37 +10,65 @@ import {
 } from '@bbc/gel-foundations/breakpoints';
 import pathOr from 'ramda/src/pathOr';
 import path from 'ramda/src/path';
-import { C_LUNAR } from '@bbc/psammead-styles/colours';
+import { C_GHOST } from '@bbc/psammead-styles/colours';
 import {
   GEL_SPACING,
   GEL_SPACING_DBL,
   GEL_SPACING_TRPL,
 } from '@bbc/gel-foundations/spacings';
 import SectionLabel from '@bbc/psammead-section-label';
+import SkipLinkWrapper from '#components/SkipLinkWrapper';
 
 import { storyItem } from '#models/propTypes/storyItem';
 import { ServiceContext } from '#contexts/ServiceContext';
 import useToggle from '#hooks/useToggle';
-import CpsOnwardJourney from '../CpsOnwardJourney';
 import { GridItemMediumNoMargin } from '#app/components/Grid';
 import RecommendationsPromo from './RecommendationsPromo';
 import RecommendationsPromoList from './RecommendationsPromoList';
 
+const Wrapper = styled.div`
+  @media (max-width: ${GEL_GROUP_1_SCREEN_WIDTH_MAX}) {
+    padding: 0 ${GEL_SPACING};
+  }
+
+  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
+    padding: 0 ${GEL_SPACING_DBL};
+  }
+`;
+
+// eslint-disable-next-line react/prop-types
+const SkipWrapper = ({ skipLink, service, children }) => {
+  return (
+    <SkipLinkWrapper service={service} {...skipLink}>
+      {children}
+    </SkipLinkWrapper>
+  );
+};
+
 const RecommendationsWrapper = styled.div`
-  background-color: ${C_LUNAR};
+  background-color: ${C_GHOST};
   margin: ${GEL_SPACING_TRPL} 0;
-  padding: ${GEL_SPACING_DBL} ${GEL_SPACING};
+  padding: ${GEL_SPACING_DBL} 0;
   @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
     margin: 0 0 ${GEL_SPACING_TRPL};
     padding: 0 0 ${GEL_SPACING_DBL};
   }
   @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
     margin: 0 0 ${GEL_SPACING_TRPL};
-    padding: ${GEL_SPACING_DBL} 0;
   }
-  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
-    padding: ${GEL_SPACING_DBL};
-  }
+`;
+
+const SingleContentWrapper = styled.div`
+  ${({ columnType }) =>
+    columnType === 'secondary' &&
+    `
+    @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
+      padding-top: ${GEL_SPACING_DBL};
+    }
+    @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
+      padding-bottom: ${GEL_SPACING_TRPL};
+    }
+  `}
 `;
 
 const LabelComponent = styled(SectionLabel)`
@@ -50,9 +79,30 @@ const LabelComponent = styled(SectionLabel)`
   }
 `;
 
-const CpsRecommendations = ({ items, parentColumns }) => {
-  const { recommendations, translations } = useContext(ServiceContext);
+const CpsRecommendations = ({ items }) => {
+  const { recommendations, translations, script, service, dir } = useContext(
+    ServiceContext,
+  );
   const { enabled } = useToggle('cpsRecommendations');
+  const labelId = 'recommendations-heading';
+  const a11yAttributes = {
+    as: 'section',
+    role: 'region',
+    'aria-labelledby': labelId,
+  };
+
+  const hasSingleContent = items.length === 1;
+  const [singleContent] = items;
+
+  const CpsOnwardJourneyWrapper = ({ children }) => (
+    <Wrapper data-e2e={labelId} {...a11yAttributes}>
+      {children}
+    </Wrapper>
+  );
+
+  CpsOnwardJourneyWrapper.propTypes = {
+    children: node.isRequired,
+  };
 
   const { hasStoryRecommendations } = recommendations;
 
@@ -82,20 +132,31 @@ const CpsRecommendations = ({ items, parentColumns }) => {
   return (
     <GridItemMediumNoMargin>
       <RecommendationsWrapper>
-        <CpsOnwardJourney
-          LabelComponent={LabelComponent}
-          labelId="recommendations-heading"
-          title={title}
-          content={items}
-          parentColumns={parentColumns}
-          promoComponent={RecommendationsPromo}
-          promoListComponent={RecommendationsPromoList}
-          sectionLabelOverrideAs="strong"
-          sectionLabelBar={false}
-          sectionLabelBackground={C_LUNAR}
-          columnType="main"
-          skipLink={skipLinkProps}
-        />
+        <CpsOnwardJourneyWrapper>
+          <SkipWrapper skipLink={skipLinkProps} service={service}>
+            {title ? (
+              <LabelComponent
+                script={script}
+                service={service}
+                dir={dir}
+                labelId={labelId}
+                columnType="main"
+                overrideHeadingAs="strong"
+                bar={false}
+                backgroundColor={C_GHOST}
+              >
+                {title}
+              </LabelComponent>
+            ) : null}
+            {hasSingleContent ? (
+              <SingleContentWrapper columnType="main">
+                <RecommendationsPromo promo={singleContent} dir={dir} />
+              </SingleContentWrapper>
+            ) : (
+              <RecommendationsPromoList promoItems={items} dir={dir} />
+            )}
+          </SkipWrapper>
+        </CpsOnwardJourneyWrapper>
       </RecommendationsWrapper>
     </GridItemMediumNoMargin>
   );
@@ -105,17 +166,8 @@ export default CpsRecommendations;
 
 CpsRecommendations.propTypes = {
   items: arrayOf(shape(storyItem)),
-  parentColumns: shape({
-    group0: number,
-    group1: number,
-    group2: number,
-    group3: number,
-    group4: number,
-    group5: number,
-  }),
 };
 
 CpsRecommendations.defaultProps = {
   items: [],
-  parentColumns: null,
 };
