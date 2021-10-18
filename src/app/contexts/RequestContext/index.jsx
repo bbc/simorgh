1aee79dc8c617feb35e06682e7cf68d15f683d1b
nicola-d-bbc <nicola.dochnenko@bbc.co.uk>
2021-10-18 16:42:36 +0100

    removed radio and podcast page from context

--- src/app/contexts/RequestContext/index.jsx
+++ src/app/contexts/RequestContext/index.jsx
@@ -19,8 +19,6 @@ import {
   PHOTO_GALLERY_PAGE,
   STORY_PAGE,
   CORRESPONDENT_STORY_PAGE,
-  RADIO_PAGE,
-  PODCAST_PAGE,
 } from '#app/routes/utils/pageTypes';
 
 export const RequestContext = React.createContext({});
@@ -96,8 +94,6 @@ RequestContextProvider.propTypes = {
     CORRESPONDENT_STORY_PAGE,
     PHOTO_GALLERY_PAGE,
     INDEX_PAGE,
-    RADIO_PAGE,
-    PODCAST_PAGE,
   ]).isRequired,
   service: string.isRequired,
   statusCode: number,
