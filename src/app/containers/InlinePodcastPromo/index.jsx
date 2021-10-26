6b952215374c310ee5bf8ca98d9c468df19a29d6
mfonofm <mfon.essien@bbc.co.uk>
2021-10-26 18:22:15 +0100

    remove unused variable

--- src/app/containers/InlinePodcastPromo/index.jsx
+++ src/app/containers/InlinePodcastPromo/index.jsx
@@ -11,7 +11,6 @@ import {
   GEL_GROUP_0_SCREEN_WIDTH_MAX,
   GEL_GROUP_1_SCREEN_WIDTH_MIN,
   GEL_GROUP_B_MIN_WIDTH,
-  GEL_GROUP_2_SCREEN_WIDTH_MIN,
   GEL_GROUP_3_SCREEN_WIDTH_MIN,
 } from '@bbc/gel-foundations/breakpoints';
 import { getPica } from '@bbc/gel-foundations/typography';
