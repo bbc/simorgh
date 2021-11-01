08ed3fb79682430d1f1124c32b74a168447f22a3
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-11-01 14:53:41 +0000

    add new test cases

--- src/app/routes/utils/sharedDataTransformers/addIdsToBlocks/index.js
+++ src/app/routes/utils/sharedDataTransformers/addIdsToBlocks/index.js
@@ -5,17 +5,22 @@ import addIdsToItems from '../addIdsToItems';
 
 export default json => {
   const content = path(['content'], json);
-  const newBlocks = addIdsToItems({
-    pathToItems: ['model', 'blocks'],
-    recursive: true,
-  })(content);
 
-  return mergeDeepLeft(
-    {
-      content: {
-        ...newBlocks,
+  if (content) {
+    const newBlocks = addIdsToItems({
+      pathToItems: ['model', 'blocks'],
+      recursive: true,
+    })(content);
+
+    return mergeDeepLeft(
+      {
+        content: {
+          ...newBlocks,
+        },
       },
-    },
-    json,
-  );
+      json,
+    );
+  }
+
+  return json;
 };
