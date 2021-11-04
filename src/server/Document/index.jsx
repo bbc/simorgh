925ce516f685451b58da36c0598554d97cecae48
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-11-04 14:29:28 +0000

    render loadable preload links

--- src/server/Document/index.jsx
+++ src/server/Document/index.jsx
@@ -73,12 +73,15 @@ const renderDocument = async ({
     return commonAttributes;
   });
 
+  const links = extractor.getLinkElements();
   const headHelmet = Helmet.renderStatic();
   const assetOrigins = getAssetOrigins(service);
   const doc = renderToStaticMarkup(
     <DocumentComponent
       assetOrigins={assetOrigins}
+      scriptPreloads={scripts}
       scripts={scripts}
+      links={links}
       app={app}
       data={data}
       helmet={headHelmet}
