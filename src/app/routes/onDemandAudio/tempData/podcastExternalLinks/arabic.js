58d54f72ff6d1c4c8e18f4c545eac9ec21eb8bcd
georgva0 <41199008+georgva0@users.noreply.github.com>
2021-11-16 09:21:20 +0000

    Update arabic.js

--- src/app/routes/onDemandAudio/tempData/podcastExternalLinks/arabic.js
+++ src/app/routes/onDemandAudio/tempData/podcastExternalLinks/arabic.js
@@ -1,6 +1,5 @@
 const externalLinks = {
   default: {
-
     p02pc9qc: [
       {
         linkText: 'Spotify',
@@ -18,8 +17,7 @@ const externalLinks = {
       },
       {
         linkText: 'Apple',
-        linkUrl:
-          'https://podcasts.apple.com/ae/podcast/%D8%AE%D8%B1%D8%A7%D9%81%D8%A7%D8%AA/id1541601477',
+        linkUrl: 'https://podcasts.apple.com/ae/podcast/%D8%AE%D8%B1%D8%A7%D9%81%D8%A7%D8%AA/id1541601477',
       },
     ],
     p086jpqy: [
@@ -29,8 +27,7 @@ const externalLinks = {
       },
       {
         linkText: 'Apple',
-        linkUrl:
-          'https://podcasts.apple.com/gb/podcast/%D8%A7%D9%84%D9%86%D8%B4%D8%B1%D8%A9-%D8%A7%D9%84%D9%8A%D9%88%D9%85%D9%8A%D8%A9-%D9%84%D9%81%D9%8A%D8%B1%D9%88%D8%B3-%D9%83%D9%88%D8%B1%D9%88%D9%86%D8%A7/id1502996667',
+        linkUrl: 'https://podcasts.apple.com/gb/podcast/%D8%A7%D9%84%D9%86%D8%B4%D8%B1%D8%A9-%D8%A7%D9%84%D9%8A%D9%88%D9%85%D9%8A%D8%A9-%D9%84%D9%81%D9%8A%D8%B1%D9%88%D8%B3-%D9%83%D9%88%D8%B1%D9%88%D9%86%D8%A7/id1502996667',
       },
     ],
     p08gp8wx: [
@@ -40,8 +37,7 @@ const externalLinks = {
       },
       {
         linkText: 'Apple',
-        linkUrl:
-          'https://podcasts.apple.com/us/podcast/%D8%AE%D8%B1%D8%A7%D9%81%D8%A7%D8%AA-%D9%83%D9%88%D8%B1%D9%88%D9%86%D8%A7/id1518556307',
+        linkUrl: 'https://podcasts.apple.com/us/podcast/%D8%AE%D8%B1%D8%A7%D9%81%D8%A7%D8%AA-%D9%83%D9%88%D8%B1%D9%88%D9%86%D8%A7/id1518556307',
       },
     ],
     p09cgw89: [
@@ -51,20 +47,17 @@ const externalLinks = {
       },
       {
         linkText: 'Apple',
-        linkUrl:
-          'https://podcasts.apple.com/us/podcast/%D8%A3%D8%A8%D8%B9%D8%A7%D8%AF-%D8%A7%D9%84%D8%B5%D9%88%D8%B1%D8%A9/id1561146726',
+        linkUrl: 'https://podcasts.apple.com/us/podcast/%D8%A3%D8%A8%D8%B9%D8%A7%D8%AF-%D8%A7%D9%84%D8%B5%D9%88%D8%B1%D8%A9/id1561146726',
       },
     ],
     p09m6x31: [
       {
         linkText: 'Spotify',
-        linkUrl:
-          'https://open.spotify.com/show/3VsAmuPq7qHIRofC5biK8c',
+        linkUrl: 'https://open.spotify.com/show/3VsAmuPq7qHIRofC5biK8c',
       },
       {
         linkText: 'Apple',
-        linkUrl:
-          'https://podcasts.apple.com/gb/podcast/%D8%A8%D9%88%D8%AF%D9%83%D8%A7%D8%B3%D8%AA-%D8%B9-%D9%84%D8%A7%D9%82%D8%A7%D8%AA/id1573549106',
+        linkUrl: 'https://podcasts.apple.com/gb/podcast/%D8%A8%D9%88%D8%AF%D9%83%D8%A7%D8%B3%D8%AA-%D8%B9-%D9%84%D8%A7%D9%82%D8%A7%D8%AA/id1573549106',
       },
     ],
     p09w8yvk: [
