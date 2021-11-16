d76d75979ccb1629990e0fd24fd66c218cbd6a0a
georgva0 <vgeorg@yahoo.com>
2021-11-16 17:54:10 +0000

    Test ignore - it will be deleted

--- src/app/routes/onDemandAudio/tempData/podcastExternalLinks/kyrgyz.js
+++ src/app/routes/onDemandAudio/tempData/podcastExternalLinks/kyrgyz.js
@@ -11,6 +11,17 @@ const externalLinks = {
           'https://podcasts.apple.com/us/podcast/25-%D0%B6%D1%8B%D0%BB-%D0%BE%D0%B1%D0%BE%D0%B4%D0%BE/id1568103058',
       },
     ],
+    p09hxksv: [
+      {
+        linkText: 'SpotifyAAA',
+        linkUrl: 'https://open.spotify.com/show/0Ns5LMWc959Cummte0k9Q1AAA',
+      },
+      {
+        linkText: 'Apple',
+        linkUrl:
+          'https://podcasts.apple.com/us/podcast/25-%D0%B6%D1%8B%D0%BB-%D0%BE%D0%B1%D0%BE%D0%B4%D0%BE/id1568103058AAA',
+      },
+    ],
   },
 };
 
