97405fa09bb2f0410eaedd3719c2e3bbf9871a2a
georgva0 <41199008+georgva0@users.noreply.github.com>
2021-11-16 12:59:50 +0000

    Update marathi.js

--- src/app/routes/onDemandAudio/tempData/podcastExternalLinks/marathi.js
+++ src/app/routes/onDemandAudio/tempData/podcastExternalLinks/marathi.js
@@ -7,8 +7,7 @@ const externalLinks = {
       },
       {
         linkText: 'Apple',
-        linkUrl: 
-          'https://podcasts.apple.com/gb/podcast/%E0%A4%A4-%E0%A4%A8-%E0%A4%97-%E0%A4%B7-%E0%A4%9F/id1550093946',
+        linkUrl: 'https://podcasts.apple.com/gb/podcast/%E0%A4%A4-%E0%A4%A8-%E0%A4%97-%E0%A4%B7-%E0%A4%9F/id1550093946',
       },
     ],
     p09437hm: [
@@ -18,8 +17,7 @@ const externalLinks = {
       },
       {
         linkText: 'Apple',
-        linkUrl: 
-          'https://podcasts.apple.com/gb/podcast/%E0%A4%B8-%E0%A4%AA-%E0%A4%97-%E0%A4%B7-%E0%A4%9F/id1550094137',
+        linkUrl: 'https://podcasts.apple.com/gb/podcast/%E0%A4%B8-%E0%A4%AA-%E0%A4%97-%E0%A4%B7-%E0%A4%9F/id1550094137',
       },
     ],
     p0b1s4nm: [
@@ -29,8 +27,7 @@ const externalLinks = {
       },
       {
         linkText: 'Apple',
-        linkUrl: 
-          'https://podcasts.apple.com/gb/podcast/%E0%A4%97-%E0%A4%B7-%E0%A4%9F-%E0%A4%A6-%E0%A4%A8-%E0%A4%AF-%E0%A4%9A/id1595097031',
+        linkUrl: 'https://podcasts.apple.com/gb/podcast/%E0%A4%97-%E0%A4%B7-%E0%A4%9F-%E0%A4%A6-%E0%A4%A8-%E0%A4%AF-%E0%A4%9A/id1595097031',
       },
     ],
   },
