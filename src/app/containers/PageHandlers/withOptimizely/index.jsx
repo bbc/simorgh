7eece61cfc76e580c8a5b54d87cdfe69025d2bc8
Harvey Peachey <harvey.peachey@bbc.co.uk>
2021-11-25 11:05:01 +0000

    adds local datafile

--- src/app/containers/PageHandlers/withOptimizely/index.jsx
+++ src/app/containers/PageHandlers/withOptimizely/index.jsx
@@ -2,11 +2,13 @@ import React, { useContext } from 'react';
 import { createInstance, OptimizelyProvider } from '@optimizely/react-sdk';
 import { ServiceContext } from '#contexts/ServiceContext';
 import { getAtUserId } from '#lib/analyticsUtils';
+import * as datafile from './datafile.json';
 
 const optimizely = createInstance({
   sdkKey: process.env.SIMORGH_OPTIMIZELY_SDK_KEY,
   eventBatchSize: 100,
   eventFlushInterval: 1000,
+  datafile,
 });
 
 const withOptimizely = Component => {
@@ -16,6 +18,8 @@ const withOptimizely = Component => {
     return (
       <OptimizelyProvider
         optimizely={optimizely}
+        isServerSide
+        timeout={500}
         user={{
           id: `${getAtUserId()}`,
           attributes: {
