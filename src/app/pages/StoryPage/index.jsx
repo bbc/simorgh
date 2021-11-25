16fc162a28848bc852ca633a7ccbdf67b8873a11
Harvey Peachey <harvey.peachey@bbc.co.uk>
2021-11-25 12:23:20 +0000

    added withOptimizely HOC to Story Page wrapper

--- src/app/pages/StoryPage/index.jsx
+++ src/app/pages/StoryPage/index.jsx
@@ -1,6 +1,8 @@
 import StoryPage from './StoryPage';
 import applyBasicPageHandlers from '../utils/applyBasicPageHandlers';
 
+import withOptimizely from '#app/containers/PageHandlers/withOptimizely';
+
 export default applyBasicPageHandlers({
   addVariantHandling: false,
-})(StoryPage);
+})(withOptimizely(StoryPage));
