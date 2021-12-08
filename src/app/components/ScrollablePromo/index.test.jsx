8e420ee42a38c0d0df3a9abcf66ddf5e1785e9f7
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2021-12-08 16:01:04 +0000

    Initial tests

---
+++ src/app/components/ScrollablePromo/index.test.jsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+
+describe('ScrollablePromo', () => {
+  it('should render max 3 promo items', () => {});
+  it('should render single promo item', () => {});
+  it('find out if this should be an unordered list', () => {});
+  it('find out about a11y requirements', () => {});
+});
