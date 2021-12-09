bf600f89439d11a802980f8d9e0a0914fe7cf793
Dario Russo <dario.russo@bbc.co.uk>
2021-12-09 11:00:09 +0000

    re-position styled component

--- src/app/components/ScrollablePromo/index.jsx
+++ src/app/components/ScrollablePromo/index.jsx
@@ -2,25 +2,27 @@ import React, { useContext } from 'react';
 import { array } from 'prop-types';
 import styled from '@emotion/styled';
 import { GEL_SPACING_DBL } from '@bbc/gel-foundations/dist/spacings';
+
 import SingleCard from './SingleCard';
 import { GridItemMedium } from '#app/components/Grid';
 import ListCard from './ListCard';
 import { ServiceContext } from '#contexts/ServiceContext';
 
+const SingleCardBox = styled.li`
+  flex-shrink: 0;
+  width: 205px;
+  background-color: #ffffff;
+  padding: ${GEL_SPACING_DBL};
+  ${({ dir }) =>
+    `margin-${dir === 'ltr' ? 'left' : 'right'}: ${GEL_SPACING_DBL};`}
+  &:first-child {
+    margin: 0;
+  }
+`;
+
 const ScrollablePromo = ({ blocks }) => {
   const isSingleItem = blocks.length === 2;
   const { dir } = useContext(ServiceContext);
-
-  const SingleCardBox = styled.li`
-    flex-shrink: 0;
-    width: 205px;
-    background-color: #ffffff;
-    padding: ${GEL_SPACING_DBL};
-    margin-${dir === 'ltr' ? 'left' : 'right'}: ${GEL_SPACING_DBL};
-    &:first-child {
-      margin: 0;
-    }
-  `;
 
   // IF NO PROMO RETURN NULL
   return (
