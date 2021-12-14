b379f66cf5ae00339554783e1cc51989d6e7edbe
pvaliani <pedram.homayoun-valiani@bbc.co.uk>
2021-12-14 16:07:02 +0000

    update cpsfeaturesanalysis unit tests - adds mock match media object

--- src/app/containers/CpsFeaturesAnalysis/index.test.jsx
+++ src/app/containers/CpsFeaturesAnalysis/index.test.jsx
@@ -99,6 +99,19 @@ jest.mock('#lib/utilities/isLive', () => jest.fn());
 describe('CpsFeaturesAnalysis', () => {
   beforeEach(() => {
     jest.resetAllMocks();
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
   });
 
   it('tests use a fixture that has multiple features', () => {
