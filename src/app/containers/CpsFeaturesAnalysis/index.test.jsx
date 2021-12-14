2536afcf94422c501033b55dd12841bb61324481
Harvey Peachey <harvey.peachey@bbc.co.uk>
2021-12-14 14:28:51 +0000

    removes unused screen

--- src/app/containers/CpsFeaturesAnalysis/index.test.jsx
+++ src/app/containers/CpsFeaturesAnalysis/index.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { ServiceContextProvider } from '#contexts/ServiceContext';
 import { RequestContextProvider } from '#contexts/RequestContext';
