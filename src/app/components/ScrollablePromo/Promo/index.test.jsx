4d8945355234e91e987aaefdbfc017f28c847e73
Dario Russo <dario.russo@bbc.co.uk>
2021-12-15 16:15:57 +0000

    snapshots

--- src/app/components/ScrollablePromo/Promo/index.test.jsx
+++ src/app/components/ScrollablePromo/Promo/index.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { shouldMatchSnapshot } from '@bbc/psammead-test-helpers';
 import { PromoSingleBlock } from '../helpers/fixtureData';
 import Promo from '.';
 
@@ -9,9 +8,4 @@ describe('ScrollablePromo', () => {
     const { queryByRole } = render(<Promo block={PromoSingleBlock} />);
     expect(queryByRole('link')).toBeInTheDocument();
   });
-
-  shouldMatchSnapshot(
-    'should truncate the link text if more than 3 lines',
-    <Promo block={PromoSingleBlock} />,
-  );
 });
