aaca0e7a747d051b4de9dec437def1220e7faed5
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2022-01-12 14:55:25 +0000

    refactor tests

--- src/app/routes/cpsAsset/getInitialData/isListWithLink/index.test.js
+++ src/app/routes/cpsAsset/getInitialData/isListWithLink/index.test.js
@@ -1,55 +1,131 @@
 import isListWithLink from '.';
 
-const block = {
+const listItemWithLink = {
+  type: 'listItem',
   model: {
     blocks: [
       {
+        type: 'paragraph',
         model: {
           blocks: [
             {
-              type: 'listItem',
-              model: {
-                blocks: [
-                  {
-                    type: 'paragraph',
-                    model: {
-                      text: "'Hidhamuun Miseensota Olaanoo KFO nu yaaddessa' - KFO",
-                      blocks: [
-                        {
-                          type: 'urlLink',
-                          model: {
-                            text: "'Hidhamuun Miseensota Olaanoo KFO nu yaaddessa' - KFO",
-                            locator:
-                              'http://www.bbc.com/afaanoromoo/oduu-53249190',
-                            blocks: [
-                              {
-                                type: 'fragment',
-                                model: {
-                                  text: "'Hidhamuun Miseensota Olaanoo KFO nu yaaddessa' - KFO",
-                                  attributes: [],
-                                },
-                              },
-                            ],
-                            isExternal: false,
-                          },
-                        },
-                      ],
-                    },
-                  },
-                ],
-              },
+              type: 'urlLink',
             },
           ],
         },
-        type: 'unorderedList',
       },
     ],
   },
-  type: 'text',
 };
 
-it('should work', () => {
-  const actual = isListWithLink(block);
+const listItemWithNoLink = {
+  type: 'listItem',
+  model: {
+    blocks: [
+      {
+        type: 'paragraph',
+        model: {
+          blocks: [
+            {
+              type: 'text',
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
 
-  expect(actual).toEqual(true);
+describe('isListWithLink', () => {
+  it('should return true if the block group structure is an unordered list with any list item containing a link', () => {
+    const blockGroup = {
+      model: {
+        blocks: [
+          {
+            model: {
+              blocks: [listItemWithLink, listItemWithNoLink],
+            },
+            type: 'unorderedList',
+          },
+        ],
+      },
+      type: 'text',
+    };
+    const actual = isListWithLink(blockGroup);
+
+    expect(actual).toEqual(true);
+  });
+
+  it('should return true if the block group structure is an ordered list with any list item containing a link', () => {
+    const blockGroup = {
+      model: {
+        blocks: [
+          {
+            model: {
+              blocks: [listItemWithLink, listItemWithNoLink],
+            },
+            type: 'orderedList',
+          },
+        ],
+      },
+      type: 'text',
+    };
+    const actual = isListWithLink(blockGroup);
+
+    expect(actual).toEqual(true);
+  });
+
+  it('should return false if the block group structure is a list with all list items containing no link', () => {
+    const blockGroup = {
+      model: {
+        blocks: [
+          {
+            model: {
+              blocks: [listItemWithNoLink],
+            },
+            type: 'unorderedList',
+          },
+        ],
+      },
+      type: 'text',
+    };
+    const actual = isListWithLink(blockGroup);
+
+    expect(actual).toEqual(false);
+  });
+
+  it('should return false if the block group structure is an empty object', () => {
+    const blockGroup = {};
+    const actual = isListWithLink(blockGroup);
+
+    expect(actual).toEqual(false);
+  });
+
+  it('should return false if the block group structure is undefined', () => {
+    const blockGroup = undefined;
+    const actual = isListWithLink(blockGroup);
+
+    expect(actual).toEqual(false);
+  });
+
+  it('should return false if the block group structure is null', () => {
+    const blockGroup = null;
+    const actual = isListWithLink(blockGroup);
+
+    expect(actual).toEqual(false);
+  });
+
+  it('should return false if the block group structure is an array', () => {
+    const blockGroup = [];
+    const actual = isListWithLink(blockGroup);
+
+    expect(actual).toEqual(false);
+  });
+
+  it('should return false if the block group structure is a string', () => {
+    const blockGroup = '{}';
+    const actual = isListWithLink(blockGroup);
+
+    expect(actual).toEqual(false);
+  });
 });
