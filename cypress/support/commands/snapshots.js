d4e9b9b697daa062482dc52f0f224e9faf8b99d1
LilyL0u <lily.higham@bbc.co.uk>
2022-01-12 20:58:13 +0000

    command in snapshots

--- cypress/support/commands/snapshots.js
+++ cypress/support/commands/snapshots.js
@@ -1,3 +1,8 @@
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
-addMatchImageSnapshotCommand();
+addMatchImageSnapshotCommand({
+  failureThreshold: 0.03, // threshold for entire image
+  failureThresholdType: 'percent', // percent of image or number of pixels
+  customDiffConfig: { threshold: 0.1 }, // threshold for each pixel
+  capture: 'viewport', // capture viewport in screenshot
+});
