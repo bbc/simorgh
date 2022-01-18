6d7bfba6712480d8f88de61eb0482f8948b3a4ef
Dario Russo <dario.russo@bbc.co.uk>
2022-01-18 16:23:55 +0000

    added regex to replace http or https

--- src/app/lib/utilities/ichefURL/index.js
+++ src/app/lib/utilities/ichefURL/index.js
@@ -3,7 +3,7 @@ const WEBP_ORIGIN_CODES = ['cpsdevpb', 'cpsprodpb'];
 
 const buildPlaceholderSrc = (src, resolution) => {
   if (src.includes('urn:')) return src;
-  const urlParts = src.replace('https://', '').split('/');
+  const urlParts = src.replace(/https:\/\/|http:\/\//, '').split('/');
   const [domain, mediaType, imgService, ...remainingUrlParts] = urlParts;
   const remainingUrlPartsWithoutResolution = remainingUrlParts.slice(1);
   const newResolution = `${resolution}xn`;
