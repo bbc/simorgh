b887dd8f6e844f39f797bcc11b23fc4e5a42284e
pvaliani <pedram.homayoun-valiani@bbc.co.uk>
2022-01-20 12:00:26 +0000

    adds scratchpad ui

--- src/app/pages/TopicPage/TopicPage.jsx
+++ src/app/pages/TopicPage/TopicPage.jsx
@@ -1,20 +1,121 @@
-// render whatever data we get from getinitialdata i.e hello world
-
 import React from 'react';
-import { string } from 'prop-types';
+import styled from '@emotion/styled';
+// import { string } from 'prop-types';
+import StoryPromoContainer from '../../containers/StoryPromo';
+// import HeadingsContainer from '../../containers/Headings';
 
-const TopicPage = ({ pageData }) => {
-  return <div>{pageData.title}</div>;
+// import { storyItem } from '#models/propTypes/storyItem';
+// display inline block
+
+const StyledTopicPromo = styled.div`
+  display: inline-block;
+  padding-left: 25px;
+  padding-bottom: 25px;
+`;
+
+const StyledTopicPromoHeader = styled.h1`
+  padding-left: 25px;
+  font-family: ReithSans;
+  font-size: 44px;
+`;
+
+const pageData = [
+  {
+    headlines: {
+      headline: 'A headline',
+    },
+    locators: {
+      assetUri: 'https://www.bbc.co.uk',
+    },
+    summary: 'Summary text',
+    timestamp: 1556795033000,
+    indexImage: {
+      path: '/cpsprodpb/0A06/production/image.jpg',
+      height: 1152,
+      width: 2048,
+      altText: 'Image Alt text',
+      copyrightHolder: 'Image provider',
+    },
+  },
+  {
+    headlines: {
+      headline: 'A headline',
+    },
+    locators: {
+      assetUri: 'https://www.bbc.co.uk',
+    },
+    summary: 'Summary text',
+    timestamp: 1556795033000,
+    indexImage: {
+      path: '/cpsprodpb/0A06/production/image.jpg',
+      height: 1152,
+      width: 2048,
+      altText: 'Image Alt text',
+      copyrightHolder: 'Image provider',
+    },
+  },
+  {
+    headlines: {
+      headline: 'A headline',
+    },
+    locators: {
+      assetUri: 'https://www.bbc.co.uk',
+    },
+    summary: 'Summary text',
+    timestamp: 1556795033000,
+    indexImage: {
+      path: '/cpsprodpb/0A06/production/image.jpg',
+      height: 1152,
+      width: 2048,
+      altText: 'Image Alt text',
+      copyrightHolder: 'Image provider',
+    },
+  },
+  {
+    headlines: {
+      headline: 'A headline',
+    },
+    locators: {
+      assetUri: 'https://www.bbc.co.uk',
+    },
+    summary: 'Summary text',
+    timestamp: 1556795033000,
+    indexImage: {
+      path: '/cpsprodpb/0A06/production/image.jpg',
+      height: 1152,
+      width: 2048,
+      altText: 'Image Alt text',
+      copyrightHolder: 'Image provider',
+    },
+  },
+];
+
+const TopicPage = () => {
+  return (
+    <>
+      <StyledTopicPromoHeader>THIS IS A HEADING</StyledTopicPromoHeader>
+      {pageData.map(promo => (
+        <StyledTopicPromo>
+          <StoryPromoContainer
+            key={promo.id}
+            platform="canonical"
+            item={promo}
+            displaySummary={false}
+          />
+        </StyledTopicPromo>
+      ))}
+    </>
+  );
 };
 
-TopicPage.propTypes = {
-  pageData: {},
-  title: string,
-};
+// TopicPage.propTypes = {
+//   pageData: {},
+//   title: string,
+// };
 
-TopicPage.defaultProps = {
-  pageData: {},
-  title: '',
-};
+// TopicPage.defaultProps = {
+//   pageData: {},
+//   title: '',
+// };
 
 export default TopicPage;
