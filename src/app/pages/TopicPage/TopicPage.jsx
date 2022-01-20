5d4ee4a84aed0055a5de0bd26ef8a21c5404642f
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2022-01-20 20:21:41 +0000

    Add emoji to topics page

--- src/app/pages/TopicPage/TopicPage.jsx
+++ src/app/pages/TopicPage/TopicPage.jsx
@@ -1,20 +1,21 @@
 // render whatever data we get from getinitialdata i.e hello world
 
 import React from 'react';
-import { string } from 'prop-types';
 
 const TopicPage = ({ pageData }) => {
-  return <div>{pageData.title}</div>;
+  return (
+    <div>
+      {pageData.thisIsA} {pageData.randoMoji}
+    </div>
+  );
 };
 
 TopicPage.propTypes = {
   pageData: {},
-  title: string,
 };
 
 TopicPage.defaultProps = {
   pageData: {},
-  title: '',
 };
 
 export default TopicPage;
