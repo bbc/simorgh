adcf5e12c59e69dd33792b1131642686943d6f5b
LilyL0u <lily.higham@bbc.co.uk>
2022-01-31 12:09:13 +0000

    most wtahced and read pages tests

--- cypress/integration/pages/mostReadPage/tests.js
+++ cypress/integration/pages/mostReadPage/tests.js
@@ -52,6 +52,21 @@ export default ({ service, pageType, variant }) => {
             });
           }
         });
+        describe(`Visual comparison tests for ${service} ${pageType}`, () => {
+          it('Most Read', () => {
+            if (
+              Cypress.env('APP_ENV') === 'local' &&
+              Cypress.browser.isHeadless
+            ) {
+              cy.document().its('fonts.status').should('equal', 'loaded');
+              // eslint-disable-next-line cypress/no-unnecessary-waiting
+              cy.wait(3000);
+              cy.matchImageSnapshot({ capture: 'fullPage' });
+            } else {
+              cy.log('Snapshot skipped in headed mode');
+            }
+          });
+        });
       });
     });
   });
--- cypress/integration/pages/mostWatchedPage/tests.js
+++ cypress/integration/pages/mostWatchedPage/tests.js
@@ -83,6 +83,21 @@ export default ({ service, pageType, variant }) => {
           });
         });
       });
+      describe(`Visual comparison tests for ${service} ${pageType}`, () => {
+        it('Articles', () => {
+          if (
+            Cypress.env('APP_ENV') === 'local' &&
+            Cypress.browser.isHeadless
+          ) {
+            cy.document().its('fonts.status').should('equal', 'loaded');
+            // eslint-disable-next-line cypress/no-unnecessary-waiting
+            cy.wait(3000);
+            cy.matchImageSnapshot({ capture: 'fullPage' });
+          } else {
+            cy.log('Snapshot skipped in headed mode');
+          }
+        });
+      });
     });
   });
 };
