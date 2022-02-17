e2100ec68cdf723924e7bceb6c098a8f241f1510
LilyL0u <lily.higham@bbc.co.uk>
2022-02-17 16:15:38 +0000

    error threshold up

--- cypress/support/commands/snapshots.js
+++ cypress/support/commands/snapshots.js
@@ -1,8 +1,8 @@
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
 addMatchImageSnapshotCommand({
-  failureThreshold: 0.03, // threshold for entire image
+  failureThreshold: 0.05, // threshold for entire image
   failureThresholdType: 'percent', // percent of image or number of pixels
-  customDiffConfig: { threshold: 0.1 }, // threshold for each pixel
+  customDiffConfig: { threshold: 0.25 }, // threshold for each pixel
   capture: 'viewport', // capture viewport in screenshot
 });
