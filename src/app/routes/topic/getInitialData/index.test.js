80081755cf069a14e4ad2d7a4281dc329ff54029
pvaliani <pedram.homayoun-valiani@bbc.co.uk>
2022-02-21 14:42:48 +0000

    adds unit test definitions

--- src/app/routes/topic/getInitialData/index.test.js
+++ src/app/routes/topic/getInitialData/index.test.js
@@ -1,8 +1,16 @@
 import getInitialData from '.';
 
 describe('get initial data for topic', () => {
-  it.skip('should return our title', () => {
+  it.skip('should return our topic title', () => {
     const { pageData } = getInitialData();
     expect(pageData.title).toEqual('Hello world');
   });
+
+  it.skip('should return title, type, firstPublished, link, imageUrl and id from a summary', () => {});
+
+  it.skip('should throw an error if the topic id is invalid', () => {});
+
+  it.skip('should append the service and id to the BFF_PATH as a query string and return the topic - using a topic without a variant', () => {});
+
+  it.skip('should append the service, id and optional service variant to the BFF_PATH as a query string and return the topic - using a topic with a service variant', () => {});
 });
