af5600be4c945fcf30ffc318e7b503c64811b0ef
pvaliani <pedram.homayoun-valiani@bbc.co.uk>
2022-02-21 14:55:25 +0000

    split error code tests off from initial request tests

--- src/app/routes/topic/getInitialData/index.test.js
+++ src/app/routes/topic/getInitialData/index.test.js
@@ -8,9 +8,15 @@ describe('get initial data for topic', () => {
 
   it.skip('should return title, type, firstPublished, link, imageUrl and id from a summary', () => {});
 
-  it.skip('should throw an error if the topic id is invalid', () => {});
-
   it.skip('should append the service and id to the BFF_PATH as a query string and return the topic - using a topic without a variant', () => {});
 
   it.skip('should append the service, id and optional service variant to the BFF_PATH as a query string and return the topic - using a topic with a service variant', () => {});
+});
+
+describe('get error codes for initial data request responses ', () => {
+  it.skip('should throw an error and status code - code 500', () => {});
+
+  it.skip('should throw an error and status code - code 404', () => {});
+
+  it.skip('should throw an error and status code - code 400', () => {});
 });
