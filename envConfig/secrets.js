4b0664eebc07fa2dbbf3240991428f24692203bc
ExcitedBlob <ryanmccombe3@gmail.com>
2022-02-25 14:54:06 +0000

    Add gitignore

--- envConfig/secrets.js
+++ envConfig/secrets.js
@@ -8,7 +8,7 @@ export default {
   BFF_PATH: () => {
     if (!BFF_PATH) {
       throw new Error(
-        'Accessing this page requires the BFF path be configured in your development development environment.  Ask the team for help with this!',
+        'Accessing this page requires the BFF path be configured in your development environment.  Ask the team for help with this!',
       );
     }
     return BFF_PATH;
