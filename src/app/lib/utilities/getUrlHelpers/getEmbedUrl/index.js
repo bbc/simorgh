fcc040b07558c41b314eef0491f01baff3c09086
ExcitedBlob <ryanmccombe3@gmail.com>
2022-03-04 14:49:42 +0000

    Refactor getEmbedUrl to use relative paths where possible

--- src/app/lib/utilities/getUrlHelpers/getEmbedUrl/index.js
+++ src/app/lib/utilities/getUrlHelpers/getEmbedUrl/index.js
@@ -2,10 +2,19 @@ import isLive from '../../isLive';
 
 const AV_ROUTE = 'ws/av-embeds';
 
-const LIVE_CANONICAL_URL = 'https://www.bbc.com';
+// On test and live canonical, we use the same base url as the parent page
+const LIVE_CANONICAL_URL = '';
+const TEST_CANONICAL_URL = '';
+
+// On dev canonical, we load media from the test environment
+// This means developers do not need to have the media player infrastructure running
+const DEV_CANONICAL_URL = 'https://www.test.bbc.com';
+
+// Amp does not allow iframes to be loaded from the same domain as the parent
+// Therefore, we make our media players available on a different domain to bypass this
 const LIVE_AMP_URL = 'https://polling.bbc.co.uk';
-const TEST_CANONICAL_URL = 'https://www.test.bbc.com';
 const TEST_AMP_URL = 'https://polling.test.bbc.co.uk';
+const DEV_AMP_URL = TEST_AMP_URL;
 
 const shouldOverrideMorphEnv = (queryString, type) => {
   if (isLive()) return false;
@@ -23,10 +32,15 @@ const shouldOverrideMorphEnv = (queryString, type) => {
 };
 
 const getBaseUrl = isAmp => {
-  if (isAmp) {
-    return isLive() ? LIVE_AMP_URL : TEST_AMP_URL;
+  const isDev = process.env.SIMORGH_APP_ENV === 'development';
+  switch (true) {
+    case isLive():
+      return isAmp ? LIVE_AMP_URL : LIVE_CANONICAL_URL;
+    case isDev:
+      return isAmp ? DEV_AMP_URL : DEV_CANONICAL_URL;
+    default:
+      return isAmp ? TEST_AMP_URL : TEST_CANONICAL_URL;
   }
-  return isLive() ? LIVE_CANONICAL_URL : TEST_CANONICAL_URL;
 };
 
 export default ({ type, mediaId, isAmp = false, queryString }) => {
--- src/app/lib/utilities/getUrlHelpers/getEmbedUrl/index.test.js
+++ src/app/lib/utilities/getUrlHelpers/getEmbedUrl/index.test.js
@@ -13,7 +13,7 @@ const setEnvironment = environment => {
 const testCases = [
   {
     description: `should build a CANONICAL url for articles in test environment`,
-    expected: `https://www.test.bbc.com/ws/av-embeds/articles/${mediaId}`,
+    expected: `/ws/av-embeds/articles/${mediaId}`,
     environment: 'test',
     before: setEnvironment,
     embedObject: {
@@ -36,7 +36,7 @@ const testCases = [
   },
   {
     description: `should build a CANONICAL url for articles in test environment with test override`,
-    expected: `https://www.test.bbc.com/ws/av-embeds/articles/${mediaId}`,
+    expected: `/ws/av-embeds/articles/${mediaId}`,
     environment: 'test',
     before: setEnvironment,
     embedObject: {
@@ -59,7 +59,7 @@ const testCases = [
   },
   {
     description: `should build a CANONICAL url for articles in test environment with live override`,
-    expected: `https://www.test.bbc.com/ws/av-embeds/articles/${mediaId}${embedUrlLiveOverride}`,
+    expected: `/ws/av-embeds/articles/${mediaId}${embedUrlLiveOverride}`,
     environment: 'test',
     before: setEnvironment,
     embedObject: {
@@ -82,7 +82,7 @@ const testCases = [
   },
   {
     description: `should build a CANONICAL url for on-demand/live radio in test environment with live override`,
-    expected: `https://www.test.bbc.com/ws/av-embeds/media/${mediaId}${embedUrlLiveOverride}`,
+    expected: `/ws/av-embeds/media/${mediaId}${embedUrlLiveOverride}`,
     environment: 'test',
     before: setEnvironment,
     embedObject: {
@@ -105,7 +105,7 @@ const testCases = [
   },
   {
     description: `should build a CANONICAL url for articles in live environment`,
-    expected: `https://www.bbc.com/ws/av-embeds/articles/${mediaId}`,
+    expected: `/ws/av-embeds/articles/${mediaId}`,
     environment: 'live',
     before: setEnvironment,
     embedObject: {
@@ -128,7 +128,7 @@ const testCases = [
   },
   {
     description: `should build a CANONICAL url for articles in live environment with test override`,
-    expected: `https://www.bbc.com/ws/av-embeds/articles/${mediaId}`,
+    expected: `/ws/av-embeds/articles/${mediaId}`,
     environment: 'live',
     before: setEnvironment,
     embedObject: {
@@ -151,7 +151,7 @@ const testCases = [
   },
   {
     description: `should build a CANONICAL url for articles in live environment with live override`,
-    expected: `https://www.bbc.com/ws/av-embeds/articles/${mediaId}`,
+    expected: `/ws/av-embeds/articles/${mediaId}`,
     environment: 'live',
     before: setEnvironment,
     embedObject: {
@@ -174,7 +174,7 @@ const testCases = [
   },
   {
     description: `should build a CANONICAL url for legacy media in live environment with live override`,
-    expected: `https://www.bbc.com/ws/av-embeds/legacy/${legacyId}`,
+    expected: `/ws/av-embeds/legacy/${legacyId}`,
     environment: 'live',
     before: setEnvironment,
     embedObject: {
@@ -197,7 +197,7 @@ const testCases = [
   },
   {
     description: `should build a CANONICAL url for legacy media in test environment with no override`,
-    expected: `https://www.test.bbc.com/ws/av-embeds/legacy/${legacyId}`,
+    expected: `/ws/av-embeds/legacy/${legacyId}`,
     environment: 'test',
     before: setEnvironment,
     embedObject: {
@@ -216,6 +216,39 @@ const testCases = [
       mediaId: legacyId,
       type: 'legacy',
       queryString: '',
+    },
+  },
+  // Development environment
+  {
+    description: `should output test urls for canonical`,
+    expected: `https://www.test.bbc.com/ws/av-embeds/articles/${mediaId}`,
+    environment: 'development',
+    before: setEnvironment,
+    embedObject: {
+      mediaId,
+      type: 'articles',
+    },
+  },
+  {
+    description: `should output test polling urls for amp`,
+    expected: `https://polling.test.bbc.co.uk/ws/av-embeds/media/${mediaId}/amp${embedUrlLiveOverride}`,
+    environment: 'development',
+    before: setEnvironment,
+    embedObject: {
+      isAmp: true,
+      mediaId,
+      type: 'media',
+    },
+  },
+  {
+    description: `should respect overrides`,
+    expected: `https://www.test.bbc.com/ws/av-embeds/media/${mediaId}${embedUrlLiveOverride}`,
+    environment: 'development',
+    before: setEnvironment,
+    embedObject: {
+      mediaId,
+      type: 'media',
+      queryString: liveOverrideParam,
     },
   },
 ];
