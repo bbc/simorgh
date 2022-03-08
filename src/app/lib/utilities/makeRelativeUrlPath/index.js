87bec6c9b3fa17f7e0113d8c5ea05a4344809e23
Aaron Moore <aaron.moore@bbc.co.uk>
2022-03-08 11:40:30 +0000

    Switch to url-parse for AMP

--- src/app/lib/utilities/makeRelativeUrlPath/index.js
+++ src/app/lib/utilities/makeRelativeUrlPath/index.js
@@ -1,13 +1,15 @@
+import Url from 'url-parse';
+
 const allowList = ['www.bbc.com', 'www.bbc.co.uk', 'www.bbcrussian.com'];
 
 const makeRelativeUrlPath = urlPath => {
   if (!urlPath) return null;
 
-  const url = new URL(urlPath);
+  const url = new Url(urlPath);
   const isBBCDomain = allowList.some(domain => url.hostname === domain);
 
   if (isBBCDomain) {
-    return `${url.pathname}${url.search}${url.hash}`;
+    return `${url.pathname}${url.query}${url.hash}`;
   }
 
   return urlPath;
