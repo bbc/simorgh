4326e7193254f8244c2fedbc2c85d18828d48647
Aaron Moore <aaron.moore@bbc.co.uk>
2022-03-08 10:32:59 +0000

    Update index.test.js

--- src/app/lib/utilities/makeRelativeUrlPath/index.test.js
+++ src/app/lib/utilities/makeRelativeUrlPath/index.test.js
@@ -13,6 +13,24 @@ describe('makeRelativeUrlPath', () => {
     expect(relativeUrl).toBe('/');
   });
 
+  it('should return search params if supplied in the URL', () => {
+    const url = 'https://www.bbc.com?limit=4';
+    const relativeUrl = makeRelativeUrlPath(url);
+    expect(relativeUrl).toBe('/?limit=4');
+  });
+
+  it('should return hash params if supplied in the URL', () => {
+    const url = 'https://www.bbc.com#content';
+    const relativeUrl = makeRelativeUrlPath(url);
+    expect(relativeUrl).toBe('/#content');
+  });
+
+  it('should return search and hash params if supplied in the URL', () => {
+    const url = 'https://www.bbc.com?limit=4#content';
+    const relativeUrl = makeRelativeUrlPath(url);
+    expect(relativeUrl).toBe('/?limit=4#content');
+  });
+
   it('should not strip the domain if it is an external link', () => {
     const url = 'https://www.facebook.com/not-the-bbc';
     const relativeUrl = makeRelativeUrlPath(url);
