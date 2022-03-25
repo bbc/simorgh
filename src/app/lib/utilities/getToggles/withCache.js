75da7fab68c24a2c1666aaa41b3f7b6f1347e91d
Dario Russo <dario.russo@bbc.co.uk>
2022-03-25 09:21:32 +0000

    fix: adds cache max fallback

--- src/app/lib/utilities/getToggles/withCache.js
+++ src/app/lib/utilities/getToggles/withCache.js
@@ -1,7 +1,11 @@
 import Cache from 'lru-cache';
 import getToggles from '.';
 
-const cacheMaxItems = parseInt(process.env.SIMORGH_CONFIG_CACHE_ITEMS, 10);
+const CACHE_MAX_ITEMS_FALLBACK = 100;
+const cacheMaxItems = parseInt(
+  process.env.SIMORGH_CONFIG_CACHE_ITEMS || CACHE_MAX_ITEMS_FALLBACK,
+  10,
+);
 const cacheMaxAge = parseInt(
   process.env.SIMORGH_CONFIG_CACHE_MAX_AGE_SECONDS,
   10,
