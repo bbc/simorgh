e6491cb6102fc3a1fc76e7aeba454a543d8b01c0
Aaron Moore <aaron.moore@bbc.co.uk>
2022-04-11 13:32:03 +0100

    High impact promo go Live tidy up

--- src/app/containers/CpsFeaturesAnalysis/index.jsx
+++ src/app/containers/CpsFeaturesAnalysis/index.jsx
@@ -2,14 +2,10 @@ import React, { useContext } from 'react';
 import { arrayOf, shape, number, oneOf, oneOfType, string } from 'prop-types';
 import pathOr from 'ramda/src/pathOr';
 import { StoryPromoLi, StoryPromoUl } from '@bbc/psammead-story-promo-list';
-// import { OptimizelyContext } from '@optimizely/react-sdk';
 import { storyItem, linkPromo } from '#models/propTypes/storyItem';
 import { ServiceContext } from '#contexts/ServiceContext';
 import useViewTracker from '#hooks/useViewTracker';
-// import useOptimizelyVariation from '#hooks/useOptimizelyVariation';
-import isLive from '#lib/utilities/isLive';
 import CpsOnwardJourney from '../CpsOnwardJourney';
-import StoryPromo from '../StoryPromo';
 import FrostedGlassPromo from '../../components/FrostedGlassPromo/lazy';
 
 const eventTrackingData = {
@@ -18,59 +14,17 @@ const eventTrackingData = {
   },
 };
 
-// const IMPROVED_PROMO_EXPERIMENT_ID = 'improved_promos';
-const IMPROVED_PROMO_VARIATIONS = {
-  variation_1: props => (
-    <FrostedGlassPromo {...props} minimumContrast={10} paletteSize={10} />
-  ),
-  variation_2: props => (
-    <FrostedGlassPromo {...props} minimumContrast={10} paletteSize={10} />
-  ),
-  variation_3: props => (
-    <FrostedGlassPromo {...props} minimumContrast={7} paletteSize={20} />
-  ),
-  variation_4: props => (
-    <FrostedGlassPromo {...props} minimumContrast={7} paletteSize={20} />
-  ),
-  Control: StoryPromo,
-};
-
 const PromoListComponent = ({ promoItems, dir }) => {
   const { serviceDatetimeLocale } = useContext(ServiceContext);
-  // const { optimizely } = useContext(OptimizelyContext);
-  const promoVariation = isLive() ? 'Control' : 'variation_2';
-
-  // const promoVariation = useOptimizelyVariation(IMPROVED_PROMO_EXPERIMENT_ID);
-  // const hasVariationKey = promoVariation !== null;
-  // const eventTrackingData = getEventTrackingData(hasVariationKey && optimizely);
 
   const viewRef = useViewTracker(eventTrackingData.block);
-
-  const selectComponent = index => {
-    switch (true) {
-      case promoVariation === 'variation_1' && index === 0:
-        return IMPROVED_PROMO_VARIATIONS.variation_1;
-      case promoVariation === 'variation_2':
-        return IMPROVED_PROMO_VARIATIONS.variation_2;
-      case promoVariation === 'variation_3' && index === 0:
-        return IMPROVED_PROMO_VARIATIONS.variation_3;
-      case promoVariation === 'variation_4':
-        return IMPROVED_PROMO_VARIATIONS.variation_4;
-      case promoVariation === 'Control':
-        return IMPROVED_PROMO_VARIATIONS.Control;
-      default:
-        return IMPROVED_PROMO_VARIATIONS.Control;
-    }
-  };
 
   return (
     <StoryPromoUl>
       {promoItems.map((item, promoIndex) => {
-        const StoryPromoComponent = selectComponent(promoIndex);
-
         return (
           <StoryPromoLi key={item.id || item.uri} ref={viewRef}>
-            <StoryPromoComponent
+            <FrostedGlassPromo
               item={item}
               index={promoIndex}
               dir={dir}
@@ -99,36 +53,12 @@ PromoListComponent.defaultProps = {
 
 const PromoComponent = ({ promo, dir }) => {
   const { serviceDatetimeLocale } = useContext(ServiceContext);
-  // const { optimizely } = useContext(OptimizelyContext);
-  const promoVariation = isLive() ? 'Control' : 'variation_2';
-
-  // const promoVariation = useOptimizelyVariation(IMPROVED_PROMO_EXPERIMENT_ID);
-  // const hasVariationKey = promoVariation !== null;
-  // const eventTrackingData = getEventTrackingData(hasVariationKey && optimizely);
 
   const viewRef = useViewTracker(eventTrackingData);
 
-  const selectComponent = () => {
-    switch (true) {
-      case [
-        'variation_1',
-        'variation_2',
-        'variation_3',
-        'variation_4',
-      ].includes(promoVariation):
-        return IMPROVED_PROMO_VARIATIONS[promoVariation];
-      case promoVariation === 'Control':
-        return IMPROVED_PROMO_VARIATIONS.Control;
-      default:
-        return IMPROVED_PROMO_VARIATIONS.Control;
-    }
-  };
-
-  const StoryPromoComponent = selectComponent();
-
   return (
     <div ref={viewRef}>
-      <StoryPromoComponent
+      <FrostedGlassPromo
         item={promo}
         dir={dir}
         displayImage
