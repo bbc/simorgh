7b845fa2098a908a1b0f0e12be6e487b77056666
Dario Russo <dario.russo@bbc.co.uk>
2022-04-12 16:22:15 +0100

    feat: external links for urdu podcast

---
+++ src/app/routes/onDemandAudio/tempData/podcastExternalLinks/urdu.js
@@ -0,0 +1,12 @@
+const externalLinks = {
+  default: {
+    p0c04t7w: [
+      {
+        linkText: 'Spotify',
+        linkUrl: 'https://open.spotify.com/show/1eqTGd8QFThO8jOOl7zWoY',
+      },
+    ],
+  },
+};
+
+export default externalLinks;
