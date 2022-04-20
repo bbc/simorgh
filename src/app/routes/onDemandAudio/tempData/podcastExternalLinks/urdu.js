637111b628266941e0c1c6d7db302124bb153ab9
jordit <jordi.texido@yahoo.com>
2022-04-20 16:43:26 +0100

    Change Arabic radio module title and add external link to Urdu podcast

--- src/app/routes/onDemandAudio/tempData/podcastExternalLinks/urdu.js
+++ src/app/routes/onDemandAudio/tempData/podcastExternalLinks/urdu.js
@@ -11,6 +11,11 @@ const externalLinks = {
           'https://podcasts.apple.com/gb/podcast/%DA%88%D8%B1%D8%A7%D9%85%DB%81-%DA%A9%D9%88%D8%A6%DB%8C%D9%86-drama-queen/id1618467017',
       },
       {
+        linkText: 'Patari',
+        linkUrl:
+          'https://patari.pk/home/song/625a05eb4950df5b58b97a4a',
+      },
+      {
         linkText: 'اپنے دل کی بات ہمارے ساتھ شیئر کیجیے',
         linkUrl: 'https://www.bbc.com/urdu/send/u106609198',
       },
