1b04432dafc15dce894d3114a725b1887b68c09a
LilyL0u <lily.higham@bbc.co.uk>
2022-04-25 16:29:17 +0100

    comments added

--- cypress/integration/pages/topicPage/tests.js
+++ cypress/integration/pages/topicPage/tests.js
@@ -30,8 +30,10 @@ export default ({ service, pageType, variant, isAmp }) => {
         `https://web-cdn.api.bbci.co.uk/fd/simorgh-bff?id=${topicId}&service=${service}`,
       ).then(({ body }) => {
         cy.log(body);
+        // Gets expected number of articles in the topic on this page
         const numberOfItems = body.data.summaries.length;
         cy.log(numberOfItems);
+        // Checks number of items on page
         cy.get('main > div > ul')
           .children()
           .its('length')
@@ -42,8 +44,10 @@ export default ({ service, pageType, variant, isAmp }) => {
       cy.request(
         `https://web-cdn.api.bbci.co.uk/fd/simorgh-bff?id=${topicId}&service=${service}`,
       ).then(({ body }) => {
+        // Gets number of pages
         const { pageCount } = body.data;
         cy.log(`pagecount is ${pageCount}`);
+        // Checks pagination only is on page if there is more than one page
         if (pageCount > 1) {
           cy.get('[data-testid="topic-pagination"]').should('exist');
         } else {
@@ -55,7 +59,9 @@ export default ({ service, pageType, variant, isAmp }) => {
       cy.request(
         `https://web-cdn.api.bbci.co.uk/fd/simorgh-bff?id=${topicId}&service=${service}`,
       ).then(({ body }) => {
+        // Gets number of pages
         const { pageCount } = body.data;
+        // Gets last pagination element and checks the number is the length of pageCount
         if (pageCount > 1) {
           cy.log(`pagecount is ${pageCount}`);
           cy.get('[data-testid="topic-pagination"] > ul > li')
@@ -70,8 +76,10 @@ export default ({ service, pageType, variant, isAmp }) => {
       cy.request(
         `https://web-cdn.api.bbci.co.uk/fd/simorgh-bff?id=${topicId}&service=${service}`,
       ).then(({ body }) => {
+        // Gets the headline from 'title' of first item
         const firstItemHeadline = body.data.summaries[0].title;
         cy.log(firstItemHeadline);
+        // Goes down into the first item's h2 text and compares to title
         cy.get('main > div > ul')
           .children()
           .first()
@@ -86,6 +94,7 @@ export default ({ service, pageType, variant, isAmp }) => {
     });
     it('Clicking the first item should navigate to the correct page (goes to live article)', () => {
       let href;
+      // Goes down into the first item's href
       cy.get('main > div > ul')
         .children()
         .first()
@@ -98,6 +107,7 @@ export default ({ service, pageType, variant, isAmp }) => {
                 .then($href => {
                   href = $href;
                   cy.log(href);
+                  // Clicks the first item, then checks the page navigates to has the expected url
                   cy.get('a').click();
                   cy.url().should('eq', href);
                 });
