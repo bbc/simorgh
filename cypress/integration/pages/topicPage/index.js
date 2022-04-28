f33a77aafb254ca792026c7570991005d629f78c
LilyL0u <lily.higham@bbc.co.uk>
2022-04-28 16:12:17 +0100

    disable local tests

--- cypress/integration/pages/topicPage/index.js
+++ cypress/integration/pages/topicPage/index.js
@@ -1,6 +1,7 @@
 import config from '../../../support/config/services';
 import getPaths from '../../../support/helpers/getPaths';
 import serviceHasPageType from '../../../support/helpers/serviceHasPageType';
+
 import crossPlatformTests from './tests';
 import visitPage from '../../../support/helpers/visitPage';
 import { overrideRendererOnTest } from '../../../support/helpers/onDemandRadioTv';
