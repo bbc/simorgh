8fad4bd174ab5936c2786358e08afc0825953bce
pvaliani <pedram.homayoun-valiani@bbc.co.uk>
2022-05-03 16:15:58 +0100

    fixes psammead-heading-index migration error

--- src/app/containers/IndexHeading/index.jsx
+++ src/app/containers/IndexHeading/index.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import styled from '@emotion/styled';
 import { node } from 'prop-types';
-import IndexHeading from '@bbc/psammead-heading-index/src';
+import IndexHeading from '#legacy/psammead-heading-index/src';
 import {
   GEL_GROUP_3_SCREEN_WIDTH_MIN,
   GEL_GROUP_3_SCREEN_WIDTH_MAX,
