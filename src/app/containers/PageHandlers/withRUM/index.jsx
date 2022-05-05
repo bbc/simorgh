a5fbaa1184f8500af070d8ae2e9889fc60aabba6
Ryan McCombe <ryanmccombe3@gmail.com>
2022-05-05 09:30:04 +0100

    Disable on amp

--- src/app/containers/PageHandlers/withRUM/index.jsx
+++ src/app/containers/PageHandlers/withRUM/index.jsx
@@ -33,7 +33,7 @@ const buildScript = ({
 
 const RUMLoader = Component => {
   const withRum = props => {
-    if (isLive()) return <Component {...props} />;
+    if (isLive() || props.isAmp) return <Component {...props} />;
 
     const testConfig = {
       applicationId: '0007b574-fe46-4f8d-94d3-fe1c1a375af6',
