7845838bb50de813912d2df5cf0c795eb9da05a4
jordit <jordi.texido@yahoo.com>
2022-05-13 08:02:56 +0100

    Update Persian podcast promo and add third party link to radio podcast

--- src/app/routes/onDemandAudio/tempData/podcastExternalLinks/persian.js
+++ src/app/routes/onDemandAudio/tempData/podcastExternalLinks/persian.js
@@ -39,8 +39,7 @@ const externalLinks = {
       },
       {
         linkText: 'Apple',
-        linkUrl:
-          'https://podcasts.apple.com/gb/podcast/%D8%AF%D8%A7%D8%B3%D8%AA%D8%A7%D9%86-%D8%A7%D9%86%D9%82%D9%84%D8%A7%D8%A8-%D8%A8%D9%87-%D8%B1%D9%88%D8%A7%DB%8C%D8%AA-%D8%A8%DB%8C-%D8%A8%DB%8C-%D8%B3%DB%8C/id1451707438',
+        linkUrl: 'https://podcasts.apple.com/gb/podcast/%D8%AF%D8%A7%D8%B3%D8%AA%D8%A7%D9%86-%D8%A7%D9%86%D9%82%D9%84%D8%A7%D8%A8-%D8%A8%D9%87-%D8%B1%D9%88%D8%A7%DB%8C%D8%AA-%D8%A8%DB%8C-%D8%A8%DB%8C-%D8%B3%DB%8C/id1451707438',
       },
     ],
     p02pc9mc: [
@@ -53,6 +52,10 @@ const externalLinks = {
         linkUrl:
           'https://podcasts.apple.com/gb/podcast/bbc-persian-radio/id262741509',
       },
+      {
+        linkText: 'Castbox',
+        linkUrl: 'https://castbox.fm/channel/رادیو-فارسی-بیبیسی-id574405?utm_source=website&utm_medium=dlink&utm_campaign=web_share&utm_content=%D8%B1%D8%A7%D8%AF%DB%8C%D9%88%20%D9%81%D8%A7%D8%B1%D8%B3%DB%8C%20%D8%A8%DB%8C%E2%80%8C%D8%A8%DB%8C%E2%80%8C%D8%B3%DB%8C-CastBox_FM',
+      },
     ],
     p09v5lgp: [
       {
@@ -61,8 +64,7 @@ const externalLinks = {
       },
       {
         linkText: 'Apple',
-        linkUrl:
-          'https://podcasts.apple.com/gb/podcast/%D8%A8%DB%8C%D9%86-%D8%B3%D8%B7%D9%88%D8%B1/id1584665242',
+        linkUrl: 'https://podcasts.apple.com/gb/podcast/%D8%A8%DB%8C%D9%86-%D8%B3%D8%B7%D9%88%D8%B1/id1584665242',
       },
     ],
     p0bw80rj: [
@@ -76,8 +78,7 @@ const externalLinks = {
       },
       {
         linkText: 'Castbox',
-        linkUrl:
-          'https://castbox.fm/channel/%D8%B4%DB%8C%D8%B1%D8%A7%D8%B2%D9%87-id4841117?country=us',
+        linkUrl: 'https://castbox.fm/channel/%D8%B4%DB%8C%D8%B1%D8%A7%D8%B2%D9%87-id4841117?country=us',
       },
     ],
   },
