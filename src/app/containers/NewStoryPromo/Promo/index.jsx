119a746203f18e1a0f8ad38dc3ec2236f593b2c6
Dario Russo <dario.russo@bbc.co.uk>
2022-06-30 11:41:57 +0100

    PromoContext for script service and dir with minor style bug

--- src/app/containers/NewStoryPromo/Promo/index.jsx
+++ src/app/containers/NewStoryPromo/Promo/index.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { node } from 'prop-types';
+import React, { useContext } from 'react';
+import { node, string } from 'prop-types';
+import { ServiceContext } from '#contexts/ServiceContext';
 import Timestamp from '#components/Promo/timestamp';
 import SectionLabel from '#legacy/psammead-section-label/src';
 import Content from '#app/containers/StoryPromo/LinkContents';
 import LiveLabel from '#legacy/psammead-live-label/src';
-import H3 from '../PromoAtoms/Headings.styles';
+import Headings from '../PromoAtoms/Headings.styles';
 import {
   ListItem,
   OrderedList,
@@ -15,25 +16,44 @@ import Link from '../PromoAtoms/Link.styles';
 import MediaIndicator from '../PromoAtoms/MediaIndicator';
 import BoxWrapper from '../PromoAtoms/BoxWrapper.styles';
 
-const Promo = ({ children, Wrapper }) => {
-  return <Wrapper role="region">{children}</Wrapper>;
+const PromoContext = React.createContext({});
+
+const Promo = ({ children }) => {
+  const { script, service, dir } = useContext(ServiceContext);
+  return (
+    <PromoContext.Provider value={{ script, service, dir }}>
+      {children}
+    </PromoContext.Provider>
+  );
 };
 
-Promo.BoxWrapper = BoxWrapper;
-Promo.Heading = H3;
-Promo.Image = Image;
-Promo.MediaIndicator = MediaIndicator;
-Promo.Link = Link;
-Promo.ListItem = ListItem;
-Promo.OrderedList = OrderedList;
-Promo.UnorderedList = UnorderedList;
+const withPromoContext = Component => props =>
+  (
+    <PromoContext.Consumer>
+      {context => <Component {...context} {...props} />}
+    </PromoContext.Consumer>
+  );
+
+Promo.BoxWrapper = withPromoContext(BoxWrapper);
+Promo.Heading = withPromoContext(Headings);
+Promo.Image = withPromoContext(Image);
+Promo.MediaIndicator = withPromoContext(MediaIndicator);
+Promo.Link = withPromoContext(Link);
+Promo.ListItem = withPromoContext(ListItem);
+Promo.OrderedList = withPromoContext(OrderedList);
+Promo.UnorderedList = withPromoContext(UnorderedList);
 
 // Curretly Outside NewStoryPromo as reusable
-Promo.Timestamp = Timestamp;
-Promo.SectionLabel = SectionLabel;
-Promo.Content = Content;
-Promo.LiveLabel = LiveLabel;
+Promo.Timestamp = withPromoContext(Timestamp);
+Promo.SectionLabel = withPromoContext(SectionLabel);
+Promo.Content = withPromoContext(Content);
+Promo.LiveLabel = withPromoContext(LiveLabel);
 
-Promo.propTypes = { children: node.isRequired, Wrapper: node.isRequired };
+Promo.propTypes = {
+  children: node.isRequired,
+  labelId: string.isRequired,
+  to: string.isRequired,
+  id: string.isRequired,
+};
 
 export default Promo;
