55af533d78da7b92827e5f55ce440ff179810788
Dario Russo <dario.russo@bbc.co.uk>
2022-07-12 10:59:04 +0100

    fix: a11y ids causing 500

--- src/app/pages/ArticlePage/PagePromoSections/utility/index.js
+++ src/app/pages/ArticlePage/PagePromoSections/utility/index.js
@@ -1,10 +1,4 @@
-export default ({
-  sectionType,
-  assetUri,
-  uri,
-  contentType,
-  promoIndex = 0,
-}) => {
+export default (sectionType, assetUri, uri, contentType, promoIndex = 0) => {
   const asset = assetUri || uri;
   const assetParts = asset.split(/www\.bbc\.(co\.uk|com)/);
   const assetId = assetParts[assetParts.length - 1].replace(/\W/g, '');
