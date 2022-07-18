197dbc32e838833949f999854e4d6b4be1445e36
Dario Russo <dario.russo@bbc.co.uk>
2022-07-18 11:37:25 +0100

    standards: image

--- src/app/components/OptimoPromos/Image/index.jsx
+++ src/app/components/OptimoPromos/Image/index.jsx
@@ -4,14 +4,15 @@ import ImageWithPlaceholder from '#app/containers/ImageWithPlaceholder';
 import StyledImage from './index.styles';
 
 const Image = ({ src, altText, srcset, fallbackSrcset, width, height }) => {
-  const RATIO = 56.25;
+  const ASPECT_RATIO = (9 / 16) * 100;
+
   return (
     <ImageWithPlaceholder
       src={src}
       alt={altText}
       srcset={srcset}
       fallbackSrcset={fallbackSrcset}
-      ratio={RATIO}
+      ratio={ASPECT_RATIO}
       width={width}
       height={height}
       lazyload
--- src/app/components/OptimoPromos/Image/index.styles.jsx
+++ src/app/components/OptimoPromos/Image/index.styles.jsx
@@ -3,6 +3,5 @@ import Image from '#legacy/psammead-image/src';
 
 export default styled(Image)`
   aspect-ratio: 16 / 9;
-  object-fit: cover;
   height: auto;
 `;
