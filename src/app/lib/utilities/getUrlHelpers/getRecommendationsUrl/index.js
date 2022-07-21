e01359fa8576ddfcb3097c9d4437a3cb87e11513
Harvey Peachey <harvey.peachey@bbc.co.uk>
2022-07-21 12:25:02 +0100

    adds minor code spacing

--- src/app/lib/utilities/getUrlHelpers/getRecommendationsUrl/index.js
+++ src/app/lib/utilities/getUrlHelpers/getRecommendationsUrl/index.js
@@ -1,10 +1,12 @@
 export default ({ assetUri, engine, engineVariant }) => {
   let queryParams = '';
+
   if (engine) {
     queryParams += `?Engine=${engine}`;
     if (engineVariant) {
       queryParams += `&EngineVariant=${engineVariant}`;
     }
   }
+
   return `${process.env.RECOMMENDATIONS_ENDPOINT}/recommendations${assetUri}${queryParams}`;
 };
