6814a2f038b76b7e77ad9f787206ee52e3f7cd09
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2022-07-21 15:39:29 +0100

    fix paths

--- src/app/pages/TopicPage/TopicDescription/index.jsx
+++ src/app/pages/TopicPage/TopicDescription/index.jsx
@@ -2,17 +2,17 @@ import React, { useContext } from 'react';
 import styled from '@emotion/styled';
 import { ServiceContext } from '#contexts/ServiceContext';
 import { string } from 'prop-types';
-import { getSansRegular } from '#legacy/psammead-styles/src/font-styles';
-import { C_GREY_10 } from '#legacy/psammead-styles/src/colours';
+import { getSansRegular } from '#psammead/psammead-styles/src/font-styles';
+import { C_GREY_10 } from '#psammead/psammead-styles/src/colours';
 import {
   GEL_GROUP_3_SCREEN_WIDTH_MIN,
   GEL_GROUP_4_SCREEN_WIDTH_MIN,
-} from '#legacy/gel-foundations/src/breakpoints';
+} from '#psammead/gel-foundations/src/breakpoints';
 import {
   GEL_SPACING,
   GEL_SPACING_DBL,
   GEL_SPACING_TRPL,
-} from '#legacy/gel-foundations/src/spacings';
+} from '#psammead/gel-foundations/src/spacings';
 
 const Paragraph = styled.p`
   ${({ service }) => getSansRegular(service)}
