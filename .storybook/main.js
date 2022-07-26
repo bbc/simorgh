8de65fb8294b3258047357743961551504099c31
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2022-07-26 11:57:16 +0100

    storybook ignore entire psammead directory

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,7 +9,7 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
-  stories: ['../src/app/**/!(**psammead)/*.stories.jsx'],
+  stories: ['../src/app/**/!(psammead)/*.stories.jsx'],
   addons: [
     '@storybook/addon-knobs',
     '@storybook/addon-a11y',
