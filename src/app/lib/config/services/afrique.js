e4a838d630df1882ad94c9a95ce1339c1ef4586a
Ryan McCombe <ryanmccombe3@gmail.com>
2022-07-31 16:52:52 +0100

    Fix afrique logo

--- src/app/lib/config/services/afrique.js
+++ src/app/lib/config/services/afrique.js
@@ -5,7 +5,7 @@ import {
   C_POSTBOX_30,
 } from '#psammead/psammead-styles/src/colours';
 import latinDiacritics from '#psammead/gel-foundations/src/scripts/latin-with-diacritics';
-import brandSVG from '#lib/chameleonLogos/afaanoromoo';
+import brandSVG from '#lib/chameleonLogos/afrique';
 import '#psammead/moment-timezone-include/tz/GMT';
 import withContext from '../../../contexts/utils/withContext';
 import 'moment/locale/fr';
