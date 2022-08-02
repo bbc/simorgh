2bb4d7775ace667013679fbd42d373942480c223
Aaron Moore <aaron.moore@bbc.co.uk>
2022-08-02 09:50:49 +0100

    Test setting `x-frame-options` and `Cross Domain` headers for Article embeds

--- src/server/index.jsx
+++ src/server/index.jsx
@@ -18,6 +18,7 @@ import {
 } from '#lib/logger.const';
 import getToggles from '#app/lib/utilities/getToggles/withCache';
 import { OK } from '#lib/statusCodes.const';
+import isLive from '#lib/utilities/isLive';
 import injectCspHeader from './utilities/cspHeader';
 import logResponseTime from './utilities/logResponseTime';
 import renderDocument from './Document';
@@ -130,10 +131,23 @@ const injectDefaultCacheHeader = (req, res, next) => {
   next();
 };
 
+const injectTestEmbedHeaderControl = (req, res, next) => {
+  if (!isLive()) {
+    res
+      .set('x-frame-options', 'SAMEORIGIN')
+      .removeHeader('X-Permitted-Cross-Domain-Policies');
+  }
+  next();
+};
+
 // Catch all for all routes
 server.get(
   '/*',
-  [injectCspHeaderProdBuild, injectDefaultCacheHeader],
+  [
+    injectCspHeaderProdBuild,
+    injectDefaultCacheHeader,
+    injectTestEmbedHeaderControl,
+  ],
   async ({ url, query, headers, path: urlPath }, res) => {
     logger.info(SERVER_SIDE_RENDER_REQUEST_RECEIVED, {
       url,
