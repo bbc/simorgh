da36c3b0472855d63f980109804675278456a6c0
Harvey Peachey <harvey.peachey@bbc.co.uk>
2022-08-03 17:04:03 +0100

    removes unecessary promise chaining

--- src/app/lib/utilities/activateExperiment/index.js
+++ src/app/lib/utilities/activateExperiment/index.js
@@ -2,12 +2,11 @@ import onClient from '#lib/utilities/onClient';
 
 const activateExperiment = async (optimizely, experimentName, variation) => {
   if (onClient()) {
-    await optimizely.onReady().then(({ success }) => {
-      if (success) {
-        optimizely.setForcedVariation(experimentName, variation);
-        optimizely.activate(experimentName);
-      }
-    });
+    const success = await optimizely.onReady();
+    if (success) {
+      optimizely.setForcedVariation(experimentName, variation);
+      optimizely.activate(experimentName);
+    }
   }
 };
 
