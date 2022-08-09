5887943c62ededfde69513a56a0da0e30230f881
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2022-08-09 12:21:17 +0100

    change const QUADRIPLE to QUADRUPLE
    Co-authored-by: ExcitedBlob <99000097+ExcitedBlob@users.noreply.github.com>

--- src/app/components/ThemeProvider/spacings.ts
+++ src/app/components/ThemeProvider/spacings.ts
@@ -3,4 +3,4 @@ import pixelsToRem from '../../utilities/pixelsToRem';
 export const FULL = pixelsToRem(16);
 export const DOUBLE = FULL * 2;
 export const TRIPLE = FULL * 3;
-export const QUADRIPLE = FULL * 4;
+export const QUADRUPLE = FULL * 4;
