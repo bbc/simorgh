e2cf195ca09e1eb9eb0366bad9b1bf95e634660f
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2022-08-09 14:46:08 +0100

    add describe block in unit test

--- src/app/components/ThemeProvider/index.test.tsx
+++ src/app/components/ThemeProvider/index.test.tsx
@@ -17,22 +17,24 @@ const Fixture = () => {
   );
 };
 
-it('should provide the theme to components', async () => {
-  let container;
+describe('ThemeProvider', () => {
+  it('should provide the theme to components', async () => {
+    let container;
 
-  await act(async () => {
-    ({ container } = render(
-      <ThemeProvider service="news">
-        <Fixture />
-      </ThemeProvider>,
-    ));
+    await act(async () => {
+      ({ container } = render(
+        <ThemeProvider service="news">
+          <Fixture />
+        </ThemeProvider>,
+      ));
+    });
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <div
+          style="background-color: rgb(184, 0, 0); color: rgb(0, 0, 0); padding: 2rem;"
+        />
+      </div>
+    `);
   });
-
-  expect(container).toMatchInlineSnapshot(`
-    <div>
-      <div
-        style="background-color: rgb(184, 0, 0); color: rgb(0, 0, 0); padding: 2rem;"
-      />
-    </div>
-  `);
 });
