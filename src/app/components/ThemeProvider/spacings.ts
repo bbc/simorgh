0f59415b4f1e9eacbf947d0919293111f3b3d497
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2022-08-09 12:34:46 +0100

    use correct GEL spacing value

--- src/app/components/ThemeProvider/spacings.ts
+++ src/app/components/ThemeProvider/spacings.ts
@@ -1,6 +1,6 @@
 import pixelsToRem from '../../utilities/pixelsToRem';
 
-export const FULL = pixelsToRem(16);
+export const FULL = pixelsToRem(8);
 export const DOUBLE = FULL * 2;
 export const TRIPLE = FULL * 3;
 export const QUADRUPLE = FULL * 4;
