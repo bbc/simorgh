19a9a72ac164cda0e17659832f87dfed08fe3e84
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2022-08-09 16:08:57 +0100

    Revert "fix ie11 storybook failure"
    This reverts commit da12b52f6528a750491ed6a3657380f2cc0bea7b.

--- .eslintrc.js
+++ .eslintrc.js
@@ -59,6 +59,7 @@ module.exports = {
       rules: {
         'react/prop-types': 'off',
         'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
         'react/jsx-filename-extension': [
           2,
           {
--- src/app/components/ThemeProvider/withThemeProvider.tsx
+++ src/app/components/ThemeProvider/withThemeProvider.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
 import mergeDeepLeft from 'ramda/src/mergeDeepLeft';
 
