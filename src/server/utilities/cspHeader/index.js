d1036e4078755ada6eed0876e4c325ef1b6f0547
emilysaffron <emily.saffron@bbc.co.uk>
2022-08-09 16:20:25 +0100

    update webvitals monitoring link

--- src/server/utilities/cspHeader/index.js
+++ src/server/utilities/cspHeader/index.js
@@ -97,7 +97,7 @@ const directives = {
       'https://*.akamaihd.net',
       'https://cdn.optimizely.com/',
       'https://logx.optimizely.com/',
-      'https://europe-west1-bbc-otg-traf-mgr-bq-prod-4591.cloudfunctions.net', // Web-Vitals monitoring
+      'https://ws.bbc-reporting-api.app', // Web-Vitals monitoring
       ...advertisingDirectives.connectSrc,
       "'self'",
     ],
@@ -119,7 +119,7 @@ const directives = {
       'https://logws1363.ati-host.net', // ATI
       'https://cdn.optimizely.com/',
       'https://logx.optimizely.com/',
-      'https://europe-west1-bbc-otg-traf-mgr-bq-dev-4105.cloudfunctions.net', // Web-Vitals monitoring
+      'https://ws.bbc-reporting-api.app', // Web-Vitals monitoring
       'https://cognito-identity.eu-west-1.amazonaws.com/', // CloudWatch RUM
       ...advertisingDirectives.connectSrc,
       "'self'",
