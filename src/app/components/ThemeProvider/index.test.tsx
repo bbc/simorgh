e4a5fe8d17e6b8d06f029a82354d81f3f69f1e29
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2022-08-10 08:46:13 +0100

    more representative unit test

--- src/app/components/ThemeProvider/index.test.tsx
+++ src/app/components/ThemeProvider/index.test.tsx
@@ -1,18 +1,21 @@
-import { useTheme } from '@emotion/react';
 import { render, act } from '@testing-library/react';
+import { css, Theme } from '@emotion/react';
 
 import ThemeProvider from '.';
 
 const Fixture = () => {
-  const { palette, spacings } = useTheme();
-
   return (
     <div
-      style={{
-        backgroundColor: palette.BRAND_BACKGROUND,
-        color: palette.BLACK,
-        padding: `${spacings.DOUBLE}rem`,
-      }}
+      css={({ spacings, mq, palette }: Theme) =>
+        css({
+          backgroundColor: palette.BRAND_BACKGROUND,
+          color: palette.WHITE,
+          padding: `${spacings.FULL}rem`,
+          [mq.GROUP_3_MIN_WIDTH]: {
+            padding: `${spacings.FULL}rem`,
+          },
+        })
+      }
     />
   );
 };
@@ -30,9 +33,21 @@ describe('ThemeProvider', () => {
     });
 
     expect(container).toMatchInlineSnapshot(`
+      .emotion-0 {
+        background-color: #B80000;
+        color: #FFFFFF;
+        padding: 0.5rem;
+      }
+
+      @media (min-width: 37.5rem) {
+        .emotion-0 {
+          padding: 0.5rem;
+        }
+      }
+
       <div>
         <div
-          style="background-color: rgb(184, 0, 0); color: rgb(0, 0, 0); padding: 1rem;"
+          class="emotion-0"
         />
       </div>
     `);
