012963bf2c98d1ab9c709b454baa75dfcf4d66b1
Dario Russo <dario.russo@bbc.co.uk>
2022-08-12 11:01:11 +0100

    chore: kickoff

---
+++ src/app/components/Bylines/index.test.tsx
@@ -0,0 +1,5 @@
+import Byline from './';
+
+describe('Byline', () => {
+  it('Should render Byline correctly when only required data is passed', () => {});
+});
---
+++ src/app/components/Bylines/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+type Props = {
+  author: string;
+  jobRole: string;
+  className: string;
+};
+
+const Byline = ({ author, jobRole, className }: Props) => {
+  return <div></div>;
+};
+
+Byline.propTypes = {};
+
+export default Byline;
