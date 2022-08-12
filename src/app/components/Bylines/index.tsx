36ab489dfd5de1c04d96ffe1167457f624e8d408
Dario Russo <dario.russo@bbc.co.uk>
2022-08-12 12:12:50 +0100

    feat: Byline template

--- src/app/components/Bylines/index.tsx
+++ src/app/components/Bylines/index.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import VisuallyHiddenText from '#psammead/psammead-visually-hidden-text/src';
+import { ServiceContext } from '#app/contexts/ServiceContext';
+import pathOr from 'ramda/src/pathOr';
 
 type Props = {
-  author: string;
+  authorName: string;
   jobRole: string;
   className: string;
+  timestamp: boolean;
 };
 
-const Byline = ({ author, jobRole, className }: Props) => {
-  return <div></div>;
+const Byline = ({ authorName, jobRole, className, timestamp }: Props) => {
+  const { translations } = useContext(ServiceContext) as {
+    translations: any;
+  };
+
+  const authorTranslated = pathOr('Reporting from', [], translations);
+  const jobRoleTranslated = pathOr('Role', [], translations);
+
+  return (
+    <>
+      <span role="text">
+        <VisuallyHiddenText>Author, </VisuallyHiddenText>
+        <span>{authorName}</span>
+      </span>
+      <span role="text">
+        <VisuallyHiddenText>Role, </VisuallyHiddenText>
+        <span>{jobRole}</span>
+      </span>
+    </>
+  );
 };
 
 Byline.propTypes = {};
