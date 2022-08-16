adc99810dfe51c001e7694c60c0d942b06d5b542
mfonofm <mfon.essien@bbc.co.uk>
2022-08-16 15:35:13 +0100

    feat: add example with timestamp to stories

--- src/app/pages/ArticlePage/Byline/index.stories.tsx
+++ src/app/pages/ArticlePage/Byline/index.stories.tsx
@@ -1,16 +1,177 @@
-import { ServiceContextProvider } from '#contexts/ServiceContext';
+import styled from '@emotion/styled';
+import { ServiceContextProvider } from '../../../contexts/ServiceContext';
+import Timestamp from '../../../legacy/containers/ArticleTimestamp';
 import Byline from '.';
 
 interface Props {
-  authorName: string;
-  jobRole: string;
   service: string;
+  children?: JSX.Element;
 }
 
-const Component = ({ authorName, jobRole, service }: Props) => {
+const data = [
+  {
+    type: 'contributor',
+    model: {
+      blocks: [
+        {
+          type: 'name',
+          model: {
+            blocks: [
+              {
+                type: 'text',
+                model: {
+                  blocks: [
+                    {
+                      type: 'paragraph',
+                      model: {
+                        text: 'Single Byline (all values)',
+                        blocks: [
+                          {
+                            type: 'fragment',
+                            model: {
+                              text: 'Single Byline (all values)',
+                              attributes: [],
+                            },
+                          },
+                        ],
+                      },
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+        {
+          type: 'role',
+          model: {
+            blocks: [
+              {
+                type: 'text',
+                model: {
+                  blocks: [
+                    {
+                      type: 'paragraph',
+                      model: {
+                        text: 'Test',
+                        blocks: [
+                          {
+                            type: 'fragment',
+                            model: {
+                              text: 'Test',
+                              attributes: [],
+                            },
+                          },
+                        ],
+                      },
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+        {
+          type: 'link',
+          model: {
+            blocks: [
+              {
+                type: 'text',
+                model: {
+                  blocks: [
+                    {
+                      type: 'paragraph',
+                      model: {
+                        text: 'test',
+                        blocks: [
+                          {
+                            type: 'urlLink',
+                            model: {
+                              text: 'test',
+                              locator: 'https://twitter.com/test',
+                              blocks: [
+                                {
+                                  type: 'fragment',
+                                  model: {
+                                    text: 'test',
+                                    attributes: [],
+                                  },
+                                },
+                              ],
+                              isExternal: true,
+                            },
+                          },
+                        ],
+                      },
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+        {
+          type: 'location',
+          model: {
+            blocks: [
+              {
+                type: 'text',
+                model: {
+                  blocks: [
+                    {
+                      type: 'paragraph',
+                      model: {
+                        text: 'London',
+                        blocks: [
+                          {
+                            type: 'fragment',
+                            model: {
+                              text: 'London',
+                              attributes: [],
+                            },
+                          },
+                        ],
+                      },
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+        {
+          type: 'image',
+          model: {
+            blocks: [
+              {
+                type: 'rawImage',
+                model: {
+                  width: 640,
+                  height: 562,
+                  locator: 'f2f8/test/49056060-fea9-11ec-8adf-7bf4017ee9f9.png',
+                  originCode: 'cpsdevpb',
+                },
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+];
+
+const StyledTimestamp = styled(Timestamp)`
+  li > span,
+  div {
+    padding: 0 !important;
+    color: red;
+  }
+`;
+
+const Component = ({ service, children }: Props) => {
   return (
     <ServiceContextProvider service={service}>
-      <Byline authorName={authorName} jobRole={jobRole} />
+      <Byline blocks={data}>{children}</Byline>
     </ServiceContextProvider>
   );
 };
@@ -21,10 +182,13 @@ export default {
   parameters: { chromatic: { disable: true } },
 };
 
-export const Example = () => (
-  <Component
-    authorName="Dario Russo"
-    jobRole="Software Engineer"
-    service="news"
-  />
+export const Example = () => <Component service="news" />;
+export const ExampleWithTimestamp = () => (
+  <Component service="news">
+    <StyledTimestamp
+      firstPublished={1660658887}
+      lastPublished={1660658887}
+      popOut={false}
+    />
+  </Component>
 );
