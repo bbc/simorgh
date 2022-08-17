dcfadbee5416e423801732a6ad78871639ad9d76
Ryan McCombe <ryanmccombe3@gmail.com>
2022-08-17 11:06:23 +0100

    Fix eslint warnings

--- src/app/pages/TopicPage/Curation/Subhead/index.stories.tsx
+++ src/app/pages/TopicPage/Curation/Subhead/index.stories.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { ServiceContextProvider } from '../../../../contexts/ServiceContext';
 import { withServicesKnob } from '../../../../legacy/psammead/psammead-storybook-helpers/src';
