9abee85be2f1fd4ee98e595ff1f1855257d93ab1
Aaron Moore <aaron.moore@bbc.co.uk>
2022-08-19 16:31:21 +0100

    Update index.tsx

--- src/app/contexts/ServiceContext/index.tsx
+++ src/app/contexts/ServiceContext/index.tsx
@@ -1,13 +1,11 @@
-import React, { PropsWithChildren } from 'react';
+import React, { createContext, PropsWithChildren } from 'react';
 import services from '../../lib/config/services/loadableConfig';
 import { getVariant } from '../../lib/utilities/variantHandler';
 import getLangOverride from '../../lib/utilities/langHandler';
 import { Services, Variants } from '../../models/types/global';
 import { ServiceConfig } from '../../models/types/serviceConfig';
 
-export const ServiceContext = React.createContext<ServiceConfig>(
-  {} as ServiceConfig,
-);
+export const ServiceContext = createContext<ServiceConfig>({} as ServiceConfig);
 
 interface Props {
   service: Services;
