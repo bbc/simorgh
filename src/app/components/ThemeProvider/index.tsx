b93d2719d88e5e835616492f8f9d12cf21003f39
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2022-08-23 13:03:20 +0100

    handle default theme for local error pages

--- src/app/components/ThemeProvider/index.tsx
+++ src/app/components/ThemeProvider/index.tsx
@@ -53,9 +53,22 @@ interface Props {
   variant: null | 'cyr' | 'lat' | 'simp' | 'trad';
 }
 
+// This is to support local 404 and 500 pages for services with variants.
+// For example, http://localhost:7080/serbian/500 does not include the variant in the path so we must define a default variant.
+// NB These routes do not exist on live. They are for dev and testing purposes.
+const defaultServiceVariants: { [index: string]: any } = {
+  serbian: 'cyr',
+  ukchina: 'simp',
+  zhongwen: 'simp',
+};
+
 const loadTheme = /* #__LOADABLE__ */ (props: Props) =>
   import(
-    `./themes/${props.service}${props.variant ? `/${props.variant}` : ''}`
+    `./themes/${props.service}${
+      props.variant
+        ? `/${props.variant}`
+        : `/${defaultServiceVariants[props.service]}` || ''
+    }`
   );
 
 const ThemeProvider: LoadableComponent<Props> = loadable(loadTheme);
