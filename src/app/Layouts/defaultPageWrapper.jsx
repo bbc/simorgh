832a80380f107d947847796bcc81fe06ae8511d1
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2022-08-23 16:04:24 +0100

    remove unused var

--- src/app/Layouts/defaultPageWrapper.jsx
+++ src/app/Layouts/defaultPageWrapper.jsx
@@ -14,7 +14,6 @@ import FooterContainer from '#containers/Footer';
 import ManifestContainer from '#containers/Manifest';
 import ServiceWorkerContainer from '#containers/ServiceWorker';
 import { ServiceContext } from '../contexts/ServiceContext';
-import { RequestContext } from '../contexts/RequestContext';
 import ThemeProvider from '../components/ThemeProvider';
 
 const Wrapper = styled.div`
