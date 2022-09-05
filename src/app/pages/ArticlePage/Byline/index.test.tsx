caaaa4c0d4ec40ae45a2085506c65d6b65fd7254
Harvey Peachey <harvey.peachey@bbc.co.uk>
2022-09-05 16:17:05 +0100

    simplifies test boilerplate

--- src/app/pages/ArticlePage/Byline/index.test.tsx
+++ src/app/pages/ArticlePage/Byline/index.test.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable no-template-curly-in-string */
-import React, { PropsWithChildren } from 'react';
-import { render, screen, act } from '@testing-library/react';
+import React from 'react';
+import {
+  render,
+  screen,
+} from '../../../components/react-testing-library-with-providers';
 import Byline from '.';
-import { ServiceContextProvider } from '../../../contexts/ServiceContext';
 import ArticleTimestamp from '../../../legacy/containers/ArticleTimestamp';
 import {
   bylineWithNoRole,
@@ -10,31 +12,10 @@ import {
   bylineWithNameAndRole,
   bylineWithLink,
 } from './fixture';
-import ThemeProvider from '../../../components/ThemeProvider';
-import { Services } from '../../../models/types/global';
-
-interface Props {
-  service: Services;
-  fixture: any;
-}
-
-const FixtureByline = ({
-  service,
-  fixture,
-  children,
-}: PropsWithChildren<Props>) => (
-  <ServiceContextProvider service={service}>
-    <ThemeProvider service={service}>
-      <Byline blocks={fixture}>{children}</Byline>
-    </ThemeProvider>
-  </ServiceContextProvider>
-);
 
 describe('Byline', () => {
-  it('Should render Byline correctly when only required data is passed', async () => {
-    await act(async () => {
-      render(<FixtureByline fixture={bylineWithNameAndRole} service="news" />);
-    });
+  it('Should render Byline correctly when only required data is passed', () => {
+    render(<Byline blocks={bylineWithNameAndRole} />);
 
     const author = screen.getByText('Single Byline (all values)');
     const role = screen.getByText('Test');
@@ -43,34 +24,20 @@ describe('Byline', () => {
     expect(role).toBeInTheDocument();
   });
 
-  it('Should return null when there is no role in the data', async () => {
-    let container;
-
-    await act(async () => {
-      ({ container } = render(
-        <FixtureByline fixture={bylineWithNoRole} service="news" />,
-      ));
-    });
+  it('Should return null when there is no role in the data', () => {
+    const { container } = render(<Byline blocks={bylineWithNoRole} />);
 
     expect(container).toBeEmptyDOMElement();
   });
 
-  it('Should return null when there is no author in the data', async () => {
-    let container;
-
-    await act(async () => {
-      ({ container } = render(
-        <FixtureByline fixture={bylineWithNoAuthor} service="news" />,
-      ));
-    });
+  it('Should return null when there is no author in the data', () => {
+    const { container } = render(<Byline blocks={bylineWithNoAuthor} />);
 
     expect(container).toBeEmptyDOMElement();
   });
 
-  it('should render Byline correctly when passed a Twitter Link', async () => {
-    await act(async () => {
-      render(<FixtureByline fixture={bylineWithLink} service="news" />);
-    });
+  it('should render Byline correctly when passed a Twitter Link', () => {
+    render(<Byline blocks={bylineWithLink} />);
 
     const AuthorLink = screen.getByText('Single Byline (all values)');
     const TwitterLink = screen.getByText('@test');
@@ -81,62 +48,56 @@ describe('Byline', () => {
     expect(Links.length).toBe(2);
   });
 
-  it('should render a section with role region', async () => {
-    await act(async () => {
-      render(<FixtureByline fixture={bylineWithNameAndRole} service="news" />);
-    });
+  it('should render a section with role region', () => {
+    render(<Byline blocks={bylineWithNameAndRole} />);
 
     const region = screen.getByRole('region');
+
     expect(region).toBeInTheDocument();
   });
 
-  it('should render a list when required data is passed correctly', async () => {
-    await act(async () => {
-      render(<FixtureByline fixture={bylineWithNameAndRole} service="news" />);
-    });
+  it('should render a list when required data is passed correctly', () => {
+    render(<Byline blocks={bylineWithNameAndRole} />);
 
     const list = screen.getByRole('list');
+
     expect(list).toBeInTheDocument();
   });
 
-  it('should render all listitems correctyle', async () => {
-    await act(async () => {
-      render(<FixtureByline fixture={bylineWithNameAndRole} service="news" />);
-    });
+  it('should render all listitems correctly', () => {
+    render(<Byline blocks={bylineWithNameAndRole} />);
 
     const listItems = screen.getAllByRole('listitem');
+
     expect(listItems.length).toBe(2);
   });
 
-  it('should correctly render Timestamp when passeed as a children', async () => {
-    await act(async () => {
-      render(
-        <FixtureByline fixture={bylineWithNameAndRole} service="news">
-          <ArticleTimestamp
-            firstPublished={1660658887}
-            lastPublished={1660658887}
-            popOut={false}
-          />
-        </FixtureByline>,
-      );
-    });
+  it('should correctly render Timestamp when passeed as a child', () => {
+    render(
+      <Byline blocks={bylineWithNameAndRole}>
+        <ArticleTimestamp
+          firstPublished={1660658887}
+          lastPublished={1660658887}
+          popOut={false}
+        />
+      </Byline>,
+    );
 
     const timestamp = screen.getByText('20 January 1970');
+
     expect(timestamp).toBeInTheDocument();
   });
 
-  it('should correctly render an extra listitem for Timestamp', async () => {
-    await act(async () => {
-      render(
-        <FixtureByline fixture={bylineWithNameAndRole} service="news">
-          <ArticleTimestamp
-            firstPublished={1660658887}
-            lastPublished={1660658887}
-            popOut={false}
-          />
-        </FixtureByline>,
-      );
-    });
+  it('should correctly render an extra listitem for Timestamp', () => {
+    render(
+      <Byline blocks={bylineWithNameAndRole}>
+        <ArticleTimestamp
+          firstPublished={1660658887}
+          lastPublished={1660658887}
+          popOut={false}
+        />
+      </Byline>,
+    );
 
     const listItems = screen.getAllByRole('listitem');
 
@@ -149,20 +110,19 @@ describe('Byline', () => {
     ${'Role'}      | ${'Role'}      | ${'Role,'}
     ${'Twitter'}   | ${'Twitter'}   | ${'Twitter,'}
     ${'Published'} | ${'Published'} | ${'Published,'}
-  `('should correctly announce $expectation for $info', async ({ text }) => {
-    await act(async () => {
-      render(
-        <FixtureByline fixture={bylineWithLink} service="news">
-          <ArticleTimestamp
-            firstPublished={1660658887}
-            lastPublished={1660658887}
-            popOut={false}
-          />
-        </FixtureByline>,
-      );
-    });
+  `('should correctly announce $expectation for $info', ({ text }) => {
+    render(
+      <Byline blocks={bylineWithLink}>
+        <ArticleTimestamp
+          firstPublished={1660658887}
+          lastPublished={1660658887}
+          popOut={false}
+        />
+      </Byline>,
+    );
 
     const findText = screen.getByText(text);
+
     expect(findText).toBeInTheDocument();
   });
 
@@ -171,23 +131,22 @@ describe('Byline', () => {
     ${'author'}    | ${'Barreessaa,'}
     ${'role'}      | ${'Gahee,'}
     ${'published'} | ${'Maxxanfame,'}
-  `(
-    'should translate $info announcement correctly',
-    async ({ translation }) => {
-      await act(async () => {
-        render(
-          <FixtureByline fixture={bylineWithLink} service="afaanoromoo">
-            <ArticleTimestamp
-              firstPublished={1660658887}
-              lastPublished={1660658887}
-              popOut={false}
-            />
-          </FixtureByline>,
-        );
-      });
+  `('should translate $info announcement correctly', ({ translation }) => {
+    render(
+      <Byline blocks={bylineWithLink}>
+        <ArticleTimestamp
+          firstPublished={1660658887}
+          lastPublished={1660658887}
+          popOut={false}
+        />
+      </Byline>,
+      {
+        service: 'afaanoromoo',
+      },
+    );
 
-      const findTranslation = screen.getByText(translation);
-      expect(findTranslation).toBeInTheDocument();
-    },
-  );
+    const findTranslation = screen.getByText(translation);
+
+    expect(findTranslation).toBeInTheDocument();
+  });
 });
