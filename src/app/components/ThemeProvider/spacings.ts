6ae15e2f9441019984ccbde402962d7ea75884c9
Harvey Peachey <harvey.peachey@bbc.co.uk>
2022-09-06 11:21:56 +0100

    Revert "renames and reorders half size"
    This reverts commit 30b86182a923519b379778371bae7de244a85e23.

--- src/app/components/ThemeProvider/spacings.ts
+++ src/app/components/ThemeProvider/spacings.ts
@@ -1,7 +1,8 @@
 import pixelsToRem from '../../utilities/pixelsToRem';
 
 export const FULL = pixelsToRem(8);
-export const HALF = FULL / 2;
 export const DOUBLE = FULL * 2;
 export const TRIPLE = FULL * 3;
 export const QUADRUPLE = FULL * 4;
+
+export const HLF = FULL / 2;
