5d5cfa767b4960c877190e184dec22c94cb9e6c3
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2022-09-07 08:41:07 +0100

    fix image story

--- src/app/components/Image/index.stories.tsx
+++ src/app/components/Image/index.stories.tsx
@@ -5,12 +5,11 @@ import Image from '.';
 
 const Component = () => {
   return (
-    <ThemeProvider service="mundo">
+    <ThemeProvider service="mundo" variant="default">
       <Image
         alt="orange 1"
         originCode="cpsdevpb"
         src="41BC/test/_63482861_orange1.jpg"
-        originalImageWidth={500}
         imageResolutions={[200, 500, 1000]}
         sizes="(max-width: 600px) 480px, 800px"
         width={500}
