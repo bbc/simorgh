cc18868cf440070109ca3557a07f6aac7e3fe028
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2022-09-07 10:12:16 +0100

    fix stories

--- src/app/components/Image/index.stories.tsx
+++ src/app/components/Image/index.stories.tsx
@@ -3,21 +3,21 @@ import ThemeProvider from '../ThemeProvider';
 
 import Image from '.';
 
-const Component = () => {
-  return (
-    <ThemeProvider service="mundo" variant="default">
-      <Image
-        alt="orange 1"
-        originCode="cpsdevpb"
-        src="41BC/test/_63482861_orange1.jpg"
-        imageResolutions={[200, 500, 1000]}
-        sizes="(max-width: 600px) 480px, 800px"
-        width={500}
-        height={281}
-      />
-    </ThemeProvider>
-  );
-};
+const Component = () => (
+  <ThemeProvider service="mundo" variant="default">
+    <Image
+      alt="orange 1"
+      src="41BC/test/_63482861_orange1.jpg"
+      primarySrcset="https://ichef.bbci.co.uk/news/200/cpsdevpb/41BC/test/_63482861_orange1.jpg.webp 200w, https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg.webp 500w"
+      primaryMimeType="image/webp"
+      fallbackSrcset="https://ichef.bbci.co.uk/news/200/cpsdevpb/41BC/test/_63482861_orange1.jpg 200w, https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg 500w"
+      fallbackMimeType="image/jpeg"
+      sizes="(max-width: 600px) 480px, 800px"
+      width={500}
+      height={281}
+    />
+  </ThemeProvider>
+);
 
 export default {
   title: 'New Components/Image',
