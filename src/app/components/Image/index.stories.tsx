1118b0b3768e50bf3d08bfc47fc8b1d94d78843e
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2022-09-14 11:08:46 +0100

    setup a11y testing

--- src/app/components/Image/index.stories.tsx
+++ src/app/components/Image/index.stories.tsx
@@ -2,87 +2,122 @@ import React from 'react';
 
 import AmpDecorator from '../../../../.storybook/helpers/ampDecorator';
 import ThemeProvider from '../ThemeProvider';
+import Heading from '../Heading';
 import Image from '.';
 
-export const BasicImage = () => (
-  <ThemeProvider service="mundo" variant="default">
-    <Image
-      alt="orange 1"
-      src="https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg"
-    />
-  </ThemeProvider>
+const BasicImage = () => (
+  <Image
+    alt="orange 1"
+    src="https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg"
+  />
 );
+
+const ResponsiveImage = () => (
+  <Image
+    alt="orange 1"
+    src="https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg"
+    srcSet="https://ichef.bbci.co.uk/news/200/cpsdevpb/41BC/test/_63482861_orange1.jpg 200w, https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg 500w"
+  />
+);
+
+const ResponsiveWebPWithJpegFallback = () => (
+  <Image
+    alt="orange 1"
+    src="https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg.webp"
+    srcSet="https://ichef.bbci.co.uk/news/200/cpsdevpb/41BC/test/_63482861_orange1.jpg.webp 200w, https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg.webp 500w"
+    mediaType="image/webp"
+    fallbackSrcSet="https://ichef.bbci.co.uk/news/200/cpsdevpb/41BC/test/_63482861_orange1.jpg 200w, https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg 500w"
+    fallbackMediaType="image/jpeg"
+  />
+);
+
+const BasicAMPImage = () => (
+  <Image
+    isAmp
+    alt="orange 1"
+    src="https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg"
+    width={500}
+    height={281}
+  />
+);
+
+const ResponsiveAMPImage = () => (
+  <Image
+    isAmp
+    alt="orange 1"
+    src="https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg"
+    srcSet="https://ichef.bbci.co.uk/news/200/cpsdevpb/41BC/test/_63482861_orange1.jpg 200w, https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg 500w"
+    width={500}
+    height={281}
+  />
+);
+
+const AMPWebPWithJpegFallback = () => (
+  <Image
+    isAmp
+    alt="orange 1"
+    src="https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg"
+    srcSet="https://ichef.bbci.co.uk/news/200/cpsdevpb/41BC/test/_63482861_orange1.jpg.webp 200w, https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg.webp 500w"
+    mediaType="image/webp"
+    fallbackSrcSet="https://ichef.bbci.co.uk/news/200/cpsdevpb/41BC/test/_63482861_orange1.jpg 200w, https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg 500w"
+    fallbackMediaType="image/jpeg"
+    width={500}
+    height={281}
+  />
+);
+
+export const ImageA11y = () => {
+  return (
+    <ThemeProvider service="mundo" variant="default">
+      <div style={{ paddingBottom: '1.5rem' }}>
+        <Heading level={1}>Image A11y Testing</Heading>
+      </div>
+      <div style={{ paddingBottom: '1.5rem' }}>
+        <Heading level={2}>Basic image (non-responsive)</Heading>
+        <BasicImage />
+      </div>
+      <div style={{ paddingBottom: '1.5rem' }}>
+        <Heading level={2}>Responsive image</Heading>
+        <ResponsiveImage />
+      </div>
+      <div style={{ paddingBottom: '1.5rem' }}>
+        <Heading level={2}>
+          Responsive webp image with jpeg fallback (uses picture element)
+        </Heading>
+        <ResponsiveWebPWithJpegFallback />
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export const AmpImageA11y = () => {
+  return (
+    <ThemeProvider service="mundo" variant="default">
+      <div style={{ paddingBottom: '1.5rem' }}>
+        <Heading level={1}>AMP Image A11y Testing</Heading>
+      </div>
+      <div style={{ paddingBottom: '1.5rem' }}>
+        <Heading level={2}>Basic image (non-responsive)</Heading>
+        <BasicAMPImage />
+      </div>
+      <div style={{ paddingBottom: '1.5rem' }}>
+        <Heading level={2}>Responsive image</Heading>
+        <ResponsiveAMPImage />
+      </div>
+      <div style={{ paddingBottom: '1.5rem' }}>
+        <Heading level={2}>
+          Responsive webp image with jpeg fallback (uses picture element)
+        </Heading>
+        <AMPWebPWithJpegFallback />
+      </div>
+    </ThemeProvider>
+  );
+};
+
+AmpImageA11y.decorators = [AmpDecorator];
 
 export default {
   title: 'New Components/Image',
-  Component: BasicImage,
+  Component: ImageA11y,
   parameters: { chromatic: { disable: true } },
 };
-
-export const ResponsiveImage = () => (
-  <ThemeProvider service="mundo" variant="default">
-    <Image
-      alt="orange 1"
-      src="https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg"
-      srcSet="https://ichef.bbci.co.uk/news/200/cpsdevpb/41BC/test/_63482861_orange1.jpg 200w, https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg 500w"
-    />
-  </ThemeProvider>
-);
-
-export const ResponsiveWebPWithJpegFallback = () => (
-  <ThemeProvider service="mundo" variant="default">
-    <Image
-      alt="orange 1"
-      src="https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg.webp"
-      srcSet="https://ichef.bbci.co.uk/news/200/cpsdevpb/41BC/test/_63482861_orange1.jpg.webp 200w, https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg.webp 500w"
-      mediaType="image/webp"
-      fallbackSrcSet="https://ichef.bbci.co.uk/news/200/cpsdevpb/41BC/test/_63482861_orange1.jpg 200w, https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg 500w"
-      fallbackMediaType="image/jpeg"
-    />
-  </ThemeProvider>
-);
-
-export const BasicAMPImage = () => (
-  <ThemeProvider service="mundo" variant="default">
-    <Image
-      isAmp
-      alt="orange 1"
-      src="https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg"
-      width={500}
-      height={281}
-    />
-  </ThemeProvider>
-);
-
-export const ResponsiveAMPImage = () => (
-  <ThemeProvider service="mundo" variant="default">
-    <Image
-      isAmp
-      alt="orange 1"
-      src="https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg"
-      srcSet="https://ichef.bbci.co.uk/news/200/cpsdevpb/41BC/test/_63482861_orange1.jpg 200w, https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg 500w"
-      width={500}
-      height={281}
-    />
-  </ThemeProvider>
-);
-
-export const AMPWebPWithJpegFallback = () => (
-  <ThemeProvider service="mundo" variant="default">
-    <Image
-      isAmp
-      alt="orange 1"
-      src="https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg"
-      srcSet="https://ichef.bbci.co.uk/news/200/cpsdevpb/41BC/test/_63482861_orange1.jpg.webp 200w, https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg.webp 500w"
-      mediaType="image/webp"
-      fallbackSrcSet="https://ichef.bbci.co.uk/news/200/cpsdevpb/41BC/test/_63482861_orange1.jpg 200w, https://ichef.bbci.co.uk/news/500/cpsdevpb/41BC/test/_63482861_orange1.jpg 500w"
-      fallbackMediaType="image/jpeg"
-      width={500}
-      height={281}
-    />
-  </ThemeProvider>
-);
-
-BasicAMPImage.decorators = [AmpDecorator];
-ResponsiveAMPImage.decorators = [AmpDecorator];
-AMPWebPWithJpegFallback.decorators = [AmpDecorator];
