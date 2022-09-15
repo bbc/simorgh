27b1ebc9506be92821b79fb42559108bedf32d4c
Dario Russo <dario.russo@bbc.co.uk>
2022-09-15 10:28:47 +0100

    fix: open-close principle

--- src/app/legacy/containers/CpsRecommendations/RecommendationsPromo/utility/index.ts
+++ src/app/legacy/containers/CpsRecommendations/RecommendationsPromo/utility/index.ts
@@ -1,47 +1,58 @@
-import pathOr from 'ramda/src/pathOr';
+import path from 'ramda/src/path';
+import hasPath from 'ramda/src/hasPath';
+import getOriginCode from '../../../../../lib/utilities/imageSrcHelpers/originCode';
+import getLocator from '../../../../../lib/utilities/imageSrcHelpers/locator';
 
-const optimoToCPSImage = ({ cpsImage }) => {
-  if (!cpsImage) return null;
-  const imageBlock = ['defaultPromoImage', 'blocks', 1, 'model'];
+const extractPromoData = ({ promo }) => {
+  if (!promo) return null;
 
-  const width = pathOr(null, imageBlock.concat(['width']), cpsImage);
-  const height = pathOr(null, imageBlock.concat(['height']), cpsImage);
-  const copyrightHolder = pathOr(
-    null,
-    imageBlock.concat(['copyrightHolder']),
-    cpsImage,
-  );
-  const optimoLocator = pathOr(null, imageBlock.concat(['locator']), cpsImage);
-  const optimoOriginCode = pathOr(
-    null,
-    imageBlock.concat(['originCode']),
-    cpsImage,
-  );
-  const altText = pathOr(
-    null,
-    [
-      'defaultPromoImage',
-      'blocks',
-      0,
-      'model',
-      'blocks',
-      0,
-      'model',
-      'blocks',
-      0,
-      'model',
-      'text',
-    ],
-    cpsImage,
-  );
+  if (hasPath(['indexImage'], promo)) {
+    const cpsImage = path(['indexImage'], promo);
+
+    return {
+      headline: path(['headlines', 'headline'], promo),
+      url: path(['locators', 'assetUri'], promo),
+      indexImage: {
+        width: path(['width'], cpsImage),
+        height: path(['height'], cpsImage),
+        altText: path(['altText'], cpsImage),
+        copyrightHolder: path(['copyrightHolder'], cpsImage),
+        originCode: getOriginCode(path(['path'], cpsImage)),
+        locator: getLocator(path(['path'], cpsImage)),
+      },
+    };
+  }
+
+  const optimoImage = path(['images', 'defaultPromoImage', 'blocks'], promo);
+
   return {
-    width,
-    height,
-    copyrightHolder,
-    optimoLocator,
-    altText,
-    optimoOriginCode,
+    headline: path(
+      [
+        'headlines',
+        'promoHeadline',
+        'blocks',
+        0,
+        'model',
+        'blocks',
+        0,
+        'model',
+        'text',
+      ],
+      promo,
+    ),
+    url: path(['locators', 'canonicalUrl'], promo),
+    indexImage: {
+      width: path([1, 'model', 'width'], optimoImage),
+      height: path([1, 'model', 'height'], optimoImage),
+      altText: path(
+        [0, 'model', 'blocks', 0, 'model', 'blocks', 0, 'model', 'text'],
+        optimoImage,
+      ),
+      copyrightHolder: path([1, 'model', 'copyrightHolder'], optimoImage),
+      originCode: path([1, 'model', 'originCode'], optimoImage),
+      locator: path([1, 'model', 'locator'], optimoImage),
+    },
   };
 };
 
-export default optimoToCPSImage;
+export default extractPromoData;
