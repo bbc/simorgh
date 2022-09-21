5cef34df55d99a8120e591a03093519319062dde
bennea27 <andrew.bennett07@bbc.co.uk>
2022-09-21 10:45:22 +0100

    Revert "add emotion styled + ignore lint for now"
    This reverts commit 91e7054b9665dfefcb5a17489ea1d2963dafb755.

--- ws-renderer/next.config.js
+++ ws-renderer/next.config.js
@@ -18,9 +18,4 @@ module.exports = {
 
     return config;
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
