c50dab9568adab1a8f44a6b6336d0f80d63e7969
Aaron Moore <aaron.moore@bbc.co.uk>
2022-09-23 10:55:26 +0100

    Remove ThemeProvider from test helper

--- src/app/legacy/containers/SocialEmbed/common/testHelper.jsx
+++ src/app/legacy/containers/SocialEmbed/common/testHelper.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { RequestContextProvider } from '#contexts/RequestContext';
 import { STORY_PAGE } from '#app/routes/utils/pageTypes';
 import { ServiceContextProvider } from '../../../../contexts/ServiceContext';
-import ThemeProvider from '../../../../components/ThemeProvider';
 
 /**
  * withContexts is a higher-order component that returns a React component
@@ -15,18 +14,16 @@ const withContexts =
   (Component, { isAmp, service = 'news', pageType = STORY_PAGE }) =>
   props =>
     (
-      <ThemeProvider service={service}>
-        <RequestContextProvider
-          isAmp={isAmp}
-          pageType={pageType}
-          service={service}
-          pathname="/pathname"
-        >
-          <ServiceContextProvider service={service}>
-            <Component {...props} />
-          </ServiceContextProvider>
-        </RequestContextProvider>
-      </ThemeProvider>
+      <RequestContextProvider
+        isAmp={isAmp}
+        pageType={pageType}
+        service={service}
+        pathname="/pathname"
+      >
+        <ServiceContextProvider service={service}>
+          <Component {...props} />
+        </ServiceContextProvider>
+      </RequestContextProvider>
     );
 
 export default withContexts;
