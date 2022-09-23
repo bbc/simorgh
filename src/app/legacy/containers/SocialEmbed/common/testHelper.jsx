3eccb7d532bf2e843a74bae40b3ca51b85e6fcdc
Aaron Moore <aaron.moore@bbc.co.uk>
2022-09-23 10:52:33 +0100

    WSTEAM1-122-123 - Enable YouTube and Instagram embeds in Articles on Live

--- src/app/legacy/containers/SocialEmbed/common/testHelper.jsx
+++ src/app/legacy/containers/SocialEmbed/common/testHelper.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { RequestContextProvider } from '#contexts/RequestContext';
 import { STORY_PAGE } from '#app/routes/utils/pageTypes';
 import { ServiceContextProvider } from '../../../../contexts/ServiceContext';
+import ThemeProvider from '../../../../components/ThemeProvider';
 
 /**
  * withContexts is a higher-order component that returns a React component
@@ -14,16 +15,18 @@ const withContexts =
   (Component, { isAmp, service = 'news', pageType = STORY_PAGE }) =>
   props =>
     (
-      <RequestContextProvider
-        isAmp={isAmp}
-        pageType={pageType}
-        service={service}
-        pathname="/pathname"
-      >
-        <ServiceContextProvider service={service}>
-          <Component {...props} />
-        </ServiceContextProvider>
-      </RequestContextProvider>
+      <ThemeProvider service={service}>
+        <RequestContextProvider
+          isAmp={isAmp}
+          pageType={pageType}
+          service={service}
+          pathname="/pathname"
+        >
+          <ServiceContextProvider service={service}>
+            <Component {...props} />
+          </ServiceContextProvider>
+        </RequestContextProvider>
+      </ThemeProvider>
     );
 
 export default withContexts;
