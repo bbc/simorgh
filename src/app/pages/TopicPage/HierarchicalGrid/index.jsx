2d72f27e0e37add40861598001d1269be34ddb5c
nicola-d-bbc <nicola.dochnenko@bbc.co.uk>
2022-09-23 14:00:42 +0100

    remove array

--- src/app/pages/TopicPage/HierarchicalGrid/index.jsx
+++ src/app/pages/TopicPage/HierarchicalGrid/index.jsx
@@ -34,22 +34,20 @@ const Item = styled.div`
 `;
 
 const getStyles = (promoCount, i) => {
-  return [
-    css`
-      @media (max-width: ${GEL_GROUP_1_SCREEN_WIDTH_MAX}) {
-        ${SMALL[promoCount - 1][i]}
-      }
-      @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_2_SCREEN_WIDTH_MAX}) {
-        ${MOBILE[promoCount - 1][i]}
-      }
-      @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
-        ${TABLET[promoCount - 1][i]}
-      }
-      @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
-        ${DESKTOP[promoCount - 1][i]}
-      }
-    `,
-  ];
+  return css`
+    @media (max-width: ${GEL_GROUP_1_SCREEN_WIDTH_MAX}) {
+      ${SMALL[promoCount - 1][i]}
+    }
+    @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_2_SCREEN_WIDTH_MAX}) {
+      ${MOBILE[promoCount - 1][i]}
+    }
+    @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
+      ${TABLET[promoCount - 1][i]}
+    }
+    @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
+      ${DESKTOP[promoCount - 1][i]}
+    }
+  `;
 };
 
 const HiearchicalGrid = ({ summaries }) => {
