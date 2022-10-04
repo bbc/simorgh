96149aa972e0dbd484056d3776bb9e826e0792cc
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2022-10-04 15:50:09 +0100

    React testing library providers for TopicPage

--- src/app/pages/TopicPage/index.test.jsx
+++ src/app/pages/TopicPage/index.test.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import { TOPIC_PAGE } from '#app/routes/utils/pageTypes';
-import { RequestContextProvider } from '#contexts/RequestContext';
-import { ToggleContext } from '#contexts/ToggleContext';
-import { ServiceContextProvider } from '../../contexts/ServiceContext';
-import ThemeProvider from '../../components/ThemeProvider';
+import { render } from '../../components/react-testing-library-with-providers';
 import TopicPage from './TopicPage';
 import {
   pidginMultipleItems,
@@ -20,64 +15,48 @@ jest.mock('../../legacy/containers/ChartbeatAnalytics', () => {
   return ChartbeatAnalytics;
 });
 
-/* eslint-disable react/prop-types */
-const TopicPageWithContext = ({
-  pageData = pidginMultipleItems,
-  lang = 'pcm',
-  service = 'pidgin',
-  adsToggledOn = false,
+const getOptionParams = ({
   showAdsBasedOnLocation = false,
-} = {}) => (
-  <BrowserRouter>
-    <ToggleContext.Provider
-      value={{
-        toggleState: {
-          ads: {
-            enabled: adsToggledOn,
-          },
-        },
-      }}
-    >
-      <RequestContextProvider
-        showAdsBasedOnLocation={showAdsBasedOnLocation}
-        isAmp={false}
-        pageType={TOPIC_PAGE}
-        service={service}
-      >
-        <ThemeProvider service={service}>
-          <ServiceContextProvider service={service} lang={lang}>
-            <TopicPage pageData={pageData} />
-          </ServiceContextProvider>
-        </ThemeProvider>
-      </RequestContextProvider>
-    </ToggleContext.Provider>
-  </BrowserRouter>
-);
+  service = 'pidgin',
+  lang = 'pcm',
+  adsToggledOn = false,
+} = {}) => ({
+  isAmp: false,
+  showAdsBasedOnLocation,
+  pageType: TOPIC_PAGE,
+  service,
+  lang,
+  toggles: {
+    toggleState: {
+      ads: {
+        enabled: adsToggledOn,
+      },
+    },
+  },
+});
 
 describe('Topic Page', () => {
   it('should not render an unordered list when there is only one promo', () => {
     const { queryByRole } = render(
-      <TopicPageWithContext
-        pageData={amharicSingleItem}
-        lang="am"
-        service="amharic"
-      />,
+      <TopicPage pageData={amharicSingleItem} />,
+      getOptionParams({ service: 'amharic', lang: 'am' }),
     );
     expect(queryByRole('list')).not.toBeInTheDocument();
   });
 
   it('should render an unordered list when there is more than one promo', () => {
-    const { container, queryByRole } = render(<TopicPageWithContext />);
+    const { container, queryByRole } = render(
+      <TopicPage pageData={pidginMultipleItems} />,
+      getOptionParams(),
+    );
     expect(queryByRole('list')).toBeInTheDocument();
     expect(container.getElementsByTagName('li').length).toEqual(4);
   });
 
   it('should render multiple curations', () => {
     const { getAllByRole } = render(
-      <TopicPageWithContext
-        pageData={mundoMultipleCurations}
-        service="mundo"
-      />,
+      <TopicPage pageData={mundoMultipleCurations} />,
+      getOptionParams({ service: 'mundo', lang: 'es' }),
     );
     expect(getAllByRole('list').length).toEqual(2);
     expect(getAllByRole('listitem').length).toEqual(4);
@@ -85,41 +64,32 @@ describe('Topic Page', () => {
 
   it('should render a section around each curation when more than one exists', () => {
     const { container } = render(
-      <TopicPageWithContext
-        pageData={mundoMultipleCurations}
-        service="mundo"
-      />,
+      <TopicPage pageData={mundoMultipleCurations} />,
+      getOptionParams({ service: 'mundo', lang: 'es' }),
     );
     expect(container.getElementsByTagName('section').length).toEqual(2);
   });
 
   it('should not render a section when one or less exists', () => {
     const { container } = render(
-      <TopicPageWithContext
-        pageData={amharicSingleItem}
-        lang="am"
-        service="amharic"
-      />,
+      <TopicPage pageData={amharicSingleItem} />,
+      getOptionParams({ service: 'amharic', lang: 'am' }),
     );
     expect(container.getElementsByTagName('section').length).toEqual(0);
   });
 
   it('should render curation subheading when curation title exists', () => {
     const { container } = render(
-      <TopicPageWithContext
-        pageData={mundoMultipleCurations}
-        service="mundo"
-      />,
+      <TopicPage pageData={mundoMultipleCurations} />,
+      getOptionParams({ service: 'mundo', lang: 'es' }),
     );
     expect(container.querySelector('h2').textContent).toEqual('Analysis');
   });
 
   it('should render promo headings as h3 when curation subheading exists', () => {
     const { container } = render(
-      <TopicPageWithContext
-        pageData={mundoMultipleCurations}
-        service="mundo"
-      />,
+      <TopicPage pageData={mundoMultipleCurations} />,
+      getOptionParams({ service: 'mundo', lang: 'es' }),
     );
 
     expect(container.getElementsByTagName('h3').length).toEqual(4);
@@ -128,7 +98,8 @@ describe('Topic Page', () => {
 
   it('should render promo headings as h2 when there is no curation subheading', () => {
     const { container } = render(
-      <TopicPageWithContext pageData={pidginMultipleItems} service="pidgin" />,
+      <TopicPage pageData={pidginMultipleItems} service="pidgin" />,
+      getOptionParams(),
     );
 
     expect(container.getElementsByTagName('h3').length).toEqual(0);
@@ -137,10 +108,8 @@ describe('Topic Page', () => {
 
   it('should render badge and description when they exist in data', () => {
     const { container, queryByTestId } = render(
-      <TopicPageWithContext
-        pageData={mundoWithBadgeAndDescr}
-        service="mundo"
-      />,
+      <TopicPage pageData={mundoWithBadgeAndDescr} />,
+      getOptionParams({ service: 'mundo', lang: 'es' }),
     );
 
     expect(queryByTestId('topic-badge')).toBeInTheDocument();
@@ -149,7 +118,8 @@ describe('Topic Page', () => {
 
   it('should render description without badge', () => {
     const { container, queryByTestId } = render(
-      <TopicPageWithContext pageData={pidginMultipleItems} service="pidgin" />,
+      <TopicPage pageData={pidginMultipleItems} service="pidgin" />,
+      getOptionParams(),
     );
 
     expect(queryByTestId('topic-badge')).not.toBeInTheDocument();
@@ -158,7 +128,8 @@ describe('Topic Page', () => {
 
   it('should render only topic title without badge or description', () => {
     const { container, queryByTestId } = render(
-      <TopicPageWithContext pageData={amharicOnlyTitle} service="amharic" />,
+      <TopicPage pageData={amharicOnlyTitle} />,
+      getOptionParams({ service: 'amharic', lang: 'am' }),
     );
 
     expect(queryByTestId('topic-badge')).not.toBeInTheDocument();
@@ -166,26 +137,20 @@ describe('Topic Page', () => {
   });
 
   it('should show ads when enabled', () => {
-    [
-      [true, true],
-      [true, false],
-      [false, true],
-      [false, false],
-    ].forEach(([adsToggledOn, showAdsBasedOnLocation]) => {
-      const { container } = render(
-        <TopicPageWithContext
-          adsToggledOn={adsToggledOn}
-          showAdsBasedOnLocation={showAdsBasedOnLocation}
-        />,
-      );
+    const { container } = render(
+      <TopicPage pageData={pidginMultipleItems} />,
+      getOptionParams({
+        adsToggledOn: true,
+        showAdsBasedOnLocation: true,
+      }),
+    );
 
-      const shouldShowAds = adsToggledOn && showAdsBasedOnLocation;
-      const adElement = container.querySelector('[data-e2e="advertisement"]');
-      if (shouldShowAds) {
-        expect(adElement).toBeInTheDocument();
-      } else {
-        expect(adElement).not.toBeInTheDocument();
-      }
-    });
+    const shouldShowAds = true;
+    const adElement = container.querySelector('[data-e2e="advertisement"]');
+    if (shouldShowAds) {
+      expect(adElement).toBeInTheDocument();
+    } else {
+      expect(adElement).not.toBeInTheDocument();
+    }
   });
 });
