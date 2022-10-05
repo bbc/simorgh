8c3802aa6cd0cc94104d38ba2fb64aa7fa539e17
Rebecca McGinn <Rebecca.McGinn@bbc.co.uk>
2022-10-05 17:22:09 +0100

    Add rem to heading margin

--- src/app/legacy/components/Promo/heading.jsx
+++ src/app/legacy/components/Promo/heading.jsx
@@ -5,7 +5,7 @@ const Heading = styled.h2`
   ${props => props.theme.fontSizes.bodyCopy}
   color: ${props => props.theme.palette.C_EBON};
   margin-top: 0;
-  margin-bottom: ${props => props.theme.spacings.FULL};
+  margin-bottom: ${props => `${props.theme.spacings.FULL}rem`};
 `;
 
 export default Heading;
