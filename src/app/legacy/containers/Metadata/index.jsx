254bd1336c5e0acc962d01742e9bbeccddf7e56b
alex-amagana <alex.magana@andela.com>
2022-10-29 09:48:04 +0300

    Add support for twitter handle in ArticleMetadata and MetadataContainer

--- src/app/legacy/containers/Metadata/index.jsx
+++ src/app/legacy/containers/Metadata/index.jsx
@@ -40,6 +40,7 @@ const MetadataContainer = ({
   title,
   socialHeadline,
   lang,
+  twitterHandle,
   description,
   openGraphType,
   aboutTags,
@@ -158,7 +159,7 @@ const MetadataContainer = ({
       <meta property="og:type" content={openGraphType} />
       <meta property="og:url" content={canonicalNonUkLink} />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:creator" content={twitterCreator} />
+      <meta name="twitter:creator" content={twitterHandle || twitterCreator} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image:alt" content={metaImageAltText} />
       <meta name="twitter:image:src" content={metaImage} />
@@ -199,6 +200,7 @@ MetadataContainer.propTypes = {
   title: string.isRequired,
   socialHeadline: string,
   lang: string.isRequired,
+  twitterHandle: string,
   description: string.isRequired,
   openGraphType: string.isRequired,
   aboutTags: arrayOf(tagPropTypes),
@@ -223,6 +225,7 @@ MetadataContainer.defaultProps = {
   children: null,
   hasAppleItunesAppBanner: false,
   hasAmpPage: true,
+  twitterHandle: null,
 };
 
 export default MetadataContainer;
