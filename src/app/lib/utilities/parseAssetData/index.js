c9dc81617c1e50c59eb3686408b9e350e4185fd9
alex-amagana <alex.magana@andela.com>
2022-11-01 17:52:15 +0300

    Move logic to a dedicated module

--- src/app/lib/utilities/parseAssetData/index.js
+++ src/app/lib/utilities/parseAssetData/index.js
@@ -1,6 +1,4 @@
 import path from 'ramda/src/path';
-import pathOr from 'ramda/src/pathOr';
-import isEmpty from 'ramda/src/isEmpty';
 
 const getISOStringDate = date => new Date(date).toISOString();
 
@@ -36,45 +34,3 @@ export const getArticleSection = path(['metadata', 'passport', 'genre']);
 export const getMentions = path(['metadata', 'tags', 'mentions']);
 
 export const getLang = path(['metadata', 'passport', 'language']);
-
-export const getAuthorTwitterHandle = articleContentBlocks => {
-  const [firstByline, ...otherBylines] = articleContentBlocks.filter(
-    block => block.type === 'byline',
-  );
-  const [firstContributor, ...otherContributors] = pathOr(
-    null,
-    ['model', 'blocks'],
-    firstByline,
-  );
-
-  if (!isEmpty(otherBylines) || !isEmpty(otherContributors)) return null;
-
-  const contributorBlocks = pathOr(null, ['model', 'blocks'], firstContributor);
-  const contributorLink = contributorBlocks.find(
-    block => block.type === 'link',
-  );
-  const contributorHandle = pathOr(
-    null,
-    [
-      'model',
-      'blocks',
-      0,
-      'model',
-      'blocks',
-      0,
-      'model',
-      'blocks',
-      0,
-      'model',
-      'blocks',
-      0,
-      'model',
-      'text',
-    ],
-    contributorLink,
-  );
-  const sanitizedcontributorHandle = contributorHandle
-    ? contributorHandle.replace(/^@*/, '@')
-    : null;
-  return sanitizedcontributorHandle;
-};
