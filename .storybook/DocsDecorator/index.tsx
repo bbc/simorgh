198e2c846c36cf9ea104b2898d813f77d332b826
Dario Russo <dario.russo@bbc.co.uk>
2022-11-17 10:22:34 +0000

    feat: missing health component

--- .storybook/DocsDecorator/index.tsx
+++ .storybook/DocsDecorator/index.tsx
@@ -7,14 +7,14 @@ import ThemeProvider from '../../src/app/components/ThemeProvider';
 import SingleDoc from './SingleDoc';
 import { DocsContextProps } from '@storybook/addon-docs';
 import Heading from '../../src/app/components/Heading';
-import { Recommend, Warning } from './Icons/icons';
+import { Recommend, Warning, Activity } from './Icons/icons';
 
 const DocsDecorator = ({
   context,
   children,
 }: {
   context: DocsContextProps;
-  children: any;
+  children: JSX.Element;
 }) => {
   const title = path(
     ['parameters', 'docs', 'component', 'title'],
@@ -41,62 +41,66 @@ const DocsDecorator = ({
   const headline = metadata
     ? actionCount === 0
       ? 'Good to show to the audience'
+      : actionCount === 1
+      ? `${toWord[actionCount]} action outstanding`
       : `${toWord[actionCount]} actions outstanding`
     : 'Component health is missing!';
 
   return (
     // @ts-ignore: type children not assignable.
     <DocsContainer context={context}>
-      {metadata ? (
-        <ThemeProvider service="news" variant="default">
-          <Title>{title}</Title>
+      <ThemeProvider service="news" variant="default">
+        <Title>{title}</Title>
 
-          <aside css={styles.componentHealthContainer}>
-            <div css={styles.titleContainer}>
-              <span css={styles.titleIcon}>
-                {actionCount === 0 ? (
+        <aside css={styles.componentHealthContainer}>
+          <div css={styles.titleContainer}>
+            <span css={styles.titleIcon}>
+              {metadata ? (
+                actionCount === 0 ? (
                   <Recommend css={styles.recommendIcon} />
                 ) : (
                   <Warning css={styles.warningIcon} />
-                )}
-              </span>
-              <Heading size="doublePica" level={2}>
-                {headline}
-              </Heading>
-            </div>
+                )
+              ) : (
+                <Activity css={styles.warningIcon} />
+              )}
+            </span>
+            <Heading size="doublePica" level={2}>
+              {headline}
+            </Heading>
+          </div>
 
-            <ul css={styles.documentationList}>
-              <SingleDoc
-                label={'Screen reader UX'}
-                url={getUrl(uxAccessibility) as string}
-                status={getDone(uxAccessibility) as boolean}
-                urlLabel={
-                  (getLabel(uxAccessibility) as string) ||
-                  'How to document the screen reader UX'
-                }
-              />
-              <SingleDoc
-                label={'Accessibility Acceptance Criteria'}
-                url={getUrl(acceptanceCriteria) as string}
-                status={getDone(acceptanceCriteria) as boolean}
-                urlLabel={
-                  (getLabel(acceptanceCriteria) as string) ||
-                  'How to write accessibility acceptance criteria'
-                }
-              />
-              <SingleDoc
-                label={'Accessibility Swarm'}
-                url={getUrl(uxSwarm) as string}
-                status={getDone(uxSwarm) as boolean}
-                urlLabel={
-                  (getLabel(uxSwarm) as string) ||
-                  'How to run an accessibility swarm'
-                }
-              />
-            </ul>
-          </aside>
-        </ThemeProvider>
-      ) : null}
+          <ul css={styles.documentationList}>
+            <SingleDoc
+              label={'Screen reader UX'}
+              url={getUrl(uxAccessibility) as string}
+              status={getDone(uxAccessibility) as boolean}
+              urlLabel={
+                (getLabel(uxAccessibility) as string) ||
+                'How to document the screen reader UX'
+              }
+            />
+            <SingleDoc
+              label={'Accessibility Acceptance Criteria'}
+              url={getUrl(acceptanceCriteria) as string}
+              status={getDone(acceptanceCriteria) as boolean}
+              urlLabel={
+                (getLabel(acceptanceCriteria) as string) ||
+                'How to write accessibility acceptance criteria'
+              }
+            />
+            <SingleDoc
+              label={'Accessibility Swarm'}
+              url={getUrl(uxSwarm) as string}
+              status={getDone(uxSwarm) as boolean}
+              urlLabel={
+                (getLabel(uxSwarm) as string) ||
+                'How to run an accessibility swarm'
+              }
+            />
+          </ul>
+        </aside>
+      </ThemeProvider>
       {children}
     </DocsContainer>
   );
