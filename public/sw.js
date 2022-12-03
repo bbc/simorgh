5a692815b472ca63db952ee5ae09192b34f6ba25
eagerterrier <eagerterrier@gmail.com>
2022-12-03 10:27:23 +0000

    ENH: add font caching

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,43 @@
 /* eslint-disable */
-var version = 'v0.0.3';
+const version = 'v0.0.3';
+const cacheName = 'simorghCache_v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(cacheName));
+});
 
 self.addEventListener('fetch', function(event) {
-    if (self.registration.scope.indexOf('/hindi/') && /^https:\/\/ichef\.bbci\.co\.uk\/news\/.+(\.jpg|\.png)$/.test(event.request.url)) {
+  if (self.registration.scope.indexOf('/hindi/') && /^https:\/\/ichef\.bbci\.co\.uk\/news\/.+(\.jpg|\.png)$/.test(event.request.url)) {
 
-        const req = event.request.clone();
+    const req = event.request.clone();
 
-        // Inspect the accept header for WebP support
-        let supportsWebp = false;
-        if (req.headers.has('accept')){
-            supportsWebp = req.headers.get('accept').includes('webp');
-        }
-        // If we support WebP
-        if (supportsWebp) {
-            event.respondWith(
-				fetch(`${req.url}.webp`, {
-					mode: 'no-cors'
-				})
-			);
-        }
-        
+    // Inspect the accept header for WebP support
+    let supportsWebp = false;
+    if (req.headers.has('accept')){
+      supportsWebp = req.headers.get('accept').includes('webp');
     }
+    // If we support WebP
+    if (supportsWebp) {
+      event.respondWith(
+        fetch(`${req.url}.webp`, {
+          mode: 'no-cors'
+        })
+      );
+    }
+    
+  }
+  else if (event.request.destination === 'font') {
+    event.respondWith(caches.open(cacheName).then(cache => {
+      return cache.match(event.request).then(cachedResponse => {
+        console.log('cachedResponse', cachedResponse);
+        return cachedResponse || fetch(event.request.url).then((fetchedResponse) => {
+          cache.put(event.request, fetchedResponse.clone());
+          return fetchedResponse;
+        });
+      });
+    }));
+  }
+  else {
+    return;
+  }
 });
