2c2dab8c30a4b9c416ee3a30378f6458770d47eb
Isabella Mitchell <i.mitchell.0707@gmail.com>
2022-12-08 15:41:49 +0000

    Update serbian article data and add service to BFF fetch list

--- src/app/routes/article/getInitialData/index.js
+++ src/app/routes/article/getInitialData/index.js
@@ -104,6 +104,7 @@ export default async ({ getAgent, path, pageType, service, variant }) => {
     'punjabi',
     'russian',
     'scotland',
+    'serbian',
     'sinhala',
     'somali',
     'sport',
