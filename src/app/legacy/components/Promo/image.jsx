95c9ab91b9a65f42471bfb0ea808c467350d7d29
nicola-d-bbc <nicola.dochnenko@bbc.co.uk>
2022-12-13 15:58:54 +0000

    add large image checl

--- src/app/legacy/components/Promo/image.jsx
+++ src/app/legacy/components/Promo/image.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { string, node } from 'prop-types';
+import { string, node, boolean } from 'prop-types';
 import { GEL_SPACING } from '#psammead/gel-foundations/src/spacings';
 import {
   GEL_GROUP_3_SCREEN_WIDTH_MIN,
@@ -21,7 +21,6 @@ const ChildWrapper = styled.div`
 const WEBP_ORIGIN_CODES = ['cpsdevpb', 'cpsprodpb'];
 
 const createSrcSet = (imageUrl, suffix = '') => {
-  // use utils function???
   const imageResolutions = [85, 120, 170, 232, 325, 450, 660];
 
   return imageResolutions
@@ -29,12 +28,11 @@ const createSrcSet = (imageUrl, suffix = '') => {
     .join(', ');
 };
 
-const createSizes = () => {
-  // ? '(max-width: 600px) 100vw, (max-width: 1008px) 50vw, 496px'
-  // : '(max-width: 1008px) 33vw, 321px';
-
+const createSizes = useLargeImages => {
   // 4 columns of fixed width
-  const DESKTOP_SIZE = `(min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) 232px`;
+  const DESKTOP_SIZE = useLargeImages
+    ? `(min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) 660px`
+    : `(min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) 232px`;
 
   // 2 columns of 50% screen width - images are 100% of the column
   const TABLET_SIZE = `(min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) 50vw`;
@@ -45,7 +43,7 @@ const createSizes = () => {
 };
 
 const Image = props => {
-  const { children, src, ...rest } = props;
+  const { children, src, useLargeImages, ...rest } = props;
   const isWebPSupported = WEBP_ORIGIN_CODES.some(originCode =>
     src.includes(originCode),
   );
@@ -53,13 +51,13 @@ const Image = props => {
   const primaryMediaType = `image/${isWebPSupported ? 'webp' : 'jpeg'}`;
   const fallbackSrcSet = isWebPSupported ? createSrcSet(src) : undefined;
   const fallbackMediaType = isWebPSupported ? 'image/jpeg' : undefined;
-  const sizes = createSizes();
+  const sizes = createSizes(useLargeImages);
 
   return (
     <Wrapper>
       <IMAGE
         {...rest}
-        src={src.replace('{width}', 660)}
+        src={src.replace('{width}', 240)}
         srcSet={primarySrcSet}
         mediaType={primaryMediaType}
         fallbackSrcSet={fallbackSrcSet}
@@ -75,10 +73,12 @@ const Image = props => {
 Image.propTypes = {
   alt: string.isRequired,
   src: string.isRequired,
+  useLargeImages: boolean,
   children: node,
 };
 
 Image.defaultProps = {
+  useLargeImages: false,
   children: null,
 };
 
