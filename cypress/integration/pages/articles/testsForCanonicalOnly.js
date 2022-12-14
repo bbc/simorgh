90fce91ec66c9a47ee39b0e911dd6980631dde26
Aaron Moore <aaron.moore@bbc.co.uk>
2022-12-14 14:17:38 +0000

    Update Cypress article JSON checks

--- cypress/integration/pages/articles/testsForCanonicalOnly.js
+++ cypress/integration/pages/articles/testsForCanonicalOnly.js
@@ -85,7 +85,9 @@ export const testsThatFollowSmokeTestConfigForCanonicalOnly = ({
     describe('Media Player: Canonical', () => {
       it('should render a visible placeholder image', () => {
         cy.window().then(win => {
-          const media = getBlockData('video', win.SIMORGH_DATA.pageData);
+          const media = getBlockData('video', {
+            data: { article: win.SIMORGH_DATA.pageData },
+          });
           if (media) {
             cy.get('[data-e2e="media-player"]').within(() => {
               cy.get('[data-e2e="media-player__placeholder"] img')
@@ -99,7 +101,9 @@ export const testsThatFollowSmokeTestConfigForCanonicalOnly = ({
 
       it('should render a visible guidance message', () => {
         cy.window().then(win => {
-          const media = getBlockData('video', win.SIMORGH_DATA.pageData);
+          const media = getBlockData('video', {
+            data: { article: win.SIMORGH_DATA.pageData },
+          });
 
           if (media) {
             const longGuidanceWarning =
@@ -130,7 +134,9 @@ export const testsThatFollowSmokeTestConfigForCanonicalOnly = ({
 
       it('should have a visible play button and valid duration', () => {
         cy.window().then(win => {
-          const media = getBlockData('video', win.SIMORGH_DATA.pageData);
+          const media = getBlockData('video', {
+            data: { article: win.SIMORGH_DATA.pageData },
+          });
           if (media && media.type === 'video') {
             const aresMediaBlocks = media.model.blocks[1].model.blocks[0];
             const { durationISO8601 } = aresMediaBlocks.model.versions[0];
@@ -153,11 +159,19 @@ export const testsThatFollowSmokeTestConfigForCanonicalOnly = ({
       it('should render an iframe with a valid URL when a user clicks play', () => {
         cy.window().then(win => {
           const body = win.SIMORGH_DATA.pageData;
-          const media = getBlockData('video', body);
+
+          const media = getBlockData('video', {
+            data: { article: body },
+          });
 
           if (media && media.type === 'video') {
             const { lang } = appConfig[service][variant];
-            const embedUrl = getVideoEmbedUrl(body, lang);
+            const embedUrl = getVideoEmbedUrl(
+              {
+                data: { article: body },
+              },
+              lang,
+            );
             cy.get('[data-e2e="media-player"] button')
               .click()
               .then(() => {
