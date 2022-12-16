b2f49634e3262f5ed6314cf02f09a8977fe654a5
mfonofm <mfon.essien@bbc.co.uk>
2022-12-16 22:29:46 +0000

    fix: update

--- src/app/legacy/containers/PodcastExternalLinks/index.jsx
+++ src/app/legacy/containers/PodcastExternalLinks/index.jsx
@@ -144,7 +144,7 @@ const PodcastExternalLinks = ({ brandTitle, links }) => {
       {hasMultipleLinks ? (
         <StyledList role="list">
           {links.map(({ linkText, linkUrl, linkType }) => (
-            <StyledListItem dir={dir} key={linkText}>
+            <StyledListItem dir={dir} key={`${linkText}${linkType}`}>
               <PodcastExternalLink
                 linkText={linkText}
                 linkUrl={linkUrl}
