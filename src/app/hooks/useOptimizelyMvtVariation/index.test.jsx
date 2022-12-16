a7ec1be486d1a7417c1c505a9bec6dd19537cfa7
Aaron Moore <aaron.moore@bbc.co.uk>
2022-12-16 09:35:40 +0000

    Lint fix

--- src/app/hooks/useOptimizelyMvtVariation/index.test.jsx
+++ src/app/hooks/useOptimizelyMvtVariation/index.test.jsx
@@ -1,4 +1,5 @@
 /* eslint react/prop-types: 0 */
+import React from 'react';
 import { renderHook } from '@testing-library/react-hooks';
 import { RequestContextProvider } from '#contexts/RequestContext';
 import useOptimizelyMvtVariation from '.';
