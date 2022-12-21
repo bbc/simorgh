bf6e0c856ec10abd6955f3bc7d67e5427c21f876
Isabella Mitchell <i.mitchell.0707@gmail.com>
2022-12-21 15:46:47 +0000

    Add focus:not selector declation to both legacy and new component and complete style property:values

--- src/app/components/InlineLink/index.styles.ts
+++ src/app/components/InlineLink/index.styles.ts
@@ -1,4 +1,5 @@
 import { css, Theme } from '@emotion/react';
+import { none } from 'ramda';
 
 import pixelsToRem from '../../utilities/pixelsToRem';
 
@@ -12,31 +13,74 @@ const styles = {
         color: palette.METAL,
         borderBottom: `${pixelsToRem(1)}rem solid ${palette.METAL}`,
       },
-      '&:focus, &:hover': {
+      // remove focus from this rule to make it into focus-indicator style. Same styles applied to both hover and focus
+      '&:hover': {
         borderBottom: `${pixelsToRem(2)}rem solid ${palette.POSTBOX}`,
         color: palette.POSTBOX,
       },
-      // original solution - does not support on older browsers
-      // '&:focus-visible': {
-      //   outline: `${pixelsToRem(3)}rem solid ${palette.BLACK}`,
-      //   boxShadow: `0 0 0 ${pixelsToRem(3)}rem ${palette.WHITE}`,
-      //   outlineOffset: `${pixelsToRem(3)}rem`,
-      // },
-      // Solution that uses @support - may not be compatible with older browsers
+      // // SOLUTION 1 - Using @supports selector
+      // // Applies all rules to focus state for browsers that don't handle focus-visible
+      '&:focus': {
+        borderBottom: `${pixelsToRem(2)}rem solid ${palette.POSTBOX}`,
+        color: palette.POSTBOX,
+        outline: `${pixelsToRem(3)}rem solid ${palette.BLACK}`,
+        boxShadow: `0 0 0 ${pixelsToRem(3)}rem ${palette.WHITE}`,
+        outlineOffset: `${pixelsToRem(3)}rem`,
+      },
+      // Overrides these rules if focus-visible is supported by browser, applies different styles to focus and focus-visible
       '@supports selector(:focus-visible)': {
+        '&:focus': {
+          borderBottom: `${pixelsToRem(2)}rem solid ${palette.POSTBOX}`,
+          color: palette.POSTBOX,
+          outline: `none`,
+          boxShadow: `none`,
+          outlineOffset: 0,
+        },
         '&:focus-visible': {
           outline: `${pixelsToRem(3)}rem solid ${palette.BLACK}`,
           boxShadow: `0 0 0 ${pixelsToRem(3)}rem ${palette.WHITE}`,
           outlineOffset: `${pixelsToRem(3)}rem`,
         },
       },
+      // // END SOLUTION 1
+      // // SOLUTION 2 - using focus:not(:focus-visible)
+      // // Applies all rules to focus state
+      // '&:focus': {
+      //   borderBottom: `${pixelsToRem(2)}rem solid ${palette.POSTBOX}`,
+      //   color: palette.POSTBOX,
+      //   outline: `${pixelsToRem(3)}rem solid ${palette.BLACK}`,
+      //   boxShadow: `0 0 0 ${pixelsToRem(3)}rem ${palette.WHITE}`,
+      //   outlineOffset: `${pixelsToRem(3)}rem`,
+      // },
+      // // Overrides these rules depending whether focus-visible state is being used, applies different styles to focus and focus-visible
+      // '&:focus:not(:focus-visible)': {
+      //   borderBottom: `${pixelsToRem(2)}rem solid ${palette.POSTBOX}`,
+      //   color: palette.POSTBOX,
+      //   outline: `none`,
+      //   boxShadow: `none`,
+      //   outlineOffset: 0,
+      // },
+      // '&:focus-visible': {
+      //   outline: `${pixelsToRem(3)}rem solid ${palette.BLACK}`,
+      //   boxShadow: `0 0 0 ${pixelsToRem(3)}rem ${palette.WHITE}`,
+      //   outlineOffset: `${pixelsToRem(3)}rem`,
+      // },
+      // // END SOLUTION 2
+      //
+      // Original solution - does not have any browser backwards capabilities
+      // '&:focus-visible': {
+      //   outline: `${pixelsToRem(3)}rem solid ${palette.BLACK}`,
+      //   boxShadow: `0 0 0 ${pixelsToRem(3)}rem ${palette.WHITE}`,
+      //   outlineOffset: `${pixelsToRem(3)}rem`,
+      // },
+      //
       // Test to see @supports without a property being declared.
-
+      //
       // This will apply this style in any state if focus-visible is supported
       // '@supports selector(:focus-visible)': {
       //   backgroundColor: palette.POSTBOX,
       // },
-
+      //
       // This will apply this style in any state if focus-visible is not supported
       // '@supports not selector(:focus-visible)': {
       //   backgroundColor: palette.POSTBOX,
