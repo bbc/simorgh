e2c7b3b16ec88b2c33f61cdde2154684afda96a5
Isabella Mitchell <i.mitchell.0707@gmail.com>
2022-12-21 11:20:09 +0000

    Add focus-visible styling to legacy inline-link componant

--- src/app/legacy/psammead/psammead-inline-link/src/index.jsx
+++ src/app/legacy/psammead/psammead-inline-link/src/index.jsx
@@ -3,7 +3,10 @@ import {
   C_POSTBOX,
   C_METAL,
   C_EBON,
+  C_BLACK,
+  C_WHITE,
 } from '#psammead/psammead-styles/src/colours';
+import { GEL_SPACING_HLF } from '#psammead/gel-foundations/src/spacings';
 
 const InlineLink = styled.a`
   color: ${C_EBON};
@@ -19,6 +22,12 @@ const InlineLink = styled.a`
   &:hover {
     border-bottom: 2px solid ${C_POSTBOX};
     color: ${C_POSTBOX};
+  }
+
+  &:focus-visible {
+    outline: ${GEL_SPACING_HLF} solid ${C_BLACK};
+    box-shadow: ${GEL_SPACING_HLF} solid ${C_WHITE};
+    outline-offset: ${GEL_SPACING_HLF};
   }
 `;
 
