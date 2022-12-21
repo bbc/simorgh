4181b4b736b6dbfd1f252217c2f7b375f55944ee
Harvey Peachey <harveypeachey@hotmail.co.uk>
2022-12-21 10:33:49 +0000

    Revert "Revert "[WSTEAM1-102] Server Side Optimizely Integration" (#10474)"
    This reverts commit e646e1bd0fc7c6719a0bdbbe30b08bb782efc6ea.

---
+++ src/server/utilities/mvtHeader/enabledExperimentsList.js
@@ -0,0 +1,14 @@
+const enabledExperimentList = [
+  {
+    name: 'simorgh_dark_mode',
+    services: ['pidgin', 'mundo'],
+    pageTypes: ['STY', 'IDX'],
+  },
+  {
+    name: 'test_2',
+    services: ['pidgin'],
+    pageTypes: ['article'],
+  },
+];
+
+export default enabledExperimentList;
