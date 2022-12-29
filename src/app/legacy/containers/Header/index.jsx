6f3b9aaeb7a205e890a4f560cc057bbd7f12dc08
Harvey Peachey <harvey.peachey@bbc.co.uk>
2022-12-29 09:38:26 +0000

    fixes dismiss bug on shift tab of last element

--- src/app/legacy/containers/Header/index.jsx
+++ src/app/legacy/containers/Header/index.jsx
@@ -19,10 +19,10 @@ const Header = ({ brandRef, borderBottom, skipLink, scriptLink, linkId }) => {
     const isRejectButton =
       event.target.getAttribute('data-terms-banner') === 'reject' ||
       event.target.getAttribute('data-cookie-banner') === 'reject';
-    const hasMovedToContent =
-      (event.relatedTarget.getAttribute('data-terms-banner') !== 'accept' ||
-        event.relatedTarget.getAttribute('data-cookie-banner') !== 'accept') &&
-      event.relatedTarget !== 'null';
+    const isAcceptButton =
+      event.relatedTarget.getAttribute('data-terms-banner') === 'accept' ||
+      event.relatedTarget.getAttribute('data-cookie-banner') === 'accept';
+    const hasMovedToContent = !isAcceptButton && event.relatedTarget !== 'null';
 
     if (isRejectButton && hasMovedToContent) {
       setShowConsentBanner(null);
