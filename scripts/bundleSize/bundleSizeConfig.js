f3c1d3256eb9853ab2b5a008d632c3e025b1eedc
Aaron Moore <aaron.moore@bbc.co.uk>
2023-01-05 10:30:42 +0000

    bundle size update

--- scripts/bundleSize/bundleSizeConfig.js
+++ scripts/bundleSize/bundleSizeConfig.js
@@ -5,6 +5,6 @@ module.exports = {
   // below the smallest value in the build output; this avoids the
   // need for frequent changes as bundle sizes fluctuate.
 
-  MIN_SIZE: 762,
-  MAX_SIZE: 1201,
+  MIN_SIZE: 750,
+  MAX_SIZE: 1189,
 };
