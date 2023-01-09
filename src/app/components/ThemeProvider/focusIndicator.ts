21fe94589857d3f6d89af8bdcddb579a082b32a2
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-01-09 15:57:57 +0000

    Make global style and add custom component styling to StoryPromo
    Signed-off-by: Isabella Mitchell <i.mitchell.0707@gmail.com>

---
+++ src/app/components/ThemeProvider/focusIndicator.ts
@@ -0,0 +1,30 @@
+import { css, Theme } from '@emotion/react';
+import pixelsToRem from '../../utilities/pixelsToRem';
+
+// export const globalStyles = css({
+//   '&a:focus-visible': {
+//     backgroundColor: yellow,
+//   },
+// });
+
+const focusIndicator = ({ palette }: Theme) => css`
+  a:focus {
+    outline: ${pixelsToRem(3)}rem solid ${palette.BLACK};
+    box-shadow: 0 0 0 ${pixelsToRem(3)}rem ${palette.WHITE};
+    outline-offset: ${pixelsToRem(3)}rem;
+  }
+
+  a:focus:not(:focus-visible) {
+    outline: none;
+    box-shadow: none;
+    outline-offset: 0;
+  }
+
+  a:focus-visible {
+    outline: ${pixelsToRem(3)}rem solid ${palette.BLACK};
+    box-shadow: 0 0 0 ${pixelsToRem(3)}rem ${palette.WHITE};
+    outline-offset: ${pixelsToRem(3)}rem;
+  }
+`;
+
+export default focusIndicator;
