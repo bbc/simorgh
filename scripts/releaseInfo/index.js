ddcb75338d3e8cb9977750795b7d447c66505a55
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-01-09 16:09:15 +0000

    Tidy up Release Info script to make it more dynamic. Remove launch date Add environment name to link Support all page types included in service file (apart from exempt page types e.g. error)

--- scripts/releaseInfo/index.js
+++ scripts/releaseInfo/index.js
@@ -2,7 +2,6 @@
 const fs = require('fs');
 const path = require('path');
 const allServices = require('../../cypress/support/config/settings');
-const launchDates = require('./launchDates');
 
 const getUrl = (pageType, env) => {
   let url;
@@ -19,139 +18,52 @@ const getUrl = (pageType, env) => {
   return url;
 };
 
-const generateLinks = (service, env, domain) => {
-  const output = [];
-
-  const {
-    frontPage,
-    liveRadio,
-    articles,
-    mediaAssetPage,
-    photoGalleryPage,
-    storyPage,
-    mostReadPage,
-    onDemandRadio,
-    idxPage,
-    onDemandTV,
-    featureIndexPage,
-  } = allServices()[service].pageTypes;
-
-  const frontPageURL = getUrl(frontPage, env);
-  if (frontPageURL) {
-    output.push(`[home](${domain}${frontPageURL})`);
-  }
-
-  const articleURL = getUrl(articles, env);
-  if (articleURL) {
-    output.push(`[articles](${domain}${articleURL})`);
-  }
-
-  const liveRadioURL = getUrl(liveRadio, env);
-  if (liveRadioURL) {
-    output.push(`[liveRadio](${domain}${liveRadioURL})`);
-  }
-
-  const mapURL = getUrl(mediaAssetPage, env);
-  if (mapURL) {
-    output.push(`[MAP](${domain}${mapURL})`);
-  }
-
-  const pglURL = getUrl(photoGalleryPage, env);
-  if (pglURL) {
-    output.push(`[PGL](${domain}${pglURL})`);
-  }
-
-  const styURL = getUrl(storyPage, env);
-  if (styURL) {
-    output.push(`[STY](${domain}${styURL})`);
-  }
-
-  const mostReadURL = getUrl(mostReadPage, env);
-  if (mostReadURL) {
-    output.push(`[mostRead](${domain}${mostReadURL})`);
-  }
-
-  const onDemandRadioURL = getUrl(onDemandRadio, env);
-  if (onDemandRadioURL) {
-    output.push(`[onDemandRadio](${domain}${onDemandRadioURL})`);
-  }
-
-  const idxURL = getUrl(idxPage, env);
-  if (idxURL) {
-    output.push(`[IDX](${domain}${idxURL})`);
-  }
-
-  const onDemandTVURL = getUrl(onDemandTV, env);
-  if (onDemandTVURL) {
-    output.push(`[onDemandTV](${domain}${onDemandTVURL})`);
-  }
-
-  const featureIndexURL = getUrl(featureIndexPage, env);
-  if (featureIndexURL) {
-    output.push(`[FIX](${domain}${featureIndexURL})`);
-  }
-
-  return output.join('<br/>');
+const pageTypeTitleMappings = {
+  frontPage: 'Home',
+  articles: 'Articles',
+  liveRadio: 'Live Radio',
+  mediaAssetPage: 'MAP',
+  photoGalleryPage: 'PGL',
+  storyPage: 'STY',
+  mostReadPage: 'Most Read',
+  onDemandRadio: 'On Demand Radio',
+  idxPage: 'IDX',
+  onDemandTV: 'On Demand TV',
+  featureIndexPage: 'FIX',
+  mostWatchedPage: 'Most Watched',
+  onDemandAudio: 'On Demand Audio',
 };
 
-const generateLaunchDates = service => {
+const excludedPageTypes = ['errorPage404'];
+
+const environmentDomainMappings = {
+  local: 'http://localhost:7080',
+  test: 'https://www.test.bbc.com',
+  live: 'https://www.bbc.com',
+};
+
+const generateLinks = (service, env) => {
   const output = [];
-  const serviceLaunch = launchDates[service];
 
-  if (serviceLaunch) {
-    if (serviceLaunch.frontPage && serviceLaunch.frontPage !== '') {
-      output.push(`__Home__: ${serviceLaunch.frontPage}`);
-    }
+  const serviceData = allServices()[service];
 
-    if (serviceLaunch.articles && serviceLaunch.articles !== '') {
-      output.push(`__Articles__: ${serviceLaunch.articles}`);
-    }
+  Object.keys(serviceData.pageTypes)
+    .sort()
+    .forEach(pageType => {
+      if (!excludedPageTypes.includes(pageType)) {
+        const url = getUrl(serviceData.pageTypes[pageType], env);
 
-    if (serviceLaunch.liveRadio && serviceLaunch.liveRadio !== '') {
-      output.push(`__Live Radio__: ${serviceLaunch.liveRadio}`);
-    }
+        if (url) {
+          output.push(
+            `[${env}: ${pageTypeTitleMappings[pageType] || pageType}](${
+              environmentDomainMappings[env]
+            }${url})`,
+          );
+        }
+      }
+    });
 
-    if (serviceLaunch.mediaAssetPage && serviceLaunch.mediaAssetPage !== '') {
-      output.push(`__MAP__: ${serviceLaunch.mediaAssetPage}`);
-    }
-
-    if (
-      serviceLaunch.photoGalleryPage &&
-      serviceLaunch.photoGalleryPage !== ''
-    ) {
-      output.push(`__PGL__: ${serviceLaunch.photoGalleryPage}`);
-    }
-
-    if (serviceLaunch.storyPage && serviceLaunch.storyPage !== '') {
-      output.push(`__STY__: ${serviceLaunch.storyPage}`);
-    }
-
-    if (serviceLaunch.mostReadPage && serviceLaunch.mostReadPage !== '') {
-      output.push(`__Most Read__: ${serviceLaunch.mostReadPage}`);
-    }
-
-    if (serviceLaunch.onDemandRadio && serviceLaunch.onDemandRadio !== '') {
-      output.push(`__On Demand Radio__: ${serviceLaunch.onDemandRadio}`);
-    }
-
-    if (serviceLaunch.idxPage && serviceLaunch.idxPage !== '') {
-      output.push(`__IDX__: ${serviceLaunch.idxPage}`);
-    }
-
-    if (serviceLaunch.onDemandTV && serviceLaunch.onDemandTV !== '') {
-      output.push(`__On Demand TV__: ${serviceLaunch.onDemandTV}`);
-    }
-
-    if (
-      serviceLaunch.featureIndexPage &&
-      serviceLaunch.featureIndexPage !== ''
-    ) {
-      output.push(`__FIX__: ${serviceLaunch.featureIndexPage}`);
-    }
-
-    return output.join('<br/>');
-  }
-  return null;
+  return output.join('<br/>');
 };
 
 const scriptDir = path.resolve(__dirname);
@@ -160,14 +72,13 @@ const stream = fs.createWriteStream(SimorghReleaseInfo);
 
 stream.once('open', () => {
   stream.write(
-    '<!--Please update the service launch date in scripts/releaseInfo/launchDates.js\n' +
-      'This table can then be generated using the following command: `node scripts/releaseInfo`\n' +
-      'Remember to commit and push the changes to both launchDates.js and Simorgh-Release-Info.md -->\n',
+    '<!--Please generate the Simorgh Release Info using the following command: `node scripts/releaseInfo`\n' +
+      'Remember to commit and push the changes to Simorgh-Release-Info.md -->\n',
   );
 
   stream.write(
-    '| Service | Local | Test | Stage | Live | Launch Dates |\n' +
-      '|---------|-------|------|-------|------|--------------|\n',
+    '| Service | Local | Test | Live |\n' +
+      '|---------|-------|------|-------|\n',
   );
 
   const services = allServices('');
@@ -176,11 +87,9 @@ stream.once('open', () => {
     console.log(`Generating information for ${service}`);
     const items = [
       service,
-      generateLinks(service, 'local', 'http://localhost:7080'),
+      generateLinks(service, 'local'),
       generateLinks(service, 'test', 'https://www.test.bbc.com'),
-      generateLinks(service, 'test', 'https://www.stage.bbc.com'),
       generateLinks(service, 'live', 'https://www.bbc.com'),
-      generateLaunchDates(service),
     ];
 
     stream.write(`| ${items.join(' | ')} |\n`);
