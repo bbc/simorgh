e951ef1f7635095ff5b3bec21429e2cbcfed2e1d
Aaron Moore <aaron.moore@bbc.co.uk>
2023-01-12 16:52:10 +0000

    Remove page type styling on embeds

--- src/app/legacy/psammead/psammead-social-embed/src/CaptionWrapper/index.jsx
+++ src/app/legacy/psammead/psammead-social-embed/src/CaptionWrapper/index.jsx
@@ -1,32 +1,22 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { node, string } from 'prop-types';
 import styled from '@emotion/styled';
-import {
-  C_WHITE,
-  C_GREY_6,
-  C_BLACK,
-} from '#psammead/psammead-styles/src/colours';
+import { C_GREY_6 } from '#psammead/psammead-styles/src/colours';
 import { getSansRegular } from '#psammead/psammead-styles/src/font-styles';
 import { GEL_BREVIER } from '#psammead/gel-foundations/src/typography';
 import { GEL_SPACING } from '#psammead/gel-foundations/src/spacings';
-import { RequestContext } from '#contexts/RequestContext';
-import { ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
 import { visuallyHiddenStyle } from '../utilities';
 
 const Figure = styled.figure`
   margin: 0;
-  background-color: ${({ isArticlePage }) =>
-    isArticlePage ? 'transparent' : C_BLACK};
+  background-color: transparent;
 `;
 
 const FigCaption = styled.figcaption`
   ${({ service }) => getSansRegular(service)}
   ${GEL_BREVIER}
-  
-  ${({ isArticlePage }) => `
-    color: ${isArticlePage ? C_GREY_6 : C_WHITE};
-    padding: ${isArticlePage ? `${GEL_SPACING} 0` : GEL_SPACING};
-  `}
+  color: ${C_GREY_6};
+  padding: ${GEL_SPACING} 0;
 
   > span {
     ${visuallyHiddenStyle}
@@ -40,13 +30,10 @@ const CaptionWrapper = ({
   text,
   additionalText,
 }) => {
-  const { pageType } = useContext(RequestContext);
-  const isArticlePage = pageType === ARTICLE_PAGE;
-
   return (
-    <Figure isArticlePage={isArticlePage}>
+    <Figure>
       {children}
-      <FigCaption isArticlePage={isArticlePage} service={service}>
+      <FigCaption service={service}>
         {textPrefixVisuallyHidden && <span>{textPrefixVisuallyHidden}</span>}
         {`${text}${additionalText ? ` ${additionalText}` : ''}`}
       </FigCaption>
