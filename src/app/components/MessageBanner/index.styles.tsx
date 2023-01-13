9745b12d435424420b8ae4a12fea50585e577114
pvaliani <pedram.homayoun-valiani@bbc.co.uk>
2023-01-13 11:40:39 +0000

    update margin for link background

--- src/app/components/MessageBanner/index.styles.tsx
+++ src/app/components/MessageBanner/index.styles.tsx
@@ -53,12 +53,12 @@ const styles = {
       width: '100%',
       [mq.GROUP_3_ONLY]: {
         width: 'auto',
-        margin: '0 0 1rem 0',
+        margin: '0 0 1.5rem 0',
         paddingBottom: '1rem',
       },
       [mq.GROUP_4_MIN_WIDTH]: {
         width: 'auto',
-        margin: '0 0 1rem 0',
+        margin: '0 0 1.5rem 0',
         paddingBottom: '1rem',
       },
     }),
