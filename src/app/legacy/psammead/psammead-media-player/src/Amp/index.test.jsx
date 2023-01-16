9495eda5921ae07d6d61bf426489492cbdbb07b8
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-01-16 14:20:12 +0000

    Suppress multiple prop type warnings

--- src/app/legacy/psammead/psammead-media-player/src/Amp/index.test.jsx
+++ src/app/legacy/psammead/psammead-media-player/src/Amp/index.test.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { shouldMatchSnapshot } from '#psammead/psammead-test-helpers/src';
+import {
+  shouldMatchSnapshot,
+  suppressPropWarnings,
+} from '#psammead/psammead-test-helpers/src';
 import { render } from '@testing-library/react';
 import Amp from '.';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('Media Player: Amp', () => {
+  suppressPropWarnings(['noJsMessage', 'AmpMediaPlayer', 'undefined']);
+  suppressPropWarnings(['service', 'AmpMediaPlayer', 'undefined']);
+  suppressPropWarnings(['service', 'Message', 'undefined']);
+  suppressPropWarnings(['message', 'Message', 'undefined']);
+
   shouldMatchSnapshot(
     'should render an amp-iframe with an amp-img nested inside',
     <Amp
