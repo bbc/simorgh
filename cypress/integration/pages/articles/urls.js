2f9a098a60035f3ba5b724208902ece1aedcec2b
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-01-19 17:32:47 +0000

    Add urls for article pages

--- cypress/integration/pages/articles/urls.js
+++ cypress/integration/pages/articles/urls.js
@@ -1,11 +1,97 @@
 export default [
   {
     service: 'afaanoromoo',
-    test: ['/afaanoromoo/articles/c4g19kgl85ko'],
+    test: ['https://test.bbc.com/afaanoromoo/articles/c4g19kgl85ko'],
   },
   {
     service: 'afrique',
-    local: ['/afrique/articles/cz216x22106o'],
-    test: ['/afrique/articles/cz216x22106o'],
+    local: ['http://localhost:7080/afrique/articles/cz216x22106o'],
+    test: ['https://test.bbc.com/afrique/articles/cz216x22106o'],
+  },
+  {
+    service: 'azeri',
+    live: ['https://www.bbc.com/azeri/articles/cv0lm08kngmo'],
+  },
+  {
+    service: 'japanese',
+    local: ['http://localhost:7080/japanese/articles/cdd6p3r9g7jo'],
+    test: ['https://www.test.bbc.com/japanese/articles/cdd6p3r9g7jo'],
+    live: ['https://www.bbc.com/japanese/articles/cj4m7n5nrd8o'],
+  },
+  {
+    service: 'kyrgyz',
+    live: ['https://www.bbc.com/kyrgyz/articles/c414v42gy75o'],
+    test: ['https://www.test.bbc.com/kyrgyz/articles/c3xd4xg3rm9o'],
+  },
+  {
+    service: 'mundo',
+    local: ['http://localhost:7080/mundo/articles/ce42wzqr2mko'],
+    test: ['https://www.test.bbc.com/mundo/articles/ce42wzqr2mko'],
+    live: ['https://www.bbc.com/mundo/articles/cdwrpl7qwqqo'],
+  },
+  {
+    service: 'nepali',
+    local: ['http://localhost:7080/nepali/articles/cl90j9m3mn6o'],
+    test: ['https://www.test.bbc.com/nepali/articles/cl90j9m3mn6o'],
+    live: ['https://www.bbc.com/nepali/articles/c16ljg1v008o'],
+  },
+  {
+    service: 'news',
+    local: ['http://localhost:7080/news/articles/cn7k01xp8kxo'],
+    test: ['https://www.test.bbc.com/news/articles/cn7k01xp8kxo'],
+    live: ['https://www.bbc.com/news/articles/cj7xrxz0e8zo'],
+  },
+  {
+    service: 'persian',
+    local: ['http://localhost:7080/persian/articles/cej3lzd5e0go'],
+    test: ['https://www.test.bbc.com/persian/articles/cej3lzd5e0go'],
+    live: ['https://www.bbc.com/persian/articles/c7eel0lmr4do'],
+  },
+  {
+    service: 'pidgin',
+    local: ['http://localhost:7080/pidgin/articles/cwl08rd38l6o'],
+    test: ['https://www.test.bbc.com/pidgin/articles/cwl08rd38l6o'],
+    live: ['https://www.bbc.com/pidgin/articles/cgwk9w4zlg8o'],
+  },
+  {
+    service: 'scotland',
+    local: ['http://localhost:7080/scotland/articles/czwj5l0n210o'],
+    live: ['https://www.bbc.com/scotland/articles/cm49v4x1r9lo'],
+  },
+  {
+    service: 'serbian',
+    variant: 'cyr',
+    local: ['http://localhost:7080/serbian/articles/c805k05kr73o/cyr'],
+  },
+  {
+    service: 'serbian',
+    variant: 'lat',
+    local: ['http://localhost:7080/serbian/articles/c805k05kr73o/lat'],
+  },
+  {
+    service: 'ukchina',
+    variant: 'simp',
+    local: ['http://localhost:7080/ukchina/articles/c0e8weny66ko/simp'],
+  },
+  {
+    service: 'ukchina',
+    variant: 'trad',
+    local: ['http://localhost:7080/ukchina/articles/c0e8weny66ko/trad'],
+  },
+  {
+    service: 'ukrainian',
+    live: ['https://www.bbc.com/ukrainian/articles/c8zv0eed9gko'],
+    test: ['https://www.test.bbc.com/ukrainian/articles/cp4l2mrejvdo'],
+    local: ['http://localhost:7080/ukrainian/articles/cp4l2mrejvdo'],
+  },
+  {
+    service: 'zhongwen',
+    variant: 'simp',
+    local: ['http://localhost:7080/zhongwen/articles/c3xd4x9prgyo/simp'],
+  },
+  {
+    service: 'zhongwen',
+    variant: 'trad',
+    local: ['http://localhost:7080/zhongwen/articles/c3xd4x9prgyo/simp'],
   },
 ];
