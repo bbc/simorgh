4382ed8947a000f83930ecba10fd17b71ff2cd50
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-01-23 12:47:04 +0000

    Revert localhost setting changes made to allow access on network

--- src/server/index.jsx
+++ src/server/index.jsx
@@ -69,7 +69,8 @@ const skipMiddleware = (_req, _res, next) => {
   next();
 };
 
-const injectCspHeaderProdBuild = skipMiddleware;
+const injectCspHeaderProdBuild =
+  process.env.NODE_ENV !== 'production' ? skipMiddleware : injectCspHeader;
 
 server
   .disable('x-powered-by')
