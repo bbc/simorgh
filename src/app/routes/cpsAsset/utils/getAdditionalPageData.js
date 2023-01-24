75e037e4d82ae23f294db05cfae0e79b1faa5372
mfonofm <mfon.essien@bbc.co.uk>
2023-01-24 16:45:44 +0000

    update: remove 'unirecs_allow_list' && 'unirecs_camino' endpoint

--- src/app/routes/cpsAsset/utils/getAdditionalPageData.js
+++ src/app/routes/cpsAsset/utils/getAdditionalPageData.js
@@ -24,17 +24,13 @@ const logger = nodeLogger(__filename);
 // 004_brasil_recommendations_experiment
 const getRecommendations = (service, assetUri) => {
   if (service !== 'portuguese') {
-    const UNIRECS_ALLOW_LIST = ['indonesia', 'mundo', 'turkce'];
-
     return [
       {
         name: 'recommendations',
         attachAgent: true,
         path: getRecommendationsUrl({
           assetUri,
-          ...(UNIRECS_ALLOW_LIST.includes(service) && {
-            engine: 'unirecs_datalab',
-          }),
+          engine: 'unirecs_datalab',
         }),
         assetUri,
         api: 'recommendations',
@@ -44,17 +40,6 @@ const getRecommendations = (service, assetUri) => {
   }
 
   return [
-    {
-      name: 'recommendations',
-      attachAgent: true,
-      path: getRecommendationsUrl({
-        assetUri,
-        engine: 'unirecs_camino',
-      }),
-      assetUri,
-      api: 'recommendations',
-      apiContext: 'secondary_data',
-    },
     {
       name: 'datalabContentRecommendations',
       attachAgent: true,
