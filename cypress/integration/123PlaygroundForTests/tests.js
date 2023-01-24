ce96f3198a01800b5c7b6129710e2c3fe93e5b10
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-01-24 09:56:32 +0000

    Delete unnecessary code

--- cypress/integration/123PlaygroundForTests/tests.js
+++
@@ -1,5 +0,0 @@
-// This function should be empty do not allow this to be merged if it isn't.
-export const testsThatFollowSmokeTestConfig = () => {};
-
-// This function should be empty do not allow this to be merged if it isn't.
-export const testsThatNeverRunDuringSmokeTesting = () => {};
