a7428315b14f06c7abcd7d4df5d0998e3a5b283a
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-01-25 18:22:57 +0000

    Do not render the message banner if the toggle is not enabled

--- src/app/components/MessageBanner/index.test.tsx
+++ src/app/components/MessageBanner/index.test.tsx
@@ -1,21 +1,54 @@
 import React from 'react';
+import { ToggleContextProvider } from '#app/contexts/ToggleContext';
 import { render, screen } from '../react-testing-library-with-providers';
-import MessageBanner from '.';
+import MessageBannerComponent from '.';
 import kyrgyzBanner from './fixtures';
 
-const { summaries } = kyrgyzBanner;
-const [summary] = summaries;
+const [summary] = kyrgyzBanner.summaries;
+
+interface MessageBanner {
+  summaries: {
+    imageUrl?: string;
+    link: string;
+    imageAlt?: string;
+    description?: string;
+    title: string;
+    id: string;
+  }[];
+  title: string;
+  enabled?: boolean;
+}
+
+const MessageBanner = ({ summaries, title, enabled = true }: MessageBanner) => (
+  <ToggleContextProvider
+    toggles={{
+      messageBanner: {
+        enabled,
+      },
+    }}
+  >
+    <MessageBannerComponent summaries={summaries} title={title} />
+  </ToggleContextProvider>
+);
 
 describe('MessageBanner', () => {
   it('should render a section with role region', () => {
-    render(<MessageBanner summaries={summaries} title={kyrgyzBanner.title} />);
+    render(
+      <MessageBanner
+        summaries={kyrgyzBanner.summaries}
+        title={kyrgyzBanner.title}
+      />,
+    );
     const region = screen.getByRole('region');
     expect(region).toBeInTheDocument();
   });
 
   it('should have a heading with an id which matches the aria-labelledby attribute', () => {
     const { getByRole } = render(
-      <MessageBanner summaries={summaries} title={kyrgyzBanner.title} />,
+      <MessageBanner
+        summaries={kyrgyzBanner.summaries}
+        title={kyrgyzBanner.title}
+      />,
     );
     const heading = screen.getByText(kyrgyzBanner.title);
     const messageBannerEl = getByRole('region');
@@ -25,24 +58,44 @@ describe('MessageBanner', () => {
   });
 
   it('should display the banner title correctly as an H2', () => {
-    render(<MessageBanner summaries={summaries} title={kyrgyzBanner.title} />);
+    render(
+      <MessageBanner
+        summaries={kyrgyzBanner.summaries}
+        title={kyrgyzBanner.title}
+      />,
+    );
     expect(screen.getByText(kyrgyzBanner.title).nodeName).toBe('H2');
   });
 
   it('should display the banner subtext correctly as a Paragraph', () => {
-    render(<MessageBanner summaries={summaries} title={kyrgyzBanner.title} />);
+    render(
+      <MessageBanner
+        summaries={kyrgyzBanner.summaries}
+        title={kyrgyzBanner.title}
+      />,
+    );
     expect(screen.getByText(summary.description).nodeName).toBe('P');
   });
 
   it('should display link text correctly as an Anchor', () => {
-    render(<MessageBanner summaries={summaries} title={kyrgyzBanner.title} />);
+    render(
+      <MessageBanner
+        summaries={kyrgyzBanner.summaries}
+        title={kyrgyzBanner.title}
+      />,
+    );
     const ctaLink = screen.getByRole('link');
     expect(ctaLink.getAttribute('href')).toEqual(summary.link);
     expect(ctaLink.textContent).toEqual(summary.title);
   });
 
   it('should render an image with the correct image src', () => {
-    render(<MessageBanner summaries={summaries} title={kyrgyzBanner.title} />);
+    render(
+      <MessageBanner
+        summaries={kyrgyzBanner.summaries}
+        title={kyrgyzBanner.title}
+      />,
+    );
     const image = screen.getByAltText('');
     expect(image.getAttribute('src')).toEqual(
       summary.imageUrl.replace('{width}', 'raw'),
@@ -50,8 +103,25 @@ describe('MessageBanner', () => {
   });
 
   it('should have an image with an empty alt text', () => {
-    render(<MessageBanner summaries={summaries} title={kyrgyzBanner.title} />);
+    render(
+      <MessageBanner
+        summaries={kyrgyzBanner.summaries}
+        title={kyrgyzBanner.title}
+      />,
+    );
     const image = screen.getByAltText('');
     expect(image).toBeInTheDocument();
+  });
+
+  it('should not render if messageBanner toggle not enabled', () => {
+    const { container } = render(
+      <MessageBanner
+        summaries={kyrgyzBanner.summaries}
+        title={kyrgyzBanner.title}
+        enabled={false}
+      />,
+    );
+
+    expect(container).toBeEmptyDOMElement();
   });
 });
--- src/app/components/MessageBanner/index.tsx
+++ src/app/components/MessageBanner/index.tsx
@@ -2,6 +2,7 @@
 /* @jsxFrag React.Fragment */
 import React, { useContext } from 'react';
 import { jsx } from '@emotion/react';
+import useToggle from '#hooks/useToggle';
 import Paragraph from '../Paragraph';
 import Heading from '../Heading';
 import Image from '../Image';
@@ -22,64 +23,69 @@ interface MessageBanner {
   title: string;
 }
 const MessageBanner = ({ summaries, title }: MessageBanner) => {
+  const { enabled: messageBannerEnabled } = useToggle('messageBanner');
+
   const { dir } = useContext(ServiceContext);
   const isRtl = dir === 'rtl';
 
-  return (
-    <section
-      css={styles.container}
-      role="region"
-      aria-labelledby="message-banner"
-    >
-      <div css={styles.card}>
-        {summaries.map(summary => {
-          return (
-            <React.Fragment key={summary.id}>
-              <div css={styles.textWrap}>
-                <Heading
-                  level={2}
-                  size="paragon"
-                  css={styles.heading}
-                  id="message-banner"
-                >
-                  {title}
-                </Heading>
-                <Paragraph size="longPrimer" css={styles.paragraph}>
-                  {summary.description}
-                </Paragraph>
-              </div>
-              <div css={styles.flex}>
-                <a href={summary.link} css={styles.linkBackground}>
-                  <div css={styles.linkAndChevron}>
-                    <Text
-                      size="pica"
-                      fontVariant="sansBold"
-                      css={isRtl ? styles.linkRtl : styles.linkLtr}
-                    >
-                      {summary.title}
-                      {isRtl ? (
-                        <LeftChevron css={styles.chevronRtl} />
-                      ) : (
-                        <RightChevron css={styles.chevronLtr} />
-                      )}
-                    </Text>
-                  </div>
-                </a>
-                {summary.imageUrl && (
-                  <div css={isRtl ? styles.imageRtl : styles.imageLtr}>
-                    <Image
-                      alt=""
-                      src={summary.imageUrl.replace('{width}', 'raw')}
-                      placeholder={false}
-                    />
-                  </div>
-                )}
-              </div>
-            </React.Fragment>
-          );
-        })}
-      </div>
-    </section>
-  );
+  if (messageBannerEnabled) {
+    return (
+      <section
+        css={styles.container}
+        role="region"
+        aria-labelledby="message-banner"
+      >
+        <div css={styles.card}>
+          {summaries.map(summary => {
+            return (
+              <React.Fragment key={summary.id}>
+                <div css={styles.textWrap}>
+                  <Heading
+                    level={2}
+                    size="paragon"
+                    css={styles.heading}
+                    id="message-banner"
+                  >
+                    {title}
+                  </Heading>
+                  <Paragraph size="longPrimer" css={styles.paragraph}>
+                    {summary.description}
+                  </Paragraph>
+                </div>
+                <div css={styles.flex}>
+                  <a href={summary.link} css={styles.linkBackground}>
+                    <div css={styles.linkAndChevron}>
+                      <Text
+                        size="pica"
+                        fontVariant="sansBold"
+                        css={isRtl ? styles.linkRtl : styles.linkLtr}
+                      >
+                        {summary.title}
+                        {isRtl ? (
+                          <LeftChevron css={styles.chevronRtl} />
+                        ) : (
+                          <RightChevron css={styles.chevronLtr} />
+                        )}
+                      </Text>
+                    </div>
+                  </a>
+                  {summary.imageUrl && (
+                    <div css={isRtl ? styles.imageRtl : styles.imageLtr}>
+                      <Image
+                        alt=""
+                        src={summary.imageUrl.replace('{width}', 'raw')}
+                        placeholder={false}
+                      />
+                    </div>
+                  )}
+                </div>
+              </React.Fragment>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
+  return null;
 };
 export default MessageBanner;
