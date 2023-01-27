4d1773b7c1e94671b76bf959b4a4e1e95aedd69c
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-01-27 15:03:43 +0000

    Revert Social Embed CPS container storybook changes and add Theme Provider to the testHelper

--- src/app/legacy/containers/SocialEmbed/common/testHelper.jsx
+++ src/app/legacy/containers/SocialEmbed/common/testHelper.jsx
@@ -4,6 +4,7 @@ import { RequestContextProvider } from '#contexts/RequestContext';
 import { STORY_PAGE } from '#app/routes/utils/pageTypes';
 import { ServiceContextProvider } from '../../../../contexts/ServiceContext';
 import { ToggleContextProvider } from '../../../../contexts/ToggleContext';
+import ThemeProvider from '../../../../components/ThemeProvider';
 
 /**
  * withContexts is a higher-order component that returns a React component
@@ -15,22 +16,24 @@ const withContexts =
   (Component, { isAmp, service = 'news', pageType = STORY_PAGE }) =>
   props =>
     (
-      <RequestContextProvider
-        isAmp={isAmp}
-        pageType={pageType}
-        service={service}
-        pathname="/pathname"
-      >
-        <ServiceContextProvider service={service}>
-          <ToggleContextProvider
-            toggles={{
-              eventTracking: { enabled: false },
-            }}
-          >
-            <Component {...props} />
-          </ToggleContextProvider>
-        </ServiceContextProvider>
-      </RequestContextProvider>
+      <ThemeProvider service={service}>
+        <RequestContextProvider
+          isAmp={isAmp}
+          pageType={pageType}
+          service={service}
+          pathname="/pathname"
+        >
+          <ServiceContextProvider service={service}>
+            <ToggleContextProvider
+              toggles={{
+                eventTracking: { enabled: false },
+              }}
+            >
+              <Component {...props} />
+            </ToggleContextProvider>
+          </ServiceContextProvider>
+        </RequestContextProvider>
+      </ThemeProvider>
     );
 
 export default withContexts;
