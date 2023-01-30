1b4102fdbdb9ed8f16cf07d3d92c5732ceecf25b
DarioR01 <dario.russo@bbc.co.uk>
2023-01-30 10:12:54 +0000

    feat: unit tests

---
+++ .storybook/SidebarLabel/index.test.tsx
@@ -0,0 +1,140 @@
+import React from 'react';
+import SidebarLabel from '.';
+import { render, screen, act } from '@testing-library/react';
+import * as api from '@storybook/api';
+
+describe('Storybook Sidebar Labels', () => {
+  beforeEach(() => {
+    api.useStorybookApi = jest.fn().mockImplementation(() => {
+      return {
+        getData: () => {
+          return {
+            parameters: {
+              metadata: {
+                uxAccessibilityDoc: {
+                  done: false,
+                },
+                acceptanceCriteria: {
+                  done: true,
+                },
+                swarm: {
+                  done: true,
+                },
+              },
+            },
+          };
+        },
+      };
+    });
+  });
+
+  it('should return name when type is root', async () => {
+    await act(async () => {
+      render(
+        <SidebarLabel
+          item={{
+            type: 'root',
+            id: 'components-healthfactors',
+            name: 'HealthFactors',
+            parent: 'components',
+            depth: 1,
+            children: [
+              'components-healthfactors--health-factors-with-complete-docs',
+              'components-healthfactors--health-factors-with-outstanding-docs',
+              'components-healthfactors--health-factors-with-invalid-data',
+              'components-healthfactors--health-factors-with-no-data',
+            ],
+            isComponent: true,
+            isLeaf: false,
+            isRoot: false,
+          }}
+        />,
+      );
+    });
+
+    const text = screen.getByText('HealthFactors');
+    expect(text).toBeInTheDocument();
+  });
+
+  it('should return name when type is group', async () => {
+    await act(async () => {
+      render(
+        <SidebarLabel
+          item={{
+            type: 'group',
+            id: 'components-healthfactors',
+            name: 'HealthFactors',
+            parent: 'components',
+            depth: 1,
+            children: [
+              'components-healthfactors--health-factors-with-complete-docs',
+              'components-healthfactors--health-factors-with-outstanding-docs',
+              'components-healthfactors--health-factors-with-invalid-data',
+              'components-healthfactors--health-factors-with-no-data',
+            ],
+            isComponent: true,
+            isLeaf: false,
+            isRoot: false,
+          }}
+        />,
+      );
+
+      const text = screen.getByText('HealthFactors');
+      expect(text).toBeInTheDocument();
+    });
+  });
+
+  it('should return SideBar component when type is component', async () => {
+    await act(async () => {
+      render(
+        <SidebarLabel
+          item={{
+            type: 'component',
+            id: 'components-healthfactors',
+            name: 'HealthFactors',
+            parent: 'components',
+            depth: 1,
+            children: [
+              'components-healthfactors--health-factors-with-complete-docs',
+              'components-healthfactors--health-factors-with-outstanding-docs',
+              'components-healthfactors--health-factors-with-invalid-data',
+              'components-healthfactors--health-factors-with-no-data',
+            ],
+            isComponent: true,
+            isLeaf: false,
+            isRoot: false,
+          }}
+        />,
+      );
+    });
+    const text = screen.getByText(', Component Health: incomplete');
+    expect(text).toBeInTheDocument();
+  });
+
+  it('should return SideBar component when type is story', async () => {
+    await act(async () => {
+      render(
+        <SidebarLabel
+          item={{
+            type: 'story',
+            id: 'components-healthfactors',
+            name: 'HealthFactors',
+            parent: 'components',
+            depth: 1,
+            children: [
+              'components-healthfactors--health-factors-with-complete-docs',
+              'components-healthfactors--health-factors-with-outstanding-docs',
+              'components-healthfactors--health-factors-with-invalid-data',
+              'components-healthfactors--health-factors-with-no-data',
+            ],
+            isComponent: true,
+            isLeaf: false,
+            isRoot: false,
+          }}
+        />,
+      );
+    });
+    const text = screen.getByText(', Component Health: incomplete');
+    expect(text).toBeInTheDocument();
+  });
+});
