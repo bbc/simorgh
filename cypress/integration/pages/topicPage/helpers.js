eb65d144cc32fff2c0338e331fa12a96d03da2a3
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-02-03 09:12:09 +0000

    Fix logic and rename utility

--- cypress/integration/pages/topicPage/helpers.js
+++ cypress/integration/pages/topicPage/helpers.js
@@ -1,16 +1,17 @@
 /* eslint-disable import/prefer-default-export */
 import envConfig from '../../../support/config/envs';
+import getAppEnv from '../../../support/helpers/getAppEnv';
 
-export const getTopicPageUrl = currentPath => {
+export const getTopicPagePath = currentPath => {
   const url = new URL(`${envConfig.baseUrl}${currentPath}`);
   const params = new URLSearchParams(url.search);
 
   /**
    * Check whether the URL has the renderer_env override already
    * If so, do nothing
-   * Otherwise, append renderer_env=live to the url to ensure that the test accesses the correct page
+   * Otherwise, append renderer_env=live to the url when running on test to ensure that the correct topic page is accessed
    */
-  if (!params.get('renderer_env')) {
+  if (!params.get('renderer_env') && getAppEnv() === 'TEST') {
     params.append('renderer_env', 'live');
   }
 
--- cypress/integration/pages/topicPage/index.js
+++ cypress/integration/pages/topicPage/index.js
@@ -4,7 +4,7 @@ import serviceHasPageType from '../../../support/helpers/serviceHasPageType';
 
 import crossPlatformTests from './tests';
 import visitPage from '../../../support/helpers/visitPage';
-import { getTopicPageUrl } from './helpers';
+import { getTopicPagePath } from './helpers';
 
 const pageType = 'topicPage';
 Object.keys(config)
@@ -17,8 +17,7 @@ Object.keys(config)
       describe(`${pageType} - ${currentPath}`, () => {
         before(() => {
           Cypress.env('currentPath', currentPath);
-
-          visitPage(getTopicPageUrl(currentPath), pageType);
+          visitPage(getTopicPagePath(currentPath), pageType);
         });
         crossPlatformTests({
           service,
