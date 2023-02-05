4bbb73b9350ab7502b49e1d317930b94f31e90d7
Aaron Moore <aaron.moore@bbc.co.uk>
2023-02-05 14:04:46 +0000

    Hydrate ClientApp when DOM is loaded

--- src/app/legacy/containers/App/index.jsx
+++ src/app/legacy/containers/App/index.jsx
@@ -8,11 +8,25 @@ import { StaticRouter, BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 export class ClientApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hydrated: false,
+    };
+  }
   // Having an error boundary here means that if hydration fails, users are left with the server-rendered DOM
   // Without this, DOM would be removed if hydration fails, leaving users with a blank white page
   componentDidCatch() {}
 
+  componentDidMount() {
+    this.setState({ hydrated: true });
+  }
+
   render() {
+    const { hydrated } = this.state;
+
+    if (!hydrated) return null;
+
     return (
       <BrowserRouter {...this.props}>
         <App initialData={this.props.data} />
