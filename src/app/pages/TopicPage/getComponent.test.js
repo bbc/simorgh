9527368ad1f7d1a465a3cc7d5607617c3e5ac053
louisearchibald <louise.archibald@bbc.co.uk>
2023-02-09 09:49:05 +0000

    refactors tests to use an it.each

--- src/app/pages/TopicPage/getComponent.test.js
+++ src/app/pages/TopicPage/getComponent.test.js
@@ -2,20 +2,15 @@ import getComponent from './getComponent';
 import { VISUAL_PROMINENCE, VISUAL_STYLE, COMPONENT_NAMES } from './constants';
 
 describe('getComponent', () => {
-  it('should render a message banner when visual style is BANNER and visual prominence is NORMAL', () => {
-    const component = getComponent(
-      VISUAL_STYLE.BANNER,
-      VISUAL_PROMINENCE.NORMAL,
-    );
-    expect(component).toBe(COMPONENT_NAMES.MESSAGE_BANNER);
-  });
-
-  it('should render a simple curation grid when visual style is NONE and visual prominence is NORMAL', () => {
-    const component = getComponent(VISUAL_STYLE.NONE, VISUAL_PROMINENCE.NORMAL);
-    expect(component).toBe(COMPONENT_NAMES.SIMPLE_CURATION_GRID);
-  });
-  it('should render a hierarchical curation grid when visual style is NONE and visual prominence is HIGH', () => {
-    const component = getComponent(VISUAL_STYLE.NONE, VISUAL_PROMINENCE.HIGH);
-    expect(component).toBe(COMPONENT_NAMES.HIERARCHICAL_CURATION_GRID);
-  });
+  it.each`
+    visualStyle            | visualProminence            | expected
+    ${VISUAL_STYLE.BANNER} | ${VISUAL_PROMINENCE.NORMAL} | ${COMPONENT_NAMES.MESSAGE_BANNER}
+    ${VISUAL_STYLE.NONE}   | ${VISUAL_PROMINENCE.NORMAL} | ${COMPONENT_NAMES.SIMPLE_CURATION_GRID}
+    ${VISUAL_STYLE.NONE}   | ${VISUAL_PROMINENCE.HIGH}   | ${COMPONENT_NAMES.HIERARCHICAL_CURATION_GRID}
+  `(
+    'should render $expected when visual style is $visualStyle and visual prominence is $visualProminence',
+    ({ visualStyle, visualProminence, expected }) => {
+      expect(getComponent(visualStyle, visualProminence)).toBe(expected);
+    },
+  );
 });
