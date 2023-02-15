41f36be6d3ada9b247917fd53d977c18bc23902f
SHAYNE MARC ENZO AHCHOON <shayne.ahchoon@bbc.co.uk>
2023-02-15 12:03:43 +0000

    WSTEAM1-262: Add Pidgin to services

--- src/server/utilities/mvtHeader/enabledExperimentsList.js
+++ src/server/utilities/mvtHeader/enabledExperimentsList.js
@@ -10,7 +10,7 @@ Add enabled experiments objects inside this array in this format:
 const enabledExperimentList = [
   {
     name: 'test_2',
-    services: ['portuguese'],
+    services: ['pidgin'],
     pageTypes: ['STY', 'article'],
   },
 ];
