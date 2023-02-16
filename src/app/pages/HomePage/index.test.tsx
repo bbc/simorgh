8de26eedc445a78515d0b55a59611c00f41678f1
louisearchibald <louise.archibald@bbc.co.uk>
2023-02-16 17:21:31 +0000

    adds unit test to check if text is being rendered

--- src/app/pages/HomePage/index.test.tsx
+++ src/app/pages/HomePage/index.test.tsx
@@ -1,1 +1,13 @@
-export default () => {};
+import React from 'react';
+import { render } from '../../components/react-testing-library-with-providers';
+import HomePage from './HomePage';
+
+describe('Home Page', () => {
+  it('should render a hello message', () => {
+    const { container } = render(<HomePage />);
+    expect(container).not.toBeEmptyDOMElement();
+    expect(container.firstChild?.textContent).toBe(
+      'Hi, I am a Home Page component!',
+    );
+  });
+});
