5a508c4fd8b64e7e687647ecc0eb06423ce32158
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-02-17 10:58:33 +0000

    Refactor - do not delete the SIMORGH_APP_ENV environment variable in the test

--- src/app/lib/utilities/isLocal/index.test.js
+++ src/app/lib/utilities/isLocal/index.test.js
@@ -1,6 +1,12 @@
 import isLocal from '.';
 
 describe('isLocal', () => {
+  const originalEnvironment = process.env.SIMORGH_APP_ENV;
+
+  afterEach(() => {
+    process.env.SIMORGH_APP_ENV = originalEnvironment;
+  });
+
   it('should return true when SIMORGH_APP_ENV is local', () => {
     process.env.SIMORGH_APP_ENV = 'local';
     expect(isLocal()).toBe(true);
@@ -9,9 +15,5 @@ describe('isLocal', () => {
   it('should return false when SIMORGH_APP_ENV is not local', () => {
     process.env.SIMORGH_APP_ENV = 'not-local';
     expect(isLocal()).toBe(false);
-  });
-
-  afterAll(() => {
-    delete process.env.SIMORGH_APP_ENV;
   });
 });
