b4c81e1ceae829877137b03176dbea864372bf00
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-02-20 09:49:32 +0000

    Simplify test

--- src/app/lib/utilities/isLocal/index.test.js
+++ src/app/lib/utilities/isLocal/index.test.js
@@ -7,13 +7,15 @@ describe('isLocal', () => {
     process.env.SIMORGH_APP_ENV = originalEnvironment;
   });
 
-  it('should return true when SIMORGH_APP_ENV is local', () => {
-    process.env.SIMORGH_APP_ENV = 'local';
-    expect(isLocal()).toBe(true);
-  });
-
-  it('should return false when SIMORGH_APP_ENV is not local', () => {
-    process.env.SIMORGH_APP_ENV = 'not-local';
-    expect(isLocal()).toBe(false);
-  });
+  it.each`
+    environment    | expected
+    ${'local'}     | ${true}
+    ${'not-local'} | ${false}
+  `(
+    'should return $expected when environment is $environment',
+    ({ environment, expected }) => {
+      process.env.SIMORGH_APP_ENV = environment;
+      expect(isLocal()).toBe(expected);
+    },
+  );
 });
