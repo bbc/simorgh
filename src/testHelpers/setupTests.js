1cb1fbaa6196ec74f84e347223d9bb9ed208f472
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-02-21 14:11:09 +0000

    Remove chalk from jest test setup

--- src/testHelpers/setupTests.js
+++ src/testHelpers/setupTests.js
@@ -1,5 +1,4 @@
 import '@testing-library/jest-dom/extend-expect';
-import chalk from 'chalk';
 
 const REACT_FAILED_PROP_TYPE = 'Failed prop';
 const REACT_NO_KEYS = 'Each child in a list should have a unique "key" prop';
@@ -49,10 +48,8 @@ console.error = (message, ...rest) => {
   if (REACT_ERRORS_REGEX.test(message)) {
     throw new Error(
       [
-        chalk.red.bold(
-          'Test failed because a React warning was detected. Please fix the following:',
-        ),
-        chalk.red(message),
+        'Test failed because a React warning was detected. Please fix the following:',
+        message,
       ].join('\n'),
     );
   }
