5ca9269a05bafbab5262289c4e24623ca9062c97
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-03-02 10:44:22 +0000

    WS-327: Fix failing unit test

--- src/app/pages/ErrorPage/index.test.jsx
+++ src/app/pages/ErrorPage/index.test.jsx
@@ -6,25 +6,45 @@ import ThemeProvider from '../../components/ThemeProvider';
 
 jest.mock('../../components/ThemeProvider');
 
-const Context = ({
+const ErrorPage404 = ({
   /* eslint-disable react/prop-types */
   service,
   /* eslint-enable react/prop-types */
 }) => (
   <ThemeProvider service={service}>
     <ServiceContextProvider service={service}>
-      <ErrorPage />
+      <ErrorPage errorCode={404} />
+    </ServiceContextProvider>
+  </ThemeProvider>
+);
+
+const ErrorPage500 = ({
+  /* eslint-disable react/prop-types */
+  service,
+  /* eslint-enable react/prop-types */
+}) => (
+  <ThemeProvider service={service}>
+    <ServiceContextProvider service={service}>
+      <ErrorPage errorCode={500} />
+    </ServiceContextProvider>
+  </ThemeProvider>
+);
+
+const ErrorPageOtherCode = ({
+  /* eslint-disable react/prop-types */
+  service,
+  /* eslint-enable react/prop-types */
+}) => (
+  <ThemeProvider service={service}>
+    <ServiceContextProvider service={service}>
+      <ErrorPage errorCode={123} />
     </ServiceContextProvider>
   </ThemeProvider>
 );
 
 describe('ErrorPage', () => {
   it('should correctly render for 404', async () => {
-    const { container } = render(
-      <Context service="news">
-        <ErrorPage errorCode={404} />
-      </Context>,
-    );
+    const { container } = render(<ErrorPage404 service="news" />);
 
     await waitFor(() => {
       expect(container).toMatchSnapshot();
@@ -32,11 +52,7 @@ describe('ErrorPage', () => {
   });
 
   it('should correctly render for 500', async () => {
-    const { container } = render(
-      <Context service="news">
-        <ErrorPage errorCode={500} />
-      </Context>,
-    );
+    const { container } = render(<ErrorPage500 service="news" />);
 
     await waitFor(() => {
       expect(container).toMatchSnapshot();
@@ -44,11 +60,7 @@ describe('ErrorPage', () => {
   });
 
   it('should correctly render for other status code', async () => {
-    const { container } = render(
-      <Context service="news">
-        <ErrorPage errorCode={123} />
-      </Context>,
-    );
+    const { container } = render(<ErrorPageOtherCode service="news" />);
 
     await waitFor(() => {
       expect(container).toMatchSnapshot();
@@ -56,11 +68,7 @@ describe('ErrorPage', () => {
   });
 
   it('should correctly render for 404 for persian', async () => {
-    const { container } = render(
-      <Context service="persian">
-        <ErrorPage errorCode={404} />
-      </Context>,
-    );
+    const { container } = render(<ErrorPage404 service="persian" />);
 
     await waitFor(() => {
       expect(container).toMatchSnapshot();
@@ -68,52 +76,10 @@ describe('ErrorPage', () => {
   });
 
   it('should correctly render for 500 for persian', async () => {
-    const { container } = render(
-      <Context service="persian">
-        <ErrorPage errorCode={500} />
-      </Context>,
-    );
+    const { container } = render(<ErrorPage500 service="persian" />);
 
     await waitFor(() => {
       expect(container).toMatchSnapshot();
     });
   });
-
-  // describe('ErrorPage', () => {
-  //   shouldMatchSnapshot(
-  //     'should correctly render for 404',
-  //     <ThemeProvider service="news">
-  //       <ServiceContextProvider service="news">
-  //         <ErrorPage errorCode={404} />
-  //       </ServiceContextProvider>
-  //     </ThemeProvider>,
-  //   );
-
-  // shouldMatchSnapshot(
-  //   'should correctly render for 500',
-  //   <ServiceContextProvider service="news">
-  //     <ErrorPage errorCode={500} />
-  //   </ServiceContextProvider>,
-  // );
-
-  // shouldMatchSnapshot(
-  //   'should correctly render for other status code',
-  //   <ServiceContextProvider service="news">
-  //     <ErrorPage errorCode={123} />
-  //   </ServiceContextProvider>,
-  // );
-
-  // shouldMatchSnapshot(
-  //   'should correctly render for 404 for persian',
-  //   <ServiceContextProvider service="persian">
-  //     <ErrorPage errorCode={404} />
-  //   </ServiceContextProvider>,
-  // );
-
-  // shouldMatchSnapshot(
-  //   'should correctly render for 500 for persian',
-  //   <ServiceContextProvider service="persian">
-  //     <ErrorPage errorCode={500} />
-  //   </ServiceContextProvider>,
-  // );
 });
