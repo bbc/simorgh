6894e289b02c56a444754778ead62f6b030e8251
LilyL0u <lily.higham@bbc.co.uk>
2023-03-06 16:58:06 +0000

    gets id from config and displays on page

--- src/app/pages/HomePage/HomePage.tsx
+++ src/app/pages/HomePage/HomePage.tsx
@@ -2,7 +2,7 @@
 /* @jsxFrag React.Fragment */
 import { jsx } from '@emotion/react';
 
-const HomePage = () => {
-  return <div>Hi, I am a Home Page component!</div>;
+const HomePage = ({ pageData }) => {
+  return <div>Hi, I am a Home Page component! My id is {pageData.id}</div>;
 };
 export default HomePage;
--- src/app/routes/homePage/getInitialData/index.js
+++ src/app/routes/homePage/getInitialData/index.js
@@ -1,6 +1,20 @@
-export default async () => {
+import HOME_PAGE_TEST_CONFIG from './test-config';
+
+export default async ({ path: pathname, service }) => {
+  let environment = process.env.SIMORGH_APP_ENV;
+
+  if (pathname.includes('renderer_env=test')) {
+    environment = 'test';
+  }
+  if (pathname.includes('renderer_env=live')) {
+    environment = 'live';
+  }
+  const id = environment === 'live' ? null : HOME_PAGE_TEST_CONFIG[service];
+  console.log(`XXXX${id}`);
   return {
     status: 200,
-    pageData: {},
+    pageData: {
+      id,
+    },
   };
 };
