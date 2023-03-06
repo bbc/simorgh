1b14a4819779cc98c83d5799f3ad6815592022ee
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-03-06 15:01:07 +0000

    WS-327: Refactor ErrorPage useTheme() hook usage

--- src/app/pages/ErrorPage/ErrorPage.jsx
+++ src/app/pages/ErrorPage/ErrorPage.jsx
@@ -9,21 +9,17 @@ import { ServiceContext } from '../../contexts/ServiceContext';
  * MVP Metadata for the error
  * This will be refactored out in https://github.com/bbc/simorgh/issues/1350
  */
-const ErrorMetadata = ({ dir, lang, messaging, brandName }) => {
+const ErrorMetadata = ({ dir, lang, messaging, brandName, themeColor }) => {
   const { title } = messaging;
 
   const pageTitle = `${title} - ${brandName}`;
-
-  const {
-    palette: { BRAND_BACKGROUND },
-  } = useTheme();
 
   return (
     <Helmet htmlAttributes={{ lang, dir }}>
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta charSet="utf-8" />
       <meta name="robots" content="noindex,nofollow" />
-      <meta name="theme-color" content={BRAND_BACKGROUND} />
+      <meta name="theme-color" content={themeColor} />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, minimum-scale=1"
@@ -70,6 +66,7 @@ ErrorMetadata.propTypes = {
   lang: string.isRequired,
   messaging: shape({ title: string.isRequired }).isRequired,
   brandName: string.isRequired,
+  themeColor: string.isRequired,
 };
 
 export default ErrorPage;
