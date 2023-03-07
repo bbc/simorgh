1a79c3029592b39134fedca71fe03c7437764647
Karina Thomas <58214768+karinathomasbbc@users.noreply.github.com>
2023-03-07 14:00:27 +0000

    Merge pull request #10665 from bbc/fetch-topic-data-on-local
    NEWSWORLDSERVICE-1615: Use fixture data from local filesystem for topic pages

--- src/app/routes/article/getInitialData/index.ts
+++ src/app/routes/article/getInitialData/index.ts
@@ -4,6 +4,7 @@ import pipe from 'ramda/src/pipe';
 import pathOr from 'ramda/src/pathOr';
 import Url from 'url-parse';
 import getAdditionalPageData from '#app/routes/cpsAsset/utils/getAdditionalPageData';
+import getEnvironment from '#app/routes/utils/getEnvironment';
 import nodeLogger from '../../../lib/logger.node';
 import { getUrlPath } from '../../../lib/utilities/urlParser';
 import { BFF_FETCH_ERROR } from '../../../lib/logger.const';
@@ -19,17 +20,6 @@ const getCpsId = (path: string) => path;
 
 const getId = (pageType: string) =>
   pipe(getUrlPath, removeAmp, pageType === 'article' ? getOptimoId : getCpsId);
-
-const getEnvironment = (pathname: string) => {
-  if (pathname.includes('renderer_env=test')) {
-    return 'test';
-  }
-  if (pathname.includes('renderer_env=live')) {
-    return 'live';
-  }
-
-  return process.env.SIMORGH_APP_ENV;
-};
 
 interface BFFError extends Error {
   status: number;
---
+++ src/app/routes/utils/getEnvironment/index.ts
@@ -0,0 +1,10 @@
+export default (pathname: string) => {
+  if (pathname.includes('renderer_env=test')) {
+    return 'test';
+  }
+  if (pathname.includes('renderer_env=live')) {
+    return 'live';
+  }
+
+  return process.env.SIMORGH_APP_ENV;
+};
