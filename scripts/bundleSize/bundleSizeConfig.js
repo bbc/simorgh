d550617df40567de91149928a11ac818af5bee28
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-03-08 18:17:02 +0000

    Update bundle size

--- scripts/bundleSize/bundleSizeConfig.js
+++ scripts/bundleSize/bundleSizeConfig.js
@@ -5,6 +5,6 @@ module.exports = {
   // below the smallest value in the build output; this avoids the
   // need for frequent changes as bundle sizes fluctuate.
 
-  MIN_SIZE: 766,
-  MAX_SIZE: 1213,
+  MIN_SIZE: 793,
+  MAX_SIZE: 1234,
 };
