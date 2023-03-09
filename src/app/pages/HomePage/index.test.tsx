c05e9c4716cf7a999b2703ddacabf0e71d066106
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-03-09 15:33:32 +0000

    Fix test

--- src/app/pages/HomePage/index.test.tsx
+++ src/app/pages/HomePage/index.test.tsx
@@ -3,11 +3,11 @@ import { render } from '../../components/react-testing-library-with-providers';
 import HomePage from './HomePage';
 
 describe('Home Page', () => {
-  it('should render a hello message', () => {
-    const { container } = render(<HomePage />);
+  it('should render a hello message with id from page data', () => {
+    const { container } = render(<HomePage pageData={{ id: '12345' }} />);
     expect(container).not.toBeEmptyDOMElement();
     expect(container.firstChild?.textContent).toBe(
-      'Hi, I am a Home Page component!',
+      'Hi, I am a Home Page component! My id is 12345',
     );
   });
 });
