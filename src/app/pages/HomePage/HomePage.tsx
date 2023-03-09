015af23ffefe5aa864deed0567d656b39894d5e1
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-03-09 15:30:45 +0000

    Fix typescript issue

--- src/app/pages/HomePage/HomePage.tsx
+++ src/app/pages/HomePage/HomePage.tsx
@@ -2,7 +2,13 @@
 /* @jsxFrag React.Fragment */
 import { jsx } from '@emotion/react';
 
-const HomePage = ({ pageData }) => {
+type HomePageProps = {
+  pageData: {
+    id: string;
+  };
+};
+
+const HomePage = ({ pageData }: HomePageProps) => {
   return <div>Hi, I am a Home Page component! My id is {pageData.id}</div>;
 };
 export default HomePage;
