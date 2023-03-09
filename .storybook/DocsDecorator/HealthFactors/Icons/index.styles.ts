eaf99ef4488cd162321f6f14dc56fb6d160b8095
DarioR01 <dario.russo@bbc.co.uk>
2023-03-09 13:11:13 +0000

    fix: remove unneeded imports

--- .storybook/DocsDecorator/HealthFactors/Icons/index.styles.ts
+++ .storybook/DocsDecorator/HealthFactors/Icons/index.styles.ts
@@ -1,12 +1,8 @@
-import { css, Theme } from '@emotion/react';
-import pixelsToRem from '../../../../src/app/utilities/pixelsToRem';
-
 const styles = {
-  icon: (theme: Theme) =>
-    css({
-      display: 'block',
-      fill: 'currentcolor',
-    }),
+  icon: {
+    display: 'block',
+    fill: 'currentcolor',
+  },
 };
 
 export default styles;
