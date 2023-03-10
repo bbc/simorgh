1319e6d01bf483dce16018eaeacf8655c47a575c
Aaron Moore <aaron.moore@bbc.co.uk>
2023-03-10 11:08:36 +0000

    revert: Does this work?

--- scripts/bundleSize/bundleSizeConfig.js
+++ scripts/bundleSize/bundleSizeConfig.js
@@ -5,6 +5,6 @@ module.exports = {
   // below the smallest value in the build output; this avoids the
   // need for frequent changes as bundle sizes fluctuate.
 
-  MIN_SIZE: 732,
-  MAX_SIZE: 1217,
+  MIN_SIZE: 766,
+  MAX_SIZE: 1220,
 };
