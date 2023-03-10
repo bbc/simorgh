7e305cfd6e81a95f915538ac1b2e763b52188e7e
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-03-10 10:40:06 +0000

    WS-329: Update legacy colour refs in Promo component

--- src/app/legacy/components/Promo/body.jsx
+++ src/app/legacy/components/Promo/body.jsx
@@ -2,12 +2,11 @@ import styled from '@emotion/styled';
 
 import { getSansRegular } from '#psammead/psammead-styles/src/font-styles';
 import { getBodyCopy } from '#psammead/gel-foundations/src/typography';
-import { C_EBON } from '#psammead/psammead-styles/src/colours';
 
 const P = styled.p`
   ${({ service }) => getSansRegular(service)}
   ${({ script }) => getBodyCopy(script)}
-  color: ${C_EBON}
+  color: ${props => props.theme.palette.EBON}
 `;
 
 export default P;
