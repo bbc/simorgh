873df05d93117a9af5057933fba9472da2b599df
SHAYNE MARC ENZO AHCHOON <shayne.ahchoon@bbc.co.uk>
2023-03-14 13:45:34 +0000

    WSTEAM1-282: Add ad bootstrap back in

--- src/app/pages/MediaArticlePage/MediaArticlePage.jsx
+++ src/app/pages/MediaArticlePage/MediaArticlePage.jsx
@@ -28,6 +28,7 @@ import { C_GREY_2, C_WHITE } from '#psammead/psammead-styles/src/colours';
 import { singleTextBlock } from '#app/models/blocks';
 import { articleDataPropTypes } from '#models/propTypes/article';
 import ArticleMetadata from '#containers/ArticleMetadata';
+import { RequestContext } from '#contexts/RequestContext';
 import headings from '#containers/Headings';
 import visuallyHiddenHeadline from '#containers/VisuallyHiddenHeadline';
 import gist from '#containers/Gist';
@@ -46,6 +47,7 @@ import MostReadContainer from '#containers/MostRead';
 import MostReadSection from '#containers/MostRead/section';
 import MostReadSectionLabel from '#containers/MostRead/label';
 import SocialEmbedContainer from '#containers/SocialEmbed';
+import CanonicalAdBootstrapJs from '#containers/Ad/Canonical/CanonicalAdBootstrapJs';
 
 import {
   getArticleId,
@@ -114,10 +116,20 @@ const StyledRelatedTopics = styled(RelatedTopics)`
 `;
 
 const MediaArticlePage = ({ pageData, mostReadEndpointOverride }) => {
+  const { isAmp, showAdsBasedOnLocation } = useContext(RequestContext);
   const { articleAuthor, isTrustProjectParticipant, showRelatedTopics } =
     useContext(ServiceContext);
   const { enabled: preloadLeadImageToggle } = useToggle('preloadLeadImage');
+  const { enabled: adsEnabled } = useToggle('ads');
   const recommendationsData = path(['recommendations'], pageData);
+
+  const isAdsEnabled = [
+    path(['metadata', 'allowAdvertising'], pageData),
+    adsEnabled,
+    showAdsBasedOnLocation,
+  ].every(Boolean);
+
+  const adcampaign = path(['metadata', 'adCampaignKeyword'], pageData);
 
   const headline = getHeadline(pageData);
   const description = getSummary(pageData) || getHeadline(pageData);
@@ -248,6 +260,9 @@ const MediaArticlePage = ({ pageData, mostReadEndpointOverride }) => {
         aboutTags={aboutTags}
         imageLocator={promoImage}
       />
+      {isAdsEnabled && !isAmp && (
+        <CanonicalAdBootstrapJs adcampaign={adcampaign} />
+      )}
       <MediaArticlePageGrid>
         <Primary>
           <Main role="main">
