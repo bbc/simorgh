fc2bbf380340522a335e0ec731fd903ff2d68f61
louisearchibald <louise.archibald@bbc.co.uk>
2023-03-15 16:49:45 +0000

    maps over curations & renders data in the browser

--- src/app/pages/HomePage/HomePage.tsx
+++ src/app/pages/HomePage/HomePage.tsx
@@ -1,20 +1,51 @@
 /** @jsx jsx */
 /* @jsxFrag React.Fragment */
+import React, { useContext } from 'react';
 import { jsx } from '@emotion/react';
+import { CurationData } from '#app/models/types/promoData';
+import Curation from '#app/components/Curation';
+import { ServiceContext } from '../../contexts/ServiceContext';
 
 interface HomePageProps {
   pageData: {
     id: string;
     title: string;
+    curations: CurationData[];
   };
 }
 
 const HomePage = ({ pageData }: HomePageProps) => {
-  return (
-    <div>
-      Hi, I am a Home Page component and your page id is {pageData.id} and the
-      title is {pageData.title}!
-    </div>
+  const { curations } = pageData;
+  const { translations } = useContext(ServiceContext);
+  const { topStoriesTitle } = translations;
+
+  return curations.map(
+    ({
+      visualProminence,
+      summaries,
+      curationId,
+      title: curationTitle,
+      link,
+      position,
+      visualStyle,
+    }) => {
+      return (
+        <React.Fragment key={`${curationId}-${position}`}>
+          <Curation
+            // headingLevel={curationTitle && 3}
+            visualStyle={visualStyle}
+            visualProminence={visualProminence}
+            promos={summaries}
+            title={curationTitle}
+            topStoriesTitle={topStoriesTitle}
+            position={position}
+            link={link}
+            curationLength={curations && curations.length}
+          />
+        </React.Fragment>
+      );
+    },
   );
 };
+
 export default HomePage;
