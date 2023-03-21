27e5f79e0cf323b259c0bf85cf6d1dd4cd89aa7f
Aaron Moore <aaron.moore@bbc.co.uk>
2023-03-21 15:48:14 +0000

    comments

--- src/app/pages/MediaArticlePage/MediaArticlePage.styles.ts
+++ src/app/pages/MediaArticlePage/MediaArticlePage.styles.ts
@@ -12,26 +12,30 @@ export default {
       margin: '0 auto',
       display: 'grid',
       gridTemplateColumns: 'repeat(12, 1fr)',
-      // gridGap: '1rem', <- This is the same as the 'enableGelGutters' prop on the Grid component
 
       [mq.GROUP_4_MIN_WIDTH]: {
         padding: '0 1rem',
+        // gridGap: '1rem', <- This is the same as the 'enableGelGutters' prop on the Grid component
       },
     }),
   primaryColumn: ({ mq }: Theme) =>
     css({
+      // Start at col 1 and span 12 columns
       gridColumn: '1 / span 12',
 
       [mq.GROUP_4_MIN_WIDTH]: {
+        // Start at col 1 and span 8 columns
         gridColumn: '1 / span 8',
       },
     }),
   secondaryColumn: ({ mq }: Theme) =>
     css({
       marginTop: '2rem',
+      // Start at col 1 and span 12 columns
       gridColumn: '1 / span 12',
 
       [mq.GROUP_4_MIN_WIDTH]: {
+        // Start at col 9 and span 4 columns
         gridColumn: '9 / span 4',
       },
     }),
