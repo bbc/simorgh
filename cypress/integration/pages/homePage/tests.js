e6a44a0bbe2179d844cabeea57a37cdc7c02a5e8
louisearchibald <louise.archibald@bbc.co.uk>
2023-03-21 15:44:41 +0000

    skips cypress test

--- cypress/integration/pages/homePage/tests.js
+++ cypress/integration/pages/homePage/tests.js
@@ -1,5 +1,5 @@
 export default ({ service, pageType }) => {
-  it(`should render a simple ${service} ${pageType} page`, () => {
+  it.skip(`should render a simple ${service} ${pageType} page`, () => {
     cy.get('[data-testid="home-page"]').should(
       'contain',
       'Hi, I am a Home Page component',
