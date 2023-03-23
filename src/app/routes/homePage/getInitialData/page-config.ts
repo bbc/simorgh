f29432c65343c60f5c665e6a7e6d2d55d6d5a798
chrish01 <holly.christopher@bbc.co.uk>
2023-03-23 11:25:48 +0000

    added in service names to page-config.json

--- src/app/routes/homePage/getInitialData/page-config.ts
+++ src/app/routes/homePage/getInitialData/page-config.ts
@@ -1,9 +1,199 @@
+import { Services } from '../../../models/types/global';
+
 type Envs = 'test' | 'live';
 
-const HOME_PAGE_CONFIG: { [key: string]: { [key in Envs]: string } } = {
+const HOME_PAGE_CONFIG: { [service in Services]: { [env in Envs]: string } } = {
   kyrgyz: {
     test: 'cm7682qz7v1t',
     live: 'crg7kj2e52nt',
+  },
+  afaanoromoo: {
+    test: '',
+    live: '',
+  },
+  afrique: {
+    test: '',
+    live: '',
+  },
+  amharic: {
+    test: '',
+    live: '',
+  },
+  arabic: {
+    test: '',
+    live: '',
+  },
+  archive: {
+    test: '',
+    live: '',
+  },
+  azeri: {
+    test: '',
+    live: '',
+  },
+  bengali: {
+    test: '',
+    live: '',
+  },
+  burmese: {
+    test: '',
+    live: '',
+  },
+  cymrufyw: {
+    test: '',
+    live: '',
+  },
+  gahuza: {
+    test: '',
+    live: '',
+  },
+  gujarati: {
+    test: '',
+    live: '',
+  },
+  hausa: {
+    test: '',
+    live: '',
+  },
+  hindi: {
+    test: '',
+    live: '',
+  },
+  igbo: {
+    test: '',
+    live: '',
+  },
+  indonesia: {
+    test: '',
+    live: '',
+  },
+  japanese: {
+    test: '',
+    live: '',
+  },
+  korean: {
+    test: '',
+    live: '',
+  },
+  marathi: {
+    test: '',
+    live: '',
+  },
+  mundo: {
+    test: '',
+    live: '',
+  },
+  naidheachdan: {
+    test: '',
+    live: '',
+  },
+  nepali: {
+    test: '',
+    live: '',
+  },
+  news: {
+    test: '',
+    live: '',
+  },
+  newsround: {
+    test: '',
+    live: '',
+  },
+  pashto: {
+    test: '',
+    live: '',
+  },
+  persian: {
+    test: '',
+    live: '',
+  },
+  pidgin: {
+    test: '',
+    live: '',
+  },
+  portuguese: {
+    test: '',
+    live: '',
+  },
+  punjabi: {
+    test: '',
+    live: '',
+  },
+  russian: {
+    test: '',
+    live: '',
+  },
+  scotland: {
+    test: '',
+    live: '',
+  },
+  sport: {
+    test: '',
+    live: '',
+  },
+  sinhala: {
+    test: '',
+    live: '',
+  },
+  somali: {
+    test: '',
+    live: '',
+  },
+  swahili: {
+    test: '',
+    live: '',
+  },
+  tamil: {
+    test: '',
+    live: '',
+  },
+  telugu: {
+    test: '',
+    live: '',
+  },
+  thai: {
+    test: '',
+    live: '',
+  },
+  tigrinya: {
+    test: '',
+    live: '',
+  },
+  turkce: {
+    test: '',
+    live: '',
+  },
+  urdu: {
+    test: '',
+    live: '',
+  },
+  uzbek: {
+    test: '',
+    live: '',
+  },
+  vietnamese: {
+    test: '',
+    live: '',
+  },
+  yoruba: {
+    test: '',
+    live: '',
+  },
+  serbian: {
+    test: '',
+    live: '',
+  },
+  ukchina: {
+    test: '',
+    live: '',
+  },
+  zhongwen: {
+    test: '',
+    live: '',
+  },
+  ukrainian: {
+    test: '',
+    live: '',
   },
 };
 
