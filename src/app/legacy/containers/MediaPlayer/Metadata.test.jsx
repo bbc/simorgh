61e4a6a3a8db0dcf99cfc13269008fed300d7751
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-04-04 09:34:22 +0100

    Fix linting issue

--- src/app/legacy/containers/MediaPlayer/Metadata.test.jsx
+++ src/app/legacy/containers/MediaPlayer/Metadata.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { render } from '../../../components/react-testing-library-with-providers';
 import Metadata from '../../../components/Metadata';
-import { Helmet } from 'react-helmet';
 
 const validAresMediaBlocks = [
   {
