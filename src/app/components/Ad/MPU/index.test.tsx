b7fed81e65478884abcaa4f169c91654e7d48795
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-04-05 14:44:02 +0100

    Fix test

--- src/app/components/Ad/MPU/index.test.tsx
+++ src/app/components/Ad/MPU/index.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StaticRouter } from 'react-router-dom';
 import { ToggleContextProvider } from '#contexts/ToggleContext';
-import { render, act } from '../../react-testing-library-with-providers';
+import { render } from '../../react-testing-library-with-providers';
 import MPU from '.';
 
 const toggles = {
@@ -12,16 +12,14 @@ const toggles = {
 
 describe('MPU', () => {
   it('should render without gel margins at all breakpoints and gel padding at smaller breakpoints', async () => {
-    let container;
-    await act(async () => {
-      ({ container } = render(
-        <StaticRouter>
-          <ToggleContextProvider toggles={toggles}>
-            <MPU />
-          </ToggleContextProvider>
-        </StaticRouter>,
-      ));
-    });
+    const { container } = render(
+      <StaticRouter>
+        <ToggleContextProvider toggles={toggles}>
+          <MPU />
+        </ToggleContextProvider>
+      </StaticRouter>,
+      { showAdsBasedOnLocation: true },
+    );
 
     expect(container).toMatchSnapshot();
   });
