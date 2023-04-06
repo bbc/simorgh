45f3b43cb8b9e2024a19e731ab3af0da9c56fa8b
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-04-06 11:52:39 +0100

    WSTEAM1-355 - Tidy up media indicator and padding

--- src/app/pages/MediaArticlePage/PagePromoSections/TestLastestMediaSection/LatestMediaItem/index.styles.tsx
+++ src/app/pages/MediaArticlePage/PagePromoSections/TestLastestMediaSection/LatestMediaItem/index.styles.tsx
@@ -1,6 +1,10 @@
 import styled from '@emotion/styled';
 import { css, Theme } from '@emotion/react';
 import Promo from '#components/OptimoPromos';
+import {
+  GEL_GROUP_3_SCREEN_WIDTH_MIN,
+  GEL_GROUP_3_SCREEN_WIDTH_MAX,
+} from '#psammead/gel-foundations/src/breakpoints';
 
 export const styles = {
   gridOuter: ({ mq, spacings }: Theme) =>
@@ -18,19 +22,33 @@ export const styles = {
     css({
       display: 'grid',
       gridTemplateColumns: '1fr 2fr',
-      columnGap: '1rem',
+      columnGap: '0.75rem',
       [mq.GROUP_3_ONLY]: {
         gridTemplateColumns: '1fr',
+      },
+    }),
+  promoTitle: ({ mq, fontSizes }: Theme) =>
+    css({
+      ...fontSizes.pica,
+      [mq.GROUP_3_ONLY]: {
+        marginTop: `0.75rem`,
       },
     }),
 };
 
 export const StyledTimestamp = styled(Promo.Timestamp)`
-  padding-top: ${props => props.theme.spacings.FULL};
+  padding-top: ${props => props.theme.spacings.FULL}rem;
 `;
 
+// export const StyledPromoTitle = styled(Promo.Title)`
+//   font-size: ${props => props.theme.fontSizes.pica};
+//   @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) and (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
+//     margin-top: 0.75rem;
+//   }
+// `;
+
 export const StyledPromoTitle = styled(Promo.Title)`
-  font-size: ${props => props.theme.fontSizes.pica};
+  ${props => styles.promoTitle(props.theme)}
 `;
 
 export const PlaceholderWrapper = styled.div`
