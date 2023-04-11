aa03f66416e53c0aa81cc6e182ffbb1028ef1b84
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-04-11 12:40:58 +0100

    Revert "Fixes Unexpected extra semicolon   no-extra-semicolons"
    This reverts commit 886c952cabba90a5172a41c99734234d513ea70a.

--- src/app/legacy/psammead/psammead-heading-index/src/index.jsx
+++ src/app/legacy/psammead/psammead-heading-index/src/index.jsx
@@ -5,8 +5,8 @@ import { getDoublePica } from '#psammead/gel-foundations/src/typography';
 import { getSansRegular } from '#psammead/psammead-styles/src/font-styles';
 
 const HeadingIndex = styled.h1`
-  ${({ script }) => script && getDoublePica(script)}
-  ${({ service }) => getSansRegular(service)}
+  ${({ script }) => script && getDoublePica(script)};
+  ${({ service }) => getSansRegular(service)};
   color: ${props => props.theme.palette.METAL};
   margin: 0;
 `;
