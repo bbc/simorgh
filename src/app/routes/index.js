418bac432401cd14f9818e55a7dc894aaf44345f
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-04-12 10:04:43 +0100

    Fix imports for all routes

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,17 +1,17 @@
-import article from './article';
-import cpsAsset from './cpsAsset';
-import frontPage from './frontPage';
-import homePage from './homePage';
-import liveRadio from './liveRadio';
-import mostRead from './mostRead';
-import mostWatched from './mostWatched';
-import podcast from './podcast';
-import onDemandRadio from './onDemandRadio';
-import onDemandTV from './onDemandTV';
-import topic from './topic';
-import idx from './idx';
-import error from './error';
-import errorNoRouteMatch from './errorNoRouteMatch';
+import article from './article/index.jsx';
+import cpsAsset from './cpsAsset/index.jsx';
+import frontPage from './frontPage/index.js';
+import homePage from './homePage/index.js';
+import liveRadio from './liveRadio/index.js';
+import mostRead from './mostRead/index.js';
+import mostWatched from './mostWatched/index.js';
+import podcast from './podcast/index.js';
+import onDemandRadio from './onDemandRadio/index.js';
+import onDemandTV from './onDemandTV/index.js';
+import topic from './topic/index.js';
+import idx from './idx/index.js';
+import error from './error/index.js';
+import errorNoRouteMatch from './errorNoRouteMatch/index.js';
 
 export default [
   article,
