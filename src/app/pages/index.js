7f75ed2a28dd9f90402e55b86880115f983c9216
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-04-12 10:06:09 +0100

    Fix page imports

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,18 +1,34 @@
 import loadable from '@loadable/component';
 
-export const ArticlePage = loadable(() => import('./ArticlePage'));
-export const MediaArticlePage = loadable(() => import('./MediaArticlePage'));
-export const ErrorPage = loadable(() => import('./ErrorPage'));
-export const FrontPage = loadable(() => import('./FrontPage'));
-export const HomePage = loadable(() => import('./HomePage'));
-export const MediaAssetPage = loadable(() => import('./MediaAssetPage'));
-export const MostReadPage = loadable(() => import('./MostReadPage'));
-export const MostWatchedPage = loadable(() => import('./MostWatchedPage'));
-export const PhotoGalleryPage = loadable(() => import('./PhotoGalleryPage'));
-export const LiveRadioPage = loadable(() => import('./LiveRadioPage'));
-export const OnDemandAudioPage = loadable(() => import('./OnDemandAudioPage'));
-export const OnDemandTvPage = loadable(() => import('./OnDemandTvPage'));
-export const TopicPage = loadable(() => import('./TopicPage'));
-export const StoryPage = loadable(() => import('./StoryPage'));
-export const IdxPage = loadable(() => import('./IdxPage'));
-export const FeatureIdxPage = loadable(() => import('./FeatureIdxPage'));
+export const ArticlePage = loadable(() => import('./ArticlePage/index.jsx'));
+export const MediaArticlePage = loadable(() =>
+  import('./MediaArticlePage/index.jsx'),
+);
+export const ErrorPage = loadable(() => import('./ErrorPage/index.jsx'));
+export const FrontPage = loadable(() => import('./FrontPage/index.jsx'));
+export const HomePage = loadable(() => import('./HomePage/index.tsx'));
+export const MediaAssetPage = loadable(() =>
+  import('./MediaAssetPage/index.jsx'),
+);
+export const MostReadPage = loadable(() => import('./MostReadPage/index.jsx'));
+export const MostWatchedPage = loadable(() =>
+  import('./MostWatchedPage/index.jsx'),
+);
+export const PhotoGalleryPage = loadable(() =>
+  import('./PhotoGalleryPage/index.jsx'),
+);
+export const LiveRadioPage = loadable(() =>
+  import('./LiveRadioPage/index.jsx'),
+);
+export const OnDemandAudioPage = loadable(() =>
+  import('./OnDemandAudioPage/index.jsx'),
+);
+export const OnDemandTvPage = loadable(() =>
+  import('./OnDemandTvPage/index.jsx'),
+);
+export const TopicPage = loadable(() => import('./TopicPage/index.jsx'));
+export const StoryPage = loadable(() => import('./StoryPage/index.jsx'));
+export const IdxPage = loadable(() => import('./IdxPage/index.jsx'));
+export const FeatureIdxPage = loadable(() =>
+  import('./FeatureIdxPage/index.jsx'),
+);
