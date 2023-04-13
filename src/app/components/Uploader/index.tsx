103917d55c7d0cc2fd6b85dfa95dff66e6b53388
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-04-13 16:05:17 +0100

    WSTEAM1-170: Format uploader using components

--- src/app/components/Uploader/index.tsx
+++ src/app/components/Uploader/index.tsx
@@ -1,46 +1,90 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import React, { PropsWithChildren } from 'react';
+import { useContext } from 'react';
 import path from 'ramda/src/path';
 import { jsx } from '@emotion/react';
+import { ServiceContext } from '../../contexts/ServiceContext';
+import Heading from '../Heading';
+import Paragraph from '../Paragraph';
 import Text from '../Text';
-// import uploaderBlockPropTypes from '../../models/propTypes/uploader';
+import { LeftChevron, RightChevron } from '../icons';
 
-type Props = {
+interface UploaderProps {
   blocks: object[];
-};
+  title: string;
+  text: string;
+  link: string;
+}
 
-const Uploader = ({ blocks, children }: PropsWithChildren<Props>) => {
-  const type = path([0, 'type'], blocks);
+const Uploader = ({ blocks }: UploaderProps) => {
+  // const type = path([0, 'type'], blocks);
   const uploaderBlocks = path([0, 'model', 'blocks'], blocks);
 
-  const titleContent = path(
+  const title = path(
     [0, 'model', 'blocks', 0, 'model', 'blocks', 0, 'model', 'text'],
     uploaderBlocks,
   );
 
-  const textContent = path(
+  const text = path(
     [1, 'model', 'blocks', 0, 'model', 'blocks', 0, 'model', 'text'],
     uploaderBlocks,
   );
 
-  const linkContent = path(
-    [2, 'model', 'blocks', 0, 'model', 'blocks', 0, 'model', 'text'],
+  const linkText = path(
+    [
+      2,
+      'model',
+      'blocks',
+      0,
+      'model',
+      'blocks',
+      0,
+      'model',
+      'blocks',
+      0,
+      'model',
+      'text',
+    ],
     uploaderBlocks,
   );
 
+  const linkAddress = path(
+    [
+      2,
+      'model',
+      'blocks',
+      0,
+      'model',
+      'blocks',
+      0,
+      'model',
+      'blocks',
+      0,
+      'model',
+      'locator',
+    ],
+    uploaderBlocks,
+  );
+
+  const { dir } = useContext(ServiceContext);
+  const isRtl = dir === 'rtl';
+
+  const id = `${(title as string).replaceAll(' ', '-')}`;
+
   return (
-    <section>
-      <p>{type as string}</p>
-      <p>{titleContent as string}</p>
-      <p>{textContent as string}</p>
-      <p>{linkContent as string}</p>
+    <section role="region" aria-labelledby={id}>
+      <Heading level={2}>{title as string}</Heading>
+      <Paragraph>{text as string}</Paragraph>
+      <a href={linkAddress as string}>
+        <div>
+          <Text size="pica" fontVariant="sansBold">
+            {linkText as string}
+            {isRtl ? <LeftChevron /> : <RightChevron />}
+          </Text>
+        </div>
+      </a>
     </section>
   );
-};
-
-Uploader.defaultProps = {
-  children: null,
 };
 
 export default Uploader;
