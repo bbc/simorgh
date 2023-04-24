8681b3c376ea9601b202bf08d16f841c944dd251
Harvey Peachey <harvey.peachey@bbc.co.uk>
2023-04-24 09:29:58 +0100

    removes isLive import

--- src/app/components/ThemeProvider/withThemeProvider.tsx
+++ src/app/components/ThemeProvider/withThemeProvider.tsx
@@ -6,7 +6,6 @@ import {
 } from '@emotion/react';
 import focusIndicator from './focusIndicator';
 import { RequestContext } from '../../contexts/RequestContext';
-import isLive from '../../lib/utilities/isLive';
 
 import {
   ARCHIVE_BLUE,
