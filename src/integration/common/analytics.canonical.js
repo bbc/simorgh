435150bbeb49cb539b32353cd5a7655931e98c88
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-04-28 15:08:32 +0100

    Remove canonical chartbeat tests, as it doesn't work on JSDOM due to async loading of chartbeat script

--- src/integration/common/analytics.canonical.js
+++ src/integration/common/analytics.canonical.js
@@ -6,24 +6,5 @@ export default () => {
       expect(noscriptImage).toMatch('x8=[simorgh-nojs]');
       expect(noscriptImage).toMatchSnapshot();
     });
-
-    it('Chartbeat', () => {
-      const chartbeatScript = document.querySelector(
-        'html > head > script[src="//static.chartbeat.com/js/chartbeat.js"]',
-      );
-
-      const CHARTBEAT_NOT_CONFIGURED = 'Chartbeat not configured';
-
-      if (chartbeatScript) {
-        const chartbeatConfig =
-          document.querySelector(
-            'html > head > script[async="true"][type="text/javascript"]',
-          )?.innerText || CHARTBEAT_NOT_CONFIGURED;
-
-        expect(chartbeatConfig).toMatchSnapshot();
-      } else {
-        expect(CHARTBEAT_NOT_CONFIGURED).toMatchSnapshot();
-      }
-    });
   });
 };
