80485385609585804d114d3dfc1541611ff66762
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-04-28 14:30:22 +0100

    Improve chartbeat integration test

--- src/integration/common/analytics.canonical.js
+++ src/integration/common/analytics.canonical.js
@@ -12,13 +12,17 @@ export default () => {
         'html > head > script[src="//static.chartbeat.com/js/chartbeat.js"]',
       );
 
+      const CHARTBEAT_NOT_CONFIGURED = 'Chartbeat not configured';
+
       if (chartbeatScript) {
         const chartbeatConfig =
           document.querySelector(
             'html > head > script[async="true"][type="text/javascript"]',
-          )?.innerText || 'Chartbeat not configured';
+          )?.innerText || CHARTBEAT_NOT_CONFIGURED;
 
         expect(chartbeatConfig).toMatchSnapshot();
+      } else {
+        expect(CHARTBEAT_NOT_CONFIGURED).toMatchSnapshot();
       }
     });
   });
