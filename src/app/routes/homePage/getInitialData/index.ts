455b8a03f3148807e9c63bc1205211375d14f910
Harvey Peachey <harvey.peachey@bbc.co.uk>
2023-05-04 15:15:57 +0100

    adds homepage support to url construction

--- src/app/routes/homePage/getInitialData/index.ts
+++ src/app/routes/homePage/getInitialData/index.ts
@@ -1,12 +1,11 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Agent } from 'https';
-import Url from 'url-parse';
 import getEnvironment from '#app/routes/utils/getEnvironment';
 import nodeLogger from '../../../lib/logger.node';
 import { BFF_FETCH_ERROR } from '../../../lib/logger.const';
 import fetchPageData from '../../utils/fetchPageData';
+import constructPageFetchUrl from '../../utils/constructPageFetchUrl';
 import { Services } from '../../../models/types/global';
-import HOME_PAGE_CONFIG from './page-config';
 
 const logger = nodeLogger(__filename);
 
@@ -14,7 +13,7 @@ type Props = {
   getAgent: () => Promise<Agent>;
   service: Services;
   path: string;
-  pageType: string;
+  pageType: 'home';
 };
 
 export default async ({
@@ -28,17 +27,12 @@ export default async ({
     const isLocal = !env || env === 'local';
 
     const agent = isLocal ? null : await getAgent();
-    const id = isLocal ? null : HOME_PAGE_CONFIG[service][env];
 
-    let fetchUrl = Url(process.env.BFF_PATH as string).set('query', {
-      id,
-      service,
+    const fetchUrl = constructPageFetchUrl({
+      pathname,
       pageType,
+      service,
     });
-
-    if (isLocal) {
-      fetchUrl = Url(`/${service}/tipohome`);
-    }
 
     const optHeaders = { 'ctx-service-env': env };
 
--- src/app/routes/utils/constructPageFetchUrl/index.ts
+++ src/app/routes/utils/constructPageFetchUrl/index.ts
@@ -4,6 +4,7 @@ import getEnvironment from '#app/routes/utils/getEnvironment';
 import { getUrlPath } from '../../../lib/utilities/urlParser';
 import handleError from '../handleError';
 import { Services, Variants } from '../../../models/types/global';
+import HOME_PAGE_CONFIG from '../../homePage/getInitialData/page-config';
 
 const PAGE_TYPES = {
   ARTICLE: 'article',
@@ -25,21 +26,33 @@ interface UrlConstructParams {
 
 const removeAmp = (path: string) => path.split('.')[0];
 const getArticleId = (path: string) => path.match(/(c[a-zA-Z0-9]{10}o)/)?.[1];
-const getTipoId = (path: string) => path.match(/(c[a-zA-Z0-9]{10}t)/)?.[1];
 const getCpsId = (path: string) => path;
+const getTipoId = (path: string) => path.match(/(c[a-zA-Z0-9]{10}t)/)?.[1];
 
-const getId = (pageType: PageTypes) => {
+const getId = (
+  pageType: PageTypes,
+  service: Services,
+  env: 'test' | 'live' | 'local',
+) => {
   let getIdFunction;
   switch (pageType) {
+    case PAGE_TYPES.ARTICLE:
+      getIdFunction = getArticleId;
+      break;
     case PAGE_TYPES.CPS_ASSET:
       getIdFunction = getCpsId;
       break;
-    case PAGE_TYPES.TOPIC:
+    case PAGE_TYPES.HOME:
+      getIdFunction = () => {
+        return env !== 'local' ? HOME_PAGE_CONFIG[service][env] : 'tipohome';
+      };
+      break;
     case PAGE_TYPES.LIVE:
+    case PAGE_TYPES.TOPIC:
       getIdFunction = getTipoId;
       break;
     default:
-      getIdFunction = getArticleId;
+      getIdFunction = () => null;
       break;
   }
   return pipe(getUrlPath, removeAmp, getIdFunction);
@@ -47,7 +60,7 @@ const getId = (pageType: PageTypes) => {
 
 const constructPageFetchUrl = ({
   pathname,
-  pageType = 'topic',
+  pageType,
   service,
   variant,
   page,
@@ -55,7 +68,7 @@ const constructPageFetchUrl = ({
   const env = getEnvironment(pathname);
   const isLocal = !env || env === 'local';
 
-  const id = getId(pageType)(pathname);
+  const id = getId(pageType, service, env)(pathname);
   const capitalisedPageType =
     pageType.charAt(0).toUpperCase() + pageType.slice(1);
 
@@ -87,6 +100,9 @@ const constructPageFetchUrl = ({
         break;
       case PAGE_TYPES.CPS_ASSET:
         fetchUrl = Url(id);
+        break;
+      case PAGE_TYPES.HOME:
+        fetchUrl = Url(`/${service}/${id}`);
         break;
       case PAGE_TYPES.TOPIC: {
         const variantPath = variant ? `/${variant}` : '';
