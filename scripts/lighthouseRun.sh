ec169cf7c7eab22090b1bff3418160e3da037d62
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-05-05 13:10:48 +0100

    Test serbian/cyr frontpage

--- scripts/lighthouseRun.sh
+++ scripts/lighthouseRun.sh
@@ -2,5 +2,5 @@
 set -e
 
 npx -p lighthouse@5 lighthouse http://localhost:7080/news/articles/cn7k01xp8kxo --chrome-flags="--no-sandbox --headless --disable-gpu" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
-npx -p lighthouse@5 lighthouse http://localhost:7080/igbo --chrome-flags="--no-sandbox --headless --disable-gpu" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
+npx -p lighthouse@5 lighthouse http://localhost:7080/serbian/cyr --chrome-flags="--no-sandbox --headless --disable-gpu" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
 
