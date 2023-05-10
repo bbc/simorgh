0a531a39fb25456118b3ca7cfcc505d9999472c7
Aaron Moore <aaron.moore@bbc.co.uk>
2023-05-10 11:24:33 +0100

    Update undici to memoize and add test

--- ws-nextjs-app/utilities/undiciAgent/index.ts
+++ ws-nextjs-app/utilities/undiciAgent/index.ts
@@ -1,19 +1,20 @@
 import { setGlobalDispatcher, Agent } from 'undici';
 import getCert from './certs';
 
+let agentMemo: Agent;
+
 const getAgent = async () => {
+  if (agentMemo) {
+    return agentMemo;
+  }
+
   const { certChain, key, ca } = await getCert();
 
-  setGlobalDispatcher(
-    new Agent({
-      connect: {
-        cert: certChain,
-        key,
-        ca,
-        rejectUnauthorized: false,
-      },
-    }),
-  );
+  const agentOptions = { cert: certChain, key, ca, rejectUnauthorized: false };
+
+  agentMemo = new Agent({ connect: agentOptions });
+
+  return setGlobalDispatcher(agentMemo);
 };
 
 export default getAgent;
