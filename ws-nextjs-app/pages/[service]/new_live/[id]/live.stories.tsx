ecdf951e12a85cd1414a1c42abc21a30b6510724
Aaron Moore <aaron.moore@bbc.co.uk>
2023-05-10 15:55:03 +0100

    Add service switcher to demo page

--- ws-nextjs-app/pages/[service]/new_live/[id]/live.stories.tsx
+++ ws-nextjs-app/pages/[service]/new_live/[id]/live.stories.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import PageLayoutWrapper from '#app/components/PageLayoutWrapper';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withServicesKnob } from '#app/legacy/psammead/psammead-storybook-helpers/src';
+import { ServiceContextProvider } from '#contexts/ServiceContext';
+import { StoryProps } from '#models/types/storybook';
 import Live from './LivePageLayout';
 
 const mockPageData = {
@@ -10,15 +14,18 @@ const mockPageData = {
   },
 };
 
-const Component = () => (
-  <PageLayoutWrapper pageData={mockPageData} status={200}>
-    <Live pageData={mockPageData} />
-  </PageLayoutWrapper>
+const Component = ({ service, variant }: StoryProps) => (
+  <ServiceContextProvider service={service} variant={variant}>
+    <PageLayoutWrapper pageData={mockPageData} status={200}>
+      <Live pageData={mockPageData} />
+    </PageLayoutWrapper>
+  </ServiceContextProvider>
 );
 
 export default {
   title: 'Pages/Live Page',
   Component,
+  decorators: [withKnobs, withServicesKnob()],
 };
 
-export const LivePage = () => <Component />;
+export const Example = Component;
