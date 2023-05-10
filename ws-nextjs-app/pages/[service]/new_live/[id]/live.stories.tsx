bd866387b652536d708ae9a031e29a8c68986cc4
Aaron Moore <aaron.moore@bbc.co.uk>
2023-05-10 16:03:15 +0100

    Remove knobs addon in favour of global service selector

--- ws-nextjs-app/pages/[service]/new_live/[id]/live.stories.tsx
+++ ws-nextjs-app/pages/[service]/new_live/[id]/live.stories.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import PageLayoutWrapper from '#app/components/PageLayoutWrapper';
-import { withKnobs } from '@storybook/addon-knobs';
-import { withServicesKnob } from '#app/legacy/psammead/psammead-storybook-helpers/src';
-import { ServiceContextProvider } from '#contexts/ServiceContext';
-import { StoryProps } from '#models/types/storybook';
 import Live from './LivePageLayout';
 
 const mockPageData = {
@@ -14,18 +10,15 @@ const mockPageData = {
   },
 };
 
-const Component = ({ service, variant }: StoryProps) => (
-  <ServiceContextProvider service={service} variant={variant}>
-    <PageLayoutWrapper pageData={mockPageData} status={200}>
-      <Live pageData={mockPageData} />
-    </PageLayoutWrapper>
-  </ServiceContextProvider>
+const Component = () => (
+  <PageLayoutWrapper pageData={mockPageData} status={200}>
+    <Live pageData={mockPageData} />
+  </PageLayoutWrapper>
 );
 
 export default {
   title: 'Pages/Live Page',
   Component,
-  decorators: [withKnobs, withServicesKnob()],
 };
 
 export const Example = Component;
