015cc16f3de0b4e2f9d20f13cf6a3928e7ffee84
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-05-11 13:08:44 +0100

    Improve comments on pixel / rem spacings

--- src/app/components/ThemeProvider/spacings.ts
+++ src/app/components/ThemeProvider/spacings.ts
@@ -1,9 +1,9 @@
 import pixelsToRem from '../../utilities/pixelsToRem';
 
-export const FULL = pixelsToRem(8); // 0.5rem 8px
-export const HALF = FULL / 2; // 0.25 rem 4px
-export const DOUBLE = FULL * 2; // 1 rem 16px
-export const TRIPLE = FULL * 3; // 1.5 rem 24px
-export const QUADRUPLE = FULL * 4; // 2 rem 32px
-export const QUINTUPLE = FULL * 5; // 2.5 rem 40px
-export const SEXTUPLE = FULL * 6; // 3 rem 48px
+export const FULL = pixelsToRem(8); // 0.5rem (8px)
+export const HALF = FULL / 2; // 0.25rem (4px)
+export const DOUBLE = FULL * 2; // 1rem (16px)
+export const TRIPLE = FULL * 3; // 1.5rem (24px)
+export const QUADRUPLE = FULL * 4; // 2rem (32px)
+export const QUINTUPLE = FULL * 5; // 2.5rem (40px)
+export const SEXTUPLE = FULL * 6; // 3rem (48px)
