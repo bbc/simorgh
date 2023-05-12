9a658a2f3527ab1cfac752c0e161313a00e16878
Aaron Moore <aaron.moore@bbc.co.uk>
2023-05-12 12:49:46 +0100

    Convert config files to TS

---
+++ .storybook/main.ts
@@ -0,0 +1,84 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+import webpack from 'webpack';
+import path from 'path';
+import { webpackDirAlias } from '../dirAlias';
+
+const toPath = (_path: string) => path.join(process.cwd(), _path);
+
+const config: StorybookConfig = {
+  // staticDirs: ['static'],
+  stories: [
+    // '../docs/**/*.stories.mdx',
+    // '../src/**/*.stories.mdx',
+    // '../AdHocCypress/**/*.stories.mdx',
+    // '../3rdPartyCypress/**/*.stories.mdx',
+    '../src/app/legacy/components/**/*.stories.@(t|j)sx',
+    // '../src/app/legacy/containers/**/*.stories.@(t|j)sx',
+    // '../src/app/components/**/*.stories.@(t|j)sx',
+    // '../src/app/pages/**/*.stories.@(t|j)sx',
+    // '../ws-nextjs-app/**/*.stories.tsx',
+    // './DocsDecorator/**/*.stories.@(t|j)sx',
+    // './SidebarLabel/**/*.stories.@(t|j)sx',
+  ],
+  // addons: [
+  //   '@storybook/addon-knobs',
+  //   '@storybook/addon-backgrounds',
+  //   '@storybook/addon-a11y',
+  //   '@storybook/addon-viewport',
+  //   '@storybook/addon-controls',
+  //   '@storybook/addon-toolbars',
+  //   // 'storybook-addon-designs',
+  //   './SidebarLabel/preset.cjs',
+  //   {
+  //     name: '@storybook/addon-docs',
+  //     options: {
+  //       configureJSX: true,
+  //       babelOptions: {},
+  //       sourceLoaderOptions: null,
+  //       transcludeMarkdown: true,
+  //     },
+  //   },
+  // ],
+  webpackFinal: async config => {
+    config.target = ['web', 'es5'];
+    config.plugins!.push(
+      /*
+       * This replaces calls to logger.node.js with logger.web.js, a client
+       * side replacement. This mimics the behaviour of the client side
+       * bundle generation in webpack.config.client.js
+       */
+      // @ts-ignore
+      new webpack.NormalModuleReplacementPlugin(
+        /(.*)logger.node(\.*)/,
+        resource => {
+          resource.request = resource.request.replace(
+            /logger.node/,
+            `logger.web`,
+          );
+        },
+      ),
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+      }),
+    );
+    config.resolve!.extensions!.push('.js', '.jsx', '.ts', '.tsx'); // resolves `import '../Foo'` to `../Foo/index.jsx`
+    config.resolve!.alias = {
+      ...config.resolve?.alias,
+      ...webpackDirAlias,
+      // Storybook 6 does not support emotion 11 - these 3 aliases work around that
+      // https://github.com/storybookjs/storybook/issues/13277
+      '@emotion/core': toPath('node_modules/@emotion/react'),
+      '@emotion/styled': toPath('node_modules/@emotion/styled'),
+      'emotion-theming': toPath('node_modules/@emotion/react'),
+    };
+    return config;
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {
+      builder: { lazyCompilation: true },
+    },
+  },
+};
+
+export default config;
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,5 @@
-/* eslint-disable import/prefer-default-export */
-
 import React, { useEffect } from 'react';
 import { Global } from '@emotion/react';
-import { addDecorator } from '@storybook/react';
 import { create } from '@storybook/theming';
 import isChromatic from 'chromatic';
 import { forceVisible } from 'react-lazyload';
@@ -14,6 +11,7 @@ import { ToggleContextProvider } from '../src/app/contexts/ToggleContext';
 import { UserContextProvider } from '../src/app/contexts/UserContext';
 import { EventTrackingContextProvider } from '../src/app/contexts/EventTrackingContext';
 import pageDataFixture from '../data/news/articles/c0g992jmmkko.json';
+import { Preview } from '@storybook/react';
 
 const REITH_SERIF_REGULAR = {
   '@font-face': {
@@ -270,328 +268,325 @@ const REITH_QALAM_BOLD = {
   },
 };
 
-addDecorator(story => {
-  useEffect(() => {
-    if (isChromatic()) {
-      forceVisible();
-    }
-  }, []);
-
-  return (
-    /* eslint-disable react/jsx-filename-extension */
-    <>
-      <Global
-        styles={[
-          REITH_SERIF_REGULAR,
-          REITH_SERIF_BOLD,
-          REITH_SERIF_LIGHT,
-          REITH_SERIF_MEDIUM,
-          REITH_SERIF_EXTRA_BOLD,
-          REITH_SANS_REGULAR,
-          REITH_SANS_BOLD,
-          REITH_SANS_LIGHT,
-          REITH_SANS_LIGHT_ITALIC,
-          REITH_SANS_MEDIUM,
-          REITH_SANS_EXTRA_BOLD,
-          REITH_SANS_EXTRA_BOLD_ITALIC,
-          REITH_SANS_CONDENSED_REGULAR,
-          REITH_SANS_CONDENSED_BOLD,
-          NOTO_SERIF_SINHALA_REGULAR,
-          NOTO_SERIF_SINHALA_BOLD,
-          NOTO_SANS_TAMIL_REGULAR,
-          NOTO_SANS_TAMIL_BOLD,
-          MALLANNA_REGULAR,
-          NOTO_SANS_ETHIOPIC_REGULAR,
-          NOTO_SANS_ETHIOPIC_BOLD,
-          PADAUK_REGULAR,
-          PADAUK_BOLD,
-          NOTO_SERIF_BENGALI_REGULAR,
-          NOTO_SERIF_BENGALI_BOLD,
-          REITH_QALAM_REGULAR,
-          REITH_QALAM_BOLD,
-        ]}
-      />
-      <GlobalStyles />
-      {story()}
-    </>
-    /* eslint-enable react/jsx-filename-extension */
-  );
-});
-
 const theme = create({
   base: 'light',
   brandTitle: 'BBC Simorgh',
   brandUrl: 'https://github.com/bbc/simorgh',
 });
 
-export const globalTypes = {
-  service: {
-    name: 'service',
-    // Text that will be shown on icon hover
-    description: 'Global service',
-    defaultValue: { service: 'news', variant: 'default' },
-    toolbar: {
-      /**
-       * You can check all available icons by this link:
-       * https://storybook.js.org/docs/riot/workflows/faq#what-icons-are-available-for-my-toolbar-or-my-addon
-       */
-      icon: 'globe',
-      items: [
-        {
-          value: { service: 'afaanoromoo', variant: 'default' },
-          title: 'afaanoromoo',
-        },
-        {
-          value: { service: 'afrique', variant: 'default' },
-          title: 'afrique',
-        },
-        {
-          value: { service: 'amharic', variant: 'default' },
-          title: 'amharic',
-        },
-        {
-          value: { service: 'arabic', variant: 'default' },
-          title: 'arabic',
-        },
-        {
-          value: { service: 'archive', variant: 'default' },
-          title: 'archive',
-        },
-        {
-          value: { service: 'azeri', variant: 'default' },
-          title: 'azeri',
-        },
-        {
-          value: { service: 'bengali', variant: 'default' },
-          title: 'bengali',
-        },
-        {
-          value: { service: 'burmese', variant: 'default' },
-          title: 'burmese',
-        },
-        {
-          value: { service: 'cymrufyw', variant: 'default' },
-          title: 'cymrufyw',
-        },
-        {
-          value: { service: 'gahuza', variant: 'default' },
-          title: 'gahuza',
-        },
-        {
-          value: { service: 'gujarati', variant: 'default' },
-          title: 'gujarati',
-        },
-        {
-          value: { service: 'hausa', variant: 'default' },
-          title: 'hausa',
-        },
-        {
-          value: { service: 'hindi', variant: 'default' },
-          title: 'hindi',
-        },
-        {
-          value: { service: 'igbo', variant: 'default' },
-          title: 'igbo',
-        },
-        {
-          value: { service: 'indonesia', variant: 'default' },
-          title: 'indonesia',
-        },
-        {
-          value: { service: 'japanese', variant: 'default' },
-          title: 'japanese',
-        },
-        {
-          value: { service: 'korean', variant: 'default' },
-          title: 'korean',
-        },
-        {
-          value: { service: 'kyrgyz', variant: 'default' },
-          title: 'kyrgyz',
-        },
-        {
-          value: { service: 'marathi', variant: 'default' },
-          title: 'marathi',
-        },
-        {
-          value: { service: 'mundo', variant: 'default' },
-          title: 'mundo',
-        },
-        {
-          value: { service: 'naidheachdan', variant: 'default' },
-          title: 'naidheachdan',
-        },
-        {
-          value: { service: 'nepali', variant: 'default' },
-          title: 'nepali',
-        },
-        {
-          value: { service: 'news', variant: 'default' },
-          title: 'news',
-        },
-        {
-          value: { service: 'newsround', variant: 'default' },
-          title: 'newsround',
-        },
-        {
-          value: { service: 'pashto', variant: 'default' },
-          title: 'pashto',
-        },
-        {
-          value: { service: 'persian', variant: 'default' },
-          title: 'persian',
-        },
-        {
-          value: { service: 'pidgin', variant: 'default' },
-          title: 'pidgin',
-        },
-        {
-          value: { service: 'portuguese', variant: 'default' },
-          title: 'portuguese',
-        },
-        {
-          value: { service: 'punjabi', variant: 'default' },
-          title: 'punjabi',
-        },
-        {
-          value: { service: 'russian', variant: 'default' },
-          title: 'russian',
-        },
-        {
-          value: { service: 'scotland', variant: 'default' },
-          title: 'scotland',
-        },
-        {
-          value: { service: 'sport', variant: 'default' },
-          title: 'sport',
-        },
-        {
-          value: { service: 'sinhala', variant: 'default' },
-          title: 'sinhala',
-        },
-        {
-          value: { service: 'somali', variant: 'default' },
-          title: 'somali',
-        },
-        {
-          value: { service: 'swahili', variant: 'default' },
-          title: 'swahili',
-        },
-        {
-          value: { service: 'tamil', variant: 'default' },
-          title: 'tamil',
-        },
-        {
-          value: { service: 'telugu', variant: 'default' },
-          title: 'telugu',
-        },
-        {
-          value: { service: 'thai', variant: 'default' },
-          title: 'thai',
-        },
-        {
-          value: { service: 'tigrinya', variant: 'default' },
-          title: 'tigrinya',
-        },
-        {
-          value: { service: 'turkce', variant: 'default' },
-          title: 'turkce',
-        },
-        {
-          value: { service: 'urdu', variant: 'default' },
-          title: 'urdu',
-        },
-        {
-          value: { service: 'uzbek', variant: 'default' },
-          title: 'uzbek',
-        },
-        {
-          value: { service: 'vietnamese', variant: 'default' },
-          title: 'vietnamese',
-        },
-        {
-          value: { service: 'yoruba', variant: 'default' },
-          title: 'yoruba',
-        },
-        {
-          value: { service: 'serbian', variant: 'cyr' },
-          title: 'serbian-cyr',
-        },
-        {
-          value: { service: 'serbian', variant: 'lat' },
-          title: 'serbian-lat',
-        },
-        {
-          value: { service: 'ukchina', variant: 'simp' },
-          title: 'ukchina-simp',
-        },
-        {
-          value: { service: 'ukchina', variant: 'trad' },
-          title: 'ukchina-trad',
-        },
-        {
-          value: { service: 'zhongwen', variant: 'simp' },
-          title: 'zhongwen-simp',
-        },
-        {
-          value: { service: 'zhongwen', variant: 'trad' },
-          title: 'zhongwen-trad',
-        },
-        {
-          value: { service: 'ukrainian', variant: 'default' },
-          title: 'ukrainian-default',
-        },
-        {
-          value: { service: 'ukrainian', variant: 'ru-UA' },
-          title: 'ukrainian-ru-UA',
-        },
-      ],
-      // Should "Container size" be shown, or just the "circlehollow" icon
-      dynamicTitle: true,
+const preview: Preview = {
+  globalTypes: {
+    service: {
+      name: 'service',
+      // Text that will be shown on icon hover
+      description: 'Global service',
+      defaultValue: { service: 'news', variant: 'default' },
+      toolbar: {
+        /**
+         * You can check all available icons by this link:
+         * https://storybook.js.org/docs/riot/workflows/faq#what-icons-are-available-for-my-toolbar-or-my-addon
+         */
+        icon: 'globe',
+        items: [
+          {
+            value: { service: 'afaanoromoo', variant: 'default' },
+            title: 'afaanoromoo',
+          },
+          {
+            value: { service: 'afrique', variant: 'default' },
+            title: 'afrique',
+          },
+          {
+            value: { service: 'amharic', variant: 'default' },
+            title: 'amharic',
+          },
+          {
+            value: { service: 'arabic', variant: 'default' },
+            title: 'arabic',
+          },
+          {
+            value: { service: 'archive', variant: 'default' },
+            title: 'archive',
+          },
+          {
+            value: { service: 'azeri', variant: 'default' },
+            title: 'azeri',
+          },
+          {
+            value: { service: 'bengali', variant: 'default' },
+            title: 'bengali',
+          },
+          {
+            value: { service: 'burmese', variant: 'default' },
+            title: 'burmese',
+          },
+          {
+            value: { service: 'cymrufyw', variant: 'default' },
+            title: 'cymrufyw',
+          },
+          {
+            value: { service: 'gahuza', variant: 'default' },
+            title: 'gahuza',
+          },
+          {
+            value: { service: 'gujarati', variant: 'default' },
+            title: 'gujarati',
+          },
+          {
+            value: { service: 'hausa', variant: 'default' },
+            title: 'hausa',
+          },
+          {
+            value: { service: 'hindi', variant: 'default' },
+            title: 'hindi',
+          },
+          {
+            value: { service: 'igbo', variant: 'default' },
+            title: 'igbo',
+          },
+          {
+            value: { service: 'indonesia', variant: 'default' },
+            title: 'indonesia',
+          },
+          {
+            value: { service: 'japanese', variant: 'default' },
+            title: 'japanese',
+          },
+          {
+            value: { service: 'korean', variant: 'default' },
+            title: 'korean',
+          },
+          {
+            value: { service: 'kyrgyz', variant: 'default' },
+            title: 'kyrgyz',
+          },
+          {
+            value: { service: 'marathi', variant: 'default' },
+            title: 'marathi',
+          },
+          {
+            value: { service: 'mundo', variant: 'default' },
+            title: 'mundo',
+          },
+          {
+            value: { service: 'naidheachdan', variant: 'default' },
+            title: 'naidheachdan',
+          },
+          {
+            value: { service: 'nepali', variant: 'default' },
+            title: 'nepali',
+          },
+          {
+            value: { service: 'news', variant: 'default' },
+            title: 'news',
+          },
+          {
+            value: { service: 'newsround', variant: 'default' },
+            title: 'newsround',
+          },
+          {
+            value: { service: 'pashto', variant: 'default' },
+            title: 'pashto',
+          },
+          {
+            value: { service: 'persian', variant: 'default' },
+            title: 'persian',
+          },
+          {
+            value: { service: 'pidgin', variant: 'default' },
+            title: 'pidgin',
+          },
+          {
+            value: { service: 'portuguese', variant: 'default' },
+            title: 'portuguese',
+          },
+          {
+            value: { service: 'punjabi', variant: 'default' },
+            title: 'punjabi',
+          },
+          {
+            value: { service: 'russian', variant: 'default' },
+            title: 'russian',
+          },
+          {
+            value: { service: 'scotland', variant: 'default' },
+            title: 'scotland',
+          },
+          {
+            value: { service: 'sport', variant: 'default' },
+            title: 'sport',
+          },
+          {
+            value: { service: 'sinhala', variant: 'default' },
+            title: 'sinhala',
+          },
+          {
+            value: { service: 'somali', variant: 'default' },
+            title: 'somali',
+          },
+          {
+            value: { service: 'swahili', variant: 'default' },
+            title: 'swahili',
+          },
+          {
+            value: { service: 'tamil', variant: 'default' },
+            title: 'tamil',
+          },
+          {
+            value: { service: 'telugu', variant: 'default' },
+            title: 'telugu',
+          },
+          {
+            value: { service: 'thai', variant: 'default' },
+            title: 'thai',
+          },
+          {
+            value: { service: 'tigrinya', variant: 'default' },
+            title: 'tigrinya',
+          },
+          {
+            value: { service: 'turkce', variant: 'default' },
+            title: 'turkce',
+          },
+          {
+            value: { service: 'urdu', variant: 'default' },
+            title: 'urdu',
+          },
+          {
+            value: { service: 'uzbek', variant: 'default' },
+            title: 'uzbek',
+          },
+          {
+            value: { service: 'vietnamese', variant: 'default' },
+            title: 'vietnamese',
+          },
+          {
+            value: { service: 'yoruba', variant: 'default' },
+            title: 'yoruba',
+          },
+          {
+            value: { service: 'serbian', variant: 'cyr' },
+            title: 'serbian-cyr',
+          },
+          {
+            value: { service: 'serbian', variant: 'lat' },
+            title: 'serbian-lat',
+          },
+          {
+            value: { service: 'ukchina', variant: 'simp' },
+            title: 'ukchina-simp',
+          },
+          {
+            value: { service: 'ukchina', variant: 'trad' },
+            title: 'ukchina-trad',
+          },
+          {
+            value: { service: 'zhongwen', variant: 'simp' },
+            title: 'zhongwen-simp',
+          },
+          {
+            value: { service: 'zhongwen', variant: 'trad' },
+            title: 'zhongwen-trad',
+          },
+          {
+            value: { service: 'ukrainian', variant: 'default' },
+            title: 'ukrainian-default',
+          },
+          {
+            value: { service: 'ukrainian', variant: 'ru-UA' },
+            title: 'ukrainian-ru-UA',
+          },
+        ],
+        // Should "Container size" be shown, or just the "circlehollow" icon
+        dynamicTitle: true,
+      },
     },
   },
-};
+  parameters: {
+    passArgsFirst: false,
+    options: {
+      panelPosition: 'right',
+      sidebarAnimcations: true,
+      theme,
+    },
+    docs: {
+      container: ({ context, children }) =>
+        DocsDecorator({ context, children }),
+    },
+    backgrounds: {
+      default: 'CPS',
+      values: [
+        {
+          name: 'Optimo',
+          value: '#F6F6F6',
+        },
+        {
+          name: 'CPS',
+          value: '#FFFFFF',
+        },
+      ],
+    },
+  },
+  decorators: [
+    story => {
+      useEffect(() => {
+        if (isChromatic()) {
+          forceVisible();
+        }
+      }, []);
 
-addDecorator((Story, context) => {
-  return (
-    <ThemeProvider service={context.globals.service.service}>
-      <ToggleContextProvider toggles={{}}>
-        <ServiceContextProvider
-          service={context.globals.service.service}
-          variant={context.globals.service.variant}
-        >
-          <EventTrackingContextProvider pageData={pageDataFixture}>
-            <UserContextProvider>
-              <Story />
-            </UserContextProvider>
-          </EventTrackingContextProvider>
-        </ServiceContextProvider>
-      </ToggleContextProvider>
-    </ThemeProvider>
-  );
-});
-
-export const parameters = {
-  passArgsFirst: false,
-  options: {
-    panelPosition: 'right',
-    sidebarAnimcations: true,
-    theme,
-  },
-  docs: {
-    container: ({ context, children }) => DocsDecorator({ context, children }),
-  },
-  backgrounds: {
-    default: 'CPS',
-    values: [
-      {
-        name: 'Optimo',
-        value: '#F6F6F6',
-      },
-      {
-        name: 'CPS',
-        value: '#FFFFFF',
-      },
-    ],
-  },
+      return (
+        <>
+          <Global
+            styles={[
+              REITH_SERIF_REGULAR,
+              REITH_SERIF_BOLD,
+              REITH_SERIF_LIGHT,
+              REITH_SERIF_MEDIUM,
+              REITH_SERIF_EXTRA_BOLD,
+              REITH_SANS_REGULAR,
+              REITH_SANS_BOLD,
+              REITH_SANS_LIGHT,
+              REITH_SANS_LIGHT_ITALIC,
+              REITH_SANS_MEDIUM,
+              REITH_SANS_EXTRA_BOLD,
+              REITH_SANS_EXTRA_BOLD_ITALIC,
+              REITH_SANS_CONDENSED_REGULAR,
+              REITH_SANS_CONDENSED_BOLD,
+              NOTO_SERIF_SINHALA_REGULAR,
+              NOTO_SERIF_SINHALA_BOLD,
+              NOTO_SANS_TAMIL_REGULAR,
+              NOTO_SANS_TAMIL_BOLD,
+              MALLANNA_REGULAR,
+              NOTO_SANS_ETHIOPIC_REGULAR,
+              NOTO_SANS_ETHIOPIC_BOLD,
+              PADAUK_REGULAR,
+              PADAUK_BOLD,
+              NOTO_SERIF_BENGALI_REGULAR,
+              NOTO_SERIF_BENGALI_BOLD,
+              REITH_QALAM_REGULAR,
+              REITH_QALAM_BOLD,
+            ]}
+          />
+          <GlobalStyles />
+          {story()}
+        </>
+      );
+    },
+    (story, context) => (
+      <ThemeProvider service={context.globals.service.service}>
+        <ToggleContextProvider toggles={{}}>
+          <ServiceContextProvider
+            service={context.globals.service.service}
+            variant={context.globals.service.variant}
+          >
+            <EventTrackingContextProvider pageData={pageDataFixture}>
+              <UserContextProvider>{story(context)}</UserContextProvider>
+            </EventTrackingContextProvider>
+          </ServiceContextProvider>
+        </ToggleContextProvider>
+      </ThemeProvider>
+    ),
+  ],
 };
+export default preview;
