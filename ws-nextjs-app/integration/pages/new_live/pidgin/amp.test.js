6b63c8a4f252c21fc94c8ca46cf796053fded389
SHAYNE MARC ENZO AHCHOON <shayne.ahchoon@bbc.co.uk>
2023-05-15 16:43:15 +0100

    WSTEAM1-424: Initial integration test config.

---
+++ ws-nextjs-app/integration/pages/new_live/pidgin/amp.test.js
@@ -0,0 +1,12 @@
+/**
+ * @service pidgin
+ * @pathname /pidgin/new_live/c7p765ynk9qt
+ */
+
+import runHeaderTests from '../headerTests';
+
+describe('AMP', () => {
+  describe(pageType, () => {
+    runHeaderTests();
+  });
+});
---
+++ ws-nextjs-app/integration/pages/new_live/pidgin/canonical.test.js
@@ -0,0 +1,12 @@
+/**
+ * @service pidgin
+ * @pathname /pidgin/new_live/c7p765ynk9qt
+ */
+
+import runHeaderTests from '../headerTests';
+
+describe('Canonical', () => {
+  describe(pageType, () => {
+    runHeaderTests();
+  });
+});
