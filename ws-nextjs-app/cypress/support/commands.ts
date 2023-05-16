f55c6838d879f5295ab4c0195f3dca63c8fc1ce1
Harvey Peachey <harvey.peachey@bbc.co.uk>
2023-05-16 10:01:18 +0100

    exports and imports custom commands

--- ws-nextjs-app/cypress/support/commands.ts
+++ ws-nextjs-app/cypress/support/commands.ts
@@ -1,4 +1,7 @@
+import { getPageData } from '../../../cypress/support/commands/application';
 /// <reference types="cypress" />
+Cypress.Commands.add('getPageData', getPageData);
+
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
@@ -11,7 +14,6 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
 //
 //
 // -- This is a child command --
