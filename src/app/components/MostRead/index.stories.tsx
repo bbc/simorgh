aa6f1377cb1553fe7b0f3eaf7f8c158a09e7e20b
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-05-17 11:40:46 +0100

    Creating baseline story for migrating Most Read component

---
+++ src/app/components/MostRead/index.stories.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { withKnobs } from '@storybook/addon-knobs';
+import ThemeProvider from '../ThemeProvider';
+import { ServiceContextProvider } from '../../contexts/ServiceContext';
+import { Services } from '../../models/types/global';
+import { StoryProps } from '../../models/types/storybook';
+import MostReadContainer from '../../legacy/containers/MostRead';
+import { withServicesKnob } from '../../legacy/psammead/psammead-storybook-helpers/src';
+import { ToggleContextProvider } from '../../contexts/ToggleContext';
+import { RequestContextProvider } from '../../contexts/RequestContext';
+import { ARTICLE_PAGE } from '../../routes/utils/pageTypes';
+
+const staticMostReadURL = (service: Services, variant: string) =>
+  variant !== 'default'
+    ? `./data/${service}/mostRead/${variant}.json`
+    : `./data/${service}/mostRead/index.json`;
+
+interface Props extends StoryProps {
+  columnLayout?: 'oneColumn' | 'twoColumn' | 'multiColumn';
+  size?: 'default' | 'small';
+}
+
+const Component = ({
+  service,
+  variant,
+  columnLayout = 'multiColumn',
+  size = 'default',
+}: Props) => (
+  <ThemeProvider service={service} variant={variant}>
+    <ToggleContextProvider>
+      <RequestContextProvider
+        isAmp={false}
+        isApp={false}
+        pageType={ARTICLE_PAGE}
+        service={service}
+        statusCode={200}
+        pathname={`/${service}`}
+        variant={variant}
+      >
+        <ServiceContextProvider service={service} variant={variant}>
+          <MostReadContainer
+            mostReadEndpointOverride={staticMostReadURL(service, variant)}
+            size={size}
+            columnLayout={columnLayout}
+          />
+        </ServiceContextProvider>
+      </RequestContextProvider>
+    </ToggleContextProvider>
+  </ThemeProvider>
+);
+
+export default {
+  title: 'New Components/Most Read',
+  Component,
+  decorators: [withKnobs, withServicesKnob({ defaultService: 'pidgin' })],
+};
+
+export const ArticlePage5Columns = ({ service, variant }: Props) => (
+  <Component service={service} variant={variant} />
+);
+
+export const Ltr2Columns = ({ service, variant }: Props) => (
+  <Component service={service} variant={variant} columnLayout="twoColumn" />
+);
+
+export const StoryPage1Column = ({ service, variant }: Props) => (
+  <Component
+    service={service}
+    variant={variant}
+    size="small"
+    columnLayout="oneColumn"
+  />
+);
+
+export const Japanese1Column = ({ variant }: Props) => (
+  <Component service="japanese" columnLayout="oneColumn" variant={variant} />
+);
+
+export const Rtl2Columns = ({ variant }: Props) => (
+  <Component service="persian" columnLayout="twoColumn" variant={variant} />
+);
