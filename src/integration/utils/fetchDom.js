cb0084347cbf758e3b217c1cd529b8b908b6584e
SHAYNE MARC ENZO AHCHOON <shayne.ahchoon@bbc.co.uk>
2023-05-17 15:47:32 +0100

    WSTEAM1-424: Merge latest

--- src/integration/utils/fetchDom.js
+++ src/integration/utils/fetchDom.js
@@ -28,7 +28,7 @@ const faultTolerantDomFetch = ({ url, runScripts, headers }) =>
           html,
           runScripts ? { runScripts: 'dangerously' } : {},
         );
-        console.log('CHECK URL', html);
+        console.log('CHECK URL', url);
         console.log('CHECK', html);
         resolve(dom);
       } catch (error) {
