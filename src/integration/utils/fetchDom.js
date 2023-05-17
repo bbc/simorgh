a4d4c10b4526270dbb8f9c6eef76bd9ab4d45885
SHAYNE MARC ENZO AHCHOON <shayne.ahchoon@bbc.co.uk>
2023-05-17 14:53:42 +0100

    WSTEAM1-424: alter workflow

--- src/integration/utils/fetchDom.js
+++ src/integration/utils/fetchDom.js
@@ -28,7 +28,7 @@ const faultTolerantDomFetch = ({ url, runScripts, headers }) =>
           html,
           runScripts ? { runScripts: 'dangerously' } : {},
         );
-
+        console.log('CHECK', html);
         resolve(dom);
       } catch (error) {
         const isSocketHangUpError = error
--- ws-nextjs-app/integration/pages/new_live/headerTests.js
+++ ws-nextjs-app/integration/pages/new_live/headerTests.js
@@ -1,5 +1,11 @@
-import { runHeaderTests } from '../../../../src/integration/common';
+// import { runHeaderTests } from '../../../../src/integration/common';
 
 export default () => {
-  runHeaderTests();
+  it('I can see the branding', () => {
+    const logo = document.querySelector('header svg');
+
+    expect(logo).toBeInTheDocument();
+    expect(logo.parentNode.textContent).toBeTruthy();
+    expect(logo.parentNode.textContent).toMatchSnapshot();
+  });
 };
