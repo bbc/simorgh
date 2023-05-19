f3db843130255b8cb07382d6f6aac2ad4b8718b2
LilyL0u <lily.higham@bbc.co.uk>
2023-05-19 16:55:00 +0100

    add cypress log to see noscript contents

--- cypress/support/commands/analytics.js
+++ cypress/support/commands/analytics.js
@@ -1,5 +1,9 @@
 // Should be moved into integration/pages/index.js once all pages have ATI
 Cypress.Commands.add('hasNoscriptImgAtiUrl', atiUrl => {
+  cy.get('noscript').then($noscript => {
+    cy.log($noscript);
+  });
+
   cy.get('noscript').should(
     'contain',
     `<img height="1px" width="1px" alt="" style="position:absolute" src="${atiUrl}`,
