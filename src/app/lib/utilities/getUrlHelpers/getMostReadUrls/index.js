97070a4274c956f75fa34338bf9fcab246473407
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-05-23 10:30:45 +0100

    Add capability to call the BFF for most read data

--- src/app/lib/utilities/getUrlHelpers/getMostReadUrls/index.js
+++ src/app/lib/utilities/getUrlHelpers/getMostReadUrls/index.js
@@ -1,9 +1,16 @@
-export const getMostReadEndpoint = ({ service, variant }) =>
-  variant
-    ? `/${service}/mostread/${variant}.json`
-    : `/${service}/mostread.json`;
-
 export const getLocalMostReadEndpoint = ({ service, variant = 'default' }) =>
   `./data/${service}/mostRead/${
     variant === 'default' ? 'index' : variant
   }.json`;
+
+export const getMostReadEndpoint = ({ service, variant, isBff = false }) => {
+  if (isBff) {
+    return variant
+      ? `/fd/simorgh-bff?pageType=mostRead&service=${service}&variant=${variant}`
+      : `/fd/simorgh-bff?pageType=mostRead&service=${service}`;
+  }
+
+  return variant
+    ? `/${service}/mostread/${variant}.json`
+    : `/${service}/mostread.json`;
+};
