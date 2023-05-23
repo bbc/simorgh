c368cc44ed7954ed8664284308ec8a0c98cddd98
Harvey Peachey <harvey.peachey@bbc.co.uk>
2023-05-23 09:24:27 +0100

    adds link to comment for potential context

--- ws-nextjs-app/pages/[service]/[[...]].page.tsx
+++ ws-nextjs-app/pages/[service]/[[...]].page.tsx
@@ -22,7 +22,7 @@ export const getServerSideProps: GetServerSideProps = async context => {
       isNextJs: true,
       service,
       status: 404,
-      timeOnServer: Date.now(), // TODO: check if needed?
+      timeOnServer: Date.now(), // TODO: check if needed? See https://github.com/bbc/simorgh/pull/10857/files#r1200274478
       variant: variant?.[0] || null,
     },
   };
