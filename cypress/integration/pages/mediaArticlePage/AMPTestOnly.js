e2f8592e83a540a971de9deebddd9018e5de3da3
Nelson Agbonson <nelson.agbonson@bbc.co.uk>
2023-05-30 06:40:51 +0100

    add testcases from article specfile

--- cypress/integration/pages/mediaArticlePage/AMPTestOnly.js
+++ cypress/integration/pages/mediaArticlePage/AMPTestOnly.js
@@ -1,10 +1,41 @@
-export default ({ service, pageType }) => {
+import appConfig from '../../../../src/server/utilities/serviceConfigs';
+import {
+  getBlockByType,
+  getBlockData,
+  getVideoEmbedUrl,
+} from '../articles/helpers';
+
+export default ({ service, pageType, variant }) => {
   describe(`AMP only test for ${service} ${pageType}`, () => {
+    let articlesData;
+    before(() => {
+      cy.getPageData({ service, pageType: 'article', variant }).then(
+        ({ body }) => {
+          articlesData = body;
+        },
+      );
+    });
+
     describe('Media Player: AMP', () => {
       it('Media player is rendered on page', () => {
         cy.get('[data-e2e="media-player"]').within(() => {
           cy.get('div').should('have.attr', 'data-e2e').should('not.be.empty');
         });
+      });
+
+      it('should render an iframe with a valid URL', () => {
+        const media = getBlockData('video', articlesData);
+        if (media && media.type === 'video') {
+          const { lang } = appConfig[service][variant];
+          const embedUrl = getVideoEmbedUrl(articlesData, lang, true);
+          cy.get(`amp-iframe[src="${embedUrl}"]`).should('be.visible');
+          cy.testResponseCodeAndTypeRetry({
+            path: embedUrl,
+            responseCode: 200,
+            type: 'text/html',
+            allowFallback: true,
+          });
+        }
       });
     });
   });
--- cypress/integration/pages/mediaArticlePage/CanonicalTestOnly.js
+++ cypress/integration/pages/mediaArticlePage/CanonicalTestOnly.js
@@ -1,4 +1,11 @@
-export default ({ service, pageType }) => {
+import appConfig from '../../../../src/server/utilities/serviceConfigs';
+import {
+  getBlockByType,
+  getBlockData,
+  getVideoEmbedUrl,
+} from '../articles/helpers';
+
+export default ({ service, pageType, variant }) => {
   describe(`Canonical only test for ${service} ${pageType}`, () => {
     describe(`Chartbeat analytics`, () => {
       it('should have a script with src value set to chartbeat source', () => {
@@ -9,39 +16,140 @@ export default ({ service, pageType }) => {
       });
     });
 
+    // describe('Media Player: Canonical', () => {
+    //   it('Media player is rendered on page', () => {
+    //     cy.get('[data-e2e="media-player"]')
+    //       .should('be.visible')
+    //       .should('have.css', 'top', '0px')
+    //       .within(() => {
+    //         cy.get('img')
+    //           .should('be.visible')
+    //           .should('have.attr', 'src')
+    //           .should('not.be.empty');
+    //         cy.get('button')
+    //           .should('be.visible')
+    //           .within(() => {
+    //             cy.get('svg').should('be.visible');
+    //             cy.get('time')
+    //               .should('be.visible')
+    //               .should('have.attr', 'datetime');
+    //             // .and('eq', durationISO8601);
+    //           });
+    //       });
+    //   });
+
+    //   it('media can be played', () => {
+    //     cy.intercept('GET', 'https://open.test.bbc.co.uk/mediaselector').as(
+    //       'mediaSelector',
+    //     );
+    //     cy.get('[data-e2e="media-player"]')
+    //       .should('be.visible')
+    //       .should('have.css', 'top', '0px')
+    //       .within(() => {
+    //         cy.get('button').click();
+    //         cy.get(`iframe`).should('be.visible');
+    //       });
+    //   });
+    // });
+
     describe('Media Player: Canonical', () => {
-      it('Media player is rendered on page', () => {
-        cy.get('[data-e2e="media-player"]')
-          .should('be.visible')
-          .should('have.css', 'top', '0px')
-          .within(() => {
-            cy.get('img')
-              .should('be.visible')
-              .should('have.attr', 'src')
-              .should('not.be.empty');
-            cy.get('button')
-              .should('be.visible')
-              .within(() => {
-                cy.get('svg').should('be.visible');
-                cy.get('time')
-                  .should('be.visible')
-                  .should('have.attr', 'datetime');
-                // .and('eq', durationISO8601);
-              });
+      it('should render a visible placeholder image', () => {
+        cy.window().then(win => {
+          const media = getBlockData('video', {
+            data: { article: win.SIMORGH_DATA.pageData },
           });
+          if (media) {
+            cy.get('[data-e2e="media-player"]').within(() => {
+              cy.get('[data-e2e="media-player__placeholder"] img')
+                .should('be.visible')
+                .should('have.attr', 'src')
+                .should('not.be.empty');
+            });
+          }
+        });
       });
 
-      it('media can be played', () => {
-        cy.intercept('GET', 'https://open.test.bbc.co.uk/mediaselector').as(
-          'mediaSelector',
-        );
-        cy.get('[data-e2e="media-player"]')
-          .should('be.visible')
-          .should('have.css', 'top', '0px')
-          .within(() => {
-            cy.get('button').click();
-            cy.get(`iframe`).should('be.visible');
+      it('should render a visible guidance message', () => {
+        cy.window().then(win => {
+          const media = getBlockData('video', {
+            data: { article: win.SIMORGH_DATA.pageData },
           });
+
+          if (media) {
+            const longGuidanceWarning =
+              media.model.blocks[1].model.blocks[0].model.versions[0].warnings
+                .long;
+
+            cy.get('[data-e2e="media-player"]')
+              .eq(0)
+              .within(() => {
+                // Check for video with guidance message
+                if (longGuidanceWarning) {
+                  cy.get('[data-e2e="media-player__placeholder"] strong')
+                    .should('be.visible')
+                    .and('contain', longGuidanceWarning);
+                  // Check for video with no guidance message
+                } else {
+                  cy.get('[data-e2e="media-player__guidance"] strong').should(
+                    'not.exist',
+                  );
+                }
+              });
+          }
+        });
+      });
+
+      it('should have a visible play button and valid duration', () => {
+        cy.window().then(win => {
+          const media = getBlockData('video', {
+            data: { article: win.SIMORGH_DATA.pageData },
+          });
+          if (media && media.type === 'video') {
+            const aresMediaBlocks = media.model.blocks[1].model.blocks[0];
+            const { durationISO8601 } = aresMediaBlocks.model.versions[0];
+
+            cy.get('[data-e2e="media-player"]').within(() => {
+              cy.get('button')
+                .should('be.visible')
+                .within(() => {
+                  cy.get('svg').should('be.visible');
+                  cy.get('time')
+                    .should('be.visible')
+                    .should('have.attr', 'datetime')
+                    .and('eq', durationISO8601);
+                });
+            });
+          }
+        });
+      });
+
+      it('should render an iframe with a valid URL when a user clicks play', () => {
+        cy.window().then(win => {
+          const body = win.SIMORGH_DATA.pageData;
+
+          const media = getBlockData('video', {
+            data: { article: body },
+          });
+
+          if (media && media.type === 'video') {
+            const { lang } = appConfig[service][variant];
+            const embedUrl = getVideoEmbedUrl(
+              {
+                data: { article: body },
+              },
+              lang,
+            );
+            cy.get('[data-e2e="media-player"] button').click();
+            cy.get(`iframe[src*="${embedUrl}"]`).should('be.visible');
+
+            cy.testResponseCodeAndTypeRetry({
+              path: embedUrl,
+              responseCode: 200,
+              type: 'text/html',
+              allowFallback: true,
+            });
+          }
+        });
       });
     });
   });
