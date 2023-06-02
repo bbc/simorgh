1ab7d92d39560d4c1820917e700eb35f39f1244c
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-06-02 15:10:14 +0100

    Fix failing unit tests

--- ws-nextjs-app/pages/[service]/new_live/[id]/live.test.tsx
+++ ws-nextjs-app/pages/[service]/new_live/[id]/live.test.tsx
@@ -15,6 +15,9 @@ const mockPageData = {
   someResponse: {
     block: 'Its a block',
   },
+  liveTextStream: {
+    content: postFixture,
+  },
 };
 
 const mockPageDataWithPosts = {
@@ -25,7 +28,9 @@ const mockPageDataWithPosts = {
   someResponse: {
     block: 'Its a block',
   },
-  posts: postFixture,
+  liveTextStream: {
+    content: postFixture,
+  },
 };
 
 describe('Live Page', () => {
