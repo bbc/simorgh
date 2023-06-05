ce4e90197c75a2f1aadd14b17ff431beb80cdf7e
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-06-05 11:29:54 +0100

    WSTEAM1-412: Update StreamResponse type and checks

--- ws-nextjs-app/pages/[service]/new_live/[id]/Posts/index.tsx
+++ ws-nextjs-app/pages/[service]/new_live/[id]/Posts/index.tsx
@@ -84,7 +84,6 @@ const PostItem = ({ postItem }: { postItem: Post }) => {
 const PostsList = ({ postData }: { postData: StreamResponse }) => {
   const { results: postResults } = postData?.data;
 
-  if (!postResults) return null;
   if (postResults.length === 0) return null;
 
   return (
