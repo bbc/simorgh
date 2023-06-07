3bb492a7598e5887525d40902e3a7cb06c7a09ea
alex-magana <alex.magana@andela.com>
2023-06-07 12:26:37 +0300

    Update Homepage

--- src/app/components/ATIAnalytics/params/genericPage/buildParams.test.ts
+++ src/app/components/ATIAnalytics/params/genericPage/buildParams.test.ts
@@ -1,7 +1,7 @@
 import * as analyticsUtils from '#lib/analyticsUtils';
 import { RequestContextProps } from '../../../../contexts/RequestContext';
 import { ServiceConfig } from '../../../../models/types/serviceConfig';
-import { buildTopicPageATIParams, buildTopicPageATIUrl } from './buildParams';
+import { buildPageATIParams, buildPageATIUrl } from './buildParams';
 
 (analyticsUtils.getAtUserId as jest.Mock) = jest.fn();
 (analyticsUtils.getCurrentTime as jest.Mock) = jest
@@ -26,40 +26,44 @@ const serviceContext: ServiceConfig = {
   lang: 'pcm',
 };
 
-const topicPage = {
-  title: 'pageTitle',
-  contentType: 'index-category',
+const atiData = {
+  analytics: {
+    contentId: 'urn:bbc:tipo:topic:cm7682qz7v1t',
+    contentType: 'index-home',
+    pageIdentifier: 'kyrgyz.page',
+  },
+  title: 'BBC News Кыргыз Кызматы -1',
 };
 
 const validTopicPageURLParams = {
   appName: serviceContext.atiAnalyticsAppName,
-  contentType: topicPage.contentType,
-  pageTitle: topicPage.title,
+  contentId: `urn:bbc:tipo:topic:${requestContext.id}`,
+  producerId: serviceContext.atiAnalyticsProducerId,
+  contentType: atiData.analytics.contentType,
+  pageIdentifier: atiData.analytics.pageIdentifier,
+  pageTitle: atiData.title,
   platform: requestContext.platform,
   statsDestination: requestContext.statsDestination,
   service: serviceContext.service,
   libraryVersion: 'simorgh',
-  producerId: serviceContext.atiAnalyticsProducerId,
   language: serviceContext.lang,
-  pageIdentifier: `${serviceContext.service}.topics.${requestContext.id}.page`,
-  contentId: `urn:bbc:tipo:topic:${requestContext.id}`,
 };
 
 describe('implementation of buildTopicPageATIParams and buildTopicPageATIUrl', () => {
   it('should return the correct object for topic page', () => {
-    const result = buildTopicPageATIParams(
-      topicPage,
+    const result = buildPageATIParams({
+      atiData,
       requestContext,
       serviceContext,
-    );
+    });
     expect(result).toEqual(validTopicPageURLParams);
   });
   it('should return the correct url for topic page', () => {
-    const result = buildTopicPageATIUrl(
-      topicPage,
+    const result = buildPageATIUrl({
+      atiData,
       requestContext,
       serviceContext,
-    );
+    });
     expect(result).toMatchInlineSnapshot(
       `"s=598285&s2=atiAnalyticsProducerId&p=pidgin.topics.validId.page&r=0x0x24x24&re=1024x768&hl=00-00-00&lng=en-US&x1=[urn%3Abbc%3Atipo%3Atopic%3AvalidId]&x2=[responsive]&x3=[atiAnalyticsAppName]&x4=[pcm]&x5=[http%253A%252F%252Flocalhost%252F]&x7=[index-category]&x8=[simorgh]&x9=[pageTitle]"`,
     );
