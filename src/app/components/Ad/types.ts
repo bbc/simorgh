44d8134bb7221ff1e758f844a7f5a9acb04bb87d
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-06-09 14:24:36 +0100

    Update className is optional

--- src/app/components/Ad/types.ts
+++ src/app/components/Ad/types.ts
@@ -1,11 +1,11 @@
 export const SLOT_TYPES = {
   LEADERBOARD: 'leaderboard',
   MPU: 'mpu',
-};
+} as const;
 
 export type SlotType = (typeof SLOT_TYPES)[keyof typeof SLOT_TYPES];
 
 export interface AdProps {
   slotType: SlotType;
-  className: string;
+  className?: string;
 }
