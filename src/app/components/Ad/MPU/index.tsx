5e5b0a8b30335420a3f694dcd1c72621a06ee184
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-06-09 15:24:22 +0100

    Use slot_types const

--- src/app/components/Ad/MPU/index.tsx
+++ src/app/components/Ad/MPU/index.tsx
@@ -1,9 +1,12 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
-import { NegativeMargin } from '#app/lib/styles.const';
+import { NegativeMargin } from '../../../lib/styles.const';
 import Ad from '..';
+import { SLOT_TYPES } from '../types';
 
 export default (className?: string) => {
-  return <Ad css={NegativeMargin} slotType="mpu" className={className} />;
+  return (
+    <Ad css={NegativeMargin} slotType={SLOT_TYPES.MPU} className={className} />
+  );
 };
