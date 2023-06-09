0044683ee4941c4c648f87b9806b9052dd8bbb86
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-06-09 11:15:28 +0100

    Rename & migrate styles.const

--- src/app/lib/styles.const/index.test.ts
+++ src/app/lib/styles.const/index.test.ts
@@ -1,7 +1,8 @@
-import * as styles from './styles.const';
+import * as styles from '.';
 
 describe('Styles', () => {
   Object.keys(styles).forEach(style => {
+    // @ts-expect-error suppress No index signature with a parameter of type string was found on type
     const value = styles[style];
     it(`${style} should match text`, () => {
       expect(value).toMatchSnapshot();
