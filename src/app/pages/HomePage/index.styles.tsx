5671ec0abd4124917d6b800073cf6069f312ae3a
louisearchibald <louise.archibald@bbc.co.uk>
2023-06-12 17:50:39 +0100

    reinstating explicit groups for breakpoints 1 & 2

--- src/app/pages/HomePage/index.styles.tsx
+++ src/app/pages/HomePage/index.styles.tsx
@@ -14,10 +14,14 @@ const styles = {
   }),
   margins: ({ spacings, mq }: Theme) =>
     css({
+      margin: `${spacings.TRIPLE}rem 0`,
       [mq.GROUP_0_MAX_WIDTH]: {
         margin: `${spacings.TRIPLE}rem 0`,
       },
-      [mq.GROUP_2_MAX_WIDTH]: {
+      [mq.GROUP_1_ONLY]: {
+        margin: `${spacings.QUADRUPLE}rem 0`,
+      },
+      [mq.GROUP_2_ONLY]: {
         margin: `${spacings.QUADRUPLE}rem 0`,
       },
       [mq.GROUP_3_ONLY]: {
