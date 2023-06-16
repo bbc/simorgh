ef35effbc8377c7418918cc5fb9bc4960947ddcf
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-06-16 14:58:20 +0100

    WSTEAM1-446: Add GREY_1 to themeprovider

--- src/app/components/ThemeProvider/palette.ts
+++ src/app/components/ThemeProvider/palette.ts
@@ -15,6 +15,7 @@ export const EBON = '#222222';
 export const GHOST = '#FDFDFD';
 export const GREY_10 = '#141414';
 export const GREY_11 = '#BABABA';
+export const GREY_1 = '#FEFEFE';
 export const GREY_2 = '#F6F6F6';
 export const GREY_3 = '#E6E8EA';
 export const GREY_4 = '#B0B2B4';
