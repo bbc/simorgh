c5f1a58d94eff0028300aa38b9a07791158247e6
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-06-19 15:54:32 +0100

    Adding global props to allow ref attributes on divs

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,11 @@
 declare module '*.md';
+
+declare namespace JSX {
+  interface DivProps extends React.HTMLAttributes<HTMLDivElement> {
+    ref?: ForwardedRef<HTMLDivElement> | null;
+  }
+
+  interface IntrinsicElements {
+    div: DivProps;
+  }
+}
