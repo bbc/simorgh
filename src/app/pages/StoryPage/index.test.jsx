81d33043403e45b806442f32b9d6a77d3e9ee71a
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-06-21 18:10:34 +0100

    Fix FrontPage & StoryPage tests

--- src/app/pages/StoryPage/index.test.jsx
+++ src/app/pages/StoryPage/index.test.jsx
@@ -12,7 +12,7 @@ import getInitialData from '#app/routes/cpsAsset/getInitialData';
 
 // mock data
 import pidginPageData from '#data/pidgin/cpsAssets/world-23252817';
-import pidginMostReadData from '#data/pidgin/mostRead/index.json';
+import { data as pidginMostReadData } from '#data/pidgin/mostRead/index.json';
 import igboPageData from '#data/igbo/cpsAssets/afirika-23252735';
 import russianPageDataWithInlinePromo from '#data/russian/cpsAssets/news-55041160';
 import ukrainianInRussianPageData from '#data/ukrainian/cpsAssets/news-russian-23333960.json';
