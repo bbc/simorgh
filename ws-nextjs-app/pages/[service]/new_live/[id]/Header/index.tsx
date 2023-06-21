161e28b9ac9a38fbd0936e026e838d534c160787
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-06-21 16:49:49 +0100

    WSTEAM1-447: Update snapshot

--- ws-nextjs-app/pages/[service]/new_live/[id]/Header/index.tsx
+++ ws-nextjs-app/pages/[service]/new_live/[id]/Header/index.tsx
@@ -32,7 +32,7 @@ const Header = ({
               <span css={styles.label} aria-hidden="true">
                 LIVE
               </span>
-              <VisuallyHiddenText>Live, </VisuallyHiddenText>
+              <VisuallyHiddenText lang="en-GB">Live, </VisuallyHiddenText>
               <span css={styles.title}>{title}</span>
             </span>
           ) : (
