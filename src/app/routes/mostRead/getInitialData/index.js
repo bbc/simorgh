57fe2fd3e913aa7fa140ff3d21f379a8ad1ad6b6
louisearchibald <louise.archibald@bbc.co.uk>
2023-06-30 15:49:04 +0100

    bff supports returning mostread data

--- src/app/routes/mostRead/getInitialData/index.js
+++ src/app/routes/mostRead/getInitialData/index.js
@@ -1,10 +1,14 @@
 import { getMostReadEndpoint } from '#lib/utilities/getUrlHelpers/getMostReadUrls';
 import fetchPageData from '../../utils/fetchPageData';
 import getErrorStatusCode from '../../utils/fetchPageData/utils/getErrorStatusCode';
+import isLocal from '../../../lib/utilities/isLocal';
 
 export default async ({ service, variant, pageType }) => {
   try {
-    const mostReadUrl = getMostReadEndpoint({ service, variant }).split('.')[0];
+    const isBff = !isLocal();
+    const mostReadUrl = getMostReadEndpoint({ service, variant, isBff }).split(
+      '.',
+    )[0];
     const { json, status } = await fetchPageData({
       path: mostReadUrl,
       pageType,
