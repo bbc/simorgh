7c1aa311359322e1fca0fe726a6406beab2c9a54
karinathomasbbc <karina.thomas@bbc.co.uk>
2023-07-03 14:52:24 +0100

    Adding check for length of most read items to the most read items page

--- cypress/integration/pages/mostReadPage/tests.js
+++ cypress/integration/pages/mostReadPage/tests.js
@@ -1,47 +1,11 @@
-import path from 'ramda/src/path';
-import config from '../../../support/config/services';
+import appConfig from '../../../../src/server/utilities/serviceConfigs';
 
-export default ({ service, pageType, variant }) => {
-  describe(`No testsToAlwaysRun to run for ${service} ${pageType}`, () => {
-    describe(`Tests for ${service} ${pageType}`, () => {
-      describe('Most Read component', () => {
-        before(() => {
-          cy.getToggles(config[service].name);
-        });
+export default ({ service, variant }) => {
+  const {
+    mostRead: { numberOfItems },
+  } = appConfig[service][variant];
 
-        // Allows constructing most watched path with variant
-        const serviceVariant = variant === 'default' ? '' : `/${variant}`;
-
-        const mostReadPath = `/${config[service].name}/mostread${serviceVariant}.json`;
-        it('should show/not show the Most Read component if the toggle is enabled for the service', function test() {
-          // Find if the service has component enabled in toggles, unless it is live environment which does not have mostRead toggle
-          cy.fixture(`toggles/${config[service].name}.json`).then(toggles => {
-            const mostReadIsEnabled = path(['mostRead', 'enabled'], toggles);
-            cy.log(`Service toggle enabled ${mostReadIsEnabled}`);
-            // If toggle is enabled for that service, also check there are more than 0 records to show
-            if (mostReadIsEnabled) {
-              cy.request(mostReadPath).then(({ body }) => {
-                const mostReadJson = body?.data || body;
-
-                const mostReadItems =
-                  mostReadJson?.totalRecords || mostReadJson.items?.length;
-
-                if (mostReadItems > 0) {
-                  cy.get('[data-e2e="most-read"]').within(() => {
-                    cy.get('li').should('exist');
-                  });
-                } else {
-                  cy.log('Not enough records to show component');
-                  cy.task('log', 'Not enough records to show component');
-                  cy.get('[data-e2e="most-read"]').within(() => {
-                    cy.get('li').should('not.exist');
-                  });
-                }
-              });
-            }
-          });
-        });
-      });
-    });
+  it(`should render ${numberOfItems} items`, () => {
+    cy.get('[data-e2e="most-read"] li').should('have.length', numberOfItems);
   });
 };
