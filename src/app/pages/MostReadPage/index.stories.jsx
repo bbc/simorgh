acfa8a71f81ac9141acfbe06f24a0d39c422ffbe
louisearchibald <louise.archibald@bbc.co.uk>
2023-07-03 16:40:10 +0100

    update to stories file

--- src/app/pages/MostReadPage/index.stories.jsx
+++ src/app/pages/MostReadPage/index.stories.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { getLocalMostReadEndpoint } from '#lib/utilities/getUrlHelpers/getMostReadUrls';
-import pidginMostReadData from '#data/pidgin/mostRead';
-import zhongwenSimpData from '#data/zhongwen/mostRead/simp.json';
+import pidginMostReadData from '../../../../data/pidgin/mostRead/index.json';
+import zhongwenSimpData from '../../../../data/zhongwen/mostRead/simp.json';
 import japaneseMostReadData from '../../../../data/japanese/mostRead/index.json';
 import persianMostReadData from '../../../../data/persian/mostRead/index.json';
 import bengaliMostReadData from '../../../../data/bengali/mostRead/index.json';
@@ -19,10 +18,6 @@ const Component = ({ service, pageData, variant } = {}) => (
       pageData={pageData}
       service={service}
       variant={variant}
-      mostReadEndpointOverride={getLocalMostReadEndpoint({
-        service,
-        variant,
-      })}
     />
   </BrowserRouter>
 );
@@ -33,21 +28,25 @@ export default {
 };
 
 export const Pidgin = () => (
-  <Component service="pidgin" pageData={pidginMostReadData} />
+  <Component service="pidgin" pageData={pidginMostReadData.data} />
 );
 
 export const ZhongwenSimple = () => (
-  <Component service="zhongwen" variant="simp" pageData={zhongwenSimpData} />
+  <Component
+    service="zhongwen"
+    variant="simp"
+    pageData={zhongwenSimpData.data}
+  />
 );
 
 export const Japanese = () => (
-  <Component service="japanese" pageData={japaneseMostReadData} />
+  <Component service="japanese" pageData={japaneseMostReadData.data} />
 );
 
 export const Persian = () => (
-  <Component service="persian" pageData={persianMostReadData} />
+  <Component service="persian" pageData={persianMostReadData.data} />
 );
 
 export const Bengali = () => (
-  <Component service="bengali" pageData={bengaliMostReadData} />
+  <Component service="bengali" pageData={bengaliMostReadData.data} />
 );
