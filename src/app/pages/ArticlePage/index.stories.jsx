e9fdbc88ef89a20d25db357b3db3eaafdb71b546
SHAYNE MARC ENZO AHCHOON <shayne.ahchoon@bbc.co.uk>
2023-07-04 16:13:22 +0100

    WSTEAM1-480: Add podcast promo article to storybook

--- src/app/pages/ArticlePage/index.stories.jsx
+++ src/app/pages/ArticlePage/index.stories.jsx
@@ -9,6 +9,7 @@ import { ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
 import articleData from '#data/news/articles/c0g992jmmkko.json';
 import articleDataWithRelatedContent from '#data/afrique/articles/c7yn6nznljdo.json';
 import articleDataWithSingleRelatedContent from '#data/afrique/articles/cz216x22106o.json';
+import articleDataWithPodcastPromo from '#data/russian/articles/c61q94n3rm3o.json';
 import withPageWrapper from '#containers/PageHandlers/withPageWrapper';
 import withOptimizelyProvider from '#containers/PageHandlers/withOptimizelyProvider';
 import ArticlePageComponent from './ArticlePage';
@@ -16,21 +17,26 @@ import ArticlePageComponent from './ArticlePage';
 const PageWithOptimizely = withOptimizelyProvider(ArticlePageComponent);
 const Page = withPageWrapper(PageWithOptimizely);
 
-const ComponentWithContext = ({ data: { data } }) => {
+const ComponentWithContext = ({
+  data: { data },
+  service = 'news',
+  podcastEnabled = false,
+}) => {
   return (
     <ToggleContextProvider
       toggles={{
         eventTracking: { enabled: true },
         mostRead: { enabled: true },
         frostedPromo: { enabled: true, value: 1 },
+        podcastPromo: { enabled: podcastEnabled },
       }}
     >
       {/* Service set to news to enable most read. Article data is in english */}
-      <ServiceContextProvider service="news">
+      <ServiceContextProvider service={service}>
         <RequestContextProvider
           isAmp={false}
           pageType={ARTICLE_PAGE}
-          service="news"
+          service={service}
         >
           <UserContextProvider>
             <MemoryRouter>
@@ -66,4 +72,13 @@ export const ArticlePageWithRelatedContent = props => (
 
 export const ArticlePageWithSingleRelatedContent = props => (
   <ComponentWithContext {...props} data={articleDataWithSingleRelatedContent} />
+);
+
+export const ArticlePageWithPodcastPromo = props => (
+  <ComponentWithContext
+    {...props}
+    data={articleDataWithPodcastPromo}
+    service="russian"
+    podcastEnabled
+  />
 );
