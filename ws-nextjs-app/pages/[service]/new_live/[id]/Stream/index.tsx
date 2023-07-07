18545190b666eb15ed1b25a826bf18b692353c8c
Harvey Peachey <harvey.peachey@bbc.co.uk>
2023-07-07 11:42:11 +0100

    adds heading to single post

--- ws-nextjs-app/pages/[service]/new_live/[id]/Stream/index.tsx
+++ ws-nextjs-app/pages/[service]/new_live/[id]/Stream/index.tsx
@@ -14,12 +14,10 @@ const Stream = ({
   if (!streamContent) return null;
   const { results: streamResults } = streamContent?.data;
 
-  if (streamResults.length === 0) return null;
+  const hasNoPost = streamResults.length === 0;
+  const hasSinglePost = streamResults.length === 1;
 
-  if (streamResults.length === 1) {
-    const post = streamResults[0];
-    return <Post post={post} />;
-  }
+  if (hasNoPost) return null;
 
   return (
     <div>
@@ -27,13 +25,17 @@ const Stream = ({
         Live Reporting
       </Heading>
       <Paragraph css={styles.subHeading}>By a random dude</Paragraph>
-      <ol css={styles.orderedList}>
-        {streamResults.map(post => (
-          <li key={post.urn} css={styles.listItem}>
-            <Post post={post} />
-          </li>
-        ))}
-      </ol>
+      {hasSinglePost ? (
+        <Post post={streamResults[0]} />
+      ) : (
+        <ol css={styles.orderedList}>
+          {streamResults.map(post => (
+            <li key={post.urn} css={styles.listItem}>
+              <Post post={post} />
+            </li>
+          ))}
+        </ol>
+      )}
     </div>
   );
 };
