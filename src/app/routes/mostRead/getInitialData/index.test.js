d225230b0baeb47b5e3ad08ad330592095981503
louisearchibald <louise.archibald@bbc.co.uk>
2023-07-11 15:24:57 +0100

    reseting mocks

--- src/app/routes/mostRead/getInitialData/index.test.js
+++ src/app/routes/mostRead/getInitialData/index.test.js
@@ -4,6 +4,9 @@ import getInitialData from '.';
 
 describe('dgkhsdkg', () => {
   beforeEach(() => {
+    jest.resetAllMocks();
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
     delete process.env.SIMORGH_APP_ENV;
   });
 
