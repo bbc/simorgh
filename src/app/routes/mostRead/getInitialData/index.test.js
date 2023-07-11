1e06418028eae83b9b900880c4fc391bb2cfd183
louisearchibald <louise.archibald@bbc.co.uk>
2023-07-11 12:05:09 +0100

    adding describe to test

--- src/app/routes/mostRead/getInitialData/index.test.js
+++ src/app/routes/mostRead/getInitialData/index.test.js
@@ -2,26 +2,28 @@ import { MOST_READ_PAGE } from '#app/routes/utils/pageTypes';
 import mostReadJson from '../../../../../data/pidgin/mostRead/index.json';
 import getInitialData from '.';
 
-beforeEach(() => {
-  delete process.env.SIMORGH_APP_ENV;
-});
-
-fetch.mockResponse(JSON.stringify(mostReadJson));
-
-it('should return essential data for a page to render', async () => {
-  const { pageData } = await getInitialData({
-    path: '/pidgin/popular/read',
-    service: 'pidgin',
-    pageType: MOST_READ_PAGE,
+describe('dgkhsdkg', () => {
+  beforeEach(() => {
+    delete process.env.SIMORGH_APP_ENV;
   });
 
-  expect(pageData.lastRecordTimeStamp).toEqual('2023-06-19T15:03:00Z');
-  expect(pageData.metadata.type).toEqual('mostRead');
-  expect(pageData.items[0].timestamp).toEqual(1687171616901);
-  expect(pageData.items[0].title).toEqual(
-    'Teams wey qualify for Afcon 2023 and how things stand for each group',
-  );
-  expect(pageData.items[0].href).toEqual(
-    'https://www.bbc.com/pidgin/articles/cz5kkgv41v0o',
-  );
+  fetch.mockResponse(JSON.stringify(mostReadJson));
+
+  it('should return essential data for a page to render', async () => {
+    const { pageData } = await getInitialData({
+      path: '/pidgin/popular/read',
+      service: 'pidgin',
+      pageType: MOST_READ_PAGE,
+    });
+
+    expect(pageData.lastRecordTimeStamp).toEqual('2023-06-19T15:03:00Z');
+    expect(pageData.metadata.type).toEqual('mostRead');
+    expect(pageData.items[0].timestamp).toEqual(1687171616901);
+    expect(pageData.items[0].title).toEqual(
+      'Teams wey qualify for Afcon 2023 and how things stand for each group',
+    );
+    expect(pageData.items[0].href).toEqual(
+      'https://www.bbc.com/pidgin/articles/cz5kkgv41v0o',
+    );
+  });
 });
