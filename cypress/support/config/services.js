261c369c635cf66f60e66a243d81b352b84ca51e
LilyL0u <lily.higham@bbc.co.uk>
2023-07-12 11:18:49 +0100

    missed some lines

--- cypress/support/config/services.js
+++ cypress/support/config/services.js
@@ -1,8 +1,10 @@
 import allServices from './settings';
 
 // Allow runs to be limited to a single service via the CYPRESS_ONLY_SERVICE env var
-const runOnlyService = Cypress.env('ONLY_SERVICE');
+const runOnlyService =
+  typeof Cypress !== 'undefined' ? Cypress.env('ONLY_SERVICE') : null;
 
+let servicesToExport;
 if (runOnlyService && Object.keys(allServices()).includes(runOnlyService)) {
   servicesToExport = {
     [runOnlyService]: allServices()[runOnlyService],
