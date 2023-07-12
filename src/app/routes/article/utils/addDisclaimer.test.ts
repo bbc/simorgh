480560c41ae5a53add78d6b25ecd1a14708952e1
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-07-12 13:01:12 +0100

    WSTEAM1-476: Tidy unit tests

--- src/app/routes/article/utils/addDisclaimer.test.ts
+++ src/app/routes/article/utils/addDisclaimer.test.ts
@@ -1,14 +1,5 @@
 import russianArticle from '#data/russian/articles/cjejv43ylr0o.json';
 import addDisclaimer from './addDisclaimer';
-import augmentWithDisclaimer from '../../cpsAsset/getInitialData/augmentWithDisclaimer';
-
-const fetchDataSpy = jest
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  .spyOn(augmentWithDisclaimer, 'default')
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  .mockImplementation(() => Promise.resolve(123));
 
 const disclaimerBlock = {
   type: 'disclaimer',
@@ -51,16 +42,6 @@ describe('addDisclaimer', () => {
     );
   });
   it('should not add disclaimer to SFV pages where toggles are disabled', async () => {
-    const actual = await addDisclaimer(
-      russianArticle.data.article,
-      buildTogglesFixture(false),
-      true,
-    );
-    expect(actual.content.model.blocks).toEqual(
-      russianArticle.data.article.content.model.blocks,
-    );
-  });
-  it('should return page data unchanged if there is an error with the function', async () => {
     const actual = await addDisclaimer(
       russianArticle.data.article,
       buildTogglesFixture(false),
