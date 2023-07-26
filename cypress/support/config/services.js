5a41e7f570ed101074af2f05283aba12c5083ca3
bennea27 <andrew.bennett07@bbc.co.uk>
2023-07-26 08:09:50 +0100

    Revert "missed some lines"
    This reverts commit 261c369c635cf66f60e66a243d81b352b84ca51e.

--- cypress/support/config/services.js
+++ cypress/support/config/services.js
@@ -1,10 +1,8 @@
 const allServices = require('./settings');
 
 // Allow runs to be limited to a single service via the CYPRESS_ONLY_SERVICE env var
-const runOnlyService =
-  typeof Cypress !== 'undefined' ? Cypress.env('ONLY_SERVICE') : null;
+const runOnlyService = Cypress.env('ONLY_SERVICE');
 
-let servicesToExport;
 if (runOnlyService && Object.keys(allServices()).includes(runOnlyService)) {
   module.exports = {
     [runOnlyService]: allServices()[runOnlyService],
