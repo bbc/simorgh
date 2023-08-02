b1f6a6150f24e2b8947d3360898ee3873b450492
Karina Thomas <karina.thomas@bbc.co.uk>
2023-08-02 17:47:05 +0100

    Add comment

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -21,7 +21,8 @@ Cypress.on(`window:before:load`, win => {
 
   if (Cypress.env('SKIP_EU') === 'true') {
     cy.stub(win.navigator.geolocation, 'getCurrentPosition').callsFake(fn => {
-      fn({ coords: { longitude: -0.001545, latitude: 51.477928 } });
+      // These are the co-ordinates of Greenwich, London
+      fn({ coords: { longitude: 0, latitude: 51.477928 } });
     });
   }
 });
