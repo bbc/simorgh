e662d54f75bed6eedef2c42aaf6e55b9dcbaa8b6
Shayne Marc Enzo Ahchoon <32307964+shayneahchoon@users.noreply.github.com>
2023-08-21 12:45:41 +0100

    WSTEAM1-309: Add Riddle embeds for Optimo Pages (#11003)
    * WSTEAM1-309: Add canonical includes to Optimo pages
    
    * WSTEAM1-309: Add test data
    
    * WSTEAM1-309: Refactor
    
    * WSTEAM1-309: Remove console log
    
    * WSTEAM1-309: Update snapshots
    
    * WSTEAM1-309: Add amp message
    
    * WSTEAM1-309: Alter test
    
    * WSTEAM1-309: Add OEmbedError
    
    * WSTEAM1-309: Refactor
    
    * WSTEAM1-309: Update test
    
    * WSTEAM1-309: Add Error message
    
    * WSTEAM1-309: Add storybook
    
    * WSTEAM1-309: Add OEmbed to OMP pages
    
    * WSTEAM1-309: Refactor
    
    * WSTEAM1-309: Remove snapshot
    
    * WSTEAM1-309: Update snapshots:
    
    * WSTEAM1-309: Update snapshots
    
    ---------
    
    Co-authored-by: Toby Cox <eagerterrier@gmail.com>
    Co-authored-by: Aaron Moore <aaron.moore@bbc.co.uk>

---
+++ src/app/components/Embeds/EmbedError/index.tsx
@@ -0,0 +1,29 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+import styles from './index.styles';
+import InlineLink from '../../InlineLink';
+
+type Props = {
+  message: string;
+  link: {
+    text: string;
+    href: string;
+  };
+};
+
+const EmbedError = ({ message, link }: Props) => {
+  return (
+    <div css={styles.embedDiv}>
+      <div>
+        <strong css={styles.errorMessage}>{message}</strong>
+      </div>
+      <div css={styles.errorLinkWrapper}>
+        {link && link.text && link.href && (
+          <InlineLink to={link.href} text={link.text} css={styles.inlineLink} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default EmbedError;
