894ed7f04ea54e2e79d5dad2996c0a4edea63dd5
louisearchibald <louise.archibald@bbc.co.uk>
2023-08-28 16:41:59 +0100

    adds view and click tracking tests

--- src/app/legacy/components/RadioSchedule/index.test.jsx
+++ src/app/legacy/components/RadioSchedule/index.test.jsx
@@ -1,6 +1,9 @@
 import { render } from '../../../components/react-testing-library-with-providers';
 import arabic from '../../../components/ThemeProvider/fontScripts/arabic';
+import latin from '../../../components/ThemeProvider/fontScripts/latin';
 import { renderRadioSchedule } from './testHelpers/helper';
+import * as viewTracking from '../../../hooks/useViewTracker';
+import * as clickTracking from '../../../hooks/useClickTrackerHandler';
 
 describe('RadioSchedule', () => {
   it('should render ltr radio schedules correctly', () => {
@@ -31,5 +34,46 @@ describe('RadioSchedule', () => {
     expect(
       container.getElementsByTagName('aside')[0].getAttribute('to'),
     ).toEqual('/news/articles/cn7k01xp8kxo');
+  });
+
+  describe('Event Tracking', () => {
+    const blockLevelEventTrackingData = {
+      componentName: 'radio-schedule',
+    };
+
+    it('should call the view tracking hook with the correct params', () => {
+      const viewTrackerSpy = jest.spyOn(viewTracking, 'default');
+      render(
+        renderRadioSchedule({
+          service: 'hausa',
+          script: latin,
+          dir: 'ltr',
+          locale: 'ha',
+          selectedService: 'hausa',
+        }),
+      );
+
+      expect(viewTrackerSpy).toHaveBeenCalledWith(blockLevelEventTrackingData);
+    });
+
+    it('should call the click tracking hook with the correct params', () => {
+      const clickTrackerSpy = jest.spyOn(clickTracking, 'default');
+      render(
+        renderRadioSchedule({
+          service: 'hausa',
+          script: latin,
+          dir: 'ltr',
+          locale: 'ha',
+          selectedService: 'hausa',
+        }),
+      );
+
+      expect(clickTrackerSpy).toHaveBeenNthCalledWith(1, {
+        componentName: 'radio-schedule-live',
+      });
+      expect(clickTrackerSpy).toHaveBeenNthCalledWith(2, {
+        componentName: 'radio-schedule-onDemand',
+      });
+    });
   });
 });
