c3832de4774083ddaa2140d11044168171723bbf
eagerterrier <eagerterrier@gmail.com>
2023-08-29 17:34:44 +0100

    FIX: remove brotli

--- src/server/local/index.js
+++ src/server/local/index.js
@@ -1,5 +1,4 @@
 import path from 'path';
-import expressStaticGzip from 'express-static-gzip';
 
 import {
   articleDataPath,
@@ -39,8 +38,6 @@ const sendDataFile = (res, dataFilePath, next) => {
   });
 };
 
-const PUBLIC_DIRECTORY = 'build/public';
-
 const removeTrailingSlash = url => {
   const hasTrailingSlash = url.length > 1 && url.endsWith('/');
   const redirectUrl = hasTrailingSlash ? url.slice(0, -1) : url;
@@ -54,13 +51,6 @@ export default server => {
       const [shouldRedirect, redirectUrl] = removeTrailingSlash(req.url);
       return shouldRedirect ? res.redirect(301, redirectUrl) : next();
     })
-    .use(
-      expressStaticGzip(PUBLIC_DIRECTORY, {
-        enableBrotli: true,
-        orderPreference: ['br'],
-        redirect: false,
-      }),
-    )
     .get(articleDataPath, async ({ params }, res, next) => {
       const { service, id, variant } = params;
 
