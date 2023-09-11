a75b7d0cd041bd8ac94b5814a3d64b3ef50dceb5
Aaron Moore <aaron.moore@bbc.co.uk>
2023-09-11 16:26:42 +0100

    Update index.tsx

--- src/app/components/ImageWithCaption/index.tsx
+++ src/app/components/ImageWithCaption/index.tsx
@@ -105,7 +105,7 @@ const ArticleFigure = ({
         fallbackSrcSet={fallbackSrcset || undefined}
         mediaType={primaryMimeType || undefined}
         fallbackMediaType={fallbackMimeType || undefined}
-        sizes={sizes}
+        sizes={!isAmp ? sizes : undefined}
         isAmp={isAmp}
       >
         {renderCopyright(copyright || '')}
