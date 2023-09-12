936ebaee453675671a809b50640bdd4a2c52a211
Karina Thomas <58214768+karinathomasbbc@users.noreply.github.com>
2023-09-12 19:17:15 +0100

    Add a recent STY mundo page which has recommendations

--- cypress/support/config/settings.js
+++ cypress/support/config/settings.js
@@ -3635,7 +3635,9 @@ module.exports = () => ({
       storyPage: {
         environments: {
           live: {
-            paths: ['/mundo/noticias-66171332'],
+            paths: ['/mundo/noticias-54274735', 
+                    '/mundo/noticias-66171332',
+            ],
             enabled: true,
           },
           test: {
