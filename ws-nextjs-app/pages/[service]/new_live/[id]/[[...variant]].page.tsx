4ea9e9014140dae6e58a51042b52d28196f1620e
Aaron Moore <aaron.moore@bbc.co.uk>
2023-09-12 12:56:17 +0100

    Update Next.JS to 13.x (#10813)
    * Update Next.JS to 13.x
    
    * Update next.config.js
    
    * Update to NextJS 13.4.1 - Include new Undici agent resolver
    
    * Add 'react' type pathing to tsconfig
    
    * convert jest.config to .ts file - add necessary devDeps
    
    * why do we need react type defs?
    
    * Force resolution of `types/react` as the latest version doesn't work in latest Next.JS
    
    * Update undici to memoize and add test
    
    * Moving resolution to Express package-json
    
    * Remove transformIgnorePatterns in favour of default behaviour
    
    * Test removing resolution
    
    * Update types/react-dom
    
    * Yarn cache updates
    
    * Add undici
    
    * Update to Next 13.4.9
    
    * Move undici to NextJS app
    
    * NextJS update
    
    * NextJS dep updates
    
    * Update Next to `13.4.12`
    
    * Use old getAgent until after Node 18 upgrade
    
    * Maybe it does need to be used...
    
    * Yarn cache
    
    * Yarn checksum fix
    
    * Pin `@types/react`
    
    * Update to `13.4.18`
    
    * Update next.config.js
    
    * cache
    
    * Try removing textencoder
    
    * Update next.config.js
    
    * Update Next + Cypress to 13.x
    
    * Remove `no-experimental-fetch` flag
    
    - They have no effect in Next.js 13.x
    
    * Cache
    
    * Use env variables for cert locations for Lambda
    
    * Remove package resolutions
    
    * Pin version of `glob-parent`

--- ws-nextjs-app/pages/[service]/new_live/[id]/[[...variant]].page.tsx
+++ ws-nextjs-app/pages/[service]/new_live/[id]/[[...variant]].page.tsx
@@ -2,7 +2,6 @@ import { GetServerSideProps } from 'next';
 import { ParsedUrlQuery } from 'querystring';
 import omit from 'ramda/src/omit';
 import constructPageFetchUrl from '#app/routes/utils/constructPageFetchUrl';
-import getAgent from '#server/utilities/getAgent';
 import getToggles from '#app/lib/utilities/getToggles/withCache';
 import { LIVE_PAGE } from '#app/routes/utils/pageTypes';
 import nodeLogger from '#lib/logger.node';
@@ -17,6 +16,8 @@ import { FetchError } from '#models/types/fetch';
 import getEnvironment from '#app/routes/utils/getEnvironment';
 import fetchPageData from '#app/routes/utils/fetchPageData';
 import certsRequired from '#app/routes/utils/certsRequired';
+import getAgent from '../../../../utilities/undiciAgent';
+
 import LivePageLayout from './LivePageLayout';
 
 interface PageDataParams extends ParsedUrlQuery {
