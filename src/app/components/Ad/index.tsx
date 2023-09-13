ea9d7bae330e973f5cf9915481983437b8a57fa7
Karina Thomas <karina.thomas@bbc.co.uk>
2023-09-13 11:19:07 +0100

    Demonstrate ads on homepage on storybook

--- src/app/components/Ad/index.tsx
+++ src/app/components/Ad/index.tsx
@@ -1,29 +1,22 @@
-import React, { useContext } from 'react';
-import useToggle from '#hooks/useToggle';
-import { RequestContext } from '#contexts/RequestContext';
+import React from 'react';
 import AmpAd from './Amp';
 import CanonicalAd from './Canonical';
 import { AdProps } from './types';
 import AdBootstrapJs from './Canonical/AdBootstrapJs';
 
 const AdContainer = ({ slotType, className, adcampaign }: AdProps) => {
-  const { isAmp, showAdsBasedOnLocation } = useContext(RequestContext);
-  const { enabled: adsEnabled } = useToggle('ads');
+  const isAmp = false;
 
   // Ads component will only be displayed if ads toggle is true and if showAdsBasedOnLocation is true
-  if ([adsEnabled, showAdsBasedOnLocation].every(Boolean)) {
-    const Ad = isAmp ? AmpAd : CanonicalAd;
+  const Ad = isAmp ? AmpAd : CanonicalAd;
 
-    return (
-      <>
-        {/* dotcom and dotcomConfig need to be setup before the main dotcom javascript file is loaded */}
-        {!isAmp && <AdBootstrapJs adcampaign={adcampaign} />}
-        <Ad slotType={slotType} className={className} />
-      </>
-    );
-  }
-
-  return null;
+  return (
+    <>
+      {/* dotcom and dotcomConfig need to be setup before the main dotcom javascript file is loaded */}
+      {!isAmp && <AdBootstrapJs adcampaign={adcampaign} />}
+      <Ad slotType={slotType} className={className} />
+    </>
+  );
 };
 
 export default AdContainer;
--- src/app/pages/HomePage/index.stories.tsx
+++ src/app/pages/HomePage/index.stories.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { HOME_PAGE } from '#app/routes/utils/pageTypes';
 import { data as kyrgyzHomePageData } from '#data/kyrgyz/homePage/index.json';
+import { MemoryRouter } from 'react-router';
 import { ServiceContextProvider } from '../../contexts/ServiceContext';
 import { withServicesKnob } from '../../legacy/psammead/psammead-storybook-helpers/src';
 import ThemeProvider from '../../components/ThemeProvider';
@@ -13,15 +14,17 @@ const Component = ({ service, variant }: StoryProps) => {
   return (
     <ThemeProvider service={service} variant={variant}>
       <ServiceContextProvider service={service} variant={variant}>
-        <HomePage
-          service={service}
-          variant={variant}
-          pageType={HOME_PAGE}
-          status={200}
-          isAmp={false}
-          pathname="/kyrgyz"
-          pageData={kyrgyzHomePageData}
-        />
+        <MemoryRouter>
+          <HomePage
+            service={service}
+            variant={variant}
+            pageType={HOME_PAGE}
+            status={200}
+            isAmp={false}
+            pathname="/kyrgyz"
+            pageData={kyrgyzHomePageData}
+          />
+        </MemoryRouter>
       </ServiceContextProvider>
     </ThemeProvider>
   );
