4ae0e9df94e65ff05e29788d66b1ba3985f845a6
Karina Thomas <karina.thomas@bbc.co.uk>
2023-09-13 17:17:21 +0100

    More ads testing

--- src/integration/common/ads.canonical.js
+++ src/integration/common/ads.canonical.js
@@ -1,14 +1,21 @@
 export default () => {
   describe('Ads', () => {
-    const ads = document.querySelectorAll('[id^=dotcom-slot]');
+    const ads = document.querySelectorAll('[id^=dotcom]');
 
     ads.forEach(ad => {
       it('should be displayed', () => {
         expect(ad).toBeInTheDocument();
       });
       it('should have the following properties', () => {
-        expect(ad.innerHTML).toMatchSnapshot();
+        expect(ad).toMatchSnapshot();
       });
+    });
+
+    it('scripts should be loaded', () => {
+      const adScript = document.querySelector(
+        "script[src*='dotcom-bootstrap.js']",
+      );
+      expect(adScript).toBeInTheDocument();
     });
   });
 };
