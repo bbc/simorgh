0ddd5fa31328c3c352e3ebc87028294863e655fe
SHAYNE MARC ENZO AHCHOON <shayne.ahchoon@bbc.co.uk>
2023-10-02 15:29:08 +0100

    WSTEAM1-445: refactor test

--- src/app/hooks/useLocation/index.test.tsx
+++ src/app/hooks/useLocation/index.test.tsx
@@ -3,22 +3,22 @@ import useLocation from '.';
 
 describe('useLocation', () => {
   it('should set location to the current window location', () => {
-    renderHook(() => useLocation());
+    const { result } = renderHook(() => useLocation());
 
     const articleURL = 'http://localhost/articles/cx000';
     window.history.pushState({}, '', new URL(articleURL));
 
-    expect(window.location.href).toBe(articleURL);
+    expect(result.current.href).toBe(articleURL);
   });
   it('should update the location when anchor hashes change', () => {
-    renderHook(() => useLocation());
+    const { result } = renderHook(() => useLocation());
 
     const articleURL = 'http://localhost/articles/cx000';
     window.history.pushState({}, '', new URL(articleURL));
     window.history.pushState({}, '', new URL(`${articleURL}#content`));
 
-    expect(window.location.href).toBe(`${articleURL}#content`);
-    expect(window.location.hash).toBe('#content');
+    expect(result.current.href).toBe(`${articleURL}#content`);
+    expect(result.current.hash).toBe('#content');
   });
   it('should remove the event listeners when the component unmounts', () => {
     const removeEventListenerSpy = jest.spyOn(window, 'removeEventListener');
