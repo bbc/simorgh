9c623491d7b7b2f76c8c5afe065a1b4b766be53f
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-10-06 12:20:07 +0100

    WSTEAM1-455: Add Key Point styles

--- ws-nextjs-app/pages/[service]/new_live/[id]/KeyPoints/index.tsx
+++ ws-nextjs-app/pages/[service]/new_live/[id]/KeyPoints/index.tsx
@@ -1,34 +1,63 @@
 /** @jsx jsx */
 
-import { jsx } from '@emotion/react';
+import { jsx, useTheme } from '@emotion/react';
 import Heading from '#app/components/Heading';
 import LegacyText from '#app/legacy/containers/Text';
 import LegacyParagraph from '#app/legacy/containers/Paragraph';
+import BulletedList from '#app/legacy/containers/BulletedList';
 import Blocks from '#app/legacy/containers/Blocks';
+import styles from './styles';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const KeyPoints = ({ keyPointBlocks }: any) => {
+  const {
+    palette: { GREY_10 },
+  } = useTheme();
+
   if (!keyPointBlocks) return null;
   const listItems = keyPointBlocks[0].model.blocks[0].model.blocks;
   const hasSingleKeyPoint = listItems.length === 1;
-  const componentsToRender = { text: LegacyText };
   const singleKeyPointComponentsToRender = { paragraph: LegacyParagraph };
 
+  // Requires translations
+  const sectionTitle = 'Summary';
+
+  const componentsToRender = () => ({
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    text: (props: any) => (
+      <LegacyText
+        {...props}
+        componentsToRender={{
+          unorderedList: innerProps => (
+            <BulletedList
+              {...innerProps}
+              bulletPointShape="square"
+              bulletPointColour={GREY_10}
+            />
+          ),
+        }}
+      />
+    ),
+  });
+
   return (
-    // Requires translations
-    <section role="region" aria-label="Summary">
-      <Heading level={2}>Summary</Heading>
-      {hasSingleKeyPoint ? (
-        <Blocks
-          blocks={listItems[0].model.blocks}
-          componentsToRender={singleKeyPointComponentsToRender}
-        />
-      ) : (
-        <Blocks
-          blocks={keyPointBlocks}
-          componentsToRender={componentsToRender}
-        />
-      )}
+    <section role="region" aria-label={sectionTitle}>
+      <Heading level={2} css={styles.headingStyles}>
+        {sectionTitle}
+      </Heading>
+      <div css={styles.bodyStyles}>
+        {hasSingleKeyPoint ? (
+          <Blocks
+            blocks={listItems[0].model.blocks}
+            componentsToRender={singleKeyPointComponentsToRender}
+          />
+        ) : (
+          <Blocks
+            blocks={keyPointBlocks}
+            componentsToRender={componentsToRender()}
+          />
+        )}
+      </div>
     </section>
   );
 };
