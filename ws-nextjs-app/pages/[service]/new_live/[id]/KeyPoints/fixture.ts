1d2bfdafeb2a71bbff76c460ae3ba01e6fd8ed21
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-10-09 15:43:34 +0100

    WSTEAM1-455: Add Ids to fixture data. Update snapshot.

--- ws-nextjs-app/pages/[service]/new_live/[id]/KeyPoints/fixture.ts
+++ ws-nextjs-app/pages/[service]/new_live/[id]/KeyPoints/fixture.ts
@@ -1,6 +1,22 @@
 export const emptyKeyPoints = {
   model: {
-    blocks: [],
+    blocks: [
+      {
+        id: 'b329001a',
+        type: 'text',
+        model: {
+          blocks: [
+            {
+              id: '5ebffdca',
+              type: 'unorderedList',
+              model: {
+                blocks: [],
+              },
+            },
+          ],
+        },
+      },
+    ],
   },
 };
 
@@ -8,23 +24,28 @@ export const singleKeyPoint = {
   model: {
     blocks: [
       {
+        id: 'b329001a',
         type: 'text',
         model: {
           blocks: [
             {
+              id: '5ebffdca',
               type: 'unorderedList',
               model: {
                 blocks: [
                   {
+                    id: '3c096af1',
                     type: 'listItem',
                     model: {
                       blocks: [
                         {
+                          id: 'a4b2ec0b',
                           type: 'paragraph',
                           model: {
                             text: 'I am the summary box single key point',
                             blocks: [
                               {
+                                id: '5f43c969',
                                 type: 'fragment',
                                 model: {
                                   text: 'I am the summary box single key point',
@@ -51,23 +72,28 @@ export const multipleKeyPoints = {
   model: {
     blocks: [
       {
+        id: 'b329001a',
         type: 'text',
         model: {
           blocks: [
             {
+              id: '5ebffdca',
               type: 'unorderedList',
               model: {
                 blocks: [
                   {
+                    id: '3c096af1',
                     type: 'listItem',
                     model: {
                       blocks: [
                         {
+                          id: 'a4b2ec0b',
                           type: 'paragraph',
                           model: {
                             text: 'I am the summary box',
                             blocks: [
                               {
+                                id: '5f43c969',
                                 type: 'fragment',
                                 model: {
                                   text: 'I am the summary box',
@@ -81,15 +107,18 @@ export const multipleKeyPoints = {
                     },
                   },
                   {
+                    id: '0822012f',
                     type: 'listItem',
                     model: {
                       blocks: [
                         {
+                          id: '7b1cd1c0',
                           type: 'paragraph',
                           model: {
                             text: 'I need to include bulletpoints',
                             blocks: [
                               {
+                                id: '60633e01',
                                 type: 'fragment',
                                 model: {
                                   text: 'I need to include bulletpoints',
@@ -103,15 +132,18 @@ export const multipleKeyPoints = {
                     },
                   },
                   {
+                    id: '5ffdc35a',
                     type: 'listItem',
                     model: {
                       blocks: [
                         {
+                          id: '110c3e12',
                           type: 'paragraph',
                           model: {
                             text: 'I want to link to somebody',
                             blocks: [
                               {
+                                id: '1371aa79',
                                 type: 'fragment',
                                 model: {
                                   text: 'I want to ',
@@ -119,11 +151,13 @@ export const multipleKeyPoints = {
                                 },
                               },
                               {
+                                id: 'b0c705da',
                                 type: 'urlLink',
                                 model: {
                                   text: 'link to somebody',
                                   blocks: [
                                     {
+                                      id: '2aae7757',
                                       type: 'fragment',
                                       model: {
                                         text: 'link to somebody',
