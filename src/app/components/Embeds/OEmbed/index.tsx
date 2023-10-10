5a940c96a3d2b83dacaf8c7d7264fed52e0a255e
Harvey Peachey <harvey.peachey@bbc.co.uk>
2023-10-10 12:23:09 +0100

    adds iframe listener code

--- src/app/components/Embeds/OEmbed/index.tsx
+++ src/app/components/Embeds/OEmbed/index.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable camelcase */
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import pathOr from 'ramda/src/pathOr';
 import { ServiceContext } from '../../../contexts/ServiceContext';
 import { RequestContext } from '../../../contexts/RequestContext';
-import EmbedHtml from '../EmbedHtml';
+// import EmbedHtml from '../EmbedHtml';
 import EmbedError from '../EmbedError';
 
 export type OEmbedProps = {
@@ -25,6 +25,27 @@ const OEmbedLoader = ({ oembed }: OEmbedProps) => {
   const { translations } = useContext(ServiceContext);
   const { isAmp, canonicalLink } = useContext(RequestContext);
   const { html } = oembed;
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  const iframeSrc = html.match(/(?<=src=").*?(?=[\?"])/)?.[0];
+  // const iframeSrc = 'https://flo.uri.sh/visualisation/14983252/embed?auto=1';
+
+  useEffect(() => {
+    const onMessage = (event: MessageEvent) => {
+      const message = typeof event.data === 'string' && JSON.parse(event.data);
+      if (
+        iframeRef.current &&
+        message?.sender === 'Flourish' &&
+        message?.context === 'iframe.resize'
+      ) {
+        iframeRef.current.style.height = `${message.height}px`;
+      }
+    };
+
+    window.addEventListener('message', onMessage);
+
+    return () => window.removeEventListener('message', onMessage);
+  });
 
   if (isAmp) {
     const errorMessage = pathOr(
@@ -50,7 +71,18 @@ const OEmbedLoader = ({ oembed }: OEmbedProps) => {
     );
   }
 
-  return <EmbedHtml embeddableContent={html} />;
+  return (
+    <iframe
+      src={`${iframeSrc}?auto=1`}
+      ref={iframeRef}
+      frameBorder="0"
+      scrolling="no"
+      height="575"
+      width="700"
+      style={{ width: '100%' }}
+      title="Interactive or visual content"
+    />
+  );
 };
 
 export default OEmbedLoader;
