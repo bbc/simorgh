3499d4e66b129b62dd3b527d71368af4406f4099
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-10-10 10:47:38 +0100

    WSTEAM1-455: Add metadata and designs to story files

--- ws-nextjs-app/pages/[service]/new_live/[id]/KeyPoints/index.stories.tsx
+++ ws-nextjs-app/pages/[service]/new_live/[id]/KeyPoints/index.stories.tsx
@@ -8,6 +8,7 @@ import KeyPoints from '.';
 import { StoryProps } from '../../../../../../src/app/models/types/storybook';
 import { SummaryListWrapper } from './types';
 import { singleKeyPoint, multipleKeyPoints } from './fixture';
+import metadata from './metadata.json';
 
 const singleKeyPointBlocks = singleKeyPoint.model.blocks;
 const multipleKeyPointsBlocks = multipleKeyPoints.model.blocks;
@@ -29,6 +30,36 @@ const Component = ({ service, variant, keyPointBlocks }: ComponentProps) => {
 export default {
   title: 'New Components/Live Page Key Points',
   Component,
+  parameters: {
+    metadata,
+    design: [
+      {
+        name: 'Group 0',
+        type: 'figma',
+        url: 'https://www.figma.com/file/mxshZlXkCPPIh0oQJvRLpZ/Live-summary-box---handover?type=design&node-id=447-66995&mode=design&t=gSZo3mYpGwIJaUrz-0',
+      },
+      {
+        name: 'Group 1',
+        type: 'figma',
+        url: 'https://www.figma.com/file/mxshZlXkCPPIh0oQJvRLpZ/Live-summary-box---handover?type=design&node-id=456-65248&mode=design&t=gSZo3mYpGwIJaUrz-0',
+      },
+      {
+        name: 'Group 2',
+        type: 'figma',
+        url: 'https://www.figma.com/file/mxshZlXkCPPIh0oQJvRLpZ/Live-summary-box---handover?type=design&node-id=456-65272&mode=design&t=gSZo3mYpGwIJaUrz-0',
+      },
+      {
+        name: 'Group 3',
+        type: 'figma',
+        url: 'https://www.figma.com/file/mxshZlXkCPPIh0oQJvRLpZ/Live-summary-box---handover?type=design&node-id=456-65320&mode=design&t=gSZo3mYpGwIJaUrz-0',
+      },
+      {
+        name: 'Group 4',
+        type: 'figma',
+        url: 'https://www.figma.com/file/mxshZlXkCPPIh0oQJvRLpZ/Live-summary-box---handover?type=design&node-id=456-65364&mode=design&t=gSZo3mYpGwIJaUrz-0',
+      },
+    ],
+  },
   decorators: [withKnobs, withServicesKnob()],
 };
 
