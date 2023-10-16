896f63d09223ed864da67c521c04b89a83929f81
Harvey Peachey <harvey.peachey@bbc.co.uk>
2023-10-16 14:47:02 +0100

    adds inline script to bypass react lifecycle

--- src/app/components/Embeds/OEmbed/Flourish/index.tsx
+++ src/app/components/Embeds/OEmbed/Flourish/index.tsx
@@ -1,44 +1,33 @@
 /** @jsx jsx */
 /* eslint-disable camelcase */
 import { jsx } from '@emotion/react';
-import { useEffect, useRef } from 'react';
+import { Helmet } from 'react-helmet';
+import React from 'react';
 import { OEmbedProps } from '../types';
 import styles from './index.styles';
 
 const Flourish = ({ oembed }: OEmbedProps) => {
   const { html, width, height } = oembed;
-  const iframeRef = useRef<HTMLIFrameElement>(null);
-
-  useEffect(() => {
-    const onMessage = (event: MessageEvent) => {
-      const message = typeof event.data === 'string' && JSON.parse(event.data);
-      if (
-        iframeRef.current &&
-        message?.sender === 'Flourish' &&
-        message?.context === 'iframe.resize'
-      ) {
-        iframeRef.current.style.height = `${message.height}px`;
-      }
-    };
-
-    window.addEventListener('message', onMessage);
-
-    return () => window.removeEventListener('message', onMessage);
-  });
-
   const iframeSrc = html.match(/(?<=src=").*?(?=[?"])/)?.[0];
+  const script =
+    "window.addEventListener('message', function (event) {  var message = typeof event.data === 'string' && JSON.parse(event.data);  if ((message === null || message === void 0 ? void 0 : message.sender) === 'Flourish' && (message === null || message === void 0 ? void 0 : message.context) === 'iframe.resize') {    document.getElementById(`test-flourish`).style.height = message.height + 'px';  }});";
 
   return (
-    <iframe
-      css={styles.iframe}
-      src={`${iframeSrc}?auto=1`}
-      ref={iframeRef}
-      frameBorder="0"
-      scrolling="no"
-      height={height}
-      width={width}
-      title="Interactive or visual content"
-    />
+    <React.Fragment>
+      <Helmet>
+        <script>{script}</script>
+      </Helmet>
+      <iframe
+        css={styles.iframe}
+        src={`${iframeSrc}?auto=1`}
+        id="test-flourish"
+        frameBorder="0"
+        scrolling="no"
+        height={height}
+        width={width}
+        title="Interactive or visual content"
+      />
+    </React.Fragment>
   );
 };
 
