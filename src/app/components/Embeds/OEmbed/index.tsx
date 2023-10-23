d829df9e0ff2761f4ea71c2f8520d283ab845de6
Isabella Mitchell <i.mitchell.0707@gmail.com>
2023-10-23 12:40:05 +0100

    WSTEAM1-623: Render VJ embeds on .amp

--- src/app/components/Embeds/OEmbed/index.tsx
+++ src/app/components/Embeds/OEmbed/index.tsx
@@ -1,10 +1,8 @@
 /* eslint-disable camelcase */
 import React, { useContext } from 'react';
-import pathOr from 'ramda/src/pathOr';
-import { ServiceContext } from '../../../contexts/ServiceContext';
 import { RequestContext } from '../../../contexts/RequestContext';
 import EmbedHtml from '../EmbedHtml';
-import EmbedError from '../EmbedError';
+import EmbedAmp, { ampParams } from '../EmbedAmp';
 
 type OEmbedData = Partial<{
   version: string;
@@ -16,6 +14,8 @@ type OEmbedData = Partial<{
   width: number;
   height: number;
   type: string;
+  parameters: ampParams;
+  oEmbedType: string;
 }>;
 
 export type OEmbedProps = {
@@ -24,32 +24,17 @@ export type OEmbedProps = {
 };
 
 const OEmbedLoader = ({ oembed }: OEmbedProps) => {
-  const { translations } = useContext(ServiceContext);
   const { isAmp, canonicalLink } = useContext(RequestContext);
-  const { html } = oembed;
+  const { html, oEmbedType, parameters, url } = oembed; // check
+  const isVDJEmbed = oEmbedType === 'vdj-embed';
 
   if (isAmp) {
-    const errorMessage = pathOr(
-      'Sorry, we can’t display this part of the story on this lightweight mobile page.',
-      ['include', 'errorMessage'],
-      translations,
-    );
-
-    const linkText = pathOr(
-      'View the full version of the page to see all the content.',
-      ['include', 'linkText'],
-      translations,
-    );
-
-    return (
-      <EmbedError
-        message={errorMessage}
-        link={{
-          text: linkText,
-          href: canonicalLink,
-        }}
-      />
-    );
+    <EmbedAmp
+      isVDJEmbed={isVDJEmbed}
+      canonicalLink={canonicalLink}
+      parameters={parameters}
+      url={url}
+    />;
   }
 
   if (html == null) {
