9606d88502570a2d1e9c9fbe2cc012db881e22cf
louisearchibald <louise.archibald@bbc.co.uk>
2023-10-24 17:33:39 +0100

    Revert "changes livelabel colour to TEAL"
    This reverts commit 2e440129257adb5075b4afe9084f1fa4603b84d9.

--- src/app/components/LiveLabel/index.styles.ts
+++ src/app/components/LiveLabel/index.styles.ts
@@ -3,7 +3,7 @@ import { css, Theme } from '@emotion/react';
 const styles = {
   liveLabel: ({ palette, spacings, fontVariants }: Theme) =>
     css({
-      color: palette.LE_TEAL,
+      color: palette.POSTBOX,
       display: 'inline-block',
       marginInlineEnd: `${spacings.FULL}rem`,
       ...fontVariants.sansBold,
