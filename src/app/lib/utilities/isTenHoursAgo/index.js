451e06e95782c1fe8100398687bd003aa4cc9755
chrish01 <holly.christopher@bbc.co.uk>
2023-11-06 16:22:49 +0000

    refactor of isTenHoursAgo function

--- src/app/lib/utilities/isTenHoursAgo/index.js
+++ src/app/lib/utilities/isTenHoursAgo/index.js
@@ -1,6 +1,6 @@
 const isTenHoursAgo = milliseconds => {
   const now = Date.now();
-  return now - milliseconds <= 10 * 60 * 60 * 1000;
+  return now - milliseconds < 11 * 60 * 60 * 1000;
 };
 
 export default isTenHoursAgo;
