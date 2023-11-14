278a3201901899cd98452e75110f5f82e6bd1ccf
Karina Thomas <karina.thomas@bbc.co.uk>
2023-11-14 12:29:40 +0000

    Log slow response time event when elapsed time is greater than 3 seconds

--- src/server/utilities/logResponseTime/index.test.js
+++ src/server/utilities/logResponseTime/index.test.js
@@ -13,17 +13,15 @@ const next = jest.fn();
 
 describe('logResponseTime', () => {
   afterEach(() => {
-    jest.resetAllMocks();
+    jest.clearAllMocks();
   });
 
-  beforeEach(() => {
+  it('should log response time in nanoseconds with path', () => {
     process.hrtime = jest
       .fn()
       .mockImplementationOnce(() => 'startTime')
       .mockImplementationOnce(() => [1, 12345]);
-  });
 
-  it('should log response time in nanoseconds with path', () => {
     logResponseTime(req, res, next);
 
     expect(loggerMock.debug).toBeCalledWith('server_response_time', {
@@ -34,5 +32,19 @@ describe('logResponseTime', () => {
     expect(process.hrtime).toHaveBeenCalledWith('startTime');
 
     expect(next).toHaveBeenCalled();
+  });
+
+  it('should log slow response time in nanoseconds with path', () => {
+    process.hrtime = jest
+      .fn()
+      .mockImplementationOnce(() => 'startTime')
+      .mockImplementationOnce(() => [3, 12345]);
+
+    logResponseTime(req, res, next);
+
+    expect(loggerMock.info).toBeCalledWith('slow_server_response_time', {
+      nanoseconds: 3000012345,
+      path: '/path',
+    });
   });
 });
