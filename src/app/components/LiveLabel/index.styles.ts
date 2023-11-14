8f5de2643dfb57e98a631a9062332597576a8086
louisearchibald <louise.archibald@bbc.co.uk>
2023-11-14 16:44:59 +0000

    adds placeholder circle for adding pulse to livelabel

--- src/app/components/LiveLabel/index.styles.ts
+++ src/app/components/LiveLabel/index.styles.ts
@@ -1,12 +1,21 @@
 import { css, Theme } from '@emotion/react';
 
 const styles = {
-  liveLabel: ({ palette, spacings, fontVariants }: Theme) =>
+  liveLabelText: ({ palette, spacings, fontVariants }: Theme) =>
     css({
       color: palette.LIVE_DARK,
       display: 'inline-block',
       marginInlineEnd: `${spacings.FULL}rem`,
       ...fontVariants.sansBold,
+    }),
+  liveLabelPulse: ({ palette, spacings }: Theme) =>
+    css({
+      height: '.9375rem',
+      width: '.9375rem',
+      backgroundColor: palette.LIVE_DARK,
+      borderRadius: '50%',
+      display: 'inline-block',
+      marginInlineEnd: `${spacings.HALF}rem`,
     }),
 };
 
--- src/app/components/LiveLabel/index.tsx
+++ src/app/components/LiveLabel/index.tsx
@@ -51,8 +51,9 @@ const LiveLabel = ({
     // The id below is a temporary fix for the a11y nested span's bug experienced in TalkBack, refer to the following issue: https://github.com/bbc/simorgh/issues/9652
     // eslint-disable-next-line jsx-a11y/aria-role
     <span id={id} role="text">
+      <span css={styles.liveLabelPulse} />
       <span
-        css={styles.liveLabel}
+        css={styles.liveLabelText}
         dir={dir}
         {...(ariaHidden && { 'aria-hidden': 'true' })}
       >
