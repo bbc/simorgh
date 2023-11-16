dc539596fdb1442bb8c03d9aad6852ced616aa94
louisearchibald <louise.archibald@bbc.co.uk>
2023-11-16 16:09:37 +0000

    css bits for live pulse

--- src/app/components/LiveLabel/index.styles.ts
+++ src/app/components/LiveLabel/index.styles.ts
@@ -8,14 +8,39 @@ const styles = {
       marginInlineEnd: `${spacings.FULL}rem`,
       ...fontVariants.sansBold,
     }),
-  liveLabelPulse: ({ palette, spacings }: Theme) =>
+  liveLabelCircle: ({ palette, spacings }: Theme) =>
     css({
-      height: '.9375rem',
-      width: '.9375rem',
-      backgroundColor: palette.LIVE_DARK,
+      color: palette.LIVE_DARK,
       borderRadius: '50%',
       display: 'inline-block',
-      marginInlineEnd: `${spacings.HALF}rem`,
+      width: '.9375rem',
+      height: '.9375rem',
+      background:
+        'radial-gradient(circle,transparent 53%,hsla(180, 100%, 20%,.5) 53.5%,#006666 54%)',
+      marginInlineEnd: `${spacings.FULL}rem`,
+      position: 'relative',
+    }),
+
+  livelabelPulse: () =>
+    css({
+      '&:before': {
+        content: `''`,
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        borderRadius: '50%',
+        background:
+          'radial-gradient(circle,#006666 37%,rgba(0,102,102,.5) 37.5%,transparent 38%)',
+      },
+
+      '&:after': {
+        content: `''`,
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        borderRadius: '100%',
+        // boxShadow: '0 0 .5rem rgba(0,0,0,.3)',
+      },
     }),
 };
 
