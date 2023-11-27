8371d337954456ce96a0c75300ba8526232f3c31
Karina Thomas <karina.thomas@bbc.co.uk>
2023-11-27 11:33:49 +0000

    Implement persian/afghanistan as a homepage

--- src/app/pages/HomePage/index.stories.tsx
+++ src/app/pages/HomePage/index.stories.tsx
@@ -3,13 +3,24 @@ import React from 'react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { HOME_PAGE } from '#app/routes/utils/pageTypes';
 import { data as kyrgyzHomePageData } from '#data/kyrgyz/homePage/index.json';
+import { data as persianAfghanistan } from '#data/persian/afghanistan/tipohome.json';
 import { ServiceContextProvider } from '../../contexts/ServiceContext';
 import { withServicesKnob } from '../../legacy/psammead/psammead-storybook-helpers/src';
 import ThemeProvider from '../../components/ThemeProvider';
 import { StoryProps } from '../../models/types/storybook';
 import HomePage from '.';
 
-const Component = ({ service, variant }: StoryProps) => {
+interface HomePageStoryProps extends StoryProps {
+  pageData: object;
+  pathname?: string;
+}
+
+const Component = ({
+  service,
+  variant,
+  pageData,
+  pathname,
+}: HomePageStoryProps) => {
   return (
     <ThemeProvider service={service} variant={variant}>
       <ServiceContextProvider service={service} variant={variant}>
@@ -19,8 +30,8 @@ const Component = ({ service, variant }: StoryProps) => {
           pageType={HOME_PAGE}
           status={200}
           isAmp={false}
-          pathname="/kyrgyz"
-          pageData={kyrgyzHomePageData}
+          pathname={`${service || pathname}`}
+          pageData={pageData}
         />
       </ServiceContextProvider>
     </ThemeProvider>
@@ -33,6 +44,15 @@ export default {
   decorators: [withKnobs, withServicesKnob()],
 };
 
-export const Kyrgyz = ({ variant }: StoryProps) => (
-  <Component service="kyrgyz" variant={variant} />
+export const Kyrgyz = ({ variant }: HomePageStoryProps) => (
+  <Component service="kyrgyz" variant={variant} pageData={kyrgyzHomePageData} />
+);
+
+export const PersianAfghanistan = ({ variant }: HomePageStoryProps) => (
+  <Component
+    service="persian"
+    variant={variant}
+    pathname="/persian/afghanistan"
+    pageData={persianAfghanistan}
+  />
 );
