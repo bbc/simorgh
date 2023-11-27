e736f9160dbdd710fe61193ac8e56e95b5610b37
Karina Thomas <karina.thomas@bbc.co.uk>
2023-11-27 13:25:40 +0000

    Remove most read tests from IDX page

--- src/integration/pages/idxPage/canonicalTests.js
+++ src/integration/pages/idxPage/canonicalTests.js
@@ -2,15 +2,12 @@ import runCrossPlatformTests from './crossPlatformTests';
 import {
   runCoreCanonicalTests,
   runCanonicalAnalyticsTests,
-  runMostReadTests,
 } from '../../common';
 
 export default service => {
   runCrossPlatformTests(service);
   runCoreCanonicalTests();
   runCanonicalAnalyticsTests();
-
-  runMostReadTests();
 
   describe('Radio Schedule', () => {
     const id = document.getElementById('Radio-Schedule');
