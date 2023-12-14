dba7026b4d2d0561d8ff51e337279c616f232047
louisearchibald <louise.archibald@bbc.co.uk>
2023-12-14 16:33:31 +0000

    wrapping pulse and live text in new span

--- src/app/components/LiveLabel/index.tsx
+++ src/app/components/LiveLabel/index.tsx
@@ -59,17 +59,18 @@ const LiveLabel = ({
     // The id below is a temporary fix for the a11y nested span's bug experienced in TalkBack, refer to the following issue: https://github.com/bbc/simorgh/issues/9652
     // eslint-disable-next-line jsx-a11y/aria-role
     <span id={id} role="text">
-      <span css={circle}>
-        <svg
-          fill="currentColor"
-          focusable="false"
-          aria-hidden="true"
-          viewBox="0 0 32 32"
-        >
-          <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z" />
-          <circle css={styles.pulseInnerCircle} cx="16" cy="16" r="8.5" />
-        </svg>
-
+      <span css={styles.liveLabelContainer}>
+        <span css={circle}>
+          <svg
+            fill="currentColor"
+            focusable="false"
+            aria-hidden="true"
+            viewBox="0 0 32 32"
+          >
+            <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z" />
+            <circle css={styles.pulseInnerCircle} cx="16" cy="16" r="8.5" />
+          </svg>
+        </span>
         <span
           css={styles.liveLabelText}
           dir={dir}
@@ -78,6 +79,7 @@ const LiveLabel = ({
           {`${liveLabel} `}
         </span>
       </span>
+
       {screenReaderText && (
         <VisuallyHiddenText lang={lang}>{screenReaderText}</VisuallyHiddenText>
       )}
