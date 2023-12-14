1d2bdf98249ed48d13647a0de314b9a1c03ae0b0
louisearchibald <louise.archibald@bbc.co.uk>
2023-12-14 18:06:38 +0000

    reinstating width and height on svg to avoid scaling up of pulse when css is disabled

--- src/app/components/LiveLabel/index.tsx
+++ src/app/components/LiveLabel/index.tsx
@@ -65,6 +65,8 @@ const LiveLabel = ({
             focusable="false"
             aria-hidden="true"
             viewBox="0 0 32 32"
+            width="1em"
+            height="1em"
           >
             <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z" />
             <circle css={styles.pulseInnerCircle} cx="16" cy="16" r="8.5" />
@@ -78,7 +80,6 @@ const LiveLabel = ({
           {`${liveLabel} `}
         </span>
       </span>
-
       {screenReaderText && (
         <VisuallyHiddenText lang={lang}>{screenReaderText}</VisuallyHiddenText>
       )}
