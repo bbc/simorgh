4c51ac6fa5252f872a7c41a7189e993373dc2889
Karina Thomas <karina.thomas@bbc.co.uk>
2023-12-15 13:02:44 +0000

    Refactor translations

---
+++ src/app/lib/config/services/arabic/translations.ts
@@ -0,0 +1,209 @@
+export default {
+  pagination: {
+    page: 'صفحة',
+    previousPage: 'السابق',
+    nextPage: 'التالي',
+    pageXOfY: 'صفحة {x} من {y}',
+  },
+  ads: {
+    advertisementLabel: 'إعلان',
+  },
+  recommendationTitle: 'قصص مقترحة',
+  splitRecommendationTitle: 'مزيد من القصص المقترحة',
+  seeAll: 'المزيد',
+  home: 'الرئيسية',
+  currentPage: 'الصفحة الحالية',
+  skipLinkText: 'إذهب الى المحتوى',
+  relatedContent: 'المزيد حول هذه القصة',
+  relatedTopics: 'مواضيع ذات صلة',
+  navMenuText: 'أقسام',
+  mediaAssetPage: {
+    mediaPlayer: 'مشغل وسائط',
+    audioPlayer: 'مشغل ملف صوتي',
+    videoPlayer: 'مشغل فيديو',
+  },
+  liveExperiencePage: {
+    liveLabel: 'مباشر',
+    liveCoverage: 'تغطية مباشرة',
+    breaking: 'عاجل',
+    postedAt: 'توقيت النشر',
+    summary: 'ملخص',
+  },
+  gist: 'ملخص',
+  error: {
+    404: {
+      statusCode: '404',
+      title: 'لم يتسن العثور على الصفحة',
+      message: 'للأسف، لم نتمكن من العثور على الصفحة التي تطلبها. الرجاء حاول:',
+      solutions: [
+        'التأكد من عنوان الصفحة (url)',
+        'نقر زر التجديد (refresh) في برنامج التصفح الذي تستخدمه',
+        'البحث عن هذه الصفحة باستخدام خانة البحث الخاصة ببي بي سي',
+      ],
+      callToActionFirst: 'كبديل، يرجى زيارة الصفحة الرئيسية لأخبار ',
+      callToActionLinkText: 'BBC News عربي',
+      callToActionLast: '',
+      callToActionLinkUrl: 'https://www.bbc.com/arabic',
+    },
+    500: {
+      statusCode: '500',
+      title: 'خلل داخلي في الخادم',
+      message: 'للأسف، لم نتمكن من العثور على الصفحة التي تطلبها. الرجاء حاول:',
+      solutions: [
+        'نقر زر التجديد (refresh) في برنامج التصفح الذي تستخدمه',
+        'تكرار الطلب بعد فترة',
+      ],
+      callToActionFirst: 'كبديل، يرجى زيارة الصفحة الرئيسية لأخبار بي بي سي ',
+      callToActionLinkText: 'Arabic',
+      callToActionLast: '',
+      callToActionLinkUrl: 'https://www.bbc.com/arabic',
+    },
+  },
+  consentBanner: {
+    privacy: {
+      title:
+        'لقد حدّثنا سياستنا المتعلقة بالخصوصية وبالشروط الخاصة بملفات الارتباط Cookies',
+      description: {
+        uk: {
+          first:
+            'لقد أدخلنا تغييرات مهمة على سياستنا المتعلقة بالخصوصية وعلى الشروط الخاصة بملفات الارتباط Cookies، ويهمنا أن تكونوا ملمين بما قد تعني هذه التغييرات بالنسبة لكم ولبياناتكم',
+          linkText: null,
+          last: null,
+          linkUrl: null,
+        },
+        international: {
+          first:
+            'لقد أدخلنا تغييرات مهمة على سياستنا المتعلقة بالخصوصية وعلى الشروط الخاصة بملفات الارتباط Cookies، ويهمنا أن تكونوا ملمين بما قد تعني هذه التغييرات بالنسبة لكم ولبياناتكم',
+          linkText: null,
+          last: null,
+          linkUrl: null,
+        },
+      },
+      accept: 'موافق',
+      reject: 'إطلع على التغييرات',
+      rejectUrl: 'https://www.bbc.com/usingthebbc/your-data-matters',
+    },
+    cookie: {
+      amp: {
+        accept: 'وافق على جمع المعلومات واستمر',
+        reject: 'رفض جمع المعلومات واستمر',
+        initial: {
+          title: 'أعلمونا بموافقتكم على جمع المعلومات من خلال AMP',
+          description: {
+            first: 'نستخدم نحن وشركاؤنا تقنيات مثل ',
+            linkText: 'ملفات الارتباط',
+            last: '، كما نقوم بجمع معلومات خاصة بالتصفح من أجل توفير أفضل خدمة رقمية ولجعل المحتوى والاعلانات، الموجهة إليك، شخصية. الرجاء إعلامنا إذا كنت موافقا على ذلك.',
+            linkUrl:
+              'https://www.bbc.com/usingthebbc/cookies/what-do-i-need-to-know-about-cookies/',
+          },
+          manage: 'ادارة خياراتي',
+        },
+        manage: {
+          title: 'ادارة خيارات الموافقة في صفحات AMP',
+          description: {
+            para1:
+              'تسري هذه الاعدادات على صفحات AMP فقط. قد يطلب منكم اعادة تحديد الاعدادات عند زيارتكم صفحات بي بي سي غير الخاضعة لـ AMP.',
+            para2:
+              'الصفحة خفيفة الوزن والمخصصة للأجهزة المحمولة التي زرتموها انتجت باستخدام تقنية غوغل للـ AMP.',
+            heading2: 'جمع المعلومات ضروري جدا ولابد منه',
+            para3:
+              'لأجل ضمان عمل صفحاتنا بشكل سلس ومقبول، نقوم بخزن بعض المعلومات المحدودة على أجهزتكم دون الحصول على موافقتكم.',
+            para4: {
+              text: 'إقرأوا المزيد عن المعلومات الضرورية والحيوية التي نقوم بخزنها على أجهزتكم من أجل ضمان عمل صفحاتنا بشكل جيد.',
+              url: 'https://www.bbc.com/usingthebbc/cookies/strictly-necessary-cookies/',
+            },
+            para5: 'نقوم بخزن أفضلياتكم التي صرحتم بها في أجهزتكم.',
+            heading3: 'شروط اختيارية لجمع المعلومات',
+            para6:
+              'في حال موافقتكم على جمع معلوماتكم من خلال صفحات AMP، فإنكم توافقون على إظهار إعلانات موجهة بشكل شخصي لها علاقة باهتماماتكم عندما تتصفحون هذه الصفحات خارج بريطانيا.',
+            para7: {
+              text: 'أقرأوا المزيد عن كيفية تخصيص الاعلانات من قبل بي بي سي وشركائها في مجال الإعلان.',
+              url: 'https://www.bbc.com/usingthebbc/cookies/how-does-the-bbc-use-cookies-for-advertising/',
+            },
+            para8:
+              'بإمكانكم اختيار تجنب استلام الإعلانات الشخصية عن طريق النقر على خيار "أرفض جمع المعلومات واستمر" أدناه.\nيرجى ملاحظة أنكم ستستمرون في مشاهدة الإعلانات، ولكنها لن تكون اعلانات موجهة لكم بشكل مخصص.',
+            para9:
+              'يمكنكم تغيير هذه الإعدادات في أي وقت عن طريق النقر على "خيارات الإعلانات/ لا تبيعوا معلوماتي" الموجود في أسفل الصفحة.',
+          },
+        },
+      },
+      canonical: {
+        title: 'أخبرنا عما إذا كنت توافق على تحميل الكوكيز',
+        description: {
+          uk: {
+            first: 'نستخدم ',
+            linkText: 'ملفات ارتباط',
+            last: ' لمنحك أفضل خدمة رقمية. الرجاء أحطنا علما إذا كنت توافق على تحميل كل هذه الملفات cookies .',
+            linkUrl:
+              'https://www.bbc.co.uk/usingthebbc/cookies/what-do-i-need-to-know-about-cookies/',
+          },
+          international: {
+            first: 'نستخدم ',
+            linkText: 'ملفات ارتباط',
+            last: ' لمنحك أفضل خدمة رقمية. الرجاء أحطنا علما إذا كنت توافق على تحميل كل هذه الملفات cookies .',
+            linkUrl:
+              'https://www.bbc.com/usingthebbc/cookies/what-do-i-need-to-know-about-cookies/',
+          },
+        },
+        accept: 'نعم، موافق',
+        reject: 'كلا، أعدني إلى الإعدادات',
+        rejectUrl:
+          'https://www.bbc.com/usingthebbc/cookies/how-can-i-change-my-bbc-cookie-settings/',
+      },
+    },
+  },
+  media: {
+    noJs: 'جهازك لا يدعم تشغيل الفيديو',
+    contentExpired: 'هذا المحتوى لم يعد متوفراً',
+    contentNotYetAvailable: 'هذا البرنامج غير متوفر للتشغيل الآن',
+    audio: 'تسجيل صوتي',
+    photogallery: 'ملف صور',
+    video: 'فيديو',
+    bbc_arabic_radio: {
+      title: 'إذاعة بي بي سي العربية',
+      subtitle:
+        'خدمة إخبارية على مدار الساعة و برامج حوارية وتفاعلية تناقش قضايا المنطقة والعالم وباقة من البرامج المنوعة من إذاعة بي بي سي',
+    },
+    listen: 'استمع',
+    watch: 'شاهد',
+    listenLive: 'استمع للبث المباشر',
+    listenNext: 'واصل الاستماع',
+    liveLabel: 'مباشر',
+    nextLabel: 'لاحق',
+    previousRadioShow: 'البرنامج الإذاعي السابق',
+    nextRadioShow: 'البرنامج الإذاعي اللاحق',
+    duration: 'المدة',
+    recentEpisodes: 'البرامج السابقة',
+    podcastExternalLinks: 'هذا البودكاست متاح عبر',
+    download: 'تحميل الحلقة',
+  },
+  socialEmbed: {
+    caption: {
+      textPrefixVisuallyHidden: 'التعليق على الفيديو، ',
+      text: 'تحذير: المحتوى من طرف ثالث قد يتضمن إعلانات',
+      articleText: 'تحذير: بي بي سي غير مسؤولة عن محتوى المواقع الخارجية',
+      articleAdditionalText: 'المحتوى في موقع %provider_name% قد يتضمن إعلانات',
+    },
+    fallback: {
+      text: 'المحتوى غير متاح',
+      linkText: '%provider_name% اطلع على المزيد في',
+      linkTextSuffixVisuallyHidden: '، خارجي',
+      warningText: 'بي بي سي ليست مسؤولة عن محتوى المواقع الخارجية.',
+    },
+    skipLink: {
+      text: 'أهمل %provider_name% مشاركة',
+      endTextVisuallyHidden: 'نهاية %provider_name% مشاركة',
+    },
+    consentBanner: {
+      heading: 'هل تسمح بعرض المحتوى من [social_media_site]؟',
+      body: `تحتوي هذه الصفحة على محتوى من موقع [social_media_site]. موافقتكم مطلوبة قبل عرض أي مواد لأنها قد تتضمن ملفات ارتباط (كوكيز) وغيرها من الأدوات التقنية. قد تفضلون الاطلاع على [link] سياسة ملفات الارتباط [/link] الخاصة بموقع [social_media_site] [link] وسياسة الخصوصية [/link] قبل الموافقة. لعرض المحتوى، اختر "موافقة وإكمال"`,
+    },
+  },
+  include: {
+    errorMessage:
+      'للأسف ليس بمقدورنا إظهار هذا الجزء من القصة على هذه الصفحة الخفيفة من الموبايل ',
+    linkText: 'انظر في السخة الكاملة لترى المحتوى بأكمله',
+  },
+  topStoriesTitle: 'الأخبار الرئيسية',
+  featuresAnalysisTitle: 'اخترنا لكم',
+};
