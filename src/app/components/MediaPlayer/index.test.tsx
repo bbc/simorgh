5e08d1b69515dbc1e207929af8aa7b9d27a3eaf0
Shayne Marc Enzo Ahchoon <shayne.ahchoon@bbc.co.uk>
2024-01-09 10:07:24 +0000

    WSTEAM1-493: Add tests

--- src/app/components/MediaPlayer/index.test.tsx
+++ src/app/components/MediaPlayer/index.test.tsx
@@ -12,40 +12,63 @@ jest.mock('react', () => ({
 }));
 
 describe('MediaPlayer', () => {
-  beforeEach(() => {
-    (useState as jest.Mock).mockImplementation(() => [false, () => false]);
-  });
+  describe('BUMP Loader', () => {
+    beforeEach(() => {
+      (useState as jest.Mock).mockImplementation(() => [false, () => false]);
+    });
 
-  it('Loads requireJS and Bump4', async () => {
-    await act(async () => {
-      render(
-        <MediaPlayer blocks={sampleBlocks} id="testID" pageType="article" />,
+    it('Loads requireJS and Bump4', async () => {
+      await act(async () => {
+        render(
+          <MediaPlayer blocks={sampleBlocks} id="testID" pageType="article" />,
+        );
+      });
+
+      const requireScript = Helmet.peek().scriptTags[0];
+      const bumpScript = Helmet.peek().scriptTags[1];
+
+      expect(requireScript.src).toEqual(
+        'https://static.bbci.co.uk/frameworks/requirejs/0.13.0/sharedmodules/require.js',
+      );
+
+      expect(bumpScript.innerHTML).toContain(
+        'https://emp.bbci.co.uk/emp/bump-4/bump-4',
       );
     });
 
-    const requireScript = Helmet.peek().scriptTags[0];
-    const bumpScript = Helmet.peek().scriptTags[1];
+    it('Calls Bump when the component loads', async () => {
+      const mockRequire = jest.fn();
 
-    expect(requireScript.src).toEqual(
-      'https://static.bbci.co.uk/frameworks/requirejs/0.13.0/sharedmodules/require.js',
-    );
+      window.requirejs = mockRequire;
 
-    expect(bumpScript.innerHTML).toContain(
-      'https://emp.bbci.co.uk/emp/bump-4/bump-4',
-    );
+      await act(async () => {
+        render(
+          <MediaPlayer blocks={sampleBlocks} id="testID" pageType="article" />,
+        );
+      });
+
+      expect(mockRequire.mock.calls[0][0]).toStrictEqual(['bump-4']);
+    });
   });
 
-  it('Calls Bump when the component loads', async () => {
-    const mockRequire = jest.fn();
-
-    window.requirejs = mockRequire;
-
-    await act(async () => {
-      render(
-        <MediaPlayer blocks={sampleBlocks} id="testID" pageType="article" />,
-      );
+  describe('Placeholder', () => {
+    beforeEach(() => {
+      (useState as jest.Mock).mockImplementation(() => [true, () => true]);
     });
 
-    expect(mockRequire.mock.calls[0][0]).toStrictEqual(['bump-4']);
+    it('Displays a placeholder image and playbutton', async () => {
+      let container;
+
+      await act(async () => {
+        ({ container } = render(
+          <MediaPlayer blocks={sampleBlocks} id="testID" pageType="article" />,
+        ));
+      });
+
+      const button = (container as unknown as HTMLElement).querySelector(
+        'button',
+      );
+      expect(button?.textContent).toBe('CLICK TO SEE VIDEO');
+    });
   });
 });
