f77b65a2bb2999495a605b61b548c4160fbce14d
louisearchibald <louise.archibald@bbc.co.uk>
2024-01-11 14:45:40 +0000

    removal of em

--- src/app/components/LiveLabel/index.tsx
+++ src/app/components/LiveLabel/index.tsx
@@ -52,24 +52,23 @@ const LiveLabel = ({
 
   const circle = [
     styles.pulseOuterCircle,
+    styles.pulseContainer,
     className === 'first-promo' && styles.firstPromo,
   ];
 
   return (
     <>
-      <span css={circle}>
+      <div css={circle}>
         <svg
           fill="currentColor"
           focusable="false"
           aria-hidden="true"
           viewBox="0 0 32 32"
-          width="1em"
-          height="1em"
         >
           <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z" />
           <circle css={styles.pulseInnerCircle} cx="16" cy="16" r="8.5" />
         </svg>
-      </span>
+      </div>
 
       <span
         // The id below is a temporary fix for the a11y nested span's bug experienced in TalkBack, refer to the following issue: https://github.com/bbc/simorgh/issues/9652
