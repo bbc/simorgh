98f74d9ba83905d58a11ff4a0e5fd09dad90763d
louisearchibald <louise.archibald@bbc.co.uk>
2024-01-12 12:41:44 +0000

    applying circle css directly to svg

--- src/app/components/LiveLabel/index.tsx
+++ src/app/components/LiveLabel/index.tsx
@@ -57,17 +57,16 @@ const LiveLabel = ({
 
   return (
     <>
-      <span css={circle}>
-        <svg
-          fill="currentColor"
-          focusable="false"
-          aria-hidden="true"
-          viewBox="0 0 32 32"
-        >
-          <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z" />
-          <circle css={styles.pulseInnerCircle} cx="16" cy="16" r="8.5" />
-        </svg>
-      </span>
+      <svg
+        css={circle}
+        fill="currentColor"
+        focusable="false"
+        aria-hidden="true"
+        viewBox="0 0 32 32"
+      >
+        <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z" />
+        <circle css={styles.pulseInnerCircle} cx="16" cy="16" r="8.5" />
+      </svg>
 
       <span
         // The id below is a temporary fix for the a11y nested span's bug experienced in TalkBack, refer to the following issue: https://github.com/bbc/simorgh/issues/9652
