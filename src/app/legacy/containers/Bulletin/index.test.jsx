d9dee76d89c6f72a2a1d4a8b02dfcb37d02f57fd
louisearchibald <louise.archibald@bbc.co.uk>
2024-01-12 17:23:04 +0000

    update to bulletin tests

--- src/app/legacy/containers/Bulletin/index.test.jsx
+++ src/app/legacy/containers/Bulletin/index.test.jsx
@@ -57,7 +57,7 @@ describe('Bulletin Container', () => {
         <BulletinContainer item={liveTvBulletinItem} />,
         { service: 'igbo' },
       );
-      const span = container.getElementsByTagName('span')[2];
+      const span = container.getElementsByTagName('span')[1];
 
       expect(span.textContent).toEqual('NA EME UGBU A ');
     });
@@ -68,7 +68,7 @@ describe('Bulletin Container', () => {
         { service: 'igbo' },
       );
 
-      const span = container.getElementsByTagName('span')[2];
+      const span = container.getElementsByTagName('span')[1];
 
       expect(span.textContent).toEqual('NA EME UGBU A ');
     });
@@ -78,7 +78,7 @@ describe('Bulletin Container', () => {
         <BulletinContainer item={liveTvBulletinItem} />,
         { service: 'igbo' },
       );
-      const span = container.getElementsByTagName('span')[2];
+      const span = container.getElementsByTagName('span')[1];
 
       expect(span.getAttribute('aria-hidden')).toBeDefined();
       expect(span.getAttribute('aria-hidden')).toEqual('true');
