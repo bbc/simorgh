95611f691a7d030db669cc1cf695e846fa00b7e2
louisearchibald <louise.archibald@bbc.co.uk>
2024-01-15 12:25:58 +0000

    adding a russian frontpage to Storybook for testing purposes

--- src/app/pages/FrontPage/index.stories.jsx
+++ src/app/pages/FrontPage/index.stories.jsx
@@ -5,27 +5,46 @@ import { data as urduData } from '#data/urdu/frontpage';
 import { data as newsData } from '#data/news/frontpage';
 import { data as serbianCyrData } from '#data/serbian/frontpage/cyr';
 import { data as serbianLatData } from '#data/serbian/frontpage/lat';
+import { data as russianData } from '#data/russian/frontpage';
 import { service as urduConfig } from '../../lib/config/services/urdu';
 import { service as newsConfig } from '../../lib/config/services/news';
 import { service as serbianConfig } from '../../lib/config/services/serbian';
+import { service as russianConfig } from '../../lib/config/services/russian';
 import { getLocalMostReadEndpoint } from '#lib/utilities/getUrlHelpers/getMostReadUrls';
 import { FRONT_PAGE } from '#app/routes/utils/pageTypes';
 import ThemeProvider from '../../components/ThemeProvider';
 import FrontPage from '.';
 
 const serviceDataSets = {
-  urdu: { default: { ...urduData.article, mostRead: urduData.secondaryData.mostRead } },
-  news: { default: { ...newsData.article, mostRead: newsData.secondaryData.mostRead } },
+  urdu: {
+    default: { ...urduData.article, mostRead: urduData.secondaryData.mostRead },
+  },
+  news: {
+    default: { ...newsData.article, mostRead: newsData.secondaryData.mostRead },
+  },
   serbian: {
-    cyr: { ...serbianCyrData.article, mostRead: serbianCyrData.secondaryData.mostRead },
-    lat: { ...serbianLatData.article, mostRead: serbianLatData.secondaryData.mostRead },
-  }
+    cyr: {
+      ...serbianCyrData.article,
+      mostRead: serbianCyrData.secondaryData.mostRead,
+    },
+    lat: {
+      ...serbianLatData.article,
+      mostRead: serbianLatData.secondaryData.mostRead,
+    },
+  },
+  russian: {
+    default: {
+      ...russianData.article,
+      mostRead: russianData.secondaryData.mostRead,
+    },
+  },
 };
 
 const serviceConfigs = {
   urdu: urduConfig,
   news: newsConfig,
   serbian: serbianConfig,
+  russian: russianConfig,
 };
 
 // eslint-disable-next-line react/prop-types
@@ -51,6 +70,8 @@ const Component = ({ service, variant } = {}) => (
   </ThemeProvider>
 );
 
+console.log('BOB', serviceDataSets);
+
 export default {
   Component,
   title: 'Pages/Front Page',
@@ -63,3 +84,4 @@ export const SerbianCyrillic = () => (
   <Component service="serbian" variant="cyr" />
 );
 export const SerbianLatin = () => <Component service="serbian" variant="lat" />;
+export const Russian = () => <Component service="russian" />;
