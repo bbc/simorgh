7cbfccf269a51cddf650f9e4a604587b42b14c44
Karina Thomas <karina.thomas@bbc.co.uk>
2024-01-15 10:50:39 +0000

    Adding a hindi storybook example for demo purposes

--- src/app/pages/HomePage/index.stories.tsx
+++ src/app/pages/HomePage/index.stories.tsx
@@ -3,13 +3,22 @@ import React from 'react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { HOME_PAGE } from '#app/routes/utils/pageTypes';
 import { data as kyrgyzHomePageData } from '#data/kyrgyz/homePage/index.json';
+import { data as hindiHomePageData } from '#data/hindi/homePage/index.json';
 import { ServiceContextProvider } from '../../contexts/ServiceContext';
 import { withServicesKnob } from '../../legacy/psammead/psammead-storybook-helpers/src';
 import ThemeProvider from '../../components/ThemeProvider';
 import { StoryProps } from '../../models/types/storybook';
 import HomePage from '.';
 
-const Component = ({ service, variant }: StoryProps) => {
+interface Props extends StoryProps {
+  pageData?: object;
+}
+
+const Component = ({
+  service,
+  variant,
+  pageData = kyrgyzHomePageData,
+}: Props) => {
   return (
     <ThemeProvider service={service} variant={variant}>
       <ServiceContextProvider service={service} variant={variant}>
@@ -20,7 +29,7 @@ const Component = ({ service, variant }: StoryProps) => {
           status={200}
           isAmp={false}
           pathname="/kyrgyz"
-          pageData={kyrgyzHomePageData}
+          pageData={pageData}
         />
       </ServiceContextProvider>
     </ThemeProvider>
@@ -35,4 +44,8 @@ export default {
 
 export const Kyrgyz = ({ variant }: StoryProps) => (
   <Component service="kyrgyz" variant={variant} />
+);
+
+export const Hindi = ({ variant }: StoryProps) => (
+  <Component service="hindi" variant={variant} pageData={hindiHomePageData} />
 );
