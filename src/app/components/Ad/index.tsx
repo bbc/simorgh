5df58a01c1be95993d43d8daebd84624de571933
Karina Thomas <karina.thomas@bbc.co.uk>
2024-01-15 11:14:45 +0000

    TEMP: Always display ad banner

--- src/app/components/Ad/index.tsx
+++ src/app/components/Ad/index.tsx
@@ -11,19 +11,18 @@ const AdContainer = ({ slotType, className, adcampaign }: AdProps) => {
   const { enabled: adsEnabled } = useToggle('ads');
 
   // Ads component will only be displayed if ads toggle is true and if showAdsBasedOnLocation is true
-  if ([adsEnabled, showAdsBasedOnLocation].every(Boolean)) {
-    const Ad = isAmp ? AmpAd : CanonicalAd;
+  // if ([adsEnabled, showAdsBasedOnLocation].every(Boolean)) {
+  const Ad = isAmp ? AmpAd : CanonicalAd;
 
-    return (
-      <>
-        {/* dotcom and dotcomConfig need to be setup before the main dotcom javascript file is loaded */}
-        {!isAmp && <AdBootstrapJs adcampaign={adcampaign} />}
-        <Ad slotType={slotType} className={className} />
-      </>
-    );
-  }
+  return (
+    <>
+      {/* dotcom and dotcomConfig need to be setup before the main dotcom javascript file is loaded */}
+      {!isAmp && <AdBootstrapJs adcampaign={adcampaign} />}
+      <Ad slotType={slotType} className={className} />
+    </>
+  );
 
-  return null;
+  // return null;
 };
 
 export default AdContainer;
