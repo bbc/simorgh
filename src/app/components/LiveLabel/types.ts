0bcf3bcfa184b37e6ec102b2874eff07355bf9d0
Harvey Peachey <harvey.peachey@bbc.co.uk>
2024-01-16 09:50:12 +0000

    extracts live label types into file

---
+++ src/app/components/LiveLabel/types.ts
@@ -0,0 +1,16 @@
+export interface LiveLabelProps {
+  offScreenText?: string;
+  lang?: string;
+  id?: string;
+  className?: string;
+}
+
+export interface TextProps {
+  offScreenText?: string;
+  lang?: string;
+  id?: string;
+}
+
+export interface PulseProps {
+  className?: string;
+}
