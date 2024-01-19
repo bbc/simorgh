4ef35aba6e56c0853866f8b3ed82cb58e3bf3d77
louisearchibald <louise.archibald@bbc.co.uk>
2024-01-19 09:47:37 +0000

    width and height on svg

--- src/app/components/LiveLabel/index.tsx
+++ src/app/components/LiveLabel/index.tsx
@@ -63,6 +63,8 @@ const LiveLabel = ({
         focusable="false"
         aria-hidden="true"
         viewBox="0 0 32 32"
+        width="16"
+        height="16"
       >
         <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z" />
         <circle css={styles.pulseInnerCircle} cx="16" cy="16" r="8.5" />
