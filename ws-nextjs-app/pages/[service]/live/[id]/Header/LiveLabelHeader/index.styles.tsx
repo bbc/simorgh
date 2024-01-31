0970681c28ec21c8ed84cdbb506dbc4cbdb3f87c
Harvey Peachey <harvey.peachey@bbc.co.uk>
2024-01-31 12:43:30 +0000

    states calc grid values and adds vertical align

--- ws-nextjs-app/pages/[service]/live/[id]/Header/LiveLabelHeader/index.styles.tsx
+++ ws-nextjs-app/pages/[service]/live/[id]/Header/LiveLabelHeader/index.styles.tsx
@@ -1,12 +1,22 @@
 import { css, Theme } from '@emotion/react';
+import { HALF, TRIPLE } from '#app/components/ThemeProvider/spacings';
+import pixelsToRem from '#app/utilities/pixelsToRem';
+
+const PULSE_END_MARGIN = HALF;
+const PULSE_SIZE = TRIPLE;
+const PULSE_SIZE_TOTAL_WIDTH = HALF + TRIPLE;
 
 const styles = {
-  liveLabelPulse: ({ spacings, palette }: Theme) =>
+  liveLabelPulse: ({ mq, palette }: Theme) =>
     css({
-      width: `${spacings.TRIPLE}rem`,
-      height: `${spacings.TRIPLE}rem`,
+      width: `${PULSE_SIZE}rem`,
+      height: `${PULSE_SIZE}rem`,
       color: palette.LIVE_LIGHT,
-      verticalAlign: '0',
+      verticalAlign: `${pixelsToRem(-3)}rem`,
+      marginInlineEnd: `${PULSE_END_MARGIN}rem`,
+      [mq.GROUP_3_MIN_WIDTH]: {
+        verticalAlign: 0,
+      },
     }),
   liveLabelTextWithImage: ({ palette }: Theme) =>
     css({
@@ -18,11 +28,12 @@ const styles = {
         display: 'inline-flex',
         color: palette.LIVE_LIGHT,
         'overflow-wrap': 'anywhere',
+        marginInlineEnd: '0',
         [mq.GROUP_4_MIN_WIDTH]: {
-          width: 'calc(100% / 3  - 2.25rem)',
+          width: `calc(100% / 3  - ${PULSE_SIZE_TOTAL_WIDTH}rem)`,
         },
         [mq.GROUP_5_MIN_WIDTH]: {
-          width: 'calc(25% - 2.25rem)',
+          width: `calc(25%  - ${PULSE_SIZE_TOTAL_WIDTH}rem)`,
         },
       },
     }),
