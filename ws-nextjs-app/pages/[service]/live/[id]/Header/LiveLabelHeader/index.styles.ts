59a0b3c6693af67e800bae301f539f38970d44fd
Harvey Peachey <harvey.peachey@bbc.co.uk>
2024-02-02 15:07:47 +0000

    add values specifically for Burmese

--- ws-nextjs-app/pages/[service]/live/[id]/Header/LiveLabelHeader/index.styles.ts
+++ ws-nextjs-app/pages/[service]/live/[id]/Header/LiveLabelHeader/index.styles.ts
@@ -1,21 +1,28 @@
 import { css, Theme } from '@emotion/react';
-import { HALF, TRIPLE } from '#app/components/ThemeProvider/spacings';
+import { HALF, QUADRUPLE } from '#app/components/ThemeProvider/spacings';
 import pixelsToRem from '#app/utilities/pixelsToRem';
 
 const PULSE_END_MARGIN = HALF;
-const PULSE_SIZE = TRIPLE;
-const PULSE_SIZE_TOTAL_WIDTH = PULSE_END_MARGIN + PULSE_SIZE;
+const PULSE_SIZE_3_4 = QUADRUPLE;
+const PULSE_SIZE_TOTAL_WIDTH_3_MIN = PULSE_END_MARGIN + PULSE_SIZE_3_4;
 
 const styles = {
-  liveLabelPulse: ({ mq, palette }: Theme) =>
+  liveLabelPulse: ({ mq, palette, spacings }: Theme) =>
     css({
-      width: `${PULSE_SIZE}rem`,
-      height: `${PULSE_SIZE}rem`,
+      width: `${spacings.HALF + spacings.DOUBLE}rem`,
+      height: `${spacings.HALF + spacings.DOUBLE}rem`,
       color: palette.LIVE_LIGHT,
-      verticalAlign: `${pixelsToRem(-3)}rem`,
+      verticalAlign: `${pixelsToRem(-5)}rem`,
       marginInlineEnd: `${PULSE_END_MARGIN}rem`,
+      [mq.GROUP_1_MIN_WIDTH]: {
+        verticalAlign: `${pixelsToRem(-7)}rem`,
+        width: `${spacings.TRIPLE}rem`,
+        height: `${spacings.TRIPLE}rem`,
+      },
       [mq.GROUP_3_MIN_WIDTH]: {
-        verticalAlign: 0,
+        verticalAlign: `${pixelsToRem(-8)}rem`,
+        width: `${spacings.QUADRUPLE}rem`,
+        height: `${spacings.QUADRUPLE}rem`,
       },
     }),
   liveLabelTextWithImage: ({ palette }: Theme) =>
@@ -30,10 +37,10 @@ const styles = {
         'overflow-wrap': 'anywhere',
         marginInlineEnd: '0',
         [mq.GROUP_4_MIN_WIDTH]: {
-          width: `calc(100% / 3  - ${PULSE_SIZE_TOTAL_WIDTH}rem)`,
+          width: `calc(100% / 3  - ${PULSE_SIZE_TOTAL_WIDTH_3_MIN}rem)`,
         },
         [mq.GROUP_5_MIN_WIDTH]: {
-          width: `calc(25%  - ${PULSE_SIZE_TOTAL_WIDTH}rem)`,
+          width: `calc(25%  - ${PULSE_SIZE_TOTAL_WIDTH_3_MIN}rem)`,
         },
       },
     }),
