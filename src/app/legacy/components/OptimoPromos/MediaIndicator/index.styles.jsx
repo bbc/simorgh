083c5f22249a47ef82ce6efb62df5d6c4cddbeb8
louisearchibald <louise.archibald@bbc.co.uk>
2024-02-05 16:40:48 +0000

    changing to linkText

--- src/app/legacy/components/OptimoPromos/MediaIndicator/index.styles.jsx
+++ src/app/legacy/components/OptimoPromos/MediaIndicator/index.styles.jsx
@@ -12,7 +12,7 @@ export const MediaIndicatorAlignment = styled.div`
   height: 100%;
   > svg {
     @media screen and (-ms-forced-colors: active) {
-      fill: canvasText;
+      fill: linkText;
     }
   }
 `;
