b46171ad8c95d0a16b5db1c3d18e44a04338056f
louisearchibald <louise.archibald@bbc.co.uk>
2024-02-09 10:07:28 +0000

    removing commented code

--- src/app/legacy/components/RadioSchedule/index.jsx
+++ src/app/legacy/components/RadioSchedule/index.jsx
@@ -67,10 +67,7 @@ const programGridProps = {
 
 const RadioSchedule = ({ schedule, ...props }) => {
   const { dir } = useContext(ServiceContext);
-  // console.log('inside RadioSchedule component', schedule, 'props', props);
-  // if (schedule.hasOwnProperty('radioSchedule')) {
-  //   schedule = schedule.radioSchedule;
-  // }
+
   const eventTrackingData = {
     componentName: 'radio-schedule',
   };
