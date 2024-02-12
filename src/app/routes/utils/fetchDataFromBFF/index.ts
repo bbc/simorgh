9f758915f51b1202dc053719401f483d1a6f7407
Karina Thomas <karina.thomas@bbc.co.uk>
2024-02-12 14:33:07 +0000

    Fix unit tests

--- src/app/routes/utils/fetchDataFromBFF/index.ts
+++ src/app/routes/utils/fetchDataFromBFF/index.ts
@@ -53,12 +53,14 @@ export default async ({
   });
 
   const agent = isLocal || BFF_IS_LOCAL ? undefined : await getAgent();
-  const timeout = isLocal || BFF_IS_LOCAL ? 60000 : null;
-  const optHeaders: OptHeaders = {
-    'ctx-service-env': environment,
-  };
+  const timeout = isLocal && BFF_IS_LOCAL ? 60000 : null;
+  const optHeaders: OptHeaders =
+    (!isLocal && {
+      'ctx-service-env': environment,
+    }) ||
+    undefined;
 
-  if (BFF_IS_LOCAL) {
+  if (BFF_IS_LOCAL && optHeaders) {
     optHeaders.Accept = 'text/html,application/xhtml+xml,application/xml';
   }
 
