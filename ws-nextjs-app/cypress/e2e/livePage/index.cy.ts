add6cd66cb6655afce24df0277838ea1ff218bce
Aaron Moore <aaron.moore@bbc.co.uk>
2024-02-12 11:09:27 +0000

    Add basic e2e test

--- ws-nextjs-app/cypress/e2e/livePage/index.cy.ts
+++ ws-nextjs-app/cypress/e2e/livePage/index.cy.ts
@@ -1,28 +1,31 @@
-describe(
-  'Live Page Spec',
-  {
-    env: {
-      optionalOvveride: 'foo',
-    },
-  },
-  // @ts-expect-error Conflicts with Jest global types describe, it etc.
-  () => {
-    // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-    let livePageData;
-    before(() => {
-      cy.getPageData({
-        service: 'pidgin',
-        pageType: 'live',
-        id: 'c7p765ynk9qt',
-      }).then(({ body }) => {
-        livePageData = body;
-      });
+describe('Live Page Spec', () => {
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  let livePageData;
+  before(() => {
+    cy.getPageData({
+      service: 'pidgin',
+      pageType: 'live',
+      id: 'c7p765ynk9qt',
+    }).then(({ body }) => {
+      livePageData = body;
+    });
+  });
+
+  if (Cypress.env('APP_ENV') === 'test') {
+    it('visits page and passes', () => {
+      cy.visit('/pidgin/live/c7p765ynk9qt?renderer_env=test');
     });
 
-    if (Cypress.env('APP_ENV') === 'test') {
-      it('passes', () => {
-        cy.visit('/pidgin/live/c7p765ynk9qt');
+    describe(`SEO Metadata`, () => {
+      it('should include mainEntityOfPage in the LinkedData', () => {
+        cy.visit('/pidgin/live/c7p765ynk9qt?renderer_env=test');
+
+        cy.get('script[type="application/ld+json"]')
+          .should('contain', 'mainEntityOfPage')
+          .and('contain', 'author')
+          .and('contain', 'headline')
+          .and('contain', 'liveBlogPosting');
       });
-    }
-  },
-);
+    });
+  }
+});
