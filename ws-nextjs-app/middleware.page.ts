a63491f6a657227e35e35aa743824cb645b2074a
Aaron Moore <aaron.moore@bbc.co.uk>
2024-02-13 15:04:22 +0000

    Remove port from hostname

--- ws-nextjs-app/middleware.page.ts
+++ ws-nextjs-app/middleware.page.ts
@@ -2,12 +2,14 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+const LOCALHOST_DOMAINS = ['localhost', '127.0.0.1'];
+
 export function middleware(request: NextRequest) {
   // Service worker is registered at the root (e.g. /pidgin) so will work as is on Test/Live
   // but will not work on localhost. This middleware rewrites the request to the sw.js file found in the 'public' folder
   const hostname = request.headers.get('host') ?? request.nextUrl.hostname;
 
-  if (['localhost', '127.0.0.1'].includes(hostname)) {
+  if (LOCALHOST_DOMAINS.includes(hostname.split(':')[0])) {
     if (request.nextUrl.pathname.endsWith('/sw.js')) {
       return NextResponse.rewrite(new URL('/sw.js', request.url));
     }
@@ -18,5 +20,5 @@ export function middleware(request: NextRequest) {
 
 // Only run middleware for sw.js requests
 export const config = {
-  matcher: '/(.*)/(sw.js)',
+  matcher: '/(.*)/sw.js',
 };
