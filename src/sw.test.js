bd7e1a245b7cea8ca5e6edd14a461b3e6f6f2b12
Aaron Moore <aaron.moore@bbc.co.uk>
2024-02-13 10:48:58 +0000

    Update sw.test.js

--- src/sw.test.js
+++ src/sw.test.js
@@ -34,6 +34,8 @@ describe('Service Worker', () => {
         ${`${BASE_IMAGE_URL}/news/puppies.png`}         | ${`${BASE_IMAGE_URL}/news/puppies.png.webp`}
         ${`${BASE_IMAGE_URL}/ace/standard/puppies.jpg`} | ${`${BASE_IMAGE_URL}/ace/standard/puppies.jpg.webp`}
         ${`${BASE_IMAGE_URL}/ace/standard/puppies.png`} | ${`${BASE_IMAGE_URL}/ace/standard/puppies.png.webp`}
+        ${`${BASE_IMAGE_URL}/ace/ws/puppies.jpg`}       | ${`${BASE_IMAGE_URL}/ace/ws/puppies.jpg.webp`}
+        ${`${BASE_IMAGE_URL}/ace/ws/puppies.png`}       | ${`${BASE_IMAGE_URL}/ace/ws/puppies.png.webp`}
       `(`for $image is $expectedUrl`, async ({ image, expectedUrl }) => {
         ({ fetchEventHandler } = await import('./service-worker-test'));
 
@@ -53,8 +55,9 @@ describe('Service Worker', () => {
     describe('image is not requested', () => {
       it.each`
         image                                                 | headers               | reason
-        ${`${BASE_IMAGE_URL}/sport/puppies.jpg`}              | ${{ accept: 'webp' }} | ${'image url must include news or ace/standard'}
-        ${`${BASE_IMAGE_URL}/ace/stndard/puppies.jpg`}        | ${{ accept: 'webp' }} | ${'image url must include news or ace/standard'}
+        ${`${BASE_IMAGE_URL}/sport/puppies.jpg`}              | ${{ accept: 'webp' }} | ${'image url must include news, ace/standard or ace/ws'}
+        ${`${BASE_IMAGE_URL}/ace/stndard/puppies.jpg`}        | ${{ accept: 'webp' }} | ${'image url must include news, ace/standard or ace/ws'}
+        ${`${BASE_IMAGE_URL}/ace/sw/puppies.jpg`}             | ${{ accept: 'webp' }} | ${'image url must include news, ace/standard or ace/ws'}
         ${`${BASE_IMAGE_URL}/news/puppies.jpeg`}              | ${{ accept: 'webp' }} | ${'image extension must be jpg or png'}
         ${`${BASE_IMAGE_URL}/news/amz/puppies.jpeg`}          | ${{ accept: 'webp' }} | ${'image url must not include amz'}
         ${`${BASE_IMAGE_URL}/news/worldservice/puppies.jpeg`} | ${{ accept: 'webp' }} | ${'image url must not include worldservice'}
