68779dc87bcc87da5efb48d96852b6bd1515b91e
Aaron Moore <aaron.moore@bbc.co.uk>
2024-02-13 14:22:09 +0000

    Update middleware.page.ts

--- ws-nextjs-app/middleware.page.ts
+++ ws-nextjs-app/middleware.page.ts
@@ -5,7 +5,9 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   // Service worker is registered at the root (e.g. /pidgin) so will work as is on Test/Live
   // but will not work on localhost. This middleware rewrites the request to the sw.js file found in the 'public' folder
-  if (['localhost', '127.0.0.1'].includes(request.nextUrl.hostname)) {
+  const hostname = request.headers.get('host') ?? request.nextUrl.hostname;
+
+  if (['localhost', '127.0.0.1'].includes(hostname)) {
     if (request.nextUrl.pathname.endsWith('/sw.js')) {
       return NextResponse.rewrite(new URL('/sw.js', request.url));
     }
