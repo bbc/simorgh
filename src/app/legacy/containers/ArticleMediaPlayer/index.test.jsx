021e38d7a0985a90854e3f65d22c3050c65bda4e
Harvey Peachey <harvey.peachey@bbc.co.uk>
2024-02-14 09:53:08 +0000

    adds unit test to check placeholder is not rendered

--- src/app/legacy/containers/ArticleMediaPlayer/index.test.jsx
+++ src/app/legacy/containers/ArticleMediaPlayer/index.test.jsx
@@ -1,40 +1,50 @@
 import React from 'react';
-import { ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
-import { render } from '../../../components/react-testing-library-with-providers';
+import { ARTICLE_PAGE, MEDIA_ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
+import {
+  render,
+  screen,
+} from '../../../components/react-testing-library-with-providers';
 import ArticleMediaPlayerContainer from '.';
 import { validAresMediaVideoBlock } from '../MediaPlayer/fixtureData';
 
 const blocks = [validAresMediaVideoBlock];
 
+const renderMediaPlayer = ({ platform, pageType }) =>
+  render(<ArticleMediaPlayerContainer blocks={blocks} />, {
+    isAmp: platform === 'amp',
+    platform,
+    service: 'news',
+    statusCode: 200,
+    id: 'c1234567890',
+    pageType,
+    pathname: '/pathname',
+  });
+
 describe('MediaPlayer', () => {
   it('Calls the canonical media player, with a placeholder', () => {
-    const { container } = render(
-      <ArticleMediaPlayerContainer blocks={blocks} />,
-      {
-        platform: 'canonical',
-        service: 'news',
-        statusCode: 200,
-        id: 'c1234567890',
-        pageType: ARTICLE_PAGE,
-        pathname: '/pathname',
-      },
-    );
+    const { container } = renderMediaPlayer({
+      platform: 'canonical',
+      pageType: ARTICLE_PAGE,
+    });
     expect(container).toMatchSnapshot();
   });
 
   it('Calls the amp media player', () => {
-    const { container } = render(
-      <ArticleMediaPlayerContainer blocks={blocks} />,
-      {
-        isAmp: true,
-        platform: 'amp',
-        service: 'news',
-        statusCode: 200,
-        id: 'c1234567890',
-        pageType: ARTICLE_PAGE,
-        pathname: '/pathname',
-      },
-    );
+    const { container } = renderMediaPlayer({
+      platform: 'amp',
+      pageType: ARTICLE_PAGE,
+    });
     expect(container).toMatchSnapshot();
+  });
+
+  it('Should not render a placeholder for canonical MediaArticle pages', () => {
+    renderMediaPlayer({
+      platform: 'canonical',
+      pageType: MEDIA_ARTICLE_PAGE,
+    });
+
+    const mediaPlayerIframe = screen.getByTitle(/media player/i);
+
+    expect(mediaPlayerIframe).toBeInTheDocument();
   });
 });
