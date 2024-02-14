2aa249c1e78a461593b6496ff945d6a20688928e
Harvey Peachey <harvey.peachey@bbc.co.uk>
2024-02-14 09:30:04 +0000

    opportunistic refactor of ArticleMediaPlayer tests

--- src/app/legacy/containers/ArticleMediaPlayer/index.test.jsx
+++ src/app/legacy/containers/ArticleMediaPlayer/index.test.jsx
@@ -1,52 +1,39 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { RequestContextProvider } from '#contexts/RequestContext';
 import { ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
 import { render } from '../../../components/react-testing-library-with-providers';
-import { ServiceContextProvider } from '../../../contexts/ServiceContext';
 import ArticleMediaPlayerContainer from '.';
 import { validAresMediaVideoBlock } from '../MediaPlayer/fixtureData';
 
-const GenerateMediaPlayer = ({
-  /* eslint-disable react/prop-types */
-  platform,
-  blocks,
-  /* eslint-enable react/prop-types */
-}) => (
-  <RequestContextProvider
-    isAmp={platform === 'amp'}
-    service="news"
-    statusCode={200}
-    platform={platform}
-    id="c1234567890"
-    pageType={ARTICLE_PAGE}
-    pathname="/pathname"
-  >
-    <ServiceContextProvider service="news">
-      <BrowserRouter>
-        <ArticleMediaPlayerContainer blocks={blocks} />
-      </BrowserRouter>
-    </ServiceContextProvider>
-  </RequestContextProvider>
-);
+const blocks = [validAresMediaVideoBlock];
 
 describe('MediaPlayer', () => {
   it('Calls the canonical media player, with a placeholder', () => {
     const { container } = render(
-      <GenerateMediaPlayer
-        platform="canonical"
-        blocks={[validAresMediaVideoBlock]}
-      />,
+      <ArticleMediaPlayerContainer blocks={blocks} />,
+      {
+        platform: 'canonical',
+        service: 'news',
+        statusCode: 200,
+        id: 'c1234567890',
+        pageType: ARTICLE_PAGE,
+        pathname: '/pathname',
+      },
     );
     expect(container).toMatchSnapshot();
   });
 
   it('Calls the amp media player', () => {
     const { container } = render(
-      <GenerateMediaPlayer
-        platform="amp"
-        blocks={[validAresMediaVideoBlock]}
-      />,
+      <ArticleMediaPlayerContainer blocks={blocks} />,
+      {
+        isAmp: true,
+        platform: 'amp',
+        service: 'news',
+        statusCode: 200,
+        id: 'c1234567890',
+        pageType: ARTICLE_PAGE,
+        pathname: '/pathname',
+      },
     );
     expect(container).toMatchSnapshot();
   });
