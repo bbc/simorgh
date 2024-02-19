00d9f6eff166cb9a9ce6b3bedcd71c73989b5cd8
Aaron Moore <aaron.moore@bbc.co.uk>
2024-02-19 21:47:33 +0000

    Use base normalize stylesheet

--- src/server/Document/component.jsx
+++ src/server/Document/component.jsx
@@ -53,14 +53,11 @@ const Document = ({
     // const figureRegex = /<figure\b[^>]*>[\s\S]*?<\/figure>/gi;
     // const pictureRegex = /<picture\b[^>]*>[\s\S]*?<\/picture>/gi;
     // const inlineStyleRegex = /style="[^"]*"/gi;
-    // // remove div with id include-3
-    // const includeDivElementByIdRegex = /<div id="include-3">[\s\S]*?<\/div>/gi;
 
     // cleanedHtml = cleanedHtml.replace(imgRegex, '');
     // cleanedHtml = cleanedHtml.replace(figureRegex, '');
     // cleanedHtml = cleanedHtml.replace(pictureRegex, '');
     // cleanedHtml = cleanedHtml.replace(inlineStyleRegex, '');
-    // cleanedHtml = cleanedHtml.replace(includeDivElementByIdRegex, '');
 
     // Prevent preloading images
     cleanedHelmetLinkTags = cleanedHelmetLinkTags.filter(
@@ -71,12 +68,17 @@ const Document = ({
 
     const $ = cheerio.load(cleanedHtml);
 
-    $('img, figure, picture').remove();
-    $('div[id^=include-]').remove();
-    $('nav[role=navigation]').remove();
-    $('span[id=BrandLink-topPage]').remove();
-    $('span[id=BrandLink-footer]').remove();
+    $(`
+      img, figure, picture, 
+      div[id^=include-],
+      nav[role=navigation],
+      span[id=BrandLink-topPage],
+      span[id=BrandLink-footer],
+      [data-e2e=media-indicator],
+      [data-e2e*="embed"]
+    `).remove();
 
+    // Remove inline styles
     $('[style]').removeAttr('style');
 
     cleanedHtml = $.html();
@@ -108,17 +110,7 @@ const Document = ({
     />
   );
 
-  const LITE_STYLES = (
-    <style type="text/css">{`
-      body {
-        margin: 0;
-        padding: 0;
-      }
-      #main-wrapper > div {
-        padding: 20px;
-      }
-  `}</style>
-  );
+  const normalize = `html{line-height:1.15;-webkit-text-size-adjust:100%;}body{margin:0}main{display:block}h1{font-size:2em;margin:.67em 0}hr{box-sizing:content-box;height:0;overflow:visible}pre{font-family:monospace,monospace;font-size:1em}a{background-color:transparent}abbr[title]{border-bottom:none;text-decoration:underline;text-decoration:underline dotted}b,strong{font-weight:bolder}code,kbd,samp{font-family:monospace,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}img{border-style:none}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}button,input{overflow:visible}button,select{text-transform:none}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner,button::-moz-focus-inner{border-style:none;padding:0}[type=button]:-moz-focusring,[type=reset]:-moz-focusring,[type=submit]:-moz-focusring,button:-moz-focusring{outline:1px dotted ButtonText}fieldset{padding:.35em .75em .625em}legend{box-sizing:border-box;color:inherit;display:table;max-width:100%;padding:0;white-space:normal}progress{vertical-align:baseline}textarea{overflow:auto}[type=checkbox],[type=radio]{box-sizing:border-box;padding:0}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}details{display:block}summary{display:list-item}[hidden],template{display:none}`;
 
   return (
     <html lang="en-GB" {...noJsHtmlAttrs} {...htmlAttrs}>
@@ -129,7 +121,7 @@ const Document = ({
         {cleanedHelmetLinkTags}
         {headScript}
         {isCanonical && CANONICAL_STYLES}
-        {isLite && LITE_STYLES}
+        {isLite && <style>{normalize}</style>}
         {isAmp && (
           <>
             {AMP_STYLES}
