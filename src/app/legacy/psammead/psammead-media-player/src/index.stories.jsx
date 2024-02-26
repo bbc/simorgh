af6dffd7d625e0907383580eae752a4c377c9b3b
Harvey Peachey <harvey.peachey@bbc.co.uk>
2024-02-26 11:21:14 +0000

    adds invidual delay for stories with live label animation

--- src/app/legacy/psammead/psammead-media-player/src/index.stories.jsx
+++ src/app/legacy/psammead/psammead-media-player/src/index.stories.jsx
@@ -147,7 +147,11 @@ storiesOf('Components/Media Player', module)
         />
       </ThemeProvider>
     ),
-    { notes, knobs: { escapeHTML: false } },
+    {
+      notes, knobs: { escapeHTML: false }, parameters: {
+        chromatic: { delay: 8000 },
+      },
+    },
   );
 
 storiesOf('Components/Media Player', module)
