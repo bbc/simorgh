de9ae44130aaed95b7678e006c4f5e2f89c11de3
Isabella Mitchell <i.mitchell.0707@gmail.com>
2024-02-26 10:07:17 +0000

    A11y: Fix please

--- src/app/components/LiveLabel/Text/index.styles.ts
+++ src/app/components/LiveLabel/Text/index.styles.ts
@@ -1,13 +1,13 @@
-// import { css, Theme } from '@emotion/react';
+import { css, Theme } from '@emotion/react';
 
-// const styles = {
-//   liveLabelText: ({ fontVariants, palette, spacings }: Theme) =>
-//     css({
-//       color: palette.LIVE_DARK,
-//       display: 'inline-block',
-//       ...fontVariants.sansBold,
-//       marginInlineEnd: `${spacings.FULL}rem`,
-//     }),
-// };
+const styles = {
+  liveLabelText: ({ fontVariants, palette, spacings }: Theme) =>
+    css({
+      color: palette.LIVE_DARK,
+      display: 'inline-block',
+      ...fontVariants.sansBold,
+      marginInlineEnd: `${spacings.FULL}rem`,
+    }),
+};
 
-// export default styles;
+export default styles;
