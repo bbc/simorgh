54e90753473815412548434b47c17819fc98bbd1
alex-magana <alex.magana@andela.com>
2024-02-27 19:36:58 +0300

    Add reverb installation step

--- scripts/installNodeModules.sh
+++ scripts/installNodeModules.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ "$1" = "--production" ]; then
   echo "Installing all node modules"
-  rm -rf node_modules | yarn workspaces focus --all --production
+  rm -rf node_modules | yarn add "@bbc/reverb@git+ssh://git@github.com:bbc/reverb.git#semver:^3.9.0" | yarn workspaces focus --all --production
 else
   echo "Installing all node modules"
-  rm -rf node_modules | yarn install --immutable --check-cache
+  rm -rf node_modules | yarn add "@bbc/reverb@git+ssh://git@github.com:bbc/reverb.git#semver:^3.9.0" | yarn install --immutable --check-cache
 fi
