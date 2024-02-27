479f1a674cd0168164e1ac9f23c650cf127f5eb0
alex-magana <alex.magana@andela.com>
2024-02-27 15:30:00 +0300

    Linting fix

--- src/app/routes/utils/getEnvironment/index.ts
+++ src/app/routes/utils/getEnvironment/index.ts
@@ -3,6 +3,7 @@ import { Environments } from '#app/models/types/global';
 import Url from 'url-parse';
 
 export default (pathname: string) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const url = new Url(`https://www.bbc.com${pathname}`, true);
 
   // const rendererEnv = url?.query?.renderer_env;
