fe52dddd86d690888278474101e570be34a6b3dd
Shayne Marc Enzo Ahchoon <shayne.ahchoon@bbc.co.uk>
2024-02-28 13:30:20 +0000

    WSTEAM1-827: Fix types

--- src/app/components/LivePageMediaPlayer/index.test.tsx
+++ src/app/components/LivePageMediaPlayer/index.test.tsx
@@ -4,8 +4,9 @@ import { render } from '../react-testing-library-with-providers';
 import LivePageMediaPlayer from '.';
 import { validLivePageVideoWithCaptionBlock } from '../../legacy/containers/MediaPlayer/fixtureData';
 import { LIVE_PAGE } from '../../routes/utils/pageTypes';
+import { MediaBlock } from '../MediaLoader/types';
 
-const GenerateMediaPlayer = ({ blocks }) => (
+const GenerateMediaPlayer = ({ blocks }: { blocks: MediaBlock[] }) => (
   <BrowserRouter>
     <LivePageMediaPlayer blocks={blocks} className="MediaPlayer" />
   </BrowserRouter>
@@ -14,7 +15,9 @@ const GenerateMediaPlayer = ({ blocks }) => (
 describe('MediaPlayer', () => {
   it('Calls the canonical media player, with a placeholder', () => {
     const { container } = render(
-      <GenerateMediaPlayer blocks={validLivePageVideoWithCaptionBlock} />,
+      <GenerateMediaPlayer
+        blocks={validLivePageVideoWithCaptionBlock as MediaBlock[]}
+      />,
       {
         id: 'c7p765ynk9qt',
         service: 'pidgin',
@@ -28,7 +31,9 @@ describe('MediaPlayer', () => {
 
   it('Calls the amp media player', () => {
     const { container } = render(
-      <GenerateMediaPlayer blocks={validLivePageVideoWithCaptionBlock} />,
+      <GenerateMediaPlayer
+        blocks={validLivePageVideoWithCaptionBlock as MediaBlock[]}
+      />,
       {
         isAmp: true,
         id: 'c7p765ynk9qt',
