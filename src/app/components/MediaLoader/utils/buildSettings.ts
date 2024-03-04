c309d0db11e810f904ef3448260181bcb6de241f
Shayne Marc Enzo Ahchoon <shayne.ahchoon@bbc.co.uk>
2024-03-04 14:15:01 +0000

    WSTEAM1-TEST

--- src/app/components/MediaLoader/utils/buildSettings.ts
+++ src/app/components/MediaLoader/utils/buildSettings.ts
@@ -3,17 +3,17 @@ import onClient from '#app/lib/utilities/onClient';
 import { BuildConfigProps } from '../types';
 
 const isTestRequested = () => {
-  let isRenderEnvLive = false;
-  const isTestServer =
-    process.env.SIMORGH_APP_ENV === 'test' ||
-    process.env.SIMORGH_APP_ENV === 'local';
-
   if (onClient()) {
+    const testLiterals = window.location.hostname.match(/localhost|test/g);
+    const isTest = testLiterals && testLiterals.length > 0;
+
     const queryParams = new URLSearchParams(window.location.search);
-    isRenderEnvLive = queryParams.get('renderer_env') === 'live';
+    const isRenderEnvLive = queryParams.get('renderer_env') === 'live';
+
+    return isTest && !isRenderEnvLive;
   }
 
-  return isTestServer && !isRenderEnvLive;
+  return false;
 };
 
 const buildConfig = ({
