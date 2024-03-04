ddf0fa9bc9fb77146aacea3161c9c45975f24697
Aaron Moore <aaron.moore@bbc.co.uk>
2024-03-04 21:37:05 +0000

    Type check `SIMORGH_APP_ENV`

--- src/app/lib/utilities/getEnvConfig/index.ts
+++ src/app/lib/utilities/getEnvConfig/index.ts
@@ -1,10 +1,11 @@
 import onClient from '#app/lib/utilities/onClient';
+import { Environments } from '#app/models/types/global';
 
 export type EnvConfig = ReturnType<typeof initialiseAppEnvVariables>;
 
 // Any environment variables added here will be available to the client and server
 export const initialiseAppEnvVariables = () => ({
-  SIMORGH_APP_ENV: process.env.SIMORGH_APP_ENV,
+  SIMORGH_APP_ENV: process.env.SIMORGH_APP_ENV as Environments,
   SIMORGH_ATI_BASE_URL: process.env.SIMORGH_ATI_BASE_URL,
   SIMORGH_ICHEF_BASE_URL: process.env.SIMORGH_ICHEF_BASE_URL,
   SIMORGH_OPTIMIZELY_SDK_KEY: process.env.SIMORGH_OPTIMIZELY_SDK_KEY,
@@ -13,12 +14,14 @@ export const initialiseAppEnvVariables = () => ({
 export function getEnvConfig(): EnvConfig {
   const APP_ENV_VARIABLES = initialiseAppEnvVariables();
 
+  // Return window object on client
   if (onClient()) {
     // Defaulting to APP_ENV_VARIABLES is mainly for tests
-    // window.SIMORGH_ENV_VARS is not set in tests, as they can sometimes manipulate process.env directly
+    // window.SIMORGH_ENV_VARS is not being set in tests
     return window.SIMORGH_ENV_VARS ?? APP_ENV_VARIABLES;
   }
 
+  // Return APP_ENV_VARIABLES on server
   return APP_ENV_VARIABLES;
 }
 
