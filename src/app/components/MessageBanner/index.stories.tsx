b60dd06fd20a08af1cacb7975f775eb8dcabb384
louisearchibald <louise.archibald@bbc.co.uk>
2024-03-05 17:12:55 +0000

    hacking stories file to show billboard in storybook

--- src/app/components/MessageBanner/index.stories.tsx
+++ src/app/components/MessageBanner/index.stories.tsx
@@ -4,7 +4,8 @@ import { withKnobs } from '@storybook/addon-knobs';
 import { ServiceContextProvider } from '../../contexts/ServiceContext';
 import { withServicesKnob } from '../../legacy/psammead/psammead-storybook-helpers/src';
 import ThemeProvider from '../ThemeProvider';
-import MessageBanner from '.';
+import Header from '../../../../ws-nextjs-app/pages/[service]/live/[id]/Header';
+// import MessageBanner from '.';
 import { StoryProps } from '../../models/types/storybook';
 
 interface Props extends StoryProps {
@@ -12,16 +13,19 @@ interface Props extends StoryProps {
   longText?: string;
 }
 
-const Component = ({ service, variant, text = '', longText = '' }: Props) => {
+const Component = ({ service, variant }: Props) => {
   return (
     <ThemeProvider service={service} variant={variant}>
       <ServiceContextProvider service={service} variant={variant}>
-        <MessageBanner
-          heading={text}
-          description={longText}
-          link="https://www.bbc.co.uk/ws/languages"
-          linkText={text}
-          image="https://ichef.bbci.co.uk/ace/standard/raw/cpsprodpb/e329/live/0a700dd0-7cb3-11ee-a503-4588075e3427.png"
+        <Header
+          showLiveLabel={true}
+          title={'Би-Би-Си ТВ жаңылыктары'}
+          description={
+            'Би-Би-Си Кыргыз кызматынын эл аралык жаңылыктарын көрүңүз.'
+          }
+          imageUrl="https://ichef.bbci.co.uk/ace/standard/480/cpsdevpb/1d5b/test/5f969ec0-c4d8-11ed-8319-9b394d8ed0dd.jpg"
+          imageUrlTemplate="https://ichef.bbci.co.uk/ace/standard/{width}/cpsdevpb/1d5b/test/5f969ec0-c4d8-11ed-8319-9b394d8ed0dd.jpg"
+          imageWidth={660}
         />
       </ServiceContextProvider>
     </ThemeProvider>
