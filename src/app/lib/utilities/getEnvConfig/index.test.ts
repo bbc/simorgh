9643ff2c4017302cff02b8bdc446429050560e79
Aaron Moore <aaron.moore@bbc.co.uk>
2024-03-05 18:59:30 +0000

    Improve tests

--- src/app/lib/utilities/getEnvConfig/index.test.ts
+++ src/app/lib/utilities/getEnvConfig/index.test.ts
@@ -1,15 +1,25 @@
 import { getEnvConfig } from '.';
 
+let windowSpy: jest.SpyInstance<Window | undefined, []>;
+
 describe('getEnvConfig', () => {
   const originalProcessEnv = process.env;
 
   beforeEach(() => {
     process.env = originalProcessEnv;
+    windowSpy = jest.spyOn(window, 'window', 'get');
+  });
+
+  afterEach(() => {
+    windowSpy.mockRestore();
   });
 
   it('server side - should return values from "getEnvConfig"', () => {
     process.env.SIMORGH_APP_ENV = 'local';
     process.env.SIMORGH_BASE_URL = 'https://example.com';
+
+    // simulate server side by removing window object
+    windowSpy.mockImplementation(() => undefined);
 
     const results = getEnvConfig();
 
@@ -17,29 +27,23 @@ describe('getEnvConfig', () => {
     expect(results.SIMORGH_BASE_URL).toEqual('https://example.com');
   });
 
-  it('client side - should set window object values', () => {
-    process.env.SIMORGH_APP_ENV = 'test';
-    process.env.SIMORGH_BASE_URL = 'https://test.com';
-
-    global.window.SIMORGH_ENV_VARS = {
-      SIMORGH_APP_ENV: 'test',
-      SIMORGH_BASE_URL: 'https://test.com',
-    } as typeof global.window.SIMORGH_ENV_VARS;
-
-    expect(global.window.SIMORGH_ENV_VARS).toEqual({
-      SIMORGH_APP_ENV: 'test',
-      SIMORGH_BASE_URL: 'https://test.com',
-    });
-  });
-
   it('client side - should return values from "getEnvConfig"', () => {
     process.env.SIMORGH_APP_ENV = 'test';
     process.env.SIMORGH_BASE_URL = 'https://test.com';
 
-    global.window.SIMORGH_ENV_VARS = {
-      SIMORGH_APP_ENV: 'test',
-      SIMORGH_BASE_URL: 'https://test.com',
-    } as typeof global.window.SIMORGH_ENV_VARS;
+    // simulate client side by adding window object
+    windowSpy.mockImplementation(
+      () =>
+        ({
+          location: {
+            origin: 'https://example.com',
+          },
+          SIMORGH_ENV_VARS: {
+            SIMORGH_APP_ENV: 'test',
+            SIMORGH_BASE_URL: 'https://test.com',
+          },
+        } as Window),
+    );
 
     const results = getEnvConfig();
 
