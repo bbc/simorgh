87a4ad7fd71fda4cab7e2cfc0053bcb8add9e72c
Harvey Peachey <harvey.peachey@bbc.co.uk>
2024-03-06 18:01:32 +0000

    adds typescript converted placeholder component

---
+++ src/app/components/MediaLoader/Placeholder/index.tsx
@@ -0,0 +1,77 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+import PlayButton from '#psammead/psammead-play-button/src';
+import NO_JS_CLASSNAME from '#app/lib/noJs.const';
+import { useContext } from 'react';
+import { ServiceContext } from '#app/contexts/ServiceContext';
+import Guidance from '#app/legacy/components/MediaPlayer/Guidance';
+import Image from '../../Image';
+import styles from './index.styles';
+
+interface Props {
+  onClick: React.MouseEventHandler<HTMLDivElement>;
+  src: string;
+  srcSet?: string;
+  mediaInfo: {
+    title: string;
+    datetime?: string;
+    duration?: string;
+    durationSpoken?: string;
+    type?: 'audio' | 'video';
+    guidanceMessage?: string;
+  };
+  noJsMessage: string;
+}
+
+const MediaPlayerPlaceholder = ({
+  onClick,
+  src,
+  srcSet,
+  mediaInfo,
+  noJsMessage,
+}: Props) => {
+  const {
+    title,
+    datetime,
+    duration,
+    durationSpoken,
+    type = 'video',
+    guidanceMessage,
+  } = mediaInfo;
+
+  const { service } = useContext(ServiceContext);
+
+  return (
+    <div
+      onClick={onClick}
+      css={styles.placeholder}
+      data-e2e="media-player__placeholder"
+      aria-hidden="true"
+    >
+      <Guidance
+        css={styles.guidance}
+        service={service}
+        guidanceMessage={guidanceMessage}
+        noJsClassName={NO_JS_CLASSNAME}
+        noJsMessage={noJsMessage}
+      />
+      <PlayButton
+        css={styles.playButton}
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        onClick={() => {}}
+        title={title}
+        service={service}
+        datetime={datetime}
+        duration={duration}
+        durationSpoken={durationSpoken}
+        type={type}
+        guidanceMessage={guidanceMessage}
+        className="focusIndicatorRemove"
+      />
+
+      <Image alt="" src={src} srcSet={srcSet} />
+    </div>
+  );
+};
+
+export default MediaPlayerPlaceholder;
