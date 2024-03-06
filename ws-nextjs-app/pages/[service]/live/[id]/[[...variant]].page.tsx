441e33398c1b68603082677e95bf2cfbca722b19
Aaron Moore <aaron.moore@bbc.co.uk>
2024-03-06 15:12:34 +0000

    Add `isLite` prop to Next.js app

--- ws-nextjs-app/pages/[service]/live/[id]/[[...variant]].page.tsx
+++ ws-nextjs-app/pages/[service]/live/[id]/[[...variant]].page.tsx
@@ -22,6 +22,7 @@ import certsRequired from '#app/routes/utils/certsRequired';
 import { OK } from '#app/lib/statusCodes.const';
 import sendCustomMetric from '#server/utilities/customMetrics';
 import { NON_200_RESPONSE } from '#server/utilities/customMetrics/metrics.const';
+import isLitePath from '#app/routes/utils/isLitePath';
 import getAgent from '../../../../utilities/undiciAgent';
 
 import LivePageLayout from './LivePageLayout';
@@ -126,6 +127,8 @@ export const getServerSideProps: GetServerSideProps = async context => {
   const { headers: reqHeaders } = context.req;
 
   const isApp = isAppPath(context.resolvedUrl);
+  const isLite =
+    isLitePath(context.resolvedUrl) || reqHeaders?.['save-data'] === 'on';
 
   if (!isValidPageNumber(page)) {
     context.res.statusCode = 404;
@@ -141,6 +144,7 @@ export const getServerSideProps: GetServerSideProps = async context => {
       props: {
         bbcOrigin: reqHeaders['bbc-origin'] || null,
         isApp,
+        isLite,
         isNextJs: true,
         service,
         status: 404,
@@ -187,6 +191,7 @@ export const getServerSideProps: GetServerSideProps = async context => {
       error: data?.error || null,
       id,
       isApp,
+      isLite,
       isAmp: false,
       isNextJs: true,
       page: page || null,
