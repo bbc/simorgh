3037f0855a92217c2043505904a3bd6ff79151c4
Aaron Moore <aaron.moore@bbc.co.uk>
2024-03-11 19:58:31 +0000

    Update index.test.ts

--- src/server/utilities/litePageTransform/index.test.ts
+++ src/server/utilities/litePageTransform/index.test.ts
@@ -1,22 +1,107 @@
 import litePageTransform from '.';
 
+let helmetMetaTags: React.ReactElement[] = [];
+let helmetScriptTags: React.ReactElement[] = [];
+let helmetLinkTags: React.ReactElement[] = [];
+
 describe('litePageTransform', () => {
-  it('should return the correct object', () => {
-    const html = '<html><head></head><body><style></style></body></html>';
-    const helmetMetaTags = [];
-    const helmetScriptTags = [];
-    const helmetLinkTags = [];
+  beforeEach(() => {
+    helmetMetaTags = [];
+    helmetScriptTags = [];
+    helmetLinkTags = [];
+  });
+
+  it('should remove images with rel="preload" from the Helmet Link tags', () => {
+    const html = '<html><head></head><body></body></html>';
+    helmetLinkTags = [
+      {
+        key: '0',
+        type: 'link',
+        props: { rel: 'preload', as: 'image', href: '/image.jpg' },
+      },
+      {
+        key: '1',
+        type: 'link',
+        props: { rel: 'manifest', href: '/manifest.json' },
+      },
+    ];
+
     const result = litePageTransform({
       html,
       helmetMetaTags,
       helmetScriptTags,
       helmetLinkTags,
     });
-    expect(result).toEqual({
-      liteHtml: '<html><head></head><body></body></html>',
-      liteHelmetLinkTags: [],
-      liteHelmetMetaTags: [],
-      liteHelmetScriptTags: [],
+
+    expect(result.liteHelmetLinkTags).toEqual([
+      {
+        key: '1',
+        type: 'link',
+        props: { rel: 'manifest', href: '/manifest.json' },
+      },
+    ]);
+  });
+
+  it('should remove script tags with src="vendor/require" from the Helmet Script tags', () => {
+    const html = '<html><head></head><body></body></html>';
+    helmetScriptTags = [
+      {
+        key: '0',
+        type: 'script',
+        props: { src: '/vendor/require.js' },
+      },
+      {
+        key: '1',
+        type: 'script',
+        props: { src: '/analytics.js' },
+      },
+    ];
+
+    const result = litePageTransform({
+      html,
+      helmetMetaTags,
+      helmetScriptTags,
+      helmetLinkTags,
     });
+
+    expect(result.liteHelmetScriptTags).toEqual([
+      {
+        key: '1',
+        type: 'script',
+        props: { src: '/analytics.js' },
+      },
+    ]);
+  });
+
+  it('should remove all style tags and inline style attributes', () => {
+    const html =
+      '<html><head><style></style></head><body><div style="padding-top:10px;"></div></body></html>';
+
+    const result = litePageTransform({
+      html,
+      helmetMetaTags,
+      helmetScriptTags,
+      helmetLinkTags,
+    });
+
+    expect(result.liteHtml).toEqual(
+      '<html><head></head><body><div></div></body></html>',
+    );
+  });
+
+  it('should remove all class names except those in defined in "classNamesToKeep" array', () => {
+    const html =
+      '<html><head></head><body><div class="visuallyHiddenText"></div><div class="skipLink"></div><div class="lite-switcher"></div><div class="analytics-pixel"></div><div class="extra-class"></div></body></html>';
+
+    const result = litePageTransform({
+      html,
+      helmetMetaTags,
+      helmetScriptTags,
+      helmetLinkTags,
+    });
+
+    expect(result.liteHtml).toEqual(
+      '<html><head></head><body><div class="visuallyHiddenText"></div><div class="skipLink"></div><div class="lite-switcher"></div><div class="analytics-pixel"></div><div></div></body></html>',
+    );
   });
 });
