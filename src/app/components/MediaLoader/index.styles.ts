eac270dabc831d6726943c09e22766a718b6821f
Aaron Moore <aaron.moore@bbc.co.uk>
2024-03-13 12:51:21 +0000

    Prevent content jumping on media player load

--- src/app/components/MediaLoader/index.styles.ts
+++ src/app/components/MediaLoader/index.styles.ts
@@ -1,7 +1,12 @@
-import { css } from '@emotion/react';
+import { css, Theme } from '@emotion/react';
 
 export default {
   figure: css({
     margin: 0,
   }),
+  mediaContainer: ({ palette }: Theme) =>
+    css({
+      backgroundColor: palette.EBON,
+      aspectRatio: '16 / 9',
+    }),
 };
