e6c174035cdd77b1dc72f626f7c3c41c802b6799
Aaron Moore <aaron.moore@bbc.co.uk>
2024-03-13 10:55:58 +0000

    unit and snapshot updates

--- ws-nextjs-app/pages/[service]/live/[id]/Post/index.test.tsx
+++ ws-nextjs-app/pages/[service]/live/[id]/Post/index.test.tsx
@@ -164,7 +164,7 @@ describe('Post', () => {
       });
 
       expect(
-        container.querySelector('[data-e2e="media-player__placeholder"]'),
+        container.querySelector('[data-e2e="media-loader__placeholder"]'),
       ).toBeInTheDocument();
     });
   });
