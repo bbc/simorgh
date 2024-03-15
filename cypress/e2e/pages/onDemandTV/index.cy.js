cd0b8250f6ff3122b7e0331ae0ee43df2b0849d6
Karina Thomas <karina.thomas@bbc.co.uk>
2024-03-15 13:30:46 +0000

    On Demand TV tidy up

--- cypress/e2e/pages/onDemandTV/index.cy.js
+++ cypress/e2e/pages/onDemandTV/index.cy.js
@@ -1,4 +1,5 @@
 import config from '../../../support/config/services';
+import envs from '../../../support/config/envs';
 import getPaths from '../../../support/helpers/getPaths';
 import serviceHasPageType from '../../../support/helpers/serviceHasPageType';
 import testsForCanonicalOnly from './testsForCanonicalOnly';
@@ -13,7 +14,7 @@ Object.keys(config)
     const { variant, name: service } = config[serviceId];
     const paths = getPaths(serviceId, pageType);
     paths.forEach(currentPath => {
-      describe(`${pageType} - ${currentPath}`, () => {
+      describe(`${pageType} - ${envs.baseUrl}${currentPath}`, () => {
         beforeEach(() => {
           Cypress.env('currentPath', currentPath);
           const newPath = `${Cypress.env(
@@ -36,7 +37,7 @@ Object.keys(config)
     paths
       .map(path => `${path}.amp`)
       .forEach(currentPath => {
-        describe(`${pageType} - ${currentPath}`, () => {
+        describe(`${pageType} - ${envs.baseUrl}${currentPath}`, () => {
           beforeEach(() => {
             Cypress.env('currentPath', currentPath);
             visitPage(currentPath, pageType);
