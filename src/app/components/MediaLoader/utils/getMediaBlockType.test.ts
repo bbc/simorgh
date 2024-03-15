4dbef4e7a04c260c8b48f4959696caa8bc1d4dc6
Aaron Moore <aaron.moore@bbc.co.uk>
2024-03-15 20:20:35 +0000

    Update getMediaBlockType.test.ts

--- src/app/components/MediaLoader/utils/getMediaBlockType.test.ts
+++ src/app/components/MediaLoader/utils/getMediaBlockType.test.ts
@@ -6,12 +6,18 @@ describe('getMediaBlockType', () => {
   it('Should return type "aresMedia" when provided aresMediaBlocks.', () => {
     const result = getMediaBlockType(aresMediaBlocks as MediaBlock[]);
 
-    expect(result).toStrictEqual('aresMedia');
+    expect(result).toEqual('aresMedia');
   });
 
   it('Should return type "clipMedia" when provided clipMediaBlocks.', () => {
     const result = getMediaBlockType(clipMediaBlocks as MediaBlock[]);
 
-    expect(result).toStrictEqual('clipMedia');
+    expect(result).toEqual('clipMedia');
+  });
+
+  it('Should return type null when the media type cannot be determined', () => {
+    const result = getMediaBlockType([] as MediaBlock[]);
+
+    expect(result).toEqual(null);
   });
 });
