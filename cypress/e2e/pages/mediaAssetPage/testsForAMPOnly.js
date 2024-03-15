714b6f45d5c257198ea6558be9b85f1729e46bd3
Karina Thomas <karina.thomas@bbc.co.uk>
2024-03-15 13:20:27 +0000

    MAP tidy up

--- cypress/e2e/pages/mediaAssetPage/testsForAMPOnly.js
+++ cypress/e2e/pages/mediaAssetPage/testsForAMPOnly.js
@@ -6,42 +6,33 @@ import { getEmbedUrl, hasMedia } from './helpers';
 // For testing features that may differ across services but share a common logic e.g. translated strings.
 export const testsThatFollowSmokeTestConfigForAMPOnly = ({
   service,
-  pageType,
   variant,
 }) => {
-  describe(`testsThatFollowSmokeTestConfigForAMPOnly for ${service} ${pageType}`, () => {
-    describe('Media Player', () => {
-      const language = appConfig[config[service].name][variant].lang;
+  describe('Media Player', () => {
+    const language = appConfig[config[service].name][variant].lang;
 
-      it('should render an iframe with a valid URL', () => {
-        if (!`${Cypress.env('currentPath')}`.includes('/russian/av/')) {
-          cy.getPageData({ service, pageType: 'cpsAsset', variant }).then(
-            ({ body }) => {
-              const {
-                data: { article: jsonData },
-              } = body;
+    it('should render an iframe with a valid URL', () => {
+      if (!`${Cypress.env('currentPath')}`.includes('/russian/av/')) {
+        cy.getPageData({ service, pageType: 'cpsAsset', variant }).then(
+          ({ body }) => {
+            const {
+              data: { article: jsonData },
+            } = body;
 
-              if (hasMedia(jsonData)) {
-                const embedUrl = getEmbedUrl(jsonData, language, true);
+            if (hasMedia(jsonData)) {
+              const embedUrl = getEmbedUrl(jsonData, language, true);
 
-                cy.get(`amp-iframe[src="${embedUrl}"]`).should('be.visible');
-                cy.testResponseCodeAndTypeRetry({
-                  path: embedUrl,
-                  responseCode: 200,
-                  type: 'text/html',
-                  allowFallback: true,
-                });
-              } else {
-                cy.log(
-                  `No media on ${pageType} for ${Cypress.env('currentPath')}`,
-                );
-              }
-            },
-          );
-        } else {
-          cy.log('skipped test for cps russian map');
-        }
-      });
+              cy.get(`amp-iframe[src="${embedUrl}"]`).should('be.visible');
+              cy.testResponseCodeAndTypeRetry({
+                path: embedUrl,
+                responseCode: 200,
+                type: 'text/html',
+                allowFallback: true,
+              });
+            }
+          },
+        );
+      }
     });
   });
 };
--- cypress/e2e/pages/mediaAssetPage/testsForCanonicalOnly.js
+++ cypress/e2e/pages/mediaAssetPage/testsForCanonicalOnly.js
@@ -9,54 +9,46 @@ import envConfig from '../../../support/config/envs';
 // For testing features that may differ across services but share a common logic e.g. translated strings.
 export const testsThatFollowSmokeTestConfigForCanonicalOnly = ({
   service,
-  pageType,
   variant,
 }) => {
-  describe(`testsThatFollowSmokeTestConfigForCanonicalOnly for ${service} ${pageType}`, () => {
-    describe('Media Player', () => {
-      const language = appConfig[config[service].name][variant].lang;
+  describe('Media Player', () => {
+    const language = appConfig[config[service].name][variant].lang;
 
-      it('should render an iframe with a valid URL', () => {
-        if (!`${Cypress.env('currentPath')}`.includes('/russian/av/')) {
-          cy.getPageData({ service, pageType: 'cpsAsset', variant }).then(
-            ({ body }) => {
-              const {
-                data: { article: jsonData },
-              } = body;
+    it('should render an iframe with a valid URL', () => {
+      if (!`${Cypress.env('currentPath')}`.includes('/russian/av/')) {
+        cy.getPageData({ service, pageType: 'cpsAsset', variant }).then(
+          ({ body }) => {
+            const {
+              data: { article: jsonData },
+            } = body;
 
-              if (hasMedia(jsonData)) {
-                const embedUrl = getEmbedUrl(jsonData, language);
-                cy.log(embedUrl);
-                cy.get(`iframe[src*="${embedUrl}"]`).should('be.visible');
-                cy.testResponseCodeAndTypeRetry({
-                  path: embedUrl,
-                  responseCode: 200,
-                  type: 'text/html',
-                  allowFallback: true,
-                });
-              } else {
-                cy.log(
-                  `No media on ${pageType} for ${Cypress.env('currentPath')}`,
-                );
-              }
-            },
-          );
-        } else {
-          cy.log('skipped test for cps russian map');
-        }
-      });
+            if (hasMedia(jsonData)) {
+              const embedUrl = getEmbedUrl(jsonData, language);
+              cy.log(embedUrl);
+              cy.get(`iframe[src*="${embedUrl}"]`).should('be.visible');
+              cy.testResponseCodeAndTypeRetry({
+                path: embedUrl,
+                responseCode: 200,
+                type: 'text/html',
+                allowFallback: true,
+              });
+            }
+          },
+        );
+      }
     });
-    describe('Most Watched component', () => {
-      before(() => {
-        cy.getToggles(config[service].name);
-      });
-      // Allows constructing most watched path with variant
-      const serviceVariant = variant === 'default' ? '' : `/${variant}`;
 
-      const mostWatchedPath = `/${config[service].name}/mostwatched${serviceVariant}.json`;
-      it('should show/not show the Most Watched component if the toggle is enabled for the service', function test() {
-        // Find if the service has component enabled in toggles
-        if (service === 'persian' || service === 'amharic') {
+    if (service === 'persian' || service === 'amharic') {
+      describe('Most Watched component', () => {
+        before(() => {
+          cy.getToggles(config[service].name);
+        });
+        // Allows constructing most watched path with variant
+        const serviceVariant = variant === 'default' ? '' : `/${variant}`;
+
+        const mostWatchedPath = `/${config[service].name}/mostwatched${serviceVariant}.json`;
+        it('should show/not show the Most Watched component if the toggle is enabled for the service', () => {
+          // Find if the service has component enabled in toggles
           cy.fixture(`toggles/${config[service].name}.json`).then(toggles => {
             const mostWatchedIsEnabled = path(
               ['mostPopularMedia', 'enabled'],
@@ -79,14 +71,9 @@ export const testsThatFollowSmokeTestConfigForCanonicalOnly = ({
               cy.get('[data-e2e=most-watched-heading]').should('not.exist');
             }
           });
-        } else {
-          cy.log(
-            'Skipping this test on services that are not Persian or Amharic',
-          );
-        }
-      });
-      it('should show correct number of items', function test() {
-        if (service === 'persian' || service === 'amharic') {
+        });
+
+        it('should show correct number of items', () => {
           cy.fixture(`toggles/${config[service].name}.json`).then(toggles => {
             const mostWatchedIsEnabled = path(
               ['mostPopularMedia', 'enabled'],
@@ -127,13 +114,10 @@ export const testsThatFollowSmokeTestConfigForCanonicalOnly = ({
               }
             });
           });
-        } else {
-          cy.log(
-            'Skipping this test on services that are not Persian or Amharic',
-          );
-        }
+        });
       });
-    });
+    }
+
     if (appToggles.chartbeatAnalytics.enabled && envConfig.chartbeatEnabled) {
       describe('Chartbeat', () => {
         it('should have a script with correct src', () => {
