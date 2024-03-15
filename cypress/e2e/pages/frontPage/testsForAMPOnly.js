043f0eb3a0aef52ca6ae90ef16ded606f486c46e
Karina Thomas <karina.thomas@bbc.co.uk>
2024-03-15 12:53:23 +0000

    Remove unnecessary describe

--- cypress/e2e/pages/frontPage/testsForAMPOnly.js
+++ cypress/e2e/pages/frontPage/testsForAMPOnly.js
@@ -2,13 +2,12 @@
 import runAMPAdsTests from '../../../support/helpers/adsTests/testsForAMPOnly';
 
 // For testing features that may differ across services but share a common logic e.g. translated strings.
-export const testsThatFollowSmokeTestConfigForAMPOnly = ({ service }) =>
-  describe(`testsThatFollowSmokeTestConfigForAMPOnly`, () => {
-    it('should contain an amp-img', () => {
-      cy.get('li').should('be.visible').find('amp-img').should('be.visible');
-    });
-
-    if (Cypress.env('APP_ENV') === 'local') {
-      runAMPAdsTests({ service });
-    }
+export const testsThatFollowSmokeTestConfigForAMPOnly = ({ service }) => {
+  it('should contain an amp-img', () => {
+    cy.get('li').should('be.visible').find('amp-img').should('be.visible');
   });
+
+  if (Cypress.env('APP_ENV') === 'local') {
+    runAMPAdsTests({ service });
+  }
+};
