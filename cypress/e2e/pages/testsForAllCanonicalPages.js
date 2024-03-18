8879d3b296e13e3e8195b5d10b76dd5b68d61cb1
Karina Thomas <karina.thomas@bbc.co.uk>
2024-03-18 12:23:52 +0000

    Tidy up scrollable nav tests

--- cypress/e2e/pages/testsForAllCanonicalPages.js
+++ cypress/e2e/pages/testsForAllCanonicalPages.js
@@ -1,10 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 import envConfig from '../../support/config/envs';
-import config from '../../support/config/services';
 
 // For testing features that may differ across services but share a common logic e.g. translated strings.
 export const testsThatFollowSmokeTestConfigForAllCanonicalPages = ({
-  service,
   pageType,
 }) => {
   if (pageType !== 'errorPage404') {
@@ -21,28 +19,5 @@ export const testsThatFollowSmokeTestConfigForAllCanonicalPages = ({
         },
       );
     }
-  }
-
-  const serviceName = config[service].name;
-  // limit number of tests to 2 services for navigation toggling
-  const testMobileNav = serviceName === 'mundo' || serviceName === 'persian';
-
-  if (testMobileNav) {
-    describe('Header Tests', () => {
-      it('should show dropdown menu and hide scrollable menu when menu button is clicked', () => {
-        cy.viewport(320, 480);
-        cy.get('nav').find('[data-e2e="scrollable-nav"]').should('be.visible');
-
-        cy.get('nav')
-          .find('[data-e2e="dropdown-nav"] ul')
-          .should('not.be.visible');
-
-        cy.get('nav button').click();
-
-        cy.get('nav').find('[data-e2e="scrollable-nav"]').should('not.exist');
-
-        cy.get('nav').find('[data-e2e="dropdown-nav"] ul').should('be.visible');
-      });
-    });
   }
 };
--- cypress/support/helpers/runTestsForPage.js
+++ cypress/support/helpers/runTestsForPage.js
@@ -2,7 +2,6 @@ import config from '../config/services';
 import envConfig from '../config/envs';
 import shouldSmokeTest from './shouldSmokeTest';
 import { testsThatAlwaysRunForAllPages } from '../../e2e/pages/testsForAllPages';
-import { testsThatFollowSmokeTestConfigForAllAMPPages } from '../../e2e/pages/testsForAllAMPPages';
 import { testsThatFollowSmokeTestConfigForAllCanonicalPages } from '../../e2e/pages/testsForAllCanonicalPages';
 
 import getPaths from './getPaths';
@@ -120,7 +119,6 @@ const runTestsForPage = ({
 
           // This runs most tests but only on Service:PageType combinations with smoke enabled
           if (shouldSmokeTest(pageType, service)) {
-            testsThatFollowSmokeTestConfigForAllAMPPages(testArgs);
             // Page specific tests
             testsThatFollowSmokeTestConfig(testArgs);
             testsThatFollowSmokeTestConfigForAMPOnly(testArgs);
