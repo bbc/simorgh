8c911aa8c87f1c90e401d02fe01ecfc3bd3938ea
Isabella Mitchell <i.mitchell.0707@gmail.com>
2024-03-20 11:32:13 +0000

    wSTEAM1-837: Reduce bottom padding for live pages to align with designs

--- ws-nextjs-app/pages/[service]/live/[id]/Post/styles.tsx
+++ ws-nextjs-app/pages/[service]/live/[id]/Post/styles.tsx
@@ -102,8 +102,7 @@ export default {
     }),
   bodyMedia: ({ spacings, mq }: Theme) =>
     css({
-      paddingLeft: `${spacings.FULL}rem`,
-      paddingRight: `${spacings.FULL}rem`,
+      padding: `0 ${spacings.FULL}rem ${spacings.DOUBLE}rem`,
       [mq.GROUP_2_MIN_WIDTH]: {
         paddingLeft: `${spacings.DOUBLE}rem`,
         paddingRight: `${spacings.DOUBLE}rem`,
