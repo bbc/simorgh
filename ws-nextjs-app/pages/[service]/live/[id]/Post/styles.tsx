ada933b8ba1ee71270d767f52608e75a7b2adb2a
Isabella Mitchell <i.mitchell.0707@gmail.com>
2024-03-20 10:50:47 +0000

    WSTEAM1-847: Allow default paddingBottom on larger screen sizes

--- ws-nextjs-app/pages/[service]/live/[id]/Post/styles.tsx
+++ ws-nextjs-app/pages/[service]/live/[id]/Post/styles.tsx
@@ -109,7 +109,8 @@ export default {
         paddingRight: `${spacings.DOUBLE}rem`,
       },
       [mq.GROUP_4_MIN_WIDTH]: {
-        padding: 0,
+        paddingLeft: 0,
+        paddingRight: 0,
       },
     }),
 };
