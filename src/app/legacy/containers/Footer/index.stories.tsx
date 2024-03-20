ea87aafac8a3ddb009972e52ab50c002bb7bfba7
Karina Thomas <karina.thomas@bbc.co.uk>
2024-03-20 08:10:25 +0000

    - Migrate Footer stories to TS - Adding extra stories for Ads, Amp & HindiCollectiveNewsroomPublication - VIsual snapshots at all breakpoints

---
+++ src/app/legacy/containers/Footer/index.stories.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withServicesKnob } from '../../psammead/psammead-storybook-helpers/src';
+import { ServiceContextProvider } from '../../../contexts/ServiceContext';
+import Footer from '.';
+import ThemeProvider from '../../../components/ThemeProvider';
+import { StoryProps } from '../../../models/types/storybook';
+import { customViewports } from '../../../../../.storybook/preview';
+import { RequestContextProvider } from '../../../contexts/RequestContext';
+
+interface Props extends StoryProps {
+  isAmp?: boolean;
+  withAds?: boolean;
+}
+
+const Component = ({
+  service,
+  variant,
+  isAmp = false,
+  withAds = false,
+}: Props) => (
+  <ThemeProvider service={service} variant={variant}>
+    <RequestContextProvider
+      isAmp={isAmp}
+      isApp={false}
+      pageType={undefined}
+      pathname=""
+      service={service}
+      showAdsBasedOnLocation={withAds}
+    >
+      <ServiceContextProvider service={service} variant={variant}>
+        <Footer />
+      </ServiceContextProvider>
+    </RequestContextProvider>
+  </ThemeProvider>
+);
+
+export default {
+  title: 'New Components/Footer',
+  Component,
+  decorators: [withKnobs, withServicesKnob()],
+  parameters: {
+    chromatic: {
+      viewports: customViewports,
+    },
+  },
+};
+
+export const Example = ({ service, variant }) => (
+  <Component service={service} variant={variant} />
+);
+
+export const WithAdsEnabled = ({ service, variant }) => (
+  <Component service={service} variant={variant} withAds />
+);
+
+export const AMP = ({ service, variant }) => (
+  <Component service={service} variant={variant} isAmp />
+);
+
+export const HindiCollectiveNewsroomPublication = ({ variant }) => (
+  <Component service="hindi" variant={variant} />
+);
