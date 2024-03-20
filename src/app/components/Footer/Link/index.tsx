6214564b6b36b22bfdaa26176126aa576acee8c5
Karina Thomas <karina.thomas@bbc.co.uk>
2024-03-20 13:02:29 +0000

    Convert Link component within Footer component

---
+++ src/app/components/Footer/Link/index.tsx
@@ -0,0 +1,48 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
+import { useState, useEffect } from 'react';
+import onClient from '#lib/utilities/onClient';
+import styles from './index.styles';
+
+interface LinkProps {
+  href: string;
+  text: string;
+  lang?: string;
+  inline?: boolean;
+  onClick?: () => void;
+  onlyShowIfJSenabled?: boolean;
+}
+
+export default ({
+  text,
+  href,
+  inline = false,
+  lang,
+  onClick,
+  onlyShowIfJSenabled = false,
+}: LinkProps) => {
+  const [isVisible, setVisible] = useState(onlyShowIfJSenabled !== true);
+
+  useEffect(() => {
+    if (onlyShowIfJSenabled && onClient()) {
+      setVisible(true);
+    }
+  }, [onlyShowIfJSenabled]);
+
+  let display = inline ? 'inline' : 'block';
+  if (!isVisible) {
+    display = 'none';
+  }
+
+  return (
+    <a
+      css={[styles.link, css({ display: `${display}` })]}
+      lang={lang}
+      href={href}
+      className="focusIndicatorInvert"
+      onClick={onClick}
+    >
+      {text}
+    </a>
+  );
+};
