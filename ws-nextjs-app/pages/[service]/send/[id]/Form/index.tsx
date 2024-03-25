0015cd965cd9c7c9cf8c7c757d37124dbf09c430
Aaron Moore <aaron.moore@bbc.co.uk>
2024-03-25 22:02:45 +0000

    Mock NextRouter for Storybook

--- ws-nextjs-app/pages/[service]/send/[id]/Form/index.tsx
+++ ws-nextjs-app/pages/[service]/send/[id]/Form/index.tsx
@@ -1,4 +1,5 @@
 /** @jsx jsx */
+import React from 'react';
 import { jsx } from '@emotion/react';
 import { useFormContext } from '../FormContext';
 import { Field } from '../types';
--- ws-nextjs-app/pages/[service]/send/[id]/send.stories.tsx
+++ ws-nextjs-app/pages/[service]/send/[id]/send.stories.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import PageLayoutWrapper from '#app/components/PageLayoutWrapper';
+import { NextRouter } from 'next/router';
+import { RouterContext } from 'next/dist/shared/lib/router-context.shared-runtime';
 import postFixture from './fixture';
 import UGCPage from './UGCPageLayout';
 import { PageProps } from './types';
 
+const NextRouterWrapper = ({ children }: PropsWithChildren) => (
+  <RouterContext.Provider
+    value={{ query: { id: '123' } } as unknown as NextRouter}
+  >
+    {children}
+  </RouterContext.Provider>
+);
+
 const Component = () => (
-  // @ts-expect-error partial data required for storybook
-  <PageLayoutWrapper pageData={postFixture} status={200}>
-    <UGCPage pageData={postFixture as PageProps['pageData']} />
-  </PageLayoutWrapper>
+  <NextRouterWrapper>
+    <PageLayoutWrapper
+      // @ts-expect-error partial data required for storybook
+      pageData={postFixture}
+      status={200}
+    >
+      <UGCPage pageData={postFixture as PageProps['pageData']} />
+    </PageLayoutWrapper>
+  </NextRouterWrapper>
 );
 
 export default {
