296889a4952c76cf1614476df78034e2e6dfcf58
Aaron Moore <aaron.moore@bbc.co.uk>
2024-03-25 16:44:09 +0000

    Update index.tsx

--- ws-nextjs-app/pages/[service]/send/[id]/SubmitButton/index.tsx
+++ ws-nextjs-app/pages/[service]/send/[id]/SubmitButton/index.tsx
@@ -1,4 +1,27 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
+import nodeLogger from '#lib/logger.node';
+import { DATA_REQUEST_RECEIVED } from '#app/lib/logger.const';
+
+const logger = nodeLogger(__filename);
+
+export const handleSubmit = async (event: FormEvent) => {
+  event.preventDefault();
+
+  // FULL IMPLEMENTATION TO BE DONE IN LATER TICKET
+  const validData = { surname: 'BBC TEST NAME' };
+  const fetchRequest = await fetch('myUrl.com', {
+    method: 'POST',
+    body: JSON.stringify(validData),
+  });
+
+  const response = fetchRequest.status;
+
+  // handle response
+  logger.info(
+    DATA_REQUEST_RECEIVED,
+    `HANDLE RESPONSE HERE RESPONSE ${response}`,
+  );
+};
 
 const Submit = () => {
   const translation = 'Submit';
