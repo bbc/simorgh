31ea6933630f141c19529d0337a6a6484a7aa8a6
Shayne Marc Enzo Ahchoon <shayne.ahchoon@bbc.co.uk>
2024-03-26 17:18:31 +0000

    WSTEAM1-890: Update

--- ws-nextjs-app/pages/[service]/send/[id]/[[...variant]].page.tsx
+++ ws-nextjs-app/pages/[service]/send/[id]/[[...variant]].page.tsx
@@ -1,30 +1,75 @@
 import { GetServerSideProps } from 'next';
 import nodeLogger from '#lib/logger.node';
-import { BFF_FETCH_ERROR, DATA_REQUEST_RECEIVED } from '#app/lib/logger.const';
+import { BFF_FETCH_ERROR } from '#app/lib/logger.const';
 import { FetchError } from '#app/models/types/fetch';
 import PageDataParams from '#models/types/pageDataParams';
 import mundoFixture from '#data/mundo/send/test2qq3x8vt.json';
+import constructPageFetchUrl from '#app/routes/utils/constructPageFetchUrl';
+import getEnvironment from '#app/routes/utils/getEnvironment';
+import fetchPageData from '#app/routes/utils/fetchPageData';
+import certsRequired from '#app/routes/utils/certsRequired';
 import UGCPageLayout from './UGCPageLayout';
-import { FetchParameters } from './types';
+import getAgent from '../../../../utilities/undiciAgent';
 
 const logger = nodeLogger(__filename);
 
-const fetchData = ({ id, service, variant }: FetchParameters) => {
-  logger.info(
-    DATA_REQUEST_RECEIVED,
-    `MAKE REQUEST HERE ${id} ${service} ${variant}`,
-  );
+const fetchData = async ({
+  id,
+  service,
+  variant,
+  rendererEnv,
+}: PageDataParams) => {
+  const pathname = `${id}${rendererEnv ? `?renderer_env=${rendererEnv}` : ''}`;
+  const livePageUrl = constructPageFetchUrl({
+    pageType: 'ugcForm',
+    pathname,
+    service,
+    variant,
+  });
+
+  const env = getEnvironment(pathname);
+  const optHeaders = { 'ctx-service-env': env };
+
+  const agent = certsRequired(pathname) ? await getAgent() : null;
+
+  let pageStatus;
+  let pageJson;
+  let errorMessage;
+
+  const path = livePageUrl.toString();
+
+  try {
+    // @ts-expect-error Due to jsdoc inference, and no TS within fetchPageData
+    const check = await fetchPageData({
+      path,
+      agent,
+      optHeaders,
+    });
+  } catch (error) {
+    console.log('CHECK3', error);
+  }
 
   return mundoFixture;
 };
 
 export const getServerSideProps: GetServerSideProps = async context => {
-  const { id, service, variant } = context.query as PageDataParams;
+  const {
+    id,
+    service,
+    variant,
+    renderer_env: rendererEnv,
+  } = context.query as PageDataParams;
 
   let pageData = null;
 
   try {
-    const data = fetchData({ id, service, variant });
+    const data = await fetchData({
+      id,
+      service,
+      variant,
+      rendererEnv,
+      resolvedUrl: context.resolvedUrl,
+    });
     pageData = data;
   } catch (error) {
     const { message, status } = error as FetchError;
@@ -45,7 +90,7 @@ export const getServerSideProps: GetServerSideProps = async context => {
       pageType: 'ugc',
       pathname: null,
       service,
-      status: 200,
+      status: 500,
       timeOnServer: Date.now(), // TODO: check if needed?
     },
   };
