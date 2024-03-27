2c07eda6bb1a45dd7c6a1c4149943c886c1ecc11
Aaron Moore <aaron.moore@bbc.co.uk>
2024-03-27 17:31:35 +0000

    WSTEAM1-851 - UGC form state management (#11425)

--- ws-nextjs-app/pages/[service]/send/[id]/send.stories.tsx
+++ ws-nextjs-app/pages/[service]/send/[id]/send.stories.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import PageLayoutWrapper from '#app/components/PageLayoutWrapper';
+import { NextRouter } from 'next/router';
+import { RouterContext } from 'next/dist/shared/lib/router-context.shared-runtime';
 import mundoFormFixture from '#data/mundo/send/test2qq3x8vt.json';
 import UGCPage from './UGCPageLayout';
+import { PageProps } from './types';
+
+const NextRouterWrapper = ({ children }: PropsWithChildren) => (
+  <RouterContext.Provider
+    value={{ query: { id: '123' } } as unknown as NextRouter}
+  >
+    {children}
+  </RouterContext.Provider>
+);
 
 const Component = () => (
-  // @ts-expect-error partial data required for storybook
-  <PageLayoutWrapper pageData={mundoFormFixture} status={200}>
-    <UGCPage pageData={mundoFormFixture} />
-  </PageLayoutWrapper>
+  <NextRouterWrapper>
+    <PageLayoutWrapper
+      // @ts-expect-error partial data required for storybook
+      pageData={mundoFormFixture}
+      status={200}
+    >
+      <UGCPage pageData={mundoFormFixture as PageProps['pageData']} />
+    </PageLayoutWrapper>
+  </NextRouterWrapper>
 );
 
 export default {
