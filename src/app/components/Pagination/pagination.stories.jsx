7b97111ee331831f3dd87fcadffbbf436229006c
Aaron Moore <aaron.moore@bbc.co.uk>
2024-03-31 19:13:54 +0100

    Update pagination.stories.jsx

--- src/app/components/Pagination/pagination.stories.jsx
+++ src/app/components/Pagination/pagination.stories.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { withKnobs, number } from '@storybook/addon-knobs';
-import { withServicesKnob } from '#psammead/psammead-storybook-helpers/src';
 import { ServiceContextProvider } from '../../contexts/ServiceContext';
 import Pager from '.';
 import ThemeProvider from '../ThemeProvider';
 
 // eslint-disable-next-line react/prop-types
-const Component = ({ service, variant }) => {
+const Component = ({
+  service = 'news',
+  variant = 'default',
+  activePage,
+  pageCount,
+}) => {
   return (
     <ThemeProvider service={service}>
       <ServiceContextProvider service={service} variant={variant}>
-        <Pager
-          activePage={number('Active Page', 5, { min: 1, max: 100 })}
-          pageCount={number('PageCount', 10, { min: 2, max: 100 })}
-        />
+        <Pager activePage={activePage} pageCount={pageCount} />
       </ServiceContextProvider>
     </ThemeProvider>
   );
@@ -22,8 +22,27 @@ const Component = ({ service, variant }) => {
 export default {
   title: 'Topic/Pager',
   Component,
-  decorators: [withKnobs, withServicesKnob()],
   parameters: { chromatic: { disable: true } },
+  args: {
+    activePage: 5,
+    pageCount: 10,
+  },
+  argTypes: {
+    activePage: {
+      control: {
+        type: 'number',
+        min: 1,
+        max: 100,
+      },
+    },
+    pageCount: {
+      control: {
+        type: 'number',
+        min: 2,
+        max: 100,
+      },
+    },
+  },
 };
 
 export const Pagination = Component;
