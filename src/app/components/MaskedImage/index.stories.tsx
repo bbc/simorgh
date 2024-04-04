15b00aa0f582c451f0a407b35a7b0ea3236b5371
“LilyL0u” <lily.higham@bbc.co.uk>
2024-04-04 14:51:17 +0100

    one story for rtl and ltr, with services knob

--- src/app/components/MaskedImage/index.stories.tsx
+++ src/app/components/MaskedImage/index.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StoryProps } from '#app/models/types/storybook';
 import { ServiceContextProvider } from '#app/contexts/ServiceContext';
+import withServicesKnob from '#app/legacy/psammead/psammead-storybook-helpers/src/withServicesKnob';
+import { withKnobs } from '@storybook/addon-knobs';
 import ThemeProvider from '../ThemeProvider';
 import MaskedImage from '.';
 import metadata from './metadata.json';
@@ -11,56 +13,42 @@ interface Props extends StoryProps {
   imageWidth: number;
 }
 
-export default {
-  title: 'New Components/Masked Image',
-  Component: MaskedImage,
-  parameters: {
-    chromatic: { disable: true },
-    metadata,
-  },
-};
-
-const Component = ({
-  service,
-  variant,
-  imageUrl,
-  imageUrlTemplate,
-  imageWidth,
-}: Props) => {
+const Component = ({ service, variant }: Props) => {
   return (
     <ThemeProvider service={service} variant={variant}>
       <ServiceContextProvider service={service} variant={variant}>
         <MaskedImage
-          imageUrl={imageUrl}
-          imageUrlTemplate={imageUrlTemplate}
-          imageWidth={imageWidth}
+          imageUrl="https://ichef.bbci.co.uk/ace/standard/480/cpsdevpb/1d5b/test/5f969ec0-c4d8-11ed-8319-9b394d8ed0dd.jpg"
+          imageUrlTemplate="https://ichef.bbci.co.uk/ace/standard/{width}/cpsdevpb/1d5b/test/5f969ec0-c4d8-11ed-8319-9b394d8ed0dd.jpg"
+          imageWidth={660}
         />
       </ServiceContextProvider>
     </ThemeProvider>
   );
 };
 
-const imageUrl =
-  'https://ichef.bbci.co.uk/ace/standard/480/cpsdevpb/1d5b/test/5f969ec0-c4d8-11ed-8319-9b394d8ed0dd.jpg';
-const imageWidth = 660;
-const imageUrlTemplate =
-  'https://ichef.bbci.co.uk/ace/standard/{width}/cpsdevpb/1d5b/test/5f969ec0-c4d8-11ed-8319-9b394d8ed0dd.jpg';
-
-export const MaskedImageRTL = () => (
-  <Component
-    imageUrl={imageUrl}
-    imageUrlTemplate={imageUrlTemplate}
-    imageWidth={imageWidth}
-    service="afaanoromoo"
-    variant="default"
-  />
-);
-export const MaskedImageLTR = () => (
-  <Component
-    imageUrl={imageUrl}
-    imageUrlTemplate={imageUrlTemplate}
-    imageWidth={imageWidth}
-    service="persian"
-    variant="default"
-  />
-);
+export default {
+  title: 'New Components/Masked Image',
+  Component,
+  decorators: [withKnobs, withServicesKnob()],
+  parameters: {
+    chromatic: { disable: true },
+    metadata,
+  },
+};
+// const Component = ({ service, variant, text = '', longText = '' }: Props) => {
+//   return (
+//     <ThemeProvider service={service} variant={variant}>
+//       <ServiceContextProvider service={service} variant={variant}>
+//         <MessageBanner
+//           heading={text}
+//           description={longText}
+//           link="https://www.bbc.co.uk/ws/languages"
+//           linkText={text}
+//           image="https://ichef.bbci.co.uk/ace/standard/raw/cpsprodpb/e329/live/0a700dd0-7cb3-11ee-a503-4588075e3427.png"
+//         />
+//       </ServiceContextProvider>
+//     </ThemeProvider>
+//   );
+// };
+export const Example = Component;
