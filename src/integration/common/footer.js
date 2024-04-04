a148b01ceb62c7322349c274750d6054037e3f7b
louisearchibald <louise.archibald@bbc.co.uk>
2024-04-04 09:55:21 +0100

    removal of duplicate test

--- src/integration/common/footer.js
+++ src/integration/common/footer.js
@@ -1,12 +1,5 @@
 export default () => {
   describe('Footer', () => {
-    it('I can see the footer copyright and external linking text', () => {
-      const footerCopyrightEl = document.querySelector('footer div p');
-      expect(footerCopyrightEl).toBeInTheDocument();
-      expect(footerCopyrightEl.textContent).toBeTruthy();
-      expect(footerCopyrightEl.textContent).toMatchSnapshot();
-    });
-
     it('I can see the BBC branding', () => {
       const brandingEl = document.querySelector('footer svg');
 
