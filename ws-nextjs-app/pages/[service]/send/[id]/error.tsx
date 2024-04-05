fd9650f4b1a4d82f5d799c7efcb587bbd981efcd
alex-magana <alex.magana@andela.com>
2024-04-05 15:39:24 +0300

    Remove redundant error page

--- ws-nextjs-app/pages/[service]/send/[id]/error.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import React from 'react';
-
-// import { UGCSendResponse } from './types';
-
-export default function UGCErrorPage() {
-  return <p>Error in form submission</p>;
-}
