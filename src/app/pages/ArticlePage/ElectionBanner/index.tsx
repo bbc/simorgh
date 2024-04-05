2a7940d905650f556f7cc9b30303a68aa8a03580
Aaron Moore <aaron.moore@bbc.co.uk>
2024-04-05 16:11:53 +0100

    Service restriction

--- src/app/pages/ArticlePage/ElectionBanner/index.tsx
+++ src/app/pages/ArticlePage/ElectionBanner/index.tsx
@@ -4,13 +4,22 @@ import { jsx } from '@emotion/react';
 import { useContext } from 'react';
 import { RequestContext } from '#app/contexts/RequestContext';
 import AmpIframe from '#app/components/AmpIframe';
+import { ServiceContext } from '#app/contexts/ServiceContext';
+import { Services } from '#app/models/types/global';
 import styles, { BANNER_HEIGHT } from './index.styles';
 
 const IFRAME_SRC =
   'https://news.test.files.bbci.co.uk/include/vjsthasia/2308-india-elections-2024-results-page/develop/english/election-banner/embed';
 
+const SERVICES_WITH_ELECTION_BANNER: Services[] = ['hindi'];
+
 export default function ElectionBanner() {
   const { isAmp } = useContext(RequestContext);
+  const { service } = useContext(ServiceContext);
+
+  if (!SERVICES_WITH_ELECTION_BANNER.includes(service)) {
+    return null;
+  }
 
   if (isAmp) {
     return (
