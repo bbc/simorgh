373d4cfd1ece66861b97d85cdf6b5f4e8e000945
chrish01 <holly.christopher@bbc.co.uk>
2024-04-10 16:24:01 +0100

    create billboard files

---
+++ src/app/components/Billboard/index.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Billboard from '.';
+import { StoryProps, StoryArgs } from '../../models/types/storybook';
+import readme from './README.md';
+
+interface Props extends StoryProps {
+  text?: string;
+  longText?: string;
+}
+
+const Component = (_: StoryArgs, { text = '', longText = '' }: Props) => {
+  return (
+    <Billboard
+      heading={text}
+      description={longText}
+      image="https://ichef.bbci.co.uk/ace/standard/{width}/cpsdevpb/1d5b/test/5f969ec0-c4d8-11ed-8319-9b394d8ed0dd.jpg"
+    />
+  );
+};
+
+export default {
+  title: 'Components/Billboard',
+  Component,
+  parameters: {
+    docs: { readme },
+  },
+};
+
+export const Example = Component;
---
+++ src/app/components/Billboard/index.test.tsx
@@ -0,0 +1,217 @@
+import React from 'react';
+import {
+  fireEvent,
+  render,
+  screen,
+} from '../react-testing-library-with-providers';
+import MessageBanner from '.';
+import { kyrgyzMessageBannerOnePromo } from './fixtures';
+import * as viewTracking from '../../hooks/useViewTracker';
+import * as clickTracking from '../../hooks/useClickTrackerHandler';
+
+describe('MessageBanner', () => {
+  const summary = kyrgyzMessageBannerOnePromo.summaries[0];
+  const eventTrackingData = { componentName: 'message-banner' };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('for a curation with 1 summary', () => {
+    it('should render a section with role region', () => {
+      render(
+        <MessageBanner
+          heading={kyrgyzMessageBannerOnePromo.title}
+          description={summary.description}
+          link={summary.link}
+          linkText={summary.title}
+          image={summary.imageUrl}
+        />,
+      );
+      const region = screen.getByRole('region');
+      expect(region).toBeInTheDocument();
+    });
+
+    it('should have a heading with an id which matches the aria-labelledby attribute', () => {
+      const { getByRole } = render(
+        <MessageBanner
+          heading={kyrgyzMessageBannerOnePromo.title}
+          description={summary.description}
+          link={summary.link}
+          linkText={summary.title}
+          image={summary.imageUrl}
+        />,
+      );
+      const heading = screen.getByText(kyrgyzMessageBannerOnePromo.title);
+      const messageBannerEl = getByRole('region');
+      expect(messageBannerEl.getAttribute('aria-labelledby')).toBe(
+        heading.getAttribute('id'),
+      );
+    });
+
+    it('should display the banner heading correctly as an H2', () => {
+      render(
+        <MessageBanner
+          heading={kyrgyzMessageBannerOnePromo.title}
+          description={summary.description}
+          link={summary.link}
+          linkText={summary.title}
+          image={summary.imageUrl}
+        />,
+      );
+      expect(screen.getByText(kyrgyzMessageBannerOnePromo.title).nodeName).toBe(
+        'H2',
+      );
+    });
+
+    it('should display the banner subtext correctly as a Paragraph', () => {
+      render(
+        <MessageBanner
+          heading={kyrgyzMessageBannerOnePromo.title}
+          description={summary.description}
+          link={summary.link}
+          linkText={summary.title}
+          image={summary.imageUrl}
+        />,
+      );
+      expect(screen.getByText(summary.description).nodeName).toBe('P');
+    });
+
+    it('should display link text correctly as an Anchor', () => {
+      render(
+        <MessageBanner
+          heading={kyrgyzMessageBannerOnePromo.title}
+          description={summary.description}
+          link={summary.link}
+          linkText={summary.title}
+          image={summary.imageUrl}
+        />,
+      );
+      const ctaLink = screen.getByRole('link');
+      expect(ctaLink.getAttribute('href')).toEqual(summary.link);
+      expect(ctaLink.textContent).toEqual(summary.title);
+    });
+
+    it('should render an image with the correct image src', () => {
+      render(
+        <MessageBanner
+          heading={kyrgyzMessageBannerOnePromo.title}
+          description={summary.description}
+          link={summary.link}
+          linkText={summary.title}
+          image={summary.imageUrl}
+        />,
+      );
+      const image = screen.getByAltText('');
+      expect(image.getAttribute('src')).toEqual(
+        summary.imageUrl.replace('{width}', 'raw'),
+      );
+    });
+
+    it('should have an image with an empty alt text', () => {
+      render(
+        <MessageBanner
+          heading={kyrgyzMessageBannerOnePromo.title}
+          description={summary.description}
+          link={summary.link}
+          linkText={summary.title}
+          image={summary.imageUrl}
+        />,
+      );
+      const image = screen.getByAltText('');
+      expect(image).toBeInTheDocument();
+    });
+  });
+
+  describe('view tracking', () => {
+    const viewTrackerSpy = jest.spyOn(viewTracking, 'default');
+
+    it('should not be enabled if event tracking data not provided', () => {
+      render(
+        <MessageBanner
+          heading={kyrgyzMessageBannerOnePromo.title}
+          description={summary.description}
+          link={summary.link}
+          linkText={summary.title}
+          image={summary.imageUrl}
+        />,
+      );
+
+      expect(viewTrackerSpy).toHaveBeenCalledWith(undefined);
+    });
+
+    it('should register view tracker if event tracking data provided', () => {
+      render(
+        <MessageBanner
+          heading={kyrgyzMessageBannerOnePromo.title}
+          description={summary.description}
+          link={summary.link}
+          linkText={summary.title}
+          image={summary.imageUrl}
+          eventTrackingData={eventTrackingData}
+        />,
+      );
+
+      expect(viewTrackerSpy).toHaveBeenCalledWith(eventTrackingData);
+    });
+  });
+
+  describe('click tracking', () => {
+    const clickTrackerSpy = jest
+      .spyOn(clickTracking, 'default')
+      .mockImplementation();
+
+    it('should not be enabled if event tracking data not provided', () => {
+      const { container } = render(
+        <MessageBanner
+          heading={kyrgyzMessageBannerOnePromo.title}
+          description={summary.description}
+          link={summary.link}
+          linkText={summary.title}
+          image={summary.imageUrl}
+        />,
+      );
+
+      expect(clickTrackerSpy).toHaveBeenCalledWith(undefined);
+
+      const [callToActionLink] = container.getElementsByTagName('a');
+      fireEvent.click(callToActionLink);
+      expect(callToActionLink.onclick).toBeFalsy();
+    });
+
+    it('should register click tracker if event tracking data provided', () => {
+      render(
+        <MessageBanner
+          heading={kyrgyzMessageBannerOnePromo.title}
+          description={summary.description}
+          link={summary.link}
+          linkText={summary.title}
+          image={summary.imageUrl}
+          eventTrackingData={eventTrackingData}
+        />,
+      );
+
+      expect(clickTrackerSpy).toHaveBeenCalledWith(eventTrackingData);
+    });
+
+    it('should handle a click event when call to action link clicked', () => {
+      clickTrackerSpy.mockRestore();
+
+      const { container } = render(
+        <MessageBanner
+          heading={kyrgyzMessageBannerOnePromo.title}
+          description={summary.description}
+          link={summary.link}
+          linkText={summary.title}
+          image={summary.imageUrl}
+          eventTrackingData={eventTrackingData}
+        />,
+      );
+
+      const [callToActionLink] = container.getElementsByTagName('a');
+      fireEvent.click(callToActionLink);
+
+      expect(callToActionLink.onclick).toBeTruthy();
+    });
+  });
+});
