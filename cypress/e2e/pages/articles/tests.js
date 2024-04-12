f3d776fa5c18e193920e4c38d0e8fa17424f91f1
“LilyL0u” <lily.higham@bbc.co.uk>
2024-04-12 10:37:37 +0100

    going through the tests still including media player related

--- cypress/e2e/pages/articles/tests.js
+++ cypress/e2e/pages/articles/tests.js
@@ -1,8 +1,6 @@
 import config from '../../../support/config/services';
 import appConfig from '../../../../src/server/utilities/serviceConfigs';
 import {
-  getBlockByType,
-  getBlockData,
   getAllBlocksDataByType,
   getAllSocialBlocksByProviderName,
 } from './helpers';
@@ -12,8 +10,6 @@ import { crossPlatform as mostReadAssertions } from '../mostReadPage/mostReadAss
 const serviceHasFigure = service =>
   ['arabic', 'news', 'pashto', 'persian', 'urdu'].includes(service);
 const serviceHasCaption = service => service === 'news';
-// TODO: Remove after https://github.com/bbc/simorgh/issues/2962
-const serviceHasCorrectlyRenderedParagraphs = service => service !== 'sinhala';
 
 const serviceHasTimestamp = service => ['news', 'urdu'].includes(service);
 
@@ -62,160 +58,149 @@ export const testsThatFollowSmokeTestConfig = ({
       });
     });
 
-      it('should render a paragraph, which contains/displays styled text', () => {
-        if (serviceHasCorrectlyRenderedParagraphs(service)) {
-          const paragraphData = getBlockData('text', articlesData);
-          const { text } = paragraphData.model.blocks[0].model;
+    if (serviceHasFigure(service)) {
+      if (serviceHasCaption(service)) {
+        it('should have a visible image with a caption, and also not be lazyloaded', () => {
+          cy.get('figure')
+            .eq(0)
+            .should('be.visible')
+            .should('to.have.descendants', 'img')
+            .should('to.have.descendants', 'figcaption')
+            .within(() => cy.get('noscript').should('not.exist'));
+        });
+      }
 
-          cy.get('p').should('contain', text);
-        }
-      });
-
-      if (serviceHasFigure(service)) {
-        if (serviceHasCaption(service)) {
-          it('should have a visible image with a caption, and also not be lazyloaded', () => {
-            cy.get('figure')
-              .eq(0)
-              .should('be.visible')
-              .should('to.have.descendants', 'img')
-              .should('to.have.descendants', 'figcaption')
-              .within(() => cy.get('noscript').should('not.exist'));
-          });
-        }
-
-        it('should have an image copyright label with styling', () => {
-          const copyrightData = getBlockData('image', articlesData);
-          const rawImageblock = getBlockByType(
-            copyrightData.model.blocks,
-            'rawImage',
-          );
-          const { copyrightHolder } = rawImageblock.model;
-
+      // eslint-disable-next-line no-only-tests/no-only-tests
+      it('should have an image copyright label with styling', () => {
+        let testAssetId;
+        cy.url().then(url => {
+          // eslint-disable-next-line prefer-destructuring
+          testAssetId = url.match(/\/([^/]+?)(?:\.[^/.]+)?$/)[1];
           cy.get('figure')
             .eq(0)
             .within(() => {
-              if (copyrightHolder === 'BBC') {
-                // If an image has a BBC copyright, the copyright holder (<p>) does not appear on images.
-                // This is why we're asserting the value. If the copyright does not appear and is not
-                // 'BBC' then it is clear there is an error with this component.
-                cy.get('p[role="text"]').should('not.exist');
-              } else {
+              // THERE IS NO TEST ASSET WITH A BBC COPYRIGHT
+              // FIND ONE?
+              // if (copyrightHolder === 'BBC') {
+              //   // If an image has a BBC copyright, the copyright holder (<p>) does not appear on images.
+              //   // This is why we're asserting the value. If the copyright does not appear and is not
+              //   // 'BBC' then it is clear there is an error with this component.
+              //   cy.get('p[role="text"]').should('not.exist');
+              // }
+              if (testAssetId === 'crgxnrdl1xvo') {
                 cy.get('p[role="text"]')
                   .should('be.visible')
-                  .and('contain', copyrightHolder);
+                  .and('contain', '@molanaabdolhamid');
+              } else if (testAssetId === 'cld9872jgyjo') {
+                cy.get('p[role="text"]')
+                  .should('be.visible')
+                  .and('contain', 'proofpoint');
               }
             });
         });
-      }
+      });
+    }
 
-      it('should have an inline link', () => {
-        if (articlesData.metadata.language === 'en-gb') {
-          cy.get('main a').should('exist');
+    if (serviceHasInlineLink(service) && Cypress.env('APP_ENV') !== 'live') {
+      it('should have an inline link to an article page', () => {
+        cy.get('a[href*="/articles/"]')
+          .should('have.attr', 'href')
+          .then(href => {
+            cy.request({
+              url: href,
+              failOnStatusCode: false,
+            }).then(resp => {
+              expect(resp.status).to.not.equal(404);
+            });
+          });
+      });
+    }
+
+    if (serviceHasTimestamp(service)) {
+      it('should render a timestamp', () => {
+        cy.get('time')
+          .eq(0)
+          .should('exist')
+          .should('be.visible')
+          .should('have.attr', 'datetime')
+          .should('not.be.empty');
+      });
+    }
+
+    describe('Media Player', () => {
+      it('should have a visible caption beneath a mediaplayer', () => {
+        // The news and pidgin test assets have media players and the others do not
+        if (service === 'news' || service === 'pidgin') {
+          const text =
+            service === 'news'
+              ? 'This is a video block containing a video about ants'
+              : 'They may be tiny, but us humans could learn a thing or two from ants.';
+          cy.get('figcaption')
+            .eq(1)
+            .within(() => {
+              cy.get('[data-testid="caption-paragraph"]')
+                .eq(0)
+                .should('be.visible')
+                .should('contain', text);
+            });
+        } else {
+          cy.log('No media player on this test asset');
         }
       });
+    });
 
-      if (serviceHasInlineLink(service) && Cypress.env('APP_ENV') !== 'live') {
-        it('should have an inline link to an article page', () => {
-          cy.get('a[href*="/articles/"]')
-            .should('have.attr', 'href')
-            .then(href => {
-              cy.request({
-                url: href,
-                failOnStatusCode: false,
-              }).then(resp => {
-                expect(resp.status).to.not.equal(404);
-              });
-            });
-        });
-      }
-
-      if (serviceHasTimestamp(service)) {
-        it('should render a timestamp', () => {
-          cy.get('time')
-            .eq(0)
-            .should('exist')
-            .should('be.visible')
-            .should('have.attr', 'datetime')
-            .should('not.be.empty');
-        });
-      }
-
-      describe('Media Player', () => {
-        it('should have a visible caption beneath a mediaplayer', () => {
-          const media = getBlockData('video', articlesData);
-          if (media) {
-            const captionBlock = getBlockByType(media.model.blocks, 'caption');
-
-            if (captionBlock) {
-              const { text } =
-                captionBlock.model.blocks[0].model.blocks[0].model;
-
-              cy.get('figcaption')
-                .eq(1)
-                .within(() => {
-                  cy.get('p')
-                    .eq(0)
-                    .should('be.visible')
-                    .should('contain', text);
-                });
-            }
-          }
-        });
-      });
-
-      describe('Social Embeds', () => {
-        const availableSocialMediaOnPage = [];
-        const socialIsOnPage = social =>
-          availableSocialMediaOnPage.includes(social);
-        before(() => {
-          availableSocialMediaOnPage.push(
-            ...getAllBlocksDataByType('social', articlesData).map(
-              block => block.model.providerName,
-            ),
-          );
-        });
-        ['YouTube', 'Instagram', 'TikTok', 'Twitter', 'Facebook'].forEach(
-          socialMediaProviderName => {
-            // eslint-disable-next-line func-names
-            it(`${socialMediaProviderName} embed is rendered when it exists on page`, function () {
-              if (socialIsOnPage(socialMediaProviderName)) {
-                const SocialEmbedsData = getAllSocialBlocksByProviderName(
-                  socialMediaProviderName,
-                  articlesData,
-                );
-                const lowercaseSocialMediaProviderName =
-                  socialMediaProviderName.toLowerCase();
-                SocialEmbedsData.forEach(content => {
-                  const socialMediaUrl = content.model.source;
-                  cy.get(
-                    `[data-e2e="${lowercaseSocialMediaProviderName}-embed-${socialMediaUrl}"]`,
-                  ).as('socialMediaEmbed');
-
-                  cy.get('@socialMediaEmbed').scrollIntoView();
-                  cy.get('@socialMediaEmbed').within(() => {
-                    cy.get(`[data-testid="consentBanner"]`).should('exist');
-                    cy.get(`iframe`).should('not.exist');
-                    // TODO: Revisit why this is failing to find the iframe in time
-                    // cy.get(`[data-testid="banner-button"]`).click();
-                    // cy.get(`iframe`).should('exist');
-                    // cy.get(
-                    //   `[href^="#end-of-${lowercaseSocialMediaProviderName}-content"]`,
-                    // ).should('exist');
-                  });
-                });
-              } else {
-                cy.log(`No ${socialMediaProviderName} embed on page`);
-                this.skip();
-              }
-            });
-          },
+    describe('Social Embeds', () => {
+      const availableSocialMediaOnPage = [];
+      const socialIsOnPage = social =>
+        availableSocialMediaOnPage.includes(social);
+      before(() => {
+        availableSocialMediaOnPage.push(
+          ...getAllBlocksDataByType('social', articlesData).map(
+            block => block.model.providerName,
+          ),
         );
       });
+      ['YouTube', 'Instagram', 'TikTok', 'Twitter', 'Facebook'].forEach(
+        socialMediaProviderName => {
+          // eslint-disable-next-line func-names
+          it(`${socialMediaProviderName} embed is rendered when it exists on page`, function () {
+            if (socialIsOnPage(socialMediaProviderName)) {
+              const SocialEmbedsData = getAllSocialBlocksByProviderName(
+                socialMediaProviderName,
+                articlesData,
+              );
+              const lowercaseSocialMediaProviderName =
+                socialMediaProviderName.toLowerCase();
+              SocialEmbedsData.forEach(content => {
+                const socialMediaUrl = content.model.source;
+                cy.get(
+                  `[data-e2e="${lowercaseSocialMediaProviderName}-embed-${socialMediaUrl}"]`,
+                ).as('socialMediaEmbed');
 
-      /**
-       * Most Read Component
-       */
-      mostReadAssertions({ service, variant });
+                cy.get('@socialMediaEmbed').scrollIntoView();
+                cy.get('@socialMediaEmbed').within(() => {
+                  cy.get(`[data-testid="consentBanner"]`).should('exist');
+                  cy.get(`iframe`).should('not.exist');
+                  // TODO: Revisit why this is failing to find the iframe in time
+                  // cy.get(`[data-testid="banner-button"]`).click();
+                  // cy.get(`iframe`).should('exist');
+                  // cy.get(
+                  //   `[href^="#end-of-${lowercaseSocialMediaProviderName}-content"]`,
+                  // ).should('exist');
+                });
+              });
+            } else {
+              cy.log(`No ${socialMediaProviderName} embed on page`);
+              this.skip();
+            }
+          });
+        },
+      );
     });
+
+    /**
+     * Most Read Component
+     */
+    mostReadAssertions({ service, variant });
   });
 };
