8b49a69e68dc40b5f0e1fa6cbd9256b14a4e6f79
Karina Thomas <karina.thomas@bbc.co.uk>
2024-04-12 13:11:24 +0100

    Add separate stories for Example (without live label) and with Live Label

--- src/app/components/Billboard/index.stories.tsx
+++ src/app/components/Billboard/index.stories.tsx
@@ -7,16 +7,21 @@ import readme from './README.md';
 interface Props extends StoryProps {
   text?: string;
   longText?: string;
+  showLiveLabel?: boolean;
 }
 
-const Component = (_: StoryArgs, { text = '', longText = '' }: Props) => {
+const Component = ({
+  text = '',
+  longText = '',
+  showLiveLabel = false,
+}: Props) => {
   return (
     <Billboard
       heading={text}
       description={longText}
       link="https://www.bbc.co.uk/ws/languages"
       image="https://ichef.bbci.co.uk/ace/standard/{width}/cpsdevpb/1d5b/test/5f969ec0-c4d8-11ed-8319-9b394d8ed0dd.jpg"
-      showLiveLabel
+      showLiveLabel={showLiveLabel}
     />
   );
 };
@@ -29,4 +34,10 @@ export default {
   },
 };
 
-export const Example = Component;
+export const Example = (_: StoryArgs, globalArgs: Props) => (
+  <Component {...globalArgs} />
+);
+
+export const WithLiveLabel = (_: StoryArgs, globalArgs: Props) => {
+  return <Component {...globalArgs} showLiveLabel />;
+};
