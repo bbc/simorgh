f503da724f1cd0b82bc03057abca934bb6f8eda9
Aaron Moore <aaron.moore@bbc.co.uk>
2024-04-16 11:07:47 +0100

    Update index.ts

--- ws-nextjs-app/utilities/derivePageType/index.ts
+++ ws-nextjs-app/utilities/derivePageType/index.ts
@@ -1,7 +1,6 @@
 import { LIVE_PAGE, UGC_PAGE } from '#app/routes/utils/pageTypes';
 
 export default function derivePageType(pathname: string) {
-  // remove query params from pathname
   const sanitisedPathname = new URL(pathname, 'http://bbc.com').pathname;
 
   if (sanitisedPathname.includes('live')) return LIVE_PAGE;
