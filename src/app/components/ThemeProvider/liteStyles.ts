5c2db6d4046d25403ef604a5a809bef3ae7e10ac
Aaron Moore <aaron.moore@bbc.co.uk>
2024-04-17 12:39:17 +0100

    Update liteStyles.ts

--- src/app/components/ThemeProvider/liteStyles.ts
+++ src/app/components/ThemeProvider/liteStyles.ts
@@ -1,16 +1,18 @@
 import { css } from '@emotion/react';
 
 const liteStyles = () => css`
-  [class*='imagewrapper' i],
-  [class*='placeholder' i] {
-    display: none !important;
-  }
   .promo-text {
     width: 100% !important;
     padding-inline-start: 0 !important;
   }
+  [class*='ImageWrapper' i] {
+    display: none;
+  }
   [class*='textwrapper' i] {
     width: 100%;
+  }
+  [class*='StoryPromoWrapper' i] {
+    display: block;
   }
 `;
 
