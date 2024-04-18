7dc67aa81305a56587eecccada1412366b6fb8ad
Aaron Moore <aaron.moore@bbc.co.uk>
2024-04-18 09:41:48 +0100

    Update index.ts

--- ws-nextjs-app/utilities/removeSensitiveHeaders/index.ts
+++ ws-nextjs-app/utilities/removeSensitiveHeaders/index.ts
@@ -3,14 +3,13 @@ import { IncomingHttpHeaders } from 'http';
 export default function removeSensitiveHeaders(headers?: IncomingHttpHeaders) {
   if (!headers) return {};
 
-  const sensitiveHeaders = process.env.SENSITIVE_HTTP_HEADERS;
-  const sensitiveHeadersArray = sensitiveHeaders
-    ?.split(',')
-    ?.map(el => el.trim());
+  const sensitiveHeadersArray = process.env.SENSITIVE_HTTP_HEADERS?.split(
+    ',',
+  )?.map(el => el.trim());
 
   return Object.keys(headers)
-    .filter(objKey => !sensitiveHeadersArray?.includes(objKey))
-    .reduce<IncomingHttpHeaders>((newObj, key) => {
+    ?.filter(objKey => !sensitiveHeadersArray?.includes(objKey))
+    ?.reduce<IncomingHttpHeaders>((newObj, key) => {
       return { ...newObj, [key]: headers[key] };
     }, {});
 }
