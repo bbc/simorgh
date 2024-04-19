b89f931ec6671bdb71173ea30b7a1f23eba52080
Isabella Mitchell <i.mitchell.0707@gmail.com>
2024-04-19 10:06:13 +0100

    WSTEAM1-967: CAF - Update MAP Integration test and snapshots

--- src/integration/common/mediaPlaceholder.js
+++ src/integration/common/mediaPlaceholder.js
@@ -1,11 +1,15 @@
+const serviceUsingCAF = ['pidgin'];
+
 export default () => {
   describe('Media Placeholder', () => {
-    it('outer iframe has z-index of 1', () => {
-      const iframe = document.querySelector('iframe');
-      expect(iframe).toBeInTheDocument();
-      expect(
-        window.getComputedStyle(iframe).getPropertyValue('z-index'),
-      ).toEqual('1');
-    });
+    if (!serviceUsingCAF.includes(service)) {
+      it('outer iframe has z-index of 1', () => {
+        const iframe = document.querySelector('iframe');
+        expect(iframe).toBeInTheDocument();
+        expect(
+          window.getComputedStyle(iframe).getPropertyValue('z-index'),
+        ).toEqual('1');
+      });
+    }
   });
 };
