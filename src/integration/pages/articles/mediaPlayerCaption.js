a2c6f93707f18a34ed16bba09b733063e66e13ec
“LilyL0u” <lily.higham@bbc.co.uk>
2024-04-24 19:31:56 +0100

    found the .only

--- src/integration/pages/articles/mediaPlayerCaption.js
+++ src/integration/pages/articles/mediaPlayerCaption.js
@@ -1,6 +1,5 @@
 export default () => {
-  // eslint-disable-next-line no-only-tests/no-only-tests
-  describe.only('Media Player Caption', () => {
+  describe('Media Player Caption', () => {
     it('should have a caption under the media player', () => {
       const figcaptionEl = document.querySelectorAll('figcaption')[1];
       expect(figcaptionEl).toBeInTheDocument();
