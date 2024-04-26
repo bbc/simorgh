4b736bdf9c3073c0f3a82ce8faa63223b36e5518
Aaron Moore <aaron.moore@bbc.co.uk>
2024-04-26 13:32:32 +0100

    Add `isLite` to custom RTL

--- src/app/components/react-testing-library-with-providers.tsx
+++ src/app/components/react-testing-library-with-providers.tsx
@@ -15,6 +15,7 @@ interface Props extends PropsWithChildren {
   id?: string | null;
   isAmp?: boolean;
   isApp?: boolean;
+  isLite?: boolean;
   pageData?: object;
   atiData?: {
     contentId?: string;
@@ -43,6 +44,7 @@ const AllTheProviders: FC<Props> = ({
   id = null,
   isAmp = false,
   isApp = false,
+  isLite = false,
   bbcOrigin = 'https://www.test.bbc.com',
   pageType = 'article',
   derivedPageType,
@@ -69,6 +71,7 @@ const AllTheProviders: FC<Props> = ({
           pageType={pageType}
           isAmp={isAmp}
           isApp={isApp}
+          isLite={isLite}
           isNextJs={isNextJs}
           service={service}
           pathname={pathname}
@@ -98,6 +101,7 @@ const customRender = (
     id,
     isAmp,
     isApp,
+    isLite,
     bbcOrigin,
     pageData,
     pageType,
@@ -120,6 +124,7 @@ const customRender = (
         id={id}
         isAmp={isAmp}
         isApp={isApp}
+        isLite={isLite}
         bbcOrigin={bbcOrigin}
         pageData={pageData}
         atiData={atiData}
