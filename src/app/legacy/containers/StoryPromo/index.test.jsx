bbc7ee40db755af025212499242c774db9c136f1
“LilyL0u” <lily.higham@bbc.co.uk>
2024-04-29 18:07:25 +0100

    forgot to save the adding webp to storyPromo test urls

--- src/app/legacy/containers/StoryPromo/index.test.jsx
+++ src/app/legacy/containers/StoryPromo/index.test.jsx
@@ -272,7 +272,7 @@ describe('StoryPromo Container', () => {
       expect(
         container.getElementsByTagName('img')[0].getAttribute('src'),
       ).toEqual(
-        `https://ichef.bbci.co.uk/ace/ws/660${cpsItem.indexImage.path}`,
+        `https://ichef.bbci.co.uk/ace/ws/660${cpsItem.indexImage.path}.webp`,
       );
       expect(
         container.getElementsByTagName('img')[0].getAttribute('alt'),
@@ -290,7 +290,7 @@ describe('StoryPromo Container', () => {
       expect(
         container.getElementsByTagName('amp-img')[0].getAttribute('src'),
       ).toEqual(
-        `https://ichef.bbci.co.uk/ace/ws/660${cpsItem.indexImage.path}`,
+        `https://ichef.bbci.co.uk/ace/ws/660${cpsItem.indexImage.path}.webp`,
       );
       expect(
         container.getElementsByTagName('amp-img')[0].getAttribute('alt'),
