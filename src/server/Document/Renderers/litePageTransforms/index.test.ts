46ef9c8d3973e21a65bf4b3c267c3e696ccd6bd8
Aaron Moore <aaron.moore@bbc.co.uk>
2024-04-29 09:55:05 +0100

    Update index.test.ts

--- src/server/Document/Renderers/litePageTransforms/index.test.ts
+++ src/server/Document/Renderers/litePageTransforms/index.test.ts
@@ -1,41 +1,43 @@
 import litePageTransforms from '.';
 
 describe('litePageTransforms', () => {
-  it('should append .lite suffix to valid hrefs', () => {
-    const html = `
-      <a href="https://www.bbc.com/news">News</a>
-      <a href="https://www.bbcrussian.com/news">News</a>
-      <a href="/news">News</a>
-    `;
+  describe('anchor tags', () => {
+    it('should append .lite suffix to valid hrefs', () => {
+      const html = `
+        <a href="https://www.bbc.com/news">News</a>
+        <a href="https://www.bbcrussian.com/news">News</a>
+        <a href="/news">News</a>
+      `;
 
-    const modifiedHtml = litePageTransforms(html);
+      const modifiedHtml = litePageTransforms(html);
 
-    expect(modifiedHtml).toEqual(`
-      <a href="https://www.bbc.com/news.lite">News</a>
-      <a href="https://www.bbcrussian.com/news.lite">News</a>
-      <a href="/news.lite">News</a>
-    `);
-  });
+      expect(modifiedHtml).toEqual(`
+        <a href="https://www.bbc.com/news.lite">News</a>
+        <a href="https://www.bbcrussian.com/news.lite">News</a>
+        <a href="/news.lite">News</a>
+      `);
+    });
 
-  it('should not append .lite suffix to invalid hrefs', () => {
-    const html = `
-      <a href="https://www.bbc.co.uk/news">News</a>
-      <a href="https://www.bbc.com/news.lite">News</a>
-      <a href="https://www.bbc.com/news.amp">News</a>
-      <a href="#news">News</a>
-      <a href="mailto:test@gmail.com">News</a>
-    `;
+    it('should not append .lite suffix to invalid hrefs', () => {
+      const html = `
+        <a href="https://www.bbc.co.uk/news">News</a>
+        <a href="https://www.bbc.com/news.lite">News</a>
+        <a href="https://www.bbc.com/news.amp">News</a>
+        <a href="#news">News</a>
+        <a href="mailto:test@gmail.com">News</a>
+      `;
 
-    const modifiedHtml = litePageTransforms(html);
+      const modifiedHtml = litePageTransforms(html);
 
-    expect(modifiedHtml).toEqual(html);
-  });
+      expect(modifiedHtml).toEqual(html);
+    });
 
-  it('should not append .lite suffix when no anchor tags are present', () => {
-    const html = '<p>I am a paragraph</p>';
+    it('should not append .lite suffix when no anchor tags are present', () => {
+      const html = '<p>I am a paragraph</p>';
 
-    const modifiedHtml = litePageTransforms(html);
+      const modifiedHtml = litePageTransforms(html);
 
-    expect(modifiedHtml).toEqual(html);
+      expect(modifiedHtml).toEqual(html);
+    });
   });
 });
