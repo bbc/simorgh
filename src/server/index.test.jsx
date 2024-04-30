e20f6732a9920a5089752eafc2b727d30411018a
eagerterrier <eagerterrier@gmail.com>
2024-04-30 15:15:15 +0100

    FIX: add vary to header

--- src/server/index.test.jsx
+++ src/server/index.test.jsx
@@ -1464,7 +1464,7 @@ describe('Server HTTP Headers - Page Endpoints', () => {
 
     const { header } = await makeRequest('/mundo/c0000000001o');
 
-    expect(header.vary).toBe('mvt-simorgh_dark_mode, Accept-Encoding');
+    expect(header.vary).toBe('mvt-simorgh_dark_mode, X-country, Accept-Encoding');
   });
 
   it(`should not add mvt experiment header names to vary if they are not enabled`, async () => {
@@ -1475,7 +1475,7 @@ describe('Server HTTP Headers - Page Endpoints', () => {
 
     const { header } = await makeRequest('/mundo/articles/c0000000001o');
 
-    expect(header.vary).toBe('Accept-Encoding');
+    expect(header.vary).toBe('X-country, Accept-Encoding');
   });
 
   it(`should not add mvt experiment header names to vary if on AMP`, async () => {
@@ -1487,7 +1487,7 @@ describe('Server HTTP Headers - Page Endpoints', () => {
 
     const { header } = await makeRequest('/mundo/articles/c0000000001o');
 
-    expect(header.vary).toBe('Accept-Encoding');
+    expect(header.vary).toBe('X-country, Accept-Encoding');
   });
 
   it(`should set isUK value to true when 'x-bbc-edge-isuk' is set to 'yes'`, async () => {
