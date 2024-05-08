7ddd1f82ceaee5f2a5b7a6b97eb45ed6778a3b01
eagerterrier <eagerterrier@gmail.com>
2024-05-08 08:53:04 +0100

    FIX: unused var

--- ws-nextjs-app/pages/[service]/downloads/downloadsPageLayout.tsx
+++ ws-nextjs-app/pages/[service]/downloads/downloadsPageLayout.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { jsx } from '@emotion/react';
 import Heading from '#app/components/Heading';
 import Metadata from '#app/components/Metadata';
