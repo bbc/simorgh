b7cdbda3eb182a8080a64fe432fa52481d7a20bb
eagerterrier <eagerterrier@gmail.com>
2024-05-08 07:56:58 +0100

    FIX: ts ftw

--- ws-nextjs-app/pages/[service]/downloads/types.ts
+++ ws-nextjs-app/pages/[service]/downloads/types.ts
@@ -1,11 +1,24 @@
 import { Services, Variants } from '#app/models/types/global';
 
+export type DownloadData = {
+    fileCreated: string;
+    files: DownloadDataFile[];
+};
+
+export type DownloadDataFile = {
+    fileCreatedDate: string;
+    fileName: string;
+    fileLink: string;
+    lastModified: string;
+    fileSize: number;
+};
+
 export type PageProps = {
   service: Services;
   variant?: Variants;
   pageData: {
     title: string;
     description: string;
-    sections: Section[];
+    downloadData?: DownloadData[];
   };
 };
