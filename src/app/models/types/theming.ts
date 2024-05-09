68cac931c3c7c6ddfff7976fc44fe99aa331aec9
emilysaffron <emily.saffron@bbc.co.uk>
2024-05-09 16:48:07 +0100

    missed changes from last commit woops

--- src/app/models/types/theming.ts
+++ src/app/models/types/theming.ts
@@ -49,6 +49,7 @@ interface Palette extends BrandPalette {
   POSTBOX: string;
   POSTBOX_30: string;
   RHINO: string;
+  SERVICE_NEUTRAL_CORE: string;
   SHADOW: string;
   SPORT_MIST: string;
   SPORT_SILVER: string;
