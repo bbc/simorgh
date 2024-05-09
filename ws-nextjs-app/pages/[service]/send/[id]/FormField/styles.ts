9971ae33e6e68bf425d6bc5f26970ba19966c4ce
Shayne Marc Enzo Ahchoon <shayne.ahchoon@bbc.co.uk>
2024-05-09 14:27:31 +0100

    WSTEAM1-969: Merge conflicts

--- ws-nextjs-app/pages/[service]/send/[id]/FormField/styles.ts
+++ ws-nextjs-app/pages/[service]/send/[id]/FormField/styles.ts
@@ -1,13 +1,28 @@
 import { focusIndicatorThickness } from '#app/components/ThemeProvider/focusIndicator';
+import pixelsToRem from '#app/utilities/pixelsToRem';
 import { Theme, css } from '@emotion/react';
 
 const CHECK_IMG = `data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 117.65 98.87'%3E%3Cpath d='M96.64 0L46.11 60.23 17.63 36.34 0 57.35l28.47 23.89 21.01 17.63 17.63-21.01 50.54-60.22L96.64 0z'/%3E%3C/svg%3E`;
 
 export default {
-  formField: ({ spacings }: Theme) =>
+  formField: ({ spacings, palette, mq }: Theme) =>
     css({
       '&:nth-child(n+2)': {
         marginTop: `${spacings.QUADRUPLE}rem`,
+      },
+
+      marginTop: `${spacings.DOUBLE}rem`,
+
+      '&:nth-child(5)': {
+        borderBottom: `${pixelsToRem(1)}rem solid ${palette.GREY_5}`,
+        paddingBottom: `${spacings.DOUBLE}rem`,
+        marginBottom: `${spacings.DOUBLE}rem`,
+      },
+      [mq.GROUP_2_MIN_WIDTH]: {
+        '&:nth-child(5)': {
+          paddingBottom: `${spacings.TRIPLE}rem`,
+          marginBottom: `${spacings.TRIPLE}rem`,
+        },
       },
     }),
   fieldLabel: ({ spacings }: Theme) =>
