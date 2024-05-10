0c1d962aee8dfa61d0ea1032fda8dbb131844c17
Toby Cox <eagerterrier@gmail.com>
2024-05-10 11:14:03 +0100

    FIX: header logic was bypassing cookie headers (#11614)
    * FIX: header logic was bypassing cookie headers
    
    * Update src/server/utilities/extractHeaders/index.ts
    
    Co-authored-by: Karina Thomas <58214768+karinathomasbbc@users.noreply.github.com>
    
    * ENH: expand tests to account for new knowledge
    
    * FIX: bad git autosuggest, bad
    
    * FIX: logic wasn't right
    
    * FIX: wrong logic
    
    * FIX: lint
    
    ---------
    
    Co-authored-by: Karina Thomas <58214768+karinathomasbbc@users.noreply.github.com>

--- src/server/utilities/extractHeaders/index.test.ts
+++ src/server/utilities/extractHeaders/index.test.ts
@@ -21,13 +21,25 @@ describe('extractHeader', () => {
     });
   });
 
-  it(`sets showCookieBannerBasedOnCountry to false when 'x-country' is set to 'za'`, () => {
+  it(`sets showCookieBannerBasedOnCountry to false when 'x-country' is set to 'za' and 'x-bbc-edge-isuk' is set to 'no'`, () => {
     const actual = extractHeaders({
       'x-country': 'za',
+      'x-bbc-edge-isuk': 'no',
     });
     expect(actual).toStrictEqual({
       isUK: false,
       showCookieBannerBasedOnCountry: false,
+    });
+  });
+
+  it(`sets showCookieBannerBasedOnCountry to true when 'x-country' is set to 'za' and 'x-bbc-edge-isuk' is set to 'yes'`, () => {
+    const actual = extractHeaders({
+      'x-country': 'za',
+      'x-bbc-edge-isuk': 'yes',
+    });
+    expect(actual).toStrictEqual({
+      isUK: true,
+      showCookieBannerBasedOnCountry: true,
     });
   });
 });
