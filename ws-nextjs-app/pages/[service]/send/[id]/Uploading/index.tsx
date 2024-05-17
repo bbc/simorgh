5f8ed2140a8b6f215a0916d14e305292122452b7
Aaron Moore <aaron.moore@bbc.co.uk>
2024-05-17 09:59:36 +0100

    Revert "Update index.tsx"
    This reverts commit c7e71d4011ce264b431fab854b8e365af14a1501.

--- ws-nextjs-app/pages/[service]/send/[id]/Uploading/index.tsx
+++ ws-nextjs-app/pages/[service]/send/[id]/Uploading/index.tsx
@@ -4,7 +4,7 @@ import Heading from '#app/components/Heading';
 import Paragraph from '#app/components/Paragraph';
 import styles from './styles';
 
-const Uploading = () => {
+export default function Uploading() {
   return (
     <>
       <Heading level={1} id="content" tabIndex={-1} css={styles.heading}>
@@ -13,6 +13,4 @@ const Uploading = () => {
       <Paragraph>Please wait until it is finished.</Paragraph>
     </>
   );
-};
-
-export default Uploading;
+}
