c7e71d4011ce264b431fab854b8e365af14a1501
Aaron Moore <aaron.moore@bbc.co.uk>
2024-05-17 09:25:25 +0100

    Update index.tsx

--- ws-nextjs-app/pages/[service]/send/[id]/Uploading/index.tsx
+++ ws-nextjs-app/pages/[service]/send/[id]/Uploading/index.tsx
@@ -4,7 +4,7 @@ import Heading from '#app/components/Heading';
 import Paragraph from '#app/components/Paragraph';
 import styles from './styles';
 
-export default function Uploading() {
+const Uploading = () => {
   return (
     <>
       <Heading level={1} id="content" tabIndex={-1} css={styles.heading}>
@@ -13,4 +13,6 @@ export default function Uploading() {
       <Paragraph>Please wait until it is finished.</Paragraph>
     </>
   );
-}
+};
+
+export default Uploading;
