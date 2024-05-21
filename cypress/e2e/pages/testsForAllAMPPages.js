576fb6229b3c83807361f806d1237efc02b6fdfc
“LilyL0u” <lily.higham@bbc.co.uk>
2024-05-21 16:39:26 +0100

    more image tests

--- cypress/e2e/pages/testsForAllAMPPages.js
+++ cypress/e2e/pages/testsForAllAMPPages.js
@@ -9,35 +9,45 @@ export const testsThatFollowSmokeTestConfigForAllAMPPages = ({
   pageType,
 }) => {
   describe(`testsThatFollowSmokeTestConfigForAllAMPPages to run for ${service} ${pageType}`, () => {
-    describe('Image Tests', () => {
-      // live radio pages do not have an image
-      if (pageType !== 'liveRadio') {
-        it('should have webp images on pages', () => {
-          // on amp there are hidden embed images, so we check only ichef ones
-          cy.get('amp-img, img[src*="ichef."').each($img => {
-            // when you use a .each loop or other JS function that take a callback function (here with $img that is executed for each image element)
-            // you leave the Cypress command queue and are using plain JS. Using .wrap converts
-            // the JQuery element into a Cypress wrapped element so we can execute Cypress commands on it
+    describe(
+      'Image Tests',
+      {
+        retries: 3,
+      },
+      () => {
+        const pageTypesNoImages = [
+          'liveRadio',
+          'errorPage404',
+          'mediaAssetPage',
+        ];
+        // live radio pages and error pages do not have an image, media asset pages do not have the most watched list with images on amp
+        if (!pageTypesNoImages.includes(pageType)) {
+          it('should have webp images on pages', () => {
+            // on amp there are hidden embed images, so we check only ichef ones
+            cy.get('amp-img[src*="ichef."], img[src*="ichef."]').each($img => {
+              // when you use a .each loop or other JS function that take a callback function (here with $img that is executed for each image element)
+              // you leave the Cypress command queue and are using plain JS. Using .wrap converts
+              // the JQuery element into a Cypress wrapped element so we can execute Cypress commands on it
 
-            // Images are lazy loaded so we need to scroll to them, check they have loaded before getting currentSrc
-            cy.wrap($img)
-              .scrollIntoView()
-              .should('be.visible')
-              .then($visibleImg => {
-                const src = $visibleImg.attr('src');
-                cy.log(src);
-                expect(src.endsWith('.webp')).to.be.true;
-              });
+              // Images are lazy loaded so we need to scroll to them, check they have loaded before getting currentSrc
+              cy.wrap($img)
+                .scrollIntoView()
+                .should('be.visible')
+                .then($visibleImg => {
+                  const src = $visibleImg.attr('src');
+                  cy.log(src);
+                  expect(src.endsWith('.webp')).to.be.true;
+                });
+            });
           });
-        });
-      }
-    });
+        }
+      },
+    );
     describe('Header Tests', () => {
       const serviceName = config[service].name;
       // limit number of tests to 2 services for navigation toggling
       const testMobileNav =
         serviceName === 'ukchina' || serviceName === 'persian';
-
       if (testMobileNav) {
         it('should show dropdown menu and hide scrollable menu when menu button is clicked', () => {
           cy.viewport(320, 480);
--- cypress/e2e/pages/testsForAllCanonicalPages.js
+++ cypress/e2e/pages/testsForAllCanonicalPages.js
@@ -11,34 +11,46 @@ export const testsThatFollowSmokeTestConfigForAllCanonicalPages = ({
 }) => {
   if (pageType !== 'errorPage404') {
     describe(`Running testsForAllCanonicalPages for ${service} ${pageType}`, () => {
-      describe('Image Tests', () => {
-        // live radio pages do not have an image
-        if (pageType !== 'liveRadio') {
-          it('should have webp images on pages', () => {
-            cy.get('img').each($img => {
-              // when you use a .each loop or other JS function that take a callback function (here with $img that is executed for each image element)
-              // you leave the Cypress command queue and are using plain JS. Using .wrap converts
-              // the JQuery element into a Cypress wrapped element so we can execute Cypress commands on it
+      describe(
+        'Image Tests',
+        {
+          retries: 3,
+        },
+        () => {
+          const pageTypesNoImages = [
+            'liveRadio',
+            'errorPage404',
+            'mediaAssetPage',
+          ];
+          // live radio pages and error pages do not have an image, media asset pages do not reliably show most watched list so this will cause flakes
+          if (!pageTypesNoImages.includes(pageType)) {
+            it('should have webp images on pages', () => {
+              cy.get('img').each($img => {
+                // when you use a .each loop or other JS function that take a callback function (here with $img that is executed for each image element)
+                // you leave the Cypress command queue and are using plain JS. Using .wrap converts
+                // the JQuery element into a Cypress wrapped element so we can execute Cypress commands on it
 
-              // Images are lazy loaded so we need to scroll to them, check they have loaded before getting currentSrc
-              cy.wrap($img)
-                .scrollIntoView()
-                .should('be.visible')
-                .then($visibleImg => {
-                  cy.wrap($visibleImg)
-                    .should($loadedImg => {
-                      expect($loadedImg.prop('currentSrc')).to.not.be.empty;
-                    })
-                    .then($imgWithCurrentSrc => {
-                      const currentSrc = $imgWithCurrentSrc.prop('currentSrc');
-                      cy.log('currentSrc', currentSrc);
-                      expect(currentSrc.endsWith('.webp')).to.be.true;
-                    });
-                });
+                // Images are lazy loaded so we need to scroll to them, check they have loaded before getting currentSrc
+                cy.wrap($img)
+                  .scrollIntoView()
+                  .should('be.visible')
+                  .then($visibleImg => {
+                    cy.wrap($visibleImg)
+                      .should($loadedImg => {
+                        expect($loadedImg.prop('currentSrc')).to.not.be.empty;
+                      })
+                      .then($imgWithCurrentSrc => {
+                        const currentSrc =
+                          $imgWithCurrentSrc.prop('currentSrc');
+                        cy.log('currentSrc', currentSrc);
+                        expect(currentSrc.endsWith('.webp')).to.be.true;
+                      });
+                  });
+              });
             });
-          });
-        }
-      });
+          }
+        },
+      );
       if (Cypress.env('SMOKE')) {
         describe(
           'ATI',
