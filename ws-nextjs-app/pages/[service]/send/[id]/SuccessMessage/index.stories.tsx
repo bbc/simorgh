7c294b8e31f982acb80fc1af9001df53bfcfbbda
Shayne Marc Enzo Ahchoon <shayne.ahchoon@bbc.co.uk>
2024-05-22 16:30:36 +0100

    WSTEAM1-1030: refactor

--- ws-nextjs-app/pages/[service]/send/[id]/SuccessMessage/index.stories.tsx
+++ ws-nextjs-app/pages/[service]/send/[id]/SuccessMessage/index.stories.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import SuccessMessage from './index';
 
-const sampleSuccessResponse = {
-  code: 'OK',
-  submissionId: '269cb7fa-6221-4586-b54f-61c7beda705e',
-  message: 'Success',
-};
-
 const Component = () => <SuccessMessage />;
 
 export default {
