51115da6a0cf2d12e3dc7e1a4b3f2590c91f7ad9
Harvey Peachey <harvey.peachey@bbc.co.uk>
2024-05-29 09:59:28 +0100

    add comment to is proxy for fat function usage

--- src/app/lib/utilities/isOperaProxy/index.js
+++ src/app/lib/utilities/isOperaProxy/index.js
@@ -1,5 +1,6 @@
 // This is the detection method recommended by opera: https://dev.opera.com/articles/opera-mini-and-javascript/
 // Note we do not have reliable access to user-agent based detection due to upstream services not forwarding that header
+// This uses the fat function syntax for compatibility reasons, as we use toString() to inline this bit of code for Opera Mini, see https://github.com/bbc/simorgh/pull/11656
 export default function isOperaProxy() {
   return (
     Object.prototype.toString.call(window.operamini) === '[object OperaMini]'
