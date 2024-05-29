f34543564bf8131dd7d2af72db84ba578c504434
Harvey Peachey <harvey.peachey@bbc.co.uk>
2024-05-29 16:05:07 +0100

    changes syntax for browser compat and escaping single quote marks

--- src/app/components/ATIAnalytics/canonical/sendBeaconOperaMiniScript.ts
+++ src/app/components/ATIAnalytics/canonical/sendBeaconOperaMiniScript.ts
@@ -1,12 +1,12 @@
 import isOperaProxy from '#app/lib/utilities/isOperaProxy';
 
 const sendBeaconOperaMiniScript = (atiPageViewUrlString: string) => `
-if ((${isOperaProxy.toString()})()) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', '${atiPageViewUrlString}', true);
+if (${isOperaProxy.toString()}()) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "${atiPageViewUrlString}", true);
   xhr.withCredentials = true;
   xhr.send();
-}
+} 
 `;
 
 export default sendBeaconOperaMiniScript;
