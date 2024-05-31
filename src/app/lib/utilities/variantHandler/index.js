562c16ebbf15149804dd8cb78a7c2dfbde3d178c
Isabella Mitchell <i.mitchell.0707@gmail.com>
2024-05-31 17:01:15 +0100

    WSTEAM1-953: Update variant handler logic

--- src/app/lib/utilities/variantHandler/index.js
+++ src/app/lib/utilities/variantHandler/index.js
@@ -4,31 +4,30 @@ export const servicesWithVariants = {
   // the first element in the array is the default variant
   serbian: ['lat', 'cyr'],
   ukchina: ['simp', 'trad'],
-  uzbek: ['default', 'cyr', 'lat'],
+  uzbek: ['cyr', 'lat'],
   zhongwen: ['simp', 'trad'],
 };
-
+export const servicesWithOptionalVariants = ['uzbek'];
 export const variants = ['simp', 'trad', 'lat', 'cyr', 'default'];
-
 const variantCookieConfig = {
   ukchina: 'chinese',
   zhongwen: 'chinese',
 };
-
 export const getVariantCookieName = service =>
   pathOr(service, [service], variantCookieConfig);
-
 // Remove leading slash from variant
 export const variantSanitiser = variant => variant && variant.replace('/', '');
-
 // If service has variants, use it or default to first variant in array
 // If service doesnt have variants, return 'default'
 export const getVariant = ({ service, variant }) => {
   const allowedVariants = servicesWithVariants[service];
-
-  if (allowedVariants) {
+  const allowedOptionalVariants =
+    servicesWithOptionalVariants.includes(service);
+  if (allowedVariants && !allowedOptionalVariants) {
     return allowedVariants.includes(variant) ? variant : allowedVariants[0];
   }
-
+  if (allowedVariants && allowedOptionalVariants) {
+    return allowedVariants.includes(variant) ? variant : 'default';
+  }
   return 'default';
 };
