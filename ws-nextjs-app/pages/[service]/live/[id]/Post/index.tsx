17be3f62497f0b9ef08bf61082b7130586f9b792
emilysaffron <emily.saffron@bbc.co.uk>
2024-06-03 16:47:06 +0100

    simpler check

--- ws-nextjs-app/pages/[service]/live/[id]/Post/index.tsx
+++ ws-nextjs-app/pages/[service]/live/[id]/Post/index.tsx
@@ -178,11 +178,7 @@ const Post = ({ post }: { post: PostType }) => {
   const [hashValue, setHashValue] = useState('');
   useEffect(() => {
     setHashValue(window.location.hash.substring(1));
-    if (
-      typeof window !== 'undefined' &&
-      typeof navigator !== 'undefined' &&
-      'share' in navigator
-    ) {
+    if (typeof navigator !== 'undefined' && 'share' in navigator) {
       setCanShare(true);
     }
   }, []);
