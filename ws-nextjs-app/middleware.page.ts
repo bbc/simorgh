7859536315a182e6063b8aac37f939d7c66422c0
Shayne Marc Enzo Ahchoon <shayne.ahchoon@bbc.co.uk>
2024-06-04 10:51:11 +0100

    WSTEAM1-1068: Add conditional logic

--- ws-nextjs-app/middleware.page.ts
+++ ws-nextjs-app/middleware.page.ts
@@ -1,32 +1,33 @@
 /* eslint-disable import/prefer-default-export */
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest } from 'next/server';
 import fallbackServiceParam from '#app/routes/utils/fetchPageData/utils/getRouteProps/fallbackServiceParam';
-import isAmpPath from '#app/routes/utils/isAmpPath/index.js';
-
+import isAmpPath from '#app/routes/utils/isAmpPath';
+import isLiveEnv from '#lib/utilities/isLive';
 import cspHeaderResponse from './utilities/cspHeaderResponse';
 
 const LOCALHOST_DOMAINS = ['localhost', '127.0.0.1'];
 
 export function middleware(request: NextRequest) {
-  // Service worker is registered at the root (e.g. /pidgin) so will work as is on Test/Live
-  // but will not work on localhost. This middleware rewrites the request to the sw.js file found in the 'public' folder
   const hostname = request.headers.get('host') ?? request.nextUrl.hostname;
   const isAmp = isAmpPath(request.url);
   const service = fallbackServiceParam(request.url);
 
+  // Service worker is registered at the root (e.g. /pidgin) so will work as is on Test/Live
+  // but will not work on localhost. This middleware rewrites the request to the sw.js file found in the 'public' folder
   if (LOCALHOST_DOMAINS.includes(hostname.split(':')[0])) {
     if (request.nextUrl.pathname.endsWith('/sw.js')) {
       return NextResponse.rewrite(new URL('/sw.js', request.url));
     }
+  } else if (process.env.NODE_ENV === 'production') {
+    return cspHeaderResponse({
+      request,
+      service,
+      isAmp,
+      isLive: isLiveEnv(),
+      reportOnlyOnLive: service === 'japanese',
+    });
   }
 
-  const responseWithCsp = cspHeaderResponse({ request, service, isAmp });
-
-  return responseWithCsp;
+  return NextResponse.next();
 }
-
-// Only run middleware for sw.js requests
-// export const config = {
-//   matcher: '/(.*)/sw.js',
-// };
