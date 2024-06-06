eb07ca9e7c7abd7092a6dc9f8ba3a65b63ef8cb1
Harvey Peachey <harvey.peachey@bbc.co.uk>
2024-06-06 16:24:31 +0100

    adds more meaningful error when provider is not used with LiveRegion

--- src/app/components/LiveRegion/index.tsx
+++ src/app/components/LiveRegion/index.tsx
@@ -5,6 +5,12 @@ import { useLiveRegionContext } from './LiveRegionContext';
 const LiveRegion = () => {
   const { liveRegionItem } = useLiveRegionContext();
 
+  if (!liveRegionItem) {
+    throw new Error(
+      'You cannot render the LiveRegion component without having the LiveRegioContextProvider higher up in the component hierarchy, due to it relying on state declared in the provider',
+    );
+  }
+
   return (
     <VisuallyHiddenText aria-live="polite">{liveRegionItem}</VisuallyHiddenText>
   );
