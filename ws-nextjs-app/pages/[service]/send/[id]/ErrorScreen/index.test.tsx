993fe2fa3c3d345d5f6cc9734691e6ce87e7dae7
Aaron Moore <aaron.moore@bbc.co.uk>
2024-06-19 09:51:37 +0100

    Add basic tests

---
+++ ws-nextjs-app/pages/[service]/send/[id]/ErrorScreen/index.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  act,
+  render,
+} from '#app/components/react-testing-library-with-providers';
+import ErrorScreen from '.';
+
+jest.mock('next/router', () => ({
+  useRouter: () => ({ query: { id: 'u1234' } }),
+}));
+
+const MOCK_TITLE = 'Submit your suggestions';
+
+describe('SuccessScreen', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('Should have an H1 tag', async () => {
+    const { container } = await act(() =>
+      render(<ErrorScreen title={MOCK_TITLE} />),
+    );
+
+    const h1 = container.querySelector('h1');
+
+    expect(h1?.innerHTML).toBe('Sorry, your message could not be sent.');
+  });
+
+  it('Should have a P tag', async () => {
+    const { container } = await act(() =>
+      render(<ErrorScreen title={MOCK_TITLE} />),
+    );
+
+    const p = container.querySelector('p');
+
+    expect(p?.innerHTML).toBe('Please try again later.');
+  });
+});
