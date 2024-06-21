b7d3587a0eb9f08a35b2a2d5fc5b010dbd055e53
Aaron Moore <aaron.moore@bbc.co.uk>
2024-06-21 09:07:16 +0100

    Update bundleSizeConfig.js

--- scripts/bundleSize/bundleSizeConfig.js
+++ scripts/bundleSize/bundleSizeConfig.js
@@ -7,5 +7,5 @@
  * We are allowing a variance of -5 on `MIN_SIZE` and +5 on `MAX_SIZE` to avoid the need for frequent changes, as bundle sizes can fluctuate
  */
 
-export const MIN_SIZE = 641 - 5;
-export const MAX_SIZE = 1087 + 5;
+export const MIN_SIZE = 635 - 5;
+export const MAX_SIZE = 1079 + 5;
