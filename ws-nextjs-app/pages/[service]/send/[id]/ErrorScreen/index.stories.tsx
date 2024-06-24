2cfaad10adcc05d57c212227a2316623e90313ec
Aaron Moore <aaron.moore@bbc.co.uk>
2024-06-24 11:30:06 +0100

    WSTEAM1-972 - Uploader error screen (#11727)
    * WSTEAM1-972 - Uploader error screen
    
    * Update send.stories.tsx
    
    * Fix webvitals pageType
    
    * Update send.stories.tsx
    
    * Update send.stories.tsx
    
    * Update index.tsx
    
    * Move Error screen to own folder + rename SuccessMessage to SuccessScreen
    
    * revert default screen
    
    * Use `useLayoutEffect`
    
    * Fix storybook type
    
    * Add basic tests
    
    * Update index.test.tsx
    
    * Rename `Form` to `FormScreen`
    
    * Update index.tsx

---
+++ ws-nextjs-app/pages/[service]/send/[id]/ErrorScreen/index.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ErrorScreen from '.';
+
+const Component = () => <ErrorScreen title="Submit your suggestions" />;
+
+export default {
+  title: 'Components/FormScreens/ErrorScreen',
+  component: Component,
+};
+
+export const Example = Component;
