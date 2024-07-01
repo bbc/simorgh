9af3bfade3785aa9d0c9b90d1cee13203f7d307a
“LilyL0u” <lily.higham@bbc.co.uk>
2024-07-01 12:57:05 +0100

    fix ts issues with missing position in billboard tests

--- src/app/components/Billboard/index.test.tsx
+++ src/app/components/Billboard/index.test.tsx
@@ -12,6 +12,7 @@ import * as clickTracking from '../../hooks/useClickTrackerHandler';
 describe('Billboard', () => {
   const summary = kyrgyzBillboard.summaries[0];
   const { title, description, link, imageUrl, imageAlt } = summary;
+  const { position } = kyrgyzBillboard;
 
   beforeEach(() => {
     jest.clearAllMocks();
@@ -25,6 +26,7 @@ describe('Billboard', () => {
         link={link}
         image={imageUrl}
         altText={imageAlt}
+        position={3}
       />,
     );
     const region = screen.getByRole('region');
@@ -39,6 +41,7 @@ describe('Billboard', () => {
         link={link}
         image={imageUrl}
         altText={imageAlt}
+        position={position}
       />,
     );
     const heading = screen.getByRole('heading', { level: 2, name: title });
@@ -56,6 +59,7 @@ describe('Billboard', () => {
         link={link}
         image={imageUrl}
         altText={imageAlt}
+        position={position}
       />,
     );
     expect(
@@ -71,6 +75,7 @@ describe('Billboard', () => {
         link={link}
         image={imageUrl}
         altText={imageAlt}
+        position={position}
       />,
     );
     expect(screen.getByText(description).nodeName).toBe('P');
@@ -84,6 +89,7 @@ describe('Billboard', () => {
         link={link}
         image={imageUrl}
         altText={imageAlt}
+        position={position}
       />,
     );
     const maskedImage = getByRole('img');
@@ -98,6 +104,7 @@ describe('Billboard', () => {
         link={link}
         image={imageUrl}
         altText={imageAlt}
+        position={position}
       />,
     );
     const maskedImage = getByAltText(imageAlt);
@@ -118,6 +125,7 @@ describe('Billboard', () => {
             link={link}
             image={imageUrl}
             altText={imageAlt}
+            position={position}
           />,
         );
 
@@ -133,6 +141,7 @@ describe('Billboard', () => {
             image={imageUrl}
             eventTrackingData={eventTrackingData}
             altText={imageAlt}
+            position={position}
           />,
         );
 
@@ -153,6 +162,7 @@ describe('Billboard', () => {
             link={link}
             image={imageUrl}
             altText={imageAlt}
+            position={position}
           />,
         );
 
@@ -172,6 +182,7 @@ describe('Billboard', () => {
             image={imageUrl}
             eventTrackingData={eventTrackingData}
             altText={imageAlt}
+            position={position}
           />,
         );
 
@@ -189,6 +200,7 @@ describe('Billboard', () => {
             image={imageUrl}
             eventTrackingData={eventTrackingData}
             altText={imageAlt}
+            position={position}
           />,
         );
 
@@ -210,6 +222,7 @@ describe('Billboard', () => {
           image={imageUrl}
           altText={imageAlt}
           showLiveLabel
+          position={position}
         />,
       );
 
@@ -229,6 +242,7 @@ describe('Billboard', () => {
           image={imageUrl}
           altText={imageAlt}
           showLiveLabel
+          position={position}
         />,
         {
           service: 'kyrgyz',
