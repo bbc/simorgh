935bbf282e5a5b2eee99f0e0dbac0a974a9b9f38
Isabella Mitchell <i.mitchell.0707@gmail.com>
2024-07-04 15:51:00 +0100

    WSTEAM1-1137: Remove ref from TextArea

--- ws-nextjs-app/pages/[service]/send/[id]/FormField/TextArea.tsx
+++ ws-nextjs-app/pages/[service]/send/[id]/FormField/TextArea.tsx
@@ -1,97 +1,88 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
-import { ForwardedRef, forwardRef } from 'react';
 import Paragraph from '#app/components/Paragraph';
 import { InputProps } from '../types';
 import Label from './FieldLabel';
 import styles from './styles';
 import InvalidMessageBox from './InvalidMessageBox';
 
-const TextArea = forwardRef(
-  (
-    {
-      id,
-      name,
-      handleChange,
-      handleFocusOut,
-      inputState,
-      label,
-      hasAttemptedSubmit,
-    }: InputProps,
-    ref: ForwardedRef<HTMLElement>,
-  ) => {
-    const {
-      isValid,
-      value = '',
-      required,
-      wasInvalid,
-      wordLimit,
-      messageCode,
-    } = inputState ?? {};
-    const hasWordLimit = !!wordLimit;
-    const translation = `Maximum ${wordLimit} Words`; // hardcoded
-    const describedByWordLimit = `${id}-wordLimit`;
-    const useErrorTheme = hasAttemptedSubmit && !isValid;
-    const labelId = `label-${id}`;
-    const errorBoxId = `${id}-error`;
+export default ({
+  id,
+  name,
+  handleChange,
+  handleFocusOut,
+  inputState,
+  label,
+  hasAttemptedSubmit,
+}: InputProps) => {
+  const {
+    isValid,
+    value = '',
+    required,
+    wasInvalid,
+    wordLimit,
+    messageCode,
+  } = inputState ?? {};
+  const hasWordLimit = !!wordLimit;
+  const translation = `Maximum ${wordLimit} Words`; // hardcoded
+  const describedByWordLimit = `${id}-wordLimit`;
+  const useErrorTheme = hasAttemptedSubmit && !isValid;
+  const labelId = `label-${id}`;
+  const errorBoxId = `${id}-error`;
 
-    return (
-      <>
-        <Label
-          forId={id}
-          id={labelId}
-          required={required}
-          useErrorTheme={useErrorTheme}
-          labelText={label}
-        />
-        {hasWordLimit && (
-          <Paragraph
-            css={[
-              styles.maxWordLabel,
-              useErrorTheme && styles.erroredMaxWordLabel,
-            ]}
-            fontVariant="sansRegular"
-            size="brevier"
-            id={describedByWordLimit}
-          >
-            {translation}
-          </Paragraph>
-        )}
-        <textarea
-          id={id}
+  return (
+    <>
+      <Label
+        forId={id}
+        id={labelId}
+        required={required}
+        useErrorTheme={useErrorTheme}
+        labelText={label}
+      />
+      {hasWordLimit && (
+        <Paragraph
           css={[
-            styles.textField,
-            styles.textArea,
-            styles.focusIndicator,
-            useErrorTheme && styles.textFieldError,
+            styles.maxWordLabel,
+            useErrorTheme && styles.erroredMaxWordLabel,
           ]}
-          name={name}
-          value={value as string}
-          onChange={e => handleChange(e.target.name, e.target.value)}
-          onBlur={e => handleFocusOut(e.target.name)}
-          {...(hasWordLimit && { 'aria-describedby': describedByWordLimit })}
-          {...(!hasAttemptedSubmit && { 'aria-invalid': 'false' })}
-          {...(hasAttemptedSubmit && {
-            ...(wasInvalid && { 'aria-invalid': !isValid }),
-            ...(required && !isValid && { 'aria-required': required }),
-            ...(!isValid && {
-              'aria-describedby':
-                errorBoxId + (hasWordLimit ? ` ${describedByWordLimit}` : ''),
-            }),
-          })}
-          rows={4}
+          fontVariant="sansRegular"
+          size="brevier"
+          id={describedByWordLimit}
+        >
+          {translation}
+        </Paragraph>
+      )}
+      <textarea
+        id={id}
+        css={[
+          styles.textField,
+          styles.textArea,
+          styles.focusIndicator,
+          useErrorTheme && styles.textFieldError,
+        ]}
+        name={name}
+        value={value as string}
+        onChange={e => handleChange(e.target.name, e.target.value)}
+        onBlur={e => handleFocusOut(e.target.name)}
+        {...(hasWordLimit && { 'aria-describedby': describedByWordLimit })}
+        {...(!hasAttemptedSubmit && { 'aria-invalid': 'false' })}
+        {...(hasAttemptedSubmit && {
+          ...(wasInvalid && { 'aria-invalid': !isValid }),
+          ...(required && !isValid && { 'aria-required': required }),
+          ...(!isValid && {
+            'aria-describedby':
+              errorBoxId + (hasWordLimit ? ` ${describedByWordLimit}` : ''),
+          }),
+        })}
+        rows={4}
+      />
+      {hasAttemptedSubmit && !isValid && (
+        <InvalidMessageBox
+          id={errorBoxId}
+          messageCode={messageCode}
+          suffix={label}
         />
-        {hasAttemptedSubmit && !isValid && (
-          <InvalidMessageBox
-            id={errorBoxId}
-            messageCode={messageCode}
-            suffix={label}
-            {...(ref && { ref })}
-          />
-        )}
-      </>
-    );
-  },
-);
-
-export default TextArea;
+      )}
+    </>
+  );
+};
