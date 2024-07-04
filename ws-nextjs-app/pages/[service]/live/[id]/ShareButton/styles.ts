3eca2064984c859f167b3fca6b5987a3529ed62d
Harvey Peachey <harvey.peachey@bbc.co.uk>
2024-07-04 12:33:44 +0100

    adds vertical alignment styling

--- ws-nextjs-app/pages/[service]/live/[id]/ShareButton/styles.ts
+++ ws-nextjs-app/pages/[service]/live/[id]/ShareButton/styles.ts
@@ -1,55 +1,58 @@
 import { Theme, css } from '@emotion/react';
 import pixelsToRem from '../../../../../../src/app/utilities/pixelsToRem';
 
-export default ({ palette, fontSizes, fontVariants, spacings, mq }: Theme) =>
-  css({
-    color: palette.BLACK,
-    ...fontSizes.pica,
-    ...fontVariants.sansBold,
-    padding: `${spacings.FULL + spacings.HALF}rem`,
-    marginTop: `${spacings.FULL}rem`,
-    marginBottom: `${spacings.TRIPLE}rem`,
-    marginInlineStart: `${spacings.DOUBLE}rem`,
-    border: `${pixelsToRem(2)}rem solid ${palette.BLACK}`,
-    backgroundColor: 'transparent',
-    cursor: 'pointer',
-    '&:hover, &:focus-visible': {
-      border: `${pixelsToRem(2)}rem solid ${palette.BRAND_BACKGROUND}`,
-      backgroundColor: palette.BRAND_BACKGROUND,
-      color: palette.WHITE,
-      span: {
+const styles = {
+  button: ({ palette, fontSizes, fontVariants, spacings, mq }: Theme) =>
+    css({
+      color: palette.BLACK,
+      ...fontSizes.pica,
+      ...fontVariants.sansBold,
+      padding: `${spacings.FULL + spacings.HALF}rem`,
+      marginTop: `${spacings.FULL}rem`,
+      marginBottom: `${spacings.TRIPLE}rem`,
+      marginInlineStart: `${spacings.DOUBLE}rem`,
+      border: `${pixelsToRem(2)}rem solid ${palette.BLACK}`,
+      backgroundColor: 'transparent',
+      cursor: 'pointer',
+      textUnderlineOffset: '2px',
+      '&:hover, &:focus-visible': {
+        color: palette.WHITE,
         textDecoration: 'underline',
-      },
-      path: {
-        fill: palette.WHITE,
-        [mq.HIGH_CONTRAST]: {
-          fill: 'canvasText',
+        border: `${pixelsToRem(2)}rem solid ${palette.BRAND_BACKGROUND}`,
+        backgroundColor: palette.BRAND_BACKGROUND,
+        path: {
+          fill: palette.WHITE,
+          [mq.HIGH_CONTRAST]: {
+            fill: 'canvasText',
+          },
         },
+        [mq.HIGH_CONTRAST]: {
+          backgroundColor: 'canvas',
+          color: 'canvasText',
+        },
+      },
+      [mq.GROUP_4_MIN_WIDTH]: {
+        marginBottom: `${spacings.QUADRUPLE}rem`,
       },
       [mq.HIGH_CONTRAST]: {
         color: 'canvasText',
-        backgroundColor: 'canvas',
       },
-    },
-    [mq.GROUP_4_MIN_WIDTH]: {
-      marginBottom: `${spacings.QUADRUPLE}rem`,
-    },
-    [mq.HIGH_CONTRAST]: {
-      color: 'canvasText',
-    },
-    span: {
-      verticalAlign: 'middle',
-    },
-    svg: {
-      verticalAlign: 'middle',
-      width: `${spacings.DOUBLE}rem`,
-      height: `${spacings.DOUBLE}rem`,
-      marginInlineEnd: `${spacings.FULL}rem`,
-      path: {
-        fill: palette.BLACK,
-        [mq.HIGH_CONTRAST]: {
-          fill: 'canvasText',
+      svg: {
+        verticalAlign: 'middle',
+        width: `${spacings.DOUBLE}rem`,
+        height: `${spacings.DOUBLE}rem`,
+        marginInlineEnd: `${spacings.FULL}rem`,
+        path: {
+          fill: palette.BLACK,
+          [mq.HIGH_CONTRAST]: {
+            fill: 'canvasText',
+          },
         },
       },
-    },
-  });
+    }),
+  buttonText: () =>
+    css({
+      verticalAlign: 'middle',
+    }),
+};
+export default styles;
