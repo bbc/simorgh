8d20b36bbf058636ce8dfaf49f1df53281b389ab
Shayne Marc Enzo Ahchoon <shayne.ahchoon@bbc.co.uk>
2024-07-08 11:45:54 +0100

    WSTEAM1-POC: Init

---
+++ src/app/components/Opinion/index.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Component from '.';
+
+export default {
+  title: 'Components/Opinion',
+  component: Component,
+};
+
+export const DefaultComponent = () => <Component />;
---
+++ src/app/components/Opinion/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => {
+  return <div>Hello world!</div>;
+};
