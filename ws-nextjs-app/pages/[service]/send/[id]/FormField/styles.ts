1d4dd61df725ac149f837d4a4ccdcd02ed1e2862
Harvey Peachey <harvey.peachey@bbc.co.uk>
2024-07-10 10:44:41 +0100

    adds fill canvasText to check img

--- ws-nextjs-app/pages/[service]/send/[id]/FormField/styles.ts
+++ ws-nextjs-app/pages/[service]/send/[id]/FormField/styles.ts
@@ -3,7 +3,7 @@ import pixelsToRem from '#app/utilities/pixelsToRem';
 import { Theme, css } from '@emotion/react';
 import { getInlineLinkStyles } from '#app/components/InlineLink/index.styles';
 
-const CHECK_IMG = `data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 117.65 98.87'%3E%3Cpath d='M96.64 0L46.11 60.23 17.63 36.34 0 57.35l28.47 23.89 21.01 17.63 17.63-21.01 50.54-60.22L96.64 0z'/%3E%3C/svg%3E`;
+const CHECK_IMG = `data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='canvasText' viewBox='0 0 117.65 98.87'%3E%3Cpath d='M96.64 0L46.11 60.23 17.63 36.34 0 57.35l28.47 23.89 21.01 17.63 17.63-21.01 50.54-60.22L96.64 0z'/%3E%3C/svg%3E`;
 
 export default {
   formField: ({ spacings, palette, mq }: Theme) =>
@@ -123,18 +123,18 @@ export default {
     }),
   errorMessageBox:
     (hasArrowStyle: boolean) =>
-    ({ palette, spacings }: Theme) =>
-      css({
-        backgroundColor: palette.ERROR_CORE,
-        outline: 'solid 0.0625rem transparent',
-        padding: '0.75rem',
-        display: 'flex',
-        alignItems: 'center',
-        ...(!hasArrowStyle && {
-          marginTop: `${spacings.FULL}rem`,
-          marginBottom: `${spacings.FULL}rem`,
+      ({ palette, spacings }: Theme) =>
+        css({
+          backgroundColor: palette.ERROR_CORE,
+          outline: 'solid 0.0625rem transparent',
+          padding: '0.75rem',
+          display: 'flex',
+          alignItems: 'center',
+          ...(!hasArrowStyle && {
+            marginTop: `${spacings.FULL}rem`,
+            marginBottom: `${spacings.FULL}rem`,
+          }),
         }),
-      }),
   errorArrow: ({ palette, spacings }: Theme) =>
     css({
       backgroundColor: palette.ERROR_CORE,
