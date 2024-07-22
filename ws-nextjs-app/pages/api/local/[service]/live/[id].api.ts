38b1e8b4375bb9bdb56fb69a755d861343ff16bd
alex-magana <alex.magana@andela.com>
2024-07-22 19:15:43 +0300

    Add live pages local API for test data

---
+++ ws-nextjs-app/pages/api/local/[service]/live/[id].api.ts
@@ -0,0 +1,26 @@
+import fs from 'node:fs/promises';
+import path from 'path';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    const { service, id } = req.query;
+    const dataFilePath = path.join(
+      process.cwd(),
+      '..',
+      'data',
+      `${service}`,
+      'livePage',
+      `${id}.json`,
+    );
+    const pageData = await fs.readFile(dataFilePath, {
+      encoding: 'utf8',
+    });
+    res.status(200).send(pageData);
+  } catch (error) {
+    res.status(500).send({ error: `Failed to load data. ${error}` });
+  }
+}
