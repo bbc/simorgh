9e005e5c53cbb4cc4658b316bde55cb7cc869f57
eagerterrier <eagerterrier@gmail.com>
2024-07-24 12:25:17 +0100

    FIX: typescript

--- ws-nextjs-app/pages/[service]/downloads/downloadsPageLayout.tsx
+++ ws-nextjs-app/pages/[service]/downloads/downloadsPageLayout.tsx
@@ -55,7 +55,7 @@ const DownloadsPageLayout = ({ service, pageData }: PageProps) => {
                     padding={false}
                     isRelative={false}
                   />
-                  <CallToActionLink css={styles.cta} href={item.files[0].fileLink} download={true} eventTrackingData={{ componentName: 'koreanDownloads', campaignID: 'korean_downloads', ~/Sites/bbc/simorgh/src/app/lib/analyticsUtils/index.js:275:   advertiserID = index }}>
+                  <CallToActionLink css={styles.cta} href={item.files[0].fileLink} download={true} eventTrackingData={{ componentName: 'koreanDownloads', campaignID: 'korean_downloads', advertiserID = index }}>
                     <svg
                       className="ws-o-download-icon"
                       viewBox="0 0 32 32"
