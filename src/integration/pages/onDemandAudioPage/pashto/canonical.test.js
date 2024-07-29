65bb57849bf72c7be9c5fa51e68af22acfa34cb6
“LilyL0u” <lily.higham@bbc.co.uk>
2024-07-29 16:08:30 +0100

    change file names

--- src/integration/pages/onDemandAudioPage/pashto/canonical.test.js
+++ src/integration/pages/onDemandAudioPage/pashto/canonical.test.js
@@ -3,10 +3,10 @@
  * @pathname /pashto/bbc_pashto_radio/w3ct0lz1
  */
 
-import runCrossPlatformTests from '../crossPlatformTests';
+import runCanonicalTests from '../canonicalTests';
 
 describe('Canonical', () => {
   describe(pageType, () => {
-    runCrossPlatformTests(service);
+    runCanonicalTests(service);
   });
 });
--- src/integration/pages/onDemandAudioPage/pashtoBrand/canonical.test.js
+++ src/integration/pages/onDemandAudioPage/pashtoBrand/canonical.test.js
@@ -3,10 +3,10 @@
  * @pathname /pashto/bbc_pashto_radio/p0340yr4
  */
 
-import runCrossPlatformTests from '../crossPlatformTests';
+import runCanonicalTests from '../canonicalTests';
 
 describe('Canonical', () => {
   describe(pageType, () => {
-    runCrossPlatformTests(service);
+    runCanonicalTests(service);
   });
 });
