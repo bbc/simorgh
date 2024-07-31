b532f073717b6fda58a9935946dca8e47c6b18a8
Isabella Mitchell <i.mitchell.0707@gmail.com>
2024-07-31 16:43:10 +0100

    WSTEAM1-1180: Adds forward ref. Updates snapshots

--- src/app/components/BulletedList/index.tsx
+++ src/app/components/BulletedList/index.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
-import { PropsWithChildren } from 'react';
+import { FC, PropsWithChildren, ForwardedRef, forwardRef } from 'react';
 import { SHADOW } from '#app/components/ThemeProvider/palette';
 import styles from './index.styles';
 
@@ -18,31 +18,37 @@ export const BulletedListItem = ({ children }: PropsWithChildren) => {
   );
 };
 
-export const BulletedList = ({
-  bulletPointShape = 'round',
-  bulletPointColour = SHADOW,
-  className,
-  children,
-}: PropsWithChildren<Props>) => {
-  const showBulletPoints = bulletPointShape !== 'hidden';
-  return (
-    <ul
-      className={className}
-      role="list"
-      css={theme => [
-        styles.bulletedList,
-        showBulletPoints && {
-          '& > li::before': {
-            border: `0.1875rem solid ${theme.isDarkUi ? theme.palette.GREY_4 : bulletPointColour}`,
-            backgroundColor: theme.isDarkUi
-              ? theme.palette.GREY_4
-              : bulletPointColour,
-            borderRadius: bulletPointShape === 'round' ? '50%' : '0',
+export const BulletedList: FC<Props> = forwardRef(
+  (
+    {
+      bulletPointShape = 'round',
+      bulletPointColour = SHADOW,
+      className,
+      children,
+    }: PropsWithChildren<Props>,
+    ref: ForwardedRef<HTMLUListElement>,
+  ) => {
+    const showBulletPoints = bulletPointShape !== 'hidden';
+    return (
+      <ul
+        className={className}
+        role="list"
+        css={theme => [
+          styles.bulletedList,
+          showBulletPoints && {
+            '& > li::before': {
+              border: `0.1875rem solid ${theme.isDarkUi ? theme.palette.GREY_4 : bulletPointColour}`,
+              backgroundColor: theme.isDarkUi
+                ? theme.palette.GREY_4
+                : bulletPointColour,
+              borderRadius: bulletPointShape === 'round' ? '50%' : '0',
+            },
           },
-        },
-      ]}
-    >
-      {children}
-    </ul>
-  );
-};
+        ]}
+        {...(ref && { ref })}
+      >
+        {children}
+      </ul>
+    );
+  },
+);
