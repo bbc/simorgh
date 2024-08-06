dd9bd59167a2cf9a2da914b3fd6397a6d1c0da6f
Aaron Moore <aaron.moore@bbc.co.uk>
2024-08-06 11:13:56 +0100

    AV-Embeds Next.js routing (#11804)
    * init
    
    * Update index.ts
    
    * initial 404 handling
    
    * more validation
    
    * Route examples
    
    * Better route handling
    
    * Update [[...]].page.tsx
    
    * try out queryparam flattening logic
    
    * Update [[...]].page.tsx
    
    * Update [[...]].page.tsx
    
    * Update index.ts
    
    * Update index.ts
    
    * refactor file import
    
    * Update _app.page.tsx
    
    * set pagetype in metadata
    
    * Update index.ts
    
    * Update [[...]].page.tsx
    
    * ThemeProvider fixes
    
    * Update [[...]].page.tsx
    
    * Update index.ts
    
    * ThemeProvider story fix
    
    * Update index.ts
    
    * try using `resolvedUrl` to parse out params
    
    * test refactor
    
    * `embedId` -> `mediaId`
    
    * Update index.ts
    
    * Update [[...]].page.tsx
    
    * Remove query params from `resolvedUrl`
    
    * Remove `id` from `MediaLoader` as it doesn't appear to be needed
    
    * Test data fetch
    
    * Move route handling to `[[...]]` catch-all
    
    * Update handleAvRoute.tsx
    
    * Add basic Storybook story
    
    ---------
    
    Co-authored-by: Toby Cox <eagerterrier@gmail.com>

---
+++ ws-nextjs-app/pages/[service]/av-embeds/handleAvRoute.tsx
@@ -0,0 +1,83 @@
+import { GetServerSidePropsContext } from 'next';
+import extractHeaders from '#server/utilities/extractHeaders';
+import { AV_EMBEDS } from '#app/routes/utils/pageTypes';
+import fetchPageData from '#app/routes/utils/fetchPageData';
+import certsRequired from '#app/routes/utils/certsRequired';
+import getEnvironment from '#app/routes/utils/getEnvironment';
+import { FetchError } from '#app/models/types/fetch';
+import { OptimoBlock } from '#app/models/types/optimo';
+import parseAvRoute from './parseAvRoute';
+import getAgent from '../../../utilities/undiciAgent';
+
+export default async (context: GetServerSidePropsContext) => {
+  const {
+    resolvedUrl,
+    req: { headers: reqHeaders },
+  } = context;
+
+  let pageStatus = 200;
+  let pageJson;
+
+  const env = getEnvironment(resolvedUrl);
+  const agent = certsRequired(resolvedUrl) ? await getAgent() : null;
+
+  const parsedRoute = parseAvRoute(resolvedUrl);
+
+  const extension = `${parsedRoute.service ?? ''}${parsedRoute.variant ? `/${parsedRoute.variant}` : ''}/${parsedRoute.assetId}`;
+
+  const { TEST_API } = process.env;
+
+  // TEMPORARY: This will be changed to the correct API path once its implemented
+  const path = `${TEST_API}/article/${extension}`;
+  const optHeaders = { 'ctx-service-env': env };
+
+  if (TEST_API) {
+    try {
+      // @ts-expect-error Due to jsdoc inference, and no TS within fetchPageData
+      const { status, json } = await fetchPageData({
+        path,
+        agent,
+        optHeaders,
+      });
+
+      pageStatus = status;
+      pageJson = json;
+    } catch (error) {
+      const { status } = error as FetchError;
+
+      pageStatus = status;
+    }
+  }
+
+  const mediaBlock = pageJson?.content?.model?.blocks?.find(
+    (block: OptimoBlock) => ['video', 'audio'].includes(block.type),
+  )?.model?.blocks;
+
+  // Determine actual service and variant (if applicable) from either the parsed route or the data returned from the fetch for the media data from BFF
+  // We should always have a service to be able to display a 404 page if needed
+  const service = parsedRoute?.service ?? 'news';
+  const variant = parsedRoute?.variant ?? null;
+
+  context.res.statusCode = pageStatus;
+
+  return {
+    props: {
+      bbcOrigin: reqHeaders['bbc-origin'] || null,
+      isNextJs: true,
+      isAvEmbeds: true,
+      pageData: parsedRoute
+        ? {
+            input: resolvedUrl,
+            output: parsedRoute,
+            mediaBlock: mediaBlock ?? null,
+            metadata: { type: AV_EMBEDS },
+          }
+        : null,
+      pageType: AV_EMBEDS,
+      service,
+      variant,
+      status: pageStatus,
+      ...extractHeaders(reqHeaders),
+    },
+  };
+};
