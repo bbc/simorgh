91b7fb6a9e459abd6efe41da7dfc361e7c1868db
Aaron Moore <aaron.moore@bbc.co.uk>
2024-08-07 15:00:39 +0100

    Revert "Revert "WSTEAM1-1050: Open CAF to all services"" (reinstate CAF rendering on all services) (#11821)
    * Revert "Revert "WSTEAM1-1050: Open CAF to all services (#11805)" (#11820)"
    
    This reverts commit db1e4739baa59067068e9400428b1f6edeacb5e4.
    
    * [DNM] Remove Welsh service transform logic (#11818)
    
    * Remove `table` component E2E checks (#11819)
    
    * Remove `table` component E2E check
    
    * Update testsForAMPOnly.js
    
    * CPS recommendation block CAF styling fix (#11822)

--- cypress/e2e/pages/mediaAssetPage/testsForAMPOnly.js
+++ cypress/e2e/pages/mediaAssetPage/testsForAMPOnly.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import config from '../../../support/config/services';
 import appConfig from '../../../../src/server/utilities/serviceConfigs';
-import CafEnabledServices from '../../../../src/app/lib/cafServices.const';
 import { getEmbedUrl, hasMedia } from './helpers';
 
 // For testing features that may differ across services but share a common logic e.g. translated strings.
@@ -13,13 +12,10 @@ export const testsThatFollowSmokeTestConfigForAMPOnly = ({
   describe(`testsThatFollowSmokeTestConfigForAMPOnly for ${service} ${pageType}`, () => {
     describe('Media Player', () => {
       const language = appConfig[config[service].name][variant].lang;
-      const pageTypeForFetch = CafEnabledServices.includes(service)
-        ? 'article'
-        : 'cpsAsset';
 
       it('should render an iframe with a valid URL', () => {
         if (!`${Cypress.env('currentPath')}`.includes('/russian/av/')) {
-          cy.getPageData({ service, pageType: pageTypeForFetch, variant }).then(
+          cy.getPageData({ service, pageType: 'article', variant }).then(
             ({ body }) => {
               const {
                 data: { article: jsonData },
