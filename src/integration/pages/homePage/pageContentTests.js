c28679c6c2d005ba84af22c3672f319525f3d29f
Nabeel Khan <nabeel.khan@bbc.co.uk>
2024-08-09 10:37:36 +0100

    re-revert

--- src/integration/pages/homePage/pageContentTests.js
+++ src/integration/pages/homePage/pageContentTests.js
@@ -14,8 +14,12 @@ export default () => {
     const hierarchicalGrid = document.querySelector(
       '[data-testid="hierarchical-grid"]',
     );
-    // hello
 
     expect(hierarchicalGrid).toBeInTheDocument();
+  });
+
+  it('should render the main html tag with an attribute of role with the value of main', () => {
+    const mainTag = document.querySelector("main[role='main']");
+    expect(mainTag).toBeInTheDocument();
   });
 };
