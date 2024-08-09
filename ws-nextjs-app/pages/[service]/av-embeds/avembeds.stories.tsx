4837a33dcf3f3fdc3a1178cf246cee95de0bdaf5
Aaron Moore <aaron.moore@bbc.co.uk>
2024-08-09 15:37:45 +0100

    Storybook update

--- ws-nextjs-app/pages/[service]/av-embeds/avembeds.stories.tsx
+++ ws-nextjs-app/pages/[service]/av-embeds/avembeds.stories.tsx
@@ -1,16 +1,143 @@
 import React from 'react';
+import { MediaBlock } from '#app/components/MediaLoader/types';
 import AvEmbedsPage from './AvEmbedsPageLayout';
-import parseAvRoute from './parseAvRoute';
 
-const Component = ({ requestUrl }: { requestUrl: string }) => {
-  const parsedAvRoute = parseAvRoute(requestUrl);
+const fixtureData = {
+  data: {
+    article: {
+      metadata: {
+        id: 'urn:bbc:ares::article:c1xv2q1gewvo',
+        locators: {
+          optimoUrn: 'urn:bbc:optimo:asset:c1xv2q1gewvo',
+          canonicalUrl: 'https://www.bbc.com/mundo/articles/c1xv2q1gewvo',
+        },
+        type: 'article',
+        createdBy: 'Mundo',
+        language: 'es',
+        lastUpdated: 1721666895000,
+        firstPublished: 1710243691008,
+        lastPublished: 1710243691008,
+        timestamp: 1710243691008,
+        options: {
+          includeComments: false,
+        },
+        analyticsLabels: {
+          contentId: 'urn:bbc:optimo:asset:c1xv2q1gewvo',
+          producer: 'Mundo',
+          page: 'mundo.articles.c1xv2q1gewvo.page',
+          irisKeyword: null,
+        },
+      },
+      content: {
+        model: {
+          blocks: [
+            {
+              type: 'aresMedia',
+              model: {
+                blocks: [
+                  {
+                    blockId: 'urn:bbc:ares::clip:p01w67hw',
+                    type: 'aresMediaMetadata',
+                    model: {
+                      id: 'p01w67hw',
+                      subType: 'clip',
+                      format: 'video',
+                      title: 'test landscape',
+                      synopses: {
+                        short: 'landscape summary',
+                      },
+                      imageUrl:
+                        'ichef.test.bbci.co.uk/images/ic/$recipe/p01w67j6.jpg',
+                      imageCopyright: 'BBC',
+                      embedding: true,
+                      advertising: true,
+                      versions: [
+                        {
+                          versionId: 'p01w67hy',
+                          types: ['Original'],
+                          duration: 30,
+                          durationISO8601: 'PT30S',
+                          warnings: {},
+                          availableTerritories: {
+                            uk: true,
+                            nonUk: true,
+                          },
+                          availableFrom: 1721654872000,
+                        },
+                      ],
+                      syndication: {
+                        destinations: [],
+                      },
+                      smpKind: 'programme',
+                      webcastVersions: [],
+                    },
+                  },
+                  {
+                    type: 'image',
+                    model: {
+                      blocks: [
+                        {
+                          type: 'rawImage',
+                          model: {
+                            width: 1920,
+                            height: 1080,
+                            locator:
+                              'ichef.test.bbci.co.uk/images/ic/$widthxn/p01w67j6.jpg',
+                            originCode: 'mpv',
+                            copyrightHolder: 'BBC',
+                          },
+                        },
+                        {
+                          type: 'altText',
+                          model: {
+                            blocks: [
+                              {
+                                type: 'text',
+                                model: {
+                                  blocks: [
+                                    {
+                                      type: 'paragraph',
+                                      model: {
+                                        text: 'Keyframe #2',
+                                        blocks: [
+                                          {
+                                            type: 'fragment',
+                                            model: {
+                                              text: 'Keyframe #2',
+                                              attributes: [],
+                                            },
+                                          },
+                                        ],
+                                      },
+                                    },
+                                  ],
+                                },
+                              },
+                            ],
+                          },
+                        },
+                      ],
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
+    },
+  },
+};
 
-  const fixtureData = {
-    input: requestUrl,
-    output: parsedAvRoute,
-  };
-
-  return <AvEmbedsPage pageData={fixtureData} />;
+const Component = () => {
+  return (
+    <AvEmbedsPage
+      pageData={{
+        mediaBlock: fixtureData.data.article.content.model
+          .blocks as unknown as MediaBlock[],
+      }}
+    />
+  );
 };
 
 export default {
@@ -21,6 +148,4 @@ export default {
   },
 };
 
-export const Example = () => (
-  <Component requestUrl="serbian/cyr/av-embeds/srbija-50103048/pid/p07rfhrv" />
-);
+export const Example = () => <Component />;
