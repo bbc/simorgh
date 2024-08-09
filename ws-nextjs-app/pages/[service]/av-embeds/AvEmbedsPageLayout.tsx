91d792fd4fa626925c2567a9dd061adb13f6f071
Aaron Moore <aaron.moore@bbc.co.uk>
2024-08-09 10:56:06 +0100

    Update AvEmbedsPageLayout.tsx

--- ws-nextjs-app/pages/[service]/av-embeds/AvEmbedsPageLayout.tsx
+++ ws-nextjs-app/pages/[service]/av-embeds/AvEmbedsPageLayout.tsx
@@ -1,23 +1,31 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/react';
+import { Global, css, jsx } from '@emotion/react';
 import MediaLoader from '#app/components/MediaLoader';
 import { AvEmbedsPageProps } from './types';
 
 const AvEmbedsPageLayout = ({ pageData }: AvEmbedsPageProps) => {
   return (
-    <div style={{ maxWidth: 600 }}>
-      <h1>AV Embeds</h1>
-      {pageData?.mediaBlock && pageData?.mediaBlock?.length > 0 && (
-        <MediaLoader
-          blocks={pageData?.mediaBlock}
-          css={{
-            paddingBottom: 0,
-            aspectRatio: '16/9',
-            '> div': { aspectRatio: 'auto' },
-          }}
-        />
-      )}
-    </div>
+    <>
+      <Global
+        styles={css`
+          body {
+            margin: 0;
+          }
+        `}
+      />
+      <div>
+        {pageData?.mediaBlock && pageData?.mediaBlock?.length > 0 && (
+          <MediaLoader
+            blocks={pageData?.mediaBlock}
+            css={{
+              paddingBottom: 0,
+              aspectRatio: '16/9',
+              '> div': { aspectRatio: 'auto' },
+            }}
+          />
+        )}
+      </div>
+    </>
   );
 };
 
