16e125c6df4c86369e1a671be8674c54dbddd194
Aaron Moore <aaron.moore@bbc.co.uk>
2024-08-12 11:31:27 +0100

    Add basic render test

--- ws-nextjs-app/pages/[service]/av-embeds/AvEmbedsPageLayout.tsx
+++ ws-nextjs-app/pages/[service]/av-embeds/AvEmbedsPageLayout.tsx
@@ -7,11 +7,11 @@ import styles from './styles';
 const AvEmbedsPageLayout = ({ pageData }: AvEmbedsPageProps) => (
   <>
     <Global styles={styles.global} />
-    <div>
-      {pageData?.mediaBlock && pageData?.mediaBlock?.length > 0 && (
+    {pageData?.mediaBlock && pageData?.mediaBlock?.length > 0 && (
+      <div data-testid="avembeds-mediaplayer">
         <MediaLoader blocks={pageData?.mediaBlock} css={styles.mediaPlayer} />
-      )}
-    </div>
+      </div>
+    )}
   </>
 );
 
