369619bc59b91b1869e18b2e28522c4b0017e901
Karina Thomas <karina.thomas@bbc.co.uk>
2024-08-16 13:17:00 +0100

    Remove explicit dependency on the history library

--- src/app/legacy/containers/PageHandlers/withVariant/index.test.jsx
+++ src/app/legacy/containers/PageHandlers/withVariant/index.test.jsx
@@ -1,127 +1,66 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
-import { render } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
+import { MemoryRouter, Route } from 'react-router-dom';
 import { frontPagePath } from '#app/routes/utils/regex';
+import { render } from '../../../../components/react-testing-library-with-providers';
 import WithVariant from '.';
 
+jest.mock('react-router-dom', () => {
+  return {
+    ...jest.requireActual('react-router-dom'),
+    Redirect: jest.fn(({ to }) => `Redirected to ${to}`),
+  };
+});
+
 describe('WithVariant', () => {
-  const Component = () => <h1>This is the BBC.</h1>;
+  const Component = ({ service }) => <p>This is BBC {service}</p>;
   const ComponentWithVariantRedirect = WithVariant(Component);
+
   const getMatchProps = (service, path = null) => ({
     path: path || frontPagePath,
     params: {
       service,
     },
   });
-
-  describe('service with no default variant', () => {
-    it('should not redirect', () => {
-      const service = 'news';
+  describe('services without variants', () => {
+    it.each`
+      service
+      ${'news'}
+      ${'uzbek'}
+    `('should render the $service page', ({ service }) => {
       const match = getMatchProps(service);
-      const history = createMemoryHistory({
-        initialEntries: [`/${service}`],
-      });
 
-      expect(history.location.pathname).toEqual(`/${service}`);
-
-      render(
-        <Router history={history}>
+      const { queryByText } = render(
+        <MemoryRouter initialEntries={[`/${service}`]}>
           <Route path="/:service">
             <ComponentWithVariantRedirect match={match} service={service} />
           </Route>
-        </Router>,
+        </MemoryRouter>,
       );
 
-      expect(history.location.pathname).toEqual('/news');
+      expect(queryByText(`This is BBC ${service}`).toBeInTheDocument);
+      expect(queryByText(`Redirected to ${service}`)).not.toBeInTheDocument();
     });
   });
 
-  describe('service (ukchina) with default variant', () => {
-    it('should redirect to ukchina/simp', () => {
-      const service = 'ukchina';
+  describe('services with default variants', () => {
+    it.each`
+      service       | redirect
+      ${'serbian'}  | ${'/serbian/lat'}
+      ${'ukchina'}  | ${'/ukchina/simp'}
+      ${'zhongwen'} | ${'/zhongwen/simp'}
+    `('should redirect from $service to $redirect', ({ service, redirect }) => {
       const match = getMatchProps(service);
-      const history = createMemoryHistory({
-        initialEntries: [`/${service}`],
-      });
 
-      expect(history.location.pathname).toEqual(`/${service}`);
-
-      render(
-        <Router history={history}>
+      const { queryByText } = render(
+        <MemoryRouter initialEntries={[`/${service}`]}>
           <Route path="/:service">
             <ComponentWithVariantRedirect match={match} service={service} />
           </Route>
-        </Router>,
+        </MemoryRouter>,
       );
 
-      expect(history.location.pathname).toEqual('/ukchina/simp');
-    });
-  });
-
-  describe('service (zhongwen) with default variant', () => {
-    it('should redirect to zhongwen/simp', () => {
-      const service = 'zhongwen';
-      const match = getMatchProps(service);
-      const history = createMemoryHistory({
-        initialEntries: [`/${service}`],
-      });
-
-      expect(history.location.pathname).toEqual(`/${service}`);
-
-      render(
-        <Router history={history}>
-          <Route path="/:service">
-            <ComponentWithVariantRedirect match={match} service={service} />
-          </Route>
-        </Router>,
-      );
-
-      expect(history.location.pathname).toEqual('/zhongwen/simp');
-    });
-  });
-
-  describe('service (serbian) with default variant', () => {
-    it('should redirect to serbian/lat', () => {
-      const service = 'serbian';
-      const match = getMatchProps(service);
-      const history = createMemoryHistory({
-        initialEntries: [`/${service}`],
-      });
-
-      expect(history.location.pathname).toEqual(`/${service}`);
-
-      render(
-        <Router history={history}>
-          <Route path="/:service">
-            <ComponentWithVariantRedirect match={match} service={service} />
-          </Route>
-        </Router>,
-      );
-
-      expect(history.location.pathname).toEqual('/serbian/lat');
-    });
-  });
-
-  describe('service (uzbek) with default variant', () => {
-    it('should not redirect', () => {
-      const service = 'uzbek';
-      const match = getMatchProps(service);
-      const history = createMemoryHistory({
-        initialEntries: [`/${service}`],
-      });
-
-      expect(history.location.pathname).toEqual(`/${service}`);
-
-      render(
-        <Router history={history}>
-          <Route path="/:service">
-            <ComponentWithVariantRedirect match={match} service={service} />
-          </Route>
-        </Router>,
-      );
-
-      expect(history.location.pathname).toEqual('/uzbek');
+      expect(queryByText(`This is BBC ${service}`)).not.toBeInTheDocument();
+      expect(queryByText(`Redirected to ${redirect}`)).toBeInTheDocument();
     });
   });
 });
