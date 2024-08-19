5008718a60afa173de7e7e0daff41a2c2e00f11c
Shayne Marc Enzo Ahchoon <shayne.ahchoon@bbc.co.uk>
2024-08-19 11:16:35 +0100

    WSTEAM1-1177: Update

--- src/app/components/ATIAnalytics/canonical/sendBeaconOperaMiniScript.ts
+++ src/app/components/ATIAnalytics/canonical/sendBeaconOperaMiniScript.ts
@@ -1,15 +1,14 @@
 const sendBeaconOperaMiniScript = (atiPageViewUrlString: string) => `
-    console.log("TEST CHECK1", runCount)
-    var runCount = 0;
-    if (runCount <= 1) {
-      runCount += 1;
-      console.log("TEST CHECK2", runCount)
+    console.log("TEST CHECK1", window.hasOperaMinScriptRun)
+    if (!Boolean(window.hasOperaMinScriptRun)) {
+      window.hasOperaMinScriptRun = true;
+      console.log("TEST CHECK2")
       var xhr = new XMLHttpRequest();
       xhr.open("GET", "${atiPageViewUrlString}", true);
       xhr.withCredentials = true;
       xhr.send();
     }
-    console.log("TEST CHECK3", runCount)
+    console.log("TEST CHECK3", window.hasOperaMinScriptRun)
 `;
 
 export default sendBeaconOperaMiniScript;
