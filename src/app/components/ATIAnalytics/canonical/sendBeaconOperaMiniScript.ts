5d78e2c9056007599420288ec67f7eb44aa1c688
Shayne Marc Enzo Ahchoon <shayne.ahchoon@bbc.co.uk>
2024-08-19 13:59:31 +0100

    WSTEAM1-1186: Refactor

--- src/app/components/ATIAnalytics/canonical/sendBeaconOperaMiniScript.ts
+++ src/app/components/ATIAnalytics/canonical/sendBeaconOperaMiniScript.ts
@@ -1,8 +1,8 @@
 import isOperaProxy from '#app/lib/utilities/isOperaProxy';
 
 const sendBeaconOperaMiniScript = (atiPageViewUrlString: string) => `
-    if (${isOperaProxy.toString()}() && !Boolean(window.hasOperaMinScriptRun)) {
-      window.hasOperaMinScriptRun = true;
+    if (${isOperaProxy.toString()}() && !Boolean(window.hasOperaMinScriptRan)) {
+      window.hasOperaMinScriptRan = true;
 
       var xhr = new XMLHttpRequest();
       xhr.open("GET", "${atiPageViewUrlString}", true);
