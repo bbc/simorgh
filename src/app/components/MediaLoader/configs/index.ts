83311c8dc2648db816d61830ab7293b981180d6d
chrish01 <holly.christopher@bbc.co.uk>
2024-08-20 11:04:13 +0100

    refactor blockTypeMapping

--- src/app/components/MediaLoader/configs/index.ts
+++ src/app/components/MediaLoader/configs/index.ts
@@ -1,25 +1,22 @@
 import filterForBlockType from '#app/lib/utilities/blockHandlers';
 import clipMedia from './clipMedia';
 import aresMedia from './aresMedia';
-import { AresMediaBlock, ClipMediaBlock, MediaBlock } from '../types';
+import tvMedia from './tvMedia';
+import { MediaBlock } from '../types';
+
+const blockTypeMapping = {
+  aresMedia: aresMedia,
+  clipMedia: clipMedia,
+  tvMedia: tvMedia,
+};
 
 export default (blocks: MediaBlock[]) => {
-  const aresMediaBlock: AresMediaBlock = filterForBlockType(
-    blocks,
-    'aresMedia',
-  );
-
-  const clipMediaBlock: ClipMediaBlock = filterForBlockType(
-    blocks,
-    'clipMedia',
-  );
-
-  switch (true) {
-    case !!aresMediaBlock:
-      return aresMedia;
-    case !!clipMediaBlock:
-      return clipMedia;
-    default:
-      return null;
-  }
+  ['aresMedia', 'clipMedia', 'tvMedia'].forEach(blockType => {
+    const mediaBlock = filterForBlockType(blocks, blockType);
+    if (!!mediaBlock) {
+      // @ts-expect-error wip, we will fix this!
+      const config = blockTypeMapping[blockType];
+      return config;
+    }
+  });
 };
