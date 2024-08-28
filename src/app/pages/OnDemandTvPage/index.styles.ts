f28ce99b521c8d64fa9209f0369c6b42ad87de5e
Karina Thomas <karina.thomas@bbc.co.uk>
2024-08-28 09:47:22 +0100

    Fix styling to ensure margins and padding are working as expected

--- src/app/pages/OnDemandTvPage/index.styles.ts
+++ src/app/pages/OnDemandTvPage/index.styles.ts
@@ -12,16 +12,16 @@ export default {
   mediaPlayer: ({ mq, spacings }: Theme) =>
     css({
       [mq.GROUP_4_MIN_WIDTH]: {
-        margin: `${spacings.QUINTUPLE}rem 0 0`,
+        margin: `${spacings.QUINTUPLE}rem 0px 0px`,
       },
       [mq.GROUP_3_MAX_WIDTH]: {
         marginTop: `${spacings.DOUBLE}rem`,
       },
       [mq.GROUP_2_MAX_WIDTH]: {
-        margin: `${spacings.DOUBLE}rem -${spacings.DOUBLE}rem 0`,
+        margin: `${spacings.DOUBLE}rem -${spacings.DOUBLE}rem 0px`,
       },
       [mq.GROUP_1_MAX_WIDTH]: {
-        margin: `${spacings.FULL}rem -${spacings.FULL}rem 0`,
+        margin: `${spacings.FULL}rem -${spacings.FULL}rem 0px`,
       },
     }),
 };
