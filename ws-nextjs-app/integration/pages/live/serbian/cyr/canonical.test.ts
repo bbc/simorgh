85f2c574e45a73df3d5fa9d6590b30bb1698c4c2
Pedram Valiani <pedram.valiani@bbc.co.uk>
2024-08-28 14:00:05 +0100

    adds new integration tests for serbian live cps - not yet working

---
+++ ws-nextjs-app/integration/pages/live/serbian/cyr/canonical.test.ts
@@ -0,0 +1,12 @@
+/**
+ * @service serbian
+ * @pathname /serbian/cyr/live/media-23179005
+ */
+
+import { runHeaderTests } from '../../../../../../src/integration/common';
+
+describe('Canonical', () => {
+  describe(pageType, () => {
+    runHeaderTests();
+  });
+});
