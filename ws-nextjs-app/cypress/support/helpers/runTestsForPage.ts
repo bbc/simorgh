f5d54713da99241f630599e6789349ab44080ad1
emilysaffron <emily.saffron@bbc.co.uk>
2024-08-28 11:32:37 +0100

    spread params

--- ws-nextjs-app/cypress/support/helpers/runTestsForPage.ts
+++ ws-nextjs-app/cypress/support/helpers/runTestsForPage.ts
@@ -1,18 +1,9 @@
 export default ({ pageType, testSuites }) => {
   testSuites.forEach(testData => {
-    const { path, id, service, tests } = testData;
-
-    const params = {
-      pageType,
-      path,
-      id,
-      service,
-    };
-
+    const { path, tests, ...params } = testData;
     before(() => {
       cy.visit(path);
     });
-
-    tests.forEach(test => test(params));
+    tests.forEach(test => test({ path, pageType, ...params }));
   });
 };
