e7df74edb5aca0b6b3e54e8a9fb837c2cbb4b533
Pedram Valiani <pedram.valiani@bbc.co.uk>
2024-08-29 10:15:13 +0100

    update integration tests and json route to check if they act as expected

--- ws-nextjs-app/integration/pages/live/serbian/canonical.test.ts
+++ ws-nextjs-app/integration/pages/live/serbian/canonical.test.ts
@@ -3,10 +3,10 @@
  * @pathname /serbian/live/media-23179005/lat
  */
 
-import { runHeaderTests } from '../../../../../src/integration/common';
+import { runCommonCrossPlatformTests } from '../../../../../src/integration/common';
 
 describe('Canonical', () => {
   describe(pageType, () => {
-    runHeaderTests();
+    runCommonCrossPlatformTests();
   });
 });
