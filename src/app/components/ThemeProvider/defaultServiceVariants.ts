d286a5d3967afd3942a59c100aeb473424c966ec
eagerterrier <eagerterrier@gmail.com>
2024-09-10 14:34:49 +0100

    TMP: no idea. Was left here when I got back to my machine

--- src/app/components/ThemeProvider/defaultServiceVariants.ts
+++ src/app/components/ThemeProvider/defaultServiceVariants.ts
@@ -1,7 +1,7 @@
 // This is to support local 404 and 500 pages for services with variants.
 // For example, http://localhost:7080/serbian/500 does not include the variant in the path so we must define a default variant.
 
-import { Variants } from '#app/models/types/global';
+import { Variants } from '#models/types/global';
 
 // NB These routes do not exist on live. They are for dev and testing purposes.
 const defaultServiceVariants: { [index: string]: Variants } = {
