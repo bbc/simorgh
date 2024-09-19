5515c1c69955ee117b66766c045f9fc8f1b0f7ef
alex-magana <alex.magana@andela.com>
2024-09-19 17:10:19 +0300

    Disable /persian/iran-23231114 and /persian/iran/2016/09/160907_tc2_testmap1 local mediaAssetPage cypress tests

--- cypress/support/config/settings.js
+++ cypress/support/config/settings.js
@@ -4536,7 +4536,7 @@ module.exports = () => ({
               '/persian/iran-23231114', // CPS MAP with audio clip
               '/persian/iran/2016/09/160907_tc2_testmap1', // TC2 MAP with video clip
             ],
-            enabled: true,
+            enabled: false,
           },
         },
         smoke: true,
