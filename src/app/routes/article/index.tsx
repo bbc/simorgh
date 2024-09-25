077731c2576fe0864656670352d5ed03f073ebb2
Aaron Moore <aaron.moore@bbc.co.uk>
2024-09-25 08:54:00 +0100

    Update index.tsx (#12007)

--- src/app/routes/article/index.tsx
+++ src/app/routes/article/index.tsx
@@ -12,6 +12,7 @@ import {
 } from '#app/routes/utils/regex';
 import {
   ARTICLE_PAGE,
+  CORRESPONDENT_STORY_PAGE,
   MEDIA_ARTICLE_PAGE,
   MEDIA_ASSET_PAGE,
   ERROR_PAGE,
@@ -24,6 +25,7 @@ import getInitialData from './getInitialData';
 
 type SupportedPageTypes =
   | typeof ARTICLE_PAGE
+  | typeof CORRESPONDENT_STORY_PAGE
   | typeof STORY_PAGE
   | typeof PHOTO_GALLERY_PAGE
   | typeof MEDIA_ARTICLE_PAGE
@@ -39,6 +41,7 @@ const ArticleVariation = (props: { pageData: Article }) => {
 
   const PageType = {
     [ARTICLE_PAGE]: ArticlePage,
+    [CORRESPONDENT_STORY_PAGE]: ArticlePage,
     [STORY_PAGE]: ArticlePage,
     [PHOTO_GALLERY_PAGE]: ArticlePage,
     [MEDIA_ARTICLE_PAGE]: MediaArticlePage,
