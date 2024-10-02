ae0a4f7986a4ef9dde0fead00c8fa9d8ca2ec6ea
emilysaffron <emily.saffron@bbc.co.uk>
2024-10-02 15:26:15 +0100

    remove optimizely code for the scrollable promo

--- src/app/pages/ArticlePage/index.tsx
+++ src/app/pages/ArticlePage/index.tsx
@@ -1,7 +1,4 @@
-import withOptimizelyProvider from '#app/legacy/containers/PageHandlers/withOptimizelyProvider';
 import ArticlePage from './ArticlePage';
 import applyBasicPageHandlers from '../utils/applyBasicPageHandlers';
 
-const OptimizelyArticle = withOptimizelyProvider(ArticlePage);
-
-export default applyBasicPageHandlers(OptimizelyArticle);
+export default applyBasicPageHandlers(ArticlePage);
