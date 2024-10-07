219fd9d23ad3fdf214247fd45b474748db214d4d
Harvey Peachey <harvey.peachey@bbc.co.uk>
2024-10-07 10:18:11 +0100

    adds extra test case

--- src/app/pages/utils/applyBasicPageHandlers.test.js
+++ src/app/pages/utils/applyBasicPageHandlers.test.js
@@ -1,11 +1,23 @@
 import withOptimizelyProvider from '#app/legacy/containers/PageHandlers/withOptimizelyProvider';
+import * as pipe from 'ramda/src/pipe';
 import applyBasicPageHandlers from './applyBasicPageHandlers';
+
+jest.mock('ramda/src/pipe', () => {
+  const originalModule = jest.requireActual('ramda/src/pipe');
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    default: originalModule,
+  };
+});
 
 jest.mock('#app/legacy/containers/PageHandlers/withOptimizelyProvider');
 
 describe('applyBasicPageHandlers', () => {
   beforeEach(() => {
     jest.clearAllMocks();
+    jest.resetModules();
   });
 
   it('should have called the withOptimizelyProvider function if applyOptimizely is true', () => {
@@ -32,5 +44,18 @@ describe('applyBasicPageHandlers', () => {
     applyBasicPageHandlers(component);
 
     expect(withOptimizelyProvider).not.toHaveBeenCalled();
+  });
+
+  it('should call pipe with withOptimizelyProvider as the last argument if applyOptimizely is true', () => {
+    const component = jest.fn();
+    const applyOptimizely = true;
+    const pipeMock = jest.spyOn(pipe, 'default');
+
+    applyBasicPageHandlers(component, { applyOptimizely });
+
+    const args = pipeMock.mock.calls[0];
+    const lastArg = args[args.length - 1];
+
+    expect(lastArg).toEqual(withOptimizelyProvider);
   });
 });
