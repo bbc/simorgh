9fc0d50825ee70a368ca2945a35da190f97f3be4
Harvey Peachey <harvey.peachey@bbc.co.uk>
2024-10-08 08:57:45 +0100

    removes optimizely from article pages as no testing is happening

--- src/app/pages/ArticlePage/index.tsx
+++ src/app/pages/ArticlePage/index.tsx
@@ -1,7 +1,4 @@
-import withOptimizelyProvider from '#app/legacy/containers/PageHandlers/withOptimizelyProvider';
 import ArticlePage from './ArticlePage';
 import applyBasicPageHandlers from '../utils/applyBasicPageHandlers';
 
-export default applyBasicPageHandlers(ArticlePage, {
-  handlerBeforeContexts: withOptimizelyProvider,
-});
+export default applyBasicPageHandlers(ArticlePage);
