aff9c63017d334320e89a2981ac1b241d0ce7ae7
emilysaffron <emily.saffron@bbc.co.uk>
2024-10-14 09:29:48 +0100

    typo

--- ws-nextjs-app/pages/[service]/live/[id]/Post/index.test.tsx
+++ ws-nextjs-app/pages/[service]/live/[id]/Post/index.test.tsx
@@ -167,7 +167,7 @@ describe('Post', () => {
       });
 
       expect(
-        container.querySelector('[data-e2e="media-loader_container"]'),
+        container.querySelector('[data-e2e="media-loader__container"]'),
       ).toBeInTheDocument();
     });
 
