79711530437c376fcc797b232e528000a9bc65e6
Karina Thomas <karina.thomas@bbc.co.uk>
2024-10-14 18:19:47 +0100

    Pass page data to amp tests
    Co-authored-by: chrish01 <holly.christopher@bbc.co.uk>

--- src/integration/pages/homePage/serbianCyr/amp.test.js
+++ src/integration/pages/homePage/serbianCyr/amp.test.js
@@ -4,9 +4,10 @@
  */
 
 import runAmpTests from '../ampTests';
+import { data as pageData } from '../../../../../data/serbian/homePage/cyr.json';
 
 describe('AMP', () => {
   describe(pageType, () => {
-    runAmpTests(service);
+    runAmpTests({ service, pageData });
   });
 });
