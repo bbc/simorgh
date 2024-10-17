c7c18c6cd0dc8aec50cf44a9ef40dc36a8d029ca
Aaron Moore <aaron.moore@bbc.co.uk>
2024-10-17 20:00:11 +0100

    Update config.ts

--- src/app/pages/ArticlePage/ElectionBanner/config.ts
+++ src/app/pages/ArticlePage/ElectionBanner/config.ts
@@ -4,5 +4,5 @@ const IFRAME_BASE_URL = getEnvConfig().SIMORGH_INCLUDES_BASE_URL;
 
 export default {
   iframeSrc: `${IFRAME_BASE_URL}/include/vjsthasia/2308-india-elections-2024-results-page/develop/english/election-banner/embed`,
-  thingId: '647d5613-e0e2-4ef5-b0ce-b491de38bdbd',
+  thingId: '647d5613-e0e2-4ef5-b0ce-b491de38bdbd', // https://www.bbc.co.uk/things/647d5613-e0e2-4ef5-b0ce-b491de38bdbd
 };
