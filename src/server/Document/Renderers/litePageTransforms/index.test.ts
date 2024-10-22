8fdb6c4e95aed281bf6c8866dc27f4c50359d7bc
Aaron Moore <aaron.moore@bbc.co.uk>
2024-10-22 19:16:55 +0100

    Re-add removed test href

--- src/server/Document/Renderers/litePageTransforms/index.test.ts
+++ src/server/Document/Renderers/litePageTransforms/index.test.ts
@@ -7,6 +7,7 @@ describe('litePageTransforms', () => {
   describe('anchor tags', () => {
     it('should append .lite suffix to valid hrefs', () => {
       const html = `
+        <a href="https://www.bbc.com/news">News</a>
         <a href="https://www.bbc.com/serbian/lat">News</a>
         <a href="https://www.bbc.com/mundo">News</a>
         <a href="https://www.bbcrussian.com/news">News</a>
@@ -16,6 +17,7 @@ describe('litePageTransforms', () => {
       const modifiedHtml = litePageTransforms(html);
 
       expect(modifiedHtml).toEqual(`
+        <a href="https://www.bbc.com/news.lite">News</a>
         <a href="https://www.bbc.com/serbian/lat.lite">News</a>
         <a href="https://www.bbc.com/mundo.lite">News</a>
         <a href="https://www.bbcrussian.com/news.lite">News</a>
