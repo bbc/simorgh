df8bf7198dd247b5d05eda1c58fa184553fdfbda
Shayne Marc Enzo Ahchoon <shayne.ahchoon@bbc.co.uk>
2024-10-22 14:48:54 +0100

    WSTEAM1-1349: Update

--- cypress/e2e/pages/articles/testsForLiteOnly.js
+++ cypress/e2e/pages/articles/testsForLiteOnly.js
@@ -1,10 +1,11 @@
 /* eslint-disable import/prefer-default-export */
+import { liteEnabledServices } from '#app/components/LiteSiteCta/liteSiteConfig';
+
 export const testsForLiteOnly = ({ service, pageType }) => {
   describe(`Running testsForLiteOnly for ${service} ${pageType}`, () => {
     describe('CTA: Lite', () => {
       it('should render a call to action component', () => {
-        const liteSiteEnabledList = ['gahuza'];
-        if (liteSiteEnabledList.includes(service)) {
+        if (liteEnabledServices.includes(service)) {
           cy.get('[data-e2e="lite-cta"]').should('be.visible');
         }
       });
