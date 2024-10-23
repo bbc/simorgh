1fef085f1c6538c1bda86b2d501bf7f6868c24d2
Aaron Moore <aaron.moore@bbc.co.uk>
2024-10-23 14:02:19 +0100

    Revert "Remove width restriction"
    This reverts commit dcdb5934d8da07185773cbeb7c68d6eac307e7bd.

--- src/app/pages/ArticlePage/ElectionBanner/index.styles.ts
+++ src/app/pages/ArticlePage/ElectionBanner/index.styles.ts
@@ -1,4 +1,4 @@
-import { css } from '@emotion/react';
+import { css, Theme } from '@emotion/react';
 import pixelsToRem from '#app/utilities/pixelsToRem';
 import BANNER_CONFIG from './config';
 
@@ -9,18 +9,30 @@ const IFRAME_STYLES = css({
 });
 
 export default {
-  electionBannerWrapper: () =>
+  electionBannerWrapper: ({ mq, spacings }: Theme) =>
     css({
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
+      maxWidth: '63rem',
+      margin: '0 auto',
+
+      [mq.GROUP_3_MIN_WIDTH]: {
+        paddingTop: `${spacings.QUINTUPLE}rem`,
+      },
     }),
 
   electionBannerIframe: () => IFRAME_STYLES,
 
-  electionBannerWrapperAmp: () =>
+  electionBannerWrapperAmp: ({ mq, spacings }: Theme) =>
     css({
       overflow: 'hidden',
+      maxWidth: '63rem',
+      margin: '0 auto',
+
+      [mq.GROUP_3_MIN_WIDTH]: {
+        paddingTop: `${spacings.QUINTUPLE}rem`,
+      },
 
       '> div': { padding: '0' },
       '& amp-iframe': IFRAME_STYLES,
