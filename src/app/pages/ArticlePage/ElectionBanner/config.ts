2b41202cd5b35280d78dc7de0bfc00cbc20d8e21
Aaron Moore <aaron.moore@bbc.co.uk>
2024-10-24 11:13:09 +0100

    Fix Election banner AMP URL + layout changes

--- src/app/pages/ArticlePage/ElectionBanner/config.ts
+++ src/app/pages/ArticlePage/ElectionBanner/config.ts
@@ -1,11 +1,12 @@
 import { getEnvConfig } from '#app/lib/utilities/getEnvConfig';
 
 const IFRAME_BASE_URL = getEnvConfig().SIMORGH_INCLUDES_BASE_URL;
+const AMP_IFRAME_BASE_URL = getEnvConfig().SIMORGH_INCLUDES_BASE_AMP_URL;
 
 export default {
   iframeHeight: 324,
   iframeSrc: `${IFRAME_BASE_URL}/include/vjafwest/1365-2024-us-presidential-election-banner/develop/{service}/app`,
-  iframeSrcAmp: `${IFRAME_BASE_URL}/include/vjafwest/1365-2024-us-presidential-election-banner/develop/{service}/app/amp`,
+  iframeSrcAmp: `${AMP_IFRAME_BASE_URL}/include/vjafwest/1365-2024-us-presidential-election-banner/develop/{service}/app/amp`,
   thingIds: [
     '647d5613-e0e2-4ef5-b0ce-b491de38bdbd', // https://www.bbc.co.uk/things/647d5613-e0e2-4ef5-b0ce-b491de38bdbd
   ],
