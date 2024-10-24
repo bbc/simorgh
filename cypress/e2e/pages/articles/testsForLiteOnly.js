1434ab1b79791b6d3ddea6a65c70978894484603
Isabella Mitchell <i.mitchell.0707@gmail.com>
2024-10-24 15:40:22 +0100

    WSTEAM1-1349: Wrap all e2e tests in service check statement

--- cypress/e2e/pages/articles/testsForLiteOnly.js
+++ cypress/e2e/pages/articles/testsForLiteOnly.js
@@ -4,36 +4,36 @@ import { liteEnabledServices } from '#app/components/LiteSiteCta/liteSiteConfig'
 export const testsForLiteOnly = ({ service, pageType }) => {
   describe(`Running testsForLiteOnly for ${service} ${pageType}`, () => {
     describe('CTA: Lite', () => {
-      it('should render a call to action component', () => {
-        if (liteEnabledServices.includes(service)) {
+      if (liteEnabledServices.includes(service)) {
+        it('should render a call to action component', () => {
           cy.get('[data-e2e="lite-cta"]').should('be.visible');
-        }
-      });
-
-      it('Clicking the link to the main site should navigate to canonical site', () => {
-        cy.get('[data-e2e="to-main-site"]').within(() => {
-          cy.get('a')
-            .should('have.attr', 'href')
-            .then($href => {
-              cy.get('a').click();
-              cy.url().should('eq', $href).should('not.contain', '.lite');
-            });
         });
-        cy.go('back');
-      });
 
-      it('Clicking the link to the Information page should navigate to lite site', () => {
-        cy.get('[data-e2e="information-page"]').within(() => {
-          cy.get('a')
-            .should('have.attr', 'href')
-            .then($href => {
-              cy.get('a').click();
-              cy.url().should('eq', $href);
-            })
-            .and('contain', '.lite');
+        it('Clicking the link to the main site should navigate to canonical site', () => {
+          cy.get('[data-e2e="to-main-site"]').within(() => {
+            cy.get('a')
+              .should('have.attr', 'href')
+              .then($href => {
+                cy.get('a').click();
+                cy.url().should('eq', $href).should('not.contain', '.lite');
+              });
+          });
+          cy.go('back');
         });
-        cy.go('back');
-      });
+
+        it('Clicking the link to the Information page should navigate to lite site', () => {
+          cy.get('[data-e2e="information-page"]').within(() => {
+            cy.get('a')
+              .should('have.attr', 'href')
+              .then($href => {
+                cy.get('a').click();
+                cy.url().should('eq', $href);
+              })
+              .and('contain', '.lite');
+          });
+          cy.go('back');
+        });
+      }
     });
   });
 };
