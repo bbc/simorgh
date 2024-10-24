465e048bcf60dbcc8588aa2f6f61a6e75ad67058
Aaron Moore <aaron.moore@bbc.co.uk>
2024-10-24 14:32:00 +0100

    Minor height adjusts

--- src/app/pages/ArticlePage/ElectionBanner/index.styles.ts
+++ src/app/pages/ArticlePage/ElectionBanner/index.styles.ts
@@ -2,26 +2,37 @@ import { css, Theme } from '@emotion/react';
 import pixelsToRem from '#app/utilities/pixelsToRem';
 import BANNER_CONFIG from './config';
 
-const IFRAME_STYLES = css({
-  border: 'none',
-  width: '100%',
-  height: `${pixelsToRem(BANNER_CONFIG.iframeHeight)}rem`,
-});
-
 export default {
   electionBannerWrapper: ({ spacings }: Theme) =>
     css({
       marginBottom: `${spacings.FULL}rem`,
     }),
 
-  electionBannerIframe: () => IFRAME_STYLES,
+  electionBannerIframe: ({ mq }: Theme) =>
+    css({
+      border: 'none',
+      width: '100%',
+      height: `${pixelsToRem(360)}rem`,
 
-  electionBannerWrapperAmp: ({ spacings }: Theme) =>
+      [mq.GROUP_4_MIN_WIDTH]: {
+        height: `${pixelsToRem(BANNER_CONFIG.iframeHeight)}rem`,
+      },
+    }),
+
+  electionBannerWrapperAmp: ({ mq, spacings }: Theme) =>
     css({
       overflow: 'hidden',
       marginBottom: `${spacings.FULL}rem`,
 
       '> div': { padding: '0' },
-      '& amp-iframe': IFRAME_STYLES,
+      '& amp-iframe': {
+        border: 'none',
+        width: '100%',
+        height: `${pixelsToRem(360)}rem`,
+
+        [mq.GROUP_4_MIN_WIDTH]: {
+          height: `${pixelsToRem(BANNER_CONFIG.iframeHeight)}rem`,
+        },
+      },
     }),
 };
