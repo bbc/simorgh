a394eff95856cfd9cc7766ebec0e03bdb4ba2d5b
Isabella Mitchell <i.mitchell.0707@gmail.com>
2025-04-10 13:34:22 +0100

    WORLDSERVICE-543: Implements as CTALinks in LiteSiteCTA Banner

--- src/app/components/LiteSiteCta/index.styles.tsx
+++ src/app/components/LiteSiteCta/index.styles.tsx
@@ -30,35 +30,6 @@ export default {
         marginBottom: `${spacings.FULL}rem`,
       },
     }),
-  chevron: ({ palette, spacings, mq }: Theme) =>
-    css({
-      color: palette.GREY_10,
-      fill: 'currentColor',
-      marginInlineStart: `${spacings.FULL}rem`,
-      verticalAlign: 'middle',
-      width: `${pixelsToRem(14)}rem`,
-      height: `${pixelsToRem(14)}rem`,
-      'a:visited &': {
-        color: palette.METAL,
-      },
-      'a:focus &, a:hover &': {
-        color: palette.POSTBOX,
-      },
-      [mq.FORCED_COLOURS]: {
-        fill: 'linkText',
-        'a:visited &': {
-          fill: 'visitedText',
-        },
-        'a:active &': {
-          fill: 'activeText',
-        },
-      },
-    }),
-  link: () =>
-    css({
-      display: 'inline-block',
-      textDecoration: 'none',
-    }),
   bottomLinkSpacing: ({ spacings, mq }: Theme) =>
     css({
       padding: `${spacings.HALF}rem 0 ${spacings.DOUBLE}rem`,
@@ -69,18 +40,5 @@ export default {
   topLinkSpacing: ({ spacings }: Theme) =>
     css({
       padding: `${spacings.FULL + spacings.HALF}rem 0 ${spacings.FULL + spacings.HALF}rem`,
-    }),
-  linkText: ({ palette }: Theme) =>
-    css({
-      borderBottom: `${pixelsToRem(1)}rem solid ${palette.GREY_10}`,
-      textDecoration: 'none',
-      'a:visited &': {
-        color: palette.METAL,
-        borderBottom: `${pixelsToRem(1)}rem solid ${palette.METAL}`,
-      },
-      'a:focus &, a:hover &': {
-        borderBottom: `${pixelsToRem(2)}rem solid ${palette.POSTBOX}`,
-        color: palette.POSTBOX,
-      },
     }),
 };
--- src/app/components/LiteSiteCta/index.tsx
+++ src/app/components/LiteSiteCta/index.tsx
@@ -4,63 +4,15 @@ import { jsx } from '@emotion/react';
 import { useATIClickTrackerHandler } from '#app/hooks/useClickTrackerHandler';
 import Paragraph from '../Paragraph';
 import Text from '../Text';
-import { LeftChevron, RightChevron } from '../icons';
 import { ServiceContext } from '../../contexts/ServiceContext';
 import { RequestContext } from '../../contexts/RequestContext';
 import styles from './index.styles';
 import { defaultTranslations } from './liteSiteConfig';
-
-type CtaLinkProps = {
-  isRtl: boolean;
-  href: string;
-  text: string;
-  fontVariant?: string;
-  showChevron?: boolean;
-  ignoreLiteExtension?: boolean;
-  className?: string;
-  useClickHandler?: boolean;
-};
-
-const CtaLink = ({
-  isRtl,
-  href,
-  text,
-  fontVariant = 'sansRegular',
-  showChevron = false,
-  ignoreLiteExtension = false,
-  useClickHandler = false,
-  className,
-}: CtaLinkProps) => {
-  const atiClickTrackerHandler = useATIClickTrackerHandler({
-    componentName: 'lite-site-cta',
-  });
-
-  const chevron = isRtl ? (
-    <LeftChevron css={styles.chevron} />
-  ) : (
-    <RightChevron css={styles.chevron} />
-  );
-
-  return (
-    <a
-      href={href}
-      className={className}
-      css={styles.link}
-      {...(ignoreLiteExtension && { 'data-ignore-lite': true })}
-      {...(useClickHandler && atiClickTrackerHandler)}
-    >
-      <Text size="brevier" fontVariant={fontVariant} css={styles.linkText}>
-        {text}
-      </Text>
-      {showChevron && chevron}
-    </a>
-  );
-};
+import CallToActionLink from '../CallToActionLinkComponent';
 
 const LiteSiteCta = () => {
-  const { dir, translations } = useContext(ServiceContext);
+  const { translations } = useContext(ServiceContext);
   const { canonicalLink } = useContext(RequestContext);
-  const isRtl = dir === 'rtl';
   const { liteSite = defaultTranslations } = translations;
   const {
     onboardingMessage,
@@ -70,6 +22,9 @@ const LiteSiteCta = () => {
     dataSaving,
   } = liteSite;
   const id = 'LiteSiteCta';
+  const atiClickTrackerHandler = useATIClickTrackerHandler({
+    componentName: 'lite-site-cta',
+  });
 
   return (
     <section
@@ -86,24 +41,29 @@ const LiteSiteCta = () => {
           {onboardingMessage}
         </Paragraph>
         <Paragraph data-e2e="to-main-site">
-          <CtaLink
-            fontVariant="sansBold"
-            isRtl={isRtl}
-            href={canonicalLink}
-            text={toMainSite}
+          <CallToActionLink
+            to={canonicalLink}
+            {...{ 'data-ignore-lite': true }}
+            {...atiClickTrackerHandler}
             css={styles.topLinkSpacing}
-            ignoreLiteExtension
-            useClickHandler
-            showChevron
-          />
+            alignWithMargin
+          >
+            <CallToActionLink.Text size="brevier" fontVariant="sansBold">
+              {toMainSite}
+            </CallToActionLink.Text>
+            <CallToActionLink.Chevron size="brevier" />
+          </CallToActionLink>
         </Paragraph>
         <Paragraph data-e2e="information-page">
-          <CtaLink
-            isRtl={isRtl}
-            href={informationPageLink}
-            text={informationPage}
+          <CallToActionLink
+            to={informationPageLink}
             css={styles.bottomLinkSpacing}
-          />
+            alignWithMargin
+          >
+            <CallToActionLink.Text size="brevier">
+              {informationPage}
+            </CallToActionLink.Text>
+          </CallToActionLink>
         </Paragraph>
       </div>
     </section>
