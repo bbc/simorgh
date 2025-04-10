6e17cf9ba847b32a22e5177a25eaa8483c966696
Shayne Marc Enzo Ahchoon <shayne.ahchoon@bbc.co.uk>
2025-04-10 17:28:06 +0100

    WSTEAM1-492: Update

--- src/app/components/Billboard/index.tsx
+++ src/app/components/Billboard/index.tsx
@@ -1,6 +1,5 @@
 /** @jsx jsx */
 /* @jsxFrag React.Fragment */
-import { ForwardedRef, forwardRef } from 'react';
 import { jsx } from '@emotion/react';
 import useViewTracker from '#app/hooks/useViewTracker';
 import useClickTrackerHandler from '#app/hooks/useClickTrackerHandler';
@@ -23,90 +22,58 @@ interface BillboardProps {
   showLiveLabel?: boolean;
 }
 
-const Billboard = forwardRef(
-  (
-    {
-      heading,
-      description,
-      link,
-      image,
-      altText,
-      id = 'billboard-1',
-      eventTrackingData,
-      showLiveLabel,
-    }: BillboardProps,
-    viewRef: ForwardedRef<HTMLDivElement>,
-  ) => {
-    const clickTrackerHandler = useClickTrackerHandler(eventTrackingData);
-
-    return (
-      <section role="region" aria-labelledby={id} data-testid={id}>
-        <div css={styles.headerContainer} {...viewRef}>
-          <div css={styles.backgroundContainer} />
-          <div css={styles.contentContainer}>
-            <MaskedImage
-              imageUrl={image.replace('{width}', '240')}
-              imageUrlTemplate={image}
-              altText={altText}
-              imageWidth={660}
-              showPlaceholder={false}
-            />
-            <div css={styles.textContainer}>
-              <Heading level={2} size="paragon" css={styles.heading} id={id}>
-                <a href={link} css={styles.link} {...clickTrackerHandler}>
-                  {showLiveLabel ? (
-                    <div data-testid="billboard-live-label">
-                      <LivePulse
-                        width="24"
-                        height="24"
-                        css={styles.liveLabelPulse}
-                      />
-                      <LiveText css={styles.liveLabelText}>
-                        <div>{heading}</div>
-                      </LiveText>
-                    </div>
-                  ) : (
-                    <div>{heading}</div>
-                  )}
-                </a>
-              </Heading>
-              {description && (
-                <Text as="p" css={styles.description}>
-                  {description}
-                </Text>
-              )}
-            </div>
-          </div>
-        </div>
-      </section>
-    );
-  },
-);
-
 export default ({
   heading,
   description,
   link,
   image,
   altText,
-  id,
+  id = 'billboard',
   eventTrackingData,
   showLiveLabel,
 }: BillboardProps) => {
   const viewRef = useViewTracker(eventTrackingData);
+  const clickTrackerHandler = useClickTrackerHandler(eventTrackingData);
 
   return (
-    <Billboard
-      heading={heading}
-      description={description}
-      link={link}
-      image={image}
-      altText={altText}
-      id={id}
-      eventTrackingData={eventTrackingData}
-      // @ts-expect-error TODO need help fixing this!
-      ref={viewRef}
-      showLiveLabel={showLiveLabel}
-    />
+    <section role="region" aria-labelledby={id} data-testid={id}>
+      <div css={styles.headerContainer} {...viewRef}>
+        <div css={styles.backgroundContainer} />
+        <div css={styles.contentContainer}>
+          <MaskedImage
+            imageUrl={image.replace('{width}', '240')}
+            imageUrlTemplate={image}
+            altText={altText}
+            imageWidth={660}
+            showPlaceholder={false}
+          />
+          <div css={styles.textContainer}>
+            <Heading level={2} size="paragon" css={styles.heading} id={id}>
+              <a href={link} css={styles.link} {...clickTrackerHandler}>
+                {showLiveLabel ? (
+                  <div data-testid="billboard-live-label">
+                    <LivePulse
+                      width="24"
+                      height="24"
+                      css={styles.liveLabelPulse}
+                    />
+                    <LiveText css={styles.liveLabelText}>
+                      <div>{heading}</div>
+                    </LiveText>
+                  </div>
+                ) : (
+                  <div>{heading}</div>
+                )}
+              </a>
+            </Heading>
+            {description && (
+              <Text as="p" css={styles.description}>
+                {description}
+              </Text>
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
