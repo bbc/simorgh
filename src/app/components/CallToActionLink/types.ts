e9b19c963c4bf7c8f1fe5ac6718033e744680256
Isabella Mitchell <i.mitchell.0707@gmail.com>
2025-04-10 13:40:25 +0100

    WORLDSERVICE-543: Deletes

--- src/app/components/CallToActionLink/types.ts
+++
@@ -1,8 +0,0 @@
-import { EventTrackingMetadata } from '#app/models/types/eventTracking';
-
-export type CallToActionLinkProps = {
-  href?: string;
-  className?: string;
-  eventTrackingData?: EventTrackingMetadata;
-  download?: boolean;
-};
