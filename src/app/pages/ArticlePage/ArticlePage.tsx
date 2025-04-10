7119efbb30b816e696e3b6cc2edb877691f84792
Aaron Moore <aaron.moore@bbc.co.uk>
2025-04-10 09:40:13 +0100

    Disable component

--- src/app/pages/ArticlePage/ArticlePage.tsx
+++ src/app/pages/ArticlePage/ArticlePage.tsx
@@ -119,7 +119,7 @@ const getHeadlineComponent = (props: ComponentToRenderProps) => (
 
 const ArticlePage = ({
   pageData,
-  continueReadingEnabled = true, // change to true when developing to see on the page (comment can be removed when the experiment is ready to run)
+  continueReadingEnabled = false, // change to true when developing to see on the page (comment can be removed when the experiment is ready to run)
 }: {
   pageData: Article;
   continueReadingEnabled?: boolean;
