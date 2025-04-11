1bb45e5fcd48e63ec55c9d6fce9da31eeb1566a0
Karina Thomas <karina.thomas@bbc.co.uk>
2025-04-11 09:43:40 +0100

    Ensure that LITE_ATI_VIEW_TRACKING value matches

--- src/server/utilities/liteATITracking/viewTracking/index.ts
+++ src/server/utilities/liteATITracking/viewTracking/index.ts
@@ -2,6 +2,7 @@ export default () => {
   const MIN_VIEWED_PERCENT = 0.5;
   const VIEWED_DURATION_MS = 1000;
   const options = { threshold: MIN_VIEWED_PERCENT };
+  const LITE_ATI_VIEW_TRACKING = 'data-lite-ati-view';
   const firedURLs: string[] = [];
 
   if (window.IntersectionObserver) {
@@ -9,7 +10,7 @@ export default () => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
           const { target } = entry;
-          const atiURL = target.getAttribute('data-lite-ati-view') as string;
+          const atiURL = target.getAttribute(LITE_ATI_VIEW_TRACKING) as string;
           setTimeout(() => {
             if (!firedURLs.includes(atiURL)) {
               window.processClientDeviceAndSendLite(atiURL);
@@ -21,7 +22,7 @@ export default () => {
       });
     }, options);
 
-    const targets = document.querySelectorAll('[data-lite-ati-view]');
+    const targets = document.querySelectorAll(`[${LITE_ATI_VIEW_TRACKING}]`);
     targets.forEach(target => observer.observe(target));
   }
 };
