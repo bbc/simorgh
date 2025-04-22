8c5fb2b87692b065694bc450bcc778878b05ce37
Karina Thomas <karina.thomas@bbc.co.uk>
2025-04-22 15:58:35 +0100

    Update tests

--- src/app/hooks/useClickTrackerHandler/index.test.jsx
+++ src/app/hooks/useClickTrackerHandler/index.test.jsx
@@ -435,7 +435,7 @@ describe('useClickTrackerHandler', () => {
         'myEvent_clicks',
         mockUserId,
         {
-          'clicked_canonical-lite-cta': true,
+          'clicked_article-lite-site-link': true,
           foo: 'bar',
         },
       );
