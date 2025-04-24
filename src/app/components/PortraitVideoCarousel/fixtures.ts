82715be9a2da78c8d4fb1a14b7a61b138fa3a940
Pedram Valiani <pedram.valiani@bbc.co.uk>
2025-04-24 11:04:42 +0100

    adds a storybook component w/ props needed for current mvp and a fixture

---
+++ src/app/components/PortraitVideoCarousel/fixtures.ts
@@ -0,0 +1,157 @@
+export const portraitVideoFixture = {
+  title: 'Portrait Video Carousel',
+  items: [
+    {
+      id: 'urn:bbc:pips:pid:p01wjx7v',
+      images: [
+        {
+          url: 'https://ichef.test.bbci.co.uk/images/ic/1024xn/p01wjx8g.jpg',
+          altText:
+            'A sensação de não saber por onde começar, a falta de uma rotina',
+        },
+      ],
+      headlines: {
+        promoHeadline: '4 erros de quem estuda para concursos públicos (9x16)',
+      },
+      link: {
+        path: '/programmes/p01wjx7v',
+      },
+    },
+    {
+      id: 'urn:bbc:optimo:asset:cgp62emnrk5o',
+      images: [
+        {
+          url: 'https://ichef.test.bbci.co.uk/images/ic/1024xn/p01wjx7z.jpg',
+          altText: 'Hoje nove países possuem armas nucleares',
+        },
+      ],
+      headlines: {
+        promoHeadline: 'Article with portrait video embed (2)',
+      },
+      link: {
+        path: '/portuguese/articles/cgp62emnrk5o',
+      },
+    },
+    {
+      id: 'urn:bbc:pips:pid:p01wjx5y',
+      images: [
+        {
+          url: 'https://ichef.test.bbci.co.uk/images/ic/1024xn/p01wjx8v.jpg',
+          altText: 'A Europa está em uma espécie de "corrida armamentista"',
+        },
+      ],
+      headlines: {
+        promoHeadline: 'Europa se armando para guerra? (9x16)',
+      },
+      link: {
+        path: '/programmes/p01wjx5y',
+      },
+    },
+    {
+      id: 'urn:bbc:pips:pid:p01wjx4r',
+      images: [
+        {
+          url: 'https://ichef.test.bbci.co.uk/images/ic/1024xn/p01wjx5j.jpg',
+          altText: 'A Nova Rota da Seda',
+        },
+      ],
+      headlines: {
+        promoHeadline: 'China: a Nova Rota da Seda (9x16)',
+      },
+      link: {
+        path: '/programmes/p01wjx4r',
+      },
+    },
+    {
+      id: 'urn:bbc:pips:pid:p01wjx3q',
+      images: [
+        {
+          url: 'https://ichef.test.bbci.co.uk/images/ic/1024xn/p01wjx51.jpg',
+          altText: 'Estupradores em série no Reino Unido',
+        },
+      ],
+      headlines: {
+        promoHeadline: 'O estudante acusado no Reino Unido (9x16)',
+      },
+      link: {
+        path: '/programmes/p01wjx3q',
+      },
+    },
+    {
+      id: 'urn:bbc:pips:pid:p01wjx3g',
+      images: [
+        {
+          url: 'https://ichef.test.bbci.co.uk/images/ic/1024xn/p01wjx48.jpg',
+          altText: 'Evitar picos de glicose',
+        },
+      ],
+      headlines: {
+        promoHeadline: 'Por que evitar glicose para emagrecer (9x16)',
+      },
+      link: {
+        path: '/programmes/p01wjx3g',
+      },
+    },
+    {
+      id: 'urn:bbc:pips:pid:p01wjx35',
+      images: [
+        {
+          url: 'https://ichef.test.bbci.co.uk/images/ic/1024xn/p01wjx3l.jpg',
+          altText: 'Papa Francisco recebe alta',
+        },
+      ],
+      headlines: {
+        promoHeadline: 'Papa Francisco recebe alta (9x16)',
+      },
+      link: {
+        path: '/programmes/p01wjx35',
+      },
+    },
+    {
+      id: 'urn:bbc:pips:pid:p01wjx7v',
+      images: [
+        {
+          url: 'https://ichef.test.bbci.co.uk/images/ic/1024xn/p01wjx8g.jpg',
+          altText:
+            'A sensação de não saber por onde começar, a falta de uma rotina',
+        },
+      ],
+      headlines: {
+        promoHeadline: '4 erros de quem estuda (9x16)',
+      },
+      link: {
+        path: '/programmes/p01wjx7v',
+      },
+    },
+    {
+      id: 'urn:bbc:optimo:asset:cgp62emnrk5o',
+      images: [
+        {
+          url: 'https://ichef.test.bbci.co.uk/images/ic/1024xn/p01wjx7z.jpg',
+          altText: 'Hoje nove países possuem armas nucleares',
+        },
+      ],
+      headlines: {
+        promoHeadline: 'Armas nucleares (9x16)',
+      },
+      link: {
+        path: '/portuguese/articles/cgp62emnrk5o',
+      },
+    },
+    {
+      id: 'urn:bbc:pips:pid:p01wjx5y',
+      images: [
+        {
+          url: 'https://ichef.test.bbci.co.uk/images/ic/1024xn/p01wjx8v.jpg',
+          altText: 'Corrida armamentista na Europa',
+        },
+      ],
+      headlines: {
+        promoHeadline: 'Corrida armamentista (9x16)',
+      },
+      link: {
+        path: '/programmes/p01wjx5y',
+      },
+    },
+  ],
+};
---
+++ src/app/components/PortraitVideoCarousel/index.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StoryArgs, StoryProps } from '../../models/types/storybook';
+import PortraitVideoCarousel from '.';
+import readme from './README.md';
+import metadata from './metadata.json';
+import { portraitVideoFixture } from './fixtures';
+
+interface Props extends StoryProps {}
+
+const Component = (_: StoryArgs, __: Props) => {
+  return (
+    <PortraitVideoCarousel
+      title={portraitVideoFixture.title}
+      items={portraitVideoFixture.items}
+    />
+  );
+};
+
+export default {
+  title: 'Components/PortraitVideoCarousel',
+  Component,
+  parameters: {
+    docs: { readme },
+    metadata,
+  },
+};
+
+export const Example = Component;
