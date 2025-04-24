4bdce987ad926df7fc7cffc803c964d6e792cbf7
HarveyPeachey <harvey.peachey@bbc.co.uk>
2025-04-24 15:53:09 +0100

    adds typing for default translations

--- src/app/components/LiteSiteSummary/liteSiteTranslations.ts
+++ src/app/components/LiteSiteSummary/liteSiteTranslations.ts
@@ -1,4 +1,10 @@
-const defaultTranslations = {
+const defaultTranslations: {
+  onboardingMessage: string;
+  toMainSite: string;
+  informationPage: string;
+  informationPageLink?: string;
+  dataSaving: string;
+} = {
   onboardingMessage: `You’re viewing a text-only version of this website that uses less data. View the main version of the website including all images and videos.`,
   toMainSite: 'Take me to the main website',
   informationPage: 'Find out more about this data-saving version',
