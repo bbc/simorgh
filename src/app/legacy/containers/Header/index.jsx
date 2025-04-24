6b8dbbff3681029d4299b201764c1b4494c74ef0
HarveyPeachey <harvey.peachey@bbc.co.uk>
2025-04-24 10:23:35 +0100

    adds missed renaming

--- src/app/legacy/containers/Header/index.jsx
+++ src/app/legacy/containers/Header/index.jsx
@@ -9,7 +9,7 @@ import {
   TOPIC_PAGE,
   ERROR_PAGE,
 } from '#app/routes/utils/pageTypes';
-import LiteSiteCta from '#app/components/LiteSiteSummary';
+import LiteSiteSummary from '#app/components/LiteSiteSummary';
 import { ServiceContext } from '../../../contexts/ServiceContext';
 import ConsentBanner from '../ConsentBanner';
 import NavigationContainer from '../Navigation';
@@ -103,7 +103,7 @@ const HeaderContainer = ({ propsForOJExperiment }) => {
           scriptLink={shouldRenderScriptSwitch && <ScriptLink />}
         />
       )}
-      {isLite && <LiteSiteCta />}
+      {isLite && <LiteSiteSummary />}
       <NavigationContainer propsForOJExperiment={propsForOJExperiment} />
     </header>
   );
