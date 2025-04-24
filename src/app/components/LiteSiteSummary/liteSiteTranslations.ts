950527c6be72d6ffa811d2d4719395212f34b9d4
HarveyPeachey <harvey.peachey@bbc.co.uk>
2025-04-24 15:32:13 +0100

    removes informationPageLink from default translations

--- src/app/components/LiteSiteSummary/liteSiteTranslations.ts
+++ src/app/components/LiteSiteSummary/liteSiteTranslations.ts
@@ -2,7 +2,6 @@ const defaultTranslations = {
   onboardingMessage: `You’re viewing a text-only version of this website that uses less data. View the main version of the website including all images and videos.`,
   toMainSite: 'Take me to the main website',
   informationPage: 'Find out more about this data-saving version',
-  informationPageLink: '#',
   dataSaving: 'Data saving version',
 };
 
