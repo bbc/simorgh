3756ea6bfe7f97e8e27f7e7bed97323d8ef244d9
HarveyPeachey <harvey.peachey@bbc.co.uk>
2025-04-24 09:47:03 +0100

    enables lite site cta to show for every service

--- src/app/legacy/containers/Header/index.jsx
+++ src/app/legacy/containers/Header/index.jsx
@@ -10,7 +10,6 @@ import {
   ERROR_PAGE,
 } from '#app/routes/utils/pageTypes';
 import LiteSiteCta from '#app/components/LiteSiteCta';
-import { liteEnabledServices } from '#app/components/LiteSiteCta/liteSiteConfig';
 import { ServiceContext } from '../../../contexts/ServiceContext';
 import ConsentBanner from '../ConsentBanner';
 import NavigationContainer from '../Navigation';
@@ -86,7 +85,6 @@ const HeaderContainer = ({ propsForOJExperiment }) => {
         break;
     }
   }
-  const renderLiteSiteCTA = isLite && liteEnabledServices.includes(service);
 
   if (isApp) return null;
 
@@ -105,7 +103,7 @@ const HeaderContainer = ({ propsForOJExperiment }) => {
           scriptLink={shouldRenderScriptSwitch && <ScriptLink />}
         />
       )}
-      {renderLiteSiteCTA && <LiteSiteCta />}
+      {isLite && <LiteSiteCta />}
       <NavigationContainer propsForOJExperiment={propsForOJExperiment} />
     </header>
   );
