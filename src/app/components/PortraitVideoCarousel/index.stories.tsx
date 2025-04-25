6993e6b89ef224ab2d508692ec81f71c91b6a56b
Pedram Valiani <pedram.valiani@bbc.co.uk>
2025-04-25 13:57:45 +0100

    update story linting issue - eslint suppression

--- src/app/components/PortraitVideoCarousel/index.stories.tsx
+++ src/app/components/PortraitVideoCarousel/index.stories.tsx
@@ -5,6 +5,8 @@ import readme from './README.md';
 import metadata from './metadata.json';
 import portraitVideoFixture from './fixture';
 
+// Suppressing for now but might require this later
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props extends StoryProps {}
 
 const Component = (_: StoryArgs, __: Props) => {
