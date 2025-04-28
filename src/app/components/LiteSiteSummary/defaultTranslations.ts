9290c6244fd99adb2410cb79ac82211e6443e128
HarveyPeachey <harvey.peachey@bbc.co.uk>
2025-04-28 14:17:24 +0100

    extracts out lite site translations

--- src/app/components/LiteSiteSummary/defaultTranslations.ts
+++ src/app/components/LiteSiteSummary/defaultTranslations.ts
@@ -1,6 +1,6 @@
-import { Translations } from '../../models/types/translations';
+import { LiteSiteTranslations } from '../../models/types/translations';
 
-const defaultTranslations: Translations['liteSite'] = {
+const defaultTranslations: LiteSiteTranslations = {
   onboardingMessage: `You’re viewing a text-only version of this website that uses less data. View the main version of the website including all images and videos.`,
   toMainSite: 'Take me to the main website',
   informationPage: 'Find out more about this data-saving version',
--- src/app/models/types/translations.ts
+++ src/app/models/types/translations.ts
@@ -17,14 +17,7 @@ export interface Translations {
   topicsPath?: string;
   relatedTopics?: string;
   navMenuText: string;
-  liteSite?: {
-    onboardingMessage: string;
-    toMainSite: string;
-    informationPage: string;
-    informationPageLink?: string;
-    dataSaving: string;
-    articleDataSavingLinkText?: string;
-  };
+  liteSite?: LiteSiteTranslations;
   mediaAssetPage: {
     mediaPlayer: string;
     audioPlayer: string;
@@ -285,3 +278,12 @@ export type UgcTranslations = {
   closedHeading: string;
   closedDescription: string;
 };
+
+export interface LiteSiteTranslations {
+  onboardingMessage: string;
+  toMainSite: string;
+  informationPage: string;
+  informationPageLink?: string;
+  dataSaving: string;
+  articleDataSavingLinkText?: string;
+}
