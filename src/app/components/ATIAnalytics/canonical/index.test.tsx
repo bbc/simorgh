3c25305c8a783b0b68109928ce4c15f7c752811e
chrish01 <holly.christopher@bbc.co.uk>
2025-04-28 18:00:23 +0100

    adds test for pixel tracking

--- src/app/components/ATIAnalytics/canonical/index.test.tsx
+++ src/app/components/ATIAnalytics/canonical/index.test.tsx
@@ -15,6 +15,37 @@ describe('Canonical ATI Analytics', () => {
 
   const atiBaseUrl = 'https://foobar.com?';
   const mockPageviewParams = 'key=value&key2=value2&x8=[simorgh]';
+  const mockReverbParams = {
+    params: {
+      page: {
+        contentId: 'urn:bbc:optimo:c0000000001o',
+        contentType: 'article',
+        destination: 'WS_NEWS_LANGUAGES_TEST',
+        name: 'news.articles.c0000000001o.page',
+        producer: 'atiAnalyticsProducerName',
+        additionalProperties: {
+          app_name: 'atiAnalyticsAppName',
+          app_type: 'responsive',
+          content_language: 'en-gb',
+          product_platform: null,
+          referrer_url: null,
+          x5: 'http%3A%2F%2Flocalhost%2F',
+          x8: 'simorgh',
+          x9: 'Article%20Headline%20for%20SEO',
+          x10: null,
+          x11: '2018-01-01T12:01:00.000Z',
+          x12: '2018-01-01T14:00:00.000Z',
+          x13: 'Royal+Wedding+2018~Duchess+of+Sussex',
+          x14: '2351f2b2-ce36-4f44-996d-c3c4f7f90eaa~803eaeb9-c0c3-4f1b-9a66-90efac3df2dc',
+          x16: '',
+          x17: 'Royal+Wedding+2018~Duchess+of+Sussex',
+          x18: false,
+        },
+      },
+      user: { isSignedIn: false },
+    },
+    eventDetails: { eventName: 'pageView' },
+  };
   const mockSendBeacon = jest.fn().mockReturnValue('beacon-return-value');
   process.env.SIMORGH_ATI_BASE_URL = atiBaseUrl;
   // @ts-expect-error - we need to mock these functions to ensure tests are deterministic
@@ -73,5 +104,18 @@ describe('Canonical ATI Analytics', () => {
       <CanonicalATIAnalytics pageviewParams={mockPageviewParams} />,
     );
     expect(container.querySelector('noscript')).toBeInTheDocument();
+  });
+
+  it('should render a noscript tracking pixel for non-JS users', () => {
+    const { container } = render(
+      <CanonicalATIAnalytics
+        reverbParams={mockReverbParams}
+        pageviewParams={mockPageviewParams}
+      />,
+    );
+    const image = container.querySelector('noscript');
+    console.log(container.innerHTML);
+    expect(container.querySelector('noscript')).toBeInTheDocument();
+    expect(image?.getAttribute('src')).toEqual('');
   });
 });
