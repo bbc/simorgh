3829c62515a2182ad1da46919057f63693466669
Karina Thomas <karina.thomas@bbc.co.uk>
2025-04-30 13:34:11 +0100

    Fix merge conflict

--- src/app/lib/analyticsUtils/staticATITracking/clickTracking/index.ts
+++ src/app/lib/analyticsUtils/staticATITracking/clickTracking/index.ts
@@ -20,11 +20,7 @@ export default () => {
       event.stopPropagation();
       event.preventDefault();
 
-<<<<<<<< HEAD:src/app/lib/analyticsUtils/staticATITracking/clickTracking/index.ts
       const atiURL = targetElement.getAttribute(STATIC_ATI_CLICK_TRACKING);
-========
-      const atiURL = targetElement.getAttribute('data-static-ati-click');
->>>>>>>> 6ab6f3db1add8163fb91a70de3378ac3fcd0a3c9:src/server/utilities/staticATITracking/clickTracking/index.ts
       const anchorElement = targetElement as HTMLAnchorElement;
       const nextPageUrl = anchorElement?.href;
 
