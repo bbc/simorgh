261022c79ef069f5c0241b9d6d2cdf5848095132
alex-magana <alex.magana@andela.com>
2025-05-01 21:07:59 +0300

    Add reverb URL helper for AMP integration

--- src/app/components/ATIAnalytics/amp/ampAnalyticsJson.ts
+++ src/app/components/ATIAnalytics/amp/ampAnalyticsJson.ts
@@ -1,20 +1,30 @@
 // The pageview request needs to have the ${base} value not interpolated.
 /* eslint-disable no-template-curly-in-string */
 /* eslint-disable prefer-template */
-
+import { reverbUrlHelper } from '@bbc/reverb-url-helper';
 import { ATIAnalyticsProps } from '../types';
 
-const ampAnalyticsJson = ({ baseUrl, pageviewParams }: ATIAnalyticsProps) => ({
-  transport: {
-    beacon: false,
-    xhrpost: false,
-    image: true,
-  },
-  requests: {
-    base: baseUrl,
-    pageview: '${base}' + pageviewParams,
-  },
-  triggers: { trackPageview: { on: 'visible', request: 'pageview' } },
-});
+const ampAnalyticsJson = ({
+  baseUrl,
+  pageviewParams,
+  reverbParams,
+}: ATIAnalyticsProps) => {
+  const ampAnalyticsRequestConfiguration = reverbParams
+    ? reverbUrlHelper.getAmpAnalyticsPageViewUrl(reverbParams)
+    : {
+        base: baseUrl,
+        pageview: '${base}' + pageviewParams,
+      };
+
+  return {
+    transport: {
+      beacon: false,
+      xhrpost: false,
+      image: true,
+    },
+    requests: ampAnalyticsRequestConfiguration,
+    triggers: { trackPageview: { on: 'visible', request: 'pageview' } },
+  };
+};
 
 export default ampAnalyticsJson;
