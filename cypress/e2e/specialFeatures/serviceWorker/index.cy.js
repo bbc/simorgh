769d5a46a3f7ef3be56bf793ef672d657415bfef
skumid01 <dmytro.skumin.ext@bbc.co.uk>
2025-05-12 13:55:40 +0300

    WORLDSERVICE-749-amp-service-worker-update with tests

--- cypress/e2e/specialFeatures/serviceWorker/index.cy.js
+++ cypress/e2e/specialFeatures/serviceWorker/index.cy.js
@@ -4,6 +4,7 @@ import {
   serviceWorkerIsRegistered,
   serviceWorkerCaching,
 } from './assertions';
+import { getPathWithSuffix } from '../atiAnalytics/helpers';
 
 const tests = [
   serviceWorkerIsAvailable,
@@ -32,6 +33,12 @@ const testSuites = [
   },
 ];
 
+const ampTestSuites = testSuites.map(testSuite => ({
+  ...testSuite,
+  path: getPathWithSuffix({ path: testSuite.path, suffix: '.amp' }),
+  applicationType: 'amp',
+}));
+
 runTestsForPage({
-  testSuites,
+  testSuites: [...testSuites, ...ampTestSuites],
 });
