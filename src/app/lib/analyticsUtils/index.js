61b0431b4ea53c6b430418e2baedd78386a99e8f
alex-magana <alex.magana@andela.com>
2025-05-12 12:04:12 +0300

    Restore AMP specific values for use by Chartbeat

--- src/app/lib/analyticsUtils/index.js
+++ src/app/lib/analyticsUtils/index.js
@@ -165,7 +165,7 @@ export const getDeviceLanguage = platform => {
 
 export const getHref = platform => {
   if (platform === 'amp') {
-    return null;
+    return `\${sourceUrl}`;
   }
 
   if (onClient() && window.location.href) {
@@ -182,7 +182,7 @@ export const getReferrer = platform => {
        to a `document.referrer` equivalent as the window document is undefined on amp pages.
        https://github.com/ampproject/amphtml/blob/master/spec/amp-var-substitutions.md#document-referrer
     */
-    return null;
+    return `\${documentReferrer}`;
   }
 
   if (onClient() && document.referrer) {
