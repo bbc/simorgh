95a3daa5d6058a243502487fafccd3fbf26dc034
holchris <holly.christopher@bbc.co.uk>
2025-05-13 11:57:15 +0100

    do not run reverb tests against news

--- src/integration/pages/articles/canonicalTests.js
+++ src/integration/pages/articles/canonicalTests.js
@@ -1,11 +1,14 @@
 import {
   runCoreCanonicalTests,
-  // runCanonicalAnalyticsTests,
+  runCanonicalAnalyticsTests,
 } from '../../common';
 import runCrossPlatformTests from './crossPlatformTests';
 
 export default service => {
   runCrossPlatformTests(service);
   runCoreCanonicalTests();
-  // runCanonicalAnalyticsTests();
+
+  if (service !== 'news') {
+    runCanonicalAnalyticsTests();
+  }
 };
