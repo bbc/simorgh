be3367f7899359c73f681a685674ef183309ac8a
HarveyPeachey <harvey.peachey@bbc.co.uk>
2025-05-13 17:22:48 +0100

    adds specific tv heading container lite styling

--- src/app/legacy/containers/OnDemandHeading/StyledTvHeadingContainer.jsx
+++ src/app/legacy/containers/OnDemandHeading/StyledTvHeadingContainer.jsx
@@ -11,11 +11,12 @@ import OnDemandHeading from '.';
 
 const StyledTvHeadingContainer = styled(OnDemandHeading)`
   @media (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
-    padding: ${GEL_SPACING} 0;
+    ${({ isLite }) => (isLite ? 'padding: 0' : `padding: ${GEL_SPACING} 0;`)}
   }
 
   @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
-    padding: ${GEL_SPACING_DBL} 0;
+    ${({ isLite }) =>
+      isLite ? 'padding: 0' : `padding: ${GEL_SPACING_DBL} 0;`}
   }
 `;
 
