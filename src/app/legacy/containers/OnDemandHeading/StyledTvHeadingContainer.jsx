70c38959fc6d963c991b61d5b5050863f6f1369e
HarveyPeachey <harvey.peachey@bbc.co.uk>
2025-05-14 09:55:13 +0100

    adds more spacing

--- src/app/legacy/containers/OnDemandHeading/StyledTvHeadingContainer.jsx
+++ src/app/legacy/containers/OnDemandHeading/StyledTvHeadingContainer.jsx
@@ -4,12 +4,16 @@ import {
   GEL_SPACING_DBL,
 } from '#psammead/gel-foundations/src/spacings';
 import {
+  GEL_GROUP_1_SCREEN_WIDTH_MAX,
   GEL_GROUP_3_SCREEN_WIDTH_MAX,
   GEL_GROUP_4_SCREEN_WIDTH_MIN,
 } from '#psammead/gel-foundations/src/breakpoints';
 import OnDemandHeading from '.';
 
 const StyledTvHeadingContainer = styled(OnDemandHeading)`
+  @media (max-width: ${GEL_GROUP_1_SCREEN_WIDTH_MAX}) {
+    ${({ isLite }) => (isLite ? `padding: ${GEL_SPACING} 0 !important` : '')}
+  }
   @media (max-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
     ${({ isLite }) => (isLite ? 'padding: 0' : `padding: ${GEL_SPACING} 0;`)}
   }
