923c410062f00b1c79bf62db17fcdf6abbc50bcb
Shayne Ahchoon <shayneah@googlemail.com>
2025-05-15 09:20:40 +0100

    WORLDSERVICE-844: Address comments

--- src/app/components/PortraitVideoCarousel/PortraitVideoPromo/index.tsx
+++ src/app/components/PortraitVideoCarousel/PortraitVideoPromo/index.tsx
@@ -1,19 +1,22 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
 import { PortraitVideoItemProps } from '#app/models/types/portraitVideo';
+import Image from '#app/components/Image';
 import Heading from '../../Heading';
 import styles from './index.styles';
 
 export default (item: PortraitVideoItemProps) => {
   const { images, headlines, onClick } = item;
 
-  const image = images?.[0]?.url;
+  const imageUrl = images?.[0]?.url;
   const alt = images?.[0]?.altText || '';
   const headline = headlines?.promoHeadline || '';
 
   return (
     <button type="button" onClick={onClick} css={styles.promoItemButton}>
-      {image && <img src={image} alt={alt} css={styles.image} loading="lazy" />}
+      {imageUrl && (
+        <Image alt={alt} src={imageUrl} css={styles.image} lazyLoad />
+      )}
       <div css={styles.gradientOverlay}>
         <Heading level={3} size="longPrimer" css={styles.promoHeading}>
           {headline}
