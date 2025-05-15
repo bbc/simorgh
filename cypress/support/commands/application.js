7c1773bcdd94f78954b03993a1ac9ba78679ec0e
Aaron Moore <aaron.moore@bbc.co.uk>
2025-05-15 14:04:22 +0100

    Remove unneeded custom function

--- cypress/support/commands/application.js
+++ cypress/support/commands/application.js
@@ -98,22 +98,6 @@ export const getPageDataFromWindow = () => {
   });
 };
 
-export const getApplicationType = () => {
-  cy.url().then(url => {
-    switch (url) {
-      case url.includes('.lite'):
-        return 'lite';
-      case url.includes('.amp'):
-        return 'amp';
-      case url.includes('.app'):
-        return 'app';
-      default:
-        return 'responsive'; // 'responsive' is considered 'canonical' in this context
-    }
-  });
-};
-
 Cypress.Commands.add('testResponseCodeAndType', testResponseCodeAndType);
 Cypress.Commands.add('testResponseCodeAndTypeRetry', testResponseCodeAndType);
 Cypress.Commands.add('getPageDataFromWindow', getPageDataFromWindow);
-Cypress.Commands.add('getApplicationType', getApplicationType);
