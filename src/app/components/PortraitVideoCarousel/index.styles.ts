a81909d669c9658a3ac7cbeb68793e11a86a522f
Shayne Marc Enzo Ahchoon <32307964+shayneahchoon@users.noreply.github.com>
2025-05-16 09:58:31 +0100

    WORLDSERVICE-844: Refactor PV Carousel to set up dev work (#12757)
    * WORLDSERVICE-844: Refactor PV Carousel to set up dev work
    
    * WORLDSERVICE-844: Add PV item unit tests.
    
    * WORLDSERVICE-844: Add PV nav and carousel unit tests.
    
    * WORLDSERVICE-844: Address comments
    
    * WORLDSERVICE-844: Address comments
    
    * WORLDSERVICE-844: Address comments
    
    * WORLDSERVICE-844: Address comments
    
    * WORLDSERVICE-844: Update fixture
    
    * WORLDSERVICE-844: Address comments
    
    * WORLDSERVICE-844: Address comments
    
    * WORLDSERVICE-844: Address comments
    
    * WORLDSERVICE-844: Address comments
    
    * WORLDSERVICE-844: Address comments
    
    * WORLDSERVICE-844: Address comments
    
    * WORLDSERVICE-844: Address comments
    
    * WORLDSERVICE-844: Address comments
    
    ---------
    
    Co-authored-by: Pedram <72317734+pvaliani@users.noreply.github.com>

---
+++ src/app/components/PortraitVideoCarousel/index.styles.ts
@@ -0,0 +1,57 @@
+import { css, Theme } from '@emotion/react';
+import pixelsToRem from '#app/utilities/pixelsToRem';
+import { PROMO_ITEM_WIDTH } from '.';
+
+const styles = {
+  heading: ({ fontSizes, fontVariants, palette }: Theme) =>
+    css({
+      ...fontVariants.sansBold,
+      ...fontSizes.doublePica,
+      color: palette.GREY_10,
+    }),
+  scrollContainer: () =>
+    css({
+      position: 'relative',
+      display: 'flex',
+      alignItems: 'center',
+    }),
+  scrollWrapper: ({ spacings }: Theme) =>
+    css({
+      display: 'flex',
+      overflowX: 'auto',
+      scrollSnapType: 'x mandatory',
+      gap: `${spacings.DOUBLE}rem`,
+      scrollBehavior: 'smooth',
+      WebkitOverflowScrolling: 'touch',
+      '&::-webkit-scrollbar': {
+        display: 'none',
+      },
+      scrollbarWidth: 'none',
+      flex: 1,
+      padding: `${spacings.FULL}rem 0`,
+    }),
+  promo: ({ mq }: Theme) =>
+    css({
+      scrollSnapAlign: 'start',
+      flex: '0 0 35%',
+      textDecoration: 'none',
+      display: 'block',
+      position: 'relative',
+      overflow: 'hidden',
+
+      [mq.GROUP_3_MIN_WIDTH]: {
+        flex: `0 0 ${pixelsToRem(PROMO_ITEM_WIDTH)}rem`,
+      },
+    }),
+  endBlankItem: ({ mq }: Theme) =>
+    css({
+      display: 'none',
+
+      [mq.GROUP_3_MIN_WIDTH]: {
+        display: 'block',
+        flex: `0 0 ${pixelsToRem(PROMO_ITEM_WIDTH / 1.5)}rem`,
+      },
+    }),
+};
+
+export default styles;
