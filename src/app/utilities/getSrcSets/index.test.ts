adadce93bbaf3e888a8aedf9959a481b1686bfe4
Shayne Ahchoon <shayneah@googlemail.com>
2025-05-28 11:15:39 +0100

    WORLDSERVICE-844: Add srcset fix to promos.

---
+++ src/app/utilities/getSrcSets/index.test.ts
@@ -0,0 +1,25 @@
+import { Theme } from '@emotion/react';
+import { GROUP_2_MAX_WIDTH } from '#app/components/ThemeProvider/mediaQueries';
+import getSrcSets from '.';
+
+describe('getSrcSets', () => {
+  it('should return the correct src sets for a given image', () => {
+    const mockMq = {
+      GROUP_2_MAX_WIDTH,
+    } as Theme['mq'];
+
+    const srcSets = getSrcSets({
+      imageUrlTemplate:
+        'https://ichef.test.bbci.co.uk/images/ic/{width}xn/p01wjx8g.jpg',
+      mq: mockMq,
+    });
+
+    expect(srcSets).toStrictEqual({
+      sizes: '(max-width: 37.4375rem) 184px, 224px',
+      srcSet: `https://ichef.test.bbci.co.uk/images/ic/184xn/p01wjx8g.jpg 184w, 
+                          https://ichef.test.bbci.co.uk/images/ic/368xn/p01wjx8g.jpg 368w, 
+                          https://ichef.test.bbci.co.uk/images/ic/224xn/p01wjx8g.jpg 224w, 
+                          https://ichef.test.bbci.co.uk/images/ic/448xn/p01wjx8g.jpg 448w`,
+    });
+  });
+});
---
+++ src/app/utilities/getSrcSets/index.ts
@@ -0,0 +1,34 @@
+import { Theme } from '@emotion/react';
+
+type FuncParams = {
+  imageUrlTemplate: string;
+  mq: Theme['mq'];
+  imageWidthSmall?: number;
+  imageWidthLarge?: number;
+};
+
+export default ({
+  imageUrlTemplate,
+  mq,
+  imageWidthSmall = 128,
+  imageWidthLarge = 512,
+}: FuncParams) => {
+  const IMAGE_SRC_SMALL_2X_UPSCALE_WIDTH = imageWidthSmall * 2;
+  const IMAGE_SRC_LARGE_2X_UPSCALE_WIDTH = imageWidthLarge * 2;
+
+  const replaceWidth = (width: number) =>
+    imageUrlTemplate?.replace('{width}', `${width}`);
+
+  const imgSrcSmall = replaceWidth(imageWidthSmall);
+  const imgSrcSmall2x = replaceWidth(IMAGE_SRC_SMALL_2X_UPSCALE_WIDTH);
+  const imgSrcLarge = replaceWidth(imageWidthLarge);
+  const imgSrcLarge2x = replaceWidth(IMAGE_SRC_LARGE_2X_UPSCALE_WIDTH);
+
+  return {
+    srcSet: `${imgSrcSmall} ${imageWidthSmall}w, 
+                          ${imgSrcSmall2x} ${IMAGE_SRC_SMALL_2X_UPSCALE_WIDTH}w, 
+                          ${imgSrcLarge} ${imageWidthLarge}w, 
+                          ${imgSrcLarge2x} ${IMAGE_SRC_LARGE_2X_UPSCALE_WIDTH}w`,
+    sizes: `${mq.GROUP_2_MAX_WIDTH.replace('@media ', '')} ${imageWidthSmall}px, ${imageWidthLarge}px`,
+  };
+};
