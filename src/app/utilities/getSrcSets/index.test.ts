e569da1884561844e87ea0cd0554cb92bafc0d9f
Shayne Ahchoon <shayneah@googlemail.com>
2025-05-28 13:55:56 +0100

    WORLDSERVICE-844: Add srcset fix to promos.

--- src/app/utilities/getSrcSets/index.test.ts
+++ src/app/utilities/getSrcSets/index.test.ts
@@ -16,10 +16,10 @@ describe('getSrcSets', () => {
 
     expect(srcSets).toStrictEqual({
       sizes: '(max-width: 37.4375rem) 128px, 512px',
-      srcSet: `https://ichef.test.bbci.co.uk/images/ic/184xn/p01wjx8g.jpg 128w, 
-                          https://ichef.test.bbci.co.uk/images/ic/368xn/p01wjx8g.jpg 256w, 
-                          https://ichef.test.bbci.co.uk/images/ic/224xn/p01wjx8g.jpg 512w, 
-                          https://ichef.test.bbci.co.uk/images/ic/448xn/p01wjx8g.jpg 1024w`,
+      srcSet: `https://ichef.test.bbci.co.uk/images/ic/128xn/p01wjx8g.jpg 128w, 
+                          https://ichef.test.bbci.co.uk/images/ic/256xn/p01wjx8g.jpg 256w, 
+                          https://ichef.test.bbci.co.uk/images/ic/512xn/p01wjx8g.jpg 512w, 
+                          https://ichef.test.bbci.co.uk/images/ic/1024xn/p01wjx8g.jpg 1024w`,
     });
   });
 });
