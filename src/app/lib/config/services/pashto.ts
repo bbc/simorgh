7130d799a81a4f0fd046ee2ca2750d2eaa385cdb
jordit <jordi.texido@yahoo.com>
2025-05-30 11:34:45 +0200

    Pashto add lite site translation

--- src/app/lib/config/services/pashto.ts
+++ src/app/lib/config/services/pashto.ts
@@ -65,6 +65,15 @@ export const service: DefaultServiceConfig = {
       relatedContent: 'ورته مطالب',
       relatedTopics: 'اړونده مطالب',
       navMenuText: 'برخې',
+      liteSite: {
+        onboardingMessage: 'تاسې د وېب‌سایټ داسې بڼه ګورئ چې یوازې متن لري او لږ انټرنېټ کاروي. که غواړئ عکسونه او ویډیوګانې وګورئ، نو د وېب‌سایټ اصلي (لومړنۍ) بڼې ته ورشئ.',
+        toMainSite: 'د وېب‌سایټ اصلي بڼې ته مې وروله',
+        informationPage:
+          'د وېب‌سایټ دې بڼې په اړه چې ډېر انټرنېټ نه مصرفوي نور معلومات دلته موندلای شئ.',
+        informationPageLink: 'https://www.bbc.com/pashto/articles/ce9vxxy2zkpo',
+        dataSaving: 'دې لپاره چې انټرنېټ مو لږ مصرف شي، یوازې متن ولولئ',
+        articleDataSavingLinkText: 'دې لپاره چې انټرنېټ مو لږ مصرف شي، یوازې متن ولولئ',
+      },
       mediaAssetPage: {
         mediaPlayer: 'میډیا پلیئر',
         audioPlayer: 'اډیو پلیئر',
@@ -381,6 +390,7 @@ export const service: DefaultServiceConfig = {
       {
         title: 'راډیویي خپرونې',
         url: '/pashto/topics/c9xz1ekw79nt',
+        hideOnLiteSite: true,
       },
     ],
   },
