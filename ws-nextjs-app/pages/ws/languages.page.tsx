f7c5545e0594f94477e4a1b0179a621c652b4abd
Lukas Freimonas <lukas.freimonas.ext@bbc.co.uk>
2025-06-02 11:55:47 +0300

    WS-265 getInitialData for /pidgin HomePage

--- ws-nextjs-app/pages/ws/languages.page.tsx
+++ ws-nextjs-app/pages/ws/languages.page.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 import { GetServerSideProps } from 'next';
 import { STATIC_PAGE, HOME_PAGE } from '#app/routes/utils/pageTypes';
+import getInitialData from '#app/routes/homePage/getInitialData';
+import { Agent } from 'undici';
+import nodeLogger from '#app/lib/logger.node';
 import { LanguagesPageProps } from './types';
+
+const logger = nodeLogger(__filename);
 
 const HomePage = dynamic(() => import('#pages/HomePage'));
 const LanguagesPageLayout = dynamic(() => import('./LanguagesPageLayout'));
@@ -15,6 +20,59 @@ export const getServerSideProps: GetServerSideProps = async context => {
 
   const isTestEnvironment = context.query.renderer_env === 'test';
   const pageType = isTestEnvironment ? HOME_PAGE : STATIC_PAGE;
+  const service = isTestEnvironment ? 'pidgin' : 'ws';
+
+  if (isTestEnvironment) {
+    const initialData = await getInitialData({
+      service,
+      path: '/pidgin',
+      // path: context?.resolvedUrl,
+      pageType,
+      variant: undefined,
+      getAgent: () => Promise.resolve({} as Agent),
+    });
+
+    if (initialData?.error) {
+      logger.info('Initial data error: ', initialData);
+      context.res.statusCode = initialData.status;
+      return {
+        props: {
+          error: initialData?.error,
+          isAmp: false,
+          isNextJs: true,
+          page: null,
+          pageData: {
+            metadata: {
+              type: pageType,
+            },
+          },
+          pageType,
+          pathname: context?.resolvedUrl,
+          service,
+          status: initialData?.status,
+          timeOnServer: Date.now(),
+          isTestEnvironment,
+        },
+      };
+    }
+
+    return {
+      props: {
+        error: null,
+        isAmp: false,
+        isNextJs: true,
+        page: null,
+        pageData: initialData?.pageData,
+        pageType,
+        pathname: '/pidgin',
+        // pathname: context?.resolvedUrl,
+        service,
+        status: initialData?.status,
+        timeOnServer: Date.now(),
+        isTestEnvironment,
+      },
+    };
+  }
 
   return {
     props: {
@@ -28,8 +86,8 @@ export const getServerSideProps: GetServerSideProps = async context => {
         },
       },
       pageType,
-      pathname: context.resolvedUrl,
-      service: isTestEnvironment ? 'pidgin' : 'ws',
+      pathname: context?.resolvedUrl,
+      service,
       status: 200,
       timeOnServer: Date.now(), // TODO: check if needed?
       isTestEnvironment,
