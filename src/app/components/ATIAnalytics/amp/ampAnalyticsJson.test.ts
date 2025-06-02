90ab88e52b1c6e5c1e74c1719a4f802a997d3152
alex-magana <alex.magana@andela.com>
2025-06-02 17:38:19 +0300

    Rewrite tests with shared helper

--- src/app/components/ATIAnalytics/amp/ampAnalyticsJson.test.ts
+++ src/app/components/ATIAnalytics/amp/ampAnalyticsJson.test.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-template-curly-in-string */
+import { getExpectedAtiDestination } from '#cypress/e2e/specialFeatures/atiAnalytics/helpers';
 import { ReverbBeaconConfig } from '../types';
 import ampAnalyticsJson from './ampAnalyticsJson';
 
@@ -60,25 +61,25 @@ describe('AMP ATI Analytics', () => {
     };
 
     it.each`
-      environment | destination                 | appName                | baseUrl
-      ${'local'}  | ${'WS_NEWS_LANGUAGES_TEST'} | ${'news-gahuza'}       | ${'https://logws1363.ati-host.net/hit.xiti?'}
-      ${'test'}   | ${'WS_NEWS_LANGUAGES_TEST'} | ${'news-gahuza'}       | ${'https://logws1363.ati-host.net/hit.xiti?'}
-      ${'live'}   | ${'WS_NEWS_LANGUAGES'}      | ${'news-gahuza'}       | ${'https://a1.api.bbc.co.uk/hit.xiti?'}
-      ${'test'}   | ${'NEWS_PS'}                | ${'news'}              | ${'https://logws1363.ati-host.net/hit.xiti?'}
-      ${'live'}   | ${'NEWS_PS_TEST'}           | ${'news'}              | ${'https://a1.api.bbc.co.uk/hit.xiti?'}
-      ${'test'}   | ${'NEWS_LANGUAGES_PS'}      | ${'news-cymrufyw'}     | ${'https://logws1363.ati-host.net/hit.xiti?'}
-      ${'live'}   | ${'NEWS_LANGUAGES_PS_TEST'} | ${'news-cymrufyw'}     | ${'https://a1.api.bbc.co.uk/hit.xiti?'}
-      ${'test'}   | ${'NEWS_LANGUAGES_PS'}      | ${'news-naidheachdan'} | ${'https://logws1363.ati-host.net/hit.xiti?'}
-      ${'live'}   | ${'NEWS_LANGUAGES_PS_TEST'} | ${'news-naidheachdan'} | ${'https://a1.api.bbc.co.uk/hit.xiti?'}
-      ${'test'}   | ${'PS_HOMEPAGE'}            | ${'scotland'}          | ${'https://logws1363.ati-host.net/hit.xiti?'}
-      ${'live'}   | ${'PS_HOMEPAGE_TEST'}       | ${'scotland'}          | ${'https://a1.api.bbc.co.uk/hit.xiti?'}
-      ${'test'}   | ${'NEWSROUND'}              | ${'newsround'}         | ${'https://logws1363.ati-host.net/hit.xiti?'}
-      ${'live'}   | ${'NEWSROUND_TEST'}         | ${'newsround'}         | ${'https://a1.api.bbc.co.uk/hit.xiti?'}
-      ${'test'}   | ${'SPORT_PS'}               | ${'sport'}             | ${'https://logws1363.ati-host.net/hit.xiti?'}
-      ${'live'}   | ${'SPORT_PS_TEST'}          | ${'sport'}             | ${'https://a1.api.bbc.co.uk/hit.xiti?'}
+      environment | destination                 | appName                | service           | baseUrl
+      ${'local'}  | ${'WS_NEWS_LANGUAGES_TEST'} | ${'news-gahuza'}       | ${'gahuza'}       | ${'https://logws1363.ati-host.net/hit.xiti?'}
+      ${'test'}   | ${'WS_NEWS_LANGUAGES_TEST'} | ${'news-gahuza'}       | ${'gahuza'}       | ${'https://logws1363.ati-host.net/hit.xiti?'}
+      ${'live'}   | ${'WS_NEWS_LANGUAGES'}      | ${'news-gahuza'}       | ${'gahuza'}       | ${'https://a1.api.bbc.co.uk/hit.xiti?'}
+      ${'live'}   | ${'NEWS_PS'}                | ${'news'}              | ${'news'}         | ${'https://a1.api.bbc.co.uk/hit.xiti?'} 
+      ${'test'}   | ${'NEWS_PS_TEST'}           | ${'news'}              | ${'news'}         | ${'https://logws1363.ati-host.net/hit.xiti?'}
+      ${'live'}   | ${'NEWS_LANGUAGES_PS'}      | ${'news-cymrufyw'}     | ${'cymrufyw'}     | ${'https://a1.api.bbc.co.uk/hit.xiti?'}
+      ${'test'}   | ${'NEWS_LANGUAGES_PS_TEST'} | ${'news-cymrufyw'}     | ${'cymrufyw'}     | ${'https://logws1363.ati-host.net/hit.xiti?'}
+      ${'live'}   | ${'NEWS_LANGUAGES_PS'}      | ${'news-naidheachdan'} | ${'naidheachdan'} | ${'https://a1.api.bbc.co.uk/hit.xiti?'}
+      ${'test'}   | ${'NEWS_LANGUAGES_PS_TEST'} | ${'news-naidheachdan'} | ${'naidheachdan'} | ${'https://logws1363.ati-host.net/hit.xiti?'}
+      ${'live'}   | ${'PS_HOMEPAGE'}            | ${'scotland'}          | ${'scotland'}     | ${'https://a1.api.bbc.co.uk/hit.xiti?'}
+      ${'test'}   | ${'PS_HOMEPAGE_TEST'}       | ${'scotland'}          | ${'scotland'}     | ${'https://logws1363.ati-host.net/hit.xiti?'}
+      ${'live'}   | ${'NEWSROUND'}              | ${'newsround'}         | ${'newsround'}    | ${'https://a1.api.bbc.co.uk/hit.xiti?'}
+      ${'test'}   | ${'NEWSROUND_TEST'}         | ${'newsround'}         | ${'newsround'}    | ${'https://logws1363.ati-host.net/hit.xiti?'}
+      ${'live'}   | ${'SPORT_PS'}               | ${'sport'}             | ${'sport'}        | ${'https://a1.api.bbc.co.uk/hit.xiti?'}
+      ${'test'}   | ${'SPORT_PS_TEST'}          | ${'sport'}             | ${'sport'}        | ${'https://logws1363.ati-host.net/hit.xiti?'}
     `(
       'should match this JSON structure for destination - $destination and app name - $appName in environment - $environment',
-      ({ environment, destination, appName, baseUrl }) => {
+      ({ environment, destination, appName, service, baseUrl }) => {
         const reverbParams = {
           ...mockReverbParams,
           params: {
@@ -95,27 +96,12 @@ describe('AMP ATI Analytics', () => {
           },
         };
 
-        const expectedDestination: { [key: string]: string } = {
-          WS_NEWS_LANGUAGES_TEST: '598343',
-          WS_NEWS_LANGUAGES: '598342',
-          NEWS_PS:
-            '$IF($EQUALS($MATCH(${ampGeo}, gbOrUnknown, 0), gbOrUnknown), 598285, 598287)',
-          NEWS_PS_TEST:
-            '$IF($EQUALS($MATCH(${ampGeo}, gbOrUnknown, 0), gbOrUnknown), 598286, 598288)',
-          NEWS_LANGUAGES_PS:
-            '$IF($EQUALS($MATCH(${ampGeo}, gbOrUnknown, 0), gbOrUnknown), 598291, 598289)',
-          NEWS_LANGUAGES_PS_TEST:
-            '$IF($EQUALS($MATCH(${ampGeo}, gbOrUnknown, 0), gbOrUnknown), 598292, 598290)',
-          PS_HOMEPAGE: '598273',
-          PS_HOMEPAGE_TEST: '598274',
-          NEWSROUND: '598293',
-          NEWSROUND_TEST: '598294',
-          SPORT_PS:
-            '$IF($EQUALS($MATCH(${ampGeo}, gbOrUnknown, 0), gbOrUnknown), 598310, 598308)',
-          SPORT_PS_TEST:
-            '$IF($EQUALS($MATCH(${ampGeo}, gbOrUnknown, 0), gbOrUnknown), 598311, 598309)',
-        };
-        const expectedPageViewParams = `\${base}s=${expectedDestination[destination]}&s2=40&p=gahuza.articles.cly1jw4x585o.page&r=\${screenWidth}x\${screenHeight}x\${screenColorDepth}&re=\${availableScreenWidth}x\${availableScreenHeight}&hl=\${timestamp}&lng=\${browserLanguage}&x1=[urn%3Abbc%3Aoptimo%3Aasset%3Acly1jw4x585o]&x2=[amp]&x3=[${appName}]&x4=[rw]&x5=[\${sourceUrl}]&x6=[\${documentReferrer}]&x7=[article]&x8=[simorgh]&x9=[US%2520irashishikariza%2520u%2520Rwanda%2520na%2520DR%2520Congo%2520kugera%2520ku%2520mahoro%2520bikazana%2520n'ishoramari%2520rya%2520miliyari%2520z'amadorari]&x11=[2025-05-02T07%3A00%3A25.419Z]&x12=[2025-05-02T07%3A00%3A25.419Z]&x13=[Democratic%2BRepublic%2Bof%2BCongo~Rwanda~March%2B23%2BMovement~M23%2Boffensive]&x14=[3548f44f-46f5-4e6e-8628-3f668f161691~8125f2a9-3259-4f35-ab75-d9a6577fdc88~b03e7bfd-9a46-4053-aeed-f9f55f5e5567~c78c7532-43b3-490d-ad5b-0fc47b906e42]&x17=[Democratic%2BRepublic%2Bof%2BCongo~Rwanda~March%2B23%2BMovement~M23%2Boffensive]&ref=\${documentReferrer}`;
+        const expectedDestination = getExpectedAtiDestination({
+          service,
+          applicationEnv: environment,
+        });
+
+        const expectedPageViewParams = `\${base}s=${expectedDestination}&s2=40&p=gahuza.articles.cly1jw4x585o.page&r=\${screenWidth}x\${screenHeight}x\${screenColorDepth}&re=\${availableScreenWidth}x\${availableScreenHeight}&hl=\${timestamp}&lng=\${browserLanguage}&x1=[urn%3Abbc%3Aoptimo%3Aasset%3Acly1jw4x585o]&x2=[amp]&x3=[${appName}]&x4=[rw]&x5=[\${sourceUrl}]&x6=[\${documentReferrer}]&x7=[article]&x8=[simorgh]&x9=[US%2520irashishikariza%2520u%2520Rwanda%2520na%2520DR%2520Congo%2520kugera%2520ku%2520mahoro%2520bikazana%2520n'ishoramari%2520rya%2520miliyari%2520z'amadorari]&x11=[2025-05-02T07%3A00%3A25.419Z]&x12=[2025-05-02T07%3A00%3A25.419Z]&x13=[Democratic%2BRepublic%2Bof%2BCongo~Rwanda~March%2B23%2BMovement~M23%2Boffensive]&x14=[3548f44f-46f5-4e6e-8628-3f668f161691~8125f2a9-3259-4f35-ab75-d9a6577fdc88~b03e7bfd-9a46-4053-aeed-f9f55f5e5567~c78c7532-43b3-490d-ad5b-0fc47b906e42]&x17=[Democratic%2BRepublic%2Bof%2BCongo~Rwanda~March%2B23%2BMovement~M23%2Boffensive]&ref=\${documentReferrer}`;
 
         expect(
           ampAnalyticsJson({
