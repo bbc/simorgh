73380bfdce5d1d42235d89bae682442fd20d730c
Toby Cox <eagerterrier@gmail.com>
2025-06-02 11:24:33 +0100

    Update directives.js

--- src/server/utilities/cspHeader/directives.js
+++ src/server/utilities/cspHeader/directives.js
@@ -320,11 +320,11 @@ export const generateMediaSrc = () => {
   return ["'self' blob: https:"];
 };
 
-export const generateScriptSrc = ({ isAmp, isLive }) => {
+export const generateScriptSrc = ({ isAmp, isLive, nonce }) => {
   if (!isLive && isAmp) return directives.scriptSrc.ampNonLive.sort();
-  if (!isLive && !isAmp) return directives.scriptSrc.canonicalNonLive.sort();
+  if (!isLive && !isAmp) return [`'nonce-${nonce}'`, ...directives.scriptSrc.canonicalNonLive].sort();
   if (isLive && isAmp) return directives.scriptSrc.ampLive.sort();
-  return directives.scriptSrc.canonicalLive.sort();
+  return [`'nonce-${nonce}'`, ...directives.scriptSrc.canonicalLive].sort();
 };
 
 export const generateStyleSrc = ({ isAmp, isLive }) => {
@@ -344,7 +344,7 @@ export const generateWorkerSrc = ({ isAmp }) =>
  * `yarn build && yarn start` & visit a localhost URL.
  * View the developer console for errors.
  */
-export const cspDirectives = ({ isAmp, isLive, service }) => ({
+export const cspDirectives = ({ isAmp, isLive, service, nonce }) => ({
   directives: {
     'default-src': generateDefaultSrc(),
     'child-src': generateChildSrc({ isAmp }),
@@ -352,7 +352,7 @@ export const cspDirectives = ({ isAmp, isLive, service }) => ({
     'font-src': generateFontSrc({ isAmp, isLive }),
     'frame-src': generateFrameSrc({ isAmp, isLive }),
     'img-src': generateImgSrc({ isAmp, isLive }),
-    'script-src': generateScriptSrc({ isAmp, isLive }),
+    'script-src': generateScriptSrc({ isAmp, isLive, nonce }),
     'style-src': generateStyleSrc({ isAmp, isLive }),
     'media-src': generateMediaSrc(),
     'worker-src': generateWorkerSrc({ isAmp }),
