bcb8a1a6b3b6bb7e8a63679b64c1a75ea536cd34
Lukas Freimonas <lukas.freimonas.ext@bbc.co.uk>
2025-06-03 10:48:04 +0300

    WS-265 props code consolidated; getInitialData() --> getPageData(); isTestEnvironment --> isLive()

--- ws-nextjs-app/pages/ws/languages.page.tsx
+++ ws-nextjs-app/pages/ws/languages.page.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 import { GetServerSideProps } from 'next';
 import { STATIC_PAGE, HOME_PAGE } from '#app/routes/utils/pageTypes';
-import getInitialData from '#app/routes/homePage/getInitialData';
-import { Agent } from 'undici';
+import PageDataParams from '#app/models/types/pageDataParams';
+import isLive from '#app/lib/utilities/isLive';
+import { Services, PageTypes } from '#app/models/types/global';
+import getPageData from '../../utilities/pageRequests/getPageData';
 import { LanguagesPageProps } from './types';
 
 const HomePage = dynamic(() => import('#pages/HomePage/HomePage'));
@@ -15,87 +17,75 @@ export const getServerSideProps: GetServerSideProps = async context => {
     'public, stale-if-error=300, stale-while-revalidate=120, max-age=30',
   );
 
-  const isTestEnvironment = context.query.renderer_env === 'test';
-  const pageType = isTestEnvironment ? HOME_PAGE : STATIC_PAGE;
-  const service = isTestEnvironment ? 'pidgin' : 'ws';
+  const { id } = context.query as PageDataParams;
+  const baseProps = {
+    error: null,
+    isAmp: false,
+    isNextJs: true,
+    page: null,
+    status: 200,
+    timeOnServer: Date.now(),
+    pageType: STATIC_PAGE as PageTypes,
+    service: 'ws' as Services,
+    pageData: {
+      metadata: {
+        type: STATIC_PAGE,
+        atiAnalytics: {},
+      },
+    },
+    pathname: context?.resolvedUrl,
+  };
 
-  if (isTestEnvironment) {
-    const initialData = await getInitialData({
-      service,
-      path: '/pidgin',
-      pageType,
-      variant: undefined,
-      getAgent: () => Promise.resolve({} as Agent),
-    });
+  if (isLive()) {
+    return {
+      props: baseProps,
+    };
+  }
 
-    if (initialData?.error) {
-      context.res.statusCode = initialData.status;
-      return {
-        props: {
-          error: initialData?.error,
-          isAmp: false,
-          isNextJs: true,
-          page: null,
-          pageData: {
-            metadata: {
-              type: pageType,
-              atiAnalytics: {},
-            },
-          },
-          pageType,
-          pathname: context?.resolvedUrl,
-          service,
-          status: initialData?.status,
-          timeOnServer: Date.now(),
-          isTestEnvironment,
-        },
-      };
-    }
+  const { data } = await getPageData({
+    id,
+    service: 'pidgin',
+    variant: undefined,
+    rendererEnv: 'live',
+    resolvedUrl: '/pidgin',
+    pageType: HOME_PAGE,
+  });
 
+  if (data?.error) {
     return {
       props: {
-        error: null,
-        isAmp: false,
-        isNextJs: true,
-        page: null,
+        ...baseProps,
+        error: data?.error,
+        status: data?.status,
+        pageType: HOME_PAGE,
+        service: 'pidgin',
         pageData: {
-          ...initialData?.pageData,
+          metadata: {
+            type: HOME_PAGE,
+            atiAnalytics: {},
+          },
         },
-        pageType,
-        pathname: '/pidgin',
-        service,
-        status: initialData?.status,
-        timeOnServer: Date.now(),
-        isTestEnvironment,
       },
     };
   }
 
   return {
     props: {
-      error: null,
-      isAmp: false,
-      isNextJs: true,
-      page: null,
+      ...baseProps,
+      pageType: HOME_PAGE,
+      service: 'pidgin',
+      pathname: '/pidgin',
+      status: data?.status,
       pageData: {
-        metadata: {
-          type: pageType,
-          atiAnalytics: {},
-        },
+        ...data?.pageData,
       },
-      pageType,
-      pathname: context?.resolvedUrl,
-      service,
-      status: 200,
-      timeOnServer: Date.now(), // TODO: check if needed?
-      isTestEnvironment,
     },
   };
 };
 
 export default function LanguagesPage({ ...props }: LanguagesPageProps) {
-  if (props.isTestEnvironment) {
-    return <HomePage pageData={props.pageData} />;
+  if (isLive()) {
+    return <LanguagesPageLayout />;
   }
-  return <LanguagesPageLayout />;
+  return <HomePage pageData={props.pageData} />;
 }
