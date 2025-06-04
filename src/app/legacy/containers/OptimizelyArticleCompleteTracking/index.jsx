40e628e94bd57202ca24cb825b9f48cb197e8a9b
HarveyPeachey <harvey.peachey@bbc.co.uk>
2025-06-04 10:23:08 +0100

    adds decision logic for article completes multi experiments

--- src/app/legacy/containers/OptimizelyArticleCompleteTracking/index.jsx
+++ src/app/legacy/containers/OptimizelyArticleCompleteTracking/index.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useContext, useEffect, useRef } from 'react';
 import { OptimizelyContext } from '@optimizely/react-sdk';
 import { RequestContext } from '#contexts/RequestContext';
-import useOptimizelyVariation from '#hooks/useOptimizelyVariation';
-import OPTIMIZELY_CONFIG from '#lib/config/optimizely';
+// import useOptimizelyVariation from '#hooks/useOptimizelyVariation';
+// import OPTIMIZELY_CONFIG from '#lib/config/optimizely';
 
-const OptimizelyArticleCompleteTracking = flagKey => {
+const experimentFlagKeys = ['dummy_experiment', 'dummy_experiment_1'];
+
+const OptimizelyArticleCompleteTracking = () => {
   const ref = useRef();
   const observer = useRef();
   const { isAmp } = useContext(RequestContext);
@@ -12,13 +14,7 @@ const OptimizelyArticleCompleteTracking = flagKey => {
   const [pageCompleteSent, setPageCompleteSent] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
 
-  // Updated
-  const experimentVariation = useOptimizelyVariation(
-    OPTIMIZELY_CONFIG.flagKeys[flagKey],
-  );
-
-  const sendPageCompleteEvent =
-    experimentVariation && !isAmp && !pageCompleteSent && isVisible;
+  const sendPageCompleteEvent = !isAmp && !pageCompleteSent && isVisible;
 
   const initObserver = async () => {
     if (typeof window.IntersectionObserver === 'undefined') {
@@ -42,7 +38,15 @@ const OptimizelyArticleCompleteTracking = flagKey => {
   useEffect(() => {
     if (sendPageCompleteEvent) {
       optimizely?.onReady().then(() => {
-        optimizely.track('article_completes');
+        const decisions = optimizely.decideAll();
+        const isUserInAnyExperiments = experimentFlagKeys.some(
+          flagKey => !(decisions[flagKey].variationKey === 'off'),
+        );
+
+        if (isUserInAnyExperiments) {
+          optimizely.track('article_completes');
+        }
+
         setPageCompleteSent(true);
       });
     }
