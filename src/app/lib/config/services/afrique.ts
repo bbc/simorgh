39d9aed36e617f19afe254d2e21aa8b7878b939c
Jordi Teixidó <jordit@users.noreply.github.com>
2025-06-04 14:08:41 +0100

    Linting

--- src/app/lib/config/services/afrique.ts
+++ src/app/lib/config/services/afrique.ts
@@ -66,13 +66,16 @@ export const service: DefaultServiceConfig = {
       relatedTopics: 'Sujets associés',
       navMenuText: 'Rubriques',
       liteSite: {
-        onboardingMessage: 'Vous visualisez une version texte de ce site web qui utilise moins de données. Voir la version principale du site, avec toutes les images et vidéos.',
+        onboardingMessage:
+          'Vous visualisez une version texte de ce site web qui utilise moins de données. Voir la version principale du site, avec toutes les images et vidéos.',
         toMainSite: 'Me rediriger vers le site principal',
         informationPage:
           'En savoir plus sur cette version économe en données',
-        informationPageLink: 'https://www.bbc.com/afrique/articles/c861e868x5eo',
+        informationPageLink:
+          'https://www.bbc.com/afrique/articles/c861e868x5eo',
         dataSaving: 'Lire uniquement le texte pour utiliser moins de données',
-        articleDataSavingLinkText: 'Lire uniquement le texte pour utiliser moins de données',
+        articleDataSavingLinkText:
+          'Lire uniquement le texte pour utiliser moins de données',
       },
       mediaAssetPage: {
         mediaPlayer: 'Lecteur média',
