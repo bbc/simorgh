5726403433c53aa508502b53abb66fb7386bef59
Lukas Freimonas <lukas.freimonas.ext@bbc.co.uk>
2025-06-12 10:07:25 +0300

    WS-396 languagePage test & live E2E tests updated

--- ws-nextjs-app/cypress/e2e/languagesPage/assertions/index.ts
+++ ws-nextjs-app/cypress/e2e/languagesPage/assertions/index.ts
@@ -1,8 +1,11 @@
 export const assertPidginHomepage = () => {
-  cy.get('h1').should('contain', 'BBC News, Pidgin, Home');
-  cy.get('h1').should('not.have.text', 'Get the news in your language');
+  it('should render the Pidgin Homepage', () => {
+    cy.get('h1').should('contain', 'BBC News, Pidgin, Home');
+    cy.get('h1').should('not.have.text', 'Get the news in your language');
+  });
 };
-
 export const assertWSLanguagesPage = () => {
-  cy.get('h1').should('have.text', 'Get the news in your language');
+  it('should render the Languages Page', () => {
+    cy.get('h1').should('have.text', 'Get the news in your language');
+  });
 };
