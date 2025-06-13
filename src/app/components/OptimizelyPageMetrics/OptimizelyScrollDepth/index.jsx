13ffff70a4c9c1fd3c9c5905769e43f954e21e02
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2025-06-13 16:57:08 +0100

    WS-114: Tidy

--- src/app/components/OptimizelyPageMetrics/OptimizelyScrollDepth/index.jsx
+++ src/app/components/OptimizelyPageMetrics/OptimizelyScrollDepth/index.jsx
@@ -1,6 +1,5 @@
 import useOptimizelyScrollDepth from '#hooks/useOptimizelyScrollDepth';
 
-// bit long winded - but avoids breaking react hooks rule
 const OptimizelyScrollDepth = () => {
   useOptimizelyScrollDepth();
   return null;
