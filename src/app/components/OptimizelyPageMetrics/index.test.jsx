53983d76bdbcacfbbd00413ae80dcbde3428f2bf
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2025-06-13 16:32:09 +0100

    WS-114: Revert

--- src/app/components/OptimizelyPageMetrics/index.test.jsx
+++ src/app/components/OptimizelyPageMetrics/index.test.jsx
@@ -1,212 +1,212 @@
-// /* eslint-disable no-console */
-// import React from 'react';
-// import { render, waitFor, act } from '@testing-library/react';
-// import { OptimizelyProvider } from '@optimizely/react-sdk';
+/* eslint-disable no-console */
+import React from 'react';
+import { render, waitFor, act } from '@testing-library/react';
+import { OptimizelyProvider } from '@optimizely/react-sdk';
 
-// import { RequestContextProvider } from '#contexts/RequestContext';
-// import { ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
-// import useOptimizelyVariation from '#hooks/useOptimizelyVariation';
+import { RequestContextProvider } from '#contexts/RequestContext';
+import { ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
+import useOptimizelyVariation from '#hooks/useOptimizelyVariation';
 
-// import OptimizelyPageMetrics from '.';
+import OptimizelyPageMetrics from '.';
 
-// jest.mock('#hooks/useOptimizelyVariation', () => jest.fn(() => null));
+jest.mock('#hooks/useOptimizelyVariation', () => jest.fn(() => null));
 
-// const optimizely = {
-//   onReady: jest.fn(() => Promise.resolve()),
-//   track: jest.fn(),
-//   setUser: jest.fn(() => Promise.resolve()),
-// };
+const optimizely = {
+  onReady: jest.fn(() => Promise.resolve()),
+  track: jest.fn(),
+  setUser: jest.fn(() => Promise.resolve()),
+};
 
-// const observers = new Map();
+const observers = new Map();
 
-// const IntersectionObserver = jest.fn(cb => {
-//   const item = {
-//     callback: cb,
-//     elements: new Set(),
-//   };
+const IntersectionObserver = jest.fn(cb => {
+  const item = {
+    callback: cb,
+    elements: new Set(),
+  };
 
-//   const instance = {
-//     observe: jest.fn(element => {
-//       item.elements.add(element);
-//     }),
-//     disconnect: jest.fn(() => {
-//       item.elements.clear();
-//     }),
-//   };
+  const instance = {
+    observe: jest.fn(element => {
+      item.elements.add(element);
+    }),
+    disconnect: jest.fn(() => {
+      item.elements.clear();
+    }),
+  };
 
-//   observers.set(instance, item);
+  observers.set(instance, item);
 
-//   return instance;
-// });
+  return instance;
+});
 
-// const getObserverInstance = element => {
-//   try {
-//     const [instance] = Array.from(observers).find(([, item]) =>
-//       item.elements.has(element),
-//     );
+const getObserverInstance = element => {
+  try {
+    const [instance] = Array.from(observers).find(([, item]) =>
+      item.elements.has(element),
+    );
 
-//     return instance;
-//   } catch (e) {
-//     throw new Error('Failed to find IntersectionObserver for element.');
-//   }
-// };
+    return instance;
+  } catch (e) {
+    throw new Error('Failed to find IntersectionObserver for element.');
+  }
+};
 
-// const triggerIntersection = ({ changes, observer }) => {
-//   const item = observers.get(observer);
+const triggerIntersection = ({ changes, observer }) => {
+  const item = observers.get(observer);
 
-//   item.callback(changes);
-// };
+  item.callback(changes);
+};
 
-// const ContextWrap = ({ pageType, isAmp, children, service }) => (
-//   <RequestContextProvider
-//     isAmp={isAmp}
-//     pageType={pageType}
-//     service={service}
-//     pathname="/pathname"
-//   >
-//     <OptimizelyProvider optimizely={optimizely} isServerSide>
-//       {children}
-//     </OptimizelyProvider>
-//   </RequestContextProvider>
-// );
+const ContextWrap = ({ pageType, isAmp, children, service }) => (
+  <RequestContextProvider
+    isAmp={isAmp}
+    pageType={pageType}
+    service={service}
+    pathname="/pathname"
+  >
+    <OptimizelyProvider optimizely={optimizely} isServerSide>
+      {children}
+    </OptimizelyProvider>
+  </RequestContextProvider>
+);
 
-// const { error } = console;
+const { error } = console;
 
-// beforeEach(() => {
-//   jest.clearAllMocks();
-//   jest.useFakeTimers();
-//   console.error = jest.fn();
-//   global.IntersectionObserver = IntersectionObserver;
-// });
+beforeEach(() => {
+  jest.clearAllMocks();
+  jest.useFakeTimers();
+  console.error = jest.fn();
+  global.IntersectionObserver = IntersectionObserver;
+});
 
-// afterEach(() => {
-//   jest.runOnlyPendingTimers();
-//   jest.useRealTimers();
-//   console.error = error;
-//   observers.clear();
-// });
+afterEach(() => {
+  jest.runOnlyPendingTimers();
+  jest.useRealTimers();
+  console.error = error;
+  observers.clear();
+});
 
-// describe('Optimizely Page Complete tracking', () => {
-//   beforeEach(() => {
-//     jest.clearAllMocks();
-//   });
-//   // add more
-//   // rewrite
-//   it('should not call Optimizely track function for Article Page on page render if no props are supplied', async () => {
-//     useOptimizelyVariation.mockReturnValue('variation_1');
+describe('Optimizely Page Complete tracking', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  // add more
+  // rewrite
+  it('should not call Optimizely track function for Article Page on page render if no props are supplied', async () => {
+    useOptimizelyVariation.mockReturnValue('variation_1');
 
-//     render(
-//       <ContextWrap pageType={ARTICLE_PAGE} service="news" isAmp={false}>
-//         <OptimizelyPageMetrics />
-//       </ContextWrap>,
-//     );
+    render(
+      <ContextWrap pageType={ARTICLE_PAGE} service="news" isAmp={false}>
+        <OptimizelyPageMetrics />
+      </ContextWrap>,
+    );
 
-//     await waitFor(() => {
-//       expect(optimizely.track).toHaveBeenCalledTimes(0);
-//     });
-//   });
+    await waitFor(() => {
+      expect(optimizely.track).toHaveBeenCalledTimes(0);
+    });
+  });
 
-//   it('should not call Optimizely track function for Article Page on page render if ...trackPageView', async () => {
-//     useOptimizelyVariation.mockReturnValue('variation_1');
+  it('should not call Optimizely track function for Article Page on page render if ...trackPageView', async () => {
+    useOptimizelyVariation.mockReturnValue('variation_1');
 
-//     render(
-//       <ContextWrap pageType={ARTICLE_PAGE} service="news" isAmp={false}>
-//         <OptimizelyPageMetrics trackPageView />
-//       </ContextWrap>,
-//     );
+    render(
+      <ContextWrap pageType={ARTICLE_PAGE} service="news" isAmp={false}>
+        <OptimizelyPageMetrics trackPageView />
+      </ContextWrap>,
+    );
 
-//     await waitFor(() => {
-//       expect(optimizely.track).toHaveBeenCalledTimes(1);
-//       expect(optimizely.track).toHaveBeenCalledWith('page-views');
-//     });
-//   });
+    await waitFor(() => {
+      expect(optimizely.track).toHaveBeenCalledTimes(1);
+      expect(optimizely.track).toHaveBeenCalledWith('page-views');
+    });
+  });
 
-//   // rewrite so it's checking whats being called
-//   it.skip('should not send tracking event when pageComplete is false', async () => {
-//     useOptimizelyVariation.mockReturnValue('variation_1');
+  // rewrite so it's checking whats being called
+  it.skip('should not send tracking event when pageComplete is false', async () => {
+    useOptimizelyVariation.mockReturnValue('variation_1');
 
-//     const { container } = render(
-//       <ContextWrap pageType={ARTICLE_PAGE} service="news" isAmp={false}>
-//         <OptimizelyPageMetrics />
-//       </ContextWrap>,
-//     );
+    const { container } = render(
+      <ContextWrap pageType={ARTICLE_PAGE} service="news" isAmp={false}>
+        <OptimizelyPageMetrics />
+      </ContextWrap>,
+    );
 
-//     const element = container.getElementsByTagName('div')[0];
-//     const observerInstance = getObserverInstance(element);
+    const element = container.getElementsByTagName('div')[0];
+    const observerInstance = getObserverInstance(element);
 
-//     act(() => {
-//       triggerIntersection({
-//         changes: [{ isIntersecting: true }],
-//         observer: observerInstance,
-//       });
-//     });
+    act(() => {
+      triggerIntersection({
+        changes: [{ isIntersecting: true }],
+        observer: observerInstance,
+      });
+    });
 
-//     await Promise.resolve();
+    await Promise.resolve();
 
-//     expect(global.IntersectionObserver).toHaveBeenCalledTimes(0);
-//     expect(optimizely.track).toHaveBeenCalledTimes(0);
-//   });
+    expect(global.IntersectionObserver).toHaveBeenCalledTimes(0);
+    expect(optimizely.track).toHaveBeenCalledTimes(0);
+  });
 
-//   // rewrite
-//   it('should do both things', async () => {
-//     useOptimizelyVariation.mockReturnValue('variation_1');
+  // rewrite
+  it('should do both things', async () => {
+    useOptimizelyVariation.mockReturnValue('variation_1');
 
-//     const { container } = render(
-//       <ContextWrap pageType={ARTICLE_PAGE} service="news" isAmp={false}>
-//         <OptimizelyPageMetrics trackPageView trackPageComplete />
-//       </ContextWrap>,
-//     );
+    const { container } = render(
+      <ContextWrap pageType={ARTICLE_PAGE} service="news" isAmp={false}>
+        <OptimizelyPageMetrics trackPageView trackPageComplete />
+      </ContextWrap>,
+    );
 
-//     await waitFor(() => {
-//       expect(optimizely.track).toHaveBeenCalledTimes(1);
-//     });
-//     const element = container.getElementsByTagName('div')[0];
-//     const observerInstance = getObserverInstance(element);
+    await waitFor(() => {
+      expect(optimizely.track).toHaveBeenCalledTimes(1);
+    });
+    const element = container.getElementsByTagName('div')[0];
+    const observerInstance = getObserverInstance(element);
 
-//     act(() => {
-//       triggerIntersection({
-//         changes: [{ isIntersecting: true }],
-//         observer: observerInstance,
-//       });
-//     });
+    act(() => {
+      triggerIntersection({
+        changes: [{ isIntersecting: true }],
+        observer: observerInstance,
+      });
+    });
 
-//     await Promise.resolve();
+    await Promise.resolve();
 
-//     expect(global.IntersectionObserver).toHaveBeenCalledTimes(1);
-//     expect(optimizely.track).toHaveBeenCalledTimes(2);
-//   });
+    expect(global.IntersectionObserver).toHaveBeenCalledTimes(1);
+    expect(optimizely.track).toHaveBeenCalledTimes(2);
+  });
 
-//   // reqrite - not sure this is a valid test
-//   it('should do both things when component used in two places', async () => {
-//     useOptimizelyVariation.mockReturnValue('variation_1');
+  // reqrite - not sure this is a valid test
+  it('should do both things when component used in two places', async () => {
+    useOptimizelyVariation.mockReturnValue('variation_1');
 
-//     const { container } = render(
-//       <ContextWrap pageType={ARTICLE_PAGE} service="news" isAmp={false}>
-//         <main>
-//           <OptimizelyPageMetrics trackPageComplete />
-//         </main>
-//         {/* pretty sure I'm not actually checking scroll depth */}
-//         <OptimizelyPageMetrics trackPageView trackPageDepth />
-//       </ContextWrap>,
-//     );
+    const { container } = render(
+      <ContextWrap pageType={ARTICLE_PAGE} service="news" isAmp={false}>
+        <main>
+          <OptimizelyPageMetrics trackPageComplete />
+        </main>
+        {/* pretty sure I'm not actually checking scroll depth */}
+        <OptimizelyPageMetrics trackPageView trackPageDepth />
+      </ContextWrap>,
+    );
 
-//     await waitFor(() => {
-//       expect(optimizely.track).toHaveBeenCalledTimes(1);
-//     });
-//     const element = container.getElementsByTagName('div')[0];
-//     const observerInstance = getObserverInstance(element);
+    await waitFor(() => {
+      expect(optimizely.track).toHaveBeenCalledTimes(1);
+    });
+    const element = container.getElementsByTagName('div')[0];
+    const observerInstance = getObserverInstance(element);
 
-//     act(() => {
-//       triggerIntersection({
-//         changes: [{ isIntersecting: true }],
-//         observer: observerInstance,
-//       });
-//     });
+    act(() => {
+      triggerIntersection({
+        changes: [{ isIntersecting: true }],
+        observer: observerInstance,
+      });
+    });
 
-//     await Promise.resolve();
+    await Promise.resolve();
 
-//     // is this ok?
-//     expect(global.IntersectionObserver).toHaveBeenCalledTimes(1);
-//     expect(optimizely.track).toHaveBeenCalledTimes(2);
-//   });
-// });
+    // is this ok?
+    expect(global.IntersectionObserver).toHaveBeenCalledTimes(1);
+    expect(optimizely.track).toHaveBeenCalledTimes(2);
+  });
+});
