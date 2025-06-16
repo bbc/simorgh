36fb29082675d2dca8ee9714a5ccd91d11a64040
Louis Matsika <louis.matsika@bbc.co.uk>
2025-06-16 16:08:03 +0100

    lint

--- src/app/components/Image/index.tsx
+++ src/app/components/Image/index.tsx
@@ -31,7 +31,6 @@ export type ImageProps = {
   width?: number;
   fetchPriority?: 'high';
   hasCaption?: boolean;
-  role?: string;
 };
 
 const roundNumber = (num: number) => Math.round(num * 100) / 100;
@@ -60,7 +59,6 @@ const Image = ({
   children,
   fetchPriority,
   hasCaption,
-  role,
 }: PropsWithChildren<ImageProps>) => {
   const { pageType, isLite, isAmp } = useContext(RequestContext);
   const [isLoaded, setIsLoaded] = useState(false);
