d168e6714697ad6dc2d55ecefd1955b01364dcd1
alex-magana <alex.magana@andela.com>
2025-06-16 22:47:50 +0300

    Filter script ids

--- puppeteer/bundleRequests/assertions/index.js
+++ puppeteer/bundleRequests/assertions/index.js
@@ -18,6 +18,7 @@ const getServiceBundleRegex = service => {
 };
 
 const staticLibraryScripts = 'comscore|reverb';
+const staticLibraryScriptIds = 'main|reverb';
 
 export const assertLoadsExpectedBundles = ({ service }) => {
   it('only loads expected js bundles', () => {
@@ -29,7 +30,7 @@ export const assertLoadsExpectedBundles = ({ service }) => {
       .forEach(url => {
         expect(url).toMatch(
           new RegExp(
-            `(\\/static\\/js\\/(?:${staticLibraryScripts}\\/)?(modern.)?(main|framework|commons|shared|${serviceRegex}|frosted_promo|themes|.+Page).+?.js)|(\\/static\\/.+?-lib.+?.js)|${service}\\/(articles\\/)?sw\\.js`,
+            `(\\/static\\/js\\/(?:${staticLibraryScripts}\\/)?(modern.)?(${staticLibraryScriptIds}|framework|commons|shared|${serviceRegex}|frosted_promo|themes|.+Page).+?.js)|(\\/static\\/.+?-lib.+?.js)|${service}\\/(articles\\/)?sw\\.js`,
             'g',
           ),
         );
