2261023c7f49d7ecfb8066e1903e1d240ac68490
emily saffron <emily.saffron@bbc.co.uk>
2025-06-20 12:13:50 +0100

    update easy read test

--- src/app/components/EasyReadCTA/index.test.tsx
+++ src/app/components/EasyReadCTA/index.test.tsx
@@ -3,18 +3,6 @@ import { render } from '../react-testing-library-with-providers';
 import EasyReadCTA from '.';
 
 describe('LiteSiteCTA', () => {
-  it('Should have a hidden strong element with lite site identifier.', () => {
-    const { container } = render(
-      <EasyReadCTA
-        easyReadAssetId="crkdy3r685jo"
-        originalAssetId="cy0grkwd3zlo"
-      />,
-    );
-    const strongText = container.querySelector('strong');
-    expect(strongText?.innerHTML).toBe('Format');
-    expect(strongText).toHaveAttribute('hidden');
-  });
-
   it('Should have a CTA link to the easy site.', () => {
     const { container } = render(
       <EasyReadCTA
