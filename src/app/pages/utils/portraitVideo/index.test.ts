7020fdf9190fb5fa2e682b2c13535909745db375
Louis Matsika <louis.matsika@bbc.co.uk>
2025-06-24 15:14:17 +0100

    add unit test for isPortraitVideoUnderHeadline

--- src/app/pages/utils/portraitVideo/index.test.ts
+++ src/app/pages/utils/portraitVideo/index.test.ts
@@ -1,4 +1,4 @@
-import { isPortraitVideo } from '.';
+import { isPortraitVideo, isPortraitVideoUnderHeadline } from '.';
 import {
   mediaBlockWithPV,
   mediaBlockWithMultipleVersionsTypes,
@@ -6,24 +6,38 @@ import {
   mediaBlockWithInvalidOrientation,
 } from './fixtures';
 
-describe('isPortraitVideo', () => {
-  it(`Should return true if media block includes a portrait video`, () => {
-    expect(isPortraitVideo(mediaBlockWithPV)).toBe(true);
+describe('Portrait Video', () => {
+  describe('isPortraitVideo', () => {
+    it(`Should return true if media block includes a portrait video`, () => {
+      expect(isPortraitVideo(mediaBlockWithPV)).toBe(true);
+    });
+
+    it(`Should return true if media block includes a versions block that has the 'Portrait' type amongst other types`, () => {
+      expect(isPortraitVideo(mediaBlockWithMultipleVersionsTypes)).toBe(true);
+    });
+
+    it(`Should return false if media block includes landscape media`, () => {
+      expect(isPortraitVideo(mediaBlockWithoutPV)).toBe(false);
+    });
+
+    it(`Should return false if media block includes an Ares media block with an invalid orientation`, () => {
+      expect(isPortraitVideo(mediaBlockWithInvalidOrientation)).toBe(false);
+    });
+
+    it('Should return false if media block does not include an Ares media block', () => {
+      expect(isPortraitVideo([])).toBe(false);
+    });
   });
 
-  it(`Should return true if media block includes a versions block that has the 'Portrait' type amongst other types`, () => {
-    expect(isPortraitVideo(mediaBlockWithMultipleVersionsTypes)).toBe(true);
-  });
-
-  it(`Should return false if media block includes landscape media`, () => {
-    expect(isPortraitVideo(mediaBlockWithoutPV)).toBe(false);
-  });
-
-  it(`Should return false if media block includes an Ares media block with an invalid orientation`, () => {
-    expect(isPortraitVideo(mediaBlockWithInvalidOrientation)).toBe(false);
-  });
-
-  it('Should return false if media block does not include an Ares media block', () => {
-    expect(isPortraitVideo([])).toBe(false);
+  describe('isPortraitVideoUnderHeadline', () => {
+    it(`Should return true if media block is a portrait video and is under the headline`, () => {
+      const articlePageBlocks = [
+        { type: 'headline', model: {} },
+        { type: 'video', model: { blocks: mediaBlockWithPV } },
+      ];
+      expect(
+        isPortraitVideoUnderHeadline(articlePageBlocks, mediaBlockWithPV),
+      ).toBe(true);
+    });
   });
 });
