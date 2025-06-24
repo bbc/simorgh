3b90a9a718b3d2444a3105e10235fbd0f0cf3f93
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2025-06-24 16:23:06 +0100

    WS-413: Update type

--- src/app/components/OptimizelyPageMetrics/experimentsForPageMetrics.ts
+++ src/app/components/OptimizelyPageMetrics/experimentsForPageMetrics.ts
@@ -3,14 +3,11 @@ import { PageTypes } from '#app/models/types/global';
 // Any running serverside and client side experiments which collect Optimizely Page Metrics; page view, page complete, scroll depth
 // Includes PageType so that different experiments can be run on different pageTypes
 
-type ExperimentsForPageTypeMetrics = [
-  {
-    pageType: PageTypes;
-    activeExperiments: string[];
-  },
-];
+type ExperimentsForPageTypeMetrics = {
+  pageType: PageTypes;
+  activeExperiments: string[];
+}[];
 
-// @ts-expect-error - no running experiments
 const experimentsForPageMetrics: ExperimentsForPageTypeMetrics = [];
 
 export default experimentsForPageMetrics;
