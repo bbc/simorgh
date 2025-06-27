6ac978b8b6ebfe80d0be591d1536d65f08e7c6ec
Toby Cox <eagerterrier@gmail.com>
2025-06-27 09:55:16 +0100

    fix: typescript

--- src/app/lib/analyticsUtils/staticATITracking/processClientDeviceAndSendStaticBeacon/index.ts
+++ src/app/lib/analyticsUtils/staticATITracking/processClientDeviceAndSendStaticBeacon/index.ts
@@ -68,20 +68,23 @@ export const addProcessClientDeviceAndSendStaticBeaconToWindow = () => {
       if (user.val) params.idclient = user.val;
       if (document.referrer) params.ref = document.referrer;
 
-      if (isLiteSite && window.location.search.length){
-        const kvpairs = window.location.search
+      if (isLiteSite && window.location.search.length) {
+        const kvpairs: Record<string, string> = window.location.search
           .substring(1)
           .split('&')
-          .map(param => param.split('='))
-          .reduce((values, kv) => {
-            values[kv[0]] = kv[1];
+          .map((param): [string, string] => {
+            const [key, value = ''] = param.split('=');
+            return [decodeURIComponent(key), decodeURIComponent(value)];
+          })
+          .reduce<Record<string, string>>((values, [key, value]) => {
+            values[key] = value;
             return values;
           }, {});
-        Object.keys(kvpairs).forEach(keyName => {
+      
+        Object.keys(kvpairs).forEach((keyName) => {
           if (keyName.indexOf('at_') === 0) {
             params[keyName.replace('at_', 'src_')] = kvpairs[keyName];
-          }
-          else if (keyName.indexOf('utm_') === 0) {
+          } else if (keyName.indexOf('utm_') === 0) {
             params[keyName] = kvpairs[keyName];
           }
         });
