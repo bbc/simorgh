76749bbc798a1f30757c1f2e327deb6b5a8d27c3
Shayne Ahchoon <shayneah@googlemail.com>
2025-06-30 10:50:12 +0100

    WS-116: Update

--- ws-nextjs-app/pages/_app.page.tsx
+++ ws-nextjs-app/pages/_app.page.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import { ATIData } from '#app/components/ATIAnalytics/types';
 import ThemeProvider from '#app/components/ThemeProvider';
-import { ToggleContextProvider } from 'simorgh/src/app/contexts/ToggleContext/ToggleContext';
-import { ServiceContextProvider } from 'simorgh/src/app/contexts/ServiceContext/ServiceContext';
-import { RequestContextProvider } from 'simorgh/src/app/contexts/RequestContext/RequestContext';
-import { EventTrackingContextProvider } from 'simorgh/src/app/contexts/EventTrackingContext/EventTrackingContext';
-import { UserContextProvider } from 'simorgh/src/app/contexts/UserContext/UserContext';
-import ErrorPage from 'simorgh/src/app/pages/ErrorPage/ErrorPage';
+import { ToggleContextProvider } from '../../src/app/contexts/ToggleContext';
+import { ServiceContextProvider } from '../../src/app/contexts/ServiceContext';
+import { RequestContextProvider } from '../../src/app/contexts/RequestContext';
+import { EventTrackingContextProvider } from '../../src/app/contexts/EventTrackingContext';
+import { UserContextProvider } from '../../src/app/contexts/UserContext';
+import ErrorPage from '../../src/app/pages/ErrorPage/ErrorPage';
 import {
   PageTypes,
   Services,
   Toggles,
   Variants,
   ServerSideExperiment,
-} from 'simorgh/src/app/models/types/global';
-import PageWrapper from 'simorgh/src/app/components/PageLayoutWrapper/PageLayoutWrapper';
+} from '../../src/app/models/types/global';
+import PageWrapper from '../../src/app/components/PageLayoutWrapper';
 
 interface Props extends AppProps {
   pageProps: {
@@ -100,8 +100,7 @@ export default function App({ Component, pageProps }: Props) {
           variant={variant}
           timeOnServer={timeOnServer}
           showAdsBasedOnLocation={showAdsBasedOnLocation}
-          serverSideExperiments={serverSideExperiments}
-          country={country}
+          serverSideExperiments={serverSideExperiments}    country={country}
           isNextJs={isNextJs}
           isUK={isUK ?? false}
         >
