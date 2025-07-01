89093e672040f8a7070d9f056fd9a3f33c6a4736
Louise Archibald <louise.archibald@bbc.co.uk>
2025-07-01 13:10:23 +0100

    removing styled components

--- src/app/components/TopBarOJs/index.tsx
+++ src/app/components/TopBarOJs/index.tsx
@@ -26,106 +26,10 @@ import { GridItemMediumNoMargin } from '#components/Grid';
 import useViewTracker from '#hooks/useViewTracker';
 import useClickTrackerHandler from '#hooks/useClickTrackerHandler';
 import idSanitiser from '#lib/utilities/idSanitiser';
-import { GREY_2 } from '#app/components/ThemeProvider/palette';
 import { ServiceContext } from '../../../contexts/ServiceContext';
 import Promo from './Promo';
 import PromoList from './PromoList';
-import { EventTrackingMetadata } from '#app/models/types/eventTracking';
 import { TopBarOJsProps } from './PromoList/types';
-
-// const PromoWrapper = styled.div`
-//   ${({ dir }) => `margin-${dir === 'ltr' ? 'left' : 'right'}: ${GEL_SPACING};`}
-//   @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
-//     ${({ dir }) =>
-//       `margin-${dir === 'ltr' ? 'left' : 'right'}: ${GEL_SPACING_DBL};`}
-//   }
-//   @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
-//     ${({ dir }) => `margin-${dir === 'ltr' ? 'left' : 'right'}: 0;`}
-//   }
-// `;
-
-// const ScrollablePromoContainer = styled.div`
-//   background: ${GREY_2};
-//   padding: ${GEL_SPACING};
-//   display: flex;
-//   overflow-x: auto;
-//   -ms-overflow-style: none;
-//   scrollbar-width: none;
-//   &::-webkit-scrollbar {
-//     display: none;
-//   }
-// `;
-
-const LabelComponent = styled.strong`
-  display: block;
-  ${({ script }) => script && getDoublePica(script)};
-  ${({ service }) => getSansRegular(service)}
-  margin-bottom: ${GEL_SPACING_DBL};
-  color: ${({ theme }) =>
-    theme.isDarkUi ? theme.palette.GREY_2 : theme.palette.SHADOW};
-
-  ${({ dir }) =>
-    `
-    @media (min-width: ${GEL_GROUP_0_SCREEN_WIDTH_MIN}){
-      margin-${dir === 'ltr' ? 'left' : 'right'}: ${GEL_SPACING};
-    }
-    @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}){
-      margin-${dir === 'ltr' ? `left` : `right`}: ${GEL_SPACING_DBL};  
-    }
-    @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}){
-        margin-${dir === 'ltr' ? `left` : `right`}: 0;
-    }
-`}
-`;
-
-// const LabelComponentOJTopBar = styled(({ ariaLabel, ...props }) => (
-//   <strong aria-label={ariaLabel} {...props} />
-// ))`
-//   ${({ script }) => script && getBrevier(script)};
-//   ${({ service }) => getSansRegular(service)}
-//   display: inline-block;
-//   margin-bottom: ${GEL_SPACING_DBL};
-//   color: ${({ theme }) =>
-//     theme.isDarkUi ? theme.palette.GREY_2 : theme.palette.SHADOW};
-
-//   ${({ dir }) =>
-//     `
-//     @media (min-width: ${GEL_GROUP_0_SCREEN_WIDTH_MIN}){
-//       margin-${dir === 'ltr' ? 'left' : 'right'}: ${GEL_SPACING};
-//     }
-//     @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}){
-//       margin-${dir === 'ltr' ? `left` : `right`}: ${GEL_SPACING_DBL};
-//     }
-//     @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}){
-//       margin-${dir === 'ltr' ? `left` : `right`}: 0;
-//     }
-//   `}
-
-//   padding: 0 ${GEL_SPACING};
-
-//   @media (min-width: ${GEL_GROUP_0_SCREEN_WIDTH_MIN}) {
-//     margin: 0rem;
-//   }
-
-//   @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
-//     padding: 0 ${GEL_SPACING_DBL};
-//     margin: 0 -0.2rem;
-//   }
-
-//   @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
-//     margin: 0 -0.8rem;
-//   }
-
-//   @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MAX}) {
-//     display: none;
-//   }
-
-//   display: flex;
-//   align-items: center;
-//   height: ${GEL_SPACING_QUAD};
-//   background: ${GREY_2};
-//   width: 100vw;
-// `;
 
 // interface TopBarOJsProps {
 //     blocks: ,
