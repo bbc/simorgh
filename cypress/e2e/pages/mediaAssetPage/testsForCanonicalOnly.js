7cf91e055cffdb98af6aa01822344c3f48dc81e7
mechrm02 <meriem.mechri01@bbc.co.uk>
2025-07-03 16:27:21 +0100

    refactor: update to match article page format

--- cypress/e2e/pages/mediaAssetPage/testsForCanonicalOnly.js
+++ cypress/e2e/pages/mediaAssetPage/testsForCanonicalOnly.js
@@ -3,10 +3,7 @@ import appToggles from '../../../support/helpers/useAppToggles';
 import envConfig from '../../../support/config/envs';
 
 // For testing features that may differ across services but share a common logic e.g. translated strings.
-export const testsThatFollowSmokeTestConfigForCanonicalOnly = ({
-  service,
-  pageType,
-}) => {
+export default ({ service, pageType }) => {
   describe(`testsThatFollowSmokeTestConfigForCanonicalOnly for ${service} ${pageType}`, () => {
     if (appToggles.chartbeatAnalytics.enabled && envConfig.chartbeatEnabled) {
       describe('Chartbeat', () => {
