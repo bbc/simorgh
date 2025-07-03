f2e517b03e92e80f21f4ec00f241cbb5f41401c4
Toby Cox <eagerterrier@gmail.com>
2025-07-03 18:07:38 +0100

    Navigator experiment (#12958)
    * navigator experiment
    
    * Update ReverbTemplate.tsx
    
    * Create test.js
    
    * Delete public/static/js/test directory
    
    * Update CanonicalRenderer.tsx
    
    * Update index.jsx
    
    * Update component.tsx
    
    * Update sw.js
    
    * Update CanonicalRenderer.tsx
    
    * typescript
    
    * Update CanonicalRenderer.tsx
    
    * Update ReverbTemplate.tsx
    
    * Update ReverbTemplate.tsx
    
    * Update CanonicalRenderer.tsx
    
    * Update ReverbTemplate.tsx
    
    * Update CanonicalRenderer.tsx
    
    * Update CanonicalRenderer.tsx
    
    * lint
    
    * Update CanonicalRenderer.tsx
    
    * Update ReverbTemplate.tsx
    
    * Update component.test.jsx.snap
    
    * Update index.test.jsx
    
    * Update component.test.jsx.snap
    
    * Update index.test.jsx
    
    * Update index.test.jsx
    
    * Update CanonicalRenderer.tsx
    
    * Update CanonicalRenderer.tsx
    
    * Update CanonicalRenderer.tsx
    
    * Update index.test.jsx
    
    * Update ReverbTemplate.tsx

--- src/server/Document/Renderers/ReverbTemplate.tsx
+++ src/server/Document/Renderers/ReverbTemplate.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { getEnvConfig } from '#app/lib/utilities/getEnvConfig';
 
 const ReverbTemplate = () => {
+  const envConfig = getEnvConfig();
+
   return (
-    <>
-      <script
-        // eslint-disable-next-line react/no-danger
-        dangerouslySetInnerHTML={{
-          __html: `
+    <script
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{
+        __html: `
             window.__reverb = {};
             window.__reverb.__reverbLoadedPromise = new Promise((resolve, reject) => {
               window.__reverb.__resolveReverbLoaded = resolve;
@@ -15,11 +16,13 @@ const ReverbTemplate = () => {
             });
             window.__reverb.__reverbTimeout = setTimeout(() => {
               window.__reverb.__rejectReverbLoaded();
-            }, 5000);`,
-        }}
-      />
-      <script async src={`${getEnvConfig().SIMORGH_REVERB_SOURCE}`} />
-    </>
+            }, 5000);
+            const reverbScript = document.createElement('script');
+            reverbScript.setAttribute('src','${envConfig?.SIMORGH_REVERB_SOURCE ?? ''}');
+            document.head.appendChild(reverbScript);
+            `,
+      }}
+    />
   );
 };
 
