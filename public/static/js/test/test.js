d0cc997c93615d30cfc6827536e9b58f518e02f3
Toby Cox <eagerterrier@gmail.com>
2025-07-03 13:02:04 +0100

    Delete public/static/js/test directory

--- public/static/js/test/test.js
+++
@@ -1,5 +0,0 @@
-const noscriptTag = window.document.getElementsByTagName('noscript')[0];
-
-const trackingDiv = document.createElement('DIV');
-trackingDiv.innerHTML = noscriptTag.innerHTML;
-window.document.body.appendChild(trackingDiv);
