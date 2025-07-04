8c35a4979727fe2f775327981119bfa0ce56a43b
Santa Zena <santa.zena.ext@bbc.co.uk>
2025-07-04 11:56:06 +0300

    Code refactoring

--- src/app/components/LanguageNavigation/index.tsx
+++ src/app/components/LanguageNavigation/index.tsx
@@ -1,79 +1,81 @@
+import { ClassNames, useTheme } from '@emotion/react';
 import React, { useState } from 'react';
+import styles from './index.styles';
 import languageSections, { CloseIconSvg } from './constants';
-import { 
-  Wrapper, 
-  NavLink, 
-  NavSummary, 
-  NavItem, 
-  DropDown, 
-  DropDownItem, 
-  DropDownHeader, 
-  DropDownTitle, 
-  CloseButton,
-  DropDownLink
-} from './index.styles';
 
 const LanguageNavigation = () => {
-
-  const [openSection, setOpenSection] = useState<string | null>(null); 
+  const [openSection, setOpenSection] = useState<string | null>(null);
+  const theme = useTheme();
 
   return (
-    <>
-      <Wrapper>
-        {languageSections.map((section, index) => {
-          const isLast = index === languageSections.length - 1;
+    <ClassNames>
+      {({ css }) => (
+        <>
+          <nav className={css(styles.wrapper(theme))}>
+            {languageSections.map((section, index) => {
+              const isLast = index === languageSections.length - 1;
 
-          //HOME
-          if (section.href) {
-            return (
-              <NavItem key={section.title} isLast={isLast}>
-                <NavLink href={section.href}>{section.title}</NavLink>
-              </NavItem>
-            );
-          }
+              if (section.href) {
+                return (
+                  <div
+                    key={section.title}
+                    className={css(styles.navItem({ isLast }))}
+                  >
+                    <a href={section.href} className={css(styles.navLink)}>
+                      {section.title}
+                    </a>
+                  </div>
+                );
+              }
 
-          //ALL OTHERS- AFRICA, ASIA ETC
-          return (
-            <NavItem 
-              key={section.title} 
-              isLast={isLast}
-              isActive={openSection === section.title}
-            >
-              <NavSummary
-                onClick={() =>
-                  setOpenSection(openSection === section.title ? null: section.title)
-                }
-              >
-                {section.title}
-              </NavSummary>
-            </NavItem>
-          );
-        })}
-      </Wrapper>
+              const isActive = openSection === section.title;
 
-      {openSection && (
-        <DropDown>
+              return (
+                <details
+                  key={section.title}
+                  open={isActive}
+                  className={css(styles.navItem({ isLast, isActive }))}
+                  onToggle={e => {
+                    const isOpen = (e.target as HTMLDetailsElement).open;
+                    setOpenSection(isOpen ? section.title : null);
+                  }}
+                >
+                  <summary className={css(styles.navSummary)}>
+                    {section.title}
+                  </summary>
+                </details>
+              );
+            })}
+          </nav>
 
-          <DropDownHeader>
-            <DropDownTitle>{openSection}</DropDownTitle>
-            <CloseButton onClick={() => setOpenSection(null)}> 
-              <CloseIconSvg /> 
-            </CloseButton>
-          </DropDownHeader>
+          {openSection && (
+            <div className={css(styles.dropDown)}>
+              <div className={css(styles.dropDownHeader)}>
+                <span className={css(styles.dropDownTitle)}>{openSection}</span>
+                <button
+                  type="button"
+                  className={css(styles.closeButton)}
+                  onClick={() => setOpenSection(null)}
+                >
+                  <CloseIconSvg />
+                </button>
+              </div>
 
-          {languageSections
-            .find((s) => s.title === openSection)
-            ?.links?.map ((link) => (
-              <DropDownItem key={link.href}>
-                <DropDownLink href={link.href}>{link.label}</DropDownLink>
-              </DropDownItem>
-            ))
-          }
-        </DropDown>
-        
+              {languageSections
+                .find(s => s.title === openSection)
+                ?.links?.map(link => (
+                  <div key={link.href} className={css(styles.dropDownItem)}>
+                    <a href={link.href} className={css(styles.dropDownLink)}>
+                      {link.label}
+                    </a>
+                  </div>
+                ))}
+            </div>
+          )}
+        </>
       )}
-    </>
-    
+    </ClassNames>
   );
-}
+};
+
 export default LanguageNavigation;
