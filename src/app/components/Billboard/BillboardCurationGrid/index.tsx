68ddca14b66b75f2eba24abc6ef5240914e5b801
“LilyL0u” <lily.higham@bbc.co.uk>
2025-07-07 21:49:30 +0100

    make variable name more clear what it is

--- src/app/components/Billboard/BillboardCurationGrid/index.tsx
+++ src/app/components/Billboard/BillboardCurationGrid/index.tsx
@@ -5,20 +5,20 @@ import CurationPromo from '../../Curation/CurationPromo';
 import { CurationGridProps } from '../../Curation/types';
 
 const BillboardCurationGrid = ({ summaries }: CurationGridProps) => {
-  const calculatedSummaries = summaries.slice(0, 4);
+  const top4SummariesForGrid = summaries.slice(0, 4);
 
-  if (calculatedSummaries.length === 0) {
+  if (top4SummariesForGrid.length === 0) {
     return null;
   }
 
-  const hasMultiplePromos = calculatedSummaries.length > 1;
-  const firstPromo = calculatedSummaries[0];
+  const hasMultiplePromos = top4SummariesForGrid.length > 1;
+  const firstPromo = top4SummariesForGrid[0];
 
   return (
     <div data-testid="billboard-curation-grid">
       {hasMultiplePromos ? (
         <ul css={styles.list} role="list" data-testid="topic-promos">
-          {calculatedSummaries.map(promo => (
+          {top4SummariesForGrid.map(promo => (
             <li css={styles.item} key={promo.id}>
               <CurationPromo
                 {...promo}
