fbf38b2b600fb743fca0f474ea6afc2375218475
Toby Cox <eagerterrier@gmail.com>
2025-07-07 11:56:36 +0100

    prettier

--- src/server/Document/Renderers/CanonicalRenderer.tsx
+++ src/server/Document/Renderers/CanonicalRenderer.tsx
@@ -121,7 +121,8 @@ export default function CanonicalRenderer({
         {links}
         <IfAboveIE9>
           {['urdu', 'hausa'].includes(service ?? '')
-            ? showScripts(modernScripts) : modernScripts}
+            ? showScripts(modernScripts)
+            : modernScripts}
           {legacyScripts}
         </IfAboveIE9>
         <script
