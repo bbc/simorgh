3aabda5114593613e06059f1835ca61af647b8c7
Toby Cox <eagerterrier@gmail.com>
2025-07-07 09:46:34 +0100

    update navigator experiment

--- src/server/Document/Renderers/CanonicalRenderer.tsx
+++ src/server/Document/Renderers/CanonicalRenderer.tsx
@@ -120,7 +120,7 @@ export default function CanonicalRenderer({
         />
         {links}
         <IfAboveIE9>
-          {service === 'urdu' ? showScripts(modernScripts) : modernScripts}
+          {['urdu', 'hausa'].includes(service) ? showScripts(modernScripts) : modernScripts}
           {legacyScripts}
         </IfAboveIE9>
         <script
