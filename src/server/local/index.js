8c5ae44a153f237c1c797dcd4444494593ed48e6
Nabeel Khan <nabeel.khan@bbc.co.uk>
2025-07-09 15:46:35 +0100

    removed local comscore route and ignore images

--- src/server/local/index.js
+++ src/server/local/index.js
@@ -205,14 +205,6 @@ export default server => {
       });
       sendDataFile(res, dataFilePath, next);
     })
-    .get('/static/js/comscore/main-:version.js', ({ params }, res, next) => {
-      const { version } = params;
-      const localComscorePath = path.join(
-        process.cwd(),
-        `/public/static/js/comscore/main-${version}.js`,
-      );
-      sendDataFile(res, localComscorePath, next);
-    })
     .get('/ckns_policy/*', (req, res) => {
       // Route to allow the cookie banner to make the cookie oven request
       // without throwing an error due to not being on a bbc domain.
