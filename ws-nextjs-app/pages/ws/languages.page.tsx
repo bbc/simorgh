f5aa4806c592c80f0f908421684d4b8b765b8c8f
Lukas Freimonas <lukas.freimonas.ext@bbc.co.uk>
2025-07-09 11:08:49 +0300

    Revert "ws/languages page to load its mapping from fabl"
    This reverts commit 0259d7b0e1284b65fce0b164c2bf2894d99cbf20.

--- ws-nextjs-app/pages/ws/languages.page.tsx
+++ ws-nextjs-app/pages/ws/languages.page.tsx
@@ -1,8 +1,14 @@
+import React from 'react';
 import dynamic from 'next/dynamic';
 import { GetServerSideProps } from 'next';
-import { STATIC_PAGE } from '#app/routes/utils/pageTypes';
+import { STATIC_PAGE, HOME_PAGE } from '#app/routes/utils/pageTypes';
+import PageDataParams from '#app/models/types/pageDataParams';
+import isLive from '#app/lib/utilities/isLive';
 import { Services, PageTypes } from '#app/models/types/global';
+import getPageData from '../../utilities/pageRequests/getPageData';
+import { LanguagesPageProps } from './types';
 
+const HomePage = dynamic(() => import('#pages/HomePage/HomePage'));
 const LanguagesPageLayout = dynamic(() => import('./LanguagesPageLayout'));
 
 export const getServerSideProps: GetServerSideProps = async context => {
@@ -11,24 +17,74 @@ export const getServerSideProps: GetServerSideProps = async context => {
     'public, stale-if-error=300, stale-while-revalidate=120, max-age=30',
   );
 
-  return {
-    props: {
-      error: null,
-      isAmp: false,
-      isNextJs: true,
-      page: null,
-      status: 200,
-      timeOnServer: Date.now(),
-      pageType: STATIC_PAGE as PageTypes,
-      service: 'ws' as Services,
-      pageData: {
-        metadata: {
-          type: STATIC_PAGE,
+  const { id, renderer_env: rendererEnv } = context.query as PageDataParams;
+  const baseProps = {
+    error: null,
+    isAmp: false,
+    isNextJs: true,
+    page: null,
+    status: 200,
+    timeOnServer: Date.now(),
+    pageType: STATIC_PAGE as PageTypes,
+    service: 'ws' as Services,
+    pageData: {
+      metadata: {
+        type: STATIC_PAGE,
+        atiAnalytics: {},
+      },
+    },
+    pathname: context?.resolvedUrl,
+  };
+
+  if (isLive()) {
+    return {
+      props: baseProps,
+    };
+  }
+
+  const { data } = await getPageData({
+    id,
+    service: 'ws',
+    rendererEnv,
+    resolvedUrl: '/ws/languages',
+    pageType: HOME_PAGE,
+  });
+
+  if (data?.error) {
+    return {
+      props: {
+        ...baseProps,
+        error: data?.error,
+        status: data?.status,
+        pageType: HOME_PAGE,
+        service: 'ws',
+        pageData: {
+          metadata: {
+            type: HOME_PAGE,
+            atiAnalytics: {},
+          },
         },
       },
-      pathname: context?.resolvedUrl,
+    };
+  }
+
+  return {
+    props: {
+      ...baseProps,
+      pageType: HOME_PAGE,
+      service: 'pidgin',
+      pathname: '/pidgin',
+      status: data?.status,
+      pageData: {
+        ...data?.pageData,
+      },
     },
   };
 };
 
-export default LanguagesPageLayout;
+export default function LanguagesPage({ ...props }: LanguagesPageProps) {
+  if (isLive()) {
+    return <LanguagesPageLayout />;
+  }
+  return <HomePage pageData={props.pageData} />;
+}
