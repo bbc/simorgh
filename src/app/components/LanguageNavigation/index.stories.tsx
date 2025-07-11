bcbdbe6f2e016230b1d2c2c4d2479112ccdec33c
Santa Zena <santa.zena.ext@bbc.co.uk>
2025-07-11 13:42:43 +0300

    Adding id to constants; Passed languageSection as props

--- src/app/components/LanguageNavigation/index.stories.tsx
+++ src/app/components/LanguageNavigation/index.stories.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import LanguageNavigation from './index';
+import languageSections from './constants';
 
 export default {
   title: 'Components/LanguageNavigation',
   component: LanguageNavigation,
 };
 
-export const Default = () => <LanguageNavigation />;
+export const Default = () => (
+  <LanguageNavigation languageSections={languageSections} />
+);
