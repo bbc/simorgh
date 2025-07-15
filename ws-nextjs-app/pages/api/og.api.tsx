74dcba510571aa60517cc43deda6e0bcbb2cb2f5
Aaron Moore <aaron.moore@bbc.co.uk>
2025-07-15 15:45:32 +0100

    Update og.api.tsx

--- ws-nextjs-app/pages/api/og.api.tsx
+++ ws-nextjs-app/pages/api/og.api.tsx
@@ -50,6 +50,27 @@ const Badge = ({ icon, text }: { icon: React.ReactNode; text: string }) => (
   </div>
 );
 
+const getImages = ({
+  service,
+  promoImage,
+}: {
+  service: Services;
+  promoImage: { originCode: string; locator: string };
+}) => {
+  const unbrandedImage =
+    buildIChefURL({
+      originCode: promoImage?.originCode,
+      locator: promoImage?.locator,
+      resolution: 800,
+    }) || `https://news.files.bbci.co.uk/ws/img/logos/og/${service}.png`;
+
+  const brandedImage =
+    getBrandedImage(promoImage?.locator, service) ||
+    `https://news.files.bbci.co.uk/ws/img/logos/og/${service}.png`;
+
+  return { unbrandedImage, brandedImage };
+};
+
 type LayoutProps = {
   image: string;
   headline?: string;
@@ -205,27 +226,6 @@ const socialCardLayout = async ({
       fonts,
     },
   );
-};
-
-const getImages = ({
-  service,
-  promoImage,
-}: {
-  service: Services;
-  promoImage: { originCode: string; locator: string };
-}) => {
-  const unbrandedImage =
-    buildIChefURL({
-      originCode: promoImage?.originCode,
-      locator: promoImage?.locator,
-      resolution: 800,
-    }) || `https://news.files.bbci.co.uk/ws/img/logos/og/${service}.png`;
-
-  const brandedImage =
-    getBrandedImage(promoImage?.locator, service) ||
-    `https://news.files.bbci.co.uk/ws/img/logos/og/${service}.png`;
-
-  return { unbrandedImage, brandedImage };
 };
 
 export default async function handler(req: NextApiRequest) {
