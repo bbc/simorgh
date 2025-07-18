bc0444a34e92fbdc74ba04bef1162f3fa6e097c6
Lukas Freimonas <lukas.freimonas.ext@bbc.co.uk>
2025-07-18 12:51:48 +0300

    refactor: consolidate test suites

--- ws-nextjs-app/cypress/e2e/languagesPage/index.cy.ts
+++ ws-nextjs-app/cypress/e2e/languagesPage/index.cy.ts
@@ -6,25 +6,19 @@ import {
 } from './assertions';
 import runTestsForPage from '../../support/helpers/runTestsForPage';
 
-const localEnvironmentTestSuites = [
+const testSuites = [
   {
     path: '/ws/languages',
     service: 'ws',
     runforEnv: ['local'],
     tests: [assertWSLanguagesPageLocal],
   },
-];
-
-const testEnvironmentTestSuites = [
   {
     path: '/ws/languages',
     service: 'ws',
     runforEnv: ['test'],
     tests: [assertWSLanguagesPageURN],
   },
-];
-
-const liveEnvironmentTestSuites = [
   {
     path: '/ws/languages',
     service: 'ws',
@@ -34,11 +28,7 @@ const liveEnvironmentTestSuites = [
 ];
 
 runTestsForPage({
-  testSuites: [
-    ...localEnvironmentTestSuites,
-    ...testEnvironmentTestSuites,
-    ...liveEnvironmentTestSuites,
-  ],
+  testSuites,
   testIsolation: true,
   pageType: STATIC_PAGE,
 });
