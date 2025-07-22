cc663e72884f9c78b7f79e521b43990adf0573aa
HarveyPeachey <harvey.peachey@bbc.co.uk>
2025-07-22 09:27:35 +0100

    debugging ati window

--- src/app/components/ATIAnalytics/canonical/sendPageViewBeaconOperaMini/index.ts
+++ src/app/components/ATIAnalytics/canonical/sendPageViewBeaconOperaMini/index.ts
@@ -4,8 +4,11 @@ export default (atiPageViewUrlString: string) => `
     if (${isOperaProxy.toString()}() && !Boolean(window.hasOperaMiniScriptRan)) {
       window.hasOperaMiniScriptRan = true;
 
-      var atiPageViewUrl = "${atiPageViewUrlString}";
+      var trackingPixelURL = "${atiPageViewUrlString}";
 
-      window.sendStaticBeacon(atiPageViewUrl);
+      var trackingPixel = new Image;
+      trackingPixel.src = trackingPixelURL;
+      var _window;
+      console.log((_window = window) === null || _window === void 0 ? void 0 : _window.ATInternet);
     }
 `;
