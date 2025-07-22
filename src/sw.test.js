7bd565764f1c60d094413e18a668321f5efe2edc
Santa Zena <santa.zena.ext@bbc.co.uk>
2025-07-22 13:28:13 +0300

    Adding woff2

--- src/sw.test.js
+++ src/sw.test.js
@@ -199,7 +199,9 @@ describe('Service Worker', () => {
     const cacheableAssets = [
       // Fonts
       'https://static.files.bbci.co.uk/fonts/reith-qalam/1.310/BBCReithQalam_W_Rg.woff',
-      'https://static.files.bbci.co.uk/fonts/reith-qalam/1.310/BBCReithQalam_W_Bd.woff ',
+      'https://static.files.bbci.co.uk/fonts/reith-qalam/1.310/BBCReithQalam_W_Bd.woff',
+      'https://static.files.bbci.co.uk/fonts/reith-qalam/1.310/BBCReithQalam_W_Rg.woff2',
+      'https://static.files.bbci.co.uk/fonts/reith-qalam/1.310/BBCReithQalam_W_Bd.woff2',
       'https://static.files.bbci.co.uk/fonts/reith/2.512/BBCReithSans_W_Bd.woff2',
       // Moment-lib - local, test & live
       'http://localhost:7080/static/js/modern.../moment-lib.abcd1234.js',
