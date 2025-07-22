f8e196d3dcf475692df67b426a30e86072b8ae7a
HarveyPeachey <harvey.peachey@bbc.co.uk>
2025-07-22 09:56:12 +0100

    send static beacon with tracking pixel url

--- src/app/components/ATIAnalytics/canonical/sendPageViewBeaconOperaMini/index.ts
+++ src/app/components/ATIAnalytics/canonical/sendPageViewBeaconOperaMini/index.ts
@@ -4,11 +4,8 @@ export default (atiPageViewUrlString: string) => `
     if (${isOperaProxy.toString()}() && !Boolean(window.hasOperaMiniScriptRan)) {
       window.hasOperaMiniScriptRan = true;
 
-      var trackingPixelURL = "${atiPageViewUrlString}";
+      var atiPageViewUrl = "${atiPageViewUrlString}";
 
-      var trackingPixel = new Image;
-      trackingPixel.src = trackingPixelURL;
-      var _window;
-      console.log((_window = window) === null || _window === void 0 ? void 0 : _window.ATInternet);
+      window.sendStaticBeacon(atiPageViewUrl);
     }
 `;
