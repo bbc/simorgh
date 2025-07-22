dbb83f7d74f64b56ed551b1b48431a714c779622
Lukas Freimonas <lukas.freimonas.ext@bbc.co.uk>
2025-07-22 18:41:55 +0300

    latest draft no.2

--- ws-nextjs-app/cypress/support/commands.ts
+++ ws-nextjs-app/cypress/support/commands.ts
@@ -33,10 +33,30 @@ declare global {
   }
 }
 
-const getPageDataFromWindow = () => {
+const getPageDataFromWindow = ({
+  service,
+  pageType,
+  variant,
+  id,
+}: {
+  service: Services;
+  pageType: PageTypes;
+  variant?: Variants;
+  id?: string;
+}) => {
+  const baseUrl =
+    Cypress.env('APP_ENV') === 'local'
+      ? 'http://localhost:7081'
+      : Cypress.config().baseUrl;
+  let path = `/${service}/${pageType}/${id}${variant ? `/${variant}` : ''}`;
+  if (service === 'ws') {
+    path = '/ws/languages';
+  }
+  cy.visit(`${baseUrl}${path}`);
   return cy.window().then(win => {
     return (
-      (win as CustomWindow).__NEXT_DATA__?.props?.pageProps?.pageData || {}
+      // eslint-disable-next-line no-underscore-dangle
+      (win as CustomWindow)?.__NEXT_DATA__?.props?.pageProps?.pageData || {}
     );
   });
 };
