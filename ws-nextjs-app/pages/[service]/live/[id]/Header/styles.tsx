aa35e488ce7742a835af2a88a5fcc5bcdee88888
Aaron Moore <aaron.moore@bbc.co.uk>
2025-07-23 18:22:34 +0100

    Update styles.tsx

--- ws-nextjs-app/pages/[service]/live/[id]/Header/styles.tsx
+++ ws-nextjs-app/pages/[service]/live/[id]/Header/styles.tsx
@@ -1,5 +1,5 @@
 import { css, Theme } from '@emotion/react';
-import pixelsToRem from 'simorgh/src/app/utilities/pixelsToRem/pixelsToRem';
+import pixelsToRem from '../../../../../../src/app/utilities/pixelsToRem';
 
 export default {
   headerContainer: ({ mq }: Theme) =>
