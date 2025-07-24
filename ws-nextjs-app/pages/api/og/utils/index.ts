e3c9253eb1d51cec14ac4009ef21dfd43a1e6c5f
Aaron Moore <aaron.moore@bbc.co.uk>
2025-07-24 11:24:29 +0100

    add syndication check

--- ws-nextjs-app/pages/api/og/utils/index.ts
+++ ws-nextjs-app/pages/api/og/utils/index.ts
@@ -9,18 +9,25 @@ const getImages = ({
   promoImage,
 }: {
   service: Services;
-  promoImage: { originCode: string; locator: string };
+  promoImage: {
+    originCode: string;
+    locator: string;
+    suitableForSyndication: boolean;
+  };
 }) => {
+  const defaultImage = `https://news.files.bbci.co.uk/ws/img/logos/og/${service}.png`;
+
+  if (!promoImage?.suitableForSyndication) return defaultImage;
+
   const unbrandedImage =
     buildIChefURL({
       originCode: promoImage?.originCode,
       locator: promoImage?.locator,
       resolution: 800,
-    }) || `https://news.files.bbci.co.uk/ws/img/logos/og/${service}.png`;
+    }) || defaultImage;
 
   const brandedImage =
-    getBrandedImage(promoImage?.locator, service) ||
-    `https://news.files.bbci.co.uk/ws/img/logos/og/${service}.png`;
+    getBrandedImage(promoImage?.locator, service) || defaultImage;
 
   return { unbrandedImage, brandedImage };
 };
