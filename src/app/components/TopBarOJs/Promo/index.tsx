a162703ef04ce15cfe613ef00f5ef75c4fd6014a
Louise Archibald <louise.archibald@bbc.co.uk>
2025-07-29 13:52:17 +0100

    converted styles for PromoList

---
+++ src/app/components/TopBarOJs/Promo/index.tsx
@@ -0,0 +1,142 @@
+import React, { use } from 'react';
+import styled from '@emotion/styled';
+import { getPica } from '#psammead/gel-foundations/src/typography';
+import { getSerifBold } from '#psammead/psammead-styles/src/font-styles';
+import { Link } from '#psammead/psammead-story-promo/src';
+import {
+  GEL_SPACING,
+  GEL_SPACING_DBL,
+} from '#psammead/gel-foundations/src/spacings';
+import {
+  GEL_GROUP_0_SCREEN_WIDTH_MIN,
+  GEL_GROUP_1_SCREEN_WIDTH_MIN,
+  GEL_GROUP_2_SCREEN_WIDTH_MIN,
+  GEL_GROUP_3_SCREEN_WIDTH_MIN,
+  GEL_GROUP_4_SCREEN_WIDTH_MIN,
+} from '#psammead/gel-foundations/src/breakpoints';
+import useOperaMiniDetection from '#hooks/useOperaMiniDetection';
+import { TopStoryItem } from '#app/pages/ArticlePage/PagePromoSections/TopStoriesSection/types';
+import { EventTrackingMetadata } from '#app/models/types/eventTracking';
+import useClickTrackerHandler from '#app/hooks/useClickTrackerHandler';
+import LiveLabel from '../../LiveLabel';
+import { ServiceContext } from '../../../contexts/ServiceContext';
+
+const StyledLink = styled(Link)`
+  ${({ script }) => script && getPica(script)}
+  ${({ service }) => service && getSerifBold(service)}
+  width: 100%;
+  text-decoration: none;
+
+  overflow-x: hidden;
+  overflow-y: hidden;
+
+  &:hover,
+  &:focus {
+    text-decoration: underline;
+  }
+
+  color: ${({ theme }) =>
+    theme.isDarkUi ? theme.palette.GREY_10 : theme.palette.GREY_8};
+  &:visited {
+    color: ${props => props.theme.palette.GREY_6};
+  }
+`;
+
+const PromoBox = styled.div`
+  position: relative;
+  display: block;
+  margin-bottom: 0;
+  background-color: ${({ theme }) =>
+    theme.isDarkUi ? theme.palette.GREY_3 : theme.palette.WHITE};
+  padding: ${GEL_SPACING};
+  height: auto;
+
+  @media (min-width: ${GEL_GROUP_0_SCREEN_WIDTH_MIN}) {
+    width: 14.8125rem;
+  }
+  @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
+    width: 11.125rem;
+  }
+  @media (min-width: ${GEL_GROUP_4_SCREEN_WIDTH_MIN}) {
+    width: 12.6875rem;
+  }
+
+  @media (min-width: ${GEL_GROUP_0_SCREEN_WIDTH_MIN}) {
+    width: 11.5rem;
+  }
+  @media (min-width: ${GEL_GROUP_1_SCREEN_WIDTH_MIN}) {
+    width: 17rem;
+  }
+  @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
+    width: 15.5rem;
+  }
+`;
+
+const OperaPromoBox = styled.div`
+  position: relative;
+  background-color: ${({ theme }) =>
+    theme.isDarkUi ? theme.palette.GREY_3 : theme.palette.WHITE};
+  padding: ${GEL_SPACING_DBL};
+  margin-bottom: ${GEL_SPACING_DBL};
+  width: calc(100% - ${GEL_SPACING});
+  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
+    width: calc(100% - ${GEL_SPACING_DBL});
+  }
+`;
+
+interface PromoProps {
+  block: TopStoryItem;
+  eventTrackingData: EventTrackingMetadata;
+}
+
+const Promo = ({ block, eventTrackingData }: PromoProps) => {
+  const { script, service } = use(ServiceContext);
+  const clickTrackerHandler = useClickTrackerHandler(eventTrackingData);
+
+  const overtypedHeadline = block?.headlines?.overtyped ?? '';
+  const mainHeadline = block?.headlines?.headline ?? '';
+  const headlineBlockText =
+    block?.headlines?.promoHeadline?.blocks?.[0]?.model?.blocks?.[0]?.model
+      ?.text ?? '';
+  const name = block?.name ?? '';
+
+  const title =
+    overtypedHeadline ||
+    mainHeadline ||
+    headlineBlockText ||
+    name ||
+    block.headline ||
+    '';
+
+  const canonicalUrl = block?.locators?.canonicalUrl ?? '';
+  const assetUri = block?.locators?.assetUri ?? '';
+  const uri = block?.uri ?? '';
+
+  const href =
+    canonicalUrl ||
+    assetUri ||
+    uri ||
+    (block.destinationUrl ? `https://www.bbc.com${block.destinationUrl}` : '');
+
+  const { isLive } = block;
+
+  const isOperaMini = useOperaMiniDetection();
+
+  const WrapperPromoBox = isOperaMini ? OperaPromoBox : PromoBox;
+
+  return (
+    <WrapperPromoBox>
+      <StyledLink
+        href={href}
+        service={service}
+        script={script}
+        {...clickTrackerHandler}
+      >
+        {isLive && <LiveLabel />}
+        {title}
+      </StyledLink>
+    </WrapperPromoBox>
+  );
+};
+
+export default Promo;
---
+++ src/app/components/TopBarOJs/PromoList/index.styles.tsx
@@ -0,0 +1,70 @@
+import { css, Theme } from '@emotion/react';
+
+export default {
+  standardScrollPromo: () => {
+    css({
+      listStyle: 'none',
+      paddingInlineStart: '0',
+      margin: '0',
+      display: 'flex',
+      overflowX: 'scroll',
+      scrollBehavior: 'auto',
+      WebkitOverflowScrolling: 'touch',
+      // Hide scrollbar
+      scrollbarWidth: 'none',
+      msOverflowStyle: 'none',
+      '&::-webkit-scrollbar': {
+        display: 'none',
+      },
+    });
+  },
+  operaScrollPromo: () => {
+    css({
+      listStyle: 'none',
+      paddingInlineStart: '0',
+      margin: '0',
+    });
+  },
+  list: ({ mq, spacings }: Theme) => {
+    css({
+      display: 'flex',
+      flexShrink: 0,
+
+      [mq.GROUP_0_MAX_WIDTH]: {
+        margin: 0,
+      },
+
+      [mq.GROUP_2_MIN_WIDTH]: {
+        marginInlineStart: `${spacings.DOUBLE}rem`,
+        'li:first-of-type': {
+          marginInlineStart: 0,
+        },
+        'li:last-of-type': {
+          marginInlineEnd: `${spacings.FULL}rem`,
+        },
+      },
+
+      [mq.GROUP_4_MIN_WIDTH]: {
+        marginInlineStart: `${spacings.DOUBLE}rem`,
+        'li:first-of-type': {
+          marginInlineStart: 0,
+        },
+      },
+    });
+  },
+  operaStyledList: ({ mq, spacings }: Theme) => {
+    css({
+      [mq.GROUP_0_MAX_WIDTH]: {
+        margin: `${spacings.FULL}rem`,
+      },
+
+      [mq.GROUP_2_MIN_WIDTH]: {
+        marginInlineStart: `${spacings.DOUBLE}rem`,
+      },
+
+      [mq.GROUP_4_MIN_WIDTH]: {
+        marginInlineStart: 0,
+      },
+    });
+  },
+};
