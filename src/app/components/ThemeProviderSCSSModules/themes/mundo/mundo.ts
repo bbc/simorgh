ed3da93021e8176f94cc67ac7ecf2a18fd51bd8a
Andrew Bennett <andrew.bennett07@bbc.co.uk>
2025-07-31 17:32:18 +0100

    mundo font nearly entirely converted to scss modules [copilot]

---
+++ src/app/components/ThemeProviderSCSSModules/themes/mundo/mundo.ts
@@ -0,0 +1,14 @@
+import '../../../fontFaces/reith-sans-bold.module.scss';
+import '../../../fontFaces/reith-sans-regular.module.scss';
+import '../../../fontFaces/reith-serif-medium.module.scss';
+import '../../../fontFaces/reith-serif-light.module.scss';
+import '../../../fontVariants/reith.module.scss';
+import './palette.module.scss';
+import '../../../fontScripts/latinWithDiacritics.module.scss';
+import brandSVG from '../../../ThemeProvider/chameleonLogos/mundo';
+
+const mundoTheme = {
+  brandSVG,
+};
+
+export default mundoTheme;
