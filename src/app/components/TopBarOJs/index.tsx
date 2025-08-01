5b0701765ce645d6cb26ddeadf93cc96f435aa06
Louise Archibald <louise.archibald@bbc.co.uk>
2025-08-01 15:31:29 +0100

    resolve type error

--- src/app/components/TopBarOJs/index.tsx
+++ src/app/components/TopBarOJs/index.tsx
@@ -1,14 +1,11 @@
 /** @jsx jsx */
 /* @jsxFrag React.Fragment */
 import { jsx } from '@emotion/react';
-import React, { use } from 'react';
-import pathOr from 'ramda/src/pathOr';
+import { use } from 'react';
 import isEmpty from 'ramda/src/isEmpty';
 import useViewTracker from '#hooks/useViewTracker';
 import useClickTrackerHandler from '#hooks/useClickTrackerHandler';
-import idSanitiser from '#lib/utilities/idSanitiser';
 import { TopStoryItem } from '#app/pages/ArticlePage/PagePromoSections/TopStoriesSection/types';
-import { EventTrackingMetadata } from '#app/models/types/eventTracking';
 import { ServiceContext } from '../../contexts/ServiceContext';
 import PromoList from './PromoList';
 import styles from './index.styles';
@@ -16,18 +13,21 @@ import styles from './index.styles';
 interface TopBarOJsProps {
   blocks: TopStoryItem[];
   id?: string;
-  eventTrackingData?: EventTrackingMetadata;
 }
 
-const TopBarOJs = ({
-  blocks,
-  id = 'top-bar-onward-journeys',
-  eventTrackingData,
-}: TopBarOJsProps) => {
+const eventTrackingData = {
+  componentName: 'top-bar-oj',
+};
+
+const TopBarOJs = ({ blocks, id }: TopBarOJsProps) => {
   const { translations } = use(ServiceContext);
 
   const viewTracker = useViewTracker(eventTrackingData);
-  const clickTracker = useClickTrackerHandler(eventTrackingData);
+  // const clickTracker = useClickTrackerHandler(eventTrackingData);
+  const clickTracker = {
+    ...eventTrackingData,
+    ...useClickTrackerHandler(eventTrackingData),
+  };
 
   if (!blocks || isEmpty(blocks)) {
     return null;
@@ -35,30 +35,16 @@ const TopBarOJs = ({
 
   const title = translations.topStoriesTitle || 'Top Stories';
 
-  const ariaLabel = title && idSanitiser(title);
-
-  const a11yAttributes = ariaLabel
-    ? { 'aria-labelledby': ariaLabel }
-    : {
-        'aria-label': pathOr(
-          'Related Content',
-          ['relatedContent'],
-          translations,
-        ),
-      };
-
   return (
-    <section role="region" aria-labelledby={id} data-testid={id}>
-      <div css={styles.labelComponent} id={ariaLabel} data-testid="oj-top-bar">
-        {title}
-      </div>
+    <section
+      role="region"
+      aria-labelledby={id}
+      data-testid={id}
+      {...viewTracker}
+    >
+      <div css={styles.labelComponent}>{title}</div>
       <div css={styles.promoContainer}>
-        <PromoList
-          blocks={blocks}
-          viewTracker={viewTracker}
-          clickTracker={clickTracker}
-          a11yAttributes={a11yAttributes}
-        />
+        <PromoList blocks={blocks} eventTrackingData={clickTracker} />
       </div>
     </section>
   );
