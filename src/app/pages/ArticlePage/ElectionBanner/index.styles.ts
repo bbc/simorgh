a5bb865496182b2fe19a39467fa733e510baaf0f
Shayne Ahchoon <shayneah@googlemail.com>
2025-08-06 11:20:29 +0100

    WS-873: Refactor Election Banner config

--- src/app/pages/ArticlePage/ElectionBanner/index.styles.ts
+++ src/app/pages/ArticlePage/ElectionBanner/index.styles.ts
@@ -2,35 +2,19 @@ import { css, Theme } from '@emotion/react';
 import pixelsToRem from '#app/utilities/pixelsToRem';
 import { ServiceConfig } from '#app/models/types/serviceConfig';
 
-type Heights = NonNullable<NonNullable<ServiceConfig['electionBanner']>['heights']>
+type Heights = NonNullable<
+  NonNullable<ServiceConfig['electionBanner']>['heights']
+>;
 
 export default {
   electionBannerWrapper: ({ spacings }: Theme) =>
     css({
       marginBottom: `${spacings.FULL}rem`,
     }),
-  electionBannerIframe: ({ mobile, tablet, desktop }: Heights) => ({ mq }: Theme) =>
-    css({
-      border: 'none',
-      width: '100%',
-      height: `${pixelsToRem(mobile)}rem`,
-      [mq.GROUP_3_MIN_WIDTH]: {
-        height: `${pixelsToRem(tablet)}rem`,
-      },
-      [mq.GROUP_4_MIN_WIDTH]: {
-        height: `${pixelsToRem(desktop)}rem`,
-      },
-    }),
-  electionBannerWrapperAmp: ({ mobile, tablet, desktop }: Heights) => ({ mq, spacings }: Theme) =>
-    css({
-      overflow: 'hidden',
-      marginBottom: `${spacings.FULL}rem`,
-      '> div': { padding: '0' },
-      '& amp-img': {
-        maxWidth: 640,
-        margin: '0 auto',
-      },
-      '& amp-iframe': {
+  electionBannerIframe:
+    ({ mobile, tablet, desktop }: Heights) =>
+    ({ mq }: Theme) =>
+      css({
         border: 'none',
         width: '100%',
         height: `${pixelsToRem(mobile)}rem`,
@@ -40,6 +24,28 @@ export default {
         [mq.GROUP_4_MIN_WIDTH]: {
           height: `${pixelsToRem(desktop)}rem`,
         },
-      },
-    }),
+      }),
+  electionBannerWrapperAmp:
+    ({ mobile, tablet, desktop }: Heights) =>
+    ({ mq, spacings }: Theme) =>
+      css({
+        overflow: 'hidden',
+        marginBottom: `${spacings.FULL}rem`,
+        '> div': { padding: '0' },
+        '& amp-img': {
+          maxWidth: 640,
+          margin: '0 auto',
+        },
+        '& amp-iframe': {
+          border: 'none',
+          width: '100%',
+          height: `${pixelsToRem(mobile)}rem`,
+          [mq.GROUP_3_MIN_WIDTH]: {
+            height: `${pixelsToRem(tablet)}rem`,
+          },
+          [mq.GROUP_4_MIN_WIDTH]: {
+            height: `${pixelsToRem(desktop)}rem`,
+          },
+        },
+      }),
 };
