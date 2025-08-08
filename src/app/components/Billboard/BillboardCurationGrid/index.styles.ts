7a6651cdf8e76aa08c4008c8999de5f2088a809a
“LilyL0u” <lily.higham@bbc.co.uk>
2025-08-08 12:48:58 +0100

    unnecessary extra

--- src/app/components/Billboard/BillboardCurationGrid/index.styles.ts
+++ src/app/components/Billboard/BillboardCurationGrid/index.styles.ts
@@ -83,9 +83,9 @@ const styles = {
             color: `${palette.WHITE}`,
           },
         },
-        '.promo-text > h3 > a:visited': {
-          color: `${palette.WHITE}`,
-        },
+        // '.promo-text > h3 > a:visited': {
+        //   color: `${palette.WHITE}`,
+        // },
 
         '& > time.promo-timestamp': {
           color: `${palette.WHITE}`,
