4d36bac6d0d0d6566f06b54c82a45fd3388682d6
mechrm02 <meriem.mechri01@bbc.co.uk>
2025-08-12 11:58:36 +0100

    refactor:Home page to follow other pages format

--- cypress/e2e/pages/homePage/index.cy.js
+++ cypress/e2e/pages/homePage/index.cy.js
@@ -1,33 +1,97 @@
+import runTestsForPage from '#nextjs/cypress/support/helpers/runTestsForPage';
 import { HOME_PAGE } from '#app/routes/utils/pageTypes';
-import getUrls from './urls';
-import getAppEnv from '../../../support/helpers/getAppEnv';
-import visitPage from '../../../support/helpers/visitPage';
-import crossPlatformTests from './tests';
+import getOptimizelyKey from '../../../support/helpers/getOptimizelyKey';
 import canonicalTests from './testsForCanonicalOnly';
 
-const pageType = HOME_PAGE;
+const tests = [canonicalTests];
 
-const environment = getAppEnv();
-const urls = getUrls();
+const testSuites = [
+  {
+    path: '/arabic',
+    runforEnv: ['local', 'test', 'live'],
+    service: 'arabic',
+    tests,
+  },
+  {
+    path: '/kyrgyz',
+    runforEnv: ['local', 'test', 'live'],
+    service: 'kyrgyz',
+    tests,
+  },
+  {
+    path: '/persian',
+    runforEnv: 'local',
+    service: 'persian',
+    tests,
+  },
+  {
+    path: '/pidgin',
+    runforEnv: ['local', 'test'],
+    service: 'pidgin',
+    tests,
+  },
+  {
+    path: '/polska',
+    runforEnv: ['local', 'test', 'live'],
+    service: 'polska',
+    tests,
+  },
+  {
+    path: '/portuguese',
+    runforEnv: ['local', 'test', 'live'],
+    service: 'portuguese',
+    tests,
+  },
+  {
+    path: '/serbian/lat',
+    runforEnv: ['local', 'test', 'live'],
+    service: '/serbian/lat',
+    tests,
+  },
+  {
+    path: '/serbian/cyr',
+    runforEnv: ['local', 'test', 'live'],
+    service: '/serbian/cyr',
+    tests,
+  },
+  {
+    path: '/uzbek/lat',
+    runforEnv: ['local', 'test', 'live'],
+    service: '/uzbek/lat',
+    tests,
+  },
+  {
+    path: '/uzbek/cyr',
+    runforEnv: ['local', 'test', 'live'],
+    service: '/uzbek/cyr',
+    tests,
+  },
+];
 
-urls.forEach(url => {
-  const { service, variant = 'default' } = url;
-  const currentPath = url[environment];
+const getTestSuites = () => {
+  const serviceToRun = Cypress.env('ONLY_SERVICE');
 
-  if (currentPath) {
-    describe(`Tests for ${pageType} page - ${currentPath}`, () => {
-      before(() => {
-        Cypress.env('currentPath', currentPath);
-        visitPage(currentPath, pageType);
-      });
-
-      crossPlatformTests({
-        service,
-        pageType,
-        variant,
-      });
-
-      canonicalTests({ service, pageType, variant });
-    });
+  if (serviceToRun) {
+    return testSuites.filter(({ service }) => service === serviceToRun);
   }
+
+  return testSuites;
+};
+
+describe('Home Page', () => {
+  beforeEach(() => {
+    cy.intercept(
+      {
+        url: `https://cdn.optimizely.com/datafiles/${getOptimizelyKey()}.json`,
+      },
+      request => {
+        request.reply({ statusCode: 404 });
+      },
+    ).as('disable-optimizely');
+  });
+
+  runTestsForPage({
+    pageType: HOME_PAGE,
+    testSuites: getTestSuites(),
+  });
 });
