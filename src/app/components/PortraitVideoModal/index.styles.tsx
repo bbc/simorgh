ee9d481aed20819b5c71ed6c5c87b81c3e266e03
Nabeel Khan <nabeel.khan@bbc.co.uk>
2025-08-12 12:50:07 +0100

    added &:focus-visible for tabbing with keyboard

--- src/app/components/PortraitVideoModal/index.styles.tsx
+++ src/app/components/PortraitVideoModal/index.styles.tsx
@@ -153,7 +153,7 @@ const styles = {
         fill: 'currentcolor',
       },
 
-      '&:hover': {
+      '&:hover, &:focus-visible' : {
         transform: 'scale(1.08)',
       },
       ':disabled': {
