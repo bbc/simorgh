80e3995a937d69c468cffe72662d4bc643232960
Karina Thomas <karina.thomas@bbc.co.uk>
2025-08-19 16:36:12 +0100

    Add correct relative time + long date format

--- src/app/legacy/psammead/psammead-locales/moment/fa-af.js
+++ src/app/legacy/psammead/psammead-locales/moment/fa-af.js
@@ -7,4 +7,15 @@ const months = 'حمل_ثور_جوزا_سرطان_اسد_سنبله_میزان_�
 moment.updateLocale('fa', {
   months,
   monthsShort: months,
+  relativeTime: {
+    past: '%s وړاندې', // %s 'ago'
+    m: '۱ دقیقه', // '1 minute'
+    mm: '%d دقیقې', // %d 'minutes'
+    h: '۱ ساعت', // '1 hour'
+    hh: '%d ساعتونه', // %d 'hours'
+  },
+  longDateFormat: {
+    LL: 'D MMMM YYYY',
+    LLL: 'D MMMM YYYY HH:mm',
+  },
 });
