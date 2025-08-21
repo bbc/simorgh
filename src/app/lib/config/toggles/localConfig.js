99174e6d48b76a7c9a01c87344ee49973253f35b
Aaron Moore <aaron.moore@bbc.co.uk>
2025-08-21 09:35:42 +0100

    Disable toggle fetching on local

--- src/app/lib/config/toggles/localConfig.js
+++ src/app/lib/config/toggles/localConfig.js
@@ -18,7 +18,7 @@ export default {
     enabled: true,
   },
   enableFetchingToggles: {
-    enabled: true,
+    enabled: false,
     value:
       '(afaanoromoo|afrique|amharic|arabic|archive|azeri|bengali|burmese|cymrufyw|dari|gahuza|gujarati|hausa|hindi|igbo|indonesia|japanese|korean|kyrgyz|marathi|mundo|naidheachdan|nepali|news|pashto|persian|pidgin|polska|portuguese|punjabi|russian|scotland|serbian|sinhala|somali|swahili|tamil|telugu|thai|tigrinya|turkce|ukchina|ukrainian|urdu|uzbek|ws|vietnamese|yoruba|zhongwen)',
   },
