f074ef45f88f567bf5783b0805a291b80a5728be
Louis Matsika <louis.matsika@bbc.co.uk>
2025-08-22 13:12:55 +0100

    refactor

--- src/app/legacy/components/Promo/readtime.jsx
+++ src/app/legacy/components/Promo/readtime.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const PromoReadTime = ({ children, isLive, className = '' }) => {
-  if (!children || isLive) {
-    return null;
-  }
+  if (!children || isLive) return null;
 
   return <p className={className}>readtime: {children}</p>;
 };
