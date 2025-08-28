b49ea05fe2c61ed99892618b37ee317a335c714a
emily saffron <emily.saffron@bbc.co.uk>
2025-08-28 12:09:13 +0100

    update test

--- src/app/legacy/containers/PageHandlers/withData/shouldRender/index.test.js
+++ src/app/legacy/containers/PageHandlers/withData/shouldRender/index.test.js
@@ -52,28 +52,6 @@ const invalidPortugueseData = {
   status: 404,
 };
 
-const articleDataWithoutKeySummaryPointsTagging = {
-  pageData: {},
-  status: 200,
-};
-
-const articleDataWithKeySummaryPointsTagging = {
-  pageData: {
-    metadata: {
-      passport: {
-        taggings: [
-          {
-            predicate: 'http://www.bbc.co.uk/ontologies/creativework/format',
-            value:
-              'http://www.bbc.co.uk/things/6b6d33cc-3e32-43e6-b06f-d43e71d44bad#id',
-          },
-        ],
-      },
-    },
-  },
-  status: 200,
-};
-
 jest.mock('../../../../../contexts/ServiceContext', () => {
   const mockReact = jest.requireActual('react');
   return jest.fn().mockImplementation(
