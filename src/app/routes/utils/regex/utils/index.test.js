357822b5e59ca3a3613c11bb812bbfc7e022ab90
Elvinas Valenas <elvinas.valenas.ext@bbc.co.uk>
2025-08-29 18:05:47 +0300

    test: fix test

--- src/app/routes/utils/regex/utils/index.test.js
+++ src/app/routes/utils/regex/utils/index.test.js
@@ -1,10 +1,13 @@
-import * as regexGenerators from '.';
+import * as regexUtils from '.';
 import serviceConfig from '../../../../lib/config/services/loadableConfig';
 
 jest.mock('#app/lib/utilities/isLive', () => jest.fn());
 
+const { getWorldServices, ...regexGenerators } = regexUtils;
+
 describe('regex utils snapshots', () => {
   const services = Object.keys(serviceConfig);
+
   Object.keys(regexGenerators).forEach(funcName => {
     it(`should create expected regex from ${funcName}`, () => {
       expect(regexGenerators[funcName](services)).toMatchSnapshot();
