e28d3040dc070dcd6b5d579937b56d679531c150
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2025-09-01 12:16:54 +0100

    WS-1086: Reflect unmerged changes from prototype branch

--- src/app/legacy/components/Promo/readtime.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const PromoReadTime = ({ children, isLive, className = '' }) => {
-  if (!children || isLive) return null;
-
-  return <p className={className}>readtime: {children}</p>;
-};
-
-export default PromoReadTime;
