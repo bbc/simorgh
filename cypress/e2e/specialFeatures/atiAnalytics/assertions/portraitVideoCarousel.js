c2b01cd333e2fb892f38e0a9fb11a9e08e94f333
Aaron Moore <aaron.moore@bbc.co.uk>
2025-09-03 13:00:37 +0100

    Add e2e test for PV carousel view

---
+++ cypress/e2e/specialFeatures/atiAnalytics/assertions/portraitVideoCarousel.js
@@ -0,0 +1,34 @@
+import { interceptATIAnalyticsBeacons, COMPONENTS } from '../helpers';
+import { assertATIComponentViewEvent } from '.';
+
+const { PORTRAIT_VIDEO_CAROUSEL } = COMPONENTS;
+
+export const assertPortraitVideoCarouselComponentView = ({
+  pageIdentifier,
+  contentType,
+  componentTrackingContentType,
+  useReverb,
+  path,
+  applicationType,
+  siteId,
+}) => {
+  it('should send a view event for the Portrait Video Carousel component', () => {
+    interceptATIAnalyticsBeacons();
+    cy.visit(path);
+
+    cy.get('[data-testid="portrait-video-carousel"]').scrollIntoView({
+      duration: 1000,
+    });
+
+    assertATIComponentViewEvent({
+      component: PORTRAIT_VIDEO_CAROUSEL,
+      pageIdentifier,
+      contentType: componentTrackingContentType || contentType,
+      useReverb,
+      applicationType,
+      siteId,
+    });
+  });
+};
+
+export default assertPortraitVideoCarouselComponentView;
