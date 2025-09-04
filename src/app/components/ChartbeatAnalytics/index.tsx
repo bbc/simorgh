7a80bb13d81b559b71852555826a5c8ac11e33f9
Karina Thomas <karina.thomas@bbc.co.uk>
2025-09-04 17:34:39 +0100

    Remove unused import

--- src/app/components/ChartbeatAnalytics/index.tsx
+++ src/app/components/ChartbeatAnalytics/index.tsx
@@ -1,5 +1,4 @@
 import React, { use } from 'react';
-import useToggle from '../../hooks/useToggle';
 import { RequestContext } from '../../contexts/RequestContext';
 import { ServiceContext } from '../../contexts/ServiceContext';
 import AmpChartbeatBeacon from './amp';
