251c642335a60e409fc0185db1576d24c4750a54
Karina Thomas <karina.thomas@bbc.co.uk>
2025-09-04 08:49:53 +0100

    Provide an override mechanism for fetching toggles locally

--- src/app/lib/config/toggles/localConfig.js
+++ src/app/lib/config/toggles/localConfig.js
@@ -18,7 +18,8 @@ export default {
     enabled: true,
   },
   enableFetchingToggles: {
-    enabled: false,
+    enabled:
+      process.env.SIMORGH_FETCH_TOGGLES.toLowerCase() === 'true' || false,
     value:
       '(afaanoromoo|afrique|amharic|arabic|archive|azeri|bengali|burmese|cymrufyw|dari|gahuza|gujarati|hausa|hindi|igbo|indonesia|japanese|korean|kyrgyz|marathi|mundo|naidheachdan|nepali|news|pashto|persian|pidgin|polska|portuguese|punjabi|russian|scotland|serbian|sinhala|somali|swahili|tamil|telugu|thai|tigrinya|turkce|ukchina|ukrainian|urdu|uzbek|ws|vietnamese|yoruba|zhongwen)',
   },
