f5c48edd0ab29e3a0fb40f7ee90dd039da1ff405
emily saffron <emily.saffron@bbc.co.uk>
2025-09-04 15:10:25 +0100

    remove footer check

--- cypress/e2e/pages/testsForAllPages.js
+++ cypress/e2e/pages/testsForAllPages.js
@@ -33,14 +33,11 @@ export const testsThatAlwaysRunForAllPages = ({
     });
 
     it('should have href values correctly formed', () => {
-      const allowedUrls = ['programmes/p0703hz7', // present on https://www.bbc.com/persian/topics/cw9qgeqd1zqt & redirects to https://www.bbc.com/persian/podcasts/p0703hz7
-      'send/u94753086' // insert comment here
+      const allowedUrls = [
+        'programmes/p0703hz7', // present on https://www.bbc.com/persian/topics/cw9qgeqd1zqt & redirects to https://www.bbc.com/persian/podcasts/p0703hz7
       ];
 
       cy.get('main a[href^="https://www.bbc.com"]').each($tag => {
-        if ($tag.closest('footer').length) {
-          return;
-        }
         const href = $tag.attr('href');
         expect(href).to.exist;
         expect(href).to.not.be.empty;
