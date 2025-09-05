c15ea9906c293315ea7def0e57dfc0a8e97ce15f
Nabeel Khan <nabeel.khan@bbc.co.uk>
2025-09-05 19:50:04 +0100

    linting issue fix

--- src/app/pages/HomePage/index.test.tsx
+++ src/app/pages/HomePage/index.test.tsx
@@ -641,10 +641,7 @@ describe('Home Page', () => {
       );
       const pidginHomePageDataWithSocialLinks = {
         ...pidginHomePageDataFixture,
-        curations: [
-          ...pidginHomePageDataFixture.curations,
-          socialLinks,
-        ],
+        curations: [...pidginHomePageDataFixture.curations, socialLinks],
       };
       // @ts-expect-error suppress pageData prop type conflicts due to missing imageAlt on selected historical test data for curations
       render(<HomePage pageData={pidginHomePageDataWithSocialLinks} />, {
@@ -655,7 +652,9 @@ describe('Home Page', () => {
         groupTracker: {
           name: socialLinks?.title,
           type: 'social-links',
-          position: socialLinks?.position ? socialLinks.position + 1 : undefined,
+          position: socialLinks?.position
+            ? socialLinks.position + 1
+            : undefined,
           resourceId: socialLinks?.curationId,
           itemCount: socialLinks?.summaries?.length,
         },
@@ -1023,69 +1022,66 @@ describe('Home Page', () => {
       );
     });
 
-
-
     describe('Social Links - click tracking', () => {
-            beforeEach(() => {
+      beforeEach(() => {
         (useViewTracker as jest.Mock).mockClear();
         (useClickTrackerHandler as jest.Mock).mockClear?.();
       });
       it('calls click tracking handler with correct data for Social Links promo link', () => {
-    const socialLinks = wsHomePageData.curations.find(
-      curation =>
-        curation.visualProminence === 'NORMAL' &&
-        curation.visualStyle === 'LINKS',
-    );
+        const socialLinks = wsHomePageData.curations.find(
+          curation =>
+            curation.visualProminence === 'NORMAL' &&
+            curation.visualStyle === 'LINKS',
+        );
 
-    const pidginHomePageDataWithSocialLinks = {
-      ...pidginHomePageDataFixture,
-      curations: [
-        ...pidginHomePageDataFixture.curations,
-        socialLinks,
-      ],
-    };
+        const pidginHomePageDataWithSocialLinks = {
+          ...pidginHomePageDataFixture,
+          curations: [...pidginHomePageDataFixture.curations, socialLinks],
+        };
 
-    // @ts-expect-error suppress pageData prop type conflicts due to missing imageAlt on selected historical test data for curations
-    render(<HomePage pageData={pidginHomePageDataWithSocialLinks} />, {
-      service: 'pidgin',
+        // @ts-expect-error suppress pageData prop type conflicts due to missing imageAlt on selected historical test data for curations
+        render(<HomePage pageData={pidginHomePageDataWithSocialLinks} />, {
+          service: 'pidgin',
+        });
+
+        const socialLinksSection = screen.getByTestId('social-links-1');
+        const firstSocialLink = socialLinksSection?.querySelector(
+          'ul[role="list"] a',
+        ) as HTMLAnchorElement;
+
+        expect(firstSocialLink).toBeInTheDocument();
+
+        fireEvent.click(firstSocialLink);
+
+        const promo = socialLinks?.summaries?.[0];
+
+        const expectedTrackingData = expect.objectContaining({
+          componentName: 'social-links',
+          groupTracker: expect.objectContaining({
+            name: socialLinks?.title,
+            type: 'social-links',
+            position: socialLinks?.position
+              ? socialLinks.position + 1
+              : undefined,
+            resourceId: socialLinks?.curationId,
+            itemCount: socialLinks?.summaries?.length,
+          }),
+          itemTracker: expect.objectContaining({
+            type: 'social-link-promo',
+            text: promo?.title,
+            position: 1,
+            resourceId: promo?.id,
+          }),
+        });
+
+        const socialLinksCalls = (
+          useClickTrackerHandler as jest.Mock
+        ).mock.calls
+          .map(([arg]) => arg)
+          .filter(call => call?.componentName === 'social-links');
+
+        expect(socialLinksCalls).toContainEqual(expectedTrackingData);
+      });
     });
-
-    const socialLinksSection = screen.getByTestId('social-links-1');
-    const firstSocialLink = socialLinksSection?.querySelector(
-      'ul[role="list"] a',
-    ) as HTMLAnchorElement;
-
-    expect(firstSocialLink).toBeInTheDocument();
-
-    fireEvent.click(firstSocialLink);
-
-    const promo = socialLinks?.summaries?.[0];
-
-  const expectedTrackingData = expect.objectContaining({
-    componentName: 'social-links',
-    groupTracker: expect.objectContaining({
-      name: socialLinks?.title,
-      type: 'social-links',
-      position: socialLinks?.position ? socialLinks.position + 1 : undefined,
-      resourceId: socialLinks?.curationId,
-      itemCount: socialLinks?.summaries?.length,
-    }),
-    itemTracker: expect.objectContaining({
-      type: 'social-link-promo',
-      text: promo?.title,
-      position: 1,
-      resourceId: promo?.id,
-    }),
   });
-
-  const socialLinksCalls = (
-    useClickTrackerHandler as jest.Mock
-  ).mock.calls
-    .map(([arg]) => arg)
-    .filter(call => call?.componentName === 'social-links');
-
-  expect(socialLinksCalls).toContainEqual(expectedTrackingData);
 });
-    });
-  })
-})
