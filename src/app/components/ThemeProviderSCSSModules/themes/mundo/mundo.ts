9fa653378b79c507c260df472930125c928ad332
Andrew Bennett <andrew.bennett07@bbc.co.uk>
2025-09-05 14:38:23 +0100

    correct imports

--- src/app/components/ThemeProviderSCSSModules/themes/mundo/mundo.ts
+++ src/app/components/ThemeProviderSCSSModules/themes/mundo/mundo.ts
@@ -1,10 +1,10 @@
-import '../../../fontFaces/reith-sans-bold.module.scss';
-import '../../../fontFaces/reith-sans-regular.module.scss';
-import '../../../fontFaces/reith-serif-medium.module.scss';
-import '../../../fontFaces/reith-serif-light.module.scss';
-import '../../../fontVariants/reith.module.scss';
+import '../../fontFaces/reith-sans-bold.module.scss';
+import '../../fontFaces/reith-sans-regular.module.scss';
+import '../../fontFaces/reith-serif-medium.module.scss';
+import '../../fontFaces/reith-serif-light.module.scss';
+import '../../fontVariants/reith.module.scss';
 import './palette.module.scss';
-import '../../../fontScripts/latinWithDiacritics.module.scss';
+import '../../fontScripts/latinWithDiacritics.module.scss';
 import brandSVG from '../../chameleonLogos/mundo';
 import withThemeProvider from '#app/components/ThemeProviderSCSSModules/withThemeProvider';
 
