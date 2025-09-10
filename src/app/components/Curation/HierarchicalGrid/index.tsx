8a9c5de9e471f8322db042bbe3fa913bb6bb4faf
Shayne Ahchoon <shayneah@googlemail.com>
2025-09-10 09:42:11 +0100

    WS-1017: Add styling for read time homepage experiment

--- src/app/components/Curation/HierarchicalGrid/index.tsx
+++ src/app/components/Curation/HierarchicalGrid/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/aria-role */
 /** @jsx jsx */
 /* @jsxFrag React.Fragment */
-import React, { use } from 'react';
+import { use } from 'react';
 import { css, jsx, Theme } from '@emotion/react';
 import moment from 'moment';
 import path from 'ramda/src/path';
