899270c13d4b5aefa68702ab75d1aaf443d536af
eagerterrier <eagerterrier@gmail.com>
2025-09-10 11:34:03 +0100

    TMP: putting back the csp inverted logic

--- src/server/index.jsx
+++ src/server/index.jsx
@@ -80,7 +80,7 @@ const skipMiddleware = (_req, _res, next) => {
 };
 
 const injectCspHeaderProdBuild =
-  process.env.NODE_ENV !== 'production' ? skipMiddleware : injectCspHeader;
+  process.env.NODE_ENV === 'production' ? skipMiddleware : injectCspHeader;
 
 server
   .disable('x-powered-by')
