327630ef659002445d6044404301037fad222744
holchris <holly.christopher@bbc.co.uk>
2025-09-11 10:45:13 +0100

    remove and clean up

---
+++ ws-nextjs-app/pages/[service]/watch/[id]/LiveTvPageLayout.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// import ChartbeatAnalytics from '#app/components/ChartbeatAnalytics';
+// import ATIAnalytics from '#app/components/ATIAnalytics';
+// import MetadataContainer from '#app/components/Metadata';
+
+export default function LiveTvLayout() {
+  return (
+    <>
+      {/* <ATIAnalytics atiData={atiAnalytics} />
+      <ChartbeatAnalytics title={pageTitle} />
+      <MetadataContainer
+        title={pageTitle}
+        lang={lang}
+        description={pageDescription}
+        openGraphType="website"
+        hasAmpPage={false}
+      /> */}
+      <main role="main">
+        <h1 id="content">HELLO WORLD</h1>
+      </main>
+    </>
+  );
+}
