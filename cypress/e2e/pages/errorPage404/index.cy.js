1ed234883c0f0c66214a94ff2ae65508f797ba79
mechrm02 <meriem.mechri01@bbc.co.uk>
2025-09-11 21:19:24 +0100

    refactor:update per review comments

--- cypress/e2e/pages/errorPage404/index.cy.js
+++ cypress/e2e/pages/errorPage404/index.cy.js
@@ -3,7 +3,7 @@ import testsForAllPages from './tests';
 import ampTests from './testsForAMPOnly';
 import canonicalTests from './testsForCanonicalOnly';
 import { ERROR_PAGE } from '../../../../src/app/routes/utils/pageTypes';
-import liteTests from '../articles/testsForLiteOnly';
+// import liteTests from '../articles/testsForLiteOnly';
 
 const tests = [testsForAllPages, canonicalTests, ampTests];
 
@@ -11,84 +11,36 @@ const canonicalTestSuites = [
   {
     path: '/arabic/articles/c123456abcdo',
     service: 'arabic',
-    runforEnv: 'local',
-    tests,
-  },
-  {
-    path: '/news/articles/cxvxrj8tvppo',
-    service: 'news',
-    runforEnv: 'local',
-    tests,
-  },
-  {
-    path: '/kyrgyz/articles/c123456abcdo',
-    service: 'kyrgyz',
-    runforEnv: 'local',
+    runforEnv: ['local', 'test', 'live'],
     tests,
   },
   {
     path: '/mundo/articles/c123456abcdo',
     service: 'mundo',
-    runforEnv: 'local',
-    tests,
-  },
-  {
-    path: '/persian/articles/cxvxrj8tvppo',
-    service: 'persian',
-    runforEnv: 'local',
-    tests,
-  },
-  {
-    path: '/pidgin/articles/cxvxrj8tvppo',
-    service: 'pidgin',
-    runforEnv: 'local',
+    runforEnv: ['local', 'test', 'live'],
     tests,
   },
   {
     path: '/serbian/articles/cabcdefghijo/cyr',
     service: 'serbian',
-    runforEnv: 'local',
+    runforEnv: ['local', 'test', 'live'],
     variant: 'cyr',
     tests,
   },
   {
     path: '/serbian/articles/cabcdefghijo/lat',
     service: 'serbian',
-    runforEnv: 'local',
+    runforEnv: ['local', 'test', 'live'],
     variant: 'lat',
-    tests,
-  },
-  {
-    path: '/russian/articles/c123456abcdo',
-    service: 'russian',
-    runforEnv: 'local',
     tests,
   },
   {
     path: '/ukrainian/articles/c123456abcdo',
     service: 'ukrainian',
-    runforEnv: 'local',
-    tests,
-  },
-  {
-    path: '/zhongwen/articles/cabcdefghijo/simp',
-    service: 'zhongwen',
-    runforEnv: 'local',
-    variant: 'simp',
-    tests,
-  },
-  {
-    path: '/zhongwen/articles/cabcdefghijo/trad',
-    service: 'zhongwen',
-    runforEnv: 'local',
-    variant: 'trad',
+    runforEnv: ['local', 'test', 'live'],
     tests,
   },
 ];
-
-const canonicalTestSuitesToRun = canonicalTestSuites.filter(
-  testSuite => testSuite.service !== 'news',
-);
 
 const ampTestSuites = [...canonicalTestSuites].map(testSuite => {
   return {
@@ -97,20 +49,15 @@ const ampTestSuites = [...canonicalTestSuites].map(testSuite => {
   };
 });
 
-const liteTestSuitesToRun = canonicalTestSuites.map(testSuite => {
-  return {
-    ...testSuite,
-    path: `${testSuite.path}.lite`,
-    tests: [liteTests],
-  };
+ampTestSuites.push({
+  path: '/news/articles/cxvxrj8tvppo.amp',
+  service: 'news',
+  runforEnv: ['local', 'test', 'live'],
+  tests,
 });
 
 runTestsForPage({
   failOnStatusCode: false,
   pageType: ERROR_PAGE,
-  testSuites: [
-    ...canonicalTestSuitesToRun,
-    ...ampTestSuites,
-    ...liteTestSuitesToRun,
-  ],
+  testSuites: [...canonicalTestSuites, ...ampTestSuites],
 });
