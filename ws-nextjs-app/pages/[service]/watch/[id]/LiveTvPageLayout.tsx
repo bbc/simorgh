523e435e6e584959ecad55bff1ac03bc797803ae
holchris <holly.christopher@bbc.co.uk>
2025-09-11 16:38:27 +0100

    pass in service and pagetype

--- ws-nextjs-app/pages/[service]/watch/[id]/LiveTvPageLayout.tsx
+++ ws-nextjs-app/pages/[service]/watch/[id]/LiveTvPageLayout.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { LiveTVPageProps } from './types';
 // import ChartbeatAnalytics from '#app/components/ChartbeatAnalytics';
 // import ATIAnalytics from '#app/components/ATIAnalytics';
 // import MetadataContainer from '#app/components/Metadata';
 
-export default function LiveTvLayout() {
+export default function LiveTvLayout({ pageType, service }: LiveTVPageProps) {
   return (
     <>
       {/* <ATIAnalytics atiData={atiAnalytics} />
@@ -16,7 +17,9 @@ export default function LiveTvLayout() {
         hasAmpPage={false}
       /> */}
       <main role="main">
-        <h1 id="content">HELLO WORLD</h1>
+        <h1 id="content">
+          HELLO WORLD pageType: {pageType} service: {service}
+        </h1>
       </main>
     </>
   );
