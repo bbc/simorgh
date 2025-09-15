4e53133a8efde8fbabd4edf83fdaefda054cd612
Aaron Moore <aaron.moore@bbc.co.uk>
2025-09-15 16:33:57 +0100

    Update route.api.test.ts

--- ws-nextjs-app/app/api/[service]/og/[id]/route.api.test.ts
+++ ws-nextjs-app/app/api/[service]/og/[id]/route.api.test.ts
@@ -14,6 +14,13 @@ jest.mock('next/og', () => {
   };
 });
 
+const mockResponse = (status: number) => {
+  // mock Response
+  global.Response = jest.fn().mockImplementation(() => {
+    return { status };
+  }) as unknown as typeof Response;
+};
+
 describe('GET /api/og', () => {
   beforeEach(() => {
     jest.restoreAllMocks();
@@ -24,6 +31,8 @@ describe('GET /api/og', () => {
       status: 200,
       json: {},
     });
+
+    mockResponse(200);
 
     const response = await api(
       new Request('https://www.bbc.com/pidgin/og/czjnnk9m8vdo'),
@@ -41,6 +50,8 @@ describe('GET /api/og', () => {
       json: {},
     });
 
+    mockResponse(200);
+
     const response = await api(
       new Request('https://www.bbc.com/mundo/og/cemn2qq3x8vt'),
       {
@@ -52,6 +63,8 @@ describe('GET /api/og', () => {
   });
 
   it('should return a 404 response when id is missing', async () => {
+    mockResponse(404);
+
     const response = await api(
       new Request('https://www.bbc.com/pidgin/og/czjnnk9m8vdo'),
       {
@@ -64,6 +77,8 @@ describe('GET /api/og', () => {
   });
 
   it('should return a 404 response when service is missing', async () => {
+    mockResponse(404);
+
     const response = await api(
       new Request('https://www.bbc.com/pidgin/og/czjnnk9m8vdo'),
       {
@@ -76,6 +91,8 @@ describe('GET /api/og', () => {
   });
 
   it('should return a 404 response when id is not an article or live id pattern', async () => {
+    mockResponse(404);
+
     const response = await api(
       new Request('https://www.bbc.com/pidgin/og/czjnnk9m8vdo'),
       {
@@ -91,6 +108,8 @@ describe('GET /api/og', () => {
       status: 500,
       json: {},
     });
+
+    mockResponse(500);
 
     const response = await api(
       new Request('https://www.bbc.com/pidgin/og/czjnnk9m8vdo'),
