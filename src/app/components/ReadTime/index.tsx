12e3c8b0cfcba938ada0adabfc98f448e8615722
Shayne Ahchoon <shayneah@googlemail.com>
2025-09-16 15:04:46 +0100

    WS-1189: Enable experiment and propogate to Live

--- src/app/components/ReadTime/index.tsx
+++ src/app/components/ReadTime/index.tsx
@@ -4,7 +4,6 @@ import { use } from 'react';
 import { ServiceContext } from '#app/contexts/ServiceContext';
 import { EventTrackingData } from '#app/lib/analyticsUtils/types';
 import useViewTracker from '#app/hooks/useViewTracker';
-import isLive from '#app/lib/utilities/isLive';
 import Text from '#app/components/Text';
 import styles from './index.styles';
 
