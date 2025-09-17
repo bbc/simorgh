a0ee5eb8feda1c02ad9d83813aefe29ba6944004
holchris <holly.christopher@bbc.co.uk>
2025-09-17 14:35:37 +0100

    refactor

--- ws-nextjs-app/cypress/e2e/liveTvPage/assertions/index.ts
+++ ws-nextjs-app/cypress/e2e/liveTvPage/assertions/index.ts
@@ -1,6 +1,9 @@
-export const assertLiveTvPage = ({ service }: { service: string }) => {
+export const assertLiveTvPage = (
+  { service }: { service: string },
+  { pageType }: { pageType: string },
+) => {
   it(`should render the Live TV Page with a h1 containing Hello World for ${service}`, () => {
-    const expectedHeading = `HELLO WORLD`;
-    cy.get('h1').should('have.text', expectedHeading);
+    const expectedHeading = `HELLO WORLD pageType: ${pageType} service: ${service}`;
+    cy.get('h1').should('contain.text', expectedHeading);
   });
 };
