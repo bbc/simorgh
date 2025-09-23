b758f329daa38637837b9c196cd7c8fc7343d3bb
“LilyL0u” <lily.higham@bbc.co.uk>
2025-09-23 14:50:23 +0100

    try different import

--- ws-nextjs-app/pages/[service]/live/[id]/[[...variant]].page.tsx
+++ ws-nextjs-app/pages/[service]/live/[id]/[[...variant]].page.tsx
@@ -13,7 +13,7 @@ import { NON_200_RESPONSE } from '#server/utilities/customMetrics/metrics.const'
 import PageDataParams from '#app/models/types/pageDataParams';
 import deriveVariant from '#nextjs/utilities/deriveVariant';
 // on latest this line is import extractHeaders from '../../../../../src/server/utilities/extractHeaders'; but when I save it changes it to 'simorgh/src/server/utilities/extractHeaders/extractHeaders' which doesn't work
-import extractHeaders from 'simorgh/src/server/utilities/extractHeaders/extractHeaders';
+import extractHeaders from '#server/utilities/extractHeaders';
 // import extractHeaders from 'simorgh/src/server/utilities/extractHeaders/extractHeaders';
 import isValidPageNumber from '../../../../utilities/pageQueryValidator';
 import getPageData from '../../../../utilities/pageRequests/getPageData';
