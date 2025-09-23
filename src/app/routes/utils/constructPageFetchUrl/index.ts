e1475a237ab42b54bfad1fc452294c7e2ad7828e
emily saffron <emily.saffron@bbc.co.uk>
2025-09-23 16:03:34 +0100

    fix regex

--- src/app/routes/utils/constructPageFetchUrl/index.ts
+++ src/app/routes/utils/constructPageFetchUrl/index.ts
@@ -42,7 +42,7 @@ const getUgcId = (path: string) => path.match(/(u[a-zA-Z0-9]{8,})/)?.[1];
 export const isOptimoIdCheck = (path: string) =>
   /\/(articles|sgeulachdan|erthyglau)\/(c[a-zA-Z0-9]{10,}o)/.test(path);
 export const isCpsIdCheck = (path: string) =>
-  /([0-9]{5,9}|[a-z0-9\-_]+-[0-9]{5,9})/.test(path);
+  /([0-9]{5,9}|[a-z0-9\-_]+-[0-9]{5,9})$/.test(path);
 const isTipoIdCheck = (path: string) => /(c[a-zA-Z0-9]{10,}t)/.test(path);
 
 interface GetIdProps {
