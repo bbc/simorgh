04420d321cdac30bbfb82ed7b0f54cdb0d7be331
Louise Archibald <louise.archibald@bbc.co.uk>
2025-09-26 13:59:03 +0100

    add mostReadTitle to translations type

--- src/app/models/types/translations.ts
+++ src/app/models/types/translations.ts
@@ -205,6 +205,7 @@ export interface Translations {
     linkText: string;
   };
   topStoriesTitle?: string;
+  mostReadTitle?: string;
   featuresAnalysisTitle?: string;
   latestMediaTitle?: string;
   infoBannerLabel?: string;
