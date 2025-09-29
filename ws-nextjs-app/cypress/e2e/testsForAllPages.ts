547c51db36224b47eac0784737b0bc095d5ace6b
Shayne Ahchoon <shayneah@googlemail.com>
2025-09-29 13:46:48 +0100

    WS-503: Add Cypress tests

--- ws-nextjs-app/cypress/e2e/testsForAllPages.ts
+++ ws-nextjs-app/cypress/e2e/testsForAllPages.ts
@@ -1,10 +1,15 @@
+/* eslint-disable import/no-relative-packages */
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable cypress/no-unnecessary-waiting */
 // For testing important features that differ between services, e.g. Timestamps.
 // We recommend using inline conditional logic to limit tests to services which differ.
-import * as checkA11y from '../../../cypress/support/helpers/checkA11y';
+import checkA11y from '../../../cypress/support/helpers/checkA11y';
+import { ServiceParametersType } from '../types';
 
-export const testsThatAlwaysRunForAllPages = ({ service, pageType }) => {
+export const testsThatAlwaysRunForAllPages = ({
+  service,
+  pageType,
+}: ServiceParametersType) => {
   describe(`testsToAlwaysRunForAllPages to run for ${service} ${pageType}`, () => {
     it('should have no detectable a11y violations on page load', () => {
       checkA11y();
