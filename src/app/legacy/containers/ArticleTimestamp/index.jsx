28c01de74eb2c79e09315c3d7f22e1d212dee583
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2025-10-01 13:33:08 +0100

    WS-504: Revert timestamp debug. Apply fix (hopefully)

--- src/app/legacy/containers/ArticleTimestamp/index.jsx
+++ src/app/legacy/containers/ArticleTimestamp/index.jsx
@@ -1,34 +1,27 @@
 import React, { use } from 'react';
 import Timestamp from '#psammead/psammead-timestamp-container/src';
-// import { GridItemMedium, PopOutGridItemMedium } from '#components/Grid';
+import { GridItemMedium, PopOutGridItemMedium } from '#components/Grid';
 import { ServiceContext } from '../../../contexts/ServiceContext';
 import { formatDateNumeric } from './timeFormats';
 import {
   isFirstRelative,
-  // isLastRelative,
+  isLastRelative,
   formatType,
   isValidDateTime,
 } from './helpers';
-// import shouldDisplayLastUpdatedTimestamp from './shouldDisplayLastUpdatedTimestamp';
-
-// const FirstPublishedTimestamp = Timestamp;
-// const LastUpdatedTimestamp = Timestamp;
+import shouldDisplayLastUpdatedTimestamp from './shouldDisplayLastUpdatedTimestamp';
 
 const ArticleTimestamp = ({
   firstPublished,
   lastPublished,
-  // eslint-disable-next-line no-unused-vars
   popOut = true,
-  // eslint-disable-next-line no-unused-vars
   minutesTolerance = 0,
-  // eslint-disable-next-line no-unused-vars
   className = '',
-  // eslint-disable-next-line no-unused-vars
   showReadTimeBelowTimestamp = false,
 }) => {
   const {
-    // articleTimestampPrefix,
-    // articleTimestampSuffix,
+    articleTimestampPrefix,
+    articleTimestampSuffix,
     datetimeLocale,
     script,
     service,
@@ -54,47 +47,46 @@ const ArticleTimestamp = ({
     isRelative: isFirstRelative(firstPublished, lastPublished),
   };
 
-  // const lastPublishedProps = {
-  //   timestamp: lastPublished,
-  //   format: formatType({
-  //     lastPublished,
-  //     firstPublished,
-  //     datetimeLocale,
-  //   }),
-  //   isRelative: isLastRelative(lastPublished),
-  //   prefix: articleTimestampPrefix,
-  //   suffix: articleTimestampSuffix,
-  // };
+  const lastPublishedProps = {
+    timestamp: lastPublished,
+    format: formatType({
+      lastPublished,
+      firstPublished,
+      datetimeLocale,
+    }),
+    isRelative: isLastRelative(lastPublished),
+    prefix: articleTimestampPrefix,
+    suffix: articleTimestampSuffix,
+  };
 
-  // const GridWrapper = popOut ? PopOutGridItemMedium : GridItemMedium;
+  const GridWrapper = popOut ? PopOutGridItemMedium : GridItemMedium;
 
-  // const displayLastUpdatedTimestamp = shouldDisplayLastUpdatedTimestamp({
-  //   minutesTolerance,
-  //   firstPublished,
-  //   lastPublished,
-  // });
+  const displayLastUpdatedTimestamp = shouldDisplayLastUpdatedTimestamp({
+    minutesTolerance,
+    firstPublished,
+    lastPublished,
+  });
 
   return (
-    <Timestamp {...timestampProps} {...firstPublishedProps} />
-    // <GridWrapper {...(className ? { className } : undefined)}>
-    //   <FirstPublishedTimestamp
-    //     {...timestampProps}
-    //     {...firstPublishedProps}
-    //     // // EXPERIMENT: Article Read Time
-    //     {...(showReadTimeBelowTimestamp && { padding: false })}
-    //   />
-    //   {displayLastUpdatedTimestamp && (
-    //     // Div has been used for No CSS formatting see #5554
-    //     <div>
-    //       <LastUpdatedTimestamp
-    //         {...timestampProps}
-    //         {...lastPublishedProps}
-    //         // // EXPERIMENT: Article Read Time
-    //         {...(showReadTimeBelowTimestamp && { padding: false })}
-    //       />
-    //     </div>
-    //   )}
-    // </GridWrapper>
+    <GridWrapper {...(className ? { className } : undefined)}>
+      <Timestamp
+        {...timestampProps}
+        {...firstPublishedProps}
+        // EXPERIMENT: Article Read Time
+        {...(showReadTimeBelowTimestamp && { padding: false })}
+      />
+      {displayLastUpdatedTimestamp && (
+        // Div has been used for No CSS formatting see #5554
+        <div>
+          <Timestamp
+            {...timestampProps}
+            {...lastPublishedProps}
+            // EXPERIMENT: Article Read Time
+            {...(showReadTimeBelowTimestamp && { padding: false })}
+          />
+        </div>
+      )}
+    </GridWrapper>
   );
 };
 
