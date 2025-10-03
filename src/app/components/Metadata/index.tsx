928bf828937e98fc231d5d4a12d595b66483ae94
louisearchibald <120198886+louisearchibald@users.noreply.github.com>
2025-10-03 14:54:37 +0100

    Revert "WS 1184 - Go live with updated og:meta images (#13193)"
    This reverts commit b16e0e2b0d5d064765ac4a5beb8914752f2f4ba7.

--- src/app/components/Metadata/index.tsx
+++ src/app/components/Metadata/index.tsx
@@ -8,6 +8,7 @@ import {
   getArticleId,
   getTipoId,
 } from '#app/routes/utils/constructPageFetchUrl';
+import isLive from '#app/lib/utilities/isLive';
 import {
   ARTICLE_PAGE,
   LIVE_PAGE,
@@ -79,6 +80,9 @@ const getSocialShareImage = ({
   pathname: string;
   service: Services;
 }) => {
+  // TODO: Remove to release experiment
+  if (isLive()) return metaImage;
+
   if (!OG_EXPERIMENT_SERVICES.includes(service)) return metaImage;
   if (!OG_EXPERIMENT_PAGETYPES.includes(pageType)) return metaImage;
 
