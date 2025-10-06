501a44e10ff4a98ce11b8667caa3f0753fdfd2bd
Shayne Ahchoon <shayneah@googlemail.com>
2025-10-06 19:33:39 +0100

    WS-503: Add Cypress tests

--- ws-nextjs-app/cypress/e2e/specialFeatures/liteSiteWeight/index.cy.ts
+++ ws-nextjs-app/cypress/e2e/specialFeatures/liteSiteWeight/index.cy.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-relative-packages */
 import { LIVE_PAGE } from '#app/routes/utils/pageTypes';
-import * as assertPageWeight from '../../../../../cypress/e2e/specialFeatures/liteSiteWeight/assertions/liteSiteWeight';
+import assertPageWeight from '../../../../../cypress/e2e/specialFeatures/liteSiteWeight/assertions/liteSiteWeight';
 import runTestsForPage from '../../../support/helpers/runTestsForPage';
 
 const testSuites = [
@@ -8,6 +8,7 @@ const testSuites = [
     path: '/burmese/live/ckg19998pldt.lite',
     runforEnv: ['local'],
     tests: [assertPageWeight],
+    service: 'burmese',
   },
 ];
 
