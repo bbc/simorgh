513702bb7419e2a0c1afb674d473d0178358f4ee
Elvinas Valenas <elvinas.valenas.ext@bbc.co.uk>
2025-10-07 14:50:08 +0300

    chore: remove test value

--- src/app/utilities/createAdNonce/index.ts
+++ src/app/utilities/createAdNonce/index.ts
@@ -47,7 +47,7 @@ export default ({
     isLite ||
     !isNonceToggleEnabled ||
     !isAdsToggleEnabled ||
-    !(showAdsBasedOnLocation || true) || // TODO: TEMP for testing purposes
+    !showAdsBasedOnLocation ||
     !isNonceEnabledForCountry(nonceCountries, country)
   ) {
     return null;
