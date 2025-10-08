148d3ece42c615e757320a93aca1c1e799bedff9
mechrm02 <meriem.mechri01@bbc.co.uk>
2025-10-08 09:55:38 +0100

    refactor:remove /config/servicesimport

--- cypress/e2e/pages/articles/tests.js
+++ cypress/e2e/pages/articles/tests.js
@@ -1,4 +1,3 @@
-import config from '../../../support/config/services';
 import appConfig from '../../../../src/server/utilities/serviceConfigs';
 import { crossPlatform as mostReadAssertions } from '../mostReadPage/mostReadAssertions';
 
@@ -17,14 +16,12 @@ const serviceHasInlineLink = service =>
 export default ({ service, pageType, variant = 'default' }) => {
   describe(`Running tests for ${service} ${pageType}`, () => {
     describe(`Metadata`, () => {
-      const serviceID = config[service]?.name || service;
-
       // Here we should only have metadata tests that are unique to articles pages
       it('should have the correct articles metadata', () => {
         cy.get('meta[name="article:author"]').should(
           'have.attr',
           'content',
-          appConfig[serviceID][variant].articleAuthor,
+          appConfig[service][variant].articleAuthor,
         );
       });
 
