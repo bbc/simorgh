79862a664d03cd5a80e453caa79e5d73f3265e86
alex-magana <alex.magana@andela.com>
2025-10-08 18:25:43 +0300

    Remove optimizely assertions from useSwipeTracker unit testsD

--- src/app/hooks/useSwipeTracker/index.test.tsx
+++ src/app/hooks/useSwipeTracker/index.test.tsx
@@ -12,12 +12,6 @@ import { Toggles } from '#app/models/types/global';
 import useSwipeTracker from '.';
 import fixtureData from './fixtureData.json';
 
-process.env.SIMORGH_ATI_BASE_URL = 'https://logws1363.ati-host.net?';
-
-jest.mock('#app/lib/utilities/getUUID', () =>
-  jest.fn().mockImplementation(() => '12345678-abcd-1fed-0123-a1b2c3d4e5f6'),
-);
-
 const {
   metadata: { atiAnalytics },
 } = fixtureData;
@@ -32,11 +26,6 @@ const defaultToggles = {
     enabled: true,
   },
 };
-
-const defaultOptimizely = {
-  track: jest.fn(),
-  user: { attributes: { foo: 'bar' }, id: 'test' },
-} as unknown as ReactSDKClient;
 
 const reverbMock = {
   isReady: jest.fn(),
@@ -54,27 +43,23 @@ const wrapper = ({
   atiData,
   children,
   toggles = defaultToggles,
-  mockOptimizely = defaultOptimizely,
 }: {
   atiData?: ATIData;
   children?: ReactNode | null;
   toggles?: Toggles;
-  mockOptimizely?: ReactSDKClient;
 }) => (
-  <OptimizelyProvider optimizely={mockOptimizely} isServerSide>
-    <AllTheProviders
-      atiData={atiData}
-      bbcOrigin="https://www.test.bbc.com"
-      pageData={fixtureData}
-      pageType={HOME_PAGE}
-      isAmp={false}
-      service="hindi"
-      pathname="/hindi"
-      toggles={toggles}
-    >
-      {children}
-    </AllTheProviders>
-  </OptimizelyProvider>
+  <AllTheProviders
+    atiData={atiData}
+    bbcOrigin="https://www.test.bbc.com"
+    pageData={fixtureData}
+    pageType={HOME_PAGE}
+    isAmp={false}
+    service="hindi"
+    pathname="/hindi"
+    toggles={toggles}
+  >
+    {children}
+  </AllTheProviders>
 );
 
 describe('useSwipeTracker', () => {
@@ -91,149 +76,6 @@ describe('useSwipeTracker', () => {
       const swipeTracker = result.current;
 
       expect(swipeTracker).toBeInstanceOf(Function);
-    });
-
-    describe('Optimizely', () => {
-      it('should send event to Optimizely when element the element is in view and optimizely object exists', async () => {
-        const {
-          result: { current: swipeTracker },
-        } = renderHook(() => useSwipeTracker(eventTrackingData), {
-          wrapper: props => wrapper({ ...props, atiData: atiAnalytics }),
-        });
-
-        await swipeTracker({
-          ...eventTrackingData,
-          sendOptimizelyEvents: true,
-          experimentName: 'dummy_experiment',
-          experimentVariant: 'variation_a',
-          groupTracker: {
-            name: 'group name',
-            itemCount: 20,
-            resourceId:
-              'urn:bbc:tipo:list:fe4a1c8c-9a7c-4a50-845d-7da91aa65204',
-            position: 4,
-            type: 'portrait-video-modal',
-          },
-          itemTracker: {
-            type: 'portrait-video',
-            text: 'टेलर स्विफ़्ट ने अपने इस डर का किया ज़िक्र',
-            mediaType: 'video',
-            position: 2,
-            duration: 22000,
-            resourceId: 'urn:bbc:pips:pid:p0m6zysd',
-          },
-        });
-
-        expect(defaultOptimizely.track).toHaveBeenCalledTimes(1);
-        expect(defaultOptimizely.track).toHaveBeenCalledWith(
-          'portrait-video-modal-views',
-          defaultOptimizely.user.id,
-          defaultOptimizely.user.attributes,
-        );
-      });
-
-      it('should not send event to Optimizely when element is in view and optimizely object is undefined', async () => {
-        const mockOptimizelyTrack = jest.fn();
-        const mockOptimizely = undefined;
-
-        const {
-          result: { current: swipeTracker },
-        } = renderHook(() => useSwipeTracker(eventTrackingData), {
-          wrapper: props =>
-            wrapper({ ...props, atiData: atiAnalytics, mockOptimizely }),
-        });
-
-        await swipeTracker({
-          ...eventTrackingData,
-          sendOptimizelyEvents: true,
-          experimentName: 'dummy_experiment',
-          experimentVariant: 'variation_a',
-          groupTracker: {
-            name: 'group name',
-            itemCount: 20,
-            resourceId:
-              'urn:bbc:tipo:list:fe4a1c8c-9a7c-4a50-845d-7da91aa65204',
-            position: 4,
-            type: 'portrait-video-modal',
-          },
-          itemTracker: {
-            type: 'portrait-video',
-            text: 'टेलर स्विफ़्ट ने अपने इस डर का किया ज़िक्र',
-            mediaType: 'video',
-            position: 2,
-            duration: 22000,
-            resourceId: 'urn:bbc:pips:pid:p0m6zysd',
-          },
-        });
-
-        expect(mockOptimizelyTrack).toHaveBeenCalledTimes(0);
-      });
-
-      it('should not send event to Optimizely when element is in view and optimizely experiment variant is set to "off"', async () => {
-        const {
-          result: { current: swipeTracker },
-        } = renderHook(() => useSwipeTracker(eventTrackingData), {
-          wrapper: props => wrapper({ ...props, atiData: atiAnalytics }),
-        });
-
-        await swipeTracker({
-          ...eventTrackingData,
-          sendOptimizelyEvents: true,
-          experimentName: 'dummy_experiment',
-          experimentVariant: 'off',
-          groupTracker: {
-            name: 'group name',
-            itemCount: 20,
-            resourceId:
-              'urn:bbc:tipo:list:fe4a1c8c-9a7c-4a50-845d-7da91aa65204',
-            position: 4,
-            type: 'portrait-video-modal',
-          },
-          itemTracker: {
-            type: 'portrait-video',
-            text: 'टेलर स्विफ़्ट ने अपने इस डर का किया ज़िक्र',
-            mediaType: 'video',
-            position: 2,
-            duration: 22000,
-            resourceId: 'urn:bbc:pips:pid:p0m6zysd',
-          },
-        });
-
-        expect(defaultOptimizely.track).toHaveBeenCalledTimes(0);
-      });
-
-      it('should not send event to Optimizely when element is in view and optimizely experiment variant is set to "off"', async () => {
-        const {
-          result: { current: swipeTracker },
-        } = renderHook(() => useSwipeTracker(eventTrackingData), {
-          wrapper: props => wrapper({ ...props, atiData: atiAnalytics }),
-        });
-
-        await swipeTracker({
-          ...eventTrackingData,
-          sendOptimizelyEvents: true,
-          experimentName: 'dummy_experiment',
-          experimentVariant: 'off',
-          groupTracker: {
-            name: 'group name',
-            itemCount: 20,
-            resourceId:
-              'urn:bbc:tipo:list:fe4a1c8c-9a7c-4a50-845d-7da91aa65204',
-            position: 4,
-            type: 'portrait-video-modal',
-          },
-          itemTracker: {
-            type: 'portrait-video',
-            text: 'टेलर स्विफ़्ट ने अपने इस डर का किया ज़िक्र',
-            mediaType: 'video',
-            position: 2,
-            duration: 22000,
-            resourceId: 'urn:bbc:pips:pid:p0m6zysd',
-          },
-        });
-
-        expect(defaultOptimizely.track).toHaveBeenCalledTimes(0);
-      });
     });
 
     describe('View tracking - Reverb', () => {
