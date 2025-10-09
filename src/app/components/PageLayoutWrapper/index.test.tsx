7d88dd83988587f64fcb9aa4d3b18d8d0cc489ef
Karina Thomas <karina.thomas@bbc.co.uk>
2025-10-09 20:59:13 +0100

    Add snapshot tests for font families for each service

--- src/app/components/PageLayoutWrapper/index.test.tsx
+++ src/app/components/PageLayoutWrapper/index.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import { render } from '../react-testing-library-with-providers';
+import { allServices } from '#app/routes/utils/regex';
+import { Services } from '#app/models/types/global';
+import { act, render } from '../react-testing-library-with-providers';
 import PageLayoutWrapper from '.';
 
 global.performance.getEntriesByName = jest.fn(() => []);
@@ -18,5 +20,31 @@ describe('PageLayoutWrapper', () => {
     );
 
     expect(container).toMatchSnapshot();
+  });
+
+  it.each(
+    allServices.filter(
+      service => !['newsround', 'ukrainian'].includes(service),
+    ) as Services[],
+  )('should render fonts for %s', async (service: Services) => {
+    await act(() =>
+      render(
+        <PageLayoutWrapper
+          status={200}
+          // @ts-expect-error - metadata type is mocked for test purposes
+          pageData={{ metadata: { type: 'test-page-type' } }}
+        />,
+        { service },
+      ),
+    );
+
+    const element = document.querySelector('[dir]');
+
+    // @ts-expect-error element will not be null
+    const style = window.getComputedStyle(element);
+
+    expect({
+      'font-family': style.getPropertyValue('font-family'),
+    }).toMatchSnapshot();
   });
 });
