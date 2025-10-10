73c55652eb5e73dd8da95251ac50fea7180e84a6
Karina Thomas <karina.thomas@bbc.co.uk>
2025-10-10 16:46:09 +0100

    Fix TS errors

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -707,7 +707,12 @@ const preview: Preview = {
           service={context.globals.service.service}
           variant={context.globals.service.variant}
         >
-          <RequestContextProvider isLite={context.globals.isLite}>
+          <RequestContextProvider
+            isLite={context.globals.isLite}
+            pageType={undefined}
+            pathname={`/${context.globals.service.service}`}
+            service={context.globals.service.service}
+          >
             <EventTrackingContextProvider
               // @ts-expect-error - mock data for Storybook
               pageData={pageDataFixture}
