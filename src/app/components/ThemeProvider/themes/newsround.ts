571f63404585026a9775943322df3f92d5392f25
Karina Thomas <karina.thomas@bbc.co.uk>
2025-10-13 23:12:54 +0100

    Move brand SVGs to the theme provider component & add test

--- src/app/components/ThemeProvider/themes/newsround.ts
+++ src/app/components/ThemeProvider/themes/newsround.ts
@@ -9,7 +9,7 @@ import {
 } from '../fontFaces';
 import reithFontVariants from '../fontVariants/reith';
 import withThemeProvider from '../withThemeProvider';
-import brandSVG from '../../../legacy/psammead/psammead-assets/src/svgs/newsround';
+import brandSVG from '../chameleonLogos/newsround';
 
 export const theme: ServiceTheme = {
   palette: {
--- src/app/components/ThemeProvider/themes/scotland.ts
+++ src/app/components/ThemeProvider/themes/scotland.ts
@@ -8,7 +8,7 @@ import {
 } from '../fontFaces';
 import reithFontVariants from '../fontVariants/reith';
 import withThemeProvider from '../withThemeProvider';
-import brandSVG from '../../../legacy/psammead/psammead-assets/src/svgs/scotland';
+import brandSVG from '../chameleonLogos/scotland';
 
 export const theme: ServiceTheme = {
   palette: {
