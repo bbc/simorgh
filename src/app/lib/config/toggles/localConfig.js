82ac51b52b2da80719a8f4ee05c73e961d0a2a12
Karina Thomas <karina.thomas@bbc.co.uk>
2025-10-13 09:04:11 +0100

    Use services exported from loadable config

--- src/app/lib/config/toggles/localConfig.js
+++ src/app/lib/config/toggles/localConfig.js
@@ -1,3 +1,5 @@
+import { services } from '../services/loadableConfig';
+
 export default {
   _environment: 'local',
   ads: {
@@ -15,8 +17,7 @@ export default {
   },
   enableFetchingToggles: {
     enabled: process?.env.FETCH_TOGGLES === 'true' || false,
-    value:
-      '(afaanoromoo|afrique|amharic|arabic|archive|azeri|bengali|burmese|cymrufyw|dari|gahuza|gujarati|hausa|hindi|igbo|indonesia|japanese|korean|kyrgyz|magyarul|marathi|mundo|naidheachdan|nepali|news|pashto|persian|pidgin|polska|portuguese|punjabi|romania|russian|scotland|serbian|sinhala|somali|swahili|tamil|telugu|thai|tigrinya|turkce|ukchina|ukrainian|urdu|uzbek|ws|vietnamese|yoruba|zhongwen)',
+    value: `(${services.join('|')})`,
   },
   eventTracking: {
     enabled: true,
