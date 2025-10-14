11974bf9f321f1d61cd8d552b391e6b1b758cfd3
Meriem M <150684501+MeriemMechri@users.noreply.github.com>
2025-10-14 15:21:35 +0100

    Delete cypress settings.js file (#13235)
    * refactor:remove /config/servicesimport
    
    * refactor:delete getPageUrls file and its refs
    
    * refactor:remove remaining /config/services refs
    
    * refactor:delete config and service files
    
    * refactor:update PAGES_WITHOUT_SERVICE_WORKER list
    
    * refactor:update PAGES_WITHOUT_SERVICE_WORKER list
    
    * refactor:update list name to PUBLIC_SERVICES
    
    * refactor:add manifest file for romania
    
    * refactor:romania.ts file to point to the new file
    
    * refactor:update romania.ts url

--- cypress/e2e/pages/liveRadio/tests.js
+++ cypress/e2e/pages/liveRadio/tests.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 
 import path from 'ramda/src/path';
-import config from '../../../support/config/services';
 import chartbeatTests from '../../../support/helpers/chartbeatTests';
 // For testing features that may differ across services but share a common logic e.g. translated strings.
 export default ({ service, pageType }) =>
@@ -34,7 +33,7 @@ export default ({ service, pageType }) =>
 
     describe('Radio Schedule', () => {
       it('should be displayed if there is enough schedule data', function test() {
-        cy.fixture(`toggles/${config[service].name}.json`).then(toggles => {
+        cy.fixture(`toggles/${service}.json`).then(toggles => {
           const scheduleIsEnabled = path(
             ['liveRadioSchedule', 'enabled'],
             toggles,
