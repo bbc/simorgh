78560f07a9b06c4d998446ada91370728ccd1f1c
Karina Thomas <karina.thomas@bbc.co.uk>
2025-10-18 10:10:14 +0100

    Add a special feature test to ensure that fonts are cached (in page layout wrapper)

---
+++ cypress/e2e/specialFeatures/fontCaching/assertions/index.ts
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable import/prefer-default-export */
+
+export const fontsAreCached = ({ expectedFonts }) => {
+  it(`${expectedFonts} fonts are cached`, () => {
+    cy.reload(true);
+
+    cy.getAllLocalStorage().then(allLocalStorage => {
+      expect(allLocalStorage).not.to.be.null;
+
+      const localStorage = allLocalStorage[Cypress.config().baseUrl];
+
+      const cachedFonts = Object.keys(localStorage)
+        .filter(key => key.startsWith('font-'))
+        .map(key => key.split('-')[1])
+        .filter(Boolean)
+        .sort();
+
+      cy.log(`Cached fonts: | ${cachedFonts.join(' | ')} | `);
+
+      expect(`${cachedFonts.join(' | ')}`).not.to.be.null;
+
+      expect(cachedFonts).to.include.members(expectedFonts);
+    });
+  });
+};
