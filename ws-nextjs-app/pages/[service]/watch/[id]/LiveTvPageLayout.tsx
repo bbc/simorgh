f2b91a232fdcf93220c4262e86efd764b5b2022e
Louise Archibald <louise.archibald@bbc.co.uk>
2025-10-21 16:56:24 +0100

    map over curations inside live tv page

--- ws-nextjs-app/pages/[service]/watch/[id]/LiveTvPageLayout.tsx
+++ ws-nextjs-app/pages/[service]/watch/[id]/LiveTvPageLayout.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import MediaLoader from '#app/components/MediaLoader';
+import { Curation } from '#app/models/types/curationData';
+import LiveTVCuration from '#app/components/Curation';
 import { LiveTVPageProps } from './types';
 // import ChartbeatAnalytics from '#app/components/ChartbeatAnalytics';
 // import ATIAnalytics from '#app/components/ATIAnalytics';
 // import MetadataContainer from '#app/components/Metadata';
 
-export default function LiveTvLayout({ pageType, service }: LiveTVPageProps) {
+export default function LiveTvLayout({
+  pageType,
+  service,
+  pageData,
+}: LiveTVPageProps) {
+  const { curations, mediaBlock } = pageData;
+  console.log('LiveTvLayout pageData:', pageData);
   return (
     <>
       {/* <ATIAnalytics atiData={atiAnalytics} />
@@ -20,6 +29,38 @@ export default function LiveTvLayout({ pageType, service }: LiveTVPageProps) {
         <h1 id="content">
           HELLO WORLD pageType: {pageType} service: {service}
         </h1>
+        {curations.map(
+          (
+            {
+              visualProminence,
+              summaries,
+              curationId,
+              title: curationTitle,
+              link,
+              position,
+              visualStyle,
+              ...curationProps
+            }: Curation,
+            index: number,
+          ) => {
+            return (
+              <React.Fragment key={`${curationId}-${position}`}>
+                <LiveTVCuration
+                  visualProminence=""
+                  summaries={summaries || []}
+                  title={curationTitle}
+                  position={position}
+                  link={link}
+                  curationLength={curations?.length}
+                  renderVisuallyHiddenH2Title={position === 0}
+                  curationId={curationId}
+                  {...curationProps}
+                />
+              </React.Fragment>
+            );
+          },
+        )}
+        <MediaLoader blocks={mediaBlock} />
       </main>
     </>
   );
