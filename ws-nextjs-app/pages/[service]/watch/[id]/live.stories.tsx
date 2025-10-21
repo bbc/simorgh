43d19740414fe0777a2e04c1faeda633af340981
Louise Archibald <louise.archibald@bbc.co.uk>
2025-10-21 09:33:35 +0100

    pass dari as a service to component

--- ws-nextjs-app/pages/[service]/watch/[id]/live.stories.tsx
+++ ws-nextjs-app/pages/[service]/watch/[id]/live.stories.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import PageLayoutWrapper from '#app/components/PageLayoutWrapper';
 import liveTvFixture from '#data/dari/watch/bbc_afghan_tv/live.json';
+import { ServiceContextProvider } from '#app/contexts/ServiceContext';
+import { Services } from '#app/models/types/global';
 import LiveTvLayout from './LiveTvPageLayout';
 import { LiveTVPageProps } from './types';
 
+const service = 'dari' as Services;
+
 const Component = ({ pageData }: LiveTVPageProps) => (
-  <PageLayoutWrapper pageData={pageData} status={200}>
-    {
-      // @ts-expect-error partial data required for storybook
-      <LiveTvLayout pageData={pageData} />
-    }
-  </PageLayoutWrapper>
+  <ServiceContextProvider service={service}>
+    <PageLayoutWrapper pageData={pageData} status={200}>
+      {
+        // @ts-expect-error partial data required for storybook
+        <LiveTvLayout pageData={pageData} />
+      }
+    </PageLayoutWrapper>
+  </ServiceContextProvider>
 );
 
 export default {
@@ -18,5 +24,7 @@ export default {
   Component,
 };
 
-// @ts-expect-error partial data required for storybook
-export const Example = () => <Component pageData={liveTvFixture.data} />;
+export const Example = () => (
+  // @ts-expect-error partial data required for storybook
+  <Component pageData={liveTvFixture.data} />
+);
