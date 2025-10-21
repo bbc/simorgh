02dcbbdf51fa679780eddcc05d1d0a961a316985
Santa Zena <santa.zena.ext@bbc.co.uk>
2025-10-21 15:03:25 +0300

    Styling banner

--- src/app/components/PWAUpsellBanner/PWAUpsellBanner.jsx
+++ src/app/components/PWAUpsellBanner/PWAUpsellBanner.jsx
@@ -1,33 +1,51 @@
 import React from 'react';
 import styles from './PWAUpsellBanner.styles';
+import { Close } from '../icons';
 
 export const PWAUpsellBanner = ({
   title,
   description,
-  handleClose,
   isDismissible = true,
   buttonOne,
   buttonTwo,
 }) => {
+  const handleClose = event => {
+    event.preventDefault();
+  };
   return (
     <div css={styles.ColoredContainer}>
       <div css={styles.Wrap}>
         <div css={styles.StyledContent}>
           <div css={styles.TextWrapper}>
-            <h1 css={styles.StyledTitle}>{title}</h1>
-            <p css={styles.StyledDescription}>{description}</p>
+            <h1 css={styles.StyledTitle} id="banner-title">
+              {title}
+            </h1>
+            <p css={styles.StyledDescription} id="banner-description">
+              {description}
+            </p>
           </div>
 
           <div css={styles.CTAWrapper}>
-            <button type="button">{buttonOne}</button>
-            <button type="button">{buttonTwo}</button>
+            <button type="button" css={styles.StyledButtonOne}>
+              {buttonOne}
+            </button>
+
+            <text css={styles.StyledText}>or</text>
+
+            <button type="button" css={styles.StyledButtonTwo}>
+              {buttonTwo}
+            </button>
           </div>
 
           {isDismissible && (
             <div css={styles.CloseButtonWrapper}>
-              <button type="button" onClick={handleClose}>
-                X
-              </button>
+              <a
+                aria-label="Close submenu"
+                css={styles.subNavCloseButton}
+                href="closeButton"
+                onClick={handleClose}
+              />
+              <Close css={styles.subNavCloseButtonIcon} />
             </div>
           )}
         </div>
--- src/app/components/PWAUpsellBanner/PWAUpsellBanner.styles.ts
+++ src/app/components/PWAUpsellBanner/PWAUpsellBanner.styles.ts
@@ -1,65 +1,124 @@
+import pixelsToRem from '#app/utilities/pixelsToRem';
 import { css, Theme } from '@emotion/react';
-// import {
-//   SPACING_2,
-//   SPACING_4,
-//   SPACING_6,
-//   fontStandard,
-//   fontScaleDescription,
-//   fontScaleIndexHeadlineLarge,
-//   fontHeadline,
-// } from '@bbc/web-gel-foundations';
 
 const styles = {
+  ColoredContainer: () =>
+    css({
+      position: 'relative',
+      margin: '0 auto',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      cursor: 'pointer',
+      zIndex: 1000,
+      backgroundColor: '#4D0B0B',
+      padding: `${pixelsToRem(16)}rem`,
+
+      width: '100%',
+      maxWidth: `${pixelsToRem(1008)}rem`,
+      minWidth: `${pixelsToRem(288)}rem`,
+    }),
+
   Wrap: () =>
     css({
       width: '100%',
     }),
 
-  ColoredContainer: ({ palette }: Theme) =>
-    css({
-      position: 'relative',
-      backgroundColor: palette.POSTBOX,
-    }),
-
   StyledContent: () =>
     css({
-      // ...fontStandard,
       display: 'grid',
       position: 'relative',
       width: '100%',
-      // rowGap: SPACING_6,
-      // padding: `${SPACING_6} 0 ${SPACING_4} 0`,
+    }),
+
+  TextWrapper: () =>
+    css({
+      display: 'grid',
     }),
 
   StyledTitle: () =>
     css({
-      // ...fontScaleIndexHeadlineLarge,
-      // ...fontHeadline,
+      color: '#F6F6F6',
+      height: `${pixelsToRem(32)}rem`, // change to correct height - Class/Line Height/Index Headline Large
+      weight: 700,
     }),
 
   StyledDescription: () =>
     css({
-      // ...fontScaleDescription,
-    }),
-
-  TextWrapper: () =>
-    // TextWrapper: ({ palette }: Theme) =>
-    css({
-      // color: palette.primary,
-      display: 'grid',
-      // rowGap: SPACING_2,
+      color: '#F6F6F6',
+      height: `${pixelsToRem(22)}rem`, // Class/Line Height/Index Headline Small
     }),
 
   CTAWrapper: () =>
     css({
       display: 'flex',
+      alignContent: 'center',
+      justifyContent: 'flex-start',
+      gap: `${pixelsToRem(12)}rem`,
+      lineHeight: 1,
     }),
 
-  CloseButtonWrapper: () =>
+  StyledButtonOne: ({ palette }: Theme) =>
+    css({
+      color: '#FFFFFF',
+      backgroundColor: palette.SERVICE_NEUTRAL_CORE,
+      border: 'none',
+      height: `${pixelsToRem(44)}rem`,
+      padding: `${pixelsToRem(12)}rem`, // change to correct
+    }),
+
+  StyledText: () =>
+    css({
+      color: '#F6F6F6',
+      fontSize: `${pixelsToRem(18)}rem`,
+      height: `${pixelsToRem(22)}rem`, // Class/Line Height/Index Headline Small
+      margin: 0,
+      alignText: 'center',
+    }),
+
+  StyledButtonTwo: () =>
+    css({
+      color: '#73B5FF',
+      backgroundColor: 'transparent',
+      border: 'none',
+      textDecoration: 'underline',
+      height: `${pixelsToRem(44)}rem`,
+      padding: `${pixelsToRem(12)}rem`, // change to correct
+    }),
+
+  subNavCloseButton: ({ palette }: Theme) =>
     css({
       position: 'absolute',
-      top: 0,
-      // right: `-${SPACING_2}`,
+      top: 0, // change to correct
+      right: 0, // change to correct
+      border: 'none',
+      background: 'transparent',
+      padding: 0,
+      cursor: 'pointer',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: `${pixelsToRem(44)}rem`,
+      height: `${pixelsToRem(44)}rem`,
+      color: palette.WHITE,
+      '&:hover, &:focus': {
+        outline: `${pixelsToRem(1)}rem solid ${palette.WHITE}`,
+        outlineOffset: `${pixelsToRem(1)}rem`,
+        backgroundColor: palette.POSTBOX,
+        fill: palette.WHITE,
+        color: palette.WHITE,
+      },
+    }),
+
+  subNavCloseButtonIcon: ({ palette }: Theme) =>
+    css({
+      position: 'absolute',
+      top: `${pixelsToRem(14)}rem`, // change to correct
+      right: `${pixelsToRem(14)}rem`, // change to correct
+      color: palette.WHITE,
+      fill: 'currentColor',
+      width: `${pixelsToRem(18)}rem`,
+      height: `${pixelsToRem(18)}rem`,
     }),
 };
 
