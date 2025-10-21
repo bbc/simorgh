957a6fabc04d4e9eb23eb7e96220726f960199a1
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2025-10-21 16:04:40 +0100

    WS-1280-extra: Fix audit name

--- lighthouse/lighthouserc.js
+++ lighthouse/lighthouserc.js
@@ -108,7 +108,7 @@ module.exports = {
           'error',
           { aggregationMethod: 'optimistic', minScore: 0.6 },
         ],
-        'uses-third-party-cookies': 'off',
+        'third-party-cookies': 'off',
       },
     },
     upload: {
--- scripts/lighthouseConfig.js
+++ scripts/lighthouseConfig.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: 'lighthouse:default',
   settings: {
     onlyCategories: ['accessibility', 'best-practices', 'seo'],
-    skipAudits: ['is-on-https', 'uses-third-party-cookies'],
+    skipAudits: ['is-on-https', 'third-party-cookies'],
   },
 };
