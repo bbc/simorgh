0d0570bfee4b5e24920b42d7195c9aae6a7dbfef
Louise Archibald <louise.archibald@bbc.co.uk>
2025-10-21 09:54:41 +0100

    pass pageType

--- ws-nextjs-app/pages/[service]/watch/[id]/live.stories.tsx
+++ ws-nextjs-app/pages/[service]/watch/[id]/live.stories.tsx
@@ -3,6 +3,8 @@ import PageLayoutWrapper from '#app/components/PageLayoutWrapper';
 import liveTvFixture from '#data/dari/watch/bbc_afghan_tv/live.json';
 import { ServiceContextProvider } from '#app/contexts/ServiceContext';
 import { Services } from '#app/models/types/global';
+import { RequestContextProvider } from '#app/contexts/RequestContext';
+import { LIVE_TV_PAGE } from '#app/routes/utils/pageTypes';
 import LiveTvLayout from './LiveTvPageLayout';
 import { LiveTVPageProps } from './types';
 
@@ -10,12 +12,18 @@ const service = 'dari' as Services;
 
 const Component = ({ pageData }: LiveTVPageProps) => (
   <ServiceContextProvider service={service}>
-    <PageLayoutWrapper pageData={pageData} status={200}>
-      {
-        // @ts-expect-error partial data required for storybook
-        <LiveTvLayout pageData={pageData} />
-      }
-    </PageLayoutWrapper>
+    <RequestContextProvider
+      pageType={LIVE_TV_PAGE}
+      pathname=""
+      service={service}
+    >
+      <PageLayoutWrapper pageData={pageData} status={200}>
+        {
+          // @ts-expect-error partial data required for storybook
+          <LiveTvLayout pageData={pageData} />
+        }
+      </PageLayoutWrapper>
+    </RequestContextProvider>
   </ServiceContextProvider>
 );
 
