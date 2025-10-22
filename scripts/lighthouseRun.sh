d19b5b7e569bccd0fdf5122cf5f80af1c46777ca
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2025-10-22 09:25:35 +0100

    WS-1280: Change asset

--- scripts/lighthouseRun.sh
+++ scripts/lighthouseRun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-lighthouse http://localhost:7081/pidgin/articles/cwl08rd38l6o --chrome-flags="--no-sandbox --headless --disable-gpu" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
+lighthouse http://localhost:7081/pidgin/articles/ce9wk6glg4lo --chrome-flags="--no-sandbox --headless --disable-gpu" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
 lighthouse http://localhost:7080/hindi --chrome-flags="--no-sandbox --headless --disable-gpu" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
 lighthouse http://localhost:7081/pidgin/live/c7p765ynk9qt --chrome-flags="--no-sandbox --headless --disable-gpu" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
 lighthouse http://localhost:7081/somali/send/u130092370 --chrome-flags="--no-sandbox --headless --disable-gpu" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
