18866c56abaeaea856f6284bde55ca99143f14aa
Aaron Moore <aaron.moore@bbc.co.uk>
2025-10-23 10:19:53 +0100

    Remove manual `react` types from `tsconfig`

--- ws-nextjs-app/jest.config.ts
+++ ws-nextjs-app/jest.config.ts
@@ -3,8 +3,7 @@ import { pathsToModuleNameMapper } from 'ts-jest';
 import type { Config } from '@jest/types';
 import { compilerOptions } from '../tsconfig.json';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const { react, ...compilerOptionsPaths } = compilerOptions.paths;
+const { ...compilerOptionsPaths } = compilerOptions.paths;
 
 const canonicalIntegrationTests = {
   displayName: 'Integration Tests - Canonical',
