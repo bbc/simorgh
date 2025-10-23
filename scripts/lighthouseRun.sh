633110c3e3dd7ac77ef2daa459797636326d447f
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2025-10-23 13:53:50 +0100

    WS-1280: Attempt upload lighthouse artifact on github workflow

--- scripts/lighthouseRun.sh
+++ scripts/lighthouseRun.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 set -e
-lighthouse http://localhost:7081/pidgin/articles/ce9wk6glg4lo --chrome-flags="--no-sandbox --headless --disable-gpu" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
-lighthouse http://localhost:7080/hindi --chrome-flags="--no-sandbox --headless --disable-gpu" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
-lighthouse http://localhost:7081/pidgin/live/c7p765ynk9qt --chrome-flags="--no-sandbox --headless --disable-gpu" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
-lighthouse http://localhost:7081/somali/send/u130092370 --chrome-flags="--no-sandbox --headless --disable-gpu" --output json --output html --output-path simorgh --config-path scripts/lighthouseConfig.js && node scripts/lighthouseBudget.js run
+
+mkdir -p lighthouse-reports
+
+lighthouse http://localhost:7081/pidgin/articles/ce9wk6glg4lo \
+  --chrome-flags="--no-sandbox --headless --disable-gpu" \
+  --output json --output html \
+  --output-path lighthouse-reports/pidgin-article \
+  --config-path scripts/lighthouseConfig.js && \
+  node scripts/lighthouseBudget.js run
+lighthouse http://localhost:7080/hindi \
+  --chrome-flags="--no-sandbox --headless --disable-gpu" \
+  --output json --output html \
+  --output-path lighthouse-reports/hindi-home \
+  --config-path scripts/lighthouseConfig.js && \
+  node scripts/lighthouseBudget.js run
+
+lighthouse http://localhost:7081/pidgin/live/c7p765ynk9qt \
+  --chrome-flags="--no-sandbox --headless --disable-gpu" \
+  --output json --output html \
+  --output-path lighthouse-reports/pidgin-live \
+  --config-path scripts/lighthouseConfig.js && \
+  node scripts/lighthouseBudget.js run
+
+lighthouse http://localhost:7081/somali/send/u130092370 \
+  --chrome-flags="--no-sandbox --headless --disable-gpu" \
+  --output json --output html \
+  --output-path lighthouse-reports/somali-send \
+  --config-path scripts/lighthouseConfig.js && \
+  node scripts/lighthouseBudget.js run
