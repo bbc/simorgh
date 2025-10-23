95f5997f9ba1707f3b87ef95022cac5cf1208dd2
Louise Archibald <louise.archibald@bbc.co.uk>
2025-10-23 10:56:27 +0100

    reinstate metadata page type

--- ws-nextjs-app/pages/[service]/watch/[id]/live.page.tsx
+++ ws-nextjs-app/pages/[service]/watch/[id]/live.page.tsx
@@ -19,6 +19,10 @@ export const getServerSideProps: GetServerSideProps = async context => {
     timeOnServer: Date.now(),
     pageType: LIVE_TV_PAGE as PageTypes,
     service,
+    metadata: {
+      type: LIVE_TV_PAGE,
+      atiAnalytics: {},
+    },
     pathname: context?.resolvedUrl,
   };
   return {
