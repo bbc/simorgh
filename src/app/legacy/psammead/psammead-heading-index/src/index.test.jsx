5edcc3439b0590c6748d09dc8b85821d12981093
Aaron Moore <aaron.moore@bbc.co.uk>
2025-10-23 08:58:51 +0100

    Update index.test.jsx

--- src/app/legacy/psammead/psammead-heading-index/src/index.test.jsx
+++ src/app/legacy/psammead/psammead-heading-index/src/index.test.jsx
@@ -6,7 +6,7 @@ import HeadingIndex from './index';
 describe('Index Heading', () => {
   it('should render correctly', () => {
     const { container } = render(
-      <HeadingIndex script={latin} service="news">
+      <HeadingIndex script={latin} service="news" tabIndex={-1}>
         This is a page heading
       </HeadingIndex>,
     );
@@ -15,7 +15,7 @@ describe('Index Heading', () => {
 
   it('should render correctly with arabic script typography values', () => {
     const { container } = render(
-      <HeadingIndex script={arabic} service="persian">
+      <HeadingIndex script={arabic} service="persian" tabIndex={-1}>
         هذا عنوان الصفحة
       </HeadingIndex>,
     );
