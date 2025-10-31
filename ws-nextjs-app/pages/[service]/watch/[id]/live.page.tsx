8768d91439960561035d1b6539ed028cde6d150b
Louise Archibald <louise.archibald@bbc.co.uk>
2025-10-31 11:32:27 +0000

    updates page params and adds getPageData fetch

--- ws-nextjs-app/pages/[service]/watch/[id]/live.page.tsx
+++ ws-nextjs-app/pages/[service]/watch/[id]/live.page.tsx
@@ -3,6 +3,7 @@ import { GetServerSideProps } from 'next';
 import { LIVE_TV_PAGE } from '#app/routes/utils/pageTypes';
 import PageDataParams from '#app/models/types/pageDataParams';
 import { PageTypes } from '#app/models/types/global';
+import getPageData from '#nextjs/utilities/pageRequests/getPageData';
 
 const LiveTvLayout = dynamic(() => import('./LiveTvPageLayout'));
 
@@ -11,13 +12,27 @@ export const getServerSideProps: GetServerSideProps = async context => {
     'Cache-Control',
     'public, stale-if-error=300, stale-while-revalidate=120, max-age=30',
   );
-  const { service } = context.query as PageDataParams;
+  const {
+    id,
+    service,
+    renderer_env: rendererEnv,
+  } = context.query as PageDataParams;
+
+  const { data, toggles } = await getPageData({
+    id,
+    service,
+    rendererEnv,
+    resolvedUrl: context.resolvedUrl,
+    pageType: LIVE_TV_PAGE,
+  });
+
   const baseProps = {
     isAmp: false,
     isNextJs: true,
-    status: 200,
+    status: data.status,
     pageType: LIVE_TV_PAGE as PageTypes,
     service,
+    toggles,
     pageData: {
       metadata: {
         type: LIVE_TV_PAGE,
