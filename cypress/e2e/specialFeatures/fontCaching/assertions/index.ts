1de0a7e7c10a14b85a15e7467f02811627fa2b74
Karina Thomas <karina.thomas@bbc.co.uk>
2025-11-04 09:31:46 +0000

    Stabilise tests

--- cypress/e2e/specialFeatures/fontCaching/assertions/index.ts
+++ cypress/e2e/specialFeatures/fontCaching/assertions/index.ts
@@ -1,31 +1,11 @@
-/* eslint-disable cypress/no-unnecessary-waiting */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable import/prefer-default-export */
 
-const interceptFontDownload = () => {
-  cy.intercept(
-    {
-      url: /https:\/\/static\.files\.bbci\.co\.uk(?:\/fonts\/|\/ws\/simorgh-assets\/public\/fonts\/)/,
-    },
-    request => {
-      request.reply({ statusCode: 200 });
-    },
-  ).as('font-download');
-};
-
-export const fontsAreCached = ({ expectedFonts, path }) => {
+export const fontsAreCached = ({ expectedFonts }) => {
   const testPrefix = expectedFonts.length > 0 ? expectedFonts : 'No ';
 
   it(`${testPrefix} fonts are cached`, () => {
-    interceptFontDownload();
-    cy.visit(path);
     cy.reload(true);
-
-    expectedFonts.forEach(() => {
-      cy.wait('@font-download')
-        .its('response')
-        .should('have.property', 'statusCode', 200);
-    });
 
     cy.getAllLocalStorage().then(allLocalStorage => {
       expect(allLocalStorage).not.to.be.null;
