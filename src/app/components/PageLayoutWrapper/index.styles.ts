06ff94812bc028afee7daa9068ec43a04cb33244
Louise Archibald <louise.archibald@bbc.co.uk>
2025-11-04 11:11:57 +0000

    revert change to PageLayoutWrapper styles

--- src/app/components/PageLayoutWrapper/index.styles.ts
+++ src/app/components/PageLayoutWrapper/index.styles.ts
@@ -1,13 +1,13 @@
 import { css, Theme } from '@emotion/react';
 
 export default {
-  wrapper: ({ isDarkUi, palette }: Theme) =>
+  wrapper: ({ palette }: Theme) =>
     css({
       minHeight: '100vh',
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'space-between',
-      backgroundColor: isDarkUi ? palette.GREY_10 : palette.GHOST,
+      backgroundColor: palette.GHOST,
     }),
   content: css({
     flexGrow: 1,
