44a78a56cf02953089481ac0280f8b6fbb0c236e
skumid01 <dmytro.skumin.ext@bbc.co.uk>
2025-11-04 13:47:28 +0200

    WS-1587 | updating text and pic for meta

--- src/app/lib/config/services/ws.ts
+++ src/app/lib/config/services/ws.ts
@@ -18,8 +18,8 @@ export const service: DefaultServiceConfig = {
     serviceLocalizedName: 'World Service',
     product: 'BBC News',
     defaultImage:
-      'https://static.files.bbci.co.uk/ws/simorgh-assets/public/news/images/metadata/poster-1200x630.png',
-    defaultImageAltText: 'BBC World Service Image',
+      'https://static.files.bbci.co.uk/ws/simorgh-assets/public/ws/images/metadata/poster-1024x576.png',
+    defaultImageAltText: 'BBC World Service',
     dir: 'ltr',
     externalLinkText: ', external',
     imageCaptionOffscreenText: 'Image caption, ',
