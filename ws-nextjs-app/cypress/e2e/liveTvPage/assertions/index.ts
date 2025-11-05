d57fde63b05c09ccdb33a241cee3ab766f2ac91d
Louise Archibald <louise.archibald@bbc.co.uk>
2025-11-05 11:05:52 +0000

    update test to avoid hardcoding a title and check pageData instead

--- ws-nextjs-app/cypress/e2e/liveTvPage/assertions/index.ts
+++ ws-nextjs-app/cypress/e2e/liveTvPage/assertions/index.ts
@@ -3,12 +3,15 @@ import { PageTypes, Services } from '#app/models/types/global';
 type LiveTvPageParams = {
   service: Services;
   pageType: PageTypes;
+  pageData: {
+    title: string;
+  };
 };
 
 // eslint-disable-next-line import/prefer-default-export
-export const assertLiveTvPage = ({ service }: LiveTvPageParams) => {
+export const assertLiveTvPage = ({ service, pageData }: LiveTvPageParams) => {
   it(`should render the Live TV Page with the correct h1 for ${service}`, () => {
-    const expectedHeading = 'Live TV Page with schedule - Dari';
+    const expectedHeading = pageData?.title || '';
     cy.get('h1').should('have.text', expectedHeading);
   });
 };
