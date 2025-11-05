834066479f4863df89df532aa102949d630c341b
Karina Thomas <karina.thomas@bbc.co.uk>
2025-11-05 12:03:06 +0000

    Use correct months for Dari

--- src/app/legacy/psammead/psammead-locales/moment/fa-af.js
+++ src/app/legacy/psammead/psammead-locales/moment/fa-af.js
@@ -2,18 +2,18 @@ const moment = require('moment');
 
 // eslint-disable-next-line import/prefer-default-export
 export const months = [
-  'حمل', // January
-  'ثور', // February
-  'جوزا', // March
-  'سرطان', // April
-  'اسد', // May
-  'سنبله', // June
-  'میزان', // July
-  'عقرب', // August
-  'قوس', // September
-  'جدی', // October
-  'دلو', // November
-  'حوت', // December
+  'جنوری', // January
+  'فبروری', // February
+  'مارچ', // March
+  'اپریل', // April
+  'می', // May
+  'جون', // June
+  'جولای', // July
+  'آگست', // August
+  'سپتمبر', // September
+  'اکتوبر', // October
+  'نوامبر', // November
+  'دسامبر', // December
 ];
 
 moment.defineLocale('fa-af', {
