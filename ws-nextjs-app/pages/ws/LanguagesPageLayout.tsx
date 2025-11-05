3080f38e97866229dd47de030010e70cf6457463
skumid01 <dmytro.skumin.ext@bbc.co.uk>
2025-11-05 16:09:27 +0200

    WS-1587 | updating text and pic for meta

--- ws-nextjs-app/pages/ws/LanguagesPageLayout.tsx
+++ ws-nextjs-app/pages/ws/LanguagesPageLayout.tsx
@@ -28,8 +28,9 @@ const languageToggle = (e: React.MouseEvent<HTMLButtonElement>) => {
   }
 };
 
-const pageTitle = 'News in your language - BBC World Service';
-const pageDescription = 'A list of BBC World Service language services';
+const pageTitle = 'BBC World Service – BBC News in 43 Languages Worldwide';
+const pageDescription =
+  'Broadcasting trusted BBC News and programmes in 43 languages worldwide - on radio, TV, apps, and digital platforms.';
 const lang = 'en';
 
 const atiAnalytics = {
