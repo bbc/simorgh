423549b72b660b8f226c54aa6d1a83a8b5e57693
eagerterrier <eagerterrier@gmail.com>
2025-11-06 14:20:42 +0000

    FIX: storybook

--- src/app/components/Weather/index.stories.tsx
+++ src/app/components/Weather/index.stories.tsx
@@ -1,49 +1,18 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
 import Weather from './index';
 
-const meta: Meta<typeof Weather> = {
-  title: 'Weather/Weather',
-  component: Weather,
-  argTypes: {
-    locationId: {
-      control: 'text',
-      description: 'Location ID for the weather forecast',
-      defaultValue: '2653822',
-    },
-    className: {
-      control: 'text',
-      description: 'Custom class name for styling',
-    },
-    datetimeLocale: {
-      control: 'text',
-      description: 'Locale string used for date/time formatting',
-      defaultValue: 'en-GB',
-    },
-  },
+export default {
+  title: 'Components/Weather',
 };
 
-export default meta;
+export const Default = () => (
+  <Weather locationId="2653822" datetimeLocale="en-GB" />
+);
 
-type Story = StoryObj<typeof Weather>;
+export const LondonWeather = () => (
+  <Weather locationId="2643743" datetimeLocale="en-GB" />
+);
 
-export const Default: Story = {
-  args: {
-    locationId: '2653822',
-    datetimeLocale: 'en-GB',
-    className: '',
-  },
-};
-
-export const LondonWeather: Story = {
-  args: {
-    locationId: '2643743', // London
-    datetimeLocale: 'en-GB',
-  },
-};
-
-export const ParisWeather: Story = {
-  args: {
-    locationId: '2988507', // Paris
-    datetimeLocale: 'fr-FR',
-  },
-};
+export const ParisWeather = () => (
+  <Weather locationId="2988507" datetimeLocale="fr-FR" />
+);
