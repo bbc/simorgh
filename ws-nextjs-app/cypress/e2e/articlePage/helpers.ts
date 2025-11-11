52bc5919c4dc8f7cb10482f3a19707335d26a13c
Aaron Moore <aaron.moore@bbc.co.uk>
2025-11-11 10:32:16 +0000

    WS-1583 - Move continue reading button `beforeEach` handler to global `beforeEach` (#13398)

--- ws-nextjs-app/cypress/e2e/articlePage/helpers.ts
+++ ws-nextjs-app/cypress/e2e/articlePage/helpers.ts
@@ -56,21 +56,3 @@ export const getVideoEmbedUrl = (
 
   return isAmp ? `${embedUrl}/amp` : embedUrl;
 };
-
-export const handleContinueReadingButton = () => {
-  const CONTINUE_READING_BUTTON_ID = '#continue-reading-button';
-
-  cy.get('body').then($body => {
-    if ($body.find(CONTINUE_READING_BUTTON_ID).length > 0) {
-      cy.get(CONTINUE_READING_BUTTON_ID).then($button => {
-        const buttonDisplay = window
-          .getComputedStyle($button[0])
-          .getPropertyValue('display');
-
-        if (buttonDisplay !== 'none') {
-          cy.get(CONTINUE_READING_BUTTON_ID).click();
-        }
-      });
-    }
-  });
-};
