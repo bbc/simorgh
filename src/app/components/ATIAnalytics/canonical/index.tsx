45ab59d3b805066f5e3cb415c924f785c51780cf
alex-magana <alex.magana@andela.com>
2025-11-11 17:49:54 +0300

    Set pageviewParams to default to an empty string

--- src/app/components/ATIAnalytics/canonical/index.tsx
+++ src/app/components/ATIAnalytics/canonical/index.tsx
@@ -41,7 +41,7 @@ const addScript = ({ script, parameters, nonce }: InlineScriptProps) => {
 };
 
 const CanonicalATIAnalytics = ({
-  pageviewParams,
+  pageviewParams = '',
   reverbParams,
 }: ATIAnalyticsProps) => {
   const { isLite, nonce } = use(RequestContext);
