d5c84a4db6d20ee726c7b60687884e61fb9bacfa
Shayne Ahchoon <shayneah@googlemail.com>
2025-11-12 15:45:53 +0000

    WS-1533: Update packages with 7 day maturity threshold

--- src/app/pages/ArticlePage/ArticlePage.tsx
+++ src/app/pages/ArticlePage/ArticlePage.tsx
@@ -49,7 +49,7 @@ import { Recommendation } from '#app/models/types/onwardJourney';
 import ScrollablePromo from '#components/ScrollablePromo';
 import Recommendations from '#app/components/Recommendations';
 import { ReadTimeArticleExperiment as ReadTime } from '#app/components/ReadTime';
-// import LiteRedirect from '#app/components/LiteRedirect';
+import LiteRedirect from '#app/components/LiteRedirect';
 import ElectionBanner from './ElectionBanner';
 import ImageWithCaption from '../../components/ImageWithCaption';
 import AdContainer from '../../components/Ad';
@@ -83,7 +83,7 @@ import {
   isPortraitVideoUnderHeadline,
 } from '../utils/portraitVideo';
 
-// EXPERIMENT: Article Read Time 2
+// EXPERIMENT: Article Read Time
 interface ReadTimeData {
   readTimeValue: number | undefined;
   readTimeVariant: string;
@@ -98,7 +98,7 @@ const getImageComponent =
     />
   );
 
-// EXPERIMENT: Article Read Time 2
+// EXPERIMENT: Article Read Time
 const Placeholder = ({ className }: { className?: string }) => {
   const { service } = use(ServiceContext);
   const servicesInExperiment = ['']; // adding services will show placeholder regardless of whether experiment is running
@@ -107,7 +107,7 @@ const Placeholder = ({ className }: { className?: string }) => {
   ) : null;
 };
 
-// EXPERIMENT: Article Read Time 2
+// EXPERIMENT: Article Read Time
 const getTimestampComponent =
   (
     hasByline: boolean,
@@ -117,7 +117,7 @@ const getTimestampComponent =
     readTimeData: ReadTimeData,
   ) =>
   (props: ComponentToRenderProps & TimeStampProps) => {
-    // EXPERIMENT: Article Read Time 2
+    // EXPERIMENT: Article Read Time
     const { readTimeValue, readTimeVariant } = readTimeData;
     const isReadTimeVariantValid = readTimeVariant !== 'off' && readTimeVariant;
     const showReadTimeBelowTimestamp =
@@ -150,7 +150,7 @@ const getTimestampComponent =
           popOut={false}
           showReadTimeBelowTimestamp={showReadTimeBelowTimestamp}
         />
-        {/* EXPERIMENT: Article Read Time 2 */}
+        {/* EXPERIMENT: Article Read Time */}
         {showReadTimeBelowTimestamp ? (
           <ReadTime
             readTimeValue={readTimeValue}
@@ -233,8 +233,8 @@ const ArticlePage = ({ pageData }: { pageData: Article }) => {
     palette: { GREY_2, WHITE },
   } = useTheme();
 
-  // EXPERIMENT: Article Read Time 2
-  const readTimeExperimentName = 'newswb_ws_article_read_time_2';
+  // EXPERIMENT: Article Read Time
+  const readTimeExperimentName = 'newswb_ws_article_read_time';
   const readTimeExperimentVariant = useOptimizelyVariation({
     experimentName: readTimeExperimentName,
     experimentType: ExperimentType.CLIENT_SIDE,
@@ -257,7 +257,7 @@ const ArticlePage = ({ pageData }: { pageData: Article }) => {
 
   const { enabled: podcastPromoEnabled } = useToggle('podcastPromo');
 
-  // EXPERIMENT: Article Read Time 2
+  // EXPERIMENT: Article Read Time
   const readTimeValue = pageData?.metadata?.stats?.readTime;
 
   const headline = getHeadline(pageData) ?? '';
@@ -295,7 +295,6 @@ const ArticlePage = ({ pageData }: { pageData: Article }) => {
   const atiData = {
     ...atiAnalytics,
     ...(isCPS && { pageTitle: `${atiAnalytics.pageTitle} - ${brandName}` }),
-    // EXPERIMENT: Article Read Time 2
     // Better way to handle this?
     ...(readTimeExperimentVariant &&
       readTimeExperimentVariant !== 'off' && {
@@ -309,7 +308,7 @@ const ArticlePage = ({ pageData }: { pageData: Article }) => {
       }),
   };
 
-  // EXPERIMENT: Article Read Time 2
+  // EXPERIMENT: Article Read Time
   const readTimeData = {
     readTimeValue,
     readTimeVariant: readTimeExperimentVariant || 'off',
@@ -335,7 +334,7 @@ const ArticlePage = ({ pageData }: { pageData: Article }) => {
     video: getVideoComponent(translations, blocks),
     text,
     image: getImageComponent(preloadLeadImageToggle),
-    // EXPERIMENT: Article Read Time 2
+    // EXPERIMENT: Article Read Time
     timestamp: getTimestampComponent(
       hasByline,
       bylineContribBlocks,
@@ -392,7 +391,7 @@ const ArticlePage = ({ pageData }: { pageData: Article }) => {
 
   return (
     <div css={styles.pageWrapper}>
-      {/* <LiteRedirect /> */}
+      <LiteRedirect />
       <ATIAnalytics atiData={atiData} />
       <ChartbeatAnalytics
         sectionName={pageData?.relatedContent?.section?.name}
