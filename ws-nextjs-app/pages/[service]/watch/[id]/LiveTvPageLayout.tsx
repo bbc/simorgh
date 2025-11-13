6e4a54513d339a4219494ebca8212ee66adf49ff
Louise Archibald <louise.archibald@bbc.co.uk>
2025-11-13 16:32:49 +0000

    pass ati analytics to live tv page

--- ws-nextjs-app/pages/[service]/watch/[id]/LiveTvPageLayout.tsx
+++ ws-nextjs-app/pages/[service]/watch/[id]/LiveTvPageLayout.tsx
@@ -7,10 +7,10 @@ import Heading from '#app/components/Heading';
 import Text from '#app/components/Text';
 import MetadataContainer from '#app/components/Metadata';
 import { ServiceContext } from '#app/contexts/ServiceContext';
+import ATIAnalytics from '#app/components/ATIAnalytics';
 import { LiveTVPageProps } from './types';
 import styles from './styles';
 // import ChartbeatAnalytics from '#app/components/ChartbeatAnalytics';
-// import ATIAnalytics from '#app/components/ATIAnalytics';
 
 const renderCuration = ({ curation }: { curation: CurationType }) => {
   const {
@@ -45,7 +45,12 @@ const getSynopses = (
 
 export default function LiveTvLayout({ pageData }: LiveTVPageProps) {
   const { lang } = use(ServiceContext);
-  const { curations, description, title } = pageData;
+  const {
+    curations,
+    description,
+    title,
+    metadata: { atiAnalytics },
+  } = pageData;
 
   const mediaCollectionCuration = curations?.find(
     curation => curation.mediaCollection,
@@ -61,7 +66,7 @@ export default function LiveTvLayout({ pageData }: LiveTVPageProps) {
 
   return (
     <div css={styles.pageWrapper}>
-      {/* <ATIAnalytics atiData={atiAnalytics} /> */}
+      <ATIAnalytics atiData={atiAnalytics} />
       {/* <ChartbeatAnalytics title={pageTitle} /> */}
       <MetadataContainer
         title={title}
