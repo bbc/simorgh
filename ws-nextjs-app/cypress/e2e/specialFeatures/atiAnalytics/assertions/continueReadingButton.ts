4e4053c10e374f6255b9f3b86ea37f248907b6a1
Aaron Moore <aaron.moore@bbc.co.uk>
2025-11-18 10:17:27 +0000

    WS-1619 - Continue reading + Top Bar OJ Cypress e2es

---
+++ ws-nextjs-app/cypress/e2e/specialFeatures/atiAnalytics/assertions/continueReadingButton.ts
@@ -0,0 +1,59 @@
+/* eslint-disable import/no-relative-packages */
+import { interceptATIAnalyticsBeacons, COMPONENTS } from '../helpers';
+import {
+  assertATIComponentClickEvent,
+  assertATIComponentViewEvent,
+} from '../../../../../../cypress/e2e/specialFeatures/atiAnalytics/assertions';
+import { AtiAssertionFnProps } from './type';
+
+const { CONTINUE_READING_BUTTON } = COMPONENTS;
+
+export const assertContinueReadingButtonComponentView = ({
+  pageIdentifier,
+  contentType,
+  siteId,
+}: AtiAssertionFnProps) => {
+  it('should send a view event for the Continue Reading Button component', () => {
+    interceptATIAnalyticsBeacons();
+
+    // Reload the page to show the continue reading button again
+    cy.reload();
+
+    cy.get('[data-testid="continue-reading-button"]').scrollIntoView({
+      duration: 1000,
+    });
+
+    assertATIComponentViewEvent({
+      component: CONTINUE_READING_BUTTON,
+      pageIdentifier,
+      contentType,
+      siteId,
+    });
+  });
+};
+
+export const assertContinueReadingButtonComponentClick = ({
+  pageIdentifier,
+  contentType,
+  siteId,
+}: AtiAssertionFnProps) => {
+  it('should send a click event for the Continue Reading Button component', () => {
+    interceptATIAnalyticsBeacons();
+
+    // Reload the page to show the continue reading button again
+    cy.reload();
+
+    cy.get('[data-testid="continue-reading-button"]').scrollIntoView({
+      duration: 1000,
+    });
+
+    cy.get('[data-testid="continue-reading-button"]').click({ force: true });
+
+    assertATIComponentClickEvent({
+      component: CONTINUE_READING_BUTTON,
+      pageIdentifier,
+      contentType,
+      siteId,
+    });
+  });
+};
