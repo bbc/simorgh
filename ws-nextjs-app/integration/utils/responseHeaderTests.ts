dab659a8ffdbf3e06d6b131238a6ced352085f65
Aaron Moore <aaron.moore@bbc.co.uk>
2025-11-20 13:09:32 +0000

    Update responseHeaderTests.ts

--- ws-nextjs-app/integration/utils/responseHeaderTests.ts
+++ ws-nextjs-app/integration/utils/responseHeaderTests.ts
@@ -1,4 +1,6 @@
 export default () => {
+  // This header is set in the Next.js middleware/proxy file: 'ws-nextjs-app/middleware.ts'
+  // The presence of this header and its value including the word 'SIMORGH' indicates that middleware/proxy is working correctly
   describe('req-svc-chain is set correctly', () => {
     it('should contain the correct svc chain', async () => {
       const fetchResponse = await fetch(window.location.href);
