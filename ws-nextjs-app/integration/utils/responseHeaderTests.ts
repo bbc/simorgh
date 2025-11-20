ab0ce62dc0bd84107013809d083be3349143e896
Aaron Moore <aaron.moore@bbc.co.uk>
2025-11-20 13:02:36 +0000

    Update responseHeaderTests.ts

--- ws-nextjs-app/integration/utils/responseHeaderTests.ts
+++ ws-nextjs-app/integration/utils/responseHeaderTests.ts
@@ -3,7 +3,6 @@ export default () => {
     it('should contain the correct svc chain', async () => {
       const fetchResponse = await fetch(window.location.href);
       const reqSvcChain = fetchResponse.headers.get('req-svc-chain');
-      console.log('req-svc-chain:', reqSvcChain);
       expect(reqSvcChain).toContain('SIMORGH');
     });
   });
