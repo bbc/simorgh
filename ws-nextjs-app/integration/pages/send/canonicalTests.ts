a2af715293db0e782b809a0b2475957d0c6b391f
Aaron Moore <aaron.moore@bbc.co.uk>
2025-11-20 15:20:28 +0000

    Add `req-svc-chain` integration test (#13449)
    * Add `req-svc-chain` integration test
    
    * Update responseHeaderTests.ts
    
    * Add to articles `crossPlatformTests`
    
    * Add to av-embeds `canonicalTests` + convert to TS
    
    * Add to mediaArticlePage `crossPlatformTests`
    
    * Add to photoGallery `crossPlatformTests`
    
    * Add to send tests + update to TS
    
    * Add to storyPage `crossPlatformTests`
    
    * Update responseHeaderTests.ts
    
    * Add to Live pages
    
    * Remove optional chain in favour of non-null assertion
    
    * Pass type to `document.querySelector` rather than casting

--- ws-nextjs-app/integration/pages/send/canonicalTests.ts
+++ ws-nextjs-app/integration/pages/send/canonicalTests.ts
@@ -2,11 +2,13 @@ import {
   runHeaderTests,
   runA11yTests,
   runFooterTests,
-} from '../../../../src/integration/common';
+} from '#src/integration/common';
+import runResponseHeaderTests from '#nextjs/integration/utils/responseHeaderTests';
 
 import ugcFormTest from './ugcFormTests';
 
 export default () => {
+  runResponseHeaderTests();
   runHeaderTests();
   runA11yTests();
   runFooterTests();
