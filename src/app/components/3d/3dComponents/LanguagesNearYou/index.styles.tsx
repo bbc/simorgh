99053ce705f900b098fa809597080983199807d9
Louis Matsika <louis.matsika@bbc.co.uk>
2025-11-21 14:08:33 +0000

    remove: unused styles

--- src/app/components/3d/3dComponents/LanguagesNearYou/index.styles.tsx
+++ src/app/components/3d/3dComponents/LanguagesNearYou/index.styles.tsx
@@ -21,29 +21,4 @@ export default {
       width: '100%',
       height: '100%',
     }),
-  infoBox: ({ palette }: Theme) =>
-    css({
-      backgroundColor: `${palette.GREY_5} !important`,
-      width: '220px',
-      height: '100px',
-      padding: '12px',
-      display: 'flex',
-      alignItems: 'center',
-      gap: '8px',
-      position: 'absolute',
-      bottom: '16px',
-      right: '16px',
-      zIndex: 10,
-      boxSizing: 'border-box',
-    }),
-  infoIcon: ({ palette }: Theme) =>
-    css({
-      color: `${palette.WHITE} !important`,
-      '& svg': {
-        width: '20px',
-        height: '20px',
-        color: `${palette.WHITE} !important`,
-        fill: `${palette.WHITE} !important`,
-      },
-    }),
 };
