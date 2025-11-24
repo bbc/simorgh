501c8cc562c0b6ca7fb11a87d35af9aedf798bc6
Shayne Ahchoon <shayneah@googlemail.com>
2025-11-24 10:27:18 +0000

    WS-1737: Add tracking for redirect

--- src/server/utilities/CanonicalToLiteRedirect/index.tsx
+++ src/server/utilities/CanonicalToLiteRedirect/index.tsx
@@ -23,7 +23,7 @@ export const redirectScript = (window: Window) => {
   }
 };
 
-// THIS COMPONENT IS ONLY TO BE USED WITH CANONICAL REDERERS
+// THIS COMPONENT IS ONLY TO BE USED WITH CANONICAL RENDERERS
 // DO NOT USE IT WITH LITE AND AMP RENDERERS
 export default () => {
   return (
