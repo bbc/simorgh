a6f3dd593aa62f06a949002450eb15658e5888d5
Lukas Freimonas <lukas.freimonas.ext@bbc.co.uk>
2025-11-25 14:26:32 +0200

    remove textUnderlineOffset

--- src/app/components/PromotionalBanner/index.styles.ts
+++ src/app/components/PromotionalBanner/index.styles.ts
@@ -99,7 +99,6 @@ const styles = {
         backgroundColor: palette.WHITE,
         color: palette.BLACK,
         textDecoration: 'underline',
-        textUnderlineOffset: `${pixelsToRem(5)}rem`,
       },
     }),
 
