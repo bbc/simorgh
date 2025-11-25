d7b3831356e871825f65134b43753a117c5d97ec
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2025-11-25 16:07:16 +0000

    WS-869: Aligns unit tests with new byline design

--- src/app/components/Byline/index.test.tsx
+++ src/app/components/Byline/index.test.tsx
@@ -37,14 +37,11 @@ describe('Byline', () => {
     render(<Byline blocks={bylineWithLink} />);
 
     const AuthorLink = screen.getByText('Single Byline (all values)');
-    const TwitterLink = screen.getByText('@test');
     const Links = screen.getAllByRole('link');
 
     expect(AuthorLink).toBeInTheDocument();
-    expect(TwitterLink).toBeInTheDocument();
-    expect(Links.length).toBe(2);
+    expect(Links.length).toBe(1);
     expect(Links[0]).toHaveAttribute('href', '/news/topics/c8qx38nq177t');
-    expect(Links[1]).toHaveAttribute('href', 'https://twitter.com/test');
   });
 
   it('should render a section with role region', () => {
@@ -70,7 +67,7 @@ describe('Byline', () => {
     const firstContributorItems =
       within(firstContributor).getAllByRole('listitem');
 
-    expect(firstContributorItems.length).toBe(5);
+    expect(firstContributorItems.length).toBe(4);
   });
 
   it('should correctly use the buildIChefURL function to create the image url', () => {
@@ -135,14 +132,10 @@ describe('Byline', () => {
     render(<Byline blocks={bylineWithPngPhoto} />);
 
     const AuthorLink = screen.getByText('Mayeni Jones');
-    const TwitterLink = screen.getByText('@MayeniJones');
-    const Links = screen.getAllByRole('link');
     const Location = screen.getByText('Lagos, Nigeria');
     const Image = screen.getByRole('img');
 
     expect(AuthorLink).toBeInTheDocument();
-    expect(TwitterLink).toBeInTheDocument();
-    expect(Links.length).toBe(1);
     expect(Location).toBeInTheDocument();
     expect(Image).toBeInTheDocument();
   });
@@ -151,8 +144,7 @@ describe('Byline', () => {
     expectation         | info                | text
     ${'Author'}         | ${'Author'}         | ${'Author,'}
     ${'Role'}           | ${'Role'}           | ${'Role,'}
-    ${'X'}              | ${'X'}              | ${'X,'}
-    ${'Reporting from'} | ${'Reporting from'} | ${'Reporting from'}
+    ${'Reporting from'} | ${'Reporting from'} | ${'Reporting from,'}
   `('should correctly announce $expectation for $info', ({ text }) => {
     render(
       <Byline blocks={bylineWithLinkAndLocation}>
@@ -173,7 +165,7 @@ describe('Byline', () => {
     info               | translation
     ${'author'}        | ${'Barreessaa,'}
     ${'role'}          | ${'Gahee,'}
-    ${'reportingFrom'} | ${'Gabaasni irraati'}
+    ${'reportingFrom'} | ${'Gabaasni irraati,'}
   `('should translate $info announcement correctly', ({ translation }) => {
     render(
       <Byline blocks={bylineWithLinkAndLocation}>
