8500ef65a621a7f5df19a993569fc7e31bb8327d
Pedram Valiani <pedram.valiani@bbc.co.uk>
2025-11-28 17:40:37 +0000

    fix error

--- src/app/pages/ArticlePage/index.test.tsx
+++ src/app/pages/ArticlePage/index.test.tsx
@@ -1261,7 +1261,7 @@ describe('Article Page', () => {
         secondaryColumn: {
           topStories: [],
           features: [],
-          personalisedContent: personalisedContent,
+          personalisedContent,
         },
       };
       render(
