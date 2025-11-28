ff65249b8ad15b15374a3ae8525b21d91ec04aef
Pedram Valiani <pedram.valiani@bbc.co.uk>
2025-11-28 15:54:20 +0000

    update linting

--- src/app/components/PersonalisedContent/index.tsx
+++ src/app/components/PersonalisedContent/index.tsx
@@ -28,11 +28,11 @@ const PersonalisedContent = ({
     topicId?: string;
   };
 
-  const personalisedContentArray =
-    (pageData.secondaryColumn?.personalisedContent ||
-      pageData.secondaryColumn?.PersonalisedContent) as
-      | PersonalisedContentType[]
-      | undefined;
+  const personalisedContentArray = (pageData.secondaryColumn
+    ?.personalisedContent ||
+    pageData.secondaryColumn?.PersonalisedContent) as
+    | PersonalisedContentType[]
+    | undefined;
 
   const getPersonalisedContentData = () => {
     if (personalisedTopicCurationExperimentVariant !== 'personalised') {
