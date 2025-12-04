11bcf52df1b5e143ea00267659c333ded1ed13cc
Aaron Moore <aaron.moore@bbc.co.uk>
2025-12-04 12:16:48 +0000

    Rename CSP header tests to be more reflective of intent

--- ws-nextjs-app/utilities/cspHeaderResponse/index.test.ts
+++ ws-nextjs-app/utilities/cspHeaderResponse/index.test.ts
@@ -57,7 +57,7 @@ describe('shouldServeRelaxedCsp', () => {
   const expectedFullCsp =
     "default-src  'self' *.bbc.co.uk *.bbc.com *.bbci.co.uk *.bbci.com https://*.googlesyndication.com;child-src  'self';connect-src  https:;font-src  *.bbc.co.uk *.bbc.com *.bbci.co.uk *.bbci.com data: https://*.teads.tv https://cdnjs.cloudflare.com/ajax/libs/font-awesome/ https://fonts.gstatic.com;frame-src  'self' *.bbc.co.uk *.bbc.com *.bbci.co.uk *.bbci.com https://*.amazon-adsystem.com https://*.chartbeat.com https://*.doubleclick.net https://*.facebook.com https://*.google.com https://*.googleadservices.com https://*.googlesyndication.com https://*.mapcreator.io https://*.teads.tv https://*.thomsonreuters.com https://*.twitter.com https://bbc-maps.carto.com https://bbc.com https://cdn.privacy-mgmt.com https://chartbeat.com https://edigitalsurvey.com https://flo.uri.sh https://public.flourish.studio https://www.instagram.com https://www.riddle.com https://www.tiktok.com https://www.youtube-nocookie.com https://www.youtube.com;img-src  *.bbc.co.uk *.bbc.com *.bbci.co.uk *.bbci.com data: 'self' http://ping.chartbeat.net https://*.adsafeprotected.com https://*.amazon-adsystem.com https://*.cdninstagram.com https://*.doubleclick.net https://*.effectivemeasure.net https://*.google.com https://*.googlesyndication.com https://*.googleusercontent.com https://*.gstatic.com https://*.imrworldwide.com https://*.teads.tv https://*.tiktokcdn.com https://*.twimg.com https://*.twitter.com https://*.xx.fbcdn.net https://i.ytimg.com https://logws1363.ati-host.net https://ping.chartbeat.net https://sb.scorecardresearch.com https://www.googleadservices.com;script-src  'self' 'unsafe-eval' 'unsafe-inline' *.bbc.co.uk *.bbc.com *.bbci.co.uk *.bbci.com http://*.chartbeat.com http://localhost:1124 http://localhost:7080 https://*.adsafeprotected.com https://*.amazon-adsystem.com https://*.chartbeat.com https://*.covatic.io https://*.doubleverify.com https://*.effectivemeasure.net https://*.facebook.com https://*.g.doubleclick.net https://*.google.ae https://*.google.at https://*.google.az https://*.google.be https://*.google.ca https://*.google.ch https://*.google.cl https://*.google.co.id https://*.google.co.il https://*.google.co.in https://*.google.co.jp https://*.google.co.kr https://*.google.co.nz https://*.google.co.tz https://*.google.co.ve https://*.google.com https://*.google.com.af https://*.google.com.ar https://*.google.com.au https://*.google.com.bo https://*.google.com.br https://*.google.com.co https://*.google.com.cy https://*.google.com.ec https://*.google.com.eg https://*.google.com.gt https://*.google.com.hk https://*.google.com.kh https://*.google.com.mm https://*.google.com.mt https://*.google.com.mx https://*.google.com.ng https://*.google.com.ni https://*.google.com.np https://*.google.com.pe https://*.google.com.pk https://*.google.com.pr https://*.google.com.py https://*.google.com.ro https://*.google.com.sa https://*.google.com.sg https://*.google.com.sv https://*.google.com.tr https://*.google.com.tw https://*.google.com.ua https://*.google.com.uy https://*.google.com.vn https://*.google.cv https://*.google.cz https://*.google.de https://*.google.dk https://*.google.es https://*.google.fi https://*.google.fr https://*.google.ge https://*.google.hn https://*.google.ie https://*.google.iq https://*.google.it https://*.google.jo https://*.google.kz https://*.google.lk https://*.google.lv https://*.google.nl https://*.google.no https://*.google.pl https://*.google.ru https://*.google.se https://*.google.so https://*.googlesyndication.com https://*.imrworldwide.com https://*.mapcreator.io https://*.permutive.com https://*.teads.tv https://*.thomsonreuters.com https://*.twimg.com https://*.twitter.com https://*.webcontentassessor.com https://*.xx.fbcdn.net https://adservice.google.co.uk https://bbc.gscontxt.net https://cdn.ampproject.org https://cdn.privacy-mgmt.com https://connect.facebook.net https://lf16-tiktok-web.ttwstatic.com https://public.flourish.studio https://sb.scorecardresearch.com https://www.googletagservices.com https://www.instagram.com https://www.riddle.com https://www.tiktok.com;style-src  'unsafe-inline' *.bbc.co.uk *.bbc.com *.bbci.co.uk *.bbci.com https://*.twimg.com https://*.twitter.com https://*.xx.fbcdn.net https://fonts.googleapis.com https://lf16-tiktok-web.ttwstatic.com;media-src  https: blob:;worker-src  blob: 'self' *.bbc.co.uk *.bbc.com;report-to  worldsvc;upgrade-insecure-requests;";
 
-  it('returns true when toggle is enabled but does not have values set', async () => {
+  it('returns "relaxed" Csp when toggle is enabled but does not have values set', async () => {
     const ctx = createDocumentContext('/pidgin/live/c7p765ynk9qt');
 
     await cspHeaderResponse({
@@ -74,7 +74,7 @@ describe('shouldServeRelaxedCsp', () => {
     expect(requestCsp).toEqual(expectedRelaxedCsp);
   });
 
-  it('returns true when country is not in omittedCountries', async () => {
+  it('returns "relaxed" Csp when country is not in omittedCountries', async () => {
     const ctx = createDocumentContext('/pidgin/live/c7p765ynk9qt', 'ax');
 
     await cspHeaderResponse({
@@ -91,7 +91,7 @@ describe('shouldServeRelaxedCsp', () => {
     expect(requestCsp).toEqual(expectedRelaxedCsp);
   });
 
-  it('returns false when toggle is enabled and given country is in omittedCountries', async () => {
+  it('returns "full" CSP when toggle is enabled and given country is in omittedCountries', async () => {
     const ctx = createDocumentContext('/pidgin/live/c7p765ynk9qt', 'gb');
 
     await cspHeaderResponse({
@@ -108,7 +108,7 @@ describe('shouldServeRelaxedCsp', () => {
     expect(requestCsp).toEqual(expectedFullCsp);
   });
 
-  it('returns false when adsNonce.enabled is false', async () => {
+  it('returns "full" CSP when adsNonce.enabled is false', async () => {
     const ctx = createDocumentContext('/pidgin/live/c7p765ynk9qt', 'gb');
 
     await cspHeaderResponse({
