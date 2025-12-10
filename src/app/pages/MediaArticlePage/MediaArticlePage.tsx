0daa35e88c66c8cc7f6fda50dd8b7ddd2bdfb385
Pedram Valiani <pedram.valiani@bbc.co.uk>
2025-12-10 00:43:52 +0000

    update media article page relative path

--- src/app/pages/MediaArticlePage/MediaArticlePage.tsx
+++ src/app/pages/MediaArticlePage/MediaArticlePage.tsx
@@ -38,7 +38,7 @@ import ChartbeatAnalytics from '../../components/ChartbeatAnalytics';
 import ComscoreAnalytics from '../../legacy/containers/ComscoreAnalytics';
 import SocialEmbedContainer from '../../legacy/containers/SocialEmbed';
 import fauxHeadline from '../../legacy/containers/FauxHeadline';
-import RelatedTopics from '#app/components/RelatedTopics';
+import RelatedTopics from '../../components/RelatedTopics';
 import NielsenAnalytics from '../../legacy/containers/NielsenAnalytics';
 import ArticleMetadata from '../../legacy/containers/ArticleMetadata';
 import EmbedImages from '../../components/Embeds/EmbedImages';
