c0f947ae6ccab92ae7112c863b98ad9d46fe64af
hotinglok <hotinglok@gmail.com>
2025-12-11 10:11:25 +0000

    Add aria-hidden to BlurredBackground

--- src/app/components/Image/BlurredBackground.tsx
+++ src/app/components/Image/BlurredBackground.tsx
@@ -8,6 +8,7 @@ const BlurredBackground = ({ src }: BlurredBackgroundProps) => {
   const lowResImageSrc = src?.replace('{width}', '10');
   return (
     <span
+      aria-hidden="true"
       css={[
         styles.blurredBackground,
         src && { backgroundImage: `url(${lowResImageSrc})` },
