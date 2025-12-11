065ff9b98e892bb9f581e0a90b7ae92f11260af9
Pedram Valiani <pedram.valiani@bbc.co.uk>
2025-12-11 20:45:37 +0000

    i h8 linting

--- src/app/pages/ArticlePage/ArticlePage.tsx
+++ src/app/pages/ArticlePage/ArticlePage.tsx
@@ -487,17 +487,17 @@ const ArticlePage = ({ pageData }: { pageData: Article }) => {
           </main>
           <OptimizelyPageMetrics trackPageView trackPageDepth />
           {showTopics && (
-          <RelatedTopics
-            css={[
-              styles.relatedTopics,
-              ...(showContinueReadingButton
-                ? [!showAllContent && styles.hideRelatedTopics]
-                : []),
-            ]}
-            topics={topics}
-            mobileDivider={false}
-          />
-        )}
+            <RelatedTopics
+              css={[
+                styles.relatedTopics,
+                ...(showContinueReadingButton
+                  ? [!showAllContent && styles.hideRelatedTopics]
+                  : []),
+              ]}
+              topics={topics}
+              mobileDivider={false}
+            />
+          )}
           {/* EXPERIMENT: Location based Topics Experiment */}
           {personalisedContentExperimentVariant === 'variation_2' &&
             personalisedContentBlock}
