70f87ce1990820fcdc389e873e89348b0a965606
Aaron Moore <aaron.moore@bbc.co.uk>
2025-12-16 15:28:37 +0000

    Change `programmes` regex to match upstream route validator

--- cypress/support/helpers/urlValidationTest.js
+++ cypress/support/helpers/urlValidationTest.js
@@ -6,7 +6,7 @@ export default () => {
     cy.get('main a[href^="https://www.bbc.com"]').each($tag => {
       const servicesPattern = SERVICES.join('|');
       const validHrefRegex = new RegExp(
-        `^https://www\\.bbc\\.com/(?:${servicesPattern}|programmes/p[0-9a-zA-Z]{7})(?:/.*)?$`,
+        `^https://www\\.bbc\\.com/(?:${servicesPattern}|programmes/[a-z0-9]{8,15})(?:/.*)?$`,
       );
       const href = $tag.attr('href');
       expect(href).to.exist;
