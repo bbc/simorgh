1e822b3cf539500383cab996602f2da356762467
Aaron Moore <aaron.moore@bbc.co.uk>
2025-12-17 11:57:10 +0000

    Update _error.page.tsx

--- ws-nextjs-app/pages/_error.page.tsx
+++ ws-nextjs-app/pages/_error.page.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import { Component } from 'react';
 import { NextPageContext } from 'next';
 import NextError from 'next/error';
 import { NOT_FOUND } from '#app/lib/statusCodes.const';
 
-class Error extends React.Component<{ statusCode: number }> {
+class Error extends Component<{ statusCode: number }> {
   static getInitialProps({ res, err }: NextPageContext) {
     let statusCode = NOT_FOUND;
 
