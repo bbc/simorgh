dee6b14c4d546901173995bdbbe593eb424ee7ed
Aaron Moore <aaron.moore@bbc.co.uk>
2025-12-17 11:22:29 +0000

    Use `UNKNOWN_PAGE` const

--- ws-nextjs-app/utilities/derivePageType/index.ts
+++ ws-nextjs-app/utilities/derivePageType/index.ts
@@ -6,6 +6,7 @@ import {
   LIVE_PAGE,
   UGC_PAGE,
   HOME_PAGE,
+  UNKNOWN_PAGE,
 } from '#app/routes/utils/pageTypes';
 import {
   isOptimoIdCheck,
@@ -52,5 +53,5 @@ export default function derivePageType(pathname: string): PageTypes {
   if (isOptimoIdCheck(sanitisedPathname)) return ARTICLE_PAGE;
   if (isCpsIdCheck(sanitisedPathname)) return ARTICLE_PAGE;
 
-  return 'Unknown';
+  return UNKNOWN_PAGE;
 }
