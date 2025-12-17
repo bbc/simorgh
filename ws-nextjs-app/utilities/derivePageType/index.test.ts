da310b10faac12708dcc7dd309f67d1b4a5c91b5
Aaron Moore <aaron.moore@bbc.co.uk>
2025-12-17 09:42:53 +0000

    Update index.test.ts

--- ws-nextjs-app/utilities/derivePageType/index.test.ts
+++ ws-nextjs-app/utilities/derivePageType/index.test.ts
@@ -8,51 +8,51 @@ import {
 import derivePageType from '.';
 
 describe('derivePageType', () => {
+  it('should strip our query params from the pathname', () => {
+    const pathname = '/pidgin/live/xxxxxxxxx?foo=bar';
+    const result = derivePageType(pathname);
+    expect(result).toEqual(LIVE_PAGE);
+  });
+
   it("should return LIVE_PAGE if pathname includes 'live'", () => {
-    const pathname = '/burmese/live/xxxxxxxxx';
+    const pathname = '/pidgin/live/xxxxxxxxx';
     const result = derivePageType(pathname);
     expect(result).toEqual(LIVE_PAGE);
   });
 
   it("should return UGC_PAGE if pathname includes 'send'", () => {
-    const pathname = '/burmese/send/xxxxxxxxx';
+    const pathname = '/pidgin/send/xxxxxxxxx';
     const result = derivePageType(pathname);
     expect(result).toEqual(UGC_PAGE);
   });
 
   it("should return AV_EMBEDS if pathname includes 'av-embeds'", () => {
-    const pathname = '/burmese/av-embeds/xxxxxxxxx';
+    const pathname = '/pidgin/av-embeds/xxxxxxxxx';
     const result = derivePageType(pathname);
     expect(result).toEqual(AV_EMBEDS);
   });
 
   it("should return DOWNLOADS_PAGE if pathname includes 'downloads'", () => {
-    const pathname = '/burmese/downloads/xxxxxxxxx';
+    const pathname = '/korean/downloads';
     const result = derivePageType(pathname);
     expect(result).toEqual(DOWNLOADS_PAGE);
   });
 
   it('should return ARTICLE_PAGE if pathname matches Optimo ID pattern', () => {
-    const pathname = '/burmese/articles/c0000000000o';
+    const pathname = '/pidgin/articles/c0000000000o';
     const result = derivePageType(pathname);
     expect(result).toEqual(ARTICLE_PAGE);
   });
 
   it('should return ARTICLE_PAGE if pathname matches CPS ID pattern', () => {
-    const pathname = '/burmese/instituional-1234567';
+    const pathname = '/pidgin/instituional-1234567';
     const result = derivePageType(pathname);
     expect(result).toEqual(ARTICLE_PAGE);
   });
 
   it('should return Unknown if pathname does not include live or send', () => {
-    const pathname = '/burmese/xxxxxxxxx';
+    const pathname = '/pidgin/xxxxxxxxx';
     const result = derivePageType(pathname);
     expect(result).toEqual('Unknown');
-  });
-
-  it('should strip our query params from the pathname', () => {
-    const pathname = '/burmese/live/xxxxxxxxx?foo=bar';
-    const result = derivePageType(pathname);
-    expect(result).toEqual(LIVE_PAGE);
   });
 });
