1935590204251284958f8d20fb259ce8a5c0f6ba
Louise Archibald <louise.archibald@bbc.co.uk>
2025-12-17 15:14:49 +0000

    update name references

--- src/app/components/ArticleLinksBlock/Promo/index.test.jsx
+++ src/app/components/ArticleLinksBlock/Promo/index.test.jsx
@@ -1,15 +1,9 @@
 import { render } from '../../react-testing-library-with-providers';
-import {
-  PromoSingleBlock,
-  oneLinkWithTimestamp,
-  topStoriesBlocks,
-  mostReadBlocks,
-  topStoriesBlocksWithLiveItem,
-} from '../helpers/fixtureData';
+import { PromoSingleBlock, oneLinkWithTimestamp } from '../helpers/fixtureData';
 import Promo from '.';
 import { ServiceContextProvider } from '../../../contexts/ServiceContext';
 
-const ScrollablePromo = ({ block, experimentVariant }) => (
+const ArticleLinksBlock = ({ block, experimentVariant }) => (
   <ServiceContextProvider service="pidgin">
     <Promo
       block={block}
@@ -19,16 +13,18 @@ const ScrollablePromo = ({ block, experimentVariant }) => (
   </ServiceContextProvider>
 );
 
-describe('ScrollablePromo', () => {
+describe('Article Links Block', () => {
   it('should render a link', () => {
     const { queryByRole } = render(
-      <ScrollablePromo block={PromoSingleBlock} />,
+      <ArticleLinksBlock block={PromoSingleBlock} />,
     );
     expect(queryByRole('link')).toBeInTheDocument();
   });
 
   it('should extract and render the correct title', () => {
-    const { getByText } = render(<ScrollablePromo block={PromoSingleBlock} />);
+    const { getByText } = render(
+      <ArticleLinksBlock block={PromoSingleBlock} />,
+    );
     expect(
       getByText(
         'This is a very long headline. I am creating this for a test purpose. I love creating these type of tests. I really do not know what to write.',
@@ -38,14 +34,14 @@ describe('ScrollablePromo', () => {
 
   it('should extract and render the correct href', () => {
     const { queryByRole } = render(
-      <ScrollablePromo block={PromoSingleBlock} />,
+      <ArticleLinksBlock block={PromoSingleBlock} />,
     );
     expect(queryByRole('link').href).toEqual('https://www.bbc.com/mundo');
   });
 
   it('should render timestamp if timestamp is available', () => {
     const { container } = render(
-      <ScrollablePromo block={oneLinkWithTimestamp[0]} />,
+      <ArticleLinksBlock block={oneLinkWithTimestamp[0]} />,
     );
     expect(container.getElementsByTagName('time')[0]).toBeInTheDocument();
   });
