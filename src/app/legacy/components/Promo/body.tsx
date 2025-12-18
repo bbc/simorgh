11e6062e9dba1c241a75d365adf574de14585439
Nabeel Khan <nabeel.khan@bbc.co.uk>
2025-12-18 11:27:12 +0000

    undid recent typescript conversion changes due to incorrect file location

--- src/app/legacy/components/Promo/body.tsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-const P = styled.p(props => ({
-  ...props.theme?.fontVariants?.sansRegular,
-  ...props.theme?.fontSizes?.bodyCopy,
-  color: props.theme?.palette?.EBON,
-}));
-
-export default P;
--- src/app/legacy/components/Promo/footer.tsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-const Footer = styled.footer(props => ({
-  ...props.theme?.fontVariants?.sansRegular,
-  ...props.theme?.fontSizes?.brevier,
-  color: props.theme?.palette?.RHINO,
-}));
-
-export default Footer;
