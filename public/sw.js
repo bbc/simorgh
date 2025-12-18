faed29464638d16c01827451150bad989b06268c
matthewp5 <mattplumeridge@gmail.com>
2025-12-18 10:24:18 +0000

    Bump service worker version

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 /* eslint-disable no-restricted-globals */
-const version = 'v0.3.0';
+const version = 'v0.3.1';
 const cacheName = 'simorghCache_v1';
 
 const service = self.location.pathname.split('/')[1];
