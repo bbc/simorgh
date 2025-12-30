2e34c5ffabf9c620bebd501a916f71f693ae4655
Aaron Moore <aaron.moore@bbc.co.uk>
2025-12-30 17:09:03 +0000

    Fix typo in process.env setting in test

--- ws-nextjs-app/utilities/addLinkHeader/index.test.ts
+++ ws-nextjs-app/utilities/addLinkHeader/index.test.ts
@@ -17,7 +17,7 @@ describe('addLinkHeader', () => {
     process.env.SIMORGH_PUBLIC_STATIC_ASSETS_ORIGIN =
       'https://static.test.files.bbci.co.uk';
 
-    process.env.ATI_BASE_URL = 'https://logws1363.ati-host.net?';
+    process.env.SIMORGH_ATI_BASE_URL = 'https://logws1363.ati-host.net?';
 
     addLinkHeader({ ctx: mockCtx });
 
@@ -31,13 +31,13 @@ describe('addLinkHeader', () => {
     process.env.SIMORGH_PUBLIC_STATIC_ASSETS_ORIGIN =
       'https://static.files.bbci.co.uk';
 
-    process.env.ATI_BASE_URL = 'https://a1.api.bbc.co.uk/hit.xiti?';
+    process.env.SIMORGH_ATI_BASE_URL = 'https://a1.api.bbc.co.uk/hit.xiti?';
 
     addLinkHeader({ ctx: mockCtx });
 
     expect(mockSetHeader).toHaveBeenCalledWith(
       'Link',
-      '<https://ichef.bbci.co.uk>; rel="dns-prefetch", <https://ichef.bbci.co.uk>; rel="preconnect",<https://static.files.bbci.co.uk>; rel="dns-prefetch", <https://static.files.bbci.co.uk>; rel="preconnect",<https://static.files.bbci.co.uk>; rel="preconnect"; crossorigin,<https://logws1363.ati-host.net?>; rel="dns-prefetch", <https://logws1363.ati-host.net?>; rel="preconnect",<https://ping.chartbeat.net>; rel="dns-prefetch", <https://ping.chartbeat.net>; rel="preconnect"',
+      '<https://ichef.bbci.co.uk>; rel="dns-prefetch", <https://ichef.bbci.co.uk>; rel="preconnect",<https://static.files.bbci.co.uk>; rel="dns-prefetch", <https://static.files.bbci.co.uk>; rel="preconnect",<https://static.files.bbci.co.uk>; rel="preconnect"; crossorigin,<https://a1.api.bbc.co.uk/hit.xiti?>; rel="dns-prefetch", <https://a1.api.bbc.co.uk/hit.xiti?>; rel="preconnect",<https://ping.chartbeat.net>; rel="dns-prefetch", <https://ping.chartbeat.net>; rel="preconnect"',
     );
   });
 });
