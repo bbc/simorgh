8b7a53d50b696a3896245ffdb1fdec051ee96c1f
Louise Archibald <louise.archibald@bbc.co.uk>
2025-12-30 14:40:51 +0000

    change width and adjust margins between items

--- src/app/components/ArticleLinksBlock/Promo/index.styles.tsx
+++ src/app/components/ArticleLinksBlock/Promo/index.styles.tsx
@@ -1,3 +1,4 @@
+import pixelsToRem from '#app/utilities/pixelsToRem';
 import { css, Theme } from '@emotion/react';
 
 export default {
@@ -25,18 +26,19 @@ export default {
       position: 'relative',
       backgroundColor: isDarkUi ? palette.GREY_3 : palette.WHITE,
       padding: `${spacings.DOUBLE}rem`,
-      marginBottom: `${spacings.TRIPLE}rem`,
+      marginBottom: `${spacings.FULL}rem`,
       height: 'auto',
       display: 'block',
-      [mq.GROUP_0_MAX_WIDTH]: {
-        width: `14.8125rem`,
-      },
-      [mq.GROUP_3_MIN_WIDTH]: {
-        width: `11.125rem`,
-      },
-      [mq.GROUP_4_MIN_WIDTH]: {
-        width: `12.6875rem`,
-      },
+      width: '100%',
+      //   [mq.GROUP_0_MAX_WIDTH]: {
+      //     width: `14.8125rem`,
+      //   },
+      //   [mq.GROUP_3_MIN_WIDTH]: {
+      //     width: `11.125rem`,
+      //   },
+      //   [mq.GROUP_4_MIN_WIDTH]: {
+      //     width: '100%',
+      //   },
     }),
   operaPromoBox: ({ isDarkUi, mq, palette, spacings }: Theme) =>
     css({
