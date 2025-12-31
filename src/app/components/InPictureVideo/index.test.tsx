57d98ab4717b951231c451d1e8d0bb1cc069081e
Shayne Ahchoon <shayneah@googlemail.com>
2025-12-31 12:55:35 +0000

    WS-1768: Resolve dependency problem

--- src/app/components/InPictureVideo/index.test.tsx
+++ src/app/components/InPictureVideo/index.test.tsx
@@ -1,10 +1,8 @@
-import * as clickTracking from '#app/hooks/useClickTrackerHandler';
 import { render } from '../react-testing-library-with-providers';
 import InPicture from '.';
 
 describe('InPictureVideo', () => {
   it('should track clicks', () => {
-    const clickTrackerSpy = jest.spyOn(clickTracking, 'default');
     render(<InPicture />);
     expect(true).toBe(true);
   });
