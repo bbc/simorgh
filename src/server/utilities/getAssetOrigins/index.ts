7321d43a6fca7c95459e262ee26ce480c6667282
Aaron Moore <aaron.moore@bbc.co.uk>
2025-12-31 09:37:14 +0000

    Move consts out of function

--- src/server/utilities/getAssetOrigins/index.ts
+++ src/server/utilities/getAssetOrigins/index.ts
@@ -1,8 +1,7 @@
+const IMAGES_ORIGIN = 'https://ichef.bbci.co.uk';
+const ANALYTICS_ORIGINS = ['https://ping.chartbeat.net'];
+
 const getAssetOrigins = () => {
-  const IMAGES_ORIGIN = 'https://ichef.bbci.co.uk';
-
-  const ANALYTICS_ORIGINS = ['https://ping.chartbeat.net'];
-
   const assetOrigins = [
     IMAGES_ORIGIN,
     process.env.SIMORGH_PUBLIC_STATIC_ASSETS_ORIGIN,
