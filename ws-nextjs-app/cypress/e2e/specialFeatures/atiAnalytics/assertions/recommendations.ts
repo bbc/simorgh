742e3ef368baba630efa1b17bfcdc5cec24f8bfe
alex-magana <alex.magana@andela.com>
2025-12-31 14:54:22 +0300

    Remove useReverb from Next.js recommendations tests

--- ws-nextjs-app/cypress/e2e/specialFeatures/atiAnalytics/assertions/recommendations.ts
+++ ws-nextjs-app/cypress/e2e/specialFeatures/atiAnalytics/assertions/recommendations.ts
@@ -11,7 +11,6 @@ const { RECOMMENDATIONS } = COMPONENTS;
 export const assertRecommendationsComponentView = ({
   pageIdentifier,
   contentType,
-  useReverb,
   path,
   applicationType,
   siteId,
@@ -32,7 +31,6 @@ export const assertRecommendationsComponentView = ({
       component: RECOMMENDATIONS,
       pageIdentifier,
       contentType,
-      useReverb,
       applicationType,
       siteId,
     });
@@ -42,7 +40,6 @@ export const assertRecommendationsComponentView = ({
 export const assertRecommendationsComponentClick = ({
   pageIdentifier,
   contentType,
-  useReverb,
   path,
   applicationType,
   siteId,
@@ -65,7 +62,6 @@ export const assertRecommendationsComponentClick = ({
       component: RECOMMENDATIONS,
       pageIdentifier,
       contentType,
-      useReverb,
       applicationType,
       siteId,
     });
