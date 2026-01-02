52d1ce26c737c170f11f8eab7ecaf97b4ba3f655
Aaron Moore <aaron.moore@bbc.co.uk>
2026-01-02 12:30:03 +0000

    Update urlValidationTest.ts

--- ws-nextjs-app/cypress/support/helpers/urlValidationTest.ts
+++ ws-nextjs-app/cypress/support/helpers/urlValidationTest.ts
@@ -1,18 +1,20 @@
 /* eslint-disable no-unused-expressions */
 import SERVICES from '#app/lib/config/services';
 
+const SERVICES_PATTERN = SERVICES.join('|');
+
+const VALID_HREF_REGEX = new RegExp(
+  `^https://www\\.bbc\\.com/(?:${SERVICES_PATTERN}|programmes/[a-z0-9]{8,15})(?:/.*)?$`,
+);
+
 export default () => {
   it('all links within <main> element should be a valid World Service URL', () => {
     cy.get('main a[href^="https://www.bbc.com"]').each($tag => {
-      const servicesPattern = SERVICES.join('|');
-      const validHrefRegex = new RegExp(
-        `^https://www\\.bbc\\.com/(?:${servicesPattern}|programmes/[a-z0-9]{8,15})(?:/.*)?$`,
-      );
       const href = $tag.attr('href');
       expect(href).to.exist;
       expect(href).to.not.be.empty;
 
-      expect(href).to.match(validHrefRegex);
+      expect(href).to.match(VALID_HREF_REGEX);
     });
   });
 };
