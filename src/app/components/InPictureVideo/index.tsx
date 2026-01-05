42cc1ad8fe20209e6d3d3176fe2abe47c7bae859
Shayne Ahchoon <shayneah@googlemail.com>
2026-01-05 10:18:53 +0000

    WS-1768: remove middleware

--- src/app/components/InPictureVideo/index.tsx
+++ src/app/components/InPictureVideo/index.tsx
@@ -5,7 +5,7 @@ export default () => {
     <section css={style.container}>
       <iframe
         css={style.video}
-        src="https://www.youtube.com/embed/IJWlBfo5Oj0?si=jUNjyayJBatKQfH8"
+        src="https://www.youtube.com/embed/EColTNIbOko?si=DmXVv_JFXkxx8YFa"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
