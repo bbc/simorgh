77241ac18774aed4e2ec3f1b345c5c23da45bb54
Nabeel Khan <nabeel.khan@bbc.co.uk>
2026-01-08 12:26:06 +0000

    added fetching for nextJS

---
+++ ws-nextjs-app/pages/[service]/topics/[id]/[[...variant]].page.tsx
@@ -0,0 +1,81 @@
+import { GetServerSideProps } from 'next';
+import dynamic from 'next/dynamic';
+import { TOPIC_PAGE } from '#app/routes/utils/pageTypes';
+import nodeLogger from '#lib/logger.node';
+import logResponseTime from '#server/utilities/logResponseTime';
+import { ROUTING_INFORMATION } from '#app/lib/logger.const';
+import { OK } from '#app/lib/statusCodes.const';
+import PageDataParams from '#app/models/types/pageDataParams';
+import deriveVariant from '#nextjs/utilities/deriveVariant';
+import getPageData from '../../../../utilities/pageRequests/getPageData';
+
+const TopicPage = dynamic(() => import('#app/pages/TopicPage/TopicPage'));
+
+const logger = nodeLogger(__filename);
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  context.res.setHeader(
+    'Cache-Control',
+    'public, stale-if-error=2400, stale-while-revalidate=960, max-age=240',
+  );
+
+  logResponseTime({ path: context.resolvedUrl }, context.res, () => null);
+
+  const {
+    id,
+    service,
+    variant: variantFromUrl,
+    renderer_env: rendererEnv,
+    page,
+  } = context.query as PageDataParams;
+
+  const variant = deriveVariant(variantFromUrl);
+
+  const { data } = await getPageData({
+    id,
+    page,
+    service,
+    variant,
+    rendererEnv,
+    resolvedUrl: context.resolvedUrl,
+    pageType: TOPIC_PAGE,
+  });
+
+  let routingInfoLogger = logger.debug;
+  if (data.status !== OK) {
+    routingInfoLogger = logger.error;
+  }
+
+  routingInfoLogger(ROUTING_INFORMATION, {
+    url: context.resolvedUrl,
+    status: data.status,
+    pageType: TOPIC_PAGE,
+  });
+
+  context.res.statusCode = data.status;
+
+  return {
+    props: {
+      error: data?.error || null,
+      id,
+      page: page || null,
+      pageData: data?.pageData
+        ? {
+            ...data.pageData,
+            metadata: {
+              ...data.pageData.metadata,
+              type: TOPIC_PAGE,
+            },
+          }
+        : null,
+      pageType: TOPIC_PAGE,
+      pathname: context.resolvedUrl,
+      service,
+      status: data.status,
+      timeOnServer: Date.now(),
+      variant,
+    },
+  };
+};
+
+export default TopicPage;
