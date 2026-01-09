19b039771138cba8f78a8d8e2cfab3657b8758b4
Aaron Moore <aaron.moore@bbc.co.uk>
2026-01-09 14:29:33 +0000

    Remove `dir` from Promo components and wrappers

--- src/app/components/ArticleLinksBlock/PromoList/index.tsx
+++ src/app/components/ArticleLinksBlock/PromoList/index.tsx
@@ -1,25 +1,20 @@
-import { use } from 'react';
-import { EventTrackingMetadata } from '#app/models/types/eventTracking';
 import { OptimoBlock } from '#app/models/types/optimo';
 import { ViewTracker } from '#app/lib/analyticsUtils/types';
 import useClickTrackerHandler from '#app/hooks/useClickTrackerHandler';
-import { ServiceContext } from '../../../contexts/ServiceContext';
 import Promo from '../Promo';
 import styles from './index.styles';
 
 interface PromoListProps {
   blocks: OptimoBlock[];
-  eventTrackingData?: EventTrackingMetadata;
   viewTracker?: ViewTracker;
   clickTracker?: ReturnType<typeof useClickTrackerHandler>;
 }
 
 const PromoList = ({ blocks, viewTracker, clickTracker }: PromoListProps) => {
-  const { dir } = use(ServiceContext);
   const listBlocks = blocks.slice(0, 3);
 
   return (
-    <div css={[styles.promo]} dir={dir} {...viewTracker}>
+    <div css={[styles.promo]} {...viewTracker}>
       <ul>
         {listBlocks.map((block, index) => {
           return (
