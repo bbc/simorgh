a91a86365b95bd6a22083b31597b833d3218d361
Santa Zena <santa.zena.ext@bbc.co.uk>
2026-01-13 16:09:25 +0200

    Adding styling for smaller screens

--- ws-nextjs-app/pages/[service]/topics/index.styles.tsx
+++ ws-nextjs-app/pages/[service]/topics/index.styles.tsx
@@ -2,10 +2,18 @@ import { css, Theme } from '@emotion/react';
 import pixelsToRem from '#app/utilities/pixelsToRem';
 
 const styles = {
-  usefulLinksWrapper: ({ fontSizes, palette }: Theme) =>
+  usefulLinksWrapper: ({ fontSizes, palette, mq, spacings }: Theme) =>
     css({
+      width: '100%',
       maxWidth: `${pixelsToRem(700)}rem`,
       margin: '0 auto',
+      paddingInlineStart: `${spacings.DOUBLE}rem`,
+      paddingInlineEnd: `${spacings.DOUBLE}rem`,
+
+      [mq.GROUP_3_MIN_WIDTH]: {
+        paddingInlineStart: `${pixelsToRem(96)}rem`,
+        paddingInlineEnd: `${spacings.DOUBLE}rem`,
+      },
 
       '& ul': {
         gridTemplateColumns: '1fr !important',
@@ -17,14 +25,8 @@ const styles = {
 
       '& a': {
         fontSize: `${pixelsToRem(18)}rem`,
-
-        '&:hover': {
-          color: palette.ARCHIVE_BLUE,
-        },
-
-        '&:visited': {
-          color: palette.GREY_6,
-        },
+        '&:hover': { color: palette.ARCHIVE_BLUE },
+        '&:visited': { color: palette.GREY_6 },
       },
     }),
 };
