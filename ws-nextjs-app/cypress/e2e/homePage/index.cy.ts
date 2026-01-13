8b8c79f2a3b5f88b613a9287bed9aa408ef84e3a
alex-magana <alex.magana@andela.com>
2026-01-13 16:14:25 +0300

    Add Romania home page e2e

--- ws-nextjs-app/cypress/e2e/homePage/index.cy.ts
+++ ws-nextjs-app/cypress/e2e/homePage/index.cy.ts
@@ -73,6 +73,12 @@ const testSuites = [
     tests,
   },
   {
+    path: '/romania',
+    runforEnv: ['local', 'test'],
+    service: 'romania',
+    tests,
+  },
+  {
     path: '/serbian/lat',
     runforEnv: ['local', 'test', 'live'],
     service: 'serbian',
@@ -188,6 +194,16 @@ const atiAnalyticsTestSuites = [
       assertPortraitVideoCarouselComponentView,
       assertPortraitVideoModalComponentView,
     ],
+  },
+  {
+    path: '/romania',
+    runforEnv: ['local', 'test'],
+    service: 'romania',
+    pageIdentifier: 'romania.page',
+    siteId: 136,
+    applicationType: 'responsive',
+    contentType: 'index-home',
+    tests: [assertPageView],
   },
   {
     path: '/serbian/lat',
