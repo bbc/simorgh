e6d5dcd4bcb5f90349229d8a1e518a1a7c5b6243
Aaron Moore <aaron.moore@bbc.co.uk>
2026-01-13 14:14:24 +0000

    Fix `pageData` object destructuring

--- ws-nextjs-app/cypress/e2e/topicPage/tests.ts
+++ ws-nextjs-app/cypress/e2e/topicPage/tests.ts
@@ -16,7 +16,7 @@ export default ({ service, pageType, variant = 'default', path }) => {
       topicId = path.match(/(c[a-zA-Z0-9]{10,}t)/)?.[1];
 
       // Gets the topic page data for all the tests
-      cy.getPageDataFromWindow().then(({ pageData }) => {
+      cy.getPageDataFromWindow().then(pageData => {
         topicTitle = pageData.title;
         pageCount = pageData.pageCount;
         numberOfItems = pageData.curations?.[0]?.summaries.length;
@@ -65,8 +65,7 @@ export default ({ service, pageType, variant = 'default', path }) => {
         const promoCount = Cypress.$(selector).length;
         cy.log(`Number of promos on the page${promoCount}`);
         if (promoCount !== numberOfItems) {
-          cy.window().then(win => {
-            const pageData = win.SIMORGH_DATA;
+          cy.getPageDataFromWindow().then(pageData => {
             cy.log(pageData);
           });
         }
