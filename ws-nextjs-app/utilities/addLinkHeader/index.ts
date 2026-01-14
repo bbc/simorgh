c5b7690895176d38fc2f655fe5e9c096ba68560b
alex-magana <alex.magana@andela.com>
2026-01-14 18:44:34 +0300

    Add reverb to resource hints header for Express and Next.js

--- ws-nextjs-app/utilities/addLinkHeader/index.ts
+++ ws-nextjs-app/utilities/addLinkHeader/index.ts
@@ -2,20 +2,20 @@ import { NextPageContext } from 'next/types';
 import getAssetOrigins from '#server/utilities/getAssetOrigins';
 
 const addLinkHeader = ({ ctx }: { ctx: NextPageContext }) => {
-  const assetOrigins = getAssetOrigins();
-
-  ctx.res?.setHeader(
-    'Link',
-    assetOrigins
-      .map(domainName => {
-        const crossOrigin =
-          domainName === 'https://static.files.bbci.co.uk'
-            ? `,<${domainName}>; rel="preconnect"; crossorigin`
-            : '';
-        return `<${domainName}>; rel="dns-prefetch", <${domainName}>; rel="preconnect"${crossOrigin}`;
-      })
-      .join(','),
+  const { dnsPrefetchOrigins, preconnectOrigins } = getAssetOrigins(
+    ctx.pathname,
   );
+
+  const resourceHintsConfig = [
+    ...dnsPrefetchOrigins.map(
+      domainName => `<${domainName}>; rel="dns-prefetch"`,
+    ),
+    ...preconnectOrigins.map(
+      domainName => `<${domainName}>; rel="preconnect; crossorigin"`,
+    ),
+  ];
+
+  ctx.res?.setHeader('Link', resourceHintsConfig.join(','));
 };
 
 export default addLinkHeader;
