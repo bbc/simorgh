03f257178ec44bf669016ccf3a06fb2e996c5bfa
alex-magana <alex.magana@andela.com>
2026-01-14 21:11:02 +0300

    Remove Piano config override for AMP

--- src/app/components/ATIAnalytics/amp/ampAnalyticsJson.ts
+++ src/app/components/ATIAnalytics/amp/ampAnalyticsJson.ts
@@ -5,48 +5,9 @@ import { reverbUrlHelper } from '@bbc/reverb-url-helper';
 import { getDestination } from '#app/lib/analyticsUtils';
 import { ATIAnalyticsProps } from '../types';
 
-type GeoVariantEvaluationParamters = {
-  appName: string;
-  destination: string;
-  ampAnalyticsRequestConfiguration: {
-    base: string;
-    pageview: string;
-  };
-};
-
-const applyGeoVariantDestinationForSupportedServices = ({
-  appName,
-  destination,
-  ampAnalyticsRequestConfiguration,
-}: GeoVariantEvaluationParamters) => {
-  if (
-    ![
-      'news',
-      'news-cymrufyw',
-      'news-japanese',
-      'news-naidheachdan',
-      'sport',
-    ].includes(appName)
-  ) {
-    return ampAnalyticsRequestConfiguration;
-  }
-
-  const { pageview } = ampAnalyticsRequestConfiguration;
-  const ampDestination = getDestination('amp', destination);
-
-  return {
-    ...ampAnalyticsRequestConfiguration,
-    pageview: pageview.replace(/s=(\d+|\$IF\(.*\))&/, `s=${ampDestination}&`), // Use destination derived via amp-geo
-  };
-};
-
 const ampAnalyticsJson = ({ reverbParams }: ATIAnalyticsProps) => {
   const ampAnalyticsRequestConfiguration =
     reverbUrlHelper.getAmpAnalyticsPageViewUrl(reverbParams);
-
-  const appName =
-    reverbParams?.params.page.additionalProperties?.app_name ?? '';
-  const destination = reverbParams?.params.page.destination ?? '';
 
   return {
     transport: {
@@ -54,11 +15,7 @@ const ampAnalyticsJson = ({ reverbParams }: ATIAnalyticsProps) => {
       xhrpost: false,
       image: true,
     },
-    requests: applyGeoVariantDestinationForSupportedServices({
-      appName,
-      destination,
-      ampAnalyticsRequestConfiguration,
-    }),
+    requests: ampAnalyticsRequestConfiguration,
     triggers: { trackPageview: { on: 'visible', request: 'pageview' } },
   };
 };
