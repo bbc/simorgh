d818b76bb636292d62407e66146b5d8e5593b4f4
Santa Zena <santa.zena.ext@bbc.co.uk>
2026-01-27 13:50:35 +0200

    Updating test

--- src/app/components/Account/AccountHeader/index.test.tsx
+++ src/app/components/Account/AccountHeader/index.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import ThemeProvider from '#app/components/ThemeProvider';
+import { ServiceContextProvider } from '#app/contexts/ServiceContext';
 import { AccountProvider } from '../AccountContext';
 import AccountHeader from '.';
 
@@ -7,25 +8,28 @@ const renderWithProviders = (
   initialConfig: Parameters<typeof AccountProvider>[0]['initialConfig'],
 ) =>
   render(
-    <ThemeProvider service="ws">
-      <AccountProvider initialConfig={initialConfig}>
-        <AccountHeader />
-      </AccountProvider>
-    </ThemeProvider>,
+    <ServiceContextProvider service="ws">
+      <ThemeProvider service="ws">
+        <AccountProvider initialConfig={initialConfig}>
+          <AccountHeader />
+        </AccountProvider>
+      </ThemeProvider>
+    </ServiceContextProvider>,
   );
 
 describe('AccountHeader', () => {
-  it('shows Sign in when signed out', () => {
+  it('shows Sign in when signed out', async () => {
     renderWithProviders({
       availability: { signin: 'GREEN' },
       unavailable_url: 'https://example.com/unavailable',
       signin_url: 'https://example.com/signin',
       register_url: 'https://example.com/register',
-      'foryou-flagpole': 'GREEN',
+      foryou_flagpole: 'GREEN',
       foryou_url: 'https://example.com/foryou',
     });
 
-    expect(screen.getByRole('link', { name: 'Sign in' })).toHaveAttribute(
+    const link = await screen.findByRole('link', { name: 'Sign in' });
+    expect(link).toHaveAttribute(
       'href',
       expect.stringContaining('https://example.com/signin'),
     );
