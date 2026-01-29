3c0fa840440c5aec14401749ae787eb2c0088ffc
Santa Zena <santa.zena.ext@bbc.co.uk>
2026-01-29 14:49:33 +0200

    Adding object parameters

--- src/app/components/Account/AccountHeader/index.stories.tsx
+++ src/app/components/Account/AccountHeader/index.stories.tsx
@@ -3,30 +3,29 @@ import { ServiceContextProvider } from '#app/contexts/ServiceContext';
 import { AccountContext } from '#app/contexts/AccountContext';
 import AccountHeader from '.';
 
-const withProviders =
-  (isSignedIn: boolean) =>
-    () => (
-      <ThemeProvider service="ws">
-        <ServiceContextProvider service="ws">
-          <AccountContext.Provider
-            value={{
-              isSignedIn,
-              signInUrl: 'https://example.com/signin',
-              forYouUrl: 'https://example.com/for-you',
-              registerUrl: 'https://example.com/register',
-              isIdctaAvailable: true,
-            }}
-          >
-            <AccountHeader />
-          </AccountContext.Provider>
-        </ServiceContextProvider>
-      </ThemeProvider>
-    );
-
-export default {
-  title: 'Account/AccountHeader',
-  component: AccountHeader,
-};
-
-export const SignedOut = withProviders(false);
-export const SignedIn = withProviders(true);
+type WithProvidersArgs = {
+    isSignedIn: boolean;
+  };
+  
+  const withProviders =
+    ({ isSignedIn }: WithProvidersArgs) =>
+      () => (
+        <ThemeProvider service="ws">
+          <ServiceContextProvider service="ws">
+            <AccountContext.Provider
+              value={{
+                isSignedIn,
+                signInUrl: 'https://example.com/signin',
+                forYouUrl: 'https://example.com/for-you',
+                registerUrl: 'https://example.com/register',
+                isIdctaAvailable: true,
+              }}
+            >
+              <AccountHeader />
+            </AccountContext.Provider>
+          </ServiceContextProvider>
+        </ThemeProvider>
+      );
+  
+  export const SignedOut = withProviders({ isSignedIn: false });
+  export const SignedIn = withProviders({ isSignedIn: true });
