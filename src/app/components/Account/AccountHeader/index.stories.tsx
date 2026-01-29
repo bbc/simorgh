4218832e2f7abf19dedcd5fe7a5b7b36a85ea596
Santa Zena <santa.zena.ext@bbc.co.uk>
2026-01-29 15:01:59 +0200

    Story update

--- src/app/components/Account/AccountHeader/index.stories.tsx
+++ src/app/components/Account/AccountHeader/index.stories.tsx
@@ -7,25 +7,29 @@ type WithProvidersArgs = {
     isSignedIn: boolean;
   };
   
-  const withProviders =
-    ({ isSignedIn }: WithProvidersArgs) =>
-      () => (
-        <ThemeProvider service="ws">
-          <ServiceContextProvider service="ws">
-            <AccountContext.Provider
-              value={{
-                isSignedIn,
-                signInUrl: 'https://example.com/signin',
-                forYouUrl: 'https://example.com/for-you',
-                registerUrl: 'https://example.com/register',
-                isIdctaAvailable: true,
-              }}
-            >
-              <AccountHeader />
-            </AccountContext.Provider>
-          </ServiceContextProvider>
-        </ThemeProvider>
-      );
-  
-  export const SignedOut = withProviders({ isSignedIn: false });
-  export const SignedIn = withProviders({ isSignedIn: true });
+const withProviders =
+  ({ isSignedIn }: WithProvidersArgs) =>
+    () => (
+      <ThemeProvider service="ws">
+        <ServiceContextProvider service="ws">
+          <AccountContext.Provider
+            value={{
+              isSignedIn,
+              signInUrl: 'https://example.com/signin',
+              forYouUrl: 'https://example.com/for-you',
+              registerUrl: 'https://example.com/register',
+              isIdctaAvailable: true,
+            }}
+          >
+            <AccountHeader />
+          </AccountContext.Provider>
+        </ServiceContextProvider>
+      </ThemeProvider>
+    );
+export default {
+  title: 'Account/AccountHeader',
+  component: AccountHeader,
+};
+
+export const SignedOut = withProviders({ isSignedIn: false });
+export const SignedIn = withProviders({ isSignedIn: true });
