63710fcfbe75f673edb48f26be9badc58a41a0f3
Santa Zena <santa.zena.ext@bbc.co.uk>
2026-01-30 15:37:06 +0200

    Simplify AccountHeader tests

--- src/app/components/Account/AccountHeader/index.test.tsx
+++ src/app/components/Account/AccountHeader/index.test.tsx
@@ -2,25 +2,34 @@ import {
   render,
   screen,
 } from '#app/components/react-testing-library-with-providers';
+import * as React from 'react';
 import { AccountContext } from '#app/contexts/AccountContext';
 import AccountHeader from '.';
 
-const renderAccountHeader = ({ isSignedIn }: { isSignedIn: boolean }) =>
-  render(
-    <AccountContext.Provider
-      value={{
+const realUseContext = React.useContext;
+const mockUseContext = jest.spyOn(React, 'useContext');
+
+const renderAccountHeader = ({ isSignedIn }: { isSignedIn: boolean }) => {
+  mockUseContext.mockImplementation((ctx: any) => {
+    if (ctx === AccountContext) {
+      return {
         isSignedIn,
         signInUrl: 'https://example.com/signin',
         forYouUrl: 'https://example.com/foryou',
         isIdctaAvailable: true,
-      }}
-    >
-      <AccountHeader />
-    </AccountContext.Provider>,
-    { service: 'ws' },
-  );
+      };
+    }
+    return realUseContext(ctx);
+  });
+
+  return render(<AccountHeader />, { service: 'ws' });
+};
 
 describe('AccountHeader', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   it('shows Sign in when signed out', async () => {
     renderAccountHeader({ isSignedIn: false });
 
