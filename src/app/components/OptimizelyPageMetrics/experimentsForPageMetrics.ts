830a32f14c1c0e7bd016b9b1127978a8e2c49ff7
“LilyL0u” <lily.higham@bbc.co.uk>
2026-02-02 12:06:49 +0000

    media article pages to also send page view and visit

--- src/app/components/OptimizelyPageMetrics/experimentsForPageMetrics.ts
+++ src/app/components/OptimizelyPageMetrics/experimentsForPageMetrics.ts
@@ -1,5 +1,5 @@
 import { PageTypes } from '#app/models/types/global';
-import { ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
+import { ARTICLE_PAGE, MEDIA_ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
 // Any running serverside and client side experiments which collect Optimizely Page Metrics; page view, page complete, scroll depth
 // Includes PageType so that different experiments can be run on different pageTypes
 
@@ -12,6 +12,10 @@ const experimentsForPageMetrics: ExperimentsForPageTypeMetrics = [
   {
     // EXPERIMENT: OJ Referrer
     pageType: ARTICLE_PAGE,
+    activeExperiments: ['newswb_ws_oj_by_referrer'],
+  },
+  {
+    pageType: MEDIA_ARTICLE_PAGE,
     activeExperiments: ['newswb_ws_oj_by_referrer'],
   },
   // TODO add map experiment names when they are confirmed
