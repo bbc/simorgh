e227aba2d501568d73db194d6fd4ba3e19e9b710
Shayne Ahchoon <shayneah@googlemail.com>
2026-02-04 12:09:01 +0000

    WS-1935: Disable chromatic

--- src/app/components/Embeds/ClientSideRiddleEmbed/index.styles.tsx
+++ src/app/components/Embeds/ClientSideRiddleEmbed/index.styles.tsx
@@ -4,17 +4,10 @@ const styles = {
   iframe: ({ spacings, mq }: Theme) =>
     css({
       maxWidth: '100%',
-      paddingLeft: `${spacings.FULL}rem`,
-      paddingRight: `${spacings.FULL}rem`,
       marginBottom: `${spacings.TRIPLE}rem`,
-
-      [mq.GROUP_2_MIN_WIDTH]: {
-        paddingLeft: `${spacings.DOUBLE}rem`,
-        paddingRight: `${spacings.DOUBLE}rem`,
-      },
-      [mq.GROUP_4_MIN_WIDTH]: {
-        paddingLeft: '0',
-        paddingRight: '0',
+      [mq.GROUP_4_MAX_WIDTH]: {
+        marginLeft: `${spacings.FULL}rem`,
+        marginRight: `${spacings.FULL}rem`,
       },
     }),
 };
