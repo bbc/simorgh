e69cf7aa6c06a59d5a3bf45d50b31b5905ecbc33
emily saffron <emily.saffron@bbc.co.uk>
2026-02-10 14:18:18 +0000

    fix build error

--- src/app/components/MediaLoader/types.ts
+++ src/app/components/MediaLoader/types.ts
@@ -8,7 +8,7 @@ import {
 } from '#app/models/types/media';
 import { OptimoImageBlock } from '#app/models/types/optimo';
 import { Translations } from '#app/models/types/translations';
-import { Dispatch, RefObject, SetStateAction } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 
 export type SMPEvent = {
   playlist?: {
