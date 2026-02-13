4e4c6d06b217bb50635c3066590de5e0b05e3288
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2026-02-13 08:29:25 +0000

    WS-NA: Removes Zhongwen readtime translations

--- src/app/lib/config/services/zhongwen.ts
+++ src/app/lib/config/services/zhongwen.ts
@@ -481,10 +481,6 @@ export const service: ZhongwenConfig = {
     },
     translations: {
       and: '和',
-      readTime: {
-        readTimePrefix: '閱讀時間',
-        minute: '分鐘',
-      },
       pagination: {
         previousPage: '前页',
         nextPage: '后页',
