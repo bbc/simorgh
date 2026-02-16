deb8bd0846765408204377d3b2cdb5e29d871c97
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2026-02-16 14:55:13 +0000

    WS-2095: Tidies

--- src/app/components/PortraitVideoCarousel/index.tsx
+++ src/app/components/PortraitVideoCarousel/index.tsx
@@ -1,5 +1,4 @@
 import { use, useCallback, useRef, useState } from 'react';
-// import { use, useCallback, useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { RequestContext } from '#app/contexts/RequestContext';
 import useViewTracker from '#app/hooks/useViewTracker';
@@ -45,18 +44,8 @@ const PortraitVideoCarousel = ({
     PortraitClipMediaBlock | undefined
   >();
   const [controlsDisplayed, setControlsDisplayed] = useState(false);
-  // const [hasShareApi, setHasShareApi] = useState(false);
-
-  // useEffect(() => {
-  //   if ('share' in navigator) {
-  //     setHasShareApi(true);
-  //   }
-  // }, []);
 
   const hasShareApi = typeof navigator !== 'undefined' && 'share' in navigator;
-
-  // eslint-disable-next-line no-console
-  console.log('hasShareApi', hasShareApi);
 
   const { isLite, isAmp, nonce } = use(RequestContext);
 
