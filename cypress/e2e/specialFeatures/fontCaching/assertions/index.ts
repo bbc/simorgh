be3793e95836fa1fb59d71953ac10be3e0951fce
Shayne Ahchoon <shayneah@googlemail.com>
2026-02-17 10:07:29 +0000

    WS-HOTFIX: Clear previously cached fonts

--- cypress/e2e/specialFeatures/fontCaching/assertions/index.ts
+++ cypress/e2e/specialFeatures/fontCaching/assertions/index.ts
@@ -5,21 +5,16 @@ export const fontsAreCached = ({ expectedFonts }) => {
   const testPrefix = expectedFonts.length > 0 ? expectedFonts : 'No ';
 
   it(`${testPrefix} fonts are cached`, () => {
-    cy.getAllLocalStorage().then(allLocalStorage => {
-      expect(allLocalStorage).not.to.be.null;
-
-      const localStorage = allLocalStorage[Cypress.config().baseUrl];
-
+    cy.window({ timeout: 10000 }).should(win => {
+      const { localStorage } = win;
       const cachedFonts = Object.keys(localStorage)
         .filter(key => key.startsWith('font-'))
         .map(key => key.split('-')[1])
         .filter(Boolean)
         .sort();
 
-      cy.log(`Cached fonts: | ${cachedFonts.join(' | ')} | `);
-
+      expect(cachedFonts).not.to.be.null;
       expect(`${cachedFonts.join(' | ')}`).not.to.be.null;
-
       expect(cachedFonts).to.include.members(expectedFonts);
     });
   });
