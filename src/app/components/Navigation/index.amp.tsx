7f35f4dc8fda2a76eb3e44aee27a6fb872274bd9
“LilyL0u” <lily.higham@bbc.co.uk>
2026-02-17 16:15:25 +0000

    amp ts error fix

--- src/app/components/Navigation/index.amp.tsx
+++ src/app/components/Navigation/index.amp.tsx
@@ -51,11 +51,12 @@ const AmpNavigationContainer: React.FC<AmpNavigationContainerProps> = ({
       script={script}
     />
     {/* Hidden attribute allows us to toggle visibility on the dropdown
-    using AMP actions. */}
+        using AMP actions. */}
     <AmpDropdown id={DROPDOWN_ID} data-e2e="dropdown-nav" hidden>
       {dropdownListItems}
     </AmpDropdown>
-    <StyledAmpScrollableNavigation dir={dir} id={SCROLLABLE_ID}>
+    {/* TODO: Implement the new navigation in AMP */}
+    <StyledAmpScrollableNavigation dir={dir} id={SCROLLABLE_ID} navType={null}>
       {scrollableListItems}
     </StyledAmpScrollableNavigation>
   </Navigation>
