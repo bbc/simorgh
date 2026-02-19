00d523d386378fb4f5035ef200e3045f6aafeaa4
Louise Archibald <louise.archibald@bbc.co.uk>
2026-02-19 13:46:09 +0000

    wip

---
+++ src/app/components/Navigation/index.canonical.test.tsx
@@ -0,0 +1,93 @@
+import latin from '../ThemeProvider/fontScripts/latin';
+import CanonicalNavigation from './index.canonical';
+import {
+  dropdownTestId,
+  scrollableTestId,
+  dropdownListItems,
+  scrollableListItems,
+} from './testHelpers';
+import { render, fireEvent } from '../react-testing-library-with-providers';
+
+const blocks = [{ id: '1', title: 'Story' }];
+
+const navigationProps = {
+  scrollableListItems,
+  dropdownListItems,
+  menuAnnouncedText: 'menu',
+  script: latin,
+  service: 'pidgin',
+  dir: 'ltr',
+};
+
+const navigation = (
+  <CanonicalNavigation
+    scrollableListItems={scrollableListItems}
+    dropdownListItems={dropdownListItems}
+    menuAnnouncedText="menu"
+    script={latin}
+    service="news"
+    dir="ltr"
+  />
+);
+
+describe('Canonical Navigation', () => {
+  describe('snapshots', () => {
+    it('should correctly render Canonical navigation', () => {
+      const { container } = render(navigation);
+      expect(container).toMatchSnapshot();
+    });
+  });
+
+  describe('assertions', () => {
+    it('should render scrollable nav and hide dropdown', () => {
+      const { queryByTestId } = render(navigation);
+      const dropdown = queryByTestId(dropdownTestId)?.parentElement;
+      const scrollableNav = queryByTestId(scrollableTestId);
+      expect(scrollableNav?.innerHTML).toBe('<li>List Items</li>');
+      expect(dropdown).toHaveAttribute('height', '0');
+    });
+
+    it('should render dropdown and no scrollable nav after menu button clicked', () => {
+      const { queryByTestId, queryByText } = render(navigation);
+
+      fireEvent.click(queryByText('menu'));
+
+      const dropdown = queryByTestId(dropdownTestId);
+      const scrollableNav = queryByTestId(scrollableTestId);
+      expect(scrollableNav).toBeNull();
+      expect(dropdown?.innerHTML).toBe('<li>Dropdown Items</li>');
+    });
+
+    describe('Top Bar OJs', () => {
+      it.each([
+        [
+          'should not render TopBarOJs when toggle is off',
+          { ...navigationProps, blocks },
+          { topBarOJs: { enabled: false } },
+          queryByTestId =>
+            expect(queryByTestId('top-bar-onward-journeys')).toBeNull(),
+        ],
+        [
+          'should render TopBarOJs when toggle is on and blocks are provided',
+          { ...navigationProps, blocks },
+          { topBarOJs: { enabled: true } },
+          queryByTestId =>
+            expect(queryByTestId('top-bar-onward-journeys')).not.toBeNull(),
+        ],
+        [
+          'should not render TopBarOJs when blocks are empty even if toggle is on',
+          { ...navigationProps, blocks: [] },
+          { topBarOJs: { enabled: true } },
+          queryByTestId =>
+            expect(queryByTestId('top-bar-onward-journeys')).toBeNull(),
+        ],
+      ])('%s', (_, props, toggles, assertion) => {
+        const { queryByTestId } = render(<CanonicalNavigation {...props} />, {
+          toggles,
+          service: props.service,
+        });
+        assertion(queryByTestId);
+      });
+    });
+  });
+});
---
+++ src/app/components/Navigation/testHelpers.tsx
@@ -0,0 +1,14 @@
+export const dropdownTestId = 'dropdown';
+export const scrollableTestId = 'scrollable-list';
+
+export const scrollableListItems = (
+  <ul data-testid={scrollableTestId}>
+    <li>List Items</li>
+  </ul>
+);
+
+export const dropdownListItems = (
+  <ul data-testid={dropdownTestId}>
+    <li>Dropdown Items</li>
+  </ul>
+);
