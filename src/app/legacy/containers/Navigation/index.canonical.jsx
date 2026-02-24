923e10ce55ea619df322c7b50d884f7473ab3a86
Santa Zena <santa.zena.ext@bbc.co.uk>
2026-02-24 13:22:12 +0200

    refactor: moving children outside the navigation

--- src/app/legacy/containers/Navigation/index.canonical.jsx
+++ src/app/legacy/containers/Navigation/index.canonical.jsx
@@ -54,28 +54,32 @@ const CanonicalNavigationContainer = ({
     }
   });
   return (
-    <Navigation script={script} service={service} dir={dir} isOpen={isOpen}>
-      <ScrollableWrapper>
-        {!isLite && (
-          <CanonicalMenuButton
-            announcedText={menuAnnouncedText}
-            isOpen={isOpen}
-            onClick={() => setIsOpen(!isOpen)}
-            dir={dir}
-            script={script}
-          />
-        )}
-        {!isOpen && (
-          <ScrollableNavigation dir={dir}>
-            {scrollableListItems}
-          </ScrollableNavigation>
-        )}
-      </ScrollableWrapper>
-      {children}
-      <CanonicalDropdown isOpen={isOpen}>{dropdownListItems}</CanonicalDropdown>
-      <Divider />
+    <>
+      <Navigation script={script} service={service} dir={dir} isOpen={isOpen}>
+        <ScrollableWrapper>
+          {!isLite && (
+            <CanonicalMenuButton
+              announcedText={menuAnnouncedText}
+              isOpen={isOpen}
+              onClick={() => setIsOpen(!isOpen)}
+              dir={dir}
+              script={script}
+            />
+          )}
+          {!isOpen && (
+            <ScrollableNavigation dir={dir}>
+              {scrollableListItems}
+            </ScrollableNavigation>
+          )}
+        </ScrollableWrapper>
+        <CanonicalDropdown isOpen={isOpen}>
+          {dropdownListItems}
+        </CanonicalDropdown>
+        <Divider />
+      </Navigation>
       {enabled && <TopBarOJs blocks={blocks} />}
-    </Navigation>
+      {children}
+    </>
   );
 };
 
