5781bef15dda922ba67e8fb0d36a9393cd7f3ce5
Pedram Valiani <pedram.valiani@bbc.co.uk>
2026-02-26 17:01:32 +0000

    remove oj referrer code and map code not in use

--- src/app/components/OptimizelyPageMetrics/experimentsForPageMetrics.ts
+++ src/app/components/OptimizelyPageMetrics/experimentsForPageMetrics.ts
@@ -1,5 +1,5 @@
 import { PageTypes } from '#app/models/types/global';
-import { ARTICLE_PAGE, MEDIA_ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
+import { ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
 // Any running serverside and client side experiments which collect Optimizely Page Metrics; page view, page complete, scroll depth
 // Includes PageType so that different experiments can be run on different pageTypes
 
@@ -11,15 +11,9 @@ type ExperimentsForPageTypeMetrics = {
 const experimentsForPageMetrics: ExperimentsForPageTypeMetrics = [
   {
     // include tod2 so page-level metrics also fire on article pages for this experiment
-    // can prob remove referrer now?
     pageType: ARTICLE_PAGE,
-    activeExperiments: ['newswb_ws_oj_by_referrer', 'newswb_ws_tod_article_2'],
+    activeExperiments: ['newswb_ws_tod_article_2'],
   },
-  {
-    pageType: MEDIA_ARTICLE_PAGE,
-    activeExperiments: ['newswb_ws_oj_by_referrer'],
-  },
-  // TODO add map experiment names when they are confirmed
 ];
 
 export default experimentsForPageMetrics;
