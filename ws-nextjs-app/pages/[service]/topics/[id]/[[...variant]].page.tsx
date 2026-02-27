5c617883f23014961a96a341dcc7ea6482d8392e
Aaron Moore <aaron.moore@bbc.co.uk>
2026-02-27 10:41:34 +0000

    Remove Topic override logic for now

--- ws-nextjs-app/pages/[service]/topics/[id]/[[...variant]].page.tsx
+++ ws-nextjs-app/pages/[service]/topics/[id]/[[...variant]].page.tsx
@@ -39,7 +39,7 @@ export const getServerSideProps = async (
     page,
     service,
     variant,
-    rendererEnv: 'live',
+    rendererEnv,
     resolvedUrl: context.resolvedUrl,
     pageType: TOPIC_PAGE,
   });
@@ -87,7 +87,6 @@ export const getServerSideProps = async (
 
   return {
     props: {
-      isSportPage: context.resolvedUrl.includes('cz74kjgv220t'),
       error: data?.error || null,
       id,
       page: page || null,
