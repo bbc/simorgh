bdb4dcfc3a5237130b76789300cdf60ef623aa3f
Pedram Valiani <pedram.valiani@bbc.co.uk>
2026-02-27 14:17:51 +0000

    linting

--- src/app/components/OptimizelyPageMetrics/experimentsForPageMetrics.ts
+++ src/app/components/OptimizelyPageMetrics/experimentsForPageMetrics.ts
@@ -1,8 +1,5 @@
 import { PageTypes } from '#app/models/types/global';
-import {
-  ARTICLE_PAGE,
-  MEDIA_ARTICLE_PAGE,
-} from '#app/routes/utils/pageTypes';
+import { ARTICLE_PAGE, MEDIA_ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
 // Any running serverside and client side experiments which collect Optimizely Page Metrics; page view, page complete, scroll depth
 // Includes PageType so that different experiments can be run on different pageTypes
 
