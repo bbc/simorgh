2e58cdf7bab55491ece66f398e3e42539d34b4d8
Aaron Moore <aaron.moore@bbc.co.uk>
2026-03-04 10:28:56 +0000

    Restructure AMP version of new nav

--- src/app/components/Navigation/index.amp.tsx
+++ src/app/components/Navigation/index.amp.tsx
@@ -7,8 +7,8 @@ import {
 } from '#psammead/psammead-navigation/src/DropdownNavigation';
 import { GEL_GROUP_2_SCREEN_WIDTH_MAX } from '#psammead/gel-foundations/src/breakpoints';
 import styled from '@emotion/styled';
-
 import { Direction } from '#app/models/types/global';
+import styles from './index.styles';
 
 const DROPDOWN_ID = 'si-nav-dropdown-menu';
 const NAVIGATION_ID = 'si-nav';
@@ -36,36 +36,52 @@ type AmpNavigationContainerProps = {
 const AmpNavigationContainer: React.FC<AmpNavigationContainerProps> = ({
   dir,
   menuAnnouncedText,
+  topScrollableListItems,
   bottomScrollableListItems,
   dropdownListItems,
 }) => (
-  <Navigation
-    dir={dir}
-    id={NAVIGATION_ID}
-    ampOpenClass={OPEN_CLASS_NAME as any}
-  >
-    <AmpMenuButton
-      announcedText={menuAnnouncedText}
-      onToggle={`
-        ${DROPDOWN_ID}.toggleVisibility,
-        ${SCROLLABLE_ID}.toggleClass(class=${HIDDEN_CLASS_NAME}),
-        ${NAVIGATION_ID}.toggleClass(class=${OPEN_CLASS_NAME})
-      `}
-      dir={dir}
-    />
-    {/* Hidden attribute allows us to toggle visibility on the dropdown
-        using AMP actions. */}
-    <AmpDropdown id={DROPDOWN_ID} data-e2e="dropdown-nav" hidden>
-      {dropdownListItems}
-    </AmpDropdown>
-    {/* TODO: Implement the new navigation in AMP */}
-    <StyledAmpScrollableNavigation
-      dir={dir}
-      id={SCROLLABLE_ID}
-      navPosition={null}
-    >
-      {bottomScrollableListItems}
-    </StyledAmpScrollableNavigation>
+  <Navigation dir={dir} id={NAVIGATION_ID} ampOpenClass={OPEN_CLASS_NAME}>
+    <div css={styles.navStack}>
+      <div style={{ position: 'relative', width: '100%' }}>
+        <div css={styles.topRow}>
+          <StyledAmpScrollableNavigation
+            dir={dir}
+            css={styles.topRowItems}
+            navPosition="primary"
+          >
+            {topScrollableListItems}
+          </StyledAmpScrollableNavigation>
+          <AmpMenuButton
+            css={styles.menuButton}
+            announcedText={menuAnnouncedText}
+            onToggle={`
+              ${DROPDOWN_ID}.toggleVisibility,
+              ${SCROLLABLE_ID}.toggleClass(class=${HIDDEN_CLASS_NAME}),
+              ${NAVIGATION_ID}.toggleClass(class=${OPEN_CLASS_NAME})
+            `}
+            dir={dir}
+          />
+        </div>
+        <AmpDropdown
+          id={DROPDOWN_ID}
+          data-e2e="dropdown-nav"
+          css={styles.dropdown}
+          hidden
+        >
+          {dropdownListItems}
+        </AmpDropdown>
+      </div>
+      <div css={styles.lowerNavWrapper}>
+        <StyledAmpScrollableNavigation
+          dir={dir}
+          id={SCROLLABLE_ID}
+          navPosition={null}
+          css={styles.bottomRowItems}
+        >
+          {bottomScrollableListItems}
+        </StyledAmpScrollableNavigation>
+      </div>
+    </div>
   </Navigation>
 );
 
