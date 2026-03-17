2d962883aea2ede47faf5b08af91f39829f83366
alex-magana <alex.magana@andela.com>
2026-03-17 22:45:27 +0300

    Test byline rendering for post contributors

--- src/app/components/Byline/utilities/bylineExtractor/index.test.tsx
+++ src/app/components/Byline/utilities/bylineExtractor/index.test.tsx
@@ -1,61 +1,140 @@
-import bylineExtractor from '.';
+import { bylineSamplePost } from 'simorgh-nextjs/pages/[service]/live/[id]/Post/fixture';
 import {
   bylineWithNoAuthor,
   bylineWithLink,
   bylineWithNonPngPhoto,
   bylineWithMultipleContributors,
 } from '../../../../pages/ArticlePage/fixtureData';
+import {
+  ARTICLE_PAGE,
+  LIVE_PAGE,
+  MEDIA_ARTICLE_PAGE,
+} from '../../../../routes/utils/pageTypes';
+import filterForBlockType from '../../../../lib/utilities/blockHandlers';
+import bylineExtractor from '.';
 
 describe('bylineExtractor', () => {
-  it('should return an empty array when authorName is not present', () => {
-    const bylineValues = bylineExtractor(bylineWithNoAuthor);
-    expect(bylineValues).toHaveLength(0);
+  describe('Article Page & Media Article Page', () => {
+    [ARTICLE_PAGE, MEDIA_ARTICLE_PAGE].forEach(pageType => {
+      it('should return an empty array when authorName is not present', () => {
+        const bylineValues = bylineExtractor({
+          blocks: bylineWithNoAuthor,
+          pageType,
+        });
+        expect(bylineValues).toHaveLength(0);
+      });
+
+      it('should return an array with all contributors containing all byline data', () => {
+        const bylineValues = bylineExtractor({
+          blocks: bylineWithMultipleContributors,
+          pageType,
+        });
+
+        const sampleContributor = {
+          authorImage:
+            'https://ichef.bbci.co.uk/ace/ws/160/cpsprodpb/f974/live/36226e20-94aa-11ec-9acc-37a09ce5ea88.png.webp',
+          authorName: 'Mayeni Jones',
+          authorTopicUrl: '/news/topics/c8qx38nq177t',
+          jobRole: 'Journalist',
+          location: 'Lagos, Nigeria',
+          twitterLink: 'https://twitter.com/MayeniJones',
+          twitterText: 'MayeniJones',
+        };
+
+        expect(bylineValues).toHaveLength(5);
+        expect(bylineValues).toEqual([
+          sampleContributor,
+          sampleContributor,
+          sampleContributor,
+          sampleContributor,
+          sampleContributor,
+        ]);
+      });
+
+      it('should return an array populated with objects containing byline data, with some optional fields missing', () => {
+        const bylineValues = bylineExtractor({
+          blocks: bylineWithLink,
+          pageType,
+        });
+
+        expect(bylineValues).toEqual([
+          {
+            authorImage: '',
+            authorName: 'Single Byline (all values)',
+            authorTopicUrl: '/news/topics/c8qx38nq177t',
+            jobRole: 'Test',
+            location: '',
+            twitterLink: 'https://twitter.com/test',
+            twitterText: 'test',
+          },
+        ]);
+      });
+      it('should return an object, with an empty string in the image field if no .png extension', () => {
+        const bylineValues = bylineExtractor({
+          blocks: bylineWithNonPngPhoto,
+          pageType,
+        });
+
+        const { authorImage } = bylineValues?.[0] || {};
+
+        expect(authorImage).toEqual('');
+      });
+    });
   });
 
-  it('should return an array with all contributors containing all byline data', () => {
-    const bylineValues = bylineExtractor(bylineWithMultipleContributors);
+  describe('Live Page', () => {
+    it('should return an array with the byline data for the post contributor', () => {
+      const {
+        header: {
+          model: { blocks: postHeaderBlocks },
+        },
+      } = bylineSamplePost;
+      const { model: contributorData } = filterForBlockType(
+        postHeaderBlocks,
+        'contributor',
+      );
 
-    const sampleContributor = {
-      authorImage:
-        'https://ichef.bbci.co.uk/ace/ws/160/cpsprodpb/f974/live/36226e20-94aa-11ec-9acc-37a09ce5ea88.png.webp',
-      authorName: 'Mayeni Jones',
-      authorTopicUrl: '/news/topics/c8qx38nq177t',
-      jobRole: 'Journalist',
-      location: 'Lagos, Nigeria',
-      twitterLink: 'https://twitter.com/MayeniJones',
-      twitterText: 'MayeniJones',
-    };
+      const bylineValues = bylineExtractor({
+        blocks: [contributorData],
+        pageType: LIVE_PAGE,
+      });
 
-    expect(bylineValues).toHaveLength(5);
-    expect(bylineValues).toEqual([
-      sampleContributor,
-      sampleContributor,
-      sampleContributor,
-      sampleContributor,
-      sampleContributor,
-    ]);
-  });
+      const sampleContributor = {
+        authorName: 'Gahuza contributor',
+        jobRole: 'gahuza contributor',
+        authorImage:
+          'https://ichef.bbci.co.uk/ace/ws/160/cpsdevpb//vivo/test/images/2016/12/12/977af52a-6eaf-481f-9a06-094860d56760.jpg.webp',
+      };
 
-  it('should return an array populated with objects containing byline data, with some optional fields missing', () => {
-    const bylineValues = bylineExtractor(bylineWithLink);
+      expect(bylineValues).toHaveLength(1);
+      expect(bylineValues).toEqual([sampleContributor]);
+    });
 
-    expect(bylineValues).toEqual([
-      {
-        authorImage: '',
-        authorName: 'Single Byline (all values)',
-        authorTopicUrl: '/news/topics/c8qx38nq177t',
-        jobRole: 'Test',
-        location: '',
-        twitterLink: 'https://twitter.com/test',
-        twitterText: 'test',
-      },
-    ]);
-  });
-  it('should return an object, with an empty string in the image field if no .png extension', () => {
-    const bylineValues = bylineExtractor(bylineWithNonPngPhoto);
+    it('should return an array with the byline data for the post contributor where the image is missing', () => {
+      const {
+        header: {
+          model: { blocks: postHeaderBlocks },
+        },
+      } = bylineSamplePost;
+      const { model: contributorData } = filterForBlockType(
+        postHeaderBlocks,
+        'contributor',
+      );
 
-    const { authorImage } = bylineValues?.[0] || {};
+      const bylineValues = bylineExtractor({
+        blocks: [contributorData],
+        pageType: LIVE_PAGE,
+      });
 
-    expect(authorImage).toEqual('');
+      const sampleContributor = {
+        authorName: 'Gahuza contributor',
+        jobRole: 'gahuza contributor',
+        authorImage:
+          'https://ichef.bbci.co.uk/ace/ws/160/cpsdevpb//vivo/test/images/2016/12/12/977af52a-6eaf-481f-9a06-094860d56760.jpg.webp',
+      };
+
+      expect(bylineValues).toHaveLength(1);
+      expect(bylineValues).toEqual([sampleContributor]);
+    });
   });
 });
