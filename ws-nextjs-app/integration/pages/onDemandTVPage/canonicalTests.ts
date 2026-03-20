1cb7b8febce1a0aa8c6ac2724708e6f139420d5a
Aaron Moore <aaron.moore@bbc.co.uk>
2026-03-20 09:19:25 +0000

    WS-2222 - ODTV Next.js integration tests

--- ws-nextjs-app/integration/pages/onDemandTVPage/canonicalTests.ts
+++ ws-nextjs-app/integration/pages/onDemandTVPage/canonicalTests.ts
@@ -13,6 +13,6 @@ export default () => {
   runCanonicalAnalyticsTests();
   runCommonCrossPlatformTests(service);
   runCommonEpisodeTests();
-  runMediaPlayerTests();
+  runMediaPlayerTests(pageType);
   runRecentEpisodesTests();
 };
