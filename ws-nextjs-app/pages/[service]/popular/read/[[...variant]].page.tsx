65dcfae8db59ac22721cab40d8d10305ff4b94ac
Aaron Moore <aaron.moore@bbc.co.uk>
2026-03-26 10:54:19 +0000

    Update [[...variant]].page.tsx

--- ws-nextjs-app/pages/[service]/popular/read/[[...variant]].page.tsx
+++ ws-nextjs-app/pages/[service]/popular/read/[[...variant]].page.tsx
@@ -9,7 +9,7 @@ import PageDataParams from '#app/models/types/pageDataParams';
 import deriveVariant from '#nextjs/utilities/deriveVariant';
 import isTest from '#app/lib/utilities/isTest';
 import handleError from '#app/routes/utils/handleError';
-import getPageData from '../../../../utilities/pageRequests/getPageData';
+import getPageData from '#nextjs/utilities/pageRequests/getPageData';
 
 const TopicPage = dynamic(() => import('#app/pages/TopicPage/TopicPage'));
 
