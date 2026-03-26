79a8812ef8398fbfa20bd2c7ddd4c8c5a8e6b656
Aaron Moore <aaron.moore@bbc.co.uk>
2026-03-26 13:27:56 +0000

    Revert "Update `constructPageFetchUrl` to account for `mostReadTopic` ID"
    This reverts commit fd225cb77e7252d163d55db8bc61cfd4f3a4e070.

--- src/app/routes/utils/constructPageFetchUrl/index.ts
+++ src/app/routes/utils/constructPageFetchUrl/index.ts
@@ -98,9 +98,6 @@ const getId = ({ pageType, service, variant }: GetIdProps) => {
 
     case TOPIC_PAGE:
       getIdFunction = (path: string) => {
-        // Special case for Most Read pages which are actually Topic pages
-        if (path === 'mostReadTopic') return path;
-
         return getTipoId(path);
       };
       break;
@@ -240,22 +237,35 @@ const constructPageFetchUrl = ({
       case CPS_ASSET:
       case AUDIO_PAGE:
       case TV_PAGE:
-        fetchUrl = Url(`${host}${port}/api/local/${id}`);
+        if (process.env?.NEXTJS) {
+          fetchUrl = Url(`${host}${port}/api/local/${id}`);
+        } else {
+          fetchUrl = Url(`/${id}`);
+        }
         break;
       case HOME_PAGE: {
-        fetchUrl = Url(
-          `${host}${port}/api/local/${service}/homePage/${variant ? `${variant}` : 'index'}`,
-        );
+        if (process.env?.NEXTJS) {
+          fetchUrl = Url(
+            `${host}${port}/api/local/${service}/homePage/${variant ? `${variant}` : 'index'}`,
+          );
+        } else {
+          fetchUrl = Url(`/${service}${variant ? `/${variant}` : ''}`);
+        }
         break;
       }
       case MOST_READ_PAGE:
         fetchUrl = Url(getMostReadEndpoint({ service, variant }).split('.')[0]);
         break;
       case TOPIC_PAGE: {
-        fetchUrl = Url(
-          `${host}${port}/api/local/${service}/topics/${id}${variant ? `/${variant}` : ''}`,
-        );
-
+        if (process.env?.NEXTJS) {
+          fetchUrl = Url(
+            `${host}${port}/api/local/${service}/topics/${id}${variant ? `/${variant}` : ''}`,
+          );
+        } else {
+          fetchUrl = Url(
+            `/${service}/topics/${id}${variant ? `/${variant}` : ''}`,
+          );
+        }
         break;
       }
       case LIVE_PAGE: {
