227c17772e07fa6af9cba9a025033aa33df96349
emily saffron <emily.saffron@bbc.co.uk>
2026-03-27 12:18:08 +0000

    remove ramda

--- src/app/hooks/useLivePagePolling/makeRequest/makeRequest.ts
+++ src/app/hooks/useLivePagePolling/makeRequest/makeRequest.ts
@@ -1,24 +1,20 @@
-import { map, pipe } from 'ramda';
 import { getEnvConfig } from '#app/lib/utilities/getEnvConfig';
 import handlePostBlocks from '../transformers/handlePostBlocks';
 import addIndexesToEmbeds from '../transformers/addIndexesToEmbeds';
 
+const transformPost = post => addIndexesToEmbeds(handlePostBlocks(post));
+
 export default async (liveTextStreamId: string) => {
   try {
     const webCdnHost = getEnvConfig().WEB_CDN_URL;
-
     const fetchUrl = `${webCdnHost}/fd/stream?liveTextStreamId=${liveTextStreamId}&type=curated`;
+
     const response = await fetch(fetchUrl);
     const { status } = response;
     const { data } = await response.json();
 
     if (status === 200 && data.results.length > 0) {
-      const formattedData = map(
-        pipe(handlePostBlocks, addIndexesToEmbeds),
-        data.results,
-      );
-
-      data.results = formattedData;
+      data.results = data.results.map(transformPost);
       return data;
     }
 
