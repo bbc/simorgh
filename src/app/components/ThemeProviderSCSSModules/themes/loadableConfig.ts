ab51c9106d0e8e3e2d93407dee40c01d8fde3801
Andrew Bennett <andrew.bennett07@bbc.co.uk>
2026-03-27 14:22:09 +0000

    portuguese theme migrated [copilot]

--- src/app/components/ThemeProviderSCSSModules/themes/loadableConfig.ts
+++ src/app/components/ThemeProviderSCSSModules/themes/loadableConfig.ts
@@ -4,6 +4,12 @@ export const themes = {
   mundo: loadable(
     () => import(/* webpackChunkName: "themes-mundo" */ './mundo/mundo'),
   ),
+  portuguese: loadable(
+    () =>
+      import(
+        /* webpackChunkName: "themes-portuguese" */ './portuguese/portuguese'
+      ),
+  ),
 };
 
 export default themes;
