8e5783bdd7bb6e4ef3e9aa53640873f174e5328e
jinidev <jinimol.joseph.ext@bbc.co.uk>
2026-03-30 13:33:57 +0300

    eXtra logs for getAUthHeader

--- src/app/lib/uasApi/getAuthHeader.ts
+++ src/app/lib/uasApi/getAuthHeader.ts
@@ -4,7 +4,9 @@ import { getEnvConfig } from '../utilities/getEnvConfig';
 const getAuthHeaders = (): Record<string, string> => {
   const cknsAtkn = Cookie.get('ckns_atkn');
   const apiKey = getEnvConfig().SIMORGH_UAS_PUBLIC_API_KEY;
-
+  // Logging for debugging - will remove before merge
+  // eslint-disable-next-line no-console
+  console.log('getAuthHeaders', { cknsAtkn, apiKey });
   if (!cknsAtkn || !apiKey) {
     throw new Error('Missing authentication for UAS request');
   }
