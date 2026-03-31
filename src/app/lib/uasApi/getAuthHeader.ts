ceabea1e24c85b4f657e496acb04770bf01b6193
jinidev <jinimol.joseph.ext@bbc.co.uk>
2026-03-31 18:26:11 +0300

    remving cknsAtkn from header

--- src/app/lib/uasApi/getAuthHeader.ts
+++ src/app/lib/uasApi/getAuthHeader.ts
@@ -1,18 +1,18 @@
-import Cookie from 'js-cookie';
-import onClient from '#app/lib/utilities/onClient';
+// import Cookie from 'js-cookie';
+// import onClient from '#app/lib/utilities/onClient';
 import { getEnvConfig } from '../utilities/getEnvConfig';
 
 const getAuthHeaders = (): Record<string, string> => {
-  const cknsAtkn = onClient() ? Cookie.get('ckns_atkn') : undefined;
+  //   const cknsAtkn = onClient() ? Cookie.get('ckns_atkn') : undefined;
 
   const apiKey = getEnvConfig().SIMORGH_UAS_PUBLIC_API_KEY;
 
-  if (!cknsAtkn || !apiKey) {
+  if (!apiKey) {
     throw new Error('Missing authentication for UAS request');
   }
 
   const headers: Record<string, string> = {
-    Authorization: `Bearer ${cknsAtkn}`,
+    // Authorization: `Bearer ${cknsAtkn}`,
     'X-Authentication-Provider': 'idv5',
     'X-API-Key': apiKey,
   };
