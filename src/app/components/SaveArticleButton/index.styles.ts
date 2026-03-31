02e338b791b359505d099275d163b4b42a28183e
jinidev <jinimol.joseph.ext@bbc.co.uk>
2026-03-31 12:40:52 +0300

    Style updates to button

--- src/app/components/SaveArticleButton/index.styles.ts
+++ src/app/components/SaveArticleButton/index.styles.ts
@@ -1,10 +1,17 @@
+import { css, Theme } from '@emotion/react';
 /** Temporary css styling until UX work is complete */
 const styles = {
-  buttonWrapper: {
-    display: 'flex',
-    marginTop: '1rem',
-    marginBottom: '1rem',
-  },
+  buttonWrapper: ({ mq }: Theme) =>
+    css({
+      display: 'flex',
+      marginTop: '1rem',
+      marginBottom: '1rem',
+      padding: '1rem',
+      marginLeft: '0.5rem',
+      [mq.GROUP_4_MIN_WIDTH]: {
+        marginLeft: 0,
+      },
+    }),
 };
 
 export default styles;
