0ee94fe645aa59161361157d493e6d4400b038bf
alex-magana <alex.magana@andela.com>
2026-04-06 17:13:18 +0300

    Add playwright sample test

---
+++ ws-nextjs-app/test/e2e/playwright.config.ts
@@ -0,0 +1,4 @@
+import { config } from '@bbc/unified-web-e2e-framework';
+config.testDir = 'specs';
+
+export default config;
---
+++ ws-nextjs-app/test/e2e/specs/setupCheck.spec.ts
@@ -0,0 +1,11 @@
+import { test } from '@bbc/unified-web-e2e-framework';
+import { expect } from '@playwright/test';
+
+test('Verify page title', async ({ page }) => {
+  await page.goto('https://www.bbc.co.uk/');
+  expect(await page.title()).toEqual(
+    expect.stringMatching(
+      /(BBC - Home|BBC Home - Breaking News, World News, US News, Sports, Business, Innovation, Climate, Culture, Travel, Video & Audio)/,
+    ),
+  );
+});
