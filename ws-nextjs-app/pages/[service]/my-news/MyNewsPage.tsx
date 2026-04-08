2ea64b806e5315cadac01221ca1a11a6af05ac3e
Elvinas Valenas <elvinas.valenas.ext@bbc.co.uk>
2026-04-08 14:55:12 +0300

    style: my news page styles

--- ws-nextjs-app/pages/[service]/my-news/MyNewsPage.tsx
+++ ws-nextjs-app/pages/[service]/my-news/MyNewsPage.tsx
@@ -1,5 +1,16 @@
+import Heading from '#app/components/Heading';
+import styles from './styles';
+
 const MyNewsPage = () => {
-  return <main>My News</main>;
+  return (
+    <main css={styles.main}>
+      <div css={styles.inner}>
+        <Heading level={2} css={styles.heading}>
+          My News
+        </Heading>
+      </div>
+    </main>
+  );
 };
 
 export default MyNewsPage;
