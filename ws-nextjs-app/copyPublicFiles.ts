1aa046516465d4086a868db9324d09a2cb0b1edb
Aaron Moore <aaron.moore@bbc.co.uk>
2026-04-13 11:05:37 +0100

    Add comment

--- ws-nextjs-app/copyPublicFiles.ts
+++ ws-nextjs-app/copyPublicFiles.ts
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 /* eslint-disable @typescript-eslint/no-var-requires */
 
+/* 
+  Copies /public folder from root directory into the Next.js standalone output folder.
+  This allows Next.js to serve the files in public (namely sw.js and manifest.json).
+  Excludes the fonts and images directories as these are served from the CDN/static-assets route.
+*/
+
 const { cpSync } = require('fs');
 const { basename } = require('path');
 
