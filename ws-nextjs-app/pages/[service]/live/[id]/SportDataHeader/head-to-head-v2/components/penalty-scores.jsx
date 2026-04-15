a7739d989bf0791708f979356b877a904b0c28c3
alex-magana <alex.magana@andela.com>
2026-04-15 20:46:56 +0300

    Add PenaltyScores component

--- ws-nextjs-app/pages/[service]/live/[id]/SportDataHeader/head-to-head-v2/components/penalty-scores.jsx
+++ ws-nextjs-app/pages/[service]/live/[id]/SportDataHeader/head-to-head-v2/components/penalty-scores.jsx
@@ -1,38 +1,59 @@
-import React from 'react';
-import styled, { css } from '@bbc/web-styled';
-import {
-  GROUP_3,
-  SPACING_1,
-  SPACING_2,
-  fontEmphasised,
-  fontScaleBody,
-  fontScaleDescription
-} from '@bbc/web-gel-foundations';
-import VisuallyHidden from '@bbc/web-components/visually-hidden/index.js';
+// import React from 'react';
+// import styled, { css } from '@bbc/web-styled';
+
+import styled from '@emotion/styled';
+// import { css } from '@emotion/react';
+
+// import {
+//   GROUP_3,
+//   SPACING_1,
+//   SPACING_2,
+//   fontEmphasised,
+//   fontScaleBody,
+//   fontScaleDescription,
+// } from '@bbc/web-gel-foundations';
+// import VisuallyHidden from '@bbc/web-components/visually-hidden/index.js';
+
+// eslint-disable-next-line import/no-relative-packages
+import VisuallyHiddenText from '../../../../../../../../src/app/components/VisuallyHiddenText';
+
+// eslint-disable-next-line import/no-relative-packages
+import pixelsToRem from '../../../../../../../../src/app/utilities/pixelsToRem';
 
 const PenaltyScoresContainer = styled.div`
-  ${fontScaleBody}
+  font-size: 1rem;
+  line-height: 1.375;
   text-align: center;
-  padding: ${SPACING_1} 0;
+  padding: '4px 0';
 
-  @media (min-width: ${GROUP_3}) {
-    padding-bottom: ${SPACING_2};
+  @media (min-width: '${pixelsToRem(600)}rem') {
+    padding-bottom: '8px';
   }
 
-  ${({ isConciseView }) =>
+  ${
+    '' /* ${({ isConciseView }) =>
     isConciseView &&
     css`
       ${fontScaleDescription}
-    `}
+    `} */
+  }
 `;
 
 const WinningTeamName = styled.span`
-  ${fontEmphasised}
-  color: ${({ theme, isConciseView }) => (isConciseView ? theme.colourPalette.primary : theme.colourPalette.accent)};
+  ${'' /* ${fontEmphasised} */}
+  font-family: 'ReithSans, Helvetica, Arial, freesans, sans-serif';
+  font-weight: 700;
+  font-feature-settings: 'ss01' off;
+  ${
+    '' /* color: ${({ theme, isConciseView }) =>
+    isConciseView ? theme.colourPalette.primary : theme.colourPalette.accent}; */
+  }
+  color: "#FFD230";
 `;
 
 const PenaltiesText = styled.div`
-  color: ${({ theme }) => theme.colourPalette.secondary};
+  ${'' /* color: ${({ theme }) => theme.colourPalette.secondary}; */}
+  color: "#A8A8A8";
 `;
 
 const PenaltyScores = ({ data, isConciseView }) => {
@@ -43,23 +64,33 @@ const PenaltyScores = ({ data, isConciseView }) => {
   const isDrawWithNoSeriesWinner = winner === 'draw' && !seriesWinner;
   const isMultiLegWithNoSeriesWinner = multiLeg?.leg > 1 && !seriesWinner;
 
-  if (!isPostEvent || !hasWinner || isDrawWithNoSeriesWinner || isMultiLegWithNoSeriesWinner) {
+  if (
+    !isPostEvent ||
+    !hasWinner ||
+    isDrawWithNoSeriesWinner ||
+    isMultiLegWithNoSeriesWinner
+  ) {
     return null;
   }
 
   const winnerOnPenalties = seriesWinner ?? winner;
-  const loserOnPenalties = winnerOnPenalties.toLowerCase() === 'home' ? 'away' : 'home';
+  const loserOnPenalties =
+    winnerOnPenalties.toLowerCase() === 'home' ? 'away' : 'home';
   const winnerOnPenaltiesName = data[winnerOnPenalties].fullName;
-  const winnerOnPenaltiesScore = data[winnerOnPenalties].runningScores.penaltyShootout;
-  const loserOnPenaltiesScore = data[loserOnPenalties].runningScores.penaltyShootout;
+  const winnerOnPenaltiesScore =
+    data[winnerOnPenalties].runningScores.penaltyShootout;
+  const loserOnPenaltiesScore =
+    data[loserOnPenalties].runningScores.penaltyShootout;
 
   return (
     <PenaltyScoresContainer isConciseView={isConciseView}>
-      <VisuallyHidden>
+      <VisuallyHiddenText>
         {`${winnerOnPenaltiesName} win ${winnerOnPenaltiesScore} - ${loserOnPenaltiesScore} on penalties`}
-      </VisuallyHidden>
+      </VisuallyHiddenText>
       <PenaltiesText aria-hidden="true" data-testid="penalties-text">
-        <WinningTeamName isConciseView={isConciseView}>{`${winnerOnPenaltiesName}`}</WinningTeamName>
+        <WinningTeamName
+          isConciseView={isConciseView}
+        >{`${winnerOnPenaltiesName}`}</WinningTeamName>
         {` win ${winnerOnPenaltiesScore}-${loserOnPenaltiesScore} on pens`}
       </PenaltiesText>
     </PenaltyScoresContainer>
