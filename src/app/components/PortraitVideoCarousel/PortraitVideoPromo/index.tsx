b493b7e1d81847a5b269b5a0a5b94c3e2e1ed90d
Aaron Moore <aaron.moore@bbc.co.uk>
2026-04-15 11:45:56 +0100

    FIX: Only render PV carousel on hydration

--- src/app/components/PortraitVideoCarousel/PortraitVideoPromo/index.tsx
+++ src/app/components/PortraitVideoCarousel/PortraitVideoPromo/index.tsx
@@ -12,6 +12,7 @@ import useViewTracker from '#app/hooks/useViewTracker';
 import getSrcSets from '#app/utilities/getSrcSets';
 import { PortraitClipMediaBlock } from '#app/components/MediaLoader/types';
 import { EventTrackingData } from '#app/lib/analyticsUtils/types';
+import useHydrationDetection from '#app/hooks/useHydrationDetection';
 import styles from './index.styles';
 
 const DEFAULT_TRANSLATION = {
@@ -37,6 +38,8 @@ export default ({
   // EXPERIMENT: Portrait Video Homepage Play Duration Sizing
   playDurationVariation,
 }: PortraitVideoPromoProps) => {
+  const isHydrated = useHydrationDetection();
+
   const { mq } = useTheme();
   const {
     defaultImage,
@@ -125,6 +128,8 @@ export default ({
     if (clickTrackerHandler) clickTrackerHandler(e);
     if (onClick) onClick();
   };
+
+  if (!isHydrated) return null;
 
   return (
     <li css={styles.container}>
