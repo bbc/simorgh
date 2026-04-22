d1a90fc4bf28d0a0589b3fd61a13deecb5f5985d
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2026-04-22 10:23:50 +0100

    WS-2542: Add fixture for status stories

---
+++ ws-nextjs-app/pages/[service]/live/[id]/SportDataHeader/head-to-head-v2/static-data/event/fabl/post-event/index.js
@@ -0,0 +1,17 @@
+import postEventAetFablData from './aet.json';
+import postEventAgg90FablData from './agg-90.json';
+import postEventAggAetFablData from './agg-aet.json';
+import postEventPens90FablData from './pens-90.json';
+import postEventPensAetFablData from './pens-aet.json';
+import postEventPensAetAggFablData from './pens-aet-agg.json';
+import postEventFablData from './post-event.json';
+
+export {
+  postEventAetFablData,
+  postEventAgg90FablData,
+  postEventAggAetFablData,
+  postEventPens90FablData,
+  postEventPensAetFablData,
+  postEventPensAetAggFablData,
+  postEventFablData
+};
---
+++ ws-nextjs-app/pages/[service]/live/[id]/SportDataHeader/head-to-head-v2/static-data/event/transformed/post-event/index.js
@@ -0,0 +1,19 @@
+import postEventAETData from './aet.json';
+import postEventAgg90Data from './agg-90.json';
+import finishedAetAggData from './agg-aet.json';
+import postEventCancelledData from './cancelled.json';
+import postEventPens90Data from './pens-90.json';
+import postEventPensAetData from './pens-aet.json';
+import postEventPensAetAggData from './pens-aet-agg.json';
+import postEventData from './post-event.json';
+
+export {
+  postEventAETData,
+  postEventAgg90Data,
+  finishedAetAggData,
+  postEventCancelledData,
+  postEventPens90Data,
+  postEventPensAetData,
+  postEventPensAetAggData,
+  postEventData
+};
