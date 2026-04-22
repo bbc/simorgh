205c0177806e5a1963781724a568cb3f0899a019
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2026-04-22 11:46:27 +0100

    WS-2524: Remove surplus fixture data

--- ws-nextjs-app/pages/[service]/live/[id]/SportDataHeader/head-to-head-v2/static-data/transformed/rugby-event/index.js
+++
@@ -1,5 +0,0 @@
-export { rugbyUnionPostEvent, homeKeyEventsData, awayKeyEventsData } from './rugby-union-post-event.js';
-export { rugbyUnionPreEvent } from './rugby-union-pre-event.js';
-export { cancelledRugbyUnionEvent } from './rugby-union-cancelled-event.js';
-export { rugbyUnionMidEvent } from './rugby-union-mid-event.js';
-export { rugbyUnionMidEventHalfTime } from './rugby-union-mid-event-half-time.js';
