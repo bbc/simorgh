baee6f62e158eb03d00892725bfe9beb49d13ebd
jinidev <jinimol.joseph.ext@bbc.co.uk>
2026-04-23 14:48:27 +0300

    removed , fixed linting issue

--- src/app/pages/ArticlePage/ArticlePage.tsx
+++ src/app/pages/ArticlePage/ArticlePage.tsx
@@ -344,7 +344,7 @@ const ArticlePage = ({ pageData }: { pageData: Article }) => {
 
   const showPortraitVideoCarousel = Boolean(
     pageData?.portraitVideoItems?.portraitVideo?.blocks?.length &&
-      articlePortraitVideoEnabled,
+    articlePortraitVideoEnabled,
   );
 
   const portraitVideoCarouselTitle =
@@ -366,10 +366,10 @@ const ArticlePage = ({ pageData }: { pageData: Article }) => {
 
   const showContinueReadingButton = Boolean(
     !isAmp &&
-      !isLite &&
-      !isApp &&
-      hasContinueReadingBlock &&
-      continueReadingButtonToggle,
+    !isLite &&
+    !isApp &&
+    hasContinueReadingBlock &&
+    continueReadingButtonToggle,
   );
 
   const promoImageBlocks =
@@ -450,11 +450,11 @@ const ArticlePage = ({ pageData }: { pageData: Article }) => {
   // show media curation only when the user is in adaptive variation
   const showAdaptiveMediaCuration = Boolean(
     !isAmp &&
-      !isLite &&
-      !isApp &&
-      !isPGL &&
-      isAdaptiveTimeOfDayVariant &&
-      mediaCurationContent?.summaries?.length,
+    !isLite &&
+    !isApp &&
+    !isPGL &&
+    isAdaptiveTimeOfDayVariant &&
+    mediaCurationContent?.summaries?.length,
   );
 
   // EXPERIMENT: PWA Promotional Banner
