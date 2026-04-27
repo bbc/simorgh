6dea15c21efdd18ae86c52573fb3e9bcee6535d4
HarveyPeachey <harvey.peachey@bbc.co.uk>
2026-04-27 12:20:57 +0100

    makes install script supported archs agnostic

--- scripts/installNodeModules.sh
+++ scripts/installNodeModules.sh
@@ -5,8 +5,9 @@ if [ "$1" = "--production" ]; then
   echo "Installing 'production' node modules"
   yarn workspaces focus --production
 else
-  yarn config set supportedArchitectures.os --json '["linux"]'
-  yarn config set supportedArchitectures.cpu --json '["arm64"]'
+  yarn config set supportedArchitectures.os --json '["current"]'
+  yarn config set supportedArchitectures.cpu --json '["current"]'
+  yarn config set supportedArchitectures.libc --json '["current"]'
   echo "Removing node modules"
   rm -rf node_modules
   echo "Installing all node modules"
