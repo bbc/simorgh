35a071adfcfda94bdb110ba99c7a136871fee244
Santa Zena <santa.zena@accenture.com>
2026-04-29 14:54:32 +0300

    Ws 2480 implement save button on article page (#13932)
    * WS-2480-implement-save-button-on-article-page
    
    * Adding styles, icon, spinner
    
    * Adding focus, hover, disbled states and introducing FilledBookmarkIcon
    
    * Adding saved state
    
    * Adding remove state
    
    * Adding theme styles
    
    * Fix: correcting styles
    
    * Fix: fix tesst imports
    
    * Fix: fix test
    
    * Fix: fix test nr2
    
    * Fix: margin fix
    
    * Fix: px to rem and adding isSaved to README file
    
    * Fix: addressing copilot comments
    
    * Fix: styles
    
    * Fix: correct styles for saved state
    
    * Fix: removing comment from testing
    
    * Fix: update test
    
    * Adding button height, fontFamily, removeText as prop
    
    * Adding removeText as prop in saveArticle button
    
    * Adding service config
    
    * Correcting story
    
    * Icon size
    
    * Refactoring some styles
    
    * Refactoring some styles nr2
    
    * Removing label
    
    * Remove fallback with hardcoded values, changing translation names
    
    * Icon sizes
    
    * Remove margin from SaveArticleButton, rem vs px
    
    * Gap spacing
    
    * Px to rem
    
    * Px to rem
    
    * Added top, bottom margin
    
    * Removed comment
    
    * Renaming ActionButton to SaveButton and updating Bundle size
    
    * Updating Bundle size
    
    * Rename ActionButton to SaveButton in README
    
    * Remove commment
    
    * Moved margins to SaveArticleButton styles
    
    * style: fix margins
    
    * chore: remove unused imports
    
    * style: improve margins
    
    ---------
    
    Co-authored-by: Elvinas Valenas <elvinas.valenas.ext@bbc.co.uk>

---
+++ src/app/components/SaveButton/index.stories.tsx
@@ -0,0 +1,75 @@
+import ThemeProvider from '#app/components/ThemeProvider';
+import { ServiceContextProvider } from '#app/contexts/ServiceContext';
+import { AccountContext } from '#app/contexts/AccountContext';
+import SaveButton from '.';
+import metadata from './metadata.json';
+import readme from './README.md';
+
+type WithProvidersArgs = {
+  isLoading?: boolean;
+  disabled?: boolean;
+  buttonText?: string;
+  isSaved?: boolean;
+  removeText?: string;
+};
+
+const withProviders =
+  ({ 
+    isLoading = false, 
+    disabled = false, 
+    isSaved = false,
+    buttonText = 'Save article', 
+    removeText,
+  }: WithProvidersArgs) =>
+  () => (
+    <ThemeProvider service="pidgin">
+      <ServiceContextProvider service="pidgin">
+        <AccountContext.Provider
+          value={{
+            isSignedIn: false,
+            signInUrl: 'https://example.com/signin',
+            registerUrl: 'https://example.com/register',
+            isIdctaAvailable: true,
+            settingsUrl: undefined,
+            signOutUrl: undefined,
+            forYouUrl: undefined,
+          }}
+        >
+          <SaveButton
+            onClick={() => console.log('Button clicked')}
+            isLoading={isLoading}
+            isSaved={isSaved}
+            disabled={disabled}
+            buttonText={buttonText}
+            removeText={removeText}
+          />
+        </AccountContext.Provider>
+      </ServiceContextProvider>
+    </ThemeProvider>
+  );
+
+export default {
+  title: 'Components/SaveButton',
+  component: SaveButton,
+  parameters: {
+    metadata,
+    docs: { readme },
+  },
+};
+
+export const Unsaved = withProviders({
+  buttonText: 'Save for later',
+});
+
+export const Loading = withProviders({
+  isLoading: true,
+  buttonText: 'Saving',
+});
+
+export const Saved = withProviders({
+  buttonText: 'Saved to My News',
+  isSaved: true,
+  removeText: 'Remove',
+});
+
+
