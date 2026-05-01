d7adc3ec19c4adef97136d92ce1c377749d4e70d
“LilyL0u” <lily.higham@bbc.co.uk>
2026-05-01 12:24:56 +0100

    update topictags integration test and snapshots

--- ws-nextjs-app/integration/common/topicTags.ts
+++ ws-nextjs-app/integration/common/topicTags.ts
@@ -1,15 +1,30 @@
 export default () => {
-  describe('topic tags', () => {
-    it('should display topic tags, if they exist', () => {
-      const topicTags = document.querySelector(
-        `aside[aria-labelledby*='related-topics'] a`,
-      );
+  describe('Topic Tags', () => {
+    const topicTags = Array.from(
+      document.querySelectorAll(`aside[aria-labelledby*='related-topics'] a`),
+    );
 
-      if (topicTags) {
-        expect(topicTags).toBeInTheDocument();
-      } else {
-        expect(topicTags).toBeNull();
-      }
-    });
+    if (topicTags.length > 0) {
+      topicTags.forEach(tag => {
+        it('should be in the document', () => {
+          expect(tag).toBeInTheDocument();
+        });
+
+        it('should have text', () => {
+          expect(tag.textContent).toBeTruthy();
+        });
+
+        it('should match text and href', () => {
+          expect({
+            text: tag.textContent,
+            href: tag.getAttribute('href'),
+          }).toMatchSnapshot();
+        });
+      });
+    } else {
+      it('should not render any topic tags', () => {
+        expect(topicTags.length).toBe(0);
+      });
+    }
   });
 };
