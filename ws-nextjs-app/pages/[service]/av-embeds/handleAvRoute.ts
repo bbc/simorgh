bfce2ca4d0195587b353d3972b142c3dbf926d0a
Toby Cox <eagerterrier@gmail.com>
2026-05-01 08:47:17 +0100

    ENH: new caching policy (#13962)

--- ws-nextjs-app/pages/[service]/av-embeds/handleAvRoute.ts
+++ ws-nextjs-app/pages/[service]/av-embeds/handleAvRoute.ts
@@ -27,7 +27,7 @@ export default async (context: GetServerSidePropsContext) => {
 
   context.res.setHeader(
     'Cache-Control',
-    'public, stale-if-error=90, stale-while-revalidate=30, max-age=30',
+    'public, stale-if-error=900, stale-while-revalidate=300, max-age=300',
   );
 
   // Remove x-frame-options header to allow embedding
