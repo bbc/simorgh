03dd825fcdefa2dfb04a8e29040d7a053c495e7c
Isabella Mitchell <98817636+Isabella-Mitchell@users.noreply.github.com>
2026-05-06 15:14:48 +0100

    Revert "ENH: new caching policy (#13962)"
    This reverts commit bfce2ca4d0195587b353d3972b142c3dbf926d0a.

--- ws-nextjs-app/pages/[service]/popular/[read]/[[...variant]].page.test.tsx
+++ ws-nextjs-app/pages/[service]/popular/[read]/[[...variant]].page.test.tsx
@@ -114,7 +114,7 @@ describe('handleMostReadRoute', () => {
 
     expect(mockSetHeader).toHaveBeenCalledWith(
       'Cache-Control',
-      expect.stringContaining('max-age=60'),
+      expect.stringContaining('max-age=30'),
     );
   });
 });
