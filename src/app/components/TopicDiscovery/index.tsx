c4f7cb501c22b5c9b7ecff415f1c2453095bff66
Aaron Moore <aaron.moore@bbc.co.uk>
2026-05-11 16:50:16 +0100

    Re-add 'moreFrom' skeleton

--- src/app/components/TopicDiscovery/index.tsx
+++ src/app/components/TopicDiscovery/index.tsx
@@ -87,19 +87,24 @@ const TopicDiscovery = ({ topics, className }: TopicDiscoveryProps) => {
           switch (true) {
             case showLoadingState:
               return (
-                <div css={styles.skeletonGrid} aria-live="polite">
-                  {Array.from({ length: 4 }).map((_, index) => (
-                    // eslint-disable-next-line react/no-array-index-key
-                    <div key={index} css={styles.skeletonCard} aria-hidden>
-                      <div css={styles.skeletonImage} />
-                      <div css={styles.skeletonTextLines}>
-                        <div css={[styles.skeletonLine, { width: '100%' }]} />
-                        <div css={[styles.skeletonLine, { width: '70%' }]} />
-                        <div css={[styles.skeletonLine, { width: '40%' }]} />
+                <>
+                  <div css={styles.skeletonGrid} aria-live="polite">
+                    {Array.from({ length: 4 }).map((_, index) => (
+                      // eslint-disable-next-line react/no-array-index-key
+                      <div key={index} css={styles.skeletonCard} aria-hidden>
+                        <div css={styles.skeletonImage} />
+                        <div css={styles.skeletonTextLines}>
+                          <div css={[styles.skeletonLine, { width: '100%' }]} />
+                          <div css={[styles.skeletonLine, { width: '70%' }]} />
+                          <div css={[styles.skeletonLine, { width: '40%' }]} />
+                        </div>
                       </div>
-                    </div>
-                  ))}
-                </div>
+                    ))}
+                  </div>
+                  <div css={styles.skeletonMoreFromLinkContainer}>
+                    <div css={styles.skeletonMoreFromLink} aria-hidden />
+                  </div>
+                </>
               );
             case showErrorMessage:
               return (
