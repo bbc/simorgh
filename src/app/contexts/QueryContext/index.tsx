6a6f61cdfe62d3203c2264bc1a69dd74bc575da5
Elvinas Valenas <elvinas.valenas.ext@bbc.co.uk>
2026-05-12 16:22:57 +0300

    refactor: use named query provider chunk

--- src/app/contexts/QueryContext/index.tsx
+++ src/app/contexts/QueryContext/index.tsx
@@ -2,7 +2,13 @@ import { Suspense, use, type PropsWithChildren } from 'react';
 import dynamic from 'next/dynamic';
 import { AccountContext } from '#app/contexts/AccountContext';
 
-const PersistentQueryProvider = dynamic(() => import('./lazy'));
+const PersistentQueryProvider = dynamic(
+  () =>
+    import(
+      /* webpackChunkName: "query_provider" */
+      './lazy'
+    ),
+);
 
 // TanstackQuery Provider is only needed when personalization features are enabled.
 // This prevents the unnecessary loading of the Tanstack Query library and its dependencies
