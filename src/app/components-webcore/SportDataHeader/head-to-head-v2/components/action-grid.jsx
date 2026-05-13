32fe0797d629999ee61115229e075cd2618c3316
hotinglok <hotinglok@gmail.com>
2026-05-13 17:41:56 +0100

    Remove javascript files

--- src/app/components-webcore/SportDataHeader/head-to-head-v2/components/action-grid.jsx
+++
@@ -1,7 +0,0 @@
-import styles, { GRID_AREAS } from './index.styles';
-
-export { GRID_AREAS };
-
-export const ActionGrid = ({ children }) => (
-  <div css={styles.actionGrid()}>{children}</div>
-);
--- src/app/components-webcore/SportDataHeader/head-to-head-v2/components/sport-badge/index.js
+++
@@ -1,3 +0,0 @@
-// eslint-disable-next-line no-restricted-exports
-export { default } from './sport-badge';
-export { hasMapping as hasTeamBadgeMapping } from './badges-map';
