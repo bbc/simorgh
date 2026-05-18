a798e3beb5d783c8bcec2afd056e2056593d3fa8
Aaron Moore <aaron.moore@bbc.co.uk>
2026-05-18 16:22:11 +0100

    Update `urlParser` to TS and extract `base` to const

--- src/app/lib/utilities/urlParser/index.js
+++
@@ -1,9 +0,0 @@
-// Returns the path, excluding any query string params
-export const getUrlPath = url => {
-  return new URL(url, 'https://www.bbc.com').pathname;
-};
-
-// Returns the query string
-export const getQueryString = url => {
-  return new URL(url, 'https://www.bbc.com').search;
-};
