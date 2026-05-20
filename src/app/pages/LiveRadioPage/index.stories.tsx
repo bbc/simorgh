b5ee850f890534cbffcff0e34add1eef6556e27a
Aaron Moore <aaron.moore@bbc.co.uk>
2026-05-20 14:18:10 +0100

    Add `liveRadioSchedule` toggle to radio page story

--- src/app/pages/LiveRadioPage/index.stories.tsx
+++ src/app/pages/LiveRadioPage/index.stories.tsx
@@ -3,28 +3,37 @@ import afrique from '#data/afrique/bbc_afrique_radio/liveradio.json';
 import LiveRadioPage from './LiveRadioPage';
 import PageLayoutWrapper from '#app/components/PageLayoutWrapper';
 import { ServiceContextProvider } from '#app/contexts/ServiceContext';
+import { ToggleContextProvider } from '#app/contexts/ToggleContext';
 
 const Component = () => {
   const pageData = afrique.data;
 
   return (
-    <ServiceContextProvider service="afrique">
-      <PageLayoutWrapper
-        // @ts-expect-error - Fixture data
-        pageData={pageData}
-        status={200}
-      >
-        <LiveRadioPage
+    <ToggleContextProvider
+      toggles={{
+        liveRadioSchedule: {
+          enabled: true,
+        },
+      }}
+    >
+      <ServiceContextProvider service="afrique">
+        <PageLayoutWrapper
           // @ts-expect-error - Fixture data
           pageData={pageData}
           status={200}
-          service="afrique"
-          loading={false}
-          error=""
-          pageType={LIVE_RADIO_PAGE}
-        />
-      </PageLayoutWrapper>
-    </ServiceContextProvider>
+        >
+          <LiveRadioPage
+            // @ts-expect-error - Fixture data
+            pageData={pageData}
+            status={200}
+            service="afrique"
+            loading={false}
+            error=""
+            pageType={LIVE_RADIO_PAGE}
+          />
+        </PageLayoutWrapper>
+      </ServiceContextProvider>
+    </ToggleContextProvider>
   );
 };
 
