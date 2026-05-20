12785195c2ae5338039ea8aaed6ca7459cb70c49
Shayne Ahchoon <shayne.ahchoon@bbc.co.uk>
2026-05-20 15:59:11 +0100

    WS-POC: Add account alternative

--- src/app/components/LiteModeButton/index.styles.tsx
+++ src/app/components/LiteModeButton/index.styles.tsx
@@ -2,22 +2,43 @@ import pixelsToRem from '#app/utilities/pixelsToRem';
 import { css, Theme } from '@emotion/react';
 
 export default {
-  container: ({ palette, fontVariants, fontSizes }: Theme) =>
+  container: ({ palette }: Theme) =>
     css({
-      ...fontSizes.brevier,
-      ...fontVariants.sansRegular,
       color: `${palette.BLACK}`,
       backgroundColor: `${palette.POSTBOX}`,
       border: `${pixelsToRem(1)}rem solid ${palette.WHITE}`,
-      padding: `0 ${pixelsToRem(2)}rem`,
     }),
-  mode: ({ palette, spacings }: Theme) =>
+  containerWithoutAccount: ({ fontVariants, fontSizes, spacings }: Theme) =>
+    css({
+      ...fontSizes.brevier,
+      ...fontVariants.sansRegular,
+      margin: `${pixelsToRem(2)}rem`,
+      padding: `0 ${pixelsToRem(2)}rem`,
+      '& span': {
+        padding: `${spacings.HALF}rem ${spacings.FULL}rem`,
+        margin: `${pixelsToRem(2)}rem 0`,
+      },
+    }),
+  containerWithAccount: ({ fontVariants, fontSizes }: Theme) =>
+    css({
+      ...fontSizes.minion,
+      ...fontVariants.sansRegular,
+      margin: `0.5rem 0.3rem`,
+      display: 'flex',
+      flexDirection: 'column',
+      '& span': {
+        display: 'block',
+        padding: `0.1rem 0.4rem`,
+        flex: '1',
+        alignItems: 'center',
+      },
+    }),
+  mode: ({ palette }: Theme) =>
     css({
       display: 'inline-block',
       color: `${palette.WHITE}`,
       backgroundColor: `${palette.POSTBOX}`,
-      padding: `${spacings.HALF}rem ${spacings.FULL}rem`,
-      margin: `${pixelsToRem(2)}rem 0`,
+      textAlign: 'center',
     }),
   on: ({ palette }: Theme) =>
     css({
--- src/app/components/LiteModeButton/index.tsx
+++ src/app/components/LiteModeButton/index.tsx
@@ -1,24 +1,33 @@
 import { use } from 'react';
 import { RequestContext } from '#app/contexts/RequestContext';
-import { MEDIA_ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
+import { AUDIO_PAGE, MEDIA_ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
+import useClickTrackerHandler from '#app/hooks/useClickTrackerHandler';
+import { PageTypes } from '#app/models/types/global';
 import CallToActionLink from '../CallToActionLink';
 import style from './index.styles';
 
+const ignoreList = [MEDIA_ARTICLE_PAGE, AUDIO_PAGE] as PageTypes[];
+
 export default () => {
   const { canonicalLink, isLite, pageType } = use(RequestContext);
-  const liteLink = `${canonicalLink}.lite`;
+  const clickTrackerHandler = useClickTrackerHandler({
+    componentName: `switch-to-${isLite ? 'canonical' : 'lite'}`,
+  });
 
-  if (pageType === MEDIA_ARTICLE_PAGE) {
+  if (ignoreList.includes(pageType)) {
     return null;
   }
+
+  const liteLink = `${canonicalLink}.lite`;
 
   return (
     <CallToActionLink
       url={isLite ? canonicalLink : liteLink}
       {...(isLite && { 'data-ignore-lite': true })}
-      css={style.container}
+      css={[style.container, style.containerWithAccount]}
+      {...clickTrackerHandler}
     >
-      <span css={[style.mode, !isLite && style.on]}>Pro</span>
+      <span css={[style.mode, !isLite && style.on]}>Classic</span>
       <span css={[style.mode, isLite && style.on]}>Lite</span>
     </CallToActionLink>
   );
