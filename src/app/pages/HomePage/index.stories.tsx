ea370fe55c0b7c2577856c512fa1d5ee9a608ae6
Aaron Moore <aaron.moore@bbc.co.uk>
2026-05-20 14:10:04 +0100

    Update index.stories.tsx

--- src/app/pages/HomePage/index.stories.tsx
+++ src/app/pages/HomePage/index.stories.tsx
@@ -7,6 +7,7 @@ import { StoryArgs, StoryProps } from '../../models/types/storybook';
 import HomePage from './HomePage';
 import PageLayoutWrapper from '#app/components/PageLayoutWrapper';
 import { ServiceContextProvider } from '#app/contexts/ServiceContext';
+import { RequestContextProvider } from '#app/contexts/RequestContext';
 
 const ONE_DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
 
@@ -61,23 +62,31 @@ const Component = ({ service, variant, isLite }: StoryProps) => {
 
   return (
     <ServiceContextProvider service={service} variant={variant}>
-      <PageLayoutWrapper
-        // @ts-expect-error - Fixture data
-        pageData={pageData}
-        status={200}
+      <RequestContextProvider
+        isLite={isLite}
+        service={service}
+        variant={variant}
+        pageType={HOME_PAGE}
+        pathname={`/${service}`}
       >
-        <HomePage
-          service={service}
-          variant={variant}
-          pageType={HOME_PAGE}
-          status={200}
-          isAmp={false}
-          isLite={isLite}
-          pathname={`/${service}`}
+        <PageLayoutWrapper
           // @ts-expect-error - Fixture data
           pageData={pageData}
-        />
-      </PageLayoutWrapper>
+          status={200}
+        >
+          <HomePage
+            service={service}
+            variant={variant}
+            pageType={HOME_PAGE}
+            status={200}
+            isAmp={false}
+            isLite={isLite}
+            pathname={`/${service}`}
+            // @ts-expect-error - Fixture data
+            pageData={pageData}
+          />
+        </PageLayoutWrapper>
+      </RequestContextProvider>
     </ServiceContextProvider>
   );
 };
