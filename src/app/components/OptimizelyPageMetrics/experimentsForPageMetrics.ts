c13fdc3749c8bc970fb9caa810225fe91eb3719e
holchris <holly.christopher@bbc.co.uk>
2026-05-21 15:45:58 +0100

    removes newswb_ws_tod_article_2

--- src/app/components/OptimizelyPageMetrics/experimentsForPageMetrics.ts
+++ src/app/components/OptimizelyPageMetrics/experimentsForPageMetrics.ts
@@ -13,7 +13,6 @@ const experimentsForPageMetrics: ExperimentsForPageTypeMetrics = [
     // include tod2 so page-level metrics also fire on article pages for this experiment
     pageType: ARTICLE_PAGE,
     activeExperiments: [
-      'newswb_ws_tod_article_2',
       'test_page_views_aa_2',
       'newswb_ws_topic_discovery_module',
     ],
@@ -21,7 +20,7 @@ const experimentsForPageMetrics: ExperimentsForPageTypeMetrics = [
   {
     // include media article pages so page metrics still count after clicking into a video page
     pageType: MEDIA_ARTICLE_PAGE,
-    activeExperiments: ['newswb_ws_tod_article_2'],
+    activeExperiments: [],
   },
 ];
 
