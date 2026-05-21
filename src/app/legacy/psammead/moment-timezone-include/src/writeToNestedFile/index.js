447fc7512c9cf35803a37bd3f88dcfb5f7cfec9a
eagerterrier <eagerterrier@gmail.com>
2026-05-21 17:21:41 +0100

    INI: remove things that don't seem to be used any more

--- src/app/legacy/psammead/moment-timezone-include/src/writeToNestedFile/index.js
+++ src/app/legacy/psammead/moment-timezone-include/src/writeToNestedFile/index.js
@@ -1,14 +1,10 @@
 // biome-ignore-all lint/style/noCommonJs: we want this
 // biome-ignore-all lint/style/useNodejsImportProtocol: we want this
 const fs = require('fs');
-const mkdirp = require('mkdirp');
 const path = require('path');
 
 const writeToNestedFile = (filePath, contents) => {
   const fullFilePath = path.join(__dirname, filePath);
-  const folder = path.dirname(fullFilePath);
-
-  mkdirp.sync(folder);
 
   fs.writeFileSync(fullFilePath, contents);
 
--- src/app/legacy/psammead/moment-timezone-include/src/writeToNestedFile/index.test.js
+++ src/app/legacy/psammead/moment-timezone-include/src/writeToNestedFile/index.test.js
@@ -2,16 +2,10 @@
 import fs from 'fs';
 import path from 'path';
 
-import mkdirp from 'mkdirp';
-
 import writeToNestedFile from '.';
 
 jest.mock('fs', () => ({
   writeFileSync: jest.fn(),
-}));
-
-jest.mock('mkdirp', () => ({
-  sync: jest.fn(),
 }));
 
 jest.mock('../writeNewTimezoneData', () => jest.fn());
@@ -19,9 +13,6 @@ jest.mock('../writeNewTimezoneData', () => jest.fn());
 describe('writeToNestedFile', () => {
   it('Should create directory and write file correctly', () => {
     writeToNestedFile('path/to/a/place', 'filecontent');
-
-    expect(mkdirp.sync).toHaveBeenCalledTimes(1);
-    expect(mkdirp.sync).toHaveBeenCalledWith(path.join(__dirname, 'path/to/a'));
 
     expect(fs.writeFileSync).toHaveBeenCalledTimes(1);
     expect(fs.writeFileSync).toHaveBeenCalledWith(
