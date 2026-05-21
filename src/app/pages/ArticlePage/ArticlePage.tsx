e03ebb328dca44b7c47d39cabc6254f3cb791fd8
Louise Archibald <louise.archibald@bbc.co.uk>
2026-05-21 17:45:52 +0100

    render component on Article page

--- src/app/pages/ArticlePage/ArticlePage.tsx
+++ src/app/pages/ArticlePage/ArticlePage.tsx
@@ -90,6 +90,7 @@ import {
   isPortraitVideo,
   isPortraitVideoUnderHeadline,
 } from '../../components/MediaLoader/utils/isPortraitVideo';
+import LocationBasedTopicOJ from '../../components/LocationBasedTopicOJ';
 
 const getImageComponent =
   (preloadLeadImageToggle: boolean) => (props: ComponentToRenderProps) => (
@@ -431,6 +432,14 @@ const ArticlePage = ({
     articleVideoCurationEnabled,
   );
 
+  const showCountryCuration = Boolean(
+    !isAmp &&
+    !isLite &&
+    !isApp &&
+    !isLive() &&
+    pageData?.countryCuration?.summaries?.length,
+  );
+
   // EXPERIMENT: PWA Promotional Banner
   const shouldRenderPWAPromotionalBanner =
     !isTopBarOJsEnabled || !pageData?.secondaryColumn?.topStories?.length;
@@ -521,6 +530,7 @@ const ArticlePage = ({
               css={styles.portraitVideoCarousel}
             />
           )}
+          {showCountryCuration && <LocationBasedTopicOJ pageData={pageData} />}
           <RelatedContentSection content={blocks} />
           {showMediaCuration && (
             <div css={styles.mediaCurationRow}>
