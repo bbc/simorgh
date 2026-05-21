4fc59c5f02a9148270d9f0b88fa4eaa13fb6a185
Jordi Teixidó <jordit@users.noreply.github.com>
2026-05-21 12:35:04 +0100

    Linting

--- src/app/lib/config/services/arabic.ts
+++ src/app/lib/config/services/arabic.ts
@@ -51,7 +51,8 @@ export const service: DefaultServiceConfig = {
     podcastPromo: {
       title: 'البودكاست',
       brandTitle: 'يستحق الانتباه',
-      brandDescription: 'شرح معمق لأبرز الأحداث والموضوعات، لمساعدتك على فهم أهم المتغيرات حولك وأثرها على حياتك',
+      brandDescription:
+        'شرح معمق لأبرز الأحداث والموضوعات، لمساعدتك على فهم أهم المتغيرات حولك وأثرها على حياتك',
       image: {
         src: 'https://ichef.bbci.co.uk/images/ic/$recipe/p0h6dt4s.jpg',
         alt: 'يستحق الانتباه',
