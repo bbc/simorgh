3684bd4d7c83fc2e5bb75cee4592c081e403b457
holchris <holly.christopher@bbc.co.uk>
2026-05-26 16:10:11 +0100

    adds media article page

--- src/app/components/OptimizelyPageMetrics/experimentsForPageMetrics.ts
+++ src/app/components/OptimizelyPageMetrics/experimentsForPageMetrics.ts
@@ -1,5 +1,5 @@
 import { PageTypes } from '#app/models/types/global';
-import { ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
+import { ARTICLE_PAGE, MEDIA_ARTICLE_PAGE } from '#app/routes/utils/pageTypes';
 // Any running serverside and client side experiments which collect Optimizely Page Metrics; page view, page complete, scroll depth
 // Includes PageType so that different experiments can be run on different pageTypes
 
@@ -11,10 +11,11 @@ type ExperimentsForPageTypeMetrics = {
 const experimentsForPageMetrics: ExperimentsForPageTypeMetrics = [
   {
     pageType: ARTICLE_PAGE,
-    activeExperiments: [
-      'test_page_views_aa_2',
-      'newswb_ws_topic_discovery_module',
-    ],
+    activeExperiments: ['newswb_ws_topic_discovery_module'],
+  },
+  {
+    pageType: MEDIA_ARTICLE_PAGE,
+    activeExperiments: ['newswb_ws_topic_discovery_module'],
   },
 ];
 
