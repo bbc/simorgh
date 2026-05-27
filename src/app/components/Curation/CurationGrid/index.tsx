64439835fd99b078be0f292cfe8d05318abe0d78
holchris <holly.christopher@bbc.co.uk>
2026-05-27 16:40:07 +0100

    lil refactor

--- src/app/components/Curation/CurationGrid/index.tsx
+++ src/app/components/Curation/CurationGrid/index.tsx
@@ -35,7 +35,14 @@ const CurationGrid = ({
   const buildPromoEventTrackingData = (promo: Summary, i: number) => {
     const componentName =
       eventTrackingData?.componentName || 'simple-curation-grid';
-    const promoType = componentName;
+    const trackingComponentName =
+      componentName === 'topic-discovery'
+        ? 'topic-discovery-curation-grid-promo'
+        : componentName;
+    const promoType =
+      componentName === 'topic-discovery'
+        ? 'topic-discovery-curation-grid-promo'
+        : `${componentName}-promo`;
     return {
       itemTracker: {
         type: promoType,
@@ -48,6 +55,7 @@ const CurationGrid = ({
         }),
       },
       ...eventTrackingData,
+      componentName: trackingComponentName,
     };
   };
 
