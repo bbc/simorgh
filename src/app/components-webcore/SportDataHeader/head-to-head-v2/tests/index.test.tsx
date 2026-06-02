e343e85d68b096e63e6db27806e81885960e5a9e
emily saffron <emily.saffron@bbc.co.uk>
2026-06-02 12:43:10 +0100

    update unit tests

--- src/app/components-webcore/SportDataHeader/head-to-head-v2/tests/index.test.tsx
+++ src/app/components-webcore/SportDataHeader/head-to-head-v2/tests/index.test.tsx
@@ -237,48 +237,6 @@ describe('Skipped Tests for MVP', () => {
 });
 
 describe('Head to Head Component', () => {
-  test('renders head to head component of PreEvent status with No teams', () => {
-    renderHeadToHead({ data: asH2HData(preEventNoTeamsMockData) });
-
-    const date = screen.queryByText('Sat 6 Aug 2022');
-    const tournament = screen.queryByText('Premier League');
-    const venue = screen.getByText('To be confirmed');
-    const teamName = screen.getByText(
-      'Team to be confirmed versus Team to be confirmed kick off 12:30',
-    );
-
-    expect(venue).toBeInTheDocument();
-    expect(date).toBeInTheDocument();
-    expect(tournament).toBeInTheDocument();
-    expect(teamName).toBeInTheDocument();
-  });
-
-  test('renders head to head component of PreEvent status', () => {
-    renderHeadToHead({ data: asH2HData(preEventMockData) });
-
-    const date = screen.queryByText('Sat 6 Aug 2022');
-    const tournamentLabel = screen.queryByText('Premier League');
-
-    expect(date).toBeInTheDocument();
-    expect(tournamentLabel).toBeInTheDocument();
-  });
-
-  test('renders the head to head of MidEvent status', () => {
-    const tournamentDescriptionLabel = 'UEFA Europa Conference League';
-
-    renderHeadToHead({
-      data: asH2HData({ ...firstHalfData, tournamentDescriptionLabel }),
-    });
-
-    const tournamentText = screen.queryByText(tournamentDescriptionLabel);
-    const time = screen.queryByText(firstHalfData.date);
-    const venue = screen.queryByText('Stadions Daugava');
-
-    expect(tournamentText).toBeInTheDocument();
-    expect(time).not.toBeInTheDocument();
-    expect(venue).toBeInTheDocument();
-  });
-
   test('renders head to head with half time, full time scores for a MidEvent in Extra Time', () => {
     renderHeadToHead({ data: asH2HData(etFirstHalfData) });
 
@@ -822,53 +780,4 @@ describe('Head to Head Component', () => {
       expect(screen.queryByTestId('penalties-text')).not.toBeInTheDocument();
     },
   );
-
-  test('renders the value of Attendance', () => {
-    renderHeadToHead({
-      data: asH2HData({ ...secondHalf90Data, attendance: { value: 73456 } }),
-    });
-
-    expect(screen.queryByText('73,456')).toBeInTheDocument();
-  });
-
-  test('renders the value of AttendanceInfo in the venue', () => {
-    renderHeadToHead({
-      data: asH2HData({
-        ...secondHalf90Data,
-        attendance: { additionalInfo: 'Limited Audience' },
-      }),
-    });
-
-    expect(
-      screen.queryByText('Stadions Daugava (Limited Audience)'),
-    ).toBeInTheDocument();
-  });
-
-  test('renders the value of Attendance and Attendance Info', () => {
-    renderHeadToHead({
-      data: asH2HData({
-        ...secondHalf90Data,
-        attendance: { value: 73456, additionalInfo: 'Limited Audience' },
-      }),
-    });
-
-    expect(screen.queryByText('73,456')).toBeInTheDocument();
-    expect(
-      screen.queryByText('Stadions Daugava (Limited Audience)'),
-    ).toBeInTheDocument();
-  });
-
-  test('does not render the value of Attendance and Attendance Info when they are not present in the data', () => {
-    renderHeadToHead({
-      data: asH2HData({ ...secondHalf90Data, attendance: {} }),
-    });
-
-    expect(screen.queryByText('73,456')).not.toBeInTheDocument();
-    expect(screen.queryByText('Stadions Daugava')).toBeInTheDocument();
-  });
-
-  // Not MVP - Rugby fixture data required
-  // test('renders head to head with a goal summary', () => {
-  // ...existing code...
-  // });
 });
