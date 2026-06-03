dd18b643cbc7f0cec9b12d4d52c915e45fd57f72
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2026-06-03 11:20:27 +0100

    WS-2756: Renders translated winsOnPens text

--- src/app/components-webcore/SportDataHeader/head-to-head-v2/components/penalty-scores.tsx
+++ src/app/components-webcore/SportDataHeader/head-to-head-v2/components/penalty-scores.tsx
@@ -1,3 +1,5 @@
+import { use } from 'react';
+import { ServiceContext } from '#app/contexts/ServiceContext';
 import VisuallyHiddenText from '../../../../components/VisuallyHiddenText';
 import styles from '../index.styles';
 import type { HeadToHeadV2Data } from '../types';
@@ -8,6 +10,10 @@ interface PenaltyScoresProps {
 
 const PenaltyScores = ({ data }: PenaltyScoresProps) => {
   const { winner, seriesWinner, multiLeg, status } = data;
+
+  const { translations } = use(ServiceContext);
+  const { winOnPenalties } = translations?.sport || {};
+  const { prefix = 'wins', suffix = 'on penalties' } = winOnPenalties || {};
 
   const isPostEvent = status?.toLowerCase() === 'postevent';
   const hasWinner = winner !== undefined;
@@ -36,7 +42,7 @@ const PenaltyScores = ({ data }: PenaltyScoresProps) => {
   return (
     <div css={styles.penaltyScoresContainer}>
       <VisuallyHiddenText>
-        {`${winnerOnPenaltiesName} win ${winnerOnPenaltiesScore} - ${loserOnPenaltiesScore} on penalties`}
+        {`${winnerOnPenaltiesName} ${prefix} ${winnerOnPenaltiesScore} - ${loserOnPenaltiesScore} ${suffix}`}
       </VisuallyHiddenText>
       <div
         css={styles.penaltiesText}
@@ -44,7 +50,7 @@ const PenaltyScores = ({ data }: PenaltyScoresProps) => {
         data-testid="penalties-text"
       >
         <span css={styles.winningTeamName}>{`${winnerOnPenaltiesName}`}</span>
-        {` win ${winnerOnPenaltiesScore}-${loserOnPenaltiesScore} on pens`}
+        {` ${prefix} ${winnerOnPenaltiesScore}-${loserOnPenaltiesScore} ${suffix}`}
       </div>
     </div>
   );
