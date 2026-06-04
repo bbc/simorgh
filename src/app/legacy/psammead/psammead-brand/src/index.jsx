3bcd782daea8cd5a794c6345ac97eff4e4d78391
Louise Archibald <louise.archibald@bbc.co.uk>
2026-06-04 08:57:44 +0100

    Revert "undo previously added media queries"
    This reverts commit 7ac57bed1c76e1a64d5c83f388d30693b3c70e61.

--- src/app/legacy/psammead/psammead-brand/src/index.jsx
+++ src/app/legacy/psammead/psammead-brand/src/index.jsx
@@ -1,12 +1,14 @@
 import { forwardRef } from 'react';
 import styled from '@emotion/styled';
 import {
+  GEL_GROUP_1_SCREEN_WIDTH_MIN,
   GEL_GROUP_2_SCREEN_WIDTH_MIN,
   GEL_GROUP_3_SCREEN_WIDTH_MIN,
 } from '#psammead/gel-foundations/src/breakpoints';
 import {
   GEL_SPACING_HLF,
   GEL_SPACING,
+  GEL_SPACING_DBL,
 } from '#psammead/gel-foundations/src/spacings';
 import VisuallyHiddenText from '../../../../components/VisuallyHiddenText';
 
@@ -32,6 +34,23 @@ const Banner = styled.div`
   padding: 0 ${GEL_SPACING};
   display: flex;
   align-items: stretch;
+
+  @media (min-width: ${GEL_GROUP_1_SCREEN_WIDTH_MIN}) {
+    min-height: ${props =>
+      props.isLanguagesPage ? `${44 / 16}rem` : `${60 / 16}rem`};
+    padding: 0 ${GEL_SPACING};
+  }
+
+  @media (min-width: ${GEL_GROUP_2_SCREEN_WIDTH_MIN}) {
+    min-height: ${props =>
+      props.isLanguagesPage ? `${44 / 16}rem` : `${60 / 16}rem`};
+    padding: 0 ${GEL_SPACING_DBL};
+  }
+
+  @media (min-width: ${GEL_GROUP_3_SCREEN_WIDTH_MIN}) {
+    min-height: ${props =>
+      props.isLanguagesPage ? `${44 / 16}rem` : `${64 / 16}rem`};
+  }
 `;
 
 const StyledLink = styled.a`
