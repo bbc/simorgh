fe1a769775abc7c20fe3974fab749601cf84f6ab
holchris <holly.christopher@bbc.co.uk>
2026-06-11 13:56:48 +0100

    adds addSendStaticBeaconToWindowScript inline script into own file

---
+++ src/app/lib/analyticsUtils/staticATITracking/sendStaticBeacon/addSendStaticBeaconToWindowScript.ts
@@ -0,0 +1,6 @@
+import { addSendStaticBeaconToWindow } from '.';
+
+const addSendStaticBeaconToWindowScript = () =>
+  `(${addSendStaticBeaconToWindow.toString()})();`;
+
+export default addSendStaticBeaconToWindowScript;
