92dd1d54ca0d916ca0e6d7164ea39af8b451fec2
alex-magana <alex.magana@andela.com>
2026-06-12 21:57:17 +0300

    Adopt ReverbParamsContext in ATIAnalytics

--- src/app/components/ATIAnalytics/index.tsx
+++ src/app/components/ATIAnalytics/index.tsx
@@ -1,26 +1,29 @@
 import { use } from 'react';
 import { RequestContext } from '#contexts/RequestContext';
-import { AccountContext } from '#contexts/AccountContext';
-import { ServiceContext } from '../../contexts/ServiceContext';
+// import { AccountContext } from '#contexts/AccountContext';
+import { ReverbParamsContext } from '#app/contexts/ReverbParamsContext';
+// import { ServiceContext } from '../../contexts/ServiceContext';
 import CanonicalATIAnalytics from './canonical';
 import AmpATIAnalytics from './amp';
 import AmpGeo from '../../legacy/components/AmpGeo';
 import { ATIProps } from './types';
-import buildReverbParams from './params';
+// import buildReverbParams from './params';
 
 const ATIAnalytics = ({ atiData = {} }: ATIProps) => {
   const requestContext = use(RequestContext);
-  const serviceContext = use(ServiceContext);
+  // const serviceContext = use(ServiceContext);
   const { isAmp } = requestContext;
-  const { isSignedIn, hashedUserId: hashedId } = use(AccountContext);
+  // const { isSignedIn, hashedUserId: hashedId } = use(AccountContext);
 
-  const reverbParams = buildReverbParams({
-    requestContext,
-    serviceContext,
-    atiData,
-    isSignedIn,
-    hashedId,
-  });
+  const reverbParams = use(ReverbParamsContext);
+
+  // const reverbParams = buildReverbParams({
+  //   requestContext,
+  //   serviceContext,
+  //   atiData,
+  //   isSignedIn,
+  //   hashedId,
+  // });
 
   return isAmp ? (
     <>
