ae6c6ebc13f64af6037382467a55c2198bd7db1b
“LilyL0u” <lily.higham@bbc.co.uk>
2026-06-12 15:38:24 +0100

    lint

--- src/app/legacy/containers/PageHandlers/withOptimizelyProvider/index.tsx
+++ src/app/legacy/containers/PageHandlers/withOptimizelyProvider/index.tsx
@@ -4,7 +4,10 @@ import {
   OptimizelyProvider,
   setLogger,
 } from '@optimizely/react-sdk';
-import { NOTIFICATION_TYPES, ListenerPayload } from '@optimizely/optimizely-sdk';
+import {
+  NOTIFICATION_TYPES,
+  ListenerPayload,
+} from '@optimizely/optimizely-sdk';
 import Cookie from 'js-cookie';
 import isLive from '#lib/utilities/isLive';
 import onClient from '#lib/utilities/onClient';
