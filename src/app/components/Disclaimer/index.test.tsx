8ed13ee78e8873fc6a61213f5dcd92b4f7d11206
jinidev <jinimol.joseph.ext@bbc.co.uk>
2026-06-15 12:27:48 +0300

    More test suites fix

--- src/app/components/Disclaimer/index.test.tsx
+++ src/app/components/Disclaimer/index.test.tsx
@@ -5,17 +5,7 @@ import DisclaimerComponent from '.';
 
 const DISCLAIMER_FIXTURE = {
   para1: 'Приложение Русской службы BBC News доступно для ',
-  para2: {
-    text: 'IOS',
-    url: 'https://apps.apple.com/us/app/%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D0%B8-%D0%B1%D0%B8-%D0%B1%D0%B8-%D1%81%D0%B8/id504278066',
-    isExternal: true,
-  },
   para3: ' и ',
-  para4: {
-    text: 'Android',
-    url: 'https://play.google.com/store/apps/details?id=uk.co.bbc.russian',
-    isExternal: false,
-  },
   para5: '. Вы можете также подписаться на наш канал в ',
   para6: {
     text: 'Telegram',
@@ -59,13 +49,11 @@ describe('Disclaimer Component', () => {
         'Приложение Русской службы BBC News доступно для и . Вы можете также подписаться на наш канал в .',
       ),
     ).toBeInTheDocument();
-    expect(getByText('IOS')).toBeInTheDocument();
-    expect(getByText('Android')).toBeInTheDocument();
     expect(getByText('Telegram')).toBeInTheDocument();
   });
   it('should render links correctly', () => {
     const { getAllByRole } = renderComponent();
-    expect(getAllByRole('link').length).toBe(3);
+    expect(getAllByRole('link').length).toBe(1);
   });
   it('should not render the disclaimer when the disclaimer toggle is not enabled', () => {
     const { container } = renderComponent({ enabled: false });
@@ -83,6 +71,6 @@ describe('Disclaimer Component', () => {
   });
   it('should render links with external label if links are external', () => {
     const { getByLabelText } = renderComponent();
-    expect(getByLabelText('IOS, внешняя')).toBeInTheDocument();
+    expect(getByLabelText('Telegram, внешняя')).toBeInTheDocument();
   });
 });
