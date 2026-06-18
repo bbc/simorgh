8545644b2c87fda84631f19ddfa1b4777bc5f625
alex-magana <alex.magana@andela.com>
2026-06-18 16:51:40 +0300

    Order variables

--- src/app/contexts/ReverbParamsContext/getEnrichedMostReadATIData/index.tsx
+++ src/app/contexts/ReverbParamsContext/getEnrichedMostReadATIData/index.tsx
@@ -1,10 +1,10 @@
 export default ({ pageMetadata, serviceContext }) => {
-  const { atiAnalytics: atiData = {} } = pageMetadata ?? {};
-
   const {
     brandName,
     mostRead: { header },
   } = serviceContext;
+
+  const { atiAnalytics: atiData = {} } = pageMetadata ?? {};
 
   const enrichedData = { ...atiData, pageTitle: `${header} - ${brandName}` };
 
