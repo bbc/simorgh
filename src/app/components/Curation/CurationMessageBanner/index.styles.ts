d593507bc77f9353127dee9548dc37c9ee2243ad
HarveyPeachey <harvey.peachey@bbc.co.uk>
2026-06-22 09:46:38 +0100

    remove unused styles

--- src/app/components/Curation/CurationMessageBanner/index.styles.ts
+++ src/app/components/Curation/CurationMessageBanner/index.styles.ts
@@ -6,24 +6,10 @@ const styles = {
       paddingTop: '2rem',
       paddingBottom: '2rem',
     }),
-  textWrap: ({ mq }: Theme) =>
-    css({
-      [mq.GROUP_3_ONLY]: {
-        width: '66%',
-      },
-      [mq.GROUP_4_MIN_WIDTH]: {
-        width: '75%',
-      },
-    }),
   heading: ({ palette }: Theme) =>
     css({
       paddingTop: '1.5rem',
       paddingBottom: '0.5rem',
-      color: palette.WHITE,
-    }),
-  paragraph: ({ palette }: Theme) =>
-    css({
-      paddingBottom: '1rem',
       color: palette.WHITE,
     }),
 };
