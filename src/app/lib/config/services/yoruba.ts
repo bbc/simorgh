1caffe8460516e9339c2651caa9a3a700291a28a
hotinglok <hotinglok@gmail.com>
2026-06-26 11:13:29 +0100

    Add yoruba translation

--- src/app/lib/config/services/yoruba.ts
+++ src/app/lib/config/services/yoruba.ts
@@ -277,6 +277,7 @@ export const service: DefaultServiceConfig = {
       featuresAnalysisTitle: 'Ìwádìí kíkún lóríi kókó ìròyìn ',
       latestMediaTitle: 'Èyí tí ó ṣẹ̀ṣẹ̀dé',
       sport: {
+        keyEventsTitle: 'Àwọn ìṣẹ̀lẹ̀ pàtàkì',
         matchSummary: 'Akopọ Ere',
         assists: 'Iranlọwọ',
         penalties: 'Penariti',
