236c09058c5b9488e381885333b32be95684406e
hotinglok <hotinglok@gmail.com>
2026-06-26 11:11:54 +0100

    Add romania translation

--- src/app/lib/config/services/romania.ts
+++ src/app/lib/config/services/romania.ts
@@ -341,6 +341,7 @@ export const service: DefaultServiceConfig = {
         closedDescription: 'Campania s-a încheiat la data {{date}}.',
       },
       sport: {
+        keyEventsTitle: 'Evenimente cheie',
         matchSummary: 'Rezumatul meciului',
         worldCupTeamNames: {
           algeria: 'Algeria',
