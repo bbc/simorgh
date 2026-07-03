b198150050482edb5d5d8a6fed645c0f7b03e23c
Louis Matsika <louis.matsika@bbc.co.uk>
2026-07-03 14:33:29 +0100

    Friday July 3rd

--- src/app/components/3d/3dComponents/Globe/scene3D.tsx
+++ src/app/components/3d/3dComponents/Globe/scene3D.tsx
@@ -1,16 +1,13 @@
-// import { jsx } from '@emotion/react';
-// import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { Globe } from '#app/components/3d/3dModels/Globe';
 import styles from './index.styles';
 
-/* eslint-disable react/no-unknown-property */
 const Scene3D = () => (
   <div css={styles.canvasContainer}>
     <Canvas>
       <OrbitControls />
-      <Globe />
+      <Globe position={[0, 0, 0]} />
     </Canvas>
   </div>
 );
