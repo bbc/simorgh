e56ede849cdd8c54b1fdad04172c1f71733300bd
alex-magana <alex.magana@andela.com>
2026-07-10 19:10:29 +0300

    Distinguish lite and opera mini

--- src/app/components/ATIAnalytics/canonical/index.tsx
+++ src/app/components/ATIAnalytics/canonical/index.tsx
@@ -17,8 +17,11 @@ import { ATIAnalyticsProps } from '../types';
 import getNoScriptTrackingPixelUrl from './getNoScriptTrackingPixelUrl';
 import sendPageViewBeaconOperaMini from './sendPageViewBeaconOperaMini';
 
-const addCustomX3 = url =>
-  url.replace(/x3=\[(.*)\]&x4/, 'x3=[$1-inline-js]&x4');
+const addCustomX3Lite = url =>
+  url.replace(/x3=\[(.*)\]&x4/, 'x3=[$1-inline-js-lite]&x4');
+
+const addCustomX3OperaMini = url =>
+  url.replace(/x3=\[(.*)\]&x4/, 'x3=[$1-inline-js-opera-mini]&x4');
 
 const renderNoScriptTrackingPixel = ({ reverbParams }: ATIAnalyticsProps) => {
   return (
@@ -56,10 +59,10 @@ const CanonicalATIAnalytics = ({ reverbParams }: ATIAnalyticsProps) => {
     if (!isOperaProxy()) sendBeacon(reverbBeaconConfig);
   }, [reverbBeaconConfig]);
 
-  const liteSiteReverbURL = addCustomX3(
+  const liteSiteReverbURL = addCustomX3Lite(
     reverbUrlHelper.getLitePageViewUrl(reverbParams),
   );
-  const operaMiniPageViewReverbURL = addCustomX3(
+  const operaMiniPageViewReverbURL = addCustomX3OperaMini(
     reverbUrlHelper.getOperaMiniPageViewUrl(reverbParams),
   );
 
