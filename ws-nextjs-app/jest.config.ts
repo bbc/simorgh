597c4443d958b9edf7762f99ac841347879cafd9
Shayne Ahchoon <shayne.ahchoon@bbc.co.uk>
2026-07-16 11:51:41 +0100

    WS-2944: Use paths, not aliases

--- ws-nextjs-app/jest.config.ts
+++ ws-nextjs-app/jest.config.ts
@@ -1,14 +1,13 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-relative-packages */
-// import { readFileSync } from 'fs';
+import { readFileSync } from 'fs';
 import { pathsToModuleNameMapper } from 'ts-jest';
 import type { Config } from 'jest';
 import nextJest from 'next/jest.js';
-import { compilerOptions } from './tsconfig.json';
 
-// const { compilerOptions } = JSON.parse(
-//   readFileSync(new URL('../tsconfig.json', import.meta.url), 'utf8'),
-// );
+const { compilerOptions } = JSON.parse(
+  readFileSync(new URL('./tsconfig.json', import.meta.url), 'utf8'),
+);
 
 const { ...compilerOptionsPaths } = compilerOptions.paths;
 
