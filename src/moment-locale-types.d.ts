e0d62201544afcb85b5e305ecf72d07654d6f48b
victranfield <catherine.v.tranfield@bbc.co.uk>
2026-07-16 09:58:20 +0100

    added module declarations

--- src/moment-locale-types.d.ts
+++ src/moment-locale-types.d.ts
@@ -1,4 +1,6 @@
 // TypeScript 6 (TS2882) requires type declarations for side-effect imports of
-// module subpaths. Moment locale files are imported purely for their side effects
-// (registering locale data) and have no exports, so we declare them here.
+// module subpaths. These modules are imported purely for their side effects
+// (registering locale/timezone data) and have no exports.
 declare module 'moment/locale/*';
+declare module '#psammead/moment-timezone-include/tz/*';
+declare module '#psammead/psammead-locales/moment/*';
