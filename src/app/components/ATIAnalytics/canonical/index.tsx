004e72fef00c589391481e8e9d6969458f798f0a
alex-magana <alex.magana@andela.com>
2026-07-20 21:51:33 +0300

    Add static tracking scripts at a high level

--- src/app/components/ATIAnalytics/canonical/index.tsx
+++ src/app/components/ATIAnalytics/canonical/index.tsx
@@ -87,7 +87,7 @@ const CanonicalATIAnalytics = ({ reverbParams }: ATIAnalyticsProps) => {
 
   return (
     <>
-      {addScript({ script: addSendStaticBeaconToWindow, nonce })}
+      {/* {addScript({ script: addSendStaticBeaconToWindow, nonce })} */}
       {isLite &&
         addScript({
           script: sendPageViewBeaconLite,
