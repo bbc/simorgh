48971724b3f7e08c13d5be9fa3d2cf0b567899c7
Nabeel Khan <nabeel.khan@bbc.co.uk>
2026-07-21 14:34:00 +0100

    Updated snapshots and fixed linting issues

--- src/app/components/Navigation/index.tsx
+++ src/app/components/Navigation/index.tsx
@@ -196,7 +196,7 @@ const getActiveTopIndex = ({
 
   // All other page types (article, topic, home, live, etc.) default to Home (index 0).
   return 0;
-};;
+};
 
 type NavigationContainerProps = {
   navItems?: Navigation[];
