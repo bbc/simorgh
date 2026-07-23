20ae7737fa54b678c953c7595ba742c137eeb0ac
alex-magana <alex.magana@andela.com>
2026-07-23 17:10:06 +0300

    Clean up sendPageViewBeaconOperaMini module

--- src/app/components/ATIAnalytics/canonical/sendPageViewBeaconOperaMini/index.ts
+++ src/app/components/ATIAnalytics/canonical/sendPageViewBeaconOperaMini/index.ts
@@ -1,22 +1,9 @@
-// import isOperaProxy from '#app/lib/utilities/isOperaProxy';
-
-// export default (atiPageViewUrlString: string) => `
-//     if (${isOperaProxy.toString()}() && !Boolean(window.hasOperaMiniScriptRan)) {
-//       window.hasOperaMiniScriptRan = true;
-
-//       var atiPageViewUrl = "${atiPageViewUrlString}";
-//       atiPageViewUrl += document.referrer ? "&ref=" + document.referrer : '';
-
-//       window.sendStaticBeacon(atiPageViewUrl);
-//     }
-// `;
-
 type OperaMiniWindow = Window & {
   hasOperaMiniScriptRan?: boolean;
 };
 
 // eslint-disable-next-line func-names
-const sendPageViewBeaconOperaMini = function (
+export default function (
   atiPageViewUrlString: string,
   isOperaProxyFn: () => boolean,
 ) {
@@ -30,6 +17,4 @@ const sendPageViewBeaconOperaMini = function (
 
     window.sendStaticBeacon(atiPageViewUrl);
   }
-};
-
-export default sendPageViewBeaconOperaMini;
+}
