f966afc4db51aad251102d8f71b186afa47af9b1
Louise Archibald <louise.archibald@bbc.co.uk>
2026-08-03 13:35:52 +0100

    create repositionCountryTopic helper

---
+++ src/app/components/TopicDiscovery/RepositionCountryTopic/index.ts
@@ -0,0 +1,22 @@
+import { ExtractedTopic } from '..';
+
+const COUNTRY_TOPIC_TARGET_INDEX = 2;
+
+const repositionCountryTopic = (
+  topics: ExtractedTopic[],
+  countryTopicIdToReorder: string | null,
+): ExtractedTopic[] => {
+  if (!countryTopicIdToReorder || topics.length === 0) return topics;
+
+  if (topics[0].topicId !== countryTopicIdToReorder) return topics;
+
+  const reorderedTopics = [...topics];
+  const [countryTopic] = reorderedTopics.splice(0, 1);
+
+  const insertAt = Math.min(COUNTRY_TOPIC_TARGET_INDEX, reorderedTopics.length);
+  reorderedTopics.splice(insertAt, 0, countryTopic);
+
+  return reorderedTopics;
+};
+
+export default repositionCountryTopic;
