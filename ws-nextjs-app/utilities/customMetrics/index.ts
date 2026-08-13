72fed8745ea3a86ad2be403b73d7daddf424111f
Shayne Ahchoon <shayne.ahchoon@bbc.co.uk>
2026-08-13 11:37:50 +0100

    WS-3069: Add additional metric for just status code

--- ws-nextjs-app/utilities/customMetrics/index.ts
+++ ws-nextjs-app/utilities/customMetrics/index.ts
@@ -9,39 +9,19 @@ export type Params = {
   requestUrl: string;
 };
 
-const sendPageTypeMetric = metricScope(
+export type SendIndividualParams = {
+  metricName: string;
+  requestUrl: string;
+  dimension: Record<string, string>;
+};
+
+const sendIndividualMetric = metricScope(
   metrics =>
-    async ({
-      metricName,
-      statusCode = 'Unknown',
-      pageType = 'Unknown',
-      requestUrl,
-    }: Params) => {
+    async ({ metricName, dimension, requestUrl }: SendIndividualParams) => {
       metrics.setNamespace('Simorgh/Server');
 
       // Specifies the metric dimensions, each dimension will counted and billed as a custom unique metric
-      metrics.putDimensions({
-        PageType: pageType,
-        StatusCode: statusCode.toString(),
-      });
-
-      // Specifies the metric name and the value to increment it by e.g. Non_200_Response +1
-      metrics.putMetric(metricName, 1, Unit.Count);
-
-      // Custom properties are included in the custom metric data but are not charged
-      metrics.setProperty('URL', requestUrl);
-    },
-);
-
-const sendStatusCodeMetric = metricScope(
-  metrics =>
-    async ({ metricName, statusCode = 'Unknown', requestUrl }: Params) => {
-      metrics.setNamespace('Simorgh/Server');
-
-      // Specifies the metric dimensions, each dimension will counted and billed as a custom unique metric
-      metrics.putDimensions({
-        StatusCode: statusCode.toString(),
-      });
+      metrics.putDimensions(dimension);
 
       // Specifies the metric name and the value to increment it by e.g. Non_200_Response +1
       metrics.putMetric(metricName, 1, Unit.Count);
@@ -53,20 +33,24 @@ const sendStatusCodeMetric = metricScope(
 
 const sendMetrics = async ({
   metricName,
-  statusCode,
-  pageType,
+  statusCode = 'Unknown',
+  pageType = 'Unknown',
   requestUrl,
 }: Params) => {
-  sendPageTypeMetric({
+  sendIndividualMetric({
     metricName,
-    statusCode,
-    pageType,
+    dimension: {
+      PageType: pageType,
+      StatusCode: statusCode.toString(),
+    },
     requestUrl,
   });
 
-  sendStatusCodeMetric({
+  sendIndividualMetric({
     metricName,
-    statusCode,
+    dimension: {
+      StatusCode: statusCode.toString(),
+    },
     requestUrl,
   });
 };
