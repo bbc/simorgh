83a481dd873b4d9aeb853c93e0516b01f025a284
Shayne Ahchoon <shayne.ahchoon@bbc.co.uk>
2026-08-13 11:31:09 +0100

    WS-3069: Add additional metric for just status code

--- ws-nextjs-app/utilities/customMetrics/index.ts
+++ ws-nextjs-app/utilities/customMetrics/index.ts
@@ -5,16 +5,16 @@ import onEnvironment from '../onEnvironment';
 export type Params = {
   metricName: string;
   statusCode?: number | string;
-  pageType: PageTypes;
+  pageType?: PageTypes;
   requestUrl: string;
 };
 
-const sendMetric = metricScope(
+const sendPageTypeMetric = metricScope(
   metrics =>
     async ({
       metricName,
       statusCode = 'Unknown',
-      pageType,
+      pageType = 'Unknown',
       requestUrl,
     }: Params) => {
       metrics.setNamespace('Simorgh/Server');
@@ -33,7 +33,45 @@ const sendMetric = metricScope(
     },
 );
 
+const sendStatusCodeMetric = metricScope(
+  metrics =>
+    async ({ metricName, statusCode = 'Unknown', requestUrl }: Params) => {
+      metrics.setNamespace('Simorgh/Server');
+
+      // Specifies the metric dimensions, each dimension will counted and billed as a custom unique metric
+      metrics.putDimensions({
+        StatusCode: statusCode.toString(),
+      });
+
+      // Specifies the metric name and the value to increment it by e.g. Non_200_Response +1
+      metrics.putMetric(metricName, 1, Unit.Count);
+
+      // Custom properties are included in the custom metric data but are not charged
+      metrics.setProperty('URL', requestUrl);
+    },
+);
+
+const sendMetrics = async ({
+  metricName,
+  statusCode,
+  pageType,
+  requestUrl,
+}: Params) => {
+  sendPageTypeMetric({
+    metricName,
+    statusCode,
+    pageType,
+    requestUrl,
+  });
+
+  sendStatusCodeMetric({
+    metricName,
+    statusCode,
+    requestUrl,
+  });
+};
+
 const sendCustomMetric = (params: Params) =>
-  onEnvironment(['test', 'live'], params)(sendMetric);
+  onEnvironment(['test', 'live'], params)(sendMetrics);
 
 export default sendCustomMetric;
