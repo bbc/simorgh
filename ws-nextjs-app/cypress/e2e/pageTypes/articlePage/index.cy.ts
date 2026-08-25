675a6f8ed1d5b98b1804d4d9932491608a2a1fee
alex-magana <alex.magana@andela.com>
2026-08-25 13:56:20 +0300

    Disable Tamil watch and listen e2es

--- ws-nextjs-app/cypress/e2e/pageTypes/articlePage/index.cy.ts
+++ ws-nextjs-app/cypress/e2e/pageTypes/articlePage/index.cy.ts
@@ -302,18 +302,18 @@ const nonSmokeCanonicalTestSuites = [
     service: 'tamil',
     tests: [...canonicalTests],
   },
-  {
-    path: '/tamil/watch/cqxdxv159rlo',
-    runforEnv: ['local', 'live'],
-    service: 'tamil',
-    tests: [...canonicalTests],
-  },
-  {
-    path: '/tamil/listen/cqxdxv159rlo',
-    runforEnv: ['local', 'live'],
-    service: 'tamil',
-    tests: [...canonicalTests],
-  },
+  // {
+  //   path: '/tamil/watch/cqxdxv159rlo',
+  //   runforEnv: ['local', 'live'],
+  //   service: 'tamil',
+  //   tests: [...canonicalTests],
+  // },
+  // {
+  //   path: '/tamil/listen/cqxdxv159rlo',
+  //   runforEnv: ['local', 'live'],
+  //   service: 'tamil',
+  //   tests: [...canonicalTests],
+  // },
   {
     path: '/ukrainian/articles/c8zv0eed9gko',
     runforEnv: ['live'],
