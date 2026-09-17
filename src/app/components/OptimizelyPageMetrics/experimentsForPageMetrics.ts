a96b0e91f77e4eea18ab65a06ebdfec3ac672a04
Pedram Valiani <pedram.valiani@bbc.co.uk>
2026-09-17 08:31:31 +0100

    add experiments for page metrics changes

--- src/app/components/OptimizelyPageMetrics/experimentsForPageMetrics.ts
+++ src/app/components/OptimizelyPageMetrics/experimentsForPageMetrics.ts
@@ -1,4 +1,5 @@
 import { PageTypes } from '#app/models/types/global';
+import { HOMEPAGE_RELATED_TOPIC_EXPERIMENT } from '#app/lib/experiments/homepageRelatedTopicPromos';
 import {
   ARTICLE_PAGE,
   HOME_PAGE,
@@ -26,7 +27,11 @@ const experimentsForPageMetrics: ExperimentsForPageTypeMetrics = [
   },
   {
     pageType: HOME_PAGE,
-    activeExperiments: ['test_page_views_aa_4'],
+    activeExperiments: [
+      'test_page_views_aa_4',
+      // experiment: newswb_ws_homepage_related_topic_promos
+      HOMEPAGE_RELATED_TOPIC_EXPERIMENT,
+    ],
   },
 ];
 
