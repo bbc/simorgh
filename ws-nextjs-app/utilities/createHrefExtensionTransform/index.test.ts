e0960b2fe989826f1b77b57bfa56fde4b29e6120
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2026-09-22 17:03:55 +0100

    WS-3180: Refactors to reduce duplication between app and lite logic

---
+++ ws-nextjs-app/utilities/createHrefExtensionTransform/index.test.ts
@@ -0,0 +1,40 @@
+import createHrefExtensionTransform from '.';
+
+describe('createHrefExtensionTransform', () => {
+  it('should append the given extension to eligible hrefs by default', () => {
+    const transform = createHrefExtensionTransform({
+      extension: 'lite',
+    });
+
+    const html = '<a href="/news">News</a>';
+
+    expect(transform(html)).toEqual('<a href="/news.lite">News</a>');
+  });
+
+  it('should only append the extension when isEligiblePath returns true', () => {
+    const transform = createHrefExtensionTransform({
+      extension: 'app',
+      isEligiblePath: pathname => pathname.includes('/articles/'),
+    });
+
+    const html = `
+      <a href="/pidgin/articles/c0000000000o">Article</a>
+      <a href="/pidgin/live/c0000000000o">Live</a>
+    `;
+
+    expect(transform(html)).toEqual(`
+      <a href="/pidgin/articles/c0000000000o.app">Article</a>
+      <a href="/pidgin/live/c0000000000o">Live</a>
+    `);
+  });
+
+  //   it('should respect a custom ignoreAttribute name', () => {
+  //     const transform = createHrefExtensionTransform({
+  //       extension: 'lite',
+  //     });
+
+  //     const html = '<a href="/news" data-ignore-lite="true">News</a>';
+
+  //     expect(transform(html)).toEqual(html);
+  //   });
+});
