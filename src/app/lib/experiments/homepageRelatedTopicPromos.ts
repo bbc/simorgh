741b21dbb1d74d5c5d2e0a7c68279c107c42b519
Pedram Valiani <pedram.valiani@bbc.co.uk>
2026-09-23 10:53:49 +0100

    add additional services to list

--- src/app/lib/experiments/homepageRelatedTopicPromos.ts
+++ src/app/lib/experiments/homepageRelatedTopicPromos.ts
@@ -1,8 +1,22 @@
 import type { Summary } from '#app/models/types/curationData';
+import type { Services } from '#app/models/types/global';
 
 // experiment: newswb_ws_homepage_related_topic_promos
 export const HOMEPAGE_RELATED_TOPIC_EXPERIMENT =
   'newswb_ws_homepage_related_topic_promos';
+
+export const HOMEPAGE_RELATED_TOPIC_SERVICES: Services[] = [
+  'afaanoromoo',
+  'afrique',
+  'amharic',
+  'gahuza',
+  'igbo',
+  'pidgin',
+  'somali',
+  'swahili',
+  'tigrinya',
+  'yoruba',
+];
 
 export const HOMEPAGE_ARTICLE_PROMO_CLICK_EVENT =
   'homepage-article-promo-clicks';
